72ef7bbb933767dd28c5141e11d75cb14f8a0f05
reallydontask <agoney.garcia-deniz@digital.justice.gov.uk>
2021-05-25 13:24:49 +0100

    Public ECR Only, we have to start somewhere

---
+++ terraform/environments/ops-engineering/main.tf
@@ -0,0 +1,15 @@
+resource "aws_ecrpublic_repository" "spike" {
+  repository_name = "opsengspike"
+
+  catalog_data {
+    about_text        = "This is an MOJ production of AWS ECR brought to you by ops eng"
+    architectures     = ["x86", "x86-64"]
+    description       = "This will like just have naff images that I create"
+    operating_systems = ["Linux"]
+    usage_text        = "Usage Carefully, Here be dragons."
+  }
+}
+
+output "repository_uri" {
+    value = aws_ecrpublic_repository.spike.repository_uri
+}
