2268ab2554e574dfbfb9529eae82f26117a58cf0
David Elliott <david.elliott@digital.justice.gov.uk>
2021-06-14 08:45:46 +0100

    Fix missed provider update

--- terraform/environments/sprinkler/providers.tf
+++ terraform/environments/sprinkler/providers.tf
@@ -22,12 +22,12 @@ provider "aws" {
   }
 }
 
-# AWS provider for core-vpc-production, to share VPCs into this account
+# AWS provider for network services to enable dns entries for certificate validation to be created
 provider "aws" {
   alias  = "core-network-services"
   region = "eu-west-2"
 
   assume_role {
-    role_arn = "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/ModernisationPlatformAccess"
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/modify-dns-records"
   }
 }
