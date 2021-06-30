704305bcb3516ea2ea7c21b191d9d5adb9624221
zuriguardiola <zuri.guardiola@digital.justice.gov.uk>
2021-06-30 15:57:50 +0100

    Add secrets manager policy to ecs task role

--- terraform/modules/windows-ecs/main.tf
+++ terraform/modules/windows-ecs/main.tf
@@ -308,6 +308,10 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+resource "aws_iam_role_policy_attachment" "ecs_task_secrets_manager" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
 
 ##### ECS autoscaling ##########
 
