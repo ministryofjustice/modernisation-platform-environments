20aa039edc792af45c7743beb09afc4afc1cc8ab
zuriguardiola <zuri.guardiola@digital.justice.gov.uk>
2021-07-16 15:53:51 +0100

    Update ECS module path

--- terraform/environments/performance-hub/main.tf
+++ terraform/environments/performance-hub/main.tf
@@ -137,7 +137,7 @@ data "template_file" "task_definition" {
 
 module "windows-ecs" {
 
-  source = "../../modules/windows-ecs"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ecs"
 
   subnet_set_name      = local.subnet_set_name
   vpc_all              = local.vpc_all
