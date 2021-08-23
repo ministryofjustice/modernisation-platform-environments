2c448dfb98ec1c8ab95a18f8e7ec7e7075216c78
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2021-08-23 11:57:15 +0100

    some tfsec, checkov fixes

--- terraform/environments/nomis/main.tf
+++ terraform/environments/nomis/main.tf
@@ -12,10 +12,10 @@ data "aws_subnet_ids" "local_account" {
   vpc_id = data.aws_vpc.shared_vpc.id
 }
 
-data "aws_subnet" "local_account" {
-  for_each = data.aws_subnet_ids.local_account.ids
-  id       = each.value
-}
+# data "aws_subnet" "local_account" {
+#   for_each = data.aws_subnet_ids.local_account.ids
+#   id       = each.value
+# }
 
 # get shared subnet-set private (az (a) subnet)
 data "aws_subnet" "private_az_a" {
@@ -25,39 +25,37 @@ data "aws_subnet" "private_az_a" {
   }
 }
 
-# get bastion_linux security group
-# data "aws_security_group" "bastion_linux" {
-#   provider = aws.share-host
-
-#   tags = {
-#     Name = "bastion-linux-${local.application_name}"
-#   }
-# }
-
 # Security Groups
 resource "aws_security_group" "weblogic_server" {
   description = "Configure weblogic access - ingress should be only from Bastion"
   name        = "weblogic-server-${local.application_name}"
   vpc_id      = data.aws_vpc.shared_vpc.id
 
+  ingress = [
+    {
+      description = "SSH from Bastion"
+      from_port   = "22"
+      to_port     = "22"
+      protocol    = "TCP"
+      cidr_blocks = ["${module.bastion_linux.bastion_private_ip}/32"]
+    }
+  ]
+  egress = [
+    {
+      description = "allow all"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
   tags = merge(
     local.tags,
     {
       Name = "weblogic-server-${local.application_name}"
     }
   )
-}
-
-
-resource "aws_security_group_rule" "weblogic_server_egress_1" {
-  security_group_id = aws_security_group.weblogic_server.id
-
-  description = "weblogic_server_ingress_of_SSH_from_Bastion"
-  type        = "ingress"
-  from_port   = "22"
-  to_port     = "22"
-  protocol    = "TCP"
-  cidr_blocks = ["${module.bastion_linux.bastion_private_ip}/32"]
 }
 
 ##### EC2 ####
@@ -79,16 +77,20 @@ data "aws_ami" "weblogic_image" {
 
 resource "aws_instance" "weblogic_server" {
   instance_type = "t3.micro"
-
   ami                         = data.aws_ami.weblogic_image.id
   associate_public_ip_address = false
   # iam_instance_profile        = aws_iam_instance_profile.bastion_profile.id
   monitoring             = false
   vpc_security_group_ids = [aws_security_group.weblogic_server.id]
   subnet_id              = data.aws_subnet.private_az_a.id
-
   # user_data = base64encode(data.template_file.user_data.rendered)
-
+  ebs_optimized = true
+  metadata_options {
+    http_tokens = "required"
+  }
+  root_block_device {
+    encrypted     = true
+  }
   tags = merge(
     local.tags,
     {
