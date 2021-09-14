20c9100ea1c7060edb88d1f52377ad062e493ef3
Ynyr Williams <ynyrwilliams@MacBook-Pro.local>
2021-09-14 15:44:27 +0100

    add assume role to eu-west-1

--- terraform/environments/nomis/providers.tf
+++ terraform/environments/nomis/providers.tf
@@ -1,9 +1,6 @@
 # AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
 provider "aws" {
   region = "eu-west-2"
-  assume_role {
-    role_arn = "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/MemberInfrastructureAccess"
-  }
 }
 
 # AWS provider for the Modernisation Platform, to get things from there if required
@@ -34,8 +31,7 @@ provider "aws" {
 provider "aws" {
   alias  = "bucket-replication"
   region = "eu-west-1"
-}
-# AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
-provider "aws" {
-  region = "eu-west-2"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/MemberInfrastructureAccess"
+  }
 }
