48e5979523cc3323e1094765247098531a6f8303
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2021-09-17 16:29:04 +0100

    amend packer permissions to enable connection with session manager and reduce scope of other permissions where possible

--- terraform/environments/nomis/packer.tf
+++ terraform/environments/nomis/packer.tf
@@ -10,6 +10,9 @@ resource "aws_iam_user" "packer_member_user" {
   name = "packer-member-user"
 }
 
+resource "aws_iam_access_key" "packer_member_user_key" {
+  user = aws_iam_user.packer_member_user.name
+}
 resource "aws_iam_group" "packer_member_group" {
   name = "packer-member-group"
 }
@@ -36,7 +39,7 @@ resource "aws_iam_group_policy_attachment" "aws_config_attach" {
   policy_arn = aws_iam_policy.policy.arn
 }
 
-resource "aws_iam_group_membership" "packer-member" {
+resource "aws_iam_group_membership" "packer_member" {
   name = "packer-member-group-membership"
 
   users = [
@@ -78,8 +81,8 @@ resource "aws_iam_role_policy" "packer" {
   role = aws_iam_role.packer.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
+    "Version" : "2012-10-17"
+    "Statement" : [
       {
         "Effect" : "Allow",
         "Action" : [
@@ -87,10 +90,11 @@ resource "aws_iam_role_policy" "packer" {
           "ec2:CopyImage",
           "ec2:CreateImage",
           "ec2:CreateKeypair",
-          "ec2:CreateSecurityGroup",
           "ec2:CreateSnapshot",
           "ec2:CreateTags",
           "ec2:CreateVolume",
+          "ec2:DeleteSnapshot",  # unfortunately Packer does not tag intermediate snapshots it creates
+          "ec2:DeregisterImage", # unfortunately Packer does not tag intermediate images it creates
           "ec2:DescribeImageAttribute",
           "ec2:DescribeImages",
           "ec2:DescribeInstances",
@@ -103,7 +107,7 @@ resource "aws_iam_role_policy" "packer" {
           "ec2:DescribeVolumes",
           "ec2:GetPasswordData",
           "ec2:RegisterImage",
-          "ec2:RunInstances",
+          "ec2:RunInstances"
         ],
         "Resource" : "*"
       },
@@ -111,17 +115,13 @@ resource "aws_iam_role_policy" "packer" {
         "Effect" : "Allow",
         "Action" : [
           "ec2:AttachVolume",
-          "ec2:DeleteKeyPair",
-          "ec2:DeleteSecurityGroup",
-          "ec2:DeleteSnapshot",
           "ec2:DeleteVolume",
-          "ec2:DeregisterImage",
           "ec2:DetachVolume",
           "ec2:ModifyImageAttribute",
           "ec2:ModifyInstanceAttribute",
           "ec2:ModifySnapshotAttribute",
           "ec2:StopInstances",
-          "ec2:TerminateInstances"
+          "ec2:TerminateInstances",
         ],
         "Resource" : "*",
         "Condition" : {
@@ -129,6 +129,53 @@ resource "aws_iam_role_policy" "packer" {
             "ec2:ResourceTag/creator" : "Packer"
           }
         }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "ssm:StartSession",
+        "Resource" : "arn:aws:ec2:eu-west-2:612659970365:instance/*",
+        "Condition" : {
+          "StringEquals" : {
+            "aws:ResourceTag/creator" : "Packer"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "ssm:StartSession",
+        "Resource" : "arn:aws:ssm:eu-west-2::document/AWS-StartPortForwardingSession"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:TerminateSession",
+          "ssm:ResumeSession"
+        ],
+        "Resource" : [
+          "arn:aws:ssm:*:*:session/$${aws:username}-*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DeleteKeyPair"
+        ],
+        "Resource" : "*",
+        "Condition" : {
+          "StringLike" : {
+            "ec2:KeyPairName" : "packer_*"
+          }
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "iam:GetInstanceProfile",
+        "Resource" : "${aws_iam_instance_profile.packer_ssm_profile.arn}"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "iam:PassRole",
+        "Resource" : "${aws_iam_instance_profile.packer_ssm_role.arn}"
       }
     ]
   })
@@ -136,7 +183,7 @@ resource "aws_iam_role_policy" "packer" {
 
 #------------------------------------------------------------------------------
 # Instance profile to be assumed by Packer build instance
-# This is required enable SSH via Systems Manager
+# This is required to enable SSH via Systems Manager
 #------------------------------------------------------------------------------
 
 resource "aws_iam_role" "packer_ssm_role" {
@@ -171,4 +218,24 @@ resource "aws_iam_instance_profile" "packer_ssm_profile" {
   name = "packer_ssm_profile"
   role = aws_iam_role.packer_ssm_role.name
   path = "/"
+}
+
+#------------------------------------------------------------------------------
+# Security Group to be used by Packer.  This is required as there is currently
+# not a simple way to restrict Packer to only allow deleting of security groups
+# it created (it does not tag the security group like other resources)
+#------------------------------------------------------------------------------
+
+resource "aws_security_group" "packer_security_group" {
+  description = "Security Group for Packer builds"
+  name        = "packer-build-${local.application_name}"
+  vpc_id      = data.aws_vpc.shared_vpc.id
+  egress {
+    description      = "allow all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
