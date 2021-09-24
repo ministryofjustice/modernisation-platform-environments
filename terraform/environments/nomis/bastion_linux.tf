826bfffeed6d05c4ca726ae56556d6f878231a94
Ynyr Williams <ynyrwilliams@MacBook-Pro.local>
2021-09-24 10:53:10 +0100

    downgrading bastion to see if it fixes public key issues

--- terraform/environments/nomis/bastion_linux.tf
+++ terraform/environments/nomis/bastion_linux.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "bastion_linux" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v1.0.2"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v1.0.1"
 
   providers = {
     aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
