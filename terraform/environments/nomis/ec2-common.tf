75c5748846bb703d940f71747090ae70be9f9dd5
Ynyr Williams <ynyrwilliams@MacBook-Pro.local>
2021-09-30 13:31:01 +0100

    change name

--- terraform/environments/nomis/ec2-common.tf
+++ terraform/environments/nomis/ec2-common.tf
@@ -8,23 +8,23 @@ resource "aws_iam_role" "ssm_ec2_role" {
   path                 = "/"
   max_session_duration = "3600"
   assume_role_policy = jsonencode(
-{
-  "Version": "2012-10-17",
-  "Statement": [
     {
-      "Effect": "Allow",
-      "Action": ["s3:ListBucket"],
-      "Resource": ["arn:aws:s3:::${module.s3_bucket.default.bucket}"]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Resource": ["arn:aws:s3:::test/*"]
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : ["s3:ListBucket"],
+          "Resource" : ["arn:aws:s3:::${module.s3-bucket.default.bucket}"]
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:GetObject"
+          ],
+          "Resource" : ["arn:aws:s3:::${module.s3-bucket.default.bucket}/*"]
+        }
+      ]
     }
-  ]
-}
   )
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
   tags = merge(
