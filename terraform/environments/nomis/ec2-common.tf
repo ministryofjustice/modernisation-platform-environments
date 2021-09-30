67e2ed1f0b5f36e7f4229f19b08d0dc817de90d2
Ynyr Williams <ynyrwilliams@MacBook-Pro.local>
2021-09-30 13:26:57 +0100

    first commit to allow access to s3

--- terraform/environments/nomis/ec2-common.tf
+++ terraform/environments/nomis/ec2-common.tf
@@ -8,6 +8,49 @@ resource "aws_iam_role" "ssm_ec2_role" {
   path                 = "/"
   max_session_duration = "3600"
   assume_role_policy = jsonencode(
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["arn:aws:s3:::${module.s3_bucket.default.bucket}"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": ["arn:aws:s3:::test/*"]
+    }
+  ]
+}
+  )
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-ec2-role"
+    },
+  )
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "ssm-ec2-profile"
+  role = aws_iam_role.ssm_ec2_role.name
+  path = "/"
+}
+
+#------------------------------------------------------------------------------
+# Instance profile to be assumed by the ec2 instance
+# This is required to allow the ec2 instance to access the s3 bucket
+#------------------------------------------------------------------------------
+
+resource "aws_iam_role" "s3_ec2_role" {
+  name                 = "s3-ec2-role"
+  path                 = "/"
+  max_session_duration = "3600"
+  assume_role_policy = jsonencode(
     {
       "Version" : "2012-10-17",
       "Statement" : [
@@ -26,13 +69,13 @@ resource "aws_iam_role" "ssm_ec2_role" {
   tags = merge(
     local.tags,
     {
-      Name = "ssm-ec2-role"
+      Name = "s3-ec2-role"
     },
   )
 }
 
-resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  name = "ssm-ec2-profile"
-  role = aws_iam_role.ssm_ec2_role.name
+resource "aws_iam_instance_profile" "ec2_s3_profile" {
+  name = "s3-ec2-profile"
+  role = aws_iam_role.s3_ec2_role.name
   path = "/"
 }
