cd881de6693110b884e28b98d60541a0cbe4332a
George Cairns <george.cairns@gmail.com>
2021-10-11 16:21:17 +0100

    add machine back

--- terraform/environments/xhibit-portal/importmachine.tf
+++ terraform/environments/xhibit-portal/importmachine.tf
@@ -1,203 +1,101 @@
-# # # Security Groups
-# # resource "aws_security_group" "importmachine" {
-# #   description = "Configure importmachine access - ingress should be only from Bastion"
-# #   name        = "importmachine-${local.application_name}"
-# #   vpc_id      = local.vpc_id
 
-# #   ingress {
-# #     description = "SSH from Bastion"
-# #     from_port   = 0
-# #     to_port     = "3389"
-# #     protocol    = "TCP"
-# #     cidr_blocks = ["${module.bastion_linux.bastion_private_ip}/32"]
-# #   }
-
-# #   egress {
-# #     description      = "allow all"
-# #     from_port        = 0
-# #     to_port          = 0
-# #     protocol         = "-1"
-# #     cidr_blocks      = ["0.0.0.0/0"]
-# #     ipv6_cidr_blocks = ["::/0"]
-# #   }
-
-# # }
-
-# # data "aws_subnet" "private_az_a" {
-# #   tags = {
-# #     Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-private-${local.region}a"
-# #   }
-# # }
-
-# # resource "aws_instance" "importmachine" {
-# #   instance_type               = "t2.large"
-# #   ami                         = "ami-0a0502ffd782e9b12"
-# #   associate_public_ip_address = false
-# #   # iam_instance_profile        = "ssm-ec2-profile"
-# #   monitoring             = false
-# #   vpc_security_group_ids = [aws_security_group.importmachine.id]
-# #   subnet_id              = data.aws_subnet.private_az_a.id
-# #   ebs_optimized          = true
-
-# #   metadata_options {
-# #     http_tokens   = "required"
-# #     http_endpoint = "enabled"
-# #   }
-
-# #   root_block_device {
-# #     encrypted = true
-# #   }
-
-# # }
-
-# # resource "aws_ebs_volume" "vmimage_disk" {
-# #   availability_zone = "${local.region}a"
-# #   type              = "gp2"
-# #   encrypted         = true
-# #   size              = 6000
-# # }
-
-# # resource "aws_volume_attachment" "vmimage_disk" {
-# #   device_name = "/dev/sde"
-# #   volume_id   = aws_ebs_volume.vmimage_disk.id
-# #   instance_id = aws_instance.importmachine.id
-# # }
+# get shared subnet-set private (az (a) subnet)
+data "aws_subnet" "private_az_a" {
+  # provider = aws.share-host
+  tags = {
+    Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-private-${local.region}a"
+  }
+}
 
 
-# # get shared subnet-set private (az (a) subnet)
-# data "aws_subnet" "private_az_a" {
-#   # provider = aws.share-host
-#   tags = {
-#     Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-private-${local.region}a"
-#   }
-# }
+# Security Groups
+resource "aws_security_group" "importmachine" {
+  description = "Configure importmachine access - ingress should be only from Bastion"
+  name        = "importmachine-${local.application_name}"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    description = "SSH from Bastion"
+    from_port   = 0
+    to_port     = "3389"
+    protocol    = "TCP"
+    cidr_blocks = ["${module.bastion_linux.bastion_private_ip}/32"]
+  }
+
+  egress {
+    description      = "allow all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
 
 
-# # Security Groups
-# resource "aws_security_group" "importmachine" {
-#   description = "Configure importmachine access - ingress should be only from Bastion"
-#   name        = "importmachine-${local.application_name}"
-#   vpc_id      = local.vpc_id
+resource "aws_key_pair" "george" {
+  key_name   = "george"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCt2geFOwgsihu1oAG3RghCqercNTMv1QUgVnJvyllJGllRDbD5cfit5u3yKPB50W5IOgm9p+21epSRYSEL9TwkSNuveI1LK4CFDOurT5QiOSXL/0pFScwhSFbYud3IzbMJ8dEj/hyRm+gnqHbO86CJRBvSvL6j1Wn9S1rTPbfa0VmMehTsD2Wk181TlddIUBMnG+Dd4eeIoi5ivEzfM8jX4NJXzadXG/wTIrsx471tBF7g8TzCcYDMgTQw9oEdR3wugFjfuUDSK/SYXFTUpDOufZefpENcSW9SPDVfzCeM6ludNKxZFqVGAKwc7BFMygAucZjwVgiKxWBDVRcqTmtuM+ujoBh+d/o4RVGTs9V0MSE8YSIqk91U+/PRlL1nXBk0KaLqzB6/EdZZWxkxfhzv+iDrPnvqQd+ayzV0KcbzIP6iCxFn4YDM9jPWBDjIksKhi3TB4XyW446v6ttord0eB6glWFytA1LJ7Y7aiKaOnWa5oW7IbCZtE7PFxp+dmTk= george.cairns@MJ001152"
+}
 
-#   ingress {
-#     description = "SSH from Bastion"
-#     from_port   = 0
-#     to_port     = "3389"
-#     protocol    = "TCP"
-#     cidr_blocks = ["${module.bastion_linux.bastion_private_ip}/32"]
-#   }
-
-#   egress {
-#     description      = "allow all"
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
-
-# }
+resource "aws_instance" "importmachine" {
+  instance_type               = "t3.large"
+  ami                         = "ami-0a0502ffd782e9b12"
+  vpc_security_group_ids      = [aws_security_group.importmachine.id]
+  monitoring                  = false
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  subnet_id                   = data.aws_subnet.private_az_a.id
+  key_name                    = aws_key_pair.george.key_name
 
 
-# ##### EC2 ####
-# # data "aws_ami" "importmachine" {
-# #   most_recent = true
-# #   owners      = ["amazon"]
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
 
-# #   filter {
-# #     name   = "name"
-# #     values = ["Windows_Server-2019-English-Full-Base-*"]
-# #   }
+  root_block_device {
+    encrypted = true
+  }
 
-# #   filter {
-# #     name   = "virtualization-type"
-# #     values = ["hvm"]
-# #   }
-# # }
+  lifecycle {
+    ignore_changes = [
+      # This prevents clobbering the tags of attached EBS volumes. See
+      # [this bug][1] in the AWS provider upstream.
+      #
+      # [1]: https://github.com/terraform-providers/terraform-provider-aws/issues/770
+      volume_tags,
+      #user_data,         # Prevent changes to user_data from destroying existing EC2s
+      root_block_device,
+      # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
+    ]
+  }
 
-# # resource "aws_instance" "importmachine" {
-# #   instance_type               = "t2.large"
-# #   ami                         = "ami-0a0502ffd782e9b12"
-# #   associate_public_ip_address = false
-# #   # iam_instance_profile        = "ssm-ec2-profile"
-# #   monitoring             = false
-# #   vpc_security_group_ids = [aws_security_group.importmachine.id]
-# #   subnet_id              = data.aws_subnet.private_az_a.id
-# #   ebs_optimized          = true
+  tags = merge(
+    local.tags,
+    {
+      Name = "importmachine-${local.application_name}"
+    }
+  )
+}
 
-# #   metadata_options {
-# #     http_tokens   = "required"
-# #     http_endpoint = "enabled"
-# #   }
+resource "aws_ebs_volume" "disk_xvdf" {
+  availability_zone = "${local.region}a"
+  type              = "gp2"
+  encrypted         = true
+  size              = 400
 
-# #   root_block_device {
-# #     encrypted = true
-# #   }
+  tags = merge(
+    local.tags,
+    {
+      Name = "importmachine-${local.application_name}-disk"
+    }
+  )
+}
 
-# # }
-
-# resource "aws_key_pair" "george" {
-#   key_name   = "george"
-#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCiOP+MmVMmmM8FnOwbHiIyw4YiFpkZhfNUjCcg13xp5yYSysqdUGWEeeHZ2U+aRmnaQRMEcYcw7PyMVNBdC90nEiqtXUgvFY/PuH/kRQJTwGNkzXN4ODIvKwvQHpg8l4ed6aKayNYuvTlgZ8VON7DP6VgXvLvahrhTKP8t5/ZKWHYAGH8mYz5rKVMArDmMXaqMFJBeAeiBU6BECFotpbzAznNMF4c1lCWG50z/ZhWrRMzFFiuzUrE1508LS58JKf66a9+L5j5+sxqM14fbkTlgnMksvS3rLxavSZddi5LiCjDT8QBEoTQvdz8uSiO+mxKtxRN1MWec8s25yPBmN1ynw11mBFurd2TdoD6xKRlJiwKXbbt/tx4IzIE9wjWti/3xFuDKqWv1b7O83Es+RPCVd7g1RCPWnQTw0OufauJrhkzOFdeyNlTAcDOGWd7SZLrny6wNzYisjfNHA5FCECNM3DZMsa2xI0OaS9ns2CsD3W5CJxQBgpoNoIT6GjgwBsU= george.cairns@MJ001152"
-# }
-
-# resource "aws_instance" "importmachine" {
-#   instance_type               = "t3.large"
-#   ami                         = "ami-0a0502ffd782e9b12"
-#   vpc_security_group_ids      = [aws_security_group.importmachine.id]
-#   monitoring                  = false
-#   associate_public_ip_address = false
-#   ebs_optimized               = true
-#   subnet_id                   = data.aws_subnet.private_az_a.id
-#   key_name                    = aws_key_pair.george.key_name
-
-
-#   metadata_options {
-#     http_tokens   = "required"
-#     http_endpoint = "enabled"
-#   }
-
-#   root_block_device {
-#     encrypted = true
-#   }
-
-#   lifecycle {
-#     ignore_changes = [
-#       # This prevents clobbering the tags of attached EBS volumes. See
-#       # [this bug][1] in the AWS provider upstream.
-#       #
-#       # [1]: https://github.com/terraform-providers/terraform-provider-aws/issues/770
-#       volume_tags,
-#       #user_data,         # Prevent changes to user_data from destroying existing EC2s
-#       root_block_device,
-#       # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
-#     ]
-#   }
-
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "importmachine-${local.application_name}"
-#     }
-#   )
-# }
-
-# resource "aws_ebs_volume" "disk_xvdf" {
-#   availability_zone = "${local.region}a"
-#   type              = "gp2"
-#   encrypted         = true
-#   size              = 400
-
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "importmachine-${local.application_name}-disk"
-#     }
-#   )
-# }
-
-# resource "aws_volume_attachment" "disk_xvdf" {
-#   device_name = "xvdf"
-#   volume_id   = aws_ebs_volume.disk_xvdf.id
-#   instance_id = aws_instance.importmachine.id
-# }
+resource "aws_volume_attachment" "disk_xvdf" {
+  device_name = "xvdf"
+  volume_id   = aws_ebs_volume.disk_xvdf.id
+  instance_id = aws_instance.importmachine.id
+}
