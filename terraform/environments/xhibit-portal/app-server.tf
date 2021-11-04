8e7211483cbb585e0befff3f1cae9feb972c8fa2
George Cairns <george.cairns@gmail.com>
2021-11-04 02:24:57 +0000

    add app server with new ami in prod

--- terraform/environments/xhibit-portal/app-server.tf
+++ terraform/environments/xhibit-portal/app-server.tf
@@ -1,5 +1,7 @@
+
+
 # Security Groups
-resource "aws_security_group" "domain-controllers" {
+resource "aws_security_group" "app-server" {
   description = "Domain traffic only"
   name        = "domaincontrollers-${local.application_name}"
   vpc_id      = local.vpc_id
@@ -26,10 +28,10 @@ resource "aws_security_group" "domain-controllers" {
 
 
 
-resource "aws_instance" "infra1" {
+resource "aws_instance" "app-server" {
   instance_type               = "t3.small"
-  ami                         = "ami-0a0502ffd782e9b12"
-  vpc_security_group_ids      = [aws_security_group.domain-controllers.id]
+  ami                         = local.application_data.accounts[local.environment].app-ami
+  vpc_security_group_ids      = [aws_security_group.app-server.id]
   monitoring                  = false
   associate_public_ip_address = false
   ebs_optimized               = true
@@ -62,49 +64,7 @@ resource "aws_instance" "infra1" {
   tags = merge(
     local.tags,
     {
-      Name = "infra1-${local.application_name}"
-    }
-  )
-}
-
-
-resource "aws_instance" "infra2" {
-  instance_type               = "t3.small"
-  ami                         = "ami-0a0502ffd782e9b12"
-  vpc_security_group_ids      = [aws_security_group.domain-controllers.id]
-  monitoring                  = false
-  associate_public_ip_address = false
-  ebs_optimized               = true
-  subnet_id                   = data.aws_subnet.private_az_b.id
-  key_name                    = aws_key_pair.george.key_name
-
-
-  metadata_options {
-    http_tokens   = "required"
-    http_endpoint = "enabled"
-  }
-
-  root_block_device {
-    encrypted = true
-  }
-
-  lifecycle {
-    ignore_changes = [
-      # This prevents clobbering the tags of attached EBS volumes. See
-      # [this bug][1] in the AWS provider upstream.
-      #
-      # [1]: https://github.com/terraform-providers/terraform-provider-aws/issues/770
-      volume_tags,
-      #user_data,         # Prevent changes to user_data from destroying existing EC2s
-      root_block_device,
-      # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
-    ]
-  }
-
-  tags = merge(
-    local.tags,
-    {
-      Name = "infra2-${local.application_name}"
+      Name = "app-${local.application_name}"
     }
   )
 }
