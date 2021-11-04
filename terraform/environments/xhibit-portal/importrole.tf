40d6337ac569c43223b7a17b4b96e077cc68c4f8
George Cairns <george.cairns@gmail.com>
2021-11-04 10:44:36 +0000

    add new input for import module

--- terraform/environments/xhibit-portal/importrole.tf
+++ terraform/environments/xhibit-portal/importrole.tf
@@ -5,5 +5,6 @@ module "vm-import" {
   bucket_prefix    = local.application_data.accounts[local.environment].bucket_prefix
   tags             = local.tags
   application_name = local.application_name
+  account_number   = local.environment_management.account_ids[terraform.workspace]
 
 }
