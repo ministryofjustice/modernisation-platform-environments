1273505dcd68f82209860a7fb7aaf5ac441367c5
George Cairns <george.cairns@gmail.com>
2021-11-04 10:14:14 +0000

    latest version of vm import module

--- terraform/environments/xhibit-portal/importrole.tf
+++ terraform/environments/xhibit-portal/importrole.tf
@@ -1,6 +1,6 @@
 module "vm-import" {
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-vm-import?ref=initial-commit"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-vm-import?ref=81c1edc3dd0f5f55c965bc83f4b71da4431f1c21"
 
   bucket_prefix    = local.application_data.accounts[local.environment].bucket_prefix
   tags             = local.tags
