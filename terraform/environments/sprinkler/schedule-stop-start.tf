a85fe22091309186af3fbf8b65db539d7b4c8282
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2021-11-10 13:41:05 +0000

    stop/start db_mgmt_server at nights

---
+++ terraform/environments/sprinkler/schedule-stop-start.tf
@@ -0,0 +1,33 @@
+#------------------------------------------------------------------------------
+# Schedule stop/start EC2 instances
+#------------------------------------------------------------------------------
+
+module "stop_ec2_instance_nights" {
+  source                         = "diodonfrost/lambda-scheduler-stop-start/aws?ref=v3.1.3"
+  name                           = "stop_ec2_instance_nights"
+  cloudwatch_schedule_expression = "cron(0/5 * * * ? *)" # "cron(0 0 ? * FRI *)" # Every Friday at 23:00 GMT
+  schedule_action                = "stop"
+  autoscaling_schedule           = "false"
+  ec2_schedule                   = "true"
+  rds_schedule                   = "false"
+  cloudwatch_alarm_schedule      = "false"
+  scheduler_tag                  = {
+    key   = "stop_nights"
+    value = "ec2"
+  }
+}
+
+module "start_ec2_instance_mornings" {
+  source                         = "diodonfrost/lambda-scheduler-stop-start/aws?ref=v3.1.3"
+  name                           = "start_ec2_instance_mornings"
+  cloudwatch_schedule_expression = "cron(0/6 * * * ? *)" # "cron(0 8 ? * MON *)" # Every Monday at 8:00 GMT
+  schedule_action                = "start"
+  autoscaling_schedule           = "false"
+  ec2_schedule                   = "true"
+  rds_schedule                   = "false"
+  cloudwatch_alarm_schedule      = "false"
+  scheduler_tag                  = {
+    key   = "stop_nights"
+    value = "ec2"
+  }
+}
