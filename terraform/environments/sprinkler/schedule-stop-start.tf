e58a49233e5fbf271a07ae9156077c8954a8ed73
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2021-11-12 13:37:42 +0000

    switch to the internal delious module

--- terraform/environments/sprinkler/schedule-stop-start.tf
+++ terraform/environments/sprinkler/schedule-stop-start.tf
@@ -3,14 +3,16 @@
 #------------------------------------------------------------------------------
 
 module "stop_ec2_instance_nights" {
-  source                         = "github.com/diodonfrost/terraform-aws-lambda-scheduler-stop-start?ref=3.1.3"
+  source                         = "github.com/ministryofjustice/hmpps-delius-network-terraform//lambda-scheduler?ref=main"
   name                           = "stop_ec2_instance_nights"
   cloudwatch_schedule_expression = "cron(0/30 * * * ? *)" # "cron(0 0 ? * FRI *)" # Every Friday at 23:00 GMT
   schedule_action                = "stop"
   autoscaling_schedule           = "false"
+  spot_schedule                  = "terminate"
   ec2_schedule                   = "true"
   rds_schedule                   = "false"
   cloudwatch_alarm_schedule      = "false"
+  event_rule_enabled             = "false"
   scheduler_tag                  = {
     key   = "stop_nights"
     value = "ec2"
@@ -18,14 +20,16 @@ module "stop_ec2_instance_nights" {
 }
 
 module "start_ec2_instance_mornings" {
-  source                         = "github.com/diodonfrost/terraform-aws-lambda-scheduler-stop-start?ref=3.1.3"
+  source                         = "github.com/ministryofjustice/hmpps-delius-network-terraform//lambda-scheduler?ref=main"
   name                           = "start_ec2_instance_mornings"
   cloudwatch_schedule_expression = "cron(0/5 * * * ? *)" # "cron(0 8 ? * MON *)" # Every Monday at 8:00 GMT
   schedule_action                = "start"
   autoscaling_schedule           = "false"
+  spot_schedule                  = "false"
   ec2_schedule                   = "true"
   rds_schedule                   = "false"
   cloudwatch_alarm_schedule      = "false"
+  event_rule_enabled             = "false"
   scheduler_tag                  = {
     key   = "stop_nights"
     value = "ec2"
