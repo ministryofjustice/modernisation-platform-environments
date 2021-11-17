cc29d8e5656bd0c3cc82e0335bdddfecee8ce342
George Cairns <george.cairns@gmail.com>
2021-11-17 13:14:57 +0000

    try to break terraform cycle with sec rule resources 1

--- terraform/environments/xhibit-portal/domain-controllers.tf
+++ terraform/environments/xhibit-portal/domain-controllers.tf
@@ -8,74 +8,6 @@ resource "aws_security_group" "domain-controllers" {
 
 }
 
-resource "aws_security_group_rule" "dc1" {  
-    security_group  = aws_security_group.domain-controllers.id
-    provider        = local.vpc_id
-    type            = "egress"
-    description      = "allow all"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-}
-
-resource "aws_security_group_rule" "dc2" {
-    security_group  = aws_security_group.domain-controllers.id
-    provider        = local.vpc_id
-    type            = "ingress"
-    description     = "SSH from Bastion"
-    from_port       = 0
-    to_port         = "3389"
-    protocol        = "TCP"
-    cidr_blocks     = ["${module.bastion_linux.bastion_private_ip}/32"]
-}
-
-resource "aws_security_group_rule" "dc3" {
-    security_group  = aws_security_group.domain-controllers.id
-    provider        = local.vpc_id
-    type            = "ingress"
-    description     = "allow DNS"
-    from_port       = 0
-    to_port         = 53
-    protocol        = "TCP"
-    source_security_group_id = aws_security_group.outbound-dns-resolver.id
-}
-
-resource "aws_security_group_rule" "dc4" {
-    security_group  = aws_security_group.domain-controllers.id
-    provider        = local.vpc_id
-    type            = "ingress"
-    description     = "allow DNS"
-    from_port       = 0
-    to_port         = 53
-    protocol        = "UDP"
-    source_security_group_id = aws_security_group.outbound-dns-resolver.id
-}
-
-resource "aws_security_group_rule" "dc5" {
-    security_group  = aws_security_group.domain-controllers.id
-    provider        = local.vpc_id
-    type            = "ingress"
-    description     = "allow DCs to listen to each other"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "TCP"
-    self            = true
-}
-
-resource "aws_security_group_rule" "dc6" {
-    security_group  = aws_security_group.domain-controllers.id
-    provider        = local.vpc_id
-    type            = "egress"
-    description     = "allow DCs to talk to each other"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "TCP"
-    self            = true
-}
-
-
 # Security Groups
 resource "aws_security_group" "outbound-dns-resolver" {
   provider = aws.core-vpc
@@ -87,28 +19,6 @@ resource "aws_security_group" "outbound-dns-resolver" {
 
 
 
-}
-
-resource "aws_security_group_rule" "res1" {  
-    security_group  = aws_security_group.outbound-dns-resolver.id
-    provider        = local.vpc_id
-    type            = "egress"
-    description     = "allow DNS"
-    from_port       = 0
-    to_port         = 53
-    protocol        = "TCP"
-    source_security_group = aws_security_group.domain-controllers.id
-}
-
-resource "aws_security_group_rule" "res2" {  
-    security_group  = aws_security_group.outbound-dns-resolver.id
-    provider        = local.vpc_id
-    type            = "egress"
-    description     = "allow DNS"
-    from_port       = 0
-    to_port         = 53
-    protocol        = "UDP"
-    source_security_group = aws_security_group.domain-controllers.id
 }
 
 
