454f855b71a6e7b717557ab01579c03f333b1755
George Cairns <george.cairns@gmail.com>
2021-11-17 13:50:48 +0000

    troubleshooting terraform

--- terraform/environments/xhibit-portal/domain-controllers.tf
+++ terraform/environments/xhibit-portal/domain-controllers.tf
@@ -8,17 +8,17 @@ resource "aws_security_group" "domain-controllers" {
 
 }
 
-# resource "aws_security_group_rule" "dc1" {  
-#     security_group_id  = aws_security_group.domain-controllers.id
-#     # provider        = aws.core-vpc
-#     type            = "egress"
-#     description      = "allow all"
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-# }
+resource "aws_security_group_rule" "dc1" {  
+    security_group_id  = aws_security_group.domain-controllers.id
+    # provider        = aws.core-vpc
+    type            = "egress"
+    description      = "allow all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+}
 
 # resource "aws_security_group_rule" "dc2" {
 #     security_group_id  = aws_security_group.domain-controllers.id
