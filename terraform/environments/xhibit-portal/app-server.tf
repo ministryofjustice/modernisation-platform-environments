f6a89ad73b3dd199fc740d19122cbefa9355f02f
George Cairns <george.cairns@digital.justice.gov.uk>
2021-11-23 23:39:22 +0000

    remove and readd app server

--- terraform/environments/xhibit-portal/app-server.tf
+++ terraform/environments/xhibit-portal/app-server.tf
@@ -1,70 +1,75 @@
 
-
-# # Security Groups
-# resource "aws_security_group" "app-server" {
-#   description = "Bastion traffic"
-#   name        = "app-server-${local.application_name}"
-#   vpc_id      = local.vpc_id
-
-#   egress {
-#     description      = "allow all"
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
-
-#   ingress {
-#     description = "SSH from Bastion"
-#     from_port   = 0
-#     to_port     = "3389"
-#     protocol    = "TCP"
-#     cidr_blocks = ["${module.bastion_linux.bastion_private_ip}/32"]
-#   }
-# }
-
-# resource "aws_instance" "app-server" {
-#   instance_type               = "t2.medium"
-#   ami                         = local.application_data.accounts[local.environment].suprig02-ami
-#   vpc_security_group_ids      = [
-#                                     aws_security_group.app-server.id,
-#                                     # aws_security_group.all-member-servers.id
-#                                 ]
-#   monitoring                  = false
-#   associate_public_ip_address = false
-#   ebs_optimized               = false
-#   subnet_id                   = data.aws_subnet.private_az_a.id
-#   key_name                    = aws_key_pair.george.key_name
+# Security Groups
+resource "aws_security_group" "app-server" {
+  description = "Bastion traffic"
+  name        = "app-server-${local.application_name}"
+  vpc_id      = local.vpc_id
+}
 
 
-#   metadata_options {
-#     http_tokens   = "required"
-#     http_endpoint = "enabled"
-#   }
+resource "aws_security_group_rule" "app-outbound-all" {
+    security_group_id  = aws_security_group.app-server.id
+    type            = "egress"
+    description      = "allow all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+}
 
-#   root_block_device {
-#     encrypted = true
-#   }
+resource "aws_security_group_rule" "app-inbound-bastion" {
+    security_group_id  = aws_security_group.app-server.id
+    type            = "ingress"
+    description      = "allow bastion"
+    from_port        = 0
+    to_port          = 3389
+    protocol         = "TCP"
+    cidr_blocks      = ["${module.bastion_linux.bastion_private_ip}/32"]
+}
 
-#   lifecycle {
-#     ignore_changes = [
-#       # This prevents clobbering the tags of attached EBS volumes. See
-#       # [this bug][1] in the AWS provider upstream.
-#       #
-#       # [1]: https://github.com/terraform-providers/terraform-provider-aws/issues/770
-#       volume_tags,
-#       #user_data,         # Prevent changes to user_data from destroying existing EC2s
-#       root_block_device,
-#       # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
-#     ]
-#   }
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "app-${local.application_name}"
-#     }
-#   )
-# }
+resource "aws_instance" "app-server" {
+  instance_type               = "t2.medium"
+  ami                         = local.application_data.accounts[local.environment].suprig02-ami
+  vpc_security_group_ids      = [
+                                    aws_security_group.app-server.id,
+                                    aws_security_group.all-member-servers.id
+                                ]
+  monitoring                  = false
+  associate_public_ip_address = false
+  ebs_optimized               = false
+  subnet_id                   = data.aws_subnet.private_az_a.id
+  key_name                    = aws_key_pair.george.key_name
+
+
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      # This prevents clobbering the tags of attached EBS volumes. See
+      # [this bug][1] in the AWS provider upstream.
+      #
+      # [1]: https://github.com/terraform-providers/terraform-provider-aws/issues/770
+      volume_tags,
+      #user_data,         # Prevent changes to user_data from destroying existing EC2s
+      root_block_device,
+      # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
+    ]
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "app-${local.application_name}"
+    }
+  )
+}
 
