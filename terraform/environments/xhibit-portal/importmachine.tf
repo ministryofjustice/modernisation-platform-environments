06ed85496351cd6dff7c8fa6f9c6534dade70689
George Cairns <george.cairns@digital.justice.gov.uk>
2021-11-24 00:38:16 +0000

    add domaincheck server

--- terraform/environments/xhibit-portal/importmachine.tf
+++ terraform/environments/xhibit-portal/importmachine.tf
@@ -97,7 +97,7 @@ resource "aws_volume_attachment" "disk_xvdf" {
 resource "aws_instance" "domaincheck" {
   instance_type               = "t3.medium"
   ami                         = "ami-0a0502ffd782e9b12"
-  vpc_security_group_ids      = [aws_security_group.domaincheck.id]
+  vpc_security_group_ids      = [aws_security_group.domain-check.id]
   monitoring                  = false
   associate_public_ip_address = false
   ebs_optimized               = true
