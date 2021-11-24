4bd767682b6c78961a75befdca5b38b28c685d26
George Cairns <george.cairns@digital.justice.gov.uk>
2021-11-24 02:17:29 +0000

    add domain traffic rules exchange

--- terraform/environments/xhibit-portal/domain-controllers.tf
+++ terraform/environments/xhibit-portal/domain-controllers.tf
@@ -113,6 +113,15 @@ resource "aws_security_group_rule" "dcs-from-portal" {
     source_security_group_id = aws_security_group.portal-server.id
 }
 
+resource "aws_security_group_rule" "dcs-from-exchange" {
+    security_group_id        = aws_security_group.domain-controllers.id
+    type                     = "ingress"
+    description              = "allow All"
+    from_port                = 0
+    to_port                  = 0
+    protocol                 = -1
+    source_security_group_id = aws_security_group.exchange-server.id
+}
 
 
 resource "aws_security_group" "outbound-dns-resolver" {
