333a556d5df7ea52a14a204ba76f38fb8ef747bb
George Cairns <george.cairns@digital.justice.gov.uk>
2021-11-25 17:09:10 +0000

    add cjim back

--- terraform/environments/xhibit-portal/cjim-server.tf
+++ terraform/environments/xhibit-portal/cjim-server.tf
@@ -39,66 +39,66 @@ resource "aws_security_group_rule" "cjip-inbound-web" {
 }
 
 
-# resource "aws_instance" "cjim-server" {
-#   instance_type               = "t2.medium"
-#   ami                         = local.application_data.accounts[local.environment].suprig04-ami
-#   vpc_security_group_ids      = [aws_security_group.cjim-server.id]
-#   monitoring                  = false
-#   associate_public_ip_address = false
-#   ebs_optimized               = false
-#   subnet_id                   = data.aws_subnet.private_az_a.id
-#   key_name                    = aws_key_pair.george.key_name
+resource "aws_instance" "cjim-server" {
+  instance_type               = "t2.medium"
+  ami                         = local.application_data.accounts[local.environment].suprig04-ami
+  vpc_security_group_ids      = [aws_security_group.cjim-server.id]
+  monitoring                  = false
+  associate_public_ip_address = false
+  ebs_optimized               = false
+  subnet_id                   = data.aws_subnet.private_az_a.id
+  key_name                    = aws_key_pair.george.key_name
 
 
-#   metadata_options {
-#     http_tokens   = "required"
-#     http_endpoint = "enabled"
-#   }
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
 
-#   root_block_device {
-#     encrypted = true
-#   }
+  root_block_device {
+    encrypted = true
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       # This prevents clobbering the tags of attached EBS volumes. See
-#       # [this bug][1] in the AWS provider upstream.
-#       #
-#       # [1]: https://github.com/terraform-providers/terraform-provider-aws/issues/770
-#       volume_tags,
-#       #user_data,         # Prevent changes to user_data from destroying existing EC2s
-#       root_block_device,
-#       # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
-#     ]
-#   }
+  lifecycle {
+    ignore_changes = [
+      # This prevents clobbering the tags of attached EBS volumes. See
+      # [this bug][1] in the AWS provider upstream.
+      #
+      # [1]: https://github.com/terraform-providers/terraform-provider-aws/issues/770
+      volume_tags,
+      #user_data,         # Prevent changes to user_data from destroying existing EC2s
+      root_block_device,
+      # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
+    ]
+  }
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "cjim-${local.application_name}"
-#     }
-#   )
-# }
+  tags = merge(
+    local.tags,
+    {
+      Name = "cjim-${local.application_name}"
+    }
+  )
+}
 
 
-# resource "aws_ebs_volume" "cjim-disk1" {
-#   availability_zone = "${local.region}a"
-#   type              = "gp2"
-#   encrypted         = true
+resource "aws_ebs_volume" "cjim-disk1" {
+  availability_zone = "${local.region}a"
+  type              = "gp2"
+  encrypted         = true
 
-#   snapshot_id = local.application_data.accounts[local.environment].suprig04-disk-1-snapshot
+  snapshot_id = local.application_data.accounts[local.environment].suprig04-disk-1-snapshot
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "cjim-disk1-${local.application_name}"
-#     }
-#   )
-# }
+  tags = merge(
+    local.tags,
+    {
+      Name = "cjim-disk1-${local.application_name}"
+    }
+  )
+}
 
-# resource "aws_volume_attachment" "cjim-disk1" {
-#   device_name = "xvdi"
-#   volume_id   = aws_ebs_volume.cjim-disk1.id
-#   instance_id = aws_instance.cjim-server.id
-# }
+resource "aws_volume_attachment" "cjim-disk1" {
+  device_name = "xvdi"
+  volume_id   = aws_ebs_volume.cjim-disk1.id
+  instance_id = aws_instance.cjim-server.id
+}
 
