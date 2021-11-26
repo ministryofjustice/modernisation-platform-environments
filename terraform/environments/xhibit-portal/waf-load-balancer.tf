ad083b99b1d354b84d0fe7d28acc3ef568d23f59
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2021-11-26 12:19:29 +0000

    Spltting traffic routes in the lb between web and ingestion

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -12,7 +12,6 @@ resource "aws_security_group" "waf_lb" {
 #     to_port          = 0
 #     protocol         = "TCP"
 #     cidr_blocks      = ["0.0.0.0/0"]
-#     source_security_group_id = aws_security_group.portal-server.id
 # }
 
 resource "aws_security_group_rule" "egress-to-portal" {
@@ -49,7 +48,7 @@ resource "aws_lb" "waf_lb" {
   )
 }
 
-resource "aws_lb_target_group" "waf_lb_tg" {
+resource "aws_lb_target_group" "waf_lb_web_tg" {
   name                 = "waf-lb-tg-${var.networking[0].application}"
   port                 = 80
   protocol             = "HTTP"
@@ -74,9 +73,40 @@ resource "aws_lb_target_group" "waf_lb_tg" {
   )
 }
 
+resource "aws_lb_target_group" "waf_lb_ingest_tg" {
+  name                 = "waf-lb-tg-${var.networking[0].application}"
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = "30"
+  vpc_id               = local.vpc_id
+
+  health_check {
+    path = "/BITSWebService/BITSWebService.asmx"
+    port = 80
+    healthy_threshold = 6
+    unhealthy_threshold = 2
+    timeout = 2
+    interval = 5
+    matcher = "200"  # change this to 200 when the database comes up
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "waf-lb_-g-${var.networking[0].application}"
+    },
+  )
+}
+
 resource "aws_lb_target_group_attachment" "portal-server-attachment" {
-  target_group_arn = aws_lb_target_group.waf_lb_tg.arn
+  target_group_arn = aws_lb_target_group.waf_lb_web_tg.arn
   target_id        = aws_instance.portal-server.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "ingestion-server-attachment" {
+  target_group_arn = aws_lb_target_group.waf_lb_ingest_tg.arn
+  target_id        = aws_instance.cjip-server.id
   port             = 80
 }
 
@@ -94,16 +124,16 @@ resource "aws_lb_listener" "waf_lb_listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.waf_lb_tg.arn
+    target_group_arn = aws_lb_target_group.waf_lb_web_tg.arn
   }
 }
 
-resource "aws_alb_listener_rule" "listener_rule" {
-  depends_on   = [aws_lb_target_group.waf_lb_tg]  
+resource "aws_alb_listener_rule" "web_listener_rule" {
+  depends_on   = [aws_lb_target_group.waf_lb_web_tg]  
   listener_arn = aws_lb_listener.waf_lb_listener.arn     
   action {    
     type             = "forward"    
-    target_group_arn = aws_lb_target_group.waf_lb_tg.id  
+    target_group_arn = aws_lb_target_group.waf_lb_web_tg.id  
   }   
   condition {    
     path_pattern {
@@ -111,21 +141,58 @@ resource "aws_alb_listener_rule" "listener_rule" {
     }    
   }
 
-  # condition {    
-  #   host-header {
-  #     values = ["web.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]  
-  #   }    
-  # }
+  condition {    
+    host-header {
+      # web.xhibit-portal.hmcts-development.modernisation-platform.service.justice.gov.uk
+      values = ["web.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]  
+    }    
+  }
+
+}
+
+resource "aws_alb_listener_rule" "ingestion_listener_rule" {
+  depends_on   = [aws_lb_target_group.waf_lb_ingest_tg]  
+  listener_arn = aws_lb_listener.waf_lb_listener.arn     
+  action {    
+    type             = "forward"    
+    target_group_arn = aws_lb_target_group.waf_lb_ingest_tg.id  
+  }   
+  condition {    
+    path_pattern {
+      values = ["/"]  
+    }    
+  }
+
+  condition {    
+    host-header {
+      # web.xhibit-portal.hmcts-development.modernisation-platform.service.justice.gov.uk
+      values = ["ingest.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]  
+    }    
+  }
 
 }
 
 
 
-resource "aws_route53_record" "waf_lb_dns" {
+resource "aws_route53_record" "waf_lb_web_dns" {
   provider = aws.core-vpc
 
   zone_id = data.aws_route53_zone.external_r53_zone.zone_id
-  name    = "${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "web.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.waf_lb.dns_name
+    zone_id                = aws_lb.waf_lb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "waf_lb_ingest_dns" {
+  provider = aws.core-vpc
+
+  zone_id = data.aws_route53_zone.external_r53_zone.zone_id
+  name    = "ingest.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
 
   alias {
