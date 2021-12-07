7f2d3979b1dc45c7f3b82ef609dd1dfd952329a2
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2021-12-07 15:25:38 +0000

    Commented out dcheck config, no longer needed

--- terraform/environments/xhibit-portal/importmachine.tf
+++ terraform/environments/xhibit-portal/importmachine.tf
@@ -141,57 +141,57 @@ resource "aws_instance" "domaincheck" {
 # 
 
 # Security Groups
-resource "aws_security_group" "domain-check" {
-  description = "Bastion traffic"
-  name        = "domaincheck-${local.application_name}"
-  vpc_id      = local.vpc_id
-}
+# resource "aws_security_group" "domain-check" {
+#   description = "Bastion traffic"
+#   name        = "domaincheck-${local.application_name}"
+#   vpc_id      = local.vpc_id
+# }
 
 
-resource "aws_security_group_rule" "dcheck-outbound-all" {
-  depends_on        = [aws_security_group.domain-check]
-  security_group_id = aws_security_group.domain-check.id
-  type              = "egress"
-  description       = "allow all"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-}
+# resource "aws_security_group_rule" "dcheck-outbound-all" {
+#   depends_on        = [aws_security_group.domain-check]
+#   security_group_id = aws_security_group.domain-check.id
+#   type              = "egress"
+#   description       = "allow all"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   ipv6_cidr_blocks  = ["::/0"]
+# }
 
-resource "aws_security_group_rule" "dcheck-inbound-bastion" {
-  depends_on        = [aws_security_group.domain-check]
-  security_group_id = aws_security_group.domain-check.id
-  type              = "ingress"
-  description       = "allow bastion"
-  from_port         = 0
-  to_port           = 3389
-  protocol          = "TCP"
-  cidr_blocks       = ["${module.bastion_linux.bastion_private_ip}/32"]
-}
+# resource "aws_security_group_rule" "dcheck-inbound-bastion" {
+#   depends_on        = [aws_security_group.domain-check]
+#   security_group_id = aws_security_group.domain-check.id
+#   type              = "ingress"
+#   description       = "allow bastion"
+#   from_port         = 0
+#   to_port           = 3389
+#   protocol          = "TCP"
+#   cidr_blocks       = ["${module.bastion_linux.bastion_private_ip}/32"]
+# }
 
-resource "aws_security_group_rule" "dcheck-to-dcs" {
-  depends_on               = [aws_security_group.domain-check]
-  security_group_id        = aws_security_group.domain-check.id
-  type                     = "egress"
-  description              = "allow All"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = -1
-  source_security_group_id = aws_security_group.domain-controllers.id
-}
+# resource "aws_security_group_rule" "dcheck-to-dcs" {
+#   depends_on               = [aws_security_group.domain-check]
+#   security_group_id        = aws_security_group.domain-check.id
+#   type                     = "egress"
+#   description              = "allow All"
+#   from_port                = 0
+#   to_port                  = 0
+#   protocol                 = -1
+#   source_security_group_id = aws_security_group.domain-controllers.id
+# }
 
-resource "aws_security_group_rule" "dcs-from-dcheck" {
-  depends_on               = [aws_security_group.domain-check]
-  security_group_id        = aws_security_group.domain-controllers.id
-  type                     = "ingress"
-  description              = "allow All"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = -1
-  source_security_group_id = aws_security_group.domain-check.id
-}
+# resource "aws_security_group_rule" "dcs-from-dcheck" {
+#   depends_on               = [aws_security_group.domain-check]
+#   security_group_id        = aws_security_group.domain-controllers.id
+#   type                     = "ingress"
+#   description              = "allow All"
+#   from_port                = 0
+#   to_port                  = 0
+#   protocol                 = -1
+#   source_security_group_id = aws_security_group.domain-check.id
+# }
 
 
 
