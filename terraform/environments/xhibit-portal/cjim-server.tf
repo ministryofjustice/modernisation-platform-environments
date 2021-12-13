dd5df02f68b93b65466280030b9287a7306344e5
George Cairns <george.cairns@digital.justice.gov.uk>
2021-12-13 13:29:22 +0000

    grrrrr device names

--- terraform/environments/xhibit-portal/cjim-server.tf
+++ terraform/environments/xhibit-portal/cjim-server.tf
@@ -102,7 +102,7 @@ resource "aws_ebs_volume" "cjim-disk1" {
 }
 
 resource "aws_volume_attachment" "cjim-disk1" {
-  device_name  = "xvdi"
+  device_name  = "xvdg"
   force_detach = true
   volume_id    = aws_ebs_volume.cjim-disk1.id
   instance_id  = aws_instance.cjim-server.id
