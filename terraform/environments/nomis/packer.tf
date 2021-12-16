4ea4a7a5b81c632b974c832d5bb6434fc1e3b271
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2021-12-16 14:53:42 +0000

    also need to add tagging snapshots after they've been created

--- terraform/environments/nomis/packer.tf
+++ terraform/environments/nomis/packer.tf
@@ -172,7 +172,10 @@ data "aws_iam_policy_document" "packer_minimum_permissions" {
   statement { # need this as Packer seems to copy the image and then tag it
     effect    = "Allow"
     actions   = ["ec2:CreateTags"]
-    resources = ["arn:aws:ec2:eu-west-2::image/ami-*"]
+    resources = [
+      "arn:aws:ec2:eu-west-2::image/ami-*",
+      "arn:aws:ec2:eu-west-2::snapshot/snap-*"
+      ]
   }
 }
 
