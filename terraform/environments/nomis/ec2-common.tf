70343fabe67cd1cf15f510d0f163a7e855803eb3
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2022-01-07 12:21:38 +0000

    add an ssm association to update ssm-agent

--- terraform/environments/nomis/ec2-common.tf
+++ terraform/environments/nomis/ec2-common.tf
@@ -228,4 +228,22 @@ resource "aws_ssm_parameter" "cloud_watch_config_linux" {
 
 # do a schedule
 # config for windows
-# add one for ssm-agent updates??
+
+#------------------------------------------------------------------------------
+# SSM Agent - update System Manager Agent
+#------------------------------------------------------------------------------
+
+resource "aws_ssm_association" "update_ssm_agent" {
+  name             = "AWS-UpdateSSMAgent" # this is an AWS provided document
+  association_name = "update-ssm-agent"
+  parameters = {
+    allowDowngrade = "false"
+  }
+  targets {
+    # we could just target all instances, but this would also include the bastion, which gets rebuilt everyday
+    key    = "tag:os_type"
+    values = ["Linux","Windows"]
+  }
+  apply_only_at_cron_interval = false
+  # schedule_expression = TODO once we have determined when machines will be on/off
+}
