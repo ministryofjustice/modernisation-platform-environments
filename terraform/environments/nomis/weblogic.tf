4896fbb5d3ad8fbe4be606d62705c1538a3f249e
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-01-13 09:19:54 +0000

    Use new Weblogic AMI & user_data script

--- terraform/environments/nomis/weblogic.tf
+++ terraform/environments/nomis/weblogic.tf
@@ -1,8 +1,10 @@
-# get shared subnet-set private (az (a) subnet)
-data "aws_subnet" "private_az_a" {
-  # provider = aws.share-host
+#------------------------------------------------------------------------------
+# Networking and Security Groups
+#------------------------------------------------------------------------------
+data "aws_subnet" "data_az_a" {
+  vpc_id = local.vpc_id
   tags = {
-    Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-private-${local.region}a"
+    Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-data-${local.region}a"
   }
 }
 
@@ -61,15 +63,16 @@ resource "aws_security_group" "weblogic_server" {
   )
 }
 
-# EC2 instance
-
+#------------------------------------------------------------------------------
+# AMI and EC2
+#------------------------------------------------------------------------------
 data "aws_ami" "weblogic_image" {
   most_recent = true
-  owners      = ["self"] # 309956199498 Red Hat - this will be a custom image later "self" .
+  owners      = ["self"]
 
   filter {
     name   = "name"
-    values = ["nomis_app-2021-09-20*"] # temp. fix this to prevent any more accidental replacements
+    values = ["weblogic-2022-01-12"]
   }
 
   filter {
@@ -87,8 +90,8 @@ resource "aws_instance" "weblogic_server" {
   iam_instance_profile        = aws_iam_instance_profile.ec2_common_profile.id
   monitoring                  = false
   vpc_security_group_ids      = [aws_security_group.weblogic_server.id]
-  subnet_id                   = data.aws_subnet.private_az_a.id
-  user_data                   = file("./templates/cloudinit.cfg")
+  subnet_id                   = data.aws_subnet.data_az_a.id
+  user_data                   = file("./templates/weblogic-init.sh")
   # ebs_optimized          = true
   key_name = aws_key_pair.ec2-user.key_name
   metadata_options {
@@ -111,22 +114,35 @@ resource "aws_instance" "weblogic_server" {
   )
 }
 
-# resource "aws_ebs_volume" "extra_disk" {
-#   availability_zone = "${local.region}a"
-#   type              = "gp2"
-#   encrypted         = true
-#   size              = 50
+#------------------------------------------------------------------------------
+# Route 53 record
+#------------------------------------------------------------------------------
+resource "aws_route53_record" "weblogic" {
+  provider = aws.core-vpc
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "weblogic-${local.application_name}-extra-disk"
-#     }
-#   )
-# }
+  zone_id = data.aws_route53_zone.internal.zone_id
+  name    = "weblogic.${local.application_name}.${local.vpc_name}-${local.environment}.modernisation-platform.internal"
+  type    = "A"
+  ttl     = "60"
+  records = [aws_instance.db_server.private_ip]
+}
 
-# resource "aws_volume_attachment" "extra_disk" {
-#   device_name = "/dev/sde"
-#   volume_id   = aws_ebs_volume.extra_disk.id
-#   instance_id = aws_instance.weblogic_server.id
-# }
+resource "aws_ebs_volume" "extra_disk" {
+  availability_zone = "${local.region}a"
+  type              = "gp2"
+  encrypted         = true
+  size              = 150
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "weblogic-${local.application_name}-extra-disk"
+    }
+  )
+}
+
+resource "aws_volume_attachment" "extra_disk" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.extra_disk.id
+  instance_id = aws_instance.weblogic_server.id
+}
