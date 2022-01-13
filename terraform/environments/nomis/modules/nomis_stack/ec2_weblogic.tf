ca11b0a34e59cc557d0b33bea54af8ca8940ec30
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-01-13 12:23:38 +0000

    Add vars to weblogic template file

--- terraform/environments/nomis/modules/nomis_stack/ec2_weblogic.tf
+++ terraform/environments/nomis/modules/nomis_stack/ec2_weblogic.tf
@@ -34,6 +34,7 @@ locals {
 data "template_file" "weblogic_init" {
   template = "${file("${path.module}/user_data/weblogic_init.sh")}"
   vars = {
+    ENV               = var.stack_name
     DB_HOSTNAME       = "database-${var.stack_name}.${var.application_name}.${var.business_unit}-${var.environment}.modernisation-platform.internal"
     DB_NAME           = var.DB_NAME
     DB_PORT           = var.DB_PORT
--- terraform/environments/nomis/modules/nomis_stack/user_data/weblogic_init.sh
+++ terraform/environments/nomis/modules/nomis_stack/user_data/weblogic_init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Place sensitive values into AWS parameter store (obtained via user input on "terraform apply")
-aws ssm put-parameter --name "${env}_DB_USERNAME" --type "String" --value "${DB_USERNAME}" --overwrite
-aws ssm put-parameter --name "${env}_DB_PASSWORD" --type "String" --value "${DB_PASSWORD}" --overwrite
-aws ssm put-parameter --name "${env}_WEBLOGIC_USERNAME" --type "String" --value "${WEBLOGIC_USERNAME}" --overwrite
-aws ssm put-parameter --name "${env}_WEBLOGIC_PASSWORD" --type "String" --value "${WEBLOGIC_PASSWORD}" --overwrite
+aws ssm put-parameter --name "${ENV}_DB_USERNAME" --type "String" --value "${DB_USERNAME}" --overwrite
+aws ssm put-parameter --name "${ENV}_DB_PASSWORD" --type "String" --value "${DB_PASSWORD}" --overwrite
+aws ssm put-parameter --name "${ENV}_WEBLOGIC_USERNAME" --type "String" --value "${WEBLOGIC_USERNAME}" --overwrite
+aws ssm put-parameter --name "${ENV}_WEBLOGIC_PASSWORD" --type "String" --value "${WEBLOGIC_PASSWORD}" --overwrite
 
 su -c "export DB_HOSTNAME=${DB_HOSTNAME} DB_NAME=${DB_NAME} DB_PORT=${DB_PORT}; bash /u01/software/weblogic/weblogic-setup.sh" - oracle
