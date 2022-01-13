9d98a23067bd77d5ba5e32cc9d7c7c3f059248c2
George Cairns <george.cairns@digital.justice.gov.uk>
2022-01-13 15:51:36 +0000

    add dns names to waf

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -213,7 +213,12 @@ resource "aws_acm_certificate" "waf_lb_cert" {
   domain_name       = "${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
-  subject_alternative_names = ["*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  subject_alternative_names = [
+    "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk",
+    local.application_data.accounts[local.environment].public_dns_name_web,
+    local.application_data.accounts[local.environment].public_dns_name_ingestion, 
+  ]
+
   tags = {
     Environment = "prod"
   }
