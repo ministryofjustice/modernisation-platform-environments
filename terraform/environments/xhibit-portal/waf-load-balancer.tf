0e6c2a4204b8199d29674f7c30c98ce9e7183bb7
George Cairns <george.cairns@digital.justice.gov.uk>
2022-01-14 16:01:22 +0000

    change validation object to use the cert object rather than te domain names because we use external domains

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -255,7 +255,7 @@ resource "aws_route53_record" "waf_lb_r53_record" {
 
 resource "aws_acm_certificate_validation" "waf_lb_cert_validation" {
   certificate_arn         = aws_acm_certificate.waf_lb_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.waf_lb_r53_record : record.fqdn]
+  validation_record_fqdns = [for dvo in aws_acm_certificate.waf_lb_cert.domain_validation_options : dvo.domain_name]
 }
 
 resource "aws_wafv2_web_acl" "waf_acl" {
