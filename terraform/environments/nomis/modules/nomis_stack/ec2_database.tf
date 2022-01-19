25f7efbfdda4d955825d2f25b936b8c66a40278d
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-01-19 15:33:46 +0000

    test

--- terraform/environments/nomis/modules/nomis_stack/ec2_database.tf
+++ terraform/environments/nomis/modules/nomis_stack/ec2_database.tf
@@ -26,17 +26,26 @@ resource "aws_security_group_rule" "weblogic_server" {
   depends_on        = [aws_instance.weblogic_server]
 }
 
-# locals {database_extra_ingress_rules = jsondecode(var.database_extra_ingress_rules)}
+# resource "aws_security_group_rule" "extra_rules" { # Extra ingress rules that might be specified
+#   for_each          = { for rule in var.database_extra_ingress_rules : "${rule.description}-${rule.to_port}" => rule }
+#   type              = "ingress"
+#   security_group_id = aws_security_group.database_server.id
+#   description       = lookup(each.value, "description")
+#   from_port         = lookup(each.value, "from_port")
+#   to_port           = lookup(each.value, "to_port")
+#   cidr_blocks       = lookup(each.value, "cidr_blocks")
+#   protocol          = lookup(each.value, "protocol")
+# }
 
 resource "aws_security_group_rule" "extra_rules" { # Extra ingress rules that might be specified
   for_each          = { for rule in var.database_extra_ingress_rules : "${rule.description}-${rule.to_port}" => rule }
   type              = "ingress"
   security_group_id = aws_security_group.database_server.id
-  description       = lookup(each.value, "description")
-  from_port         = lookup(each.value, "from_port")
-  to_port           = lookup(each.value, "to_port")
-  cidr_blocks       = lookup(each.value, "cidr_blocks")
-  protocol          = lookup(each.value, "protocol")
+  description       = each.value.description
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  cidr_blocks       = each.value.cidr_blocks
+  protocol          = each.value.protocol
 }
 
 #------------------------------------------------------------------------------
