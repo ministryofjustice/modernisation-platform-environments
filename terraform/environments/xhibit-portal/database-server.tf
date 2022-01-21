a62c9b76aee2869228bc364b5f9ccb823a2ef6de
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2022-01-21 01:25:46 +0000

    mised coment

--- terraform/environments/xhibit-portal/database-server.tf
+++ terraform/environments/xhibit-portal/database-server.tf
@@ -156,7 +156,7 @@ resource "aws_security_group_rule" "app-to-sql-2" {
 #   to_port                  = 1030
 #   protocol                 = "TCP"
 #   source_security_group_id = aws_security_group.app-server.id
-}
+# }
 
 # resource "aws_security_group_rule" "all-portal-to-sql" {
 #   depends_on               = [aws_security_group.database-server]
