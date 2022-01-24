dcca27d0b57dcb0d9727649103606fea759a8a95
George Cairns <george.cairns@digital.justice.gov.uk>
2022-01-24 10:25:28 +0000

    attempt log policy

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -56,11 +56,11 @@ resource "aws_lb" "waf_lb" {
   subnets                    = data.aws_subnet_ids.shared-public.ids
   enable_deletion_protection = false
 
-  # access_logs {
-  #   bucket  = "${aws_s3_bucket.loadbalancer_logs.bucket}"
-  #   prefix  = "http-lb"
-  #   enabled = true
-  # }
+  access_logs {
+    bucket  = "${aws_s3_bucket.loadbalancer_logs.bucket}"
+    prefix  = "http-lb"
+    enabled = true
+  }
 
   tags = merge(
     local.tags,
@@ -354,62 +354,62 @@ resource "aws_wafv2_web_acl_association" "aws_lb_waf_association" {
 
 
 
-# resource "aws_s3_bucket" "loadbalancer_logs" {
-#   bucket        = "ingest.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}-lblogs"
-#   acl           = "log-delivery-write"
-#   force_destroy = true
-# }
+resource "aws_s3_bucket" "loadbalancer_logs" {
+  bucket        = "ingest.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}-lblogs"
+  acl           = "log-delivery-write"
+  force_destroy = true
+}
 
-# resource "aws_s3_bucket_policy" "loadbalancer_logs_policy" {
-#   bucket = "aws_s3_bucket.this.id"
-#   policy = "${data.aws_iam_policy_document.s3_bucket_lb_write.json}"
-# }
-
-
-# data "aws_iam_policy_document" "s3_bucket_lb_write" {
-
-#   policy_id = "s3_bucket_lb_logs"
-
-#   statement {
-#     actions = [
-#       "s3:PutObject",
-#     ]
-#     effect = "Allow"
-#     resources = [
-#       "${aws_s3_bucket.loadbalancer_logs.arn}/*",
-#     ]
-
-#     principals {
-#       identifiers = ["delivery.logs.amazonaws.com"]
-#       type        = "Service"
-#     }
-#   }
-
-#   statement {
-#     actions = [
-#       "s3:PutObject"
-#     ]
-#     effect = "Allow"
-#     resources = ["${aws_s3_bucket.loadbalancer_logs.arn}/*"]
-#     principals {
-#       identifiers = ["delivery.logs.amazonaws.com"]
-#       type        = "Service"
-#     }
-#   }
+resource "aws_s3_bucket_policy" "loadbalancer_logs_policy" {
+  bucket = "aws_s3_bucket.this.id"
+  policy = "${data.aws_iam_policy_document.s3_bucket_lb_write.json}"
+}
 
 
-#   statement {
-#     actions = [
-#       "s3:GetBucketAcl"
-#     ]
-#     effect = "Allow"
-#     resources = ["${aws_s3_bucket.loadbalancer_logs.arn}"]
-#     principals {
-#       identifiers = ["delivery.logs.amazonaws.com"]
-#       type        = "Service"
-#     }
-#   }
-# }
+data "aws_iam_policy_document" "s3_bucket_lb_write" {
+
+  policy_id = "s3_bucket_lb_logs"
+
+  statement {
+    actions = [
+      "s3:PutObject",
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.loadbalancer_logs.arn}/*",
+    ]
+
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject"
+    ]
+    effect = "Allow"
+    resources = ["${aws_s3_bucket.loadbalancer_logs.arn}/*"]
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+
+
+  statement {
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+    effect = "Allow"
+    resources = ["${aws_s3_bucket.loadbalancer_logs.arn}"]
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
 
 
 
