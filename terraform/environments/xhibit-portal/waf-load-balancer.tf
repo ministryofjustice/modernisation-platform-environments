c577e2ce61b1819445db0ae0ba32217d4f5bc5f3
George Cairns <george.cairns@digital.justice.gov.uk>
2022-01-24 10:38:20 +0000

    update waf log config

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -361,7 +361,7 @@ resource "aws_s3_bucket" "loadbalancer_logs" {
 }
 
 resource "aws_s3_bucket_policy" "loadbalancer_logs_policy" {
-  bucket = "aws_s3_bucket.this.id"
+  bucket = "aws_s3_bucket.loadbalancer_logs.id"
   policy = "${data.aws_iam_policy_document.s3_bucket_lb_write.json}"
 }
 
