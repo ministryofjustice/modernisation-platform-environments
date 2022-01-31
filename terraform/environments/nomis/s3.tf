7fc13d0e125267cb1115aba33798bf13bad10bef
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-01-31 13:31:16 +0000

    Moving s3 bucket to module.

--- terraform/environments/nomis/s3.tf
+++ terraform/environments/nomis/s3.tf
@@ -49,54 +49,54 @@ module "s3-bucket" {
   tags = local.tags
 }
 
-module "nomis-db-backup-bucket" {
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v4.0.0"
+# module "nomis-db-backup-bucket" {
+#   source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v4.0.0"
 
-  providers = {
-    aws.bucket-replication = aws
-  }
-  bucket_prefix       = "nomis-db-backup-bucket"
-  replication_enabled = false
+#   providers = {
+#     aws.bucket-replication = aws
+#   }
+#   bucket_prefix       = "nomis-db-backup-bucket"
+#   replication_enabled = false
 
-  lifecycle_rule = [
-    {
-      id      = "main"
-      enabled = true
-      prefix  = ""
+#   lifecycle_rule = [
+#     {
+#       id      = "main"
+#       enabled = true
+#       prefix  = ""
 
-      tags = {
-        rule      = "log"
-        autoclean = "true"
-      }
+#       tags = {
+#         rule      = "log"
+#         autoclean = "true"
+#       }
 
-      transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-        }
-      ]
+#       transition = [
+#         {
+#           days          = 90
+#           storage_class = "STANDARD_IA"
+#         }
+#       ]
 
-      expiration = {
-        days = 730
-      }
+#       expiration = {
+#         days = 730
+#       }
 
-      noncurrent_version_transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
+#       noncurrent_version_transition = [
+#         {
+#           days          = 90
+#           storage_class = "STANDARD_IA"
+#           }, {
+#           days          = 365
+#           storage_class = "GLACIER"
+#         }
+#       ]
 
-      noncurrent_version_expiration = {
-        days = 730
-      }
-    }
-  ]
+#       noncurrent_version_expiration = {
+#         days = 730
+#       }
+#     }
+#   ]
 
-  tags = local.tags
+#   tags = local.tags
 
-}
+# }
 
