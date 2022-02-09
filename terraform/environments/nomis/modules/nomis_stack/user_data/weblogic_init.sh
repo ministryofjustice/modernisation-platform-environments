1e09cadcfd34110671cb167e976d51cc6efe103a
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-02-09 15:12:00 +0000

    weblogic init script

--- terraform/environments/nomis/modules/nomis_stack/user_data/weblogic_init.sh
+++ terraform/environments/nomis/modules/nomis_stack/user_data/weblogic_init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 su -c "export USE_DEFAULT_CREDS=${USE_DEFAULT_CREDS} DB_HOSTNAME=${DB_HOSTNAME} ENV=${ENV}; bash /u01/software/weblogic/weblogic-setup.sh" - oracle
+
+# Create nomis_weblogic service
+chkconfig --add nomis_weblogic
+chkconfig --level 3 nomis_weblogic on
