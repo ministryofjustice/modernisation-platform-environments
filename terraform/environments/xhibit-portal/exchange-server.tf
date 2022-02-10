305b4f825c778f65a9e2a6f16556aec83db6ea72
George Cairns <george.cairns@digital.justice.gov.uk>
2022-02-10 11:32:49 +0000

    move exchange server

--- terraform/environments/xhibit-portal/exchange-server.tf
+++ terraform/environments/xhibit-portal/exchange-server.tf
@@ -106,38 +106,6 @@ resource "aws_network_interface" "email" {
   }
 }
 
-resource "aws_security_group" "exchange-server-email" {
-  description = "Domain traffic only"
-  name        = "exchange-server-${local.application_name}"
-  vpc_id      = local.vpc_id
-}
-
-resource "aws_security_group_rule" "email-outbound-all" {
-  depends_on        = [aws_security_group.exchange-server-email]
-  security_group_id = aws_security_group.exchange-server-email.id
-  type              = "egress"
-  description       = "allow all"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-}
-
-resource "aws_security_group_rule" "email-inbound-all" {
-  depends_on        = [aws_security_group.exchange-server-email]
-  security_group_id = aws_security_group.exchange-server-email.id
-  type              = "ingress"
-  description       = "allow all"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-}
-
-
-
 resource "aws_ebs_volume" "exchange-disk1" {
   depends_on        = [aws_instance.exchange-server]
   availability_zone = "${local.region}a"
