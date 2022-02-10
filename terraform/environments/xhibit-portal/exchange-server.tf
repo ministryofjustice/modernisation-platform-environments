63fe5e5f3c80b2608397834880801fd43a42389f
George Cairns <george.cairns@digital.justice.gov.uk>
2022-02-10 15:19:01 +0000

    add elastic ip

--- terraform/environments/xhibit-portal/exchange-server.tf
+++ terraform/environments/xhibit-portal/exchange-server.tf
@@ -6,7 +6,6 @@ resource "aws_security_group" "exchange-server" {
   vpc_id      = local.vpc_id
 }
 
-
 resource "aws_security_group_rule" "web-outbound-all" {
   depends_on        = [aws_security_group.exchange-server]
   security_group_id = aws_security_group.exchange-server.id
@@ -17,9 +16,7 @@ resource "aws_security_group_rule" "web-outbound-all" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   ipv6_cidr_blocks  = ["::/0"]
-
 }
-
 
 resource "aws_security_group_rule" "infra-inbound-all" {
   depends_on               = [aws_security_group.exchange-server]
@@ -32,8 +29,6 @@ resource "aws_security_group_rule" "infra-inbound-all" {
   source_security_group_id = aws_security_group.app-server.id
 }
 
-
-
 resource "aws_security_group_rule" "infra-outbound-all" {
   depends_on               = [aws_security_group.exchange-server]
   security_group_id        = aws_security_group.exchange-server.id
@@ -45,18 +40,21 @@ resource "aws_security_group_rule" "infra-outbound-all" {
   source_security_group_id = aws_security_group.app-server.id
 }
 
- resource "aws_security_group_rule" "app-inbound-bastion" {
-   depends_on        = [aws_security_group.exchange-server]
-   security_group_id = aws_security_group.exchange-server.id
-   type              = "ingress"
-   description       = "allow bastion"
-   from_port         = 3389
-   to_port           = 3389
-   protocol          = "TCP"
-   cidr_blocks       = ["${module.bastion_linux.bastion_private_ip}/32"]
- }
+resource "aws_security_group_rule" "app-inbound-bastion" {
+  depends_on        = [aws_security_group.exchange-server]
+  security_group_id = aws_security_group.exchange-server.id
+  type              = "ingress"
+  description       = "allow bastion"
+  from_port         = 3389
+  to_port           = 3389
+  protocol          = "TCP"
+  cidr_blocks       = ["${module.bastion_linux.bastion_private_ip}/32"]
+}
 
-
+resource "aws_eip" "exchange" {
+  instance = aws_instance.exchange-server.id
+  vpc      = true
+}
 
 resource "aws_instance" "exchange-server" {
   depends_on                  = [aws_security_group.exchange-server]
