ce7c0841a8f74eefdf90db477ee3fffbb5085ab6
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-02-10 16:22:31 +0000

    Destroy everything :)

--- terraform/environments/nomis/modules/nomis_stack/user_data/weblogic_init.sh
+++ terraform/environments/nomis/modules/nomis_stack/user_data/weblogic_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-su -c "export USE_DEFAULT_CREDS=${USE_DEFAULT_CREDS} DB_HOSTNAME=${DB_HOSTNAME} ENV=${ENV}; bash /u01/software/weblogic/weblogic-setup.sh" - oracle
+# su -c "export USE_DEFAULT_CREDS=${USE_DEFAULT_CREDS} DB_HOSTNAME=${DB_HOSTNAME} ENV=${ENV}; bash /u01/software/weblogic/weblogic-setup.sh" - oracle
+su -c "export USE_DEFAULT_CREDS='true' DB_HOSTNAME=db.t1.nomis.hmpps-test.modernisation-platform.service.justice.gov.uk ENV=T1; bash /u01/software/weblogic/weblogic-setup.sh" - oracle
 
 # Create nomis_weblogic service
 chkconfig --add nomis_weblogic
