54884aff2c6bec359ff9213902b47cdf6819fd83
George Cairns <george.cairns@digital.justice.gov.uk>
2022-02-14 16:25:52 +0000

    back to version 4 to see if permissions errors go away

--- terraform/environments/xhibit-portal/versions.tf
+++ terraform/environments/xhibit-portal/versions.tf
@@ -1,9 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      version = "< 4.0.0"
+      version = ">= 3.47.0"
       source  = "hashicorp/aws"
     }
   }
   required_version = ">= 1.0.1"
 }
+
+
