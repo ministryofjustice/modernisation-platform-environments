6df149b550a84d29ef3ccb41c56889595f09aa24
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2022-02-15 14:58:20 +0000

    removing redundant r53 record

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -250,24 +250,6 @@ resource "aws_acm_certificate" "waf_lb_cert" {
   }
 }
 
-resource "aws_route53_record" "waf_lb_r53_record" {
-  provider = aws.core-vpc
-  for_each = {
-    for dvo in aws_acm_certificate.waf_lb_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.external_r53_zone.zone_id
-}
-
 resource "aws_acm_certificate_validation" "waf_lb_cert_validation" {
   certificate_arn = aws_acm_certificate.waf_lb_cert.arn
   //validation_record_fqdns = [for record in aws_route53_record.waf_lb_r53_record : record.fqdn]
