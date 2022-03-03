ebe10f5990b836206657af0cf0a0a3b4d1396d90
garygrantjustice <98090448+garygrantjustice@users.noreply.github.com>
2022-03-03 15:43:16 +0000

    fiximportmachine-changeinstancetype

--- terraform/environments/xhibit-portal/importmachine.tf
+++ terraform/environments/xhibit-portal/importmachine.tf
@@ -43,7 +43,7 @@ resource "aws_key_pair" "george" {
 
 resource "aws_instance" "importmachine" {
   depends_on                  = [aws_security_group.importmachine]
-  instance_type               = "t3.large"
+  instance_type               = "t3a.large"
   ami                         = local.application_data.accounts[local.environment].importmachine-ami
   vpc_security_group_ids      = [aws_security_group.importmachine.id]
   monitoring                  = false
