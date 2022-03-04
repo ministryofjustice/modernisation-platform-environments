ee2870dbfcc26354547aa6fc6eaae4531432f2b6
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2022-03-04 15:45:34 +0000

    changed to Anthony's public  IP

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -44,7 +44,7 @@ resource "aws_security_group_rule" "allow_web_users" {
     "20.49.163.194/32",  # Azure function proxy
     "20.49.163.244/32",  # Azure function proxy
     "82.44.118.20/32",   # Nick
-    "10.175.52.4/32",    # Anthony Fletcher
+    "194.33.192.4/32",   # Anthony Fletcher
     "10.182.60.51/32",   # NLE CGI proxy 
     "10.175.165.159/32", # Helen Dawes
     "10.175.72.157/32",  # Alan Brightmore
