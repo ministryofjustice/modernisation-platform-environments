80de84ae753b9bf699dd34a5d1346cadc1ec9503
garygrantjustice <98090448+garygrantjustice@users.noreply.github.com>
2022-03-05 00:15:26 +0000

    Update importmachine.tf

--- terraform/environments/xhibit-portal/importmachine.tf
+++ terraform/environments/xhibit-portal/importmachine.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "importmachine" {
   monitoring                  = false
   ebs_optimized               = true
   subnet_id                   = data.aws_subnet.private_az_a.id
-  key_name                    = aws_key_pair.gary.key_name
+  key_name                    = aws_key_pair.george.key_name
 
   metadata_options {
     http_tokens   = "required"
