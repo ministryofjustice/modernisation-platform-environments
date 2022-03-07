e77a00ab69c3d598327a608f72146fb8a9f3f9b0
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-03-07 08:12:59 +0000

    Adding CMK

--- terraform/environments/nomis/cmk.tf
+++ terraform/environments/nomis/cmk.tf
@@ -1,7 +1,7 @@
 resource "aws_kms_key" "nomis-cmk" {
   description             = "Nomis Managed Key for AMI Sharing"
   deletion_window_in_days = 10
-  policy                  = file("${path.module}files/policy.json")
+  policy                  = file("${path.module}/files/policy.json")
 }
 
 resource "aws_kms_alias" "nomis-key" {
