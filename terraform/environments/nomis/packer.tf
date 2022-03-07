1c617c57d58bed72db7a2651f05d9377c3710fdb
julialawrence <98329494+julialawrence@users.noreply.github.com>
2022-03-07 12:05:07 +0000

    Performance Hub: CannotPullContainerError: a Windows version 10.0.20348-based image is incompatible with a 10.0.17763 host
    Updating ECS host to Windows 2022

--- terraform/environments/nomis/packer.tf
+++ terraform/environments/nomis/packer.tf
@@ -238,7 +238,8 @@ data "aws_iam_policy_document" "packer_ansible_permissions" {
       "ec2:DescribeInstanceTypes",
       "ec2:DescribeVpcs",
       "ec2:DescribeKeyPairs",
-      "sts:DecodeAuthorizationMessage"
+      "sts:DecodeAuthorizationMessage",
+      "kms:ReEncrypt*" # for building from cmk-encrypted AMIs
     ]
     resources = ["*"]
   }
