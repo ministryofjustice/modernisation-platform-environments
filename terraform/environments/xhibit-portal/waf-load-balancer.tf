ad1d2fa8782d59cfb49a1679cdf6459821447c18
George Cairns <george.cairns@digital.justice.gov.uk>
2022-03-09 15:45:31 +0000

    working on lb and cert

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -192,7 +192,7 @@ resource "aws_lb_listener" "waf_lb_listener" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.ingestion_cert.arn
+  certificate_arn   = data.aws_acm_certificate.ingestion_cert.arn
 
   default_action {
     type             = "forward"
