d1eb98e2dacccfc1142b23288c96ccc94fb52f4e
George Cairns <george.cairns@digital.justice.gov.uk>
2022-03-10 17:03:39 +0000

    working on cf

--- terraform/environments/xhibit-portal/waf-load-balancer.tf
+++ terraform/environments/xhibit-portal/waf-load-balancer.tf
@@ -318,18 +318,15 @@ data "aws_route53_zone" "external_r53_zone" {
 }
 
 resource "aws_route53_record" "waf_lb_cname" {
-  # ingest.cjsonline.gov.uk points at this name. We can then point this name at whatever we like.
-
   provider = aws.core-vpc
 
   zone_id = data.aws_route53_zone.external_r53_zone.zone_id
   name    = "lb.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk."
-  type    = "CNAME"
+  type    = "A"
 
   alias {
     name                   = aws_lb.waf_lb.dns_name
     zone_id                = aws_lb.waf_lb.zone_id
-    evaluate_target_health = true
   }
 
 }
@@ -343,10 +340,6 @@ resource "aws_acm_certificate" "waf_lb_cert" {
     local.application_data.accounts[local.environment].public_dns_name_web,
     # local.application_data.accounts[local.environment].public_dns_name_ingestion,
   ]
-
-  tags = {
-    Environment = "prod"
-  }
 
   lifecycle {
     create_before_destroy = true
@@ -378,7 +371,6 @@ resource "aws_route53_record" "waf_lb_r53_record" {
   type            = each.value.type
   zone_id         = data.aws_route53_zone.external_r53_zone.zone_id
 }
-
 
 
 resource "aws_wafv2_web_acl" "waf_acl" {
