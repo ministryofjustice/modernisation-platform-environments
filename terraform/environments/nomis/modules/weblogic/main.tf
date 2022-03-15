d3878f845a2ea0d418c6aa1c99af9cb4bcb87f6c
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2022-03-15 13:40:24 +0000

    attach policy to role

--- terraform/environments/nomis/modules/weblogic/main.tf
+++ terraform/environments/nomis/modules/weblogic/main.tf
@@ -367,6 +367,12 @@ resource "aws_iam_role" "weblogic" {
       ]
     }
   )
+
+  inline_policy {
+    name = "weblogic-policy"
+    policy = data.aws_iam_policy_document.weblogic
+  }
+
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
     var.instance_profile_policy_arn
