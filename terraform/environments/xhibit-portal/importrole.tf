3b6096ff88ab8dfc908a845ce6ce0b6a74dae17d
David Sibley <david.sibley@digital.justice.gov.uk>
2022-03-16 10:56:38 +0000

    updated reference to downstream version of import module

--- terraform/environments/xhibit-portal/importrole.tf
+++ terraform/environments/xhibit-portal/importrole.tf
@@ -1,7 +1,6 @@
 module "vm-import" {
 
-  #source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-vm-import?ref=412c8e6d7a4c33c51c9ad6fa739857e64f405804"
-  source           = "../../../../modernisation-platform-terraform-aws-vm-import"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-vm-import?ref=v1.0.0"
   bucket_prefix    = local.application_data.accounts[local.environment].bucket_prefix
   tags             = local.tags
   application_name = local.application_name
