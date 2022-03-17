35d06608daa5d1ec95faf0f78a928e8488c4abc9
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2022-03-17 14:52:42 +0000

    test weblogic image with additional ebs volumes defined inline with instance resource

--- terraform/environments/nomis/modules/nomis_stack/ec2_weblogic.tf
+++ terraform/environments/nomis/modules/nomis_stack/ec2_weblogic.tf
@@ -67,14 +67,14 @@ resource "aws_instance" "weblogic_server" {
     volume_type           = "gp3"
   }
 
-  dynamic "ephemeral_block_device" { # block devices specified inline cannot be resized later so we need to make sure they are not mounted here
-    for_each = [for bdm in data.aws_ami.weblogic_image.block_device_mappings : bdm if bdm.device_name != data.aws_ami.weblogic_image.root_device_name]
-    iterator = device
-    content {
-      device_name = device.value.device_name
-      no_device   = true
-    }
-  }
+  # dynamic "ephemeral_block_device" { # block devices specified inline cannot be resized later so we need to make sure they are not mounted here
+  #   for_each = [for bdm in data.aws_ami.weblogic_image.block_device_mappings : bdm if bdm.device_name != data.aws_ami.weblogic_image.root_device_name]
+  #   iterator = device
+  #   content {
+  #     device_name = device.value.device_name
+  #     no_device   = true
+  #   }
+  # }
 
   tags = merge(
     var.tags,
@@ -88,32 +88,32 @@ resource "aws_instance" "weblogic_server" {
   )
 }
 
-resource "aws_ebs_volume" "weblogic_ami_volume" {
-  for_each = { for bdm in data.aws_ami.weblogic_image.block_device_mappings : bdm.device_name => bdm if bdm.device_name != data.aws_ami.weblogic_image.root_device_name }
+# resource "aws_ebs_volume" "weblogic_ami_volume" {
+#   for_each = { for bdm in data.aws_ami.weblogic_image.block_device_mappings : bdm.device_name => bdm if bdm.device_name != data.aws_ami.weblogic_image.root_device_name }
 
-  availability_zone = "${var.region}a"
-  encrypted         = true
-  iops              = each.value["ebs"]["iops"]
-  snapshot_id       = each.value["ebs"]["snapshot_id"]
-  size              = lookup(var.weblogic_drive_map, each.value["device_name"], each.value["ebs"]["volume_size"])
-  type              = each.value["ebs"]["volume_type"] == "gp2" ? "gp3" : each.value["ebs"]["volume_type"]
+#   availability_zone = "${var.region}a"
+#   encrypted         = true
+#   iops              = each.value["ebs"]["iops"]
+#   snapshot_id       = each.value["ebs"]["snapshot_id"]
+#   size              = lookup(var.weblogic_drive_map, each.value["device_name"], each.value["ebs"]["volume_size"])
+#   type              = each.value["ebs"]["volume_type"] == "gp2" ? "gp3" : each.value["ebs"]["volume_type"]
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "weblogic-${var.stack_name}-${each.value.device_name}"
-    }
-  )
-}
+#   tags = merge(
+#     var.tags,
+#     {
+#       Name = "weblogic-${var.stack_name}-${each.value.device_name}"
+#     }
+#   )
+# }
 
-resource "aws_volume_attachment" "weblogic_ami_volume" {
-  for_each = aws_ebs_volume.weblogic_ami_volume
+# resource "aws_volume_attachment" "weblogic_ami_volume" {
+#   for_each = aws_ebs_volume.weblogic_ami_volume
 
-  device_name  = each.key
-  volume_id    = each.value.id
-  instance_id  = aws_instance.weblogic_server.id
-  force_detach = true
-}
+#   device_name  = each.key
+#   volume_id    = each.value.id
+#   instance_id  = aws_instance.weblogic_server.id
+#   force_detach = true
+# }
 
 #------------------------------------------------------------------------------
 # Route 53 record
