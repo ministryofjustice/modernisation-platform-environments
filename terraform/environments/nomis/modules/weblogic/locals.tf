da6dd7fe22d918fd23c8b697dbcb8bb727d11dc6
Rob Whittle <69803842+rwhittlemoj@users.noreply.github.com>
2022-03-18 12:14:25 +0000

    Nomis/application module (#451)
    * some changes to database module to add delete protection option and remove unecessary lifecyle rules
    
    * add lifecycle rules to DATA and FLASH ebs volumes to retain them when AMI is updated
    
    * ASG for weblogic
    
    * add user data
    
    * formatted
    
    * correct some wording in vars
    
    * hard coding DB hostname for now
    
    * fix target group naming and user data encoding
    
    * Commit changes made by code formatters
    
    * add volume size
    
    * formatting
    
    * fix lb target group type
    
    * need to rotate random string each time lb target group replaced
    
    * revert to name prefix for lb target group
    
    * Commit changes made by code formatters
    
    * update provider version to match main
    
    * weblogics take an age to initialise so added a warm pool with lifecycle hooks to notify when the cloud-init process has finished
    
    * correct the region
    
    * some IAM policies required to trigger lifecycle, also modify parameter store access condition
    
    * Commit changes made by code formatters
    
    * need to ensure instance profile is updated with required IAM permissions before the ASG is created
    
    * fix sids
    
    * some policy amendments required, also prevent terraform for waiting for weblogic status checks
    
    * Commit changes made by code formatters
    
    * managing permissions for ASG instances is a bit tricky so creating a dedicated instance profile for those instances
    
    * hopefully fix dependency between instance profile and ASG
    
    * attach policy to role
    
    * Commit changes made by code formatters
    
    * forgot about jason
    
    * Commit changes made by code formatters
    
    * test creating with a target database
    
    * adjust lifecycle timeout and added rudimentary schedule based scaling and add README
    
    * add readme for database module
    
    * amended user-data script to ensure lifecycle hook is triggered when an instance state is restarted on exit from the warm pool
    
    * correct some wrong information in the database module readme
    
    * testing scale up of instances to ensure correctly exit from warm pool.  Also added to readme and added new Weblogic AMI to the nomis stack
    
    * Commit changes made by code formatters
    
    * fix up weblogic AMI name to pull latest
    
    * test weblogic image with additional ebs volumes defined inline with instance resource
    
    * I think we need target group stickiness enabled, at least that appears to be how it is currently set up in Azure
    
    * Commit changes made by code formatters
    
    * random change to user-data to trigger weblogic rebuild
    
    * nuclear option
    
    * remember to terraform validate before you push changes!
    
    * back to the imagebuilder weblogic image
    
    * tidy up and remove nomis-stack module
    
    * skip a few security checks.  We have a ticket to review all of these so only added a few that are unavoidable
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ terraform/environments/nomis/modules/weblogic/locals.tf
@@ -0,0 +1,45 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "weblogic" {
+  most_recent = true
+  owners      = [var.ami_owner]
+
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_ec2_instance_type" "weblogic" {
+  instance_type = var.instance_type
+}
+
+data "aws_route53_zone" "internal" {
+  provider = aws.core-vpc
+
+  name         = "${var.business_unit}-${var.environment}.modernisation-platform.internal."
+  private_zone = true
+}
+
+data "aws_route53_zone" "external" {
+  provider = aws.core-vpc
+
+  name         = "${var.business_unit}-${var.environment}.modernisation-platform.service.justice.gov.uk."
+  private_zone = false
+}
+
+locals {
+  # region = substr(var.availability_zone, 0, length(var.availability_zone) - 1)
+  ebs_optimized    = data.aws_ec2_instance_type.weblogic.ebs_optimized_support == "unsupported" ? false : true
+  block_device_map = { for bdm in data.aws_ami.weblogic.block_device_mappings : bdm.device_name => bdm }
+  root_device_size = one([for bdm in data.aws_ami.weblogic.block_device_mappings : bdm.ebs.volume_size if bdm.device_name == data.aws_ami.weblogic.root_device_name])
+
+  # Auto-scaling group related locals
+  initial_lifecycle_hook_name = "weblogic-${var.name}-ready-hook"
+  auto_scaling_group_name     = "weblogic-${var.name}"
+}
