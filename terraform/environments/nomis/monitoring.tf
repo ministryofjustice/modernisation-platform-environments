259d2c90ea507039bb92a3a9be34f616e1ecca13
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-03-23 16:52:37 +0000

    Create Prometheus Programmatic User

---
+++ terraform/environments/nomis/monitoring.tf
@@ -0,0 +1,21 @@
+#------------------------------------------------------------------------------
+# Prometheus Cloud Platform User
+# Used to monitor mod-platform resources from the Cloud Platform cluster
+# See Cloud Platform namespace "dso-monitoring-prod"
+#------------------------------------------------------------------------------
+
+resource "aws_iam_user" "prometheus_cp_user" {
+  name = "prometheus_cp_user"
+}
+
+resource "aws_iam_access_key" "prometheus_cp_user_key" {
+  user = aws_iam_user.prometheus_cp_user.name
+}
+
+output "secret_key" {
+  value = aws_iam_access_key.prometheus_cp_user_key.secret
+}
+
+output "access_key" {
+  value = aws_iam_access_key.prometheus_cp_user_key.id
+}
