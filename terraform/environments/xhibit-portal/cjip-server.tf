923406307cbecfbf423eb78ebcf7f867c61267ba
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2022-03-24 17:30:38 +0000

    Revert "Upgrading environment to t3.large as a test"
    This reverts commit 92689c8bb7be717f6db9d32e6fcc7087afb2e666.

--- terraform/environments/xhibit-portal/cjip-server.tf
+++ terraform/environments/xhibit-portal/cjip-server.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "cjip-server" {
   depends_on                  = [aws_security_group.ingestion_server]
-  instance_type               = "t3.large"
+  instance_type               = "t2.medium"
   ami                         = local.application_data.accounts[local.environment].suprig05-ami
   vpc_security_group_ids      = [aws_security_group.ingestion_server.id]
   monitoring                  = false
