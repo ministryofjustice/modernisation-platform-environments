aa111f0820eb556f56c866e8b14dce5ba9ff2bfb
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-03-25 13:03:08 +0000

    Create Prometheus programmatic user

--- terraform/environments/nomis/monitoring.tf
+++ terraform/environments/nomis/monitoring.tf
@@ -12,11 +12,51 @@ resource "aws_iam_access_key" "prometheus_cp_user_key" {
   user = aws_iam_user.prometheus_cp_user.name
 }
 
-output "secret_key" {
-  value     = aws_iam_access_key.prometheus_cp_user_key.secret
-  sensitive = true
+resource "aws_iam_user_policy" "policy" {
+  name        = "EC2-Read-Only"
+  user        = aws_iam_user.prometheus_cp_user.name
+  description = "AmazonEC2ReadOnlyAccess for Prometheus user"
+  policy      = <<EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "ec2:Describe*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "elasticloadbalancing:Describe*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:ListMetrics",
+                "cloudwatch:GetMetricStatistics",
+                "cloudwatch:Describe*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "autoscaling:Describe*",
+            "Resource": "*"
+        }
+    ]
+}
+EOT
 }
 
-output "access_key" {
-  value = aws_iam_access_key.prometheus_cp_user_key.id
+resource "github_actions_secret" "prometheus_key_id" {
+  repository      = "dso-monitoring"
+  secret_name     = "prometheus_key_id"
+  plaintext_value = aws_iam_access_key.prometheus_cp_user_key.id
+}
+
+resource "github_actions_secret" "prometheus_secret_key" {
+  repository      = "dso-monitoring"
+  secret_name     = "prometheus_secret_key"
+  plaintext_value = aws_iam_access_key.prometheus_cp_user_key.secret
 }
