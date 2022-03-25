b0027fb34dbbd0087e0b7a3330ec97f1098b7f4e
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2022-03-25 18:01:01 +0000

    Opening SMS server ports to fix XP SMS

--- terraform/environments/xhibit-portal/network-infrastructure.tf
+++ terraform/environments/xhibit-portal/network-infrastructure.tf
@@ -179,7 +179,8 @@ resource "aws_security_group_rule" "sms-inbound-all" {
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
-  source_security_group_id = aws_security_group.app_servers.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
 }
 
 resource "aws_security_group_rule" "sms-outbound-all-ipv4" {
