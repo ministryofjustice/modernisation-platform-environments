a85dda1840545a88f4fe663e6d84eaaf49ed1e18
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-03-28 11:16:20 +0100

    Remove Github provider from modules

--- terraform/environments/nomis/modules/weblogic/versions.tf
+++ terraform/environments/nomis/modules/weblogic/versions.tf
@@ -5,10 +5,6 @@ terraform {
       source                = "hashicorp/aws"
       configuration_aliases = [aws.core-vpc]
     }
-    github = {
-      version = "4.9.4"
-      source  = "integrations/github"
-    }
   }
   required_version = ">= 1.1.7"
 }
