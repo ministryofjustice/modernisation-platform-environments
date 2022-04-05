9e122abdfafb2bffac7ce5fc89b0fdd243169d6e
vadisalar <ravi.vadisala@techddata.com>
2022-04-05 21:13:10 +0530

    Modified Citrix Instance to launch in Private

--- terraform/environments/equip/citrix_adc.tf
+++ terraform/environments/equip/citrix_adc.tf
@@ -5,11 +5,14 @@ variable "additional_tags" {
 }
 
 resource "aws_instance" "citrix_adc_instance" {
-  ami           = "ami-0dd0aa051b3fc4e4b"
-  instance_type = "m5.xlarge"
-  key_name      = aws_key_pair.windowskey.key_name
-  monitoring    = true
-  ebs_optimized = true
+  ami                    = "ami-0dd0aa051b3fc4e4b"
+  instance_type          = "m5.xlarge"
+  key_name               = aws_key_pair.windowskey.key_name
+  subnet_id              = data.aws_subnet.private_subnets_a.id
+  vpc_security_group_ids = [aws_security_group.citrix_adc_security_group.id]
+  iam_instance_profile   = aws_iam_instance_profile.instance-profile-moj.name
+  monitoring             = true
+  ebs_optimized          = true
 
   root_block_device {
     encrypted   = true
@@ -22,59 +25,12 @@ resource "aws_instance" "citrix_adc_instance" {
     http_tokens   = "required"
   }
 
-  network_interface {
-    network_interface_id = aws_network_interface.public_interface.id
-    device_index         = 0
-  }
-
   tags = {
     Name = "NPS-COR-A-ADC01"
     ROLE = "Citrix Netscaler ADC VPX"
   }
 }
 
-
-resource "aws_network_interface" "public_interface" {
-  subnet_id       = data.aws_subnet.public_az_a.id
-  security_groups = [aws_security_group.citrix_adc_security_group.id]
-
-  tags = {
-    Name        = "Citrix ADC VPX External Interface"
-    Description = "External Interface for Citrix ADC"
-  }
-}
-
-resource "null_resource" "previous" {}
-
-resource "time_sleep" "wait_period" {
-  depends_on = [
-    aws_instance.citrix_adc_instance,
-    aws_network_interface.public_interface,
-    null_resource.previous
-  ]
-
-  create_duration = "300s"
-}
-
-resource "aws_network_interface" "private_interface" {
-  subnet_id       = data.aws_subnet.private_subnets_a.id
-  security_groups = [aws_security_group.citrix_adc_security_group.id]
-
-  depends_on = [
-    aws_network_interface.public_interface,
-    time_sleep.wait_period
-  ]
-
-  attachment {
-    instance     = aws_instance.citrix_adc_instance.id
-    device_index = 1
-  }
-
-  tags = {
-    Name        = "Private Network Internal Interface"
-    Description = "Private Interface for Citrix ADC"
-  }
-}
 
 resource "aws_security_group" "citrix_adc_security_group" {
   #checkov:skip=CKV_AWS_24: Attaching using Module
