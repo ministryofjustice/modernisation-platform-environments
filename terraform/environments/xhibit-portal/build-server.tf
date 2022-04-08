1f7bfb0bedbe93180b426087843cdc8be152a3d9
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2022-04-08 14:10:28 +0200

    removing prevent destroy on build server

--- terraform/environments/xhibit-portal/build-server.tf
+++ terraform/environments/xhibit-portal/build-server.tf
@@ -32,7 +32,6 @@ resource "aws_instance" "build-server" {
       #user_data,         # Prevent changes to user_data from destroying existing EC2s
       # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
     ]
-    prevent_destroy = true
   }
 
   tags = merge(
