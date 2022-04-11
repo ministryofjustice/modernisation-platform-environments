d31e7b3510719cea7993d00a2f8dd16bc431d9e6
Aman Chopra <aman.chopra@digital.justice.gov.uk>
2022-04-11 23:23:38 +0100

    Reverted prevent_destroy flags

--- terraform/environments/xhibit-portal/database-server.tf
+++ terraform/environments/xhibit-portal/database-server.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "database-server" {
       #user_data,         # Prevent changes to user_data from destroying existing EC2s
       # Prevent changes to encryption from destroying existing EC2s - can delete once encryption complete
     ]
-    prevent_destroy = true
+    #prevent_destroy = true
   }
 
   tags = merge(
