492995684fe51b80de3f0eef931dec6bf1a7bbfb
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-04-12 13:04:11 +0100

    fix path

--- scripts/generate-nuke-config.sh
+++ scripts/generate-nuke-config.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 eval "$(jq -r '.NUKE_ACCOUNT_IDS | to_entries | .[] |"export " + .key + "=" + (.value | @sh)' <<<"$NUKE_ACCOUNT_IDS")"
-cat ./scripts/nuke-config-template.txt | envsubst >./scripts/nuke-config.yml
+cat ./scripts/nuke-config-template.txt | envsubst >nuke-config.yml
--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 TF_ENV='development'
 bash scripts/terraform-init.sh terraform/environments/sprinkler
 terraform -chdir="terraform/environments/sprinkler" workspace select "sprinkler-${TF_ENV}"
-$HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" --secret-access-key "$AWS_SECRET_ACCESS_KEY" nuke-config.yml
+$HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" --secret-access-key "$AWS_SECRET_ACCESS_KEY" --config nuke-config.yml
