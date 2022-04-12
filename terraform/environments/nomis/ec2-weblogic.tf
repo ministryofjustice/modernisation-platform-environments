e3798cbc27604fa3859e4925c73339e8ee955d9a
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-04-12 14:08:13 +0100

    Typo

--- terraform/environments/nomis/ec2-weblogic.tf
+++ terraform/environments/nomis/ec2-weblogic.tf
@@ -80,7 +80,7 @@ resource "aws_security_group" "weblogic_common" {
   ingress {
     description = "access from Cloud Platform Prometheus server"
     from_port   = "9100"
-    to_port     = "9090"
+    to_port     = "9100"
     protocol    = "TCP"
     cidr_blocks = ["172.20.0.0/16"]
   }
