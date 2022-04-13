5142206ff9cd3b78e637e1f611b7f6ab54327333
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-04-13 16:54:11 +0100

    experimenting

--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -13,10 +13,6 @@ export AWS_REGION=eu-west-2
 
 eval $(aws sts assume-role --role-arn "arn:aws:iam::${SPRINKLER_DEVELOPMENT_ACCID}:role/MemberInfrastructureAccess" --role-session-name "SPRINKLER_DEVELOPMENT_ACCID_SESSION" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 
-echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
-echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
-echo "AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN"
-
 $HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" \
   --secret-access-key "$AWS_SECRET_ACCESS_KEY" \
   --session-token "$AWS_SESSION_TOKEN" \
