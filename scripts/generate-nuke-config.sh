1fe5d6d81348119a61c80330ecba672e44006264
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-04-13 10:34:52 +0100

    assume role

--- scripts/generate-nuke-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-eval "$(jq -r '.NUKE_ACCOUNT_IDS | to_entries | .[] |"export " + .key + "=" + (.value | @sh)' <<<"$NUKE_ACCOUNT_IDS")"
-cat ./scripts/nuke-config-template.txt | envsubst >nuke-config.yml
--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -2,9 +2,15 @@
 
 set -euo pipefail
 
-./scripts/generate-nuke-config.sh
+# Parse the env variable NUKE_ACCOUNT_IDS which holds a JSON string of account IDs.
+# Export the account IDs to env variables, for example: export SPRINKLER_DEVELOPMENT_ACCID=111111111111
+eval "$(jq -r '.NUKE_ACCOUNT_IDS | to_entries | .[] |"export " + .key + "=" + (.value | @sh)' <<<"$NUKE_ACCOUNT_IDS")"
 
-TF_ENV='development'
-bash scripts/terraform-init.sh terraform/environments/sprinkler
-terraform -chdir="terraform/environments/sprinkler" workspace select "sprinkler-${TF_ENV}"
+# Generate nuke-config.yml interpolating env variables with account IDs.
+cat ./scripts/nuke-config-template.txt | envsubst >nuke-config.yml
+
+aws sts assume-role \
+  --role-arn "arn:aws:iam::${SPRINKLER_DEVELOPMENT_ACCID}:role/MemberInfrastructureAccess" \
+  --role-session-name SPRINKLER_DEVELOPMENT_ACCID
+
 $HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" --secret-access-key "$AWS_SECRET_ACCESS_KEY" --config nuke-config.yml
