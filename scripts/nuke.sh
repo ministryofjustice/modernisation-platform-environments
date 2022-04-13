8ad9543fedd41d44f25b2718c8c5abd67939a6ff
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-04-13 10:54:38 +0100

    redact output

--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -12,6 +12,6 @@ cat ./scripts/nuke-config-template.txt | envsubst >nuke-config.yml
 aws sts assume-role \
   --role-arn "arn:aws:iam::${SPRINKLER_DEVELOPMENT_ACCID}:role/MemberInfrastructureAccess" \
   --role-session-name SPRINKLER_DEVELOPMENT_ACCID \
-  --region eu-west-2
+  --region eu-west-2 | ./scripts/redact-output.sh
 
-$HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" --secret-access-key "$AWS_SECRET_ACCESS_KEY" --config nuke-config.yml
+$HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" --secret-access-key "$AWS_SECRET_ACCESS_KEY" --config nuke-config.yml | ./scripts/redact-output.sh
