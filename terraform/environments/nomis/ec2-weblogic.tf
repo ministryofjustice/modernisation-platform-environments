de8cec14c01220363d5929a38d28d5e91392a03b
dteelingmoj <david.teeling@digital.justice.gov.uk>
2022-04-13 15:04:17 +0100

    Use variable

--- terraform/environments/nomis/ec2-weblogic.tf
+++ terraform/environments/nomis/ec2-weblogic.tf
@@ -82,7 +82,7 @@ resource "aws_security_group" "weblogic_common" {
   from_port   = "9100"
   to_port     = "9100"
   protocol    = "TCP"
-  cidr_blocks = ["172.20.0.0/16"]
+  cidr_blocks = [local.application_data.accounts[local.environment].database_external_access_cidr.cloud_platform]
 }
 
   egress {
