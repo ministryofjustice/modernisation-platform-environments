d496ab4cd022b416ac1b5efca838626b2c4df99a
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-04-20 18:06:29 +0100

    added error handling

--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -euo pipefail
-
 # Parse the env variable NUKE_ACCOUNT_IDS which holds a JSON string of account IDs. Example value held in
 # NUKE_ACCOUNT_IDS is as follows
 #    NUKE_ACCOUNT_IDS='
@@ -19,30 +17,46 @@ cat ./scripts/nuke-config-template.txt | envsubst >nuke-config.yml
 
 export AWS_REGION=eu-west-2
 
+FAILED_ENVS=()
 for d in terraform/environments/*; do
+  EXIT_CODE=0
   dir_name=$(basename "$d")
   dir_name_upper_case=${dir_name^^}
   echo "Search for development workspace to nuke in $d"
-  bash scripts/terraform-init.sh "$d"
-  tf_workspaces=$(terraform -chdir="$d" workspace list)
-  if [[ "$tf_workspaces" == *"${dir_name}-development"* ]]; then
-    if [[ "$NUKE_SKIP_ENVIRONMENTS" != *"${dir_name}-development"* ]]; then
-      echo "BEGIN: nuke ${dir_name}-development"
-      acc_id_var_name="${dir_name_upper_case}_DEVELOPMENT_ACCID"
-      acc_id=${!acc_id_var_name}
-      eval $(aws sts assume-role --role-arn "arn:aws:iam::${acc_id}:role/MemberInfrastructureAccess" --role-session-name "${dir_name_upper_case}_DEVELOPMENT_ACCID_SESSION" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
-
-      $HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" \
-        --secret-access-key "$AWS_SECRET_ACCESS_KEY" \
-        --session-token "$AWS_SESSION_TOKEN" \
-        --config nuke-config.yml \
-        --force \
-        --no-dry-run
-      echo "END: nuke ${dir_name}-development"
-    else
-      echo "Skipped nuking ${dir_name}-development because of the env variable NUKE_SKIP_ENVIRONMENTS=${NUKE_SKIP_ENVIRONMENTS}"
-    fi
+  bash scripts/terraform-init.sh "$d" || EXIT_CODE=$?
+  tf_workspaces=$(terraform -chdir="$d" workspace list) || EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    FAILED_ENVS+=("$dir_name")
   else
-    echo "No development workspace was found to nuke in $d"
+    if [[ "$tf_workspaces" == *"${dir_name}-development"* ]]; then
+      if [[ "$NUKE_SKIP_ENVIRONMENTS" != *"${dir_name}-development"* ]]; then
+        echo "BEGIN: nuke ${dir_name}-development"
+        acc_id_var_name="${dir_name_upper_case}_DEVELOPMENT_ACCID"
+        acc_id=${!acc_id_var_name} # Evaluate the variable name stored in acc_id_var_name
+        eval $(aws sts assume-role --role-arn "arn:aws:iam::${acc_id}:role/MemberInfrastructureAccess" --role-session-name "${dir_name_upper_case}_DEVELOPMENT_ACCID_SESSION" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+
+        $HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" \
+          --secret-access-key "$AWS_SECRET_ACCESS_KEY" \
+          --session-token "$AWS_SESSION_TOKEN" \
+          --config nuke-config.yml \
+          --force \
+          --no-dry-run || EXIT_CODE=$?
+
+        if [ $EXIT_CODE -ne 0 ]; then
+          FAILED_ENVS+=("${dir_name}-development")
+        fi
+        echo "END: nuke ${dir_name}-development"
+      else
+        echo "Skipped nuking ${dir_name}-development because of the env variable NUKE_SKIP_ENVIRONMENTS=${NUKE_SKIP_ENVIRONMENTS}"
+      fi
+    else
+      echo "No development workspace was found to nuke in $d"
+    fi
   fi
 done
+
+if [ ${#FAILED_ENVS[@]} -ne 0 ]; then
+  echo "ERROR: could not nuke environments: ${FAILED_ENVS[@]}"
+  echo "Refer to previous errors for details."
+  exit 1
+fi
