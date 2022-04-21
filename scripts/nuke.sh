eeabf9cf93ce3340893c92d31bfcceb8895c5bf6
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-04-21 10:49:19 +0100

    - Rename actions - Allow skipping entire directories in addition to just specific environments

--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -19,39 +19,43 @@ export AWS_REGION=eu-west-2
 
 failed_envs=()
 for d in terraform/environments/*; do
-  exit_code=0
   dir_name=$(basename "$d")
-  dir_name_upper_case=$(echo "${dir_name^^}" | tr '-' '_')
-  echo "Search for development workspace to nuke in $d"
-  bash scripts/terraform-init.sh "$d" || exit_code=$?
-  tf_workspaces=$(terraform -chdir="$d" workspace list) || exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-    failed_envs+=("$dir_name")
-  else
-    if [[ "$tf_workspaces" == *"${dir_name}-development"* ]]; then
-      if [[ "$NUKE_SKIP_ENVIRONMENTS" != *"${dir_name}-development"* ]]; then
-        echo "BEGIN: nuke ${dir_name}-development"
-        acc_id_var_name="${dir_name_upper_case}_DEVELOPMENT_ACCID"
-        acc_id=${!acc_id_var_name} # Evaluate the variable name stored in acc_id_var_name
-        eval $(aws sts assume-role --role-arn "arn:aws:iam::${acc_id}:role/MemberInfrastructureAccess" --role-session-name "${dir_name_upper_case}_DEVELOPMENT_ACCID_SESSION" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
-
-        $HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" \
-          --secret-access-key "$AWS_SECRET_ACCESS_KEY" \
-          --session-token "$AWS_SESSION_TOKEN" \
-          --config nuke-config.yml \
-          --force \
-          --no-dry-run || exit_code=$?
-
-        if [ $exit_code -ne 0 ]; then
-          failed_envs+=("${dir_name}-development")
-        fi
-        echo "END: nuke ${dir_name}-development"
-      else
-        echo "Skipped nuking ${dir_name}-development because of the env variable NUKE_SKIP_ENVIRONMENTS=${NUKE_SKIP_ENVIRONMENTS}"
-      fi
+  if [[ "$NUKE_SKIP_ENVIRONMENTS" != *"${dir_name},"* ]]; then
+    echo "Search for development workspace to nuke in $d"
+    exit_code=0
+    bash scripts/terraform-init.sh "$d" || exit_code=$?
+    tf_workspaces=$(terraform -chdir="$d" workspace list) || exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+      failed_envs+=("$dir_name")
     else
-      echo "No development workspace was found to nuke in $d"
+      if [[ "$tf_workspaces" == *"${dir_name}-development"* ]]; then
+        if [[ "$NUKE_SKIP_ENVIRONMENTS" != *"${dir_name}-development,"* ]]; then
+          echo "BEGIN: nuke ${dir_name}-development"
+          dir_name_upper_case=$(echo "${dir_name^^}" | tr '-' '_')
+          acc_id_var_name="${dir_name_upper_case}_DEVELOPMENT_ACCID"
+          acc_id=${!acc_id_var_name} # Evaluate the variable name stored in acc_id_var_name
+          eval $(aws sts assume-role --role-arn "arn:aws:iam::${acc_id}:role/MemberInfrastructureAccess" --role-session-name "${dir_name_upper_case}_DEVELOPMENT_ACCID_SESSION" | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+
+          $HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" \
+            --secret-access-key "$AWS_SECRET_ACCESS_KEY" \
+            --session-token "$AWS_SESSION_TOKEN" \
+            --config nuke-config.yml \
+            --force \
+            --no-dry-run || exit_code=$?
+
+          if [ $exit_code -ne 0 ]; then
+            failed_envs+=("${dir_name}-development")
+          fi
+          echo "END: nuke ${dir_name}-development"
+        else
+          echo "Skipped nuking ${dir_name}-development because of the env variable NUKE_SKIP_ENVIRONMENTS=${NUKE_SKIP_ENVIRONMENTS}"
+        fi
+      else
+        echo "No development workspace was found to nuke in $d"
+      fi
     fi
+  else
+    echo "Skipped nuking ${dir_name} because of the env variable NUKE_SKIP_ENVIRONMENTS=${NUKE_SKIP_ENVIRONMENTS}"
   fi
 done
 
--- scripts/terraform-apply-after-nuke.sh
+++ scripts/terraform-apply-after-nuke.sh
@@ -2,33 +2,37 @@
 
 failed_envs=()
 for d in terraform/environments/*; do
-  exit_code=0
   dir_name=$(basename "$d")
-  echo "Search for development workspace to perform terraform apply in $d"
-  bash scripts/terraform-init.sh "$d" || exit_code=$?
-  tf_workspaces=$(terraform -chdir="$d" workspace list) || exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-    failed_envs+=("$dir_name")
-  else
-    if [[ "$tf_workspaces" == *"${dir_name}-development"* ]]; then
-      if [[ "$NUKE_SKIP_ENVIRONMENTS" != *"${dir_name}-development"* ]]; then
-        if [[ "$NUKE_DO_NOT_RECREATE_ENVIRONMENTS" != *"${dir_name}-development"* ]]; then
-          echo "BEGIN: terraform apply ${dir_name}-development"
-          terraform -chdir="$d" workspace select "${dir_name}-development" || exit_code=$?
-          bash scripts/terraform-apply.sh "$d" || exit_code=$?
-          if [ $exit_code -ne 0 ]; then
-            failed_envs+=("${dir_name}-development")
+  if [[ "$NUKE_SKIP_ENVIRONMENTS" != *"${dir_name},"* ]]; then
+    exit_code=0
+    echo "Search for development workspace to perform terraform apply in $d"
+    bash scripts/terraform-init.sh "$d" || exit_code=$?
+    tf_workspaces=$(terraform -chdir="$d" workspace list) || exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+      failed_envs+=("$dir_name")
+    else
+      if [[ "$tf_workspaces" == *"${dir_name}-development"* ]]; then
+        if [[ "$NUKE_SKIP_ENVIRONMENTS" != *"${dir_name}-development,"* ]]; then
+          if [[ "$NUKE_DO_NOT_RECREATE_ENVIRONMENTS" != *"${dir_name}-development,"* ]]; then
+            echo "BEGIN: terraform apply ${dir_name}-development"
+            terraform -chdir="$d" workspace select "${dir_name}-development" || exit_code=$?
+            bash scripts/terraform-apply.sh "$d" || exit_code=$?
+            if [ $exit_code -ne 0 ]; then
+              failed_envs+=("${dir_name}-development")
+            fi
+            echo "END: terraform apply ${dir_name}-development"
+          else
+            echo "Skipped terraform apply for ${dir_name}-development because of the env variable NUKE_DO_NOT_RECREATE_ENVIRONMENTS=${NUKE_DO_NOT_RECREATE_ENVIRONMENTS}"
           fi
-          echo "END: terraform apply ${dir_name}-development"
         else
-          echo "Skipped terraform apply for ${dir_name}-development because of the env variable NUKE_DO_NOT_RECREATE_ENVIRONMENTS=${NUKE_DO_NOT_RECREATE_ENVIRONMENTS}"
+          echo "Skipped terraform apply for ${dir_name}-development because of the env variable NUKE_SKIP_ENVIRONMENTS=${NUKE_SKIP_ENVIRONMENTS}"
         fi
       else
-        echo "Skipped terraform apply for ${dir_name}-development because of the env variable NUKE_SKIP_ENVIRONMENTS=${NUKE_SKIP_ENVIRONMENTS}"
+        echo "No development workspace was found to perform terraform apply in $d"
       fi
-    else
-      echo "No development workspace was found to perform terraform apply in $d"
     fi
+  else
+    echo "Skipped terraform apply for ${dir_name} because of the env variable NUKE_SKIP_ENVIRONMENTS=${NUKE_SKIP_ENVIRONMENTS}"
   fi
 done
 
