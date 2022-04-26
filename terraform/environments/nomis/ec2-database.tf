3af0ade953e870f8878cebbf22d179277fa62b65
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2022-04-26 16:57:29 +0100

    hopefully all fixed for prod

--- terraform/environments/nomis/ec2-database.tf
+++ terraform/environments/nomis/ec2-database.tf
@@ -18,7 +18,7 @@ module "database" {
   asm_data_capacity  = each.value.asm_data_capacity
   asm_flash_capacity = each.value.asm_flash_capacity
 
-  ami_owner              = try(each.value.ami_owner, null)
+  ami_owner              = try(each.value.ami_owner, "${local.environment_management.account_ids["nomis-test"]}")
   asm_data_iops          = try(each.value.asm_data_iops, null)
   asm_data_throughput    = try(each.value.asm_data_throughput, null)
   asm_flash_iops         = try(each.value.asm_flash_iops, null)
