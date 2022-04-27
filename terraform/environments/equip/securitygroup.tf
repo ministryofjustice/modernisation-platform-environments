6d837bbea7af072f2cf97e27a909c201a7f988cc
David Sibley <david.sibley@digital.justice.gov.uk>
2022-04-27 14:11:17 +0100

    quick bit of refactoring for tagging

--- terraform/environments/equip/securitygroup.tf
+++ terraform/environments/equip/securitygroup.tf
@@ -1,12 +1,11 @@
 #Application Load balancer Security Group
 resource "aws_security_group" "alb_sg" {
-
   name        = "alb_sg"
   description = "Security Group for Application LoadBalancer"
   vpc_id      = data.aws_vpc.shared.id
-  tags = {
-    Name = "alb_sg"
-  }
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-alb", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "ingress_internet_to_alb_traffic" {
@@ -40,9 +39,9 @@ resource "aws_security_group" "citrix_adc" {
   name        = "citrix_adc"
   description = "Security Group for citrix_adc"
   vpc_id      = data.aws_vpc.shared.id
-  tags = {
-    Name = "citrix_adc"
-  }
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-citrix-adc", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "ingress_alb_to_citrix-adc_traffic" {
@@ -75,11 +74,9 @@ resource "aws_security_group" "aws_citrix_security_group" {
   name        = "aws_citrix_security_group"
   description = "Security Group for AWS_Citrix "
   vpc_id      = data.aws_vpc.shared.id
-
-  depends_on = [aws_security_group.citrix_adc]
-  tags = {
-    Name = "aws_citrix_security_group"
-  }
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-citrix-host", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "ingress_citrix-adc_to_ctx-host_traffic" {
@@ -134,9 +131,9 @@ resource "aws_security_group" "aws_equip_security_group" {
   name        = "aws_equip_security_group"
   description = "Security Group for AWS_Equip"
   vpc_id      = data.aws_vpc.shared.id
-  tags = {
-    Name = "aws_equip_security_group"
-  }
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-equip", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "aws_equip_security_group_ingress_1" {
@@ -180,10 +177,9 @@ resource "aws_security_group" "aws_spotfire_security_group" {
   name        = "aws_spotfire_security_group"
   description = "Security Group for AWS_SpotFire"
   vpc_id      = data.aws_vpc.shared.id
-
-  tags = {
-    Name = "aws_spotfire_security_group"
-  }
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-spotfire", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "aws_spotfire_security_group_ingress_1" {
@@ -225,12 +221,9 @@ resource "aws_security_group" "aws_proxy_security_group" {
   name        = "aws_proxy_security_group"
   description = "Security Group for AWS_Proxy"
   vpc_id      = data.aws_vpc.shared.id
-
-  depends_on = [aws_security_group.all_internal_groups]
-
-  tags = {
-    Name = "aws_proxy_security_group"
-  }
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-proxy", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "ingress_hosts_to_proxies_traffic" {
@@ -263,11 +256,9 @@ resource "aws_security_group" "aws_domain_security_group" {
   name        = "aws_domain_security_group"
   description = "Security Group for AWS_Domain"
   vpc_id      = data.aws_vpc.shared.id
-
-  depends_on = [aws_security_group.all_internal_groups]
-  tags = {
-    Name = "aws_domain_security_group"
-  }
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-domain-controller", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "ingress_hosts_to_domain_contoller_traffic" {
@@ -278,7 +269,7 @@ resource "aws_security_group_rule" "ingress_hosts_to_domain_contoller_traffic" {
   security_group_id = aws_security_group.aws_domain_security_group.id
   to_port           = each.value.to_port
   type              = "ingress"
-  cidr_blocks       = [data.aws_vpc.shared.cidr_block]
+  source_security_group_id  = aws_security_group.all_internal_groups.id
 }
 
 resource "aws_security_group_rule" "ingress_dns_endpoints_to_domain_contoller_traffic" {
@@ -336,10 +327,9 @@ resource "aws_security_group" "all_internal_groups" {
   name        = "all_internal_groups"
   description = "Security Group for all_internal_groups"
   vpc_id      = data.aws_vpc.shared.id
-
-  tags = {
-    Name = "all_internal_groups"
-  }
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-all-hosts", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "all_internal_groups_egress" {
@@ -361,6 +351,9 @@ resource "aws_security_group" "aws_dns_resolver" {
   name        = "dns_resolver"
   description = "Security Group for DNS resolver request"
   vpc_id      = data.aws_vpc.shared.id
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-equip-dns", local.application_name, local.environment)) }
+  )
 }
 
 resource "aws_security_group_rule" "ingress_dns_endpoint_traffic" {
