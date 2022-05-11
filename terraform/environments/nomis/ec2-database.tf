3af73133bd8a30ce4f816ad0258c5421886081d5
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2022-05-11 15:25:19 +0100

    added a test instance. testing rehl7 ami owned by nomis-test

--- terraform/environments/nomis/ec2-database.tf
+++ terraform/environments/nomis/ec2-database.tf
@@ -126,3 +126,78 @@ resource "aws_iam_policy" "s3_db_backup_bucket_access" {
     },
   )
 }
+
+##########################################
+
+data "aws_subnet" "private_az_a" {
+  tags = {
+    Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-private-${local.region}a"
+  }
+}
+
+data "aws_ami" "image" {
+  most_recent = true
+  owners      = ["${local.environment_management.account_ids["nomis-test"]}"]
+
+  filter {
+    name   = "name"
+    values = ["nomis_RHEL7_9_2022-05-11T12-46-53*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "test" {
+  instance_type               = "t3.medium"
+  ami                         = data.aws_ami.image.id
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  monitoring                  = false
+  subnet_id                   = data.aws_subnet.private_az_a.id
+  key_name                    = aws_key_pair.ec2-user.key_name
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = true
+    volume_type           = "gp3"
+  }
+  dynamic "ephemeral_block_device" { # block devices specified inline cannot be resized later so we need to make sure they are not mounted here
+    for_each = [for bdm in data.aws_ami.image.block_device_mappings : bdm if bdm.device_name != data.aws_ami.image.root_device_name]
+    iterator = device
+    content {
+      device_name = device.value.device_name
+      no_device   = true
+    }
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "test-rhe7"
+
+    }
+  )
+}
+
+resource "aws_ebs_volume" "disk" {
+  for_each = { for bdm in data.aws_ami.image.block_device_mappings : bdm.device_name => bdm if bdm.device_name != data.aws_ami.image.root_device_name }
+
+  availability_zone = "eu-west-2a"
+  encrypted         = true
+  snapshot_id       = each.value.snapshot_id
+  type              = "gp3"
+}
+
+resource "aws_volume_attachment" "disk" {
+  for_each = aws_ebs_volume.disk
+
+  device_name = each.key
+  volume_id   = each.value.id
+  instance_id = aws_instance.test.id
+}
