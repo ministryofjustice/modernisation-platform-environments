d6add0e7d2ba7cde2b8fcd34c5f19279ad98ef3b
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-05-12 15:27:26 +0100

    Equip: Added policy for the Citrix NetScaler instance https://mojdt.slack.com/archives/C01A7QK5VM1/p1652352369447769

--- terraform/environments/equip/policy.tf
+++ terraform/environments/equip/policy.tf
@@ -19,3 +19,32 @@ resource "aws_iam_policy" "policy-ssm" {
     ]
   })
 }
+
+resource "aws_iam_policy" "citrix_adc_instance_policy" {
+  name        = "citrix_adc_instance_policy"
+  path        = "/"
+  description = "Policy for Citrix NetScaler instance"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DescribeInstances",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DetachNetworkInterface",
+          "ec2:AttachNetworkInterface",
+          "ec2:StartInstances",
+          "ec2:StopInstances",
+          "ec2:RebootInstances",
+          "autoscaling:*",
+          "sns:*",
+          "sqs:*",
+          "iam:SimulatePrincipalPolicy",
+          "iam:GetRole"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
