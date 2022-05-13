4e56469c33aee69bab885df728a1886e0abeac70
SteveLinden <stephen.linden@digital.justice.gov.uk>
2022-05-13 15:13:06 +0100

    Example code for users
    Includes EC2, rds, load balancer, bastion, s3 and associated variable settings.

---
+++ terraform/environments/example/platform_secrets.tf
@@ -0,0 +1,40 @@
+######################### Run Terraform via CICD ##################################
+resource "aws_secretsmanager_secret" "db_password" {
+  #checkov:skip=CKV_AWS_149
+
+  name                    = "${var.networking[0].application}-database-password"
+  recovery_window_in_days = 0
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.networking[0].application}-db-password"
+    },
+  )
+}
+
+resource "aws_secretsmanager_secret_version" "db_password" {
+  secret_id     = aws_secretsmanager_secret.db_password.id
+  secret_string = random_password.random_password.result
+}
+
+######################### Run Terraform via CICD ##################################
+
+
+######################### Run Terraform Plan Locally Only ##################################
+# To run a Terraform Plan locally, uncomment this bottom section of code and comment out the top section
+
+# # Get secret by arn for environment management
+# data "aws_ssm_parameter" "environment_management_arn" {
+#   name = "environment_management_arn"
+# }
+#
+# data "aws_secretsmanager_secret" "environment_management" {
+#   arn = data.aws_ssm_parameter.environment_management_arn.value
+# }
+# # Get latest secret value with ID from above. This secret stores account IDs for the Modernisation Platform sub-accounts
+# data "aws_secretsmanager_secret_version" "environment_management" {
+#   secret_id = data.aws_secretsmanager_secret.environment_management.id
+# }
+
+######################### Run Terraform Plan Locally Only ##################################
