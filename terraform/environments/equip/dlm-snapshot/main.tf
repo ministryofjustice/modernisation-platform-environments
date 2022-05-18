6e25a9d7f54ddf18c4ef232cf53e501530378350
David Sibley <david.sibley@digital.justice.gov.uk>
2022-05-18 10:55:04 +0100

    added tag for name

--- terraform/environments/equip/dlm-snapshot/main.tf
+++ terraform/environments/equip/dlm-snapshot/main.tf
@@ -2,7 +2,9 @@ resource "aws_dlm_lifecycle_policy" "dlm_daily_snapshots" {
   description        = format("DLM lifecycle policy for %s-%s EC2 volume snapshots", var.service, var.environment)
   execution_role_arn = aws_iam_role.dlm_lifecycle_role.arn
   state              = upper(var.state)
-  tags               = var.tags
+  tags               = merge(var.tags,
+    { Name = format("%s-%s-dlm-policy", var.service, var.environment) }
+  )
 
   policy_details {
     resource_types = ["VOLUME"]
