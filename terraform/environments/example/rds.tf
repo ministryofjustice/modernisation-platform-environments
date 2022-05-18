1af91fdb55f419612b86ce7d7177dd26bc056013
SteveLinden <stephen.linden@digital.justice.gov.uk>
2022-05-18 11:38:35 +0100

    a

--- terraform/environments/example/rds.tf
+++ terraform/environments/example/rds.tf
@@ -1,15 +1,15 @@
 # Example code to build an RDS database - based on mysql but could be :
 # Amazon Aurora, PostgreSQL, MariaDB, Oracle, MicroSoft SQL Server. These will require the correct version
+
 resource "aws_db_instance" "Example-RDS" {
-  allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  db_name              = "example"
-  username             = "admin"
-  password             = "foobarbaz"
+  instance_class       = local.app_variables[local.environment].db_instance_class
+  db_name              = "${local.application_name}-${local.environment}-database"
+  username             = local.app_variables[local.environment].db_user
+  password             = aws_secretsmanager_secret_version.db_password.arn
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
- # allocated_storage     = 50
-  max_allocated_storage = 100
+  allocated_storage     = local.app_variables[local.environment].db_allocated_storage
+  max_allocated_storage = local.app_variables[local.environment].db_max_allocated_storage
 }
