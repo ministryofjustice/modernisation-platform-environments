763e1b8e5f370746c59ecb58a9d87cef6ac5296d
SteveLinden <stephen.linden@digital.justice.gov.uk>
2022-05-18 14:14:40 +0100

    a¬

--- terraform/environments/example/rds.tf
+++ terraform/environments/example/rds.tf
@@ -4,12 +4,13 @@
 resource "aws_db_instance" "Example-RDS" {
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = local.app_variables[local.environment].db_instance_class
-  db_name              = "${local.application_name}-${local.environment}-database"
-  username             = local.app_variables[local.environment].db_user
-  password             = aws_secretsmanager_secret_version.db_password.arn
+  instance_class       = local.app_variables.accounts[local.environment].db_instance_class
+  db_name              = "${local.application_name}${local.environment}database"
+  username             = local.app_variables.accounts[local.environment].db_user
+  password             = aws_secretsmanager_secret_version.db_password.id
+  #password             = "TestPassword123"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-  allocated_storage     = local.app_variables[local.environment].db_allocated_storage
-  max_allocated_storage = local.app_variables[local.environment].db_max_allocated_storage
+  allocated_storage     = local.app_variables.accounts[local.environment].db_allocated_storage
+  max_allocated_storage = local.app_variables.accounts[local.environment].db_max_allocated_storage
 }
