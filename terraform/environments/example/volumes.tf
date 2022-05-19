890758bd2476786df3195e444fd8ea31d559b641
SteveLinden <stephen.linden@digital.justice.gov.uk>
2022-05-19 13:28:15 +0100

    Testing

--- terraform/environments/example/volumes.tf
+++ terraform/environments/example/volumes.tf
@@ -1,20 +1,22 @@
 # Volumes built for use by EC2.
                   
  resource "aws_ebs_volume" "ebs_volume" {
-              # device_name = "/dev/sdf"
-              availability_zone = local.app_variables.accounts[local.environment].region
-              type = "gp3"
-              size = 50
-              throughput  = 200
-              encrypted   = true
+              availability_zone = "${local.app_variables.accounts[local.environment].region}a"
+              #availability_zone = "eu-west-2a"
+              type              = "gp3"
+              size              = 50
+              throughput        = 200
+              encrypted         = true
               # kms_key_id  = aws_kms_key.this.arn
               tags = {
                 Name = "ebs-data-volume"
               }
+            
+              depends_on = [aws_instance.develop]
 }
-
+# Attach to the EC2
  resource "aws_volume_attachment" "mountvolumetoec2" {
-  # device_name = "/dev/sdb"
+  device_name = "/dev/sdb"
   instance_id = aws_instance.develop.id
-  volume_id = aws_volume_attachment.mountvolumetoec2.id
- }
+  volume_id = aws_ebs_volume.ebs_volume.id
+}
