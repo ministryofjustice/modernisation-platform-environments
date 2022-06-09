d96233572b9b930429f6c22452bec613c1a285d2
Rob Whittle <robert.whittle@digital.justice.gov.uk>
2022-06-09 13:46:12 +0100

    fix launch template permissions

--- terraform/environments/nomis/ec2-image-builder-iam.tf
+++ terraform/environments/nomis/ec2-image-builder-iam.tf
@@ -44,7 +44,9 @@ data "aws_iam_policy_document" "image-builder-launch-template-policy" {
       "ec2:DescribeLaunchTemplates",
       "ec2:CreateTags"
     ]
-    resources = [for item in module.weblogic : item.launch_template_arn]
+    #checkov:skip=CKV_AWS_111: "Ensure IAM policies does not allow write access without constraints"
+    #tfsec:ignore:aws-iam-no-policy-wildcards:exp:2022-08-25
+    resources = ["*"]
   }
 }
 
