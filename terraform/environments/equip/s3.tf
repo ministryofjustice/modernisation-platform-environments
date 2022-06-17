cbff3355e4ebdb9b5a25af49f6d1fbb5ce16367f
David Sibley <david.sibley@digital.justice.gov.uk>
2022-06-17 13:23:23 +0100

    add s3 bucket for production only

---
+++ terraform/environments/equip/s3.tf
@@ -0,0 +1,54 @@
+module "equip-s3-bucket" {
+  count               = local.is-production ? 1 : 0
+  source              = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.0.5"
+  bucket_prefix       = format("%s-%s", local.application_name, local.environment)
+  versioning_enabled  = false
+  replication_enabled = false
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "tmp"
+      enabled = "Enabled"
+      prefix  = "/tmp"
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
