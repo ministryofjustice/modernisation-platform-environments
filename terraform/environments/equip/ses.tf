b8d204dca9cddb09c28d369c6b1eb51e872a27d7
David Sibley <david.sibley@digital.justice.gov.uk>
2022-06-20 14:30:26 +0100

    added content to create SES identity and email user

---
+++ terraform/environments/equip/ses.tf
@@ -0,0 +1,28 @@
+resource "aws_ses_domain_identity" "external" {
+  provider = aws.core-network-services
+  domain = data.aws_route53_zone.application-zone.name
+}
+
+# `allow_overwrite` is used here as this is a verification record
+resource "aws_route53_record" "external_amazonses_verification_record" {
+  zone_id = data.aws_route53_zone.external.id
+  allow_overwrite = true
+  provider = aws.core-network-services
+  name    = format("_amazonses.%s", data.aws_route53_zone.application-zone.name)
+  type    = "TXT"
+  ttl     = "300"
+  records = [aws_ses_domain_identity.external.verification_token]
+}
+
+resource "aws_iam_user" "email" {
+  name = format("%s-%s-email_user", local.application_name, local.environment)
+  tags = merge(local.tags,
+    { Name = format("%s-%s-email_user", local.application_name, local.environment) }
+  )
+}
+
+resource "aws_iam_user_policy" "email_policy" {
+  name = format("%s-%s-email_policy", local.application_name, local.environment)
+  policy = data.aws_iam_policy_document.email.json
+  user   = aws_iam_user.email.name
+}
