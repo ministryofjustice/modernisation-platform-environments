42a4c2bf92d49d7fa25c26d383226023f8744023
David Sibley <david.sibley@digital.justice.gov.uk>
2022-06-20 14:48:17 +0100

    added domain identity verification resource

--- terraform/environments/equip/ses.tf
+++ terraform/environments/equip/ses.tf
@@ -14,11 +14,22 @@ resource "aws_route53_record" "external_amazonses_verification_record" {
   records = [aws_ses_domain_identity.external.verification_token]
 }
 
+resource "aws_ses_domain_identity_verification" "external" {
+  provider = aws.core-network-services
+  domain = aws_ses_domain_identity.external.id
+
+  depends_on = ["aws_route53_record.external_amazonses_verification_record"]
+}
+
 resource "aws_iam_user" "email" {
   name = format("%s-%s-email_user", local.application_name, local.environment)
   tags = merge(local.tags,
     { Name = format("%s-%s-email_user", local.application_name, local.environment) }
   )
+}
+
+resource "aws_iam_access_key "email {
+  user = aws_iam_user.email.name
 }
 
 resource "aws_iam_user_policy" "email_policy" {
