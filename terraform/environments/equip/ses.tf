ff94b478dad5651bb996a265e7753695ecccb069
David Sibley <david.sibley@digital.justice.gov.uk>
2022-06-20 14:54:43 +0100

    fixed linting, fixed some typographical errors

--- terraform/environments/equip/ses.tf
+++ terraform/environments/equip/ses.tf
@@ -1,24 +1,24 @@
 resource "aws_ses_domain_identity" "external" {
   provider = aws.core-network-services
-  domain = data.aws_route53_zone.application-zone.name
+  domain   = data.aws_route53_zone.application-zone.name
 }
 
 # `allow_overwrite` is used here as this is a verification record
 resource "aws_route53_record" "external_amazonses_verification_record" {
-  zone_id = data.aws_route53_zone.external.id
+  zone_id         = data.aws_route53_zone.external.id
   allow_overwrite = true
-  provider = aws.core-network-services
-  name    = format("_amazonses.%s", data.aws_route53_zone.application-zone.name)
-  type    = "TXT"
-  ttl     = "300"
-  records = [aws_ses_domain_identity.external.verification_token]
+  provider        = aws.core-network-services
+  name            = format("_amazonses.%s", data.aws_route53_zone.application-zone.name)
+  type            = "TXT"
+  ttl             = "300"
+  records         = [aws_ses_domain_identity.external.verification_token]
 }
 
 resource "aws_ses_domain_identity_verification" "external" {
   provider = aws.core-network-services
-  domain = aws_ses_domain_identity.external.id
+  domain   = aws_ses_domain_identity.external.id
 
-  depends_on = ["aws_route53_record.external_amazonses_verification_record"]
+  depends_on = [aws_route53_record.external_amazonses_verification_record]
 }
 
 resource "aws_iam_user" "email" {
@@ -28,12 +28,12 @@ resource "aws_iam_user" "email" {
   )
 }
 
-resource "aws_iam_access_key "email {
+resource "aws_iam_access_key" "email" {
   user = aws_iam_user.email.name
 }
 
 resource "aws_iam_user_policy" "email_policy" {
-  name = format("%s-%s-email_policy", local.application_name, local.environment)
+  name   = format("%s-%s-email_policy", local.application_name, local.environment)
   policy = data.aws_iam_policy_document.email.json
   user   = aws_iam_user.email.name
 }
