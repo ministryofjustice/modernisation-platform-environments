3a2b3a9fa01fa7254974a85e52a100fe7fa970c6
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-06-20 16:01:13 +0100

    Adding var.

--- terraform/environments/nomis/sns.tf
+++ terraform/environments/nomis/sns.tf
@@ -1,7 +1,8 @@
 module "monitoring-sns-topic" {
-  source      = "./modules/sns_topic"
-  application = "nomis-monitoring"
-  env         = local.environment
+  source       = "./modules/sns_topic"
+  application  = "nomis-monitoring"
+  env          = local.environment
+  display_name = "${application} ${env} SNS topic"
 
 }
 
