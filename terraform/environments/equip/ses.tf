cbabcfa36db19cfcf66d7f65bb3c7f8954fa13fd
David Sibley <david.sibley@digital.justice.gov.uk>
2022-06-21 11:30:41 +0100

    moved provider statement for readability

--- terraform/environments/equip/ses.tf
+++ terraform/environments/equip/ses.tf
@@ -5,9 +5,9 @@ resource "aws_ses_domain_identity" "external" {
 
 # `allow_overwrite` is used here as this is a verification record
 resource "aws_route53_record" "external_amazonses_verification_record" {
+  provider        = aws.core-network-services
   zone_id         = data.aws_route53_zone.external.id
   allow_overwrite = true
-  provider        = aws.core-network-services
   name            = format("_amazonses.%s", data.aws_route53_zone.application-zone.name)
   type            = "TXT"
   ttl             = "300"
