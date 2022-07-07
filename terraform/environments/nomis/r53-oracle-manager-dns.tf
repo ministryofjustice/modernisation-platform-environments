1de4e911d77dda6a994cb8af157db8d1cafeee6c
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-07-07 11:14:50 +0100

    Fix for_each references.

--- terraform/environments/nomis/r53-oracle-manager-dns.tf
+++ terraform/environments/nomis/r53-oracle-manager-dns.tf
@@ -3,8 +3,8 @@ resource "aws_route53_record" "oracle-manager" {
   for_each = local.accounts[local.environment].database_oracle_manager
 
   zone_id = data.aws_route53_zone.internal.zone_id
-  name    = "${each.oms_hostname.value}.${local.application_name}.${data.aws_route53_zone.internal.name}"
+  name    = "${each.value.oms_hostname}.${local.application_name}.${data.aws_route53_zone.internal.name}"
   type    = "A"
   ttl     = "60"
-  records = [each.oms_ip_address.value]
+  records = [each.value.oms_ip_address]
 }
