88cdff49dd96e9260ab9df8b7366001e2b949072
Julia Lawrence <julia.lawrence@digital.justice.gov.uk>
2022-07-25 09:57:04 +0100

    Checking if attribute is defined.

--- terraform/environments/nomis/ec2-backup.tf
+++ terraform/environments/nomis/ec2-backup.tf
@@ -1,5 +1,5 @@
 module "backup" {
-  count       = local.accounts[local.environment].backup == null ? 0 : 1
+  count       = try(local.accounts[local.environment].backup, null) == null ? 0 : 1
   source      = "./modules/backup"
   key         = local.accounts[local.environment].backup.key
   value       = local.accounts[local.environment].backup.value
