6d257db283460e7631213b3e4291bdceb9c11e45
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-08-03 07:28:00 +0100

    Adding DB instance for DR testing in production.

--- terraform/environments/nomis/application_variables.tf
+++ terraform/environments/nomis/application_variables.tf
@@ -130,6 +130,23 @@ locals {
             monitored = true
           }
         },
+        AUDITDR = {
+          always_on              = true
+          ami_name               = "nomis_database_2022-07-21T11-43-27.346Z"
+          ami_owner              = local.environment_management.account_ids[terraform.workspace]
+          instance_type          = "r6i.2xlarge"
+          asm_data_capacity      = 4000
+          asm_flash_capacity     = 1000
+          description            = "DR instance of Oracle DB to perform backup/restore testing."
+          termination_protection = false # temporary VM, will be destroyed
+          oracle_app_disk_size = {
+            "/dev/sdb" = 100  # /u01
+            "/dev/sdc" = 5120 # /u02
+          }
+          tags = {
+            monitored = false
+          }
+        },
         NOMIS = {
           always_on              = true
           ami_name               = "nomis_database_2022-07-21T11-43-27.346Z"
