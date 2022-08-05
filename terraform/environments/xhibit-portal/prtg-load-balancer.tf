5780d9bcf80f59bbe830d02b9d482a2220963495
garygrantjustice <98090448+garygrantjustice@users.noreply.github.com>
2022-08-05 13:01:05 +0100

    listenerupdated

--- terraform/environments/xhibit-portal/prtg-load-balancer.tf
+++ terraform/environments/xhibit-portal/prtg-load-balancer.tf
@@ -349,7 +349,7 @@ resource "aws_alb_listener_rule" "prtg_http_to_https_redirect" {
 
     redirect {
       status_code = "HTTP_301"
-      port        = 443
+      port        = "443"
       protocol    = "HTTPS"
       # path       = "monitoring.pportal.cjsonline.gov.uk/public/mapshow.htm?id=2270&mapid=EE1CB6BA-590C-4D06-BB63-F6FDB8E09C06"
     }
