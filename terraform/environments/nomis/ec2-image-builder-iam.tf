4eee38d9d06d598362965d6a6db656985810f43c
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-08-08 16:37:48 +0100

    reference key arn as aws_kms_grant can't handle aliases

--- terraform/environments/nomis/ec2-image-builder-iam.tf
+++ terraform/environments/nomis/ec2-image-builder-iam.tf
@@ -170,7 +170,7 @@ resource "aws_iam_role_policy_attachment" "launch-template-reader-policy-attach"
 
 resource "aws_kms_grant" "image-builder-shared-cmk-grant" {
   name              = "image-builder-shared-cmk-grant"
-  key_id            = data.aws_kms_key.hmpps_key.key_id
+  key_id            = data.aws_kms_key.hmpps_key.arn
   grantee_principal = "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
   operations = [
     "Encrypt",
