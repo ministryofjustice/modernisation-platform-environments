7c7ea17f9d877e89a1bafd96e491097020726eb1
julialawrence <98329494+julialawrence@users.noreply.github.com>
2022-08-08 12:51:11 +0100

    DSOS-158: remove t1 DR testing VM

--- terraform/environments/nomis/application_variables.tf
+++ terraform/environments/nomis/application_variables.tf
@@ -65,19 +65,6 @@ locals {
           termination_protection = true
           oracle_sids            = ["T1CNMAUD"]
           tags = {
-            monitored = false // TODO: while the testing is going on to avoid alert spam
-          }
-        },
-        CNAUDT1DR = {
-          always_on              = true
-          restored_from_snapshot = true
-          ami_name               = "database-CNAUDT1-snapshot-20220803"
-          asm_data_capacity      = 200
-          asm_flash_capacity     = 2
-          description            = "Copy of Test NOMIS Audit database in Azure T1PDL0010, Restored from snapshot."
-          termination_protection = true
-          oracle_sids            = ["T1CNMAUD"]
-          tags = {
             monitored = true
           }
         },
