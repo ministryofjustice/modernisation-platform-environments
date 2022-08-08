33eba768dab3485c514499272309bd7eedb515ab
garygrantjustice <98090448+garygrantjustice@users.noreply.github.com>
2022-08-08 13:44:56 +0100

    add prtgdomaintypes

--- terraform/environments/xhibit-portal/locals.tf
+++ terraform/environments/xhibit-portal/locals.tf
@@ -66,6 +66,23 @@ locals {
   domain_type_main   = [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
   domain_type_sub    = [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
 
+  # create name, record,type for monitoring lb aka prtg_lb
+  prtg_domain_types = { for dvo in aws_acm_certificate.prtg_lb_cert.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+    }
+  }
+
+  prtg_domain_name_main   = [for k, v in local.domain_types : v.name if k == "modernisation-platform.service.justice.gov.uk"]
+  prtg_domain_name_sub    = [for k, v in local.domain_types : v.name if k != "modernisation-platform.service.justice.gov.uk"]
+  prtg_domain_record_main = [for k, v in local.domain_types : v.record if k == "modernisation-platform.service.justice.gov.uk"]
+  prtg_domain_record_sub  = [for k, v in local.domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
+  prtg_domain_type_main   = [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
+  prtg_domain_type_sub    = [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
+
+  
+  
   # This is used to prevent our bare metal server from deploying in environments other than production
   only_in_production_mapping = {
     development   = 0
