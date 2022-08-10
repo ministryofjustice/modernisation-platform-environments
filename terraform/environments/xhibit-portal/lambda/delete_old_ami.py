6584b1a1c2637a631e1eb74d14381448e1ba238a
Ben Moore <ben.moore@digital.justice.gov.uk>
2022-08-10 09:40:48 +0100

    Fix dry run variable

--- terraform/environments/xhibit-portal/lambda/delete_old_ami.py
+++ terraform/environments/xhibit-portal/lambda/delete_old_ami.py
@@ -26,21 +26,19 @@ def lambda_handler(event, context):
                         bdm.get("Ebs") is not None
                         and bdm.get("Ebs").get("SnapshotId") is not None
                     ):
+                        print(f"Deleting Snapshot {snap_id}")
                         try:
                             snap_id = bdm.get("Ebs").get("SnapshotId")
-                            print(f"Deleting Snapshot {snap_id}")
-                            client.delete_snapshot(
-                                SnapshotId=snap_id, dry_run=True)
+                            client.delete_snapshot(SnapshotId=snap_id, DryRun=True)
                         except botocore.exceptions.ClientError as e:
                             print(
                                 f"Error deleting Snapshot {e.response['Error']['Message']}"
                             )
                             continue
+                print(f"Deleting Image {image_id}")
                 try:
                     image_id = image["ImageId"]
-                    print(f"Deleting Image {image_id}")
-                    client.deregister_image(ImageId=image_id, dry_run=True)
+                    client.deregister_image(ImageId=image_id, DryRun=True)
                 except botocore.exceptions.ClientError as e:
-                    print(
-                        f"Error deleting AMI {e.response['Error']['Message']}")
+                    print(f"Error deleting AMI {e.response['Error']['Message']}")
                     continue
