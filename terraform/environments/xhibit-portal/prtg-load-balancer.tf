4158f0b5cb6e939b432b099bb42ff9149c434bef
garygrantjustice <98090448+garygrantjustice@users.noreply.github.com>
2022-08-10 11:52:39 +0100

    add priority to listener

--- terraform/environments/xhibit-portal/prtg-load-balancer.tf
+++ terraform/environments/xhibit-portal/prtg-load-balancer.tf
@@ -338,8 +338,11 @@ data "aws_iam_policy_document" "s3_bucket_prtg_logs_policy" {
 }
 
 resource "aws_alb_listener_rule" "prtg_http_to_https_redirect" {
+  priority = 1
+  
   depends_on   = [aws_lb_listener.prtg_lb_listener]
   listener_arn = aws_lb_listener.prtg_lb_listener.arn
+  
   # port         = "80"
   # protocol     = "HTTP"
   action {
