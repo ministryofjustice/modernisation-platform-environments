0536b2561ac11f81d57662cf143172c645b80bde
justin-joseph-moj <justin.joseph@digital.justice.gov.uk>
2022-08-11 17:17:03 +0100

    Comment out policy for now

--- terraform/environments/xhibit-portal/xp-secrets.tf
+++ terraform/environments/xhibit-portal/xp-secrets.tf
@@ -1,45 +1,32 @@
 # JSON Policy for accessing created secrets
 
-resource "aws_secretsmanager_secret_policy" "test-policy" {
-  secret_arn = aws_secretsmanager_secret.test.arn
+#resource "aws_secretsmanager_secret_policy" "test-policy" {
+#  secret_arn = aws_secretsmanager_secret.test.arn
+#
+#  policy = <<POLICY
+#{
+#  "Version" : "2012-10-17",
+#  "Statement" : [ {
+#    "Sid" : "AdministratorFullAccess",
+#    "Effect" : "Allow",
+#    "Principal" : {
+#      "AWS" : "arn:aws:iam::"${data.aws_caller_identity.current.account_id}":role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_AdministratorAccess_1c8be6e1a517e14c"
+#    },
+#    "Action" : "secretsmanager:*",
+#    "Resource" : "*"
+#  } ]
+#}
+#POLICY
+#}
 
-  policy = <<POLICY
-{
-  "Version" : "2012-10-17",
-  "Statement" : [ {
-    "Sid" : "AdministratorFullAccess",
-    "Effect" : "Allow",
-    "Principal" : {
-      "AWS" : "arn:aws:iam::"${data.aws_caller_identity.current.account_id}":role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_AdministratorAccess_1c8be6e1a517e14c"
-    },
-    "Action" : "secretsmanager:*",
-    "Resource" : "*"
-  } ]
-}
-POLICY
-}
-
-# random placeholder secret to create the secret name
+# random string generator used as a placeholder
 
 resource "random_password" "random_password" {
-
   length  = 32
   special = false
 }
 
 # placeholder plainttext data
-
-data "aws_secretsmanager_secrets" "ssh" {
-  filter {
-    name   = "plaintext"
-    values = [<<EOT
-----start----
-gdfgdfgdfgdfg
-----end----
-EOT
-]
-  }
-}
 
 resource "aws_secretsmanager_secret" "test" {
   name        = "test"
@@ -48,7 +35,7 @@ resource "aws_secretsmanager_secret" "test" {
 
 resource "aws_secretsmanager_secret_version" "test" {
   secret_id     = aws_secretsmanager_secret.test.id
-  secret_string = data.aws_secretsmanager_secrets.ssh.plaintext
+  secret_string = random_password.random_password.result
 }
 
 #resource "aws_secretsmanager_secret" "prtgadmin" {
