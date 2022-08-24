de10c13ecee08390f6bf33eea359d6f932c8d3c5
Andrew Gardner <andrew.gardner@agilisys.co.uk>
2022-08-24 15:23:56 +0100

    Corrected role policy attachment resource type

--- terraform/environments/data-and-insights-wepi/iam.tf
+++ terraform/environments/data-and-insights-wepi/iam.tf
@@ -13,7 +13,7 @@ resource "aws_iam_policy" "wepi_iam_policy_redshift_scheduler" {
   )
 }
 
-resource "aws_iam_policy_attachment" "wepi_iam_attach_redshift_scheduler" {
+resource "aws_iam_role_policy_attachment" "wepi_iam_attach_redshift_scheduler" {
   policy_arn = aws_iam_policy.wepi_iam_policy_redshift_scheduler.arn
   role       = aws_iam_role.wepi_iam_role_redshift_scheduler.name
 }
