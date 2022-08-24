c7e3c1216276562084a21abff280b2aa3c64e46b
Andrew Gardner <andrew.gardner@agilisys.co.uk>
2022-08-24 15:20:19 +0100

    Tracked network security group config file

---
+++ terraform/environments/data-and-insights-wepi/network.tf
@@ -0,0 +1,19 @@
+# Redshift VPC security group for bastion access
+resource "aws_security_group" "wepi_sg_allow_redshift" {
+	# checkov:skip=CKV2_AWS_5: Configured in Redshfit cluster, Checkov not detecting reference.
+  name        = "wepi_allow_redshift"
+  description = "Allow Redshift inbound traffic from bastion"
+  vpc_id      = data.aws_vpc.wepi_vpc.id
+
+  ingress {
+    description     = "Redshift from bastion"
+    from_port       = 5439
+    to_port         = 5439
+    protocol        = "tcp"
+    security_groups = [
+      module.wepi_bastion.bastion_security_group
+    ]
+  }
+
+  tags = local.tags
+}
