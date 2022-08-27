b0a590aab3ac4786510dda5f19b596142063f8ac
Andrew Gardner <andrew.gardner@agilisys.co.uk>
2022-08-27 10:42:27 +0100

    DIH: Corrected final snapshot handling on Redshift

--- terraform/environments/data-and-insights-wepi/redshift.tf
+++ terraform/environments/data-and-insights-wepi/redshift.tf
@@ -50,6 +50,7 @@ resource "aws_redshift_cluster" "wepi_redshift_cluster" {
 
   automated_snapshot_retention_period = local.app_data.accounts[local.environment].redshift_auto_snapshot_retention
   manual_snapshot_retention_period    = local.app_data.accounts[local.environment].redshift_manual_snapshot_retention
+  skip_final_snapshot = true
 
   logging {
     enable               = true
