28e0e658b965a4da136fe46c9b76a639275cd521
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-09-15 08:52:15 +0100

    More tfsec.

--- terraform/environments/nomis/loadbalancer.tf
+++ terraform/environments/nomis/loadbalancer.tf
@@ -226,7 +226,7 @@ resource "aws_acm_certificate_validation" "internal_lb" {
 resource "aws_route53_zone" "az" {
   #Raised DSOS-1495 to investigate
   #checkov:skip=CKV2_AWS_38: "Ensure Domain Name System Security Extensions (DNSSEC) signing is enabled for Amazon Route 53 public hosted zones"
-  #checkof:skip=CKV2_AWS_39: "Ensure Domain Name System (DNS) query logging is enabled for Amazon Route 53 hosted zones"
+  #checkov:skip=CKV2_AWS_39: "Ensure Domain Name System (DNS) query logging is enabled for Amazon Route 53 hosted zones"
   count = local.environment == "test" ? 1 : 0
   name  = "modernisation-platform.nomis.az.justice.gov.uk"
   tags = merge(
