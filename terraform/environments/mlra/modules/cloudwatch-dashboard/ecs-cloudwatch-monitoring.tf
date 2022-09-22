6759b2be6afe67bb91d85fc0806e59f59aa9ebf0
Malaz K <malaz.khalil@digital.justice.gov.uk>
2022-09-22 09:56:52 +0100

    property args

--- terraform/environments/mlra/modules/cloudwatch-dashboard/ecs-cloudwatch-monitoring.tf
+++ terraform/environments/mlra/modules/cloudwatch-dashboard/ecs-cloudwatch-monitoring.tf
@@ -23,19 +23,11 @@ resource "aws_cloudwatch_dashboard" "mlra" {
         "period": 60,
         "stat": "Average",
         "region": "${var.aws_region}",
+        "view": "timeSeries"
+        "stacked": false
         "title": "Application ELB 5xx Error"
       }
     },
-    {
-      "type": "text",
-      "x": 0,
-      "y": 7,
-      "width": 3,
-      "height": 3,
-      "properties": {
-        "markdown": "Hello world"
-      }
-    }
   ]
 },
 {
@@ -62,6 +54,37 @@ resource "aws_cloudwatch_dashboard" "mlra" {
     },
     {
       "type": "metric",
+      "x": 0,
+      "y": 7,
+      "width": 3,
+      "height": 3,
+      "properties": {
+        "markdown": "Hello world"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "CPUUtilization",
+            "InstanceId",
+            "i-012345"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "EC2 Instance CPU"
+      }
+    },
+    {
+      "type": "text",
       "x": 0,
       "y": 7,
       "width": 3,
