7c8714b3f9cfdad344426d5ec1b508cd5f7d9bdb
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2022-09-22 11:31:39 +0100

    mlra alb build for dev - v1.00

---
+++ terraform/environments/mlra/main.tf
@@ -0,0 +1,46 @@
+module "lb-access-logs-enabled" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer"
+
+  vpc_all = local.vpc_name
+  #existing_bucket_name               = "my-bucket-name"
+  application_name           = local.application_name
+  public_subnets             = [data.aws_subnet.public_subnets_a.id, data.aws_subnet.public_subnets_b.id, data.aws_subnet.public_subnets_c.id]
+  loadbalancer_egress_rules  = local.loadbalancer_egress_rules
+  loadbalancer_ingress_rules = local.loadbalancer_ingress_rules
+  tags                       = local.tags
+  account_number             = local.environment_management.account_ids[terraform.workspace]
+  region                     = local.application_data.accounts[local.environment].region
+  enable_deletion_protection = false
+  idle_timeout               = 60
+}
+
+locals {
+  loadbalancer_ingress_rules = {
+    "cluster_ec2_lb_ingress" = {
+      description     = "Cluster EC2 loadbalancer ingress rule"
+      from_port       = 32768
+      to_port         = 61000
+      protocol        = "tcp"
+      cidr_blocks     = []
+      security_groups = []
+    },
+    "cluster_c2_bastion_ingress" = {
+      description     = "Cluster EC2 bastion ingress rule"
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      cidr_blocks     = ["10.200.0.0/20"]
+      security_groups = []
+    }
+  }
+  loadbalancer_egress_rules = {
+    "cluster_ec2_lb_egress" = {
+      description     = "Cluster EC2 loadbalancer egress rule"
+      from_port       = 443
+      to_port         = 443
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  }
+}
