9b8867774a7e67b94bd59fddc3fe952e379a6b63
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2022-09-26 12:36:56 +0100

    create alb listener and target group for mlra

--- terraform/environments/mlra/main.tf
+++ terraform/environments/mlra/main.tf
@@ -47,3 +47,32 @@ locals {
     }
   }
 }
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = module.lb-access-logs-enabled.load_balancer.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name        = "mlra-arget-group"
+  target_type = "alb"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.shared.id
+}
+
+
+
+
+
+
+
+
