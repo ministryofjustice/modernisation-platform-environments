15cbefc70d9fe7641ccd43063a113cbfe6a0c05a
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-09-27 12:19:03 +0100

    OIDC: Amend provider, secret and locals tf files to allow local plans.

--- terraform/environments/example/locals.tf
+++ terraform/environments/example/locals.tf
@@ -21,6 +21,8 @@ locals {
 
   environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
 
+  modernisation_platform_account_id = data.aws_ssm_parameter.modernisation_platform_account_id.value
+
   # This takes the name of the Terraform workspace (e.g. core-vpc-production), strips out the application name (e.g. core-vpc), and checks if
   # the string leftover is `-production`, if it isn't (e.g. core-vpc-non-production => -non-production) then it sets the var to false.
   is-production    = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production"
@@ -46,7 +48,7 @@ locals {
   provider_name = "core-vpc-${local.environment}"
 
   # environment specfic variables
-  # example usage:  
+  # example usage:
   # example_data = local.application_data.accounts[local.environment].example_var
   application_data = jsondecode(file("./application_variables.json"))
 }
