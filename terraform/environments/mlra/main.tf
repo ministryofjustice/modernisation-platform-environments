7325b0e7773163340681e5c094c2147c8d4cf311
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2022-09-27 15:09:39 +0100

    add force_destroy_bucket and todo comment

--- terraform/environments/mlra/main.tf
+++ terraform/environments/mlra/main.tf
@@ -49,27 +49,27 @@ locals {
   }
 }
 
-# resource "aws_lb_listener" "alb_listener" {
-#   load_balancer_arn = module.lb-access-logs-enabled.load_balancer.arn
-#   port              = "443"
-#   protocol          = "HTTP"
-#   #TODO_CHANGE_TO_HTTPS
-#   #ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = module.lb-access-logs-enabled.load_balancer.arn
+  port              = "443"
+  protocol          = "HTTP"
+  #TODO_CHANGE_TO_HTTPS
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb_target_group.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
 
-# resource "aws_lb_target_group" "alb_target_group" {
-#   name = "mlra-target-group"
-#   #target_type = "alb"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = data.aws_vpc.shared.id
-# }
+resource "aws_lb_target_group" "alb_target_group" {
+  name = "mlra-target-group"
+  #target_type = "alb"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.shared.id
+}
 
 
 
