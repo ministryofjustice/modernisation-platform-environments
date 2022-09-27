cd00f862901cfced30571a61ccb08bba153e9af0
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2022-09-27 14:32:07 +0100

    listener protocol changed to HTTP

--- terraform/environments/mlra/main.tf
+++ terraform/environments/mlra/main.tf
@@ -51,7 +51,7 @@ locals {
 resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = module.lb-access-logs-enabled.load_balancer.arn
   port              = "443"
-  protocol          = "HTTPS"
+  protocol          = "HTTP"
   #ssl_policy        = "ELBSecurityPolicy-2016-08"
   #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
