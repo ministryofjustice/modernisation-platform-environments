1bb3451697627934a4d7b6f5c21af53597c1aa1c
Mike Reid <mike.reid@digital.justice.gov.k>
2022-09-27 12:34:22 +0100

    ...

--- terraform/environments/mlra/lz-conn-test.tf
+++ terraform/environments/mlra/lz-conn-test.tf
@@ -34,10 +34,10 @@ module "mikereidhttptest_sg" {
   description = "Security group for TG connectivity testing between LAA LZ & MP"
   vpc_id      = "vpc-06febffe7b87ab37f"
 
-  ingress_rules = [
+  ingress_with_cidr_blocks = [
     {
-      from_port   = "80"
-      to_port     = "80"
+      from_port   = 80
+      to_port     = 80
       protocol    = "tcp"
       description = "HTTP"
       cidr_blocks = "10.200.0.0/20"
