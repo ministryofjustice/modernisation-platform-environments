d58592f1096982fb6f9a30acebffa3cb3d6ab824
Jonathan Quinn <jonathan.quinn@digital.justice.gov.uk>
2022-09-29 15:28:30 +0100

    we aren't going to use this after all, it would create too much chat in the plans

--- terraform/environments/nomis/variables.tf
+++ terraform/environments/nomis/variables.tf
@@ -3,10 +3,3 @@ variable "BRANCH_NAME" {
   default     = "main"
   description = "Github actions running branch"
 }
-
-# tflint-ignore: terraform_unused_declarations
-variable "GH_ACTOR_NAME" {
-  type        = string
-  default     = ""
-  description = "GH username triggering Github action"
-}
