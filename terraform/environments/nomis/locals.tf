a4ec09748f038099a1d285000ad5a162636dd43d
Jonathan Quinn <jonathan.quinn@digital.justice.gov.uk>
2022-09-29 11:32:18 +0100

    remove deprecated template_file. add exception for each of the is-* vars

--- terraform/environments/nomis/locals.tf
+++ terraform/environments/nomis/locals.tf
@@ -25,8 +25,11 @@ locals {
   # the string leftover is `-production`, if it isn't (e.g. core-vpc-non-production => -non-production) then it sets the var to false.
   # tflint-ignore: terraform_unused_declarations
   is-production    = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production"
+  # tflint-ignore: terraform_unused_declarations
   is-preproduction = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction"
+  # tflint-ignore: terraform_unused_declarations
   is-test          = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-test"
+  # tflint-ignore: terraform_unused_declarations
   is-development   = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-development"
 
   # Merge tags from the environment json file with additional ones
