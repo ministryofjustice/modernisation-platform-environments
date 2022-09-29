cf0d5a64b938ed3c07631015ebe6597f0f5b664a
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2022-09-29 15:25:41 +0100

    Tidied up naming, as well as commented out public cidrs. Not 100 percent sure this is right, but looking at the change in the module they have removed it, would like to see it run a plan.

--- terraform/environments/performance-hub/main.tf
+++ terraform/environments/performance-hub/main.tf
@@ -164,7 +164,7 @@ module "windows-ecs" {
   network_mode            = local.app_data.accounts[local.environment].network_mode
   server_port             = local.app_data.accounts[local.environment].server_port
   app_count               = local.app_data.accounts[local.environment].app_count
-  public_cidrs            = [data.aws_subnet.public_az_a.cidr_block, data.aws_subnet.public_az_b.cidr_block, data.aws_subnet.public_az_c.cidr_block]
+#  public_cidrs            = [data.aws_subnet.public_az_a.cidr_block, data.aws_subnet.public_az_b.cidr_block, data.aws_subnet.public_az_c.cidr_block]
   ec2_ingress_rules       = local.ec2_ingress_rules
   ec2_egress_rules        = local.ec2_egress_rules
   tags_common             = local.tags
