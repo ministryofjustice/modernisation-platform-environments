64d8f9d4397a98d5a03090396885304882f15eed
Hari Chintala <h.chintala@isaggio.com>
2022-09-29 14:08:35 +0100

    Adding create_s3 flag to variables

--- terraform/environments/digital-prison-reporting/modules/s3_bucket/variables.tf
+++ terraform/environments/digital-prison-reporting/modules/s3_bucket/variables.tf
@@ -8,3 +8,8 @@ variable "tags" {
   description = "A mapping of tags to assign to the resource."
   type        = map(any)
 }
+
+variable "create_s3" {
+  description = "Setup S3 Buckets"
+  default     = false
+}
