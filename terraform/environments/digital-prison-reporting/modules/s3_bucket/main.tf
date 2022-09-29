9ab43ebc6839c0e38bbcf5ed3b599a5320de300c
Hari Chintala <h.chintala@isaggio.com>
2022-09-29 14:42:42 +0100

    Fixing S3 Bucket References

--- terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
+++ terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_acl" "application_tf_state" {
 #}
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "application_tf_state" {
-  bucket = aws_s3_bucket.application_tf_state.id
+  bucket = aws_s3_bucket.application_tf_state.*.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
