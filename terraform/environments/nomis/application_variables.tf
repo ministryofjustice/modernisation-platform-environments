f88965b8c4eabb598771e5a1a4571b581f4fa74f
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-09-30 09:05:42 +0100

    DSOS-1439: re-test

--- terraform/environments/nomis/application_variables.tf
+++ terraform/environments/nomis/application_variables.tf
@@ -108,7 +108,7 @@ locals {
       },
 
       databases = {
-        t1a-db-audit = {
+        t1b-db-audit = {
           tags = {
             description = "Test NOMIS Audit database replicating with T1PDL0010"
             oracle-sids = "T1CNMAUD"
