5ad0224354041c61157968e5bcaf9df0b8756bb8
Hari Chintala <h.chintala@isaggio.com>
2022-10-13 04:00:34 +0100

    Fix Bootstrap

--- terraform/environments/digital-prison-reporting/modules/ec2/scripts/bootstrap.sh
+++ terraform/environments/digital-prison-reporting/modules/ec2/scripts/bootstrap.sh
@@ -25,14 +25,14 @@ sudo yum install –y aws-kinesis-agent
 #sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 
 # Install AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
 sudo ./aws/install
 
 # Configure and Enable Kinesis Agent
 # /tmp/random.log*
 # Additional Configuration here, https://docs.aws.amazon.com/streams/latest/dev/writing-with-agents.html
-cat <<EOF >/etc/aws-kinesis/agent.json
+sudo cat <<EOF >/etc/aws-kinesis/agent.json
 {
     "cloudwatch.emitMetrics":true,
     "kinesis.endpoint":"https://kinesis.eu-west-2.amazonaws.com",
