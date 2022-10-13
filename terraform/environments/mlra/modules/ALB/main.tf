eb90f2d36cd0c840063fa8c8b035f5cc692bea5f
GWood <gareth.wood@digital.justice.gov.uk>
2022-10-13 12:41:57 +0100

    Add Provider to Module, pt2

--- terraform/environments/mlra/modules/ALB/main.tf
+++ terraform/environments/mlra/modules/ALB/main.tf
@@ -1,7 +1,7 @@
 module "lb-access-logs-enabled" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer"
   providers = {
-    aws.bucket-replication = aws
+    aws.bucket-replication = aws.bucket-replication
   }
 
   vpc_all                    = var.vpc_all
