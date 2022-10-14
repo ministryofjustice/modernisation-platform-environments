a5b5f51f150b938bcf76782298b3aac63ffb91b9
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-10-14 08:07:53 +0100

    Fixing ssl policy.

--- terraform/environments/example/loadbalancer.tf
+++ terraform/environments/example/loadbalancer.tf
@@ -101,7 +101,7 @@ resource "aws_lb_listener" "external" {
   port              = local.application_data.accounts[local.environment].server_port
   protocol          = local.application_data.accounts[local.environment].lb_listener_protocol
   #checkov:skip=CKV_AWS_2: "protocol for lb set in application_variables"
-  ssl_policy = "ELBSecurityPolicy-2016-08"
+  ssl_policy = local.application_data.accounts[local.environment].lb_listener_protocol == "HTTP" ? "" : "ELBSecurityPolicy-2016-08"
   #checkov:skip=CKV_AWS_103: "ssl_policy for lb set in application_variables"
 
   default_action {
