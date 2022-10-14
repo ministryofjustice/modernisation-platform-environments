a2bf017db485c1f4937f62739977bad446d7e213
mark roberts <mark.roberts@digital.justice.gov.uk>
2022-10-14 15:02:11 +0100

    upadte of alarm name and TODO actions

--- terraform/environments/mlra/cloudwatch.tf
+++
@@ -1,9 +0,0 @@
-odule "cwalarm_1" {
-  source = "./modules/cloudwatch"
-
-  pClusterName = " "
-  pAutoscalingGroupName = " "
-  pLoadBalancerName = " "
-  pTargetGroupName = aws_lb_target_group.alb_target_group.name 
-  appnameenv = "${local.application_name}-${local.environment}"
-}
--- terraform/environments/mlra/module/cloudwatch/main.tf
+++ terraform/environments/mlra/module/cloudwatch/main.tf
@@ -1,11 +1,16 @@
 resource "aws_cloudwatch_metric_alarm" "esccpuoverthreshold" {
-  alarm_name         = "ECS-CPU-high-threshold-alarm"
+  alarm_name         = "${var.appnameenv}-ECS-CPU-high-threshold-alarm"
   alarm_description  = "If the CPU exceeds the predefined threshold, this alarm will trigger. \n Please investigate."
   namespace          = "AWS/ECS"
   metric_name        = "CPUUtilization"
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn
   threshold          = var.pECSCPUAlarmThreshold
   treat_missing_data = "breaching"
   dimensions = {
@@ -22,6 +27,11 @@ resource "aws_cloudwatch_metric_alarm" "ecsmemoryoverthreshold" {
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pECSMemoryAlarmThreshold
   treat_missing_data = "breaching"
   dimensions = {
@@ -31,13 +41,18 @@ resource "aws_cloudwatch_metric_alarm" "ecsmemoryoverthreshold" {
 }
 resource "aws_cloudwatch_metric_alarm" "cpuoverthreshold" {
 
-  alarm_name         = "CPU-high-threshold-alarm"
+  alarm_name         = "${var.appnameenv}-CPU-high-threshold-alarm"
   alarm_description  = "If the CPU exceeds the predefined threshold, this alarm will trigger. \n Please investigate."
   namespace          = "AWS/EC2"
   metric_name        = "CPUUtilization"
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pASGCPUAlarmThreshold
   treat_missing_data = "breaching"
   dimensions = {
@@ -46,13 +61,18 @@ resource "aws_cloudwatch_metric_alarm" "cpuoverthreshold" {
   comparison_operator = "GreaterThanThreshold"
 }
 resource "aws_cloudwatch_metric_alarm" "statuscheckfailure" {
-  alarm_name         = "status-check-failure-alarm"
+  alarm_name         = "${var.appnameenv}-status-check-failure-alarm"
   alarm_description  = "If a status check failure occurs on an instance, please investigate. http=//docs.aws.amazon.com/AWSEC2/latest/UserGuide/monitoring-system-instance-status-check.html"
   namespace          = "AWS/EC2"
   metric_name        = "StatusCheckFailed"
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pASGStatusFailureAlarmThreshold
   treat_missing_data = "breaching"
   dimensions = {
@@ -62,13 +82,18 @@ resource "aws_cloudwatch_metric_alarm" "statuscheckfailure" {
 }
 # Application Load Balancer Alerting
 resource "aws_cloudwatch_metric_alarm" "targetresponsetime" {
-  alarm_name         = "alb-target-response-time-alarm"
+  alarm_name         = "${var.appnameenv}-alb-target-response-time-alarm"
   alarm_description  = "The time elapsed, in seconds, after the request leaves the load balancer until a response from the target is received"
   namespace          = "AWS/ApplicationELB"
   metric_name        = "TargetResponseTime"
   extended_statistic = "p99"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pALBTargetResponseTimeThreshold
   treat_missing_data = "notBreaching"
   dimensions = {
@@ -77,13 +102,18 @@ resource "aws_cloudwatch_metric_alarm" "targetresponsetime" {
   comparison_operator = "GreaterThanThreshold"
 }
 resource "aws_cloudwatch_metric_alarm" "targetResponsetimemaximum" {
-  alarm_name         = "alb-target-response-time-alarm-maximum"
+  alarm_name         = "${var.appnameenv}-alb-target-response-time-alarm-maximum"
   alarm_description  = "The time elapsed, in seconds, after the request leaves the load balancer until a response from the target is received. Triggered if response is longer than 60s."
   namespace          = "AWS/ApplicationELB"
   metric_name        = "TargetResponseTime"
   statistic          = "Maximum"
   period             = "60"
   evaluation_periods = "1"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pALBTargetResponseTimeThresholdMaximum
   treat_missing_data = "notBreaching"
   dimensions = {
@@ -92,13 +122,18 @@ resource "aws_cloudwatch_metric_alarm" "targetResponsetimemaximum" {
   comparison_operator = "GreaterThanThreshold"
 }
 resource "aws_cloudwatch_metric_alarm" "unhealthyhosts" {
-  alarm_name         = "unhealthy-hosts-alarm"
+  alarm_name         = "${var.appnameenv}-unhealthy-hosts-alarm"
   alarm_description  = "The unhealthy hosts alarm triggers if your load balancer recognises there is an unhealthy host and has been there for over 15 minutes."
   namespace          = "AWS/ApplicationELB"
   metric_name        = "UnHealthyHostCount"
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pALBUnhealthyAlarmThreshold
   treat_missing_data = "notBreaching"
   dimensions = {
@@ -108,13 +143,18 @@ resource "aws_cloudwatch_metric_alarm" "unhealthyhosts" {
   comparison_operator = "GreaterThanThreshold"
 }
 resource "aws_cloudwatch_metric_alarm" "rejectedconnectioncount" {
-  alarm_name         = "RejectedConnectionCount-alarm"
+  alarm_name         = "${var.appnameenv}-RejectedConnectionCount-alarm"
   alarm_description  = "There is no surge queue on ALB's. Alert triggers in ALB rejects too many requests, usually due to backend being busy."
   namespace          = "AWS/ApplicationELB"
   metric_name        = "RejectedConnectionCount"
   statistic          = "Sum"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pALBRejectedAlarmThreshold
   treat_missing_data = "notBreaching"
   dimensions = {
@@ -123,13 +163,18 @@ resource "aws_cloudwatch_metric_alarm" "rejectedconnectioncount" {
   comparison_operator = "GreaterThanThreshold"
 }
 resource "aws_cloudwatch_metric_alarm" "http5xxerror" {
-  alarm_name         = "http-5xx-error-alarm"
+  alarm_name         = "${var.appnameenv}-http-5xx-error-alarm"
   alarm_description  = "This alarm will trigger if we receive 4 5XX http alerts in a 5 minute period."
   namespace          = "AWS/ApplicationELB"
   metric_name        = "HTTPCode_Target_5XX_Count"
   statistic          = "Sum"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pALBTarget5xxAlarmThreshold
   treat_missing_data = "notBreaching"
   dimensions = {
@@ -138,13 +183,18 @@ resource "aws_cloudwatch_metric_alarm" "http5xxerror" {
   comparison_operator = "GreaterThanThreshold"
 }
 resource "aws_cloudwatch_metric_alarm" "applicationelb5xxerror" {
-  alarm_name         = "elb-5xx-error-alarm"
+  alarm_name         = "${var.appnameenv}-elb-5xx-error-alarm"
   alarm_description  = "This alarm will trigger if we receive 4 5XX elb alerts in a 5 minute period."
   namespace          = "AWS/ApplicationELB"
   metric_name        = "HTTPCode_ELB_5XX_Count"
   statistic          = "Sum"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pALB5xxAlarmThreshold
   treat_missing_data = "notBreaching"
   dimensions = {
@@ -153,13 +203,18 @@ resource "aws_cloudwatch_metric_alarm" "applicationelb5xxerror" {
   comparison_operator = "GreaterThanThreshold"
 }
 resource "aws_cloudwatch_metric_alarm" "http4xxerror" {
-  alarm_name         = "http-4xx-error-alarm"
+  alarm_name         = "${var.appnameenv}-http-4xx-error-alarm"
   alarm_description  = "This alarm will trigger if we receive 4 4XX http alerts in a 5 minute period."
   namespace          = "AWS/ApplicationELB"
   metric_name        = "HTTPCode_Target_4XX_Count"
   statistic          = "Sum"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pALBTarget4xxAlarmThreshold
   treat_missing_data = "notBreaching"
   dimensions = {
@@ -168,13 +223,18 @@ resource "aws_cloudwatch_metric_alarm" "http4xxerror" {
   comparison_operator = "GreaterThanThreshold"
 }
 resource "aws_cloudwatch_metric_alarm" "applicationelb4xxerror" {
-  alarm_name         = "elb-4xx-error-alarm"
+  alarm_name         = "${var.appnameenv}-elb-4xx-error-alarm"
   alarm_description  = "This alarm will trigger if we receive 4 4XX elb alerts in a 5 minute period."
   namespace          = "AWS/ApplicationELB"
   metric_name        = "HTTPCode_ELB_4XX_Count"
   statistic          = "Sum"
   period             = "60"
   evaluation_periods = "5"
+  #TODO needs alarm actions and snstopics resources added
+  #AlarmActions
+  #      - !Ref pSnsTopicArn
+  #OKActions
+  #      - !Ref pSnsTopicArn  
   threshold          = var.pALB4xxAlarmThreshold
   treat_missing_data = "notBreaching"
   dimensions = {
