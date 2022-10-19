08cd56fcc6313d8678a48a49d60261f6b41b46f5
James Borwick <james.borwick@icloud.com>
2022-10-19 08:57:11 +0100

    Added RHEL 6.10 ASG for nomis-dev

--- terraform/environments/nomis/nomis-development.tf
+++ terraform/environments/nomis/nomis-development.tf
@@ -50,6 +50,14 @@ locals {
         ami_name = "nomis_rhel_7_9_baseimage*"
         # branch   = var.BRANCH_NAME # comment in if testing ansible
       }
+      dev-base-rhel610 = {
+        tags = {
+          description = "Standalone EC2 for testing RHEL6.10 base image"
+          monitored   = false
+        }
+        ami_name = "nomis_rhel_6_10_baseimage*"
+        # branch   = var.BRANCH_NAME # comment in if testing ansible
+      }
     }
   }
 }
