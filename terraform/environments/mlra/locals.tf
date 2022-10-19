3684ed00f897a2a57a34e7cb488d0fd1a7cac7fa
Alan <alan@automationlogic.com>
2022-10-19 08:15:27 +0100

    mlra ecs foundation setup

--- terraform/environments/mlra/locals.tf
+++ terraform/environments/mlra/locals.tf
@@ -52,4 +52,6 @@ locals {
   # example usage:
   # example_data = local.application_data.accounts[local.environment].example_var
   application_data = fileexists("./application_variables.json") ? jsondecode(file("./application_variables.json")) : {}
+
+  cluster_name = "mlra-ecs-cluster"
 }
