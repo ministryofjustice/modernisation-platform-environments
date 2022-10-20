44a1344013f0646fd4260cb01f8600f8803a5f93
GWood <gareth.wood@digital.justice.gov.uk>
2022-10-20 14:02:19 +0100

    Undo Local Plan

--- terraform/environments/mlra/versions.tf
+++ terraform/environments/mlra/versions.tf
@@ -4,6 +4,10 @@ terraform {
       version = "~> 4.0"
       source  = "hashicorp/aws"
     }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
   required_version = "~> 1.0"
 }
