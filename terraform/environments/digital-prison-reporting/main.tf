ebe84b0e5e1abc4aaf9d8fa232b873a368f09a9c
Hari Chintala <h.chintala@isaggio.com>
2022-10-20 03:38:06 +0100

    Change Scedule for test

--- terraform/environments/digital-prison-reporting/main.tf
+++ terraform/environments/digital-prison-reporting/main.tf
@@ -792,7 +792,7 @@ module "datamart" {
   subnet_ids              = [data.aws_subnet.private_subnets_a.id, data.aws_subnet.private_subnets_b.id, data.aws_subnet.private_subnets_c.id]
   vpc                     = data.aws_vpc.shared.id
   cidr                    = [data.aws_vpc.shared.cidr_block]
-  iam_role_arns           = aws_iam_role.redshift-role.*.arn
+ # iam_role_arns           = aws_iam_role.redshift-role.*.arn
 
   # Endpoint access - only available when using the ra3.x type, for S3 Simple Service
   create_endpoint_access = false
@@ -809,7 +809,7 @@ module "datamart" {
     resume = {
       name           = "${local.redshift_cluster_name}-resume"
       description    = "Resume cluster every morning"
-      schedule       = "cron(0 03 * * ? *)"
+      schedule       = "cron(50 03 * * ? *)"
       resume_cluster = true
     }
   }
