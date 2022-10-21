d4248404277a0f6fe508e5172652d8f7d6650c4b
mikereiddigital <mike.reid@digital.justice.gov.uk>
2022-10-21 11:36:28 +0100

    First round of bug fixing.

--- terraform/environments/mlra/networklb.tf
+++ terraform/environments/mlra/networklb.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "nlb-ingress" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [10.202.0.0/20]
+    cidr_blocks      = ["10.202.0.0/20"]
   }
 
   egress {
