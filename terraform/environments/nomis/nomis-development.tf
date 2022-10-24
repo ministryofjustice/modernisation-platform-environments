3c532758b5ffdedb859ea938871cf664301a7f36
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-10-24 16:29:46 +0100

    add test ec2_instance to nomis-development env

--- terraform/environments/nomis/nomis-development.tf
+++ terraform/environments/nomis/nomis-development.tf
@@ -40,7 +40,15 @@ locals {
 
     }
     weblogics          = {}
-    ec2_test_instances = {}
+    ec2_test_instances = {
+      ssm_test_instance = {
+        tags = {
+          description = "test instance for ssm permissions testing"
+          monitored   = false
+        }
+        ami_name = "nomis_rhel_7_9_baseimage*"
+      }
+    }
     ec2_test_autoscaling_groups = {
       dev-base-rhel79 = {
         tags = {
