22ba57b6ee72b931f58318003c4bf56f44e30fd8
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2022-10-28 16:44:00 +0100

    Enable CW modules and SNS Topic

--- terraform/environments/mlra/snstopic.tf
+++ terraform/environments/mlra/snstopic.tf
@@ -18,22 +18,22 @@ data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
 #TODO currently the cloud watch module is ready but is missing a few key imputs from the ALB setup
 #just waiting for these to be complete before making this section live
 
-# module "cwalarm" {
-#  source = "./modules/cloudwatch"
-#
-#  pClusterName = " "
-#  pAutoscalingGroupName = " "
-#  pLoadBalancerName = " "
-#  pTargetGroupName = aws_lb_target_group.alb_target_group.name
-#  appnameenv = "${local.application_name}-${local.environment}"
-#  snsTopicName = local.sns_topic_name
-# }
-#
-# module "pagerduty_core_alerts" {
-#   depends_on = [
-#     module.cwalarm
-#   ]
-#   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
-#   sns_topics                = [local.sns_topic_name]
-#   pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_cloudwatch"]
-# }
+module "cwalarm" {
+  source = "./modules/cloudwatch"
+
+  pClusterName = local.application_name
+  pAutoscalingGroupName = "${local.application_name}-cluster-scaling-group"
+  pLoadBalancerName = module.alb.load_balancer.arn_suffix
+  pTargetGroupName = module.alb.target_group_name
+  appnameenv = "${local.application_name}-${local.environment}"
+  snsTopicName = local.sns_topic_name
+}
+
+module "pagerduty_core_alerts" {
+  depends_on = [
+    module.cwalarm
+  ]
+  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
+  sns_topics                = [local.sns_topic_name]
+  pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_cloudwatch"]
+}
