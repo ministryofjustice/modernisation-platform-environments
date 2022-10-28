5ff64bc3ffa2891ea10bb867a4de3404f31b6837
David Elliott <david.elliott@digital.justice.gov.uk>
2022-10-28 16:54:10 +0100

    Clean up PPUD files
    Clean up the old PPUD files from the instance transfer and update with
    latest template files in preparation for the PPUD migration.

--- terraform/environments/ppud/versions.tf
+++ terraform/environments/ppud/versions.tf
@@ -1,20 +1,13 @@
 terraform {
-  required_version = "~> 1.0"
-
   required_providers {
     aws = {
       version = "~> 4.0"
       source  = "hashicorp/aws"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1"
-    }
-
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2"
+    http = {
+      version = "~> 3.0"
+      source  = "hashicorp/http"
     }
   }
+  required_version = "~> 1.0"
 }
