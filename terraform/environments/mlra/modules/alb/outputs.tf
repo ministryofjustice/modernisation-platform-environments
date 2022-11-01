c1c4c14720f35a42d0e85df34d37c0c3d8c795a4
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2022-11-01 12:39:48 +0000

    Prepare merging of CW work

--- terraform/environments/mlra/modules/alb/outputs.tf
+++ terraform/environments/mlra/modules/alb/outputs.tf
@@ -9,3 +9,7 @@ output "security_group" {
 output "load_balancer" {
   value = module.lb-access-logs-enabled.load_balancer
 }
+
+output "target_group_name" {
+  value = aws_lb_target_group.alb_target_group.name
+}
