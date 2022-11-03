d31cfc04218f1fddc4b710be042c36c8b18ce949
Hari Chintala <h.chintala@isaggio.com>
2022-11-03 13:18:30 +0000

    KMS Policy

--- terraform/environments/digital-prison-reporting/modules/glue_job/main.tf
+++ terraform/environments/digital-prison-reporting/modules/glue_job/main.tf
@@ -129,7 +129,8 @@ data "aws_iam_policy_document" "extra-policy-document" {
       "iam:ListRolePolicies",
       "iam:GetRole",
       "iam:GetRolePolicy",
-      "cloudwatch:PutMetricData"
+      "cloudwatch:PutMetricData",
+      "kms:*"
     ]
     resources = [
       "*"
