55cb32cbc5c0b6e6f783ef8d05255ca7fae5e144
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-11-10 14:28:59 +0000

    DSOS-1440: let autoscale groups access SSM params

--- terraform/environments/nomis/nomis-development.tf
+++ terraform/environments/nomis/nomis-development.tf
@@ -122,8 +122,9 @@ locals {
           instance_type                = "t2.medium"
           metadata_options_http_tokens = "optional"
         }
-        ami_name = "nomis_rhel_6_10_weblogic_appserver_10_3*"
-        branch   = var.BRANCH_NAME # comment in if testing ansible
+        ssm_parameters = {}
+        ami_name       = "nomis_rhel_6_10_weblogic_appserver_10_3*"
+        branch         = var.BRANCH_NAME # comment in if testing ansible
       }
     }
   }
