24c17a9d052ba30196d14fff98d47d742f080f2a
James Borwick <james.borwick@icloud.com>
2022-11-15 12:18:22 +0000

    Uncommenting data resource

--- terraform/environments/nomis/loadbalancer.tf
+++ terraform/environments/nomis/loadbalancer.tf
@@ -1,15 +1,15 @@
 #------------------------------------------------------------------------------
 # Load Balancer - Internal
 #------------------------------------------------------------------------------
-# data "aws_subnets" "private" {
-#   filter {
-#     name   = "vpc-id"
-#     values = [local.vpc_id]
-#   }
-#   tags = {
-#     Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-private-${local.region}*"
-#   }
-# }
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
+  tags = {
+    Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-private-${local.region}*"
+  }
+}
 
 # resource "aws_security_group" "internal_elb" {
 
