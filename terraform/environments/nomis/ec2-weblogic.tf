a7f90a149aa762b9262ad75bd8ea5517720ee2c9
James Borwick <james.borwick@icloud.com>
2022-11-17 16:39:38 +0000

    Removing security group rules

--- terraform/environments/nomis/ec2-weblogic.tf
+++ terraform/environments/nomis/ec2-weblogic.tf
@@ -158,6 +158,66 @@ resource "aws_security_group" "weblogic_common" {
   name        = "weblogic-common"
   vpc_id      = local.vpc_id
 
+  # ingress {
+  #   description     = "SSH from Bastion"
+  #   from_port       = "22"
+  #   to_port         = "22"
+  #   protocol        = "TCP"
+  #   security_groups = [module.bastion_linux.bastion_security_group]
+  # }
+
+  # ingress {
+  #   description     = "access from Windows Jumpserver (admin console)"
+  #   from_port       = "7001"
+  #   to_port         = "7001"
+  #   protocol        = "TCP"
+  #   security_groups = [aws_security_group.jumpserver-windows.id]
+  # }
+
+  # ingress {
+  #   description     = "access from Windows Jumpserver"
+  #   from_port       = "80"
+  #   to_port         = "80"
+  #   protocol        = "TCP"
+  #   security_groups = [aws_security_group.jumpserver-windows.id]
+  # }
+
+  # ingress {
+  #   description = "access from Windows Jumpserver and loadbalancer (forms/reports)"
+  #   from_port   = "7777"
+  #   to_port     = "7777"
+  #   protocol    = "TCP"
+  #   security_groups = [
+  #     aws_security_group.jumpserver-windows.id,
+  #     local.environment == "test" ? "sg-075cf385b5a966b04" : aws_security_group.internal_elb.id
+  #   ]
+  # }
+
+  # ingress {
+  #   description = "access from Cloud Platform Prometheus server"
+  #   from_port   = "9100"
+  #   to_port     = "9100"
+  #   protocol    = "TCP"
+  #   cidr_blocks = [local.cidrs.cloud_platform]
+  # }
+
+  ingress {
+    description = "access from Cloud Platform Prometheus script exporter collector"
+    from_port   = "9172"
+    to_port     = "9172"
+    protocol    = "TCP"
+    cidr_blocks = [local.cidrs.cloud_platform]
+  }
+
+  # egress {
+  #   description = "allow all"
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   #tfsec:ignore:aws-vpc-no-public-egress-sgr
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
   tags = merge(
     local.tags,
     {
