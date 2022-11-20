d46dd3b2553d73f7c248e863dc6bf8094caeddb0
Hari Chintala <h.chintala@isaggio.com>
2022-11-20 23:44:21 +0000

    Fix MR Conflicts

--- terraform/environments/digital-prison-reporting/modules/kinesis_firehose/main.tf
+++ terraform/environments/digital-prison-reporting/modules/kinesis_firehose/main.tf
@@ -39,47 +39,4 @@ resource "aws_iam_role" "firehose_role" {
   ]
 }
 EOF
-
-  inline_policy = {
-    name = "kinesis-s3-inline-policy"
-    policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Effect = "Allow",
-          Action = [
-            "s3:AbortMultipartUpload",
-            "s3:GetBucketLocation",
-            "s3:GetObject",
-            "s3:ListBucket",
-            "s3:ListBucketMultipartUploads",
-            "s3:PutObject"
-          ],
-          Resource = [
-            "arn:aws:s3:::${var.source_s3_id}",
-            "arn:aws:s3:::${var.source_s3_id}/*"
-          ]
-        },
-        {
-          Effect = "Allow",
-          Action = [
-            "kinesis:DescribeStream",
-            "kinesis:GetShardIterator",
-            "kinesis:GetRecords",
-            "kinesis:ListShards"
-          ],
-          Resource = "arn:aws:kinesis:${var.aws_region}:${var.aws_account_id}:stream/${var.kinesis_source_stream_name}"
-        },
-        {
-          Effect = "Allow",
-          Action = [
-            "logs:PutLogEvents"
-          ],
-          Resource = [
-            "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:/aws/kinesisfirehose/*"
-          ]
-        },
-      ]
-    })
-  }
 }
