656d11f06176bc486195565489146cf357a62320
Hari Chintala <h.chintala@isaggio.com>
2022-11-21 00:02:17 +0000

    correcting s3 notification sqs

--- terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
+++ terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
@@ -64,7 +64,7 @@ resource "aws_sqs_queue" "notification_queue" {
       "Action": "sqs:SendMessage",
       "Resource": "arn:aws:sqs:*:*:s3-event-queue",
       "Condition": {
-        "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.application_tf_state.arn}" }
+        "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.application_tf_state[0].arn}" }
       }
     }
   ]
@@ -74,7 +74,7 @@ POLICY
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
   count  = var.create_notification_queue ? 1 : 0
-  bucket = aws_s3_bucket.application_tf_state.*.id
+  bucket = aws_s3_bucket.application_tf_state[0].id
 
   queue {
     queue_arn = aws_sqs_queue.notification_queue.arn
