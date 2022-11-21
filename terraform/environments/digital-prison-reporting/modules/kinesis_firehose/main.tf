6d3ef2eeb0bf92cf4b23163473e89b250fd2f920
Hari Chintala <h.chintala@isaggio.com>
2022-11-21 11:07:20 +0000

    Fixing the Policy

--- terraform/environments/digital-prison-reporting/modules/kinesis_firehose/main.tf
+++ terraform/environments/digital-prison-reporting/modules/kinesis_firehose/main.tf
@@ -20,23 +20,3 @@ resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
     }
   }
 }
-
-resource "aws_iam_role" "firehose_role" {
-  name = "firehose_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "firehose.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
