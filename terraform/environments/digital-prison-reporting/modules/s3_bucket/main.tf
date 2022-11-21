6520a6feb93764c50abb6d65e59a418039e3545c
Hari Chintala <h.chintala@isaggio.com>
2022-11-21 12:45:51 +0000

    Adding Principals *

--- terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
+++ terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
@@ -58,7 +58,7 @@ resource "aws_s3_bucket_policy" "allow_sqs_access" {
 
 data "aws_iam_policy_document" "allow_sqs_access" {
   count = var.create_notification_queue ? 1 : 0
-  statement {
+  statement {  
     principals {
       type        = "AWS"
       identifiers = ["*"]
@@ -69,8 +69,7 @@ data "aws_iam_policy_document" "allow_sqs_access" {
     ]
 
     resources = [
-      aws_s3_bucket.application_tf_state[0].arn,
-      "${aws_s3_bucket.application_tf_state[0].arn}/*",
+      aws_sqs_queue.notification_queue[0].arn,
     ]
   }
 }
