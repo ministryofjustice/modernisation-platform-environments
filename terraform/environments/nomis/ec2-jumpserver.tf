b539ff11326041ba1fe78cea87c86ead3d18c9b9
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-11-23 11:10:43 +0000

    rename files so the PR is easier to read

--- terraform/environments/nomis/ec2-jumpserver.tf
+++ terraform/environments/nomis/ec2-jumpserver.tf
@@ -1,209 +1,135 @@
-#--------------------------------------------------------------------------------
-# Jumpserver
-# This is not in use YET as we are still using the old ec2-jumpserver-old.tf file
-# Once the password rotation is worked out we can switch to this new version as
-# it uses the ec2_autoscaling_group module
-#
-# Obtain your user password from the AWS Secrets Manager for your user e.g. 
-# /jumpserver-asg/Users/<your-github-username>
-#--------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------
+# Windows Jumpserver - Currently in use
+# See comments in the README.md files and DSOS-1584 with respect to this EC" instance.
+# This all needs to be removed once the new jumpserver is in place.
+#-------------------------------------------------------------------------------------
 
 locals {
-
-  secret_prefix_asg = "/jumpserver-asg/users"
-
-  ec2_jumpserver = {
-
-    tags = {
-      description = "nomis windows jumpserver"
-      component   = "jumpserver"
-    }
-
-    instance = {
-      disable_api_termination      = false
-      instance_type                = "t3.medium"
-      key_name                     = aws_key_pair.ec2-user.key_name
-      monitoring                   = false
-      metadata_options_http_tokens = "required"
-      vpc_security_group_ids       = [aws_security_group.jumpserver-windows.id]
-    }
-
-    user_data_raw = base64encode(templatefile("./templates/jumpserver-user-data.yaml", { SECRET_PREFIX = local.secret_prefix_asg, S3_BUCKET = module.s3-bucket.bucket.id }))
-
-    autoscaling_group = {
-      desired_capacity = 1
-      max_size         = 1
-      min_size         = 0
-      force_delete     = true
-    }
-  }
+  secret_prefix = "/Jumpserver/Users"
 }
 
-module "ec2_jumpserver_autoscaling_group" {
-  source = "./modules/ec2_autoscaling_group"
-
-
-  providers = {
-    aws.core-vpc = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
-  }
-
-  for_each = try(local.environment_config.ec2_jumpserver_autoscaling_groups, {})
-
-  name                  = each.key
-  ami_name              = each.value.ami_name
-  ami_owner             = try(each.value.ami_owner, "core-shared-services-production")
-  instance              = merge(local.ec2_jumpserver.instance, lookup(each.value, "instance", {}))
-  user_data_raw         = local.ec2_jumpserver.user_data_raw
-  ebs_volume_config     = lookup(each.value, "ebs_volume_config", {})
-  ebs_volumes           = lookup(each.value, "ebs_volumes", {})
-  ssm_parameters_prefix = "jumpserver/"
-  ssm_parameters        = {}
-  autoscaling_group     = merge(local.ec2_jumpserver.autoscaling_group, lookup(each.value, "autoscaling_group", {}))
-  autoscaling_schedules = coalesce(lookup(each.value, "autoscaling_schedules", null), {
-    # if sizes not set, use the values defined in autoscaling_group
-    "scale_up" = {
-      recurrence = "0 7 * * Mon-Fri"
-    }
-    "scale_down" = {
-      desired_capacity = lookup(each.value, "offpeak_desired_capacity", 0)
-      recurrence       = "0 19 * * Mon-Fri"
-    }
-  })
-
-  iam_resource_names_prefix = "ec2-jumpserver-asg"
-  instance_profile_policies = concat(local.ec2_common_managed_policies, [aws_iam_policy.secret_access_jumpserver_asg.arn])
-  business_unit             = local.vpc_name
-  application_name          = local.application_name
-  environment               = local.environment
-  region                    = local.region
-  availability_zone         = local.availability_zone
-  subnet_set                = local.subnet_set
-  subnet_name               = "private"
-  tags                      = merge(local.tags, local.ec2_jumpserver.tags, try(each.value.tags, {}))
-  account_ids_lookup        = local.environment_management.account_ids
-  ansible_repo              = "modernisation-platform-configuration-management"
-  ansible_repo_basedir      = "ansible"
-  branch                    = try(each.value.branch, "main")
-
-
-}
-
-#------------------------------------------------------------------------------
-# Common Security Group for Jumpserver Instances
-#------------------------------------------------------------------------------
-
-resource "aws_security_group" "jumpserver-windows" {
-  description = "Configure Windows jumpserver egress"
-  name        = "jumpserver-windows-${local.application_name}"
-  vpc_id      = local.vpc_id
-
-  ingress {
-    description = "access from Cloud Platform Prometheus server"
-    from_port   = "9100"
-    to_port     = "9100"
-    protocol    = "TCP"
-    cidr_blocks = [local.cidrs.cloud_platform]
-  }
-
-  egress {
-    description = "allow all"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    #tfsec:ignore:aws-vpc-no-public-egress-sgr
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(
-    local.tags,
-    {
-      Name = "jumpserver-commmon"
-    }
-  )
-}
-
-# required as part of the user manager setup
-data "github_team" "jumpserver" {
+# This is defined in the new ec-jumpserver.tf file
+/* data "github_team" "jumpserver" {
   slug = "studio-webops"
-}
+} */
 
-# resource policy to restrict access to secret value to specific user and the CICD role used to deploy terraform
-data "aws_iam_policy_document" "jumpserver_secrets_asg" {
-  for_each = toset(data.github_team.jumpserver.members)
-  statement {
-    effect    = "Deny"
-    actions   = ["secretsmanager:GetSecretValue"]
-    resources = ["*"]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "StringNotLike"
-      variable = "aws:userid"
-      values = [
-        "*:${each.value}@digital.justice.gov.uk",                       # specific user
-        "${data.aws_iam_role.member_infrastructure_access.unique_id}:*" # terraform CICD role
-      ]
-    }
+data "aws_vpc" "jumpserver" {
+  tags = {
+    Name = "${local.vpc_name}-${local.environment}"
+  }
+}
+data "aws_subnets" "jumpserver" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.jumpserver.id]
+  }
+  tags = {
+    Name = "${local.vpc_name}-${local.environment}-${local.subnet_set}-private-${local.region}*"
   }
 }
 
-#------
-# Jumpserver specific
-#------
-resource "aws_iam_instance_profile" "jumpserver_asg" {
-  name = "ec2-jumpserver-profile-asg"
-  role = aws_iam_role.jumpserver_asg.name
-  path = "/"
-}
+data "aws_ami" "jumpserver" {
+  most_recent = true
+  owners      = [local.environment_management.account_ids["core-shared-services-production"]]
 
-# create empty secret in secret manager
-#tfsec:ignore:aws-ssm-secret-use-customer-key
-resource "aws_secretsmanager_secret" "jumpserver_asg" {
-  #checkov:skip=CKV_AWS_149: "Ensure that Secrets Manager secret is encrypted using KMS CMK"
-  for_each                = toset(data.github_team.jumpserver.members)
-  name                    = "${local.secret_prefix_asg}/${each.value}"
-  policy                  = data.aws_iam_policy_document.jumpserver_secrets_asg[each.value].json
-  recovery_window_in_days = 0
-  tags = merge(
-    local.tags,
-    {
-      Name = "jumpserver-user-${each.value}"
-    },
-  )
-}
-
-# IAM policy permissions to enable jumpserver to list secrets and put user passwords into secret manager
-data "aws_iam_policy_document" "jumpserver_users_asg" {
-  statement {
-    effect    = "Allow"
-    actions   = ["secretsmanager:PutSecretValue"]
-    resources = ["arn:aws:secretsmanager:${local.region}:${data.aws_caller_identity.current.id}:secret:${local.secret_prefix_asg}/*"]
+  filter {
+    name   = "name"
+    values = ["nomis_windows_server_2022_jumpserver*"]
   }
-  statement {
-    effect    = "Allow"
-    actions   = ["secretsmanager:ListSecrets"]
-    resources = ["*"]
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
-# IAM role for jumpserver instances
-resource "aws_iam_policy" "secret_access_jumpserver_asg" {
-  name        = "write-access-to-secret-store"
-  path        = "/"
-  description = "Policy for access to secret store"
-  policy      = data.aws_iam_policy_document.jumpserver_users_asg.json
-  tags = merge(
-    local.tags,
-    {
-      Name = "write-access-to-secret-store"
-    },
-  )
+# instance launch template
+resource "aws_launch_template" "jumpserver" {
+  name                                 = "${local.vpc_name}-${local.environment}-jumpserver"
+  image_id                             = data.aws_ami.jumpserver.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type                        = "t3.medium"
+  key_name                             = aws_key_pair.ec2-user.key_name
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.jumpserver.arn
+  }
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  monitoring {
+    enabled = false
+  }
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.jumpserver-windows.id]
+    delete_on_termination       = true
+  }
+
+  #checkov:skip=CKV_SECRET_6: "Base64 High Entropy String"
+  user_data = base64encode(templatefile("./templates/jumpserver-user-data.yaml", { SECRET_PREFIX = local.secret_prefix, S3_BUCKET = module.s3-bucket.bucket.id }))
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(
+      local.tags,
+      {
+        Name          = "jumpserver_windows"
+        os_type       = "Windows"
+        os_version    = "2022"
+        "Patch Group" = aws_ssm_patch_group.windows.patch_group
+      }
+    )
+  }
+
+  lifecycle {
+    ignore_changes = [image_id, description, tags, tags_all]
+  }
 }
 
-resource "aws_iam_role" "jumpserver_asg" {
-  name                 = "ec2-jumpserver-role-asg"
+# autoscaling
+resource "aws_autoscaling_group" "jumpserver" {
+  launch_template {
+    id      = aws_launch_template.jumpserver.id
+    version = "$Default"
+  }
+  desired_capacity    = 1
+  name                = "jumpserver-autoscaling-group"
+  min_size            = 0
+  max_size            = 1
+  force_delete        = true
+  vpc_zone_identifier = data.aws_subnets.jumpserver.ids
+  tag {
+    key                 = "Name"
+    value               = "jumpserver"
+    propagate_at_launch = true
+  }
+  depends_on = [
+    aws_launch_template.jumpserver
+  ]
+}
+resource "aws_autoscaling_schedule" "scale_up" {
+  scheduled_action_name  = "jumpserver_scale_up"
+  min_size               = 0
+  max_size               = 1
+  desired_capacity       = 1
+  recurrence             = "0 7 * * Mon-Fri"
+  autoscaling_group_name = aws_autoscaling_group.jumpserver.name
+}
+
+resource "aws_autoscaling_schedule" "scale_down" {
+  scheduled_action_name  = "jumpserver_scale_down"
+  min_size               = 0
+  max_size               = 0
+  desired_capacity       = 0
+  recurrence             = "0 19 * * Mon-Fri"
+  autoscaling_group_name = aws_autoscaling_group.jumpserver.name
+}
+
+resource "aws_iam_role" "jumpserver" {
+  name                 = "ec2-jumpserver-role"
   path                 = "/"
   max_session_duration = "3600"
   assume_role_policy = jsonencode(
@@ -225,7 +151,72 @@ resource "aws_iam_role" "jumpserver_asg" {
   tags = merge(
     local.tags,
     {
-      Name = "ec2-jumpserver-role-asg"
+      Name = "ec2-jumpserver-role"
     },
   )
+}
+
+resource "aws_iam_instance_profile" "jumpserver" {
+  name = "ec2-jumpserver-profile"
+  role = aws_iam_role.jumpserver.name
+  path = "/"
+}
+
+# create empty secret in secret manager
+#tfsec:ignore:aws-ssm-secret-use-customer-key
+resource "aws_secretsmanager_secret" "jumpserver" {
+  #checkov:skip=CKV_AWS_149: "Ensure that Secrets Manager secret is encrypted using KMS CMK"
+  for_each                = toset(data.github_team.jumpserver.members)
+  name                    = "${local.secret_prefix}/${each.value}"
+  policy                  = data.aws_iam_policy_document.jumpserver_secrets[each.value].json
+  recovery_window_in_days = 0
+  tags = merge(
+    local.tags,
+    {
+      Name = "jumpserver-user-${each.value}"
+    },
+  )
+}
+
+# resource policy to restrict access to secret value to specific user and the CICD role used to deploy terraform
+data "aws_iam_policy_document" "jumpserver_secrets" {
+  for_each = toset(data.github_team.jumpserver.members)
+  statement {
+    effect    = "Deny"
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["*"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "StringNotLike"
+      variable = "aws:userid"
+      values = [
+        "*:${each.value}@digital.justice.gov.uk",                       # specific user
+        "${data.aws_iam_role.member_infrastructure_access.unique_id}:*" # terraform CICD role
+      ]
+    }
+  }
+}
+
+# IAM policy permissions to enable jumpserver to list secrets and put user passwords into secret manager
+data "aws_iam_policy_document" "jumpserver_users" {
+  statement {
+    effect    = "Allow"
+    actions   = ["secretsmanager:PutSecretValue"]
+    resources = ["arn:aws:secretsmanager:${local.region}:${data.aws_caller_identity.current.id}:secret:${local.secret_prefix}/*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["secretsmanager:ListSecrets"]
+    resources = ["*"]
+  }
+}
+
+# Add policy to role
+resource "aws_iam_role_policy" "jumpserver_users" {
+  name   = "secrets-access-jumpserver-users"
+  role   = aws_iam_role.jumpserver.id
+  policy = data.aws_iam_policy_document.jumpserver_users.json
 }
