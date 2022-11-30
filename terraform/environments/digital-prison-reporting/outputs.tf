53d70234277862a4a93884a52afd90934edee68e
Hari Chintala <h.chintala@isaggio.com>
2022-11-30 17:13:53 +0000

    Removing Shard Count References few more

--- terraform/environments/digital-prison-reporting/outputs.tf
+++ terraform/environments/digital-prison-reporting/outputs.tf
@@ -8,10 +8,10 @@ output "kinesis_stream_name" {
   value       = module.kinesis_stream_ingestor.kinesis_stream_name
 }
 
-output "kinesis_stream_shard_count" {
-  description = "The count of Shards for this Stream"
-  value       = module.kinesis_stream_ingestor.kinesis_stream_shard_count
-}
+#output "kinesis_stream_shard_count" {
+#  description = "The count of Shards for this Stream"
+#  value       = module.kinesis_stream_ingestor.kinesis_stream_shard_count
+#}
 
 output "kinesis_stream_arn" {
   description = "The Amazon Resource Name (ARN) specifying the Stream"
