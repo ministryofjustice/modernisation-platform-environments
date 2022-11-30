e1f48b979090c872af6d8f4d69cb37cc7826a3ca
Hari Chintala <h.chintala@isaggio.com>
2022-11-30 16:52:52 +0000

    Adding sudo amazon-linux-extras install postgresql10

--- terraform/environments/digital-prison-reporting/modules/ec2/scripts/bootstrap_apps.sh
+++ terraform/environments/digital-prison-reporting/modules/ec2/scripts/bootstrap_apps.sh
@@ -28,6 +28,9 @@ sudo yum install https://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclien
 sudo yum install https://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient21/x86_64/getPackage/oracle-instantclient-devel-21.8.0.0.0-1.x86_64.rpm
 sudo yum install https://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient21/x86_64/getPackage/oracle-instantclient-sqlplus-21.8.0.0.0-1.x86_64.rpm
 
+# Install Postgresql
+sudo amazon-linux-extras install postgresql10
+
 # Install SSM Agent
 #cd /tmp
 #sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
