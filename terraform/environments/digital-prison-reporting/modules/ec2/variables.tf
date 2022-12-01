98da3103a7441956be50e7975e10458e80c82065
Hari Chintala <h.chintala@isaggio.com>
2022-12-01 17:28:29 +0000

    Adding Postgres Entries for Ec2 Access

--- terraform/environments/digital-prison-reporting/modules/ec2/variables.tf
+++ terraform/environments/digital-prison-reporting/modules/ec2/variables.tf
@@ -42,7 +42,12 @@ variable "ec2_sec_rules" {
       "from_port" = 5439,
       "to_port"   = 5439,
       "protocol"  = "TCP"
-    }
+    },
+    "postgres" = {
+      "from_port" = 5432,
+      "to_port"   = 5432,
+      "protocol"  = "TCP"
+    }    
   }
 }
 
