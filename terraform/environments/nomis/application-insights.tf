396b016d467eb59b918f818e16495d0ba8eb3506
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-12-05 17:03:19 +0000

    comment out non-working part

--- terraform/environments/nomis/application-insights.tf
+++ terraform/environments/nomis/application-insights.tf
@@ -1,6 +1,7 @@
-resource "aws_applicationinsights_application" "oracle" {
-  resource_group_name = aws_resourcegroups_group.oracle.name
-}
+# User: arn:aws:sts::***:assumed-role/MemberInfrastructureAccess/aws-go-sdk-1670248274835387038 is not authorized to perform: applicationinsights:CreateApplication
+# resource "aws_applicationinsights_application" "oracle" {
+#   resource_group_name = aws_resourcegroups_group.oracle.name
+#}
 
 resource "aws_resourcegroups_group" "oracle" {
   name = "oracle"
