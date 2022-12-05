57252c365688bf9270b32e9a58701a9ef2f8a551
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2022-12-05 15:54:08 +0000

    Add AD logging for delius-iaps

--- terraform/environments/delius-iaps/ad.tf
+++ terraform/environments/delius-iaps/ad.tf
@@ -32,7 +32,36 @@ resource "aws_directory_service_directory" "active_directory" {
 }
 
 # Set up logging for the Managed AD
-# To do...
+# Commented out while we wait for ability to deploy an AD instance
+# resource "aws_cloudwatch_log_group" "active_directory" {
+#   name              = "/aws/directoryservice/${aws_directory_service_directory.active_directory.id}"
+#   retention_in_days = 14
+# }
 
-# Set up auto-join domain doc for EC2 instances
-# To do...
+# data "aws_iam_policy_document" "ad-log-policy" {
+#   statement {
+#     actions = [
+#       "logs:CreateLogStream",
+#       "logs:PutLogEvents",
+#     ]
+
+#     principals {
+#       identifiers = ["ds.amazonaws.com"]
+#       type        = "Service"
+#     }
+
+#     resources = ["${aws_cloudwatch_log_group.active_directory.arn}:*"]
+
+#     effect = "Allow"
+#   }
+# }
+
+# resource "aws_cloudwatch_log_resource_policy" "active_directory-log-policy" {
+#   policy_document = data.aws_iam_policy_document.ad-log-policy.json
+#   policy_name     = "ad-log-policy-${local.application_name}-${local.environment}"
+# }
+
+# resource "aws_directory_service_log_subscription" "active_directory" {
+#   directory_id   = aws_directory_service_directory.active_directory.id
+#   log_group_name = aws_cloudwatch_log_group.active_directory.name
+# }
