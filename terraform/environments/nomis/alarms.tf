9f9102c7a81e93f608fd120683e354ce73636afb
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-12-06 18:20:52 +0000

    create application insights now that permissions are added to mp

--- terraform/environments/nomis/alarms.tf
+++ terraform/environments/nomis/alarms.tf
@@ -8,9 +8,9 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
   statistic           = "Average"
   threshold           = "80"
   alarm_description   = "This metric monitors ec2 cpu utilization"
-  # alarm_actions       = [aws_sns_topic.alarm.arn]
+  # alarm_actions       = [aws_sns_topic.alarm.arn] TODO: needs changing - need our own SNS topic using our own pagerduty-integration module
   # dimensions = {
-  #   InstanceId = aws_instance.example.id
+  #   InstanceId = aws_instance.example.id TODO: change this so it works on a per-instance basis
   #}
   tags = {
     Name = "cpu_utilization"
