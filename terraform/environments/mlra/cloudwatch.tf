01d4b9a9e8ec85050b8bc015978415bd7d1695dc
mikereiddigital <mike.reid@digital.justice.gov.uk>
2022-12-07 17:23:39 +0000

    and delete cloudwatch resources.

--- terraform/environments/mlra/cloudwatch.tf
+++ terraform/environments/mlra/cloudwatch.tf
@@ -1,35 +1,35 @@
 #Get Pagerduty keys from modplatform
-locals {
-  pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
-  sns_topic_name             = "${local.application_name}-${local.environment}-alerting-topic"
-}
-
-data "aws_secretsmanager_secret" "pagerduty_integration_keys" {
-  provider = aws.modernisation-platform
-  name     = "pagerduty_integration_keys"
-}
-
-data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
-  provider  = aws.modernisation-platform
-  secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
-}
-
-module "cwalarm" {
-  source = "./modules/cloudwatch"
-
-  pClusterName          = local.application_name
-  pAutoscalingGroupName = "${local.application_name}-cluster-scaling-group"
-  pLoadBalancerName     = module.alb.load_balancer.arn_suffix
-  pTargetGroupName      = module.alb.target_group_name
-  appnameenv            = "${local.application_name}-${local.environment}"
-  snsTopicName          = local.sns_topic_name
-}
-
-module "pagerduty_core_alerts" {
-  depends_on = [
-    module.cwalarm
-  ]
-  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
-  sns_topics                = [local.sns_topic_name]
-  pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_cloudwatch"]
-}
+# locals {
+#   pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
+#   sns_topic_name             = "${local.application_name}-${local.environment}-alerting-topic"
+# }
+# 
+# data "aws_secretsmanager_secret" "pagerduty_integration_keys" {
+#   provider = aws.modernisation-platform
+#   name     = "pagerduty_integration_keys"
+# }
+# 
+# data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
+#   provider  = aws.modernisation-platform
+#   secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
+# }
+# 
+# module "cwalarm" {
+#   source = "./modules/cloudwatch"
+# 
+#   pClusterName          = local.application_name
+#   pAutoscalingGroupName = "${local.application_name}-cluster-scaling-group"
+#   pLoadBalancerName     = module.alb.load_balancer.arn_suffix
+#   pTargetGroupName      = module.alb.target_group_name
+#   appnameenv            = "${local.application_name}-${local.environment}"
+#   snsTopicName          = local.sns_topic_name
+# }
+# 
+# module "pagerduty_core_alerts" {
+#   depends_on = [
+#     module.cwalarm
+#   ]
+#   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
+#   sns_topics                = [local.sns_topic_name]
+#   pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_cloudwatch"]
+# }
