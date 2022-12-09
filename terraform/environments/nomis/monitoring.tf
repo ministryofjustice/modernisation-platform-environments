e454dbc6a54bdd2950a82905a79f87b1339f3fcb
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-12-09 15:25:31 +0000

    test creating alerts

--- terraform/environments/nomis/monitoring.tf
+++ terraform/environments/nomis/monitoring.tf
@@ -1,17 +1,17 @@
 
 # Restricts monitoring to nomis-production environment and monitored instances only
-data "aws_instances" "nomis" {
+/* data "aws_instances" "nomis" {
   instance_tags = {
     environment = "nomis-production"
     monitored   = true 
   }
   instance_state_names = ["running"]
-}
+} */
 
 # Status and Instance Health Check Alarm
 
 resource "aws_cloudwatch_metric_alarm" "status_and_instance_health_check" {
-  for_each            = toset(data.aws_instances.nomis.ids)
+  # for_each            = toset(data.aws_instances.nomis.ids)
   alarm_name          = "status_and_instance_health_check_${each.value}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
@@ -22,9 +22,9 @@ resource "aws_cloudwatch_metric_alarm" "status_and_instance_health_check" {
   threshold           = "1"
   alarm_description   = "This metric monitors ec2 status and instance health check"
   alarm_actions       = [aws_sns_topic.nomis_alarms.arn]
-  dimensions = {
+  /* dimensions = {
     InstanceId = "${each.value}"
-  }
+  } */
   tags = {
     Name = "status_and_instance_health_check"
   }
@@ -33,7 +33,7 @@ resource "aws_cloudwatch_metric_alarm" "status_and_instance_health_check" {
 # CPU Utilization Alarm
 
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
-  for_each            = toset(data.aws_instances.nomis.ids)
+  # for_each            = toset(data.aws_instances.nomis.ids)
   alarm_name          = "cpu_utilization_${each.value}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
@@ -44,9 +44,9 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
   threshold           = "80"
   alarm_description   = "This metric monitors ec2 cpu utilization"
   alarm_actions       = [aws_sns_topic.nomis_alarms.arn]
-  dimensions = {
+  /* dimensions = {
     InstanceId = "${each.value}"
-  }
+  } */
   tags = {
     Name = "cpu_utilization"
   }
