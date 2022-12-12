86d227ffaf9777c85d7a0af993777f5193cf8297
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-12-12 10:57:22 +0000

    supply latest provider version explicitly

--- terraform/environments/nomis/modules/sns_topic/versions.tf
+++ terraform/environments/nomis/modules/sns_topic/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.1.7"
   required_providers {
     aws = {
-      version = "= 4.44.0"
+      version = "= 4.46.0"
       source  = "hashicorp/aws"
     }
     random = {
--- terraform/environments/nomis/versions.tf
+++ terraform/environments/nomis/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      version = "= 4.44.0"
+      version = "= 4.46.0"
       source  = "hashicorp/aws"
     }
 
