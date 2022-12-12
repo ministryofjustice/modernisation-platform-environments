a625021c8c474c885a404df9867e5b58de7af8cf
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2022-12-12 15:15:20 +0000

    create new jumpserver

--- terraform/environments/nomis/nomis-development.tf
+++ terraform/environments/nomis/nomis-development.tf
@@ -112,18 +112,17 @@ locals {
       }
     }
     ec2_jumpservers = {
-      #      jumpserver0 = {
-      #        ami_name = "nomis_windows_server_2022_jumpserver_2022*"
-      #        tags = {
-      #          server-type = "jumpserver"
-      #          description = "Jumpserver for NOMIS"
-      #        }
-      #        ebs_volumes = {}
-      #        autoscaling_group = {
-      #          min_size = 0
-      #          max_size = 1
-      #        }
-      #      }
+      jumpserver-2022 = {
+        ami_name = "nomis_windows_server_2022_jumpserver_2022*"
+        tags = {
+          server-type = "jumpserver"
+          description = "Windows Server 2022 Jumpserver for NOMIS"
+        }
+        autoscaling_group = {
+          min_size = 0
+          max_size = 1
+        }
+      }
     }
   }
 }
