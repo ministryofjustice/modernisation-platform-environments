a78ef71136f2e83c5a9f1c226f28bebf82a22794
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-12-12 10:44:31 +0000

    fix nomis aws module versions to 4.44.0

--- terraform/environments/nomis/modules/sns_topic/versions.tf
+++ terraform/environments/nomis/modules/sns_topic/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.1.7"
   required_providers {
     aws = {
-      version = "~> 4.9"
+      version = "= 4.44.0"
       source  = "hashicorp/aws"
     }
     random = {
--- terraform/environments/nomis/versions.tf
+++ terraform/environments/nomis/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      version = "= 4.45.0"
+      version = "= 4.44.0"
       source  = "hashicorp/aws"
     }
 
