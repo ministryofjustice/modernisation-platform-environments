e045ad43719dd23536d42c91802d505248b4561d
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2022-12-13 14:53:19 +0000

    revert explicit aws plugin selection

--- terraform/environments/nomis/modules/sns_topic/versions.tf
+++ terraform/environments/nomis/modules/sns_topic/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.1.7"
   required_providers {
     aws = {
-      version = "= 4.46.0"
+      version = "~> 4.9"
       source  = "hashicorp/aws"
     }
     random = {
