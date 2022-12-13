84c45f42812dc34821955152875fb9bb41e7d01f
mikereiddigital <mike.reid@digital.justice.gov.uk>
2022-12-13 13:02:43 +0000

    formatting

--- terraform/environments/mlra/modules/alb/versions.tf
+++ terraform/environments/mlra/modules/alb/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "~> 4.0"
+      source = "hashicorp/aws"
+      version = "~> 4.0"
       # configuration_aliases = [aws.bucket-replication]
     }
   }
