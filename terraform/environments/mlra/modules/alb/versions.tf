9492a6041d0959816ca8520a798dec044acbe817
mikereiddigital <mike.reid@digital.justice.gov.uk>
2022-12-13 13:04:10 +0000

    bugs

--- terraform/environments/mlra/modules/alb/versions.tf
+++ terraform/environments/mlra/modules/alb/versions.tf
@@ -3,7 +3,6 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "~> 4.0"
-      # configuration_aliases = [aws.bucket-replication]
     }
   }
   required_version = ">= 1.0.1"
