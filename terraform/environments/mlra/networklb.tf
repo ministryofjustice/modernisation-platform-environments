5a0fce1baba2a27abcc6a80f6221f61bee0b3266
mikereiddigital <mike.reid@digital.justice.gov.uk>
2022-12-13 12:06:55 +0000

    to fix the issue with the sg not releasing, we need to delete these resources and then add them back in.

--- terraform/environments/mlra/networklb.tf
+++ terraform/environments/mlra/networklb.tf
@@ -24,21 +24,21 @@ resource "aws_lb_listener" "lz-ingress" {
   }
 }
 
-resource "aws_lb_target_group" "alb-target" {
-  name        = "alb-target"
-  target_type = "alb"
-  port        = 80
-  protocol    = "TCP"
-  vpc_id      = data.aws_vpc.shared.id
-  tags = {
-    Name = "${local.application_name}-${local.environment}-alb-target"
-  }
-}
-
-
-resource "aws_lb_target_group_attachment" "alb-target-attachment" {
-  target_group_arn = aws_lb_target_group.alb-target.arn
-  target_id        = module.alb.load_balancer.id
-}
+# resource "aws_lb_target_group" "alb-target" {
+#   name        = "alb-target"
+#   target_type = "alb"
+#   port        = 80
+#   protocol    = "TCP"
+#   vpc_id      = data.aws_vpc.shared.id
+#   tags = {
+#     Name = "${local.application_name}-${local.environment}-alb-target"
+#   }
+# }
+# 
+# 
+# resource "aws_lb_target_group_attachment" "alb-target-attachment" {
+#   target_group_arn = aws_lb_target_group.alb-target.arn
+#   target_id        = module.alb.load_balancer.id
+# }
 
 
