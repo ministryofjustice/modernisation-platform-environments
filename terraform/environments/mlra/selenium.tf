f1ceb652e7a823efd369e35eed425f22534b36ed
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2022-12-23 13:03:20 +0000

    Clear out and rebuild all selenium resources

--- terraform/environments/mlra/selenium.tf
+++ terraform/environments/mlra/selenium.tf
@@ -1,11 +1,11 @@
-module "mlra-selenium" {
-  count  = local.environment == "development" ? 1 : 0
-  source = "./modules/selenium"
-
-  app_name                  = local.application_name
-  environment               = local.environment
-  tags                      = local.tags
-  s3_lifecycle_expiration_days = 31
-  s3_lifecycle_noncurr_version_expiration_days = 31
-  application_test_url      = local.application_test_url
-}
+# module "mlra-selenium" {
+#   count  = local.environment == "development" ? 1 : 0
+#   source = "./modules/selenium"
+#
+#   app_name                  = local.application_name
+#   environment               = local.environment
+#   tags                      = local.tags
+#   s3_lifecycle_expiration_days = 31
+#   s3_lifecycle_noncurr_version_expiration_days = 31
+#   application_test_url      = local.application_test_url
+# }
