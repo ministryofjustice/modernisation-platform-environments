93cc3f8d6b963b7adb992d961c727befb609be19
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2022-12-30 16:03:03 +0000

    Fix iaps s3 cross account bucket policy

--- terraform/environments/delius-iaps/s3.tf
+++ terraform/environments/delius-iaps/s3.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "iaps_s3_policy" {
     principals {
       type = "AWS"
       identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/iaps_ec2_role",
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ec2_role_iaps_server",
         "arn:aws:iam::${local.environment_management.account_ids["core-shared-services-production"]}:root"
       ]
     }
