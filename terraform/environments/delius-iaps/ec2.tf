ff377b04ac6547a7ba968b39b594879ee6e205fd
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2022-12-30 15:30:51 +0000

    Refactored ec2 code to use ASG module

--- terraform/environments/delius-iaps/ec2.tf
+++ terraform/environments/delius-iaps/ec2.tf
@@ -1,211 +1,211 @@
-##
-# Local vars for ec2
-##
-locals {
-  ec2_tags = merge(local.tags, {
-    Name = lower(format("%s-%s", local.application_name, local.environment))
-    }
-  )
-}
+# ##
+# # Local vars for ec2
+# ##
+# locals {
+#   ec2_tags = merge(local.tags, {
+#     Name = lower(format("%s-%s", local.application_name, local.environment))
+#     }
+#   )
+# }
 
-##
-# Data
-## 
-# Can use aws ec2 describe-images --filters "Name=owner-id,Values=374269020027" --filters "Name=description,Values='Delius IAPS server'" --query 'reverse(sort_by(Images, &CreationDate))[0].[Name,ImageId]' to test
-data "aws_ami" "delius_iaps_server" {
-  most_recent = true
+# ##
+# # Data
+# ## 
+# # Can use aws ec2 describe-images --filters "Name=owner-id,Values=374269020027" --filters "Name=description,Values='Delius IAPS server'" --query 'reverse(sort_by(Images, &CreationDate))[0].[Name,ImageId]' to test
+# data "aws_ami" "delius_iaps_server" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["delius_iaps_server_*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["delius_iaps_server_*"]
+#   }
 
-  owners = [local.environment_management.account_ids["core-shared-services-production"]]
-}
+#   owners = [local.environment_management.account_ids["core-shared-services-production"]]
+# }
 
-##
-# Resources
-##
-resource "aws_key_pair" "ec2-user" {
-  key_name   = "ec2-user"
-  public_key = file(".ssh/${terraform.workspace}/ec2-user.pub")
-  tags = merge(
-    local.ec2_tags,
-    {
-      Name = "ec2-user"
-    }
-  )
-}
+# ##
+# # Resources
+# ##
+# resource "aws_key_pair" "ec2-user" {
+#   key_name   = "ec2-user"
+#   public_key = file(".ssh/${terraform.workspace}/ec2-user.pub")
+#   tags = merge(
+#     local.ec2_tags,
+#     {
+#       Name = "ec2-user"
+#     }
+#   )
+# }
 
-resource "aws_security_group" "iaps" {
-  name        = lower(format("%s-%s", local.application_name, local.environment))
-  description = "Controls access to IAPS EC2 instance"
-  vpc_id      = data.aws_vpc.shared.id
-  tags        = local.ec2_tags
-}
+# resource "aws_security_group" "iaps" {
+#   name        = lower(format("%s-%s", local.application_name, local.environment))
+#   description = "Controls access to IAPS EC2 instance"
+#   vpc_id      = data.aws_vpc.shared.id
+#   tags        = local.ec2_tags
+# }
 
-resource "aws_security_group_rule" "ingress_traffic_vpc" {
-  for_each          = local.application_data.iaps_sg_ingress_rules_vpc
-  description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
-  from_port         = each.value.from_port
-  protocol          = each.value.protocol
-  security_group_id = aws_security_group.iaps.id
-  to_port           = each.value.to_port
-  type              = "ingress"
-  cidr_blocks       = [data.aws_vpc.shared.cidr_block]
-}
+# resource "aws_security_group_rule" "ingress_traffic_vpc" {
+#   for_each          = local.application_data.iaps_sg_ingress_rules_vpc
+#   description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
+#   from_port         = each.value.from_port
+#   protocol          = each.value.protocol
+#   security_group_id = aws_security_group.iaps.id
+#   to_port           = each.value.to_port
+#   type              = "ingress"
+#   cidr_blocks       = [data.aws_vpc.shared.cidr_block]
+# }
 
-resource "aws_security_group_rule" "egress_traffic_cidr" {
-  for_each          = local.application_data.iaps_sg_egress_rules_cidr
-  description       = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
-  from_port         = each.value.from_port
-  protocol          = each.value.protocol
-  security_group_id = aws_security_group.iaps.id
-  to_port           = each.value.to_port
-  type              = "egress"
-  cidr_blocks       = [each.value.destination_cidr]
-}
+# resource "aws_security_group_rule" "egress_traffic_cidr" {
+#   for_each          = local.application_data.iaps_sg_egress_rules_cidr
+#   description       = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
+#   from_port         = each.value.from_port
+#   protocol          = each.value.protocol
+#   security_group_id = aws_security_group.iaps.id
+#   to_port           = each.value.to_port
+#   type              = "egress"
+#   cidr_blocks       = [each.value.destination_cidr]
+# }
 
-resource "aws_security_group_rule" "egress_traffic_ad" {
-  for_each                 = local.application_data.iaps_sg_egress_rules_ad
-  description              = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
-  from_port                = each.value.from_port
-  protocol                 = each.value.protocol
-  security_group_id        = aws_security_group.iaps.id
-  to_port                  = each.value.to_port
-  type                     = "egress"
-  source_security_group_id = aws_directory_service_directory.active_directory.security_group_id
-}
+# resource "aws_security_group_rule" "egress_traffic_ad" {
+#   for_each                 = local.application_data.iaps_sg_egress_rules_ad
+#   description              = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
+#   from_port                = each.value.from_port
+#   protocol                 = each.value.protocol
+#   security_group_id        = aws_security_group.iaps.id
+#   to_port                  = each.value.to_port
+#   type                     = "egress"
+#   source_security_group_id = aws_directory_service_directory.active_directory.security_group_id
+# }
 
-data "aws_iam_policy_document" "iaps_ec2_assume_role_policy" {
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
+# data "aws_iam_policy_document" "iaps_ec2_assume_role_policy" {
+#   statement {
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ec2.amazonaws.com"]
+#     }
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
 
-data "aws_iam_policy_document" "iaps_ec2_policy" {
-  statement {
-    sid       = "BucketPermissions"
-    actions   = ["s3:ListBucket"]
-    resources = ["arn:aws:s3:::${local.artefact_bucket_name}"]
-  }
+# data "aws_iam_policy_document" "iaps_ec2_policy" {
+#   statement {
+#     sid       = "BucketPermissions"
+#     actions   = ["s3:ListBucket"]
+#     resources = ["arn:aws:s3:::${local.artefact_bucket_name}"]
+#   }
 
-  statement {
-    sid       = "ObjectPermissions"
-    actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::${local.artefact_bucket_name}/*"]
-  }
+#   statement {
+#     sid       = "ObjectPermissions"
+#     actions   = ["s3:GetObject"]
+#     resources = ["arn:aws:s3:::${local.artefact_bucket_name}/*"]
+#   }
 
-  statement {
-    sid = "SecretPermissions"
-    actions = [
-      "secretsmanager:GetSecretValue"
-    ]
-    resources = [aws_secretsmanager_secret.ad_password.arn]
-  }
-}
+#   statement {
+#     sid = "SecretPermissions"
+#     actions = [
+#       "secretsmanager:GetSecretValue"
+#     ]
+#     resources = [aws_secretsmanager_secret.ad_password.arn]
+#   }
+# }
 
-resource "aws_iam_role" "iaps_ec2_role" {
-  name                = "iaps_ec2_role"
-  assume_role_policy  = data.aws_iam_policy_document.iaps_ec2_assume_role_policy.json
-  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
-  inline_policy {
-    name   = "IapsEc2Policy"
-    policy = data.aws_iam_policy_document.iaps_ec2_policy.json
-  }
-  tags = merge(
-    local.ec2_tags,
-    {
-      Name = "iaps_ec2_role"
-    },
-  )
-}
+# resource "aws_iam_role" "iaps_ec2_role" {
+#   name                = "iaps_ec2_role"
+#   assume_role_policy  = data.aws_iam_policy_document.iaps_ec2_assume_role_policy.json
+#   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+#   inline_policy {
+#     name   = "IapsEc2Policy"
+#     policy = data.aws_iam_policy_document.iaps_ec2_policy.json
+#   }
+#   tags = merge(
+#     local.ec2_tags,
+#     {
+#       Name = "iaps_ec2_role"
+#     },
+#   )
+# }
 
-resource "aws_iam_instance_profile" "iaps_ec2_profile" {
-  name = "iaps_ec2_profile"
-  role = aws_iam_role.iaps_ec2_role.name
-}
+# resource "aws_iam_instance_profile" "iaps_ec2_profile" {
+#   name = "iaps_ec2_profile"
+#   role = aws_iam_role.iaps_ec2_role.name
+# }
 
-data "template_file" "iaps_ec2_config" {
-  template = file("${path.module}/templates/iaps-EC2LaunchV2.yaml.tpl")
-  vars = {
+# data "template_file" "iaps_ec2_config" {
+#   template = file("${path.module}/templates/iaps-EC2LaunchV2.yaml.tpl")
+#   vars = {
 
-  }
-}
+#   }
+# }
 
-resource "aws_launch_template" "iaps_instance_launch_template" {
-  # Basic options
-  name                   = "iaps-launch-template"
-  image_id               = data.aws_ami.delius_iaps_server.id # "ami-0e322684a5a0074ce" # 
-  instance_type          = local.application_data.accounts[local.environment].ec2_iaps_instance_type
-  key_name               = aws_key_pair.ec2-user.key_name
-  vpc_security_group_ids = [aws_security_group.iaps.id]
-  user_data              = base64encode(data.template_file.iaps_ec2_config.rendered)
+# resource "aws_launch_template" "iaps_instance_launch_template" {
+#   # Basic options
+#   name                   = "iaps-launch-template"
+#   image_id               = data.aws_ami.delius_iaps_server.id # "ami-0e322684a5a0074ce" # 
+#   instance_type          = local.application_data.accounts[local.environment].ec2_iaps_instance_type
+#   key_name               = aws_key_pair.ec2-user.key_name
+#   vpc_security_group_ids = [aws_security_group.iaps.id]
+#   user_data              = base64encode(data.template_file.iaps_ec2_config.rendered)
 
-  iam_instance_profile {
-    name = aws_iam_instance_profile.iaps_ec2_profile.id
-  }
+#   iam_instance_profile {
+#     name = aws_iam_instance_profile.iaps_ec2_profile.id
+#   }
 
-  # Monitoring
-  monitoring {
-    enabled = true
-  }
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
+#   # Monitoring
+#   monitoring {
+#     enabled = true
+#   }
+#   metadata_options {
+#     http_endpoint = "enabled"
+#     http_tokens   = "required"
+#   }
 
-  # Storage
-  ebs_optimized = true
-  block_device_mappings {
-    device_name = "/dev/sda1"
-    ebs {
-      volume_type           = "gp3"
-      volume_size           = 50
-      encrypted             = true
-      delete_on_termination = true
-      kms_key_id            = data.aws_kms_key.ebs_shared.arn
-    }
-  }
+#   # Storage
+#   ebs_optimized = true
+#   block_device_mappings {
+#     device_name = "/dev/sda1"
+#     ebs {
+#       volume_type           = "gp3"
+#       volume_size           = 50
+#       encrypted             = true
+#       delete_on_termination = true
+#       kms_key_id            = data.aws_kms_key.ebs_shared.arn
+#     }
+#   }
 
-  block_device_mappings {
-    device_name = "/dev/xvdf"
-    no_device   = true
-  }
+#   block_device_mappings {
+#     device_name = "/dev/xvdf"
+#     no_device   = true
+#   }
 
-  # Tags
-  dynamic "tag_specifications" {
-    for_each = toset(local.application_data.launch_template_tag_resource_types)
-    content {
-      resource_type = tag_specifications.key # relevant resources in this use case (see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template#tag_specifications)
-      tags          = local.ec2_tags
-    }
-  }
-}
+#   # Tags
+#   dynamic "tag_specifications" {
+#     for_each = toset(local.application_data.launch_template_tag_resource_types)
+#     content {
+#       resource_type = tag_specifications.key # relevant resources in this use case (see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template#tag_specifications)
+#       tags          = local.ec2_tags
+#     }
+#   }
+# }
 
-resource "aws_autoscaling_group" "iaps_instance_asg" {
-  name_prefix               = "iaps-instance-"
-  min_size                  = 1
-  max_size                  = 1
-  desired_capacity          = 1
-  vpc_zone_identifier       = data.aws_subnets.private-public.ids
-  health_check_grace_period = 300
-  health_check_type         = "EC2"
-  launch_template {
-    id      = aws_launch_template.iaps_instance_launch_template.id
-    version = "$Latest"
-  }
-  dynamic "tag" {
-    for_each = local.ec2_tags
-    content {
-      key                 = tag.key
-      value               = tag.value
-      propagate_at_launch = true
-    }
+# resource "aws_autoscaling_group" "iaps_instance_asg" {
+#   name_prefix               = "iaps-instance-"
+#   min_size                  = 1
+#   max_size                  = 1
+#   desired_capacity          = 1
+#   vpc_zone_identifier       = data.aws_subnets.private-public.ids
+#   health_check_grace_period = 300
+#   health_check_type         = "EC2"
+#   launch_template {
+#     id      = aws_launch_template.iaps_instance_launch_template.id
+#     version = "$Latest"
+#   }
+#   dynamic "tag" {
+#     for_each = local.ec2_tags
+#     content {
+#       key                 = tag.key
+#       value               = tag.value
+#       propagate_at_launch = true
+#     }
 
-  }
-}
+#   }
+# }
