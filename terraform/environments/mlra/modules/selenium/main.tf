ed724a522a2a9e6e8f685688949737779122212f
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-01-04 16:15:50 +0000

    Comment out artifact section

--- terraform/environments/mlra/modules/selenium/main.tf
+++ terraform/environments/mlra/modules/selenium/main.tf
@@ -63,7 +63,7 @@ resource "aws_s3_bucket_policy" "allow_access_from_codebuild_report" {
 ###################################################################
 # KMS and S3 resources to have CodeBuild Artifacts if required
 # Taken from https://github.com/ministryofjustice/laa-aws-infrastructure/blob/5d89457e67eca00e42406724cfd8380c156060cb/management/templates/LAA-Management-Pipeline-PreReqs.template
-# If enabled make sure to add aws_s3_bucket.codebuild_artifactbucket to be accessible by the CodeBuild job in codebuild_iam_policy.json.tpl
+# If enabled make sure to add aws_s3_bucket.codebuild_artifactbucket to be accessible by the CodeBuild job in codebuild_iam_policy.json.tpl, and add in artifact section to CodeBuild resource
 ###################################################################
 
 # data "template_file" "kms_policy" {
@@ -144,10 +144,11 @@ resource "aws_codebuild_project" "selenium" {
   # encryption_key = aws_kms_key.codebuild.arn
   service_role  = aws_iam_role.codebuild_s3.arn
 
-  artifacts {
-    type = "S3"
-    location = aws_s3_bucket.codebuild_artifact.id
-  }
+  # Uncomment to use artifact
+  # artifacts {
+  #   type = "S3"
+  #   location = aws_s3_bucket.codebuild_artifact.id
+  # }
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
