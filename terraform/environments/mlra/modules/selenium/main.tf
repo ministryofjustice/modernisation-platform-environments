31c0d50366e2b9f13413e5d069347052f72629f4
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-01-04 16:18:52 +0000

    Specify no artifacts for CodeBuild job :

--- terraform/environments/mlra/modules/selenium/main.tf
+++ terraform/environments/mlra/modules/selenium/main.tf
@@ -144,7 +144,10 @@ resource "aws_codebuild_project" "selenium" {
   # encryption_key = aws_kms_key.codebuild.arn
   service_role  = aws_iam_role.codebuild_s3.arn
 
-  # Uncomment to use artifact
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+  # Comment above and uncomment below to use artifact
   # artifacts {
   #   type = "S3"
   #   location = aws_s3_bucket.codebuild_artifact.id
