540404773bc4c299be1caae7e6e8a640aabe37d0
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-01-04 13:58:52 +0000

    Rebuilding selenium resources

--- terraform/environments/mlra/modules/selenium/main.tf
+++ terraform/environments/mlra/modules/selenium/main.tf
@@ -1,6 +1,10 @@
+#############################################
+# S3 Bucket for storing Selenium Report
+#############################################
+
 resource "aws_s3_bucket" "selenium_report" {
   bucket = "laa-${var.app_name}-deployment-pipeline-pipelinereportbucket"
-
+  # force_destroy = true # Enable to recreate bucket deleting everything inside
   tags = merge(
     var.tags,
     {
@@ -35,60 +39,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "report_lifecycle" {
 }
 
 resource "aws_s3_bucket_versioning" "report_versioning" {
-  count  = var.environment == "development" ? 1 : 0
   bucket = aws_s3_bucket.selenium_report.id
   versioning_configuration {
     status = "Enabled"
   }
-}
-
-# KMS and S3 resources to allow CodeBuild connecting to S3 - taken from https://github.com/ministryofjustice/laa-aws-infrastructure/blob/5d89457e67eca00e42406724cfd8380c156060cb/management/templates/LAA-Management-Pipeline-PreReqs.template
-
-data "template_file" "kms_policy" {
-  template = "${file("${path.module}/kms_policy.json.tpl")}"
-
-  vars = {
-    account_id = var.account_id
-  }
-}
-
-resource "aws_kms_key" "codebuild" {
-  description             = "For CodeBuild to access S3 artifacts"
-  enable_key_rotation     = true
-  policy                  = data.template_file.kms_policy.rendered
-}
-
-resource "aws_kms_alias" "codebuild_alias" {
-  name          = "alias/codebuild"
-  target_key_id = aws_kms_key.codebuild.key_id
-}
-
-resource "aws_s3_bucket" "codebuild_artifact" {
-  bucket = "${var.app_name}-selenium-artifact"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "codebuild_artifact" {
-  bucket = aws_s3_bucket.codebuild_artifact.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-data "template_file" "s3_art_bucket_policy" {
-  template = "${file("${path.module}/s3_bucket_policy.json.tpl")}"
-
-  vars = {
-    account_id = var.account_id,
-    s3_artifact_name = aws_s3_bucket.codebuild_artifact.id,
-    codebuild_role_name = aws_iam_role.codebuild_s3.id
-  }
-}
-
-resource "aws_s3_bucket_policy" "fallow_access_from_codebuild_art" {
-  bucket = aws_s3_bucket.codebuild_artifact.id
-  policy = data.template_file.s3_art_bucket_policy.rendered
 }
 
 data "template_file" "s3_report_bucket_policy" {
@@ -106,8 +60,61 @@ resource "aws_s3_bucket_policy" "allow_access_from_codebuild_report" {
   policy = data.template_file.s3_report_bucket_policy.rendered
 }
 
+###################################################################
+# KMS and S3 resources to have CodeBuild Artifacts if required - taken from https://github.com/ministryofjustice/laa-aws-infrastructure/blob/5d89457e67eca00e42406724cfd8380c156060cb/management/templates/LAA-Management-Pipeline-PreReqs.template
+###################################################################
 
+# data "template_file" "kms_policy" {
+#   template = "${file("${path.module}/kms_policy.json.tpl")}"
+#
+#   vars = {
+#     account_id = var.account_id
+#   }
+# }
+#
+# resource "aws_kms_key" "codebuild" {
+#   description             = "For CodeBuild to access S3 artifacts"
+#   enable_key_rotation     = true
+#   policy                  = data.template_file.kms_policy.rendered
+# }
+#
+# resource "aws_kms_alias" "codebuild_alias" {
+#   name          = "alias/codebuild"
+#   target_key_id = aws_kms_key.codebuild.key_id
+# }
+#
+# resource "aws_s3_bucket" "codebuild_artifact" {
+#   bucket = "${var.app_name}-selenium-artifact"
+#   # force_destroy = true
+# }
+#
+# resource "aws_s3_bucket_server_side_encryption_configuration" "codebuild_artifact" {
+#   bucket = aws_s3_bucket.codebuild_artifact.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+#
+# data "template_file" "s3_art_bucket_policy" {
+#   template = "${file("${path.module}/s3_bucket_policy.json.tpl")}"
+#
+#   vars = {
+#     account_id = var.account_id,
+#     s3_artifact_name = aws_s3_bucket.codebuild_artifact.id,
+#     codebuild_role_name = aws_iam_role.codebuild_s3.id
+#   }
+# }
+#
+# resource "aws_s3_bucket_policy" "allow_access_from_codebuild_art" {
+#   bucket = aws_s3_bucket.codebuild_artifact.id
+#   policy = data.template_file.s3_art_bucket_policy.rendered
+# }
+
+######################################################
 # Selenium CodeBuild job lifting to MP directly
+######################################################
 
 resource "aws_iam_role" "codebuild_s3" {
   name = "${var.app_name}-CodeBuildRole"
@@ -132,7 +139,7 @@ resource "aws_codebuild_project" "selenium" {
   name          = "${var.app_name}-selenium-test"
   description   = "Project to test the Java application ${var.app_name}"
   build_timeout = 20
-  encryption_key = aws_kms_key.codebuild.arn
+  # encryption_key = aws_kms_key.codebuild.arn
   service_role  = aws_iam_role.codebuild_s3.arn
 
   artifacts {
