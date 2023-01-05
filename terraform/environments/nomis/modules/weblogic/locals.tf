227425270dc8e7b5fa954cab203e58cf62a1734e
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-01-05 12:23:27 +0000

    fix

--- terraform/environments/nomis/modules/weblogic/locals.tf
+++ terraform/environments/nomis/modules/weblogic/locals.tf
@@ -34,10 +34,11 @@ data "aws_route53_zone" "internal" {
 # }
 
 # temporarily using az.justice.gov.uk
-data "aws_route53_zone" "external" {
-  name         = "modernisation-platform.nomis.az.justice.gov.uk."
-  private_zone = false
-}
+# data "aws_route53_zone" "external" {
+#   name         = "modernisation-platform.nomis.az.justice.gov.uk."
+#   private_zone = false
+# }
+
 
 locals {
   # region = substr(var.availability_zone, 0, length(var.availability_zone) - 1)
