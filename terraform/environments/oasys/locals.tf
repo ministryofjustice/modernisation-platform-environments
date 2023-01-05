f947771d2790c77c315be4c23911af9823d9a1d7
wullub <william@automationlogic.com>
2023-01-05 10:43:55 +0000

    ..

--- terraform/environments/oasys/locals.tf
+++ terraform/environments/oasys/locals.tf
@@ -1,16 +1,11 @@
-# get shared subnet-set vpc object
-data "aws_vpc" "shared_vpc" {
-  # provider = aws.share-host
-  tags = {
-    Name = "${local.vpc_name}-${local.environment}"
-  }
-}
+# env independent common vars
+# env independent webserver vars
 
-data "aws_iam_session_context" "whoami" {
-  provider = aws.oidc-session
-  arn      = data.aws_caller_identity.oidc_session.arn
-}
 locals {
+
+  ###
+  ### env independent common vars
+  ###
 
   application_name = "oasys"
   business_unit    = "hmpps"
@@ -22,8 +17,6 @@ locals {
     preproduction = local.oasys_preproduction
     production    = local.oasys_production
   }
-
-
 
   account_id         = local.environment_management.account_ids[terraform.workspace]
   environment_config = local.accounts[local.environment]
@@ -87,5 +80,73 @@ locals {
 
     # AWS
     cloud_platform = "172.20.0.0/16"
+  }
+
+  ###
+  ### env independent webserver vars
+  ###
+  webserver = { 
+    # server-type and nomis-environment auto set by module
+    tags = {
+      description = "oasys webserver"
+      component   = "web"
+      server-type = "webserver"
+    }
+
+    instance = {
+      disable_api_termination      = false
+      instance_type                = "t2.large"
+      key_name                     = aws_key_pair.ec2-user.key_name
+      monitoring                   = true
+      metadata_options_http_tokens = "optional"
+      vpc_security_group_ids       = [aws_security_group.webserver.id]
+    }
+
+    user_data_cloud_init = {
+      args = {
+        lifecycle_hook_name = "ready-hook"
+      }
+      scripts = [ # it would make sense to have these templates in a common area 
+        "ansible-ec2provision.sh.tftpl",
+        "post-ec2provision.sh.tftpl"
+      ]
+      write_files = {}
+    }
+
+    ssm_parameters_prefix = "webserver/"
+    iam_resource_names_prefix = "ec2-webserver-asg"
+
+    autoscaling_group = {
+      desired_capacity = 1
+      max_size         = 2
+      min_size         = 0
+
+      health_check_grace_period = 300
+      health_check_type         = "ELB"
+      force_delete              = true
+      termination_policies      = ["OldestInstance"]
+      target_group_arns         = [] # TODO
+      vpc_zone_identifier       = data.aws_subnets.private.ids
+      wait_for_capacity_timeout = 0
+
+      # this hook is triggered by the post-ec2provision.sh
+      initial_lifecycle_hooks = {
+        "ready-hook" = {
+          default_result       = "ABANDON"
+          heartbeat_timeout    = 7200 # on a good day it takes 30 mins, but can be much longer
+          lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+        }
+      }
+      warm_pool = {
+        reuse_on_scale_in           = true
+        max_group_prepared_capacity = 1
+      }
+
+      instance_refresh = {
+        strategy               = "Rolling"
+        min_healthy_percentage = 90 # seems that instances in the warm pool are included in the % health count so this needs to be set fairly high
+        instance_warmup        = 300
+      }
+    }
   }
 }
