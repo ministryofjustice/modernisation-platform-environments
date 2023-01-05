95bb5fe4871d3701bd12a5ad9f11f169ef0eaad5
Piotr Grzeskowiak <piotr.grzeskowiak@digital.justice.gov.uk>
2023-01-05 14:11:10 +0000

    temporary artefact bucket for iaps image builds

--- terraform/environments/delius-iaps/locals.tf
+++ terraform/environments/delius-iaps/locals.tf
@@ -47,7 +47,7 @@ locals {
   is_live       = [substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" || substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction" ? "live" : "non-live"]
   provider_name = "core-vpc-${local.environment}"
 
-  artefact_bucket_name = "${local.application_name}-${local.environment}-artefacts"
+  artefact_bucket_name = "ec2-image-builder-iaps-artefacts"
 
   # environment specfic variables
   # example usage:
