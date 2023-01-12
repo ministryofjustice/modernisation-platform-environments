8efe376436834597123507da08865a1428272e3c
garygrantjustice <98090448+garygrantjustice@users.noreply.github.com>
2023-01-12 18:35:18 +0000

    Update network-infrastructure.tf
    add SG rules for iisrelay server

--- terraform/environments/xhibit-portal/network-infrastructure.tf
+++ terraform/environments/xhibit-portal/network-infrastructure.tf
@@ -670,3 +670,48 @@ resource "aws_security_group_rule" "portal-all-to-app" {
   protocol                 = "-1"
   source_security_group_id = aws_security_group.app_servers.id
 }
+
+resource "aws_security_group_rule" "iisrelay-inbound-importmachine" {
+  depends_on               = [aws_security_group.iisrelay_server]
+  security_group_id        = aws_security_group.iisrelay_server.id
+  type                     = "ingress"
+  description              = "allow all from importmachine"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.importmachine.id
+}
+
+resource "aws_security_group_rule" "iisrelay-outbound-all" {
+  depends_on        = [aws_security_group.iisrelay_server]
+  security_group_id = aws_security_group.iisrelay_server.id
+  type              = "egress"
+  description       = "allow all"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "iisrelay-inbound-app" {
+  depends_on               = [aws_security_group.iisrelay_server]
+  security_group_id        = aws_security_group.iisrelay_server.id
+  type                     = "ingress"
+  description              = "allow all"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.app_servers.id
+}
+
+resource "aws_security_group_rule" "iisrelay-inbound-bastion" {
+  depends_on               = [aws_security_group.iisrelay_server]
+  security_group_id        = aws_security_group.iisrelay_server.id
+  type                     = "ingress"
+  description              = "allow all from bastion"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = module.bastion_linux.bastion_security_group
+}
