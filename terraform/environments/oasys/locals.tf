e7d242e475176582b66290841ed89abc972bd678
W <willg258@hotmail.co.uk>
2023-01-13 10:05:13 +0000

    ..

--- terraform/environments/oasys/locals.tf
+++ terraform/environments/oasys/locals.tf
@@ -111,7 +111,7 @@ locals {
       disable_api_termination      = false
       instance_type                = "t3.large"
       key_name                     = aws_key_pair.ec2-user.key_name
-      monitored                    = false
+      monitoring                    = false
       metadata_options_http_tokens = "required"
       vpc_security_group_ids       = [aws_security_group.webserver.id] #[aws_security_group.ec2_test.id]
     }
