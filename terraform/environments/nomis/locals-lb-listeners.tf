a72a84c3b0cc1bd15e8ac4ea1d911a67c678515e
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-01-16 17:55:12 +0000

    re-add listeners

--- terraform/environments/nomis/locals-lb-listeners.tf
+++ terraform/environments/nomis/locals-lb-listeners.tf
@@ -82,63 +82,63 @@ locals {
     development = {}
 
     test = {
-#      http = local.lb_listener_defaults.nomis_web_http
-#
-#      https = merge(local.lb_listener_defaults.nomis_web_https, {
-#        target_groups = {
-#          http-7777-asg      = local.lb_http_7777_rule
-#          http-7777-instance = merge(local.lb_http_7777_rule, {
-#            attachments = [
-#              {
-#                # temporary until circular dependencies fixed
-#                target_id = local.environment == "test" ? "i-0983877a07e735688" : null
-#              }
-#            ]
-#          })
-#        }
-#
-#        rules = {
-#          http-7777-asg = {
-#            actions = [{
-#              type              = "forward"
-#              target_group_name = "http-7777-asg"
-#            }]
-#            conditions = [{
-#              host_header = {
-#                values = ["*-nomis-web.nomis.${local.vpc_name}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-#              }
-#            }]
-#          }
-#          http-7777-instance = {
-#            actions = [{
-#              type              = "forward"
-#              target_group_name = "http-7777-instance"
-#            }]
-#            conditions = [{
-#              host_header = {
-#                values = ["*-nomis-web-instance.nomis.${local.vpc_name}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-#              }
-#            }]
-#          }
-#          http-7777-weblogic-cnomt1 = {
-#            actions = [{
-#              type             = "forward"
-#              target_group_arn = local.environment == "test" ? module.weblogic["CNOMT1"].target_group_arn : null
-#            }]
-#            conditions = [{
-#              host_header = {
-#                values = ["weblogic-cnomt1.nomis.${local.vpc_name}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-#              }
-#            }]
-#          }
-#        }
-#
-#        route53_records = {
-#          "t1-nomis-web.nomis" = local.lb_listener_defaults.environment_external_dns_zone
-#          "t1-nomis-web-instance.nomis" = local.lb_listener_defaults.environment_external_dns_zone
-#          "weblogic-cnomt1.nomis" = local.lb_listener_defaults.environment_external_dns_zone
-#        }
-#      })
+      http = local.lb_listener_defaults.http
+
+      https = merge(local.lb_listener_defaults.https, {
+        target_groups = {
+          http-7777-asg = local.lb_http_7777_rule
+          http-7777-instance = merge(local.lb_http_7777_rule, {
+            attachments = [
+              {
+                # temporary until circular dependencies fixed
+                target_id = local.environment == "test" ? "i-0983877a07e735688" : null
+              }
+            ]
+          })
+        }
+
+        rules = {
+          http-7777-asg = {
+            actions = [{
+              type              = "forward"
+              target_group_name = "http-7777-asg"
+            }]
+            conditions = [{
+              host_header = {
+                values = ["*-nomis-web.nomis.${local.vpc_name}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+              }
+            }]
+          }
+          http-7777-instance = {
+            actions = [{
+              type              = "forward"
+              target_group_name = "http-7777-instance"
+            }]
+            conditions = [{
+              host_header = {
+                values = ["*-nomis-web-instance.nomis.${local.vpc_name}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+              }
+            }]
+          }
+          http-7777-weblogic-cnomt1 = {
+            actions = [{
+              type             = "forward"
+              target_group_arn = local.environment == "test" ? module.weblogic["CNOMT1"].target_group_arn : null
+            }]
+            conditions = [{
+              host_header = {
+                values = ["weblogic-cnomt1.nomis.${local.vpc_name}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+              }
+            }]
+          }
+        }
+
+        route53_records = {
+          "t1-nomis-web.nomis"          = local.lb_listener_defaults.environment_external_dns_zone
+          "t1-nomis-web-instance.nomis" = local.lb_listener_defaults.environment_external_dns_zone
+          "weblogic-cnomt1.nomis"       = local.lb_listener_defaults.environment_external_dns_zone
+        }
+      })
     }
 
     preproduction = {}
