3c810c7714505f0ea9400aa294429d269acb3b34
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-01-17 11:48:09 +0000

    policy fix

--- terraform/environments/ccms-ebs/ec2_oracle_base_second.tf
+++ terraform/environments/ccms-ebs/ec2_oracle_base_second.tf
@@ -23,8 +23,8 @@ resource "aws_instance" "ec2_oracle_base_second" {
   subnet_id                   = data.aws_subnet.private_subnets_a.id
   monitoring                  = true
   ebs_optimized               = false
-  associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.iam_instace_profile_oracle_base.name
+  associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.iam_instace_profile_oracle_base_2.name
 
   metadata_options {
     http_endpoint = "enabled"
@@ -40,4 +40,54 @@ resource "aws_instance" "ec2_oracle_base_second" {
     { Name = lower(format("ec2-%s-%s-OracleBase-second", local.application_name, local.environment)) }
   )
   depends_on = [aws_security_group.ec2_sg_oracle_base]
+}
+
+
+resource "aws_iam_role" "role_stsassume_oracle_base_2" {
+  name                 = "role_stsassume_oracle_base_2"
+  path                 = "/"
+  max_session_duration = "3600"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ec2.amazonaws.com"
+          }
+          "Action" : "sts:AssumeRole",
+          "Condition" : {}
+        }
+      ]
+    }
+  )
+  tags = merge(local.tags,
+    { Name = lower(format("RoleSsm-%s-%s-OracleBase", local.application_name, local.environment)) }
+  )
+}
+
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_base_2" {
+  role = aws_iam_role.role_stsassume_oracle_base_2.name
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    "arn:aws:iam::aws:policy/AmazonSSMFullAccess",
+    "arn:aws:iam::aws:policy/${local.application_data.accounts[local.environment].ec2_policy_3}"
+  ])
+  policy_arn = each.value
+}
+
+resource "aws_iam_instance_profile" "iam_instace_profile_oracle_base_2" {
+  name = "iam_instace_profile_oracle_base_2"
+  role = aws_iam_role.role_stsassume_oracle_base_2.name
+  path = "/"
+  tags = merge(local.tags,
+    { Name = lower(format("IamProfile-%s-%s-OracleBase", local.application_name, local.environment)) }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_logging_oracle_base_2" {
+  role       = aws_iam_role.role_stsassume_oracle_base_2.name
+  policy_arn = aws_iam_policy.oracle_ec2_ssm_policy.arn
 }
