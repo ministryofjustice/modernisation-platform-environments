f4d790b37c23e582e5aae41aaa824b52d1421ddf
W <willg258@hotmail.co.uk>
2023-01-17 11:59:54 +0000

    ..

--- terraform/modules/ec2_autoscaling_group/variables.tf
+++ terraform/modules/ec2_autoscaling_group/variables.tf
@@ -14,6 +14,12 @@ variable "region" {
   default     = "eu-west-2"
 }
 
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+  default     = null
+}
+
 variable "subnet_ids" {
   type        = list(string)
   description = "List of subnet ids given to the ASG to set the associated AZs (and therefore redundancy of the ASG instances)"
