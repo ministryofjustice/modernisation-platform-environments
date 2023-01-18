09bf88c8273394f5a4c66a4c2a074f938026e301
Ewa Stempel <ewa.stempel@digital.justice.gov.uk>
2023-01-18 19:49:41 +0000

    Correcting variables to run terraform plan

--- terraform/environments/example/ecs.tf
+++ terraform/environments/example/ecs.tf
@@ -8,9 +8,9 @@ module "ecs" {
   container_instance_type = local.application_data.accounts[local.environment].container_os_type
   ami_image_id            = local.application_data.accounts[local.environment].container_ami_image_id
   instance_type           = local.application_data.accounts[local.environment].container_instance_type
-  user_data               = base64encode(templatefile("${path.module}/templates/user_data.sh.tftpl",{}))
+  user_data               = base64encode(templatefile("${path.module}/templates/user_data.sh.tftpl", {}))
   key_name                = local.application_data.accounts[local.environment].container_key_name
-  task_definition         = data.aws_ecs_task_definition.task_definition
+  task_definition         = templatefile("${path.module}/templates/task_definition.json.tftpl", {})
   ec2_desired_capacity    = local.application_data.accounts[local.environment].ec2_desired_capacity
   ec2_max_size            = local.application_data.accounts[local.environment].ec2_max_size
   ec2_min_size            = local.application_data.accounts[local.environment].ec2_min_size
@@ -22,38 +22,10 @@ module "ecs" {
   app_count               = local.application_data.accounts[local.environment].app_count
   ec2_ingress_rules       = local.ec2_ingress_rules
   ec2_egress_rules        = local.ec2_egress_rules
-  lb_tg_name              = aws_lb.external
+  lb_tg_name              = aws_lb_target_group.target_group.name
   tags_common             = local.tags
-}
 
-data "aws_ecs_task_definition" "task_definition" {
-  task_definition = "${var.app_name}-task-definition"
-  depends_on      = [aws_ecs_task_definition.windows_ecs_task_definition, aws_ecs_task_definition.linux_ecs_task_definition]
-}
-
-resource "aws_ecs_task_definition" "linux_ecs_task_definition" {
-  family             = "${var.app_name}-task-definition"
-  network_mode       = var.network_mode
-  cpu                = var.container_cpu
-  memory             = var.container_memory
-  count              = var.container_instance_type == "linux" ? 1 : 0
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
-  requires_compatibilities = [
-    "EC2",
-  ]
-
-  volume {
-    name = var.task_definition_volume
-  }
-
-  container_definitions = var.task_definition
-
-  tags = merge(
-  var.tags_common,
-  {
-    Name = "${var.app_name}-linux-task-definition"
-  }
-  )
+  depends_on = [aws_lb_listener.external]
 }
 
 locals {
