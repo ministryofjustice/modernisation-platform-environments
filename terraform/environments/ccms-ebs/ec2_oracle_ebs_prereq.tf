34086bd8f7072336c8eeac3b9063add447f696b3
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-01-19 17:57:56 +0000

    restructure and add EBS server

--- terraform/environments/ccms-ebs/ec2_oracle_ebs_prereq.tf
+++ terraform/environments/ccms-ebs/ec2_oracle_ebs_prereq.tf
@@ -1,7 +1,7 @@
 #  Build EC2 
-resource "aws_instance" "ec2_oracle_ebs" {
+resource "aws_instance" "ec2_oracle_base_ebs" {
   instance_type               = local.application_data.accounts[local.environment].ec2_oracle_instance_type_ebs
-  ami                         = data.aws_ami.oracle_base_prereqs.id
+  ami                         = data.aws_ami.oracle_base_ready.id
   key_name                    = local.application_data.accounts[local.environment].key_name
   vpc_security_group_ids      = [aws_security_group.ec2_sg_oracle_base.id]
   subnet_id                   = data.aws_subnet.private_subnets_a.id
@@ -52,7 +52,7 @@ EOF
     )
   }
   tags = merge(local.tags,
-    { Name = lower(format("ec2-%s-%s-Oracle-EBS", local.application_name, local.environment)) }
+    { Name = lower(format("ec2-%s-%s-Oracle-EBS-base", local.application_name, local.environment)) }
   )
   depends_on = [aws_security_group.ec2_sg_oracle_base]
 }
--- terraform/environments/ccms-ebs/iam.tf
+++ terraform/environments/ccms-ebs/iam.tf
@@ -1,3 +1,4 @@
+## SSM Specific
 resource "aws_iam_policy" "oracle_ec2_ssm_policy" {
   name        = "ssm_oracle_ec2_policy-${local.environment}"
   description = "allows SSM Connect logging"
@@ -47,8 +48,44 @@ resource "aws_iam_policy" "oracle_ec2_ssm_policy" {
   )
 
 }
-
 resource "aws_iam_role_policy_attachment" "ssm_logging_oracle_base" {
   role       = aws_iam_role.role_stsassume_oracle_base.name
   policy_arn = aws_iam_policy.oracle_ec2_ssm_policy.arn
+}
+
+## Oracle EC2 Policies
+resource "aws_iam_role" "role_stsassume_oracle_base" {
+  name                 = "role_stsassume_oracle_base"
+  path                 = "/"
+  max_session_duration = "3600"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ec2.amazonaws.com"
+          }
+          "Action" : "sts:AssumeRole",
+          "Condition" : {}
+        }
+      ]
+    }
+  )
+  tags = merge(local.tags,
+    { Name = lower(format("RoleSsm-%s-%s-OracleBase", local.application_name, local.environment)) }
+  )
+}
+resource "aws_iam_role_policy_attachment" "ssm_policy_oracle_base" {
+  role       = aws_iam_role.role_stsassume_oracle_base.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+resource "aws_iam_instance_profile" "iam_instace_profile_oracle_base" {
+  name = "iam_instace_profile_oracle_base"
+  role = aws_iam_role.role_stsassume_oracle_base.name
+  path = "/"
+  tags = merge(local.tags,
+    { Name = lower(format("IamProfile-%s-%s-OracleBase", local.application_name, local.environment)) }
+  )
 }
