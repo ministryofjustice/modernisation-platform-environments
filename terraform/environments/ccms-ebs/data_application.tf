b64b15df46973eb319f1e440b7c96eaa3c2f7ef1
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-01-19 17:57:56 +0000

    restructure and add EBS server

---
+++ terraform/environments/ccms-ebs/data_application.tf
@@ -0,0 +1,54 @@
+## AMI data blocks
+data "aws_ami" "oracle_base" {
+  most_recent = true
+  owners      = ["self"]
+  filter {
+    name   = "name"
+    values = [local.application_data.accounts[local.environment].orace_base_ami_name]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+data "aws_ami" "oracle_base_marketplace" {
+  most_recent = true
+  owners      = ["131827586825"]
+
+  filter {
+    name   = "name"
+    values = [local.application_data.accounts[local.environment].orace_base_ami_name_mp]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+data "aws_ami" "oracle_base_ready" {
+  most_recent = true
+
+  #owners = ["amazon"]
+  owners = ["self"]
+  filter {
+    name   = "name"
+    values = [local.application_data.accounts[local.environment].orace_base_ami_name_ready]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+data "aws_ami" "oracle_base_prereqs" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = [local.application_data.accounts[local.environment].orace_base_prereqs_ami_name]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
