bea0796d485b04d20dc0697dbe42636be6b26179
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-01-23 11:23:27 +0000

    no kms

--- terraform/environments/ccms-ebs/ec2_oracle_ebs_prereq_no_kms.tf
+++ terraform/environments/ccms-ebs/ec2_oracle_ebs_prereq_no_kms.tf
@@ -1,10 +1,10 @@
 #  Build EC2 
-resource "aws_instance" "ec2_oracle_ebs" {
+resource "aws_instance" "ec2_oracle_base_ebs_nokms" {
   instance_type               = local.application_data.accounts[local.environment].ec2_oracle_instance_type_ebs
-  ami                         = data.aws_ami.oracle_base_prereqs.id
+  ami                         = data.aws_ami.oracle_base_ready.id
   key_name                    = local.application_data.accounts[local.environment].key_name
   vpc_security_group_ids      = [aws_security_group.ec2_sg_oracle_base.id]
-  subnet_id                   = data.aws_subnet.data_subnets_a.id
+  subnet_id                   = data.aws_subnet.private_subnets_a.id
   monitoring                  = true
   ebs_optimized               = false
   associate_public_ip_address = false
@@ -35,7 +35,7 @@ EOF
   root_block_device {
     volume_type = "gp3"
     volume_size = 50
-    encrypted   = true
+    encrypted   = false
     tags = merge(local.tags,
       { Name = "root-block" }
     )
@@ -45,14 +45,14 @@ EOF
     device_name = "/dev/sdf"
     volume_type = "gp3"
     volume_size = 200
-    encrypted   = true
+    encrypted   = false
     #    kms_key_id  = aws_kms_key.this.arn
     tags = merge(local.tags,
       { Name = "ebs-block1" }
     )
   }
   tags = merge(local.tags,
-    { Name = lower(format("ec2-%s-%s-Oracle-EBS", local.application_name, local.environment)) }
+    { Name = lower(format("ec2-%s-%s-Oracle-EBS-base", local.application_name, local.environment)) }
   )
   depends_on = [aws_security_group.ec2_sg_oracle_base]
 }
