750b7974f06da0843e945c6cbeaaf2639571e592
W <willg258@hotmail.co.uk>
2023-01-24 15:40:05 +0000

    ..

--- terraform/environments/oasys/locals.tf
+++ terraform/environments/oasys/locals.tf
@@ -102,52 +102,52 @@ locals {
 
     instance = {
       disable_api_termination      = false
-      instance_type                = "t2.large"
+      instance_type                = "t3.large"
       key_name                     = aws_key_pair.ec2-user.key_name
       monitoring                   = true
       metadata_options_http_tokens = "optional"
       vpc_security_group_ids       = [aws_security_group.webserver.id]
     }
 
-    user_data_cloud_init = {
-      args = {
-        lifecycle_hook_name = "ready-hook"
-      }
-      scripts = [ # it would make sense to have these templates in a common area 
-        "ansible-ec2provision.sh.tftpl",
-        "post-ec2provision.sh.tftpl"
-      ]
-      write_files = {}
-    }
+    # user_data_cloud_init = {
+    #   args = {
+    #     lifecycle_hook_name = "ready-hook"
+    #   }
+    #   scripts = [ # it would make sense to have these templates in a common area 
+    #     "ansible-ec2provision.sh.tftpl",
+    #     "post-ec2provision.sh.tftpl"
+    #   ]
+    #   write_files = {}
+    # }
 
-    ssm_parameters_prefix     = "webserver/"
-    iam_resource_names_prefix = "ec2-webserver-asg"
+    # ssm_parameters_prefix     = "webserver/"
+    iam_resource_names_prefix = "webserver-asg"
 
     autoscaling_group = {
       desired_capacity = 1
       max_size         = 2
       min_size         = 0
 
-      health_check_grace_period = 300
-      health_check_type         = "ELB"
-      force_delete              = true
-      termination_policies      = ["OldestInstance"]
-      target_group_arns         = [] # TODO
-      vpc_zone_identifier       = data.aws_subnets.private.ids
-      wait_for_capacity_timeout = 0
+      # health_check_grace_period = 300
+      # health_check_type         = "ELB"
+      # force_delete              = true
+      # termination_policies      = ["OldestInstance"]
+      # target_group_arns         = [] # TODO
+      # vpc_zone_identifier       = data.aws_subnets.private.ids
+      # wait_for_capacity_timeout = 0
 
       # this hook is triggered by the post-ec2provision.sh
-      initial_lifecycle_hooks = {
-        "ready-hook" = {
-          default_result       = "ABANDON"
-          heartbeat_timeout    = 7200 # on a good day it takes 30 mins, but can be much longer
-          lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-        }
-      }
-      warm_pool = {
-        reuse_on_scale_in           = true
-        max_group_prepared_capacity = 1
-      }
+      # initial_lifecycle_hooks = {
+      #   "ready-hook" = {
+      #     default_result       = "ABANDON"
+      #     heartbeat_timeout    = 7200 # on a good day it takes 30 mins, but can be much longer
+      #     lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      #   }
+      # }
+      # warm_pool = {
+      #   reuse_on_scale_in           = true
+      #   max_group_prepared_capacity = 1
+      # }
     }
   }
 }
