0e3efca67e9b7f88fe3bc3bcec096b71cb2dde6e
Seb Norris <sebastian@automationlogic.com>
2023-01-24 14:01:46 +0000

    add tfsec and checkov ignores

--- terraform/environments/delius-jitbit/ecr.tf
+++ terraform/environments/delius-jitbit/ecr.tf
@@ -1,5 +1,9 @@
 # Create an ECR to hold docker images for the jitbit app
+#tfsec:ignore:aws-ecr-repository-customer-key
+
 resource "aws_ecr_repository" "jitbit_app_ecr_repo" {
+  # checkov:skip=CKV_AWS_136
+  
   name                 = "jitbit-app"
   image_tag_mutability = "IMMUTABLE"
 
