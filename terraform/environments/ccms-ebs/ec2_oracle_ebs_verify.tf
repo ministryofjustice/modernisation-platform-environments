a5126a92e31f56a051fbfbc4fffa8ce8b98fe6b5
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-01-24 17:29:24 +0000

    add 2nd box to verify image and mounts

---
+++ terraform/environments/ccms-ebs/ec2_oracle_ebs_verify.tf
@@ -0,0 +1,158 @@
+#  Build EC2 
+resource "aws_instance" "ec2_oracle_ebs_verify" {
+  instance_type               = local.application_data.accounts[local.environment].ec2_oracle_instance_type_ebs_db_verify
+  ami                         = data.aws_ami.oracle_base_prereqs_verify.id
+  key_name                    = local.application_data.accounts[local.environment].key_name
+  vpc_security_group_ids      = [aws_security_group.ec2_sg_oracle_base.id]
+  subnet_id                   = data.aws_subnet.data_subnets_a.id
+  monitoring                  = true
+  ebs_optimized               = false
+  associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.iam_instace_profile_oracle_base.name
+
+  # Due to a bug in terraform wanting to rebuild the ec2 if more than 1 ebs block is attached, we need the lifecycle clause below
+  #lifecycle {
+  #  ignore_changes = [ebs_block_device]
+  #}
+
+  user_data = <<EOF
+#!/bin/bash
+
+exec > /tmp/userdata.log 2>&1
+sudo systemctl stop amazon-ssm-agent
+sudo rm -rf /var/lib/amazon/ssm/ipc/
+sudo systemctl start amazon-ssm-agent
+sudo mount -a
+
+EOF
+
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+  /*
+  # Increase the volume size of the root volume
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 50
+    encrypted   = true
+    tags = merge(local.tags,
+      { Name = "root-block" }
+    )
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_type = "gp3"
+    volume_size = 200
+    encrypted   = true
+    tags = merge(local.tags,
+      { Name = "ebs-block1" }
+    )
+  }
+  */
+  tags = merge(local.tags,
+    { Name = lower(format("ec2-%s-%s-Oracle-EBS-db-image-test", local.application_name, local.environment)) }
+  )
+  depends_on = [aws_security_group.ec2_sg_oracle_base]
+}
+
+resource "aws_ebs_volume" "export_home_verify" {
+  availability_zone = "eu-west-2a"
+  size              = "60"
+  type              = "io2"
+  iops              = 3000
+  encrypted         = true
+  kms_key_id        = data.aws_kms_key.ebs_shared.key_id
+  lifecycle {
+    ignore_changes = [kms_key_id]
+  }
+}
+resource "aws_volume_attachment" "export_home_att_verify" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.export_home_verify.id
+  instance_id = aws_instance.ec2_oracle_ebs_verify.id
+}
+
+resource "aws_ebs_volume" "u01_verify" {
+  availability_zone = "eu-west-2a"
+  size              = "75"
+  type              = "io2"
+  iops              = 3000
+  encrypted         = true
+  kms_key_id        = data.aws_kms_key.ebs_shared.key_id
+  lifecycle {
+    ignore_changes = [kms_key_id]
+  }
+}
+resource "aws_volume_attachment" "u01_att_verify" {
+  device_name = "/dev/sdi"
+  volume_id   = aws_ebs_volume.u01_verify.id
+  instance_id = aws_instance.ec2_oracle_ebs_verify.id
+}
+
+resource "aws_ebs_volume" "arch_verify" {
+  availability_zone = "eu-west-2a"
+  size              = "50"
+  type              = "io2"
+  iops              = 3000
+  encrypted         = true
+  kms_key_id        = data.aws_kms_key.ebs_shared.key_id
+  lifecycle {
+    ignore_changes = [kms_key_id]
+  }
+}
+resource "aws_volume_attachment" "arch_att_verify" {
+  device_name = "/dev/sdj"
+  volume_id   = aws_ebs_volume.arch_verify.id
+  instance_id = aws_instance.ec2_oracle_ebs_verify.id
+}
+resource "aws_ebs_volume" "dbf_verify" {
+  availability_zone = "eu-west-2a"
+  size              = "8000"
+  type              = "io2"
+  iops              = 3000
+  encrypted         = true
+  kms_key_id        = data.aws_kms_key.ebs_shared.key_id
+  lifecycle {
+    ignore_changes = [kms_key_id]
+  }
+}
+resource "aws_volume_attachment" "dbf_att_verify" {
+  device_name = "/dev/sdk"
+  volume_id   = aws_ebs_volume.dbf_verify.id
+  instance_id = aws_instance.ec2_oracle_ebs_verify.id
+}
+resource "aws_ebs_volume" "redoA_verify" {
+  availability_zone = "eu-west-2a"
+  size              = "100"
+  type              = "io2"
+  iops              = 3000
+  encrypted         = true
+  kms_key_id        = data.aws_kms_key.ebs_shared.key_id
+  lifecycle {
+    ignore_changes = [kms_key_id]
+  }
+}
+resource "aws_volume_attachment" "redoA_att_verify" {
+  device_name = "/dev/sdl"
+  volume_id   = aws_ebs_volume.redoA_verify.id
+  instance_id = aws_instance.ec2_oracle_ebs_verify.id
+}
+resource "aws_ebs_volume" "techst_verify" {
+  availability_zone = "eu-west-2a"
+  size              = "50"
+  type              = "io2"
+  iops              = 3000
+  encrypted         = true
+  kms_key_id        = data.aws_kms_key.ebs_shared.key_id
+  lifecycle {
+    ignore_changes = [kms_key_id]
+  }
+}
+resource "aws_volume_attachment" "techst_att_verify" {
+  device_name = "/dev/sdm"
+  volume_id   = aws_ebs_volume.techst_verify.id
+  instance_id = aws_instance.ec2_oracle_ebs_verify.id
+}
