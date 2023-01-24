e4e653318ff2478041503c9c35d36a4f9641baac
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-01-24 13:23:03 +0000

    Setting tag immutability to ON

--- terraform/environments/delius-jitbit/ecr.tf
+++ terraform/environments/delius-jitbit/ecr.tf
@@ -1,7 +1,7 @@
 # Create an ECR to hold docker images for the jitbit app
 resource "aws_ecr_repository" "jitbit_app_ecr_repo" {
   name                 = "jitbit-app"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
