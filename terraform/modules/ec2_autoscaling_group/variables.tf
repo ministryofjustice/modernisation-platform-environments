40d8f5ed1300acde4f2dbd552fba0d4e4c9e3707
W <willg258@hotmail.co.uk>
2023-01-24 18:31:42 +0000

    ..

--- terraform/modules/ec2_autoscaling_group/variables.tf
+++ terraform/modules/ec2_autoscaling_group/variables.tf
@@ -77,7 +77,7 @@ variable "instance" {
     disable_api_termination      = bool
     instance_type                = string
     key_name                     = string
-    monitoring                   = optional(bool, "enabled")
+    monitoring                   = optional(string, "enabled")
     metadata_options_http_tokens = optional(string, "required")
     metadata_endpoint_enabled    = optional(string, "enabled")
     vpc_security_group_ids       = list(string)
