de7d284ad60d82db4945ea5bcfb349dc9e7306c8
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-01-24 09:09:20 +0000

    DSOS-1647: add ip_addresses module

---
+++ terraform/modules/ip_addresses/outputs.tf
@@ -0,0 +1,39 @@
+output "moj_cidr" {
+  value       = local.moj_cidr
+  description = "MoJ Infrastructure cidrs: map(string)"
+}
+
+output "moj_cidrs" {
+  value       = local.moj_cidrs
+  description = "MoJ Infrastructure aggregate cidrs: map(list(string))"
+}
+
+output "azure_fixngo_cidr" {
+  value       = local.azure_fixngo_cidr
+  description = "Azure FixNGo cidrs: map(string)"
+}
+
+output "azure_fixngo_cidrs" {
+  value       = local.azure_fixngo_cidrs
+  description = "Azure FixNGo aggregate cidrs: map(list(string))"
+}
+
+output "azure_nomisapi_cidr" {
+  value       = local.azure_nomisapi_cidr
+  description = "Azure Nomis API AKS cidrs: map(string)"
+}
+
+output "azure_nomisapi_cidrs" {
+  value       = local.azure_nomisapi_cidrs
+  description = "Azure Nomis API AKS aggregate cidrs: map(list(string))"
+}
+
+output "azure_studio_hosting_cidr" {
+  value       = local.azure_studio_hosting_cidr
+  description = "Azure Studio Hosting AKS cidrs: map(string)"
+}
+
+output "azure_studio_hosting_cidrs" {
+  value       = local.azure_studio_hosting_cidrs
+  description = "Azure Studio Hosting AKS aggregate cidrs: map(list(string))"
+}
