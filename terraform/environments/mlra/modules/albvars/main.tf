86ec44d0e6080a23cb73e446c152e035f07d3349
mikereiddigital <mike.reid@digital.justice.gov.uk>
2023-01-25 09:59:09 +0000

    Minor fixes following rebuild test.

--- terraform/environments/mlra/modules/albvars/main.tf
+++ terraform/environments/mlra/modules/albvars/main.tf
@@ -123,7 +123,7 @@ resource "aws_lb_listener_rule" "alb_listener_rule" {
 }
 
 resource "aws_lb_target_group" "alb_target_group" {
-  name                 = "${var.application_name}-tg-${var.environment}"
+  name                 = "${var.application_name}-tg-${var.environment}-alb-tg"
   port                 = var.target_group_port
   protocol             = var.target_group_protocol
   vpc_id               = var.vpc_id
@@ -143,7 +143,7 @@ resource "aws_lb_target_group" "alb_target_group" {
   }
 
   tags = {
-    Name = "${var.application_name}-tg-${var.environment}"
+    Name = "${var.application_name}-tg-${var.environment}-tg"
   }
 
 }
--- terraform/environments/mlra/networklb.tf
+++ terraform/environments/mlra/networklb.tf
@@ -1,7 +1,7 @@
 # This creates a network load balancer listening on port 80 with a target of the internal ALB.
 
 resource "aws_lb" "ingress-network-lb" {
-  name                       = "ingress-network-lb"
+  name                       = "${local.application_name}-${local.environment}-nlb"
   internal                   = true
   load_balancer_type         = "network"
   subnets                    = [data.aws_subnet.private_subnets_a.id, data.aws_subnet.private_subnets_b.id, data.aws_subnet.private_subnets_c.id]
@@ -17,27 +17,27 @@ resource "aws_lb_listener" "lz-ingress" {
   protocol          = "TCP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.alb-target.arn
+    target_group_arn = aws_lb_target_group.nlb-target.arn
   }
   tags = {
     Name = "${local.application_name}-${local.environment}-lz-ingress"
   }
 }
 
-resource "aws_lb_target_group" "alb-target" {
-  name        = "alb-target"
+resource "aws_lb_target_group" "nlb-target" {
+  name        = "${local.application_name}-${local.environment}-nlb-tg"
   target_type = "alb"
   port        = 80
-  protocol    = "TCP"
+  protocol    = "HTTP"
   vpc_id      = data.aws_vpc.shared.id
   tags = {
-    Name = "${local.application_name}-${local.environment}-alb-target"
+    Name = "${local.application_name}-${local.environment}-nlb-tg"
   }
 }
 
 
-resource "aws_lb_target_group_attachment" "alb-target-attachment" {
-  target_group_arn = aws_lb_target_group.alb-target.arn
+resource "aws_lb_target_group_attachment" "nlb-target-attachment" {
+  target_group_arn = aws_lb_target_group.nlb-target.arn
   target_id        = module.alb.load_balancer.id
 }
 
