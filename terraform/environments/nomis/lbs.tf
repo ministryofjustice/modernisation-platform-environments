f8e396bb2d5d20a88d33adbaad9e41a2cf36f689
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-01-26 17:35:00 +0000

    added bodge again

--- terraform/environments/nomis/lbs.tf
+++ terraform/environments/nomis/lbs.tf
@@ -3,7 +3,7 @@
 module "loadbalancer" {
   for_each = merge(local.lbs.common, local.lbs[local.environment])
 
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=v2.1.2"
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=feature/temp-bodge"
   providers = {
     aws.bucket-replication = aws
   }
@@ -15,8 +15,11 @@ module "loadbalancer" {
   idle_timeout               = coalesce(lookup(each.value, "idle_timeout", null), local.lb_defaults.idle_timeout)
   internal_lb                = coalesce(lookup(each.value, "internal_lb", null), local.lb_defaults.internal_lb)
   security_groups            = coalesce(lookup(each.value, "security_groups", null), local.lb_defaults.security_groups)
-  public_subnets             = coalesce(lookup(each.value, "public_subnets", null), local.lb_defaults.public_subnets)
-  region                     = local.region
-  vpc_all                    = module.environment.vpc_name
-  tags                       = coalesce(lookup(each.value, "tags", null), local.lb_defaults.tags)
+  loadbalancer_ingress_rules = {
+    dummy = local.security_group_common.prometheus_node_exporter_ingress
+  }
+  public_subnets = coalesce(lookup(each.value, "public_subnets", null), local.lb_defaults.public_subnets)
+  region         = local.region
+  vpc_all        = module.environment.vpc_name
+  tags           = coalesce(lookup(each.value, "tags", null), local.lb_defaults.tags)
 }
