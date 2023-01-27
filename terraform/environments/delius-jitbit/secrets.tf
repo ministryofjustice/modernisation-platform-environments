5a5efb9c161f6ef8705d4ba0b55d222e64786db9
adeweetman-al <89138740+adeweetman-al@users.noreply.github.com>
2023-01-27 12:17:46 +0000

    Driving db connection string from secret and inject as env var (#1329)
    * Driving hmpps delius jitibit db connection string and app url from secret and env vars

--- terraform/environments/delius-jitbit/secrets.tf
+++ terraform/environments/delius-jitbit/secrets.tf
@@ -1,7 +1,7 @@
 ##
 # Create password for rds master user
 ##
-resource "random_password" "db_password" {
+resource "random_password" "db_admin_password" {
   length  = 30
   lower   = true
   upper   = true
@@ -9,19 +9,44 @@ resource "random_password" "db_password" {
 }
 
 #tfsec:ignore:aws-ssm-secret-use-customer-key
-resource "aws_secretsmanager_secret" "db_password" {
+resource "aws_secretsmanager_secret" "db_admin_password" {
   #checkov:skip=CKV_AWS_149
-  name                    = "${var.networking[0].application}-db-password"
+  name                    = "${var.networking[0].application}-db-admin-password"
   recovery_window_in_days = 0
   tags = merge(
     local.tags,
     {
-      Name = "${var.networking[0].application}-db-password"
+      Name = "${var.networking[0].application}-db-admin-password"
     },
   )
 }
 
-resource "aws_secretsmanager_secret_version" "db_password" {
-  secret_id     = aws_secretsmanager_secret.db_password.id
-  secret_string = random_password.db_password.result
+resource "aws_secretsmanager_secret_version" "db_admin_password" {
+  secret_id     = aws_secretsmanager_secret.db_admin_password.id
+  secret_string = random_password.db_admin_password.result
+}
+
+
+##
+# Create connection string for app
+##
+#tfsec:ignore:aws-ssm-secret-use-customer-key
+resource "aws_secretsmanager_secret" "db_app_connection_string" {
+  #checkov:skip=CKV_AWS_149
+  name                    = "${var.networking[0].application}-app-connection-string"
+  recovery_window_in_days = 0
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.networking[0].application}-app-connection-string"
+    },
+  )
+}
+
+resource "aws_secretsmanager_secret_version" "db_app_connection_string" {
+  secret_id     = aws_secretsmanager_secret.db_app_connection_string.id
+  secret_string = "user id=;data source=${aws_db_instance.jitbit.address};initial catalog=;password="
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
 }
