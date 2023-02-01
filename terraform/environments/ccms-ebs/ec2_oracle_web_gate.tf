526ae94a8b84818d6582ed4b51e6e31b0258eb27
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-02-01 16:07:03 +0000

    add grant

--- terraform/environments/ccms-ebs/ec2_oracle_web_gate.tf
+++ terraform/environments/ccms-ebs/ec2_oracle_web_gate.tf
@@ -173,11 +173,15 @@ resource "aws_iam_role" "kms_assume" {
 }
 EOF
 }
+arn:aws:iam::111122223333:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling
+*/
 
 resource "aws_kms_grant" "kms_assume" {
   name              = "kms-grant"
   key_id            = data.aws_kms_key.ebs_shared.key_id
-  grantee_principal = aws_iam_role.kms_assume.arn
+  grantee_principal = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+  #grantee_principal = aws_iam_role.kms_assume.arn
   operations        = ["Encrypt", "Decrypt", "GenerateDataKey"]
 }
-*/
+
+
