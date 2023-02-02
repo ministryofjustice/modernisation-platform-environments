44464be8e7046f147e7b290959314faac7555f0e
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-02-02 09:26:39 +0000

    Commenting out all resources to try and solve resources receration issue

--- terraform/environments/oas/cloudwatch.tf
+++ terraform/environments/oas/cloudwatch.tf
@@ -1,138 +1,138 @@
-locals {
-  appnameenv               = "${local.application_name}-${local.environment}"
-  sns_topic_name           = "${local.appnameenv}-alerting-topic"
-  dashboard_name           = "${local.appnameenv}-Appication-Dashboard"
-  pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
-  cloudwatch_metric_alarms = {
-    ec2_cpu_utilisation_too_high = {
-      alarm_name          = "${local.appnameenv}-EC2-CPU-High-Threshold-Alarm"
-      alarm_description   = "Average CPU utilization is too high"
-      comparison_operator = "GreaterThanOrEqualToThreshold"
-      evaluation_periods  = "5"
-      metric_name         = "CPUUtilization"
-      namespace           = "AWS/EC2"
-      period              = "60"
-      statistic           = "Average"
-      threshold           = "90"
-      treat_missing_data  = "breaching"
-      dimensions = {
-        InstanceId = aws_instance.oas_app_instance.id
-      }
-      widget_name = "EC2 CPU Usage"
-      # dashboard_widget_type = "metric"
-      # coord_x = 0
-      # coord_y = 0
-      # dashboard_widget_height = 5
-      # dashboard_widget_width = 8
-      # dashboard_widget_view = "timeSeries"
-      dashboard_widget_refresh_period = 60
-
-    },
-    ec2_memory_over_threshold = {
-      alarm_name          = "${local.appnameenv}--EC2-Memory-High-Threshold-Alarm"
-      alarm_description   = "Average EC2 memory usage exceeds the predefined threshold"
-      comparison_operator = "GreaterThanOrEqualToThreshold"
-      evaluation_periods  = "5"
-      metric_name         = "mem_used_percent"
-      namespace           = "CWAgent"
-      period              = "60"
-      statistic           = "Average"
-      threshold           = "90"
-      treat_missing_data  = "breaching"
-      dimensions = {
-        ImageId      = local.application_data.accounts[local.environment].ec2amiid
-        InstanceId   = aws_instance.oas_app_instance.id
-        InstanceType = "db.t3.small"
-      }
-      widget_name = "EC2 Memory Usage"
-      # dashboard_widget_type = "metric"
-      # coord_x = 0
-      # coord_y = 1
-      # dashboard_widget_height = 5
-      # dashboard_widget_width = 8
-      # dashboard_widget_view = "timeSeries"
-      dashboard_widget_refresh_period = 60
-    },
-    ebs_software_disk_space_used_over_threshold = {
-      alarm_name          = "${local.appnameenv}-EBS-DiskSpace-Alarm"
-      alarm_description   = "Software EBS volume - disk space is Low"
-      comparison_operator = "GreaterThanOrEqualToThreshold"
-      evaluation_periods  = "1"
-      metric_name         = "disk_used_percent"
-      namespace           = "CWAgent"
-      period              = "60"
-      statistic           = "Average"
-      threshold           = "90"
-      treat_missing_data  = "breaching"
-      dimensions = {
-        ImageId    = local.application_data.accounts[local.environment].ec2amiid
-        InstanceId = aws_instance.oas_app_instance.id
-        path       = "/oracle/software"
-        fstype     = "ext4"
-      }
-      widget_name = "EBS Disk Usage"
-      # dashboard_widget_type = "metric"
-      # coord_x = 1
-      # coord_y = 0
-      # dashboard_widget_height = 5
-      # dashboard_widget_width = 8
-      # dashboard_widget_view = "timeSeries"
-      dashboard_widget_refresh_period = 60
-    },
-    ebs_root_disk_space_used_over_threshold = {
-      alarm_name          = "${local.appnameenv}-EBS-Root-DiskSpace-Alarm"
-      alarm_description   = "Root EBS volume - disk space is low"
-      comparison_operator = "GreaterThanOrEqualToThreshold"
-      evaluation_periods  = "1"
-      metric_name         = "disk_used_percent"
-      namespace           = "CWAgent"
-      period              = "60"
-      statistic           = "Average"
-      threshold           = "90"
-      treat_missing_data  = "breaching"
-      dimensions = {
-        ImageId    = local.application_data.accounts[local.environment].ec2amiid
-        InstanceId = aws_instance.oas_app_instance.id
-        path       = "/"
-        fstype     = "xfs"
-      }
-      widget_name = "Root EBS Disk Usage"
-      # dashboard_widget_type = "metric"
-      # coord_x = 1
-      # coord_y = 1
-      # dashboard_widget_height = 5
-      # dashboard_widget_width = 8
-      # dashboard_widget_view = "timeSeries"
-      dashboard_widget_refresh_period = 60
-    }
-  }
-
-  dashboard_widgets = [for widget in local.cloudwatch_metric_alarms : widget if widget.widget_name != null]
-}
-
-data "aws_secretsmanager_secret" "pagerduty_integration_keys" {
-  provider = aws.modernisation-platform
-  name     = "pagerduty_integration_keys"
-}
-
-data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
-  provider  = aws.modernisation-platform
-  secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
-}
-
-module "cwalarm" {
-  source = "./modules/cloudwatch"
-  snsTopicName             = local.sns_topic_name
-  cloudwatch_metric_alarms = local.cloudwatch_metric_alarms
-  dashboard_widgets        = local.dashboard_widgets
-  dashboard_name           = local.dashboard_name
-}
-
-module "pagerduty_core_alerts" {
-  depends_on = [
-    module.cwalarm
-  ]
-  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
-  sns_topics                = [local.sns_topic_name]
-  pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_cloudwatch"]
-}
+# locals {
+#   appnameenv               = "${local.application_name}-${local.environment}"
+#   sns_topic_name           = "${local.appnameenv}-alerting-topic"
+#   dashboard_name           = "${local.appnameenv}-Appication-Dashboard"
+#   pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
+#   cloudwatch_metric_alarms = {
+#     ec2_cpu_utilisation_too_high = {
+#       alarm_name          = "${local.appnameenv}-EC2-CPU-High-Threshold-Alarm"
+#       alarm_description   = "Average CPU utilization is too high"
+#       comparison_operator = "GreaterThanOrEqualToThreshold"
+#       evaluation_periods  = "5"
+#       metric_name         = "CPUUtilization"
+#       namespace           = "AWS/EC2"
+#       period              = "60"
+#       statistic           = "Average"
+#       threshold           = "90"
+#       treat_missing_data  = "breaching"
+#       dimensions = {
+#         InstanceId = aws_instance.oas_app_instance.id
+#       }
+#       widget_name = "EC2 CPU Usage"
+#       # dashboard_widget_type = "metric"
+#       # coord_x = 0
+#       # coord_y = 0
+#       # dashboard_widget_height = 5
+#       # dashboard_widget_width = 8
+#       # dashboard_widget_view = "timeSeries"
+#       dashboard_widget_refresh_period = 60
+#
+#     },
+#     ec2_memory_over_threshold = {
+#       alarm_name          = "${local.appnameenv}--EC2-Memory-High-Threshold-Alarm"
+#       alarm_description   = "Average EC2 memory usage exceeds the predefined threshold"
+#       comparison_operator = "GreaterThanOrEqualToThreshold"
+#       evaluation_periods  = "5"
+#       metric_name         = "mem_used_percent"
+#       namespace           = "CWAgent"
+#       period              = "60"
+#       statistic           = "Average"
+#       threshold           = "90"
+#       treat_missing_data  = "breaching"
+#       dimensions = {
+#         ImageId      = local.application_data.accounts[local.environment].ec2amiid
+#         InstanceId   = aws_instance.oas_app_instance.id
+#         InstanceType = "db.t3.small"
+#       }
+#       widget_name = "EC2 Memory Usage"
+#       # dashboard_widget_type = "metric"
+#       # coord_x = 0
+#       # coord_y = 1
+#       # dashboard_widget_height = 5
+#       # dashboard_widget_width = 8
+#       # dashboard_widget_view = "timeSeries"
+#       dashboard_widget_refresh_period = 60
+#     },
+#     ebs_software_disk_space_used_over_threshold = {
+#       alarm_name          = "${local.appnameenv}-EBS-DiskSpace-Alarm"
+#       alarm_description   = "Software EBS volume - disk space is Low"
+#       comparison_operator = "GreaterThanOrEqualToThreshold"
+#       evaluation_periods  = "1"
+#       metric_name         = "disk_used_percent"
+#       namespace           = "CWAgent"
+#       period              = "60"
+#       statistic           = "Average"
+#       threshold           = "90"
+#       treat_missing_data  = "breaching"
+#       dimensions = {
+#         ImageId    = local.application_data.accounts[local.environment].ec2amiid
+#         InstanceId = aws_instance.oas_app_instance.id
+#         path       = "/oracle/software"
+#         fstype     = "ext4"
+#       }
+#       widget_name = "EBS Disk Usage"
+#       # dashboard_widget_type = "metric"
+#       # coord_x = 1
+#       # coord_y = 0
+#       # dashboard_widget_height = 5
+#       # dashboard_widget_width = 8
+#       # dashboard_widget_view = "timeSeries"
+#       dashboard_widget_refresh_period = 60
+#     },
+#     ebs_root_disk_space_used_over_threshold = {
+#       alarm_name          = "${local.appnameenv}-EBS-Root-DiskSpace-Alarm"
+#       alarm_description   = "Root EBS volume - disk space is low"
+#       comparison_operator = "GreaterThanOrEqualToThreshold"
+#       evaluation_periods  = "1"
+#       metric_name         = "disk_used_percent"
+#       namespace           = "CWAgent"
+#       period              = "60"
+#       statistic           = "Average"
+#       threshold           = "90"
+#       treat_missing_data  = "breaching"
+#       dimensions = {
+#         ImageId    = local.application_data.accounts[local.environment].ec2amiid
+#         InstanceId = aws_instance.oas_app_instance.id
+#         path       = "/"
+#         fstype     = "xfs"
+#       }
+#       widget_name = "Root EBS Disk Usage"
+#       # dashboard_widget_type = "metric"
+#       # coord_x = 1
+#       # coord_y = 1
+#       # dashboard_widget_height = 5
+#       # dashboard_widget_width = 8
+#       # dashboard_widget_view = "timeSeries"
+#       dashboard_widget_refresh_period = 60
+#     }
+#   }
+#
+#   dashboard_widgets = [for widget in local.cloudwatch_metric_alarms : widget if widget.widget_name != null]
+# }
+#
+# data "aws_secretsmanager_secret" "pagerduty_integration_keys" {
+#   provider = aws.modernisation-platform
+#   name     = "pagerduty_integration_keys"
+# }
+#
+# data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
+#   provider  = aws.modernisation-platform
+#   secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
+# }
+#
+# module "cwalarm" {
+#   source = "./modules/cloudwatch"
+#   snsTopicName             = local.sns_topic_name
+#   cloudwatch_metric_alarms = local.cloudwatch_metric_alarms
+#   dashboard_widgets        = local.dashboard_widgets
+#   dashboard_name           = local.dashboard_name
+# }
+#
+# module "pagerduty_core_alerts" {
+#   depends_on = [
+#     module.cwalarm
+#   ]
+#   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
+#   sns_topics                = [local.sns_topic_name]
+#   pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_cloudwatch"]
+# }
--- terraform/environments/oas/rds.tf
+++ terraform/environments/oas/rds.tf
@@ -1,34 +1,34 @@
-# This calls a custom RDS module to create a single RDS instance with option & parameter groups & multi-az and perf insights engabled.
-# Also includes secrets manager storage for the randomised password that is (TO BE DONE) cycled periodically.
-
-module "rds" {
-  source = "./modules/rds"
-
-  application_name            = local.application_name
-  identifier_name             = local.application_name
-  environment                 = local.environment
-  region                      = local.application_data.accounts[local.environment].region
-  allocated_storage           = local.application_data.accounts[local.environment].allocated_storage
-  engine                      = local.application_data.accounts[local.environment].engine
-  engine_version              = local.application_data.accounts[local.environment].engine_version
-  instance_class              = local.application_data.accounts[local.environment].instance_class
-  allow_major_version_upgrade = local.application_data.accounts[local.environment].allow_major_version_upgrade
-  auto_minor_version_upgrade  = local.application_data.accounts[local.environment].auto_minor_version_upgrade
-  storage_type                = local.application_data.accounts[local.environment].storage_type
-  backup_retention_period     = local.application_data.accounts[local.environment].backup_retention_period
-  backup_window               = local.application_data.accounts[local.environment].backup_window
-  maintenance_window          = local.application_data.accounts[local.environment].maintenance_window
-  character_set_name          = local.application_data.accounts[local.environment].character_set_name
-  availability_zone           = local.application_data.accounts[local.environment].availability_zone
-  multi_az                    = local.application_data.accounts[local.environment].multi_az
-  username                    = local.application_data.accounts[local.environment].username
-  db_password_rotation_period = local.application_data.accounts[local.environment].db_password_rotation_period
-  license_model               = local.application_data.accounts[local.environment].license_model
-  lz_vpc_cidr                 = local.application_data.accounts[local.environment].lz_vpc_cidr
-  deletion_protection         = local.application_data.accounts[local.environment].deletion_protection
-  vpc_shared_id               = data.aws_vpc.shared.id
-  vpc_shared_cidr             = data.aws_vpc.shared.cidr_block
-  vpc_subnet_a_id             = data.aws_subnet.data_subnets_a.id
-  vpc_subnet_b_id             = data.aws_subnet.data_subnets_b.id
-  vpc_subnet_c_id             = data.aws_subnet.data_subnets_c.id
-}
+# # This calls a custom RDS module to create a single RDS instance with option & parameter groups & multi-az and perf insights engabled.
+# # Also includes secrets manager storage for the randomised password that is (TO BE DONE) cycled periodically.
+#
+# module "rds" {
+#   source = "./modules/rds"
+#
+#   application_name            = local.application_name
+#   identifier_name             = local.application_name
+#   environment                 = local.environment
+#   region                      = local.application_data.accounts[local.environment].region
+#   allocated_storage           = local.application_data.accounts[local.environment].allocated_storage
+#   engine                      = local.application_data.accounts[local.environment].engine
+#   engine_version              = local.application_data.accounts[local.environment].engine_version
+#   instance_class              = local.application_data.accounts[local.environment].instance_class
+#   allow_major_version_upgrade = local.application_data.accounts[local.environment].allow_major_version_upgrade
+#   auto_minor_version_upgrade  = local.application_data.accounts[local.environment].auto_minor_version_upgrade
+#   storage_type                = local.application_data.accounts[local.environment].storage_type
+#   backup_retention_period     = local.application_data.accounts[local.environment].backup_retention_period
+#   backup_window               = local.application_data.accounts[local.environment].backup_window
+#   maintenance_window          = local.application_data.accounts[local.environment].maintenance_window
+#   character_set_name          = local.application_data.accounts[local.environment].character_set_name
+#   availability_zone           = local.application_data.accounts[local.environment].availability_zone
+#   multi_az                    = local.application_data.accounts[local.environment].multi_az
+#   username                    = local.application_data.accounts[local.environment].username
+#   db_password_rotation_period = local.application_data.accounts[local.environment].db_password_rotation_period
+#   license_model               = local.application_data.accounts[local.environment].license_model
+#   lz_vpc_cidr                 = local.application_data.accounts[local.environment].lz_vpc_cidr
+#   deletion_protection         = local.application_data.accounts[local.environment].deletion_protection
+#   vpc_shared_id               = data.aws_vpc.shared.id
+#   vpc_shared_cidr             = data.aws_vpc.shared.cidr_block
+#   vpc_subnet_a_id             = data.aws_subnet.data_subnets_a.id
+#   vpc_subnet_b_id             = data.aws_subnet.data_subnets_b.id
+#   vpc_subnet_c_id             = data.aws_subnet.data_subnets_c.id
+# }
