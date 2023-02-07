93739cc4522b12ace98ee203a6857b04b0f7ad5d
W <willg258@hotmail.co.uk>
2023-02-07 12:28:59 +0000

    ..

--- terraform/environments/oasys/s3.tf
+++ terraform/environments/oasys/s3.tf
@@ -1,13 +1,15 @@
-
 module "s3-bucket" {
   source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.2.0"
 
   providers = {
     aws.bucket-replication = aws
   }
-  bucket_prefix       = "s3-bucket"
-  replication_enabled = false
 
+  for_each = try(local.environment_config.s3_buckets, {})
+
+  bucket_prefix             = each.key
+  replication_enabled       = false
+  bucket_policy             = lookup(each.value, "bucket_policy", null)
   lifecycle_rule = [
     {
       id      = "main"
@@ -45,60 +47,5 @@ module "s3-bucket" {
       }
     }
   ]
-
   tags = local.tags
 }
-
-module "image-builder-bucket" {
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.2.0"
-
-  providers = {
-    aws.bucket-replication = aws
-  }
-  bucket_prefix       = "ec2-image-builder-${local.application_name}"
-  replication_enabled = false
-
-  bucket_policy = [data.aws_iam_policy_document.cross-account-s3.json]
-
-  lifecycle_rule = [
-    {
-      id      = "main"
-      enabled = "Enabled"
-      prefix  = ""
-
-      tags = {
-        rule      = "log"
-        autoclean = "Enabled"
-      }
-
-      transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-        }
-      ]
-
-      expiration = {
-        days = 730
-      }
-
-      noncurrent_version_transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
-
-      noncurrent_version_expiration = {
-        days = 730
-      }
-    }
-  ]
-
-  tags = local.tags
-
-}
-
