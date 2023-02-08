ee4e51a350a1d8a449f75dacaf0a48620758235b
umeshc-roy <umeshc.roy@gmail.com>
2023-02-08 11:12:57 +0000

    Security group correction

--- terraform/environments/ppud/security_group.tf
+++ terraform/environments/ppud/security_group.tf
@@ -488,7 +488,7 @@ resource "aws_security_group_rule" "WAM-ALB-Egress" {
   cidr_blocks       = [data.aws_vpc.shared.cidr_block]
   security_group_id = aws_security_group.WAM-ALB.id
 }
-resource "aws_security_group_rule" "WAM-ALB-Egress" {
+resource "aws_security_group_rule" "WAM-ALB-Egress-1" {
   type              = "egress"
   from_port         = 443
   to_port           = 443
