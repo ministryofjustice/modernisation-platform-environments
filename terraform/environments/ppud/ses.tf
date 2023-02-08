3698c4f2cf9114895e91ae254b56c1e197e62b9b
umeshc-roy <umeshc.roy@gmail.com>
2023-02-08 13:42:47 +0000

    correction

--- terraform/environments/ppud/ses.tf
+++ terraform/environments/ppud/ses.tf
@@ -5,7 +5,7 @@ resource "aws_ses_domain_identity" "ppud" {
 
 resource "aws_ses_domain_identity_verification" "ppud_verification" {
   count  = local.is-development == true ? 1 : 0
-  domain = aws_ses_domain_identity.ppud.id
+  domain = aws_ses_domain_identity.ppud[count.index].id
 
   timeouts {
     create = "40m"
@@ -20,14 +20,14 @@ resource "aws_ses_domain_identity" "DKIM-Identity" {
 }
 
 resource "aws_ses_domain_dkim" "Domain-DKIM" {
-# count  = local.is-development == true ? 1 : 0
-  domain = aws_ses_domain_identity.DKIM-Identity.domain
+  count  = local.is-development == true ? 1 : 0
+  domain = aws_ses_domain_identity.DKIM-Identity[count.index].domain
 }
 
 #Domain Identity MAIL FROM
 
 resource "aws_ses_domain_mail_from" "ppud" {
-# count            = local.is-development == true ? 1 : 0
-  domain           = aws_ses_domain_identity.ppud.domain
-  mail_from_domain = "bounce.${aws_ses_domain_identity.ppud.domain}"
+  count            = local.is-development == true ? 1 : 0
+  domain           = aws_ses_domain_identity.ppud[count.index].domain
+  mail_from_domain = "bounce.${aws_ses_domain_identity.ppud[count.index].domain}"
 }
--- terraform/environments/ppud/waf.tf
+++ terraform/environments/ppud/waf.tf
@@ -1,6 +1,6 @@
 
 resource "aws_wafv2_web_acl" "WAM-rule" {
-# count = local.is-development == true ? 1 : 0
+  count = local.is-development == true ? 1 : 0
   name  = "WAM-rule"
   scope = "REGIONAL"
 
@@ -222,10 +222,10 @@ resource "aws_wafv2_web_acl" "WAM-rule" {
 
 resource "aws_wafv2_web_acl_association" "ALB1-WAF" {
   resource_arn = aws_lb.PPUD-ALB.arn
-  web_acl_arn  = aws_wafv2_web_acl.WAM-rule.arn
+  web_acl_arn  = aws_wafv2_web_acl.WAM-rule[count.index].arn
 }
 
 resource "aws_wafv2_web_acl_association" "ALB2-WAF" {
   resource_arn = aws_lb.WAM-ALB.arn
-  web_acl_arn  = aws_wafv2_web_acl.WAM-rule.arn
+  web_acl_arn  = aws_wafv2_web_acl.WAM-rule[count.index].arn
 }
