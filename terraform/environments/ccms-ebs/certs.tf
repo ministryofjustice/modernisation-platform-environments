06267b8ad7472f263bb3d908b7633a973ad79522
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-02-09 15:38:15 +0000

    add r53 record for loadbalancer

--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -9,15 +9,21 @@ resource "aws_acm_certificate" "external" {
   domain_name               = "modernisation-platform.service.justice.gov.uk"
   validation_method         = "DNS"
   subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-  tags = {
-    Environment = local.environment
-  }
+
+  tags = merge(local.tags,
+    { Environment = local.environment }
+  )
 
   lifecycle {
     create_before_destroy = true
   }
 }
-
+/*
+resource "aws_acm_certificate_validation" "external" {
+  certificate_arn         = aws_acm_certificate.external.arn
+  validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
+}
+*/
 resource "aws_route53_record" "external_validation" {
   provider = aws.core-vpc
   for_each = {
@@ -34,10 +40,4 @@ resource "aws_route53_record" "external_validation" {
   ttl             = 60
   type            = each.value.type
   zone_id         = data.aws_route53_zone.external.zone_id
-}
-
-
-resource "aws_acm_certificate_validation" "external" {
-  certificate_arn         = aws_acm_certificate.external.arn
-  validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
 }
