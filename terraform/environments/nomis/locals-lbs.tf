977ae8a24fad2c3ff662514eac2a7e0f39bfc39b
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-02-09 14:13:04 +0000

    fix

--- terraform/environments/nomis/locals-lbs.tf
+++ terraform/environments/nomis/locals-lbs.tf
@@ -4,10 +4,12 @@ locals {
     enable_delete_protection = false
     idle_timeout             = "60"
     public_subnets           = module.environment.subnets["public"].ids
-    force_destroy_bucket     = true
-    internal_lb              = true
-    tags                     = local.tags
-    security_groups          = [aws_security_group.public.id]
+    # TODO: no point having in eu-west-2c
+    public_subnets       = [module.environment.subnet["public"]["eu-west-2a"].id]
+    force_destroy_bucket = true
+    internal_lb          = true
+    tags                 = local.tags
+    security_groups      = [aws_security_group.public.id]
   }
 
   lbs = {
