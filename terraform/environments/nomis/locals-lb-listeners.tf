96cb6bae66b5bd9d22eb87334e382ec331507098
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-02-09 13:36:24 +0000

    test

--- terraform/environments/nomis/locals-lb-listeners.tf
+++ terraform/environments/nomis/locals-lb-listeners.tf
@@ -101,41 +101,41 @@ locals {
     development = {}
 
     test = {
-      nomis-internal-t1-nomis-web-http-7001 = merge(
-        local.lb_listener_defaults.http-7001,
-        local.lb_listener_defaults.nomis_internal, {
-          replace = {
-            target_group_name_replace     = "t1-nomis-web-internal"
-            condition_host_header_replace = "t1-nomis-web-internal"
-          }
-        }
-      )
-      nomis-internal-t1-nomis-web-http-7777 = merge(
-        local.lb_listener_defaults.http-7777,
-        local.lb_listener_defaults.nomis_internal, {
-          replace = {
-            target_group_name_replace     = "t1-nomis-web-internal"
-            condition_host_header_replace = "t1-nomis-web-internal"
-          }
-        }
-      )
-      nomis-internal-t1-nomis-web-https = merge(
-        local.lb_listener_defaults.https,
-        local.lb_listener_defaults.nomis_internal,
-        local.lb_listener_defaults.route53, {
-          replace = {
-            target_group_name_replace     = "t1-nomis-web-internal"
-            condition_host_header_replace = "t1-nomis-web-internal"
-            route53_record_name_replace   = "t1-nomis-web-internal"
-          }
-      })
+#     nomis-internal-t1-nomis-web-http-7001 = merge(
+#       local.lb_listener_defaults.http-7001,
+#       local.lb_listener_defaults.nomis_internal, {
+#         replace = {
+#           target_group_name_replace     = "t1-nomis-web-internal"
+#           condition_host_header_replace = "t1-nomis-web-internal"
+#         }
+#       }
+#     )
+#     nomis-internal-t1-nomis-web-http-7777 = merge(
+#       local.lb_listener_defaults.http-7777,
+#       local.lb_listener_defaults.nomis_internal, {
+#         replace = {
+#           target_group_name_replace     = "t1-nomis-web-internal"
+#           condition_host_header_replace = "t1-nomis-web-internal"
+#         }
+#       }
+#     )
+#     nomis-internal-t1-nomis-web-https = merge(
+#       local.lb_listener_defaults.https,
+#       local.lb_listener_defaults.nomis_internal,
+#       local.lb_listener_defaults.route53, {
+#         replace = {
+#           target_group_name_replace     = "t1-nomis-web-internal"
+#           condition_host_header_replace = "t1-nomis-web-internal"
+#           route53_record_name_replace   = "t1-nomis-web-internal"
+#         }
+#     })
 
       nomis-public-t1-nomis-web-http-7001 = merge(
         local.lb_listener_defaults.http-7001,
         local.lb_listener_defaults.nomis_public, {
           replace = {
             target_group_name_replace     = "t1-nomis-web-public"
-            condition_host_header_replace = "t1-nomis-web-public"
+            condition_host_header_replace = "t1-nomis-web"
           }
         }
       )
@@ -144,7 +144,7 @@ locals {
         local.lb_listener_defaults.nomis_public, {
           replace = {
             target_group_name_replace     = "t1-nomis-web-public"
-            condition_host_header_replace = "t1-nomis-web-public"
+            condition_host_header_replace = "t1-nomis-web"
           }
         }
       )
@@ -154,7 +154,7 @@ locals {
         local.lb_listener_defaults.route53, {
           replace = {
             target_group_name_replace     = "t1-nomis-web-public"
-            condition_host_header_replace = "t1-nomis-web-public"
+            condition_host_header_replace = "t1-nomis-web"
             route53_record_name_replace   = "t1-nomis-web"
           }
       })
