ae92faaf62e2ffc3aaaa29f008ae47605747263a
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-02-09 15:17:41 +0000

    split out certs to new file

---
+++ terraform/environments/ccms-ebs/certs.tf
@@ -0,0 +1,44 @@
+/*
+ccms-ebs.laa-dev.modernisation-platform.service.justice.gov.uk
+ccms-ebs.laa-test.modernisation-platform.service.justice.gov.uk
+ccms-ebs.laa-preproduction.modernisation-platform.service.justice.gov.uk
+ccms-ebs.laa-production.modernisation-platform.service.justice.gov.uk
+*/
+
+resource "aws_acm_certificate" "external" {
+  domain_name       = "modernisation-platform.service.justice.gov.uk"
+  validation_method = "DNS"
+  subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  tags = {
+    Environment = local.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "external_validation" {
+  provider = aws.core-vpc
+  for_each = {
+    for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.external.zone_id
+}
+
+/*
+resource "aws_acm_certificate_validation" "external" {
+  certificate_arn         = aws_acm_certificate.external.arn
+  validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
+}
+*/
