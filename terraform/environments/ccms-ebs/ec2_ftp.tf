653429dff67aaccb4fc9e1b4b2cc2f0d5ee01203
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2023-02-14 09:46:48 +0000

    update userdata

--- terraform/environments/ccms-ebs/ec2_ftp.tf
+++ terraform/environments/ccms-ebs/ec2_ftp.tf
@@ -41,9 +41,9 @@ cat > /etc/mount_s3.sh <<- EOM
 
 B=(laa-ccms-inbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment} laa-ccms-outbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment} laa-cis-outbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment} laa-cis-inbound-development bacway-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment}-eu-west-2-${local.application_data.accounts[local.environment].lz_aws_account_id_env})
 
-C=$(aws secretsmanager get-secret-value --secret-id ftp-s3-${local.environment} --region eu-west-2)
-K=$(jq -r '.SecretString' <<< \$${C} |cut -d'"' -f2)
-S=$(jq -r '.SecretString' <<< \$${C} |cut -d'"' -f4)
+C=\$(aws secretsmanager get-secret-value --secret-id ftp-s3-${local.environment} --region eu-west-2)
+K=\$(jq -r '.SecretString' <<< \$${C} |cut -d'"' -f2)
+S=\$(jq -r '.SecretString' <<< \$${C} |cut -d'"' -f4)
 F=/etc/passwd-s3fs
 echo "\$${K}:\$${S}" > "\$${F}"
 chmod 600 \$${F}
@@ -71,7 +71,9 @@ EOM
 
 chmod +x /etc/mount_s3.sh
 
+chmod +x /etc/rc.d/rc.local
 echo "/etc/mount_s3.sh" >> /etc/rc.local
+systemctl start rc-local.service
 
 EOF
   metadata_options {
