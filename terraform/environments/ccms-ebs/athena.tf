21e61617f4400088d7f06c3f46b435afab7c5147
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-02-14 18:29:14 +0000

    add query for http GET requests

--- terraform/environments/ccms-ebs/athena.tf
+++ terraform/environments/ccms-ebs/athena.tf
@@ -36,3 +36,18 @@ resource "aws_athena_named_query" "main_table" {
     }
   )
 }
+
+resource "aws_athena_named_query" "http_requests" {
+  name      = lower(format("%s-%s-http-get-requests", local.application_name, local.environment))
+  workgroup = aws_athena_workgroup.lb-access-logs.id
+  database  = aws_athena_database.lb-access-logs.name
+  query = templatefile(
+    "./templates/lb_http_gets.sql",
+    {
+      bucket     = module.s3-bucket-logging.bucket.id
+      key        = local.lb_log_prefix
+      account_id = data.aws_caller_identity.current.id
+      region     = data.aws_region.current.id
+    }
+  )
+}
