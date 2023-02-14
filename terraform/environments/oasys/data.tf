d39ba026391f5c42537082a82dd9000cb1523b74
W <willg258@hotmail.co.uk>
2023-02-14 14:10:27 +0000

    ..

--- terraform/environments/oasys/data.tf
+++ terraform/environments/oasys/data.tf
@@ -356,8 +356,8 @@ data "aws_iam_policy_document" "user-s3-access" {
       "s3:PutObjectAcl",
       "s3:ListBucket"
     ]
-    resources = ["${module.s3-bucket["ec2-image-builder-${local.application_name}"].bucket.arn}/*",
-    module.s3-bucket["ec2-image-builder-${local.application_name}"].bucket.arn, ]
+    resources = ["${module.s3-bucket.bucket.arn}/*",
+    module.s3-bucket.bucket.arn, ]
     principals {
       type = "AWS"
       identifiers = sort([ # sort to avoid plan changes
