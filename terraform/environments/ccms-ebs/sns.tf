51f068a5eca374a38d04a9acf3748892f973f4c6
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-02-16 21:15:04 +0000

    sns and alerts

---
+++ terraform/environments/ccms-ebs/sns.tf
@@ -0,0 +1,31 @@
+resource "aws_sns_topic" "cw_alerts" {
+    name = "cw-alerts-topic"
+}
+
+resource "aws_sns_topic_policy" "sns_policy" {
+    arn = aws_sns_topic.cw_alerts.arn
+    policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+    policy_id = "SnsTopicId"
+    statement {
+      sid = "statement1"
+      principals {
+        type = "AWS"
+        identifiers = ["*"]
+      }
+      effect = "Allow"
+      actions = [
+        "SNS:GetTopicAttributes",
+        "SNS:SetTopicAttributes",
+        "SNS:AddPermission",
+        "SNS:DeleteTopic",
+        "SNS:Subscribe",
+        "SNS:ListSubscriptionsByTopic",
+        "SNS:Publish",
+        "SNS:Receive"
+      ]
+      resources = [aws_sns_topic.cw_alerts.arn]
+    }
+}
