f0bdd9de9e0f625c6346ed6c436c5cf5a9802349
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-02-21 13:10:35 +0000

    fix types

--- terraform/modules/ec2_autoscaling_group/main.tf
+++ terraform/modules/ec2_autoscaling_group/main.tf
@@ -303,11 +303,11 @@ resource "aws_cloudwatch_metric_alarm" "this" {
   comparison_operator = each.value.comparison_operator
   evaluation_periods  = each.value.evaluation_periods
   metric_name         = each.value.metric_name
-  namespace           = each.value.namespace.value
+  namespace           = each.value.namespace
   period              = each.value.period
   statistic           = each.value.statistic
   threshold           = each.value.threshold
-  alarm_actions       = [each.value.alarm_actions]
+  alarm_actions       = each.value.alarm_actions
   alarm_description   = each.value.alarm_description
   datapoints_to_alarm = each.value.datapoints_to_alarm
   treat_missing_data  = each.value.treat_missing_data
