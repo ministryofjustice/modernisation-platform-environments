5da2d78012da98174e90821a5f98f6a1dd9cfd63
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-02-24 12:44:05 +0000

    Destroy mlra-test env

--- terraform/environments/mlra/selenium.tf
+++ terraform/environments/mlra/selenium.tf
@@ -1,12 +1,12 @@
-module "mlra-selenium" {
-  count  = local.environment == "development" ? 1 : 0
-  source = "./modules/selenium"
-
-  app_name                                     = local.application_name
-  environment                                  = local.environment
-  tags                                         = local.tags
-  s3_lifecycle_expiration_days                 = 31
-  s3_lifecycle_noncurr_version_expiration_days = 31
-  application_test_url                         = local.application_test_url
-  account_id                                   = local.environment_management.account_ids[terraform.workspace]
-}
+# module "mlra-selenium" {
+#   count  = local.environment == "development" ? 1 : 0
+#   source = "./modules/selenium"
+#
+#   app_name                                     = local.application_name
+#   environment                                  = local.environment
+#   tags                                         = local.tags
+#   s3_lifecycle_expiration_days                 = 31
+#   s3_lifecycle_noncurr_version_expiration_days = 31
+#   application_test_url                         = local.application_test_url
+#   account_id                                   = local.environment_management.account_ids[terraform.workspace]
+# }
