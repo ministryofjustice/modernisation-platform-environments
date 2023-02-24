ba0b240bdf00481067a93bc6256a792c71c9c406
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-02-24 13:55:08 +0000

    Looking into data dependency for sns topic 3

--- terraform/environments/mlra/cloudwatch.tf
+++ terraform/environments/mlra/cloudwatch.tf
@@ -27,7 +27,6 @@ module "cwalarm" {
 }
 
 module "pagerduty_core_alerts" {
-  depends_on = [module.cwalarm]
   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
   sns_topics                = [module.cwalarm.sns_topic_name] # Establish dependency with module cwalarm's sns topic
   pagerduty_integration_key = local.pagerduty_integration_keys[local.pagerduty_integration_key_name]
