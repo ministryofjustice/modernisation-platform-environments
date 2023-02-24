f9740cf1b7b35ed72b6a09808f9731d03160c649
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-02-24 16:10:59 +0000

    Renamed ALB module

--- terraform/environments/mlra/modules/alb/main.tf
+++ terraform/environments/mlra/modules/alb/main.tf
@@ -1,214 +1,149 @@
-data "aws_vpc" "shared" {
+# module "lb-access-logs-enabled" {
+#   source = "../alb"
+#   providers = {
+#     aws.bucket-replication = aws.bucket-replication
+#   }
+#
+#   vpc_all                    = var.vpc_all
+#   internal_lb                = var.internal_lb
+#   application_name           = var.application_name
+#   public_subnets             = var.public_subnets
+#   private_subnets            = var.private_subnets
+#   region                     = var.region
+#   enable_deletion_protection = var.enable_deletion_protection
+#   idle_timeout               = var.idle_timeout
+#   force_destroy_bucket       = var.force_destroy_bucket
+#   tags                       = var.tags
+#   account_number             = var.account_number
+#   loadbalancer_ingress_rules = local.loadbalancer_ingress_rules
+#   loadbalancer_egress_rules  = local.loadbalancer_egress_rules
+# }
+
+locals {
+  loadbalancer_ingress_rules = {
+    "lb_ingress" = {
+      description     = "Loadbalancer ingress rule"
+      from_port       = var.security_group_ingress_from_port
+      to_port         = var.security_group_ingress_to_port
+      protocol        = var.security_group_ingress_protocol
+      cidr_blocks     = [var.ingress_cidr_block]
+      security_groups = []
+    }
+    "lb_workspace_ingress" = {
+      description     = "LB workspace ingress rule"
+      from_port       = var.security_group_ingress_from_port
+      to_port         = var.security_group_ingress_to_port
+      protocol        = var.security_group_ingress_protocol
+      cidr_blocks     = [var.lz_workspace_ingress_cidr]
+      security_groups = []
+    }
+  }
+  loadbalancer_egress_rules = {
+    "lb_egress" = {
+      description     = "Loadbalancer egress rule"
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  }
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = var.listener_port
+  #checkov:skip=CKV_AWS_2:The ALB protocol is HTTP
+  protocol = var.listener_protocol #tfsec:ignore:aws-elb-http-not-used
+
+  default_action {
+    type = "forward"
+    # during phase 1 of migration into modernisation platform, an effort
+    # is being made to retain the current application url in order to
+    # limit disruption to the application architecture itself. therefore,
+    # the current laa alb which is performing tls termination is going to
+    # forward queries on here. this also means that waf and cdn resources
+    # are retained in laa. the cdn there adds a custom header to the query,
+    # with the alb there then forwarding those permitted queries on:
+    #
+    # - Type: fixed-response
+    #   FixedResponseConfig:
+    #     ContentType: text/plain
+    #     MessageBody: Access Denied - must access via CloudFront
+    #     StatusCode: '403'
+    #
+    # in the meantime, therefore, we simply forward queries to a target
+    # group. however, in another phase of the migration, where cdn resources
+    # are carried into the modernisation platform, the above configuration
+    # may need to be applied.
+    #
+    # see: https://docs.google.com/document/d/15BUaNNx6SW2fa6QNzdMUWscWWBQ44YCiFz-e3SOwouQ
+
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "alb_listener_rule" {
+  listener_arn = aws_lb_listener.alb_listener.arn
+
+  # during phase 1 of migration into modernisation platform, an effort
+  # is being made to retain the current application url in order to
+  # limit disruption to the application architecture itself. therefore,
+  # the current laa alb which is performing tls termination is going to
+  # forward queries on here. this also means that waf and cdn resources
+  # are retained in laa. the cdn there adds a custom header to the query,
+  # with the alb there then forwarding those permitted queries on:
+  #
+  # Actions:
+  #   - Type: forward
+  #     TargetGroupArn: !Ref 'TargetGroup'
+  # Conditions:
+  #   - Field: http-header
+  #     HttpHeaderConfig:
+  #     HttpHeaderName: X-Custom-Header-LAA-MLRA
+  #     Values:
+  #       - '{{resolve:secretsmanager:cloudfront-secret-MLRA}}'
+  #
+  # in the meantime, therefore, we are simply forwarding traffic to a
+  # target group here. However, in another phase of the migration, where
+  # cdn resources are carried into modernisation platform, the above
+  # configuration is very likely going to be required.
+  #
+  # see: https://docs.google.com/document/d/15BUaNNx6SW2fa6QNzdMUWscWWBQ44YCiFz-e3SOwouQ
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name                 = "${var.application_name}-alb-tg"
+  port                 = var.target_group_port
+  protocol             = var.target_group_protocol
+  vpc_id               = var.vpc_id
+  deregistration_delay = var.target_group_deregistration_delay
+  health_check {
+    interval            = var.healthcheck_interval
+    path                = var.healthcheck_path
+    protocol            = var.healthcheck_protocol
+    timeout             = var.healthcheck_timeout
+    healthy_threshold   = var.healthcheck_healthy_threshold
+    unhealthy_threshold = var.healthcheck_unhealthy_threshold
+  }
+  stickiness {
+    enabled         = var.stickiness_enabled
+    type            = var.stickiness_type
+    cookie_duration = var.stickiness_cookie_duration
+  }
+
   tags = {
-    "Name" = var.vpc_all
-  }
-}
-
-# Terraform module which creates S3 Bucket resources for Load Balancer Access Logs on AWS.
-
-module "s3-bucket" {
-  count  = var.existing_bucket_name == "" ? 1 : 0
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.2.0"
-
-  providers = {
-    aws.bucket-replication = aws.bucket-replication
-  }
-  bucket_prefix       = "${var.application_name}-lb-access-logs"
-  bucket_policy       = [data.aws_iam_policy_document.bucket_policy.json]
-  replication_enabled = false
-  versioning_enabled  = true
-  force_destroy       = var.force_destroy_bucket
-  lifecycle_rule = [
-    {
-      id      = "main"
-      enabled = "Enabled"
-      prefix  = ""
-
-      tags = {
-        rule      = "log"
-        autoclean = "true"
-      }
-
-      transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
-
-      expiration = {
-        days = 730
-      }
-
-      noncurrent_version_transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
-
-      noncurrent_version_expiration = {
-        days = 730
-      }
-    }
-  ]
-
-  tags = var.tags
-}
-
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:PutObject"
-    ]
-    resources = [var.existing_bucket_name != "" ? "arn:aws:s3:::${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/*" : "${module.s3-bucket[0].bucket.arn}/${var.application_name}/AWSLogs/${var.account_number}/*"]
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_elb_service_account.default.arn]
-    }
-  }
-  statement {
-    sid = "AWSLogDeliveryWrite"
-
-    principals {
-      type        = "Service"
-      identifiers = ["delivery.logs.amazonaws.com"]
-    }
-
-    actions = [
-      "s3:PutObject"
-    ]
-
-    resources = [var.existing_bucket_name != "" ? "arn:aws:s3:::${var.existing_bucket_name}/${var.application_name}/AWSLogs/${var.account_number}/*" : "${module.s3-bucket[0].bucket.arn}/${var.application_name}/AWSLogs/${var.account_number}/*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-
-      values = [
-        "bucket-owner-full-control"
-      ]
-    }
+    Name = "${var.application_name}-alb-tg"
   }
 
-  statement {
-    sid = "AWSLogDeliveryAclCheck"
-
-    principals {
-      type        = "Service"
-      identifiers = ["delivery.logs.amazonaws.com"]
-    }
-
-    actions = [
-      "s3:GetBucketAcl"
-    ]
-
-    resources = [
-      var.existing_bucket_name != "" ? "arn:aws:s3:::${var.existing_bucket_name}" : module.s3-bucket[0].bucket.arn
-    ]
-  }
-}
-
-data "aws_elb_service_account" "default" {}
-
-#tfsec:ignore:aws-elb-alb-not-public
-resource "aws_lb" "loadbalancer" {
-  #checkov:skip=CKV_AWS_150:preventing destroy can be controlled outside of the module
-  #checkov:skip=CKV2_AWS_28:WAF is configured outside of the module for more flexibility
-  name                       = "${var.application_name}-application-lb"
-  internal                   = var.internal_lb
-  load_balancer_type         = "application"
-  security_groups            = [aws_security_group.lb.id]
-  subnets                    = [var.private_subnets[0], var.private_subnets[1], var.private_subnets[2]]
-  enable_deletion_protection = var.enable_deletion_protection
-  idle_timeout               = var.idle_timeout
-  drop_invalid_header_fields = true
-
-  access_logs {
-    bucket  = var.existing_bucket_name != "" ? var.existing_bucket_name : module.s3-bucket[0].bucket.id
-    prefix  = var.application_name
-    enabled = true
-  }
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.application_name}-alb"
-    },
-  )
-}
-
-resource "aws_security_group" "lb" {
-  name        = "${var.application_name}-lb-security-group"
-  description = "Controls access to the loadbalancer"
-  vpc_id      = data.aws_vpc.shared.id
-
-  dynamic "ingress" {
-    for_each = var.loadbalancer_ingress_rules
-    content {
-      description     = lookup(ingress.value, "description", null)
-      from_port       = lookup(ingress.value, "from_port", null)
-      to_port         = lookup(ingress.value, "to_port", null)
-      protocol        = lookup(ingress.value, "protocol", null)
-      cidr_blocks     = lookup(ingress.value, "cidr_blocks", null)
-      security_groups = lookup(ingress.value, "security_groups", null)
-    }
-  }
-
-  dynamic "egress" {
-    for_each = var.loadbalancer_egress_rules
-    content {
-      description     = lookup(egress.value, "description", null)
-      from_port       = lookup(egress.value, "from_port", null)
-      to_port         = lookup(egress.value, "to_port", null)
-      protocol        = lookup(egress.value, "protocol", null)
-      cidr_blocks     = lookup(egress.value, "cidr_blocks", null)
-      security_groups = lookup(egress.value, "security_groups", null)
-    }
-  }
-}
-
-
-resource "aws_athena_database" "lb-access-logs" {
-  name   = "loadbalancer_access_logs"
-  bucket = var.existing_bucket_name != "" ? var.existing_bucket_name : module.s3-bucket[0].bucket.id
-  encryption_configuration {
-    encryption_option = "SSE_S3"
-  }
-}
-
-resource "aws_athena_named_query" "main" {
-  name     = "${var.application_name}-create-table"
-  database = aws_athena_database.lb-access-logs.name
-  query = templatefile(
-    "${path.module}/templates/create_table.sql",
-    {
-      bucket     = var.existing_bucket_name != "" ? var.existing_bucket_name : module.s3-bucket[0].bucket.id
-      account_id = var.account_number
-      region     = var.region
-    }
-  )
-}
-
-resource "aws_athena_workgroup" "lb-access-logs" {
-  name = "${var.application_name}-lb-access-logs"
-
-  configuration {
-    enforce_workgroup_configuration    = true
-    publish_cloudwatch_metrics_enabled = true
-
-    result_configuration {
-      output_location = var.existing_bucket_name != "" ? "s3://${var.existing_bucket_name}/output/" : "s3://${module.s3-bucket[0].bucket.id}/output/"
-      encryption_configuration {
-        encryption_option = "SSE_S3"
-      }
-    }
-  }
 }
