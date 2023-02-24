b90f0a5296ed33ebf7ee748fadf1df0674da5585
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-02-24 10:01:13 +0000

    Looking into data changing pagerduty issue

--- terraform/environments/mlra/locals.tf
+++ terraform/environments/mlra/locals.tf
@@ -5,8 +5,11 @@ locals {
   # sns variables
   pagerduty_integration_keys     = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
   pagerduty_integration_key_name = local.application_data.accounts[local.environment].pagerduty_integration_key_name
-  sns_topic_name                 = "${local.application_name}-${local.environment}-alerting-topic"
-
+  sns_topic_name                 = "aws_sns_topic"
+  # environment specfic variables
+  # example usage:
+  # example_data = local.application_data.accounts[local.environment].example_var
+  application_data     = fileexists("./application_variables.json") ? jsondecode(file("./application_variables.json")) : {}
   application_test_url = "https://mlra.dev.legalservices.gov.uk"
 
 }
