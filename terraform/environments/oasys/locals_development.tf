1a12ee2a1ba65cb11edefbccd0ad71f9cdc03bbd
W <willg258@hotmail.co.uk>
2023-02-24 11:45:50 +0000

    DSOS-1697/onr-db
    DSOS-1697/onr-db

--- terraform/environments/oasys/locals_development.tf
+++ terraform/environments/oasys/locals_development.tf
@@ -24,12 +24,12 @@ locals {
     }
 
     autoscaling_groups = {
-      webservers = merge(local.webserver, { # merge common config and env specific
-        tags = merge(local.webserver_tags, {
-          oasys-environment = "t1"
-        })
-      })
-      # test = {
+      # webservers = merge(local.webserver, { # merge common config and env specific
+      #   tags = merge(local.webserver_tags, {
+      #     oasys-environment = "t1"
+      #   })
+      # })
+      # test = { # minimum config
       #   ami_name              = "base_rhel_7_9_*"
       #   autoscaling_schedules = {}
       #   instance = {
@@ -85,6 +85,30 @@ locals {
         # ami_owner = "self" # remove this line next time AMI is updated so core-shared-services-production used instead
         instance = {
           instance_type             = "r6i.2xlarge"
+          disable_api_termination   = true
+          metadata_endpoint_enabled = "enabled"
+        }
+        ebs_volumes = {
+          "/dev/sdb" = { size = 100 }
+          "/dev/sdc" = { size = 5120 }
+        }
+        ebs_volume_config = {
+          data  = { total_size = 4000 }
+          flash = { total_size = 1000 }
+        }
+      }
+
+      dev-onr-db-1 = {
+        tags = {
+          oasys-environment = "development"
+          server-type       = "onr-db"
+          description       = "Development ONR database"
+          oracle-sids       = "onrbods ONRAUD ONRSYS MISTRANS OASYSREP"
+          monitored         = true
+        }
+        ami_name = "onr_oracle_db_*"
+        instance = {
+          instance_type             = "r6i.xlarge"
           disable_api_termination   = true
           metadata_endpoint_enabled = "enabled"
         }
