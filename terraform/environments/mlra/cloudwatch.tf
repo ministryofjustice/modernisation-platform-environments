ae50e5a61081fa63e70bfa6bb72791c77de2827b
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-02-24 10:09:17 +0000

    Looking into data changing pagerduty issue 3

--- terraform/environments/mlra/cloudwatch.tf
+++ terraform/environments/mlra/cloudwatch.tf
@@ -21,9 +21,9 @@ module "cwalarm" {
 }
 
 module "pagerduty_core_alerts" {
-  depends_on = [
-    module.cwalarm
-  ]
+  # depends_on = [
+  #   module.cwalarm
+  # ]
   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
   sns_topics                = [local.sns_topic_name]
   pagerduty_integration_key = local.pagerduty_integration_keys[local.pagerduty_integration_key_name]
