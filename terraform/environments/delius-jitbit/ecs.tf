88dccd74a617b05e35951266ace281be87328a16
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-02-25 20:44:19 +0000

    use new ecs module

--- terraform/environments/delius-jitbit/ecs.tf
+++ terraform/environments/delius-jitbit/ecs.tf
@@ -62,19 +62,10 @@ data "aws_ami" "ecs_ami" {
 }
 
 module "ecs-new" {
-  source        = "github.com/ministryofjustice/terraform-ecs//ecs-ec2?ref=d0d070a"
-  egress_rules  = local.ec2_egress_rules
-  ingress_rules = local.ec2_ingress_rules
-  name          = format("%s-new", local.application_name)
-  subnet_ids    = data.aws_subnets.private-public.ids
-  user_data = templatefile("templates/user-data.txt", {
-    CLUSTER_NAME = format("%s-new", local.application_name)
-  })
-  vpc_id           = data.aws_vpc.shared.id
-  min_size         = local.app_data.accounts[local.environment].ec2_min_size
-  max_size         = local.app_data.accounts[local.environment].ec2_max_size
-  desired_capacity = local.app_data.accounts[local.environment].ec2_desired_capacity
-  instance_type    = local.app_data.accounts[local.environment].instance_type
-  key_name         = local.app_data.accounts[local.environment].key_name
-  tags_common      = local.tags
+  source        = "github.com/ministryofjustice/terraform-ecs//cluster?ref=4f18199b40db858581c0e21af018e1cf8575d0f3"
+
+  environment = local.environment
+  name        = format("%s-new", local.application_name)
+
+  tags = local.tags
 }
