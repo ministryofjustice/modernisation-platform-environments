5bc3039f50e9067490e021aaa481e5abfca764d8
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-02-25 20:48:43 +0000

    add versions back

--- terraform/environments/delius-jitbit/versions.tf
+++ terraform/environments/delius-jitbit/versions.tf
@@ -12,6 +12,10 @@ terraform {
       version = "3.4.3"
       source  = "hashicorp/random"
     }
+    template = {
+      version = "2.2.0"
+      source  = "hashicorp/template"
+    }
   }
   required_version = "~> 1.0"
 }
