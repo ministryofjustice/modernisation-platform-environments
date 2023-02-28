ad5f20e9503430337deb5c63c6d20cec503b3948
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-02-28 11:04:15 +0000

    baseline terraform code

---
+++ terraform/environments/nomis-combined-reporting/baseline.tf
@@ -0,0 +1,25 @@
+module "baseline" {
+  # source = "../../modules/baseline"
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-environments//terraform/modules/baseline?ref=nomis/DSOS-1759/baseline-module"
+
+  providers = {
+    aws                       = aws
+    aws.core-network-services = aws.core-network-services
+    aws.core-vpc              = aws.core-vpc
+  }
+
+  environment = module.environment
+
+  security_groups       = local.baseline_security_groups
+  acm_certificates      = module.baseline_presets.acm_certificates
+  cloudwatch_log_groups = module.baseline_presets.cloudwatch_log_groups
+  iam_policies          = module.baseline_presets.iam_policies
+  iam_roles             = module.baseline_presets.iam_roles
+  key_pairs             = module.baseline_presets.key_pairs
+  kms_grants            = module.baseline_presets.kms_grants
+  s3_buckets            = local.baseline_s3_buckets
+
+  ec2_instances          = lookup(local.environment_config, "baseline_ec2_instances", {})
+  ec2_autoscaling_groups = lookup(local.environment_config, "baseline_ec2_autoscaling_groups", {})
+  lbs                    = lookup(local.environment_config, "baseline_lbs", {})
+}
---
+++ terraform/environments/nomis-combined-reporting/baseline_presets.tf
@@ -0,0 +1,14 @@
+module "baseline_presets" {
+  # source = "../../modules/baseline_presets"
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-environments//terraform/modules/baseline_presets?ref=nomis/DSOS-1759/baseline-module"
+
+  environment = module.environment
+
+  options = {
+    enable_application_environment_wildcard_cert = true
+    enable_business_unit_kms_cmks                = true
+    enable_image_builder                         = true
+    enable_ec2_cloud_watch_agent                 = true
+    s3_iam_policies                              = ["EC2S3BucketWriteAndDeleteAccessPolicy"]
+  }
+}
