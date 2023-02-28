7ecc6eeed82becc54e58954dec2a840b88d40b80
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-28 11:01:44 +0000

    add s3 for app and remove deprecated template provider

--- terraform/environments/delius-jitbit/ecs.tf
+++ terraform/environments/delius-jitbit/ecs.tf
@@ -69,3 +69,46 @@ module "ecs-new" {
 
   tags = local.tags
 }
+
+#Create s3 bucket for deployment state
+module "s3_bucket_app_deployment" {
+  count = local.environment == "development" ? 1 : 0
+
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.2.0"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  bucket_prefix      = "${local.application_name}-${local.environment}-app-deployment-"
+  versioning_enabled = true
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
