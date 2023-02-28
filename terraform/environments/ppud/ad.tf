65c6cce06a3f034892420eb57ac2f49f37a4acac
umeshc-roy <umeshc.roy@gmail.com>
2023-02-28 10:44:27 +0000

    ppud-preprod-resource

--- terraform/environments/ppud/ad.tf
+++ terraform/environments/ppud/ad.tf
@@ -1,3 +1,4 @@
+/*
 locals {
   ad_creds = jsondecode(
     data.aws_secretsmanager_secret_version.creds.secret_string
@@ -16,17 +17,30 @@ output "Password" {
   value       = local.ad_creds.password
   sensitive   = true
 }
+*/
+
+##
+# Create AWS Managed AD
+##
 
 resource "aws_directory_service_directory" "UKGOV" {
-  count    = local.is-development == true ? 1 : 0
-  name     = "UKGOV.DEV"
-  password = local.ad_creds.password
+  # count    = local.is-development == true ? 1 : 0
+  # name     = "UKGOV.DEV"
+  # identifier  = local.application_name
+  name     = local.application_data.accounts[local.environment].directory_service_name
+  password = aws_secretsmanager_secret_version.sversion.secret_string
   edition  = "Standard"
   type     = "MicrosoftAD"
 
   vpc_settings {
     vpc_id     = data.aws_vpc.shared.id
     subnet_ids = [data.aws_subnet.public_subnets_a.id, data.aws_subnet.public_subnets_b.id]
+  }
+
+  lifecycle {
+    ignore_changes = [
+      password
+    ]
   }
 
   tags = {
@@ -47,40 +61,40 @@ data "aws_iam_policy_document" "ec2_assume_role" {
 }
 # Create EC2 IAM Role
 resource "aws_iam_role" "ec2_iam_role" {
-  count              = local.is-development == true ? 1 : 0
+  # count              = local.is-development == true ? 1 : 0
   name               = "ec2-iam-role"
   path               = "/"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 # Create EC2 IAM Instance Profile
 resource "aws_iam_instance_profile" "ec2_profile" {
-  count = local.is-development == true ? 1 : 0
-  name  = "ec2-profile"
-  role  = aws_iam_role.ec2_iam_role[0].name
+  # count = local.is-development == true ? 1 : 0
+  name = "ec2-profile"
+  role = aws_iam_role.ec2_iam_role.name
 }
 
 # Attach Policies to Instance Role
 resource "aws_iam_policy_attachment" "ec2_attach1" {
-  count      = local.is-development == true ? 1 : 0
+  # count      = local.is-development == true ? 1 : 0
   name       = "ec2-iam-attachment"
-  roles      = [aws_iam_role.ec2_iam_role[0].id]
+  roles      = [aws_iam_role.ec2_iam_role.id]
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 resource "aws_iam_policy_attachment" "ec2_attach2" {
-  count      = local.is-development == true ? 1 : 0
+  # count      = local.is-development == true ? 1 : 0
   name       = "ec2-iam-attachment"
-  roles      = [aws_iam_role.ec2_iam_role[0].id]
+  roles      = [aws_iam_role.ec2_iam_role.id]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
 
 # Connect to AWS Directory Service
 data "aws_directory_service_directory" "ad" {
-  directory_id = aws_directory_service_directory.UKGOV[0].id
+  directory_id = aws_directory_service_directory.UKGOV.id
 }
 
 # AD Join 
 resource "aws_ssm_document" "api_ad_join_domain" {
-  count         = local.is-development == true ? 1 : 0
+  # count         = local.is-development == true ? 1 : 0
   name          = "ad-join-domain"
   document_type = "Command"
   content = jsonencode(
@@ -103,12 +117,29 @@ resource "aws_ssm_document" "api_ad_join_domain" {
 }
 
 # Associate Policy to Instance
-resource "aws_ssm_association" "ad_join_domain_association" {
-  count      = local.is-development == true ? 1 : 0
+resource "aws_ssm_association" "ad_join_domain_association_dev" {
+  # count      = local.is-development == true ? 1 : 0
   depends_on = [aws_instance.s609693lo6vw109, aws_instance.s609693lo6vw105, aws_instance.s609693lo6vw104, aws_instance.s609693lo6vw100, aws_instance.s609693lo6vw101, aws_instance.s609693lo6vw103, aws_instance.s609693lo6vw106, aws_instance.s609693lo6vw107, aws_instance.PPUDWEBSERVER2, aws_instance.s609693lo6vw102, aws_instance.s609693lo6vw108, aws_instance.PPUD-DEV-AWS-AD]
-  name       = aws_ssm_document.api_ad_join_domain[0].name
+  # depends_on = var.instance_ids_ad_name[terraform.workspace][count.index]
+  # name       = aws_ssm_document.api_ad_join_domain[0].name
+  name = aws_ssm_document.api_ad_join_domain.name
   targets {
-    key    = "InstanceIds"
-    values = [aws_instance.s609693lo6vw109[0].id, aws_instance.s609693lo6vw105[0].id, aws_instance.s609693lo6vw104[0].id, aws_instance.s609693lo6vw100[0].id, aws_instance.s609693lo6vw101[0].id, aws_instance.s609693lo6vw103[0].id, aws_instance.s609693lo6vw106[0].id, aws_instance.s609693lo6vw107[0].id, aws_instance.PPUDWEBSERVER2[0].id, aws_instance.s609693lo6vw102[0].id, aws_instance.s609693lo6vw108[0].id, aws_instance.PPUD-DEV-AWS-AD[0].id]
+    key = "InstanceIds"
+    #  values = [aws_instance.s609693lo6vw109[0].id, aws_instance.s609693lo6vw105[0].id, aws_instance.s609693lo6vw104[0].id, aws_instance.s609693lo6vw100[0].id, aws_instance.s609693lo6vw101[0].id, aws_instance.s609693lo6vw103[0].id, aws_instance.s609693lo6vw106[0].id, aws_instance.s609693lo6vw107[0].id, aws_instance.PPUDWEBSERVER2[0].id, aws_instance.s609693lo6vw102[0].id, aws_instance.s609693lo6vw108[0].id, aws_instance.PPUD-DEV-AWS-AD[0].id]
+    values = var.instance_ids_ad_ids[terraform.workspace][count.index]
+  }
+}
+
+# Associate Policy to Instance
+resource "aws_ssm_association" "ad_join_domain_association_preprod" {
+  # count      = local.is-development == true ? 1 : 0
+  depends_on = [aws_instance.s618358rgvw201, aws_instance.S618358RGVW202, aws_instance.s618358rgsw025, aws_instance.s618358rgvw024, aws_instance.s618358rgvw023]
+  # depends_on = var.instance_ids_ad_name[terraform.workspace][count.index]
+  # name       = aws_ssm_document.api_ad_join_domain[0].name
+  name = aws_ssm_document.api_ad_join_domain.name
+  targets {
+    key = "InstanceIds"
+    #  values = [aws_instance.s609693lo6vw109[0].id, aws_instance.s609693lo6vw105[0].id, aws_instance.s609693lo6vw104[0].id, aws_instance.s609693lo6vw100[0].id, aws_instance.s609693lo6vw101[0].id, aws_instance.s609693lo6vw103[0].id, aws_instance.s609693lo6vw106[0].id, aws_instance.s609693lo6vw107[0].id, aws_instance.PPUDWEBSERVER2[0].id, aws_instance.s609693lo6vw102[0].id, aws_instance.s609693lo6vw108[0].id, aws_instance.PPUD-DEV-AWS-AD[0].id]
+    values = var.instance_ids_ad_ids[terraform.workspace][count.index]
   }
 }
