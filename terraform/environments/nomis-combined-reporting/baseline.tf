9c6c63edb98b9b090e0a5a3073de6513f3026536
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-02-28 11:54:39 +0000

    add policies

--- terraform/environments/nomis-combined-reporting/baseline.tf
+++ terraform/environments/nomis-combined-reporting/baseline.tf
@@ -13,7 +13,7 @@ module "baseline" {
   security_groups       = local.baseline_security_groups
   #acm_certificates      = module.baseline_presets.acm_certificates
   cloudwatch_log_groups = module.baseline_presets.cloudwatch_log_groups
-  #iam_policies          = module.baseline_presets.iam_policies
+  iam_policies          = module.baseline_presets.iam_policies
   #iam_roles             = module.baseline_presets.iam_roles
   #key_pairs             = module.baseline_presets.key_pairs
   #kms_grants            = module.baseline_presets.kms_grants
