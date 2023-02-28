49ddbf9d4d049470fd880ae99e746e390e749114
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-02-28 16:18:12 +0000

    rebuild with new AMIs

--- terraform/environments/ccms-ebs/ec2_ftp.tf
+++ terraform/environments/ccms-ebs/ec2_ftp.tf
@@ -104,7 +104,9 @@ EOF
   }
 
   tags = merge(local.tags,
-    { Name = lower(format("ec2-%s-%s-FTP", local.application_name, local.environment)) }
+    { Name = lower(format("ec2-%s-%s-FTP", local.application_name, local.environment)) },
+    { instance-scheduling = "skip-scheduling" },
+    { backup = "true" }
   )
 
   depends_on = [aws_security_group.ec2_sg_ftp]
