fa88f05c9ab26a5e9994673bb32daa09ea8416bc
umeshc-roy <umeshc.roy@gmail.com>
2023-02-28 16:50:32 +0000

    uattrainppud-9

--- terraform/environments/ppud/base_variables.tf
+++ terraform/environments/ppud/base_variables.tf
@@ -5,8 +5,8 @@ variable "networking" {
 variable "instance_ids_wam_alb" {
   type = map(list(string))
   default = {
-    "development" = ["i-0250650f9a020e097"]
-    "preprod" = ["i-0250650f9a020e097"]
+     development = ["i-0250650f9a020e097"]
+     preprod = ["i-0250650f9a020e097"]
     #  prod = ["i-0123456789abcdef", "i-abcdef0123456789"]
   }
 }
@@ -14,7 +14,7 @@ variable "instance_ids_wam_alb" {
 variable "instance_ids_ppud_internal_alb" {
   type = map(list(string))
   default = {
-     "preprod" = ["i-0250650f9a020e097"]
+     preprod = ["i-0250650f9a020e097"]
   #  prod = ["i-0123456789abcdef", "i-abcdef0123456789"]
   }
 }
