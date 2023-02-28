5186df98427f4b8c9c8b6bbee856dc474112dd8a
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-02-28 11:06:18 +0000

    apply one by one

--- terraform/environments/nomis-combined-reporting/baseline.tf
+++ terraform/environments/nomis-combined-reporting/baseline.tf
@@ -11,15 +11,15 @@ module "baseline" {
   environment = module.environment
 
   security_groups       = local.baseline_security_groups
-  acm_certificates      = module.baseline_presets.acm_certificates
-  cloudwatch_log_groups = module.baseline_presets.cloudwatch_log_groups
-  iam_policies          = module.baseline_presets.iam_policies
-  iam_roles             = module.baseline_presets.iam_roles
-  key_pairs             = module.baseline_presets.key_pairs
-  kms_grants            = module.baseline_presets.kms_grants
-  s3_buckets            = local.baseline_s3_buckets
+  #acm_certificates      = module.baseline_presets.acm_certificates
+  #cloudwatch_log_groups = module.baseline_presets.cloudwatch_log_groups
+  #iam_policies          = module.baseline_presets.iam_policies
+  #iam_roles             = module.baseline_presets.iam_roles
+  #key_pairs             = module.baseline_presets.key_pairs
+  #kms_grants            = module.baseline_presets.kms_grants
+  #s3_buckets            = local.baseline_s3_buckets
 
-  ec2_instances          = lookup(local.environment_config, "baseline_ec2_instances", {})
-  ec2_autoscaling_groups = lookup(local.environment_config, "baseline_ec2_autoscaling_groups", {})
-  lbs                    = lookup(local.environment_config, "baseline_lbs", {})
+  #ec2_instances          = lookup(local.environment_config, "baseline_ec2_instances", {})
+  #ec2_autoscaling_groups = lookup(local.environment_config, "baseline_ec2_autoscaling_groups", {})
+  #lbs                    = lookup(local.environment_config, "baseline_lbs", {})
 }
