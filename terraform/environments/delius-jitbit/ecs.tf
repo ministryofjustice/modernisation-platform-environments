00f56d355a0f689b6477a7b79a6ee87ebee7c6cf
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-28 11:34:18 +0000

    name prefix length reduction

--- terraform/environments/delius-jitbit/ecs.tf
+++ terraform/environments/delius-jitbit/ecs.tf
@@ -79,8 +79,7 @@ module "s3_bucket_app_deployment" {
   providers = {
     aws.bucket-replication = aws
   }
-
-  bucket_prefix      = "${local.application_name}-${local.environment}-app-deployment-"
+  bucket_prefix      = "${local.application_name}-${local.environment}-app-"
   versioning_enabled = true
 
   lifecycle_rule = [
