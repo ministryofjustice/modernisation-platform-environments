d145d1344c1a41b7152a241df325f2ff9885282f
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-02-28 14:27:05 +0000

    remove test instances

--- terraform/environments/nomis-combined-reporting/locals_development.tf
+++ terraform/environments/nomis-combined-reporting/locals_development.tf
@@ -15,58 +15,64 @@ locals {
     }
 
     baseline_ec2_instances = {
-      dev-redhat-rhel79-1 = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name  = "RHEL-7.9_HVM-*"
-          ami_owner = "309956199498"
-        })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["private"]
-        })
-        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-        tags = {
-          description = "For testing with official RedHat RHEL7.9 image"
-          os-type     = "Linux"
-          component   = "test"
-        }
-      }
-      dev-base-rhel79-1 = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name = "base_rhel_7_9_*"
-        })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["private"]
-        })
-        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-        tags = {
-          description = "For testing with official RedHat RHEL7.9 image"
-          os-type     = "Linux"
-          component   = "test"
-        }
-      }
+
+      # example instance using RedHat image
+      # dev-redhat-rhel79-1 = {
+      #   config = merge(module.baseline_presets.ec2_instance.config.default, {
+      #     ami_name  = "RHEL-7.9_HVM-*"
+      #     ami_owner = "309956199498"
+      #   })
+      #   instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+      #     vpc_security_group_ids = ["private"]
+      #   })
+      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+      #   tags = {
+      #     description = "For testing with official RedHat RHEL7.9 image"
+      #     os-type     = "Linux"
+      #     component   = "test"
+      #   }
+      # }
+
+      # example instance using core-shared-services-production image
+      # dev-base-rhel79-1 = {
+      #   config = merge(module.baseline_presets.ec2_instance.config.default, {
+      #     ami_name = "base_rhel_7_9_*"
+      #   })
+      #   instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+      #     vpc_security_group_ids = ["private"]
+      #   })
+      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+      #   tags = {
+      #     description = "For testing with official RedHat RHEL7.9 image"
+      #     os-type     = "Linux"
+      #     component   = "test"
+      #   }
+      # }
     }
 
     baseline_ec2_autoscaling_groups = {
-      dev-base-rhel79 = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name = "base_rhel_7_9_*"
-        })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["private"]
-        })
-        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-        autoscaling_group = {
-          desired_capacity    = 1
-          max_size            = 2
-          vpc_zone_identifier = module.environment.subnets["private"].ids
-        }
-        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
-        tags = {
-          description = "For testing with official RedHat RHEL7.9 image"
-          os-type     = "Linux"
-          component   = "test"
-        }
-      }
+
+      # example ASG using core-shared-services-production image
+      # dev-base-rhel79 = {
+      #   config = merge(module.baseline_presets.ec2_instance.config.default, {
+      #     ami_name = "base_rhel_7_9_*"
+      #   })
+      #   instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+      #     vpc_security_group_ids = ["private"]
+      #   })
+      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+      #   autoscaling_group = {
+      #     desired_capacity    = 1
+      #     max_size      #       # = 2
+      #     vpc_zone_identifier = module.environment.subnets["private"].ids
+      #   }
+      #   autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+      #   tags = {
+      #     description = "For testing with official RedHat RHEL7.9 image"
+      #     os-type     = "Linux"
+      #     component   = "test"
+      #   }
+      # }
     }
 
     baseline_lbs = {
