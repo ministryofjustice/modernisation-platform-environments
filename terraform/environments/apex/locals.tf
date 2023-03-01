678891c89809eb3952a335cc9db1a38792115efd
mikereiddigital <mike.reid@digital.justice.gov.uk>
2023-03-01 11:58:17 +0000

    Initial commit

--- terraform/environments/apex/locals.tf
+++ terraform/environments/apex/locals.tf
@@ -1,1 +1,18 @@
 #### This file can be used to store locals specific to the member account ####
+{
+  "accounts": {
+    "development": {
+      "shared_services_cidr": "10.200.0.0/20",
+      "account_cidr": "10.202.0.0/20"
+    },
+    "test": {
+      "example_var": "test-data"
+    },
+    "preproduction": {
+      "example_var": "preproduction-data"
+    },
+    "production": {
+      "example_var": "production-data"
+    }
+  }
+}
