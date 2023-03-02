b108e20fff73091e57cecce728b993b6a4a0733d
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-03-02 16:52:44 +0000

    change the security group to the rds one

--- terraform/environments/tipstaff/ec2.tf
+++ terraform/environments/tipstaff/ec2.tf
@@ -4,32 +4,33 @@ resource "aws_instance" "tipstaff-ec2-instance-dev" {
   ami                    = local.application_data.accounts[local.environment].ami
   count                  = "1"
   subnet_id              = data.aws_subnet.data_subnets_a.id
-  vpc_security_group_ids = [aws_security_group.tipstaff-dev-ec2-sc.id]
+  # vpc_security_group_ids = [aws_security_group.tipstaff-dev-ec2-sc.id]
+  vpc_security_group_ids = [resource.aws_security_group.postgresql_db_sc.id]
 }
 
-resource "aws_security_group" "tipstaff-dev-ec2-sc" {
-  name        = "ec2 security group"
-  description = "control access to the ec2 instance"
-  vpc_id      = data.aws_vpc.shared.id
-  ingress {
-    description = "Allow all traffic through HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "Allow all traffic through HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    description = "allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_security_group" "tipstaff-dev-ec2-sc" {
+#   name        = "ec2 security group"
+#   description = "control access to the ec2 instance"
+#   vpc_id      = data.aws_vpc.shared.id
+#   ingress {
+#     description = "Allow all traffic through HTTP"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     description = "Allow all traffic through HTTPS"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     description = "allow all outbound traffic"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
--- terraform/environments/tipstaff/load_balancer.tf
+++ terraform/environments/tipstaff/load_balancer.tf
@@ -3,30 +3,31 @@ resource "aws_lb" "tipstaff-dev-lb" {
   name                       = "tipstaff-dev-load-balancer"
   internal                   = false
   load_balancer_type         = "application"
-  security_groups            = [aws_security_group.tipstaff-dev-lb-sc.id]
+  # security_groups            = [aws_security_group.tipstaff-dev-lb-sc.id]
+  security_groups            = [resource.aws_security_group.postgresql_db_sc.id]
   subnets                    = data.aws_subnets.shared-public.ids
   enable_deletion_protection = false
 }
 
-resource "aws_security_group" "tipstaff-dev-lb-sc" {
-  name        = "load balancer security group"
-  description = "control access to the load balancer"
-  vpc_id      = data.aws_vpc.shared.id
-  ingress {
-    description = "Allow all traffic through HTTP"
-    from_port   = "80"
-    to_port     = "80"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    description = "allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_security_group" "tipstaff-dev-lb-sc" {
+#   name        = "load balancer security group"
+#   description = "control access to the load balancer"
+#   vpc_id      = data.aws_vpc.shared.id
+#   ingress {
+#     description = "Allow all traffic through HTTP"
+#     from_port   = "80"
+#     to_port     = "80"
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     description = "allow all outbound traffic"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 resource "aws_route53_record" "external" {
   provider = aws.core-vpc
