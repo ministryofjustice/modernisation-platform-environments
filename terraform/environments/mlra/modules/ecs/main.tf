8e612dff4c05f15f547694d473fa1812338688c5
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-03-02 10:21:10 +0000

    Correct cap prov managed_scaling block

--- terraform/environments/mlra/modules/ecs/main.tf
+++ terraform/environments/mlra/modules/ecs/main.tf
@@ -406,12 +406,13 @@ resource "aws_ecs_capacity_provider" "capacity_provider" {
 
   auto_scaling_group_provider {
     auto_scaling_group_arn = aws_autoscaling_group.cluster-scaling-group.arn
-  }
-  managed_scaling {
-      maximum_scaling_step_size = 1000
-      minimum_scaling_step_size = 1
-      status                    = "ENABLED"
-      target_capacity           = 10
+    managed_scaling {
+        maximum_scaling_step_size = 1000
+        minimum_scaling_step_size = 1
+        status                    = "ENABLED"
+        target_capacity           = 10
+    }
+
   }
 
   tags = merge(
