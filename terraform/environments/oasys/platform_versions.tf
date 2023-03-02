b99ca81e2ed52f0ede92c4dbf93df86035e01a67
Ewa Stempel <ewa.stempel@digital.justice.gov.uk>
2023-03-02 08:49:46 +0000

    Refactoring default files in oasys and nomis

--- terraform/environments/oasys/platform_versions.tf
+++ terraform/environments/oasys/platform_versions.tf
@@ -1,15 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 4.9"
+      version = ">= 4.0.0, < 5.0.0"
       source  = "hashicorp/aws"
     }
-
     http = {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
-
     github = {
       version = "~> 4.28.0"
       source  = "integrations/github"
--- terraform/modules/ec2_instance/versions.tf
+++ terraform/modules/ec2_instance/versions.tf
@@ -11,7 +11,7 @@ terraform {
     }
 
     random = {
-      version = "= 3.4.1"
+      version = "~> 3.4.1"
       source  = "hashicorp/random"
     }
 
