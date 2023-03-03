ee6de595a82477f4f9dcc0a7bae3d6766423af42
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-03-03 14:04:47 +0000

    add moj_vpn cidr block to the ec2 sg

--- terraform/environments/tipstaff/ec2.tf
+++ terraform/environments/tipstaff/ec2.tf
@@ -4,18 +4,18 @@ resource "aws_security_group" "tipstaff_dev_ec2_sc" {
   vpc_id      = data.aws_vpc.shared.id
 }
 resource "aws_security_group_rule" "ingress_traffic" {
-  for_each          = local.application_data.ec2_sg_rules
+  for_each          = local.application_data.accounts[local.environment].ec2_sg_rules
   description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
   from_port         = each.value.from_port
   protocol          = each.value.protocol
   security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
   to_port           = each.value.to_port
   type              = "ingress"
-  cidr_blocks       = [data.aws_vpc.shared.cidr_block]
+  cidr_blocks       = [data.aws_vpc.shared.cidr_block, local.application_data.accounts[local.environment].moj_ip]
 }
 
 resource "aws_security_group_rule" "egress_traffic" {
-  for_each                 = local.application_data.ec2_sg_rules
+  for_each                 = local.application_data.accounts[local.environment].ec2_sg_rules
   description              = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
   from_port                = each.value.from_port
   protocol                 = each.value.protocol
