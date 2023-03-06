be788f6385bd64c72f48a3c7c836add8cc60ce02
umeshc-roy <umeshc.roy@gmail.com>
2023-03-06 15:43:25 +0000

    ppud pre-prod modify-3

--- terraform/environments/ppud/ad.tf
+++ terraform/environments/ppud/ad.tf
@@ -1,8 +1,6 @@
-
-/*
 locals {
   ad_creds = jsondecode(
-    data.aws_secretsmanager_secret_version.creds.secret_string
+    data.aws_secretsmanager_secret_version.creds[0].secret_string
     )
 }
 
@@ -14,6 +12,7 @@ resource "null_resource" "test_pass" {
   }
 }
 
+/*
 output "Password" {
   description = "AD ADMIN password"
   value       = local.ad_creds.password
