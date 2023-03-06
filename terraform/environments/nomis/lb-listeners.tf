909ae5193779cfb62201322b6e549c4c0f48d69f
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-03-06 09:47:48 +0000

    add other alerts and sns_topic over-ride

--- terraform/environments/nomis/lb-listeners.tf
+++ terraform/environments/nomis/lb-listeners.tf
@@ -21,6 +21,28 @@ locals {
       alarm_description   = "This metric monitors the number of unhealthy hosts in the target table for the load balancer. If the number of unhealthy hosts is greater than 0 for 3 minutes."
       alarm_actions       = [aws_sns_topic.nomis_nonprod_alarms.arn]
     }
+    load-balancer-unhealthy-state-routing = {
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      evaluation_periods  = "3"
+      metric_name         = "UnHealthyStateRouting"
+      namespace           = "AWS/ApplicationELB"
+      period              = "60"
+      statistic           = "Min"
+      threshold           = "1"
+      alarm_description   = "This metric monitors the number of unhealthy hosts in the routing table for the load balancer. If the number of unhealthy hosts is greater than 0 for 3 minutes."
+      alarm_actions       = [aws_sns_topic.nomis_nonprod_alarms.arn]
+    }
+    load-balancer-unhealthy-state-dns = {
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      evaluation_periods  = "3"
+      metric_name         = "UnHealthyStateDNS"
+      namespace           = "AWS/ApplicationELB"
+      period              = "60"
+      statistic           = "Min"
+      threshold           = "1"
+      alarm_description   = "This metric monitors the number of unhealthy hosts in the DNS table for the load balancer. If the number of unhealthy hosts is greater than 0 for 3 minutes."
+      alarm_actions       = [aws_sns_topic.nomis_nonprod_alarms.arn]
+    }
   }
 }
 
@@ -33,20 +55,24 @@ module "lb_listener" {
     aws.core-vpc = aws.core-vpc
   }
 
-  name                     = each.key
-  business_unit            = local.business_unit
-  environment              = local.environment
-  load_balancer_arn        = module.loadbalancer[each.value.lb_application_name].load_balancer.arn
-  target_groups            = try(each.value.target_groups, {})
-  existing_target_groups   = local.existing_target_groups
-  port                     = each.value.port
-  protocol                 = each.value.protocol
-  ssl_policy               = try(each.value.ssl_policy, null)
-  certificate_arns         = try(each.value.certificate_arns, [])
-  default_action           = each.value.default_action
-  rules                    = try(each.value.rules, {})
-  route53_records          = try(each.value.route53_records, {})
-  replace                  = try(each.value.replace, {})
-  tags                     = try(each.value.tags, local.tags)
-  cloudwatch_metric_alarms = local.cloudwatch_metric_alarms_listener
+  name                   = each.key
+  business_unit          = local.business_unit
+  environment            = local.environment
+  load_balancer_arn      = module.loadbalancer[each.value.lb_application_name].load_balancer.arn
+  target_groups          = try(each.value.target_groups, {})
+  existing_target_groups = local.existing_target_groups
+  port                   = each.value.port
+  protocol               = each.value.protocol
+  ssl_policy             = try(each.value.ssl_policy, null)
+  certificate_arns       = try(each.value.certificate_arns, [])
+  default_action         = each.value.default_action
+  rules                  = try(each.value.rules, {})
+  route53_records        = try(each.value.route53_records, {})
+  replace                = try(each.value.replace, {})
+  tags                   = try(each.value.tags, local.tags)
+  cloudwatch_metric_alarms = {
+    for key, value in local.cloudwatch_metric_alarms_listener :
+    key => merge(value,
+      { alarm_actions = [lookup(each.value, "sns_topic", aws_sns_topic.nomis_nonprod_alarms.arn)]
+  }) }
 }
