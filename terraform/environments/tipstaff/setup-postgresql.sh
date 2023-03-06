5bdd4104a15f559c7bb5431f1d2355d1e364e617
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-03-06 13:43:16 +0000

    delete parameters in shell script

--- terraform/environments/tipstaff/setup-postgresql.sh
+++ terraform/environments/tipstaff/setup-postgresql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws rds-data execute-statement --database $DB_NAME --resource-arn $RDS_ARN --secret-arn $SECRETS_ARN --sql tipstaff_staging_predata_backup.sql --parameters '{"name":"username","value":{"stringValue":"'"$LOCAL_DB_USERNAME"'"}},{"name":"password","value":{"stringValue":"'"$LOCAL_DB_PASSWORD"'"}}'
+aws rds-data execute-statement --database $DB_NAME --resource-arn $RDS_ARN --secret-arn $SECRETS_ARN --sql tipstaff_staging_predata_backup.sql
