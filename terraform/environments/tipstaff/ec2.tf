94693dc99b85c4acc4fd4ed6c432bd14f9200ae6
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-03-06 17:05:23 +0000

    add ec2 and load balancer back in

--- terraform/environments/tipstaff/ec2.tf
+++ terraform/environments/tipstaff/ec2.tf
@@ -1,37 +1,51 @@
-# resource "aws_security_group" "tipstaff_dev_ec2_sc" {
-#   name        = "ec2 security group"
-#   description = "control access to the ec2 instance"
-#   vpc_id      = data.aws_vpc.shared.id
-# }
-# resource "aws_security_group_rule" "ingress_traffic" {
-#   for_each          = local.application_data.ec2_sg_rules
-#   description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
-#   from_port         = each.value.from_port
-#   protocol          = each.value.protocol
-#   security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
-#   to_port           = each.value.to_port
-#   type              = "ingress"
-#   cidr_blocks       = [data.aws_vpc.shared.cidr_block, local.application_data.accounts[local.environment].moj_ip]
-# }
+resource "aws_security_group" "tipstaff_dev_ec2_sc" {
+  name        = "ec2 security group"
+  description = "control access to the ec2 instance"
+  vpc_id      = data.aws_vpc.shared.id
+}
+resource "aws_security_group_rule" "ingress_traffic" {
+  for_each          = local.application_data.ec2_sg_rules
+  description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
+  from_port         = each.value.from_port
+  protocol          = each.value.protocol
+  security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
+  to_port           = each.value.to_port
+  type              = "ingress"
+  cidr_blocks       = [data.aws_vpc.shared.cidr_block, local.application_data.accounts[local.environment].moj_ip]
+}
 
-# resource "aws_security_group_rule" "egress_traffic" {
-#   for_each                 = local.application_data.ec2_sg_rules
-#   description              = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
-#   from_port                = each.value.from_port
-#   protocol                 = each.value.protocol
-#   security_group_id        = aws_security_group.tipstaff_dev_ec2_sc.id
-#   to_port                  = each.value.to_port
-#   type                     = "egress"
-#   source_security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
-# }
-# resource "aws_instance" "tipstaff_ec2_instance_dev" {
-#   instance_type          = local.application_data.accounts[local.environment].instance_type
-#   ami                    = local.application_data.accounts[local.environment].ami_image_id
-#   subnet_id              = data.aws_subnet.private_subnets_a.id
-#   vpc_security_group_ids = [aws_security_group.tipstaff_dev_ec2_sc.id]
-#   # monitoring             = true
-#   # ebs_optimized          = true
-#   depends_on = [aws_security_group.tipstaff_dev_ec2_sc]
-# }
+resource "aws_security_group_rule" "egress_traffic" {
+  for_each                 = local.application_data.ec2_sg_rules
+  description              = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
+  from_port                = each.value.from_port
+  protocol                 = each.value.protocol
+  security_group_id        = aws_security_group.tipstaff_dev_ec2_sc.id
+  to_port                  = each.value.to_port
+  type                     = "egress"
+  source_security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
+}
+# Get latest Windows Server 2019 AMI
+data "aws_ami" "windows-2019" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base*"]
+  }
+}
+resource "aws_instance" "tipstaff_ec2_instance_dev" {
+  instance_type          = local.application_data.accounts[local.environment].instance_type
+  ami                    = data.aws_ami.windows-2019
+  subnet_id              = data.aws_subnet.private_subnets_a.id
+  vpc_security_group_ids = [aws_security_group.tipstaff_dev_ec2_sc.id]
+  # monitoring             = true
+  # ebs_optimized          = true
+  user_data = <<-EOF
+              <powershell>
+              Install-WindowsFeature -name Web-Server -IncludeManagementTools
+              </powershell>
+              EOF
+  depends_on = [aws_security_group.tipstaff_dev_ec2_sc]
+}
 
 
--- terraform/environments/tipstaff/load_balancer.tf
+++ terraform/environments/tipstaff/load_balancer.tf
@@ -1,142 +1,97 @@
-# resource "aws_security_group" "tipstaff_dev_lb_sc" {
-#   name        = "load balancer security group"
-#   description = "control access to the load balancer"
-#   vpc_id      = data.aws_vpc.shared.id
-# }
-# resource "aws_security_group_rule" "ingress_traffic_lb" {
-#   for_each          = local.application_data.ec2_sg_rules
-#   description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
-#   from_port         = each.value.from_port
-#   protocol          = each.value.protocol
-#   security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
-#   to_port           = each.value.to_port
-#   type              = "ingress"
-#   cidr_blocks       = [data.aws_vpc.shared.cidr_block, local.application_data.accounts[local.environment].moj_ip]
-# }
-# resource "aws_security_group_rule" "egress_traffic_lb" {
-#   for_each                 = local.application_data.ec2_sg_rules
-#   description              = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
-#   from_port                = each.value.from_port
-#   protocol                 = each.value.protocol
-#   security_group_id        = aws_security_group.tipstaff_dev_lb_sc.id
-#   to_port                  = each.value.to_port
-#   type                     = "egress"
-#   source_security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
-# }
-# resource "aws_lb" "tipstaff_dev_lb" {
-#   name                       = "tipstaff-dev-load-balancer"
-#   load_balancer_type         = "application"
-#   security_groups            = [aws_security_group.tipstaff_dev_lb_sc.id]
-#   subnets                    = data.aws_subnets.shared-public.ids
-#   enable_deletion_protection = false
+resource "aws_security_group" "tipstaff_dev_lb_sc" {
+  name        = "load balancer security group"
+  description = "control access to the load balancer"
+  vpc_id      = data.aws_vpc.shared.id
+}
+resource "aws_security_group_rule" "ingress_traffic_lb" {
+  for_each          = local.application_data.ec2_sg_rules
+  description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
+  from_port         = each.value.from_port
+  protocol          = each.value.protocol
+  security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
+  to_port           = each.value.to_port
+  type              = "ingress"
+  cidr_blocks       = [data.aws_vpc.shared.cidr_block, local.application_data.accounts[local.environment].moj_ip]
+}
+resource "aws_security_group_rule" "egress_traffic_lb" {
+  for_each                 = local.application_data.ec2_sg_rules
+  description              = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
+  from_port                = each.value.from_port
+  protocol                 = each.value.protocol
+  security_group_id        = aws_security_group.tipstaff_dev_lb_sc.id
+  to_port                  = each.value.to_port
+  type                     = "egress"
+  source_security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
+}
+resource "aws_lb" "tipstaff_dev_lb" {
+  name                       = "tipstaff-dev-load-balancer"
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.tipstaff_dev_lb_sc.id]
+  subnets                    = data.aws_subnets.shared-public.ids
+  enable_deletion_protection = false
 
-#   depends_on = [aws_security_group.tipstaff_dev_ec2_sc]
-# }
-# resource "aws_lb_target_group" "tipstaff_dev_target_group" {
-#   name                 = "tipstaff-dev-target-group"
-#   port                 = local.application_data.accounts[local.environment].server_port
-#   protocol             = "HTTP"
-#   vpc_id               = data.aws_vpc.shared.id
-#   target_type          = "instance"
-#   deregistration_delay = 30
+  depends_on = [aws_security_group.tipstaff_dev_lb_sc]
+}
+resource "aws_lb_target_group" "tipstaff_dev_target_group" {
+  name                 = "tipstaff-dev-target-group"
+  port                 = local.application_data.accounts[local.environment].server_port
+  protocol             = "HTTP"
+  vpc_id               = data.aws_vpc.shared.id
+  target_type          = "instance"
+  deregistration_delay = 30
 
-#   stickiness {
-#     type = "lb_cookie"
-#   }
-#   #checkov:skip=CKV_AWS_261: "health_check defined below, but not picked up"
-#   health_check {
-#     healthy_threshold   = "5"
-#     interval            = "120"
-#     protocol            = "HTTP"
-#     unhealthy_threshold = "2"
-#     matcher             = "200-499"
-#     timeout             = "5"
-#   }
+  stickiness {
+    type = "lb_cookie"
+  }
+  #checkov:skip=CKV_AWS_261: "health_check defined below, but not picked up"
+  health_check {
+    healthy_threshold   = "5"
+    interval            = "120"
+    protocol            = "HTTP"
+    unhealthy_threshold = "2"
+    matcher             = "200-499"
+    timeout             = "5"
+  }
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "${local.application_name}-tg-${local.environment}"
-#     }
-#   )
-# }
-# # Link target group to the EC2 instance on port 80
-# resource "aws_lb_target_group_attachment" "tipstaff_ec2_instance_dev" {
-#   target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
-#   target_id        = aws_instance.tipstaff_ec2_instance_dev.id
-#   port             = 80
-# }
-# resource "aws_lb_listener" "tipstaff_dev_lb" {
-#   load_balancer_arn = aws_lb.tipstaff_dev_lb.arn
-#   port              = local.application_data.accounts[local.environment].server_port
-#   protocol          = local.application_data.accounts[local.environment].lb_listener_protocol
-#   #checkov:skip=CKV_AWS_2: "protocol for lb set in application_variables"
-#   ssl_policy = local.application_data.accounts[local.environment].lb_listener_protocol == "HTTP" ? "" : "ELBSecurityPolicy-2016-08"
-#   #checkov:skip=CKV_AWS_103: "ssl_policy for lb set in application_variables"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${local.application_name}-tg-${local.environment}"
+    }
+  )
+}
+# Link target group to the EC2 instance on port 80
+resource "aws_lb_target_group_attachment" "tipstaff_ec2_instance_dev" {
+  target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
+  target_id        = aws_instance.tipstaff_ec2_instance_dev.id
+  port             = 80
+}
+resource "aws_lb_listener" "tipstaff_dev_lb" {
+  load_balancer_arn = aws_lb.tipstaff_dev_lb.arn
+  port              = local.application_data.accounts[local.environment].server_port
+  protocol          = local.application_data.accounts[local.environment].lb_listener_protocol
+  #checkov:skip=CKV_AWS_2: "protocol for lb set in application_variables"
+  ssl_policy = local.application_data.accounts[local.environment].lb_listener_protocol == "HTTP" ? "" : "ELBSecurityPolicy-2016-08"
+  #checkov:skip=CKV_AWS_103: "ssl_policy for lb set in application_variables"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
+  }
+}
 
-# resource "aws_wafv2_web_acl" "tipstaff_dev_waf" {
-#   #checkov:skip=CKV2_AWS_31:Logging example commented out below, example is sound but no logging configuration for it to build.
-#   name  = "example-web-acl"
-#   scope = "REGIONAL"
 
-#   default_action {
-#     allow {}
-#   }
+// Look into the route53 stuff as well:
+# resource "aws_route53_record" "external" {
+#   provider = aws.core-vpc
 
-#   rule {
-#     name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
-#     priority = 1
+#   zone_id = data.aws_route53_zone.external.zone_id
+#   name    = "${local.application_data.accounts[local.environment].subdomain_name}.modernisation-platform.service.justice.gov.uk"
+#   type    = "A"
 
-#     override_action {
-#       none {}
-#     }
-
-#     statement {
-#       managed_rule_group_statement {
-#         name        = "AWSManagedRulesKnownBadInputsRuleSet"
-#         vendor_name = "AWS"
-#       }
-#     }
-
-#     visibility_config {
-#       cloudwatch_metrics_enabled = false
-#       metric_name                = "friendly-rule-metric-name"
-#       sampled_requests_enabled   = false
-#     }
-#   }
-
-#   visibility_config {
-#     cloudwatch_metrics_enabled = false
-#     metric_name                = "my-web-acl"
-#     sampled_requests_enabled   = false
+#   alias {
+#     name                   = aws_lb.tipstaff-dev-lb.dns_name
+#     zone_id                = aws_lb.tipstaff-dev-lb.zone_id
+#     evaluate_target_health = true
 #   }
 # }
-
-# # Association code for WAFv2 to the LB
-
-# resource "aws_wafv2_web_acl_association" "web_acl_association_my_lb" {
-#   resource_arn = aws_lb.tipstaff_dev_lb.arn
-#   web_acl_arn  = aws_wafv2_web_acl.tipstaff_dev_waf.arn
-# }
-
-
-# // Look into the route53 stuff as well:
-# # resource "aws_route53_record" "external" {
-# #   provider = aws.core-vpc
-
-# #   zone_id = data.aws_route53_zone.external.zone_id
-# #   name    = "${local.application_data.accounts[local.environment].subdomain_name}.modernisation-platform.service.justice.gov.uk"
-# #   type    = "A"
-
-# #   alias {
-# #     name                   = aws_lb.tipstaff-dev-lb.dns_name
-# #     zone_id                = aws_lb.tipstaff-dev-lb.zone_id
-# #     evaluate_target_health = true
-# #   }
-# # }
