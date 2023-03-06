9269b5fb3e24cf09c1e6920c7ddb8e726dd5eafb
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-03-06 14:59:22 +0000

    add DB password

--- terraform/environments/tipstaff/setup-postgresql.sh
+++ terraform/environments/tipstaff/setup-postgresql.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PGPASSWORD=$LOCAL_DB_PASSWORD;
 # if database contains schema dbo then store schema name inside variable.
 SCHEMA=$(psql -h ${DB_HOSTNAME} -p 5432 -U $LOCAL_DB_USERNAME -d $DB_NAME -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'dbo'" | grep -o 'dbo') 
 echo "$SCHEMA"
