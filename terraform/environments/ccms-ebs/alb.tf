88bed938a1f8433266f2d233a92519385dde1761
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-07 16:45:22 +0000

    cert fixes

--- terraform/environments/ccms-ebs/alb.tf
+++ terraform/environments/ccms-ebs/alb.tf
@@ -41,7 +41,7 @@ resource "aws_lb_listener" "ebsapps_listener" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.external-mp[0].arn
+  certificate_arn   = aws_acm_certificate.external-mp.arn
 
   default_action {
     type             = "forward"
--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -1,9 +1,11 @@
+
+
 ################################################################
 #   *.modernisation-platform.service.justice.gov.uk
 ################################################################
 resource "aws_acm_certificate" "external-mp" {
-  count             = local.is-production ? 0 : 1
-  domain_name       = "*.modernisation-platform.service.justice.gov.uk"
+  #count             = local.is-production ? 0 : 1
+  domain_name       = "*.${local.application_data.accounts[local.environment].dns}.modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
   #subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   ########  ccms-ebs.laa-development.modernisation-platform.service.justice.gov.uk
@@ -22,7 +24,7 @@ resource "aws_route53_record" "external-mp" {
   ]
   provider = aws.core-network-services
   for_each = {
-    for dvo in aws_acm_certificate.external-mp[0].domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.external-mp.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -41,14 +43,14 @@ resource "aws_acm_certificate_validation" "external-mp" {
   depends_on = [
     aws_route53_record.external-mp
   ]
-  certificate_arn         = aws_acm_certificate.external-mp[0].arn
+  certificate_arn         = aws_acm_certificate.external-mp.arn
   validation_record_fqdns = [for record in aws_route53_record.external-mp : record.fqdn]
 }
-
+/*
 ################################################################
 #   *.service.justice.gov.uk
 ################################################################
-/*
+
 resource "aws_acm_certificate" "external-service" {
   count             = local.is-production ? 0 : 1
   domain_name       = "*.service.justice.gov.uk"
