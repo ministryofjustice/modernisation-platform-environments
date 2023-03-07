77681f6c19037cf90401a5b4756dd99ecb204b84
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-03-07 15:02:11 +0000

    rename service exec iam role to task exec

--- terraform/environments/delius-jitbit/iam.tf
+++ terraform/environments/delius-jitbit/iam.tf
@@ -90,7 +90,7 @@ data "aws_iam_policy_document" "ecs_task_exec" {
 }
 
 resource "aws_iam_role" "ecs_exec" {
-  name               = format("hmpps-%s-%s-service-exec", local.environment, local.application_name)
+  name               = format("hmpps-%s-%s-task-exec", local.environment, local.application_name)
   assume_role_policy = data.aws_iam_policy_document.ecs_task_exec.json
   tags               = local.tags
 }
