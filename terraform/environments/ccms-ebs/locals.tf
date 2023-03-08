d2f8bca2bb23d0b038d12eb50dc104cd2a9d6b73
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-08 14:17:11 +0000

    new s3 name

--- terraform/environments/ccms-ebs/locals.tf
+++ terraform/environments/ccms-ebs/locals.tf
@@ -3,5 +3,6 @@
 locals {
   artefact_bucket_name = "${local.application_name}-${local.environment}-artefacts"
   logging_bucket_name  = "${local.application_name}-${local.environment}-logging"
+  rsync_bucket_name    = "${local.application_name}-${local.environment}-db-backup"
   lb_log_prefix        = "ebsapps-lb"
 }
--- terraform/environments/ccms-ebs/s3.tf
+++ terraform/environments/ccms-ebs/s3.tf
@@ -161,3 +161,113 @@ data "aws_iam_policy_document" "logging_s3_policy" {
     resources = ["${module.s3-bucket-logging.bucket.arn}/*"]
   }
 }
+
+/*
+#------------------------------------------------------------------------------
+# S3 Bucket - R-sync
+#------------------------------------------------------------------------------
+module "s3-bucket-db-backup" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.2.0"
+
+  bucket_name        = local.rsync_bucket_name
+  versioning_enabled = false
+  bucket_policy      = [data.aws_iam_policy_document.rsync_s3_policy.json]
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below three variables and providers configuration are only relevant if 'replication_enabled' is set to true
+  replication_region                       = "eu-west-2"
+  versioning_enabled_on_replication_bucket = false
+  # replication_role_arn                     = module.s3-bucket-replication-role.role.arn
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-%s-%s-db-backup", local.application_name, local.environment)) }
+  )
+}
+
+data "aws_iam_policy_document" "rsync_s3_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::652711504416:root"]
+    }
+    actions   = ["s3:PutObject"]
+    resources = ["${module.s3-bucket-db-backup.bucket.arn}/*"]
+  }
+}
+data "aws_iam_policy_document" "deny_http_s3_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::652711504416:root"]
+    }
+    actions   = ["s3:*"]
+    resources = ["${module.s3-bucket-db-backup.bucket.arn}/"]
+    effect = "Deny"
+    condition {
+      test      = "StringNotEquals"
+      variable  = "aws:SecureTransport"
+      values    = ["https"]
+    }
+  }
+}
+*/
+/*
+ "Statement": [ 
+    { 
+      "Effect": "Deny", 
+      "Action": "s3:", 
+      "Resource": "arn:aws:s3:::bucketname/", 
+      "Condition": { 
+        "StringNotEquals": { "aws:SecureTransport": "https" } 
+      } 
+    }
+ ]
+ */
