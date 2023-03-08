73a535dcd5a842dc0532af9b37624ce68e6bd31d
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-08 15:06:17 +0000

    switching back to old method of validation

--- terraform/environments/tipstaff/dns_ssl.tf
+++ terraform/environments/tipstaff/dns_ssl.tf
@@ -14,11 +14,13 @@ resource "aws_acm_certificate" "tipstaff_app_cert" {
 // Points domain to load balancer
 resource "aws_route53_record" "tipstaff_app_direct_traffic" {
   provider = aws.core-vpc
-  zone_id  = data.aws_route53_zone.inner.zone_id
+
+  allow_overwrite = true
   name     = "${local.application_data.accounts[local.environment].subdomain_name}.modernisation-platform.internal"
-  type     = "CNAME"
-  ttl      = 900
   records  = [aws_lb.tipstaff_dev_lb.dns_name]
+  ttl      = 900
+  type     = "CNAME"
+  zone_id  = data.aws_route53_zone.inner.zone_id
 }
 
 
@@ -33,36 +35,37 @@ resource "aws_route53_record" "tipstaff_app_direct_traffic" {
 #   zone_id         = data.aws_route53_zone.network-services.zone_id
 # }
 
-# resource "aws_route53_record" "internal_validation_subdomain_tipstaff" {
-#   count    = length(local.tipstaff_domain_name_sub)
-#   provider = aws.core-network-services
-
-#   allow_overwrite = true
-#   name            = local.tipstaff_domain_name_sub[count.index]
-#   records         = [local.tipstaff_domain_record_sub[count.index]]
-#   ttl             = 60
-#   type            = local.tipstaff_domain_type_sub[count.index]
-#   zone_id         = data.aws_route53_zone.inner.zone_id
-# }
-
 resource "aws_route53_record" "internal_validation_subdomain_tipstaff" {
-  provider = aws.core-vpc
-
-  for_each = {
-    for dvo in aws_acm_certificate.tipstaff_app_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+  count    = length(local.tipstaff_domain_name_sub)
+  provider = aws.core-network-services
 
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
+  name            = local.tipstaff_domain_name_sub[count.index]
+  records         = [local.tipstaff_domain_record_sub[count.index]]
   ttl             = 60
-  type            = each.value.type
+  type            = local.tipstaff_domain_type_sub[count.index]
   zone_id         = data.aws_route53_zone.inner.zone_id
 }
+
+// validates the ACM Certificate by creating a record in Route53
+# resource "aws_route53_record" "internal_validation_subdomain_tipstaff" {
+#   provider = aws.core-vpc
+
+#   for_each = {
+#     for dvo in aws_acm_certificate.tipstaff_app_cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
+
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.inner.zone_id
+# }
 
 resource "aws_acm_certificate_validation" "tipstaff_lb_cert_validation" {
   certificate_arn         = aws_acm_certificate.tipstaff_app_cert.arn
