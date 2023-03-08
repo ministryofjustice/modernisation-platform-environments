1741ce5c657c3d45fa485261deed56299beb5134
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-08 16:46:44 +0000

    adding validation resources

--- terraform/environments/tipstaff/dns_ssl.tf
+++ terraform/environments/tipstaff/dns_ssl.tf
@@ -63,3 +63,26 @@ resource "aws_acm_certificate" "inner" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "inner_validation" {
+  provider = aws.core-vpc
+  for_each = {
+    for dvo in aws_acm_certificate.inner.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.external.zone_id
+}
+
+resource "aws_acm_certificate_validation" "tipstaff_lb_cert_validation" {
+  certificate_arn         = aws_acm_certificate.inner.arn
+  validation_record_fqdns = [for record in aws_route53_record.inner_validation : record.fqdn]
+}
