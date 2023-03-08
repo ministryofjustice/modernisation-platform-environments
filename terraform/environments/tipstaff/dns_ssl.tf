e7a6a726e272917adbe27d5fd167762dd7f32c6a
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-08 15:21:02 +0000

    switching back to previous route53 internal_validation_tipstaff

--- terraform/environments/tipstaff/dns_ssl.tf
+++ terraform/environments/tipstaff/dns_ssl.tf
@@ -12,28 +12,28 @@ resource "aws_acm_certificate" "tipstaff_app_cert" {
 }
 
 // Points domain to load balancer
-resource "aws_route53_record" "tipstaff_app_direct_traffic" {
-  provider = aws.core-network-services
-
-  allow_overwrite = true
-  name            = "${local.application_data.accounts[local.environment].subdomain_name}.modernisation-platform.internal"
-  records         = [aws_lb.tipstaff_dev_lb.dns_name]
-  ttl             = 900
-  type            = "CNAME"
-  zone_id         = data.aws_route53_zone.inner.zone_id
-}
-
-
-# resource "aws_route53_record" "internal_validation_tipstaff" {
+# resource "aws_route53_record" "tipstaff_app_direct_traffic" {
 #   provider = aws.core-network-services
 
 #   allow_overwrite = true
-#   name            = local.tipstaff_domain_name_main[0]
-#   records         = local.tipstaff_domain_record_main
-#   ttl             = 60
-#   type            = local.tipstaff_domain_type_main[0]
-#   zone_id         = data.aws_route53_zone.network-services.zone_id
+#   name            = "${local.application_data.accounts[local.environment].subdomain_name}.modernisation-platform.internal"
+#   records         = [aws_lb.tipstaff_dev_lb.dns_name]
+#   ttl             = 900
+#   type            = "CNAME"
+#   zone_id         = data.aws_route53_zone.inner.zone_id
 # }
+
+
+resource "aws_route53_record" "internal_validation_tipstaff" {
+  provider = aws.core-network-services
+
+  allow_overwrite = true
+  name            = local.tipstaff_domain_name_main[0]
+  records         = local.tipstaff_domain_record_main
+  ttl             = 60
+  type            = local.tipstaff_domain_type_main[0]
+  zone_id         = data.aws_route53_zone.network-services.zone_id
+}
 
 resource "aws_route53_record" "internal_validation_subdomain_tipstaff" {
   count    = length(local.tipstaff_domain_name_sub)
