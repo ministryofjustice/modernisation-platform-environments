46bd5840ad6666157f8d5df48da55e22f5e43ba5
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-03-08 11:35:27 +0000

    change aws_route53_zone to external

--- terraform/environments/tipstaff/dns_ssl.tf
+++ terraform/environments/tipstaff/dns_ssl.tf
@@ -40,7 +40,7 @@ resource "aws_route53_record" "external_validation_subdomain_tipstaff" {
   records         = [local.tipstaff_domain_record_sub[count.index]]
   ttl             = 60
   type            = local.tipstaff_domain_type_sub[count.index]
-  zone_id         = data.aws_route53_zone.external_r53_zone.zone_id
+  zone_id         = data.aws_route53_zone.external.zone_id
 }
 
 resource "aws_acm_certificate_validation" "tipstaff_lb_cert_validation" {
