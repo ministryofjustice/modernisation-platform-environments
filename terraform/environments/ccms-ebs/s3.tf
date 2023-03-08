c4c623afeceb4babe98889656e097c2c002edcfe
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-08 18:20:11 +0000

    fix actions

--- terraform/environments/ccms-ebs/s3.tf
+++ terraform/environments/ccms-ebs/s3.tf
@@ -241,9 +241,7 @@ data "aws_iam_policy_document" "dbbackup_s3_policy" {
       ]
     }
     actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:ListBucket"
+      "s3:PutObject"
     ]
     resources = ["${module.s3-bucket-dbbackup.bucket.arn}/*"]
   }
