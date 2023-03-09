9a032f410f81b0106993ef25d057427b60f2d131
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-03-09 16:57:01 +0000

    add everything except for ec2 and target group

--- terraform/environments/tipstaff/ec2.tf
+++ terraform/environments/tipstaff/ec2.tf
@@ -1,31 +1,31 @@
-# resource "aws_security_group" "tipstaff_dev_ec2_sc" {
-#   name        = "ec2 security group"
-#   description = "control access to the ec2 instance"
-#   # vpc_id      = data.aws_vpc.shared.id
-# }
+resource "aws_security_group" "tipstaff_dev_ec2_sc" {
+  name        = "ec2 security group"
+  description = "control access to the ec2 instance"
+  # vpc_id      = data.aws_vpc.shared.id
+}
 
-# resource "aws_security_group_rule" "ingress_traffic" {
-#   for_each                 = local.application_data.ec2_sg_rules
-#   description              = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
-#   from_port                = each.value.from_port
-#   protocol                 = each.value.protocol
-#   security_group_id        = aws_security_group.tipstaff_dev_ec2_sc.id
-#   to_port                  = each.value.to_port
-#   type                     = "ingress"
-#   source_security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
-# }
+resource "aws_security_group_rule" "ingress_traffic" {
+  for_each                 = local.application_data.ec2_sg_rules
+  description              = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
+  from_port                = each.value.from_port
+  protocol                 = each.value.protocol
+  security_group_id        = aws_security_group.tipstaff_dev_ec2_sc.id
+  to_port                  = each.value.to_port
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
+}
 
-# resource "aws_security_group_rule" "egress_traffic" {
-#   for_each          = local.application_data.ec2_sg_rules
-#   description       = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
-#   from_port         = each.value.from_port
-#   protocol          = each.value.protocol
-#   security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
-#   to_port           = each.value.to_port
-#   type              = "egress"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   ipv6_cidr_blocks  = ["::/0"]
-# }
+resource "aws_security_group_rule" "egress_traffic" {
+  for_each          = local.application_data.ec2_sg_rules
+  description       = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
+  from_port         = each.value.from_port
+  protocol          = each.value.protocol
+  security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
+  to_port           = each.value.to_port
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
 
 # resource "aws_instance" "tipstaff_ec2_instance_dev" {
 #   instance_type               = local.application_data.accounts[local.environment].instance_type
@@ -43,7 +43,7 @@
 #   depends_on                  = [aws_security_group.tipstaff_dev_ec2_sc]
 # }
 
-# resource "aws_key_pair" "ec2_access_key" {
-#   key_name   = "ec2_access_key"
-#   public_key = jsondecode(data.aws_secretsmanager_secret_version.public_key.secret_string)["tipstaff_public_key"]
-# }
+resource "aws_key_pair" "ec2_access_key" {
+  key_name   = "ec2_access_key"
+  public_key = jsondecode(data.aws_secretsmanager_secret_version.public_key.secret_string)["tipstaff_public_key"]
+}
--- terraform/environments/tipstaff/load_balancer.tf
+++ terraform/environments/tipstaff/load_balancer.tf
@@ -1,39 +1,39 @@
-# resource "aws_security_group" "tipstaff_dev_lb_sc" {
-#   name        = "load balancer security group"
-#   description = "control access to the load balancer"
-#   # vpc_id      = data.aws_vpc.shared.id
-# }
+resource "aws_security_group" "tipstaff_dev_lb_sc" {
+  name        = "load balancer security group"
+  description = "control access to the load balancer"
+  # vpc_id      = data.aws_vpc.shared.id
+}
 
-# resource "aws_security_group_rule" "ingress_traffic_lb" {
-#   for_each          = local.application_data.ec2_sg_rules
-#   description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
-#   from_port         = each.value.from_port
-#   protocol          = each.value.protocol
-#   security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
-#   to_port           = each.value.to_port
-#   type              = "ingress"
-#   cidr_blocks       = [local.application_data.accounts[local.environment].moj_ip]
-# }
+resource "aws_security_group_rule" "ingress_traffic_lb" {
+  for_each          = local.application_data.ec2_sg_rules
+  description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
+  from_port         = each.value.from_port
+  protocol          = each.value.protocol
+  security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
+  to_port           = each.value.to_port
+  type              = "ingress"
+  cidr_blocks       = [local.application_data.accounts[local.environment].moj_ip]
+}
 
-# resource "aws_security_group_rule" "egress_traffic_lb" {
-#   for_each          = local.application_data.ec2_sg_rules
-#   description       = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
-#   from_port         = each.value.from_port
-#   protocol          = each.value.protocol
-#   security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
-#   to_port           = each.value.to_port
-#   type              = "egress"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
+resource "aws_security_group_rule" "egress_traffic_lb" {
+  for_each          = local.application_data.ec2_sg_rules
+  description       = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
+  from_port         = each.value.from_port
+  protocol          = each.value.protocol
+  security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
+  to_port           = each.value.to_port
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-# resource "aws_lb" "tipstaff_dev_lb" {
-#   name                       = "tipstaff-dev-load-balancer"
-#   load_balancer_type         = "application"
-#   security_groups            = [aws_security_group.tipstaff_dev_lb_sc.id]
-#   subnets                    = data.aws_subnets.shared-public.ids
-#   enable_deletion_protection = false
-#   depends_on                 = [aws_security_group.tipstaff_dev_lb_sc]
-# }
+resource "aws_lb" "tipstaff_dev_lb" {
+  name                       = "tipstaff-dev-load-balancer"
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.tipstaff_dev_lb_sc.id]
+  subnets                    = data.aws_subnets.shared-public.ids
+  enable_deletion_protection = false
+  depends_on                 = [aws_security_group.tipstaff_dev_lb_sc]
+}
 
 # resource "aws_lb_target_group" "tipstaff_dev_target_group" {
 #   name                 = "tipstaff-dev-target-group"
@@ -64,7 +64,7 @@
 #   )
 # }
 
-# # Link target group to the EC2 instance on port 80
+# Link target group to the EC2 instance on port 80
 # resource "aws_lb_target_group_attachment" "tipstaff_ec2_instance_dev" {
 #   target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
 #   target_id        = aws_instance.tipstaff_ec2_instance_dev.id
