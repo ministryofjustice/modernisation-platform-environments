dd02ec3f048f3efb46a46433e859bf252d9f2f5b
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-09 10:22:24 +0000

    rewrote dns settings

--- terraform/environments/tipstaff/dns_ssl.tf
+++ terraform/environments/tipstaff/dns_ssl.tf
@@ -1,47 +1,8 @@
-# resource "aws_acm_certificate" "tipstaff_app_cert" {
-#   domain_name       = "modernisation-platform.service.justice.gov.uk"
-#   validation_method = "DNS"
-#   tags = {
-#     Environment = local.environment
-#   }
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
-# resource "aws_route53_record" "external_validation_tipstaff" {
-#   provider        = aws.core-network-services
-#   allow_overwrite = true
-#   name            = local.tipstaff_domain_name_main[0]
-#   records         = local.tipstaff_domain_record_main
-#   ttl             = 60
-#   type            = local.tipstaff_domain_type_main[0]
-#   zone_id         = data.aws_route53_zone.network-services.zone_id
-# }
-
-# resource "aws_route53_record" "external_validation_subdomain_tipstaff" {
-#   count           = length(local.tipstaff_domain_name_sub)
-#   provider        = aws.core-vpc
-#   allow_overwrite = true
-#   name            = local.tipstaff_domain_name_sub[count.index]
-#   records         = [local.tipstaff_domain_record_sub[count.index]]
-#   ttl             = 60
-#   type            = local.tipstaff_domain_type_sub[count.index]
-#   zone_id         = data.aws_route53_zone.external.zone_id
-# }
-
-# resource "aws_acm_certificate_validation" "tipstaff_lb_cert_validation" {
-#   certificate_arn         = aws_acm_certificate.tipstaff_app_cert.arn
-#   validation_record_fqdns = [for record in local.tipstaff_domain_types : record.name]
-# }
-
-#------------------------------------------------------------------------------
-
-resource "aws_route53_record" "inner" {
+resource "aws_route53_record" "external" {
   provider = aws.core-vpc
 
-  zone_id = data.aws_route53_zone.inner.zone_id
-  name    = "${var.networking[0].application}.${var.networking[0].business-unit}-sandbox.modernisation-platform.internal"
+  zone_id = data.aws_route53_zone.external.zone_id
+  name    = "${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
 
   alias {
@@ -51,38 +12,43 @@ resource "aws_route53_record" "inner" {
   }
 }
 
-resource "aws_acm_certificate" "inner" {
-  domain_name       = "${var.networking[0].business-unit}-sandbox.modernisation-platform.internal"
+resource "aws_acm_certificate" "external" {
+  domain_name       = "modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
-  subject_alternative_names = ["*.${var.networking[0].business-unit}-sandbox.modernisation-platform.internal"]
+  subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   tags = {
-    Environment = "test"
+    Environment = local.environment
   }
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_route53_record" "inner_validation" {
-  provider = aws.core-vpc
-  for_each = {
-    for dvo in aws_acm_certificate.inner.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+resource "aws_route53_record" "external_validation" {
+  provider = aws.core-network-services
 
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
+  name            = local.domain_name_main[0]
+  records         = local.domain_record_main
   ttl             = 60
-  type            = each.value.type
+  type            = local.domain_type_main[0]
+  zone_id         = data.aws_route53_zone.network-services.zone_id
+}
+
+resource "aws_route53_record" "external_validation_subdomain" {
+  provider = aws.core-vpc
+
+  allow_overwrite = true
+  name            = local.domain_name_sub[0]
+  records         = local.domain_record_sub
+  ttl             = 60
+  type            = local.domain_type_sub[0]
   zone_id         = data.aws_route53_zone.external.zone_id
 }
 
-resource "aws_acm_certificate_validation" "tipstaff_lb_cert_validation" {
-  certificate_arn         = aws_acm_certificate.inner.arn
-  validation_record_fqdns = [for record in aws_route53_record.inner_validation : record.fqdn]
+resource "aws_acm_certificate_validation" "external" {
+  certificate_arn         = aws_acm_certificate.external.arn
+  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
 }
--- terraform/environments/tipstaff/locals.tf
+++ terraform/environments/tipstaff/locals.tf
@@ -1,18 +1,18 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
   # create name, record,type for monitoring lb aka tipstaff_dev_lb
-  tipstaff_domain_types = { for dvo in aws_acm_certificate.inner.domain_validation_options : dvo.domain_name => {
+  domain_types = { for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
     name   = dvo.resource_record_name
     record = dvo.resource_record_value
     type   = dvo.resource_record_type
     }
   }
 
-  tipstaff_domain_name_main   = [for k, v in local.tipstaff_domain_types : v.name if k == "modernisation-platform.service.justice.gov.uk"]
-  tipstaff_domain_name_sub    = [for k, v in local.tipstaff_domain_types : v.name if k != "modernisation-platform.service.justice.gov.uk"]
-  tipstaff_domain_record_main = [for k, v in local.tipstaff_domain_types : v.record if k == "modernisation-platform.service.justice.gov.uk"]
-  tipstaff_domain_record_sub  = [for k, v in local.tipstaff_domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
-  tipstaff_domain_type_main   = [for k, v in local.tipstaff_domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
-  tipstaff_domain_type_sub    = [for k, v in local.tipstaff_domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_name_main   = [for k, v in local.domain_types : v.name if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_name_sub    = [for k, v in local.domain_types : v.name if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_record_main = [for k, v in local.domain_types : v.record if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_record_sub  = [for k, v in local.domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_type_main   = [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_type_sub    = [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
 
 }
