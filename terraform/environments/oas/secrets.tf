79e71830326c7b96bdd5ef2868c152c01ec76bfd
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-03-09 15:36:58 +0000

    LAWS-3037: lambda changes for secrets.tf

--- terraform/environments/oas/secrets.tf
+++ terraform/environments/oas/secrets.tf
@@ -3,19 +3,35 @@
 resource "aws_lambda_function" "secrets" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename          = "terraform/environments/oas/secret_rotation.py"
-  function_name     = local.application_data.accounts[local.environment].lambda_function_name
-  # role            = aws_iam_role.iam_for_lambda.arn
-  role_arn          = aws_iam_role.lambda.arn
-  handler           = local.application_data.accounts[local.environment].lambda_handler
-  timeout           = 30
-  runtime           = local.application_data.accounts[local.environment].lambda_runtime
-  source_code_hash  = data.archive_file.lambda.output_base64sha256
+  filename        = "terraform/environments/oas/secret_rotation.py"
+  function_name   = local.application_data.accounts[local.environment].lambda_function_name
+  role            = aws_iam_role.iam_for_lambda.arn
+  # role_arn      = aws_iam_role.lambda.arn
+  handler         = local.application_data.accounts[local.environment].lambda_handler
+  timeout         = 30
+  runtime         = local.application_data.accounts[local.environment].lambda_runtime
+  # source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  module_tags = {
-    Environment = "development"
+  # module_tags = {
+  #   Environment = "development"
+  # }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
   }
 }
 
-
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
 
