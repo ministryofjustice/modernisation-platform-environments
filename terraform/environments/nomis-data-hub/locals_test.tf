f9aff0b62ed03bcddfde40f842fd10da507fe5c9
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-03-09 15:32:11 +0000

    baseline terraform

---
+++ terraform/environments/nomis-data-hub/locals_test.tf
@@ -0,0 +1,38 @@
+locals {
+
+  # remove once software copied from nomis-test
+  test_s3_policies = {
+    NomisTestWriteAccessBucketPolicy = {
+      effect = "Allow"
+      actions = [
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+      ]
+      principals = {
+        type = "AWS"
+        identifiers = [
+          module.environment.account_root_arns["nomis-test"]
+        ]
+      }
+    }
+  }
+
+  test_config = {
+
+    baseline_s3_buckets = {
+
+      # the shared image builder bucket is just created in development
+      nomis-data-hub-software = {
+        custom_kms_key = module.environment.kms_keys["general"].arn
+        bucket_policy_v2 = [
+          module.baseline_presets.s3_bucket_policies.ImageBuilderWriteAccessBucketPolicy,
+          module.baseline_presets.s3_bucket_policies.AllEnvironmentsWriteAccessBucketPolicy,
+          local.test_s3_policies.NomisTestWriteAccessBucketPolicy # remove once software copied from nomis-test
+        ]
+        iam_policies = module.baseline_presets.s3_iam_policies
+      }
+    }
+  }
+}
