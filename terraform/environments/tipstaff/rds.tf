c6a8cb78b58327895e160fdfbd7eee4f2c472302
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-03-09 17:09:51 +0000

    delete rds

--- terraform/environments/tipstaff/rds.tf
+++ terraform/environments/tipstaff/rds.tf
@@ -1,18 +1,18 @@
-resource "aws_db_instance" "tipstaffdbdev" {
-  allocated_storage      = local.application_data.accounts[local.environment].allocated_storage
-  db_name                = local.application_data.accounts[local.environment].db_name
-  storage_type           = local.application_data.accounts[local.environment].storage_type
-  identifier             = local.application_data.accounts[local.environment].identifier
-  engine                 = local.application_data.accounts[local.environment].engine
-  engine_version         = local.application_data.accounts[local.environment].engine_version
-  instance_class         = local.application_data.accounts[local.environment].instance_class
-  username               = jsondecode(data.aws_secretsmanager_secret_version.db_username.secret_string)["LOCAL_DB_USERNAME"]
-  password               = jsondecode(data.aws_secretsmanager_secret_version.db_password.secret_string)["LOCAL_DB_PASSWORD"]
-  skip_final_snapshot    = true
-  publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.postgresql_db_sc.id]
-  # db_subnet_group_name   = aws_db_subnet_group.dbsubnetgroup.name
-}
+# resource "aws_db_instance" "tipstaffdbdev" {
+#   allocated_storage      = local.application_data.accounts[local.environment].allocated_storage
+#   db_name                = local.application_data.accounts[local.environment].db_name
+#   storage_type           = local.application_data.accounts[local.environment].storage_type
+#   identifier             = local.application_data.accounts[local.environment].identifier
+#   engine                 = local.application_data.accounts[local.environment].engine
+#   engine_version         = local.application_data.accounts[local.environment].engine_version
+#   instance_class         = local.application_data.accounts[local.environment].instance_class
+#   username               = jsondecode(data.aws_secretsmanager_secret_version.db_username.secret_string)["LOCAL_DB_USERNAME"]
+#   password               = jsondecode(data.aws_secretsmanager_secret_version.db_password.secret_string)["LOCAL_DB_PASSWORD"]
+#   skip_final_snapshot    = true
+#   publicly_accessible    = true
+#   vpc_security_group_ids = [aws_security_group.postgresql_db_sc.id]
+#   # db_subnet_group_name   = aws_db_subnet_group.dbsubnetgroup.name
+# }
 
 //Not needed??
 # resource "aws_db_subnet_group" "dbsubnetgroup" {
@@ -21,48 +21,48 @@ resource "aws_db_instance" "tipstaffdbdev" {
 # }
 
 //Not needed??
-resource "aws_security_group" "postgresql_db_sc" {
-  name        = "postgres_security_group"
-  description = "control access to the database"
-  # vpc_id      = data.aws_vpc.shared.id
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    description = "MOJ Digital VPN access"
-    cidr_blocks = [local.application_data.accounts[local.environment].moj_ip]
-  }
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    description = "Allows codebuild access to RDS"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    description = "allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_security_group" "postgresql_db_sc" {
+#   name        = "postgres_security_group"
+#   description = "control access to the database"
+#   # vpc_id      = data.aws_vpc.shared.id
+#   ingress {
+#     from_port   = 0
+#     to_port     = 65535
+#     protocol    = "tcp"
+#     description = "MOJ Digital VPN access"
+#     cidr_blocks = [local.application_data.accounts[local.environment].moj_ip]
+#   }
+#   ingress {
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     description = "Allows codebuild access to RDS"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     description = "allow all outbound traffic"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "null_resource" "setup_db" {
-  depends_on = [aws_db_instance.tipstaffdbdev]
+# resource "null_resource" "setup_db" {
+#   depends_on = [aws_db_instance.tipstaffdbdev]
 
-  provisioner "local-exec" {
-    interpreter = ["bash", "-c"]
-    command     = "chmod +x ./setup-postgresql.sh; ./setup-postgresql.sh"
+#   provisioner "local-exec" {
+#     interpreter = ["bash", "-c"]
+#     command     = "chmod +x ./setup-postgresql.sh; ./setup-postgresql.sh"
 
-    environment = {
-      DB_HOSTNAME       = aws_db_instance.tipstaffdbdev.address
-      DB_NAME           = aws_db_instance.tipstaffdbdev.db_name
-      LOCAL_DB_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.db_username.secret_string)["LOCAL_DB_USERNAME"]
-      LOCAL_DB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.db_password.secret_string)["LOCAL_DB_PASSWORD"]
-    }
-  }
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-}
+#     environment = {
+#       DB_HOSTNAME       = aws_db_instance.tipstaffdbdev.address
+#       DB_NAME           = aws_db_instance.tipstaffdbdev.db_name
+#       LOCAL_DB_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.db_username.secret_string)["LOCAL_DB_USERNAME"]
+#       LOCAL_DB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.db_password.secret_string)["LOCAL_DB_PASSWORD"]
+#     }
+#   }
+#   triggers = {
+#     always_run = "${timestamp()}"
+#   }
+# }
