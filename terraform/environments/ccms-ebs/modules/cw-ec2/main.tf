cd29a8fc006a9514e27c7935d2380922fb3d8c1c
martytaggart-laa <119940130+martytaggart-laa@users.noreply.github.com>
2023-03-10 13:38:18 +0000

    Ccms/cw alert fixes (#1608)
    * rebuild with new AMIs
    
    * remove dimension
    
    * Commit changes made by code formatters
    
    * enable ec2 cw-alerts
    
    * remove dimensions for all alerts
    
    * cert fixes
    
    * change to full env name
    
    * alerts
    
    * Commit changes made by code formatters
    
    * replace providers
    
    * change namespace
    
    * temp removal
    
    * temp
    
    * Commit changes made by code formatters
    
    * update to metrics
    
    * fix dimensions
    
    * Commit changes made by code formatters
    
    * dimension fixes
    
    * Commit changes made by code formatters
    
    * module dimension changes
    
    * Commit changes made by code formatters
    
    * update for root
    
    * Commit changes made by code formatters
    
    * include other ec2s
    
    * Commit changes made by code formatters
    
    * cleanup
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/environments/ccms-ebs/modules/cw-ec2/main.tf
+++ terraform/environments/ccms-ebs/modules/cw-ec2/main.tf
@@ -1,7 +1,6 @@
 locals {
-  topic       = var.topic
-  instanceIds = var.instanceIds
-  name        = var.name
+  topic = var.topic
+  name  = var.name
 }
 
 # CPU Utilization
@@ -21,19 +20,22 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
   threshold           = var.threshold
   alarm_actions       = [local.topic]
   dimensions = {
-    instanceId = tostring(local.instanceIds)
+    InstanceId   = var.instanceId
+    ImageId      = var.imageId
+    InstanceType = var.instanceType
   }
 }
 
 # Low Available Memory Alarm
 resource "aws_cloudwatch_metric_alarm" "low_available_memory" {
-  count               = var.metric == "mem" ? 1 : 0
-  alarm_name          = "${local.name}-low_available_memory"
-  alarm_description   = "This metric monitors the amount of available memory. If the amount of available memory is less than 10% for 2 minutes, the alarm will trigger."
-  comparison_operator = "LessThanOrEqualToThreshold"
-  metric_name         = "mem_available_percent"
-  namespace           = "CWAgent"
-  statistic           = "Average"
+  count                     = var.metric == "mem" ? 1 : 0
+  alarm_name                = "${local.name}-low_available_memory"
+  alarm_description         = "This metric monitors the amount of available memory. If the amount of available memory is less than 10% for 2 minutes, the alarm will trigger."
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  metric_name               = "mem_available_percent"
+  namespace                 = "CWAgent"
+  statistic                 = "Average"
+  insufficient_data_actions = []
 
   evaluation_periods  = var.eval_periods
   datapoints_to_alarm = var.eval_periods
@@ -41,19 +43,22 @@ resource "aws_cloudwatch_metric_alarm" "low_available_memory" {
   threshold           = var.threshold
   alarm_actions       = [var.topic]
   dimensions = {
-    instanceId = tostring(local.instanceIds)
+    InstanceId   = var.instanceId
+    ImageId      = var.imageId
+    InstanceType = var.instanceType
   }
 }
 
 # Disk Free Alarm
 resource "aws_cloudwatch_metric_alarm" "disk_free" {
-  count               = var.metric == "disk" ? 1 : 0
-  alarm_name          = "${local.name}-disk_free"
-  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 15% for 2 minutes, the alarm will trigger"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  metric_name         = "disk_free"
-  namespace           = "CWAgent"
-  statistic           = "Average"
+  count                     = var.metric == "disk" ? 1 : 0
+  alarm_name                = "${local.name}-disk_free_root"
+  alarm_description         = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space on root falls below 15% for 2 minutes, the alarm will trigger"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  metric_name               = "disk_free"
+  namespace                 = "CWAgent"
+  statistic                 = "Average"
+  insufficient_data_actions = []
 
   evaluation_periods  = var.eval_periods
   datapoints_to_alarm = var.eval_periods
@@ -61,19 +66,27 @@ resource "aws_cloudwatch_metric_alarm" "disk_free" {
   threshold           = var.threshold
   alarm_actions       = [var.topic]
   dimensions = {
-    instanceId = tostring(local.instanceIds)
+    InstanceId   = var.instanceId
+    ImageId      = var.imageId
+    InstanceType = var.instanceType
+    path         = "/"
+    device       = var.rootDevice
+    fstype       = var.fileSystem
+
   }
 }
 
+/*
 # High CPU IOwait Alarm
 resource "aws_cloudwatch_metric_alarm" "cpu_usage_iowait" {
-  count               = var.metric == "iowait" ? 1 : 0
-  alarm_name          = "${local.name}-cpu_usage_iowait"
-  alarm_description   = "This metric monitors the amount of CPU time spent waiting for I/O to complete. If the average CPU time spent waiting for I/O to complete is greater than 90% for 30 minutes, the alarm will trigger."
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  metric_name         = "cpu_usage_iowait"
-  namespace           = "CWAgent"
-  statistic           = "Average"
+  count                     = var.metric == "iowait" ? 1 : 0
+  alarm_name                = "${local.name}-cpu_usage_iowait"
+  alarm_description         = "This metric monitors the amount of CPU time spent waiting for I/O to complete. If the average CPU time spent waiting for I/O to complete is greater than 90% for 30 minutes, the alarm will trigger."
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "cpu_usage_iowait"
+  namespace                 = "CWAgent"
+  statistic                 = "Average"
+  insufficient_data_actions = []
 
   evaluation_periods  = var.eval_periods
   datapoints_to_alarm = var.eval_periods
@@ -81,11 +94,13 @@ resource "aws_cloudwatch_metric_alarm" "cpu_usage_iowait" {
   threshold           = var.threshold
   alarm_actions       = [var.topic]
   dimensions = {
-    instanceId = tostring(local.instanceIds)
+    InstanceId   = var.instanceId
+    ImageId      = var.imageId
+    InstanceType = var.instanceType
+    cpu          = ""
   }
-
 }
-
+*/
 
 # ==============================================================================
 # EC2 Instance Statuses
@@ -93,38 +108,40 @@ resource "aws_cloudwatch_metric_alarm" "cpu_usage_iowait" {
 
 # Instance Health Alarm
 resource "aws_cloudwatch_metric_alarm" "instance_health_check" {
-  count               = var.metric == "insthc" ? 1 : 0
-  alarm_name          = "${local.name}-instance_health_check_failed"
-  alarm_description   = "Instance status checks monitor the software and network configuration of your individual instance. When an instance status check fails, you typically must address the problem yourself: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/monitoring-system-instance-status-check.html"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  metric_name         = "StatusCheckFailed_Instance"
-  namespace           = "AWS/EC2"
-  statistic           = "Average"
+  count                     = var.metric == "insthc" ? 1 : 0
+  alarm_name                = "${local.name}-instance_health_check_failed"
+  alarm_description         = "Instance status checks monitor the software and network configuration of your individual instance. When an instance status check fails, you typically must address the problem yourself: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/monitoring-system-instance-status-check.html"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "StatusCheckFailed_Instance"
+  namespace                 = "AWS/EC2"
+  statistic                 = "Average"
+  insufficient_data_actions = []
 
   evaluation_periods = var.eval_periods
   period             = var.period
   threshold          = var.threshold
   alarm_actions      = [var.topic]
   dimensions = {
-    instanceId = tostring(local.instanceIds)
+    InstanceId = var.instanceId
   }
 }
 
 # Status Check Alarm
 resource "aws_cloudwatch_metric_alarm" "system_health_check" {
-  count               = var.metric == "syshc" ? 1 : 0
-  alarm_name          = "${local.name}-system_health_check_failed"
-  alarm_description   = "System status checks monitor the AWS systems on which your instance runs. These checks detect underlying problems with your instance that require AWS involvement to repair: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/monitoring-system-instance-status-check.html"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  metric_name         = "StatusCheckFailed_System"
-  namespace           = "AWS/EC2"
-  statistic           = "Average"
+  count                     = var.metric == "syshc" ? 1 : 0
+  alarm_name                = "${local.name}-system_health_check_failed"
+  alarm_description         = "System status checks monitor the AWS systems on which your instance runs. These checks detect underlying problems with your instance that require AWS involvement to repair: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/monitoring-system-instance-status-check.html"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "StatusCheckFailed_System"
+  namespace                 = "AWS/EC2"
+  statistic                 = "Average"
+  insufficient_data_actions = []
 
   evaluation_periods = var.eval_periods
   period             = var.period
   threshold          = var.threshold
   alarm_actions      = [var.topic]
   dimensions = {
-    instanceId = tostring(local.instanceIds)
+    InstanceId = var.instanceId
   }
 }
