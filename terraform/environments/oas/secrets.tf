88e34d0d5dca51ff976c1601f8a24cd194a5600c
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-03-10 10:50:37 +0000

    LAWS-3037: remove all code for Lambda function

--- terraform/environments/oas/secrets.tf
+++ terraform/environments/oas/secrets.tf
@@ -1,34 +1,34 @@
 #### This file can be used to store secrets specific to the member account ####
 
-resource "aws_lambda_function" "secrets" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename        = "${path.module}/secret_rotation.py"
-  function_name   = local.application_data.accounts[local.environment].lambda_function_name
-  role            = aws_iam_role.iam_for_lambda.arn
-  # role_arn      = aws_iam_role.lambda.arn
-  handler         = local.application_data.accounts[local.environment].lambda_handler
-  timeout         = 30
-  runtime         = local.application_data.accounts[local.environment].lambda_runtime
-  # source_code_hash = data.archive_file.lambda.output_base64sha256
+# resource "aws_lambda_function" "secrets" {
+#   # If the file is not in the current working directory you will need to include a
+#   # path.module in the filename.
+#   filename        = "${path.module}/secret_rotation.py"
+#   function_name   = local.application_data.accounts[local.environment].lambda_function_name
+#   role            = aws_iam_role.iam_for_lambda.arn
+#   # role_arn      = aws_iam_role.lambda.arn
+#   handler         = local.application_data.accounts[local.environment].lambda_handler
+#   timeout         = 30
+#   runtime         = local.application_data.accounts[local.environment].lambda_runtime
+#   # source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  # module_tags = {
-  #   Environment = "development"
-  # }
-}
+#   # module_tags = {
+#   #   Environment = "development"
+#   # }
+# }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
+#     }
 
-    actions = ["sts:AssumeRole"]
-  }
-}
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
 
 # resource "aws_iam_instance_profile" "lambda_profile" {
 #   name = "${var.app_name}-lambda-profile"
@@ -41,10 +41,10 @@ data "aws_iam_policy_document" "assume_role" {
 #   )
 # }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name               = "iam_for_lambda"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
 
 # resource "aws_iam_policy" "iam_lambda_policy" { #tfsec:ignore:aws-iam-no-policy-wildcards
 #   name = "iam_for_lambda_policy"
