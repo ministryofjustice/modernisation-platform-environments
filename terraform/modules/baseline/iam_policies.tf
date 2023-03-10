0654d6ce97162fe13278d8d80f4f1477649d2ad5
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-03-10 10:02:08 +0000

    revert sensitive bodge

--- terraform/modules/baseline/iam_policies.tf
+++ terraform/modules/baseline/iam_policies.tf
@@ -3,11 +3,10 @@ locals {
 }
 
 data "aws_iam_policy_document" "this" {
-  # for_each workaround as iam_policies may sometimes contain sensitive values
-  for_each = nonsensitive(sensitive(toset(keys(local.iam_policies))))
+  for_each = local.iam_policies
 
   dynamic "statement" {
-    for_each = local.iam_policies[each.key].statements
+    for_each = each.value.statements
 
     content {
       effect    = statement.value.effect
@@ -36,14 +35,11 @@ data "aws_iam_policy_document" "this" {
 }
 
 resource "aws_iam_policy" "this" {
-  # There's a weird issue where args can flip between sensitive and non-sensitive
-  # value seen in nomis-data-hub accounts only.  Hence the nonsensitive workaround
-  # here.  Looks like a bug so try removing at some point in future.
-  for_each = nonsensitive(sensitive(toset(keys(local.iam_policies))))
+  for_each = local.iam_policies
 
   name        = each.key
-  path        = nonsensitive(sensitive(local.iam_policies[each.key].path))
-  description = nonsensitive(sensitive(local.iam_policies[each.key].description))
+  path        = each.value.path
+  description = each.value.description
   policy      = data.aws_iam_policy_document.this[each.key].json
 
   tags = merge(local.tags, {
--- terraform/modules/baseline/kms.tf
+++ terraform/modules/baseline/kms.tf
@@ -1,13 +1,10 @@
 resource "aws_kms_grant" "this" {
-  # There's a weird issue where args can flip between sensitive and non-sensitive
-  # value seen in nomis-data-hub accounts only.  Hence the nonsensitive workaround
-  # here.  Looks like a bug so try removing at some point in future.
-  for_each = nonsensitive(sensitive(toset(keys(var.kms_grants))))
+  for_each = var.kms_grants
 
   name              = each.key
-  key_id            = nonsensitive(sensitive(var.kms_grants[each.key].key_id))
-  grantee_principal = var.kms_grants[each.key].grantee_principal
-  operations        = nonsensitive(sensitive(var.kms_grants[each.key].operations))
+  key_id            = each.value.key_id
+  grantee_principal = each.value.grantee_principal
+  operations        = each.value.operations
 
   # ensure principals are created first
   depends_on = [
