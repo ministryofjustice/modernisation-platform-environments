a77a843086ba664bffc8f5ed3ab420afe0fafa3c
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-03-13 11:09:44 +0000

    Remove duplicate var declaration

--- terraform/environments/oas/modules/rotate_secrets_lambda/variables.tf
+++ terraform/environments/oas/modules/rotate_secrets_lambda/variables.tf
@@ -23,11 +23,6 @@ variable "database_user" {
   description = "TBC "
 }
 
-variable "lambda_runtime" {
-  type        = string
-  description = "Runtime for the rotate secret lambda function"
-}
-
 variable "log_group_retention_days" {
   type        = string
   description = "CloudWatch Log Group for Lambda function retention in days"
