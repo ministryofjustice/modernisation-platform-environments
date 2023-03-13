7afa33beb883e8f9d2434f20d8611158e280a094
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-13 12:48:10 +0000

    A record name

--- terraform/environments/ccms-ebs/r53.tf
+++ terraform/environments/ccms-ebs/r53.tf
@@ -10,7 +10,7 @@ resource "aws_route53_record" "ebsdb" {
   provider = aws.core-vpc
 
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "ebsdb.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
   ttl     = 300
   records = [aws_instance.ec2_oracle_ebs.private_ip]
