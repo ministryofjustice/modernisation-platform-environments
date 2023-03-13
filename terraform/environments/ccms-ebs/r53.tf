f82a531170bf8b58a76d3f7034a528121c6ce7f4
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-13 13:50:20 +0000

    namechanges

--- terraform/environments/ccms-ebs/r53.tf
+++ terraform/environments/ccms-ebs/r53.tf
@@ -33,24 +33,22 @@ resource "aws_route53_record" "external" {
     evaluate_target_health = true
   }
 }
-
 resource "aws_route53_record" "ebsapps_cname" {
   provider = aws.core-vpc
 
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "ebsapps"
+  name    = "ccms-ebsapps"
   ttl     = "300"
   type    = "CNAME"
   records = [aws_route53_record.external.fqdn]
 }
-
 
 ## EBSDB
 resource "aws_route53_record" "ebsdb" {
   provider = aws.core-vpc
 
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "ebsdb.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "ccms-ebsdb.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
   ttl     = 300
   records = [aws_instance.ec2_oracle_ebs.private_ip]
@@ -60,9 +58,8 @@ resource "aws_route53_record" "ebsdb_cname" {
   provider = aws.core-vpc
 
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "ebsdb"
+  name    = "ccms-ebsdb"
   ttl     = "300"
   type    = "CNAME"
   records = [aws_route53_record.ebsdb.fqdn]
 }
-
