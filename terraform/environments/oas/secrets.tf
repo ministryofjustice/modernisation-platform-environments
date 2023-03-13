0e2d9717fa1a590f243f1d0818aed49b3b33450c
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-03-13 10:08:09 +0000

    Add in missing mismatches from landing zone template

--- terraform/environments/oas/secrets.tf
+++ terraform/environments/oas/secrets.tf
@@ -1,20 +1,32 @@
 #### This file can be used to store secrets specific to the member account ####
 
+# TODO Do we need to add account ids to the lambda policy?
+# TODO Turn this into a module with appropriate variables
+
 resource "aws_lambda_function" "rotate_secrets" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "${path.module}/secret_rotation.zip"
-  function_name = local.application_data.accounts[local.environment].lambda_function_name
-  role          = aws_iam_role.iam_for_lambda.arn
+  function_name = "SecretsRotation"
+  description   = "Secrets Manager password rotation"
+  role          = aws_iam_role.lambda.arn
   # role_arn      = aws_iam_role.lambda.arn
-  handler = local.application_data.accounts[local.environment].lambda_handler
-  timeout = 30
-  runtime = local.application_data.accounts[local.environment].lambda_runtime
-  # source_code_hash = data.archive_file.lambda.output_base64sha256
+  handler = "index.lambda_handler"
+  timeout = 300
+  runtime = "python3.8"
 
-  # module_tags = {
-  #   Environment = "development"
-  # }
+  environment {
+    variables = {
+      databaseName = "lambdadb"
+      databaseUser = "admin"
+      SECRETS_MANAGER_ENDPOINT = "https://secretsmanager.eu-west-2.amazonaws.com"
+    }
+  }
+
+  tags = merge(
+    local.tags,
+    { "Name" = "SecretsRotation" },
+  )
 }
 
 data "archive_file" "lambda" {
@@ -36,13 +48,21 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+resource "aws_iam_role" "lambda" {
+  name               = "${aws_lambda_function.rotate_secrets.function_name}-ExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  tags = merge(
+    local.tags,
+    { "Name" = "${aws_lambda_function.rotate_secrets.function_name}-ExecutionRole" },
+  )
 }
 
-resource "aws_iam_policy" "iam_lambda_policy" { #tfsec:ignore:aws-iam-no-policy-wildcards
-  name   = "iam_lambda_policy"
+resource "aws_iam_policy" "lambda" { #tfsec:ignore:aws-iam-no-policy-wildcards
+  name   = "${aws_lambda_function.rotate_secrets.function_name}-Policy"
+  tags = merge(
+    local.tags,
+    { "Name" = "${aws_lambda_function.rotate_secrets.function_name}-Policy" },
+  )
   policy = jsonencode({
     Version: "2012-10-17"
     Statement: [
@@ -76,6 +96,7 @@ resource "aws_iam_policy" "iam_lambda_policy" { #tfsec:ignore:aws-iam-no-policy-
             Resources = "*"
         },
         {
+            Sid = "GenerateARandomStringToExecuteRotation"
             Effect = "Allow",
             Action = [
                 "secretsmanager:GetRandomPassword"
@@ -88,6 +109,20 @@ resource "aws_iam_policy" "iam_lambda_policy" { #tfsec:ignore:aws-iam-no-policy-
 }
 
 resource "aws_iam_role_policy_attachment" "attach_lambda_policy" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.iam_lambda_policy.arn
+  role       = aws_iam_role.lambda.name
+  policy_arn = aws_iam_policy.lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_secret_manager" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.rotate_secrets.function_name
+  principal     = "secretsmanager.amazonaws.com"
+}
+
+resource "aws_cloudwatch_log_group" "rotate_secrets_lambda" {
+  name = aws_lambda_function.rotate_secrets.function_name
+  retention_in_days = 180
+  lifecycle {
+    prevent_destroy = true
+  }
 }
