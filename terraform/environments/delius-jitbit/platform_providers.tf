519dfe262b6d9bc89b5c2eae868906ec58019463
SteveLinden <stephen.linden@digital.justice.gov.uk>
2023-03-13 14:55:04 +0000

    Adding code to platform_providers.tf for all environments

--- terraform/environments/delius-jitbit/platform_providers.tf
+++ terraform/environments/delius-jitbit/platform_providers.tf
@@ -38,6 +38,14 @@ provider "aws" {
     role_arn = "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/modify-dns-records"
   }
 }
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/MemberInfrastructureAccessUSEast"
+  }
+}
 ######################### Run Terraform via CICD ##################################
 
 
