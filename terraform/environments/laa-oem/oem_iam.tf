f4ba56713b8af7cf4bffb2c9db6e35fab958d2a4
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-03-14 08:35:35 +0000

    CC-1621: Change in IAM resources 'oracle' -> 'oem'.

--- terraform/environments/laa-oem/oem_iam.tf
+++ terraform/environments/laa-oem/oem_iam.tf
@@ -1,4 +1,3 @@
-## SSM Specific
 resource "aws_iam_policy" "ec2_ssm_policy" {
   name        = "ssm_ec2_policy-${local.environment}"
   description = "allows SSM Connect logging"
@@ -48,14 +47,8 @@ resource "aws_iam_policy" "ec2_ssm_policy" {
   )
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_logging_oracle_base" {
-  role       = aws_iam_role.role_stsassume_oracle_base.name
-  policy_arn = aws_iam_policy.ec2_ssm_policy.arn
-}
-
-## Oracle EC2 Policies
-resource "aws_iam_role" "role_stsassume_oracle_base" {
-  name                 = "role_stsassume_oracle_base"
+resource "aws_iam_role" "role_stsassume_oem_base" {
+  name                 = "role_stsassume_oem_base"
   path                 = "/"
   max_session_duration = "3600"
   assume_role_policy = jsonencode(
@@ -74,30 +67,35 @@ resource "aws_iam_role" "role_stsassume_oracle_base" {
     }
   )
   tags = merge(local.tags,
-    { Name = lower(format("RoleSsm-%s-%s-OracleBase", local.application_name, local.environment)) }
+    { Name = lower(format("RoleSsm-%s-%s-OEM-Base", local.application_name, local.environment)) }
   )
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_policy_oracle_base" {
-  role       = aws_iam_role.role_stsassume_oracle_base.name
+resource "aws_iam_role_policy_attachment" "ssm_logging_oem_base" {
+  role       = aws_iam_role.role_stsassume_oem_base.name
+  policy_arn = aws_iam_policy.ec2_ssm_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_oem_base" {
+  role       = aws_iam_role.role_stsassume_oem_base.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_role_policy_attachment" "secrets_manager_policy_oracle_base" {
-  role       = aws_iam_role.role_stsassume_oracle_base.name
+resource "aws_iam_role_policy_attachment" "secrets_manager_policy_oem_base" {
+  role       = aws_iam_role.role_stsassume_oem_base.name
   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_readonly_policy_oracle_base" {
-  role       = aws_iam_role.role_stsassume_oracle_base.name
+resource "aws_iam_role_policy_attachment" "ec2_readonly_policy_oem_base" {
+  role       = aws_iam_role.role_stsassume_oem_base.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
 resource "aws_iam_instance_profile" "iam_instace_profile_ccms_base" {
   name = "iam_instace_profile_ccms_base"
-  role = aws_iam_role.role_stsassume_oracle_base.name
+  role = aws_iam_role.role_stsassume_oem_base.name
   path = "/"
   tags = merge(local.tags,
-    { Name = lower(format("IamProfile-%s-%s-OracleBase", local.application_name, local.environment)) }
+    { Name = lower(format("IamProfile-%s-%s-OEM-Base", local.application_name, local.environment)) }
   )
 }
