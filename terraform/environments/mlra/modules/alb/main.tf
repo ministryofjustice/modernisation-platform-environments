fccbdbfe6e8b08db0857877de3a8ece5c8235acc
mikereiddigital <mike.reid@digital.justice.gov.uk>
2023-03-14 10:05:30 +0000

    Removes the cloudfront secrets manager resource for now.

--- terraform/environments/mlra/modules/alb/main.tf
+++ terraform/environments/mlra/modules/alb/main.tf
@@ -220,31 +220,33 @@ resource "aws_security_group" "lb" {
 }
 
 ## Cloudfront
+## - Note this is commented out because of an issue with an MP apply from main that deleted the original entry
+## - it cannot be recreated for 7 days following 14th March if the resource is to have the SAME AWS NAME. 
 
-resource "random_password" "cloudfront" {
-  length  = 16
-  special = false
-}
+# resource "random_password" "cloudfront" {
+#   length  = 16
+#   special = false
+# }
 
-resource "aws_secretsmanager_secret" "cloudfront" {
-  name        = "cloudfront-secret-${var.application_name}"
-  description = "Simple secret created by AWS CloudFormation to be shared between ALB and CloudFront"
-}
+# resource "aws_secretsmanager_secret" "cloudfront" {
+#   name        = "cloudfront-secret-${var.application_name}"
+#   description = "Simple secret created by AWS CloudFormation to be shared between ALB and CloudFront"
+# }
 
-resource "aws_secretsmanager_secret_version" "cloudfront" {
-  secret_id     = aws_secretsmanager_secret.cloudfront.id
-  secret_string = random_password.cloudfront.result
-}
+# resource "aws_secretsmanager_secret_version" "cloudfront" {
+#   secret_id     = aws_secretsmanager_secret.cloudfront.id
+#   secret_string = random_password.cloudfront.result
+# }
 
-# Importing the AWS secrets created previously using arn.
-data "aws_secretsmanager_secret" "cloudfront" {
-  arn = aws_secretsmanager_secret.cloudfront.arn
-}
+# # Importing the AWS secrets created previously using arn.
+# data "aws_secretsmanager_secret" "cloudfront" {
+#   arn = aws_secretsmanager_secret.cloudfront.arn
+# }
 
-# Importing the AWS secret version created previously using arn.
-data "aws_secretsmanager_secret_version" "cloudfront" {
-  secret_id = data.aws_secretsmanager_secret.cloudfront.arn
-}
+# # Importing the AWS secret version created previously using arn.
+# data "aws_secretsmanager_secret_version" "cloudfront" {
+#   secret_id = data.aws_secretsmanager_secret.cloudfront.arn
+# }
 
 # resource "aws_acm_certificate" "cloudfront" {
 #   domain_name       = var.acm_cert_domain_name
