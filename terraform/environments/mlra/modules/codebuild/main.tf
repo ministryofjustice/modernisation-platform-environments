bf48b5ff489092acf549429f45372106d64b918c
mikereiddigital <mike.reid@digital.justice.gov.uk>
2023-03-14 13:04:59 +0000

    Remove source version.

--- terraform/environments/mlra/modules/codebuild/main.tf
+++ terraform/environments/mlra/modules/codebuild/main.tf
@@ -190,7 +190,6 @@ resource "aws_codebuild_project" "app-build" {
   build_timeout = 20
   # encryption_key = aws_kms_key.codebuild.arn
   service_role   = aws_iam_role.codebuild_s3.arn
-  source_version = "LAWS-3074-gha"
 
   artifacts {
     type = "NO_ARTIFACTS"
