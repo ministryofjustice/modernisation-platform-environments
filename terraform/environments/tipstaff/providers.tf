283c80f6d9098aff99adba09af5c70910102b242
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-14 11:29:51 +0000

    add assumerole to eu-west-1 provider

--- terraform/environments/tipstaff/providers.tf
+++ terraform/environments/tipstaff/providers.tf
@@ -1,4 +1,7 @@
 provider "aws" {
   region = "eu-west-1"
   alias  = "ireland_provider"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.modernisation_platform_account_id}:role/modernisation-account-limited-read-member-access"
+  }
 }
