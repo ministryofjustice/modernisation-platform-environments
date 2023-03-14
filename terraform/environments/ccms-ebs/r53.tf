d8355d72f0c17a465e3e99fb8ca49d4f8976186f
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-14 11:57:48 +0000

    fix name typo

--- terraform/environments/ccms-ebs/r53.tf
+++ terraform/environments/ccms-ebs/r53.tf
@@ -72,7 +72,7 @@ resource "aws_route53_record" "ebsapps" {
   count    = local.application_data.accounts[local.environment].ebsapps_no_instances
 
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "wgate${local.application_data.accounts[local.environment].short_env}${count.index + 1}.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "ccms-ebs-app${count.index + 1}.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
   ttl     = 300
   records = [aws_instance.ec2_ebsapps[count.index].private_ip]
