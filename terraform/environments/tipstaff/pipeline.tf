fc01db30c3b44d0dfaa54da6e8602f74108e9ff6
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-15 17:20:32 +0000

    move codedeploy app back to Ireland

--- terraform/environments/tipstaff/pipeline.tf
+++ terraform/environments/tipstaff/pipeline.tf
@@ -195,12 +195,12 @@ resource "aws_iam_role_policy" "codebuild_role_policy" {
 // Create CodeDeploy app and deployment group
 
 resource "aws_codedeploy_app" "tipstaff_codedeploy" {
-  # provider = aws.ireland_provider
+  provider = aws.ireland_provider
   name = "tipstaff-codedeploy"
 }
 
 resource "aws_codedeploy_deployment_group" "tipstaff_deployment_group" {
-  # provider              = aws.ireland_provider
+  provider              = aws.ireland_provider
   app_name              = aws_codedeploy_app.tipstaff_codedeploy.name
   deployment_group_name = "tipstaff-deployment-group"
   service_role_arn      = aws_iam_role.codedeploy_role.arn
