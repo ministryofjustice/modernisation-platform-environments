8ea72bb9582a502c6e39d559c6a13add9cfc2c5c
martytaggart-laa <119940130+martytaggart-laa@users.noreply.github.com>
2023-03-16 15:32:49 +0000

    Ccms/cert fixes (#1689)
    * rebuild with new AMIs
    
    * whitespace
    
    * change cert
    
    * Commit changes made by code formatters
    
    * revert
    
    * Commit changes made by code formatters
    
    * move LB to private
    
    * move LB to private
    
    * move internal
    
    * Commit changes made by code formatters
    
    * add app vars
    
    * Commit changes made by code formatters
    
    * remove dns for old LB
    
    * add dns for new LB
    
    * fix cert name
    
    * remove
    
    * rename cert
    
    * cert changes
    
    * redo
    
    * Commit changes made by code formatters
    
    * redo cert route
    
    * Commit changes made by code formatters
    
    * remove loadbalancer - temp
    
    * try SAN for cert
    
    * Commit changes made by code formatters
    
    * change provider
    
    * Commit changes made by code formatters
    
    * change route
    
    * Commit changes made by code formatters
    
    * put lb back in
    
    * remove san
    
    * Commit changes made by code formatters
    
    * bump
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -1,15 +1,13 @@
+######################################################################
+#   *.laa-development.modernisation-platform.service.justice.gov.uk
+######################################################################
+resource "aws_acm_certificate" "external" {
+  count = local.is-production ? 0 : 1
 
-
-################################################################
-#   *.modernisation-platform.service.justice.gov.uk
-################################################################
-resource "aws_acm_certificate" "external-mp" {
-  count       = local.is-production ? 0 : 1
-  domain_name = "*.modernisation-platform.service.justice.gov.uk"
-  #domain_name       = "*.${local.application_data.accounts[local.environment].dns}.modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
-  #subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-  #######  ccms-ebs.laa-development.modernisation-platform.service.justice.gov.uk
+  domain_name       = "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  #subject_alternative_names = ["*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+
   tags = merge(local.tags,
     { Environment = local.environment }
   )
@@ -19,20 +17,43 @@ resource "aws_acm_certificate" "external-mp" {
   }
 }
 
-resource "aws_acm_certificate_validation" "external-mp" {
+resource "aws_acm_certificate_validation" "external" {
+  count = local.is-production ? 0 : 1
   depends_on = [
-    aws_route53_record.external-mp
+    aws_route53_record.external_validation
   ]
-  certificate_arn         = aws_acm_certificate.external-mp[0].arn
-  validation_record_fqdns = [for record in aws_route53_record.external-mp : record.fqdn]
+  certificate_arn         = aws_acm_certificate.external[0].arn
+  validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
 }
+
+resource "aws_route53_record" "external_validation" {
+
+  depends_on = [
+    aws_acm_certificate.external
+  ]
+  provider = aws.core-vpc
+  for_each = {
+    for dvo in aws_acm_certificate.external[0].domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.external.zone_id
+}
+
 /*
 ################################################################
 #   *.service.justice.gov.uk
 ################################################################
 
 resource "aws_acm_certificate" "external-service" {
-  count             = local.is-production ? 0 : 1
+  count             = local.is-production ? 1 : 0
   domain_name       = "*.service.justice.gov.uk"
   validation_method = "DNS"
   #subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
@@ -64,7 +85,7 @@ resource "aws_route53_record" "external-service-validation" {
   ttl             = 60
   type            = each.value.type
   #zone_id         = data.aws_route53_zone.external.zone_id
-  zone_id = data.aws_route53_zone.network-services.zone_id
+  zone_id         = data.aws_route53_zone.network-services.zone_id
 }
 
 resource "aws_acm_certificate_validation" "external-servvice-validation" {
