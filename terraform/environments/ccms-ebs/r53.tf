5c0523597462389eb8a39b47c61df5b52ca11f30
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-16 13:14:38 +0000

    remove san

--- terraform/environments/ccms-ebs/r53.tf
+++ terraform/environments/ccms-ebs/r53.tf
@@ -1,26 +1,3 @@
-/*
-## CERT
-resource "aws_route53_record" "external_validation" {
-  depends_on = [
-    aws_acm_certificate.external
-  ]
-  provider = aws.core-network-services
-  for_each = {
-    for dvo in aws_acm_certificate.external[0].domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.network-services.zone_id
-}
-*/
-
 ## LOADBALANCER
 resource "aws_route53_record" "external" {
   provider = aws.core-vpc
