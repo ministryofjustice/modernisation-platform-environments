d2eae65b07cc818cfed9f47b93a5293ce5bd6544
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-16 11:58:33 +0000

    try SAN for cert

--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -7,9 +7,11 @@ resource "aws_acm_certificate" "external" {
   count = local.is-production ? 0 : 1
 
   validation_method = "DNS"
-  domain_name       = "*.laa-${local.environment}.modernisation-platform.service.justice.gov.uk"
-  #domain_name = "modernisation-platform.service.justice.gov.uk"
+  #domain_name       = "*.laa-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  #domain_name = "*.modernisation-platform.service.justice.gov.uk"
   #subject_alternative_names = ["*.modernisation-platform.service.justice.gov.uk"]
+  domain_name       = "modernisation-platform.service.justice.gov.uk"
+  subject_alternative_names = ["*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
 
   tags = merge(local.tags,
     { Environment = local.environment }
