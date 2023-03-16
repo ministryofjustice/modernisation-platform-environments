69e87c9e5bd4fffe2c562280e482501f53c47e35
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-03-16 15:31:04 +0000

    CC-1621: Fixed typos.

--- terraform/environments/laa-oem/oem_ec2_app.tf
+++ terraform/environments/laa-oem/oem_ec2_app.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "oem_app" {
   subnet_id                   = data.aws_subnet.data_subnets_b.id
   user_data_replace_on_change = true
   user_data = base64encode(templatefile("./templates/oem-user-data-app.sh", {
-    efs_fqdn = aws_efs_file_system.oem-app-efs.dns_name
+    efs_fqdn = aws_efs_file_system.oem_app_efs.dns_name
     env_fqdn = "${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
     hostname = "laa-oem-app"
   }))
