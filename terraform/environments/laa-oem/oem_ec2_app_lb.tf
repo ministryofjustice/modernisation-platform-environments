871744b6840a203a17f3578f4050acce2678a13a
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-03-17 16:37:51 +0000

    CC-1621: Added LB:4903.

--- terraform/environments/laa-oem/oem_ec2_app_lb.tf
+++ terraform/environments/laa-oem/oem_ec2_app_lb.tf
@@ -32,6 +32,30 @@ resource "aws_lb_target_group" "oem_app" {
   }
 }
 
+resource "aws_lb_target_group" "oem_app_4903" {
+  name        = "tg-${local.application_name}-app-4903"
+  port        = 4903
+  protocol    = "HTTPS"
+  vpc_id      = data.aws_vpc.shared.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/empbs/upload"
+    healthy_threshold   = "5"
+    interval            = "60"
+    protocol            = "HTTPS"
+    unhealthy_threshold = "2"
+    matcher             = "200"
+    timeout             = "5"
+  }
+
+  tags = local.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_lb_target_group" "oem_app_7102" {
   name        = "tg-${local.application_name}-app-7102"
   port        = 7102
@@ -86,6 +110,12 @@ resource "aws_lb_target_group_attachment" "oem_app" {
   port             = 8000
 }
 
+resource "aws_lb_target_group_attachment" "oem_app_4903" {
+  target_group_arn = aws_lb_target_group.oem_app_4903.arn
+  target_id        = aws_instance.oem_app.id
+  port             = 4903
+}
+
 resource "aws_lb_target_group_attachment" "oem_app_7102" {
   target_group_arn = aws_lb_target_group.oem_app_7102.arn
   target_id        = aws_instance.oem_app.id
@@ -108,6 +138,20 @@ resource "aws_lb_listener" "oem_app" {
 
   default_action {
     target_group_arn = aws_lb_target_group.oem_app.id
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "oem_app_4903" {
+  load_balancer_arn = aws_lb.oem_app.id
+  port              = 4903
+  protocol          = "HTTPS"
+
+  ssl_policy      = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn = aws_acm_certificate.external-mp[0].arn
+
+  default_action {
+    target_group_arn = aws_lb_target_group.oem_app_4903.id
     type             = "forward"
   }
 }
@@ -153,6 +197,13 @@ resource "aws_security_group" "load_balancer_security_group" {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
+    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_lz_workspaces]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 4903
+    to_port     = 4903
     cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_lz_workspaces]
   }
 
