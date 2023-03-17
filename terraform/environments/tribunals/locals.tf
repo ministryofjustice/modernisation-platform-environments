89cee0ef7b9b643d679f4b45c88595436a05a446
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-03-17 15:09:02 +0000

    unused locals removed

--- terraform/environments/tribunals/locals.tf
+++ terraform/environments/tribunals/locals.tf
@@ -1,18 +1,1 @@
 #### This file can be used to store locals specific to the member account ####
-locals {
-  # create name, record,type for monitoring lb aka tribunals_dev_lb
-  domain_types = { for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-    }
-  }
-
-  domain_name_main   = [for k, v in local.domain_types : v.name if k == "modernisation-platform.service.justice.gov.uk"]
-  domain_name_sub    = [for k, v in local.domain_types : v.name if k != "modernisation-platform.service.justice.gov.uk"]
-  domain_record_main = [for k, v in local.domain_types : v.record if k == "modernisation-platform.service.justice.gov.uk"]
-  domain_record_sub  = [for k, v in local.domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
-  domain_type_main   = [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
-  domain_type_sub    = [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
-
-}
