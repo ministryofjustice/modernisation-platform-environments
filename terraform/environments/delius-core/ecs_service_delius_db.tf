8dad94d7d11e8c9456fe46bb81888cbfd1d3d72b
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-03-17 17:01:47 +0000

    correct specification for all ports for SG

--- terraform/environments/delius-core/ecs_service_delius_db.tf
+++ terraform/environments/delius-core/ecs_service_delius_db.tf
@@ -212,7 +212,7 @@ resource "aws_vpc_security_group_egress_rule" "delius_db_security_group_egress_i
   security_group_id = aws_security_group.delius_db_security_group.id
   description       = "outbound from the testing db ecs service"
   from_port         = 0
-  to_port           = 0
+  to_port           = 65535
   ip_protocol       = "-1"
   cidr_ipv4         = "0.0.0.0/0"
 }
