29a9610f8f6b24589e456f138f5fc19901899510
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-20 09:52:42 +0000

    add pipeline back in

--- terraform/environments/tipstaff/pipeline.tf
+++ terraform/environments/tipstaff/pipeline.tf
@@ -2,83 +2,83 @@ data "github_repository" "my_repo" {
   full_name = "ministryofjustice/Tipstaff"
 }
 
-# resource "aws_codepipeline" "codepipeline" {
-#   depends_on = [
-#     aws_codedeploy_app.tipstaff_codedeploy
-#   ]
-#   provider = aws.eu-west-1
-#   name     = "tf_tipstaff_pipeline"
-#   role_arn = aws_iam_role.codepipeline_role.arn
+resource "aws_codepipeline" "codepipeline" {
+  depends_on = [
+    aws_codedeploy_app.tipstaff_codedeploy
+  ]
+  provider = aws.eu-west-1
+  name     = "tf_tipstaff_pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
 
-#   artifact_store {
-#     location = aws_s3_bucket.pipeline-s3-eu-west-1.bucket
-#     type     = "S3"
-#     region   = "eu-west-1"
-#   }
+  artifact_store {
+    location = aws_s3_bucket.pipeline-s3-eu-west-1.bucket
+    type     = "S3"
+    region   = "eu-west-1"
+  }
 
-#   artifact_store {
-#     location = aws_s3_bucket.pipeline-s3-eu-west-2.bucket
-#     type     = "S3"
-#     region   = "eu-west-2"
-#   }
+  artifact_store {
+    location = aws_s3_bucket.pipeline-s3-eu-west-2.bucket
+    type     = "S3"
+    region   = "eu-west-2"
+  }
 
-#   stage {
-#     name = "Source"
+  stage {
+    name = "Source"
 
-#     action {
-#       name             = "Source"
-#       category         = "Source"
-#       owner            = "ThirdParty"
-#       provider         = "GitHub"
-#       version          = "1"
-#       output_artifacts = ["source_output"]
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
 
-#       configuration = {
-#         Owner      = "ministryofjustice"
-#         Repo       = "Tipstaff"
-#         Branch     = "terraform-build"
-#         OAuthToken = jsondecode(data.aws_secretsmanager_secret_version.oauth_token.secret_string)["OAUTH_TOKEN"]
-#       }
-#     }
-#   }
+      configuration = {
+        Owner      = "ministryofjustice"
+        Repo       = "Tipstaff"
+        Branch     = "terraform-build"
+        OAuthToken = jsondecode(data.aws_secretsmanager_secret_version.oauth_token.secret_string)["OAUTH_TOKEN"]
+      }
+    }
+  }
 
-#   stage {
-#     name = "Build"
+  stage {
+    name = "Build"
 
-#     action {
-#       name             = "Build"
-#       category         = "Build"
-#       owner            = "AWS"
-#       provider         = "CodeBuild"
-#       input_artifacts  = ["source_output"]
-#       output_artifacts = ["build_output"]
-#       version          = "1"
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
 
-#       configuration = {
-#         ProjectName = "my-dotnet-build-project"
-#       }
-#     }
-#   }
+      configuration = {
+        ProjectName = "my-dotnet-build-project"
+      }
+    }
+  }
 
-#   stage {
-#     name = "Deploy"
+  stage {
+    name = "Deploy"
 
-#     action {
-#       name            = "Deploy"
-#       category        = "Deploy"
-#       owner           = "AWS"
-#       provider        = "CodeDeploy"
-#       input_artifacts = ["build_output"]
-#       version         = "1"
-#       region          = "eu-west-2"
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeploy"
+      input_artifacts = ["build_output"]
+      version         = "1"
+      region          = "eu-west-2"
 
-#       configuration = {
-#         ApplicationName     = "tipstaff-codedeploy"
-#         DeploymentGroupName = "tipstaff-deployment-group"
-#       }
-#     }
-#   }
-# }
+      configuration = {
+        ApplicationName     = "tipstaff-codedeploy"
+        DeploymentGroupName = "tipstaff-deployment-group"
+      }
+    }
+  }
+}
 
 resource "aws_s3_bucket" "pipeline-s3-eu-west-1" {
   provider = aws.eu-west-1
@@ -213,28 +213,28 @@ resource "aws_codedeploy_app" "tipstaff_codedeploy" {
   name = "tipstaff-codedeploy"
 }
 
-# resource "aws_codedeploy_deployment_group" "tipstaff_deployment_group" {
-# depends_on = [
-#   aws_codedeploy_app.tipstaff_codedeploy
-# ]
-#   app_name              = aws_codedeploy_app.tipstaff_codedeploy.name
-#   deployment_group_name = "tipstaff-deployment-group"
-#   service_role_arn      = aws_iam_role.codedeploy_role.arn
+resource "aws_codedeploy_deployment_group" "tipstaff_deployment_group" {
+  depends_on = [
+    aws_codedeploy_app.tipstaff_codedeploy
+  ]
+  app_name              = aws_codedeploy_app.tipstaff_codedeploy.name
+  deployment_group_name = "tipstaff-deployment-group"
+  service_role_arn      = aws_iam_role.codedeploy_role.arn
 
-#   auto_rollback_configuration {
-#     enabled = false
-#     events  = ["DEPLOYMENT_FAILURE"]
-#   }
+  auto_rollback_configuration {
+    enabled = false
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
 
-#   ec2_tag_set {
-#     ec2_tag_filter {
-#       key   = "Name"
-#       type  = "KEY_AND_VALUE"
-#       value = aws_instance.tipstaff_ec2_instance.tags["Name"]
-#     }
-#   }
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "Name"
+      type  = "KEY_AND_VALUE"
+      value = aws_instance.tipstaff_ec2_instance.tags["Name"]
+    }
+  }
 
-# }
+}
 
 resource "aws_iam_role" "codedeploy_role" {
   name = "CodeDeployRole"
