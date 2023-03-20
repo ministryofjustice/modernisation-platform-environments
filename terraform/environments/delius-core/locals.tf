0ee7acafd71854c5df282872528b2f948a08f921
George Taylor <george.taylor@digital.justice.gov.uk>
2023-03-20 15:29:28 +0000

    update tag

--- terraform/environments/delius-core/locals.tf
+++ terraform/environments/delius-core/locals.tf
@@ -7,6 +7,6 @@ locals {
 
   frontend_service_name         = "testing-frontend"
   frontend_fully_qualified_name = "${local.application_name}-${local.frontend_service_name}"
-  frontend_image_tag            = "5.7.4"
+  frontend_image_tag            = "5.7.6"
   frontend_container_port       = 7001
 }
