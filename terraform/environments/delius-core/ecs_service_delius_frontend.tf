a483774febba7a0943755b6b07828405079386af
George Taylor <george.taylor@digital.justice.gov.uk>
2023-03-20 13:13:52 +0000

    decode

--- terraform/environments/delius-core/ecs_service_delius_frontend.tf
+++ terraform/environments/delius-core/ecs_service_delius_frontend.tf
@@ -171,7 +171,7 @@ resource "aws_ecs_task_definition" "delius_core_frontend_task_definition" {
         ]
         readonlyRootFilesystem = false
         volumesFrom            = []
-        environment            = data.aws_ssm_parameter.delius_core_frontend_envs.value
+        environment            = jsondecode(data.aws_ssm_parameter.delius_core_frontend_envs.value)
       }
   ])
   cpu = "1024"
