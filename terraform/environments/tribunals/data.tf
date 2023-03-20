93b50ec91229df3c4c1fa7af1bb10ff1eb588d39
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-03-20 16:47:46 +0000

    rds db name changed

--- terraform/environments/tribunals/data.tf
+++ terraform/environments/tribunals/data.tf
@@ -1,6 +1,6 @@
 #### This file can be used to store data specific to the member account ####
 data "aws_db_instance" "database" {
-  db_instance_identifier = "tf-tribunals-dev"
+  db_instance_identifier = "tribunals-db-dev"
 }
 
 data "aws_secretsmanager_secret" "rds-secrets" {
