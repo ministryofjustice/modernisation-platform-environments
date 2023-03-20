55f082e63f9a72d70c57ecfb47c9e7233384d5c6
George Taylor <george.taylor@digital.justice.gov.uk>
2023-03-20 13:35:15 +0000

    for iterator

--- terraform/environments/delius-core/ecs_service_delius_frontend.tf
+++ terraform/environments/delius-core/ecs_service_delius_frontend.tf
@@ -172,14 +172,7 @@ resource "aws_ecs_task_definition" "delius_core_frontend_task_definition" {
         readonlyRootFilesystem = false
         volumesFrom            = []
 
-        // loop over locals.frontend_envs_json and create a list of environment variables
-        environment = [
-          {
-            for_each = jsondecode(data.aws_ssm_parameter.delius_core_frontend_envs.value)
-            name     = each.key
-            value    = each.value
-          }
-        ]
+        environment = [for key, value in jsondecode(data.aws_ssm_parameter.delius_core_frontend_envs.value) : { name = key, value = value }]
       }
   ])
   cpu = "1024"
