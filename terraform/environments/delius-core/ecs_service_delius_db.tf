b21e1ad26b2eb8a66502d07d8e3493973ff69d45
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-03-21 11:48:49 +0000

    Adding service discovery but commenting out pending permissions

--- terraform/environments/delius-core/ecs_service_delius_db.tf
+++ terraform/environments/delius-core/ecs_service_delius_db.tf
@@ -214,6 +214,22 @@ resource "aws_cloudwatch_log_group" "delius_db_log_group" {
   tags              = local.tags
 }
 
+# Pre-req - service discovery
+# Commenting out until permissions have been changed to support this resource
+# resource "aws_service_discovery_service" "delius_db_service" {
+#   name = local.db_service_name
+#   tags = local.tags
+
+#   dns_config {
+#     namespace_id   = aws_service_discovery_private_dns_namespace.ecs_cluster_namespace.id
+#     routing_policy = "MULTIVALUE"
+#     dns_records {
+#       ttl  = 30
+#       type = "A"
+#     }
+#   }
+# }
+
 # Create the ECS service
 resource "aws_ecs_service" "delius-db-service" {
   cluster         = aws_ecs_cluster.aws_ecs_cluster.id
@@ -224,6 +240,10 @@ resource "aws_ecs_service" "delius-db-service" {
     subnets          = data.aws_subnets.private-public.ids
     security_groups  = [aws_security_group.delius_db_security_group.id]
   }
+  # Commenting out until permissions have been changed to support service discovery
+  # service_registries {
+  #   registry_arn = aws_service_discovery_service.delius_db_service.arn
+  # }
   desired_count                      = 1
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
