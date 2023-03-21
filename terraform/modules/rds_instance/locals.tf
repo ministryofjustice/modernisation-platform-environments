d7e5715303d9d4edeb40949986ba7fd7221d1f49
Craig Gilbertson <craig.gilbertson@digital.justice.gov.uk>
2023-03-21 00:20:03 +0000

    Final db instance module build v1 for baseline

--- terraform/modules/rds_instance/locals.tf
+++ terraform/modules/rds_instance/locals.tf
@@ -3,27 +3,4 @@ locals {
     instance-name = var.instance.db_name
   }
   tags = merge(local.default_tags, var.tags)
-
-  public_cidr_block      = [data.terraform_remote_state.common.outputs.db_cidr_block]
-  private_cidr_block     = [data.terraform_remote_state.common.outputs.private_cidr_block]
-  db_cidr_block          = [data.terraform_remote_state.common.outputs.db_cidr_block]
-  private_subnet_map     = data.terraform_remote_state.common.outputs.private_subnet_map
-  db_subnet_ids          = data.terraform_remote_state.common.outputs.db_subnet_ids
-
-  security_group_ids = [
-    data.terraform_remote_state.security-groups.outputs.security_groups_sg_rds_id,
-    data.terraform_remote_state.security-groups.outputs.security_groups_sg_delius_db,
-  ]
-
-  family               = var.rds_family
-  major_engine_version = var.rds_major_engine_version
-  engine               = var.rds_engine
-  engine_version       = var.rds_engine_version
-  instance_class       = var.rds_instance_class
-  allocated_storage    = var.rds_allocated_storage
-  character_set_name   = var.rds_character_set_name
-
-  enabled_cloudwatch_logs_exports = ["alert", "audit", "listener", "trace"]
-
-  multi_az = var.multi_az
 }
--- terraform/modules/rds_instance/main.tf
+++ terraform/modules/rds_instance/main.tf
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 
 resource "aws_db_instance" "this" {
-  identifier = var.instance.identifier
+  identifier = var.identifier
 
   engine            = var.instance.engine
   engine_version    = var.instance.engine_version
@@ -49,14 +49,14 @@ resource "aws_db_instance" "this" {
 
   character_set_name = var.instance.character_set_name
 
-  tags = merge(var.tags, map("Name", format("%s", var.identifier)))
- 
-  enabled_cloudwatch_logs_exports = ["alert", "audit", "listener", "trace"]
+  tags = merge(var.tags, map("Name", format("%s", var.instance.identifier)))
+
+  enabled_cloudwatch_logs_exports = var.instance.enabled_cloudwatch_logs_exports
 }
 
 resource "aws_db_instance_automated_backups_replication" "this" {
   source_db_instance_arn = aws_db_instance.this.arn
-  retention_period       = var.db_instance_automated_backups_replication
+  retention_period       = var.instance_automated_backups_replication
 }
 
 #------------------------------------------------------------------------------
@@ -64,26 +64,14 @@ resource "aws_db_instance_automated_backups_replication" "this" {
 #------------------------------------------------------------------------------
 
 resource "aws_db_option_group" "this" {
-  name                     = var.db_option_group.name
-  option_group_description = var.db_option_group.description
-  engine_name              = var.db_option_group.engine_name
-  major_engine_version     = var.db_option_group.major_engine_version
+  count = var.option_group.create ? 1 : 0
 
-  dynamic "option" {
-    for_each = var.db_option_group.options
-
-    content {
-      option_name = db_option_group.options.value["name"]
-
-      dynamic "option_settings" {
-        for_each = db_option_group.options.value["settings"]
-        content {
-          name  = settings.value["name"]
-          value = settings.value["value"]
-        }
-      }
-    }
-  }
+  name_prefix              = var.option_group.name_prefix
+  option_group_description = var.option_group.description == "" ? format("Option group for %s", var.identifier) : var.option_group.description
+  engine_name              = var.option_group.engine_name
+  major_engine_version     = var.option_group.major_engine_version
+  option                   = var.option_group.options
+  tags                     = merge(var.option_group.tags, map("Name", format("%s", var.identifier)))
 }
 
 #------------------------------------------------------------------------------
@@ -91,52 +79,29 @@ resource "aws_db_option_group" "this" {
 #------------------------------------------------------------------------------
 
 resource "aws_db_parameter_group" "this" {
-  name   = "rds-pg"
-  family = "mysql5.6"
+  count = var.parameter_group.create ? 1 : 0
 
-  dynamic "parameter" {
-    for_each = var.db_parameter_group.parameter
-    content {
-      name  = parameter.value["name"]
-      value = parameter.value["value"]
-    }
+  name_prefix = var.parameter_group.name_prefix
+  description = "Database parameter group for ${var.identifier}"
+  family      = var.parameter_group.family
+
+  parameter = var.parameter_group.parameters
+
+  tags = merge(var.parameter_group.tags, map("Name", format("%s", var.identifier)))
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
 #------------------------------------------------------------------------------
-# PROXY OPTIONS
+# SUBNET GROUPS
 #------------------------------------------------------------------------------
 
-resource "aws_db_proxy" "this" {
-  name                   = var.db_proxy.name
-  debug_logging          = var.db_proxy.debug_logging
-  engine_family          = var.db_proxy.engine_family
-  idle_client_timeout    = var.db_proxy.idle_client_timeout
-  require_tls            = var.db_proxy.require_tls
-  role_arn               = var.db_proxy.role_arn
-  vpc_security_group_ids = var.db_proxy.vpc_security_group_ids
-  vpc_subnet_ids         = var.db_proxy.vpc_subnet_ids
+resource "aws_db_subnet_group" "this" {
+  name_prefix = var.subnet_group.name_prefix
+  description = "Database subnet group for ${var.identifier}"
+  subnet_ids  = [var.subnet_group.subnet_ids]
 
-  auth {
-    auth_scheme = var.db_proxy.auth.auth_scheme
-    description = var.db_proxy.auth.description
-    iam_auth    = var.db_proxy.auth.iam_auth
-    secret_arn  = var.db_proxy.auth.secret_arn
-  }
-
-  tags = merge(local.tags, {
-    Name = var.instance.name
-  })
-}
-
-resource "aws_db_proxy_default_target_group" "this" {
-  db_proxy_name = var.db_proxy_default_target_group.db_proxy_name
-
-  connection_pool_config {
-    connection_borrow_timeout    = var.db_proxy_default_target_group.connection_pool_config.connection_borrow_timeout
-    init_query                   = var.db_proxy_default_target_group.connection_pool_config.init_query
-    max_connections_percent      = var.db_proxy_default_target_group.connection_pool_config.max_connections_percent
-    max_idle_connections_percent = var.db_proxy_default_target_group.connection_pool_config.max_idle_connections_percent
-    session_pinning_filters      = var.db_proxy_default_target_group.connection_pool_config.session_pinning_filters
-  }
+  tags = merge(var.tags, map("Name", format("%s", var.identifier)))
 }
