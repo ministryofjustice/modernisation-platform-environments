ef9394ac551435e84e8be4100c34e65e15fe5401
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2023-03-22 10:35:20 +0000

    DSOS-1506:add_ndh_vms_to_ndh_environment

--- terraform/environments/nomis-data-hub/locals_test.tf
+++ terraform/environments/nomis-data-hub/locals_test.tf
@@ -15,61 +15,131 @@ locals {
     }
 
     baseline_ec2_instances = {
+      # Example instance using RedHat image with ansible provisioning
+      # dev-redhat-rhel79-1 = {
+      #   config = merge(module.baseline_presets.ec2_instance.config.default, {
+      #     ami_name  = "RHEL-7.9_HVM-*"
+      #     ami_owner = "309956199498"
+      #   })
+      #   instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+      #     vpc_security_group_ids = ["private"]
+      #   })
+      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+      #   tags = {
+      #     description = "For testing with official RedHat RHEL7.9 image"
+      #     os-type     = "Linux"
+      #     component   = "test"
+      #     server-type = "set me to the ansible server type group vars"
+      #   }
+      # }
+
+
       t1_ndh_app_1 = {
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name  = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+          ami_owner = "309956199498"
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private"]
+        })
+        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
         tags = {
-          server-type       = "ndh-app"
-          description       = "Standalone EC2 for testing RHEL7.9 NDH App"
-          monitored         = false
-          os-type           = "Linux"
-          component         = "ndh"
-          nomis-environment = "t1"
+          description = "Standalone EC2 for testing RHEL7.9 NDH App"
+          os-type     = "Linux"
+          component   = "ndh"
+          server-type = "ndh-app"
+          monitored   = false
         }
-        ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
       }
+
       t1_ndh_ems_1 = {
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name  = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+          ami_owner = "309956199498"
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private"]
+        })
+        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
         tags = {
-          server-type       = "ndh-ems"
-          description       = "Standalone EC2 for testing RHEL7.9 NDH EMS"
-          monitored         = false
-          os-type           = "Linux"
-          component         = "ndh"
-          nomis-environment = "t1"
+          description = "Standalone EC2 for testing RHEL7.9 NDH App"
+          os-type     = "Linux"
+          component   = "ndh"
+          server-type = "ndh-ems"
+          monitored   = false
         }
-        ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
       }
     }
     baseline_ec2_autoscaling_group = {
+
+      # Example ASG using base image with ansible provisioning
+      # Include the autoscale-trigger-hook ansible role when using hooks
+      # dev-base-rhel79 = {
+      #   config = merge(module.baseline_presets.ec2_instance.config.default, {
+      #     ami_name = "base_rhel_7_9_*"
+      #   })
+      #   instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+      #     vpc_security_group_ids = ["private"]
+      #   })
+      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+      #   autoscaling_group = {
+      #     desired_capacity    = 1
+      #     max_size      #       # = 2
+      #     vpc_zone_identifier = module.environment.subnets["private"].ids
+      #   }
+      #   autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+      #   tags = {
+      #     description = "For testing with official RedHat RHEL7.9 image"
+      #     os-type     = "Linux"
+      #     component   = "test"
+      #     server-type = "set me to the ansible server type group vars"
+      #   }
+
+
       t1_ndh_app = {
-        tags = {
-          server-type       = "ndh-app"
-          description       = "Standalone EC2 for testing RHEL7.9 NDH App"
-          monitored         = false
-          os-type           = "Linux"
-          component         = "ndh"
-          nomis-environment = "t1"
-        }
-        ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private"]
+        })
+        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
         autoscaling_group = {
-          desired_capacity = 1
+          desired_capacity    = 1
+          max_size            = 2
+          vpc_zone_identifier = module.environment.subnets["private"].ids
         }
-        autoscaling_schedules = {}
-        subnet_name           = "data"
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Standalone EC2 for testing RHEL7.9 NDH App"
+          os-type     = "Linux"
+          component   = "ndh"
+          server-type = "ndh-app"
+          monitored   = false
+        }
       }
+
       t1_ndh_ems = {
-        tags = {
-          server-type       = "ndh-ems"
-          description       = "Standalone EC2 for testing RHEL7.9 NDH EMS"
-          monitored         = false
-          os-type           = "Linux"
-          component         = "ndh"
-          nomis-environment = "t1"
-        }
-        ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private"]
+        })
+        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
         autoscaling_group = {
-          desired_capacity = 1
+          desired_capacity    = 1
+          max_size            = 2
+          vpc_zone_identifier = module.environment.subnets["private"].ids
         }
-        autoscaling_schedules = {}
-        subnet_name           = "data"
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Standalone EC2 for testing RHEL7.9 NDH ems"
+          os-type     = "Linux"
+          component   = "ndh"
+          server-type = "ndh-ems"
+          monitored   = false
+        }
       }
     }
   }
