fcfcb7c300f0adff460c1627b9931e42b959ded1
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2023-03-22 11:29:17 +0000

    DSOS-1506:add_ndh_vms_to_ndh_environment

--- terraform/environments/nomis-data-hub/locals_test.tf
+++ terraform/environments/nomis-data-hub/locals_test.tf
@@ -94,11 +94,36 @@ locals {
       #     component   = "test"
       #     server-type = "set me to the ansible server type group vars"
       #   }
-
+      #
+      # Example target group setup below
+      #
+      #   lb_target_groups = {
+      #     http-7777 = {
+      #       port                 = 7777
+      #       protocol             = "HTTP"
+      #       target_type          = "instance"
+      #       deregistration_delay = 30
+      #       health_check = {
+      #         enabled             = true
+      #         interval            = 30
+      #         healthy_threshold   = 3
+      #         matcher             = "200-399"
+      #         path                = "/"
+      #         port                = 7777
+      #         timeout             = 5
+      #         unhealthy_threshold = 5
+      #       }
+      #       stickiness = {
+      #         enabled = true
+      #         type    = "lb_cookie"
+      #       }
+      #     }
+      #   }
+      # }
 
       t1_ndh_app = {
         config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name = "base_rhel_7_9_*"
+          ami_name  = "base_rhel_7_9_*"
           ami_owner = "374269020027"
         })
         instance = merge(module.baseline_presets.ec2_instance.instance.default, {
@@ -122,7 +147,7 @@ locals {
 
       t1_ndh_ems = {
         config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name = "base_rhel_7_9_*"
+          ami_name  = "base_rhel_7_9_*"
           ami_owner = "374269020027"
         })
         instance = merge(module.baseline_presets.ec2_instance.instance.default, {
@@ -143,6 +168,30 @@ locals {
           monitored   = false
         }
       }
+
+      lb_target_groups = {
+        http-7777 = {
+          port                 = 7777
+          protocol             = "HTTP"
+          target_type          = "instance"
+          deregistration_delay = 30
+          health_check = {
+            enabled             = true
+            interval            = 30
+            healthy_threshold   = 3
+            matcher             = "200-399"
+            path                = "/"
+            port                = 7777
+            timeout             = 5
+            unhealthy_threshold = 5
+          }
+          stickiness = {
+            enabled = true
+            type    = "lb_cookie"
+          }
+        }
+      }
+
     }
   }
 }
