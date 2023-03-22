0370f60f97d5536a3caf1b9a9dd8d873707fb459
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-03-22 15:52:22 +0000

    CC-1621: Use the example code for certificates.

--- terraform/environments/laa-oem/member_certificates.tf
+++ terraform/environments/laa-oem/member_certificates.tf
@@ -1,6 +1,57 @@
 ###################################################
 # *.modernisation-platform.service.justice.gov.uk #
 ###################################################
+resource "aws_acm_certificate" "laa_cert" {
+  count             = local.is-production ? 0 : 1
+  domain_name       = "modernisation-platform.service.justice.gov.uk"
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    format("%s.%s.%s.%s.modernisation-platform.service.justice.gov.uk", "app", local.application_name, var.networking[0].business-unit, local.environment),
+    format("%s.%s.%s.%s.modernisation-platform.service.justice.gov.uk", "db", local.application_name, var.networking[0].business-unit, local.environment),
+    format("%s.%s.%s.%s.modernisation-platform.service.justice.gov.uk", "laa-oem-app", local.application_name, var.networking[0].business-unit, local.environment),
+    format("%s.%s.%s.%s.modernisation-platform.service.justice.gov.uk", "laa-oem-db", local.application_name, var.networking[0].business-unit, local.environment),
+    format("%s.%s.%s.%s.modernisation-platform.service.justice.gov.uk", "oem", local.application_name, var.networking[0].business-unit, local.environment),
+    format("%s.%s.%s.%s.modernisation-platform.service.justice.gov.uk", "oem-internal", local.application_name, var.networking[0].business-unit, local.environment)
+  ]
+
+  tags = merge(local.tags,
+    { Name = lower(format("%s-%s-certificate", local.application_name, local.environment)) }
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "laa_cert" {
+  count                   = local.is-production ? 0 : 1
+  certificate_arn         = aws_acm_certificate.laa_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.laa_cert_validation : record.fqdn]
+  timeouts {
+    create = "10m"
+  }
+}
+
+resource "aws_route53_record" "laa_cert_validation" {
+  provider = aws.core-network-services
+  for_each = {
+    for dvo in aws_acm_certificate.laa_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.network-services.zone_id
+}
+
+/*
 resource "aws_acm_certificate" "external-mp" {
   count             = local.is-production ? 0 : 1
   domain_name       = "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
@@ -54,7 +105,6 @@ resource "aws_acm_certificate_validation" "external-mp" {
 ############################
 # *.service.justice.gov.uk #
 ############################
-/*
 resource "aws_acm_certificate" "external-service" {
   count             = local.is-production ? 0 : 1
   domain_name       = "*.service.justice.gov.uk"
