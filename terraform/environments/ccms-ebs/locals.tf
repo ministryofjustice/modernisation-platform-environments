238764799e2d0d87302286e629ee360d9a739e1a
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-22 15:07:09 +0000

    another whitespace to bump pipeline

--- terraform/environments/ccms-ebs/locals.tf
+++ terraform/environments/ccms-ebs/locals.tf
@@ -1,5 +1,4 @@
 #### This file can be used to store locals specific to the member account ####
-
 locals {
   artefact_bucket_name = "${local.application_name}-${local.environment}-artefacts"
   logging_bucket_name  = "${local.application_name}-${local.environment}-logging"
