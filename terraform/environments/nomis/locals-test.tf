636c91bf48a91d5e69861be9ca3beb4eac549d8e
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2023-03-22 09:52:00 +0000

    DSOS-1506/remove_ndh_vms_from_nomis_environment

--- terraform/environments/nomis/locals-test.tf
+++ terraform/environments/nomis/locals-test.tf
@@ -132,69 +132,10 @@ locals {
 
     ec2_test_instances = {
       # Remove data.aws_kms_key from cmk.tf once the NDH servers are removed
-      t1-ndh-app-1 = {
-        tags = {
-          server-type       = "ndh-app"
-          description       = "Standalone EC2 for testing RHEL7.9 NDH App"
-          monitored         = false
-          os-type           = "Linux"
-          component         = "ndh"
-          nomis-environment = "t1"
-        }
-        ebs_volumes = {
-          "/dev/sda1" = { kms_key_id = data.aws_kms_key.default_ebs.arn }
-        }
-        ami_name = "nomis_rhel_7_9_baseimage_2022-11-01T13-43-46.384Z"
       }
-      t1-ndh-ems-1 = {
-        tags = {
-          server-type       = "ndh-ems"
-          description       = "Standalone EC2 for testing RHEL7.9 NDH EMS"
-          monitored         = false
-          os-type           = "Linux"
-          component         = "ndh"
-          nomis-environment = "t1"
-        }
-        ebs_volumes = {
-          "/dev/sda1" = { kms_key_id = data.aws_kms_key.default_ebs.arn }
-        }
-        ami_name = "nomis_rhel_7_9_baseimage_2022-11-01T13-43-46.384Z"
-      }
-    }
-    ec2_test_autoscaling_groups = {
-      t1-ndh-app = {
-        tags = {
-          server-type       = "ndh-app"
-          description       = "Standalone EC2 for testing RHEL7.9 NDH App"
-          monitored         = false
-          os-type           = "Linux"
-          component         = "ndh"
-          nomis-environment = "t1"
-        }
-        ami_name = "nomis_rhel_7_9_baseimage_2022-11-01T13-43-46.384Z"
-        autoscaling_group = {
-          desired_capacity = 1
-        }
-        autoscaling_schedules = {}
-        subnet_name           = "data"
-      }
-      t1-ndh-ems = {
-        tags = {
-          server-type       = "ndh-ems"
-          description       = "Standalone EC2 for testing RHEL7.9 NDH EMS"
-          monitored         = false
-          os-type           = "Linux"
-          component         = "ndh"
-          nomis-environment = "t1"
-        }
-        ami_name = "nomis_rhel_7_9_baseimage_2022-11-01T13-43-46.384Z"
-        autoscaling_group = {
-          desired_capacity = 1
-        }
-        autoscaling_schedules = {}
-        subnet_name           = "data"
-      }
-    }
+
+    ec2_test_autoscaling_groups = { }
+
     ec2_jumpservers = {
       jumpserver-2022 = {
         ami_name = "nomis_windows_server_2022_jumpserver_release_*"
