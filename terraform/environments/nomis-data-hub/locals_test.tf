f1f6b4d7382949167c925b57a474feb883cf1649
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2023-03-22 09:17:47 +0000

    DSOS-1506/move_ndh_servers_to_ndh_account

--- terraform/environments/nomis-data-hub/locals_test.tf
+++ terraform/environments/nomis-data-hub/locals_test.tf
@@ -1,5 +1,9 @@
-locals {
+    git pull <remote> <branch>
 
+If you wish to set tracking information for this branch you can do so with:
+
+    git branch --set-upstream-to=origin/<branch> feature/DSOS-1506/move_ndh_serv# ndh-test environment settings
+locals {
   test_config = {
 
     baseline_s3_buckets = {
@@ -12,6 +16,65 @@ locals {
           module.baseline_presets.s3_bucket_policies.AllEnvironmentsWriteAccessBucketPolicy,
         ]
         iam_policies = module.baseline_presets.s3_iam_policies
+      }
+    }
+
+    baseline_ec2_instances = {
+      t1_ndh_app_1 = {
+        tags = {
+          server-type       = "ndh-app"
+          description       = "Standalone EC2 for testing RHEL7.9 NDH App"
+          monitored         = false
+          os-type           = "Linux"
+          component         = "ndh"
+          nomis-environment = "t1"
+        }
+        ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+      }
+      t1_ndh_ems_1 = {
+        tags = {
+          server-type       = "ndh-ems"
+          description       = "Standalone EC2 for testing RHEL7.9 NDH EMS"
+          monitored         = false
+          os-type           = "Linux"
+          component         = "ndh"
+          nomis-environment = "t1"
+        }
+        ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+      }
+    }
+    baseline_ec2_autoscaling_group = {
+      t1_ndh_app = {
+        tags = {
+          server-type       = "ndh-app"
+          description       = "Standalone EC2 for testing RHEL7.9 NDH App"
+          monitored         = false
+          os-type           = "Linux"
+          component         = "ndh"
+          nomis-environment = "t1"
+        }
+        ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+        autoscaling_group = {
+          desired_capacity = 1
+        }
+        autoscaling_schedules = {}
+        subnet_name           = "data"
+      }
+      t1_ndh_ems = {
+        tags = {
+          server-type       = "ndh-ems"
+          description       = "Standalone EC2 for testing RHEL7.9 NDH EMS"
+          monitored         = false
+          os-type           = "Linux"
+          component         = "ndh"
+          nomis-environment = "t1"
+        }
+        ami_name = "base_rhel_7_9_2023-03-21T13-46-44.297Z"
+        autoscaling_group = {
+          desired_capacity = 1
+        }
+        autoscaling_schedules = {}
+        subnet_name           = "data"
       }
     }
   }
