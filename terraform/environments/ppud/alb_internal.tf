6a767c507696fa44237838f34a9adde924b09a3d
umeshc-roy <umeshc.roy@gmail.com>
2023-03-23 09:11:47 +0000

    Prod-Add

--- terraform/environments/ppud/alb_internal.tf
+++ terraform/environments/ppud/alb_internal.tf
@@ -22,21 +22,10 @@ resource "aws_lb_listener" "PPUD-Front-End" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = data.aws_acm_certificate.internaltest_cert.arn
-  /*
+  
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.PPUD-internal-Target-Group[0].arn
-  }
-
-*/
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Hi, I am PPUD Internal ALB"
-      status_code  = "200"
-    }
   }
 }
 
@@ -70,9 +59,23 @@ resource "aws_lb_target_group" "PPUD-internal-Target-Group" {
 }
 
 
-resource "aws_lb_target_group_attachment" "PPUD-PORTAL-internal-development" {
+resource "aws_lb_target_group_attachment" "PPUD-PORTAL-internal-preproduction" {
   count            = local.is-preproduction == true ? 1 : 0
   target_group_arn = aws_lb_target_group.PPUD-internal-Target-Group[0].arn
   target_id        = aws_instance.s618358rgvw023[0].id
+  port             = 443
+}
+
+resource "aws_lb_target_group_attachment" "PPUD-PORTAL-internal-production" {
+  count            = local.is-production == true ? 1 : 0
+  target_group_arn = aws_lb_target_group.PPUD-internal-Target-Group[0].arn
+  target_id        = aws_instance.s618358rgvw019[0].id
+  port             = 443
+}
+
+resource "aws_lb_target_group_attachment" "PPUD-PORTAL-internal-production-1" {
+  count            = local.is-production == true ? 1 : 0
+  target_group_arn = aws_lb_target_group.PPUD-internal-Target-Group[0].arn
+  target_id        = aws_instance.s618358rgvw020[0].id
   port             = 443
 }
--- terraform/environments/ppud/instances.tf
+++ terraform/environments/ppud/instances.tf
@@ -1,10 +1,10 @@
-########################################
-#           Development Instances       #
-########################################
+#################################
+#   Development Instances       #
+#################################
 
 resource "aws_instance" "s609693lo6vw109" {
   count                  = local.is-development == true ? 1 : 0
-  ami                    = "ami-0401c99f36918962f"
+  ami                    = "ami-0ca7365d53b8b8028"
   instance_type          = "m5.large"
   vpc_security_group_ids = [aws_security_group.SCR-Team-Foundation-Server[0].id]
   source_dest_check      = false
@@ -161,9 +161,9 @@ resource "aws_instance" "PPUD-DEV-AWS-AD" {
   }
 }
 
-#########################
-#     UAT Instances     #
-#########################
+#################################
+# Pre-Production (UAT Instances) #
+#################################
 
 
 resource "aws_instance" "s618358rgvw201" {
@@ -211,7 +211,7 @@ resource "aws_instance" "s618358rgsw025" {
 resource "aws_instance" "s618358rgvw024" {
   count                  = local.is-preproduction == true ? 1 : 0
   ami                    = "ami-0d3d8251678e13330"
-  instance_type          = "m6i.xlarge"
+  instance_type          = "m6i.2xlarge"
   source_dest_check      = false
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
   vpc_security_group_ids = [aws_security_group.UAT-Document-Service[0].id]
@@ -233,5 +233,107 @@ resource "aws_instance" "s618358rgvw023" {
   tags = {
     Name   = "s618358rgvw023"
     backup = true
+  }
+}
+
+#########################
+# Production Instances  #
+#########################
+
+resource "aws_instance" "s618358rgvw019" {
+  count                  = local.is-production == true ? 1 : 0
+  ami                    = "ami-053a1bdfccde5a440"
+  instance_type          = "c5.xlarge"
+  source_dest_check      = false
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
+  vpc_security_group_ids = [aws_security_group.PPUD-WEB-Portal.id]
+  subnet_id              = data.aws_subnet.private_subnets_b.id
+  tags = {
+    Name   = "s618358rgvw019"
+    is-production = true
+  }
+}
+
+resource "aws_instance" "s618358rgvw020" {
+  count                  = local.is-production == true ? 1 : 0
+  ami                    = "ami-0f40fd8eeb25ed216"
+  instance_type          = "c5.xlarge"
+  source_dest_check      = false
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
+  vpc_security_group_ids = [aws_security_group.PPUD-WEB-Portal.id]
+  subnet_id              = data.aws_subnet.private_subnets_c.id
+  tags = {
+    Name   = "s618358rgvw020"
+    is-production = true
+  }
+}
+
+resource "aws_instance" "s618358rgvw021" {
+  count                  = local.is-production == true ? 1 : 0
+  ami                    = "ami-06f04ef58ddbec1fb"
+  instance_type          = "m5.2xlarge"
+  source_dest_check      = false
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
+  vpc_security_group_ids = [aws_security_group.PPUD-PROD-Database[0].id]
+  subnet_id              = data.aws_subnet.data_subnets_a.id
+  tags = {
+    Name   = "s618358rgvw021"
+    is-production = true
+  }
+}
+
+resource "aws_instance" "s618358rgvw022" {
+  count                  = local.is-production == true ? 1 : 0
+  ami                    = "ami-099e1bff1ed376c3c"
+  instance_type          = "m5.xlarge"
+  source_dest_check      = false
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
+  vpc_security_group_ids = [aws_security_group.Primary-DOC-Server[0].id]
+  subnet_id              = data.aws_subnet.private_subnets_b.id
+  tags = {
+    Name   = "s618358rgvw022"
+    is-production = true
+  }
+}
+
+resource "aws_instance" "s618358rgvw027" {
+  count                  = local.is-production == true ? 1 : 0
+  ami                    = "ami-0d47d882709bccb38"
+  instance_type          = "m5.xlarge"
+  source_dest_check      = false
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
+  vpc_security_group_ids = [aws_security_group.Secondary-DOC-Server[0].id]
+  subnet_id              = data.aws_subnet.private_subnets_c.id
+  tags = {
+    Name   = "s618358rgvw027"
+    is-production = true
+  }
+}
+
+resource "aws_instance" "s618358rgvw204" {
+  count                  = local.is-production == true ? 1 : 0
+  ami                    = "ami-0e570893d0761b36d"
+  instance_type          = "c5.xlarge"
+  source_dest_check      = false
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
+  vpc_security_group_ids = [aws_security_group.PPUD-WEB-Portal.id]
+  subnet_id              = data.aws_subnet.private_subnets_a.id
+  tags = {
+    Name   = "s618358rgvw204"
+    is-production = true
+  }
+}
+
+resource "aws_instance" "s618358rgvw205" {
+  count                  = local.is-production == true ? 1 : 0
+  ami                    = "ami-0e863bfbce3cfe8eb"
+  instance_type          = "c5.large"
+  source_dest_check      = false
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
+  vpc_security_group_ids = [aws_security_group.Bridge-Server[0].id]
+  subnet_id              = data.aws_subnet.private_subnets_a.id
+  tags = {
+    Name   = "s618358rgvw205"
+    is-production = true
   }
 }
