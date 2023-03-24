0e6bc43d0768f465bd3e8cb65a1af61d4da3e632
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-24 17:19:37 +0000

    removal

--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -34,10 +34,10 @@ resource "aws_acm_certificate" "external-service" {
   }
 }
 
-
+/*
 resource "aws_route53_record" "external_validation" {
-  #provider = aws.core-vpc
-  provider = aws.core-network-services
+  provider = aws.core-vpc
+  #provider = aws.core-network-services
   for_each = {
     for dvo in local.cert_opts : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -50,9 +50,11 @@ resource "aws_route53_record" "external_validation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.network-services.zone_id
+  zone_id         = data.aws_route53_zone.external.zone_id
+  #zone_id         = data.aws_route53_zone.network-services.zone_id
 }
-
+*/
+/*
 resource "aws_acm_certificate_validation" "external" {
   depends_on = [
     aws_route53_record.external_validation
@@ -60,3 +62,4 @@ resource "aws_acm_certificate_validation" "external" {
   certificate_arn         = local.cert_arn
   validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
 }
+*/
