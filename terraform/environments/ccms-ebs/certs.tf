8c5e3220860531a7e1f3840b89a478c59ddd6e72
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-24 17:14:09 +0000

    change zone

--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -36,8 +36,8 @@ resource "aws_acm_certificate" "external-service" {
 
 
 resource "aws_route53_record" "external_validation" {
-  provider = aws.core-vpc
-  #provider = aws.core-network-services
+  #provider = aws.core-vpc
+  provider = aws.core-network-services
   for_each = {
     for dvo in local.cert_opts : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -50,7 +50,7 @@ resource "aws_route53_record" "external_validation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.external.zone_id
+  zone_id         = data.aws_route53_zone.network-services.zone_id
 }
 
 resource "aws_acm_certificate_validation" "external" {
