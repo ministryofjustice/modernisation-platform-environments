5ae328e3c4cb3741fef0bedd61dd4d94ae7bcbb1
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-03-24 16:49:19 +0000

    CC-1621: Force remove DNS validation records.

--- terraform/environments/laa-oem/member_certificates.tf
+++ terraform/environments/laa-oem/member_certificates.tf
@@ -23,31 +23,31 @@ resource "aws_acm_certificate" "laa_cert" {
   }
 }
 
-resource "aws_acm_certificate_validation" "laa_cert" {
-  certificate_arn         = aws_acm_certificate.laa_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.laa_cert_validation : record.fqdn]
-  timeouts {
-    create = "16m"
-  }
-}
-
-resource "aws_route53_record" "laa_cert_validation" {
-  provider = aws.core-vpc
-  for_each = {
-    for dvo in aws_acm_certificate.laa_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.external.zone_id
-}
+#resource "aws_acm_certificate_validation" "laa_cert" {
+#  certificate_arn         = aws_acm_certificate.laa_cert.arn
+#  validation_record_fqdns = [for record in aws_route53_record.laa_cert_validation : record.fqdn]
+#  timeouts {
+#    create = "16m"
+#  }
+#}
+#
+#resource "aws_route53_record" "laa_cert_validation" {
+#  provider = aws.core-vpc
+#  for_each = {
+#    for dvo in aws_acm_certificate.laa_cert.domain_validation_options : dvo.domain_name => {
+#      name   = dvo.resource_record_name
+#      record = dvo.resource_record_value
+#      type   = dvo.resource_record_type
+#    }
+#  }
+#
+#  allow_overwrite = true
+#  name            = each.value.name
+#  records         = [each.value.record]
+#  ttl             = 60
+#  type            = each.value.type
+#  zone_id         = data.aws_route53_zone.external.zone_id
+#}
 
 /*
 resource "aws_acm_certificate" "external-mp" {
