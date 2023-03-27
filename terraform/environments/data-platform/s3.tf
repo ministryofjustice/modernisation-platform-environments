ac0c4d3e582cf18fd549253ed9ad417dcc292214
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-03-27 15:57:49 +0100

    Added more s3 config

--- terraform/environments/data-platform/s3.tf
+++ terraform/environments/data-platform/s3.tf
@@ -2,10 +2,58 @@ module "s3-bucket" { #tfsec:ignore:aws-s3-enable-versioning
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.3.0"
 
   bucket_prefix      = "data-platform-landing"
-  versioning_enabled = false
-  bucket_policy      = [data.aws_iam_policy_document.bucket_policy.json]
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled                      = false
 
-  tags = local.tags
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags                 = local.tags
 }
 
 
