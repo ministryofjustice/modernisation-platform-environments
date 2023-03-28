0e273b215686d06802705bf0d3ba43241403709b
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-03-28 16:15:10 +0100

    Adding an web-identity-assumable role to be used instead of cicd-member-user keys.

--- terraform/environments/data-platform/data.tf
+++ terraform/environments/data-platform/data.tf
@@ -1,1 +1,4 @@
 #### This file can be used to store data specific to the member account ####
+data "aws_iam_policy" "cicd_member_policy" {
+  name = "cicd-member-policy"
+}
--- terraform/environments/data-platform/locals.tf
+++ terraform/environments/data-platform/locals.tf
@@ -1,1 +1,5 @@
 #### This file can be used to store locals specific to the member account ####
+locals {
+  oidc_repositories        = ["ministryofjustice/data-platform-products:*"]
+  oidc_default_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"]
+}
