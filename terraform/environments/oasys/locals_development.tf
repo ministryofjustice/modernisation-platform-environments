291563d2b28999aff550b80e9da921ace7b06bfc
W <willg258@hotmail.co.uk>
2023-03-28 12:21:48 +0100

    ..

--- terraform/environments/oasys/locals_development.tf
+++ terraform/environments/oasys/locals_development.tf
@@ -142,52 +142,67 @@ locals {
 
       # Example ASG using base image with ansible provisioning
       # Include the autoscale-trigger-hook ansible role when using hooks
-      # dev-base-rhel79 = {
-      #   config = merge(module.baseline_presets.ec2_instance.config.default, {
-      #     ami_name = "base_rhel_7_9_*"
-      #   })
-      #   instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-      #     vpc_security_group_ids = ["private"]
-      #   })
-      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-      #   autoscaling_group = {
-      #     desired_capacity    = 1
-      #     max_size      #       # = 2
-      #     vpc_zone_identifier = module.environment.subnets["private"].ids
-      #   }
-      #   autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
-      #   tags = {
-      #     description = "For testing with official RedHat RHEL7.9 image"
-      #     os-type     = "Linux"
-      #     component   = "test"
-      #     server-type = "set me to the ansible server type group vars"
-      #   }
-      #
+      development-oasys-web = {
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name = "oasys_webserver_*"
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private"]
+        })
+        user_data_cloud_init = {
+          args = {
+            lifecycle_hook_name  = "ready-hook"
+            branch               = "main"
+            ansible_repo         = "modernisation-platform-configuration-management"
+            ansible_repo_basedir = "ansible"
+            ansible_args         = ""
+          }
+          scripts = [
+            "install-ssm-agent.sh.tftpl",
+            "ansible-ec2provision.sh.tftpl",
+            "post-ec2provision.sh.tftpl"
+          ]
+        }
+        autoscaling_group = {
+          desired_capacity    = 1
+          max_size            = 2
+          vpc_zone_identifier = module.environment.subnets["private"].ids
+        }
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          os-type           = "Linux"
+          oasys-environment = "t1"
+          description       = "oasys webserver"
+          component         = "web"
+          server-type       = "oasys-web"
+          os-version        = "RHEL 7.9"
+        }
+      
       # Example target group setup below
-      #
-      #   lb_target_groups = {
-      #     http-7777 = {
-      #       port                 = 7777
-      #       protocol             = "HTTP"
-      #       target_type          = "instance"
-      #       deregistration_delay = 30
-      #       health_check = {
-      #         enabled             = true
-      #         interval            = 30
-      #         healthy_threshold   = 3
-      #         matcher             = "200-399"
-      #         path                = "/"
-      #         port                = 7777
-      #         timeout             = 5
-      #         unhealthy_threshold = 5
-      #       }
-      #       stickiness = {
-      #         enabled = true
-      #         type    = "lb_cookie"
-      #       }
-      #     }
-      #   }
-      # }
+      
+        lb_target_groups = {
+          http-8080 = {
+            port                 = 8080
+            protocol             = "HTTP"
+            target_type          = "instance"
+            deregistration_delay = 30
+            health_check = {
+              enabled             = true
+              interval            = 30
+              healthy_threshold   = 3
+              matcher             = "200-399"
+              path                = "/"
+              port                = 8080
+              timeout             = 5
+              unhealthy_threshold = 5
+            }
+            stickiness = {
+              enabled = true
+              type    = "lb_cookie"
+            }
+          }
+        }
+      }
 
 
       # webservers = merge(local.webserver, { # merge common config and env specific
--- terraform/environments/oasys/locals_test.tf
+++ terraform/environments/oasys/locals_test.tf
@@ -52,48 +52,48 @@ locals {
 
     baseline_ec2_autoscaling_groups = {
 
-      test-base-rhel85 = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name = "base_rhel_8_5_*"
-        })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["private"]
-        })
-        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-        autoscaling_group = {
-          desired_capacity    = 1
-          max_size            = 2
-          vpc_zone_identifier = module.environment.subnets["private"].ids
-        }
-        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
-        tags = {
-          description = "For testing with official RedHat RHEL8.5 image"
-          os-type     = "Linux"
-          component   = "test"
-        }
-        lb_target_groups = {
-          http-7777 = {
-            port                 = 7777
-            protocol             = "HTTP"
-            target_type          = "instance"
-            deregistration_delay = 30
-            health_check = {
-              enabled             = true
-              interval            = 30
-              healthy_threshold   = 3
-              matcher             = "200-399"
-              path                = "/"
-              port                = 7777
-              timeout             = 5
-              unhealthy_threshold = 5
-            }
-            stickiness = {
-              enabled = true
-              type    = "lb_cookie"
-            }
-          }
-        }
-      }
+      # test-base-rhel85 = {
+      #   config = merge(module.baseline_presets.ec2_instance.config.default, {
+      #     ami_name = "base_rhel_8_5_*"
+      #   })
+      #   instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+      #     vpc_security_group_ids = ["private"]
+      #   })
+      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+      #   autoscaling_group = {
+      #     desired_capacity    = 1
+      #     max_size            = 2
+      #     vpc_zone_identifier = module.environment.subnets["private"].ids
+      #   }
+      #   autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+      #   tags = {
+      #     description = "For testing with official RedHat RHEL8.5 image"
+      #     os-type     = "Linux"
+      #     component   = "test"
+      #   }
+      #   lb_target_groups = {
+      #     http-7777 = {
+      #       port                 = 7777
+      #       protocol             = "HTTP"
+      #       target_type          = "instance"
+      #       deregistration_delay = 30
+      #       health_check = {
+      #         enabled             = true
+      #         interval            = 30
+      #         healthy_threshold   = 3
+      #         matcher             = "200-399"
+      #         path                = "/"
+      #         port                = 7777
+      #         timeout             = 5
+      #         unhealthy_threshold = 5
+      #       }
+      #       stickiness = {
+      #         enabled = true
+      #         type    = "lb_cookie"
+      #       }
+      #     }
+      #   }
+      # }
     }
     # baseline_lbs = {
     #   rhel85-test = {
