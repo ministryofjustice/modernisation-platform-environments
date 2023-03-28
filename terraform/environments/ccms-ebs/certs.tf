fbab9c01685ca98a4c4a9c0cef2cc609d66e6b31
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-28 13:09:24 +0100

    temp addition so can remove

--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -41,8 +41,8 @@ resource "aws_acm_certificate" "external-service" {
 }
 
 resource "aws_route53_record" "external_validation" {
-  provider = aws.core-vpc
-  #provider = aws.core-network-services
+  #provider = aws.core-vpc
+  provider = aws.core-network-services
   for_each = {
     for dvo in local.cert_opts : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -55,8 +55,8 @@ resource "aws_route53_record" "external_validation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.external.zone_id
-  #zone_id = data.aws_route53_zone.network-services.zone_id
+  #zone_id         = data.aws_route53_zone.external.zone_id
+  zone_id = data.aws_route53_zone.network-services.zone_id
 }
 
 resource "aws_acm_certificate_validation" "external" {
@@ -65,4 +65,7 @@ resource "aws_acm_certificate_validation" "external" {
   ]
   certificate_arn         = local.cert_arn
   validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
+  timeouts {
+    create = "5m"
+  }
 }
