e2f074e420743cc53b17ff2a6a175fc652ddc73a
David Elliott <david.elliott@digital.justice.gov.uk>
2023-03-28 15:22:01 +0100

    Changes to switch logic to not our admin roles
    This switches the logic so that if the user running the Terraform
    doesn't match either the SSO admin user or the GithubActions session,
    then it falls back to the lesser priviledged providers.
    
    This should allow us to expand and add more roles without needing to
    change this logic.

--- terraform/environments/sprinkler/outputs.tf
+++ terraform/environments/sprinkler/outputs.tf
@@ -1,3 +1,19 @@
-output "original_session" {
-	value = data.aws_caller_identity.original_session
+output "provider_original_session" {
+  value = data.aws_caller_identity.original_session
+}
+
+output "provider_default" {
+  value = data.aws_caller_identity.current
+}
+
+output "provider_core_vpc" {
+  value = data.aws_caller_identity.core_vpc
+}
+
+output "provider_core_network_services" {
+  value = data.aws_caller_identity.core_network_services
+}
+
+output "provider_modernisation_platform" {
+  value = data.aws_caller_identity.modernisation_platform
 }
