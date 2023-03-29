6f7d8effaf3ac89c5c5b252253aa5851a169e79c
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-29 14:29:06 +0100

    recreate resources

--- terraform/environments/tipstaff/ecs.tf
+++ terraform/environments/tipstaff/ecs.tf
@@ -1,203 +1,203 @@
-# # Create ECS cluster
-# resource "aws_ecs_cluster" "tipstaff_cluster" {
-#   name = "tipstaff_cluster"
-# }
+# Create ECS cluster
+resource "aws_ecs_cluster" "tipstaff_cluster" {
+  name = "tipstaff_cluster"
+}
 
-# # Create a task definition for the Windows container
-# resource "aws_ecs_task_definition" "tipstaff_ecs_task" {
-#   family                = "tipstaff_ecs_task"
-#   execution_role_arn    = aws_iam_role.app_execution.arn
-#   task_role_arn         = aws_iam_role.app_task.arn
-#   cpu                   = 256
-#   memory                = 1024
-#   container_definitions = <<TASK_DEFINITION
-#   [
-#     {
-#       "name": "${var.networking[0].application}",
-#       "image": "mcr.microsoft.com/windows/servercore/iis:windowsservercore-ltsc2019",
-#       "cpu": 256,
-#       "memory": 1024,
-#       "essential": true,
-#       "portMappings": [
-#         {
-#           "containerPort": 80,
-#           "protocol": "tcp"
-#         }
-#       ],
-#       "LogConfiguration": {
-#         "LogDriver": "awslogs",
-#         "Options": {
-#           "awslogs-group": "${var.networking[0].application}",
-#           "awslogs-region": "eu-west-2",
-#           "awslogs-stream-prefix": "${var.networking[0].application}"
-#         }
-#       },
-#       "environment" : [
-#         {
-#           "name" : "DB_HOST",
-#           "value" : "${aws_db_instance.tipstaffdbdev.address}"
-#         },
-#         {
-#           "name" : "DB_PORT",
-#           "value" : "5432"
-#         }
-#       ],
-#       "secrets": [
-#         {
-#           "name" : "DB_USER",
-#           "valueFrom" : "${data.aws_secretsmanager_secret_version.db_username.arn}"
-#         },
-#         {
-#           "name": "DB_PASSWORD",
-#           "valueFrom": "${data.aws_secretsmanager_secret_version.db_password.arn}"
-#         }
-#       ]
-#     }
-#   ]
-#   TASK_DEFINITION
+# Create a task definition for the Windows container
+resource "aws_ecs_task_definition" "tipstaff_ecs_task" {
+  family                = "tipstaff_ecs_task"
+  execution_role_arn    = aws_iam_role.app_execution.arn
+  task_role_arn         = aws_iam_role.app_task.arn
+  cpu                   = 256
+  memory                = 1024
+  container_definitions = <<TASK_DEFINITION
+  [
+    {
+      "name": "${var.networking[0].application}",
+      "image": "mcr.microsoft.com/windows/servercore/iis:windowsservercore-ltsc2019",
+      "cpu": 256,
+      "memory": 1024,
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "protocol": "tcp"
+        }
+      ],
+      "LogConfiguration": {
+        "LogDriver": "awslogs",
+        "Options": {
+          "awslogs-group": "${var.networking[0].application}",
+          "awslogs-region": "eu-west-2",
+          "awslogs-stream-prefix": "${var.networking[0].application}"
+        }
+      },
+      "environment" : [
+        {
+          "name" : "DB_HOST",
+          "value" : "${aws_db_instance.tipstaffdbdev.address}"
+        },
+        {
+          "name" : "DB_PORT",
+          "value" : "5432"
+        }
+      ],
+      "secrets": [
+        {
+          "name" : "DB_USER",
+          "valueFrom" : "${data.aws_secretsmanager_secret_version.db_username.arn}"
+        },
+        {
+          "name": "DB_PASSWORD",
+          "valueFrom": "${data.aws_secretsmanager_secret_version.db_password.arn}"
+        }
+      ]
+    }
+  ]
+  TASK_DEFINITION
 
-#   requires_compatibilities = ["FARGATE"]
-#   network_mode             = "awsvpc"
-# }
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+}
 
-# # Create a Fargate service to run the Windows container task
-# resource "aws_ecs_service" "tipstaff_ecs_service" {
-#   name = var.networking[0].application
-#   deployment_controller {
-#     type = "ECS"
-#   }
-#   cluster                           = aws_ecs_cluster.tipstaff_cluster.id
-#   task_definition                   = aws_ecs_task_definition.tipstaff_ecs_task.arn
-#   launch_type                       = "FARGATE"
-#   enable_execute_command            = true
-#   desired_count                     = 1
-#   health_check_grace_period_seconds = 120
+# Create a Fargate service to run the Windows container task
+resource "aws_ecs_service" "tipstaff_ecs_service" {
+  name = var.networking[0].application
+  deployment_controller {
+    type = "ECS"
+  }
+  cluster                           = aws_ecs_cluster.tipstaff_cluster.id
+  task_definition                   = aws_ecs_task_definition.tipstaff_ecs_task.arn
+  launch_type                       = "FARGATE"
+  enable_execute_command            = true
+  desired_count                     = 1
+  health_check_grace_period_seconds = 120
 
-#   network_configuration {
-#     subnets          = data.aws_subnets.private-public.ids
-#     security_groups  = [aws_security_group.tipstaff_dev_lb_sc.id]
-#     assign_public_ip = true
-#   }
+  network_configuration {
+    subnets          = data.aws_subnets.private-public.ids
+    security_groups  = [aws_security_group.tipstaff_dev_lb_sc.id]
+    assign_public_ip = true
+  }
 
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
-#     container_name   = "${var.networking[0].application}"
-#     container_port   = 80
-#   }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
+    container_name   = "${var.networking[0].application}"
+    container_port   = 80
+  }
 
-# }
+}
 
-# resource "aws_iam_role" "app_execution" {
-#   name = "execution-${var.networking[0].application}"
+resource "aws_iam_role" "app_execution" {
+  name = "execution-${var.networking[0].application}"
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "ecs-tasks.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "execution-${var.networking[0].application}"
-#     },
-#   )
-# }
+  tags = merge(
+    local.tags,
+    {
+      Name = "execution-${var.networking[0].application}"
+    },
+  )
+}
 
-# resource "aws_iam_role_policy" "app_execution" {
-#   name = "execution-${var.networking[0].application}"
-#   role = aws_iam_role.app_execution.id
+resource "aws_iam_role_policy" "app_execution" {
+  name = "execution-${var.networking[0].application}"
+  role = aws_iam_role.app_execution.id
 
-#   policy = <<-EOF
-#   {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#       {
-#            "Action": [
-#                "logs:CreateLogStream",
-#                "logs:PutLogEvents",
-#                "ecr:GetAuthorizationToken"
-#            ],
-#            "Resource": "*",
-#            "Effect": "Allow"
-#       },
-#       {
-#             "Action": [
-#               "ecr:BatchCheckLayerAvailability",
-#               "ecr:GetDownloadUrlForLayer",
-#               "ecr:BatchGetImage"
-#             ],
-#               "Resource": "*",
-#             "Effect": "Allow"
-#       },
-#       {
-#           "Action": [
-#                "secretsmanager:GetSecretValue"
-#            ],
-#           "Resource": "*",
-#           "Effect": "Allow"
-#       }
-#     ]
-#   }
-#   EOF
-# }
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+           "Action": [
+               "logs:CreateLogStream",
+               "logs:PutLogEvents",
+               "ecr:GetAuthorizationToken"
+           ],
+           "Resource": "*",
+           "Effect": "Allow"
+      },
+      {
+            "Action": [
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:BatchGetImage"
+            ],
+              "Resource": "*",
+            "Effect": "Allow"
+      },
+      {
+          "Action": [
+               "secretsmanager:GetSecretValue"
+           ],
+          "Resource": "*",
+          "Effect": "Allow"
+      }
+    ]
+  }
+  EOF
+}
 
-# resource "aws_iam_role" "app_task" {
-#   name = "task-${var.networking[0].application}"
+resource "aws_iam_role" "app_task" {
+  name = "task-${var.networking[0].application}"
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "ecs-tasks.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "task-${var.networking[0].application}"
-#     },
-#   )
-# }
+  tags = merge(
+    local.tags,
+    {
+      Name = "task-${var.networking[0].application}"
+    },
+  )
+}
 
-# resource "aws_iam_role_policy" "app_task" {
-#   name = "task-${var.networking[0].application}"
-#   role = aws_iam_role.app_task.id
+resource "aws_iam_role_policy" "app_task" {
+  name = "task-${var.networking[0].application}"
+  role = aws_iam_role.app_task.id
 
-#   policy = <<-EOF
-#   {
-#    "Version": "2012-10-17",
-#    "Statement": [
-#      {
-#        "Effect": "Allow",
-#        "Action": [
-#             "ssmmessages:CreateControlChannel",
-#             "ssmmessages:CreateDataChannel",
-#             "ssmmessages:OpenControlChannel",
-#             "ssmmessages:OpenDataChannel"
-#        ],
-#        "Resource": "*"
-#      }
-#    ]
-#   }
-#   EOF
-# }
+  policy = <<-EOF
+  {
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Effect": "Allow",
+       "Action": [
+            "ssmmessages:CreateControlChannel",
+            "ssmmessages:CreateDataChannel",
+            "ssmmessages:OpenControlChannel",
+            "ssmmessages:OpenDataChannel"
+       ],
+       "Resource": "*"
+     }
+   ]
+  }
+  EOF
+}
