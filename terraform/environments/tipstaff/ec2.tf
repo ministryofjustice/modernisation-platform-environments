9407a7a7a3205b1b97f4290aeb818cdfe2148da1
matthewsearle01 <matthewsearle01@gmail.com>
2023-03-29 10:37:47 +0100

    ecs first draft

--- terraform/environments/tipstaff/ec2.tf
+++ terraform/environments/tipstaff/ec2.tf
@@ -1,48 +1,48 @@
-resource "aws_security_group" "tipstaff_dev_ec2_sc" {
-  name        = "ec2 security group"
-  description = "control access to the ec2 instance"
-  vpc_id      = data.aws_vpc.shared.id
-}
+# resource "aws_security_group" "tipstaff_dev_ec2_sc" {
+#   name        = "ec2 security group"
+#   description = "control access to the ec2 instance"
+#   vpc_id      = data.aws_vpc.shared.id
+# }
 
-resource "aws_security_group_rule" "ingress_traffic" {
-  for_each                 = local.application_data.ec2_sg_rules
-  description              = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
-  from_port                = each.value.from_port
-  protocol                 = each.value.protocol
-  security_group_id        = aws_security_group.tipstaff_dev_ec2_sc.id
-  to_port                  = each.value.to_port
-  type                     = "ingress"
-  source_security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
-}
+# resource "aws_security_group_rule" "ingress_traffic" {
+#   for_each                 = local.application_data.ec2_sg_rules
+#   description              = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
+#   from_port                = each.value.from_port
+#   protocol                 = each.value.protocol
+#   security_group_id        = aws_security_group.tipstaff_dev_ec2_sc.id
+#   to_port                  = each.value.to_port
+#   type                     = "ingress"
+#   source_security_group_id = aws_security_group.tipstaff_dev_lb_sc.id
+# }
 
-resource "aws_security_group_rule" "egress_traffic" {
-  for_each          = local.application_data.ec2_sg_rules
-  description       = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
-  from_port         = each.value.from_port
-  protocol          = each.value.protocol
-  security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
-  to_port           = each.value.to_port
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+# resource "aws_security_group_rule" "egress_traffic" {
+#   for_each          = local.application_data.ec2_sg_rules
+#   description       = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
+#   from_port         = each.value.from_port
+#   protocol          = each.value.protocol
+#   security_group_id = aws_security_group.tipstaff_dev_ec2_sc.id
+#   to_port           = each.value.to_port
+#   type              = "egress"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
-resource "aws_instance" "tipstaff_ec2_instance_dev" {
-  instance_type               = local.application_data.accounts[local.environment].instance_type
-  ami                         = local.application_data.accounts[local.environment].ami_image_id
-  subnet_id                   = data.aws_subnet.private_subnets_a.id
-  vpc_security_group_ids      = [aws_security_group.tipstaff_dev_ec2_sc.id]
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.ec2_access_key.key_name
-  user_data                   = <<-EOF
-              <powershell>
-              Install-WindowsFeature -name Web-Server -IncludeManagementTools
-              New-Item -Path C:\inetpub\wwwroot\index.html -ItemType File -Value "Hello, World!" -Force
-              </powershell>
-              EOF
-  depends_on                  = [aws_security_group.tipstaff_dev_ec2_sc]
-}
+# resource "aws_instance" "tipstaff_ec2_instance_dev" {
+#   instance_type               = local.application_data.accounts[local.environment].instance_type
+#   ami                         = local.application_data.accounts[local.environment].ami_image_id
+#   subnet_id                   = data.aws_subnet.private_subnets_a.id
+#   vpc_security_group_ids      = [aws_security_group.tipstaff_dev_ec2_sc.id]
+#   associate_public_ip_address = true
+#   key_name                    = aws_key_pair.ec2_access_key.key_name
+#   user_data                   = <<-EOF
+#               <powershell>
+#               Install-WindowsFeature -name Web-Server -IncludeManagementTools
+#               New-Item -Path C:\inetpub\wwwroot\index.html -ItemType File -Value "Hello, World!" -Force
+#               </powershell>
+#               EOF
+#   depends_on                  = [aws_security_group.tipstaff_dev_ec2_sc]
+# }
 
-resource "aws_key_pair" "ec2_access_key" {
-  key_name   = "ec2_access_key"
-  public_key = jsondecode(data.aws_secretsmanager_secret_version.public_key.secret_string)["tipstaff_public_key"]
-}
+# resource "aws_key_pair" "ec2_access_key" {
+#   key_name   = "ec2_access_key"
+#   public_key = jsondecode(data.aws_secretsmanager_secret_version.public_key.secret_string)["tipstaff_public_key"]
+# }
---
+++ terraform/environments/tipstaff/ecs.tf
@@ -0,0 +1,232 @@
+# Create ECS cluster
+resource "aws_ecs_cluster" "tipstaff_cluster" {
+  name = "tipstaff_cluster"
+}
+
+# Create a task definition for the Windows container
+resource "aws_ecs_task_definition" "tipstaff_ecs_task" {
+  family                   = "tipstaff_ecs_task"
+  execution_role_arn       = aws_iam_role.app_execution.arn
+  task_role_arn            = aws_iam_role.app_task.arn
+  cpu                      = 256
+  memory                   = 1024
+  container_definitions    = <<TASK_DEFINITION
+  [
+    {
+      "name": "tipstaff",
+      "image": "mcr.microsoft.com/windows/servercore/iis:windowsservercore-ltsc2019",
+      "cpu": 256,
+      "memory": 1024,
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "protocol": "tcp"
+        }
+      ],
+      "LogConfiguration": {
+        "LogDriver": "awslogs",
+        "Options": {
+          "awslogs-group": "${var.networking[0].application}",
+          "awslogs-region": "eu-west-2",
+          "awslogs-stream-prefix": "${var.networking[0].application}"
+        }
+      },
+      "environment" : [
+        {
+          "name" : "DB_HOST",
+          "value" : "${aws_db_instance.tipstaffdbdev.address}"
+        },
+        {
+          "name" : "DB_PORT",
+          "value" : "5432"
+        }
+      ],
+      "secrets": [
+        {
+          "name" : "DB_USER",
+          "valueFrom" : "${aws_secretsmanager_secret_version.db_username.arn}"
+        },
+        {
+          "name": "DB_PASSWORD",
+          "valueFrom": "${aws_secretsmanager_secret_version.db_password.arn}"
+        }
+      ]
+    }
+  ]
+  TASK_DEFINITION
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+}
+
+# Create a Fargate service to run the Windows container task
+resource "aws_ecs_service" "tipstaff_ecs_service" {
+  name = var.networking[0].application
+  deployment_controller {
+    type = "ECS"
+  }
+  cluster                           = aws_ecs_cluster.tipstaff_cluster.id
+  task_definition                   = aws_ecs_task_definition.tipstaff_ecs_task.arn
+  launch_type                       = "FARGATE"
+  enable_execute_command            = true
+  desired_count                     = 1
+  health_check_grace_period_seconds = 120
+
+  network_configuration {
+    subnets          = data.aws_subnets.private-public.ids
+    security_groups  = [aws_security_group.app.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
+    container_name   = var.networking[0].application
+    container_port   = 3000
+  }
+
+}
+
+resource "aws_iam_role" "app_execution" {
+  name = "execution-${var.networking[0].application}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "execution-${var.networking[0].application}"
+    },
+  )
+}
+
+resource "aws_iam_role_policy" "app_execution" {
+  name = "execution-${var.networking[0].application}"
+  role = aws_iam_role.app_execution.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+           "Action": [
+               "logs:CreateLogStream",
+               "logs:PutLogEvents",
+               "ecr:GetAuthorizationToken"
+           ],
+           "Resource": "*",
+           "Effect": "Allow"
+      },
+      {
+            "Action": [
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:BatchGetImage"
+            ],
+              "Resource": "*",
+            "Effect": "Allow"
+      },
+      {
+          "Action": [
+               "secretsmanager:GetSecretValue"
+           ],
+          "Resource": "*",
+          "Effect": "Allow"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role" "app_task" {
+  name = "task-${var.networking[0].application}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "task-${var.networking[0].application}"
+    },
+  )
+}
+
+resource "aws_iam_role_policy" "app_task" {
+  name = "task-${var.networking[0].application}"
+  role = aws_iam_role.app_task.id
+
+  policy = <<-EOF
+  {
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Effect": "Allow",
+       "Action": [
+            "ssmmessages:CreateControlChannel",
+            "ssmmessages:CreateDataChannel",
+            "ssmmessages:OpenControlChannel",
+            "ssmmessages:OpenDataChannel"
+       ],
+       "Resource": "*"
+     }
+   ]
+  }
+  EOF
+}
+
+resource "aws_lb_target_group" "tipstaff_dev_target_group" {
+  name                 = "tipstaff-dev-target-group"
+  port                 = local.application_data.accounts[local.environment].server_port_1
+  protocol             = "HTTP"
+  vpc_id               = data.aws_vpc.shared.id
+  target_type          = "instance"
+  deregistration_delay = 30
+
+  stickiness {
+    type = "lb_cookie"
+  }
+
+  health_check {
+    healthy_threshold   = "2"
+    interval            = "120"
+    protocol            = "HTTP"
+    unhealthy_threshold = "2"
+    matcher             = "200-499"
+    timeout             = "5"
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${local.application_name}-tg-${local.environment}"
+    }
+  )
+}
