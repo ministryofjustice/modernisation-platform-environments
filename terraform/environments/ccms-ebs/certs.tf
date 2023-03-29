84c281f55fa4c79449a0bbfe6b40e33230a30be0
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-29 13:51:56 +0100

    try

--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -39,10 +39,9 @@ resource "aws_acm_certificate" "external-service" {
 resource "aws_route53_record" "external_validation" {
 
   provider  = aws.core-network-services
-  #zone_id   = data.aws_route53_zone.network-services.zone_id
-
   #provider  = aws.core-vpc
   zone_id   = data.aws_route53_zone.external.zone_id
+  #zone_id   = data.aws_route53_zone.network-services.zone_id
 
   for_each = {
     for dvo in local.cert_opts : dvo.domain_name => {
