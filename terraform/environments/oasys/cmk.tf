4875ddbb7a59b41f062bfbb277483f97b9a934ba
W <willg258@hotmail.co.uk>
2023-03-29 10:02:14 +0100

    ..

--- terraform/environments/oasys/cmk.tf
+++ terraform/environments/oasys/cmk.tf
@@ -6,8 +6,6 @@
 #------------------------------------------------------------------------------
 
 
-data "aws_kms_key" "ebs_hmpps" { key_id = "arn:aws:kms:${local.region}:${local.environment_management.account_ids["core-shared-services-production"]}:alias/ebs-${local.business_unit}" }
-
 resource "aws_kms_grant" "image-builder-shared-cmk-grant" {
   name              = "image-builder-shared-cmk-grant"
   key_id            = data.aws_kms_key.ebs_hmpps.arn
