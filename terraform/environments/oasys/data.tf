ee765993e14f168f5d404ce2bc42b62486bf80aa
W <willg258@hotmail.co.uk>
2023-03-30 13:33:22 +0100

    Update data.tf

--- terraform/environments/oasys/data.tf
+++ terraform/environments/oasys/data.tf
@@ -1,18 +1,22 @@
 
-data "aws_iam_policy_document" "user-s3-access" {
-  statement {
-    sid = "user-s3-access"
-    actions = [
-      # "s3:GetObject",
-      # "s3:PutObject",
-      # "s3:PutObjectAcl",
-      # "s3:ListBucket"
-      "s3:*"
-    ]
-    resources = ["${module.s3-bucket.bucket.arn}/*",
-    module.s3-bucket.bucket.arn, ]
-  }
-}
+# data "aws_iam_policy_document" "user-s3-access" {
+#   statement {
+#     sid = "user-s3-access"
+#     actions = [
+#       # "s3:GetObject",
+#       # "s3:PutObject",
+#       # "s3:PutObjectAcl",
+#       # "s3:ListBucket"
+#       "s3:*"
+#     ]
+#     resources = ["${module.s3-bucket.bucket.arn}/*",
+#     module.s3-bucket.bucket.arn, ]
+#     principals {
+#       type = "AWS"
+#       identifiers = "*"
+#     }
+#   }
+# }
 
 
 data "aws_iam_policy_document" "shared_image_builder_cmk_policy" {
