929f88ab787ee99c183e384e42a3decab1cec098
W <willg258@hotmail.co.uk>
2023-03-30 10:45:12 +0100

    ..

--- terraform/environments/oasys/s3.tf
+++ terraform/environments/oasys/s3.tf
@@ -1,53 +1,53 @@
-# module "s3-bucket" { # need to backup files , then baseline this, then upload files
-#   source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.3.0"
+module "s3-bucket" { # need to backup files , then baseline this, then upload files
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.3.0"
 
-#   providers = {
-#     aws.bucket-replication = aws
-#   }
+  providers = {
+    aws.bucket-replication = aws
+  }
 
-#   bucket_prefix       = "${local.application_name}-${local.environment}-"
-#   replication_enabled = false
+  bucket_prefix       = "${local.application_name}-${local.environment}-"
+  replication_enabled = false
 
-#   bucket_policy = [data.aws_iam_policy_document.user-s3-access.json]
+  bucket_policy = [data.aws_iam_policy_document.user-s3-access.json]
 
 
-#   lifecycle_rule = [
-#     {
-#       id      = "main"
-#       enabled = "Enabled"
-#       prefix  = ""
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
 
-#       tags = {
-#         rule      = "log"
-#         autoclean = "true"
-#       }
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
 
-#       transition = [
-#         {
-#           days          = 90
-#           storage_class = "STANDARD_IA"
-#         }
-#       ]
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+        }
+      ]
 
-#       expiration = {
-#         days = 730
-#       }
+      expiration = {
+        days = 730
+      }
 
-#       noncurrent_version_transition = [
-#         {
-#           days          = 90
-#           storage_class = "STANDARD_IA"
-#           }, {
-#           days          = 365
-#           storage_class = "GLACIER"
-#         }
-#       ]
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
 
-#       noncurrent_version_expiration = {
-#         days = 730
-#       }
-#     }
-#   ]
-#   tags = local.tags
-# }
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+  tags = local.tags
+}
 
