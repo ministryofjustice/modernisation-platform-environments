d0c5b10e85e6d059794321171d98f1b795211a48
David Elliott <david.elliott@digital.justice.gov.uk>
2023-03-30 10:05:53 +0100

    Switch to dynamic provider role assumption
    Moving MP controlled apps to dynamic provider role assumption for
    testing.  See issue https://github.com/ministryofjustice/modernisation-platform/issues/2943

--- terraform/environments/example/platform_providers.tf
+++ terraform/environments/example/platform_providers.tf
@@ -1,13 +1,14 @@
-# AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
+# AWS provider for the original session which you connect with
 provider "aws" {
   alias  = "original-session"
   region = "eu-west-2"
 }
 
+# AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
 provider "aws" {
   region = "eu-west-2"
   assume_role {
-    role_arn = can(regex("modernisation-platform-developer", data.aws_iam_session_context.whoami.issuer_arn)) ? null : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
+    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
   }
 }
 
@@ -20,12 +21,12 @@ provider "aws" {
   }
 }
 
-# AWS provider for core-vpc-<environment>, to share VPCs into this account
+# AWS provider for core-vpc-<environment>, to access resources in the core-vpc accounts
 provider "aws" {
   alias  = "core-vpc"
   region = "eu-west-2"
   assume_role {
-    role_arn = can(regex("modernisation-platform-developer", data.aws_iam_session_context.whoami.issuer_arn)) ? "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/member-delegation-read-only" : "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/member-delegation-${local.vpc_name}-${local.environment}"
+    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/member-delegation-read-only" : "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/member-delegation-${local.vpc_name}-sandbox"
   }
 }
 
@@ -34,10 +35,11 @@ provider "aws" {
   alias  = "core-network-services"
   region = "eu-west-2"
   assume_role {
-    role_arn = can(regex("modernisation-platform-developer", data.aws_iam_session_context.whoami.issuer_arn)) ? "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/read-dns-records" : "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/modify-dns-records"
+    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/read-dns-records" : "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/modify-dns-records"
   }
 }
 
+# Provider for creating resources in us-east-1, eg ACM resources for CloudFront
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
--- terraform/environments/sprinkler/platform_base_variables.tf
+++ terraform/environments/sprinkler/platform_base_variables.tf
@@ -7,5 +7,5 @@ variable "networking" {
 variable "collaborator_access" {
   type        = string
   default     = "developer"
-  description = "Collaborators must specify which access level they are using"
+  description = "Collaborators must specify which access level they are using, eg set an environment variable of export TF_VAR_collaborator_access=migration"
 }
