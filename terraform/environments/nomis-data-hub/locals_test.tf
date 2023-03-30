1d95d9a14e5ff8e6440ebcb60b673c5e6ca25abe
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2023-03-30 09:15:41 +0100

    DSOS-1506:update_ndh_ami_and_asg

--- terraform/environments/nomis-data-hub/locals_test.tf
+++ terraform/environments/nomis-data-hub/locals_test.tf
@@ -183,28 +183,28 @@ locals {
         }
       }
 
-      lb_target_groups = {
-        http-7777 = {
-          port                 = 7777
-          protocol             = "HTTP"
-          target_type          = "instance"
-          deregistration_delay = 30
-          health_check = {
-            enabled             = true
-            interval            = 30
-            healthy_threshold   = 3
-            matcher             = "200-399"
-            path                = "/"
-            port                = 7777
-            timeout             = 5
-            unhealthy_threshold = 5
-          }
-          stickiness = {
-            enabled = true
-            type    = "lb_cookie"
-          }
-        }
-      }
+      #      lb_target_groups = {
+      #        http-7777 = {
+      #          port                 = 7777
+      #          protocol             = "HTTP"
+      #          target_type          = "instance"
+      #          deregistration_delay = 30
+      #          health_check = {
+      #            enabled             = true
+      #            interval            = 30
+      #            healthy_threshold   = 3
+      #            matcher             = "200-399"
+      #            path                = "/"
+      #            port                = 7777
+      #            timeout             = 5
+      #            unhealthy_threshold = 5
+      #          }
+      #          stickiness = {
+      #            enabled = true
+      #            type    = "lb_cookie"
+      #          }
+      #        }
+      #      }
     }
   }
 }
