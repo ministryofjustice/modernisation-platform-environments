6720b21e4291b57528e53f5b847a9c5faa213e7d
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-03-31 12:01:00 +0100

    remove old wildcard cert

--- terraform/environments/nomis/baseline_presets.tf
+++ terraform/environments/nomis/baseline_presets.tf
@@ -5,7 +5,7 @@ module "baseline_presets" {
   ip_addresses = module.ip_addresses
 
   options = {
-    enable_application_environment_wildcard_cert = true
+    enable_application_environment_wildcard_cert = false
     enable_business_unit_kms_cmks                = true
     enable_image_builder                         = true
     enable_ec2_cloud_watch_agent                 = true
