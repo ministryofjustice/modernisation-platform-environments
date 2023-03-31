96d7bd0c570b84008607a3ada99eab1724e32c93
James Borwick <james.borwick@icloud.com>
2023-03-31 16:40:28 +0100

    Making default view

--- terraform/environments/oasys/main.tf
+++ terraform/environments/oasys/main.tf
@@ -73,11 +73,12 @@ module "baseline_presets" {
   }
 }
 
+# --- AWS Resource Explorer ---
 resource "aws_resourceexplorer2_index" "this" {
   type = "LOCAL"
 }
 
-resource "aws_resourceexplorer2_view" "example" {
+resource "aws_resourceexplorer2_view" "all_resources" {
   name         = "all-resources"
   default_view = true
   depends_on   = [aws_resourceexplorer2_index.this]
