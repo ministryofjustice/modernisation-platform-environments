fc0880e70796016d5e7250a8fdebb4f356b963ab
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-03-31 10:04:50 +0100

    nothing major, just push so I can change branch

--- terraform/environments/ccms-ebs/certs.tf
+++ terraform/environments/ccms-ebs/certs.tf
@@ -55,7 +55,7 @@ resource "aws_route53_record" "external_validation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id   = data.aws_route53_zone.network-services.zone_id
+  zone_id         = data.aws_route53_zone.network-services.zone_id
 }
 
 resource "aws_acm_certificate_validation" "external" {
