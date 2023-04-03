065131569fff2fea49eded452fdc031a376f1326
matthewsearle01 <matthewsearle01@gmail.com>
2023-04-03 12:27:22 +0100

    update container port

--- terraform/environments/tipstaff/ecs.tf
+++ terraform/environments/tipstaff/ecs.tf
@@ -84,7 +84,7 @@ resource "aws_ecs_service" "tipstaff_ecs_service" {
   load_balancer {
     target_group_arn = aws_lb_target_group.tipstaff_dev_target_group.arn
     container_name   = "tipstaff-container"
-    container_port   = 3000
+    container_port   = 80
   }
 
 }
