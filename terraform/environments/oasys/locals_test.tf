a28055de0a37ded22522d599227bccb5e35710b0
W <willg258@hotmail.co.uk>
2023-04-04 17:05:25 +0100

    ..

--- terraform/environments/oasys/locals_test.tf
+++ terraform/environments/oasys/locals_test.tf
@@ -33,7 +33,7 @@ locals {
         bucket_policy_v2 = [
           module.baseline_presets.s3_bucket_policies.ImageBuilderWriteAccessBucketPolicy,
           module.baseline_presets.s3_bucket_policies.DevTestEnvironmentsWriteAndDeleteAccessBucketPolicy,
-          module.baseline_presets.s3_bucket_policies.DevTestAccountsWriteAndDeleteAccessBucketPolicy
+          module.baseline_presets.s3_bucket_policies.DevTestAccountsWriteAndDeleteAccessBucketPolicy2
         ]
         iam_policies = module.baseline_presets.s3_iam_policies
       }
