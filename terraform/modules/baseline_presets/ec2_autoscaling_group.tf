ec1b6dd6e9dccebf608aa26d8132aff0c7f2b4e2
W <willg258@hotmail.co.uk>
2023-04-05 14:38:28 +0100

    ..

--- terraform/modules/baseline_presets/ec2_autoscaling_group.tf
+++ terraform/modules/baseline_presets/ec2_autoscaling_group.tf
@@ -1,7 +1,7 @@
 locals {
 
   ec2_autoscaling_group = {
-    min_size            = 1
+    min_size            = 0
     desired_capacity    = 1
     max_size            = 2
     vpc_zone_identifier = var.environment.subnets["private"].ids
