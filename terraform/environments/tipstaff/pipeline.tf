841846491d4640ab26e8553736781896fdf12a09
matthewsearle01 <matthewsearle01@gmail.com>
2023-04-05 09:33:40 +0100

    add blue green deployment config

--- terraform/environments/tipstaff/pipeline.tf
+++ terraform/environments/tipstaff/pipeline.tf
@@ -229,6 +229,27 @@ resource "aws_codedeploy_deployment_group" "tipstaff_deployment_group" {
   deployment_group_name   = "tipstaff-deployment-group"
   service_role_arn        = aws_iam_role.codedeploy_role.arn
 
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
   ecs_service {
     cluster_name = aws_ecs_cluster.tipstaff_cluster.name
     service_name = aws_ecs_service.tipstaff_ecs_service.name
