28c0bf01d34d191edfaeec486a0624983f734fed
matthewsearle01 <matthewsearle01@gmail.com>
2023-04-05 09:28:33 +0100

    update provider to assume role

--- terraform/environments/tipstaff/providers.tf
+++ terraform/environments/tipstaff/providers.tf
@@ -1,4 +1,7 @@
 provider "aws" {
-  alias  = "eu-west-1"
   region = "eu-west-1"
+  alias  = "eu-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
+  }
 }
