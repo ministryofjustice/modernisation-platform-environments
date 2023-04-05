58d2b26c19e4a6184f417683fe859ebbc5564d36
W <willg258@hotmail.co.uk>
2023-04-05 11:08:16 +0100

    ..

--- terraform/environments/oasys/locals.tf
+++ terraform/environments/oasys/locals.tf
@@ -232,28 +232,7 @@ locals {
         description = "ASMSNMP password"
       }
     }
-    lb_target_groups = {
-      http-8080 = {
-        port                 = 8080
-        protocol             = "HTTP"
-        target_type          = "instance"
-        deregistration_delay = 30
-        health_check = {
-          enabled             = true
-          interval            = 30
-          healthy_threshold   = 3
-          matcher             = "200-399"
-          path                = "/"
-          port                = 8080
-          timeout             = 5
-          unhealthy_threshold = 5
-        }
-        stickiness = {
-          enabled = true
-          type    = "lb_cookie"
-        }
-      }
-    }
+    lb_target_groups = {}
     # Example target group setup below
     # lb_target_groups = local.lb_target_groups # This won't be correct for db, will correct later
   }
