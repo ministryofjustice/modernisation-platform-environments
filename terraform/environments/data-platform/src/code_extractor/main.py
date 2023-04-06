6324c2c1abb56d0ed1716dc2c7234a8a52baf0d8
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-04-06 13:01:15 +0100

    Formatting python file

--- terraform/environments/data-platform/src/code_extractor/main.py
+++ terraform/environments/data-platform/src/code_extractor/main.py
@@ -1,20 +1,19 @@
-
 import boto3
 import zipfile
 import io
 
-s3 = boto3.resource('s3')
+s3 = boto3.resource("s3")
+
 
 def handler(event, context):
-    
+
     print("event", event)
-    print("bucketname:", event['detail']['bucket']['name'])
-    print("Key:", event['detail']['object']['key'])
-  
+    print("bucketname:", event["detail"]["bucket"]["name"])
+    print("Key:", event["detail"]["object"]["key"])
 
     # specify the bucket name and the key of the zip file
-    bucket_name = event['detail']['bucket']['name']
-    zip_key = event['detail']['object']['key']
+    bucket_name = event["detail"]["bucket"]["name"]
+    zip_key = event["detail"]["object"]["key"]
 
     # get the zip file object from S3
     zip_obj = s3.Object(bucket_name, zip_key)
@@ -28,15 +27,14 @@ def handler(event, context):
 
     # specify the output bucket name and prefix for the unzipped files
     output_bucket_name = bucket_name
-    output_prefix = 'code/output_script/'
+    output_prefix = "code/output_script/"
 
     # iterate through each file in the zip file
     for file in zipfile_object.namelist():
         # read the file from the zip file
         file_data = zipfile_object.read(file)
-       
+
         # write the file to the output bucket
         s3.Object(output_bucket_name, output_prefix + file).put(Body=file_data)
-        
+
     print("File saved")
- 
