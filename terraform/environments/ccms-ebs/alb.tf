e7579cd79d7bfeb000ba703622fe60b29f577484
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-04-12 13:21:04 +0100

    rename var for apps target group

--- terraform/environments/ccms-ebs/alb.tf
+++ terraform/environments/ccms-ebs/alb.tf
@@ -37,11 +37,11 @@ resource "aws_lb_listener" "ebsapps_listener" {
 
 resource "aws_lb_target_group" "ebsapp_tg" {
   name     = lower(format("tg-%s-%s-ebsapp", local.application_name, local.environment))
-  port     = local.application_data.accounts[local.environment].tg_port
+  port     = local.application_data.accounts[local.environment].tg_apps_port
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.shared.id
   health_check {
-    port     = local.application_data.accounts[local.environment].tg_port
+    port     = local.application_data.accounts[local.environment].tg_apps_port
     protocol = "HTTP"
   }
 }
@@ -50,5 +50,5 @@ resource "aws_lb_target_group_attachment" "ebsapps" {
   count            = local.application_data.accounts[local.environment].accessgate_no_instances
   target_group_arn = aws_lb_target_group.ebsapp_tg.arn
   target_id        = element(aws_instance.ec2_ebsapps.*.id, count.index)
-  port             = local.application_data.accounts[local.environment].tg_port
+  port             = local.application_data.accounts[local.environment].tg_apps_port
 }
