39f8ab45e246a9485a6b8e97d166ad2869fd68a2
martytaggart-laa <119940130+martytaggart-laa@users.noreply.github.com>
2023-04-13 09:46:55 +0100

    Ccms/wgate name (#1936)
    * rebuild with new AMIs
    
    * whitespace
    
    * shorten webgate name for alb
    
    * remove listener and tg
    
    * remove alb, listener and tg
    
    * remove listener and tg
    
    * change deletion protection to false - temporary
    
    * revert name and protection off

--- terraform/environments/ccms-ebs/alb.tf
+++ terraform/environments/ccms-ebs/alb.tf
@@ -62,7 +62,7 @@ resource "aws_lb" "webgate_lb" {
   security_groups    = [aws_security_group.sg_webgate_lb.id]
   subnets            = data.aws_subnets.private-public.ids
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   # access_logs {
   #   bucket  = module.s3-bucket-logging.bucket.id
@@ -74,7 +74,7 @@ resource "aws_lb" "webgate_lb" {
     { Name = lower(format("lb-%s-%s-webgate", local.application_name, local.environment)) }
   )
 }
-
+/*
 resource "aws_lb_listener" "webgate_listener" {
   depends_on = [
     aws_acm_certificate_validation.external
@@ -93,7 +93,7 @@ resource "aws_lb_listener" "webgate_listener" {
 }
 
 resource "aws_lb_target_group" "webgate_tg" {
-  name     = lower(format("tg-%s-%s-webgate", local.application_name, local.environment))
+  name     = lower(format("tg-%s-%s-wgate", local.application_name, local.environment))
   port     = 5401
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.shared.id
@@ -109,3 +109,4 @@ resource "aws_lb_target_group_attachment" "webgate" {
   target_id        = element(aws_instance.ec2_webgate.*.id, count.index)
   port             = 5401
 }
+*/
