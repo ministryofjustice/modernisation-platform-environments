d1c9b7500fee6edea11c92ddebd11e70c69909a4
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-04-13 15:15:54 +0100

    jitbit app monitoring

---
+++ terraform/environments/delius-jitbit/monitoring_app.tf
@@ -0,0 +1,17 @@
+resource aws_cloudwatch_log_group app_logs {
+  name              = "delius-jitbit-ecs"
+  retention_in_days = 30
+}
+
+// log metric filter for error logs in container that contain the word error or exception
+resource "aws_cloudwatch_log_metric_filter" "error" {
+  name           = "jitbit-application-error"
+  pattern        = "Error in Helpdesk"
+  log_group_name = aws_cloudwatch_log_group.app_logs.name
+
+  metric_transformation {
+    name      = "ErrorCount"
+    namespace = "JitbitMetrics"
+    value     = "1"
+  }
+}
