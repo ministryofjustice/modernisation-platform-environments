1b17f81d6547200c6d7cb0cbba7fabd4fd8efd67
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-04-13 16:31:51 +0100

    add alarm for custom error log metric that triggers if there are more than 5 errors in 5 minutes

--- terraform/environments/delius-jitbit/monitoring_app.tf
+++ terraform/environments/delius-jitbit/monitoring_app.tf
@@ -1,4 +1,4 @@
-resource aws_cloudwatch_log_group app_logs {
+resource "aws_cloudwatch_log_group" "app_logs" {
   name              = "delius-jitbit-app"
   retention_in_days = 30
 
@@ -16,4 +16,19 @@ resource "aws_cloudwatch_log_metric_filter" "error" {
     namespace = "JitbitMetrics"
     value     = "1"
   }
+}
+
+resource "aws_cloudwatch_metric_alarm" "jitbit_high_error_volume" {
+  alarm_name          = "jitbit-high-error-count"
+  alarm_description   = "Triggers alarm if there are more than 5 errors in the last 5 minutes"
+  namespace           = "JitbitMetrics"
+  metric_name         = "ErrorCount"
+  statistic           = "Sum"
+  period              = "300"
+  evaluation_periods  = "1"
+  alarm_actions       = [aws_sns_topic.jitbit_alerting.arn]
+  ok_actions          = [aws_sns_topic.jitbit_alerting.arn]
+  threshold           = "5"
+  treat_missing_data  = "missing"
+  comparison_operator = "GreaterThanThreshold"
 }
