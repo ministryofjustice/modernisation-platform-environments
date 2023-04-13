e5d311acb0aa828e5e625d424e6ef32020b090c7
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-04-13 15:24:03 +0100

    add tags to log group

--- terraform/environments/delius-jitbit/monitoring_app.tf
+++ terraform/environments/delius-jitbit/monitoring_app.tf
@@ -1,6 +1,8 @@
 resource aws_cloudwatch_log_group app_logs {
   name              = "delius-jitbit-app"
   retention_in_days = 30
+
+  tags = local.tags
 }
 
 // log metric filter for error logs in container that contain the word error or exception
