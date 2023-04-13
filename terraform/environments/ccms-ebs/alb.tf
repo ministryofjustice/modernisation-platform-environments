45c8382e8db3002f240dad6fd77fb6486a6f73f2
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-04-13 13:01:00 +0100

    remove dependency

--- terraform/environments/ccms-ebs/alb.tf
+++ terraform/environments/ccms-ebs/alb.tf
@@ -19,9 +19,9 @@ resource "aws_lb" "ebsapps_lb" {
 }
 
 resource "aws_lb_listener" "ebsapps_listener" {
-  depends_on = [
-    aws_acm_certificate_validation.external
-  ]
+  #depends_on = [
+  #  aws_acm_certificate_validation.external
+  #]
 
   load_balancer_arn = aws_lb.ebsapps_lb.arn
   port              = "443"
