07616fff1eb3cfc98dee54519de6f8e334a19c72
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-04-14 13:59:50 +0100

    add back in old sg

--- terraform/environments/nomis/locals-test.tf
+++ terraform/environments/nomis/locals-test.tf
@@ -169,7 +169,10 @@ locals {
         force_destroy_bucket     = true
         idle_timeout             = 3600
         public_subnets           = module.environment.subnets["private"].ids
-        security_groups          = ["private-lb"]
+        security_groups = [
+          aws_security_group.public.id, # TODO: remove once weblogic servers refreshed
+          "private-lb",
+        ]
 
         listeners = {
           https = merge(
