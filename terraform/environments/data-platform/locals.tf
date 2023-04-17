9a4aec5f0fa0ef369765f039e4c52a46e592e970
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-04-17 17:07:03 +0100

    Added new lambda to trigger glue job

--- terraform/environments/data-platform/locals.tf
+++ terraform/environments/data-platform/locals.tf
@@ -4,12 +4,22 @@ locals {
   ##
   # Variables code_extractor lambda
   ##
-  function_name               = "code_extractor"
-  function_handler            = "main.handler"
-  function_runtime            = "python3.9"
-  function_timeout_in_seconds = 15
+  code_function_name               = "code_extractor"
+  code_function_handler            = "main.handler"
+  code_function_runtime            = "python3.9"
+  code_function_timeout_in_seconds = 15
 
-  function_source_dir = "${path.module}/src/${local.function_name}"
+  code_function_source_dir = "${path.module}/src/${local.code_function_name}"
+
+  ##
+  # Variables data_extractor lambda
+  ##
+  data_function_name               = "data_extractor"
+  data_function_handler            = "main.handler"
+  data_function_runtime            = "python3.9"
+  data_function_timeout_in_seconds = 15
+
+  data_function_source_dir = "${path.module}/src/${local.data_function_name}"
 
   ##
   # Variables for glue job
