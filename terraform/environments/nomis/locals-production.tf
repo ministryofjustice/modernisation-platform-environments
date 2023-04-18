76cf5be5182f16e6e2606b0893bc522cf541e425
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-04-18 17:01:21 +0100

    new preprod and prod certs for nomis (#1994)

--- terraform/environments/nomis/locals-production.tf
+++ terraform/environments/nomis/locals-production.tf
@@ -136,6 +136,27 @@ locals {
 
   # baseline config
   production_config = {
+
+    baseline_acm_certificates = {
+      nomis_wildcard_cert = {
+        # domain_name limited to 64 chars so use modernisation platform domain for this
+        # and put the wildcard in the san
+        domain_name = module.environment.domains.public.modernisation_platform
+        subject_alternate_names = [
+          "*.${module.environment.domains.public.application_environment}",
+          "*.${local.environment}.nomis.service.justice.gov.uk",
+          "*.${local.environment}.nomis.az.justice.gov.uk",
+          "*.nomis.service.justice.gov.uk",
+          "*.nomis.az.justice.gov.uk",
+        ]
+        external_validation_records_created = false
+        # cloudwatch_metric_alarms            = module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso"].acm_default
+        tags = {
+          description = "wildcard cert for nomis ${local.environment} domains"
+        }
+      }
+    }
+
     baseline_ec2_autoscaling_groups = {
       prod-nomis-web-a = merge(local.ec2_weblogic_zone_a, {
         tags = merge(local.ec2_weblogic_zone_a.tags, {
