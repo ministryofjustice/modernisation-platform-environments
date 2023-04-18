5ebada53e34bbb6dc73a517a6f0f7a96fff86c01
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-04-18 17:19:51 +0100

    update preprod and prod certs

--- terraform/environments/nomis/locals-production.tf
+++ terraform/environments/nomis/locals-production.tf
@@ -149,8 +149,8 @@ locals {
           "*.nomis.service.justice.gov.uk",
           "*.nomis.az.justice.gov.uk",
         ]
-        external_validation_records_created = false
-        # cloudwatch_metric_alarms            = module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso"].acm_default
+        external_validation_records_created = true
+        cloudwatch_metric_alarms            = module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso"].acm_default
         tags = {
           description = "wildcard cert for nomis ${local.environment} domains"
         }
