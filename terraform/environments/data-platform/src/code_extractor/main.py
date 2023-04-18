7a72da5cfa4045512635634d380c8a48cf331dce
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-04-18 13:12:16 +0100

    Improved pathing

--- terraform/environments/data-platform/src/code_extractor/main.py
+++ terraform/environments/data-platform/src/code_extractor/main.py
@@ -5,7 +5,7 @@ import os
 import logging
 
 logging.getLogger().setLevel(logging.INFO)
-s3 = boto3.resource("s3")
+s3 = boto3.client("s3")
 
 
 def handler(event, context):
@@ -15,14 +15,15 @@ def handler(event, context):
     # specify the bucket name and the key of the zip file
     bucket_name = event["detail"]["requestParameters"]["bucketName"]
     zip_key = event["detail"]["requestParameters"]["key"]
+
     logging.info(f"bucketname: {bucket_name}")
     logging.info(f"Key: {zip_key}")
 
     # get the zip file object from S3
-    zip_obj = s3.Object(bucket_name, zip_key)
+    zip_obj = s3.get_object(Bucket=bucket_name, Key=zip_key)
 
     # read the contents of the zip file
-    buffer = io.BytesIO(zip_obj.get()["Body"].read())
+    buffer = io.BytesIO(zip_obj.get("Body").read())
     zipfile_object = zipfile.ZipFile(buffer)
 
     # specify the output bucket name and prefix for the unzipped files
@@ -34,12 +35,13 @@ def handler(event, context):
     for file in zipfile_object.namelist():
         # read the file from the zip file
         file_data = zipfile_object.open(file)
+        output_key = os.path.join(output_prefix, file)
 
         # write the file to the output bucket
         try:
-            s3.upload_fileobj(file_data, output_bucket_name, output_prefix)
+            s3.upload_fileobj(file_data, output_bucket_name, output_key)
         except Exception as e:
             logging.error(e)
-        logging.info(f"{file} successfully extracted")
+        logging.info(f"{file} successfully uploaded")
 
     logging.info("Unzip completed")
