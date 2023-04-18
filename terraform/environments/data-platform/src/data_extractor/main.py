fc634621fb210c6a2d11a20d3f9c91ebcdb7d41c
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-04-18 14:57:50 +0100

    Added extra-py-files

--- terraform/environments/data-platform/src/data_extractor/main.py
+++ terraform/environments/data-platform/src/data_extractor/main.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import boto3
 import logging
 import os
+from pathlib import Path
 
 logging.getLogger().setLevel(logging.INFO)
 glue = boto3.client("glue")
@@ -17,8 +18,16 @@ def lambda_handler(event, context):
         file_key = event["detail"]["requestParameters"]["key"]
         logging.info(f"Bucket name: {bucket_name}")
         logging.info(f"File key: {file_key}")
+        project = Path(file_key).parts[1]
+        transform_file_path = os.path.join(
+            "code", project, "extracted", "application", "transform.py"
+        )
 
-        args = {"--bucketName": bucket_name, "--key": file_key}
+        args = {
+            "--bucketName": bucket_name,
+            "--key": file_key,
+            "--extra-py-files": transform_file_path,
+        }
         runId = glue.start_job_run(JobName=gluejobname, Arguments=args)
 
         status = glue.get_job_run(JobName=gluejobname, RunId=runId["JobRunId"])
