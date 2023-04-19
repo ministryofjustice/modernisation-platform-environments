8fcdadc5e8c249a00b770522f1f1a4c6732ad4e8
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-04-19 16:28:09 +0100

    Split lambdas into files, refactor locals

---
+++ terraform/environments/data-platform/lambda_for_data.tf
@@ -0,0 +1,101 @@
+data "archive_file" "data_extractor_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src/data_extractor_${local.environment}"
+  output_path = "${path.module}/src/data_extractor_${local.environment}/data_extractor_lambda.zip"
+}
+
+resource "aws_lambda_function" "data_extractor" {
+  function_name    = "data_extractor_${local.environment}"
+  description      = "Lambda to extract code and store in another location"
+  handler          = "main.handler"
+  runtime          = local.lambda_runtime
+  timeout          = local.lambda_timeout_in_seconds
+  filename         = data.archive_file.data_extractor_zip.output_path
+  source_code_hash = data.archive_file.data_extractor_zip.output_base64sha256
+  publish          = true
+  role             = aws_iam_role.data_lambda_role.arn
+  depends_on       = [aws_iam_role_policy_attachment.attach_data_lambda_policy_to_iam_role]
+  environment {
+    variables = {
+      ENVIRONMENT = local.environment
+      GLUE_JOB_NAME = "placeholder"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda_trust_policy_doc" {
+  statement {
+    sid     = "LambdaAssumeRole"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "data_lambda_role" {
+  name               = "${local.data_function_name}-role-${local.environment}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust_policy_doc.json
+}
+
+data "aws_iam_policy_document" "iam_policy_document_for_data_lambda" {
+  statement {
+    sid       = "LambdaLogGroup"
+    effect    = "Allow"
+    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+  statement {
+    sid       = "GETPUTBucketAccess"
+    effect    = "Allow"
+    actions   = ["s3:GetObject*", "s3:PutObject*"]
+    resources = ["${module.s3-bucket.bucket.arn}/raw_data/*"]
+  }
+}
+
+resource "aws_iam_policy" "data_lambda_policy" {
+  name        = "${local.data_function_name}-policy-${local.environment}"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = data.aws_iam_policy_document.iam_policy_document_for_data_lambda.json
+
+}
+
+resource "aws_iam_role_policy_attachment" "attach_data_lambda_policy_to_iam_role" {
+  role       = aws_iam_role.code_lambda_role.name
+  policy_arn = aws_iam_policy.data_lambda_policy.arn
+}
+
+resource "aws_cloudwatch_event_rule" "put_to_data_directory" {
+  name = "put_to_data_directory"
+
+  event_pattern = jsonencode({
+    "source" : ["aws.s3"],
+    "detail-type" : ["AWS API Call via CloudTrail"],
+    "detail" : {
+      "eventSource" : ["s3.amazonaws.com"],
+      "eventName" : ["PutObject"],
+      "requestParameters" : {
+        "bucketName" : [module.s3-bucket.bucket.id],
+        "key" : [{ "prefix" : "raw_data/" }]
+      }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "data_directory_lambda_trigger" {
+  rule      = aws_cloudwatch_event_rule.put_to_data_directory.name
+  target_id = "data"
+  arn       = aws_lambda_function.data_extractor.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_data_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.data_extractor.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.put_to_data_directory.arn
+}
+
--- terraform/environments/data-platform/locals.tf
+++ terraform/environments/data-platform/locals.tf
@@ -1,23 +1,5 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
-
-  ##
-  # Variables code_extractor lambda
-  ##
-  code_function_name               = "code_extractor"
-  code_function_handler            = "main.handler"
-  code_function_runtime            = "python3.9"
-  code_function_timeout_in_seconds = 15
-
-  code_function_source_dir = "${path.module}/src/${local.code_function_name}"
-
-  ##
-  # Variables data_extractor lambda
-  ##
-  data_function_name               = "data_extractor"
-  data_function_handler            = "main.handler"
-  data_function_runtime            = "python3.9"
-  data_function_timeout_in_seconds = 15
-
-  data_function_source_dir = "${path.module}/src/${local.data_function_name}"
+  lambda_runtime            = "python3.9"
+  lambda_timeout_in_seconds = 15
 }
