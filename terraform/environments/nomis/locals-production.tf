53a23f5a7042e71d2b1b14474b355a0c8905eec3
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-04-20 17:31:59 +0100

    Nomis: DSOS-1847: move DB instances into baseline (#2018)
    * move databases to baseline
    
    * remove state move
    
    * fixes

--- terraform/environments/nomis/locals-production.tf
+++ terraform/environments/nomis/locals-production.tf
@@ -15,97 +15,7 @@ locals {
     }
 
     # Add database instances here. They will be created using ec2-database.tf
-    databases = {
-      # Naming
-      # *-nomis-db-1: NOMIS, NDH, TRDATA
-      # *-nomis-db-2: MIS, AUDIT
-      # *-nomis-db-3: HA
-
-      # NOTE: this is temporarily under prod account while we wait for network connectivity
-      preprod-nomis-db-2 = {
-        tags = {
-          nomis-environment = "preprod"
-          server-type       = "nomis-db"
-          description       = "PreProduction NOMIS MIS and Audit database to replace Azure PPPDL00017"
-          oracle-sids       = "PPCNMAUD"
-          monitored         = true
-        }
-        ami_name  = "nomis_rhel_7_9_oracledb_11_2_release_2022-10-03T12-51-25.032Z"
-        ami_owner = "self" # remove this line next time AMI is updated so core-shared-services-production used instead
-        instance = {
-          instance_type             = "r6i.2xlarge"
-          disable_api_termination   = true
-          metadata_endpoint_enabled = "enabled"
-        }
-        # reduce sdc to 1000 when we move into preprod subscription
-        ebs_volumes = {
-          "/dev/sdb" = { size = 100 }
-          "/dev/sdc" = { size = 5120 }
-        }
-        ebs_volume_config = {
-          data  = { total_size = 4000 }
-          flash = { total_size = 1000 }
-        }
-        sns_topic = "nomis_alarms"
-      }
-
-      prod-nomis-db-2 = {
-        tags = {
-          nomis-environment        = "prod"
-          server-type              = "nomis-db"
-          description              = "Production NOMIS MIS and Audit database to replace Azure PDPDL00036 and PDPDL00038"
-          oracle-sids              = "CNMAUD"
-          monitored                = true
-          fixngo-connection-target = "10.40.0.136"
-        }
-        ami_name  = "nomis_rhel_7_9_oracledb_11_2_release_2022-10-07T12-48-08.562Z"
-        ami_owner = "self" # remove this line next time AMI is updated so core-shared-services-production used instead
-        instance = {
-          instance_type             = "r6i.2xlarge"
-          disable_api_termination   = true
-          metadata_endpoint_enabled = "enabled"
-        }
-        ebs_volumes = {
-          "/dev/sdb" = { size = 100 }
-          "/dev/sdc" = {
-            size = 3000
-            iops = 9000
-          }
-        }
-        ebs_volume_config = {
-          data  = { total_size = 4000 }
-          flash = { total_size = 1000 }
-        }
-        sns_topic                = "nomis_alarms"
-        cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso"].fixngo_connection
-      }
-
-      prod-nomis-db-3 = {
-        tags = {
-          nomis-environment = "prod"
-          server-type       = "nomis-db"
-          description       = "Production NOMIS HA database to replace Azure PDPDL00062"
-          monitored         = true
-          oracle-sids       = "PCNOMHA"
-        }
-        ami_name  = "nomis_rhel_7_9_oracledb_11_2_release_2022-10-07T12-48-08.562Z"
-        ami_owner = "self" # remove this line next time AMI is updated so core-shared-services-production used instead
-        instance = {
-          instance_type             = "r6i.4xlarge"
-          disable_api_termination   = true
-          metadata_endpoint_enabled = "enabled"
-        }
-        ebs_volumes = {
-          "/dev/sdb" = { size = 100 }
-          "/dev/sdc" = { size = 1000 }
-        }
-        ebs_volume_config = {
-          data  = { total_size = 3000, iops = 3750, throughput = 750 }
-          flash = { total_size = 500 }
-        }
-        sns_topic = "nomis_alarms"
-      }
-    }
+    databases = {}
 
     # Add weblogic instances here.  They will be created using the weblogic module
     weblogics       = {}
@@ -162,6 +72,74 @@ locals {
           oracle-db-hostname = "PDPDL00035.azure.hmpp.root"
           nomis-environment  = "prod"
           oracle-db-name     = "CNOMP"
+        })
+      })
+    }
+
+    baseline_ec2_instances = {
+      preprod-nomis-db-2 = merge(local.database_zone_a, {
+        tags = merge(local.database_zone_a.tags, {
+          nomis-environment = "preprod"
+          description       = "PreProduction NOMIS MIS and Audit database to replace Azure PPPDL00017"
+          oracle-sids       = "PPCNMAUD"
+        })
+        config = merge(local.database_zone_a.config, {
+          ami_name = "nomis_rhel_7_9_oracledb_11_2_release_2022-10-03T12-51-25.032Z"
+        })
+        instance = merge(local.database_zone_a.instance, {
+          instance_type = "r6i.2xlarge"
+        })
+        ebs_volumes = merge(local.database_zone_a.ebs_volumes, {
+          # reduce sdc to 1000 when we move into preprod subscription
+          "/dev/sdb" = { label = "app", size = 100 }
+          "/dev/sdc" = { label = "app", size = 5120 }
+        })
+        ebs_volume_config = merge(local.database_zone_a.ebs_volume_config, {
+          data  = { total_size = 4000 }
+          flash = { total_size = 1000 }
+        })
+      })
+
+      prod-nomis-db-2 = merge(local.database_zone_a, {
+        tags = merge(local.database_zone_a.tags, {
+          nomis-environment        = "prod"
+          description              = "Production NOMIS MIS and Audit database to replace Azure PDPDL00036 and PDPDL00038"
+          oracle-sids              = "CNMAUD"
+          fixngo-connection-target = "10.40.0.136"
+        })
+        instance = merge(local.database_zone_a.instance, {
+          instance_type = "r6i.2xlarge"
+        })
+        ebs_volumes = merge(local.database_zone_a.ebs_volumes, {
+          "/dev/sdb" = { label = "app", size = 100 }
+          "/dev/sdc" = { label = "app", size = 3000, iops = 9000 }
+        })
+        ebs_volume_config = merge(local.database_zone_a.ebs_volume_config, {
+          data  = { total_size = 4000 }
+          flash = { total_size = 1000 }
+        })
+        cloudwatch_metric_alarms = merge(
+          local.database_zone_a.cloudwatch_metric_alarms,
+          module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso"].fixngo_connection
+        )
+      })
+
+      prod-nomis-db-3 = merge(local.database_zone_a, {
+        tags = merge(local.database_zone_a.tags, {
+          nomis-environment = "prod"
+          description       = "Production NOMIS HA database to replace Azure PDPDL00062"
+          oracle-sids       = "PCNOMHA"
+        })
+        instance = merge(local.database_zone_a.instance, {
+          instance_type = "r6i.4xlarge"
+        })
+        ebs_volumes = merge(local.database_zone_a.ebs_volumes, {
+          "/dev/sdb" = { label = "app", size = 100 }
+          "/dev/sdc" = { label = "app", size = 1000 }
+        })
+        ebs_volume_config = merge(local.database_zone_a.ebs_volume_config, {
+          data  = { total_size = 3000, iops = 3750, throughput = 750 }
+          flash = { total_size = 500 }
         })
       })
     }
