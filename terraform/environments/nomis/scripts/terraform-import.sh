5e533f580f6cdb7e4645a5956d823a76ed09a34e
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-04-21 12:31:17 +0100

    remove comment

--- terraform/environments/nomis/scripts/terraform-import.sh
+++ terraform/environments/nomis/scripts/terraform-import.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo terraform import 'module.baseline.aws_iam_service_linked_role.this["autoscaling.amazonaws.com"]' "arn:aws:iam::${ACCOUNT_NUMBER}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+terraform import 'module.baseline.aws_iam_service_linked_role.this["autoscaling.amazonaws.com"]' "arn:aws:iam::${ACCOUNT_NUMBER}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
