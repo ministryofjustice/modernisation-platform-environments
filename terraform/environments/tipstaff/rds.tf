911ae9e590cb3d7743fa1610fec89d944d13a552
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-04-24 12:09:38 +0100

    attempt no 2 to get the github actions pipeline IP address

--- terraform/environments/tipstaff/rds.tf
+++ terraform/environments/tipstaff/rds.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "postgresql_db_sc" {
     to_port     = 5432
     protocol    = "tcp"
     description = "Allows Github Actions to access RDS"
-    cidr_blocks = [local.pipeline_ip]
+    cidr_blocks = ["${data.external.current_ip.result.ip}/32"]
   }
   egress {
     description = "allow all outbound traffic"
@@ -61,7 +61,7 @@ data "external" "pipeline_ip" {
 }
 
 output "pipeline_ip" {
-  value = join("", data.external.pipeline_ip.result.ip, "/32")
+  value = data.external.pipeline_ip.result.ip
 }
 
 resource "null_resource" "setup_db" {
