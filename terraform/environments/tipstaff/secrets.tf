fa5a7c2f404daeccb88b5493eff30ca2c75754bd
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-04-24 16:46:18 +0100

    rebuild everything, clean up secrets

--- terraform/environments/tipstaff/secrets.tf
+++ terraform/environments/tipstaff/secrets.tf
@@ -1,28 +1,16 @@
 #### This file can be used to store secrets specific to the member account ####
-data "aws_secretsmanager_secret" "tipstaff-dev-db-secrets" {
+data "aws_secretsmanager_secret" "tipstaff_dev_db_secrets" {
   arn = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:tipstaff-dev-db-secrets-8Qc18f"
 }
 
-data "aws_secretsmanager_secret" "tipstaff_public_key" {
-  arn = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:tipstaff_public_key-B1zpNE"
+data "aws_secretsmanager_secret" "tactical_products_db_secrets" {
+  arn = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:tactical-products-db-secrets-ox3sNi"
 }
 
-data "aws_secretsmanager_secret_version" "public_key" {
-  secret_id = data.aws_secretsmanager_secret.tipstaff_public_key.id
+data "aws_secretsmanager_secret_version" "db_credentials" {
+  secret_id = data.aws_secretsmanager_secret.tipstaff_dev_db_secrets.id
 }
 
-data "aws_secretsmanager_secret_version" "db_username" {
-  secret_id = data.aws_secretsmanager_secret.tipstaff-dev-db-secrets.id
-}
-
-data "aws_secretsmanager_secret_version" "db_password" {
-  secret_id = data.aws_secretsmanager_secret.tipstaff-dev-db-secrets.id
-}
-
-data "aws_secretsmanager_secret" "github_oauth_token" {
-  arn = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:Github_Oauth_Token-kApArG"
-}
-
-data "aws_secretsmanager_secret_version" "oauth_token" {
-  secret_id = data.aws_secretsmanager_secret.github_oauth_token.id
+data "aws_secretsmanager_secret_version" "dms_source_credentials" {
+  secret_id = data.aws_secretsmanager_secret.tactical_products_db_secrets.id
 }
