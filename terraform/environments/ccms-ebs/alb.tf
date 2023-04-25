8f905b464a29d395aaf914df4b25bc7246ed65e4
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2023-04-25 17:08:39 +0100

    CC-1711: Change ALB from internal to public facing and creation of WAF with trust

--- terraform/environments/ccms-ebs/alb.tf
+++ terraform/environments/ccms-ebs/alb.tf
@@ -18,23 +18,23 @@ resource "aws_lb" "ebsapps_lb" {
   )
 }
 
-# resource "aws_lb_listener" "ebsapps_listener" {
-#   count       = local.is-production ? 0 : 1
-#   depends_on  = [
-#     aws_acm_certificate_validation.external
-#   ]
+resource "aws_lb_listener" "ebsapps_listener" {
+  count       = local.is-production ? 0 : 1
+  depends_on  = [
+    aws_acm_certificate_validation.external
+  ]
 
-#   load_balancer_arn = aws_lb.ebsapps_lb.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = local.cert_arn
+  load_balancer_arn = aws_lb.ebsapps_lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = local.cert_arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ebsapp_tg.id
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ebsapp_tg.id
+  }
+}
 
 resource "aws_lb_target_group" "ebsapp_tg" {
   name     = lower(format("tg-%s-%s-ebsapp", local.application_name, local.environment))
@@ -47,12 +47,12 @@ resource "aws_lb_target_group" "ebsapp_tg" {
   }
 }
 
-# resource "aws_lb_target_group_attachment" "ebsapps" {
-#   count            = local.application_data.accounts[local.environment].accessgate_no_instances
-#   target_group_arn = aws_lb_target_group.ebsapp_tg.arn
-#   target_id        = element(aws_instance.ec2_ebsapps.*.id, count.index)
-#   port             = local.application_data.accounts[local.environment].tg_apps_port
-# }
+resource "aws_lb_target_group_attachment" "ebsapps" {
+  count            = local.application_data.accounts[local.environment].accessgate_no_instances
+  target_group_arn = aws_lb_target_group.ebsapp_tg.arn
+  target_id        = element(aws_instance.ec2_ebsapps.*.id, count.index)
+  port             = local.application_data.accounts[local.environment].tg_apps_port
+}
 
 resource "aws_wafv2_web_acl_association" "ebs_waf_association" {
   resource_arn = aws_lb.ebsapps_lb.arn
