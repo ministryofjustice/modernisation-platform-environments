40d3b6d5e58aada7cbc8bd52edc9762b0197abf2
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-04-25 18:19:30 +0100

    Split files, had to use locals.tf more however.

--- terraform/environments/example/ec2.tf
+++ terraform/environments/example/ec2.tf
@@ -2,44 +2,7 @@
 #------------------------Comment out file if not required----------------------------------
 ###########################################################################################
 
-locals {
-  public_key_data = jsondecode(file("./bastion_linux.json"))
-}
-
-# tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
-module "bastion_linux" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v3.0.6"
-
-  providers = {
-    aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
-    aws.share-tenant = aws          # The default provider (unaliased, `aws`) is the tenant
-  }
-  # s3 - used for logs and user ssh public keys
-  bucket_name          = "bastion-example"
-  bucket_versioning    = true
-  bucket_force_destroy = true
-  # public keys
-  public_key_data = local.public_key_data.keys[local.environment]
-  # logs
-  log_auto_clean       = "Enabled"
-  log_standard_ia_days = 30  # days before moving to IA storage
-  log_glacier_days     = 60  # days before moving to Glacier
-  log_expiry_days      = 180 # days before log expiration
-  # bastion
-  allow_ssh_commands = false
-  app_name      = var.networking[0].application
-  business_unit = local.vpc_name
-  subnet_set    = local.subnet_set
-  environment   = local.environment
-  region        = "eu-west-2"
-
-  # Tags
-  tags_common = local.tags
-  tags_prefix = terraform.workspace
-}
-
-
-# First build the security group for the EC2
+# EC2 Sec Group
 resource "aws_security_group" "example_ec2_sg" {
   name        = "example_ec2_sg"
   description = "Controls access to EC2"
@@ -100,122 +63,6 @@ resource "aws_instance" "develop" {
 }
 
 
-
-#------------------------------------------------------------------------------
-# EC2 Instances following naming convention
-#------------------------------------------------------------------------------
-
-locals {
-  region              = "eu-west-2"
-  availability_zone_1 = "eu-west-2a"
-  availability_zone_2 = "eu-west-2b"
-  autoscaling_schedules_default = {
-    "scale_up" = {
-      recurrence = "0 7 * * Mon-Fri"
-    }
-    "scale_down" = {
-      desired_capacity = 0
-      recurrence       = "0 19 * * Mon-Fri"
-    }
-  }
-  ec2_test = {
-    tags = {
-      component = "test"
-    }
-
-    instance = {
-      disable_api_termination      = false
-      instance_type                = "t3.medium"
-      key_name                     = aws_key_pair.ec2-user.key_name
-      monitoring                   = false
-      metadata_options_http_tokens = "required"
-      vpc_security_group_ids       = [aws_security_group.example_ec2_sg.id]
-    }
-
-    user_data_cloud_init = {
-      args = {
-        lifecycle_hook_name  = "ready-hook"
-        branch               = "main"
-        ansible_repo         = "modernisation-platform-configuration-management"
-        ansible_repo_basedir = "ansible"
-        ansible_args         = "--tags ec2provision"
-      }
-      scripts = [
-        "install-ssm-agent.sh.tftpl",
-        "ansible-ec2provision.sh.tftpl",
-        "post-ec2provision.sh.tftpl"
-      ]
-    }
-
-    route53_records = {
-      create_internal_record = true
-      create_external_record = false
-    }
-
-    # user can manually increase the desired capacity to 1 via CLI/console
-    # to create an instance
-    autoscaling_group = {
-      desired_capacity = 0
-      max_size         = 2
-      min_size         = 0
-    }
-
-    ec2_test_instances = {
-      # Remove data.aws_kms_key from cmk.tf once the NDH servers are removed
-      example-test-instance-1 = {
-        tags = {
-          server-type = "private"
-          description = "Standalone EC2 for testing RHEL7.9 NDH App"
-          monitored   = false
-          os-type     = "Linux"
-          component   = "ndh"
-          environment = "test"
-        }
-        ebs_volumes = {
-          "/dev/sda1" = { kms_key_id = data.aws_kms_key.default_ebs.arn }
-        }
-        ami_name  = "RHEL-7.9_HVM-*"
-        ami_owner = "309956199498"
-      }
-      example-test-instance-2 = {
-        tags = {
-          server-type = "private"
-          description = "Standalone EC2 for testing RHEL7.9 NDH EMS"
-          monitored   = false
-          os-type     = "Linux"
-          component   = "ndh"
-          environment = "test"
-        }
-        ebs_volumes = {
-          "/dev/sda1" = { kms_key_id = data.aws_kms_key.default_ebs.arn }
-        }
-        ami_name  = "RHEL-7.9_HVM-*"
-        ami_owner = "309956199498"
-      }
-    }
-    ec2_test_autoscaling_groups = {
-      dev-redhat-rhel79 = {
-        tags = {
-          description = "For testing official RedHat RHEL7.9 image"
-          monitored   = false
-          os-type     = "Linux"
-          component   = "test"
-        }
-        instance = {
-          instance_type                = "t2.medium"
-          metadata_options_http_tokens = "optional"
-        }
-        ami_name  = "RHEL-7.9_HVM-*"
-        ami_owner = "309956199498"
-      }
-    }
-  }
-}
-
-data "aws_kms_key" "default_ebs" {
-  key_id = "alias/aws/ebs"
-}
-
 # create single managed policy
 resource "aws_iam_policy" "ec2_common_policy" {
   name        = "ec2-common-policy"
@@ -242,6 +89,14 @@ locals {
   ec2_common_managed_policies = [
     aws_iam_policy.ec2_common_policy.arn
   ]
+    instance = {
+      disable_api_termination      = false
+      instance_type                = "t3.medium"
+      key_name                     = try(aws_key_pair.ec2-user.key_name)
+      monitoring                   = false
+      metadata_options_http_tokens = "required"
+      vpc_security_group_ids       = try([aws_security_group.example_ec2_sg.id])
+    }
 }
 
 # custom policy for SSM as managed policy AmazonSSMManagedInstanceCore is too permissive
@@ -280,7 +135,7 @@ module "ec2_test_instance" {
   name = each.key
   ami_name                      = each.value.ami_name
   ami_owner                     = try(each.value.ami_owner, "core-shared-services-production")
-  instance                      = merge(local.ec2_test.instance, lookup(each.value, "instance", {}))
+  instance                      = merge(local.instance, lookup(each.value, "instance", {}))
   ebs_volumes_copy_all_from_ami = try(each.value.ebs_volumes_copy_all_from_ami, true)
   ebs_kms_key_id                = module.environment.kms_keys["ebs"].arn
   ebs_volume_config             = lookup(each.value, "ebs_volume_config", {})
@@ -301,39 +156,6 @@ module "ec2_test_instance" {
   tags                     = merge(local.tags, local.ec2_test.tags, try(each.value.tags, {}))
   account_ids_lookup       = local.environment_management.account_ids
   cloudwatch_metric_alarms = {}
-}
-
-module "ec2_test_autoscaling_group" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-autoscaling-group"
-
-  providers = {
-    aws.core-vpc = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
-  }
-
-  for_each = try(local.ec2_test.ec2_test_autoscaling_groups, {})
-
-  name = each.key
-
-  ami_name                      = each.value.ami_name
-  ami_owner                     = try(each.value.ami_owner, "core-shared-services-production")
-  instance                      = merge(local.ec2_test.instance, lookup(each.value, "instance", {}))
-  ebs_volumes_copy_all_from_ami = try(each.value.ebs_volumes_copy_all_from_ami, true)
-  ebs_kms_key_id                = module.environment.kms_keys["ebs"].arn
-  ebs_volume_config             = lookup(each.value, "ebs_volume_config", {})
-  ebs_volumes                   = lookup(each.value, "ebs_volumes", {})
-  ssm_parameters_prefix         = lookup(each.value, "ssm_parameters_prefix", "test/")
-  ssm_parameters                = lookup(each.value, "ssm_parameters", null)
-  autoscaling_group             = merge(local.ec2_test.autoscaling_group, lookup(each.value, "autoscaling_group", {}))
-  autoscaling_schedules         = lookup(each.value, "autoscaling_schedules", local.autoscaling_schedules_default)
-
-  iam_resource_names_prefix = "ec2-test-asg"
-  instance_profile_policies = local.ec2_common_managed_policies
-  application_name          = local.application_name
-  region                    = local.region
-  subnet_ids                = module.environment.subnets["private"].ids
-  tags                      = merge(local.tags, local.ec2_test.tags, try(each.value.tags, {}))
-  account_ids_lookup        = local.environment_management.account_ids
-  cloudwatch_metric_alarms  = {}
 }
 
 # Volumes built for use by EC2.
--- terraform/environments/example/locals.tf
+++ terraform/environments/example/locals.tf
@@ -2,3 +2,139 @@
 locals {
   business_unit = var.networking[0].business-unit
 }
+
+locals {
+  region              = "eu-west-2"
+  availability_zone_1 = "eu-west-2a"
+  availability_zone_2 = "eu-west-2b"
+  autoscaling_schedules_default = {
+    "scale_up" = {
+      recurrence = "0 7 * * Mon-Fri"
+    }
+    "scale_down" = {
+      desired_capacity = 0
+      recurrence       = "0 19 * * Mon-Fri"
+    }
+  }
+  ec2_test = {
+    tags = {
+      component = "test"
+    }
+
+    user_data_cloud_init = {
+      args = {
+        lifecycle_hook_name  = "ready-hook"
+        branch               = "main"
+        ansible_repo         = "modernisation-platform-configuration-management"
+        ansible_repo_basedir = "ansible"
+        ansible_args         = "--tags ec2provision"
+      }
+      scripts = [
+        "install-ssm-agent.sh.tftpl",
+        "ansible-ec2provision.sh.tftpl",
+        "post-ec2provision.sh.tftpl"
+      ]
+    }
+
+    route53_records = {
+      create_internal_record = true
+      create_external_record = false
+    }
+
+    # user can manually increase the desired capacity to 1 via CLI/console
+    # to create an instance
+    autoscaling_group = {
+      desired_capacity = 0
+      max_size         = 2
+      min_size         = 0
+    }
+
+    ec2_test_instances = {
+      # Remove data.aws_kms_key from cmk.tf once the NDH servers are removed
+      example-test-instance-1 = {
+        tags = {
+          server-type = "private"
+          description = "Standalone EC2 for testing RHEL7.9 NDH App"
+          monitored   = false
+          os-type     = "Linux"
+          component   = "ndh"
+          environment = "test"
+        }
+        ebs_volumes = {
+          "/dev/sda1" = { kms_key_id = data.aws_kms_key.default_ebs.arn }
+        }
+        ami_name  = "RHEL-7.9_HVM-*"
+        ami_owner = "309956199498"
+      }
+      example-test-instance-2 = {
+        tags = {
+          server-type = "private"
+          description = "Standalone EC2 for testing RHEL7.9 NDH EMS"
+          monitored   = false
+          os-type     = "Linux"
+          component   = "ndh"
+          environment = "test"
+        }
+        ebs_volumes = {
+          "/dev/sda1" = { kms_key_id = data.aws_kms_key.default_ebs.arn }
+        }
+        ami_name  = "RHEL-7.9_HVM-*"
+        ami_owner = "309956199498"
+      }
+    }
+    ec2_test_autoscaling_groups = {
+      dev-redhat-rhel79 = {
+        tags = {
+          description = "For testing official RedHat RHEL7.9 image"
+          monitored   = false
+          os-type     = "Linux"
+          component   = "test"
+        }
+        instance = {
+          instance_type                = "t2.medium"
+          metadata_options_http_tokens = "optional"
+        }
+        ami_name  = "RHEL-7.9_HVM-*"
+        ami_owner = "309956199498"
+      }
+    }
+  }
+}
+
+# Has to be in the locals else
+data "aws_kms_key" "default_ebs" {
+  key_id = "alias/aws/ebs"
+}
+
+# Build ingress and egress rules
+locals {
+  loadbalancer_ingress_rules = {
+    "cluster_ec2_lb_ingress" = {
+      description     = "Cluster EC2 loadbalancer ingress rule"
+      from_port       = 80
+      to_port         = 80
+      protocol        = "tcp"
+      cidr_blocks     = [data.aws_vpc.shared.cidr_block]
+      security_groups = []
+    },
+    "cluster_ec2_bastion_ingress" = {
+      description     = "Cluster EC2 bastion ingress rule"
+      from_port       = 3389
+      to_port         = 3389
+      protocol        = "tcp"
+      cidr_blocks     = [data.aws_vpc.shared.cidr_block]
+      security_groups = []
+    }
+  }
+
+  loadbalancer_egress_rules = {
+    "cluster_ec2_lb_egress" = {
+      description     = "Cluster EC2 loadbalancer egress rule"
+      from_port       = 443
+      to_port         = 443
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  }
+}
