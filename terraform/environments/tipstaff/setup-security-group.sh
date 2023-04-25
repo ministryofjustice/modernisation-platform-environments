068c7bc045edb7edbe6798924839261fd7d5e7d4
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-04-25 15:48:38 +0100

    print aws cli version

--- terraform/environments/tipstaff/setup-security-group.sh
+++ terraform/environments/tipstaff/setup-security-group.sh
@@ -4,4 +4,6 @@ export AWS_ACCESS_KEY_ID=$DMS_TARGET_ACCOUNT_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$DMS_TARGET_ACCOUNT_SECRET_KEY
 export AWS_DEFAULT_REGION=$DMS_TARGET_ACCOUNT_REGION
 
+echo "aws version:" | aws --version
+
 aws rds modify-db-instance --db-instance-identifier postgresql-staging --vpc-security-group-ids sg-08244ba362f922899 sg-0e0f5cf0883f81945 sg-04e9fe073afcc6b65 ${DMS_SECURITY_GROUP}
