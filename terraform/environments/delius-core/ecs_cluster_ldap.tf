a5151c210c2c93cbff91d60c59ed53af0eea6fd9
George Taylor <george.taylor@digital.justice.gov.uk>
2023-04-25 16:17:36 +0100

    rm s3

--- terraform/environments/delius-core/ecs_cluster_ldap.tf
+++ terraform/environments/delius-core/ecs_cluster_ldap.tf
@@ -7,52 +7,52 @@ module "ecs" {
   tags = local.tags
 }
 
-# Create s3 bucket for deployment state
-module "s3_bucket_app_deployment" {
+# # Create s3 bucket for deployment state
+# module "s3_bucket_app_deployment" {
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.2.0"
+#   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.2.0"
 
-  providers = {
-    aws.bucket-replication = aws
-  }
-  bucket_name        = "${local.application_name}-${local.environment}-openldap-deployment"
-  versioning_enabled = true
+#   providers = {
+#     aws.bucket-replication = aws
+#   }
+#   bucket_name        = "${local.application_name}-${local.environment}-openldap-deployment"
+#   versioning_enabled = true
 
-  lifecycle_rule = [
-    {
-      id      = "main"
-      enabled = "Enabled"
-      prefix  = ""
+#   lifecycle_rule = [
+#     {
+#       id      = "main"
+#       enabled = "Enabled"
+#       prefix  = ""
 
-      tags = {
-        rule      = "log"
-        autoclean = "true"
-      }
+#       tags = {
+#         rule      = "log"
+#         autoclean = "true"
+#       }
 
-      noncurrent_version_transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
+#       noncurrent_version_transition = [
+#         {
+#           days          = 90
+#           storage_class = "STANDARD_IA"
+#           }, {
+#           days          = 365
+#           storage_class = "GLACIER"
+#         }
+#       ]
 
-      noncurrent_version_expiration = {
-        days = 730
-      }
-    }
-  ]
+#       noncurrent_version_expiration = {
+#         days = 730
+#       }
+#     }
+#   ]
 
-  tags = local.tags
-}
-resource "aws_s3_bucket_ownership_controls" "openldap" {
-  bucket = module.s3_bucket_app_deployment.bucket.id
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
+#   tags = local.tags
+# }
+# resource "aws_s3_bucket_ownership_controls" "openldap" {
+#   bucket = module.s3_bucket_app_deployment.bucket.id
+#   rule {
+#     object_ownership = "BucketOwnerEnforced"
+#   }
+# }
 
 resource "aws_security_group" "openldap" {
   vpc_id      = data.aws_vpc.shared.id
@@ -89,6 +89,6 @@ resource "aws_cloudwatch_log_group" "openldap" {
   retention_in_days = 30
 }
 
-output "s3_bucket_app_deployment_name" {
-  value = module.s3_bucket_app_deployment.bucket.id
-}
+# output "s3_bucket_app_deployment_name" {
+#   value = module.s3_bucket_app_deployment.bucket.id
+# }
