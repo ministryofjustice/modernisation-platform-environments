06e67c2a015231d20ac57bb5efd8f6bd2bbf33ba
LavMatt <mattlaverty@gmail.com>
2023-04-25 09:01:35 +0100

    adding bucket to dump athena query results

--- terraform/environments/data-platform/s3.tf
+++ terraform/environments/data-platform/s3.tf
@@ -102,3 +102,63 @@ data "aws_iam_policy_document" "bucket_policy" {
     }
   }
 }
+
+
+module "s3_athena_query_results_bucket" { #tfsec:ignore:aws-s3-enable-versioning
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.3.0"
+
+  bucket_prefix      = "athena-query-results-${data.aws_caller_identity.current.account_id}"
+  versioning_enabled = true
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 30
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 90
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 365
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 30
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 90
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 365
+      }
+    }
+  ]
+
+  tags = local.tags
+}
