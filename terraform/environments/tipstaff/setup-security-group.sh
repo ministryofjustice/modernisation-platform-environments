31a2d460db4483c900e739347f61c3c9fba5f049
matthewsearle01 <matthewsearle01@gmail.com>
2023-04-25 14:36:46 +0100

    add echo commands to debug aws credentials

--- terraform/environments/tipstaff/setup-security-group.sh
+++ terraform/environments/tipstaff/setup-security-group.sh
@@ -4,6 +4,10 @@ export AWS_ACCESS_KEY_ID=$DMS_TARGET_ACCOUNT_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$DMS_TARGET_ACCOUNT_SECRET_KEY
 export AWS_REGION=$DMS_TARGET_ACCOUNT_REGION
 
+echo "AWS_ACCESS_KEY_ID = $DMS_TARGET_ACCOUNT_ACCESS_KEY"
+echo "AWS_SECRET_ACCESS_KEY = $DMS_TARGET_ACCOUNT_SECRET_KEY"
+echo "AWS_REGION = $DMS_TARGET_ACCOUNT_REGION"
+
 aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile dts-legacy-apps-user &&
 aws configure set aws_secret_access_key "$AWS_ACCESS_KEY_SECRET" --profile dts-legacy-apps-user &&
 aws configure set region "$AWS_REGION" --profile dts-legacy-apps-user &&
