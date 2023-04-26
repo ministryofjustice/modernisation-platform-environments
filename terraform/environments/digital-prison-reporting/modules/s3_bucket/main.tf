dbc224b2b53bbcf085701449fcf4b5bc388bdc4b
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-04-26 23:59:11 +0100

    Add S3 Versinoning Feature

--- terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
+++ terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
@@ -22,6 +22,15 @@ resource "aws_s3_bucket_acl" "acl" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "storage" {
+  bucket = aws_s3_bucket.storage[0].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
   count  = var.enable_lifecycle ? 1 : 0
   bucket = aws_s3_bucket.storage[0].id
