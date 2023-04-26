7ad94f991ed83b1536a9dbda2cd512fa2d0e6bfb
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-04-26 09:24:04 +0100

    Cleanup and Fix Terraform Resources

--- terraform/environments/digital-prison-reporting/main.tf
+++ terraform/environments/digital-prison-reporting/main.tf
@@ -23,7 +23,7 @@ locals {
   create_datamart            = local.application_data.accounts[local.environment].setup_redshift
   redshift_cluster_name      = "${local.application_data.accounts[local.environment].project_short_id}-redshift-${local.environment}"
   kinesis_stream_ingestor    = "${local.application_data.accounts[local.environment].project_short_id}-kinesis-ingestor-${local.environment}"
-  kinesis_stream_data_domain = "${local.application_data.accounts[local.environment].project_short_id}-kinesis-data-domain-${local.environment}"
+# DPR-378 #  kinesis_stream_data_domain = "${local.application_data.accounts[local.environment].project_short_id}-kinesis-data-domain-${local.environment}"
   kinesis_endpoint           = "https://kinesis.eu-west-2.amazonaws.com"
 
   all_tags = merge(
@@ -137,85 +137,87 @@ module "glue_cloudplatform_reporting_job" {
   }
 }
 
+# REMOVE - (DPR-378)
 # Glue Kinesis Reader Job (DPR-340)
-module "glue_kinesis_reader_job" {
-  source                        = "./modules/glue_job"
-  create_job                    = local.create_job
-  name                          = "${local.project}-kinesis-reader-${local.env}"
-  description                   = "kinesis Reader Job"
-  job_language                  = "scala"
-  command_type                  = "gluestreaming"
-  create_security_configuration = local.create_sec_conf
-  temp_dir                      = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/tmp/reporting-hub/"
-  checkpoint_dir                = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/checkpoint/reporting-hub/"
-  spark_event_logs              = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/spark-logs/reporting-hub/"
-  enable_continuous_log_filter  = false
-  project_id                    = local.project
-  aws_kms_key                   = local.s3_kms_arn
-  create_kinesis_ingester       = local.create_kinesis # If True, Kinesis Policies are applied - Defaults to True
-  additional_policies           = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_admin_arn
-  timeout                       = 2880 # This is in Mins
-  execution_class               = "STANDARD"
-  # Placeholder Script Location
-  script_location = "s3://${local.project}-artifact-store-${local.environment}/artifacts/cloud-platform/digital-prison-reporting-jobs/scripts/${local.project}-kinesis-reader-vLatest.scala"
+#module "glue_kinesis_reader_job" {
+#  source                        = "./modules/glue_job"
+#  create_job                    = local.create_job
+#  name                          = "${local.project}-kinesis-reader-${local.env}"
+#  description                   = "kinesis Reader Job"
+#  job_language                  = "scala"
+#  command_type                  = "gluestreaming"
+#  create_security_configuration = local.create_sec_conf
+#  temp_dir                      = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/tmp/reporting-hub/"
+#  checkpoint_dir                = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/checkpoint/reporting-hub/"
+#  spark_event_logs              = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/spark-logs/reporting-hub/"
+#  enable_continuous_log_filter  = false
+#  project_id                    = local.project
+#  aws_kms_key                   = local.s3_kms_arn
+#  create_kinesis_ingester       = local.create_kinesis # If True, Kinesis Policies are applied - Defaults to True
+#  additional_policies           = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_admin_arn
+#  timeout                       = 2880 # This is in Mins
+#  execution_class               = "STANDARD"
+#  # Placeholder Script Location
+#  script_location = "s3://${local.project}-artifact-store-${local.environment}/artifacts/cloud-platform/digital-prison-reporting-jobs/scripts/${local.project}-kinesis-reader-vLatest.scala"
 
-  class = "uk.gov.justice.digital.job.DataHubJob"
+#  class = "uk.gov.justice.digital.job.DataHubJob"
 
-  tags = merge(
-    local.all_tags,
-    {
-      Name          = "${local.project}-kinesis-reader-${local.env}"
-      Resource_Type = "Glue Job"
-      Ticket        = "DPR-340"
-    }
-  )
+#  tags = merge(
+#    local.all_tags,
+#    {
+#      Name          = "${local.project}-kinesis-reader-${local.env}"
+#      Resource_Type = "Glue Job"
+#      Ticket        = "DPR-340"
+#    }
+#  )
 
-  arguments = {
-    "--extra-jars"                          = "s3://${local.project}-artifact-store-${local.environment}/artifacts/cloud-platform/digital-prison-reporting-jobs/jars/digital-prison-reporting-jobs-vLatest.jar"
-    "--job-bookmark-option"                 = "job-bookmark-disable"
-    "--enable-metrics"                      = true
-    "--enable-spark-ui"                     = false
-    "--enable-job-insights"                 = true
-    "--kinesis.reader.streamName"           = "${local.project}-kinesis-reader-${local.env}-stream"
-    "--aws.kinesis.endpointUrl"             = "https://kinesis-${local.account_region}.amazonaws.com"
-    "--aws.region"                          = local.account_region
-    "--kinesis.reader.batchDurationSeconds" = 1
-    "--class"                               = "uk.gov.justice.digital.job.DataHubJob"
-  }
-}
+#  arguments = {
+#    "--extra-jars"                          = "s3://${local.project}-artifact-store-${local.environment}/artifacts/cloud-platform/digital-prison-reporting-jobs/jars/digital-prison-reporting-jobs-vLatest.jar"
+#    "--job-bookmark-option"                 = "job-bookmark-disable"
+#    "--enable-metrics"                      = true
+#    "--enable-spark-ui"                     = false
+#    "--enable-job-insights"                 = true
+#    "--kinesis.reader.streamName"           = "${local.project}-kinesis-reader-${local.env}-stream"
+#    "--aws.kinesis.endpointUrl"             = "https://kinesis-${local.account_region}.amazonaws.com"
+#    "--aws.region"                          = local.account_region
+#    "--kinesis.reader.batchDurationSeconds" = 1
+#    "--class"                               = "uk.gov.justice.digital.job.DataHubJob"
+#  }
+#}
 
+# REMOVE - (DPR-378)
 # Glue Domain Platform Change Monitor Job
-module "glue_domainplatform_change_monitor_job" {
-  source                        = "./modules/glue_job"
-  create_job                    = local.create_job
-  name                          = "${local.project}-domain-platform-table-change-monitor-${local.env}"
-  description                   = "Monitors the reporting hub for table changes and applies them to domains"
-  create_security_configuration = local.create_sec_conf
-  job_language                  = "scala"
-  temp_dir                      = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/tmp/change-monitor/"
-  checkpoint_dir                = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/checkpoint/change-monitor/"
-  spark_event_logs              = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/spark-logs/change-monitor/"
-  tags                          = local.all_tags
-  script_location               = "s3://${local.project}-artifact-store-${local.environment}/artifacts/domain-platform/digital-prison-reporting-poc/domain-platform-table-change-monitor-vLatest.scala"
-  enable_continuous_log_filter  = false
-  project_id                    = local.project
-  aws_kms_key                   = local.s3_kms_arn
-  create_kinesis_ingester       = local.create_kinesis # If True, Kinesis Policies are applied
-  additional_policies           = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_read_only_arn
-  timeout                       = 120
-  execution_class               = "FLEX"
-  arguments = {
-    "--extra-jars"          = "s3://${local.project}-artifact-store-${local.environment}/artifacts/domain-platform/digital-prison-reporting-poc/domain-platform-vLatest.jar"
-    "--class"               = "GlueApp"
-    "--cloud.platform.path" = "s3://${module.s3_curated_bucket[0].bucket.id}"
-    "--domain.files.path"   = "s3://${module.s3_domain_config_bucket[0].bucket.id}/"
-    "--domain.repo.path"    = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/domain-repo/" ## Added /
-    "--source.queue"        = "domain-cdc-event-notification"                                ## DPR-287, needs right source - TBC
-    "--source.region"       = local.account_region
-    "--target.path"         = "s3://${module.s3_domain_bucket[0].bucket.id}/" # Added /
-    "--checkpoint.location" = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/checkpoint/change-monitor/"
-  }
-}
+#module "glue_domainplatform_change_monitor_job" {
+#  source                        = "./modules/glue_job"
+#  create_job                    = local.create_job
+#  name                          = "${local.project}-domain-platform-table-change-monitor-${local.env}"
+#  description                   = "Monitors the reporting hub for table changes and applies them to domains"
+#  create_security_configuration = local.create_sec_conf
+#  job_language                  = "scala"
+#  temp_dir                      = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/tmp/change-monitor/"
+#  checkpoint_dir                = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/checkpoint/change-monitor/"
+#  spark_event_logs              = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/spark-logs/change-monitor/"
+#  tags                          = local.all_tags
+#  script_location               = "s3://${local.project}-artifact-store-${local.environment}/artifacts/domain-platform/digital-prison-reporting-poc/domain-platform-table-change-monitor-vLatest.scala"
+#  enable_continuous_log_filter  = false
+#  project_id                    = local.project
+#  aws_kms_key                   = local.s3_kms_arn
+#  create_kinesis_ingester       = local.create_kinesis # If True, Kinesis Policies are applied
+#  additional_policies           = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_read_only_arn
+#  timeout                       = 120
+#  execution_class               = "FLEX"
+#  arguments = {
+#    "--extra-jars"          = "s3://${local.project}-artifact-store-${local.environment}/artifacts/domain-platform/digital-prison-reporting-poc/domain-platform-vLatest.jar"
+#    "--class"               = "GlueApp"
+#    "--cloud.platform.path" = "s3://${module.s3_curated_bucket[0].bucket.id}"
+#    "--domain.files.path"   = "s3://${module.s3_domain_config_bucket[0].bucket.id}/"
+#    "--domain.repo.path"    = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/domain-repo/" ## Added /
+#    "--source.queue"        = "domain-cdc-event-notification"                                ## DPR-287, needs right source - TBC
+#    "--source.region"       = local.account_region
+#    "--target.path"         = "s3://${module.s3_domain_bucket[0].bucket.id}/" # Added /
+#    "--checkpoint.location" = "s3://${module.s3_glue_jobs_bucket[0].bucket.id}/checkpoint/change-monitor/"
+#  }
+#}
 
 # Glue Domain Platform Refresh Job
 module "glue_domainplatform_refresh_job" {
@@ -270,51 +272,53 @@ module "kinesis_stream_ingestor" {
   )
 }
 
+# REMOVE - (DPR-378)
 # kinesis Domain Data Stream
-module "kinesis_stream_domain_data" {
-  source                    = "./modules/kinesis_stream"
-  create_kinesis_stream     = local.create_kinesis
-  name                      = local.kinesis_stream_data_domain
-  shard_count               = 1 # Not Valid when ON-DEMAND Mode
-  retention_period          = 24
-  shard_level_metrics       = ["IncomingBytes", "OutgoingBytes"]
-  enforce_consumer_deletion = false
-  encryption_type           = "KMS"
-  kms_key_id                = local.kinesis_kms_id
-  project_id                = local.project
+#module "kinesis_stream_domain_data" {
+#  source                    = "./modules/kinesis_stream"
+#  create_kinesis_stream     = local.create_kinesis
+#  name                      = local.kinesis_stream_data_domain
+#  shard_count               = 1 # Not Valid when ON-DEMAND Mode
+#  retention_period          = 24
+#  shard_level_metrics       = ["IncomingBytes", "OutgoingBytes"]
+#  enforce_consumer_deletion = false
+#  encryption_type           = "KMS"
+#  kms_key_id                = local.kinesis_kms_id
+#  project_id                = local.project
 
-  tags = merge(
-    local.all_tags,
-    {
-      Name          = "${local.project}-kinesis-domain-data-${local.env}"
-      Resource_Type = "Kinesis Data Stream"
-      Component     = "Domain Data"
-    }
-  )
-}
+#  tags = merge(
+#   local.all_tags,
+#    {
+#      Name          = "${local.project}-kinesis-domain-data-${local.env}"
+#      Resource_Type = "Kinesis Data Stream"
+#      Component     = "Domain Data"
+#    }
+#  )
+#}
 
+# REMOVE - (DPR-378)
 # kinesis DEMO Data Stream
-module "kinesis_stream_demo_data" {
-  source                    = "./modules/kinesis_stream"
-  create_kinesis_stream     = local.create_kinesis
-  name                      = "${local.project}-kinesis-data-demo-${local.env}"
-  shard_count               = 1 # Not Valid when ON-DEMAND Mode
-  retention_period          = 24
-  shard_level_metrics       = ["IncomingBytes", "OutgoingBytes"]
-  enforce_consumer_deletion = false
-  encryption_type           = "KMS"
-  kms_key_id                = local.kinesis_kms_id
-  project_id                = local.project
+#module "kinesis_stream_demo_data" {
+#  source                    = "./modules/kinesis_stream"
+#  create_kinesis_stream     = local.create_kinesis
+#  name                      = "${local.project}-kinesis-data-demo-${local.env}"
+#  shard_count               = 1 # Not Valid when ON-DEMAND Mode
+#  retention_period          = 24
+#  shard_level_metrics       = ["IncomingBytes", "OutgoingBytes"]
+#  enforce_consumer_deletion = false
+#  encryption_type           = "KMS"
+#  kms_key_id                = local.kinesis_kms_id
+#  project_id                = local.project
 
-  tags = merge(
-    local.all_tags,
-    {
-      Name          = "${local.project}-kinesis-data-demo-${local.env}"
-      Resource_Type = "Kinesis Data Stream"
-      Component     = "Demo"
-    }
-  )
-}
+#  tags = merge(
+#    local.all_tags,
+#    {
+#      Name          = "${local.project}-kinesis-data-demo-${local.env}"
+#      Resource_Type = "Kinesis Data Stream"
+#      Component     = "Demo"
+#    }
+#  )
+#}
 
 # Glue Registry
 module "glue_registry_avro" {
@@ -867,7 +871,7 @@ module "glue_connection_redshift" {
   security_groups   = []
   availability_zone = ""
   subnet            = ""
-  password          = ""
+  password          = ""   ## Needs to pull from Secrets Manager, #TD
   username          = ""
 }
 
@@ -909,7 +913,7 @@ module "datamart" {
   number_of_nodes         = 1
   database_name           = "datamart"
   master_username         = "dpruser"
-  master_password         = "Datamartpass2022"
+  master_password         = "Datamartpass2022" ## Needs to pull from Secrets Manager, #TD
   create_random_password  = false
   encrypted               = true
   create_subnet_group     = true
@@ -971,16 +975,16 @@ module "s3_artifacts_store" {
 # DMS Nomis Data Collector
 module "dms_nomis_t3" {
   source                = "./modules/dms"
-  name                  = "${local.project}-dms-t3nomis-ingestor-${local.env}"
+  name                  = "${local.project}-dms-nomis-ingestor-${local.env}"
   vpc_cidr              = [data.aws_vpc.shared.cidr_block]
   source_engine_name    = "oracle"
   source_db_name        = "CNOMT3"
   source_app_username   = "digital_prison_reporting"
-  source_app_password   = "DSkpo4n7GhnmIV"
+  source_app_password   = "DSkpo4n7GhnmIV" ## Needs to pull from Secrets Manager, #TD
   source_address        = "10.101.63.135"
   source_db_port        = 1521
   vpc                   = data.aws_vpc.shared.id
-  kinesis_target_stream = "arn:aws:kinesis:eu-west-2:${data.aws_caller_identity.current.account_id}:stream/dpr-kinesis-ingestor-development"
+  kinesis_target_stream = "arn:aws:kinesis:eu-west-2:${data.aws_caller_identity.current.account_id}:stream/dpr-kinesis-ingestor-${local.env}"
   kinesis_stream_policy = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_admin_arn
   project_id            = local.project
   env                   = local.environment
@@ -1002,49 +1006,51 @@ module "dms_nomis_t3" {
     {
       Name          = "${local.project}-dms-t3nomis-ingestor-${local.env}"
       Resource_Type = "DMS Replication"
+      Nomis_Source  = "T3"
     }
   )
 }
 
+## REMOVE (DPR-378)
 # DMS Useforce Data Collector
-module "dms_use_of_force" {
-  source                = "./modules/dms"
-  name                  = "${local.project}-dms-use-force-ingestor-${local.env}"
-  vpc_cidr              = [data.aws_vpc.shared.cidr_block]
-  source_engine_name    = "postgres"
-  source_db_name        = "use_of_force"
-  source_app_username   = "postgres"
-  source_app_password   = ""
-  source_address        = "dpr-development-use-of-force-rds.cja8lnnvvipo.eu-west-2.rds.amazonaws.com"
-  source_db_port        = 5432
-  vpc                   = data.aws_vpc.shared.id
-  kinesis_target_stream = "arn:aws:kinesis:eu-west-2:${data.aws_caller_identity.current.account_id}:stream/dpr-kinesis-ingestor-development"
-  kinesis_stream_policy = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_admin_arn
-  project_id            = local.project
-  env                   = local.environment
-  dms_source_name       = "postgres"
-  dms_target_name       = "kinesis"
-  short_name            = "useforce"
-  migration_type        = "full-load-and-cdc"
-  subnet_ids            = [data.aws_subnet.data_subnets_a.id, data.aws_subnet.data_subnets_b.id, data.aws_subnet.data_subnets_c.id]
+#module "dms_use_of_force" {
+#  source                = "./modules/dms"
+#  name                  = "${local.project}-dms-use-force-ingestor-${local.env}"
+#  vpc_cidr              = [data.aws_vpc.shared.cidr_block]
+#  source_engine_name    = "postgres"
+#  source_db_name        = "use_of_force"
+#  source_app_username   = "postgres"
+#  source_app_password   = ""
+#  source_address        = "dpr-development-use-of-force-rds.cja8lnnvvipo.eu-west-2.rds.amazonaws.com"
+#  source_db_port        = 5432
+#  vpc                   = data.aws_vpc.shared.id
+#  kinesis_target_stream = "arn:aws:kinesis:eu-west-2:${data.aws_caller_identity.current.account_id}:stream/dpr-kinesis-ingestor-development"
+#  kinesis_stream_policy = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_admin_arn
+#  project_id            = local.project
+#  env                   = local.environment
+#  dms_source_name       = "postgres"
+#  dms_target_name       = "kinesis"
+#  short_name            = "useforce"
+#  migration_type        = "full-load-and-cdc"
+#  subnet_ids            = [data.aws_subnet.data_subnets_a.id, data.aws_subnet.data_subnets_b.id, data.aws_subnet.data_subnets_c.id]
 
-  vpc_role_dependency        = [aws_iam_role.dmsvpcrole]
-  cloudwatch_role_dependency = [aws_iam_role.dms_cloudwatch_logs_role]
+#  vpc_role_dependency        = [aws_iam_role.dmsvpcrole]
+#  cloudwatch_role_dependency = [aws_iam_role.dms_cloudwatch_logs_role]
 
-  extra_attributes = "PluginName=PGLOGICAL"
+#  extra_attributes = "PluginName=PGLOGICAL"
 
-  availability_zones = {
-    0 = "eu-west-2a"
-  }
+#  availability_zones = {
+#    0 = "eu-west-2a"
+#  }
 
-  tags = merge(
-    local.all_tags,
-    {
-      Name          = "${local.project}-dms-use-of-force-ingestor-${local.env}"
-      Resource_Type = "DMS Replication"
-    }
-  )
-}
+#  tags = merge(
+#    local.all_tags,
+#    {
+#      Name          = "${local.project}-dms-use-of-force-ingestor-${local.env}"
+#      Resource_Type = "DMS Replication"
+#   }
+#  )
+#}
 
 # S3 Oracle to Nomis SQS Notification # Disabled DPR-287 - TBC
 # module "s3_nomis_oracle_sqs" {
