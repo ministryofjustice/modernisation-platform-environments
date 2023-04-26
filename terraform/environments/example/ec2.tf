9e11b8495435f25f4bbe04f54d4337cfa0777e7c
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-04-26 11:51:56 +0100

    Moved module in file and added cert.

--- terraform/environments/example/ec2.tf
+++ terraform/environments/example/ec2.tf
@@ -2,6 +2,40 @@
 #------------------------Comment out file if not required----------------------------------
 ###########################################################################################
 
+# EC2 Created via module
+module "ec2_test_instance" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance"
+
+  providers = {
+    aws.core-vpc = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
+  }
+  for_each = try(local.ec2_test.ec2_test_instances, {})
+  name = each.key
+  ami_name                      = each.value.ami_name
+  ami_owner                     = try(each.value.ami_owner, "core-shared-services-production")
+  instance                      = merge(local.instance, lookup(each.value, "instance", {}))
+  ebs_volumes_copy_all_from_ami = try(each.value.ebs_volumes_copy_all_from_ami, true)
+  ebs_kms_key_id                = module.environment.kms_keys["ebs"].arn
+  ebs_volume_config             = lookup(each.value, "ebs_volume_config", {})
+  ebs_volumes                   = lookup(each.value, "ebs_volumes", {})
+  ssm_parameters_prefix         = lookup(each.value, "ssm_parameters_prefix", "test/")
+  ssm_parameters                = lookup(each.value, "ssm_parameters", null)
+  route53_records               = merge(local.ec2_test.route53_records, lookup(each.value, "route53_records", {}))
+
+  iam_resource_names_prefix = "ec2-test-instance"
+  instance_profile_policies = local.ec2_common_managed_policies
+
+  business_unit            = local.business_unit
+  application_name         = local.application_name
+  environment              = local.environment
+  region                   = local.region
+  availability_zone        = local.availability_zone_1
+  subnet_id                = module.environment.subnet["private"][local.availability_zone_1].id
+  tags                     = merge(local.tags, local.ec2_test.tags, try(each.value.tags, {}))
+  account_ids_lookup       = local.environment_management.account_ids
+  cloudwatch_metric_alarms = {}
+}
+
 # EC2 Sec Group
 resource "aws_security_group" "example_ec2_sg" {
   name        = "example_ec2_sg"
@@ -11,6 +45,7 @@ resource "aws_security_group" "example_ec2_sg" {
     { Name = lower(format("sg-%s-%s-example", local.application_name, local.environment)) }
   )
 }
+
 resource "aws_security_group_rule" "ingress_traffic" {
   for_each          = local.application_data.example_ec2_sg_rules
   description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
@@ -32,8 +67,6 @@ resource "aws_security_group_rule" "egress_traffic" {
   type                     = "egress"
   source_security_group_id = aws_security_group.example_ec2_sg.id
 }
-
-
 
 #  Build EC2 "example-ec2"
 resource "aws_instance" "develop" {
@@ -61,7 +94,6 @@ resource "aws_instance" "develop" {
   )
   depends_on = [aws_security_group.example_ec2_sg]
 }
-
 
 # create single managed policy
 resource "aws_iam_policy" "ec2_common_policy" {
@@ -125,39 +157,6 @@ resource "aws_key_pair" "ec2-user" {
   )
 }
 
-module "ec2_test_instance" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance"
-
-  providers = {
-    aws.core-vpc = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
-  }
-  for_each = try(local.ec2_test.ec2_test_instances, {})
-  name = each.key
-  ami_name                      = each.value.ami_name
-  ami_owner                     = try(each.value.ami_owner, "core-shared-services-production")
-  instance                      = merge(local.instance, lookup(each.value, "instance", {}))
-  ebs_volumes_copy_all_from_ami = try(each.value.ebs_volumes_copy_all_from_ami, true)
-  ebs_kms_key_id                = module.environment.kms_keys["ebs"].arn
-  ebs_volume_config             = lookup(each.value, "ebs_volume_config", {})
-  ebs_volumes                   = lookup(each.value, "ebs_volumes", {})
-  ssm_parameters_prefix         = lookup(each.value, "ssm_parameters_prefix", "test/")
-  ssm_parameters                = lookup(each.value, "ssm_parameters", null)
-  route53_records               = merge(local.ec2_test.route53_records, lookup(each.value, "route53_records", {}))
-
-  iam_resource_names_prefix = "ec2-test-instance"
-  instance_profile_policies = local.ec2_common_managed_policies
-
-  business_unit            = local.business_unit
-  application_name         = local.application_name
-  environment              = local.environment
-  region                   = local.region
-  availability_zone        = local.availability_zone_1
-  subnet_id                = module.environment.subnet["private"][local.availability_zone_1].id
-  tags                     = merge(local.tags, local.ec2_test.tags, try(each.value.tags, {}))
-  account_ids_lookup       = local.environment_management.account_ids
-  cloudwatch_metric_alarms = {}
-}
-
 # Volumes built for use by EC2.
 resource "aws_kms_key" "ec2" {
   description         = "Encryption key for EBS"
@@ -185,13 +184,13 @@ resource "aws_ebs_volume" "ebs_volume" {
 
   depends_on = [aws_instance.develop, aws_kms_key.ec2]
 }
+
 # Attach to the EC2
 resource "aws_volume_attachment" "mountvolumetoec2" {
   device_name = "/dev/sdb"
   instance_id = aws_instance.develop.id
   volume_id   = aws_ebs_volume.ebs_volume.id
 }
-
 
 #### This file can be used to store data specific to the member account ####
 data "aws_iam_policy_document" "ebs-kms" {
