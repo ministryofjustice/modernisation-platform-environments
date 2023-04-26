740ae653fb4cd9199ce4b8b622e4a44a39a5c8ae
Seb Norris <sebastian@automationlogic.com>
2023-04-26 15:24:55 +0100

    add datasync bucket for jitbit dev

--- terraform/environments/delius-jitbit/s3.tf
+++ terraform/environments/delius-jitbit/s3.tf
@@ -56,3 +56,46 @@ resource "aws_s3_bucket_intelligent_tiering_configuration" "jitbit_bucket_tierin
     days        = 180
   }
 }
+
+
+module "datasync_bucket" {
+  count = local.environment == "development" ? 1 : 0
+
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.2.0"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  bucket_prefix      = "${local.application_name}-${local.environment}-datasync-"
+  versioning_enabled = true
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
