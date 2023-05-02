4036880d994ab23516c6720a4dd31261ed1efe3f
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-05-02 17:30:40 +0100

    Nomis: DSOS-1618: tidy s3 buckets (#2121)
    * remove image builder bucket from all accounts apart from nomis-test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * align s3 policies
    
    * test
    
    * zap storage account
    
    * add other bucket
    
    * delete old bucket
    
    * revert bodges
    
    * revert bodges

--- terraform/environments/nomis/locals.tf
+++ terraform/environments/nomis/locals.tf
@@ -82,14 +82,6 @@ locals {
     s3-bucket = {
       iam_policies = module.baseline_presets.s3_iam_policies
     }
-    ec2-image-builder-nomis = {
-      custom_kms_key = module.environment.kms_keys["general"].arn
-      bucket_policy_v2 = [
-        module.baseline_presets.s3_bucket_policies.ImageBuilderWriteAccessBucketPolicy,
-        module.baseline_presets.s3_bucket_policies.AllEnvironmentsWriteAccessBucketPolicy,
-      ]
-      iam_policies = module.baseline_presets.s3_iam_policies
-    }
     nomis-db-backup-bucket = {
       custom_kms_key = module.environment.kms_keys["general"].arn
       iam_policies   = module.baseline_presets.s3_iam_policies
