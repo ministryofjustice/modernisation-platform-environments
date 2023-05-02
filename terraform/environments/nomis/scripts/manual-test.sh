5668ad7ef0b3dce2e1f86f76a46868f583c779af
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-05-02 12:01:51 +0100

    test

--- terraform/environments/nomis/scripts/manual-test.sh
+++ terraform/environments/nomis/scripts/manual-test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-aws s3api list-object-versions --bucket int-nomis-lb-access-logs20230105131538061200000001 | jq -r '.Versions[] | "aws s3api delete-object --bucket int-nomis-lb-access-logs20230105131538061200000001 --key \"\(.Key)\" --version-id \"\(.VersionId)\""'
+bucket=int-nomis-lb-access-logs20230105131538061200000001
+aws s3api list-object-versions --bucket $bucket | jq -r '.Versions[] | "--key \"\(.Key)\" --version-id \"\(.VersionId)\""' | xargs -L 1 aws s3api delete-object --bucket $bucket
