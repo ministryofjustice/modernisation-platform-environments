3ea7a6c0e663ccd094b86be56275f31bbb197057
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-05-02 10:34:59 +0100

    remove image builder bucket from all accounts apart from nomis-test

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -264,5 +264,16 @@ locals {
         ]
       }
     }
+    baseline_s3_buckets = {
+      # use this bucket for storing artefacts for use across all accounts
+      ec2-image-builder-nomis = {
+        custom_kms_key = module.environment.kms_keys["general"].arn
+        bucket_policy_v2 = [
+          module.baseline_presets.s3_bucket_policies.ImageBuilderWriteAccessBucketPolicy,
+          module.baseline_presets.s3_bucket_policies.AllEnvironmentsWriteAccessBucketPolicy,
+        ]
+        iam_policies = module.baseline_presets.s3_iam_policies
+      }
+    }
   }
 }
