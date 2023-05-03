be23a60380b1941eb9bf4e2895710e75e001847e
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-05-03 11:46:30 +0100

    Refactored all and removed some files and renamed others. Now each file can be commented out and terraform still run.

--- terraform/environments/example/ec2_bastion_linux.tf
+++ terraform/environments/example/ec2_bastion_linux.tf
@@ -1,16 +1,11 @@
-locals {
-  public_key_data = jsondecode(file("./bastion_linux.json"))
-}
-
 # tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
 module "bastion_linux" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v3.0.7"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v3.0.8"
 
   providers = {
     aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
     aws.share-tenant = aws          # The default provider (unaliased, `aws`) is the tenant
   }
-
   # s3 - used for logs and user ssh public keys
   bucket_name          = "bastion-example"
   bucket_versioning    = true
@@ -24,7 +19,6 @@ module "bastion_linux" {
   log_expiry_days      = 180 # days before log expiration
   # bastion
   allow_ssh_commands = false
-
   app_name      = var.networking[0].application
   business_unit = local.vpc_name
   subnet_set    = local.subnet_set
@@ -34,4 +28,9 @@ module "bastion_linux" {
   # Tags
   tags_common = local.tags
   tags_prefix = terraform.workspace
+}
+
+
+locals {
+  public_key_data = jsondecode(file("./bastion_linux.json"))
 }
