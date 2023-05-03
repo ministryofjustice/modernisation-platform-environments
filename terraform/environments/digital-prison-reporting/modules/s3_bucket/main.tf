ab884580a2a628568aaac90d278e49721e84a515
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-05-03 20:18:35 +0100

    Adjust S3 Policies for DMS

--- terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
+++ terraform/environments/digital-prison-reporting/modules/s3_bucket/main.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket_public_access_block" "storage" {
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
