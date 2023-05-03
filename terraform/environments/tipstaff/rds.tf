524988ef7d8a40feee03b829939534c86d949a55
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-05-03 10:43:35 +0100

    add /32 to the end of cidr_blocks

--- terraform/environments/tipstaff/rds.tf
+++ terraform/environments/tipstaff/rds.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "postgresql_db_sc" {
     to_port     = 5432
     protocol    = "tcp"
     description = "Allows Github Actions to access RDS"
-    cidr_blocks = [jsondecode(data.http.myip.response_body)["ip"]]
+    cidr_blocks = ["${jsondecode(data.http.myip.response_body)["ip"]}/32"]
   }
   egress {
     description = "allow all outbound traffic"
