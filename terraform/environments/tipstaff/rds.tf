7206fbf3dee5597a963b931e714ccf2b7e726ce6
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-05-03 09:33:19 +0100

    attempt to add github actions ip addresses for rds

--- terraform/environments/tipstaff/rds.tf
+++ terraform/environments/tipstaff/rds.tf
@@ -42,8 +42,8 @@ resource "aws_security_group" "postgresql_db_sc" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    description = "Allows access to RDS from any IP"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allows Github Actions to access RDS"
+    cidr_blocks = data.github_ip_ranges.github_actions_ips.actions_ipv4
   }
   egress {
     description = "allow all outbound traffic"
@@ -54,6 +54,8 @@ resource "aws_security_group" "postgresql_db_sc" {
   }
 
 }
+
+data "github_ip_ranges" "github_actions_ips" {}
 
 resource "null_resource" "setup_db" {
   depends_on = [aws_db_instance.tipstaff_db]
