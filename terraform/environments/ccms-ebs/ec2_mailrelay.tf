f4389aea3daeb319552bcd49d02b4f9979714e09
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-05-05 16:15:20 +0100

    CC-1804: Updated user_data.

--- terraform/environments/ccms-ebs/ec2_mailrelay.tf
+++ terraform/environments/ccms-ebs/ec2_mailrelay.tf
@@ -26,8 +26,7 @@ yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/li
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/oracle_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-rpm -U ./amazon-cloudwatch-agent.rpm
+yum install -y https://s3.amazonaws.com/amazoncloudwatch-agent/oracle_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 amazon-linux-extras install -y epel
 
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:cloud-watch-config
