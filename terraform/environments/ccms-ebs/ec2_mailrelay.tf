30cd98a6913409008c14ea5851091e555d0b301e
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-05-05 15:45:40 +0100

    CC-1804: Updated user_data.

--- terraform/environments/ccms-ebs/ec2_mailrelay.tf
+++ terraform/environments/ccms-ebs/ec2_mailrelay.tf
@@ -21,6 +21,7 @@ resource "aws_instance" "ec2_mailrelay" {
 
 exec > /tmp/userdata.log 2>&1
 
+yum install -y jq unzip wget
 yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
