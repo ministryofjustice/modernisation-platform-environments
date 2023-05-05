9a0d804fe483354e88d93c11a8cd2e4bbe0c238e
keith-h <keith.humphreys@digital.justice.gov.uk>
2023-05-05 11:39:22 +0100

    chore(*): fix A Name for ec2 route53 record

--- terraform/environments/laa-ccms-infra-azure-ad-sso/route_53.tf
+++ terraform/environments/laa-ccms-infra-azure-ad-sso/route_53.tf
@@ -30,7 +30,7 @@ resource "aws_route53_record" "ebs_vision_db" {
 
   zone_id = data.aws_route53_zone.external.zone_id
   #name    = "ccms-ebs-db.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-  name    = "ebs-vision-db-lb.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "ebs-vision-db.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
   ttl     = 300
   records = [aws_instance.ec2_oracle_vision_ebs.private_ip]
