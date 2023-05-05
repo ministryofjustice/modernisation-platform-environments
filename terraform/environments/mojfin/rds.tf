87a6ba57cd9624fb97e38c88027599a7639e66f2
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-05-05 15:15:13 +0100

    spelling mistake

--- terraform/environments/mojfin/rds.tf
+++ terraform/environments/mojfin/rds.tf
@@ -181,7 +181,7 @@ resource "aws_db_instance" "appdb1" {
 
 
 resource "aws_route53_record" "prd-mojfin-rds" {
-  count=  local.enviroment == "production" ? 1 : 0
+  count=  local.environment == "production" ? 1 : 0
   provider = aws.core-network-services
   zone_id  =  local.prod_domain_name 
   name     =  "rds.${local.prod_domain_name}" 
@@ -191,7 +191,7 @@ resource "aws_route53_record" "prd-mojfin-rds" {
 }
 
 resource "aws_route53_record" "nonprd-mojfin-rds" {
-  count =  local.enviroment != "production" ? 1 : 0
+  count =  local.environment != "production" ? 1 : 0
   provider = aws.core-vpc
   zone_id  =   data.aws_route53_zone.external.zone_id
   name     =  "rds.${local.application_name}.${data.aws_route53_zone.external.name}"
