f46305e59fda027e585a317447f2c38f36923107
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-05-05 17:42:19 +0100

    add egress over https

--- terraform/environments/delius-core/ec2_instance_db.tf
+++ terraform/environments/delius-core/ec2_instance_db.tf
@@ -12,6 +12,18 @@ resource "aws_security_group" "base_ami_test_instance_sg" {
   )
 }
 
+resource "aws_vpc_security_group_egress_rule" "base_ami_test_instance_https_out" {
+  security_group_id = aws_security_group.base_ami_test_instance_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  description       = "Allow communication out on port 443, e.g. for SSM"
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-base-ami-test-instance", local.application_name, local.environment)) }
+  )
+}
+
 # Pre-req - IAM role, attachment for SSM usage and instance profile
 data "aws_iam_policy_document" "base_ami_test_instance_iam_assume_policy" {
   statement {
