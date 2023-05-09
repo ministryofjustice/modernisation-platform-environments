dd481571e8740e5bb5415b9e976fe321872b5000
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-05-09 15:05:56 +0100

    try to add a policy to MemberInfrastructureAccess

--- terraform/environments/tipstaff/secrets.tf
+++ terraform/environments/tipstaff/secrets.tf
@@ -1,18 +1,35 @@
 #### This file can be used to store secrets specific to the member account ####
-# data "aws_secretsmanager_secret" "tipstaff_dev_db_secrets" {
-#   arn   = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:tipstaff-dev-db-secrets-8Qc18f"
+
+# data "aws_secretsmanager_secret" "tactical_products_db_secrets" {
+#   arn = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:tactical-products-db-secrets-ox3sNi"
 # }
 
-data "aws_secretsmanager_secret" "tactical_products_db_secrets" {
-  arn = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:tactical-products-db-secrets-ox3sNi"
+# data "aws_secretsmanager_secret_version" "dms_source_credentials" {
+#   secret_id = data.aws_secretsmanager_secret.tactical_products_db_secrets.id
+# }
+
+data "aws_iam_session_context" "member_infrastructure_access" {
+  arn = "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
 }
 
-# data "aws_secretsmanager_secret_version" "db_credentials" {
-#   secret_id = data.aws_secretsmanager_secret.tipstaff_dev_db_secrets.id
-# }
+resource "aws_iam_role_policy" "add_secrets_manager_policy" {
+  name = "execution-${var.networking[0].application}"
+  role = aws_iam_session_context.member_infrastructure_access.issuer_name
 
-data "aws_secretsmanager_secret_version" "dms_source_credentials" {
-  secret_id = data.aws_secretsmanager_secret.tactical_products_db_secrets.id
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+           "Action": [
+              "secretsmanager:*"
+           ],
+           "Resource": "*",
+           "Effect": "Allow"
+      }
+    ]
+  }
+  EOF
 }
 
 # Cannot create a secret using the console in pre-production, so trying a different approach
