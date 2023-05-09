b57b5869e355ce73aefb1f1d321029bb26c67a90
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-05-09 10:33:15 +0100

    Cleanup - add backup TF

---
+++ terraform/environments/digital-prison-reporting/backup-obsolete.tf
@@ -0,0 +1,258 @@
+
+### Commented ###
+# REMOVE - (DPR-378)
+# Glue Kinesis Reader Job (DPR-340)
+#module "glue_kinesis_reader_job" {
+#  source                        = "./modules/glue_job"
+#  create_job                    = local.create_job
+#  name                          = "${local.project}-kinesis-reader-${local.env}"
+#  description                   = "kinesis Reader Job"
+#  job_language                  = "scala"
+#  command_type                  = "gluestreaming"
+#  create_security_configuration = local.create_sec_conf
+#  temp_dir                      = "s3://${module.s3_glue_job_bucket.bucket_id}/tmp/reporting-hub/"
+#  checkpoint_dir                = "s3://${module.s3_glue_job_bucket.bucket_id}/checkpoint/reporting-hub/"
+#  spark_event_logs              = "s3://${module.s3_glue_job_bucket.bucket_id}/spark-logs/reporting-hub/"
+#  enable_continuous_log_filter  = false
+#  project_id                    = local.project
+#  aws_kms_key                   = local.s3_kms_arn
+#  create_kinesis_ingester       = local.create_kinesis # If True, Kinesis Policies are applied - Defaults to True
+#  additional_policies           = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_admin_arn
+#  timeout                       = 2880 # This is in Mins
+#  execution_class               = "STANDARD"
+#  # Placeholder Script Location
+#  script_location = "s3://${local.project}-artifact-store-${local.environment}/artifacts/cloud-platform/digital-prison-reporting-jobs/scripts/${local.project}-kinesis-reader-vLatest.scala"
+
+#  class = "uk.gov.justice.digital.job.DataHubJob"
+
+#  tags = merge(
+#    local.all_tags,
+#    {
+#      Name          = "${local.project}-kinesis-reader-${local.env}"
+#      Resource_Type = "Glue Job"
+#      Ticket        = "DPR-340"
+#    }
+#  )
+
+#  arguments = {
+#    "--extra-jars"                          = "s3://${local.project}-artifact-store-${local.environment}/artifacts/cloud-platform/digital-prison-reporting-jobs/jars/digital-prison-reporting-jobs-vLatest.jar"
+#    "--job-bookmark-option"                 = "job-bookmark-disable"
+#    "--enable-metrics"                      = true
+#    "--enable-spark-ui"                     = false
+#    "--enable-job-insights"                 = true
+#    "--kinesis.reader.streamName"           = "${local.project}-kinesis-reader-${local.env}-stream"
+#    "--aws.kinesis.endpointUrl"             = "https://kinesis-${local.account_region}.amazonaws.com"
+#    "--aws.region"                          = local.account_region
+#    "--kinesis.reader.batchDurationSeconds" = 1
+#    "--class"                               = "uk.gov.justice.digital.job.DataHubJob"
+#  }
+#}
+
+# REMOVE - (DPR-378)
+# Glue Domain Platform Change Monitor Job
+#module "glue_domainplatform_change_monitor_job" {
+#  source                        = "./modules/glue_job"
+#  create_job                    = local.create_job
+#  name                          = "${local.project}-domain-platform-table-change-monitor-${local.env}"
+#  description                   = "Monitors the reporting hub for table changes and applies them to domains"
+#  create_security_configuration = local.create_sec_conf
+#  job_language                  = "scala"
+#  temp_dir                      = "s3://${module.s3_glue_job_bucket.bucket_id}/tmp/change-monitor/"
+#  checkpoint_dir                = "s3://${module.s3_glue_job_bucket.bucket_id}/checkpoint/change-monitor/"
+#  spark_event_logs              = "s3://${module.s3_glue_job_bucket.bucket_id}/spark-logs/change-monitor/"
+#  tags                          = local.all_tags
+#  script_location               = "s3://${local.project}-artifact-store-${local.environment}/artifacts/domain-platform/digital-prison-reporting-poc/domain-platform-table-change-monitor-vLatest.scala"
+#  enable_continuous_log_filter  = false
+#  project_id                    = local.project
+#  aws_kms_key                   = local.s3_kms_arn
+#  create_kinesis_ingester       = local.create_kinesis # If True, Kinesis Policies are applied
+#  additional_policies           = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_read_only_arn
+#  timeout                       = 120
+#  execution_class               = "FLEX"
+#  arguments = {
+#    "--extra-jars"          = "s3://${local.project}-artifact-store-${local.environment}/artifacts/domain-platform/digital-prison-reporting-poc/domain-platform-vLatest.jar"
+#    "--class"               = "GlueApp"
+#    "--cloud.platform.path" = "s3://${module.s3_curated_bucket.bucket_id}"
+#    "--domain.files.path"   = "s3://${module.s3_domain_config_bucket.bucket_id}/"
+#    "--domain.repo.path"    = "s3://${module.s3_glue_job_bucket.bucket_id}/domain-repo/" ## Added /
+#    "--source.queue"        = "domain-cdc-event-notification"                            ## DPR-287, needs right source - TBC
+#    "--source.region"       = local.account_region
+#    "--target.path"         = "s3://${module.s3_domain_bucket.bucket_id}/"               ## Added /
+#    "--checkpoint.location" = "s3://${module.s3_glue_job_bucket.bucket_id}/checkpoint/change-monitor/"
+#  }
+#}
+
+# REMOVE - (DPR-378)
+# kinesis Domain Data Stream
+#module "kinesis_stream_domain_data" {
+#  source                    = "./modules/kinesis_stream"
+#  create_kinesis_stream     = local.create_kinesis
+#  name                      = local.kinesis_stream_data_domain
+#  shard_count               = 1 # Not Valid when ON-DEMAND Mode
+#  retention_period          = 24
+#  shard_level_metrics       = ["IncomingBytes", "OutgoingBytes"]
+#  enforce_consumer_deletion = false
+#  encryption_type           = "KMS"
+#  kms_key_id                = local.kinesis_kms_id
+#  project_id                = local.project
+
+#  tags = merge(
+#   local.all_tags,
+#    {
+#      Name          = "${local.project}-kinesis-domain-data-${local.env}"
+#      Resource_Type = "Kinesis Data Stream"
+#      Component     = "Domain Data"
+#    }
+#  )
+#}
+
+# REMOVE - (DPR-378)
+# kinesis DEMO Data Stream
+#module "kinesis_stream_demo_data" {
+#  source                    = "./modules/kinesis_stream"
+#  create_kinesis_stream     = local.create_kinesis
+#  name                      = "${local.project}-kinesis-data-demo-${local.env}"
+#  shard_count               = 1 # Not Valid when ON-DEMAND Mode
+#  retention_period          = 24
+#  shard_level_metrics       = ["IncomingBytes", "OutgoingBytes"]
+#  enforce_consumer_deletion = false
+#  encryption_type           = "KMS"
+#  kms_key_id                = local.kinesis_kms_id
+#  project_id                = local.project
+
+#  tags = merge(
+#    local.all_tags,
+#    {
+#      Name          = "${local.project}-kinesis-data-demo-${local.env}"
+#      Resource_Type = "Kinesis Data Stream"
+#      Component     = "Demo"
+#    }
+#  )
+#}
+
+## Glue Database Raw Glue Catalog 
+##module "glue_raw_database" {
+##  source         = "./modules/glue_database"
+##  create_db      = local.create_db
+##  name           = "${local.project}-raw-${local.env}"
+##  description    = "Glue Database Raw Catalog"
+##  aws_account_id = local.account_id
+##  aws_region     = local.account_region
+##}
+
+## REMOVE (DPR-378)
+# DMS Useforce Data Collector
+#module "dms_use_of_force" {
+#  source                = "./modules/dms"
+#  name                  = "${local.project}-dms-use-force-ingestor-${local.env}"
+#  vpc_cidr              = [data.aws_vpc.shared.cidr_block]
+#  source_engine_name    = "postgres"
+#  source_db_name        = "use_of_force"
+#  source_app_username   = "postgres"
+#  source_app_password   = ""
+#  source_address        = "dpr-development-use-of-force-rds.cja8lnnvvipo.eu-west-2.rds.amazonaws.com"
+#  source_db_port        = 5432
+#  vpc                   = data.aws_vpc.shared.id
+#  kinesis_target_stream = "arn:aws:kinesis:eu-west-2:${data.aws_caller_identity.current.account_id}:stream/dpr-kinesis-ingestor-development"
+#  kinesis_stream_policy = module.kinesis_stream_ingestor.kinesis_stream_iam_policy_admin_arn
+#  project_id            = local.project
+#  env                   = local.environment
+#  dms_source_name       = "postgres"
+#  dms_target_name       = "kinesis"
+#  short_name            = "useforce"
+#  migration_type        = "full-load-and-cdc"
+#  subnet_ids            = [data.aws_subnet.data_subnets_a.id, data.aws_subnet.data_subnets_b.id, data.aws_subnet.data_subnets_c.id]
+
+#  vpc_role_dependency        = [aws_iam_role.dmsvpcrole]
+#  cloudwatch_role_dependency = [aws_iam_role.dms_cloudwatch_logs_role]
+
+#  extra_attributes = "PluginName=PGLOGICAL"
+
+#  availability_zones = {
+#    0 = "eu-west-2a"
+#  }
+
+#  tags = merge(
+#    local.all_tags,
+#    {
+#      Name          = "${local.project}-dms-use-of-force-ingestor-${local.env}"
+#      Resource_Type = "DMS Replication"
+#   }
+#  )
+#}
+
+# S3 Oracle to Nomis SQS Notification # Disabled DPR-287 - TBC
+# module "s3_nomis_oracle_sqs" {
+#  source                    = "./modules/s3_bucket"
+#  create_s3                 = local.setup_buckets
+#  name                      = "${local.project}-nomis-cdc-event-${local.environment}"
+#  custom_kms_key            = local.s3_kms_arn
+#  create_notification_queue = true
+#  filter_prefix             = "cdc/"
+#  s3_notification_name      = "nomis-cdc-event-notification"
+#  sqs_msg_retention_seconds = 1209600
+
+#  tags = merge(
+#    local.all_tags,
+#    {
+#      Name          = "${local.project}-nomis-cdc-event-${local.environment}"
+#      Resource_Type = "S3 Bucket"
+#    }
+#  )
+#}
+
+# S3 - CDC Domain Events SQS Notification (DPR-116) # Disabled DPR-287 - TBC
+# module "s3_domain_cdc_sqs" {
+#  source                    = "./modules/s3_bucket"
+#  create_s3                 = local.setup_buckets
+#  name                      = "${local.project}-domain-cdc-event-${local.environment}"
+#  custom_kms_key            = local.s3_kms_arn
+#  create_notification_queue = true
+#  filter_prefix             = "cdc/"
+#  s3_notification_name      = "domain-cdc-event-notification"
+#  sqs_msg_retention_seconds = 1209600
+
+#  tags = merge(
+#    local.all_tags,
+#    {
+#      Name          = "${local.project}-domain-cdc-event-${local.environment}"
+#      Resource_Type = "S3 Bucket"
+#    }
+#  )
+#}
+
+# Kinesis Nomis Stream # Commented DPR-287 - TBC
+# module "kinesis_nomis_stream" {
+#  source                     = "./modules/kinesis_firehose"
+#  name                       = "${local.project}-nomis-target-stream-${local.env}"
+#  kinesis_source_stream_arn  = module.kinesis_stream_ingestor.kinesis_stream_arn  # KDS Cloud Platform
+#  kinesis_source_stream_name = module.kinesis_stream_ingestor.kinesis_stream_name # KDS Cloud Platform
+#  target_s3_id               = module.s3_nomis_oracle_sqs.bucket_id
+#  target_s3_arn              = module.s3_nomis_oracle_sqs.bucket_arn
+#  target_s3_kms              = local.s3_kms_arn
+#  target_s3_prefix           = "cdc/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/hour=!{timestamp:HH}/"
+#  target_s3_error_prefix     = "cdc-error/type=!{firehose:error-output-type}/"
+#  aws_account_id             = local.account_id
+#  aws_region                 = local.account_region
+#  cloudwatch_log_group_name  = "/aws/kinesisfirehose/nomis-target-stream"
+#  cloudwatch_log_stream_name = "NomisTargetStream"
+#  cloudwatch_logging_enabled = true
+#}
+
+# Kinesis cdc domain Stream (DPR-116) # Commented DPR-287 - TBC
+# module "kinesis_cdc_domain_stream" {
+#  source                     = "./modules/kinesis_firehose"
+#  name                       = "${local.project}-cdc-domain-stream-${local.env}"
+#  kinesis_source_stream_arn  = module.kinesis_stream_domain_data.kinesis_stream_arn  # KDS Domain Platform
+#  kinesis_source_stream_name = module.kinesis_stream_domain_data.kinesis_stream_name # KDS Domain Platform
+#  target_s3_id               = module.s3_domain_cdc_sqs.bucket_id
+#  target_s3_arn              = module.s3_domain_cdc_sqs.bucket_arn
+#  target_s3_kms              = local.s3_kms_arn
+#  target_s3_prefix           = "cdc/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/hour=!{timestamp:HH}/"
+#  target_s3_error_prefix     = "cdc-error/type=!{firehose:error-output-type}/"
+#  aws_account_id             = local.account_id
+#  aws_region                 = local.account_region
+#  cloudwatch_log_group_name  = "/aws/kinesisfirehose/cdc-domain-stream"
+#  cloudwatch_log_stream_name = "CdcDomainStream"
+#  cloudwatch_logging_enabled = true
+#}
