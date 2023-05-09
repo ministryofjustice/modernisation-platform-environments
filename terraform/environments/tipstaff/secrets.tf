7f05ef9df1c13bbb442131015d1c8f5b319bf98d
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-05-09 16:02:25 +0100

    try adding [count.index] again

--- terraform/environments/tipstaff/secrets.tf
+++ terraform/environments/tipstaff/secrets.tf
@@ -1,11 +1,13 @@
 #### This file can be used to store secrets specific to the member account ####
 
 data "aws_secretsmanager_secret" "tactical_products_db_secrets" {
-  arn = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:tactical-products-db-secrets-ox3sNi"
+  count = local.is-development ? 1 : 0
+  arn   = "arn:aws:secretsmanager:eu-west-2:913862848426:secret:tactical-products-db-secrets-ox3sNi"
 }
 
 data "aws_secretsmanager_secret_version" "dms_source_credentials" {
-  secret_id = data.aws_secretsmanager_secret.tactical_products_db_secrets.id
+  count     = local.is-development ? 1 : 0
+  secret_id = data.aws_secretsmanager_secret.tactical_products_db_secrets[count.index].id
 }
 
 resource "random_string" "username" {
