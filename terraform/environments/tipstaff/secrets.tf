efefe2df10c9155759698dbb62365c89dec1f7cc
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-05-09 11:00:08 +0100

    change the syntax in secrets.tf

--- terraform/environments/tipstaff/secrets.tf
+++ terraform/environments/tipstaff/secrets.tf
@@ -11,12 +11,12 @@ data "aws_secretsmanager_secret" "tactical_products_db_secrets" {
 
 data "aws_secretsmanager_secret_version" "db_credentials" {
   count     = local.application_data.accounts[local.environment] == "development" ? 1 : 0
-  secret_id = data.aws_secretsmanager_secret.tipstaff_dev_db_secrets.id
+  secret_id = data.aws_secretsmanager_secret.tipstaff_dev_db_secrets[count.index].id
 }
 
 data "aws_secretsmanager_secret_version" "dms_source_credentials" {
   count     = local.application_data.accounts[local.environment] == "development" ? 1 : 0
-  secret_id = data.aws_secretsmanager_secret.tactical_products_db_secrets.id
+  secret_id = data.aws_secretsmanager_secret.tactical_products_db_secrets[count.index].id
 }
 
 # Cannot create a secret using the console in pre-production, so trying a different approach
@@ -46,12 +46,12 @@ resource "aws_secretsmanager_secret" "tipstaff_pre_prod_db_secrets" {
 
 resource "aws_secretsmanager_secret_version" "secrets_version_username" {
   count         = local.application_data.accounts[local.environment] == "preproduction" ? 1 : 0
-  secret_id     = aws_secretsmanager_secret.tipstaff_pre_prod_db_secrets.id
-  secret_string = random_string.username.result
+  secret_id     = aws_secretsmanager_secret.tipstaff_pre_prod_db_secrets[count.index].id
+  secret_string = random_string.username[count.index].result
 }
 
 resource "aws_secretsmanager_secret_version" "secrets_version_password" {
   count         = local.application_data.accounts[local.environment] == "preproduction" ? 1 : 0
-  secret_id     = aws_secretsmanager_secret.tipstaff_pre_prod_db_secrets.id
-  secret_string = random_password.password.result
+  secret_id     = aws_secretsmanager_secret.tipstaff_pre_prod_db_secrets[count.index].id
+  secret_string = random_password.password[count.index].result
 }
