14aa276c05199287f13d06a8c9a58a39415d1172
George Taylor <george.taylor@digital.justice.gov.uk>
2023-05-10 17:19:33 +0100

    efs sec group rule

--- terraform/environments/delius-core/efs_openldap.tf
+++ terraform/environments/delius-core/efs_openldap.tf
@@ -5,6 +5,31 @@ resource "aws_efs_file_system" "openldap" {
   }
 }
 
+resource "aws_security_group" "efs" {
+  name        = format("%s-openldap-efs", local.application_name)
+  description = format("%s-openldap-efs", local.application_name)
+  vpc_id      = var.vpc_id
+  tags        = local.tags
+}
+
+resource "aws_security_group_rule" "efs" {
+  type              = "ingress"
+  from_port         = 2049
+  to_port           = 2049
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.efs.id
+}
+
+resource "aws_security_group_rule" "efs" {
+  type              = "egress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.efs.id
+}
+
 module "s3_bucket_openldap_migration" {
 
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.4.0"
