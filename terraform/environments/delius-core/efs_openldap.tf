e31861c0b34a7f66c24d5eb4e2098fdc93db4dc1
George Taylor <george.taylor@digital.justice.gov.uk>
2023-05-10 16:25:55 +0100

    add migration s3

--- terraform/environments/delius-core/efs_openldap.tf
+++ terraform/environments/delius-core/efs_openldap.tf
@@ -1,3 +1,43 @@
 resource "aws_efs_file_system" "foo" {
   creation_token = format("%s-openldap", local.application_name)
 }
+
+module "s3_bucket_app_deployment" {
+
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.4.0"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+  bucket_name        = "${local.application_name}-${local.environment}-openldap-migration"
+  versioning_enabled = false
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
