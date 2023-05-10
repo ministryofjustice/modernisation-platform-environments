d6e5d5c79357c1730fe8b13cf9280f9ab14588cc
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2023-05-10 16:22:31 +0100

    add logging to ebs waf

--- terraform/environments/ccms-ebs/waf.tf
+++ terraform/environments/ccms-ebs/waf.tf
@@ -84,7 +84,7 @@ resource "aws_cloudwatch_log_group" "ebs_waf_logs" {
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "ebs_waf_logging" {
-  log_destination_configs = aws_cloudwatch_log_group.ebs_waf_logs.arn
+  log_destination_configs = [aws_cloudwatch_log_group.ebs_waf_logs.arn]
   resource_arn            = aws_wafv2_web_acl.ebs_web_acl.arn
 
 }
