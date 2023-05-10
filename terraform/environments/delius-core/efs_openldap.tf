5fdf409074887ce7722f183712b12dbaf53d2c95
George Taylor <george.taylor@digital.justice.gov.uk>
2023-05-10 21:29:45 +0100

    update proto

--- terraform/environments/delius-core/efs_openldap.tf
+++ terraform/environments/delius-core/efs_openldap.tf
@@ -23,9 +23,9 @@ resource "aws_security_group_rule" "efs_ingress" {
 
 resource "aws_security_group_rule" "efs_egress" {
   type              = "egress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "all"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.efs.id
 }
