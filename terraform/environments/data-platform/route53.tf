b13381cfad772fca9f2ca5484ea7506e5a9283c5
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-05-10 06:31:59 +0000

    Switch to native resource

--- terraform/environments/data-platform/route53.tf
+++ terraform/environments/data-platform/route53.tf
@@ -1,11 +1,4 @@
-module "data_platform_development_route53" {
-  source  = "terraform-aws-modules/route53/aws//modules/zones"
-  version = "2.10.2"
-
-  zones = {
-    "development.data-platform.service.justice.gov.uk" = {
-      comment = "Data Platform Development"
-      tags = local.tags
-    }
-  }
+resource "aws_route53_zone" "development_data_platform_service_justice_gov_uk" {
+  name = "development.data-platform.service.justice.gov.uk"
+  tags = local.tags
 }
