02bcb3f2f411b3e7ea1df737f8d7eafa97e6aba4
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-05-11 14:29:42 +0100

    CC-1804: Added amazon-ssm-agent for Mailrelay.

--- terraform/environments/ccms-ebs/templates/ec2_user_data_mailrelay.sh
+++ terraform/environments/ccms-ebs/templates/ec2_user_data_mailrelay.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sudo snap install amazon-ssm-agent --classic
+sudo snap start amazon-ssm-agent
+
 hostnamectl set-hostname mailrelay
 
 #/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:cloud-watch-config
