6ae9a25cbad75b8be8afae09e69f9547a3871b2e
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-05-11 14:55:10 +0100

    Tydiying up glue_domain_refresh_job

--- terraform/environments/digital-prison-reporting/main.tf
+++ terraform/environments/digital-prison-reporting/main.tf
@@ -102,11 +102,9 @@ module "glue_domain_refresh_job" {
     "--dpr.aws.kinesis.endpointUrl"   = "https://kinesis.${local.account_region}.amazonaws.com"
     "--dpr.aws.region"                = local.account_region
     "--dpr.curated.s3.path"           = "s3://${module.s3_curated_bucket.bucket_id}/"
-    "--dpr.domain.name"               = "establishment"
-    "--dpr.domain.operation"          = "UPDATE"
     "--dpr.domain.registry"           = "${local.project}-domain-registry-${local.environment}"
-    "--dpr.domain.table.name"         = "establishment"
     "--dpr.domain.target.path"        =  "s3://${module.s3_domain_bucket.bucket_id}/"
+    "--dpr.domain.catalog.db"         = module.glue_data_domain_database.db_name
   }
 }
 
