1a5b07c0d474786ee830b921921f3ae64713fa9a
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-05-12 15:03:47 +0100

    Fix DynamoDB - Indexes

--- terraform/environments/digital-prison-reporting/main.tf
+++ terraform/environments/digital-prison-reporting/main.tf
@@ -629,7 +629,7 @@ module "dynamo_tab_domain_registry" {
 
   global_secondary_indexes = [
     {
-      name            = "primaryId-type-index"
+      name            = "test1"
       hash_key        = "primaryId"
       range_key       = "type"
       write_capacity  = 10
@@ -637,7 +637,7 @@ module "dynamo_tab_domain_registry" {
       projection_type = "ALL"
     },
     {
-      name            = "secondaryId-type-index"
+      name            = "test2"
       hash_key        = "secondaryId"
       range_key       = "type"
       write_capacity  = 10
