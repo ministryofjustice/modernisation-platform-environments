9659613f12c116c6d2d4d5caa15607a8e5b3ded7
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-05-12 14:17:48 +0100

    update db username value (#2191)

--- terraform/environments/tipstaff/secrets.tf
+++ terraform/environments/tipstaff/secrets.tf
@@ -1,13 +1,5 @@
 #### This file can be used to store secrets specific to the member account ####
 
-resource "random_string" "username" {
-  length  = 16
-  lower   = true
-  upper   = true
-  numeric = false
-  special = false
-}
-
 resource "random_password" "password" {
   length  = 16
   lower   = true
@@ -16,6 +8,7 @@ resource "random_password" "password" {
   special = false
 }
 
+// Secrets for the tipstaff database on the modernisation platform
 resource "aws_secretsmanager_secret" "tipstaff_db_secrets" {
   name                    = "tipstaff-db-secrets"
   recovery_window_in_days = 0
@@ -23,9 +16,20 @@ resource "aws_secretsmanager_secret" "tipstaff_db_secrets" {
 
 resource "aws_secretsmanager_secret_version" "rds_credentials" {
   secret_id     = aws_secretsmanager_secret.tipstaff_db_secrets.id
-  secret_string = jsonencode({ "TIPSTAFF_DB_USERNAME" : "${random_string.username.result}", "TIPSTAFF_DB_PASSWORD" : "${random_password.password.result}" })
+  secret_string = jsonencode({ "TIPSTAFF_DB_PASSWORD" : "${random_password.password.result}" })
 }
 
+data "aws_secretsmanager_secret" "get_tipstaff_db_secrets" {
+  depends_on = [aws_secretsmanager_secret_version.rds_credentials]
+  arn        = aws_secretsmanager_secret_version.rds_credentials.arn
+}
+
+data "aws_secretsmanager_secret_version" "get_rds_credentials" {
+  depends_on = [aws_secretsmanager_secret_version.rds_credentials]
+  secret_id  = data.aws_secretsmanager_secret.get_tipstaff_db_secrets.id
+}
+
+// Secrets for the tactical products database
 resource "aws_secretsmanager_secret" "tactical_products_db_secrets" {
   name                    = "tipstaff-tactical-products-db-secrets"
   recovery_window_in_days = 0
@@ -36,13 +40,23 @@ resource "aws_secretsmanager_secret_version" "tactical_products_rds_credentials"
   secret_string = jsonencode({ "" : "", "" : "" })
 }
 
-data "aws_secretsmanager_secret" "secrets" {
+data "aws_secretsmanager_secret" "get_tactical_products_db_secrets" {
   depends_on = [aws_secretsmanager_secret_version.tactical_products_rds_credentials]
   arn        = aws_secretsmanager_secret_version.tactical_products_rds_credentials.arn
 }
 
-data "aws_secretsmanager_secret_version" "current" {
+data "aws_secretsmanager_secret_version" "get_tactical_products_rds_credentials" {
   depends_on = [aws_secretsmanager_secret_version.tactical_products_rds_credentials]
-  secret_id  = data.aws_secretsmanager_secret.secrets.id
+  secret_id  = data.aws_secretsmanager_secret.get_tactical_products_db_secrets.id
 }
 
+// Create secret to store the CICD user credentials
+resource "aws_secretsmanager_secret" "cicd_user_credentials" {
+  name                    = "cicd-user-credentials"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "cicd_user_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.cicd_user_credentials.id
+  secret_string = jsonencode({ "" : "", "" : "" })
+}
