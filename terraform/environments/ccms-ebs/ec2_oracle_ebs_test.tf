f42b6f9328ae9ea568f156c5eacb36df65e5d0ba
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-05-12 12:40:13 +0100

    ami change

--- terraform/environments/ccms-ebs/ec2_oracle_ebs_test.tf
+++ terraform/environments/ccms-ebs/ec2_oracle_ebs_test.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "ec2_oracle_ebs2" {
   instance_type = local.application_data.accounts[local.environment].ec2_oracle_instance_type_ebsdb
   #ami                         = data.aws_ami.oracle_db.id
-  ami                         = data.aws_ami.accessgate_ami_name.id
+  ami                         = data.aws_ami.accessgate.id
   key_name                    = local.application_data.accounts[local.environment].key_name
   vpc_security_group_ids      = [aws_security_group.ec2_sg_ebsdb.id]
   subnet_id                   = data.aws_subnet.data_subnets_a.id
