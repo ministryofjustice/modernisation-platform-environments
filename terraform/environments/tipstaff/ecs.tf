fc490f236829e3f6ea7e773664a3e3cca6436816
matthewsearle01 <matthewsearle01@gmail.com>
2023-05-12 16:00:12 +0100

    comment out resources to destroy and recreate rds db

--- terraform/environments/tipstaff/ecs.tf
+++ terraform/environments/tipstaff/ecs.tf
@@ -10,112 +10,112 @@ resource "aws_cloudwatch_log_group" "tipstaffFamily_logs" {
   name = "/ecs/tipstaffFamily"
 }
 
-resource "aws_ecs_task_definition" "tipstaff_task_definition" {
-  family                   = "tipstaffFamily"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  execution_role_arn       = aws_iam_role.app_execution.arn
-  task_role_arn            = aws_iam_role.app_task.arn
-  cpu                      = 1024
-  memory                   = 2048
-  container_definitions = jsonencode([
-    {
-      name      = "tipstaff-container"
-      image     = "mcr.microsoft.com/dotnet/framework/aspnet:4.8"
-      cpu       = 1024
-      memory    = 2048
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          protocol      = "tcp"
-          hostPort      = 80
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = "${aws_cloudwatch_log_group.tipstaffFamily_logs.name}"
-          awslogs-region        = "eu-west-2"
-          awslogs-stream-prefix = "ecs"
-        }
-      }
-      environment = [
-        {
-          name  = "RDS_HOSTNAME"
-          value = "${aws_db_instance.tipstaff_db.address}"
-        },
-        {
-          name  = "RDS_PORT"
-          value = "${local.application_data.accounts[local.environment].rds_port}"
-        },
-        {
-          name  = "RDS_USERNAME"
-          value = "${aws_db_instance.tipstaff_db.username}"
-        },
-        {
-          name  = "RDS_PASSWORD"
-          value = "${aws_db_instance.tipstaff_db.password}"
-        },
-        {
-          name  = "DB_NAME"
-          value = "${aws_db_instance.tipstaff_db.db_name}"
-        },
-        {
-          name  = "supportEmail"
-          value = "${local.application_data.accounts[local.environment].support_email}"
-        },
-        {
-          name  = "supportTeam"
-          value = "${local.application_data.accounts[local.environment].support_team}"
-        },
-        {
-          name  = "CurServer"
-          value = "${local.application_data.accounts[local.environment].curserver}"
-        },
-        {
-          name  = "ida:ClientId"
-          value = "${local.application_data.accounts[local.environment].client_id}"
-        }
-      ]
-    }
-  ])
-  runtime_platform {
-    operating_system_family = "WINDOWS_SERVER_2019_CORE"
-    cpu_architecture        = "X86_64"
-  }
-}
+# resource "aws_ecs_task_definition" "tipstaff_task_definition" {
+#   family                   = "tipstaffFamily"
+#   requires_compatibilities = ["FARGATE"]
+#   network_mode             = "awsvpc"
+#   execution_role_arn       = aws_iam_role.app_execution.arn
+#   task_role_arn            = aws_iam_role.app_task.arn
+#   cpu                      = 1024
+#   memory                   = 2048
+#   container_definitions = jsonencode([
+#     {
+#       name      = "tipstaff-container"
+#       image     = "mcr.microsoft.com/dotnet/framework/aspnet:4.8"
+#       cpu       = 1024
+#       memory    = 2048
+#       essential = true
+#       portMappings = [
+#         {
+#           containerPort = 80
+#           protocol      = "tcp"
+#           hostPort      = 80
+#         }
+#       ]
+#       logConfiguration = {
+#         logDriver = "awslogs"
+#         options = {
+#           awslogs-group         = "${aws_cloudwatch_log_group.tipstaffFamily_logs.name}"
+#           awslogs-region        = "eu-west-2"
+#           awslogs-stream-prefix = "ecs"
+#         }
+#       }
+#       environment = [
+#         {
+#           name  = "RDS_HOSTNAME"
+#           value = "${aws_db_instance.tipstaff_db.address}"
+#         },
+#         {
+#           name  = "RDS_PORT"
+#           value = "${local.application_data.accounts[local.environment].rds_port}"
+#         },
+#         {
+#           name  = "RDS_USERNAME"
+#           value = "${aws_db_instance.tipstaff_db.username}"
+#         },
+#         {
+#           name  = "RDS_PASSWORD"
+#           value = "${aws_db_instance.tipstaff_db.password}"
+#         },
+#         {
+#           name  = "DB_NAME"
+#           value = "${aws_db_instance.tipstaff_db.db_name}"
+#         },
+#         {
+#           name  = "supportEmail"
+#           value = "${local.application_data.accounts[local.environment].support_email}"
+#         },
+#         {
+#           name  = "supportTeam"
+#           value = "${local.application_data.accounts[local.environment].support_team}"
+#         },
+#         {
+#           name  = "CurServer"
+#           value = "${local.application_data.accounts[local.environment].curserver}"
+#         },
+#         {
+#           name  = "ida:ClientId"
+#           value = "${local.application_data.accounts[local.environment].client_id}"
+#         }
+#       ]
+#     }
+#   ])
+#   runtime_platform {
+#     operating_system_family = "WINDOWS_SERVER_2019_CORE"
+#     cpu_architecture        = "X86_64"
+#   }
+# }
 
-resource "aws_ecs_service" "tipstaff_ecs_service" {
-  depends_on = [
-    aws_lb_listener.tipstaff_lb_1,
-    aws_lb_listener.tipstaff_lb_2
-  ]
+# resource "aws_ecs_service" "tipstaff_ecs_service" {
+#   depends_on = [
+#     aws_lb_listener.tipstaff_lb_1,
+#     aws_lb_listener.tipstaff_lb_2
+#   ]
 
-  name                              = var.networking[0].application
-  cluster                           = aws_ecs_cluster.tipstaff_cluster.id
-  task_definition                   = aws_ecs_task_definition.tipstaff_task_definition.arn
-  launch_type                       = "FARGATE"
-  enable_execute_command            = true
-  desired_count                     = 1
-  health_check_grace_period_seconds = 90
+#   name                              = var.networking[0].application
+#   cluster                           = aws_ecs_cluster.tipstaff_cluster.id
+#   task_definition                   = aws_ecs_task_definition.tipstaff_task_definition.arn
+#   launch_type                       = "FARGATE"
+#   enable_execute_command            = true
+#   desired_count                     = 1
+#   health_check_grace_period_seconds = 90
 
-  network_configuration {
-    subnets          = data.aws_subnets.shared-public.ids
-    security_groups  = [aws_security_group.ecs_service.id]
-    assign_public_ip = true
-  }
+#   network_configuration {
+#     subnets          = data.aws_subnets.shared-public.ids
+#     security_groups  = [aws_security_group.ecs_service.id]
+#     assign_public_ip = true
+#   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.tipstaff_target_group.arn
-    container_name   = "tipstaff-container"
-    container_port   = 80
-  }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.tipstaff_target_group.arn
+#     container_name   = "tipstaff-container"
+#     container_port   = 80
+#   }
 
-  deployment_controller {
-    type = "ECS"
-  }
-}
+#   deployment_controller {
+#     type = "ECS"
+#   }
+# }
 
 resource "aws_iam_role" "app_execution" {
   name = "execution-${var.networking[0].application}"
