a0e9edc4b0b1e5b8015b4e5b44876b5f2b1ce84f
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-05-12 11:36:20 +0100

    delius-core openldap: ALB -> NLB

--- terraform/environments/delius-core/alb_ldap.tf
+++ terraform/environments/delius-core/alb_ldap.tf
@@ -1,5 +1,6 @@
 locals {
-  openldap_alb_name = "openldap-alb"
+  openldap_name     = format("%s-openldap", local.application_name)
+  openldap_alb_name = format("%s-alb", local.openldap_name)
   openldap_alb_tags = merge(
     local.tags,
     {
@@ -8,13 +9,12 @@ locals {
   )
 
   openldap_protocol = "tcp"
-
 }
 resource "aws_lb" "ldap" {
   name                       = local.openldap_alb_name
   internal                   = true
-  load_balancer_type         = "application"
-  security_groups            = [aws_security_group.ldap_alb.id]
+  load_balancer_type         = "network"
+  security_groups            = [aws_security_group.ldap_nlb.id]
   subnets                    = data.aws_subnets.shared-private.ids
   drop_invalid_header_fields = true
   enable_deletion_protection = false
@@ -22,7 +22,7 @@ resource "aws_lb" "ldap" {
   tags = local.openldap_alb_tags
 }
 
-resource "aws_security_group" "ldap_alb" {
+resource "aws_security_group" "ldap_nlb" {
   name        = local.openldap_alb_name
   description = "allow inbound traffic from the VPN and within the VPC on port 389"
   vpc_id      = data.aws_vpc.shared.id
@@ -32,8 +32,7 @@ resource "aws_security_group" "ldap_alb" {
     to_port   = local.openldap_port
     protocol  = local.openldap_protocol
     cidr_blocks = [
-      "81.134.202.29/32",
-      data.aws_vpc.shared.cidr_block,
+      data.aws_vpc.shared.cidr_block
     ]
   }
 
@@ -61,7 +60,7 @@ resource "aws_lb_listener" "ldap" {
 }
 
 resource "aws_lb_target_group" "ldap" {
-  name     = "openldap"
+  name     = local.openldap_name
   port     = local.openldap_port
   protocol = local.openldap_protocol
   vpc_id   = data.aws_vpc.shared.id
@@ -71,7 +70,7 @@ resource "aws_lb_target_group" "ldap" {
   tags = merge(
     local.tags,
     {
-      Name = "openldap"
+      Name = local.openldap_name
     }
   )
 }
