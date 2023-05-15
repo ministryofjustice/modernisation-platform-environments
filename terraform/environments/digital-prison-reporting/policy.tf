13bcf6cde2e2f71bd7d9154bfc9b8c5410c549ba
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-05-15 08:58:21 +0100

    Redshift Spectrum Role Policy

--- terraform/environments/digital-prison-reporting/policy.tf
+++ terraform/environments/digital-prison-reporting/policy.tf
@@ -28,44 +28,63 @@ data "aws_iam_policy_document" "glue-policy-data" {
   }
 }
 
-# Resuse for all S3 read Only -- TBC
+# Resuse for all S3 read Only
 # S3 Read Only Policy
-#resource "aws_iam_policy" "read_s3_read_access_policy" {
-#  name = "dpr_s3_read_policy"
-#  policy = jsonencode({
-#    "Version" : "2012-10-17",
-#    "Statement" : [
-#      {
-#        "Sid" : "AllowUserToSeeBucketListInTheConsole",
-#        "Action" : ["s3:ListAllMyBuckets", "s3:GetBucketLocation"],
-#        "Effect" : "Allow",
-#        "Resource" : ["arn:aws:s3:::*"]
-#      },
-#      {
-#        "Effect" : "Allow",
-#        "Action" : [
-#          "s3:List*"
-#          "s3:Get*",
-#        ],
-#        "Resource" : [
-#          module.s3_glue_job_bucket.bucket_arn,
-#          "${module.s3_glue_job_bucket.bucket_arn}/*",
-#          module.s3_landing_bucket.bucket_arn,
-#          "${module.s3_landing_bucket.bucket_arn}/*",
-#          module.s3_working_bucket.bucket_arn,
-#          "${module.s3_working_bucket.bucket_arn}/*"                 
-#        ]
-#      }
-#    ]
-#  })
-#}
+resource "aws_iam_policy" "s3_read_access_policy" {
+  name = "dpr_s3_read_policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowUserToSeeBucketListInTheConsole",
+        "Action" : ["s3:ListAllMyBuckets", "s3:GetBucketLocation"],
+        "Effect" : "Allow",
+        "Resource" : ["arn:aws:s3:::*"]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:List*",
+          "s3:Get*",
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${var.project_id}-*/*",
+          "arn:aws:s3:::${var.project_id}-*"              
+        ]
+      }
+    ]
+  })
+}
+
+# KMS Read/Decrypt Policy
+resource "aws_iam_policy" "kms_read_access_policy" {
+  name = "dpr_kms_read_policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:Encrypt*",
+          "kms:Decrypt*",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey",
+        ],
+        "Resource" : [
+          "arn:aws:kms:*:${var.account}:key/*"              
+        ]
+      }
+    ]
+  })
+}
 
 ### Iam Role for AWS Redshift
 # Amazon Redshift supports only identity-based policies (IAM policies).
 
 resource "aws_iam_role" "redshift-role" {
   count = local.setup_datamart ? 1 : 0
-  name  = "dpr-redshift-cluster-role"
+  name  = "${local.project}-redshift-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -222,4 +241,78 @@ resource "aws_iam_role_policy" "dmsvpcpolicy" {
     ]
 }
 EOF
+}
+
+### Iam User Role for AWS Redshift Spectrum, 
+resource "aws_iam_role" "redshift-spectrum-role" {
+  name  = "${local.project}-redshift-spectrum-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sts:AssumeRole"
+        ]
+        Principal = {
+          "Service" = "redshift.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = merge(
+    local.tags,
+    {
+      name    = "redshift-spectrum-role"
+      project = "dpr"
+    }
+  )
+}
+
+data "aws_iam_policy_document" "extra_policy_document" {   
+  statement {
+    actions = [
+				"glue:BatchCreatePartition",
+				"glue:UpdateDatabase",
+				"glue:CreateTable",
+				"glue:DeleteDatabase",
+				"glue:GetTables",
+				"glue:GetPartitions",
+				"glue:BatchDeletePartition",
+				"glue:UpdateTable",
+				"glue:BatchGetPartition",
+				"glue:DeleteTable",
+				"glue:GetDatabases",
+				"glue:GetTable",
+				"glue:GetDatabase",
+				"glue:GetPartition",
+				"glue:CreateDatabase",
+				"glue:BatchDeleteTable",
+				"glue:CreatePartition",
+				"glue:DeletePartition",
+				"glue:UpdatePartition"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "redshift_extra_policy" {
+  name        = "${local.project}-redshift-spectrum-policy"
+  description = "Extra Policy for AWS Redshift Spectrum"
+  policy      = data.aws_iam_policy_document.extra_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "redshift_spectrum" {
+  for_each = toset([
+    aws_iam_policy.s3_read_access_policy.arn,
+    aws_iam_policy.kms_read_access_policy.arn,
+    aws_iam_policy.redshift_extra_policy.arn
+  ])
+
+  role = aws_iam_role.redshift-spectrum-role.name
+  policy_arn = each.value
 }
