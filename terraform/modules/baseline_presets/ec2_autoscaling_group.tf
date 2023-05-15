78310f3c4e3f52652fedd4c3f5f0aced7f47dc9d
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-05-15 11:10:11 +0100

    fix

--- terraform/modules/baseline_presets/ec2_autoscaling_group.tf
+++ terraform/modules/baseline_presets/ec2_autoscaling_group.tf
@@ -5,12 +5,14 @@ locals {
     default = {
       desired_capacity    = 1
       max_size            = 1
+      force_delete        = true
       vpc_zone_identifier = var.environment.subnets["private"].ids
     }
 
     default_with_ready_hook = {
       desired_capacity    = 1
       max_size            = 1
+      force_delete        = true
       vpc_zone_identifier = var.environment.subnets["private"].ids
 
       initial_lifecycle_hooks = {
@@ -25,6 +27,7 @@ locals {
     default_with_ready_hook_and_warm_pool = {
       desired_capacity    = 1
       max_size            = 1
+      force_delete        = true
       vpc_zone_identifier = var.environment.subnets["private"].ids
 
       initial_lifecycle_hooks = {
