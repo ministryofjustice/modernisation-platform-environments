48c16b86846886ad0e465f418e0a3a25a7125413
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-05-16 13:53:20 +0100

    nomis: DSOS-1893: enable warm pools (#2219)
    * test ready hooks in dev
    
    * add abandon step into ec2provision
    
    * test
    
    * test
    
    * test
    
    * fix
    
    * fix post script
    
    * test
    
    * fix
    
    * test
    
    * fix
    
    * test
    
    * test
    
    * tag
    
    * add warm pools
    
    * zap db
    
    * oasys fix
    
    * Fix
    
    * Fix

--- terraform/modules/baseline_presets/ec2_autoscaling_group.tf
+++ terraform/modules/baseline_presets/ec2_autoscaling_group.tf
@@ -1,9 +1,50 @@
 locals {
 
   ec2_autoscaling_group = {
-    desired_capacity    = 1
-    max_size            = 2
-    vpc_zone_identifier = var.environment.subnets["private"].ids
+
+    default = {
+      desired_capacity    = 1
+      max_size            = 1
+      force_delete        = true
+      vpc_zone_identifier = var.environment.subnets["private"].ids
+    }
+
+    default_with_ready_hook = {
+      desired_capacity          = 1
+      max_size                  = 1
+      force_delete              = true
+      vpc_zone_identifier       = var.environment.subnets["private"].ids
+      wait_for_capacity_timeout = 0
+
+      initial_lifecycle_hooks = {
+        "ready-hook" = {
+          default_result       = "ABANDON"
+          heartbeat_timeout    = 7200
+          lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+        }
+      }
+    }
+
+    default_with_ready_hook_and_warm_pool = {
+      desired_capacity          = 1
+      max_size                  = 1
+      force_delete              = true
+      vpc_zone_identifier       = var.environment.subnets["private"].ids
+      wait_for_capacity_timeout = 0
+
+      initial_lifecycle_hooks = {
+        "ready-hook" = {
+          default_result       = "ABANDON"
+          heartbeat_timeout    = 7200
+          lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+        }
+      }
+
+      warm_pool = {
+        reuse_on_scale_in           = true
+        max_group_prepared_capacity = 1
+      }
+    }
   }
 
   ec2_autoscaling_schedules = {
