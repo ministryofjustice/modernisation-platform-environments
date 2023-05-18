065f2002de065ec51a2ada2e18cd9df851b503b4
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-05-18 10:55:15 +0100

    add dev-jumpserver-2022 asg with zero instances to nomis-test env

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -46,6 +46,32 @@ locals {
           oracle-db-name       = "T1CNOM"
         })
       })
+
+      dev-jumpserver-2022 = {
+        # ami has unwanted ephemeral device, don't copy all the ebs_volumess
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "nomis_windows_server_2022_jumpserver_release_*"
+          availability_zone             = null
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = base64encode(file("./templates/jumpserver-user-data.yaml"))
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private-jumpserver"]
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        autoscaling_group     = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 0 # set to 0 while testing
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Windows Server 2022 Jumpserver for NOMIS"
+          os-type     = "Windows"
+          component   = "jumpserver"
+          server-type = "nomis-jumpserver"
+        }
+      }
     }
 
     baseline_ec2_instances = {
@@ -104,6 +130,8 @@ locals {
         })
         cloudwatch_metric_alarms = {} # disabled until migration
       })
+
+      
     }
 
     baseline_lbs = {
