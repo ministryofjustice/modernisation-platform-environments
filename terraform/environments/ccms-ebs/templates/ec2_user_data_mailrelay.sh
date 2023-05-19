7b8ced7f3d77bcb1635fd5bd7960a98df60f1e50
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-05-19 10:55:21 +0100

    CC-1804: Fixed typos.

--- terraform/environments/ccms-ebs/templates/ec2_user_data_mailrelay.sh
+++ terraform/environments/ccms-ebs/templates/ec2_user_data_mailrelay.sh
@@ -6,10 +6,10 @@ yum install -y amazon-cloudwatch-agent cyrus-sasl-plain jq nc postfix telnet
 amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:cloud-watch-config
 
 S=$(aws secretsmanager get-secret-value --secret-id ses-smtp-credentials --region eu-west-2 |jq '.SecretString')
-U=$(cut -d'"' -f 3 <<< ${S} |tr -d \\134)
-P=$(cut -d'"' -f 5 <<< ${S} |tr -d \\134)
+U=$(cut -d'"' -f 3 <<< $${S} |tr -d \\134)
+P=$(cut -d'"' -f 5 <<< $${S} |tr -d \\134)
 
-echo "[email-smtp.us-west-2.amazonaws.com]:587 ${U}:${P}" >  /etc/postfix/sasl_passwd
+echo "[email-smtp.us-west-2.amazonaws.com]:587 $${U}:$${P}" > /etc/postfix/sasl_passwd
 postmap hash:/etc/postfix/sasl_passwd
 chmod 600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
 echo -e "\n# Configuration for SES:\n" >> /etc/postfix/main.cf
