4b33ca8f3491985e145beda0c21e7a30123622a6
James Borwick <james.borwick@icloud.com>
2023-05-22 19:44:20 +0100

    Adding load balancer for development

--- terraform/environments/oasys/locals_development.tf
+++ terraform/environments/oasys/locals_development.tf
@@ -57,46 +57,53 @@ locals {
 
     baseline_lbs = {
 
-      # private = {
-      #   internal_lb              = true
-      #   enable_delete_protection = false
-      #   existing_target_groups   = {}
-      #   idle_timeout             = 60 # 60 is default
-      #   security_groups          = ["private"]
-      #   public_subnets           = module.environment.subnets["private"].ids
-      #   tags                     = local.tags
+      private = {
+        internal_lb              = true
+        enable_delete_protection = false
+        existing_target_groups   = {}
+        idle_timeout             = 60 # 60 is default
+        security_groups          = ["private"]
+        public_subnets           = module.environment.subnets["private"].ids
+        tags                     = local.tags
 
-      #   listeners = {
-      #     https = {
-      #       port                      = 443
-      #       protocol                  = "HTTPS"
-      #       ssl_policy                = "ELBSecurityPolicy-2016-08"
-      #       certificate_names_or_arns = ["application_environment_wildcard_cert"]
-      #       default_action = {
-      #         type              = "forward"
-      #         target_group_name = "${local.application_name}-web-http-8080"
-      #       }
-      #       rules = {
-      #         web-http-8080 = {
-      #           priority = 100
-      #           actions = [{
-      #             type              = "forward"
-      #             target_group_name = "${local.application_name}-web-http-8080"
-      #           }]
-      #           conditions = [
-      #             {
-      #               host_header = {
-      #                 values = [
-      #                   "web.${module.environment.domains.public.application_environment}", # web.oasys.hmpps-development.modernisation-platform.service.justice.gov.uk
-      #                 ]
-      #               }
-      #             }
-      #           ]
-      #         }
-      #       }
-      #     }
-      #   }
-      # }
+        listeners = {
+          https = {
+            port                      = 443
+            protocol                  = "HTTPS"
+            ssl_policy                = "ELBSecurityPolicy-2016-08"
+            certificate_names_or_arns = ["application_environment_wildcard_cert"]
+            # cloudwatch_metric_alarms  = module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso_pagerduty"].lb_default
+
+            default_action = {
+              type = "fixed-response"
+              fixed_response = {
+                content_type = "text/plain"
+                message_body = "Not implemented"
+                status_code  = "501"
+              }
+            }
+            rules = {
+              web-http-8080 = {
+                priority = 100
+                actions = [{
+                  type              = "forward"
+                  target_group_name = "${local.application_name}-web-http-8080"
+                }]
+                conditions = [
+                  {
+                    host_header = {
+                      values = [
+                        # web.oasys.hmpps-development.modernisation-platform.service.justice.gov.uk
+                        "web.${module.environment.domains.public.application_environment}",
+                      ]
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }
     }
 
     baseline_route53_zones = {
