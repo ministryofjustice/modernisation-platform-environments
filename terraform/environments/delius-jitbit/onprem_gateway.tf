e382ff46a4496e5bd5af6e6344a71856eb87f4a7
Seb Norris <sebastian@automationlogic.com>
2023-05-22 10:15:55 +0100

    try using module

--- terraform/environments/delius-jitbit/onprem_gateway.tf
+++ terraform/environments/delius-jitbit/onprem_gateway.tf
@@ -8,45 +8,36 @@ resource "aws_instance" "onprem_gateway" {
   }
 }
 
-#resource "aws_iam_instance_profile" "onprem_gateway" {
-#  name = "onprem_gateway"
-#  role = aws_iam_role.onprem_gateway.name
-#  path = "/"
-#}
+module "onprem_gateway" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance"
 
-#resource "aws_iam_role" "onprem_gateway" {
-#  name               = "onprem_gateway"
-#  assume_role_policy = data.aws_iam_policy_document.onprem_gateway_trust_policy_doc.json
-#}
+  providers = {
+    aws.core-vpc = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
+  }
 
+  name = "onprem_gateway"
 
-#data "aws_iam_policy_document" "onprem_gateway_trust_policy_doc" {
-#  statement {
-#  }
-#}
+  ami_name                      = "374269020027/mp_WindowsServer2022_2023-04-01T00-00-17.453Z"
+  ami_owner                     = "core-shared-services-production"
+  instance                      = "t3.medium"
+  ebs_volumes_copy_all_from_ami = true
+  ebs_kms_key_id                = data.aws_kms_key.ebs_shared.arn
+  ebs_volume_config             = {}
+  ebs_volumes                   = {}
+  ssm_parameters_prefix         = null
+  ssm_parameters                = null
+  route53_records               = {}
 
+  iam_resource_names_prefix = ""
+  instance_profile_policies = local.ec2_common_managed_policies
 
-#resource "aws_security_group" "onprem_gateway" {
-    
-#   name        = "onprem_gateway"
-#   description = "jitbit onprem gateway"
-#   vpc_id      = aws_vpc.REPLACEME.id
-   
-#   ingress {
-#    description      = ""
-#    from_port        = 443
-#    to_port          = 443
-#    protocol         = "tcp"
-#    cidr_blocks      = [aws_vpc.main.cidr_block]
-#    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-#  }
-
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#}
+  business_unit            = ${var.networking[0].business-unit}
+  application_name         = local.application_name
+  environment              = local.environment
+  region                   = local.region
+  availability_zone        = local.availability_zone_1
+  subnet_id                = module.environment.subnet["private"][local.availability_zone_1].id
+  tags                     = {}
+  account_ids_lookup       = local.environment_management.account_ids
+  cloudwatch_metric_alarms = {}
+}
