74023321379c94eb03af9509a8f604e2e01b5043
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-05-22 14:36:32 +0100

    test

--- terraform/modules/baseline/route53.tf
+++ terraform/modules/baseline/route53.tf
@@ -170,7 +170,10 @@ resource "aws_route53_query_log" "this" {
   cloudwatch_log_group_arn = aws_cloudwatch_log_group.route53[each.key].arn
   zone_id                  = aws_route53_zone.this[each.key].zone_id
 
-  depends_on = [aws_iam_policy.this]
+  depends_on = [
+    aws_cloudwatch_log_group.route53,
+    aws_iam_policy.this,
+  ]
 }
 
 resource "aws_route53_record" "self" {
