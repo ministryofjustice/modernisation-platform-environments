e6d555f7e76a57fb843c6a425170aa67b99a2c99
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-05-23 13:41:16 +0100

    Change naming

--- terraform/environments/data-platform/api.tf
+++ terraform/environments/data-platform/api.tf
@@ -1,81 +1,29 @@
-# resource "aws_api_gateway_rest_api" "data_platform" {
-#   name          = "data_platform"
-#   protocol_type = "HTTP"
-#   tags = local.tags
-# }
-
-# resource "aws_apigatewayv2_stage" "api_gateway_stage" {
-#   api_id = aws_api_gateway_rest_api.data_platform.id
-
-#   name        = local.environment
-#   auto_deploy = true
-
-#   access_log_settings {
-#     destination_arn = aws_cloudwatch_log_group.api_gw.arn
-
-#     format = jsonencode({
-#       requestId               = "$context.requestId"
-#       sourceIp                = "$context.identity.sourceIp"
-#       requestTime             = "$context.requestTime"
-#       protocol                = "$context.protocol"
-#       httpMethod              = "$context.httpMethod"
-#       resourcePath            = "$context.resourcePath"
-#       routeKey                = "$context.routeKey"
-#       status                  = "$context.status"
-#       responseLength          = "$context.responseLength"
-#       integrationErrorMessage = "$context.integrationErrorMessage"
-#       }
-#     )
-#   }
-# }
-
-# resource "aws_apigatewayv2_integration" "presigned_url" {
-#   api_id = aws_api_gateway_rest_api.data_platform.id
-
-#   integration_uri    = aws_lambda_function.presigned_url.invoke_arn
-#   integration_type   = "AWS_PROXY"
-#   integration_method = "GET"
-# }
-
-# resource "aws_apigatewayv2_route" "presigned_url" {
-#   api_id = aws_api_gateway_rest_api.data_platform.id
-
-#   route_key = "GET /upload_data"
-#   target    = "integrations/${aws_apigatewayv2_integration.presigned_url.id}"
-# }
-
-# resource "aws_cloudwatch_log_group" "api_gw" {
-#   name = "/aws/api_gw/${aws_api_gateway_rest_api.data_platform.name}"
-
-#   retention_in_days = 30
-# }
-
-resource "aws_api_gateway_rest_api" "example" {
-  name = "example"
+resource "aws_api_gateway_rest_api" "data_platform" {
+  name = "data_platform"
 }
 
-resource "aws_api_gateway_resource" "example" {
-  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "example"
-  rest_api_id = aws_api_gateway_rest_api.example.id
+resource "aws_api_gateway_resource" "upload_data" {
+  parent_id   = aws_api_gateway_rest_api.data_platform.root_resource_id
+  path_part   = "upload_data"
+  rest_api_id = aws_api_gateway_rest_api.data_platform.id
 }
 
-resource "aws_api_gateway_method" "example" {
+resource "aws_api_gateway_method" "upload_data_get" {
   authorization = "CUSTOM"
   http_method   = "GET"
-  resource_id   = aws_api_gateway_resource.example.id
-  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.upload_data.id
+  rest_api_id   = aws_api_gateway_rest_api.data_platform.id
 }
 
 resource "aws_api_gateway_integration" "example" {
-  http_method = aws_api_gateway_method.example.http_method
-  resource_id = aws_api_gateway_resource.example.id
-  rest_api_id = aws_api_gateway_rest_api.example.id
+  http_method = aws_api_gateway_method.upload_data_get.http_method
+  resource_id = aws_api_gateway_resource.upload_data.id
+  rest_api_id = aws_api_gateway_rest_api.data_platform.id
   type        = "MOCK"
 }
 
 resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
+  rest_api_id = aws_api_gateway_rest_api.data_platform.id
 
   triggers = {
     # NOTE: The configuration below will satisfy ordering considerations,
@@ -99,13 +47,13 @@ resource "aws_api_gateway_deployment" "example" {
 
 resource "aws_api_gateway_stage" "example" {
   deployment_id = aws_api_gateway_deployment.example.id
-  rest_api_id   = aws_api_gateway_rest_api.example.id
+  rest_api_id   = aws_api_gateway_rest_api.data_platform.id
   stage_name    = "example"
 }
 
 resource "aws_api_gateway_authorizer" "authorizer" {
   name                   = "authorizer-${local.environment}"
-  rest_api_id            = aws_api_gateway_rest_api.example.id
+  rest_api_id            = aws_api_gateway_rest_api.data_platform.id
   authorizer_uri         = aws_lambda_function.authoriser.invoke_arn
   authorizer_credentials = aws_iam_role.authoriser_lambda_role.arn
 }
