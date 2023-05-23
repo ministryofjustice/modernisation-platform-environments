ac27e2f85fc97ae962b16209afa7517135fec253
James Borwick <james.borwick@icloud.com>
2023-05-23 11:56:26 +0100

    New resources in oasys-development (#2290)
    * Adding load balancer for development
    
    * Creating alias record
    
    * Adding certificate
    
    * Changed cert
    
    * Changed listener cert
    
    * Modified alias record
    
    * terraform fmt

--- terraform/environments/oasys/locals_development.tf
+++ terraform/environments/oasys/locals_development.tf
@@ -38,75 +38,83 @@ locals {
     }
 
     baseline_acm_certificates = {
-      "${local.application_name}_wildcard_cert" = {
-        # domain_name limited to 64 chars so use modernisation platform domain for this
-        # and put the wildcard in the san
+      "${local.application_name}_wildcard_cert_02" = {
+        # Domain_name limited to 64 chars so use modernisation platform domain
+        # for this and put the wildcard in the san.
         domain_name = module.environment.domains.public.modernisation_platform
         subject_alternate_names = [
-          "*.${module.environment.domains.public.application_environment}", # *.oasys.hmpps-development.modernisation-platform.service.justice.gov.uk
-          "*.dev.${module.environment.domains.public.short_name}",          # "dev.oasys.service.justice.gov.uk"
-          "*.dev.${local.application_name}.az.justice.gov.uk",
+          # *.oasys.hmpps-development.modernisation-platform.service.justice.gov.uk
+          "*.${module.environment.domains.public.application_environment}",
+          # web.oasys.hmpps-development.modernisation-platform.service.justice.gov.uk
+          "web.${module.environment.domains.public.application_environment}",
         ]
         external_validation_records_created = true
-        cloudwatch_metric_alarms            = module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso_pagerduty"].acm_default
+        # cloudwatch_metric_alarms            = module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso_pagerduty"].acm_default
         tags = {
-          description = "wildcard cert for ${local.application_name} ${local.environment} domains"
+          description = "Web cert for ${local.application_name} ${local.environment} domains"
         }
       }
     }
 
     baseline_lbs = {
 
-      # private = {
-      #   internal_lb              = true
-      #   enable_delete_protection = false
-      #   existing_target_groups   = {}
-      #   idle_timeout             = 60 # 60 is default
-      #   security_groups          = ["private"]
-      #   public_subnets           = module.environment.subnets["private"].ids
-      #   tags                     = local.tags
+      private = {
+        internal_lb              = true
+        enable_delete_protection = false
+        existing_target_groups   = {}
+        idle_timeout             = 60 # 60 is default
+        security_groups          = ["private"]
+        public_subnets           = module.environment.subnets["private"].ids
+        tags                     = local.tags
 
-      #   listeners = {
-      #     https = {
-      #       port                      = 443
-      #       protocol                  = "HTTPS"
-      #       ssl_policy                = "ELBSecurityPolicy-2016-08"
-      #       certificate_names_or_arns = ["application_environment_wildcard_cert"]
-      #       default_action = {
-      #         type              = "forward"
-      #         target_group_name = "${local.application_name}-web-http-8080"
-      #       }
-      #       rules = {
-      #         web-http-8080 = {
-      #           priority = 100
-      #           actions = [{
-      #             type              = "forward"
-      #             target_group_name = "${local.application_name}-web-http-8080"
-      #           }]
-      #           conditions = [
-      #             {
-      #               host_header = {
-      #                 values = [
-      #                   "web.${module.environment.domains.public.application_environment}", # web.oasys.hmpps-development.modernisation-platform.service.justice.gov.uk
-      #                 ]
-      #               }
-      #             }
-      #           ]
-      #         }
-      #       }
-      #     }
-      #   }
-      # }
+        listeners = {
+          https = {
+            port                      = 443
+            protocol                  = "HTTPS"
+            ssl_policy                = "ELBSecurityPolicy-2016-08"
+            certificate_names_or_arns = ["${local.application_name}_wildcard_cert_02"]
+            # cloudwatch_metric_alarms  = module.baseline_presets.cloudwatch_metric_alarms_lists_with_actions["dso_pagerduty"].lb_default
+
+            default_action = {
+              type = "fixed-response"
+              fixed_response = {
+                content_type = "text/plain"
+                message_body = "Not implemented"
+                status_code  = "501"
+              }
+            }
+            rules = {
+              web-http-8080 = {
+                priority = 100
+                actions = [{
+                  type              = "forward"
+                  target_group_name = "${local.application_name}-web-http-8080"
+                }]
+                conditions = [
+                  {
+                    host_header = {
+                      values = [
+                        # web-oasys.hmpps-development.modernisation-platform.service.justice.gov.uk
+                        "web-${module.environment.domains.public.application_environment}",
+                      ]
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }
     }
 
     baseline_route53_zones = {
 
       # hmpps-development.modernisation-platform.service.justice.gov.uk
-      # (module.environment.domains.public.business_unit_environment) = {
-      #   lb_alias_records = [
-      #     { name = "web.${local.application_name}", type = "A", lbs_map_key = "public" },
-      #   ]
-      # }
+      (module.environment.domains.public.business_unit_environment) = {
+        lb_alias_records = [
+          { name = "web.${local.application_name}", type = "A", lbs_map_key = "private" },
+        ]
+      }
     }
   }
 }
