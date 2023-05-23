26c6893312b7f507e8049079925103196fa2a9aa
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-05-23 13:32:01 +0100

    Using custom authorization

--- terraform/environments/data-platform/api.tf
+++ terraform/environments/data-platform/api.tf
@@ -61,7 +61,7 @@ resource "aws_api_gateway_resource" "example" {
 }
 
 resource "aws_api_gateway_method" "example" {
-  authorization = aws_api_gateway_authorizer.authorizer.id
+  authorization = "CUSTOM"
   http_method   = "GET"
   resource_id   = aws_api_gateway_resource.example.id
   rest_api_id   = aws_api_gateway_rest_api.example.id
