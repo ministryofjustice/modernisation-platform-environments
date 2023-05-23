6841a7a57296d4ecdc1de3ea2edb6a610dd866f5
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-05-23 10:49:36 +0100

    fix incorrect variable

--- terraform/environments/delius-iaps/iam.tf
+++ terraform/environments/delius-iaps/iam.tf
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "snapshot_sharer" {
       "rds:ModifyDBSnapshotAttribute"
     ]
     resources = [
-      local.iaps_rds_snapshot_arn_prefix,
+      local.iaps_rds_snapshot_arn_pattern,
       aws_db_instance.iaps.arn
     ]
     statement {
