9ee181cebe6ac396283ee3b6353c4883c814b7cd
keith-h <keith.humphreys@digital.justice.gov.uk>
2023-05-24 12:57:03 +0100

    refactor(targets): remove 1521 target group

--- terraform/environments/laa-ccms-infra-azure-ad-sso/application_load_balancer.tf
+++ terraform/environments/laa-ccms-infra-azure-ad-sso/application_load_balancer.tf
@@ -41,23 +41,3 @@ resource "aws_lb_target_group_attachment" "ebs_vision_db_attachment" {
 }
 
 
-#
-resource "aws_lb_target_group" "ebs_vision_db_tg_jdbc" {
-  name       = lower(format("tgd-%s-%s", substr(local.application_name, 0, 23), substr(local.environment, 0, 3)))
-  port     = 1521
-  protocol = "TCP"
-  vpc_id   = data.aws_vpc.shared.id
-  health_check {
-    port     = 1521
-    path     = "/"
-    timeout  = 2
-    interval = 5
-    matcher  = "200,301,302"
-  }
-}
-
-resource "aws_lb_target_group_attachment" "ebs_vision_db_jdbc_attachment" {
-  target_group_arn = aws_lb_target_group.ebs_vision_db_tg_jdbc.arn
-  target_id        = aws_instance.ec2_oracle_vision_ebs.id
-  port             = 1521
-}
