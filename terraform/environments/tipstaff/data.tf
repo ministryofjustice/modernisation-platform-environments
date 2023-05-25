f73922ddc3e8d1c24d74d1aacbb6079639904529
matthewsearle01 <matthewsearle01@gmail.com>
2023-05-25 16:13:15 +0100

    add in prod dns

--- terraform/environments/tipstaff/data.tf
+++ terraform/environments/tipstaff/data.tf
@@ -1,13 +1,13 @@
-# ## PROD CERT
-# data "aws_route53_zone" "application_zone" {
-#   provider     = aws.core-network-services
-#   name         = "tipstaff.service.justice.gov.uk."
-#   private_zone = false
-# }
+## PROD CERT
+data "aws_route53_zone" "application_zone" {
+  provider     = aws.core-network-services
+  name         = "tipstaff.service.justice.gov.uk."
+  private_zone = false
+}
 
-# ## PROD DNS
-# data "aws_route53_zone" "prod_network_services" {
-#   provider     = aws.core-network-services
-#   name         = "tipstaff.service.justice.gov.uk."
-#   private_zone = false
-# }
+## PROD DNS
+data "aws_route53_zone" "prod_network_services" {
+  provider     = aws.core-network-services
+  name         = "tipstaff.service.justice.gov.uk."
+  private_zone = false
+}
