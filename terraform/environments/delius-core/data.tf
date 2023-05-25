702562f6c42f6f8210b66ac555114e455d28952f
George Taylor <george.taylor@digital.justice.gov.uk>
2023-05-25 16:56:22 +0100

    typo

--- terraform/environments/delius-core/data.tf
+++ terraform/environments/delius-core/data.tf
@@ -1,5 +1,5 @@
 #### This file can be used to store data specific to the member account ####
 
 data "aws_secretsmanager_secret" "ldap_credential" {
-  name = "${local.app_name}-openldap-bind-password"
+  name = "${local.application_name}-openldap-bind-password"
 }
