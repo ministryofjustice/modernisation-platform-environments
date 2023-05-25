749fce22748ec3d1b81cc631e6ade26eeab2d886
matthewsearle01 <matthewsearle01@gmail.com>
2023-05-25 10:26:47 +0100

    remove count from non-prod dns resources

--- terraform/environments/tipstaff/locals.tf
+++ terraform/environments/tipstaff/locals.tf
@@ -1,19 +1,19 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
   # create name, record,type for monitoring lb aka tipstaff_lb
-  domain_types = local.is-production ? {} : { for dvo in aws_acm_certificate.external[0].domain_validation_options : dvo.domain_name => {
+  domain_types = { for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
     name   = dvo.resource_record_name
     record = dvo.resource_record_value
     type   = dvo.resource_record_type
     }
   }
 
-  domain_name_main   = local.is-production ? [] : [for k, v in local.domain_types : v.name if k == "modernisation-platform.service.justice.gov.uk"]
-  domain_name_sub    = local.is-production ? [] : [for k, v in local.domain_types : v.name if k != "modernisation-platform.service.justice.gov.uk"]
-  domain_record_main = local.is-production ? [] : [for k, v in local.domain_types : v.record if k == "modernisation-platform.service.justice.gov.uk"]
-  domain_record_sub  = local.is-production ? [] : [for k, v in local.domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
-  domain_type_main   = local.is-production ? [] : [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
-  domain_type_sub    = local.is-production ? [] : [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_name_main   = [for k, v in local.domain_types : v.name if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_name_sub    = [for k, v in local.domain_types : v.name if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_record_main = [for k, v in local.domain_types : v.record if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_record_sub  = [for k, v in local.domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_type_main   = [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_type_sub    = [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
 
   # domain_types_prod = local.is-production ? { for dvo in aws_acm_certificate.external_prod[0].domain_validation_options : dvo.domain_name => {
   #   name   = dvo.resource_record_name
