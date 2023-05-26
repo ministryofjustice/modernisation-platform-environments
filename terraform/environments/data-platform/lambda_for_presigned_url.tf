b30b01b8f465e17b706360774b0370407fbe05c3
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-05-26 09:28:44 +0100

    Alter lambda permission

--- terraform/environments/data-platform/lambda_for_presigned_url.tf
+++ terraform/environments/data-platform/lambda_for_presigned_url.tf
@@ -55,5 +55,5 @@ resource "aws_lambda_permission" "api_gw" {
   function_name = aws_lambda_function.presigned_url.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.data_platform.execution_arn}/*/${aws_api_gateway_method.upload_data_get.http_method}${aws_api_gateway_resource.upload_data.path}"
+  source_arn = "arn:aws:execute-api:${local.region}:${local.accountId}:${aws_api_gateway_rest_api.data_platform.id}/*/${aws_api_gateway_method.upload_data_get.http_method}${aws_api_gateway_resource.upload_data.path}"
 }
