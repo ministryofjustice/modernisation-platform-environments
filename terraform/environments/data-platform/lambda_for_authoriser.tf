889b8d50fc6ef0e9ee4e7aa755df8a7321cb0a8e
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-05-26 14:21:59 +0100

    Adding lambda permission

--- terraform/environments/data-platform/lambda_for_authoriser.tf
+++ terraform/environments/data-platform/lambda_for_authoriser.tf
@@ -66,3 +66,12 @@ resource "aws_lambda_function" "authoriser" {
   }
   tags = local.tags
 }
+
+resource "aws_lambda_permission" "api_gw_authorizer" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.authoriser.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.data_platform.id}/*/*"
+}
