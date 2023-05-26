2255a6d1afbc2d698f1a710e13e75104ac493b26
George Taylor <george.taylor@digital.justice.gov.uk>
2023-05-26 16:49:56 +0100

    IP

--- terraform/environments/delius-core/ecs_service_delius_db.tf
+++ terraform/environments/delius-core/ecs_service_delius_db.tf
@@ -280,7 +280,7 @@ resource "aws_route53_record" "delius-core-db" {
   name     = "${local.db_service_name}-${local.application_name}.${data.aws_route53_zone.inner.name}"
   type     = "A"
   ttl      = 300
-  records  = ["10.26.26.165"]
+  records  = ["10.26.24.174"]
 }
 
 ##
