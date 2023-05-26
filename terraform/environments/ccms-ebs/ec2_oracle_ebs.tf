e2465aceee8c6177d860b5ee9681223bad9d923c
Marty Taggart <martin.taggart@digital.justice.gov.uk>
2023-05-26 12:23:13 +0100

    dbf disk used

--- terraform/environments/ccms-ebs/ec2_oracle_ebs.tf
+++ terraform/environments/ccms-ebs/ec2_oracle_ebs.tf
@@ -314,9 +314,9 @@ module "cw-ebs-ec2" {
 # Disk Free Alarm for EBSDB /dbf mount
 resource "aws_cloudwatch_metric_alarm" "disk_free_dbf" {
   alarm_name                = "${local.application_data.accounts[local.environment].short_env}-EBSDB-disk_free_DBF"
-  alarm_description         = "This metric monitors the amount of free disk space on dbf mount. If the amount of free disk space on root falls below 15% for 2 minutes, the alarm will trigger"
-  comparison_operator       = "LessThanOrEqualToThreshold"
-  metric_name               = "disk_free"
+  alarm_description         = "This metric monitors the amount of free disk space on dbf mount. If the amount of free disk space on root falls below 20% for 2 minutes, the alarm will trigger"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "disk_used_percentage"
   namespace                 = "CWAgent"
   statistic                 = "Average"
   insufficient_data_actions = []
@@ -324,7 +324,7 @@ resource "aws_cloudwatch_metric_alarm" "disk_free_dbf" {
   evaluation_periods  = local.application_data.cloudwatch_ec2.disk.eval_periods
   datapoints_to_alarm = local.application_data.cloudwatch_ec2.disk.eval_periods
   period              = local.application_data.cloudwatch_ec2.disk.period
-  threshold           = local.application_data.cloudwatch_ec2.disk.threshold
+  threshold           = local.application_data.cloudwatch_ec2.disk.threshold_dbf
   alarm_actions       = [aws_sns_topic.cw_alerts.arn]
   dimensions = {
     InstanceId   = aws_instance.ec2_oracle_ebs.id
