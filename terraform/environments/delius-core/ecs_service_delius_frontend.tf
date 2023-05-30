5a10ad4961207d6851eb99ae34c5c36d5abd7331
George Taylor <george.taylor@digital.justice.gov.uk>
2023-05-30 19:40:39 +0100

    add test ec2

--- terraform/environments/delius-core/ecs_service_delius_frontend.tf
+++ terraform/environments/delius-core/ecs_service_delius_frontend.tf
@@ -400,3 +400,32 @@ resource "aws_ecs_service" "delius-frontend-service" {
   triggers                           = {} # Change this for force redeployment
 
 }
+
+
+resource "aws_instance" "docker" {
+  #checkov:skip=CKV2_AWS_41:"IAM role is not implemented for this example EC2. SSH/AWS keys are not used either."
+  # Specify the instance type and ami to be used (this is the Amazon free tier option)
+  instance_type = "t2.xlarge"
+  ami           = ami-0e1c5be2aa956338b
+  # ami = "ami-0e3dd4f4b84ef84f5" # AL2 amzn2-ami-hvm-2.0.20230418.0-x86_64-gp2
+  vpc_security_group_ids      = [aws_security_group.base_ami_test_instance_sg.id]
+  subnet_id                   = data.aws_subnet.private_subnets_a.id
+  iam_instance_profile        = aws_iam_instance_profile.base_ami_test_instance_profile.name
+  associate_public_ip_address = false
+  monitoring                  = false
+  ebs_optimized               = false
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+  # Increase the volume size of the root volume
+  # root_block_device {
+  #   volume_type = "gp3"
+  #   volume_size = 30
+  #   encrypted   = true
+  # }
+  tags = merge(local.tags,
+    { Name = lower(format("ec2-%s-%s-docker", local.application_name, local.environment)) }
+  )
+}
