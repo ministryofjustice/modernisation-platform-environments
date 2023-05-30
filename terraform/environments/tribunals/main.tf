962306d78f34cbdf9906c2f588d2c48a7a76591a
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-05-30 15:11:53 +0100

    test

--- terraform/environments/tribunals/main.tf
+++ terraform/environments/tribunals/main.tf
@@ -1,6 +1,6 @@
-module "transport" {
-  source                 = "./modules/transport"
-  application_name       = "tribs-transport-${local.environment}"
-  environment            = local.environment
-  db_instance_identifier = local.application_data.accounts[local.environment].identifier
-}
+# module "transport" {
+#   source                 = "./modules/transport"
+#   application_name       = "tribs-transport-${local.environment}"
+#   environment            = local.environment
+#   db_instance_identifier = local.application_data.accounts[local.environment].identifier
+# }
