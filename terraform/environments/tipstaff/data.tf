b4d50bcc8449dc00e2bd1c7f88159f3f8ca743d5
matthewsearle01 <matthewsearle01@gmail.com>
2023-05-30 11:03:46 +0100

    update prod dns config

--- terraform/environments/tipstaff/data.tf
+++ terraform/environments/tipstaff/data.tf
@@ -1,13 +1,5 @@
-## PROD CERT
 data "aws_route53_zone" "application_zone" {
   provider     = aws.core-network-services
   name         = "tipstaff.service.justice.gov.uk."
-  private_zone = false
-}
-
-## PROD DNS
-data "aws_route53_zone" "prod_network_services" {
-  provider     = aws.core-network-services
-  name         = "service.justice.gov.uk."
   private_zone = false
 }
--- terraform/environments/tipstaff/dns_ssl.tf
+++ terraform/environments/tipstaff/dns_ssl.tf
@@ -13,19 +13,9 @@ resource "aws_acm_certificate" "external" {
   }
 }
 
-// Route53 DNS record for directing traffic to the service 
-resource "aws_route53_record" "external" {
-  provider = aws.core-vpc
-
-  zone_id = data.aws_route53_zone.external.zone_id
-  name    = "${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.tipstaff_lb.dns_name
-    zone_id                = aws_lb.tipstaff_lb.zone_id
-    evaluate_target_health = true
-  }
+resource "aws_acm_certificate_validation" "external" {
+  certificate_arn         = aws_acm_certificate.external.arn
+  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
 }
 
 // Route53 DNS record for certificate validation
@@ -51,9 +41,19 @@ resource "aws_route53_record" "external_validation_subdomain" {
   zone_id         = data.aws_route53_zone.external.zone_id
 }
 
-resource "aws_acm_certificate_validation" "external" {
-  certificate_arn         = aws_acm_certificate.external.arn
-  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
+// Route53 DNS record for directing traffic to the service 
+resource "aws_route53_record" "external" {
+  provider = aws.core-vpc
+
+  zone_id = data.aws_route53_zone.external.zone_id
+  name    = "${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.tipstaff_lb.dns_name
+    zone_id                = aws_lb.tipstaff_lb.zone_id
+    evaluate_target_health = true
+  }
 }
 
 // PRODUCTION DNS CONFIGURATION
@@ -61,7 +61,7 @@ resource "aws_acm_certificate_validation" "external" {
 // ACM Public Certificate
 resource "aws_acm_certificate" "external_prod" {
   count                     = local.is-production ? 1 : 0
-  domain_name               = "service.justice.gov.uk"
+  domain_name               = "tipstaff.service.justice.gov.uk"
   validation_method         = "DNS"
   subject_alternative_names = ["tipstaff.service.justice.gov.uk"]
   tags = {
@@ -70,6 +70,39 @@ resource "aws_acm_certificate" "external_prod" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_acm_certificate_validation" "external_prod" {
+  count = local.is-production ? 1 : 0
+  depends_on = [
+    aws_route53_record.external_validation_prod[0]
+  ]
+  certificate_arn         = aws_acm_certificate.external_prod[0].arn
+  validation_record_fqdns = [for record in aws_route53_record.external_validation_prod[0] : record.fqdn]
+  timeouts {
+    create = "10m"
+  }
+}
+
+// Route53 DNS record for certificate validation
+resource "aws_route53_record" "external_validation_prod" {
+  count    = local.is-production ? 1 : 0
+  provider = aws.core-network-services
+
+  for_each = {
+    for dvo in aws_acm_certificate.external_prod[0].domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.application_zone.zone_id
 }
 
 // Route53 DNS record for directing traffic to the service
@@ -85,30 +118,5 @@ resource "aws_route53_record" "external_prod" {
     name                   = aws_lb.tipstaff_lb.dns_name
     zone_id                = aws_lb.tipstaff_lb.zone_id
     evaluate_target_health = true
-  }
-}
-
-// Route53 DNS record for certificate validation
-resource "aws_route53_record" "external_validation_prod" {
-  count    = local.is-production ? 1 : 0
-  provider = aws.core-network-services
-
-  allow_overwrite = true
-  name            = local.domain_name_main_prod[0]
-  records         = local.domain_record_main_prod
-  ttl             = 60
-  type            = local.domain_type_main_prod[0]
-  zone_id         = data.aws_route53_zone.prod_network_services.zone_id
-}
-
-resource "aws_acm_certificate_validation" "external_prod" {
-  count = local.is-production ? 1 : 0
-  depends_on = [
-    aws_route53_record.external_validation_prod[0]
-  ]
-  certificate_arn         = aws_acm_certificate.external_prod[0].arn
-  validation_record_fqdns = [local.domain_name_main_prod[0]]
-  timeouts {
-    create = "10m"
   }
 }
