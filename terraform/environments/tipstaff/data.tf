52ddb368503e2ebae48802be1715bb2e9a2e375a
matthewsearle01 <matthewsearle01@gmail.com>
2023-05-30 15:50:47 +0100

    destroy tipstaff dev resources

--- terraform/environments/tipstaff/data.tf
+++ terraform/environments/tipstaff/data.tf
@@ -1,5 +1,5 @@
-data "aws_route53_zone" "application_zone" {
-  provider     = aws.core-network-services
-  name         = "tipstaff.service.justice.gov.uk."
-  private_zone = false
-}
+# data "aws_route53_zone" "application_zone" {
+#   provider     = aws.core-network-services
+#   name         = "tipstaff.service.justice.gov.uk."
+#   private_zone = false
+# }
