dda3bf9484501be30e7b6cea56f4acdffa026b8a
matthewsearle01 <matthewsearle01@gmail.com>
2023-05-30 16:21:20 +0100

    rebuild dns only

--- terraform/environments/tipstaff/dns_ssl.tf
+++ terraform/environments/tipstaff/dns_ssl.tf
@@ -115,3 +115,140 @@
 #     evaluate_target_health = true
 #   }
 # }
+
+resource "aws_route53_record" "external" {
+  # count    = local.is-production ? 0 : 1
+  provider = aws.core-vpc
+
+  zone_id = data.aws_route53_zone.external.zone_id
+  name    = "${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.tipstaff_lb.dns_name
+    zone_id                = aws_lb.tipstaff_lb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_acm_certificate" "external" {
+  # count             = local.is-production ? 0 : 1
+  domain_name       = local.application_data.accounts[local.environment].domain_name
+  validation_method = "DNS"
+
+  subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  tags = {
+    Environment = local.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "external_validation" {
+  # count    = local.is-production ? 0 : 1
+  provider = aws.core-network-services
+
+
+  for_each = {
+    for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.network-services.zone_id
+  # allow_overwrite = true
+  # name            = local.domain_name_main[0]
+  # records         = local.domain_record_main
+  # ttl             = 60
+  # type            = local.domain_type_main[0]
+  # zone_id         = data.aws_route53_zone.network-services.zone_id
+}
+
+# resource "aws_route53_record" "external_validation_subdomain" {
+#   # count    = local.is-production ? 0 : 1
+#   provider = aws.core-vpc
+
+#   allow_overwrite = true
+#   name            = local.domain_name_sub[0]
+#   records         = local.domain_record_sub
+#   ttl             = 60
+#   type            = local.domain_type_sub[0]
+#   zone_id         = data.aws_route53_zone.external.zone_id
+# }
+
+resource "aws_acm_certificate_validation" "external" {
+  # count                   = local.is-production ? 0 : 1
+  certificate_arn = aws_acm_certificate.external.arn
+  # validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
+  validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
+}
+
+// PROD DNS
+resource "aws_route53_record" "external_prod" {
+  # count    = local.is-production ? 1 : 0
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.prod_network_services.zone_id
+  name     = "tipstaff.service.justice.gov.uk"
+  type     = "A"
+
+  alias {
+    name                   = aws_lb.tipstaff_lb.dns_name
+    zone_id                = aws_lb.tipstaff_lb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_acm_certificate" "external_prod" {
+  # count             = local.is-production ? 1 : 0
+  domain_name       = "tipstaff.service.justice.gov.uk"
+  validation_method = "DNS"
+  subject_alternative_names = [
+    "*.tipstaff.service.justice.gov.uk"
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+## Validation 
+resource "aws_route53_record" "external_validation_prod" {
+  # count    = local.is-production ? 1 : 0
+  provider = aws.core-network-services
+
+  for_each = {
+    for dvo in aws_acm_certificate.external_prod.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.application_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "external_prod" {
+  # count = local.is-production ? 1 : 0
+  depends_on = [
+    aws_route53_record.external_validation_prod
+  ]
+  certificate_arn         = aws_acm_certificate.external_prod.arn
+  validation_record_fqdns = [for record in aws_route53_record.external_validation_prod : record.fqdn]
+  timeouts {
+    create = "10m"
+  }
+}
