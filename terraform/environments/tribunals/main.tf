a26a83d94eab49bd4c366c2729e5ac507ec0d8a6
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-05-30 09:26:38 +0100

    plan errorfix

--- terraform/environments/tribunals/main.tf
+++ terraform/environments/tribunals/main.tf
@@ -3,11 +3,4 @@ module "transport" {
   application_name       = "${local.project}-transport-${local.environment}"
   environment            = local.environment
   db_instance_identifier = local.application_data.accounts[local.environment].identifier
-
-  tags = merge(
-    local.all_tags,
-    {
-      Name          = "${local.project}-transport-${local.environment}"
-    }
-  )
 }
