2f615952f3e324964ba2f27b3aa7d4bb3e9bc542
matthewsearle01 <matthewsearle01@gmail.com>
2023-05-31 15:07:47 +0100

    try with prod. subdomain

--- terraform/environments/tipstaff/locals.tf
+++ terraform/environments/tipstaff/locals.tf
@@ -1,32 +1,32 @@
-# #### This file can be used to store locals specific to the member account ####
-# locals {
-#   # create name, record,type for monitoring lb aka tipstaff_dev_lb
-#   domain_types = { for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
-#     name   = dvo.resource_record_name
-#     record = dvo.resource_record_value
-#     type   = dvo.resource_record_type
-#     }
-#   }
+#### This file can be used to store locals specific to the member account ####
+locals {
+  # create name, record,type for monitoring lb aka tipstaff_dev_lb
+  domain_types = { for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+    }
+  }
 
-#   domain_name_main   = [for k, v in local.domain_types : v.name if k == "modernisation-platform.service.justice.gov.uk"]
-#   domain_name_sub    = [for k, v in local.domain_types : v.name if k != "modernisation-platform.service.justice.gov.uk"]
-#   domain_record_main = [for k, v in local.domain_types : v.record if k == "modernisation-platform.service.justice.gov.uk"]
-#   domain_record_sub  = [for k, v in local.domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
-#   domain_type_main   = [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
-#   domain_type_sub    = [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_name_main   = [for k, v in local.domain_types : v.name if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_name_sub    = [for k, v in local.domain_types : v.name if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_record_main = [for k, v in local.domain_types : v.record if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_record_sub  = [for k, v in local.domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
+  domain_type_main   = [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
+  domain_type_sub    = [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
 
-#   # domain_types_prod = { for dvo in aws_acm_certificate.external_prod.domain_validation_options : dvo.domain_name => {
-#   #   name   = dvo.resource_record_name
-#   #   record = dvo.resource_record_value
-#   #   type   = dvo.resource_record_type
-#   #   }
-#   # }
+  # domain_types_prod = { for dvo in aws_acm_certificate.external_prod.domain_validation_options : dvo.domain_name => {
+  #   name   = dvo.resource_record_name
+  #   record = dvo.resource_record_value
+  #   type   = dvo.resource_record_type
+  #   }
+  # }
 
-#   # domain_name_main_prod   = [for k, v in local.domain_types_prod : v.name if k == "tipstaff.service.justice.gov.uk"]
-#   # domain_name_sub_prod    = [for k, v in local.domain_types_prod : v.name if k != "tipstaff.service.justice.gov.uk"]
-#   # domain_record_main_prod = [for k, v in local.domain_types_prod : v.record if k == "tipstaff.service.justice.gov.uk"]
-#   # domain_record_sub_prod  = [for k, v in local.domain_types_prod : v.record if k != "tipstaff.service.justice.gov.uk"]
-#   # domain_type_main_prod   = [for k, v in local.domain_types_prod : v.type if k == "tipstaff.service.justice.gov.uk"]
-#   # domain_type_sub_prod    = [for k, v in local.domain_types_prod : v.type if k != "tipstaff.service.justice.gov.uk"]
+  # domain_name_main_prod   = [for k, v in local.domain_types_prod : v.name if k == "tipstaff.service.justice.gov.uk"]
+  # domain_name_sub_prod    = [for k, v in local.domain_types_prod : v.name if k != "tipstaff.service.justice.gov.uk"]
+  # domain_record_main_prod = [for k, v in local.domain_types_prod : v.record if k == "tipstaff.service.justice.gov.uk"]
+  # domain_record_sub_prod  = [for k, v in local.domain_types_prod : v.record if k != "tipstaff.service.justice.gov.uk"]
+  # domain_type_main_prod   = [for k, v in local.domain_types_prod : v.type if k == "tipstaff.service.justice.gov.uk"]
+  # domain_type_sub_prod    = [for k, v in local.domain_types_prod : v.type if k != "tipstaff.service.justice.gov.uk"]
 
-# }
+}
