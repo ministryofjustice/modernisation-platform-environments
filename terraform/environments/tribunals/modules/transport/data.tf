1ddd729caa82b8758b5cb2b711f450e693c5283a
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-05-31 08:56:46 +0100

    naming check

--- terraform/environments/tribunals/modules/transport/data.tf
+++ terraform/environments/tribunals/modules/transport/data.tf
@@ -1,6 +1,6 @@
 
 data "aws_db_instance" "database" {
-  db_instance_identifier = var.db_instance_identifier
+  db_instance_identifier = "tribunals-db-dev" #var.db_instance_identifier
 }
 
 data "aws_secretsmanager_secret" "rds-secrets" {
