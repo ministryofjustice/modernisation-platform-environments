e3cad39f6014dcbe2c751b23507a272fb5388ea1
matthewsearle01 <matthewsearle01@gmail.com>
2023-05-31 10:16:56 +0100

    update dns config for clean run

--- terraform/environments/tipstaff/data.tf
+++ terraform/environments/tipstaff/data.tf
@@ -1,19 +1,11 @@
-# data "aws_route53_zone" "application_zone" {
-#   provider     = aws.core-network-services
-#   name         = "tipstaff.service.justice.gov.uk."
-#   private_zone = false
-# }
-
-## PROD CERT
 data "aws_route53_zone" "application_zone" {
   provider     = aws.core-network-services
   name         = "tipstaff.service.justice.gov.uk."
   private_zone = false
 }
 
-## PROD DNS
-data "aws_route53_zone" "prod_network_services" {
-  provider     = aws.core-network-services
-  name         = "tipstaff.service.justice.gov.uk."
-  private_zone = false
-}
+# data "aws_route53_zone" "prod_network_services" {
+#   provider     = aws.core-network-services
+#   name         = "tipstaff.service.justice.gov.uk."
+#   private_zone = false
+# }
