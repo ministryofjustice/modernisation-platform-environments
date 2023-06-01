b97d75f0220e17abd38e118a51a2da858f68dd9d
matthewsearle01 <matthewsearle01@gmail.com>
2023-06-01 16:20:27 +0100

    update health check interval and timeout

--- terraform/environments/tipstaff/load_balancer.tf
+++ terraform/environments/tipstaff/load_balancer.tf
@@ -52,12 +52,12 @@ resource "aws_lb_target_group" "tipstaff_target_group" {
 
   health_check {
     healthy_threshold   = "3"
-    interval            = "15"
+    interval            = "40"
     protocol            = "HTTPS"
     port                = "443"
     unhealthy_threshold = "3"
     matcher             = "200-302"
-    timeout             = "45"
+    timeout             = "30"
   }
 
 }
