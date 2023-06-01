e7ff2335abfa99860339ebac7d7939db2260d9ed
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-01 11:18:42 +0000

    actually stage my changes

--- terraform/environments/data-platform-apps-and-tools/mwaa-environments.tf
+++ terraform/environments/data-platform-apps-and-tools/mwaa-environments.tf
@@ -3,9 +3,9 @@
 ##################################################
 
 resource "aws_mwaa_environment" "main" {
-  name               = "data-platform-airflow"
-  airflow_version    = "2.5.1"
-  environment_class  = "mw1.medium"
+  name              = "data-platform-airflow"
+  airflow_version   = "2.5.1"
+  environment_class = "mw1.medium"
 
   execution_role_arn = module.airflow_execution_role.iam_role_arn
 
