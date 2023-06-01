3f4ab58e8c36a7277387457a98190e1619616293
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-01 11:18:25 +0000

    Added egress rules Ran a format

--- terraform/environments/data-platform-apps-and-tools/security-groups.tf
+++ terraform/environments/data-platform-apps-and-tools/security-groups.tf
@@ -7,8 +7,11 @@ module "airflow_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
 
-  name        = "data-platform-mwaa"
-  vpc_id      = data.aws_vpc.mp_platforms_development.id
+  name   = "data-platform-mwaa"
+  vpc_id = data.aws_vpc.mp_platforms_development.id
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
 
   ingress_with_self = [
     {
