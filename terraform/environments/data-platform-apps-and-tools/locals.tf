bc229b040a4bccc9e2b4d77da06d4fa4f46a80e8
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-01 11:48:02 +0000

    environment is a local, not a var

--- terraform/environments/data-platform-apps-and-tools/locals.tf
+++ terraform/environments/data-platform-apps-and-tools/locals.tf
@@ -1,4 +1,4 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
-  airflow_environment_name = "data-platform-airflow-${var.environment}"
+  airflow_environment_name = "data-platform-airflow-${local.environment}"
 }
