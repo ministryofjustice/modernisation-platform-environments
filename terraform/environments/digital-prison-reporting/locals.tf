a82e49e5b0f509b25de8798879cf4e28f064b4be
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-06-01 15:46:57 +0100

    DPR-377, Enable SG to allow CP Traffic

--- terraform/environments/digital-prison-reporting/locals.tf
+++ terraform/environments/digital-prison-reporting/locals.tf
@@ -26,6 +26,7 @@ locals {
     redshift_cluster_name      = "${local.application_data.accounts[local.environment].project_short_id}-redshift-${local.environment}"
     kinesis_stream_ingestor    = "${local.application_data.accounts[local.environment].project_short_id}-kinesis-ingestor-${local.environment}"
     kinesis_endpoint           = "https://kinesis.eu-west-2.amazonaws.com"
+    cloud_platform_cidr        = "172.20.0.0/16"
 
     all_tags = merge(
     local.tags,
