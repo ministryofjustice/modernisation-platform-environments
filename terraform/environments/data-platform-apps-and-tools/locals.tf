cae3721f38d1cc03d8167969768b397ce15cdf2a
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-02 10:41:28 +0000

    Removed superfluous VPC data sources Updated resources with tagging Added environment specific locals Bumped security-group module

--- terraform/environments/data-platform-apps-and-tools/locals.tf
+++ terraform/environments/data-platform-apps-and-tools/locals.tf
@@ -1,4 +1,12 @@
-#### This file can be used to store locals specific to the member account ####
 locals {
-  airflow_environment_name = "data-platform-airflow-${local.environment}"
+  airflow_name        = local.environment
+  airflow_dag_s3_path = "dags/"
+
+  environment_configuration = local.environment_configurations[local.environment]
+  environment_configurations = {
+    development = {
+      airflow_version           = "2.4.3"
+      airflow_environment_class = "mw1.medium"
+    }
+  }
 }
