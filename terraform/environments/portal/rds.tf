8c1c694954afd4039bb761fbdd586114a290aec3
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-06-02 11:17:45 +0100

    LAWS-3241: changed SG name to includeDB

--- terraform/environments/portal/rds.tf
+++ terraform/environments/portal/rds.tf
@@ -101,7 +101,8 @@ resource "aws_db_option_group" "PortalIGDB19OptionGroup" {
 }
 
 resource "aws_security_group" "igdb" {
-  name        = "${local.application_name}-${local.environment}-secgroup"
+  # name        = "${local.application_name}-${local.environment}-secgroup"
+  name        = "${local.application_name}-${local.environment}-secgroup-${local.igdb_dbname}"
   description = "RDS access with the LAA Landing Zone"
   vpc_id      = data.aws_vpc.shared.id
 
