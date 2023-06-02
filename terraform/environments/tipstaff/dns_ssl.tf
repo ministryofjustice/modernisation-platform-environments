c6b15b00d4d0c8732732a3de6670b3768558c511
matthewsearle01 <matthewsearle01@gmail.com>
2023-06-02 09:40:46 +0100

    destroy

--- terraform/environments/tipstaff/dns_ssl.tf
+++ terraform/environments/tipstaff/dns_ssl.tf
@@ -1,50 +1,50 @@
-// ACM Public Certificate
-resource "aws_acm_certificate" "external" {
-  domain_name       = "modernisation-platform.service.justice.gov.uk"
-  validation_method = "DNS"
+# // ACM Public Certificate
+# resource "aws_acm_certificate" "external" {
+#   domain_name       = "modernisation-platform.service.justice.gov.uk"
+#   validation_method = "DNS"
 
-  subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-  tags = {
-    Environment = local.environment
-  }
+#   subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+#   tags = {
+#     Environment = local.environment
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_acm_certificate_validation" "external" {
-  certificate_arn         = aws_acm_certificate.external.arn
-  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
-  timeouts {
-    create = "10m"
-  }
-}
+# resource "aws_acm_certificate_validation" "external" {
+#   certificate_arn         = aws_acm_certificate.external.arn
+#   validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
+#   timeouts {
+#     create = "10m"
+#   }
+# }
 
-// Route53 DNS record for certificate validation
-resource "aws_route53_record" "external_validation" {
-  provider = aws.core-network-services
+# // Route53 DNS record for certificate validation
+# resource "aws_route53_record" "external_validation" {
+#   provider = aws.core-network-services
 
-  allow_overwrite = true
-  name            = local.domain_name_main[0]
-  records         = local.domain_record_main
-  ttl             = 60
-  type            = local.domain_type_main[0]
-  zone_id         = data.aws_route53_zone.network-services.zone_id
-}
+#   allow_overwrite = true
+#   name            = local.domain_name_main[0]
+#   records         = local.domain_record_main
+#   ttl             = 60
+#   type            = local.domain_type_main[0]
+#   zone_id         = data.aws_route53_zone.network-services.zone_id
+# }
 
-resource "aws_route53_record" "external_validation_subdomain" {
-  provider = aws.core-vpc
+# resource "aws_route53_record" "external_validation_subdomain" {
+#   provider = aws.core-vpc
 
-  allow_overwrite = true
-  name            = local.domain_name_sub[0]
-  records         = local.domain_record_sub
-  ttl             = 60
-  type            = local.domain_type_sub[0]
-  zone_id         = data.aws_route53_zone.external.zone_id
-}
+#   allow_overwrite = true
+#   name            = local.domain_name_sub[0]
+#   records         = local.domain_record_sub
+#   ttl             = 60
+#   type            = local.domain_type_sub[0]
+#   zone_id         = data.aws_route53_zone.external.zone_id
+# }
 
-// Route53 DNS record for directing traffic to the service 
+# // Route53 DNS record for directing traffic to the service 
 # resource "aws_route53_record" "external" {
 #   provider = aws.core-vpc
 
@@ -59,43 +59,43 @@ resource "aws_route53_record" "external_validation_subdomain" {
 #   }
 # }
 
-// PRODUCTION DNS CONFIGURATION
+# // PRODUCTION DNS CONFIGURATION
 
-// ACM Public Certificate
-resource "aws_acm_certificate" "external_prod" {
-  count = local.is-production ? 1 : 0
+# // ACM Public Certificate
+# resource "aws_acm_certificate" "external_prod" {
+#   count = local.is-production ? 1 : 0
 
-  domain_name       = "tipstaff.service.justice.gov.uk"
-  validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   domain_name       = "tipstaff.service.justice.gov.uk"
+#   validation_method = "DNS"
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_acm_certificate_validation" "external_prod" {
-  count = local.is-production ? 1 : 0
+# resource "aws_acm_certificate_validation" "external_prod" {
+#   count = local.is-production ? 1 : 0
 
-  certificate_arn         = aws_acm_certificate.external_prod[0].arn
-  validation_record_fqdns = [aws_route53_record.external_validation_prod[0].fqdn]
-  timeouts {
-    create = "10m"
-  }
-}
+#   certificate_arn         = aws_acm_certificate.external_prod[0].arn
+#   validation_record_fqdns = [aws_route53_record.external_validation_prod[0].fqdn]
+#   timeouts {
+#     create = "10m"
+#   }
+# }
 
-// Route53 DNS record for certificate validation
-resource "aws_route53_record" "external_validation_prod" {
-  count    = local.is-production ? 1 : 0
-  provider = aws.core-network-services
+# // Route53 DNS record for certificate validation
+# resource "aws_route53_record" "external_validation_prod" {
+#   count    = local.is-production ? 1 : 0
+#   provider = aws.core-network-services
 
-  allow_overwrite = true
-  name            = tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_name
-  records         = [tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_value]
-  type            = tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_type
-  zone_id         = data.aws_route53_zone.application_zone.zone_id
-  ttl             = 60
-}
+#   allow_overwrite = true
+#   name            = tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_name
+#   records         = [tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_value]
+#   type            = tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_type
+#   zone_id         = data.aws_route53_zone.application_zone.zone_id
+#   ttl             = 60
+# }
 
-// Route53 DNS record for directing traffic to the service
+# // Route53 DNS record for directing traffic to the service
 # resource "aws_route53_record" "external_prod" {
 #   count    = local.is-production ? 1 : 0
 #   provider = aws.core-network-services
