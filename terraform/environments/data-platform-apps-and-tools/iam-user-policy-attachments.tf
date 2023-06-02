09d79bee2407f6f6a7fcf537208daed1afe3b1bb
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-02 12:09:53 +0000

    Added SES Added MWAA config

---
+++ terraform/environments/data-platform-apps-and-tools/iam-user-policy-attachments.tf
@@ -0,0 +1,8 @@
+##################################################
+# Airflow SES
+##################################################
+
+resource "aws_iam_user_policy_attachment" "airflow_ses" {
+  user       = module.airflow_iam_user.iam_user_name
+  policy_arn = module.airflow_ses_policy.arn
+}
--- terraform/environments/data-platform-apps-and-tools/locals.tf
+++ terraform/environments/data-platform-apps-and-tools/locals.tf
@@ -1,12 +1,24 @@
 locals {
-  airflow_name        = local.environment
-  airflow_dag_s3_path = "dags/"
+  airflow_name                            = local.environment
+  airflow_dag_s3_path                     = "dags/"
+  airflow_requirements_s3_path            = "requirements.txt"
+  airflow_webserver_access_mode           = "PUBLIC_ONLY"
+  airflow_weekly_maintenance_window_start = "SAT:00:00"
+  airflow_mail_from_address               = "airflow@${local.ses_domain_identity}"
+
+  ses_domain_identity = "apps-tools.${local.environment}.data-platform.service.justice.gov.uk"
 
   environment_configuration = local.environment_configurations[local.environment]
   environment_configurations = {
     development = {
       airflow_version           = "2.4.3"
       airflow_environment_class = "mw1.medium"
+      airflow_max_workers       = 10
+      airflow_min_workers       = 1
+      airflow_schedulers        = 2
+      airflow_configuration_options = {
+        "webserver.warn_deployment_exposure" = 0
+      }
     }
   }
 }
