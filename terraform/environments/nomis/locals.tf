e6996978200af704831a10eff71fd8f0f4c91626
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-06-06 17:40:30 +0100

    test

--- terraform/environments/nomis/locals.tf
+++ terraform/environments/nomis/locals.tf
@@ -113,33 +113,35 @@ locals {
       names = {
         test-cloud-watch-config-linux = {
           description = "cloud watch agent config for linux"
-          file        = "./templates/cloud_watch_linux.json"
-          type        = "String"
+          # file        = "./templates/cloud_watch_linux.json"
+          file = "./templates/cloud_watch_windows.json"
+          type = "String"
         }
         test-cloud-watch-config-windows = {
           description = "cloud watch agent config for windows"
-          file        = "./templates/cloud_watch_windows.json"
-          type        = "String"
+          # file        = "./templates/cloud_watch_windows.json"
+          file = "./templates/cloud_watch_linux.json"
+          type = "String"
         }
         test-ec2-user_pem       = {}
         test-github-ci-user-pat = {}
         test-random = {
           random = {
-            length  = 30
+            length  = 24
             special = false
           }
         }
         test-random2 = {
           random = {
-            length  = 30
+            length  = 24
             special = false
           }
         }
         test-value = {
-          value = "test"
+          value = "test3"
         }
         test-value2 = {
-          value = "test2"
+          value = "test24"
         }
       }
     }
