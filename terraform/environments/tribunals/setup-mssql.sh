b25e0196bdaae2df3f61f6283ee7ebea08649c69
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-06-06 13:05:55 +0100

    transport sp migration

--- terraform/environments/tribunals/setup-mssql.sh
+++ terraform/environments/tribunals/setup-mssql.sh
@@ -38,7 +38,7 @@ EOF
       sqlcmd -S ${DB_URL} -U ${USER_NAME} -P ${PASSWORD} -Q "CREATE LOGIN ${NEW_USER_NAME} WITH PASSWORD = '${NEW_PASSWORD}'"      
       sqlcmd -S ${DB_URL} -U ${USER_NAME} -P ${PASSWORD} -Q "CREATE USER ${NEW_USER_NAME} FOR LOGIN ${NEW_USER_NAME}"
       sqlcmd -S ${DB_URL} -U ${USER_NAME} -P ${PASSWORD} -Q "USE ${NEW_DB_NAME} GO EXEC sp_addrolemember N'db_owner', N'${NEW_USER_NAME}'; GO"
-      sqlcmd -S ${DB_URL} -U ${USER_NAME} -P ${PASSWORD} -i "sp_migration.sql"
+      sqlcmd -S ${DB_URL} -U ${USER_NAME} -P ${PASSWORD} -i "transport_sp_migration.sql"
     else
       echo "sqlcmd not found"
     fi
