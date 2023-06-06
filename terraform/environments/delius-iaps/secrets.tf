22bd20ee34c38c2d01114fd679382c4d9983adb5
Piotr Grzeskowiak <piotr.grzeskowiak@digital.justice.gov.uk>
2023-06-06 15:47:18 +0100

    Temporarily revert to using self managed password

--- terraform/environments/delius-iaps/secrets.tf
+++ terraform/environments/delius-iaps/secrets.tf
@@ -1,4 +1,32 @@
 ##
+# Create password for rds master user
+##
+resource "random_password" "db_password" {
+  length  = 30
+  lower   = true
+  upper   = true
+  special = false
+}
+
+#tfsec:ignore:aws-ssm-secret-use-customer-key
+resource "aws_secretsmanager_secret" "db_password" {
+  #checkov:skip=CKV_AWS_149
+  name                    = "${var.networking[0].application}-db-password"
+  recovery_window_in_days = 0
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.networking[0].application}-db-password"
+    },
+  )
+}
+
+resource "aws_secretsmanager_secret_version" "db_password" {
+  secret_id     = aws_secretsmanager_secret.db_password.id
+  secret_string = random_password.db_password.result
+}
+
+##
 # Create password for AD root admin
 ##
 resource "random_password" "ad_password" {
