44207f059da754eadbec1a577cee767a3344f746
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-06-07 12:46:21 +0100

    tipstaff - clean up (#2449)
    * load balancer - allow all IPs from tactical products lb
    
    * remove lb listener on port 80 and remove vpn access on port 80
    
    * tidy up dns code and add comments
    
    * remove duplicate lb rule for vpn
    
    * update ecs security group to only accept port 80 from load balancer
    
    * update sequences script
    
    * rename lb listener
    
    ---------
    
    Co-authored-by: matthewsearle01 <matthewsearle01@gmail.com>

--- terraform/environments/tipstaff/load_balancer.tf
+++ terraform/environments/tipstaff/load_balancer.tf
@@ -4,19 +4,45 @@ resource "aws_security_group" "tipstaff_lb_sc" {
   vpc_id      = data.aws_vpc.shared.id
 
   ingress {
-    description = "allow access on HTTP for the MOJ VPN"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [local.application_data.accounts[local.environment].moj_ip]
-  }
-
-  ingress {
     description = "allow access on HTTPS for the MOJ VPN"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = [local.application_data.accounts[local.environment].moj_ip]
+  }
+
+  // Allow all IP addresses that had load balancer access in the Tactical Products environment
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+      "178.248.34.44/32",
+      "194.33.192.0/25",
+      "195.59.75.0/24",
+      "178.248.34.45/32",
+      "201.33.21.5/32",
+      "178.248.34.46/32",
+      "188.172.252.34/32",
+      "178.248.34.43/32",
+      "92.177.120.49/32",
+      "157.203.176.0/25",
+      "179.50.12.212/32",
+      "213.121.161.112/28",
+      "2.138.20.8/32",
+      "93.56.171.15/32",
+      "213.121.161.124/32",
+      "52.67.148.55/32",
+      "194.33.196.0/25",
+      "194.33.197.0/25",
+      "79.152.189.104/32",
+      "89.32.121.144/32",
+      "178.248.34.47/32",
+      "185.191.249.100/32",
+      "54.94.206.111/32",
+      "194.33.193.0/25",
+      "178.248.34.42/32"
+    ]
   }
 
   egress {
@@ -70,19 +96,7 @@ resource "aws_lb_target_group" "tipstaff_target_group" {
 
 }
 
-resource "aws_lb_listener" "tipstaff_lb_1" {
-  load_balancer_arn = aws_lb.tipstaff_lb.arn
-  port              = local.application_data.accounts[local.environment].server_port_1
-  protocol          = local.application_data.accounts[local.environment].lb_listener_protocol_1
-  ssl_policy        = local.application_data.accounts[local.environment].lb_listener_protocol_1 == "HTTP" ? "" : "ELBSecurityPolicy-2016-08"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tipstaff_target_group.arn
-  }
-}
-
-resource "aws_lb_listener" "tipstaff_lb_2" {
+resource "aws_lb_listener" "tipstaff_lb" {
   depends_on = [
     aws_acm_certificate.external
   ]
