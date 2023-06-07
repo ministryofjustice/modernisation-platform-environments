cd9946716da1fc4a0250b9171cca0d3ef2fafbca
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-06-07 16:09:42 +0100

    Nomis: DSOS-1918: ssm parameters in code (#2453)
    * add ssm params to baseline
    
    * add local ssm params
    
    * test
    
    * test
    
    * fix
    
    * test
    
    * add db and weblogic SSM params
    
    * fixes
    
    * fix
    
    * test

--- terraform/modules/baseline/ec2_autoscaling_group.tf
+++ terraform/modules/baseline/ec2_autoscaling_group.tf
@@ -71,6 +71,8 @@ module "ec2_autoscaling_group" {
 
   # ensure service linked role is created first if defined in code
   depends_on = [
-    aws_iam_service_linked_role.this
+    aws_iam_service_linked_role.this,
+    aws_ssm_parameter.fixed,
+    aws_ssm_parameter.placeholder,
   ]
 }
