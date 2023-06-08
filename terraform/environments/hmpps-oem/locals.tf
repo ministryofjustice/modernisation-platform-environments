c511410263ea6cc4394a4fe4140a86e7e5967c6a
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-06-08 15:34:12 +0100

    Nomis: DSOS-1933: prepare oem accounts (#2464)
    * DSOS-1933: prep OEM account
    
    * bump s3 module version
    
    * fix
    
    * readme
    
    * typo

--- terraform/environments/hmpps-oem/locals.tf
+++ terraform/environments/hmpps-oem/locals.tf
@@ -1,1 +1,71 @@
-#### This file can be used to store locals specific to the member account ####
+locals {
+  business_unit = var.networking[0].business-unit
+  region        = "eu-west-2"
+
+  environment_configs = {
+    development   = local.development_config
+    test          = local.test_config
+    preproduction = local.preproduction_config
+    production    = local.production_config
+  }
+  baseline_environment_config = local.environment_configs[local.environment]
+
+  baseline_presets_options = {
+    enable_application_environment_wildcard_cert = false
+    enable_business_unit_kms_cmks                = true
+    enable_image_builder                         = true
+    enable_ec2_cloud_watch_agent                 = true
+    enable_ec2_self_provision                    = true
+    enable_oracle_secure_web                     = true
+    cloudwatch_metric_alarms                     = {}
+    cloudwatch_metric_alarms_lists               = {}
+    cloudwatch_metric_alarms_lists_with_actions = {
+      dso_pagerduty               = ["dso_pagerduty"]
+      dba_pagerduty               = ["dba_pagerduty"]
+      dba_high_priority_pagerduty = ["dba_high_priority_pagerduty"]
+    }
+    route53_resolver_rules = {
+      # outbound-data-and-private-subnets = ["azure-fixngo-domain"]  # already set by nomis account
+    }
+    iam_policies_filter      = ["ImageBuilderS3BucketWriteAndDeleteAccessPolicy"]
+    iam_policies_ec2_default = ["EC2S3BucketWriteAndDeleteAccessPolicy", "ImageBuilderS3BucketWriteAndDeleteAccessPolicy"]
+    s3_iam_policies          = ["EC2S3BucketWriteAndDeleteAccessPolicy"]
+    sns_topics               = {}
+  }
+
+  baseline_acm_certificates         = {}
+  baseline_cloudwatch_log_groups    = {}
+  baseline_ec2_autoscaling_groups   = {}
+  baseline_ec2_instances            = {}
+  baseline_iam_policies             = {}
+  baseline_iam_roles                = {}
+  baseline_iam_service_linked_roles = {}
+  baseline_key_pairs                = {}
+  baseline_kms_grants               = {}
+  baseline_lbs                      = {}
+  baseline_route53_resolvers        = {}
+  baseline_route53_zones            = {}
+
+  baseline_s3_buckets = {
+    s3-bucket = {
+      iam_policies = module.baseline_presets.s3_iam_policies
+    }
+  }
+
+  baseline_security_groups = {
+    data-oem = local.security_groups.data_oem
+  }
+
+  baseline_sns_topics = {}
+
+  baseline_ssm_parameters = {
+    # ssm params at root level
+    "" = {
+      prefix  = ""
+      postfix = ""
+      parameters = {
+        ec2-user_pem = {}
+      }
+    }
+  }
+}
---
+++ terraform/environments/hmpps-oem/locals_security_groups.tf
@@ -0,0 +1,27 @@
+locals {
+
+  security_groups = {
+    data_oem = {
+      description = "Security group for OEM servers"
+      ingress = {
+        all-from-self = {
+          description = "Allow all ingress to self"
+          from_port   = 0
+          to_port     = 0
+          protocol    = -1
+          self        = true
+        }
+      }
+      egress = {
+        all = {
+          description     = "Allow all egress"
+          from_port       = 0
+          to_port         = 0
+          protocol        = "-1"
+          cidr_blocks     = ["0.0.0.0/0"]
+          security_groups = []
+        }
+      }
+    }
+  }
+}
