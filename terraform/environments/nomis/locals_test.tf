859efd261f6f4bded0147a5b2b5f8cd5610f256a
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-06-08 09:28:01 +0100

    fix branch

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -79,7 +79,7 @@ locals {
         })
         user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
           args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
-            branch = "nomis/xtag_weblogic_setup"
+            branch = "main"
           })
         })
         autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
