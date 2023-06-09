10f746d2539dbb1c85f33d4db70a1d7dfd44e400
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-06-09 01:21:59 +0100

    Fix Bucket Name, s3_artifacts_store

--- terraform/environments/digital-prison-reporting/domain_builder.tf
+++ terraform/environments/digital-prison-reporting/domain_builder.tf
@@ -9,7 +9,7 @@ module "domain_builder_backend_Lambda" {
   enable_lambda = local.enable_domain_builder_lambda
   name          = "${local.project}-domain-builder-backend-api"
   s3_bucket     = module.s3_artifacts_store.bucket_id
-  s3_key        = "/build-artifacts/domain-builder/jars/domain-builder-backend-api-vLatest-all.jar"
+  s3_key        = "build-artifacts/domain-builder/jars/domain-builder-backend-api-vLatest-all.jar"
   handler       = "io.micronaut.function.aws.proxy.MicronautLambdaHandler"
   runtime       = "java11"
   policies      = [aws_iam_policy.s3_read_access_policy.arn, ]
