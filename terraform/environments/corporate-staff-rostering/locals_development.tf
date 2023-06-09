b96a5cbd533ade4ff25ea25105c1fc420c78880d
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-06-09 10:11:28 +0100

    fix

--- terraform/environments/corporate-staff-rostering/locals_development.tf
+++ terraform/environments/corporate-staff-rostering/locals_development.tf
@@ -11,7 +11,7 @@ locals {
           availability_zone = null
         })
         instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["data-oem"]
+          vpc_security_group_ids = ["data-db"]
         })
         user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
           args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
