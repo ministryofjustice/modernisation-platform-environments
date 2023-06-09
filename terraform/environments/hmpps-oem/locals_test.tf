24fedd9df7b304bee0edd012831a43acb6eca4dc
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-06-09 09:09:02 +0100

    DSOS-1933: setup oem server (#2471)
    * add more OEM configuration
    
    * swap to main branch

--- terraform/environments/hmpps-oem/locals_test.tf
+++ terraform/environments/hmpps-oem/locals_test.tf
@@ -5,31 +5,13 @@ locals {
   test_config = {
 
     baseline_ec2_autoscaling_groups = {
-      test-oem = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name          = "base_ol_8_5_*"
-          availability_zone = null
-        })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["data-oem"]
-        })
-        user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
-          args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
+      test-oem = merge(local.oem_ec2_default, {
+        user_data_cloud_init = merge(local.oem_ec2_default.user_data_cloud_init, {
+          args = merge(local.oem_ec2_default.user_data_cloud_init.args, {
             branch = "main"
           })
         })
-        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
-          desired_capacity = 0
-        })
-        # autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
-        tags = {
-          description = "For testing Oracle Enterprise Manager image"
-          ami         = "base_ol_8_5"
-          os-type     = "Linux"
-          component   = "test"
-          server-type = "base-ol-8-5"
-        }
-      }
+      })
     }
 
     baseline_s3_buckets = {
