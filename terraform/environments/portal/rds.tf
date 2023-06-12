c140fe875a4bdeafa91817a8fbdd6292aa929d15
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-06-12 14:25:31 +0100

    LAWS-3242: delete all the IGDB resources because last run failed to delete resources because rds in use and dependancies

--- terraform/environments/portal/rds.tf
+++ terraform/environments/portal/rds.tf
@@ -40,18 +40,18 @@ locals {
   lzprd-vpc                       = "10.205.0.0/20"
  }
 
-resource "aws_db_subnet_group" "igdb" {
-  name       = "${local.application_name}-${local.environment}-subnetgrp-${lower(local.igdb_dbname)}"
-  subnet_ids = [data.aws_subnet.data_subnets_a.id, data.aws_subnet.data_subnets_b.id, data.aws_subnet.data_subnets_c.id]
+# resource "aws_db_subnet_group" "igdb" {
+#   name       = "${local.application_name}-${local.environment}-subnetgrp-${lower(local.igdb_dbname)}"
+#   subnet_ids = [data.aws_subnet.data_subnets_a.id, data.aws_subnet.data_subnets_b.id, data.aws_subnet.data_subnets_c.id]
 
-  tags = merge(
-    local.tags,
-    { "Name" = "${local.application_name}-${local.environment}-subnetgrp-igdb" },
-    { "Keep" = "true" }
+#   tags = merge(
+#     local.tags,
+#     { "Name" = "${local.application_name}-${local.environment}-subnetgrp-igdb" },
+#     { "Keep" = "true" }
 
-  )
+#   )
 
-}
+# }
 
 resource "aws_db_subnet_group" "iadb" {
   name       = "${local.application_name}-${local.environment}-subnetgrp-${lower(local.iadb_dbname)}"
@@ -66,54 +66,54 @@ resource "aws_db_subnet_group" "iadb" {
 
 }
 
-resource "aws_db_parameter_group" "igdb-parametergroup-19c" {
-  name        = "${local.application_name}-${local.environment}-parametergroup-${lower(local.igdb_dbname)}"
-  family      = "oracle-ee-19"
-  description = "${local.application_name}-${local.environment}-parametergroup-${lower(local.igdb_dbname)}"
+# resource "aws_db_parameter_group" "igdb-parametergroup-19c" {
+#   name        = "${local.application_name}-${local.environment}-parametergroup-${lower(local.igdb_dbname)}"
+#   family      = "oracle-ee-19"
+#   description = "${local.application_name}-${local.environment}-parametergroup-${lower(local.igdb_dbname)}"
 
 
-  parameter {
-    name  = "open_cursors"
-    value = "1000"
-    apply_method = "pending-reboot"
-  }
+#   parameter {
+#     name  = "open_cursors"
+#     value = "1000"
+#     apply_method = "pending-reboot"
+#   }
 
-  parameter {
-    name  = "processes"
-    value = "1000"
-    apply_method = "pending-reboot"
-  }
+#   parameter {
+#     name  = "processes"
+#     value = "1000"
+#     apply_method = "pending-reboot"
+#   }
 
-  parameter {
-    name  = "query_rewrite_enabled"
-    value = "TRUE"
-    apply_method = "pending-reboot"
-  }
+#   parameter {
+#     name  = "query_rewrite_enabled"
+#     value = "TRUE"
+#     apply_method = "pending-reboot"
+#   }
 
-  parameter {
-    name  = "query_rewrite_integrity"
-    value = "TRUSTED"
-    apply_method = "pending-reboot"
-  }
+#   parameter {
+#     name  = "query_rewrite_integrity"
+#     value = "TRUSTED"
+#     apply_method = "pending-reboot"
+#   }
 
-  parameter {
-    name  = "sessions"
-    value = "1000"
-    apply_method = "pending-reboot"
-  }
+#   parameter {
+#     name  = "sessions"
+#     value = "1000"
+#     apply_method = "pending-reboot"
+#   }
 
-  parameter {
-    name  = "sqlnetora.sqlnet.allowed_logon_version_server"
-    value = "11"
-    apply_method = "pending-reboot"
-  }
+#   parameter {
+#     name  = "sqlnetora.sqlnet.allowed_logon_version_server"
+#     value = "11"
+#     apply_method = "pending-reboot"
+#   }
 
-  tags = merge(
-    local.tags,
-    { "Name" = "${local.application_name}-${local.environment}-parametergroup-igdb" },
-    { "Keep" = "true" }
-  )
-}
+#   tags = merge(
+#     local.tags,
+#     { "Name" = "${local.application_name}-${local.environment}-parametergroup-igdb" },
+#     { "Keep" = "true" }
+#   )
+# }
 
 resource "aws_db_parameter_group" "iadb-parametergroup-19c" {
   name        = "${local.application_name}-${local.environment}-parametergroup-${lower(local.iadb_dbname)}"
@@ -164,22 +164,22 @@ resource "aws_db_parameter_group" "iadb-parametergroup-19c" {
   )
 }
 
-resource "aws_db_option_group" "PortalIGDB19OptionGroup" {
-  name                     = "${local.application_name}-${local.environment}-optiongroup-${lower(local.igdb_dbname)}"
-  option_group_description = "Portal IGDB DB 19- enables STATSPACK"
-  engine_name              = "oracle-ee"
-  major_engine_version     = "19"
+# resource "aws_db_option_group" "PortalIGDB19OptionGroup" {
+#   name                     = "${local.application_name}-${local.environment}-optiongroup-${lower(local.igdb_dbname)}"
+#   option_group_description = "Portal IGDB DB 19- enables STATSPACK"
+#   engine_name              = "oracle-ee"
+#   major_engine_version     = "19"
 
-  option {
-    option_name = "STATSPACK"
-  }
+#   option {
+#     option_name = "STATSPACK"
+#   }
 
-  tags = merge(
-    local.tags,
-    { "Name" = "${local.application_name}-${local.environment}-optiongroup-igdb" },
-    { "Keep" = "true" }
-  )
-}
+#   tags = merge(
+#     local.tags,
+#     { "Name" = "${local.application_name}-${local.environment}-optiongroup-igdb" },
+#     { "Keep" = "true" }
+#   )
+# }
 
 resource "aws_db_option_group" "PortalIADB19OptionGroup" {
   name                     = "${local.application_name}-${local.environment}-optiongroup-${lower(local.iadb_dbname)}"
@@ -198,106 +198,106 @@ resource "aws_db_option_group" "PortalIADB19OptionGroup" {
   )
 }
 
-resource "aws_security_group" "igdb" {
-  name        = "${local.application_name}-${local.environment}-secgroup-DB-${lower(local.igdb_dbname)}"
-  description = "RDS access with the LAA Landing Zone"
-  vpc_id      = data.aws_vpc.shared.id
+# resource "aws_security_group" "igdb" {
+#   name        = "${local.application_name}-${local.environment}-secgroup-DB-${lower(local.igdb_dbname)}"
+#   description = "RDS access with the LAA Landing Zone"
+#   vpc_id      = data.aws_vpc.shared.id
 
-  ingress {
-    description = "AppStream Inbound"
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = [local.appstream_cidr]
+#   ingress {
+#     description = "AppStream Inbound"
+#     from_port   = 1521
+#     to_port     = 1521
+#     protocol    = "tcp"
+#     cidr_blocks = [local.appstream_cidr]
 
-  }
+#   }
 
-  ingress {
-    description = "Ireland Shared Services Inbound - Workspaces etc"
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = [local.cidr_ire_workspace]
+#   ingress {
+#     description = "Ireland Shared Services Inbound - Workspaces etc"
+#     from_port   = 1521
+#     to_port     = 1521
+#     protocol    = "tcp"
+#     cidr_blocks = [local.cidr_ire_workspace]
 
-  }
+#   }
 
-  ingress {
-    description = "SharedServices Inbound - Workspaces etc"
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = [local.workspaces_cidr]
+#   ingress {
+#     description = "SharedServices Inbound - Workspaces etc"
+#     from_port   = 1521
+#     to_port     = 1521
+#     protocol    = "tcp"
+#     cidr_blocks = [local.workspaces_cidr]
 
-  }
+#   }
 
-  ingress {
-    description = "Cloud Platform VPC Internal Traffic inbound"
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = [local.cp_vpc_cidr]
+#   ingress {
+#     description = "Cloud Platform VPC Internal Traffic inbound"
+#     from_port   = 1521
+#     to_port     = 1521
+#     protocol    = "tcp"
+#     cidr_blocks = [local.cp_vpc_cidr]
 
-  }
-
-
-  ingress {
-    description = "Connectivity from MP Environment VPC"
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.shared.cidr_block]
-  }
+#   }
 
 
-  # ingress {
-  #   description = "Connectivity from Portal IDM VPC"
-  #   from_port   = 1521
-  #   to_port     = 1521
-  #   protocol    = "tcp"
-  #   cidr_blocks = [data.aws_vpc.shared.cidr_block_IDM]
-  # }
+#   ingress {
+#     description = "Connectivity from MP Environment VPC"
+#     from_port   = 1521
+#     to_port     = 1521
+#     protocol    = "tcp"
+#     cidr_blocks = [data.aws_vpc.shared.cidr_block]
+#   }
 
 
-  # ingress {
-  #   description = "Connectivity from Portal OAM VPC"
-  #   from_port   = 1521
-  #   to_port     = 1521
-  #   protocol    = "tcp"
-  #   cidr_blocks = [data.aws_vpc.shared.cidr_block_OAM]
-  # }
+#   # ingress {
+#   #   description = "Connectivity from Portal IDM VPC"
+#   #   from_port   = 1521
+#   #   to_port     = 1521
+#   #   protocol    = "tcp"
+#   #   cidr_blocks = [data.aws_vpc.shared.cidr_block_IDM]
+#   # }
 
 
-  # ingress {
-  #   description = "Connectivity from Portal OIM VPC"
-  #   from_port   = 1521
-  #   to_port     = 1521
-  #   protocol    = "tcp"
-  #   cidr_blocks = [data.aws_vpc.shared.cidr_block_OIM]
-  # }
+#   # ingress {
+#   #   description = "Connectivity from Portal OAM VPC"
+#   #   from_port   = 1521
+#   #   to_port     = 1521
+#   #   protocol    = "tcp"
+#   #   cidr_blocks = [data.aws_vpc.shared.cidr_block_OAM]
+#   # }
 
 
-  # ingress {
-  #   description = "Connectivity from Portal OHS VPC"
-  #   from_port   = 1521
-  #   to_port     = 1521
-  #   protocol    = "tcp"
-  #   cidr_blocks = [data.aws_vpc.shared.cidr_block_OHS]
-  # }
+#   # ingress {
+#   #   description = "Connectivity from Portal OIM VPC"
+#   #   from_port   = 1521
+#   #   to_port     = 1521
+#   #   protocol    = "tcp"
+#   #   cidr_blocks = [data.aws_vpc.shared.cidr_block_OIM]
+#   # }
 
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   # ingress {
+#   #   description = "Connectivity from Portal OHS VPC"
+#   #   from_port   = 1521
+#   #   to_port     = 1521
+#   #   protocol    = "tcp"
+#   #   cidr_blocks = [data.aws_vpc.shared.cidr_block_OHS]
+#   # }
 
 
-  tags = merge(
-    local.tags,
-    { "Name" = "${local.application_name}-${local.environment}-igdb" }
-  )
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+
+#   tags = merge(
+#     local.tags,
+#     { "Name" = "${local.application_name}-${local.environment}-igdb" }
+#   )
+# }
 
 resource "aws_security_group" "iadb" {
   name        = "${local.application_name}-${local.environment}-secgroup-DB-${lower(local.iadb_dbname)}"
@@ -401,24 +401,24 @@ resource "aws_security_group" "iadb" {
 }
 
 
-resource "random_password" "rds_password_igdb" {
-  length  = 16
-  special = false
-}
+# resource "random_password" "rds_password_igdb" {
+#   length  = 16
+#   special = false
+# }
 
 resource "random_password" "rds_password_iadb" {
   length  = 16
   special = false
 }
 
-resource "aws_secretsmanager_secret" "rds_password_secret_igdb" {
-  name        = "${local.application_name}/app/db-master-password-igdb"
-  description = "This secret has a dynamically generated password."
-  tags = merge(
-    local.tags,
-    { "Name" = "${local.application_name}/app/db-master-password-igdb" },
-  )
-}
+# resource "aws_secretsmanager_secret" "rds_password_secret_igdb" {
+#   name        = "${local.application_name}/app/db-master-password-igdb"
+#   description = "This secret has a dynamically generated password."
+#   tags = merge(
+#     local.tags,
+#     { "Name" = "${local.application_name}/app/db-master-password-igdb" },
+#   )
+# }
 
 resource "aws_secretsmanager_secret" "rds_password_secret_iadb" {
   name        = "${local.application_name}/app/db-master-password-iadb"
@@ -429,15 +429,15 @@ resource "aws_secretsmanager_secret" "rds_password_secret_iadb" {
   )
 }
 
-resource "aws_secretsmanager_secret_version" "rds_password_secret_version_igdb" {
-  secret_id = aws_secretsmanager_secret.rds_password_secret_igdb.id
-  secret_string = jsonencode(
-    {
-      username = local.igdb_username
-      password = random_password.rds_password_igdb.result
-    }
-  )
-}
+# resource "aws_secretsmanager_secret_version" "rds_password_secret_version_igdb" {
+#   secret_id = aws_secretsmanager_secret.rds_password_secret_igdb.id
+#   secret_string = jsonencode(
+#     {
+#       username = local.igdb_username
+#       password = random_password.rds_password_igdb.result
+#     }
+#   )
+# }
 
 resource "aws_secretsmanager_secret_version" "rds_password_secret_version_iadb" {
   secret_id = aws_secretsmanager_secret.rds_password_secret_iadb.id
@@ -449,54 +449,54 @@ resource "aws_secretsmanager_secret_version" "rds_password_secret_version_iadb" 
   )
 }
 
-resource "aws_db_instance" "appdb1" {
-  allocated_storage               = local.igdb_storage_size
-  db_name                         = local.igdb_dbname
-  identifier                      = lower(local.igdb_dbname)
-  engine                          = local.igdb_engine
-  engine_version                  = local.igdb_engine_version
-  enabled_cloudwatch_logs_exports = ["alert", "audit", "listener", "trace"]
-  performance_insights_enabled    = true
-  instance_class                  = local.igdb_instance_class
-  auto_minor_version_upgrade      = local.igdb_auto_minor_version_upgrade
-  storage_type                    = local.igdb_storage_type
-  backup_retention_period         = local.igdb_backup_retention_period
-  backup_window                   = local.igdb_backup_window
-  character_set_name              = local.igdb_character_set_name
-  #max_allocated_storage           = local.max_allocated_storage
-  username               = local.igdb_username
-  password               = random_password.rds_password_igdb.result
-  vpc_security_group_ids = [aws_security_group.igdb.id]
-  #skip_final_snapshot             = false
-  final_snapshot_identifier       = "${local.application_name}-${formatdate("DDMMMYYYYhhmm", timestamp())}-finalsnapshot"
-  parameter_group_name  = aws_db_parameter_group.igdb-parametergroup-19c.name
-  db_subnet_group_name  = aws_db_subnet_group.igdb.name
-  maintenance_window    = local.igdb_maintenance_window
-  license_model         = "bring-your-own-license"
-  #TODO deletion_protection   = true
-  copy_tags_to_snapshot = true
-  storage_encrypted     = true
-  #apply_immediately           = true
-  snapshot_identifier         = format("arn:aws:rds:eu-west-2:%s:snapshot:%s", data.aws_caller_identity.current.account_id, local.igdb_snapshot_name)
-  kms_key_id                  = data.aws_kms_key.rds_shared.arn
-  publicly_accessible         = false
-  allow_major_version_upgrade = true
-  multi_az                    = false
-  option_group_name           = aws_db_option_group.PortalIGDB19OptionGroup.name
+# resource "aws_db_instance" "appdb1" {
+#   allocated_storage               = local.igdb_storage_size
+#   db_name                         = local.igdb_dbname
+#   identifier                      = lower(local.igdb_dbname)
+#   engine                          = local.igdb_engine
+#   engine_version                  = local.igdb_engine_version
+#   enabled_cloudwatch_logs_exports = ["alert", "audit", "listener", "trace"]
+#   performance_insights_enabled    = true
+#   instance_class                  = local.igdb_instance_class
+#   auto_minor_version_upgrade      = local.igdb_auto_minor_version_upgrade
+#   storage_type                    = local.igdb_storage_type
+#   backup_retention_period         = local.igdb_backup_retention_period
+#   backup_window                   = local.igdb_backup_window
+#   character_set_name              = local.igdb_character_set_name
+#   #max_allocated_storage           = local.max_allocated_storage
+#   username               = local.igdb_username
+#   password               = random_password.rds_password_igdb.result
+#   vpc_security_group_ids = [aws_security_group.igdb.id]
+#   #skip_final_snapshot             = false
+#   final_snapshot_identifier       = "${local.application_name}-${formatdate("DDMMMYYYYhhmm", timestamp())}-finalsnapshot"
+#   parameter_group_name  = aws_db_parameter_group.igdb-parametergroup-19c.name
+#   db_subnet_group_name  = aws_db_subnet_group.igdb.name
+#   maintenance_window    = local.igdb_maintenance_window
+#   license_model         = "bring-your-own-license"
+#   #TODO deletion_protection   = true
+#   copy_tags_to_snapshot = true
+#   storage_encrypted     = true
+#   #apply_immediately           = true
+#   snapshot_identifier         = format("arn:aws:rds:eu-west-2:%s:snapshot:%s", data.aws_caller_identity.current.account_id, local.igdb_snapshot_name)
+#   kms_key_id                  = data.aws_kms_key.rds_shared.arn
+#   publicly_accessible         = false
+#   allow_major_version_upgrade = true
+#   multi_az                    = false
+#   option_group_name           = aws_db_option_group.PortalIGDB19OptionGroup.name
 
-  timeouts {
-    create = "60m"
-    delete = "2h"
-  }
+#   timeouts {
+#     create = "60m"
+#     delete = "2h"
+#   }
 
-  tags = merge(
-    local.tags,
-    { "Name" = "${local.igdb_dbname}" },
-    { "Keep" = "true" },
-    { "scheduler:ebs-snapshot" = "True" }
-  )
+#   tags = merge(
+#     local.tags,
+#     { "Name" = "${local.igdb_dbname}" },
+#     { "Keep" = "true" },
+#     { "scheduler:ebs-snapshot" = "True" }
+#   )
 
-}
+# }
 
 resource "aws_db_instance" "appdb2" {
   allocated_storage               = local.iadb_storage_size
@@ -548,14 +548,14 @@ resource "aws_db_instance" "appdb2" {
 }
 
 
-resource "aws_route53_record" "igdb_rds" {
-  provider = aws.core-vpc
-  zone_id  = data.aws_route53_zone.external.zone_id
-  name     = "rds.${local.application_name}.${data.aws_route53_zone.external.name}"
-  type     = "CNAME"
-  ttl      = 60
-  records  = [aws_db_instance.appdb1.address]
-}
+# resource "aws_route53_record" "igdb_rds" {
+#   provider = aws.core-vpc
+#   zone_id  = data.aws_route53_zone.external.zone_id
+#   name     = "rds.${local.application_name}.${data.aws_route53_zone.external.name}"
+#   type     = "CNAME"
+#   ttl      = 60
+#   records  = [aws_db_instance.appdb1.address]
+# }
 
 resource "aws_route53_record" "iadb_rds" {
   provider = aws.core-vpc
