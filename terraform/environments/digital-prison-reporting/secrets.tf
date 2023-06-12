9c62835808447f2e9a000d15bd99c831c73972fe
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-06-12 11:45:42 +0100

    Add aws_secretsmanager_secret metadata

--- terraform/environments/digital-prison-reporting/secrets.tf
+++ terraform/environments/digital-prison-reporting/secrets.tf
@@ -1,1 +1,4 @@
 #### This file can be used to store secrets specific to the member account ####
+resource "aws_secretsmanager_secret" "nomis" {
+  name = "dpr/${local.project}-nomis-source-secrets"
+}
