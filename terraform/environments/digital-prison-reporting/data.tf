9a176ea3593e8cd9e52a9db70e518f2e91d9dd88
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-06-12 12:21:02 +0100

    Add Variable for kinesis Endpoints and add Support for DMS/AWS Secrets

--- terraform/environments/digital-prison-reporting/data.tf
+++ terraform/environments/digital-prison-reporting/data.tf
@@ -3,6 +3,11 @@ data "aws_kms_key" "kinesis_kms_key" {
   key_id = aws_kms_key.kinesis-kms-key.arn
 }
 
+# Source Nomis Secrets
+data "aws_secretsmanager_secret" "nomis" {
+  name = "dpr/${local.project}-nomis-source-secrets"
+}
+
 data "aws_secretsmanager_secret_version" "nomis" {
   secret_id = data.aws_secretsmanager_secret.nomis.id
 }
