331c24648aaaa91e2d8a621af01c3e67e6ca9b97
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-13 16:13:35 +0100

    enable access logs

--- terraform/environments/delius-core/nlb_ldap.tf
+++ terraform/environments/delius-core/nlb_ldap.tf
@@ -18,7 +18,54 @@ resource "aws_lb" "ldap" {
   drop_invalid_header_fields = true
   enable_deletion_protection = false
 
+  access_logs {
+    bucket  = module.s3_bucket_nlb_logs.bucket.id
+    prefix  = format("%s/%s", local.application_name, local.openldap_name)
+    enabled = true
+  }
+
   tags = local.openldap_nlb_tags
+}
+
+
+module "s3_bucket_nlb_logs" {
+
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.4.0"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+  bucket_name        = "${local.application_name}-${local.environment}-openldap-nlb-logs"
+  versioning_enabled = true
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
 }
 
 resource "aws_lb_listener" "ldap" {
