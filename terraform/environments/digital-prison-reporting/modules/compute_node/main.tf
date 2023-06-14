5d997e5a06c50430df7dee60bc7a3c7c94f782e2
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-06-14 09:17:41 +0100

    Add Conditional Linux Schedule

--- terraform/environments/digital-prison-reporting/modules/compute_node/main.tf
+++ terraform/environments/digital-prison-reporting/modules/compute_node/main.tf
@@ -148,6 +148,8 @@ resource "aws_autoscaling_group" "bastion_linux_daily" {
 }
 
 resource "aws_autoscaling_schedule" "bastion_linux_scale_down" {
+  count = var.enable_compute_node ? 1 : 0
+
   scheduled_action_name  = "${var.name}_scaledown"
   min_size               = 0
   max_size               = 0
@@ -157,6 +159,8 @@ resource "aws_autoscaling_schedule" "bastion_linux_scale_down" {
 }
 
 resource "aws_autoscaling_schedule" "bastion_linux_scale_up" {
+  count = var.enable_compute_node ? 1 : 0
+
   scheduled_action_name  = "${var.name}_scaleup"
   min_size               = 1
   max_size               = 1
