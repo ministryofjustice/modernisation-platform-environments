6d96c9dc35317822e326e9da1e454c7dad686beb
George Taylor <george.taylor@digital.justice.gov.uk>
2023-06-14 16:37:44 +0100

    remove access logs

--- terraform/environments/delius-core/nlb_ldap.tf
+++ terraform/environments/delius-core/nlb_ldap.tf
@@ -28,46 +28,6 @@ resource "aws_lb" "ldap" {
 }
 
 
-module "s3_bucket_nlb_logs" {
-
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.4.0"
-
-  providers = {
-    aws.bucket-replication = aws
-  }
-  bucket_name        = "${local.application_name}-${local.environment}-openldap-nlb-logs"
-  versioning_enabled = true
-
-  lifecycle_rule = [
-    {
-      id      = "main"
-      enabled = "Enabled"
-      prefix  = ""
-
-      tags = {
-        rule      = "log"
-        autoclean = "true"
-      }
-
-      noncurrent_version_transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
-
-      noncurrent_version_expiration = {
-        days = 730
-      }
-    }
-  ]
-
-  tags = local.tags
-}
-
 resource "aws_lb_listener" "ldap" {
   load_balancer_arn = aws_lb.ldap.arn
   port              = local.openldap_port
