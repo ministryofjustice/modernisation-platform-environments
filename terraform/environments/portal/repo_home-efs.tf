5dbf91d7122999235a71aae31c0940a5f94805fa
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-06-14 12:34:09 +0100

    efs repo home creatation

---
+++ terraform/environments/portal/repo_home-efs.tf
@@ -0,0 +1,29 @@
+
+resource "aws_security_group" "efs_sg" {
+  
+  name        = "${local.application_name}-${local.environment}-repo_home-efs-security-group"
+  description = "Portal ${upper(repo_home)} Product EFS Security Group"
+  vpc_id      = data.aws_vpc.shared.id
+}
+
+
+
+resource "aws_efs_file_system" "efs" {
+
+    performance_mode    = "generalPurpose"
+    throughput_mode     = "bursting"
+    encrypted           = "true"
+    tags                = merge(
+    local.tags,
+    { "Name" = "${local.application_name}-repo_home" },
+    local.environment != "production" ? { "snapshot-with-daily-35-day-retention" = "yes" } : { "snapshot-with-hourly-35-day-retention" = "yes" }
+  )
+ }
+
+
+resource "aws_efs_mount_target" "target" {
+ 
+  file_system_id = aws_efs_file_system.efs.id
+  subnet_id      = data.aws_subnet.private_subnets_a.id
+  security_groups = [aws_security_group.efs_sg.id]
+}
