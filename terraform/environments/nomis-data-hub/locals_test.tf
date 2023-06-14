88d2d619cd8649cffcaf10bafe5e8c8639f179e1
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2023-06-14 09:43:52 +0100

    lb settings test

--- terraform/environments/nomis-data-hub/locals_test.tf
+++ terraform/environments/nomis-data-hub/locals_test.tf
@@ -72,55 +72,6 @@ locals {
     }
     baseline_ec2_autoscaling_groups = {
 
-      # Example ASG using base image with ansible provisioning
-      # Include the autoscale-trigger-hook ansible role when using hooks
-      # dev-base-rhel79 = {
-      #   config = merge(module.baseline_presets.ec2_instance.config.default, {
-      #     ami_name = "base_rhel_7_9_*"
-      #   })
-      #   instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-      #     vpc_security_group_ids = ["private"]
-      #   })
-      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-      #   autoscaling_group = {
-      #     desired_capacity    = 1
-      #     max_size      #       # = 2
-      #     vpc_zone_identifier = module.environment.subnets["private"].ids
-      #   }
-      #   autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
-      #   tags = {
-      #     description = "For testing with official RedHat RHEL7.9 image"
-      #     os-type     = "Linux"
-      #     component   = "test"
-      #     server-type = "set me to the ansible server type group vars"
-      #   }
-      #
-      # Example target group setup below
-      #
-      #   lb_target_groups = {
-      #     http-7777 = {
-      #       port                 = 7777
-      #       protocol             = "HTTP"
-      #       target_type          = "instance"
-      #       deregistration_delay = 30
-      #       health_check = {
-      #         enabled             = true
-      #         interval            = 30
-      #         healthy_threshold   = 3
-      #         matcher             = "200-399"
-      #         path                = "/"
-      #         port                = 7777
-      #         timeout             = 5
-      #         unhealthy_threshold = 5
-      #       }
-      #       stickiness = {
-      #         enabled = true
-      #         type    = "lb_cookie"
-      #       }
-      #     }
-      #   }
-      # }
-
       t1_ndh_app_a = {
         config = merge(module.baseline_presets.ec2_instance.config.default, {
           ami_name  = "nomis_data_hub_rhel_7_9_app_release_2023-05-02T00-00-47.783Z"
@@ -194,28 +145,28 @@ locals {
         }
       }
 
-      #      lb_target_groups = {
-      #        http-7777 = {
-      #          port                 = 7777
-      #          protocol             = "HTTP"
-      #          target_type          = "instance"
-      #          deregistration_delay = 30
-      #          health_check = {
-      #            enabled             = true
-      #            interval            = 30
-      #            healthy_threshold   = 3
-      #            matcher             = "200-399"
-      #            path                = "/"
-      #            port                = 7777
-      #            timeout             = 5
-      #            unhealthy_threshold = 5
-      #          }
-      #          stickiness = {
-      #            enabled = true
-      #            type    = "lb_cookie"
-      #          }
-      #        }
-      #      }
+      lb_target_groups = {
+        http-7777 = {
+          port                 = 7777
+          protocol             = "HTTP"
+          target_type          = "instance"
+          deregistration_delay = 30
+          health_check = {
+            enabled             = true
+            interval            = 30
+            healthy_threshold   = 3
+            matcher             = "200-399"
+            path                = "/"
+            port                = 7777
+            timeout             = 5
+            unhealthy_threshold = 5
+          }
+          stickiness = {
+            enabled = true
+            type    = "lb_cookie"
+          }
+        }
+      }
     }
   }
 }
