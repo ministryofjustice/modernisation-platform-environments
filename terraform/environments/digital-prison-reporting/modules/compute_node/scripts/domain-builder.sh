92cd45ddb469a1e3ea1f67278ac371643af7e6df
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-06-14 18:16:43 +0100

    Switch to /etc/profile and sudo source

--- terraform/environments/digital-prison-reporting/modules/compute_node/scripts/domain-builder.sh
+++ terraform/environments/digital-prison-reporting/modules/compute_node/scripts/domain-builder.sh
@@ -22,8 +22,7 @@ unzip awscliv2.zip
 
 # Set Env Configuration
 sudo mkdir -p /home/ssm-user/domain-builder/jars
-sudo touch /home/ssm-user/.bash_profile; echo "export PATH=$PATH:/home/ssm-user/domain-builder/jars" >> /home/ssm-user/.bash_profile
-sudo chmod +x /home/ssm-user/.bash_profile
+sudo echo "export PATH=$PATH:/home/ssm-user/domain-builder/jars" > /etc/profile; sudo source /etc/profile
 
 # Sync S3 Domain Builder Artifacts
 aws s3 cp s3://dpr-artifact-store-development/build-artifacts/domain-builder/jars/domain-builder-cli-frontend-vLatest-all.jar /home/ssm-user/domain-builder/jars
