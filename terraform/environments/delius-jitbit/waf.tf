6907e6c1380306b2622a5f6a99f63357d9cb19de
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-06-15 15:26:17 +0100

    use SSE for waf cloudwatch log group encryption

--- terraform/environments/delius-jitbit/waf.tf
+++ terraform/environments/delius-jitbit/waf.tf
@@ -70,7 +70,6 @@ resource "aws_wafv2_web_acl_association" "this" {
 resource "aws_cloudwatch_log_group" "waf" {
   name_prefix       = "${local.application_name}-waf-"
   retention_in_days = 14
-  kms_key_id        = data.aws_kms_key.general_shared.arn
 
   tags = local.tags
 }
