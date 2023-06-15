b67d0dc87c000e6be6cb4ad1b058ff6dc12b90a3
ministry-riaz-jamil <mahwish.riaz-jamil@digital.justice.gov.uk>
2023-06-15 14:16:44 +0100

    ++ mrj linux server block

--- terraform/environments/corporate-staff-rostering/locals_test.tf
+++ terraform/environments/corporate-staff-rostering/locals_test.tf
@@ -4,7 +4,24 @@ locals {
   # baseline config
   test_config = {
 
-    baseline_ec2_autoscaling_groups = {}
+    baseline_ec2_autoscaling_groups = {
+      mrj-dev-base-ol85 = {
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name          = "base_ol_8_5_*"
+          availability_zone = null
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["data-db"]
+        })
+        tags = {
+          description = "For testing our base OL8.5 base image"
+          ami         = "base_ol_8_5"
+          os-type     = "Linux"
+          component   = "test"
+          server-type = "base-ol-8-5"
+        }
+      }
+    }
 
     baseline_s3_buckets = {
       # use this bucket for storing artefacts for use across all accounts
