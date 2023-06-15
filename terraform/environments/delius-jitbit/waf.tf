53d0f7a43bb8d5b1a429cd4fcf68526456f1eb82
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-06-15 17:00:15 +0100

    remove logging policy

--- terraform/environments/delius-jitbit/waf.tf
+++ terraform/environments/delius-jitbit/waf.tf
@@ -67,36 +67,10 @@ resource "aws_wafv2_web_acl_association" "this" {
   web_acl_arn  = aws_wafv2_web_acl.this.arn
 }
 
-resource "aws_cloudwatch_log_group" "example" {
-  name = "aws-waf-logs-some-uniq-suffix"
+resource "aws_cloudwatch_log_group" "waf" {
+  name = "aws-waf-logs-${local.application_name}"
 }
-resource "aws_wafv2_web_acl_logging_configuration" "example" {
-  log_destination_configs = [aws_cloudwatch_log_group.example.arn]
+resource "aws_wafv2_web_acl_logging_configuration" "waf" {
+  log_destination_configs = [aws_cloudwatch_log_group.waf.arn]
   resource_arn            = aws_wafv2_web_acl.this.arn
-}
-resource "aws_cloudwatch_log_resource_policy" "example" {
-  policy_document = data.aws_iam_policy_document.example.json
-  policy_name     = "webacl-policy-uniq-name"
-}
-data "aws_iam_policy_document" "example" {
-  version = "2012-10-17"
-  statement {
-    effect = "Allow"
-    principals {
-      identifiers = ["delivery.logs.amazonaws.com"]
-      type        = "AWS"
-    }
-    actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
-    resources = ["${aws_cloudwatch_log_group.example.arn}:*"]
-    condition {
-      test     = "ArnLike"
-      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
-      variable = "aws:SourceArn"
-    }
-    condition {
-      test     = "StringEquals"
-      values   = [tostring(data.aws_caller_identity.current.account_id)]
-      variable = "aws:SourceAccount"
-    }
-  }
 }
