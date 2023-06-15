9e13c1f7550bbcdbdf09cca0e77d0f40d84e8435
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-06-15 16:46:56 +0100

    use cloudwatch group name not name_prefix

--- terraform/environments/delius-jitbit/waf.tf
+++ terraform/environments/delius-jitbit/waf.tf
@@ -75,13 +75,13 @@ resource "aws_cloudwatch_log_group" "waf" {
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "this" {
-  log_destination_configs = ["${aws_cloudwatch_log_group.waf.arn}:*"]
+  log_destination_configs = [aws_cloudwatch_log_group.waf.arn]
   resource_arn            = aws_wafv2_web_acl.this.arn
 }
 
 resource "aws_cloudwatch_log_resource_policy" "waf" {
   policy_document = data.aws_iam_policy_document.waf.json
-  policy_name     = "webacl-policy-uniq-waf-log"
+  policy_name     = "${local.application_name}-waf-log"
 }
 
 data "aws_iam_policy_document" "waf" {
