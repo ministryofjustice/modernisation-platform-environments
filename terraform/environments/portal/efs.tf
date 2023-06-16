ceefacc9bc9e906c7e473780fe2573cda75c7eea
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-06-16 11:55:19 +0100

    Reverting OHS sec group name since it cannot be deleted anyways

--- terraform/environments/portal/efs.tf
+++ terraform/environments/portal/efs.tf
@@ -1,7 +1,7 @@
 locals {
   efs = {
     ohs = {
-      sec_group_id          = aws_security_group.ohs_instance_rebuild.id
+      sec_group_id          = aws_security_group.ohs_instance.id
 
     },
     oam = {
