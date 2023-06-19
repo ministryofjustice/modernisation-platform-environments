9ed6f879e97d49dfe0c93eef45160d7a55702e7a
silvianahorga-al <silviana.horga@digital.justice.gov.uk>
2023-06-19 17:09:15 +0100

    Try resolve template error by adding template provider

--- terraform/environments/delius-core/platform_versions.tf
+++ terraform/environments/delius-core/platform_versions.tf
@@ -8,6 +8,10 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
   required_version = "~> 1.0"
 }
