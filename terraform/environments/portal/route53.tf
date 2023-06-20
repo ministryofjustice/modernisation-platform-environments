ad45b3139fabd758a0c6a3f026556c91afcb2751
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-06-20 12:42:38 +0100

    LAWS-3284: added data.aws_vpc.shared.id to the vpc_association authoriation

--- terraform/environments/portal/route53.tf
+++ terraform/environments/portal/route53.tf
@@ -7,6 +7,6 @@ resource "aws_route53_zone" "private" {
 
 resource "aws_route53_vpc_association_authorization" "vpc_zone_association_auth" {
   provider  = aws.core-vpc
-  vpc_id    = aws_vpc.core-vpc.id
+  vpc_id    = data.aws_vpc.shared.id
   zone_id   = aws_route53_zone.private.id
 }
