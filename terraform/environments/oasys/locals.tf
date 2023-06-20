c308c9ca543afb0040a9457ffadcaeb729b35626
W <willg258@hotmail.co.uk>
2023-06-20 16:47:10 +0100

    Update locals.tf

--- terraform/environments/oasys/locals.tf
+++ terraform/environments/oasys/locals.tf
@@ -74,6 +74,7 @@ locals {
     })
     instance = merge(module.baseline_presets.ec2_instance.instance.default, {
       monitoring = true
+      vpc_security_group_ids = ["private-web"]
     })
     cloudwatch_metric_alarms = {}
     user_data_cloud_init     = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_ansible_no_tags
@@ -136,55 +137,45 @@ locals {
         size        = 100
         label       = "app"
         type        = "gp3"
-        snapshot_id = null
       }
       "/dev/sdc" = { # /u02
         size        = 500
         label       = "app"
         type        = "gp3"
-        snapshot_id = null
       }
       "/dev/sde" = { # DATA01
         label       = "data"
         size        = 200
         type        = "gp3"
-        snapshot_id = null
       }
       # "/dev/sdf" = {  # DATA02
       #   label = "data"
       #   type = null
-      #   snapshot_id = null
       # }
       # "/dev/sdg" = {  # DATA03
       #   label = "data"
       #   type = null
-      #   snapshot_id = null
       # }
       # "/dev/sdh" = {  # DATA04
       #   label = "data"
       #   type = null
-      #   snapshot_id = null
       # }
       # "/dev/sdi" = {  # DATA05
       #   label = "data"
       #   type = null
-      #   snapshot_id = null
       # }
       "/dev/sdj" = { # FLASH01
         label       = "flash"
         type        = "gp3"
-        snapshot_id = null
         size        = 50
       }
       # "/dev/sdk" = { # FLASH02
       #   label = "flash"
       #   type = null
-      #   snapshot_id = null
       # }
       "/dev/sds" = {
         label       = "swap"
         type        = "gp3"
-        snapshot_id = null
         size        = 2
       }
     }
