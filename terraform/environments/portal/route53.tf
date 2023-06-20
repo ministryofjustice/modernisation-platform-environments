98ea603071740767239f0b61097dc2ae39691a9d
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-06-20 11:22:39 +0100

    LAWS-3284: added vpc association authorization to connect zone to core-vpc

--- terraform/environments/portal/route53.tf
+++ terraform/environments/portal/route53.tf
@@ -1,7 +1,11 @@
 resource "aws_route53_zone" "private" {
   name = "aws.dev.legalservices.gov.uk"
   vpc {
-    vpc_id = [aws_vpc.core-vpc.id]
+    vpc_id = [data.aws_vpc.shared.id]
   }
 }
 
+resource "aws_route53_vpc_association_authorization" "vpc_zone_association_auth" {
+  vpc_id  = aws_vpc.core-vpc.id
+  zone_id = aws_route53_zone.private.id
+}
