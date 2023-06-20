4b064947d16b2c4716f8eb71418123716b814861
silvianahorga-al <silviana.horga@digital.justice.gov.uk>
2023-06-20 15:26:28 +0100

    Remove template provider after applying changes

--- terraform/environments/delius-core/platform_versions.tf
+++ terraform/environments/delius-core/platform_versions.tf
@@ -8,10 +8,6 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
   }
   required_version = "~> 1.0"
 }
