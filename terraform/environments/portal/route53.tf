f82515976b1cf4b19100bac10a73cfe5703f86f5
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-06-20 13:06:21 +0100

    LAWS-3284: added zone association

--- terraform/environments/portal/route53.tf
+++ terraform/environments/portal/route53.tf
@@ -10,3 +10,9 @@ resource "aws_route53_vpc_association_authorization" "vpc_zone_association_auth"
   vpc_id    = data.aws_vpc.shared.id
   zone_id   = aws_route53_zone.private.id
 }
+
+resource "aws_route53_zone_association" "zone_association" {
+  provider = aws.core-vpc
+  vpc_id  = aws_route53_vpc_association_authorization.vpc_zone_association_auth.vpc_id
+  zone_id = aws_route53_vpc_association_authorization.vpc_zone_association_auth.zone_id
+}
