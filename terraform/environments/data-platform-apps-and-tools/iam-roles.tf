462e60eae4d06a5750149b3cf23184a045d5f69c
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-20 14:01:07 +0000

    comment out all resources create route 53 zone

--- terraform/environments/data-platform-apps-and-tools/iam-roles.tf
+++ terraform/environments/data-platform-apps-and-tools/iam-roles.tf
@@ -1,25 +1,23 @@
-/*
 ##################################################
 # Airflow
 ##################################################
 
-module "airflow_execution_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "5.20.0"
+# module "airflow_execution_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+#   version = "5.20.0"
 
-  create_role       = true
-  role_name         = "data-platform-airflow-${local.environment}-execution-role"
-  role_requires_mfa = false
+#   create_role       = true
+#   role_name         = "data-platform-airflow-${local.environment}-execution-role"
+#   role_requires_mfa = false
 
-  trusted_role_services = [
-    "airflow.amazonaws.com",
-    "airflow-env.amazonaws.com"
-  ]
+#   trusted_role_services = [
+#     "airflow.amazonaws.com",
+#     "airflow-env.amazonaws.com"
+#   ]
 
-  custom_role_policy_arns = [
-    module.airflow_execution_policy.arn
-  ]
+#   custom_role_policy_arns = [
+#     module.airflow_execution_policy.arn
+#   ]
 
-  tags = local.tags
-}
-*/
+#   tags = local.tags
+# }
---
+++ terraform/environments/data-platform-apps-and-tools/route53.tf
@@ -0,0 +1,3 @@
+resource "aws_route53_zone" "apps_tools" {
+  name = local.route53_zone
+}
