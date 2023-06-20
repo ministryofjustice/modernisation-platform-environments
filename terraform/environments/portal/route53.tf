553976ccbecff3ff561a5e05beeaf12052759c2a
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-06-20 11:41:10 +0100

    LAWS-3284: added brackets to core-vpc id

--- terraform/environments/portal/route53.tf
+++ terraform/environments/portal/route53.tf
@@ -6,6 +6,6 @@ resource "aws_route53_zone" "private" {
 }
 
 resource "aws_route53_vpc_association_authorization" "vpc_zone_association_auth" {
-  vpc_id  = aws_vpc.core-vpc.id
+  vpc_id  = [aws_vpc.core-vpc.id]
   zone_id = aws_route53_zone.private.id
 }
