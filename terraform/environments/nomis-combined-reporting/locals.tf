a2073237b182aea113ef87e93f641cc445ff75a5
Jonathan Quinn <jonathan.quinn@digital.justice.gov.uk>
2023-06-21 16:01:50 +0100

    refactor locals

--- terraform/environments/nomis-combined-reporting/locals.tf
+++ terraform/environments/nomis-combined-reporting/locals.tf
@@ -1,15 +1,44 @@
-#### This file can be used to store locals specific to the member account ####
-
 locals {
   business_unit = var.networking[0].business-unit
-
+  region        = "eu-west-2"
   environment_configs = {
     development   = local.development_config
     test          = local.test_config
     preproduction = local.preproduction_config
     production    = local.production_config
   }
-  environment_config        = local.environment_configs[local.environment]
-  baseline_acm_certificates = {}
-  baseline_route53_zones    = {}
+  baseline_preset_options = {
+    enable_application_environment_wildcard_cert = false
+    enable_business_unit_kms_cmks                = true
+    enable_image_builder                         = true
+    enable_ec2_cloud_watch_agent                 = true
+    enable_ec2_self_provision                    = true
+    iam_policies_filter                          = ["ImageBuilderS3BucketWriteAndDeleteAccessPolicy"]
+    iam_policies_ec2_default                     = ["EC2S3BucketWriteAndDeleteAccessPolicy", "ImageBuilderS3BucketWriteAndDeleteAccessPolicy"]
+    s3_iam_policies                              = ["EC2S3BucketWriteAndDeleteAccessPolicy"]
+
+    # comment this in if you need to resolve FixNGo hostnames
+    # route53_resolver_rules = {
+    #   outbound-data-and-private-subnets = ["azure-fixngo-domain"]
+    # }
+  }
+  baseline_acm_certificates         = {}
+  baseline_cloudwatch_log_groups    = {}
+  baseline_ec2_autoscaling_groups   = {}
+  baseline_ec2_instances            = {}
+  baseline_iam_policies             = {}
+  baseline_iam_roles                = {}
+  baseline_iam_service_linked_roles = {}
+  baseline_key_pairs                = {}
+  baseline_kms_grants               = {}
+  baseline_lbs                      = {}
+  baseline_rds_instances            = {}
+  baseline_route53_resolvers        = {}
+  baseline_route53_zones            = {}
+  baseline_s3_buckets = {
+    s3-bucket = {
+      iam_policies = module.baseline_presets.s3_iam_policies
+    }
+  }
+  environment_config = local.environment_configs[local.environment]
 }
