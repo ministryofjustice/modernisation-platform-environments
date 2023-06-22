f1e635710c074936034559d59ed29a330df2bd6a
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-06-22 12:00:33 +0100

    Updating mounting part of user data of OIM without rebuild

--- terraform/environments/portal/oim_ec2.tf
+++ terraform/environments/portal/oim_ec2.tf
@@ -10,6 +10,14 @@ echo "${aws_efs_file_system.product["oim"].dns_name}:/runtime/Domain/config /IDA
 echo "/dev/xvde /IDAM/product/runtime/Domain/mserver ext4 defaults 0 0" >> /etc/fstab
 echo "${aws_efs_file_system.efs.dns_name}:/ /IDMLCM/repo_home nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
 mount -a
+mount_status=$?
+while [[ $mount_status != 0 ]]
+do
+  sleep 10
+  mount -a
+  mount_status=$?
+done
+
 hostnamectl set-hostname ${local.application_name}-oim1-ms.${local.portal_hosted_zone}
 
 # Setting up CloudWatch Agent
@@ -125,7 +133,7 @@ resource "aws_instance" "oim_instance_1" {
   subnet_id                   = data.aws_subnet.data_subnets_a.id
   iam_instance_profile        = aws_iam_instance_profile.portal.id
   user_data_base64            = base64encode(local.oim_1_userdata)
-  user_data_replace_on_change = true
+  # user_data_replace_on_change = true
 
   # root_block_device {
   #   delete_on_termination      = false
