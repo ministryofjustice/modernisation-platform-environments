24b1e6272438aef9edbbdb6b7d90b7986f1982eb
umeshc-roy <umeshc.roy@gmail.com>
2023-06-23 11:02:12 +0100

    restore023

--- terraform/environments/ppud/instances.tf
+++ terraform/environments/ppud/instances.tf
@@ -211,7 +211,7 @@ resource "aws_instance" "s618358rgvw024" {
 
 resource "aws_instance" "s618358rgvw023" {
   count                  = local.is-preproduction == true ? 1 : 0
-  ami                    = "ami-04944a7de56185ec3"
+  ami                    = "ami-0f073b401ba3f1cff"
   instance_type          = "c5.large"
   source_dest_check      = false
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
