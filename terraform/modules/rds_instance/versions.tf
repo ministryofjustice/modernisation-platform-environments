9011373d77e51560643a9c0d3a89d520f00bdd39
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-06-27 14:57:29 +0100

    Nomis changes as well as module changes in repo for v5 upgrade.

--- terraform/modules/rds_instance/versions.tf
+++ terraform/modules/rds_instance/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      version               = "~> 4.9"
+      version               = "~> 5.0"
       source                = "hashicorp/aws"
       configuration_aliases = [aws.core-vpc]
     }
