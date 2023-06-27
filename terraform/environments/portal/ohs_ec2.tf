2315e09cbd8d1bd6b428d9d0df0e530c29649b38
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-06-27 16:56:03 +0100

    removing rogue ohs userdata line

--- terraform/environments/portal/ohs_ec2.tf
+++ terraform/environments/portal/ohs_ec2.tf
@@ -5,7 +5,6 @@ locals {
   ohs_1_userdata = <<EOF
 #!/bin/bash
 echo "${aws_efs_file_system.product["ohs"].dns_name}:/fmw /IDAM/product/fmw nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
-file_system_id.efs.aws-region.amazonaws.com:/ mount_point nfs4
 echo "/dev/xvdc /IDAM/product/runtime/Domain/mserver ext4 defaults 0 0" >> /etc/fstab
 echo "${aws_efs_file_system.efs.dns_name}:/ /IDMLCM/repo_home nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
 mount -a
