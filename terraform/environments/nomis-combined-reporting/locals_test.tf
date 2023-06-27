0368242330a52f6473919f30184ff4355df654ea
Craig Gilbertson <craig.gilbertson@digital.justice.gov.uk>
2023-06-27 15:34:17 +0100

    resize

--- terraform/environments/nomis-combined-reporting/locals_test.tf
+++ terraform/environments/nomis-combined-reporting/locals_test.tf
@@ -40,6 +40,7 @@ locals {
           ami_name = "base_rhel_8_5_*"
         })
         instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          instance_type          = "t3.large",
           vpc_security_group_ids = ["private"]
         })
         ebs_volumes = {
@@ -86,6 +87,7 @@ locals {
           ami_name = "base_rhel_8_5_*"
         })
         instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          instance_type          = "t3.large",
           vpc_security_group_ids = ["private"]
         })
         ebs_volumes = {
