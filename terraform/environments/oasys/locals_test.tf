ace9df62771b422f1179b97303be3a292d00d811
W <willg258@hotmail.co.uk>
2023-06-27 13:15:31 +0100

    Update locals_test.tf

--- terraform/environments/oasys/locals_test.tf
+++ terraform/environments/oasys/locals_test.tf
@@ -14,7 +14,7 @@ locals {
       "t2-${local.application_name}-db-a" = local.database_a
       "t2-${local.application_name}-db-b" = merge(local.database_b, {
         user_data_cloud_init  = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_ansible_no_tags, {
-          args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_ansible_no_tags, {
+          args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_ansible_no_tags.args, {
             branch = "oasys/oracle-19c-disk-sector-size-512-change"
           })
         })
