00d727b892ae0b0c81516eafaa2f919a3ce85521
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-27 09:38:11 +0000

    Adding S3 and SES

--- terraform/environments/data-platform-apps-and-tools/data.tf
+++ terraform/environments/data-platform-apps-and-tools/data.tf
@@ -2,38 +2,38 @@
 # Data Platform Apps and Tools VPC
 ##################################################
 
-data "aws_vpc" "data_platform_apps_and_tools" {
-  filter {
-    name   = "tag:Name"
-    values = ["data-platform-apps-and-tools-${local.environment}"]
-  }
-}
+# data "aws_vpc" "data_platform_apps_and_tools" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["data-platform-apps-and-tools-${local.environment}"]
+#   }
+# }
 
 ##################################################
 # Data Platform Apps and Tools Subnets
 ##################################################
 
-data "aws_subnets" "data_platform_apps_and_tools_private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.data_platform_apps_and_tools.id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = ["data-platform-apps-and-tools-${local.environment}-private-*"]
-  }
-}
+# data "aws_subnets" "data_platform_apps_and_tools_private" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.data_platform_apps_and_tools.id]
+#   }
+#   filter {
+#     name   = "tag:Name"
+#     values = ["data-platform-apps-and-tools-${local.environment}-private-*"]
+#   }
+# }
 
-data "aws_subnets" "data_platform_apps_and_tools_public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.data_platform_apps_and_tools.id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = ["data-platform-apps-and-tools-${local.environment}-public-*"]
-  }
-}
+# data "aws_subnets" "data_platform_apps_and_tools_public" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.data_platform_apps_and_tools.id]
+#   }
+#   filter {
+#     name   = "tag:Name"
+#     values = ["data-platform-apps-and-tools-${local.environment}-public-*"]
+#   }
+# }
 
 ##################################################
 # Airflow SES
