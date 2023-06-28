0bfbd5a34aaac47edff534092fa6542265a637b1
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-06-28 15:13:47 +0100

    DPR-356-domain-builder-bootstrap DPR-356 reinstate original script

--- terraform/environments/digital-prison-reporting/modules/compute_node/scripts/domain-builder.sh
+++ terraform/environments/digital-prison-reporting/modules/compute_node/scripts/domain-builder.sh
@@ -20,36 +20,12 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 ./aws/install
 
-# Create home directory with dir structure for domain builder jars
-sudo mkdir -p /home/ssm-user/domain-builder/jars
-sudo chown -R ssm-user /home/ssm-user
-chmod -R 0777 /home/ssm-user/domain-builder
+# Set Env Configuration
+sudo mkdir -p /home/ssm-user/domain-builder/jars; chmod -R 0777 /home/ssm-user/domain-builder
 cd /home/ssm-user
+sudo chown -R ssm-user domain-builder
 
 # Sync S3 Domain Builder Artifacts
 aws s3 cp s3://dpr-artifact-store-development/build-artifacts/domain-builder/jars/domain-builder-cli-frontend-vLatest-all.jar /home/ssm-user/domain-builder/jars
-
-# Location of script that will be used to launch the domain builder jar.
-launcher_script_location=/usr/bin/domain-builder
-
-# TODO - disabled until we have an endpoint configured for the function.
-# Get the configured function url...
-# function_url=$(aws lambda get-function-url-config --function-name dpr-domain-builder-backend-api-function --output table | tr -d ' ' | grep '|FunctionUrl|' | cut -d '|' -f3 )
-# ...and remove the trailing slash using vanilla bash parameter expansion.
-# domain_builder_url=${function_url%?}
-domain_builder_url="http://localhost:8080"
-
-# Generate a launcher script for the jar that starts domain-builder in interactive mode
-# and configured to use the function URL via the DOMAIN_API_URL environment variable.
-#cat <<EOF > $launcher_script_location
-##!/bin/bash
-#
-#cd /home/ssm-user
-#
-#DOMAIN_API_URL="$domain_builder_url" java -jar domain-builder/jars/domain-builder-cli-vLatest-all.jar -i --enable-ansi
-#
-#EOF
-#
-#chmod 0755 $launcher_script_location
 
 echo "Bootstrap Complete"
