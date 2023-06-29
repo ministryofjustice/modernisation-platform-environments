5b952b48b68c8ac03c10c40aa24797f7494d3e6f
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-06-29 15:35:25 +0100

    add kubectl to libs

--- terraform/environments/digital-prison-reporting/modules/ec2/scripts/bootstrap_apps.sh
+++ terraform/environments/digital-prison-reporting/modules/ec2/scripts/bootstrap_apps.sh
@@ -41,7 +41,7 @@ unzip awscliv2.zip
 ./aws/install
 
 # Install KUBECTL Libs
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/darwin/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 cp ./kubectl /usr/bin/kubectl
 chmod +x /usr/bin/kubectl
