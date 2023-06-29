255ab571175b97fd469ca6b5c2dc172b5ac1ea55
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-06-29 17:08:17 +0100

    enable loggin

--- terraform/environments/portal/idm_int_clb.tf
+++ terraform/environments/portal/idm_int_clb.tf
@@ -1,6 +1,7 @@
 resource "aws_elb" "idm_lb" {
 name        = "${local.application_name}-internal-lb-idm"
 internal                   = true
+idle_timeout               = 3600
 security_groups            = [aws_security_group.internal_idm_sg.id]
 subnets                    = [data.aws_subnet.private_subnets_a.id, data.aws_subnet.private_subnets_b.id, data.aws_subnet.private_subnets_c.id]
 
@@ -25,7 +26,7 @@ listener {
     health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 10
-    timeout             = 3
+    timeout             = 5
     target              = "TCP:1389"
     interval            = 15
   }
@@ -60,7 +61,7 @@ resource "aws_security_group" "internal_idm_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "internal_inbound" {
   security_group_id = aws_security_group.internal_idm_sg.id
-  cidr_ipv4         = local.laa_mp_dev_cidr
+  cidr_ipv4         = data.aws_vpc.shared.cidr_block
   from_port         = 1389
   ip_protocol       = "tcp"
   to_port           = 1389
@@ -69,8 +70,127 @@ resource "aws_vpc_security_group_ingress_rule" "internal_inbound" {
 
 resource "aws_vpc_security_group_ingress_rule" "internal_inbound1" {
   security_group_id = aws_security_group.internal_idm_sg.id
-  cidr_ipv4         = local.laa_mp_dev_cidr
+  cidr_ipv4         = data.aws_vpc.shared.cidr_block
   from_port         = 1636
   ip_protocol       = "tcp"
   to_port           = 1636
 }
+
+
+
+####################################
+# ELB Access Logging
+####################################
+
+module "elb-logs-s3" {
+  count  = local.lb_logs_bucket == "" ? 1 : 0
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v6.4.0"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  bucket_prefix       = "${local.application_name}-lb-access-logs"
+  bucket_policy       = [data.aws_iam_policy_document.bucket_policy.json]
+  replication_enabled = false
+  versioning_enabled  = true
+  force_destroy       = local.force_destroy_lb_logs_bucket
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [local.lb_logs_bucket != "" ? "arn:aws:s3:::${local.lb_logs_bucket}/*" : "${module.elb-logs-s3[0].bucket.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_elb_service_account.default.arn]
+    }
+  }
+  statement {
+    sid = "AWSLogDeliveryWrite"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [local.lb_logs_bucket != "" ? "arn:aws:s3:::${local.lb_logs_bucket}/*" : "${module.elb-logs-s3[0].bucket.arn}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "bucket-owner-full-control"
+      ]
+    }
+  }
+
+  statement {
+    sid = "AWSLogDeliveryAclCheck"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+
+    resources = [
+      local.lb_logs_bucket != "" ? "arn:aws:s3:::${local.lb_logs_bucket}" : module.elb-logs-s3[0].bucket.arn
+    ]
+  }
+}
+
+data "aws_elb_service_account" "default" {}
