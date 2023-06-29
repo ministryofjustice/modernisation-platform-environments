928c0ae48eaed1158a523735e7ee228e1ef5fc84
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-06-29 12:21:52 +0100

    remove instance

--- terraform/environments/portal/idm_int_clb.tf
+++ terraform/environments/portal/idm_int_clb.tf
@@ -4,8 +4,7 @@ internal                   = true
 security_groups            = [aws_security_group.internal_idm_sg.id]
 subnets                    = [data.aws_subnet.public_subnets_a.id, data.aws_subnet.public_subnets_b.id, data.aws_subnet.public_subnets_c.id]
 #count                      = contains(["development", "testing"], local.environment) ? 0 : 1
-instances                  = [aws_instance.idm_instance_1.id]
-
+#instances                  = [aws_instance.idm_instance_1.id]
 
 
 listener {
@@ -15,6 +14,22 @@ listener {
     lb_protocol       = "TCP"
   }
 
+
+listener {
+    instance_port     = 1636
+    instance_protocol = "TCP"
+    lb_port           = 1636
+    lb_protocol       = "TCP"
+  }
+
+
+    health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    timeout             = 3
+    target              = "TCP:1389"
+    interval            = 15
+  }
   tags = merge(
     local.tags,
     {
