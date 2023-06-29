8fe23344e9c61828e91d07c8e4be4457db572827
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-29 22:11:22 +0000

    Add IAM policy and role for GitHub Actions

---
+++ terraform/environments/data-platform/iam-policies.tf
@@ -0,0 +1,29 @@
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    sid    = "AllowECR"
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+    ]
+    resources = ["*"]
+  }
+}
+
+module "github_actions_iam_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.22.0"
+
+  name = "${local.application_name}-github-actions"
+
+  policy = data.aws_iam_policy_document.github_actions.json
+
+  tags = local.tags
+}
