2af14b755751b8f35e662a54fbe1e4e22b04d3d0
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-06-29 12:00:43 +0100

    reduce code for lb creation

--- terraform/environments/portal/idm_int_clb.tf
+++ terraform/environments/portal/idm_int_clb.tf
@@ -1,11 +1,9 @@
-
 resource "aws_elb" "idm_lb" {
-name                       = "${local.application_name}-internal-lb"
+name        = "${local.application_name}-internal-lb"
 internal                   = true
 security_groups            = [aws_security_group.internal_idm_sg.id]
 subnets                    = [data.aws_subnet.public_subnets_a.id, data.aws_subnet.public_subnets_b.id, data.aws_subnet.public_subnets_c.id]
-count                      = contains(["development", "testing"], local.environment) ? 0 : 1
-instances                  = [aws_instance.idm_instance_1.id, aws_instance.idm_instance_2[0].id]
+
 
 listener {
     instance_port     = 1389
@@ -14,32 +12,13 @@ listener {
     lb_protocol       = "TCP"
   }
 
-
-listener {
-    instance_port     = 1636
-    instance_protocol = "TCP"
-    lb_port           = 1636
-    lb_protocol       = "TCP"
-  }
-
-
-    health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 10
-    timeout             = 3
-    target              = "TCP:1389"
-    interval            = 15
-  }
-
-tags = merge(
+  tags = merge(
     local.tags,
     {
       Name = "${local.application_name}-internal-lb"
-    },
+    }
   )
 }
-
-
 
 resource "aws_security_group" "internal_idm_sg" {
   name        = "${local.application_name}-internal-clb-security-group"
@@ -63,5 +42,3 @@ resource "aws_vpc_security_group_ingress_rule" "internal_inbound1" {
   ip_protocol       = "tcp"
   to_port           = 1636
 }
-
-
