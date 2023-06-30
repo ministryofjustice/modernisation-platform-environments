57c4d3009bff2ff7c4da5253c12049b9c49549eb
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-30 10:31:44 +0000

    Update GitHub Actions OIDC role name

--- terraform/environments/data-platform/iam-roles.tf
+++ terraform/environments/data-platform/iam-roles.tf
@@ -2,7 +2,7 @@ module "github_actions_iam_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
   version = "5.22.0"
 
-  name = "${local.application_name}-github-actions"
+  name = "${local.application_name}-gha"
 
   subjects = ["ministryofjustice/data-platform:*"]
 
