31d3c2ec58c84fbcd71629c266fe5304ac9fe7c6
W <willg258@hotmail.co.uk>
2023-07-04 15:45:52 +0100

    oasys dev db instance
    oasys dev db instance

--- terraform/environments/oasys/locals_test.tf
+++ terraform/environments/oasys/locals_test.tf
@@ -160,7 +160,7 @@ locals {
       "${local.application_name}.service.justice.gov.uk" = {
         lb_alias_records = [
           { name = "t2", type = "A", lbs_map_key = "public" },    # t2.oasys.service.justice.gov.uk # need to add an ns record to oasys.service.justice.gov.uk -> t2, 
-          # { name = "db.t2", type = "A", lbs_map_key = "public" },  # db.t2.oasys.service.justice.gov.uk currently pointing to azure db T2ODL0009
+          { name = "db.t2", type = "A", lbs_map_key = "private" },  # db.t2.oasys.service.justice.gov.uk currently pointing to azure db T2ODL0009
         ]
       }
       # "t1.${local.application_name}.service.justice.gov.uk" = {
