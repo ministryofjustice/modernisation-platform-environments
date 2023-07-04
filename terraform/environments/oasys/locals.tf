429800bac78ee75c67da05b47ce4ef891f9c9431
James Borwick <james.borwick@icloud.com>
2023-07-04 15:00:42 +0100

    Pinning AMI

--- terraform/environments/oasys/locals.tf
+++ terraform/environments/oasys/locals.tf
@@ -125,7 +125,8 @@ locals {
 
   database_a = {
     config = merge(module.baseline_presets.ec2_instance.config.db, {
-      ami_name          = "oasys_oracle_db_*"
+      # ami_name          = "oasys_oracle_db_*"
+      ami_name          = "oasys_oracle_db_release_2023-06-26T10-16-03.670Z"
       ami_owner         = "self"
       availability_zone = "${local.region}a"
     })
