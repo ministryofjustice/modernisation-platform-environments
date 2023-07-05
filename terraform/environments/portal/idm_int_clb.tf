bc7dd61612c3f772fcbcd06d34f0bb6922089471
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-07-05 12:06:32 +0100

    add outbound rule

--- terraform/environments/portal/idm_int_clb.tf
+++ terraform/environments/portal/idm_int_clb.tf
@@ -83,5 +83,9 @@ resource "aws_vpc_security_group_ingress_rule" "internal_inbound1" {
 }
 
 
-
+resource "aws_vpc_security_group_egress_rule" "internal_lb_outbound" {
+  security_group_id = aws_security_group.internal_idm_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
 
