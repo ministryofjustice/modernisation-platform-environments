b2a93360d9f75d58aa7b114b8998afca6b6b21df
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-07-05 10:23:21 +0100

    Comment out snapshot identifier

--- terraform/environments/mojfin/rds.tf
+++ terraform/environments/mojfin/rds.tf
@@ -171,7 +171,7 @@ resource "aws_db_instance" "appdb1" {
   copy_tags_to_snapshot           = true
   storage_encrypted               = true
   apply_immediately               = true
-  snapshot_identifier             = format("arn:aws:rds:eu-west-2:%s:snapshot:%s", data.aws_caller_identity.current.account_id,local.application_data.accounts[local.environment].mojfinrdssnapshotid)
+  # snapshot_identifier             = format("arn:aws:rds:eu-west-2:%s:snapshot:%s", data.aws_caller_identity.current.account_id,local.application_data.accounts[local.environment].mojfinrdssnapshotid)
   kms_key_id = data.aws_kms_key.rds_shared.arn
 
   # restore_to_point_in_time {
