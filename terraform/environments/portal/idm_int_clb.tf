06c390f71b77715c7bd78225b4f829723657e908
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-07-05 12:37:52 +0100

    amend variable

--- terraform/environments/portal/idm_int_clb.tf
+++ terraform/environments/portal/idm_int_clb.tf
@@ -83,7 +83,7 @@ resource "aws_vpc_security_group_ingress_rule" "internal_inbound1" {
 }
 
 
-resource "aws_vpc_security_group_egress_rule" "internal_lb_outbound" {
+resource "aws_vpc_security_group_egress_rule" "internal_idm_lb_outbound" {
   security_group_id = aws_security_group.internal_idm_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
