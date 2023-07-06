7a9e195f0e58df375e6c5e95fb031e40a86a9802
W <willg258@hotmail.co.uk>
2023-07-06 17:02:55 +0100

    Update locals_security_groups.tf

--- terraform/environments/oasys/locals_security_groups.tf
+++ terraform/environments/oasys/locals_security_groups.tf
@@ -105,22 +105,20 @@ locals {
           protocol    = "tcp"
           # no security groups on an NLB so need to put public and private on the internal ALB
           cidr_blocks = flatten([
-            "0.0.0.0/0",
             local.security_group_cidrs.https_internal,
           ])
-          #security_groups = ["private", "private_lb"]
+          security_groups = ["private", "private_lb"]
         }
         https = {
           description = "Allow HTTPS ingress"
-          from_port   = 443
+          from_port   = 0
           to_port     = 443
           protocol    = "tcp"
           # no security groups on an NLB so need to put public and private on the internal ALB
           cidr_blocks = flatten([
             local.security_group_cidrs.https_internal,
-            "0.0.0.0/0",
           ])
-          #security_groups = ["private", "private_lb"]
+          security_groups = ["private", "private_lb"]
         }
       }
       egress = {
@@ -290,32 +288,32 @@ locals {
           from_port       = 80
           to_port         = 80
           protocol        = "tcp"
-          security_groups = ["private_lb"]
+          security_groups = ["private_lb_internal"]
           cidr_blocks = flatten([
             local.security_group_cidrs.https_internal,
           ])
         }
         http8080 = {
           description = "Allow http8080 ingress"
-          from_port   = 0
+          from_port   = 8080
           to_port     = 8080
           protocol    = "tcp"
           # no security groups on an NLB so need to put public and private on the internal ALB
           cidr_blocks = flatten([
             local.security_group_cidrs.https_internal,
           ])
-          security_groups = ["private_lb"]
+          security_groups = ["private_lb_internal"]
         }
         https = {
           description = "Allow HTTPS ingress"
-          from_port   = 0
+          from_port   = 443
           to_port     = 443
           protocol    = "tcp"
           # no security groups on an NLB so need to put public and private on the internal ALB
           cidr_blocks = flatten([
             local.security_group_cidrs.https_internal,
           ])
-          security_groups = ["private_lb"]
+          security_groups = ["private_lb_internal"]
         }
       }
       egress = {
