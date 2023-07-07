e1c69dbb7c17f9271f65497fb9bd7b90bd541fea
Craig Gilbertson <craig.gilbertson@digital.justice.gov.uk>
2023-07-07 15:30:06 +0100

    DSOS-1997-added-ssm-parameters

--- terraform/environments/nomis-combined-reporting/locals_test.tf
+++ terraform/environments/nomis-combined-reporting/locals_test.tf
@@ -62,6 +62,13 @@ locals {
             }
             description = "CMS password for connection to BI Platform"
           }
+          product-key = {
+            random = {
+              length  = 32
+              special = false
+            }
+            description = "Product key for BI Platform"
+          }
         }
         tags = {
           description = "For testing SAP tomcat installation"
@@ -153,53 +160,62 @@ locals {
         }
       }
 
-      bip-application-server = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name = "base_rhel_8_5_*"
-        })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          instance_type          = "t3.large",
-          vpc_security_group_ids = ["private"]
-        })
-        ebs_volumes = {
-          "/dev/sdb" = { type = "gp3", size = 100 }
-          "/dev/sds" = { type = "gp3", size = 100 }
-        }
-        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-        autoscaling_group = {
-          desired_capacity    = 1
-          max_size            = 2
-          vpc_zone_identifier = module.environment.subnets["private"].ids
-        }
-        # autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
-        tags = {
-          description = "For testing BIP 4.3 installation and connections with official RedHat RHEL8.5 image"
-          os-type     = "Linux"
-          component   = "test"
-        }
-        lb_target_groups = {
-          http = {
-            port                 = 7777
-            protocol             = "HTTP"
-            target_type          = "instance"
-            deregistration_delay = 30
-            health_check = {
-              enabled             = true
-              interval            = 30
-              healthy_threshold   = 3
-              matcher             = "200-399"
-              path                = "/"
-              port                = 7777
-              timeout             = 5
-              unhealthy_threshold = 5
-            }
-            stickiness = {
-              enabled = true
-              type    = "lb_cookie"
-            }
-          }
-        }
-      }
+    #   bi-plarform = {
+    #     config = merge(module.baseline_presets.ec2_instance.config.default, {
+    #       ami_name = "base_rhel_8_5_*"
+    #     })
+    #     instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+    #       instance_type          = "t3.large",
+    #       vpc_security_group_ids = ["private"]
+    #     })
+    #     ebs_volumes = {
+    #       "/dev/sdb" = { type = "gp3", size = 100 }
+    #       "/dev/sds" = { type = "gp3", size = 100 }
+    #     }
+    #     user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+    #     autoscaling_group = {
+    #       desired_capacity    = 1
+    #       max_size            = 2
+    #       vpc_zone_identifier = module.environment.subnets["private"].ids
+    #     }
+    #     # autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+    #     ssm_parameters = {
+    #       product-key = {
+    #         random = {
+    #           length  = 32
+    #           special = false
+    #         }
+    #         description = "Product key for BI Platform"
+    #       }
+    #     }
+    #     tags = {
+    #       description = "For testing BIP 4.3 installation and connections with official RedHat RHEL8.5 image"
+    #       os-type     = "Linux"
+    #       component   = "test"
+    #     }
+    #     lb_target_groups = {
+    #       http = {
+    #         port                 = 7777
+    #         protocol             = "HTTP"
+    #         target_type          = "instance"
+    #         deregistration_delay = 30
+    #         health_check = {
+    #           enabled             = true
+    #           interval            = 30
+    #           healthy_threshold   = 3
+    #           matcher             = "200-399"
+    #           path                = "/"
+    #           port                = 7777
+    #           timeout             = 5
+    #           unhealthy_threshold = 5
+    #         }
+    #         stickiness = {
+    #           enabled = true
+    #           type    = "lb_cookie"
+    #         }
+    #       }
+    #     }
+    #   }
     }
     baseline_lbs = {
       private = {
