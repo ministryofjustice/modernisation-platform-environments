e0575cc8462fabeb8bed7ffb259fb53619512704
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-07-07 11:59:55 +0100

    Adjust Description

--- terraform/environments/digital-prison-reporting/modules/dms/variables.tf
+++ terraform/environments/digital-prison-reporting/modules/dms/variables.tf
@@ -3,7 +3,7 @@ variable "name" {
 }
 
 variable "enable_replication_task" {
-  description = "Enable DMS Replication Task, Yes or NO" 
+  description = "Enable DMS Replication Task, True or False" 
   type        = bool
   default     = false
 }
