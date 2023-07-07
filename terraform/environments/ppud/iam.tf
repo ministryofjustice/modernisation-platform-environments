a7249a89c4d0b6bd31859e7c13e1a328886d53bc
umeshc-roy <umeshc.roy@gmail.com>
2023-07-07 16:44:43 +0100

    S3 access to Ec2-1

--- terraform/environments/ppud/iam.tf
+++ terraform/environments/ppud/iam.tf
@@ -36,7 +36,7 @@ resource "aws_iam_policy_attachment" "ec2_attach2" {
   roles      = [aws_iam_role.ec2_iam_role.id]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
-resource "aws_iam_policy_attachment" "ec2_attach2" {
+resource "aws_iam_policy_attachment" "ec2_attach3" {
   name       = "ec2-iam-attachment"
   roles      = [aws_iam_role.ec2_iam_role.id]
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
