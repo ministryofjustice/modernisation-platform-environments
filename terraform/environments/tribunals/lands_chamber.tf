256123027a5ba1581c12cfe2aad3b2d56f3462c5
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-07-12 15:17:31 +0100

    resource names changed in lands chamber file

--- terraform/environments/tribunals/lands_chamber.tf
+++ terraform/environments/tribunals/lands_chamber.tf
@@ -1,58 +1,59 @@
 locals {
-  application = "lands" 
-  app_db_name = "lands" 
-  app_db_login_name = "lands-app"
-  source_db_name = "Lands"
-  rds_url               = "${aws_db_instance.rdsdb.address}"      
-  rds_user              = jsondecode(data.aws_secretsmanager_secret_version.data_rds_secret_current.secret_string)["username"]
-  rds_port              = "1433"
-  rds_password          = jsondecode(data.aws_secretsmanager_secret_version.data_rds_secret_current.secret_string)["password"]
-  source_db_url         = jsondecode(data.aws_secretsmanager_secret_version.source_db_secret_current.secret_string)["host"]
-  source_db_user        = jsondecode(data.aws_secretsmanager_secret_version.source_db_secret_current.secret_string)["username"]
-  source_db_password    = jsondecode(data.aws_secretsmanager_secret_version.source_db_secret_current.secret_string)["password"]
+  lands = "lands" 
+  lands_folder = "lands_chamber"
+  lands_db_name = "lands" 
+  lands_db_login_name = "lands-app"
+  lands_source_db_name = "Lands"
+  lands_rds_url               = "${aws_db_instance.rdsdb.address}"      
+  lands_rds_user              = jsondecode(data.aws_secretsmanager_secret_version.data_rds_secret_current.secret_string)["username"]
+  lands_rds_port              = "1433"
+  lands_rds_password          = jsondecode(data.aws_secretsmanager_secret_version.data_rds_secret_current.secret_string)["password"]
+  lands_source_db_url         = jsondecode(data.aws_secretsmanager_secret_version.source_db_secret_current.secret_string)["host"]
+  lands_source_db_user        = jsondecode(data.aws_secretsmanager_secret_version.source_db_secret_current.secret_string)["username"]
+  lands_source_db_password    = jsondecode(data.aws_secretsmanager_secret_version.source_db_secret_current.secret_string)["password"]
 }
 
 ######################## DMS #############################################
 
-module "dms" {
+module "lands_dms" {
   source                      = "./modules/dms"
   replication_instance_arn    = aws_dms_replication_instance.tribunals_replication_instance.replication_instance_arn
-  replication_task_id         = "${local.application}-migration-task"
+  replication_task_id         = "${local.lands}-migration-task"
   #target_db_instance          = 0
-  target_endpoint_id          = "${local.application}-target"
-  target_database_name        = local.app_db_name
-  target_server_name          = local.rds_url
-  target_username             = local.rds_user
-  target_password             = local.rds_password
-  source_endpoint_id          = "${local.application}-source"
-  source_database_name        = local.source_db_name
-  source_server_name          = local.source_db_url
-  source_username             = local.source_db_user
-  source_password             = local.source_db_password
+  target_endpoint_id          = "${local.lands}-target"
+  target_database_name        = local.lands_db_name
+  target_server_name          = local.lands_rds_url
+  target_username             = local.lands_rds_user
+  target_password             = local.lands_rds_password
+  source_endpoint_id          = "${local.lands}-source"
+  source_database_name        = local.lands_source_db_name
+  source_server_name          = local.lands_source_db_url
+  source_username             = local.lands_source_db_user
+  source_password             = local.lands_source_db_password
  
 }
 
 ############################################################################
 
-resource "random_password" "new_password" {
+resource "random_password" "lands_new_password" {
   length  = 16
   special = false 
 }
 
-resource "null_resource" "setup_db" {
+resource "null_resource" "lands_setup_db" {
  
   provisioner "local-exec" {
     interpreter = ["bash", "-c"]
     command     = "ifconfig -a; chmod +x ./setup-mssql.sh; ./setup-mssql.sh"
 
     environment = {
-      DB_URL = local.rds_url   
-      USER_NAME = local.rds_user
-      PASSWORD = local.rds_password
-      NEW_DB_NAME = local.app_db_name
-      NEW_USER_NAME = local.app_db_login_name
-      NEW_PASSWORD = random_password.new_password.result
-      APP_FOLDER = local.application
+      DB_URL = local.lands_rds_url   
+      USER_NAME = local.lands_rds_user
+      PASSWORD = local.lands_rds_password
+      NEW_DB_NAME = local.lands_db_name
+      NEW_USER_NAME = local.lands_db_login_name
+      NEW_PASSWORD = random_password.lands_new_password.result
+      APP_FOLDER = local.lands_folder
     }
   }
   triggers = {
@@ -60,18 +61,18 @@ resource "null_resource" "setup_db" {
   }
 }
 
- resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "${local.application}-credentials"
+ resource "aws_secretsmanager_secret" "lands_db_credentials" {
+  name = "${local.lands}-credentials"
 }
 
-resource "aws_secretsmanager_secret_version" "db_credentials_version" {
-  secret_id     = aws_secretsmanager_secret.db_credentials.id
+resource "aws_secretsmanager_secret_version" "lands_db_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.lands_db_credentials.id
   secret_string = <<EOF
 {
-  "username": "${local.app_db_login_name}",
-  "password": "${random_password.new_password.result}",  
-  "host": "${local.rds_url}",  
-  "database_name": "${local.app_db_name}"
+  "username": "${local.lands_db_login_name}",
+  "password": "${random_password.lands_new_password.result}",  
+  "host": "${local.lands_rds_url}",  
+  "database_name": "${local.lands_db_name}"
 }
 EOF
 }
@@ -79,11 +80,11 @@ EOF
 ####################### DNS #########################################
 
 // ACM Public Certificate
-resource "aws_acm_certificate" "external" {
+resource "aws_acm_certificate" "lands_external" {
   domain_name       = "modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
-  subject_alternative_names = ["${local.application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  subject_alternative_names = ["${local.lands}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   tags = {
     Environment = local.environment
   }
@@ -93,13 +94,13 @@ resource "aws_acm_certificate" "external" {
   }
 }
 
-resource "aws_acm_certificate_validation" "external" {
-  certificate_arn         = aws_acm_certificate.external.arn
+resource "aws_acm_certificate_validation" "lands_external" {
+  certificate_arn         = aws_acm_certificate.lands_external.arn
   validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
 }
 
 // Route53 DNS records for certificate validation
-resource "aws_route53_record" "external_validation" {
+resource "aws_route53_record" "lands_external_validation" {
   provider = aws.core-network-services
 
   allow_overwrite = true
@@ -110,7 +111,7 @@ resource "aws_route53_record" "external_validation" {
   zone_id         = data.aws_route53_zone.network-services.zone_id
 }
 
-resource "aws_route53_record" "external_validation_subdomain" {
+resource "aws_route53_record" "lands_external_validation_subdomain" {
   provider = aws.core-vpc
 
   allow_overwrite = true
@@ -122,11 +123,11 @@ resource "aws_route53_record" "external_validation_subdomain" {
 }
 
 // Route53 DNS record for directing traffic to the service
-resource "aws_route53_record" "external" {
+resource "aws_route53_record" "lands_external" {
   provider = aws.core-vpc
 
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "${local.application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "${local.lands_application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
 
   alias {
@@ -139,80 +140,80 @@ resource "aws_route53_record" "external" {
 // PRODUCTION DNS CONFIGURATION
 
 // ACM Public Certificate
-# resource "aws_acm_certificate" "external_prod" {
+# resource "aws_acm_certificate" "lands_external_prod" {
 #   count = local.is-production ? 1 : 0
 
-#   domain_name       = "${local.application}.service.justice.gov.uk"
+#   domain_name       = "${local.lands}.service.justice.gov.uk"
 #   validation_method = "DNS"
 #   lifecycle {
 #     create_before_destroy = true
 #   }
 # }
 
-# resource "aws_acm_certificate_validation" "external_prod" {
+# resource "aws_acm_certificate_validation" "lands_external_prod" {
 #   count = local.is-production ? 1 : 0
 
-#   certificate_arn         = aws_acm_certificate.external_prod[0].arn
-#   validation_record_fqdns = [aws_route53_record.external_validation_prod[0].fqdn]
+#   certificate_arn         = aws_acm_certificate.lands_external_prod[0].arn
+#   validation_record_fqdns = [aws_route53_record.lands_external_validation_prod[0].fqdn]
 #   timeouts {
 #     create = "10m"
 #   }
 # }
 
 // Route53 DNS record for certificate validation
-# resource "aws_route53_record" "external_validation_prod" {
+# resource "aws_route53_record" "lands_external_validation_prod" {
 #   count    = local.is-production ? 1 : 0
 #   provider = aws.core-network-services
 
 #   allow_overwrite = true
-#   name            = tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_name
-#   records         = [tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_value]
-#   type            = tolist(aws_acm_certificate.external_prod[0].domain_validation_options)[0].resource_record_type
+#   name            = tolist(aws_acm_certificate.lands_external_prod[0].domain_validation_options)[0].resource_record_name
+#   records         = [tolist(aws_acm_certificate.lands_external_prod[0].domain_validation_options)[0].resource_record_value]
+#   type            = tolist(aws_acm_certificate.lands_external_prod[0].domain_validation_options)[0].resource_record_type
 #   zone_id         = data.aws_route53_zone.application_zone.zone_id
 #   ttl             = 60
 # }
 
 // Route53 DNS record for directing traffic to the service
-# resource "aws_route53_record" "external_prod" {
+# resource "aws_route53_record" "lands_external_prod" {
 #   count    = local.is-production ? 1 : 0
 #   provider = aws.core-network-services
 
 #   zone_id = data.aws_route53_zone.application_zone.zone_id
-#   name    = "${local.application}.service.justice.gov.uk"
+#   name    = "${local.lands}.service.justice.gov.uk"
 #   type    = "A"
 
 #   alias {
-#     name                   = aws_lb.app_lb.dns_name
-#     zone_id                = aws_lb.app_lb.zone_id
+#     name                   = aws_lb.lands_lb.dns_name
+#     zone_id                = aws_lb.lands_lb.zone_id
 #     evaluate_target_health = true
 #   }
 # }
 
 ####################### ECS #########################################
 
-resource "aws_ecs_cluster" "app_cluster" {
-  name = "${local.application}_app_cluster"
+resource "aws_ecs_cluster" "lands_cluster" {
+  name = "${local.lands}_app_cluster"
   setting {
     name  = "containerInsights"
     value = "enabled"
   }
 }
 
-resource "aws_cloudwatch_log_group" "appFamily_logs" {
-  name = "/ecs/${local.application}Family"
+resource "aws_cloudwatch_log_group" "landsFamily_logs" {
+  name = "/ecs/${local.lands}Family"
 }
 
-resource "aws_ecs_task_definition" "app_task_definition" {
-  family                   = "${local.application}Family"
+resource "aws_ecs_task_definition" "lands_task_definition" {
+  family                   = "${local.lands}Family"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  execution_role_arn       = aws_iam_role.app_execution.arn
-  task_role_arn            = aws_iam_role.app_task.arn
+  execution_role_arn       = aws_iam_role.lands_execution.arn
+  task_role_arn            = aws_iam_role.lands_task.arn
   cpu                      = 1024
   memory                   = 2048
   container_definitions = jsonencode([
     {
-      name      = "${local.application}-container"
+      name      = "${local.lands}-container"
       image     = "mcr.microsoft.com/dotnet/framework/aspnet:4.8"
       cpu       = 1024
       memory    = 2048
@@ -227,7 +228,7 @@ resource "aws_ecs_task_definition" "app_task_definition" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = "${aws_cloudwatch_log_group.appFamily_logs.name}"
+          awslogs-group         = "${aws_cloudwatch_log_group.landsFamily_logs.name}"
           awslogs-region        = "eu-west-2"
           awslogs-stream-prefix = "ecs"
         }
@@ -235,23 +236,23 @@ resource "aws_ecs_task_definition" "app_task_definition" {
       environment = [
         {
           name  = "RDS_HOSTNAME"
-          value = "${local.rds_url}"
+          value = "${local.lands_rds_url}"
         },
         {
           name  = "RDS_PORT"
-          value = "${local.rds_port}"
+          value = "${local.lands_rds_port}"
         },
         {
           name  = "RDS_USERNAME"
-          value = "${local.rds_user}"
+          value = "${local.lands_rds_user}"
         },
         {
           name  = "RDS_PASSWORD"
-          value = "${local.rds_password}"
+          value = "${local.lands_rds_password}"
         },
         {
           name  = "DB_NAME"
-          value = "${local.app_db_name}"
+          value = "${local.lands_db_name}"
         },
         {
           name  = "supportEmail"
@@ -278,14 +279,14 @@ resource "aws_ecs_task_definition" "app_task_definition" {
   }
 }
 
-resource "aws_ecs_service" "app_ecs_service" {
+resource "aws_ecs_service" "lands_ecs_service" {
   depends_on = [
-    aws_lb_listener.app_lb
+    aws_lb_listener.lands_lb
   ]
 
   name                              = local.application
-  cluster                           = aws_ecs_cluster.app_cluster.id
-  task_definition                   = aws_ecs_task_definition.app_task_definition.arn
+  cluster                           = aws_ecs_cluster.lands_cluster.id
+  task_definition                   = aws_ecs_task_definition.lands_task_definition.arn
   launch_type                       = "FARGATE"
   enable_execute_command            = true
   desired_count                     = 1
@@ -293,12 +294,12 @@ resource "aws_ecs_service" "app_ecs_service" {
 
   network_configuration {
     subnets          = data.aws_subnets.shared-public.ids
-    security_groups  = [aws_security_group.ecs_service.id]
+    security_groups  = [aws_security_group.lands_ecs_service.id]
     assign_public_ip = true
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.app_target_group.arn
+    target_group_arn = aws_lb_target_group.lands_target_group.arn
     container_name   = "${local.application}-container"
     container_port   = 80
   }
@@ -308,7 +309,7 @@ resource "aws_ecs_service" "app_ecs_service" {
   }
 }
 
-resource "aws_iam_role" "app_execution" {
+resource "aws_iam_role" "lands_execution" {
   name = "execution-${local.application}"
 
   assume_role_policy = <<EOF
@@ -335,9 +336,9 @@ EOF
   )
 }
 
-resource "aws_iam_role_policy" "app_execution" {
+resource "aws_iam_role_policy" "lands_execution" {
   name = "execution-${local.application}"
-  role = aws_iam_role.app_execution.id
+  role = aws_iam_role.lands_execution.id
 
   policy = <<-EOF
   {
@@ -358,7 +359,7 @@ resource "aws_iam_role_policy" "app_execution" {
   EOF
 }
 
-resource "aws_iam_role" "app_task" {
+resource "aws_iam_role" "lands_task" {
   name = "task-${local.application}"
 
   assume_role_policy = <<EOF
@@ -385,9 +386,9 @@ EOF
   )
 }
 
-resource "aws_iam_role_policy" "app_task" {
+resource "aws_iam_role_policy" "lands_task" {
   name = "task-${local.application}"
-  role = aws_iam_role.app_task.id
+  role = aws_iam_role.lands_task.id
 
   policy = <<-EOF
   {
@@ -409,7 +410,7 @@ resource "aws_iam_role_policy" "app_task" {
   EOF
 }
 
-resource "aws_security_group" "ecs_service" {
+resource "aws_security_group" "lands_ecs_service" {
   name_prefix = "ecs-service-sg-"
   vpc_id      = data.aws_vpc.shared.id
 
@@ -418,7 +419,7 @@ resource "aws_security_group" "ecs_service" {
     to_port         = 80
     protocol        = "tcp"
     description     = "Allow traffic on port 80 from load balancer"
-    security_groups = [aws_security_group.app_lb_sc.id]
+    security_groups = [aws_security_group.lands_lb_sc.id]
   }
 
   egress {
@@ -429,13 +430,13 @@ resource "aws_security_group" "ecs_service" {
   }
 }
 
-resource "aws_ecr_repository" "app-ecr-repo" {
+resource "aws_ecr_repository" "lands-ecr-repo" {
   name         = "${local.application}-ecr-repo"
   force_delete = true
 }
 
 ####################### LOAD BALANCER #########################################
-resource "aws_security_group" "app_lb_sc" {
+resource "aws_security_group" "lands_lb_sc" {
   name        = "load balancer security group"
   description = "control access to the load balancer"
   vpc_id      = data.aws_vpc.shared.id
@@ -472,17 +473,17 @@ resource "aws_security_group" "app_lb_sc" {
   }
 }
 
-resource "aws_lb" "app_lb" {
+resource "aws_lb" "lands_lb" {
   name                       = "${local.application}-load-balancer"
   load_balancer_type         = "application"
-  security_groups            = [aws_security_group.app_lb_sc.id]
+  security_groups            = [aws_security_group.lands_lb_sc.id]
   subnets                    = data.aws_subnets.shared-public.ids
   enable_deletion_protection = false
   internal                   = false
-  depends_on                 = [aws_security_group.app_lb_sc]
+  depends_on                 = [aws_security_group.lands_lb_sc]
 }
 
-resource "aws_lb_target_group" "app_target_group" {
+resource "aws_lb_target_group" "lands_target_group" {
   name                 = "${local.application}-target-group"
   port                 = 80
   protocol             = "HTTP"
@@ -506,19 +507,19 @@ resource "aws_lb_target_group" "app_target_group" {
 
 }
 
-resource "aws_lb_listener" "app_lb" {
+resource "aws_lb_listener" "lands_lb" {
   depends_on = [
-    aws_acm_certificate.external
+    aws_acm_certificate.lands_external
   ]
-  certificate_arn   = aws_acm_certificate.external.arn
-  #certificate_arn   = local.is-production ? aws_acm_certificate.external_prod[0].arn : aws_acm_certificate.external.arn
-  load_balancer_arn = aws_lb.app_lb.arn
+  certificate_arn   = aws_acm_certificate.lands_external.arn
+  #certificate_arn   = local.is-production ? aws_acm_certificate.lands_external_prod[0].arn : aws_acm_certificate.lands_external.arn
+  load_balancer_arn = aws_lb.lands_lb.arn
   port              = local.application_data.accounts[local.environment].server_port_2
   protocol          = local.application_data.accounts[local.environment].lb_listener_protocol_2
   ssl_policy        = local.application_data.accounts[local.environment].lb_listener_protocol_2 == "HTTP" ? "" : "ELBSecurityPolicy-2016-08"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.app_target_group.arn
+    target_group_arn = aws_lb_target_group.lands_target_group.arn
   }
 }
