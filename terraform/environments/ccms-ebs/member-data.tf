57cb7983dbd24d935837131cd1ddeb8f0249d8a7
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-07-13 11:21:29 +0100

    CC-1947: Rearranged aws_iam_policy_document.sns_topic_policy_s3

--- terraform/environments/ccms-ebs/member-data.tf
+++ terraform/environments/ccms-ebs/member-data.tf
@@ -100,25 +100,27 @@ data "aws_iam_policy_document" "sns_topic_policy_ec2cw" {
 data "aws_iam_policy_document" "sns_topic_policy_s3" {
   policy_id = "SnsTopicId"
   statement {
-    sid = "statement1"
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    effect = "Allow"
     actions = [
-      "SNS:GetTopicAttributes",
-      "SNS:SetTopicAttributes",
       "SNS:AddPermission",
       "SNS:DeleteTopic",
-      "SNS:Subscribe",
+      "SNS:GetTopicAttributes",
       "SNS:ListSubscriptionsByTopic",
       "SNS:Publish",
-      "SNS:Receive"
+      "SNS:Receive",
+      "SNS:SetTopicAttributes",
+      "SNS:Subscribe",
     ]
+    effect = "Allow"
     resources = [
       aws_sns_topic.s3_topic.arn
     ]
+    sid = "statement1"
+    principals {
+      identifiers = [
+        "*",
+      ]
+      type        = "AWS"
+    }
   }
 }
 
