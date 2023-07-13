a547da8fd104d9126ec7a261a5ec4f514d4ddac5
wullub <william@automationlogic.com>
2023-07-13 14:44:56 +0100

    Update locals_test.tf

--- terraform/environments/oasys/locals_test.tf
+++ terraform/environments/oasys/locals_test.tf
@@ -114,24 +114,24 @@ locals {
               }
             }
             rules = {
-              # t2-web-http-8080 = {
-              #   priority = 100
-              #   actions = [{
-              #     type              = "forward"
-              #     target_group_name = "t2-${local.application_name}-web-a-http-8080"
-              #   }]
-              #   conditions = [
-              #     {
-              #       host_header = {
-              #         values = [
-              #           "t2.oasys.service.justice.gov.uk",
-              #           "*.t2.oasys.service.justice.gov.uk",
-              #           "t2-oasys.hmpp-azdt.justice.gov.uk",
-              #         ]
-              #       }
-              #     }
-              #   ]
-              # }
+              t2-web-http-8080 = {
+                priority = 100
+                actions = [{
+                  type              = "forward"
+                  target_group_name = "t2-${local.application_name}-web-a-http-8080"
+                }]
+                conditions = [
+                  {
+                    host_header = {
+                      values = [
+                        "t2.oasys.service.justice.gov.uk",
+                        "*.t2.oasys.service.justice.gov.uk",
+                        "t2-oasys.hmpp-azdt.justice.gov.uk",
+                      ]
+                    }
+                  }
+                ]
+              }
             }
           }
         }
