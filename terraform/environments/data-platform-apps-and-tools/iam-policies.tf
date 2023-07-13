6977c0f4db8bc38b794a0507a5e0f7d4d9b2cee5
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-07-13 15:26:17 +0000

    Removed a comma

--- terraform/environments/data-platform-apps-and-tools/iam-policies.tf
+++ terraform/environments/data-platform-apps-and-tools/iam-policies.tf
@@ -1,8 +1,8 @@
 data "aws_iam_policy_document" "airflow_user" {
   statement {
-    sid = "AllowAirflowCreateWebLoginToken"
-    effect = "Allow"
-    actions = ["airflow:CreateWebLoginToken"],
+    sid       = "AllowAirflowCreateWebLoginToken"
+    effect    = "Allow"
+    actions   = ["airflow:CreateWebLoginToken"]
     resources = ["arn:aws:airflow:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:role/${local.airflow_name}/User"]
   }
 }
