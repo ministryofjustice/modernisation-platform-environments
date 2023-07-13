bba14e70a1ee06ffd4d108750828c10cffc5d424
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-07-13 20:30:27 +0100

    destroy development

--- terraform/environments/tribunals/rds.tf
+++ terraform/environments/tribunals/rds.tf
@@ -1,95 +1,95 @@
-resource "aws_db_instance" "rdsdb" {
-  allocated_storage = local.application_data.accounts[local.environment].allocated_storage
-  //db_name                 = DBName must be null for engine: sqlserver-se
-  storage_type   = local.application_data.accounts[local.environment].storage_type
-  identifier     = local.application_data.accounts[local.environment].db_identifier
-  engine         = local.application_data.accounts[local.environment].engine
-  engine_version = local.application_data.accounts[local.environment].engine_version
-  instance_class = local.application_data.accounts[local.environment].instance_class
-  username       = local.application_data.accounts[local.environment].username
-  password       = random_password.password.result
-  port           = 1433
+# resource "aws_db_instance" "rdsdb" {
+#   allocated_storage = local.application_data.accounts[local.environment].allocated_storage
+#   //db_name                 = DBName must be null for engine: sqlserver-se
+#   storage_type   = local.application_data.accounts[local.environment].storage_type
+#   identifier     = local.application_data.accounts[local.environment].db_identifier
+#   engine         = local.application_data.accounts[local.environment].engine
+#   engine_version = local.application_data.accounts[local.environment].engine_version
+#   instance_class = local.application_data.accounts[local.environment].instance_class
+#   username       = local.application_data.accounts[local.environment].username
+#   password       = random_password.password.result
+#   port           = 1433
 
-  skip_final_snapshot = true
+#   skip_final_snapshot = true
 
-  license_model       = "license-included"
-  publicly_accessible = true
+#   license_model       = "license-included"
+#   publicly_accessible = true
 
-  multi_az               = false
-  db_subnet_group_name   = aws_db_subnet_group.dbsubnetgroup.name
-  vpc_security_group_ids = [aws_security_group.sqlserver_db_sc.id]
+#   multi_az               = false
+#   db_subnet_group_name   = aws_db_subnet_group.dbsubnetgroup.name
+#   vpc_security_group_ids = [aws_security_group.sqlserver_db_sc.id]
 
-  tags = {
-    Name = "tribunals"
-  }
-}
-
-resource "aws_db_subnet_group" "dbsubnetgroup" {
-  name       = "dbsubnetgroup"
-  subnet_ids = data.aws_subnets.shared-public.ids
-}
-
-resource "aws_security_group" "sqlserver_db_sc" {
-  name        = "sqlserver_security_group"
-  description = "control access to the database"
-  vpc_id      = data.aws_vpc.shared.id
-
-  ingress {
-    from_port   = 1433
-    to_port     = 1433
-    protocol    = "tcp"
-    description = "MOJ Digital VPN access"
-    cidr_blocks = [local.application_data.accounts[local.environment].moj_ip]
-  }
-  ingress {
-    from_port   = 1433
-    to_port     = 1433
-    protocol    = "tcp"
-    description = "Allows Github Actions to access RDS"
-    cidr_blocks = ["${jsondecode(data.http.myip.response_body)["ip"]}/32"]
-  }
-  ingress {
-    from_port       = 1433
-    to_port         = 1433
-    protocol        = "tcp"
-    description     = "Allows DMS to access RDS"
-    security_groups = [aws_security_group.vpc_dms_replication_instance_group.id]
-  }
-  egress {
-    description = "allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-data "http" "myip" {
-  url = "http://ipinfo.io/json"
-}
-
-# resource "random_password" "new_password" {
-#   length  = 16
-#   special = false 
-# }
-
-# resource "null_resource" "setup_db" {
-#   depends_on = [aws_db_instance.rdsdb] #wait for the db to be ready
-
-#   provisioner "local-exec" {
-#     interpreter = ["bash", "-c"]
-#     command     = "ifconfig -a; chmod +x ./setup-mssql.sh; ./setup-mssql.sh"
-
-#     environment = {
-#       DB_URL = aws_db_instance.rdsdb.address      
-#       USER_NAME = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.data_rds_secret_current.secret_string)["username"])
-#       PASSWORD = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.data_rds_secret_current.secret_string)["password"])
-#       NEW_DB_NAME = "transport"
-#       #NEW_USER_NAME = "transport_admin"
-#       #NEW_PASSWORD = random_password.new_password.result
-#     }
-#   }
-#   triggers = {
-#     always_run = "${timestamp()}"
+#   tags = {
+#     Name = "tribunals"
 #   }
 # }
+
+# resource "aws_db_subnet_group" "dbsubnetgroup" {
+#   name       = "dbsubnetgroup"
+#   subnet_ids = data.aws_subnets.shared-public.ids
+# }
+
+# resource "aws_security_group" "sqlserver_db_sc" {
+#   name        = "sqlserver_security_group"
+#   description = "control access to the database"
+#   vpc_id      = data.aws_vpc.shared.id
+
+#   ingress {
+#     from_port   = 1433
+#     to_port     = 1433
+#     protocol    = "tcp"
+#     description = "MOJ Digital VPN access"
+#     cidr_blocks = [local.application_data.accounts[local.environment].moj_ip]
+#   }
+#   ingress {
+#     from_port   = 1433
+#     to_port     = 1433
+#     protocol    = "tcp"
+#     description = "Allows Github Actions to access RDS"
+#     cidr_blocks = ["${jsondecode(data.http.myip.response_body)["ip"]}/32"]
+#   }
+#   ingress {
+#     from_port       = 1433
+#     to_port         = 1433
+#     protocol        = "tcp"
+#     description     = "Allows DMS to access RDS"
+#     security_groups = [aws_security_group.vpc_dms_replication_instance_group.id]
+#   }
+#   egress {
+#     description = "allow all outbound traffic"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+# data "http" "myip" {
+#   url = "http://ipinfo.io/json"
+# }
+
+# # resource "random_password" "new_password" {
+# #   length  = 16
+# #   special = false 
+# # }
+
+# # resource "null_resource" "setup_db" {
+# #   depends_on = [aws_db_instance.rdsdb] #wait for the db to be ready
+
+# #   provisioner "local-exec" {
+# #     interpreter = ["bash", "-c"]
+# #     command     = "ifconfig -a; chmod +x ./setup-mssql.sh; ./setup-mssql.sh"
+
+# #     environment = {
+# #       DB_URL = aws_db_instance.rdsdb.address      
+# #       USER_NAME = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.data_rds_secret_current.secret_string)["username"])
+# #       PASSWORD = nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.data_rds_secret_current.secret_string)["password"])
+# #       NEW_DB_NAME = "transport"
+# #       #NEW_USER_NAME = "transport_admin"
+# #       #NEW_PASSWORD = random_password.new_password.result
+# #     }
+# #   }
+# #   triggers = {
+# #     always_run = "${timestamp()}"
+# #   }
+# # }
