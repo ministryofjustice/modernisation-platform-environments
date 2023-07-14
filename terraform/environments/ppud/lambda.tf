c68f8b6f7694ce1e0012ceb71ac5d10c7fff8616
umeshc-roy <umeshc.roy@gmail.com>
2023-07-14 16:18:15 +0100

    Lambda and Eventbridge

--- terraform/environments/ppud/lambda.tf
+++ terraform/environments/ppud/lambda.tf
@@ -54,7 +54,7 @@ resource "aws_cloudwatch_event_rule" "stop_instance" {
 
 resource "aws_cloudwatch_event_target" "trigger_lambda_monthly_once_stop" {
   count      = local.is-production == true ? 1 : 0
-  rule      = aws_cloudwatch_event_rule.stop_instance.name
+  rule      = aws_cloudwatch_event_rule.stop_instance[0].name
   target_id = "stop-instance"
   arn       = aws_lambda_function.terraform_lambda_func_stop[0].arn
 }
@@ -63,7 +63,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_stop" {
   count      = local.is-production == true ? 1 : 0
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.terraform_lambda_func_stop.function_name
+  function_name = aws_lambda_function.terraform_lambda_func_stop[0].function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.stop_instance[0].arn
 }
@@ -78,7 +78,7 @@ resource "aws_cloudwatch_event_rule" "start_instance" {
 
 resource "aws_cloudwatch_event_target" "trigger_lambda_monthly_once_start" {
   count      = local.is-production == true ? 1 : 0
-  rule      = aws_cloudwatch_event_rule.start_instance.name
+  rule      = aws_cloudwatch_event_rule.start_instance[0].name
   target_id = "start-instance"
   arn       = aws_lambda_function.terraform_lambda_func_start[0].arn
 }
@@ -87,7 +87,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_start" {
   count      = local.is-production == true ? 1 : 0
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.terraform_lambda_func_start.function_name
+  function_name = aws_lambda_function.terraform_lambda_func_start[0].function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.start_instance[0].arn
 }
