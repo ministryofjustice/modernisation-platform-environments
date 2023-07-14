cfed86760d5f79f5d3251cab1a010ca4a4b4ee45
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-07-14 16:25:16 +0100

    delete cursever env var and some unneeded code

--- terraform/environments/dacp/rds.tf
+++ terraform/environments/dacp/rds.tf
@@ -39,13 +39,6 @@ resource "aws_security_group" "postgresql_db_sc" {
     description     = "Allows ECS service to access RDS"
     security_groups = [aws_security_group.ecs_service.id]
   }
-#   ingress {
-#     from_port   = 5432
-#     to_port     = 5432
-#     protocol    = "tcp"
-#     description = "Allows Github Actions to access RDS"
-#     cidr_blocks = ["${jsondecode(data.http.myip.response_body)["ip"]}/32"]
-#   }
   egress {
     description = "allow all outbound traffic"
     from_port   = 0
@@ -55,26 +48,3 @@ resource "aws_security_group" "postgresql_db_sc" {
   }
 
 }
-
-# data "http" "myip" {
-#   url = "http://ipinfo.io/json"
-# }
-
-# resource "null_resource" "setup_db" {
-#   depends_on = [aws_db_instance.dacp_db]
-
-#   provisioner "local-exec" {
-#     interpreter = ["bash", "-c"]
-#     command     = "chmod +x ./setup-postgresql.sh; ./setup-postgresql.sh"
-
-#     environment = {
-#       DB_HOSTNAME          = aws_db_instance.tipstaff_db.address
-#       DB_NAME              = aws_db_instance.tipstaff_db.db_name
-#       TIPSTAFF_DB_USERNAME = local.application_data.accounts[local.environment].db_username
-#       TIPSTAFF_DB_PASSWORD = random_password.password.result
-#     }
-#   }
-#   triggers = {
-#     always_run = "${timestamp()}"
-#   }
-# }
