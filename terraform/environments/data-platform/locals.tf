9d76ff7d4496d08c35b4bc60223bec48920e35bb
Matt <38562764+LavMatt@users.noreply.github.com>
2023-07-14 13:00:16 +0100

    revert number and type of workers to original settings

--- terraform/environments/data-platform/locals.tf
+++ terraform/environments/data-platform/locals.tf
@@ -17,8 +17,8 @@ locals {
   name                             = "data-platform-product"
   glue_version                     = "4.0"
   max_retries                      = 0
-  worker_type                      = "G.2X"
-  number_of_workers                = 10
+  worker_type                      = "G.1X"
+  number_of_workers                = 2
   timeout                          = 120 # minutes
   execution_class                  = "STANDARD"
   max_concurrent                   = 5
