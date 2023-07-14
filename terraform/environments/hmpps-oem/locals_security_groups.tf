43299467afbcde56eb7189840583a216ffc04db4
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-07-14 11:15:05 +0100

    Allow Azure access to console

--- terraform/environments/hmpps-oem/locals_security_groups.tf
+++ terraform/environments/hmpps-oem/locals_security_groups.tf
@@ -1,5 +1,23 @@
 locals {
 
+  security_group_cidrs_devtest = {
+    oracle_oem_console = flatten([
+      module.ip_addresses.azure_fixngo_cidrs.devtest,
+    ])
+  }
+  security_group_cidrs_preprod_prod = {
+    oracle_oem_console = flatten([
+      module.ip_addresses.azure_fixngo_cidrs.prod,
+    ])
+  }
+  security_group_cidrs_by_environment = {
+    development   = local.security_group_cidrs_devtest
+    test          = local.security_group_cidrs_devtest
+    preproduction = local.security_group_cidrs_preprod_prod
+    production    = local.security_group_cidrs_preprod_prod
+  }
+  security_group_cidrs = local.security_group_cidrs_by_environment[local.environment]
+
   security_groups = {
     data_oem = {
       description = "Security group for OEM servers"
@@ -19,6 +37,13 @@ locals {
           cidr_blocks = [
             "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}"
           ]
+        }
+        oracle7803 = {
+          description = "Allow oracle OEM console"
+          from_port   = "7803"
+          to_port     = "7803"
+          protocol    = "TCP"
+          cidr_blocks = local.security_group_cidrs.oracle_oem_console
         }
       }
       egress = {
