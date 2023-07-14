8dc928c0359de1c3c19d9a93265b304a615b800d
ministry-riaz-jamil <mahwish.riaz-jamil@digital.justice.gov.uk>
2023-07-14 12:21:30 +0100

    ++ config for t3 db server

--- terraform/environments/corporate-staff-rostering/locals_test.tf
+++ terraform/environments/corporate-staff-rostering/locals_test.tf
@@ -4,23 +4,87 @@ locals {
   # baseline config
   test_config = {
 
-    baseline_ec2_autoscaling_groups = {
-      mrj-dev-base-ol85 = {
+    baseline_ec2_instances = {
+      t3-csr-db-a = {
         config = merge(module.baseline_presets.ec2_instance.config.default, {
           ami_name          = "base_ol_8_5_*"
+          ami_owner         = "self"
+          availability_zone = "${local.region}a"
         })
         instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["data-db"]
+          instance_type                = "r6i.xlarge"
+          disable_api_termination      = true
+          metadata_options_http_tokens = "optional" # the Oracle installer cannot accommodate a token
+          monitoring                   = true
+          vpc_security_group_ids       = ["data-db"]
         })
-        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
-          desired_capacity = 1
-        })
+
+      user_data_cloud_init = {
+        args = {
+          branch               = "main"
+          ansible_repo         = "modernisation-platform-configuration-management"
+          ansible_repo_basedir = "ansible"
+          ansible_args         = "--tags ec2provision,oracle_19c_download,oracle_19c_install"
+        }
+        scripts = [
+          "ansible-ec2provision.sh.tftpl",
+        ]
+    }
+
+    ebs_volumes = {
+      "/dev/sdb" = { type = "gp3", label = "app", size = 100 } # /u01
+      "/dev/sdc" = { type = "gp3", label = "app", size = 100 } # /u02
+      "/dev/sde" = { type = "gp3", label = "data" }            # DATA01
+      "/dev/sdf" = { type = "gp3", label = "data" }            # DATA02
+      "/dev/sdg" = { type = "gp3", label = "data" }            # DATA03
+      "/dev/sdh" = { type = "gp3", label = "data" }            # DATA04
+      "/dev/sdi" = { type = "gp3", label = "data" }            # DATA05
+      "/dev/sdj" = { type = "gp3", label = "flash" }           # FLASH01
+      "/dev/sdk" = { type = "gp3", label = "flash" }           # FLASH02
+      "/dev/sds" = { type = "gp3", label = "swap" }
+    }
+
+    ebs_volume_config = {
+      data = {
+        iops       = 3000
+        throughput = 125
+        total_size = 500
+      }
+      flash = {
+        iops       = 3000
+        throughput = 125
+        total_size = 50
+      }
+    }
+
+    route53_records = {
+      create_internal_record = true
+      create_external_record = true
+    }
+
+    ssm_parameters = {
+      ASMSYS = {
+        random = {
+          length  = 30
+          special = false
+        }
+        description = "ASMSYS password"
+      }
+      ASMSNMP = {
+        random = {
+          length  = 30
+          special = false
+        }
+        description = "ASMSNMP password"
+      }
+    }
+
         tags = {
           description = "For testing our base OL8.5 base image"
           ami         = "base_ol_8_5"
           os-type     = "Linux"
           component   = "test"
-          servser-type = "base-ol-8-5"
+          server-type = "csr-db"
         }
       }
     }
