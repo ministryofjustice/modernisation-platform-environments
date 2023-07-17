9201f0e91397369f844e5f262f647f8e5b073681
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-07-17 20:28:54 +0100

    Endpoint IDs with List

--- terraform/environments/digital-prison-reporting/modules/vpc_endpoint/outputs.tf
+++ terraform/environments/digital-prison-reporting/modules/vpc_endpoint/outputs.tf
@@ -1,7 +1,7 @@
 output "vpc_endpoint_arn" {
-  value = var.setup_vpc_endpoint ? join("", aws_vpc_endpoint.this.arn) : ""
+  value = aws_vpc_endpoint.this.arn
 }
 
 output "vpc_endpoint_id" {
-  value = var.setup_vpc_endpoint ? join("", aws_vpc_endpoint.this.id) : ""
+  value = aws_vpc_endpoint.this.id
 }
