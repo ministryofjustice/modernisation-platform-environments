3bc594fb595b424a44ffe98a2b4b917e7a1b283e
ministry-riaz-jamil <mahwish.riaz-jamil@digital.justice.gov.uk>
2023-07-18 16:13:05 +0100

    ++ moved tested code to locals_db, reverted locals_test to match main

--- terraform/environments/corporate-staff-rostering/locals_test.tf
+++ terraform/environments/corporate-staff-rostering/locals_test.tf
@@ -4,97 +4,7 @@ locals {
   # baseline config
   test_config = {
 
-    baseline_ec2_instances = {
-      t3-csr-db-a = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name          = "hmpps_ol_8_5_oracledb_19c_release_2023-07-14T15-36-30.795Z"
-          ami_owner         = "self"
-          availability_zone = "${local.region}a"
-        })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          instance_type                = "r6i.xlarge"
-          disable_api_termination      = true
-          metadata_options_http_tokens = "optional" # the Oracle installer cannot accommodate a token
-          monitoring                   = true
-          vpc_security_group_ids       = ["data-db"]
-        })
-
-      user_data_cloud_init = {
-        args = {
-          branch               = "main"
-          ansible_repo         = "modernisation-platform-configuration-management"
-          ansible_repo_basedir = "ansible"
-          ansible_args         = "--tags ec2provision"
-        }
-        scripts = [
-          "ansible-ec2provision.sh.tftpl",
-        ]
-    }
-
-    ebs_volumes = {
-      "/dev/sdb" = { type = "gp3", label = "app", size = 100 } # /u01
-      "/dev/sdc" = { type = "gp3", label = "app", size = 100 } # /u02
-      "/dev/sde" = { type = "gp3", label = "data" }            # DATA01
-      "/dev/sdf" = { type = "gp3", label = "data" }            # DATA02
-      "/dev/sdg" = { type = "gp3", label = "data" }            # DATA03
-      "/dev/sdh" = { type = "gp3", label = "data" }            # DATA04
-      "/dev/sdi" = { type = "gp3", label = "data" }            # DATA05
-      "/dev/sdj" = { type = "gp3", label = "flash" }           # FLASH01
-      "/dev/sdk" = { type = "gp3", label = "flash" }           # FLASH02
-      "/dev/sds" = { type = "gp3", label = "swap" }
-    }
-
-    ebs_volume_config = {
-      data = {
-        iops       = 3000
-        throughput = 125
-        total_size = 500
-      }
-      flash = {
-        iops       = 3000
-        throughput = 125
-        total_size = 50
-      }
-    }
-
-    route53_records = {
-      create_internal_record = true
-      create_external_record = true
-    }
-
-    ssm_parameters = {
-      ASMSYS = {
-        random = {
-          length  = 30
-          special = false
-        }
-        description = "ASMSYS password"
-      }
-      ASMSNMP = {
-        random = {
-          length  = 30
-          special = false
-        }
-        description = "ASMSNMP password"
-      }
-    }
-
-    tags = {
-      description = "Test CSR DB server"
-      ami         = "base_ol_8_5"
-      os-type     = "Linux"
-      component   = "test"
-      server-type = "csr-db"
-    }
-      }
-    }
-      baseline_route53_zones = {
-        "hmpps-test.modernisation-platform.service.justice.gov.uk" = {
-          records = [
-            { name = "t3-csr-db-a", type = "CNAME", ttl = "300", records = ["t3-csr-db-a.corporate-staff-rostering.hmpps-test.modernisation-platform.service.justice.gov.uk"] },
-          ]
-        }
-    }
+    baseline_ec2_autoscaling_groups = {}
 
     baseline_s3_buckets = {
       # use this bucket for storing artefacts for use across all accounts
@@ -107,5 +17,6 @@ locals {
         iam_policies = module.baseline_presets.s3_iam_policies
       }
     }
+
   }
 }
