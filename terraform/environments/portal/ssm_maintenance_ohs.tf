ce60646c6e698aa18877c9d05e12a97e7e2c3fdf
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-07-18 17:22:59 +0100

    cleanned up ohs1 code

--- terraform/environments/portal/ssm_maintenance_ohs.tf
+++ terraform/environments/portal/ssm_maintenance_ohs.tf
@@ -1,73 +1,73 @@
 
-# locals {
-#   script = <<EOF
-# #!/bin/bash
-# . $HOME/.bash_profile
-# FOL=`date +%d%m%y`
-# LHOME=/IDAM/product/runtime/Domain/mserver/instances/ohs1/diagnostics/logs/OHS/ohs1
-# MDOMAIN_HOME=/IDAM/product/runtime/Domain/mserver/domains/IAMGovernanceDomain
-# cd $LHOME
-# #find . -name "AdminServer.log*" -mtime +30 -exec ls -lt {} \;
-# #find . -name "AdminServer.log*" -mtime +30 -exec rm -f {} \;
-# #find . -name "AdminServer.log*" -mtime +30 -exec ls -1t {} \; |aws s3 cp AdminServer.log00001 s3://laa-portal-development-archive/
-# #find . -name "AdminServer.log*" -o -name "IAMAccessDomain.log*" -mtime +30 -exec ls -1t {} \; |while read FILE
-# #find . -name "*.log*" -mtime +90 -exec ls -1t {} \; |while read FILE
-# find . -type f \( -name "*.log" -o -name "*_log*" -o -name "oblog.log.*" \) -mtime +90 -exec ls -1t {} \; |while read FILE
-# do
-#   aws s3 cp $FILE  ${local.application_data.accounts[local.environment].url}/ohs1/$FOL/
-#   rm $FILE
-# done
+locals {
+  script_ohs = <<EOF
+#!/bin/bash
+. $HOME/.bash_profile
+FOL=`date +%d%m%y`
+LHOME=/IDAM/product/runtime/Domain/mserver/instances/ohs1/diagnostics/logs/OHS/ohs1
+MDOMAIN_HOME=/IDAM/product/runtime/Domain/mserver/domains/IAMGovernanceDomain
+cd $LHOME
+#find . -name "AdminServer.log*" -mtime +30 -exec ls -lt {} \;
+#find . -name "AdminServer.log*" -mtime +30 -exec rm -f {} \;
+#find . -name "AdminServer.log*" -mtime +30 -exec ls -1t {} \; |aws s3 cp AdminServer.log00001 s3://laa-portal-development-archive/
+#find . -name "AdminServer.log*" -o -name "IAMAccessDomain.log*" -mtime +30 -exec ls -1t {} \; |while read FILE
+#find . -name "*.log*" -mtime +90 -exec ls -1t {} \; |while read FILE
+find . -type f \( -name "*.log" -o -name "*_log*" -o -name "oblog.log.*" \) -mtime +90 -exec ls -1t {} \; |while read FILE
+do
+  aws s3 cp $FILE  ${local.application_data.accounts[local.environment].url}/ohs1/$FOL/
+  rm $FILE
+done
 
-# EOF
+EOF
 
-# }
+}
 
-# resource "aws_ssm_maintenance_window" "window" {
-#   name     = "ohs1-diagnostics-log-archive-poc"
-#   schedule = "cron(0 0 9 ? * * *)"
-#   duration = 4
-#   cutoff   = 1
-#   schedule_timezone = "Europe/London"
-# }
-
-
-# resource "aws_ssm_maintenance_window_target" "reg_target" {
-#   window_id     = aws_ssm_maintenance_window.window.id 
-#   name          = "maintenance-window-target"
-#   description   = "This is a maintenance window target"
-#   resource_type = "INSTANCE"
-
-#   targets {
-#     key    = "InstanceIds"
-#     values = [aws_instance.ohs_instance_1.id]
-#   }
-# }
-
-# resource "aws_ssm_maintenance_window_task" "commands" {
-#   max_concurrency = 2
-#   max_errors      = 3
-#   priority        = 1
-#   task_arn        = "AWS-RunShellScript"
-#   task_type       = "RUN_COMMAND"
-#   window_id       = aws_ssm_maintenance_window.window.id
-
-#   targets {
-#     key    = "InstanceIds"
-#     values = [aws_instance.ohs_instance_1.id]
-#   }
+resource "aws_ssm_maintenance_window" "ohs_window" {
+  name     = "ohs1-diagnostics-log-archive-poc"
+  schedule = "cron(0 0 9 ? * * *)"
+  duration = 4
+  cutoff   = 1
+  schedule_timezone = "Europe/London"
+}
 
 
-#   task_invocation_parameters {
+resource "aws_ssm_maintenance_window_target" "reg_target_ohs" {
+  window_id     = aws_ssm_maintenance_window.ohs_window.id 
+  name          = "maintenance-window-target"
+  description   = "This is a maintenance window target"
+  resource_type = "INSTANCE"
+
+  targets {
+    key    = "InstanceIds"
+    values = [aws_instance.ohs_instance_1.id]
+  }
+}
+
+resource "aws_ssm_maintenance_window_task" "commands_ohs" {
+  max_concurrency = 2
+  max_errors      = 3
+  priority        = 1
+  task_arn        = "AWS-RunShellScript"
+  task_type       = "RUN_COMMAND"
+  window_id       = aws_ssm_maintenance_window.ohs_window.id
+
+  targets {
+    key    = "InstanceIds"
+    values = [aws_instance.ohs_instance_1.id]
+  }
+
+
+  task_invocation_parameters {
     
-#     run_command_parameters {
-#       document_version = "$LATEST"
+    run_command_parameters {
+      document_version = "$LATEST"
 
-#      parameter {
-#         name = "commands"
-#         values = [local.script]
-#       }
-#     }
+     parameter {
+        name = "commands"
+        values = [local.script_ohs]
+      }
+    }
     
-#   }
+  }
 
-# }
+}
