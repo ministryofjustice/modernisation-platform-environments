b274779fc25decee0a9990e899758ffa50305a02
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-07-18 10:12:51 +0100

    update SGs

--- terraform/environments/hmpps-oem/locals_security_groups.tf
+++ terraform/environments/hmpps-oem/locals_security_groups.tf
@@ -1,5 +1,37 @@
 locals {
 
+  security_group_cidrs_devtest = {
+    oracle_db = flatten([
+      "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}",
+    ])
+    oracle_oem_agent = flatten([
+      "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}",
+    ])
+    oracle_oem_console = flatten([
+      module.ip_addresses.azure_fixngo_cidrs.devtest,
+      "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}",
+    ])
+  }
+  security_group_cidrs_preprod_prod = {
+    oracle_db = flatten([
+      "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}",
+    ])
+    oracle_oem_agent = flatten([
+      "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}",
+    ])
+    oracle_oem_console = flatten([
+      module.ip_addresses.azure_fixngo_cidrs.prod,
+      "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}",
+    ])
+  }
+  security_group_cidrs_by_environment = {
+    development   = local.security_group_cidrs_devtest
+    test          = local.security_group_cidrs_devtest
+    preproduction = local.security_group_cidrs_preprod_prod
+    production    = local.security_group_cidrs_preprod_prod
+  }
+  security_group_cidrs = local.security_group_cidrs_by_environment[local.environment]
+
   security_groups = {
     data_oem = {
       description = "Security group for OEM servers"
@@ -16,9 +48,21 @@ locals {
           from_port   = "1521"
           to_port     = "1521"
           protocol    = "TCP"
-          cidr_blocks = [
-            "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}"
-          ]
+          cidr_blocks = local.security_group_cidrs.oracle_db
+        }
+        oracle3872 = {
+          description = "Allow oem agent ingress"
+          from_port   = "3872"
+          to_port     = "3872"
+          protocol    = "TCP"
+          cidr_blocks = local.security_group_cidrs.oracle_oem_agent
+        }
+        oracle7803 = {
+          description = "Allow oracle OEM console"
+          from_port   = "7803"
+          to_port     = "7803"
+          protocol    = "TCP"
+          cidr_blocks = local.security_group_cidrs.oracle_oem_console
         }
       }
       egress = {
