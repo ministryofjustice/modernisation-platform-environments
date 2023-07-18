d77d152bfabf02e0ee6240ec74243fce9134c5c4
LavMatt <mattlaverty@gmail.com>
2023-07-18 14:44:55 +0100

    new file for eventbridge

---
+++ terraform/environments/data-platform/eventbridge_rules.tf
@@ -0,0 +1,15 @@
+
+resource "aws_cloudwatch_event_rule" "object_created_raw_data" {
+  name = "object_created_raw_data"
+  tags = local.tags
+  event_pattern = jsonencode({
+    "source" : ["aws.s3"],
+    "detail-type" : ["Object Created"],
+    "detail" : {
+      "bucket": {"name":  [module.s3-bucket.bucket.id]},
+      "object": {
+          "key": [{"prefix": "raw_data/"}]
+      }
+    }
+  })
+}
