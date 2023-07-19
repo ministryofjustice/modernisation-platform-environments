1d21be6a6cd12563dd9eec415ccced391fb7c878
W <willg258@hotmail.co.uk>
2023-07-19 18:00:48 +0100

    Update locals_security_groups.tf

--- terraform/environments/oasys/locals_security_groups.tf
+++ terraform/environments/oasys/locals_security_groups.tf
@@ -13,7 +13,6 @@ locals {
       module.ip_addresses.azure_fixngo_cidrs.internet_egress,
       module.ip_addresses.moj_cidrs.trusted_moj_digital_staff_public,
       module.ip_addresses.moj_cidr.aws_cloud_platform_vpc, # "172.20.0.0/16"
-      "146.200.228.107/32",
     ])
 
     http7xxx = flatten([
