f6b55b951056bfed17a74f7ac115aa845ec9298f
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-07-20 12:58:42 +0100

    remove null value

--- terraform/environments/nomis-data-hub/locals_test.tf
+++ terraform/environments/nomis-data-hub/locals_test.tf
@@ -7,7 +7,7 @@ locals {
         # ami has unwanted ephemeral device, don't copy all the ebs_volumess
         config = merge(module.baseline_presets.ec2_instance.config.default, {
           ami_name                      = "nomis_windows_server_2022_jumpserver_release_*"
-          availability_zone             = null
+          # availability_zone             = null
           ebs_volumes_copy_all_from_ami = false
           user_data_raw                 = base64encode(file("./templates/ndh-user-data.yaml"))
         })
