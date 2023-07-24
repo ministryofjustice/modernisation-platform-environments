7593a7668298d7b627b8fd965844b522b80a1a07
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-07-24 15:38:45 +0100

    Ignore Local Secrets

--- terraform/environments/digital-prison-reporting/modules/secrets_manager/outputs.tf
+++ terraform/environments/digital-prison-reporting/modules/secrets_manager/outputs.tf
@@ -8,8 +8,3 @@ output "secret_arn" {
   description = "The ARN of the secret"
   value       = aws_secretsmanager_secret.secret.arn
 }
-
-output "version_id" {
-  description = "The unique identifier of the version of the secret."
-  value       = aws_secretsmanager_secret_version.secret_val.version_id
-}
