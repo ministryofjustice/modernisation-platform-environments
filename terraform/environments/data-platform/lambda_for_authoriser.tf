d22f345a538a00b902d017046fea806e4777ffc9
Bogdan Mania <bogdan.mania@digital.justice.gov.uk>
2023-07-25 15:53:25 +0100

    move authorizer lambda to containers

--- terraform/environments/data-platform/lambda_for_authoriser.tf
+++ terraform/environments/data-platform/lambda_for_authoriser.tf
@@ -1,7 +1,43 @@
-data "archive_file" "authoriser_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/src/authoriser"
-  output_path = "${path.module}/src/authoriser_${local.environment}/authoriser_lambda.zip"
+data "aws_iam_policy_document" "iam_policy_document_for_authorizer_lambda" {
+  statement {
+    sid       = "LambdaLogGroup"
+    effect    = "Allow"
+    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:${local.region}:${local.account_id}:log-group:/aws/lambda/*"]
+  }
+}
+
+module "data_product_authorizer_lambda" {
+  source                         = "github.com/ministryofjustice/modernisation-platform-terraform-lambda-function?ref=v2.0.1"
+  application_name               = "data_product_authorizer"
+  tags                           = local.tags
+  description                    = "Lambda for custom API Gateway authorizer"
+  role_name                      = "authorizer_lambda_role_${local.environment}"
+  policy_json                    = data.aws_iam_policy_document.iam_policy_document_for_authorizer_lambda.json
+  function_name                  = "data_product_authorizer_${local.environment}"
+  create_role                    = true
+  reserved_concurrent_executions = 1
+
+  image_uri    = "374269020027.dkr.ecr.eu-west-2.amazonaws.com/data-platform-authorizer-lambda-ecr-repo:1.0.0"
+  timeout      = 600
+  tracing_mode = "Active"
+  memory_size  = 512
+
+  environment_variables = {
+    authorizationToken = "placeholder"
+    api_resource_arn   = "${aws_api_gateway_rest_api.data_platform.execution_arn}/*/*"
+  }
+
+  allowed_triggers = {
+
+    AllowExecutionFromAPIGateway = {
+      action        = "lambda:InvokeFunction"
+      function_name = "data_product_authorizer_${local.environment}"
+      principal     = "apigateway.amazonaws.com"
+      source_arn    = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.data_platform.id}/*/*"
+    }
+  }
+
 }
 
 data "aws_iam_policy_document" "apigateway_trust_policy" {
@@ -27,7 +63,7 @@ data "aws_iam_policy_document" "allow_invoke_authoriser_lambda_doc" {
   statement {
     effect    = "Allow"
     actions   = ["lambda:InvokeFunction"]
-    resources = [aws_lambda_function.authoriser.arn]
+    resources = [module.data_product_authorizer_lambda.lambda_function_arn]
   }
 }
 
@@ -41,36 +77,4 @@ resource "aws_iam_policy" "allow_invoke_authoriser_lambda" {
 resource "aws_iam_role_policy_attachment" "attach_allow_invoke_authoriser_lambda" {
   role       = aws_iam_role.authoriser_role.name
   policy_arn = aws_iam_policy.allow_invoke_authoriser_lambda.arn
-}
-
-resource "aws_iam_role" "authoriser_lambda_role" {
-  name               = "authoriser_lambda_role_${local.environment}"
-  assume_role_policy = data.aws_iam_policy_document.lambda_trust_policy_doc.json
-  tags               = local.tags
-}
-
-resource "aws_lambda_function" "authoriser" {
-  function_name    = "authoriser_${local.environment}"
-  description      = "api gateway authoriser"
-  handler          = "main.handler"
-  runtime          = local.lambda_runtime
-  filename         = data.archive_file.authoriser_zip.output_path
-  source_code_hash = data.archive_file.authoriser_zip.output_base64sha256
-  role             = aws_iam_role.authoriser_lambda_role.arn
-  environment {
-    variables = {
-      authorizationToken = "placeholder"
-      api_resource_arn   = "${aws_api_gateway_rest_api.data_platform.execution_arn}/*/*"
-    }
-  }
-  tags = local.tags
-}
-
-resource "aws_lambda_permission" "api_gw_authorizer" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.authoriser.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.data_platform.execution_arn}/*/*"
 }
