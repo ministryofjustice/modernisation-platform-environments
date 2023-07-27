04557ba8069bd7b1da7ec5801dfb048905bcfbe3
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-07-27 13:35:59 +0100

    Jitbit: remove on prem gateway from all environments

--- terraform/environments/delius-jitbit/onprem_gateway.tf
+++
@@ -1,9 +0,0 @@
-# Pre-req - security group
-resource "aws_security_group" "onprem_gateway" {
-  name        = "onprem-gateway"
-  description = "Controls access onprem gateway instance"
-  vpc_id      = data.aws_vpc.shared.id
-  tags = merge(local.tags,
-    { Name = lower(local.on_prem_dgw_name) }
-  )
-}
