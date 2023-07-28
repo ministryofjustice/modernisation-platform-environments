43227b90072c726b4f4c352ceed168e3037b3d3a
silvianahorga-al <silviana.horga@digital.justice.gov.uk>
2023-07-28 13:09:37 +0100

    Comment out creation of resources in dev2 env

--- terraform/environments/delius-core/main_development.tf
+++ terraform/environments/delius-core/main_development.tf
@@ -24,24 +24,24 @@ module "environment_dev" {
   tags = local.tags
 }
 
-module "environment_dev2" {
-  # We're in dev account and dev environment, could reference different version
-  source = "./modules/environment_all_components"
-  count  = local.environment == "development" ? 1 : 0
-
-  providers = {
-    aws.bucket-replication = aws
-    aws.core-vpc           = aws.core-vpc
-  }
-
-  env_name = "dev2"
-  app_name = local.application_name
-   
-  network_config = local.network_config_dev2
-  ldap_config = local.ldap_config_dev2
-  db_config   = local.db_config_dev2
-
-  account_info = local.account_info
-
-  tags = local.tags
-}
+#module "environment_dev2" {
+#  # We're in dev account and dev environment, could reference different version
+#  source = "./modules/environment_all_components"
+#  count  = local.environment == "development" ? 1 : 0
+#
+#  providers = {
+#    aws.bucket-replication = aws
+#    aws.core-vpc           = aws.core-vpc
+#  }
+#
+#  env_name = "dev2"
+#  app_name = local.application_name
+#   
+#  network_config = local.network_config_dev2
+#  ldap_config = local.ldap_config_dev2
+#  db_config   = local.db_config_dev2
+#
+#  account_info = local.account_info
+#
+#  tags = local.tags
+#}
