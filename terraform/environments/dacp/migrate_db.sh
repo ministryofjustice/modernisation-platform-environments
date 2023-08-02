0ff60a0488c7e57720e2d531218f689ceb84c13f
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-08-02 15:54:17 +0100

    try removing password from commands

--- terraform/environments/dacp/migrate_db.sh
+++ terraform/environments/dacp/migrate_db.sh
@@ -11,13 +11,16 @@ else
     echo "You reached the ELSE"
 fi
 
+export PGPASSWORD=$SOURCE_DB_PASSWORD;
 # psql -h ${DB_HOSTNAME} -p 5432 -U $DACP_DB_USERNAME -d $DB_NAME -c "CREATE SCHEMA IF NOT EXISTS dbo;";
-pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME --password $SOURCE_DB_PASSWORD -O --section=pre-data > pre-data.sql
+pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -O --section=pre-data > pre-data.sql
 # pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME --password $SOURCE_DB_PASSWORD -t 'dbo.*_seq' > sequences.sql
 # pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME --password $SOURCE_DB_PASSWORD -O --section=data > data.sql
 # pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME --password $SOURCE_DB_PASSWORD -O --section=post-data > post-data.sql
 
-psql -U $DACP_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME --password $DACP_DB_PASSWORD -f pre-data.sql
+export PGPASSWORD=$DACP_DB_PASSWORD;
+
+psql -U $DACP_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f pre-data.sql
 # psql -U $DACP_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME --password $DACP_DB_PASSWORD -f sequences.sql
 # psql -U $DACP_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME --password $DACP_DB_PASSWORD -f data.sql
 # psql -U $DACP_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME --password $DACP_DB_PASSWORD -f post-data.sql
