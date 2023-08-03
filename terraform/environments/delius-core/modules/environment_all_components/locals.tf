6edf07adf675fdb34a2840158c800995b5f68026
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-03 14:58:36 +0100

    context as map

--- terraform/environments/delius-core/modules/environment_all_components/locals.tf
+++ terraform/environments/delius-core/modules/environment_all_components/locals.tf
@@ -23,25 +23,30 @@ locals {
 
   certificate_arn = aws_acm_certificate.external.arn
 
-  context = jsonencode({
-    "additional_tag_map" : {},
-    "attributes" : [],
-    "delimiter" : null,
-    "descriptor_formats" : {},
-    "enabled" : true,
-    "environment" : null,
-    "id_length_limit" : null,
-    "label_key_case" : null,
-    "label_order" : [],
-    "label_value_case" : null,
-    "labels_as_tags" : [
-      "unset"
-    ],
-    "name" : null,
-    "namespace" : "${var.app_name}-${var.env_name}",
-    "regex_replace_chars" : null,
-    "stage" : null,
-    "tags" : {},
-    "tenant" : null
-  })
+  context = {
+    enabled             = true
+    namespace           = "${var.app_name}-${var.env_name}"
+    tenant              = null
+    environment         = null
+    name                = null
+    delimiter           = null
+    stage               = null
+    attributes          = []
+    tags                = {}
+    additional_tag_map  = {}
+    regex_replace_chars = null
+    label_order         = []
+    id_length_limit     = null
+    label_key_case      = null
+    label_value_case    = null
+    descriptor_formats  = {}
+    # Note: we have to use [] instead of null for unset lists due to
+    # https://github.com/hashicorp/terraform/issues/28137
+    # which was not fixed until Terraform 1.0.0,
+    # but we want the default to be all the labels in `label_order`
+    # and we want users to be able to prevent all tag generation
+    # by setting `labels_as_tags` to `[]`, so we need
+    # a different sentinel to indicate "default"
+    labels_as_tags = ["unset"]
+  }
 }
