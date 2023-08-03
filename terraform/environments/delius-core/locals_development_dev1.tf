7e846ecf8468b99a4d054c699f7b6ccdca5df457
George Taylor <george.taylor@digital.justice.gov.uk>
2023-08-03 11:11:50 +0100

    typo

--- terraform/environments/delius-core/locals_development_dev1.tf
+++ terraform/environments/delius-core/locals_development_dev1.tf
@@ -15,7 +15,7 @@ locals {
   }
 
   ldap_config_dev = {
-    name                        = try(local.ldap_config_lower_environments.name, "lda5p")
+    name                        = try(local.ldap_config_lower_environments.name, "ldap")
     migration_source_account_id = local.ldap_config_lower_environments.migration_source_account_id
     migration_lambda_role       = local.ldap_config_lower_environments.migration_lambda_role
     efs_throughput_mode         = local.ldap_config_lower_environments.efs_throughput_mode
