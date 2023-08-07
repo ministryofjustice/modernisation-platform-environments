2d436dc4fc372210c581d33132242f56a2b47c8b
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-08-07 14:33:38 +0100

    Updates to locals env lower

--- terraform/environments/delius-core/locals_environments_lower.tf
+++ terraform/environments/delius-core/locals_environments_lower.tf
@@ -7,6 +7,27 @@ locals {
     efs_provisioned_throughput  = null
   }
 
+  db_config_lower_environments = {
+    name     = "core-db"
+    ami_name = "delius_core_ol_8_5_oracle_db_19c_patch_2023-06-12T12-32-07.259Z"
+    instance = {
+      disable_api_termination      = true
+      instance_type                = "r6i.xlarge"
+      key_name                     = "key_name_here"
+      metadata_endpoint_enabled    = "enabled"
+      metadata_options_http_tokens = "required"
+      associate_public_ip_address  = false
+      monitoring                   = true
+      ebs_block_device_inline      = false
+      vpc_security_group_ids       = []
+      private_dns_name_options = {
+        enable_resource_name_dns_aaaa_record = false
+        enable_resource_name_dns_a_record    = true
+        hostname_type                        = "ip-name"
+      }
+    }
+  }
+
   weblogic_config_lower_environments = {
     name                          = "weblogic_for_lower_environments"
     frontend_service_name         = "weblogic"
@@ -15,10 +36,4 @@ locals {
     frontend_container_port       = 8080
     frontend_url_suffix           = "${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   }
-
-  db_config_lower_environments = {
-    name     = "db_for_lower_environments"
-    ami_name = "delius_core_ol_8_5_oracle_db_19c_patch_2023-06-12T12-32-07.259Z"
-  }
-
 }
