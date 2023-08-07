b2e6bb001d7af9118f42a5074c9bfa2e667dfc49
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-08-07 14:35:58 +0100

    Updates to test locals

--- terraform/environments/delius-core/locals_test_test1.tf
+++ terraform/environments/delius-core/locals_test_test1.tf
@@ -9,7 +9,33 @@ locals {
   }
 
   db_config_test1 = {
-    name                 = "db"
-    some_other_attribute = "some_other_attribute_for_db_in_test1"
+    name      = "db"
+    ami_name  = local.db_config_lower_environments.ami_name
+    ami_owner = local.environment_management.account_ids["core-shared-services-production"]
+    user_data_raw = base64encode(
+      templatefile(
+        "${path.module}/templates/userdata.sh.tftpl",
+        {
+          branch               = local.db_config.user_data_param.branch
+          ansible_repo         = local.db_config.user_data_param.ansible_repo
+          ansible_repo_basedir = local.db_config.user_data_param.ansible_repo_basedir
+          ansible_args         = local.db_config.user_data_param.ansible_args
+        }
+      )
+    )
+    instance = merge(local.db_config_lower_environments.instance, {
+      instance_type = "r6i.xlarge"
+      monitoring    = false
+    })
+    ebs_volume_config = {}
+    ebs_volumes       = {}
+    route53_records = {
+      create_internal_record = true
+      create_external_record = false
+    }
+    tags = merge(local.tags_all,
+      { Name = lower(format("ec2-%s-%s-base-ami-test-instance", local.application_name, local.environment)) },
+      { server-type = "delius_core_db" }
+    )
   }
 }
