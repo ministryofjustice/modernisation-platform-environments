2e77aefc03cb6e76b9f2c8a2713e729b20c1f848
Vladimirs Kovalovs <vladimirs.kovalovs@digital.justice.gov.uk>
2023-08-07 17:30:10 +0100

    LAWS-3315: added .aws to all domain names for route53 records

--- terraform/environments/portal/route53.tf
+++ terraform/environments/portal/route53.tf
@@ -12,7 +12,7 @@
 resource "aws_route53_record" "oam_internal" {
   provider = aws.core-network-services
   zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
-  name     = "${local.application_name}-oam-internal.${data.aws_route53_zone.portal-dev-private.name}"
+  name     = "${local.application_name}-oam-internal.aws.${data.aws_route53_zone.portal-dev-private.name}"
   type     = "A"
 
   alias {
@@ -25,7 +25,7 @@ resource "aws_route53_record" "oam_internal" {
 resource "aws_route53_record" "oam_console" {
   provider = aws.core-network-services
   zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
-  name     = "${local.application_name}-oam-console.${data.aws_route53_zone.portal-dev-private.name}"
+  name     = "${local.application_name}-oam-console.aws.${data.aws_route53_zone.portal-dev-private.name}"
   type     = "A"
 
   alias {
@@ -38,7 +38,7 @@ resource "aws_route53_record" "oam_console" {
 resource "aws_route53_record" "oam_admin" {
   provider = aws.core-network-services
   zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
-  name     = "${local.application_name}-oam-admin.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-oam2-ms.aws.dev.legalservices.gov.uk
+  name     = "${local.application_name}-oam-admin.aws.${data.aws_route53_zone.portal-dev-private.name}"
   type     = "A"
   ttl      = 60
   records  = [aws_instance.oam_instance_1.private_ip]
@@ -47,7 +47,7 @@ resource "aws_route53_record" "oam_admin" {
 resource "aws_route53_record" "oam1_nonprod" {
   provider = aws.core-network-services
   zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
-  name     = "${local.application_name}-oam1-ms.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-oam1-ms.aws.dev.legalservices.gov.uk
+  name     = "${local.application_name}-oam1-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-oam1-ms.aws.[env].legalservices.gov.uk
   type     = "A"
   ttl      = 60
   records  = [aws_instance.oam_instance_1.private_ip]
@@ -57,9 +57,231 @@ resource "aws_route53_record" "oam2_prod" {
   count    = contains(["development", "testing"], local.environment) ? 0 : 1
   provider = aws.core-network-services
   zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
-  name     = "${local.application_name}-oam2-ms.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-oam2-ms.aws.dev.legalservices.gov.uk
+  name     = "${local.application_name}-oam2-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-oam2-ms.aws.[env].legalservices.gov.uk
   type     = "A"
   ttl      = 60
   records  = [aws_instance.oam_instance_2[0].private_ip]
 }
 
+
+###############################################################################################################
+############################              OIM Route 53 records                  ###############################
+###############################################################################################################
+
+### http://portal-oim-internal.aws.prd.legalservices.gov.uk:80 → Now points to Internal ALB to OHS
+### https://portal-oim-console.aws.prd.legalservices.gov.uk:443
+### portal-oim-admin.aws.prd.legalservices.gov.uk
+### portal-oim1-ms.aws.prd.legalservices.gov.uk
+### portal-oim2-ms.aws.prd.legalservices.gov.uk
+### portal-bip1-ms.aws.prd.legalservices.gov.uk
+### portal-bip2-ms.aws.prd.legalservices.gov.uk
+### portal-soa1-ms.aws.prd.legalservices.gov.uk
+### portal-soa2-ms.aws.prd.legalservices.gov.uk
+
+
+resource "aws_route53_record" "oim_internal" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-oim-internal.aws.${data.aws_route53_zone.portal-dev-private.name}"
+  type     = "A"
+
+  alias {
+    name                   = aws_lb.internal.dns_name
+    zone_id                = aws_lb.internal.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "oim_console" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-oim-console.aws.${data.aws_route53_zone.portal-dev-private.name}"
+  type     = "A"
+
+  alias {
+    name                   = aws_lb.internal.dns_name
+    zone_id                = aws_lb.internal.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "oim_admin" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-oim-admin.aws.${data.aws_route53_zone.portal-dev-private.name}" 
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.oim_instance_1.private_ip]
+}
+
+resource "aws_route53_record" "oim1_nonprod" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-oim1-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-oim1-ms.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.oim_instance_1.private_ip]
+}
+
+resource "aws_route53_record" "oim2_prod" {
+  count    = contains(["development", "testing"], local.environment) ? 0 : 1
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-oim2-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-oim2-ms.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.oim_instance_2[0].private_ip]
+}
+
+resource "aws_route53_record" "bip1_nonprod" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-bip1-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-bip1-ms.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.oim_instance_1.private_ip]
+}
+
+resource "aws_route53_record" "bip2_prod" {
+  count    = contains(["development", "testing"], local.environment) ? 0 : 1
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-bip2-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-bip2-ms.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.oim_instance_2[0].private_ip]
+}
+
+resource "aws_route53_record" "soa1_nonprod" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-soa1-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-soa1-ms.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.oim_instance_1.private_ip]
+}
+
+resource "aws_route53_record" "soa2_prod" {
+  count    = contains(["development", "testing"], local.environment) ? 0 : 1
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-soa2-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-soa2-ms.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.oim_instance_2[0].private_ip]
+}
+
+
+###############################################################################################################
+############################              OID Route 53 records                  ###############################
+###############################################################################################################
+
+### portal-oid.aws.prd.legalservices.gov.uk
+
+
+resource "aws_route53_record" "oid_internal" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-oid.aws.${data.aws_route53_zone.portal-dev-private.name}"
+  type     = "A"
+
+  alias {
+    name                   = aws_lb.internal.dns_name
+    zone_id                = aws_lb.internal.zone_id
+    evaluate_target_health = true
+  }
+}
+
+
+###############################################################################################################
+#########################              IDM / ODS Route 53 records               ###############################
+###############################################################################################################
+
+### http://portal-idm-console.aws.prd.legalservices.gov.uk → Now points to Internal ALB to OHS
+### portal-idm-admin.aws.prd.legalservices.gov.uk
+### portal-ods1-ms.aws.prd.legalservices.gov.uk
+### portal-ods2-ms.aws.prd.legalservices.gov.uk
+
+
+resource "aws_route53_record" "idm_console" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-idm-console.aws.${data.aws_route53_zone.portal-dev-private.name}"
+  type     = "A"
+
+  alias {
+    name                   = aws_lb.internal.dns_name
+    zone_id                = aws_lb.internal.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "idm_admin" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-idm-admin.aws.${data.aws_route53_zone.portal-dev-private.name}" 
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.idm_instance_1.private_ip]
+}
+
+resource "aws_route53_record" "ods1_nonprod" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-ods1-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-ods1-ms.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.idm_instance_1.private_ip]
+}
+
+resource "aws_route53_record" "ods2_prod" {
+  count    = contains(["development", "testing"], local.environment) ? 0 : 1
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-ods2-ms.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-ods2-ms.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.idm_instance_2[0].private_ip]
+}
+
+
+###############################################################################################################
+############################              OHS Route 53 records                  ###############################
+###############################################################################################################
+
+# portal-ohs-internal.aws.prd.legalservices.gov.uk
+# portal-ohs1.aws.prd.legalservices.gov.uk
+# portal-ohs2.aws.prd.legalservices.gov.uk
+
+
+resource "aws_route53_record" "ohs_internal" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-ohs-internal.aws.${data.aws_route53_zone.portal-dev-private.name}"
+  type     = "A"
+
+  alias {
+    name                   = aws_lb.internal.dns_name
+    zone_id                = aws_lb.internal.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "ohs1_nonprod" {
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-ohs1.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-ohs1.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.ohs_instance_1.private_ip]
+}
+
+resource "aws_route53_record" "ods2_prod" {
+  count    = contains(["development", "testing"], local.environment) ? 0 : 1
+  provider = aws.core-network-services
+  zone_id  = data.aws_route53_zone.portal-dev-private.zone_id
+  name     = "${local.application_name}-ohs2.aws.${data.aws_route53_zone.portal-dev-private.name}" # Correspond to portal-ohs2.aws.[env].legalservices.gov.uk
+  type     = "A"
+  ttl      = 60
+  records  = [aws_instance.ohs_instance_2[0].private_ip]
+}
