df58b866f15815d23df3b94b79baf26cf2ee0401
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-08-08 15:51:40 +0100

    Use updated oracle 19c code

--- terraform/environments/nomis-combined-reporting/locals_database.tf
+++ terraform/environments/nomis-combined-reporting/locals_database.tf
@@ -3,77 +3,34 @@ locals {
   oracle_ec2_default = {
 
     config = merge(module.baseline_presets.ec2_instance.config.db, {
-      ami_name  = "hmpps_ol_8_5_oracledb_19c_release_2023-07-14T15-36-30.795Z"
+      ami_name  = "hmpps_ol_8_5_oracledb_19c_release_2023-08-08T13-49-56.195Z"
       ami_owner = "self"
     })
 
-    instance              = module.baseline_presets.ec2_instance.instance.default_db
-    autoscaling_schedules = {}
-    autoscaling_group     = module.baseline_presets.ec2_autoscaling_group.default
-    user_data_cloud_init  = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_ansible_no_tags
+    instance = module.baseline_presets.ec2_instance.instance.default_db
+
+    user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
 
     ebs_volumes = {
-      "/dev/sdb" = { # /u01
-        size  = 100
-        label = "app"
-        type  = "gp3"
-      }
-      "/dev/sdc" = { # /u02
-        size  = 500
-        label = "app"
-        type  = "gp3"
-      }
-      "/dev/sde" = { # DATA01
-        label = "data"
-        size  = 500
-        type  = "gp3"
-      }
-      # "/dev/sdf" = {  # DATA02
-      #   label = "data"
-      #   type = null
-      # }
-      # "/dev/sdg" = {  # DATA03
-      #   label = "data"
-      #   type = null
-      # }
-      # "/dev/sdh" = {  # DATA04
-      #   label = "data"
-      #   type = null
-      # }
-      # "/dev/sdi" = {  # DATA05
-      #   label = "data"
-      #   type = null
-      # }
-      "/dev/sdj" = { # FLASH01
-        label = "flash"
-        type  = "gp3"
-        size  = 50
-      }
-      # "/dev/sdk" = { # FLASH02
-      #   label = "flash"
-      #   type = null
-      # }
-      "/dev/sds" = {
-        label = "swap"
-        type  = "gp3"
-        size  = 2
-      }
+      "/dev/sdb" = { type = "gp3", label = "app", size = 100 } # /u01
+      "/dev/sdc" = { type = "gp3", label = "app", size = 100 } # /u02
+      "/dev/sde" = { type = "gp3", label = "data" }            # DATA01
+      "/dev/sdf" = { type = "gp3", label = "data" }            # DATA02
+      "/dev/sdg" = { type = "gp3", label = "data" }            # DATA03
+      "/dev/sdh" = { type = "gp3", label = "data" }            # DATA04
+      "/dev/sdi" = { type = "gp3", label = "data" }            # DATA05
+      "/dev/sdj" = { type = "gp3", label = "flash" }           # FLASH01
+      "/dev/sdk" = { type = "gp3", label = "flash" }           # FLASH02
+      "/dev/sds" = { type = "gp3", label = "swap" }
     }
+
     ebs_volume_config = {
-      data = {
-        iops       = 3000
-        type       = "gp3"
-        throughput = 125
-        total_size = 200
-      }
-      flash = {
-        iops       = 3000
-        type       = "gp3"
-        throughput = 125
-        total_size = 50
-      }
+      data  = { total_size = 100 }
+      flash = { total_size = 50 }
     }
+
     route53_records = module.baseline_presets.ec2_instance.route53_records.internal_and_external
+
     ssm_parameters = {
       ASMSYS = {
         random = {
@@ -90,8 +47,6 @@ locals {
         description = "ASMSNMP password"
       }
     }
-    # Example target group setup below
-    lb_target_groups = {}
 
     tags = {
       ami                  = "hmpps_ol_8_5_oracledb_19c"
