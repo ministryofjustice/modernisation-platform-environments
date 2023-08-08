83cf35b7d981edbb4b21e00cae5d6e933c574350
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-08-08 18:04:25 +0100

    revert db migration (#3039)

--- terraform/environments/tipstaff/migrate_db.sh
+++ terraform/environments/tipstaff/migrate_db.sh
@@ -5,29 +5,18 @@ export PGPASSWORD=$TIPSTAFF_DB_PASSWORD;
 SCHEMA=$(psql -h ${DB_HOSTNAME} -p 5432 -U $TIPSTAFF_DB_USERNAME -d $DB_NAME -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'dbo'" | grep -o 'dbo')
 echo "Schema = $SCHEMA"
 
-# if [ "$SCHEMA" == "dbo" ]; then
-#     echo "The Schema dbo is already present in the database"
-# else
-#     export PGPASSWORD=$SOURCE_DB_PASSWORD;
-#     pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -O --section=pre-data > pre-data.sql
-#     pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -t 'dbo.*_seq' > sequences.sql
-#     pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -O --section=data > data.sql
-#     pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -O --section=post-data > post-data.sql
+if [ "$SCHEMA" == "dbo" ]; then
+    echo "The Schema dbo is already present in the database"
+else
+    export PGPASSWORD=$SOURCE_DB_PASSWORD;
+    pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -O --section=pre-data > pre-data.sql
+    pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -t 'dbo.*_seq' > sequences.sql
+    pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -O --section=data > data.sql
+    pg_dump -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -O --section=post-data > post-data.sql
 
-#     export PGPASSWORD=$TIPSTAFF_DB_PASSWORD;
-#     psql -U $TIPSTAFF_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f pre-data.sql
-#     psql -U $TIPSTAFF_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f sequences.sql
-#     psql -U $TIPSTAFF_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f data.sql
-#     psql -U $TIPSTAFF_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f post-data.sql
-# fi
-
-export PGPASSWORD=$SOURCE_DB_PASSWORD;
-pg_dump -U $SOURCE_DB_USERNAME -h $PROD_DB_HOSTNAME -d $PROD_DB_NAME -O --section=pre-data > pre-data.sql
-pg_dump -U $SOURCE_DB_USERNAME -h $PROD_DB_HOSTNAME -d $PROD_DB_NAME -t 'dbo.*_seq' > sequences.sql
-pg_dump -U $SOURCE_DB_USERNAME -h $PROD_DB_HOSTNAME -d $PROD_DB_NAME -O --section=data > data.sql
-pg_dump -U $SOURCE_DB_USERNAME -h $PROD_DB_HOSTNAME -d $PROD_DB_NAME -O --section=post-data > post-data.sql
-
-psql -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -f pre-data.sql
-psql -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -f sequences.sql
-psql -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -f data.sql
-psql -U $SOURCE_DB_USERNAME -h $SOURCE_DB_HOSTNAME -d $SOURCE_DB_NAME -f post-data.sql
+    export PGPASSWORD=$TIPSTAFF_DB_PASSWORD;
+    psql -U $TIPSTAFF_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f pre-data.sql
+    psql -U $TIPSTAFF_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f sequences.sql
+    psql -U $TIPSTAFF_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f data.sql
+    psql -U $TIPSTAFF_DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -f post-data.sql
+fi
