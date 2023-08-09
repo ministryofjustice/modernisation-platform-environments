bb53b4234e8201e76212fe465806f20aa943a3f1
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-08-09 16:03:24 +0100

    uncomment dacp null resources

--- terraform/environments/dacp/rds.tf
+++ terraform/environments/dacp/rds.tf
@@ -82,43 +82,43 @@ data "http" "myip" {
   url = "http://ipinfo.io/json"
 }
 
-# resource "null_resource" "setup_db" {
-#   depends_on = [aws_db_instance.dacp_db]
+resource "null_resource" "setup_db" {
+  depends_on = [aws_db_instance.dacp_db]
 
-#   provisioner "local-exec" {
-#     interpreter = ["bash", "-c"]
-#     command     = "chmod +x ./migrate_db.sh; ./migrate_db.sh"
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command     = "chmod +x ./migrate_db.sh; ./migrate_db.sh"
 
-#     environment = {
-#       SOURCE_DB_HOSTNAME = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_HOSTNAME"]
-#       SOURCE_DB_NAME     = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_NAME"]
-#       SOURCE_DB_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_USERNAME"]
-#       SOURCE_DB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_PASSWORD"]
-#       DB_HOSTNAME        = aws_db_instance.dacp_db.address
-#       DB_NAME            = aws_db_instance.dacp_db.db_name
-#       DACP_DB_USERNAME   = local.application_data.accounts[local.environment].db_username
-#       DACP_DB_PASSWORD   = random_password.password.result
-#     }
-#   }
-#   triggers = {
-#     always_run = "${timestamp()}"
-#   }
-# }
+    environment = {
+      SOURCE_DB_HOSTNAME = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_HOSTNAME"]
+      SOURCE_DB_NAME     = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_NAME"]
+      SOURCE_DB_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_USERNAME"]
+      SOURCE_DB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_PASSWORD"]
+      DB_HOSTNAME        = aws_db_instance.dacp_db.address
+      DB_NAME            = aws_db_instance.dacp_db.db_name
+      DACP_DB_USERNAME   = local.application_data.accounts[local.environment].db_username
+      DACP_DB_PASSWORD   = random_password.password.result
+    }
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
 
-# // executes a local script to set up the security group for the source RDS instance.
-# resource "null_resource" "setup_source_rds_security_group" {
-#   provisioner "local-exec" {
-#     interpreter = ["bash", "-c"]
-#     command     = "chmod +x ./setup-security-group-${local.environment}.sh; ./setup-security-group-${local.environment}.sh"
+// executes a local script to set up the security group for the source RDS instance.
+resource "null_resource" "setup_source_rds_security_group" {
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command     = "chmod +x ./setup-security-group-${local.environment}.sh; ./setup-security-group-${local.environment}.sh"
 
-#     environment = {
-#       RDS_SECURITY_GROUP            = aws_security_group.modernisation_dacp_access.id
-#       RDS_SOURCE_ACCOUNT_ACCESS_KEY = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["ACCESS_KEY"]
-#       RDS_SOURCE_ACCOUNT_SECRET_KEY = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SECRET_KEY"]
-#       RDS_SOURCE_ACCOUNT_REGION     = "eu-west-2"
-#     }
-#   }
-#   triggers = {
-#     always_run = "${timestamp()}"
-#   }
-# }
+    environment = {
+      RDS_SECURITY_GROUP            = aws_security_group.modernisation_dacp_access.id
+      RDS_SOURCE_ACCOUNT_ACCESS_KEY = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["ACCESS_KEY"]
+      RDS_SOURCE_ACCOUNT_SECRET_KEY = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SECRET_KEY"]
+      RDS_SOURCE_ACCOUNT_REGION     = "eu-west-2"
+    }
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
