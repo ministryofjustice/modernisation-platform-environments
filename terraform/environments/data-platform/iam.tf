40e9a026a2a530a7918c2b7c95504595a3cc10b6
Bogdan Mania <bogdan.mania@digital.justice.gov.uk>
2023-08-09 15:06:37 +0100

    move athena load lambda fuction to use the module

--- terraform/environments/data-platform/iam.tf
+++ terraform/environments/data-platform/iam.tf
@@ -157,32 +157,6 @@ data "aws_iam_policy_document" "iam_policy_document_for_authorizer_lambda" {
   }
 }
 
-resource "aws_iam_role" "authoriser_role" {
-  name               = "authoriser_role_${local.environment}"
-  assume_role_policy = data.aws_iam_policy_document.apigateway_trust_policy.json
-  tags               = local.tags
-}
-
-data "aws_iam_policy_document" "allow_invoke_authoriser_lambda_doc" {
-  statement {
-    effect    = "Allow"
-    actions   = ["lambda:InvokeFunction"]
-    resources = [module.data_product_authorizer_lambda.lambda_function_arn]
-  }
-}
-
-resource "aws_iam_policy" "allow_invoke_authoriser_lambda" {
-  name   = "allow_invoke_authoriser_lambda"
-  path   = "/"
-  policy = data.aws_iam_policy_document.allow_invoke_authoriser_lambda_doc.json
-  tags   = local.tags
-}
-
-resource "aws_iam_role_policy_attachment" "attach_allow_invoke_authoriser_lambda" {
-  role       = aws_iam_role.authoriser_role.name
-  policy_arn = aws_iam_policy.allow_invoke_authoriser_lambda.arn
-}
-
 data "aws_iam_policy_document" "iam_policy_document_for_get_glue_metadata_lambda" {
   statement {
     sid     = "GlueReadOnly"
@@ -209,6 +183,47 @@ data "aws_iam_policy_document" "iam_policy_document_for_presigned_url_lambda" {
     actions   = ["s3:GetObject", "s3:PutObject"]
     resources = ["${module.s3-bucket.bucket.arn}/raw_data/*"]
   }
+}
+
+# API Gateway authoriser IAM permissions
+
+data "aws_iam_policy_document" "apigateway_trust_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "authoriser_role" {
+  name               = "authoriser_role_${local.environment}"
+  assume_role_policy = data.aws_iam_policy_document.apigateway_trust_policy.json
+  tags               = local.tags
+}
+
+data "aws_iam_policy_document" "allow_invoke_authoriser_lambda_doc" {
+  statement {
+    effect    = "Allow"
+    actions   = ["lambda:InvokeFunction"]
+    resources = [module.data_product_authorizer_lambda.lambda_function_arn]
+  }
+}
+
+resource "aws_iam_policy" "allow_invoke_authoriser_lambda" {
+  name   = "allow_invoke_authoriser_lambda"
+  path   = "/"
+  policy = data.aws_iam_policy_document.allow_invoke_authoriser_lambda_doc.json
+  tags   = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "attach_allow_invoke_authoriser_lambda" {
+  role       = aws_iam_role.authoriser_role.name
+  policy_arn = aws_iam_policy.allow_invoke_authoriser_lambda.arn
 }
 
 # S3 policy
@@ -249,64 +264,4 @@ data "aws_iam_policy_document" "data_platform_product_bucket_policy_document" {
     }
   }
 
-}
-
-# Lambda trust policies
-# TODO: remove duplicate and unused resources
-
-data "aws_iam_policy_document" "lambda_trust_policy_doc" {
-  statement {
-    sid     = "LambdaAssumeRole"
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "apigateway_trust_policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["apigateway.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-# IAM resources for athena load lambda function
-# TODO: move the lambda function to using the lambda module and remove the resources below
-
-resource "aws_iam_role" "athena_load_lambda_role" {
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-  name               = "athena_load_lambda_role_${local.environment}"
-  tags               = local.tags
-}
-
-resource "aws_iam_policy" "athena_load_lambda_function_policy" {
-  name   = "athena_load_lambda_function_policy"
-  policy = data.aws_iam_policy_document.athena_load_lambda_function_policy.json
-  tags   = local.tags
-}
-
-resource "aws_iam_role_policy_attachment" "policy_from_json" {
-  role       = aws_iam_role.athena_load_lambda_role.name
-  policy_arn = aws_iam_policy.athena_load_lambda_function_policy.arn
 }
