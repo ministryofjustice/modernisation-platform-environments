cf3a574f52581fd960778c9bdd85ed80cea3626f
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-08-09 16:14:02 +0100

    CC-1993: Fixed typos.

--- terraform/environments/ccms-ebs/templates/ec2_user_data_ftp.sh
+++ terraform/environments/ccms-ebs/templates/ec2_user_data_ftp.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 exec > /tmp/userdata.log 2>&1
 
+amazon-linux-extras install -y epel
 yum install -y wget unzip vsftpd jq s3fs-fuse
-
 yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
 
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/oracle_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 rpm -U ./amazon-cloudwatch-agent.rpm
-amazon-linux-extras install -y epel
 
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:cloud-watch-config
 
@@ -30,9 +30,9 @@ systemctl restart vsftpd.service
 cat > /etc/mount_s3.sh <<- EOM
 #!/bin/bash
 
-B=(laa-ccms-inbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment} laa-ccms-outbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment} laa-cis-outbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment} laa-cis-inbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment} bacway-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment}-eu-west-2-${local.application_data.accounts[local.environment].lz_aws_account_id_env})
+B=(laa-ccms-inbound-${lz_ftp_bucket_environment} laa-ccms-outbound-${lz_ftp_bucket_environment} laa-cis-outbound-${lz_ftp_bucket_environment} laa-cis-inbound-${lz_ftp_bucket_environment} bacway-${lz_ftp_bucket_environment}-eu-west-2-${lz_aws_account_id_env})
 
-C=\$(aws secretsmanager get-secret-value --secret-id ftp-s3-${local.environment}-aws-key --region eu-west-2)
+C=\$(aws secretsmanager get-secret-value --secret-id ftp-s3-${environment}-aws-key --region eu-west-2)
 K=\$(jq -r '.SecretString' <<< \$${C} |cut -d'"' -f2)
 S=\$(jq -r '.SecretString' <<< \$${C} |cut -d'"' -f4)
 U=\$(id -u s3xfer)
@@ -62,8 +62,8 @@ for b in "\$${B[@]}"; do
   fi
 done
 
-ln -s /s3xfer/S3/laa-ccms-inbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment}/CCMS_PRD_TDX/Inbound /home/s3xfer/CCMS_PRD_TDX_Inbound
-ln -s /s3xfer/S3/laa-ccms-outbound-${local.application_data.accounts[local.environment].lz_ftp_bucket_environment}/CCMS_PRD_TDX/Outbound /home/s3xfer/CCMS_PRD_TDX_Outbound
+ln -s /s3xfer/S3/laa-ccms-inbound-${lz_ftp_bucket_environment}/CCMS_PRD_TDX/Inbound /home/s3xfer/CCMS_PRD_TDX_Inbound
+ln -s /s3xfer/S3/laa-ccms-outbound-${lz_ftp_bucket_environment}/CCMS_PRD_TDX/Outbound /home/s3xfer/CCMS_PRD_TDX_Outbound
 
 chown -h s3xfer:s3xfer /home/s3xfer/CCMS_PRD_TDX_Inbound
 chown -h s3xfer:s3xfer /home/s3xfer/CCMS_PRD_TDX_Outbound
