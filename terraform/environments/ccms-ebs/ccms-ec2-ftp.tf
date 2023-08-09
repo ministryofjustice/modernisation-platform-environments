e180f711d06c167a168961a3c967348612fdd1ca
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-08-09 15:48:52 +0100

    CC-1993: Fixed typos.

--- terraform/environments/ccms-ebs/ccms-ec2-ftp.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-ftp.tf
@@ -21,8 +21,8 @@ resource "aws_instance" "ec2_ftp" {
   user_data_replace_on_change = false
   user_data = base64encode(templatefile("./templates/ec2_user_data_ftp.sh", {
     environment = "${local.environment}"
-    lz_aws_account_id_env = "${local.application_data.accounts[${local.environment}].lz_aws_account_id_env}"
-    lz_ftp_bucket_environment = "${local.application_data.accounts[${local.environment}].lz_ftp_bucket_environment}"
+    lz_aws_account_id_env = "${local.application_data.accounts[{local.environment}].lz_aws_account_id_env}"
+    lz_ftp_bucket_environment = "${local.application_data.accounts[{local.environment}].lz_ftp_bucket_environment}"
     hostname = "ftp"
   }))
 
