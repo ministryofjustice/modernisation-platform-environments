95e3bb9bad6ba07d36aeb8f5648eb083b2c9e3dc
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-08-09 16:02:18 +0100

    update security group scripts

--- terraform/environments/ncas/setup-security-group-development.sh
+++ terraform/environments/ncas/setup-security-group-development.sh
@@ -9,4 +9,4 @@ aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile dts-l
 aws configure set region "$AWS_DEFAULT_REGION" --profile dts-legacy-apps-user &&
 aws configure set output "json" --profile dts-legacy-apps-user
 
-aws rds modify-db-instance --db-instance-identifier postgresql-dev --vpc-security-group-ids sg-08244ba362f922899 sg-0e0f5cf0883f81945 sg-02938dce60af69c14 sg-05ea046c ${RDS_SECURITY_GROUP} --profile dts-legacy-apps-user
+aws rds modify-db-instance --db-instance-identifier postgresql-11-dev --vpc-security-group-ids sg-08244ba362f922899 sg-0e0f5cf0883f81945 sg-05ea046c ${RDS_SECURITY_GROUP} --profile dts-legacy-apps-user
--- terraform/environments/ncas/setup-security-group-preproduction.sh
+++ terraform/environments/ncas/setup-security-group-preproduction.sh
@@ -9,4 +9,4 @@ aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile dts-l
 aws configure set region "$AWS_DEFAULT_REGION" --profile dts-legacy-apps-user &&
 aws configure set output "json" --profile dts-legacy-apps-user
 
-aws rds modify-db-instance --db-instance-identifier postgresql-staging --vpc-security-group-ids sg-08244ba362f922899 sg-0e0f5cf0883f81945 sg-04e9fe073afcc6b65 ${RDS_SECURITY_GROUP} --profile dts-legacy-apps-user
+aws rds modify-db-instance --db-instance-identifier postgresql-11-staging --vpc-security-group-ids sg-08244ba362f922899 sg-0e0f5cf0883f81945 sg-05ea046c ${RDS_SECURITY_GROUP} --profile dts-legacy-apps-user
