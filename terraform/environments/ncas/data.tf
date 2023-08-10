e65e1b64cc1288e5418763f6c0a659d1becc7564
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2023-08-10 15:28:41 +0100

    NCAS - add initial infrastructure (#3045)

--- terraform/environments/ncas/data.tf
+++ terraform/environments/ncas/data.tf
@@ -1,1 +1,6 @@
 #### This file can be used to store data specific to the member account ####
+# data "aws_route53_zone" "application_zone" {
+#   provider     = aws.core-network-services
+#   name         = "neutral-citation-allocation-system.service.justice.gov.uk."
+#   private_zone = false
+# }
