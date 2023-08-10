912c41e8dae8a6797c4e0071cdb47412f143bdba
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-08-10 11:51:36 +0100

    Jitbit: manage master password using Secrets Manager (#3059)

--- terraform/environments/delius-jitbit/secrets.tf
+++ terraform/environments/delius-jitbit/secrets.tf
@@ -1,33 +1,4 @@
 ##
-# Create password for rds master user
-##
-resource "random_password" "db_admin_password" {
-  length  = 30
-  lower   = true
-  upper   = true
-  special = false
-}
-
-#tfsec:ignore:aws-ssm-secret-use-customer-key
-resource "aws_secretsmanager_secret" "db_admin_password" {
-  #checkov:skip=CKV_AWS_149
-  name                    = "${var.networking[0].application}-db-admin-password"
-  recovery_window_in_days = 0
-  tags = merge(
-    local.tags,
-    {
-      Name = "${var.networking[0].application}-db-admin-password"
-    },
-  )
-}
-
-resource "aws_secretsmanager_secret_version" "db_admin_password" {
-  secret_id     = aws_secretsmanager_secret.db_admin_password.id
-  secret_string = random_password.db_admin_password.result
-}
-
-
-##
 # Create connection string for app
 ##
 #tfsec:ignore:aws-ssm-secret-use-customer-key
