eceb078b38755e283d0ed893805fa1a007fddf4d
umeshc-roy <umeshc.roy@gmail.com>
2023-08-11 13:09:07 +0100

    Add endpoint for Homeoffice

--- terraform/environments/ppud/endpointservice.tf
+++ terraform/environments/ppud/endpointservice.tf
@@ -9,7 +9,7 @@ resource "aws_vpc_endpoint_service" "HomeOffice" {
 
 resource "aws_vpc_endpoint_service_allowed_principal" "HomeOffice" {
   count                   = local.is-production == true ? 1 : 0
-  vpc_endpoint_service_id = aws_vpc_endpoint_service.HomeOffice.id
+  vpc_endpoint_service_id = aws_vpc_endpoint_service.HomeOffice[0].id
   principal_arn           = "arn:aws:iam::518406511151:root"
 }
 
@@ -54,8 +54,8 @@ resource "aws_lb_target_group" "nlb_target_group" {
 
 resource "aws_lb_target_group_attachment" "alb_attachment" {
   count     = local.is-production == true ? 1 : 0
-  target_group_arn = aws_lb_target_group.nlb_target_group.arn
-  target_id        = aws_lb.PPUD-internal-ALB.id
+  target_group_arn = aws_lb_target_group.nlb_target_group[0].arn
+  target_id        = aws_lb.PPUD-internal-ALB[0].id
   port             = "443"
   depends_on = [
     aws_lb_listener.nlb_forward_rule,
