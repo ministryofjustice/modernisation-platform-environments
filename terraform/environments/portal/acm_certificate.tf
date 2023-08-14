1b09ae139926f8ce23dfc4e2c6acb8e3cf634f27
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-08-14 12:53:25 +0100

    new acm-cert for dev.legal.service

---
+++ terraform/environments/portal/acm_certificate.tf
@@ -0,0 +1,14 @@
+resource "aws_acm_certificate" "dev_legalservices_cert" {
+  domain_name = "${local.application_data.accounts[local.environment].acm_domain_name}"
+  validation_method = "DNS"
+   
+   
+   tags = merge(
+    local.tags,
+    { Name = "laa-${local.application_name}-${local.environment}" }
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
