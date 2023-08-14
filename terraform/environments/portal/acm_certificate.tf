8d2c25621689b119fb490e9986dbbd394f69eaea
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-08-14 13:07:14 +0100

    change to string

--- terraform/environments/portal/acm_certificate.tf
+++ terraform/environments/portal/acm_certificate.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "dev_legalservices_cert" {
   domain_name = "${local.application_data.accounts[local.environment].acm_domain_name}"
-  subject_alternative_names = ["${local.application_data.accounts[local.environment].acm_alt_domain_name}"]
+  subject_alternative_names = "${local.application_data.accounts[local.environment].acm_alt_domain_name}"
   validation_method = "DNS"
    
    
