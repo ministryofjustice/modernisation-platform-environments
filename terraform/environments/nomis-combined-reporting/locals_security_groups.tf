6921cda7fe74c6cea22b159408fdc329c2fbd484
Craig Gilbertson <crvgilbertson@gmail.com>
2023-08-15 13:06:27 +0100

    Undid unneccesary changes

--- terraform/environments/nomis-combined-reporting/locals_security_groups.tf
+++ terraform/environments/nomis-combined-reporting/locals_security_groups.tf
@@ -1,51 +1,28 @@
 locals {
 
   security_group_cidrs_devtest = {
-    icmp = flatten([
-      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc
-    ])
-    ssh = module.ip_addresses.azure_fixngo_cidrs.devtest
-    https_internal = flatten([
-      "10.0.0.0/8",
-      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc, # "172.20.0.0/16"
-    ])
-    https_external = flatten([
+    https = flatten([
+      module.ip_addresses.azure_fixngo_cidrs.devtest,
       module.ip_addresses.azure_fixngo_cidrs.internet_egress,
-      module.ip_addresses.moj_cidrs.trusted_moj_digital_staff_public,
-      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc, # "172.20.0.0/16"
-      module.ip_addresses.external_cidrs.cloud_platform
+      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc,
+      module.ip_addresses.moj_cidrs.trusted_moj_enduser_internal,
     ])
     oracle_db = flatten([
-      "10.0.0.0/8",
-      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc, # "172.20.0.0/16"
-    ])
-    oracle_oem_agent = flatten([
       module.ip_addresses.azure_fixngo_cidrs.devtest,
-      "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}",
+      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc,
     ])
   }
 
   security_group_cidrs_preprod_prod = {
-    icmp = flatten([
-      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc
-    ])
-    ssh = module.ip_addresses.azure_fixngo_cidrs.prod
-    https_internal = flatten([
-      "10.0.0.0/8",
-      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc,
-    ])
-    https_external = flatten([
+    https = flatten([
+      module.ip_addresses.azure_fixngo_cidrs.prod,
       module.ip_addresses.azure_fixngo_cidrs.internet_egress,
-      module.ip_addresses.moj_cidrs.trusted_moj_digital_staff_public,
-      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc, # "172.20.0.0/16"
+      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc,
+      module.ip_addresses.moj_cidrs.trusted_moj_enduser_internal,
     ])
     oracle_db = flatten([
-      "10.0.0.0/8",
-      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc,
-    ])
-    oracle_oem_agent = flatten([
       module.ip_addresses.azure_fixngo_cidrs.prod,
-      "${module.ip_addresses.mp_cidr[module.environment.vpc_name]}",
+      module.ip_addresses.moj_cidr.aws_cloud_platform_vpc,
     ])
   }
 
@@ -126,55 +103,13 @@ locals {
           protocol    = -1
           self        = true
         }
-        icmp = {
-          description = "Allow icmp ingress"
-          from_port   = -1
-          to_port     = -1
-          protocol    = "icmp"
-          cidr_blocks = local.security_group_cidrs.icmp
-        }
-        ssh = {
-          description = "Allow ssh ingress"
-          from_port   = "22"
-          to_port     = "22"
-          protocol    = "TCP"
-          cidr_blocks = local.security_group_cidrs.ssh
-          security_groups = [
-            # "bastion-linux",
-          ]
-        }
-        http8080 = {
-          description = "Allow http 8080 ingress"
-          from_port   = 8080
-          to_port     = 8080
-          protocol    = "tcp"
-          cidr_blocks = local.security_group_cidrs.oracle_db
-          security_groups = [
-            "private_lb",
-            # "private-jumpserver",
-            # "private-web",
-            # "bastion-linux",
-          ]
-        }
         oracle1521 = {
-          description = "Allow oracle database 1521 ingress"
-          from_port   = "1521"
-          to_port     = "1521"
-          protocol    = "tcp"
-          cidr_blocks = local.security_group_cidrs.oracle_db
-          security_groups = [
-            "private_lb",
-            # "private-jumpserver",
-            # "private-web",
-            # "bastion-linux",
-          ]
-        }
-        oracle3872 = {
-          description = "Allow oem agent ingress"
-          from_port   = "3872"
-          to_port     = "3872"
-          protocol    = "TCP"
-          cidr_blocks = local.security_group_cidrs.oracle_oem_agent
+          description     = "Allow oracle database 1521 ingress"
+          from_port       = "1521"
+          to_port         = "1521"
+          protocol        = "tcp"
+          cidr_blocks     = local.security_group_cidrs.oracle_db
+          security_groups = ["private"]
         }
       }
       egress = {
