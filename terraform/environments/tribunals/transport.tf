976653dbd26c225dc0599ae914f846f2dd92ed4a
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-08-16 16:35:03 +0100

    active directory creation added for fsx

--- terraform/environments/tribunals/transport.tf
+++ terraform/environments/tribunals/transport.tf
@@ -227,8 +227,51 @@ resource "aws_route53_record" "transport_external" {
 
 ####################### ECS #########################################
 
+resource "random_password" "ad_password" {
+  length  = 16
+  lower   = true
+  upper   = true
+  numeric = true
+  special = true
+}
+
+# Creating a AWS secret for AWS managed AD
+resource "aws_secretsmanager_secret" "secretdirectoryservice" {
+  name                    = "${local.transport}-AWSADPASS"
+  recovery_window_in_days = 0
+}
+
+# Creating a AWS secret versions for AWS managed AD
+resource "aws_secretsmanager_secret_version" "sversion" {
+  secret_id     = aws_secretsmanager_secret.secretdirectoryservice.id
+  secret_string = random_password.ad_password.result
+}
+
+resource "aws_directory_service_directory" "transport_ad" {
+  name     = "${local.transport}-AD"
+  password = aws_secretsmanager_secret_version.sversion.secret_string
+  edition  = "Standard"
+  type     = "MicrosoftAD"
+
+  vpc_settings {
+    vpc_id     = data.aws_vpc.shared.id
+    subnet_ids = [data.aws_subnet.public_subnets_a.id, data.aws_subnet.public_subnets_b.id]
+  }
+
+  lifecycle {
+    ignore_changes = [
+      password
+    ]
+  }
+
+  tags = {
+    Name = "${local.transport}-${var.networking[0].business-unit}-${local.environment}"
+  }
+}
+
 resource "aws_fsx_windows_file_system" "transport_fsx" {
 
+  active_directory_id = "${aws_directory_service_directory.transport_ad.id}"
   storage_capacity    = 300
   subnet_ids          = [data.aws_subnets.shared-public.ids[0]]
   security_group_ids  = [aws_security_group.transport_lb_sc.id]
