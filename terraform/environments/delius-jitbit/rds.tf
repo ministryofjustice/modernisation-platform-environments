1aade94f5a938f1122dc9718a813232b47892d52
Seb <sebastian.norris@digital.justice.gov.uk>
2023-08-16 13:21:55 +0100

    add rds deletion protection for jitbit (#3106)
    Co-authored-by: Seb Norris <sebastian@automationlogic.com>

--- terraform/environments/delius-jitbit/rds.tf
+++ terraform/environments/delius-jitbit/rds.tf
@@ -48,6 +48,7 @@ resource "aws_db_instance" "jitbit" {
   # tflint-ignore: aws_db_instance_default_parameter_group
   parameter_group_name        = "default.sqlserver-se-15.0"
   deletion_protection         = local.application_data.accounts[local.environment].db_deletion_protection
+  delete_automated_backups    = local.application_data.accounts[local.environment].db_delete_automated_backups
   skip_final_snapshot         = local.application_data.accounts[local.environment].db_skip_final_snapshot
   allocated_storage           = local.application_data.accounts[local.environment].db_allocated_storage
   max_allocated_storage       = local.application_data.accounts[local.environment].db_max_allocated_storage
