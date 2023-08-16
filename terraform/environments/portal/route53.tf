44cc77588cb88082eb5303e3b03575fc8149b536
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-08-16 10:46:23 +0100

    Merge in certs from other branch

--- terraform/environments/portal/route53.tf
+++ terraform/environments/portal/route53.tf
@@ -205,6 +205,22 @@ resource "aws_route53_record" "oid_lb" {
   }
 }
 
+# To be removed later
+resource "aws_acm_certificate" "dev_legalservices_cert" {
+  domain_name = "${local.application_data.accounts[local.environment].acm_domain_name}"
+  subject_alternative_names = ["${local.application_data.accounts[local.environment].acm_alt_domain_name}"]
+  validation_method = "DNS"
+
+
+   tags = merge(
+    local.tags,
+    { Name = "laa-${local.application_name}-${local.environment}" }
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 ###############################################################################################################
 #########################              IDM / ODS Route 53 records               ###############################
