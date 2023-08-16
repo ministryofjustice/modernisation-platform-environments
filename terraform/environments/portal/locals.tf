1ef346ccc0b8950d953744c76cc59a9a99a0c366
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-08-16 12:14:12 +0100

    copied the data ref for core-network services

--- terraform/environments/portal/locals.tf
+++ terraform/environments/portal/locals.tf
@@ -59,4 +59,14 @@ locals {
 
     external_validation_records_created = false
 
+   core_network_services_domains = {
+    for domain, value in var.validation : domain => value if value.account == "core-network-services"
+  }
+  core_vpc_domains = {
+    for domain, value in var.validation : domain => value if value.account == "core-vpc"
+  }
+  self_domains = {
+    for domain, value in var.validation : domain => value if value.account == "self"
+  }
+
 }
