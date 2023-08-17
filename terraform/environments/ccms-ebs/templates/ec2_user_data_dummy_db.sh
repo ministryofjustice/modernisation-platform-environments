2558d8177d5f7d42047c86915560d6b559a86bb7
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-08-17 15:28:23 +0100

    CC-1991: Updated dummy templates.

--- terraform/environments/ccms-ebs/templates/ec2_user_data_dummy_db.sh
+++ terraform/environments/ccms-ebs/templates/ec2_user_data_dummy_db.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+amazon_ssm_agent_setup() {
+    yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    systemctl stop amazon-ssm-agent
+    rm -rf /var/lib/amazon/ssm/ipc/
+    systemctl start amazon-ssm-agent
+}
+
 cloudwatch_agent_setup() {
     amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:cloud-watch-config
 }
@@ -14,4 +21,5 @@ yum_install() {
 
 hostname_setup
 yum_install
+amazon_ssm_agent_setup
 cloudwatch_agent_setup
