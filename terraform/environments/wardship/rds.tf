c0e5ec3118e43e7e44dbff1ea00ff6e3a20d0be4
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-08-17 11:41:47 +0100

    add null resources for DB migration

--- terraform/environments/wardship/rds.tf
+++ terraform/environments/wardship/rds.tf
@@ -20,29 +20,27 @@ resource "aws_db_subnet_group" "dbsubnetgroup" {
   subnet_ids = data.aws_subnets.shared-public.ids
 }
 
-//TODO: uncomment this
-
 //SG for accessing the tacticalproducts source DB:
-# resource "aws_security_group" "modernisation_wardship_access" {
-#   provider    = aws.tacticalproducts
-#   name        = "modernisation_wardship_access-${local.environment}"
-#   description = "Allow wardship on modernisation platform to access the source database"
+resource "aws_security_group" "modernisation_wardship_access" {
+  provider    = aws.tacticalproducts
+  name        = "modernisation_wardship_access-${local.environment}"
+  description = "Allow wardship on modernisation platform to access the source database"
 
-#   ingress {
-#     from_port   = 5432
-#     to_port     = 5432
-#     protocol    = "tcp"
-#     description = "Allow wardship on modernisation platform to connect to source database"
-#     cidr_blocks = ["${jsondecode(data.http.myip.response_body)["ip"]}/32"]
-#   }
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    description = "Allow wardship on modernisation platform to connect to source database"
+    cidr_blocks = ["${jsondecode(data.http.myip.response_body)["ip"]}/32"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_security_group" "postgresql_db_sc" {
   name        = "postgres_security_group"
@@ -84,45 +82,43 @@ data "http" "myip" {
   url = "http://ipinfo.io/json"
 }
 
-//TODO: Uncomment this after adding the secrets
+resource "null_resource" "setup_db" {
+  depends_on = [aws_db_instance.wardship_db]
 
-# resource "null_resource" "setup_db" {
-#   depends_on = [aws_db_instance.wardship_db]
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command     = "chmod +x ./migrate_db.sh; ./migrate_db.sh"
 
-#   provisioner "local-exec" {
-#     interpreter = ["bash", "-c"]
-#     command     = "chmod +x ./migrate_db.sh; ./migrate_db.sh"
+    environment = {
+      SOURCE_DB_HOSTNAME     = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_HOSTNAME"]
+      SOURCE_DB_NAME         = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_NAME"]
+      SOURCE_DB_USERNAME     = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_USERNAME"]
+      SOURCE_DB_PASSWORD     = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_PASSWORD"]
+      DB_HOSTNAME            = aws_db_instance.wardship_db.address
+      DB_NAME                = aws_db_instance.wardship_db.db_name
+      WARDSHIP_DB_USERNAME   = local.application_data.accounts[local.environment].db_username
+      WARDSHIP_DB_PASSWORD   = random_password.password.result
+    }
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
 
-#     environment = {
-#       SOURCE_DB_HOSTNAME     = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_HOSTNAME"]
-#       SOURCE_DB_NAME         = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_NAME"]
-#       SOURCE_DB_USERNAME     = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_USERNAME"]
-#       SOURCE_DB_PASSWORD     = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SOURCE_DB_PASSWORD"]
-#       DB_HOSTNAME            = aws_db_instance.wardship_db.address
-#       DB_NAME                = aws_db_instance.wardship_db.db_name
-#       WARDSHIP_DB_USERNAME   = local.application_data.accounts[local.environment].db_username
-#       WARDSHIP_DB_PASSWORD   = random_password.password.result
-#     }
-#   }
-#   triggers = {
-#     always_run = "${timestamp()}"
-#   }
-# }
+// executes a local script to set up the security group for the source RDS instance.
+resource "null_resource" "setup_source_rds_security_group" {
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command     = "chmod +x ./setup-security-group-${local.environment}.sh; ./setup-security-group-${local.environment}.sh"
 
-# // executes a local script to set up the security group for the source RDS instance.
-# resource "null_resource" "setup_source_rds_security_group" {
-#   provisioner "local-exec" {
-#     interpreter = ["bash", "-c"]
-#     command     = "chmod +x ./setup-security-group-${local.environment}.sh; ./setup-security-group-${local.environment}.sh"
-
-#     environment = {
-#       RDS_SECURITY_GROUP            = aws_security_group.modernisation_wardship_access.id
-#       RDS_SOURCE_ACCOUNT_ACCESS_KEY = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["ACCESS_KEY"]
-#       RDS_SOURCE_ACCOUNT_SECRET_KEY = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SECRET_KEY"]
-#       RDS_SOURCE_ACCOUNT_REGION     = "eu-west-2"
-#     }
-#   }
-#   triggers = {
-#     always_run = "${timestamp()}"
-#   }
-# }
+    environment = {
+      RDS_SECURITY_GROUP            = aws_security_group.modernisation_wardship_access.id
+      RDS_SOURCE_ACCOUNT_ACCESS_KEY = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["ACCESS_KEY"]
+      RDS_SOURCE_ACCOUNT_SECRET_KEY = jsondecode(data.aws_secretsmanager_secret_version.get_tactical_products_rds_credentials.secret_string)["SECRET_KEY"]
+      RDS_SOURCE_ACCOUNT_REGION     = "eu-west-2"
+    }
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
