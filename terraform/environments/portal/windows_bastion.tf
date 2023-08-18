50e05310cd718e51dde8c847481c0a1dc2d54822
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2023-08-18 12:14:41 +0100

    Add Windows Bastion for testing purposes

---
+++ terraform/environments/portal/windows_bastion.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "windows_bastion" {
+  ami                         = "ami-070c9a4ee0e5d83d5"
+  availability_zone           = "eu-west-2a"
+  instance_type               = "t2.small"
+  vpc_security_group_ids      = [aws_security_group.bastion.id]
+  monitoring                  = true
+  subnet_id                   = data.aws_subnet.private_subnets_a.id
+  iam_instance_profile        = aws_iam_instance_profile.portal.id
+
+  tags = merge(
+    local.tags,
+    { "Name" = "${local.application_name} Windows Bastion" }
+}
+
+
+resource "aws_security_group" "bastion" {
+  name        = "windows-bastion-sg"
+  description = "Windows Bastion Security Group"
+  vpc_id      = data.aws_vpc.shared.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "bastion_outbound" {
+  security_group_id = aws_security_group.bastion.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
