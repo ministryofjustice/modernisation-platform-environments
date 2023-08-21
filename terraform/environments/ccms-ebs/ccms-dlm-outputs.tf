0e0cea402da637cfd866df1205746986b29a22c0
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2023-08-21 15:02:54 +0100

    CC-2017: [count.index]/[each.key] -> [*], added try().

--- terraform/environments/ccms-ebs/ccms-dlm-outputs.tf
+++ terraform/environments/ccms-ebs/ccms-dlm-outputs.tf
@@ -1,38 +1,38 @@
 output "aws_iam_role_dlm_lifecycle_role_arn" {
   description = "aws_iam_role dlm_lifecycle_role arn"
-  value       = aws_iam_role.dlm_lifecycle_role[count.index].arn
+  value       = try(aws_iam_role.dlm_lifecycle_role[*].arn, "None")
 }
 
 output "aws_iam_role_dlm_lifecycle_role_name" {
   description = "aws_iam_role dlm_lifecycle_role name"
-  value       = aws_iam_role.dlm_lifecycle_role[count.index].name
+  value       = try(aws_iam_role.dlm_lifecycle_role[*].name, "None")
 }
 
 #
 
 output "aws_iam_role_policy_dlm_lifecycle_id" {
   description = "aws_iam_role_policy dlm_lifecycle id"
-  value       = aws_iam_role_policy.dlm_lifecycle[count.index].id
+  value       = try(aws_iam_role_policy.dlm_lifecycle[*].id, "None")
 }
 
 output "aws_iam_role_policy_dlm_lifecycle_name" {
   description = "aws_iam_role_policy dlm_lifecycle name"
-  value       = aws_iam_role_policy.dlm_lifecycle[count.index].name
+  value       = try(aws_iam_role_policy.dlm_lifecycle[*].name, "None")
 }
 
 output "aws_iam_role_policy_dlm_lifecycle_policy" {
   description = "aws_iam_role_policy dlm_lifecycle policy"
-  value       = aws_iam_role_policy.dlm_lifecycle[count.index].policy
+  value       = try(aws_iam_role_policy.dlm_lifecycle[*].policy, "None")
 }
 
 output "aws_iam_role_policy_dlm_lifecycle_role" {
   description = "aws_iam_role_policy dlm_lifecycle role"
-  value       = aws_iam_role_policy.dlm_lifecycle[count.index].role
+  value       = try(aws_iam_role_policy.dlm_lifecycle[*].role, "None")
 }
 
 #
 
 output "aws_dlm_lifecycle_policy_lifecyclerole_arn" {
   description = "aws_dlm_lifecycle_policy lifecyclerole arn"
-  value       = aws_dlm_lifecycle_policy.lifecyclerole[count.index].arn
+  value       = try(aws_dlm_lifecycle_policy.lifecyclerole[*].arn, "None")
 }
