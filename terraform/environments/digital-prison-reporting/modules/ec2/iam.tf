c988ad5967522e6bb4395c08f4f9ef7085a95c68
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-08-21 11:02:45 +0100

    Rename the Resource

--- terraform/environments/digital-prison-reporting/modules/ec2/iam.tf
+++ terraform/environments/digital-prison-reporting/modules/ec2/iam.tf
@@ -303,12 +303,12 @@ resource "aws_iam_role_policy_attachment" "redshift-admin" {
   policy_arn = data.aws_iam_policy.AmazonRedshiftFullAccess.arn
 }
 
-resource "aws_iam_role_policy_attachment" "instance-core-for-ssm" {
+resource "aws_iam_role_policy_attachment" "ec2-ssm-core" {
   role       = aws_iam_role.kinesis-agent-instance-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2-role-for-ssm" {
+resource "aws_iam_role_policy_attachment" "ec2-ssm" {
   role       = aws_iam_role.kinesis-agent-instance-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
