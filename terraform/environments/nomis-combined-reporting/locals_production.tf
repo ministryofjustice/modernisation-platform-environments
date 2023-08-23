9ec4b4eace8b9ba524af614ce2a7fd827f9a903e
Craig Gilbertson <crvgilbertson@gmail.com>
2023-08-23 11:52:54 +0100

    Added DNS entries

--- terraform/environments/nomis-combined-reporting/locals_production.tf
+++ terraform/environments/nomis-combined-reporting/locals_production.tf
@@ -1,4 +1,16 @@
 locals {
   production_config = {
+
+    baseline_route53_zones = {
+      "reporting.nomis.service.justice.gov.uk" = {
+        # { name = "preproduction", type = "NS", ttl = "300", records = ["t1ncr-a.test.reporting.nomis.service.justice.gov.uk"] },
+        # { name = "development", type = "NS", ttl = "300", records = ["t1-ncr-db-1-a.nomis-combined-reporting.hmpps-test.modernisation-platform.service.justice.gov.uk"] },
+        # { name = "test", type = "NS", ttl = "300", records = ["t1-ncr-db-1-a.nomis-combined-reporting.hmpps-test.modernisation-platform.service.justice.gov.uk"] },
+      }
+      "production.reporting.nomis.service.justice.gov.uk" = {
+      }
+      
+    }
+
   }
 }
