18735ecce5e8bda65fcc59f7a5464dfcd4e43c61
Ijaz Sultan <mohammed.ijazsultan@digital.justice.gov.uk>
2023-08-24 16:08:03 +0100

    s3 bucket config added

--- terraform/environments/corporate-staff-rostering/locals_development.tf
+++ terraform/environments/corporate-staff-rostering/locals_development.tf
@@ -137,6 +137,19 @@ locals {
         ]
       }
     }
+    baseline_s3_buckets = {
+      # use this bucket for storing artefacts for use across all accounts
+      csr-software = {
+        custom_kms_key = module.environment.kms_keys["general"].arn
+        bucket_policy_v2 = [
+          module.baseline_presets.s3_bucket_policies.ImageBuilderWriteAccessBucketPolicy,
+          module.baseline_presets.s3_bucket_policies.AllEnvironmentsWriteAccessBucketPolicy,
+        ]
+        iam_policies = module.baseline_presets.s3_iam_policies
+      }
+    }
+
+  
 
   }
 }
