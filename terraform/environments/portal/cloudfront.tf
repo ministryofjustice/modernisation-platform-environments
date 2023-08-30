fd27105e1ba8372671d1067c18b11b5fe493d55b
Alan <alan@automationlogic.com>
2023-08-30 16:22:40 +0100

    adding back cert validation

--- terraform/environments/portal/cloudfront.tf
+++ terraform/environments/portal/cloudfront.tf
@@ -342,19 +342,19 @@ resource "aws_cloudfront_distribution" "external" {
 }
 
 
-###### Cloudfront Cert
-# resource "aws_acm_certificate_validation" "cloudfront_certificate_validation" {
-#   count           = (length(local.validation_records_cloudfront) == 0 || local.external_validation_records_created) ? 1 : 0
-#   provider        = aws.us-east-1
-#   certificate_arn = aws_acm_certificate.cloudfront.arn
-#   validation_record_fqdns = [
-#     for key, value in local.validation_records_cloudfront : replace(value.name, "/\\.$/", "")
-#   ]
-#   depends_on = [
-#     aws_route53_record.cloudfront_validation_core_network_services,
-#     aws_route53_record.cloudfront_validation_core_vpc
-#   ]
-# }
+##### Cloudfront Cert
+resource "aws_acm_certificate_validation" "cloudfront_certificate_validation" {
+  count           = (length(local.validation_records_cloudfront) == 0 || local.external_validation_records_created) ? 1 : 0
+  provider        = aws.us-east-1
+  certificate_arn = aws_acm_certificate.cloudfront.arn
+  validation_record_fqdns = [
+    for key, value in local.validation_records_cloudfront : replace(value.name, "/\\.$/", "")
+  ]
+  depends_on = [
+    aws_route53_record.cloudfront_validation_core_network_services,
+    aws_route53_record.cloudfront_validation_core_vpc
+  ]
+}
 
 resource "aws_acm_certificate" "cloudfront" {
   domain_name               = local.application_data.accounts[local.environment].cloudfront_acm_domain_name
@@ -368,41 +368,41 @@ resource "aws_acm_certificate" "cloudfront" {
   }
 }
 
-# resource "aws_route53_record" "cloudfront_validation_core_network_services" {
-#   provider = aws.core-network-services
-#   for_each = {
-#     for key, value in local.cloudfront_validation_records : key => value if value.zone.provider == "core-network-services"
-#   }
+resource "aws_route53_record" "cloudfront_validation_core_network_services" {
+  provider = aws.core-network-services
+  for_each = {
+    for key, value in local.cloudfront_validation_records : key => value if value.zone.provider == "core-network-services"
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   # NOTE: value.zone is null indicates the validation zone could not be found
-#   # Ensure route53_zones variable contains the given validation zone or
-#   # explicitly provide the zone details in the validation variable.
-#   zone_id = each.value.zone.zone_id
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  # NOTE: value.zone is null indicates the validation zone could not be found
+  # Ensure route53_zones variable contains the given validation zone or
+  # explicitly provide the zone details in the validation variable.
+  zone_id = each.value.zone.zone_id
 
-#   depends_on = [
-#     aws_acm_certificate.cloudfront
-#   ]
-# }
+  depends_on = [
+    aws_acm_certificate.cloudfront
+  ]
+}
 
-# resource "aws_route53_record" "cloudfront_validation_core_vpc" {
-#   provider = aws.core-vpc
-#   for_each = {
-#     for key, value in local.cloudfront_validation_records : key => value if value.zone.provider == "core-vpc"
-#   }
+resource "aws_route53_record" "cloudfront_validation_core_vpc" {
+  provider = aws.core-vpc
+  for_each = {
+    for key, value in local.cloudfront_validation_records : key => value if value.zone.provider == "core-vpc"
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = each.value.zone.zone_id
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone.zone_id
 
-#   depends_on = [
-#     aws_acm_certificate.cloudfront
-#   ]
-# }
+  depends_on = [
+    aws_acm_certificate.cloudfront
+  ]
+}
