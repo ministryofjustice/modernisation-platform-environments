72a96e2df60eed118caf8ef9677b39aaee6a8c39
Andrew Moore <andrew@automationlogic.com>
2023-09-05 10:31:21 +0100

    NIT-813 Add ebs_volume module for delius-core

---
+++ terraform/environments/delius-core/modules/ebs_volume/main.tf
@@ -0,0 +1,17 @@
+resource "aws_ebs_volume" "this" {
+  availability_zone = var.availability_zone
+  type              = var.type
+  iops              = var.iops
+  throughput        = var.throughput
+  size              = var.size
+  encrypted         = true
+  kms_key_id        = var.kms_key_id
+  tags              = var.tags
+}
+
+resource "aws_volume_attachment" "this" {
+  device_name  = var.device_name
+  volume_id    = aws_ebs_volume.this.id
+  instance_id  = var.instance_id
+  skip_destroy = true
+} 
