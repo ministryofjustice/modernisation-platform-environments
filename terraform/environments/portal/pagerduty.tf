2f33572ffa79264c8a4c62319cac0fa23b76d06e
ALVincent <vincent.cheung@automationlogic.com>
2023-09-05 13:13:43 +0100

    Uncommend pagerduty_core_alerts again due to error

--- terraform/environments/portal/pagerduty.tf
+++ terraform/environments/portal/pagerduty.tf
@@ -16,11 +16,12 @@ data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
   secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
 }
 
-module "pagerduty_core_alerts" {
-  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v2.0.0"
-  sns_topics                = [aws_sns_topic.alerting_topic.name]
-  pagerduty_integration_key = local.pagerduty_integration_keys[local.pagerduty_integration_key_name]
-}
+# When rebuilding in the future, leave this commented until aws_sns_topic.alerting_topic has been created. Alternatively, move the resource in that module to this repo instead like we did for MLRA
+# module "pagerduty_core_alerts" {
+#   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v2.0.0"
+#   sns_topics                = [aws_sns_topic.alerting_topic.name]
+#   pagerduty_integration_key = local.pagerduty_integration_keys[local.pagerduty_integration_key_name]
+# }
 
 resource "aws_sns_topic" "alerting_topic" {
   name = local.sns_topic_name
