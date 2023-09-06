172e813c81f028425f770926579463f9919ff3be
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-06 17:50:35 +0000

    Move root zone to top Address count issue
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform/infra_route53.tf
+++ terraform/environments/data-platform/infra_route53.tf
@@ -1,9 +1,36 @@
 ##################################################
+# Production
+##################################################
+
+resource "aws_route53_zone" "data_platform_service_justice_gov_uk" {
+  count = terraform.workspace == "data-platform-production" ? 1 : 0
+
+  name = "data-platform.service.justice.gov.uk"
+  tags = local.tags
+}
+
+# Delegating to data-platform-development
+resource "aws_route53_record" "delegate_development_data_platform_service_justice_gov_uk" {
+  count = terraform.workspace == "data-platform-production" ? 1 : 0
+
+  zone_id = aws_route53_zone.data_platform_service_justice_gov_uk[0].zone_id
+  name    = "development.data-platform.service.justice.gov.uk"
+  type    = "NS"
+  ttl     = "300"
+  records = [
+    "ns-1741.awsdns-25.co.uk.",
+    "ns-446.awsdns-55.com.",
+    "ns-1406.awsdns-47.org.",
+    "ns-952.awsdns-55.net."
+  ]
+}
+
+##################################################
 # Development
 ##################################################
 
 resource "aws_route53_zone" "development_data_platform_service_justice_gov_uk" {
-  count = terraform.workspace == "development" ? 1 : 0
+  count = terraform.workspace == "data-platform-development" ? 1 : 0
 
   name = "development.data-platform.service.justice.gov.uk"
   tags = local.tags
@@ -11,9 +38,9 @@ resource "aws_route53_zone" "development_data_platform_service_justice_gov_uk" {
 
 # Delegating to data-platform-apps-and-tools-development
 resource "aws_route53_record" "delegate_apps_tools_development_data_platform_service_justice_gov_uk" {
-  count = terraform.workspace == "development" ? 1 : 0
+  count = terraform.workspace == "data-platform-development" ? 1 : 0
 
-  zone_id = aws_route53_zone.development_data_platform_service_justice_gov_uk.zone_id
+  zone_id = aws_route53_zone.development_data_platform_service_justice_gov_uk[0].zone_id
   name    = "apps-tools.development.data-platform.service.justice.gov.uk"
   type    = "NS"
   ttl     = "300"
@@ -30,7 +57,7 @@ resource "aws_route53_record" "delegate_apps_tools_development_data_platform_ser
 ##################################################
 
 resource "aws_route53_zone" "test_data_platform_service_justice_gov_uk" {
-  count = terraform.workspace == "test" ? 1 : 0
+  count = terraform.workspace == "data-platform-test" ? 1 : 0
 
   name = "test.data-platform.service.justice.gov.uk"
   tags = local.tags
@@ -41,34 +68,8 @@ resource "aws_route53_zone" "test_data_platform_service_justice_gov_uk" {
 ##################################################
 
 resource "aws_route53_zone" "preproduction_data_platform_service_justice_gov_uk" {
-  count = terraform.workspace == "preproduction" ? 1 : 0
+  count = terraform.workspace == "data-platform-preproduction" ? 1 : 0
 
   name = "preproduction.data-platform.service.justice.gov.uk"
   tags = local.tags
-}
-
-##################################################
-# Production
-##################################################
-
-resource "aws_route53_zone" "data_platform_service_justice_gov_uk" {
-  count = terraform.workspace == "production" ? 1 : 0
-
-  name = "data-platform.service.justice.gov.uk"
-  tags = local.tags
-}
-
-resource "aws_route53_record" "delegate_development_data_platform_service_justice_gov_uk" {
-  count = terraform.workspace == "production" ? 1 : 0
-
-  zone_id = aws_route53_zone.data_platform_service_justice_gov_uk.zone_id
-  name    = "development.data-platform.service.justice.gov.uk"
-  type    = "NS"
-  ttl     = "300"
-  records = [
-    "ns-1741.awsdns-25.co.uk.",
-    "ns-446.awsdns-55.com.",
-    "ns-1406.awsdns-47.org.",
-    "ns-952.awsdns-55.net."
-  ]
 }
