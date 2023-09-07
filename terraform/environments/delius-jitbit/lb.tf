0232fcf6ac91e3a815d94fb945dad652296dc80f
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-09-07 14:03:48 +0100

    Jitbit: Add missing ingress CIDRs to load balancer security group (#3307)

--- terraform/environments/delius-jitbit/lb.tf
+++ terraform/environments/delius-jitbit/lb.tf
@@ -34,12 +34,33 @@ resource "aws_security_group" "load_balancer_security_group" {
     from_port   = 443
     to_port     = 443
     cidr_blocks = [
-      "81.134.202.29/32", # MoJ Digital VPN
-      "195.59.75.0/24",   # ARK internet (DOM1)
-      "194.33.192.0/25",  # ARK internet (DOM1)
-      "194.33.193.0/25",  # ARK internet (DOM1)
-      "194.33.196.0/25",  # ARK internet (DOM1)
-      "194.33.197.0/25"   # ARK internet (DOM1)
+      "81.134.202.29/32",  # MoJ Digital VPN
+      "217.33.148.210/32", # Digital studio
+      "195.59.75.0/24",    # ARK internet (DOM1)
+      "194.33.192.0/25",   # ARK internet (DOM1)
+      "194.33.193.0/25",   # ARK internet (DOM1)
+      "194.33.196.0/25",   # ARK internet (DOM1)
+      "194.33.197.0/25",   # ARK internet (DOM1)
+
+      # Route53 Healthcheck Access Cidrs
+      # London Region not support yet, so metrics are not yet publised, can be enabled at later stage for Route53 endpoint monitor
+      "15.177.0.0/18",     # GLOBAL Region
+      "54.251.31.128/26",  # ap-southeast-1 Region
+      "54.255.254.192/26", # ap-southeast-1 Region
+      "176.34.159.192/26", # eu-west-1 Region
+      "54.228.16.0/26",    # eu-west-1 Region
+      "107.23.255.0/26",   # us-east-1 Region
+      "54.243.31.192/26",  # us-east-1 Region
+    ]
+
+    ipv6_cidr_blocks = [
+      # Route53 Healthcheck Access Cidrs IPv6
+      "2406:da18:7ff:f800::/53",  # ap-southeast-1 Region
+      "2406:da18:fff:f800::/53",  # ap-southeast-1 Region
+      "2a05:d018:fff:f800::/53",  # eu-west-1 Region
+      "2a05:d018:7ff:f800::/53",  # eu-west-1 Region
+      "2600:1f18:7fff:f800::/53", # us-east-1 Region
+      "2600:1f18:3fff:f800::/53", # us-east-1 Region
     ]
   }
 
