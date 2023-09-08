e2d1a7f7a8f320879a90f7d8ee552e52c5ab75d2
LavMatt <mattlaverty@gmail.com>
2023-09-08 08:15:38 +0100

    correct indent

--- terraform/environments/data-platform/iam.tf
+++ terraform/environments/data-platform/iam.tf
@@ -240,7 +240,7 @@ data "aws_iam_policy_document" "iam_policy_document_for_create_metadata_lambda" 
     ]
   }
 
-    statement {
+  statement {
     sid     = "PutDataObject"
     effect  = "Allow"
     actions = ["s3:PutObject"]
