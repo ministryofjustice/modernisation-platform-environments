f986c68f6e208680cd822f6298baf7ed884574e9
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-09-11 16:42:26 +0100

    fix

--- terraform/modules/baseline_presets/iam_policies.tf
+++ terraform/modules/baseline_presets/iam_policies.tf
@@ -6,8 +6,7 @@ locals {
     var.options.enable_ec2_cloud_watch_agent ? ["CloudWatchAgentServerReducedPolicy"] : [],
     var.options.enable_ec2_self_provision ? ["Ec2SelfProvisionPolicy"] : [],
     var.options.enable_shared_s3 ? ["Ec2AccessSharedS3Policy"] : [],
-    # var.options.enable_ec2_put_parameter ? ["Ec2PutParameterPolicy"] : [],
-    [ "Ec2PutParameterPolicy" ],
+    var.options.enable_ec2_put_parameter ? ["Ec2PutParameterPolicy"] : [],
     var.options.enable_ec2_put_secret ? ["Ec2PutSecretPolicy"] : [],
     var.options.enable_oracle_secure_web ? ["S3ListAllBucketsAndGetLocationPolicy"] : [],
     var.options.iam_policies_filter,
@@ -155,7 +154,7 @@ locals {
           "ssm:PutParameter",
           "ssm:PutParameters"
         ]
-        resources = ["arn:aws:ssm:*:*:parameter/ec2/*"]
+        resources = ["arn:aws:ssm:*:*:parameter/*"] # TODO scope to ec2/ path
       }]
     }
     Ec2PutSecretPolicy = {
