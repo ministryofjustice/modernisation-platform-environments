cf15eb98879eb4f8641619b81b38a38a07c691cf
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-09-11 16:43:11 +0100

    ebs test

--- terraform/environments/tribunals/ebs.tf
+++ terraform/environments/tribunals/ebs.tf
@@ -1,9 +1,10 @@
 resource "aws_ebs_volume" "tribunals-ebs" {
-  depends_on        = [aws_instance.cjip-server]
+  #depends_on        = [aws_instance.cjip-server]
   availability_zone = "${local.region}a"
   type              = "gp2"
+  size              = 40
 
-  snapshot_id = local.application_data.accounts[local.environment].suprig05-disk-1-snapshot
+  #snapshot_id = local.application_data.accounts[local.environment].suprig05-disk-1-snapshot
 
   tags = merge(
     local.tags,
@@ -13,10 +14,10 @@ resource "aws_ebs_volume" "tribunals-ebs" {
   )
 }
 
-resource "aws_volume_attachment" "tribunals-ebs" {
-  depends_on   = [aws_instance.cjip-server]
-  device_name  = "xvdk"
-  force_detach = true
-  volume_id    = aws_ebs_volume.tribunals-ebs.id
-  instance_id  = aws_instance.cjip-server.id
-}
+# resource "aws_volume_attachment" "tribunals-ebs" {
+#   depends_on   = [aws_instance.cjip-server]
+#   device_name  = "xvdk"
+#   force_detach = true
+#   volume_id    = aws_ebs_volume.tribunals-ebs.id
+#   instance_id  = aws_instance.cjip-server.id
+# }
