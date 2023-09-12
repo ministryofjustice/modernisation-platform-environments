6833c5da49893b52aeb7167c6f8558eb994311a2
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-09-12 16:48:38 +0100

    Remove ResourcePolicy permission

--- terraform/environments/hmpps-oem/locals_oem.tf
+++ terraform/environments/hmpps-oem/locals_oem.tf
@@ -46,7 +46,6 @@ locals {
   oem_secret_policy_read = {
     effect = "Allow"
     actions = [
-      "secretsmanager:GetResourcePolicy",
       "secretsmanager:GetSecretValue",
     ]
     principals = {
--- terraform/modules/baseline_presets/iam_policies.tf
+++ terraform/modules/baseline_presets/iam_policies.tf
@@ -170,7 +170,6 @@ locals {
       statements = [{
         effect = "Allow"
         actions = [
-          "secretsmanager:GetResourcePolicy",
           "secretsmanager:GetSecret",
         ]
         resources = ["arn:aws:secretsmanager:*:*:secret:/ec2/*"]
