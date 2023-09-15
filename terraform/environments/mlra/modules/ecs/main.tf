dd7b2ac09443bc50bd02d2d09c0c7dc9c1d535c7
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2023-09-15 14:45:00 +0100

    Add capacity provider

--- terraform/environments/mlra/modules/ecs/main.tf
+++ terraform/environments/mlra/modules/ecs/main.tf
@@ -562,19 +562,19 @@ resource "aws_appautoscaling_policy" "ecs_target_memory" {
   }
 }
 
-# resource "aws_ecs_capacity_provider" "mlra" {
-#   name = "${var.app_name}-capacity-provider"
+resource "aws_ecs_capacity_provider" "mlra" {
+  name = "${var.app_name}-capacity-provider"
 
-#   auto_scaling_group_provider {
-#     auto_scaling_group_arn         = aws_autoscaling_group.cluster-scaling-group.arn
-#     managed_termination_protection = "ENABLED"
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.cluster-scaling-group.arn
+    managed_termination_protection = "ENABLED"
 
-#     managed_scaling {
-#       # maximum_scaling_step_size = 1000
-#       # minimum_scaling_step_size = 1
-#       status                    = "ENABLED"
-#       target_capacity           = var.ecs_target_capacity
-#     }
-#   }
-# }
+    managed_scaling {
+      # maximum_scaling_step_size = 1000
+      # minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = var.ecs_target_capacity
+    }
+  }
+}
 
