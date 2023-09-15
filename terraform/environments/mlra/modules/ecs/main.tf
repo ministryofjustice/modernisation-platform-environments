99986bed699ef791f743390c0e8db2fa929028d0
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2023-09-15 16:14:46 +0100

    Add aws_ecs_cluster_capacity_providers

--- terraform/environments/mlra/modules/ecs/main.tf
+++ terraform/environments/mlra/modules/ecs/main.tf
@@ -585,9 +585,9 @@ resource "aws_ecs_capacity_provider" "mlra" {
 }
 
 
-# resource "aws_ecs_cluster_capacity_providers" "mlra" {
-#   cluster_name = aws_ecs_cluster.ecs_cluster.name
+resource "aws_ecs_cluster_capacity_providers" "mlra" {
+  cluster_name = aws_ecs_cluster.ecs_cluster.name
 
-#   capacity_providers = [aws_ecs_capacity_provider.mlra.name]
-# }
+  capacity_providers = [aws_ecs_capacity_provider.mlra.name]
+}
 
