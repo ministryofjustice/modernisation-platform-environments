90d8c3fd67a21c1c7d2a9fb28a79a11ac9d8b0ba
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-09-15 20:20:42 +0100

    Destroy jitbit prod db to launch fresh

--- terraform/environments/delius-jitbit/secrets.tf
+++ terraform/environments/delius-jitbit/secrets.tf
@@ -16,7 +16,7 @@ resource "aws_secretsmanager_secret" "db_app_connection_string" {
 
 resource "aws_secretsmanager_secret_version" "db_app_connection_string" {
   secret_id     = aws_secretsmanager_secret.db_app_connection_string.id
-  secret_string = "user id=;data source=${aws_db_instance.jitbit.address};initial catalog=;password="
+  secret_string = "user id=;data source=aws_db_instance.jitbit.address;initial catalog=;password="
   lifecycle {
     ignore_changes = [secret_string]
   }
