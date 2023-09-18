16433b474dcef10f9ceb1ad148206191f1901d54
Jonathan Quinn <jonathan.quinn@digital.justice.gov.uk>
2023-09-18 15:05:54 +0100

    add preprod and test NS records to prod

--- terraform/environments/nomis-data-hub/locals_production.tf
+++ terraform/environments/nomis-data-hub/locals_production.tf
@@ -2,7 +2,10 @@ locals {
   production_config = {
     baseline_route53_zones = {
       "ndh.nomis.service.justice.gov.uk" = {
-        records = []
+        records = [
+          { name = "preproduction", records = ["ns-1411.awsdns-48.org", "ns-885.awsdns-46.net", "ns-56.awsdns-07.com", "ns-1799.awsdns-32.co.uk"], type = "NS", ttl = "86400" },
+          { name = "test", records = ["ns-498.awsdns-62.com", "ns-881.awsdns-46.net", "ns-1294.awsdns-33.org", "ns-1610.awsdns-09.co.uk"], type = "NS", ttl = "86400" }
+        ]
       }
     }
   }
