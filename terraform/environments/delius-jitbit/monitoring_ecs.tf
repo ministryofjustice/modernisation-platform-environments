1df72ed26d4771423cd4ef716ea76214171c823a
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-09-18 11:07:37 +0100

    Correct alarms

--- terraform/environments/delius-jitbit/monitoring_ecs.tf
+++ terraform/environments/delius-jitbit/monitoring_ecs.tf
@@ -4,14 +4,14 @@
 resource "aws_cloudwatch_metric_alarm" "jitbit_cpu_over_threshold" {
   alarm_name          = "jitbit-ecs-cpu-threshold"
   alarm_description   = "Triggers alarm if ECS CPU crosses a threshold"
-  namespace           = "AWS/ECS"
-  metric_name         = "CPUUtilization"
+  namespace           = "ECS/ContainerInsights"
+  metric_name         = "CpuUtilized"
   statistic           = "Average"
   period              = "60"
   evaluation_periods  = "5"
   alarm_actions       = [aws_sns_topic.jitbit_alerting.arn]
   ok_actions          = [aws_sns_topic.jitbit_alerting.arn]
-  threshold           = "80"
+  threshold           = "10"
   treat_missing_data  = "missing"
   comparison_operator = "GreaterThanThreshold"
 }
@@ -20,14 +20,14 @@ resource "aws_cloudwatch_metric_alarm" "jitbit_cpu_over_threshold" {
 resource "aws_cloudwatch_metric_alarm" "jitbit_memory_over_threshold" {
   alarm_name          = "jitbit-ecs-memory-threshold"
   alarm_description   = "Triggers alarm if ECS memory crosses a threshold"
-  namespace           = "AWS/ECS"
-  metric_name         = "MemoryUtilization"
+  namespace           = "ECS/ContainerInsights"
+  metric_name         = "MemoryUtilized"
   statistic           = "Average"
   period              = "60"
   evaluation_periods  = "5"
   alarm_actions       = [aws_sns_topic.jitbit_alerting.arn]
   ok_actions          = [aws_sns_topic.jitbit_alerting.arn]
-  threshold           = "80"
+  threshold           = "5"
   treat_missing_data  = "missing"
   comparison_operator = "GreaterThanThreshold"
 }
