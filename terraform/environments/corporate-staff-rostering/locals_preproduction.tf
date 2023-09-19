26372ef9f32801f72500d8272b11074810b34736
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2023-09-19 17:52:58 +0100

    updating instance parameters

--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -3,30 +3,36 @@ locals {
 
   # baseline config
   preproduction_config = {
+
+    baseline_ec2_autoscaling_groups = {
       test-srv-1 = {
         config = merge(module.baseline_presets.ec2_instance.config.default, {
           ami_name                      = "mp_WindowsServer2019_2023-*" # Microsoft Windows Server 2019 Base
-          ami_owner                     = "self"
+          ami_owner                     = "374269020027"
           ebs_volumes_copy_all_from_ami = false
-          # user_data_raw                 = base64encode(file("./templates/app-server-user-data.yaml"))
+          user_data_raw                 = base64encode(file("./templates/app-server-user-data.yaml"))
           instance_profile_policies     = concat(module.baseline_presets.ec2_instance.config.default.instance_profile_policies, ["CSRWebServerPolicy"])
         })
 
         instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["migration-web-sg"]
+          vpc_security_group_ids = ["migration-web-sg", "domain-controller"]
+          instance_type          = "t3.medium"
+
         })
         ebs_volumes = {
           "/dev/sda1" = { type = "gp3", size = 256 }
         }
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
         autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
           desired_capacity = 1 # set to 0 while testing
         })
         tags = {
-          description = "Test Windows Web Server 2012 R2 includes Ec2LaunchV2 NVMe and PV drivers"
+          description = "Test Windows Web Server 2019"
           os-type     = "Windows"
           component   = "Test"
           server-type = "test-windows-server"
         }
+      }
     }
   }
 }
