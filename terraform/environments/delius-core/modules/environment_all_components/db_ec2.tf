ea64b3b2603b91e9146fe4748b3d8f2142ed805f
Andrew Moore <andrew@automationlogic.com>
2023-09-20 10:58:57 +0100

    NIT-843 update db_snapshot_identifier for iaps refresh

--- terraform/environments/delius-core/modules/environment_all_components/db_ec2.tf
+++ terraform/environments/delius-core/modules/environment_all_components/db_ec2.tf
@@ -199,7 +199,7 @@ resource "aws_instance" "db_ec2_primary_instance" {
     iops        = var.db_config.ebs_volumes.iops
     throughput  = var.db_config.ebs_volumes.throughput
     encrypted   = true
-    # We want to include kms_key_id here
+    kms_key_id  = var.db_config.ebs_volumes.kms_key_id
     tags = local.tags
   }
 
