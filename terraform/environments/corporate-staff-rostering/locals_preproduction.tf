d0d3607f47e225475f3dffb6dbc8b4fd02e9c639
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-09-20 09:47:11 +0100

    destroy and rename asg add networking

--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -5,12 +5,12 @@ locals {
   preproduction_config = {
 
     baseline_ec2_autoscaling_groups = {
-      test-srv-1 = {
+      ppt-srv-1 = {
         config = merge(module.baseline_presets.ec2_instance.config.default, {
           ami_name                      = "mp_WindowsServer2019_2023-*" # Microsoft Windows Server 2019 Base
           ami_owner                     = "374269020027"
           ebs_volumes_copy_all_from_ami = false
-          user_data_raw                 = base64encode(file("./templates/app-server-user-data.yaml"))
+          # user_data_raw                 = base64encode(file("./templates/app-server-user-data.yaml"))
           instance_profile_policies     = concat(module.baseline_presets.ec2_instance.config.default.instance_profile_policies, ["CSRWebServerPolicy"])
         })
 
@@ -32,6 +32,12 @@ locals {
           component   = "Test"
           server-type = "test-windows-server"
         }
+      }
+    }
+    baseline_route53_zones = {
+      "hmpps-test.modernisation-platform.service.justice.gov.uk" = {
+        records = [
+        ]
       }
     }
   }
