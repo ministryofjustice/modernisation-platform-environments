08ffe765bb9929c20925ea2c7f52e2f93d81ebaf
Mat Moore <MatMoore@users.noreply.github.com>
2023-09-21 14:39:42 +0100

    Bump presigned url lambda

--- terraform/environments/data-platform/lambda.tf
+++ terraform/environments/data-platform/lambda.tf
@@ -105,7 +105,11 @@ module "data_product_presigned_url_lambda" {
   memory_size  = 512
 
   environment_variables = {
-    BUCKET_NAME = module.s3-bucket.bucket.id
+    RAW_DATA_BUCKET     = module.s3-bucket.bucket.id
+    CURATED_DATA_BUCKET = module.s3-bucket.bucket.id
+    LOG_BUCKET          = module.s3-bucket.bucket.id
+    METADATA_BUCKET     = module.s3-bucket.bucket.id
+    LANDING_ZONE_BUCKET = module.s3-bucket.bucket.id
   }
 
   allowed_triggers = {
