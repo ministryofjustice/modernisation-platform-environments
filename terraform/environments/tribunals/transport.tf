c022155e040f56711a06ce435e24513795c43abb
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-09-26 14:07:21 +0100

    ecs creation for transport point of call changed

--- terraform/environments/tribunals/transport.tf
+++ terraform/environments/tribunals/transport.tf
@@ -225,49 +225,6 @@ resource "aws_route53_record" "transport_external" {
 #   }
 # }
 
-####################### ECS #########################################
-
-module "transport-ecs" {
-
-  source = "./modules/ecs"
-
-  subnet_set_name           = local.subnet_set_name
-  vpc_all                   = local.vpc_all
-  app_name                  = local.transport
-  container_instance_type   = "windows"
-  ami_image_id              = local.application_data.accounts[local.environment].ami_image_id
-  instance_type             = local.application_data.accounts[local.environment].instance_type
-  user_data                 = ""
-  key_name                  = ""
-  task_definition           = local.task_definition
-  ec2_desired_capacity      = local.application_data.accounts[local.environment].ec2_desired_capacity
-  ec2_max_size              = local.application_data.accounts[local.environment].ec2_max_size
-  ec2_min_size              = local.application_data.accounts[local.environment].ec2_min_size
-  task_definition_volume    = local.application_data.accounts[local.environment].task_definition_volume
-  network_mode              = local.application_data.accounts[local.environment].network_mode
-  server_port               = local.application_data.accounts[local.environment].server_port_1
-  app_count                 = local.application_data.accounts[local.environment].app_count
-  ec2_ingress_rules         = local.transport_ec2_ingress_rules
-  ec2_egress_rules          = local.transport_ec2_egress_rules
-  lb_tg_arn                 = aws_lb_target_group.transport_target_group.arn
-  tags_common               = local.tags
-  appscaling_min_capacity   = local.application_data.accounts[local.environment].appscaling_min_capacity
-  appscaling_max_capacity   = local.application_data.accounts[local.environment].appscaling_max_capacity
-  ec2_scaling_cpu_threshold = local.application_data.accounts[local.environment].ec2_scaling_cpu_threshold
-  ec2_scaling_mem_threshold = local.application_data.accounts[local.environment].ec2_scaling_mem_threshold
-  ecs_scaling_cpu_threshold = local.application_data.accounts[local.environment].ecs_scaling_cpu_threshold
-  ecs_scaling_mem_threshold = local.application_data.accounts[local.environment].ecs_scaling_mem_threshold
-  fsx_subnet_ids            = [data.aws_subnets.shared-public.ids[0]]
-  environment               = local.environment
-  //fsx_vpc_id                = data.aws_vpc.shared.id
-  lb_listener               = aws_lb_listener.transport_lb
-}
-
-resource "aws_ecr_repository" "transport-ecr-repo" {
-  name         = "${local.transport}-ecr-repo"
-  force_delete = true
-}
-
 ####################### LOAD BALANCER #########################################
 resource "aws_security_group" "transport_lb_sc" {
   name        = "${local.transport} load balancer security group"
@@ -407,3 +364,46 @@ resource "aws_lb_listener" "transport_lb" {
 #     create_before_destroy = true
 #   }
 # }
+
+####################### ECS #########################################
+
+module "transport-ecs" {
+
+  source = "./modules/ecs"
+
+  subnet_set_name           = local.subnet_set_name
+  vpc_all                   = local.vpc_all
+  app_name                  = local.transport
+  container_instance_type   = "windows"
+  ami_image_id              = local.application_data.accounts[local.environment].ami_image_id
+  instance_type             = local.application_data.accounts[local.environment].instance_type
+  user_data                 = ""
+  key_name                  = ""
+  task_definition           = local.task_definition
+  ec2_desired_capacity      = local.application_data.accounts[local.environment].ec2_desired_capacity
+  ec2_max_size              = local.application_data.accounts[local.environment].ec2_max_size
+  ec2_min_size              = local.application_data.accounts[local.environment].ec2_min_size
+  task_definition_volume    = local.application_data.accounts[local.environment].task_definition_volume
+  network_mode              = local.application_data.accounts[local.environment].network_mode
+  server_port               = local.application_data.accounts[local.environment].server_port_1
+  app_count                 = local.application_data.accounts[local.environment].app_count
+  ec2_ingress_rules         = local.transport_ec2_ingress_rules
+  ec2_egress_rules          = local.transport_ec2_egress_rules
+  lb_tg_arn                 = aws_lb_target_group.transport_target_group.arn
+  tags_common               = local.tags
+  appscaling_min_capacity   = local.application_data.accounts[local.environment].appscaling_min_capacity
+  appscaling_max_capacity   = local.application_data.accounts[local.environment].appscaling_max_capacity
+  ec2_scaling_cpu_threshold = local.application_data.accounts[local.environment].ec2_scaling_cpu_threshold
+  ec2_scaling_mem_threshold = local.application_data.accounts[local.environment].ec2_scaling_mem_threshold
+  ecs_scaling_cpu_threshold = local.application_data.accounts[local.environment].ecs_scaling_cpu_threshold
+  ecs_scaling_mem_threshold = local.application_data.accounts[local.environment].ecs_scaling_mem_threshold
+  fsx_subnet_ids            = [data.aws_subnets.shared-public.ids[0]]
+  environment               = local.environment
+  //fsx_vpc_id                = data.aws_vpc.shared.id
+  lb_listener               = aws_lb_listener.transport_lb
+}
+
+resource "aws_ecr_repository" "transport-ecr-repo" {
+  name         = "${local.transport}-ecr-repo"
+  force_delete = true
+}
