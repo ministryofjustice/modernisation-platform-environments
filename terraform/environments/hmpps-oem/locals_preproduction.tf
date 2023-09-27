9dd52c42b5654c19d1d5984a508a4e401697bec9
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-09-27 12:00:41 +0100

    enable preprod OEM

--- terraform/environments/hmpps-oem/locals_preproduction.tf
+++ terraform/environments/hmpps-oem/locals_preproduction.tf
@@ -17,16 +17,16 @@ locals {
     }
 
     baseline_ec2_instances = {
-      # preprod-oem-a = merge(local.oem_ec2_default, {
-      #   config = merge(local.oem_ec2_default.config, {
-      #     availability_zone = "eu-west-2a"
-      #   })
-      #   user_data_cloud_init = merge(local.oem_ec2_default.user_data_cloud_init, {
-      #     args = merge(local.oem_ec2_default.user_data_cloud_init.args, {
-      #       branch = "3d2586abc9063aee8d09add6098e534962145a73" # 2023-09-26 preprod ansible config
-      #     })
-      #   })
-      # })
+      preprod-oem-a = merge(local.oem_ec2_default, {
+        config = merge(local.oem_ec2_default.config, {
+          availability_zone = "eu-west-2a"
+        })
+        user_data_cloud_init = merge(local.oem_ec2_default.user_data_cloud_init, {
+          args = merge(local.oem_ec2_default.user_data_cloud_init.args, {
+            branch = "cb226d7bc7cbce9a252cb4ea79e237f4c074d66d" # 2023-09-27 preprod ansible config
+          })
+        })
+      })
       # preprod-oem-b = merge(local.oem_ec2_default, {
       #   config = merge(local.oem_ec2_default.config, {
       #     availability_zone = "eu-west-2b"
