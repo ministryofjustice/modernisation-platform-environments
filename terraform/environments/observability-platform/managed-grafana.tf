533b3d85c154b37b83f64a845081a6591032654c
Emterry <emma.terry@digital.justice.gov.uk>
2023-10-02 09:53:56 +0100

    managed grafana test

---
+++ terraform/environments/observability-platform/managed-grafana.tf
@@ -0,0 +1,11 @@
+module "managed_grafana" {
+  source  = "terraform-aws-modules/managed-service-grafana/aws"
+  version = "2.0.0"
+
+  account_access_type       = "CURRENT_ACCOUNT"
+  authentication_providers  = ["AWS_SSO"]
+  permission_type           = "SERVICE_MANAGED"
+  data_sources              = ["CLOUDWATCH", "PROMETHEUS"]
+  notification_destinations = ["SNS"]
+  
+}
