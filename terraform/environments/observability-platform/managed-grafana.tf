c88e4d71cae60b3aa7e4efc475b7382a276d7be0
Emterry <emma.terry@digital.justice.gov.uk>
2023-10-02 10:11:43 +0100

    bug

--- terraform/environments/observability-platform/managed-grafana.tf
+++ terraform/environments/observability-platform/managed-grafana.tf
@@ -2,10 +2,10 @@ module "managed_grafana" {
   source  = "terraform-aws-modules/managed-service-grafana/aws"
   version = "2.0.0"
 
+  name         = "open-metadata"
   account_access_type       = "CURRENT_ACCOUNT"
   authentication_providers  = ["AWS_SSO"]
   permission_type           = "SERVICE_MANAGED"
   data_sources              = ["CLOUDWATCH", "PROMETHEUS"]
   notification_destinations = ["SNS"]
-  
 }
