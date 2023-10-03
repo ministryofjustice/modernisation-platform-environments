9c114b9dd97bed2e9fe672135d8dcf44173a5923
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-10-03 16:38:44 +0100

    stash

--- terraform/environments/data-platform/s3.tf
+++ terraform/environments/data-platform/s3.tf
@@ -1,8 +1,188 @@
-
-module "s3-bucket" { #tfsec:ignore:aws-s3-enable-versioning
+module "data_s3_bucket" { #tfsec:ignore:aws-s3-enable-versioning
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.0.0"
 
-  bucket_prefix      = "data-platform-products-${local.environment}"
+  bucket_prefix      = "data-${local.environment}"
+  versioning_enabled = true
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  bucket_policy       = [data.aws_iam_policy_document.data_platform_product_bucket_policy_document.json]
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
+
+module "metadata_s3_bucket" { #tfsec:ignore:aws-s3-enable-versioning
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.0.0"
+
+  bucket_prefix      = "metadata-${local.environment}"
+  versioning_enabled = true
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  bucket_policy       = [data.aws_iam_policy_document.data_platform_product_bucket_policy_document.json]
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
+
+module "logs_s3_bucket" { #tfsec:ignore:aws-s3-enable-versioning
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.0.0"
+
+  bucket_prefix      = "logs-${local.environment}"
+  versioning_enabled = true
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  bucket_policy       = [data.aws_iam_policy_document.data_platform_product_bucket_policy_document.json]
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
+
+
+module "data_landing_s3_bucket" { #tfsec:ignore:aws-s3-enable-versioning
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.0.0"
+
+  bucket_prefix      = "data-landing-${local.environment}"
   versioning_enabled = true
   # Refer to the below section "Replication" before enabling replication
   replication_enabled = false
--- terraform/environments/data-platform/triggers.tf
+++ terraform/environments/data-platform/triggers.tf
@@ -6,12 +6,33 @@ resource "aws_cloudwatch_event_rule" "object_created_raw_data" {
     "source" : ["aws.s3"],
     "detail-type" : ["Object Created"],
     "detail" : {
-      "bucket" : { "name" : [module.s3-bucket.bucket.id] },
+      "bucket" : { "name" : [module.data_s3_bucket.bucket.id] },
       "object" : {
         "key" : [{ "prefix" : "raw_data/" }]
       }
     }
   })
+}
+
+resource "aws_cloudwatch_event_rule" "object_created_data_landing" {
+  name = "object_created_raw_data"
+  tags = local.tags
+  event_pattern = jsonencode({
+    "source" : ["aws.s3"],
+    "detail-type" : ["Object Created"],
+    "detail" : {
+      "bucket" : { "name" : [module.data_s3_bucket.bucket.id] },
+      "object" : {
+        "key" : [{ "prefix" : "raw_data/" }]
+      }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "athena_load_lambda_trigger" {
+  rule      = aws_cloudwatch_event_rule.object_created_raw_data.name
+  target_id = "athena"
+  arn       = module.data_product_athena_load_lambda.lambda_function_arn
 }
 
 resource "aws_cloudwatch_event_target" "athena_load_lambda_trigger" {
