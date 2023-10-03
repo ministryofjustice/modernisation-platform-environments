57e31619b3a993e36639649cc92aaa2dd48f5fdc
wullub <william@automationlogic.com>
2023-10-03 11:35:44 +0100

    add mgmt server sec group

--- terraform/environments/nomis-data-hub/locals_security_groups.tf
+++ terraform/environments/nomis-data-hub/locals_security_groups.tf
@@ -122,6 +122,28 @@ locals {
         }
       }
     }
+    management_server = {
+      description = "Security group for management server"
+      ingress = {
+        all-within-subnet = {
+          description = "Allow all ingress to self"
+          from_port   = 0
+          to_port     = 0
+          protocol    = -1
+          self        = true
+        }
+      }
+      egress = {
+        all = {
+          description     = "Allow all egress"
+          from_port       = 0
+          to_port         = 0
+          protocol        = "-1"
+          cidr_blocks     = ["0.0.0.0/0"]
+          security_groups = []
+        }
+      }
+    }
     ndh_app = {
       description = "Security group for ndh app"
       ingress = {
