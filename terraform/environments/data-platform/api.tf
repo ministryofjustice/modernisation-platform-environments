7ad9daa5fb0df3d280897a30f570a2109eada3bc
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-10-03 14:27:43 +0100

    :truck: Update data-product upload resources, methods and integration

--- terraform/environments/data-platform/api.tf
+++ terraform/environments/data-platform/api.tf
@@ -18,15 +18,17 @@ resource "aws_api_gateway_deployment" "deployment" {
       aws_api_gateway_resource.get_glue_metadata,
       aws_api_gateway_resource.docs,
       aws_api_gateway_resource.data_product,
-      aws_api_gateway_resource.data_product_name,
       aws_api_gateway_resource.register_data_product,
-      aws_api_gateway_resource.ingest_data_for_data_product,
+      aws_api_gateway_resource.data_product_name,
+      aws_api_gateway_resource.data_product_table,
+      aws_api_gateway_resource.data_product_table_name,
+      aws_api_gateway_resource.upload_data_for_data_product_table_name,
       aws_api_gateway_method.docs,
       aws_api_gateway_method.get_glue_metadata,
       aws_api_gateway_method.register_data_product,
-      aws_api_gateway_method.ingest_data_for_data_product,
+      aws_api_gateway_method.upload_data_for_data_product_table_name,
       aws_api_gateway_integration.docs_to_lambda,
-      aws_api_gateway_integration.ingest_data_for_data_product_to_lambda,
+      aws_api_gateway_integration.upload_data_for_data_product_table_name_to_lambda,
       aws_api_gateway_integration.proxy_to_lambda,
       aws_api_gateway_integration.docs_lambda_root,
       aws_api_gateway_integration.get_glue_metadata,
@@ -97,31 +99,46 @@ resource "aws_api_gateway_resource" "data_product_name" {
   rest_api_id = aws_api_gateway_rest_api.data_platform.id
 }
 
-# /data-product/{data-product-name}/ingest resource
-resource "aws_api_gateway_resource" "ingest_data_for_data_product" {
+# /data-product/{data-product-name}/table resource
+resource "aws_api_gateway_resource" "data_product_table" {
   parent_id   = aws_api_gateway_resource.data_product_name.id
-  path_part   = "ingest"
+  path_part   = "table"
   rest_api_id = aws_api_gateway_rest_api.data_platform.id
 }
 
-# /data-product/{data-product-name}/ingest POST method
-resource "aws_api_gateway_method" "ingest_data_for_data_product" {
+# /data-product/{data-product-name}/table/{table-name} resource
+resource "aws_api_gateway_resource" "data_product_table_name" {
+  parent_id   = aws_api_gateway_resource.data_product_table.id
+  path_part   = "{table-name}"
+  rest_api_id = aws_api_gateway_rest_api.data_platform.id
+}
+
+# /data-product/{data-product-name}/table/{table-name}/upload resource
+resource "aws_api_gateway_resource" "upload_data_for_data_product_table_name" {
+  parent_id   = aws_api_gateway_resource.data_product_table_name.id
+  path_part   = "upload"
+  rest_api_id = aws_api_gateway_rest_api.data_platform.id
+}
+
+# /data-product/{data-product-name}/table/{table-name}/upload POST method
+resource "aws_api_gateway_method" "upload_data_for_data_product_table_name" {
   authorization = "CUSTOM"
   authorizer_id = aws_api_gateway_authorizer.authorizer.id
   http_method   = "POST"
-  resource_id   = aws_api_gateway_resource.ingest_data_for_data_product.id
+  resource_id   = aws_api_gateway_resource.upload_data_for_data_product_table_name.id
   rest_api_id   = aws_api_gateway_rest_api.data_platform.id
 
   request_parameters = {
     "method.request.header.Authorization"   = true,
     "method.request.path.data-product-name" = true,
+    "method.request.path.table-name"        = true,
   }
 }
 
-# /data-product/{data-product-name}/ingest lambda integration
-resource "aws_api_gateway_integration" "ingest_data_for_data_product_to_lambda" {
-  http_method             = aws_api_gateway_method.ingest_data_for_data_product.http_method
-  resource_id             = aws_api_gateway_resource.ingest_data_for_data_product.id
+# /data-product/{data-product-name}/table/{table-name}/upload lambda integration
+resource "aws_api_gateway_integration" "upload_data_for_data_product_table_name_to_lambda" {
+  http_method             = aws_api_gateway_method.upload_data_for_data_product_table_name.http_method
+  resource_id             = aws_api_gateway_resource.upload_data_for_data_product_table_name.id
   rest_api_id             = aws_api_gateway_rest_api.data_platform.id
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -129,6 +146,7 @@ resource "aws_api_gateway_integration" "ingest_data_for_data_product_to_lambda" 
 
   request_parameters = {
     "integration.request.path.data-product-name" = "method.request.path.data-product-name",
+    "integration.request.path.table-name"        = "method.request.path.table-name",
   }
 }
 
