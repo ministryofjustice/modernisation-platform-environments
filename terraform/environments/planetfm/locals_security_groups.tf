62c21a6bdbba134fae02e8a14525fe903aa19afa
Ijaz Sultan <mohammed.ijazsultan@digital.justice.gov.uk>
2023-10-04 11:29:40 +0100

    initial cutover SG config created for planetfm

--- terraform/environments/planetfm/locals_security_groups.tf
+++ terraform/environments/planetfm/locals_security_groups.tf
@@ -23,5 +23,39 @@ locals {
         }
       }
     }
+
+    Migration_cutover_sg = {
+      description = "Security group for migrated instances"
+      ingress = {
+        all-from-self = {
+          description = "Allow all ingress to self"
+          from_port   = 0
+          to_port     = 0
+          protocol    = -1
+          self        = true
+        }
+        https = {
+          description     = "443: https ingress"
+          from_port       = 443
+          to_port         = 443
+          protocol        = "TCP"
+          cidr_blocks     = ["10.0.0.0/8"]
+          security_groups = []
+        }
+
+        rdp = {
+          description     = "3389: Allow RDP ingress"
+          from_port       = 3389
+          to_port         = 3389
+          protocol        = "TCP"
+          cidr_blocks     = ["10.40.50.128/26","10.40.50.64/26","10.40.50.0/26"]
+          security_groups = []
+        }
+      }
+      
+
+
+
+    }
   }
 }
