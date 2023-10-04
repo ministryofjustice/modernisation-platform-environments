2f1538e5dedc46ac50d4918602d65c92eb6531af
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-10-04 16:18:35 +0100

    :pencil2: Typo

--- terraform/environments/data-platform/iam-roles.tf
+++ terraform/environments/data-platform/iam-roles.tf
@@ -1,4 +1,4 @@
-module "iam_assumable_role_admin" {
+module "openmetadata_iam_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "~> 5.0"
   
@@ -10,7 +10,7 @@ module "iam_assumable_role_admin" {
 
   role_policy_arns = [
     "arn:aws:iam::aws:policy/service-role/AWSQuicksightAthenaAccess",
-    module.openmetadata_iam_policy.iam_role_arn
+    module.openmetadata_iam_policy.arn
   ]
 
   oidc_fully_qualified_subjects = ["system:serviceaccount:openmetadata:airflow"]
