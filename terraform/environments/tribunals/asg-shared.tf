041edf54907aa3c2e3bc5db5d62ab11c6c0a6e1f
Mark Butler <mark.butler@solirius.com>
2023-10-05 18:22:53 +0100

    Introduce a new shared ecs cluster with a shared load balancer and a shared EC2 instance (created by a launch template wihc has an ASG with a min/max of 1 instance)

---
+++ terraform/environments/tribunals/asg-shared.tf
@@ -0,0 +1,30 @@
+resource "aws_launch_template" "tribunals-all-lt" {
+  name_prefix   = "tribunals-all"
+  image_id      = "ami-0d20b6fc5007adcb3"
+  instance_type = "m5.large"
+
+  block_device_mappings {
+    device_name = "/dev/sdf"
+
+    ebs {
+      volume_size = 80
+    }
+  }
+  ebs_optimized = true
+
+  vpc_security_group_ids = [aws_security_group.tribunals_lb_sc.id]
+
+  user_data = filebase64("ec2-shared-user-data.sh")
+}
+
+resource "aws_autoscaling_group" "tribunals-all-asg" {
+  availability_zones = ["eu-west-2a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template = {
+    id      = "${aws_launch_template.tribunals-all-lt.id}"
+    version = "$$Latest"
+  }
+}
