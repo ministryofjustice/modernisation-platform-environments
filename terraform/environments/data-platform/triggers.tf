2cbc83b5ecab991f6602ad98d2212fd2ad8220eb
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-10-05 16:22:36 +0100

    Remove trigger dependencies

--- terraform/environments/data-platform/triggers.tf
+++ terraform/environments/data-platform/triggers.tf
@@ -33,16 +33,10 @@ resource "aws_cloudwatch_event_target" "athena_load_lambda_trigger" {
   rule      = aws_cloudwatch_event_rule.object_created_raw_data.name
   target_id = "athena"
   arn       = module.data_product_athena_load_lambda.lambda_function_arn
-  depends_on = [
-    aws_cloudwatch_event_rule.object_created_raw_data
-  ]
 }
 
 resource "aws_cloudwatch_event_target" "object_created_data_landing" {
   rule      = aws_cloudwatch_event_rule.object_created_data_landing.name
   target_id = "landing_to_raw"
   arn       = module.data_product_landing_to_raw_lambda.lambda_function_arn
-  depends_on = [
-    aws_cloudwatch_event_rule.object_created_data_landing
-  ]
 }
