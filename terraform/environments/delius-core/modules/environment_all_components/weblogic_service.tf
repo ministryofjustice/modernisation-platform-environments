5357ab37b08596ab7ac237f2eb0a8515df629212
George Taylor <george.taylor@digital.justice.gov.uk>
2023-10-06 21:15:31 +0100

    proto typo

--- terraform/environments/delius-core/modules/environment_all_components/weblogic_service.tf
+++ terraform/environments/delius-core/modules/environment_all_components/weblogic_service.tf
@@ -200,7 +200,7 @@ resource "aws_security_group_rule" "weblogic_allow_all_egress" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
-  protocol          = "443"
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.weblogic_service.id
 }
