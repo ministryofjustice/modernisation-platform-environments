6f1815e517bf5e3d0f43962e3028b1559f7edb53
George Taylor <george.taylor@digital.justice.gov.uk>
2023-10-06 12:40:20 +0100

    Nit 859 ldap data refresh poc datasync method (#3598)
    * add test env to dev account temporarily
    
    * add new test env
    
    * remove accidental plan output inclusion

--- terraform/environments/delius-core/main_development.tf
+++ terraform/environments/delius-core/main_development.tf
@@ -5,7 +5,7 @@
 module "environment_dev" {
   # We're in dev account and dev environment, could reference different version
   source = "./modules/environment_all_components"
-  count  = local.environment == "development" ? 1 : 0
+  count  = local.is-development ? 1 : 0
 
   providers = {
     aws.bucket-replication    = aws
@@ -30,30 +30,30 @@ module "environment_dev" {
   tags = local.tags
 }
 
-# module "environment_dev2" {
-#   #  We're in dev account and dev2 environment, could reference different version
-#   source = "./modules/environment_all_components"
-#   count  = local.environment == "development" ? 1 : 0
+module "environment_test" {
+  #  We're in dev account and test environment, could reference different version
+  source = "./modules/environment_all_components"
+  count  = local.is-development ? 1 : 0
 
-#   providers = {
-#     aws.bucket-replication    = aws
-#     aws.core-vpc              = aws.core-vpc
-#     aws.core-network-services = aws.core-network-services
-#   }
+  providers = {
+    aws.bucket-replication    = aws
+    aws.core-vpc              = aws.core-vpc
+    aws.core-network-services = aws.core-network-services
+  }
 
-#   env_name      = "dev2"
-#   app_name      = local.application_name
-#   platform_vars = local.platform_vars
+  env_name      = "test"
+  app_name      = local.application_name
+  platform_vars = local.platform_vars
 
-#   account_config             = local.account_config_dev
-#   environment_config         = local.environment_config_dev
-#   ldap_config                = local.ldap_config_dev
-#   db_config                  = local.db_config_dev
-#   weblogic_config            = local.weblogic_config_dev
-#   delius_db_container_config = local.delius_db_container_config_dev2
-#   bastion                    = local.bastion
+  account_config             = local.account_config_test
+  environment_config         = local.environment_config_test
+  ldap_config                = local.ldap_config_test
+  db_config                  = local.db_config_test
+  weblogic_config            = local.weblogic_config_test
+  delius_db_container_config = local.delius_db_container_config_test
+  bastion                    = local.bastion
 
-#   account_info = local.account_info
+  account_info = local.account_info
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
