b7400fd5f55790b7a732f6f0cb8bf67175c16f1b
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-10-06 12:24:02 +0100

    add AWS.LogGroup variable to dev

--- terraform/environments/dacp/ecs.tf
+++ terraform/environments/dacp/ecs.tf
@@ -164,7 +164,7 @@ resource "aws_iam_role_policy" "app_execution" {
           "logs:DescribeLogStreams",
           "logs:PutLogEvents"
         ],
-        "Resource": "arn:aws:logs:eu-west-2:${data.aws_caller_identity.current.account_id}:log-group:/aws/ecs/${aws_cloudwatch_log_group.dacpFamily_logs.name}:*"
+        "Resource": "arn:aws:logs:eu-west-2:${data.aws_caller_identity.current.account_id}:log-group:/aws/ecs${aws_cloudwatch_log_group.dacpFamily_logs.name}:*"
       }
     ]
   }
