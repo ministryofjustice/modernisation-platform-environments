a8c55c1df9bc6dc5060cee5938e58716d3245e40
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-10-06 14:39:56 +0100

    DSOS-2228: rebuild prod OEM server, zapping first

--- terraform/environments/hmpps-oem/locals_production.tf
+++ terraform/environments/hmpps-oem/locals_production.tf
@@ -17,16 +17,16 @@ locals {
     }
 
     baseline_ec2_instances = {
-      prod-oem-a = merge(local.oem_ec2_default, {
-        config = merge(local.oem_ec2_default.config, {
-          availability_zone = "eu-west-2a"
-        })
-        user_data_cloud_init = merge(local.oem_ec2_default.user_data_cloud_init, {
-          args = merge(local.oem_ec2_default.user_data_cloud_init.args, {
-            branch = "c958e86e4b1b062ca21d46f7ff204c60377519c5" # 2023-10-04 prod ansible config
-          })
-        })
-      })
+      # prod-oem-a = merge(local.oem_ec2_default, {
+      #   config = merge(local.oem_ec2_default.config, {
+      #     availability_zone = "eu-west-2a"
+      #   })
+      #   user_data_cloud_init = merge(local.oem_ec2_default.user_data_cloud_init, {
+      #     args = merge(local.oem_ec2_default.user_data_cloud_init.args, {
+      #       branch = "c958e86e4b1b062ca21d46f7ff204c60377519c5" # 2023-10-04 prod ansible config
+      #     })
+      #   })
+      # })
     }
 
     baseline_route53_zones = {
