a2754fff701a4d7e2f62a38f81158d50524dbded
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-09 15:27:57 +0100

    Clean up

--- terraform/environments/observability-platform/managed-prometheus.tf
+++ terraform/environments/observability-platform/managed-prometheus.tf
@@ -1,4 +1,4 @@
-module "managed-service-prometheus" {
-  source  = "terraform-aws-modules/managed-service-prometheus/aws"
-  version = "2.2.2"
-}
+# module "managed-service-prometheus" {
+#   source  = "terraform-aws-modules/managed-service-prometheus/aws"
+#   version = "2.2.2"
+# }
