cb52aa0bfd29acea7b27bc7d3a94beabf9503d4f
George Taylor <george.taylor@digital.justice.gov.uk>
2023-10-09 20:35:54 +0100

    Update ldap_datasync.tf

--- terraform/environments/delius-core/modules/environment_all_components/ldap_datasync.tf
+++ terraform/environments/delius-core/modules/environment_all_components/ldap_datasync.tf
@@ -1,23 +1,32 @@
-#data "aws_efs_file_system" "ldap_efs_datasync" {
-#  for_each = toset(var.environments_in_account)
-#  tags = {
-#    Name = "ldap-efs-${each.key}"
-#  }
-#}
-#
-#data "aws_security_group" "ldap_efs_datasync" {
-#  for_each = toset(var.environments_in_account)
-#  tags = {
-#    Name = "ldap-efs-${each.key}"
-#  }
-#}
+data "aws_efs_file_system" "ldap_efs_datasync" {
+  for_each = toset(var.environments_in_account)
+  tags = {
+    Name = "ldap-efs-${each.value}"
+  }
+}
+
+data "aws_security_group" "ldap_efs_datasync" {
+  for_each = toset(var.environments_in_account)
+  tags = {
+    Name = "ldap-efs-${each.value}"
+  }
+}
+
+output "ldap_efs_datasync_security_group_arns" {
+  value = [for sg in data.aws_security_group.ldap_efs_datasync : sg.arn]
+}
+
+output "ldap_efs_datasync_file_system_arns" {
+  value = [for fs in data.aws_efs_file_system.ldap_efs_datasync : fs.arn]
+}
+
 #
 #resource "aws_datasync_location_efs" "ldap" {
 #  for_each = toset(var.environments_in_account)
-#  efs_file_system_arn = data.aws_efs_file_system.ldap_efs_datasync[each.key].arn
+#  efs_file_system_arn = data.aws_efs_file_system.ldap_efs_datasync[each.value].arn
 #
 #  ec2_config {
-#    security_group_arns = data.aws_security_group.ldap_efs_datasync[each.key].arn
+#    security_group_arns = data.aws_security_group.ldap_efs_datasync[each.value].arn
 #    subnet_arn          = var.account_config.private_subnet_ids[0]
 #  }
 #  tags = var.tags
