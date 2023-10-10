7c4477656933239a548e0ef0fa73d433e54fb564
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2023-10-10 16:28:53 +0100

    shared resource creation

--- terraform/environments/tribunals/ecs-cluster-shared.tf
+++ terraform/environments/tribunals/ecs-cluster-shared.tf
@@ -1,137 +1,137 @@
-# resource "aws_ecs_cluster" "tribunals_cluster" {
-#   name = "tribunals-all-cluster"
-#   setting {
-#     name  = "containerInsights"
-#     value = "enabled"
-#   }
-# }
+resource "aws_ecs_cluster" "tribunals_cluster" {
+  name = "tribunals-all-cluster"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
 
-# resource "aws_cloudwatch_log_group" "tribunalsFamily_logs" {
-#   name = "/ecs/tribunalsFamily"
-# }
+resource "aws_cloudwatch_log_group" "tribunalsFamily_logs" {
+  name = "/ecs/tribunalsFamily"
+}
 
-# resource "aws_iam_role" "app_execution" {
-#   name = "execution-${var.networking[0].application}"
+resource "aws_iam_role" "app_execution" {
+  name = "execution-${var.networking[0].application}"
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "ecs-tasks.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "execution-${var.networking[0].application}"
-#     },
-#   )
-# }
+  tags = merge(
+    local.tags,
+    {
+      Name = "execution-${var.networking[0].application}"
+    },
+  )
+}
 
-# resource "aws_iam_role_policy" "app_execution" {
-#   name = "execution-${var.networking[0].application}"
-#   role = aws_iam_role.app_execution.id
+resource "aws_iam_role_policy" "app_execution" {
+  name = "execution-${var.networking[0].application}"
+  role = aws_iam_role.app_execution.id
 
-#   policy = <<-EOF
-#   {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#       {
-#            "Action": [
-#               "ecr:*",
-#               "logs:CreateLogStream",
-#               "logs:PutLogEvents",
-#               "secretsmanager:GetSecretValue"
-#            ],
-#            "Resource": "*",
-#            "Effect": "Allow"
-#       }
-#     ]
-#   }
-#   EOF
-# }
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+           "Action": [
+              "ecr:*",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents",
+              "secretsmanager:GetSecretValue"
+           ],
+           "Resource": "*",
+           "Effect": "Allow"
+      }
+    ]
+  }
+  EOF
+}
 
-# resource "aws_iam_role" "app_task" {
-#   name = "task-${var.networking[0].application}"
+resource "aws_iam_role" "app_task" {
+  name = "task-${var.networking[0].application}"
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "ecs-tasks.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "task-${var.networking[0].application}"
-#     },
-#   )
-# }
+  tags = merge(
+    local.tags,
+    {
+      Name = "task-${var.networking[0].application}"
+    },
+  )
+}
 
-# resource "aws_iam_role_policy" "app_task" {
-#   name = "task-${var.networking[0].application}"
-#   role = aws_iam_role.app_task.id
+resource "aws_iam_role_policy" "app_task" {
+  name = "task-${var.networking[0].application}"
+  role = aws_iam_role.app_task.id
 
-#   policy = <<-EOF
-#   {
-#    "Version": "2012-10-17",
-#    "Statement": [
-#      {
-#        "Effect": "Allow",
-#         "Action": [
-#           "logs:CreateLogStream",
-#           "logs:PutLogEvents",
-#           "ecr:*",
-#           "iam:*",
-#           "ec2:*"
-#         ],
-#        "Resource": "*"
-#      }
-#    ]
-#   }
-#   EOF
-# }
+  policy = <<-EOF
+  {
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Effect": "Allow",
+        "Action": [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "ecr:*",
+          "iam:*",
+          "ec2:*"
+        ],
+       "Resource": "*"
+     }
+   ]
+  }
+  EOF
+}
 
-# resource "aws_security_group" "ecs_service" {
-#   name_prefix = "ecs-service-sg-"
-#   vpc_id      = data.aws_vpc.shared.id
+resource "aws_security_group" "ecs_service" {
+  name_prefix = "ecs-service-sg-"
+  vpc_id      = data.aws_vpc.shared.id
 
-#   ingress {
-#     from_port       = 80
-#     to_port         = 80
-#     protocol        = "tcp"
-#     description     = "Allow traffic on port 80 from load balancer"
-#     security_groups = [aws_security_group.tribunals_lb_sc.id]
-#   }
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    description     = "Allow traffic on port 80 from load balancer"
+    security_groups = [aws_security_group.tribunals_lb_sc.id]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# resource "aws_ecr_repository" "tribunals-ecr-repo" {
-#   name         = "tribunals-ecr-repo"
-#   force_delete = true
-# }
+resource "aws_ecr_repository" "tribunals-ecr-repo" {
+  name         = "tribunals-ecr-repo"
+  force_delete = true
+}
