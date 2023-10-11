f45b9dfbfc70e4a819b0f8ab00703f5d91dd209a
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-10-11 12:32:50 +0100

    added the relevant logs

--- terraform/environments/apex/ec2.tf
+++ terraform/environments/apex/ec2.tf
@@ -9,10 +9,6 @@ echo "${aws_efs_file_system.efs.dns_name}:/ /backups nfs4 rsize=1048576,wsize=10
 mount -a
 
 # Setting up CloudWatch Agent
-mkdir cloudwatch_agent
-cd cloudwatch_agent
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
-rpm -U ./amazon-cloudwatch-agent.rpm
 echo '${data.local_file.cloudwatch_agent.content}' > cloudwatch_agent_config.json
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:cloudwatch_agent_config.json
 EOF
