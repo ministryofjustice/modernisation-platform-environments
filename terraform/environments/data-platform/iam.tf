6c7e1c09b80b03cb86e5aa0a96d82ecd18c25908
Mat Moore <MatMoore@users.noreply.github.com>
2023-10-11 15:36:11 +0100

    Add previous bucket back in
    We don't want to delete this until we're sure the new bucket structure
    is working, and either way we can't delete it while there are files
    still there.

--- terraform/environments/data-platform/iam.tf
+++ terraform/environments/data-platform/iam.tf
@@ -270,6 +270,44 @@ resource "aws_iam_role_policy_attachment" "attach_allow_invoke_authoriser_lambda
 
 # S3 policy
 
+# TO BE REMOVED
+data "aws_iam_policy_document" "data_platform_product_bucket_policy_document" {
+  statement {
+    sid    = "AllowPutFromCiUser"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/cicd-member-user"]
+    }
+
+    actions = ["s3:PutObject", "s3:ListBucket"]
+
+    resources = [module.s3-bucket.bucket.arn, "${module.s3-bucket.bucket.arn}/*"]
+  }
+
+  statement {
+    sid       = "DenyNonFullControlObjects"
+    effect    = "Deny"
+    actions   = ["s3:PutObject"]
+    resources = ["${module.s3-bucket.bucket.arn}/*"]
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "bucket-owner-full-control"
+      ]
+    }
+  }
+}
+
 data "aws_iam_policy_document" "data_s3_bucket_policy_document" {
   statement {
     sid    = "AllowPutFromCiUser"
