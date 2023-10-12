a119a49c0cd2f826e2965fa735dea159ee24d672
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-10-12 11:29:43 +0100

    add monitoring for web instances

--- terraform/environments/corporate-staff-rostering/locals_database.tf
+++ terraform/environments/corporate-staff-rostering/locals_database.tf
@@ -6,4 +6,9 @@ locals {
     }
   }
 
+  database_ec2_cloudwatch_metric_alarms = merge(
+    module.baseline_presets.cloudwatch_metric_alarms.ec2,
+    module.baseline_presets.cloudwatch_metric_alarms.ec2_cwagent_linux
+  )
+
 }
--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -244,7 +244,7 @@ locals {
     }
 
     baseline_ec2_autoscaling_groups = {
-      prepprod-tst-1 = {
+      pp-web-tst-1 = {
         config = merge(module.baseline_presets.ec2_instance.config.default, {
           ami_name                      = "hmpps_windows_server_2022_release_2023-*" # Microsoft Windows Server 2019 Base
           ami_owner                     = "754260907303"
@@ -252,9 +252,10 @@ locals {
           user_data_raw                 = base64encode(file("./templates/test-user-data.yaml"))
           instance_profile_policies     = concat(module.baseline_presets.ec2_instance.config.default.instance_profile_policies, ["CSRWebServerPolicy"])
         })
+        cloudwatch_metric_alarms = local.app_ec2_cloudwatch_metric_alarms
 
         instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["migration-web-sg", "domain-controller"]
+          vpc_security_group_ids = ["web", "domain", "jumpserver"]
           instance_type          = "t3.medium"
 
         })
