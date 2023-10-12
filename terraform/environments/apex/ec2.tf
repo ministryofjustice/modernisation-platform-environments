bcdb924d898c06e4d8c502566fc9ae01f75d19fe
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-10-12 09:51:43 +0100

    change file location

--- terraform/environments/apex/ec2.tf
+++ terraform/environments/apex/ec2.tf
@@ -8,11 +8,11 @@ sudo systemctl enable amazon-ssm-agent
 echo "${aws_efs_file_system.efs.dns_name}:/ /backups nfs4 rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport" >> /etc/fstab
 mount -a
 
-cd /home
+cd /etc
 mkdir cloudwatch_agent
 cd cloudwatch_agent
 echo '${data.local_file.cloudwatch_agent.content}' > cloudwatch_agent_config.json
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/home/cloudwatch_agent/cloudwatch_agent_config.json
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/etc/cloudwatch_agent/cloudwatch_agent_config.json
 
 EOF
 }
