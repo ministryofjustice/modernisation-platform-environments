a743387f8a375d3de91d39526ed6db04bbf34fc7
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-10-13 14:48:20 +0100

    add log configuration back in

--- terraform/environments/wardship/ecs.tf
+++ terraform/environments/wardship/ecs.tf
@@ -32,6 +32,14 @@ resource "aws_ecs_task_definition" "wardship_task_definition" {
           hostPort      = 80
         }
       ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "${aws_cloudwatch_log_group.deployment_logs.name}"
+          awslogs-region        = "eu-west-2"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
       environment = [
         {
           name  = "RDS_HOSTNAME"
