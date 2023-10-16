a9574115423c29760e8325f128cf58b106e95749
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-10-16 15:53:34 +0100

    LAWS-3514: updated snapshotDBFunction with correct iam role name

--- terraform/environments/apex/lambda.tf
+++ terraform/environments/apex/lambda.tf
@@ -88,7 +88,7 @@ resource "aws_lambda_function" "snapshotDBFunction" {
   # path.module in the filename.
   filename      = "snapshotDBFunction.zip"
   function_name = "snapshotDBFunction"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.backuplambdarole.arn
   handler       = "snapshot/dbsnapshot.handler"
 
   source_code_hash = data.archive_file.lambda_dbsnapshot.output_base64sha256
