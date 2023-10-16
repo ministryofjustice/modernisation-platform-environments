fe11bd82bcd858c4543f5659706c8c3ed138503c
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-10-16 12:02:03 +0100

    LAWS-3514: added module for s3 bucket in apex

---
+++ terraform/environments/apex/s3.tf
@@ -0,0 +1,12 @@
+module "s3_bucket_lambda" {
+  source = "./module/s3"
+
+  bucket_name = "laa-${local.application_name}-${local.environment}-mp" # Added suffix -mp to the name as it must be unique from the existing bucket in LZ
+  # bucket_prefix not used in case bucket name get referenced as part of EC2 AMIs
+
+  tags = merge(
+    local.tags,
+    { Name = "laa-${local.application_name}-${local.environment}-mp" }
+  )
+
+}
