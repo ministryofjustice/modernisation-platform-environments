8c35131dfc0b991b76e17a565959424fa3de8add
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-10-16 13:13:02 +0100

    add monitoring backend

---
+++ terraform/environments/corporate-staff-rostering/monitoring.tf
@@ -0,0 +1,26 @@
+# Pager duty integration
+
+# Get the map of pagerduty integration keys from the modernisation platform account
+data "aws_secretsmanager_secret" "pagerduty_integration_keys" {
+  provider = aws.modernisation-platform
+  name     = "pagerduty_integration_keys"
+}
+data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
+  provider  = aws.modernisation-platform
+  secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
+}
+
+# Add a local to get the keys
+locals {
+  pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
+}
+
+# link the sns topic to the service
+module "pagerduty_core_alerts" {
+  depends_on = [
+    aws_sns_topic.csr_alarms,
+  ]
+  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
+  sns_topics                = [aws_sns_topic.csr_alarms.name]
+  pagerduty_integration_key = local.pagerduty_integration_keys["csr_alarms"]
+}
