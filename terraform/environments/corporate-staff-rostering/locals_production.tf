c8b831157d9abf626307a8bea0fc127692e9640b
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-10-17 14:00:13 +0100

    formatting

--- terraform/environments/corporate-staff-rostering/locals_production.tf
+++ terraform/environments/corporate-staff-rostering/locals_production.tf
@@ -86,7 +86,7 @@ locals {
         ]
         records = [
           { name = "test", type = "NS", ttl = "86400", records = ["ns-1332.awsdns-38.org", "ns-2038.awsdns-62.co.uk", "ns-62.awsdns-07.com", "ns-689.awsdns-22.net"] },
-          { name = "pp", type = "NS", ttl = "86400", records = [ "ns-1408.awsdns-48.org", "ns-1844.awsdns-38.co.uk", "ns-447.awsdns-55.com", "ns-542.awsdns-03.net",] },
+          { name = "pp", type = "NS", ttl = "86400", records = ["ns-1408.awsdns-48.org", "ns-1844.awsdns-38.co.uk", "ns-447.awsdns-55.com", "ns-542.awsdns-03.net"] },
         ]
       }
       "pd.csr.service.justice.gov.uk" = {
