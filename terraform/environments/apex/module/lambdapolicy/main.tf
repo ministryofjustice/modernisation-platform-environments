d12c2dd2ff408ffa94a34988035aaba735569e3f
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-10-17 14:55:59 +0100

    change backup role name

--- terraform/environments/apex/module/lambdapolicy/main.tf
+++ terraform/environments/apex/module/lambdapolicy/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "backuplambdarole" {
-  name               = "backuplambdarole"
+  name               = "apex-backuplambdarole"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
