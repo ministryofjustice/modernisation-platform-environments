ca5a4c31c4bc5a639c7dab736f810913d6e09177
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-10-17 16:19:38 +0100

    remove double quotes

--- terraform/environments/apex/lambda.tf
+++ terraform/environments/apex/lambda.tf
@@ -10,9 +10,9 @@ module "lambda_backup" {
 
 backup_policy_name = "${local.application_name}-lambda-instance-policy"
 source_file   = ["dbsnapshot.js","deletesnapshots.py","dbconnect.js"]
-output_path   = ["snapshotDBFunction.zip","deletesnapshotFunction.zip","connectDBFunction.zip”"]
+output_path   = ["snapshotDBFunction.zip","deletesnapshotFunction.zip","connectDBFunction.zip"]
 filename      = ["snapshotDBFunction.zip", "deletesnapshotFunction.zip","connectDBFunction.zip"]
-function_name = ["snapshotDBFunction","deletesnapshotFunction", "connectDBFunction"]
+function_name = ["snapshotDBFunction","deletesnapshotFunction","connectDBFunction"]
 handler       = ["snapshot/dbsnapshot.handler","deletesnapshots.lambda_handler","ssh/dbconnect.handler"]
 role = module.iambackup.backuprole
 runtime = [ "nodejs18.x","python3.8","nodejs18.x"]
