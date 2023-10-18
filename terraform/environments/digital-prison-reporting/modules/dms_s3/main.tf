afa79ee874885084b8b8bdebc03d099222e6b2d1
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2023-10-18 13:27:42 +0100

    DPR2-165: Remove invalid GlueCatalogGeneration setting

--- terraform/environments/digital-prison-reporting/modules/dms_s3/main.tf
+++ terraform/environments/digital-prison-reporting/modules/dms_s3/main.tf
@@ -94,7 +94,6 @@ resource "aws_dms_s3_endpoint" "dms-s3-target-endpoint" {
   cdc_max_batch_interval   = 10
   include_op_for_full_load = true
   cdc_inserts_and_updates  = true
-  glue_catalog_generation  = true
 
   depends_on = [aws_iam_policy.dms-s3-target-policy, aws_iam_policy.dms-operator-s3-policy]
 
