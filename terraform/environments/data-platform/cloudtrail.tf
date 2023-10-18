291bbd31c1527283eb177e0a1aa68158f72059b0
LavMatt <mattlaverty@gmail.com>
2023-10-18 14:40:42 +0100

    add trtail back in

--- terraform/environments/data-platform/cloudtrail.tf
+++ terraform/environments/data-platform/cloudtrail.tf
@@ -1,49 +1,49 @@
-# # a cloudtrail trail to save log files for putObject S3 events in the landing and data
-# # buckets
-# resource "aws_cloudtrail" "data_s3_put_objects" {
-#   name                          = "data_platform_s3_putobject_trail_${local.environment}"
-#   s3_bucket_name                = module.logs_s3_bucket.bucket.id
+# a cloudtrail trail to save log files for putObject S3 events in the landing and data
+# buckets
+resource "aws_cloudtrail" "data_s3_put_objects" {
+  name                          = "data_platform_s3_putobject_trail_${local.environment}"
+  s3_bucket_name                = module.logs_s3_bucket.bucket.id
 
-#   # this is needed if monitoring services without a specific region. Don't need for s3
-#   include_global_service_events = false
+  # this is needed if monitoring services without a specific region. Don't need for s3
+  include_global_service_events = false
 
-#   # enabling this would allow detection of modified log files
-#   enable_log_file_validation    = false
-#   advanced_event_selector {
-#     name = "Log PutObject events for landing and data S3 buckets"
+  # enabling this would allow detection of modified log files
+  enable_log_file_validation    = false
+  advanced_event_selector {
+    name = "Log PutObject events for landing and data S3 buckets"
 
-#     field_selector {
-#       field  = "eventCategory"
-#       equals = ["Data"]
-#     }
+    field_selector {
+      field  = "eventCategory"
+      equals = ["Data"]
+    }
 
-#     field_selector {
-#       field = "eventName"
+    field_selector {
+      field = "eventName"
 
-#       equals = [
-#         "PutObject"
-#       ]
-#     }
+      equals = [
+        "PutObject"
+      ]
+    }
 
-#     field_selector {
-#       field = "resources.ARN"
+    field_selector {
+      field = "resources.ARN"
 
-#       # The trailing slash is intentional; do not exclude it.
-#       starts_with = [
-#         "${module.data_s3_bucket.bucket.arn}/",
-#         "${module.data_landing_s3_bucket.bucket.arn}/"
-#       ]
-#     }
+      # The trailing slash is intentional; do not exclude it.
+      starts_with = [
+        "${module.data_s3_bucket.bucket.arn}/",
+        "${module.data_landing_s3_bucket.bucket.arn}/"
+      ]
+    }
 
-#     # Remove this if we want to log read events too, like getObject
-#     field_selector {
-#       field  = "readOnly"
-#       equals = ["false"]
-#     }
+    # Remove this if we want to log read events too, like getObject
+    field_selector {
+      field  = "readOnly"
+      equals = ["false"]
+    }
 
-#     field_selector {
-#       field  = "resources.type"
-#       equals = ["AWS::S3::Object"]
-#     }
-#   }
-# }
+    field_selector {
+      field  = "resources.type"
+      equals = ["AWS::S3::Object"]
+    }
+  }
+}
