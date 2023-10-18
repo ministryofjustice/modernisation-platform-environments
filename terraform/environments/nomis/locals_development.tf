c2bb5c6229112602ec299d76f346c4c2b50a05bd
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-10-18 17:22:24 +0100

    Nomis: DSOS-2093: add oracle linux test and ansible fix (#3723)
    * remove accidental cut-and-paste commit
    
    * script for creating .ssh directory links
    
    * add OL85 test ASG
    
    * missing tag
    
    * test
    
    * rhel6 fix
    
    * update branch

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -92,6 +92,32 @@ locals {
         }
       }
 
+      dev-base-ol85 = {
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 0
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name          = "base_ol_8_5*"
+          availability_zone = null
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private-web"]
+        })
+        user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
+          args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
+            branch = "main"
+          })
+        })
+        tags = {
+          description = "For testing our base OL 8.5 base image"
+          ami         = "base_ol_8_5"
+          os-type     = "Linux"
+          component   = "test"
+          server-type = "base-ol85"
+        }
+      }
+
       dev-base-rhel85 = {
         autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
           desired_capacity = 0
@@ -231,7 +257,7 @@ locals {
         })
         user_data_cloud_init = merge(local.weblogic_ec2.user_data_cloud_init, {
           args = merge(local.weblogic_ec2.user_data_cloud_init.args, {
-            branch = "085f630e04fcfe3b521d0f7f698188df849ccb7e" # 2022-10-06 ssm changes
+            branch = "main"
           })
         })
         tags = merge(local.weblogic_ec2.tags, {
