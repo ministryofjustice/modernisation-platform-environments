6a787841725262bd2b76a60f74c0221679e00cd8
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-18 10:00:54 +0000

    Add Open Metadata Entra ID secrets
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/secrets.tf
+++ terraform/environments/data-platform-apps-and-tools/secrets.tf
@@ -1,1 +1,17 @@
-#### This file can be used to store secrets specific to the member account ####
+/*
+
+These have been created in code as we do not have permission to "secretsmanager:CreateSecret"
+
+*/
+
+resource "aws_secretsmanager_secret" "openmetadata_entra_id_client_id" {
+  count = terraform.workspace == "data-platform-apps-and-tools-production" ? 1 : 0
+
+  name = "openmetadata/entra-id/client-id"
+}
+
+resource "aws_secretsmanager_secret" "openmetadata_entra_id_tenant_id" {
+  count = terraform.workspace == "data-platform-apps-and-tools-production" ? 1 : 0
+
+  name = "openmetadata/entra-id/tenant-id"
+}
