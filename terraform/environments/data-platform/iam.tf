60448f0db861d1cea6737a5b1a51761d0c07b1b1
LavMatt <mattlaverty@gmail.com>
2023-10-18 12:25:33 +0100

    more cloudtrail perms for log bucket policy

--- terraform/environments/data-platform/iam.tf
+++ terraform/environments/data-platform/iam.tf
@@ -430,14 +430,28 @@ data "aws_iam_policy_document" "logs_s3_bucket_policy_document" {
     }
 
     actions = [
-      "s3:PutObject",
-      "s3:ListBucket"
+      "s3:PutObject"
     ]
 
     resources = [
       module.logs_s3_bucket.bucket.arn,
       "${module.logs_s3_bucket.bucket.arn}/*",
     ]
+    
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "bucket-owner-full-control"
+      ]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+
+      values   = [aws_cloudtrail.data_s3_put_objects.arn]
+    }
   }
 
   statement {
@@ -449,9 +463,21 @@ data "aws_iam_policy_document" "logs_s3_bucket_policy_document" {
       identifiers = ["cloudtrail.amazonaws.com"]
     }
 
-    actions = ["s3:GetBucketAcl"]
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:GetBucketAcl"
+    ]
 
     resources = [module.logs_s3_bucket.bucket.arn]
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+
+      values   = [aws_cloudtrail.data_s3_put_objects.arn]
+    }
   }
 
   statement {
@@ -566,4 +592,8 @@ data "aws_iam_policy_document" "iam_policy_document_for_create_schema_lambda" {
     data.aws_iam_policy_document.write_metadata.json,
     data.aws_iam_policy_document.create_write_lambda_logs.json,
   ]
+}
+
+output "cloudtrail_policy" {
+  value = data.aws_iam_policy_document.logs_s3_bucket_policy_document.json
 }
