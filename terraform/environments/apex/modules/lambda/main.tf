1b7b1e5a993c3affa4a9714ad008cfd5efa55873
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-10-20 11:03:45 +0100

    added input paramater and change cron time

--- terraform/environments/apex/modules/lambda/main.tf
+++ terraform/environments/apex/modules/lambda/main.tf
@@ -32,13 +32,17 @@ resource "aws_lambda_function" "snapshotDBFunction" {
 resource "aws_cloudwatch_event_rule" "mon_sun" {
     name = "laa-createSnapshotRule-LWN8E1LNHFJR"
     description = "Fires every five minutes"
-    schedule_expression = "cron(30 09 ? * MON-SUN *)"
+    schedule_expression = "cron(15 11 ? * MON-SUN *)"
+    
+    
 }
 
 resource "aws_cloudwatch_event_target" "check_mon_sun" {
     count = 1
     rule = aws_cloudwatch_event_rule.mon_sun.name
     arn = "${aws_lambda_function.snapshotDBFunction[0].arn}"
+    input = {"appname": "apex Database Server"}
+  
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_mon_sun" {
