6f8efb571bd0a403799cc4089b15138dd004dcb9
tajewole-moj <tunde.ajewole@digital.justice.gov.uk>
2023-10-20 11:26:21 +0100

    deploying everything again

--- terraform/environments/apex/modules/lambda/main.tf
+++ terraform/environments/apex/modules/lambda/main.tf
@@ -29,27 +29,27 @@ resource "aws_lambda_function" "snapshotDBFunction" {
 #   }
 }
 
-# resource "aws_cloudwatch_event_rule" "mon_sun" {
-#     name = "laa-createSnapshotRule-LWN8E1LNHFJR"
-#     description = "Fires every five minutes"
-#     schedule_expression = "cron(15 11 ? * MON-SUN *)"
+resource "aws_cloudwatch_event_rule" "mon_sun" {
+    name = "laa-createSnapshotRule-LWN8E1LNHFJR"
+    description = "Fires every five minutes"
+    schedule_expression = "cron(45 11 ? * MON-SUN *)"
     
     
-# }
+}
 
-# resource "aws_cloudwatch_event_target" "check_mon_sun" {
-#     count = 1
-#     rule = aws_cloudwatch_event_rule.mon_sun.name
-#     arn = "${aws_lambda_function.snapshotDBFunction[0].arn}"
-#     input = {"appname": "apex Database Server"}
+resource "aws_cloudwatch_event_target" "check_mon_sun" {
+    count = 1
+    rule = aws_cloudwatch_event_rule.mon_sun.name
+    arn = "${aws_lambda_function.snapshotDBFunction[0].arn}"
+    input = {"appname": "apex Database Server"}
   
-# }
+}
 
-# resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_mon_sun" {
-#     count = 1
-#     statement_id = "AllowExecutionFromCloudWatch"
-#     action = "lambda:InvokeFunction"
-#     function_name = aws_lambda_function.snapshotDBFunction[0].function_name
-#     principal = "events.amazonaws.com"
-#     source_arn = aws_cloudwatch_event_rule.mon_sun.arn
-# }
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_mon_sun" {
+    count = 1
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.snapshotDBFunction[0].function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.mon_sun.arn
+}
