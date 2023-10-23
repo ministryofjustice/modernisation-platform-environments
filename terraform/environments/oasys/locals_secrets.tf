2629b58e3411b6ac20ed57243ee8b7b37746d32e
W <willg258@hotmail.co.uk>
2023-10-23 15:42:05 +0100

    ..

--- terraform/environments/oasys/locals_secrets.tf
+++ terraform/environments/oasys/locals_secrets.tf
@@ -40,10 +40,10 @@ locals {
 
 
   secretsmanager_secrets_db = {
-    # policy = [
-    #   # local.secret_policy_read_db,
-    #   # local.secret_policy_write_db,
-    # ]
+    policy = [
+      local.secret_policy_read_db,
+      local.secret_policy_write_db,
+    ]
     secrets = {
       passwords = {}
     }
