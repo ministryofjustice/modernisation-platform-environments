2aa766f6e32c14bc001e737829405b63c7d28219
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2023-10-23 13:33:24 +0000

    Add three new secrets for ARC values

--- terraform/environments/data-platform-apps-and-tools/secrets.tf
+++ terraform/environments/data-platform-apps-and-tools/secrets.tf
@@ -15,3 +15,21 @@ resource "aws_secretsmanager_secret" "openmetadata_entra_id_tenant_id" {
 
   name = "openmetadata/entra-id/tenant-id"
 }
+
+resource "aws_secretsmanager_secret" "github_app_arc_app_id" {
+  count = terraform.workspace == "data-platform-apps-and-tools-production" ? 1 : 0
+
+  name = "github/arc/app-id"
+}
+
+resource "aws_secretsmanager_secret" "github_app_arc_install_id" {
+  count = terraform.workspace == "data-platform-apps-and-tools-production" ? 1 : 0
+
+  name = "github/arc/install-id"
+}
+
+resource "aws_secretsmanager_secret" "github_app_arc_private_key" {
+  count = terraform.workspace == "data-platform-apps-and-tools-production" ? 1 : 0
+
+  name = "github/arc/private-key"
+}
