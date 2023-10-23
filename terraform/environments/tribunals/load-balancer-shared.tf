d46406d7b64f5aaa395f1b858a822eb62b2b207e
Mark Butler <mark.butler@solirius.com>
2023-10-23 16:55:47 +0100

    Correct ingres rules for all traffic Remove appeals SAN

--- terraform/environments/tribunals/load-balancer-shared.tf
+++ terraform/environments/tribunals/load-balancer-shared.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "tribunals_lb_sc" {
     description = "allow all traffic on HTTPS port 443"
     from_port   = 443
     to_port     = 443
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -15,7 +15,7 @@ resource "aws_security_group" "tribunals_lb_sc" {
     description = "allow all traffic on HTTP port 80"
     from_port   = 80
     to_port     = 80
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
