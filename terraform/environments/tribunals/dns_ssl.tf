de011442158d12dbbc7f3f1596ae649a9fab6c50
Mark Butler <mark.butler@solirius.com>
2023-10-24 18:24:43 +0100

    Setup an array of tribunals domain names and use this to generate the Route 53 records
    Use a wildcard cert for sandbox

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -1,43 +1,15 @@
-locals {
-  
-  transport_url = "transportappeals"
-  appeals_url = "administrativeappeals" 
-}
-  
-resource "aws_acm_certificate_validation" "external" {
-  certificate_arn         = aws_acm_certificate.external.arn
-  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
+variable "record_names" {
+  type    = list(string)
+  default = ["transportappeals", "administrativeappeals"]
 }
 
-// Route53 DNS records for certificate validation
-resource "aws_route53_record" "external_validation" {
-  provider = aws.core-network-services
-
-  allow_overwrite = true
-  name            = local.domain_name_main[0]
-  records         = local.domain_record_main
-  ttl             = 60
-  type            = local.domain_type_main[0]
-  zone_id         = data.aws_route53_zone.network-services.zone_id
-}
-
-resource "aws_route53_record" "external_validation_subdomain" {
-  provider = aws.core-vpc
-
-  allow_overwrite = true
-  name            = local.domain_name_sub[0]
-  records         = local.domain_record_sub
-  ttl             = 60
-  type            = local.domain_type_sub[0]
-  zone_id         = data.aws_route53_zone.external.zone_id
-}
-
+# Define a wildcard ACM certificate for sandbox
 resource "aws_acm_certificate" "external" {
   domain_name       = "modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
   subject_alternative_names = [
-    "${local.transport_url}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+    "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   ]
   tags = {
     Environment = local.environment
@@ -48,30 +20,12 @@ resource "aws_acm_certificate" "external" {
   }
 }
 
-
-########## Transport Tribunals ###################
-
-// ACM Public Certificate
-# resource "aws_acm_certificate" "transport_external" {
-#   domain_name       = "modernisation-platform.service.justice.gov.uk"
-#   validation_method = "DNS"
-
-#   subject_alternative_names = ["${local.transport_url}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-#   tags = {
-#     Environment = local.environment
-#   }
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
-// Route53 DNS record for directing traffic to the service
+// Create one Route 53 record for each entry in record names
 resource "aws_route53_record" "external" {
   provider = aws.core-vpc
-
+  count = length(var.record_names)
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "${local.transport_url}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "${var.record_names[count.index]}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
 
   alias {
@@ -81,34 +35,47 @@ resource "aws_route53_record" "external" {
   }
 }
 
-########## Administrative Appeals ###################
+# ACM certificate validation
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn = aws_acm_certificate.external.arn
+}
 
-// ACM Public Certificate
-# resource "aws_acm_certificate" "appeals_external" {
-#   domain_name       = "modernisation-platform.service.justice.gov.uk"
-#   validation_method = "DNS"
+output "acm_certificate_arn" {
+  value = aws_acm_certificate.external.arn
+}
 
-#   subject_alternative_names = ["${local.appeals_url}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-#   tags = {
-#     Environment = local.environment
-#   }
+output "acm_certificate_validation_dns" {
+  value = aws_acm_certificate.external.domain_validation_options.0.resource_record_name
+}
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+output "acm_certificate_validation_route53" {
+  value = aws_acm_certificate.external.domain_validation_options.0.resource_record_value
+}
 
-// Route53 DNS record for directing traffic to the service
-# resource "aws_route53_record" "appeals_external" {
-#   provider = aws.core-vpc
-
-#   zone_id = data.aws_route53_zone.external.zone_id
-#   name    = "${local.appeals_url}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-#   type    = "A"
-
-#   alias {
-#     name                   = aws_lb.tribunals_lb.dns_name
-#     zone_id                = aws_lb.tribunals_lb.zone_id
-#     evaluate_target_health = true
-#   }
-# }
+//// Route53 DNS records for certificate validation
+//resource "aws_acm_certificate_validation" "external" {
+//  certificate_arn         = aws_acm_certificate.external.arn
+//  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
+//}
+//
+//resource "aws_route53_record" "external_validation" {
+//  provider = aws.core-network-services
+//
+//  allow_overwrite = true
+//  name            = local.domain_name_main[0]
+//  records         = local.domain_record_main
+//  ttl             = 60
+//  type            = local.domain_type_main[0]
+//  zone_id         = data.aws_route53_zone.network-services.zone_id
+//}
+//
+//resource "aws_route53_record" "external_validation_subdomain" {
+//  provider = aws.core-vpc
+//
+//  allow_overwrite = true
+//  name            = local.domain_name_sub[0]
+//  records         = local.domain_record_sub
+//  ttl             = 60
+//  type            = local.domain_type_sub[0]
+//  zone_id         = data.aws_route53_zone.external.zone_id
+//}
