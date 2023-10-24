7cd2fd17cadf5d6ade26e226a8acd7f36006e5ff
Mark Butler <mark.butler@solirius.com>
2023-10-24 18:41:38 +0100

    use for_each for outputs

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -45,11 +45,20 @@ output "acm_certificate_arn" {
 }
 
 output "acm_certificate_validation_dns" {
-  value = aws_acm_certificate.external.domain_validation_options[0].resource_record_name
+  for_each = {
+    for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
+      value = dvo.resource_record_name
+  }
+}
+
 }
 
 output "acm_certificate_validation_route53" {
-  value = aws_acm_certificate.external.domain_validation_options[0].resource_record_value
+  for_each = {
+    for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
+      value = dvo.resource_record_value
+    }
+  }
 }
 
 //// Route53 DNS records for certificate validation
