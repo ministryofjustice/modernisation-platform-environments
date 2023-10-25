9862e13e334e9565b9db8b8b78daf3110ff43ea5
Mark Butler <mark.butler@solirius.com>
2023-10-25 15:02:14 +0100

    Use a modified domain name and a new list of validation fqdns

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -7,30 +7,25 @@ variable "record_names" {
 # Just validate on the main domain name record
 resource "aws_acm_certificate_validation" "external" {
   certificate_arn = aws_acm_certificate.external.arn
-  validation_record_fqdns = [local.domain_name_main[0]]
+  validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
 }
 
-// Route53 record for Validation of external
 resource "aws_route53_record" "external_validation" {
   provider = aws.core-network-services
+  for_each = {
+    for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
   allow_overwrite = true
-  name            = local.domain_name_main[0]
-  records         = local.domain_record_main
+  name            = each.value.name
+  records         = [each.value.record]
   ttl             = 60
-  type            = local.domain_type_main[0]
+  type            = each.value.type
   zone_id         = data.aws_route53_zone.network-services.zone_id
-}
-
-resource "aws_route53_record" "external_validation_subdomain" {
-  provider = aws.core-vpc
-
-  allow_overwrite = true
-  name            = local.domain_name_sub[0]
-  records         = local.domain_record_sub
-  ttl             = 60
-  type            = local.domain_type_sub[0]
-  zone_id         = data.aws_route53_zone.external.zone_id
 }
 
 // Create one Route 53 record for each entry in record names
@@ -48,9 +43,9 @@ resource "aws_route53_record" "external" {
   }
 }
 
-# Define a wildcard ACM certificate for sandbox
+# Define a wildcard ACM certificate for sandbox/dev
 resource "aws_acm_certificate" "external" {
-  domain_name       = "modernisation-platform.service.justice.gov.uk"
+  domain_name       = "${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
   subject_alternative_names = [
