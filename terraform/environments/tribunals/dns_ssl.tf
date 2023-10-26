6ff1408958e998500f22a16c95de9b13cedf3263
Mark Butler <mark.butler@solirius.com>
2023-10-26 17:18:20 +0100

    Move the list of tribunals into platform_locals so we can reuse it to build counts of unique resources for each tribunal

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -1,15 +1,10 @@
-variable "record_names" {
-  type    = list(string)
-  default = ["transportappeals", "administrativeappeals"]
-}
-
 # ACM certificate validation
-# Just validate on the main domain name record
 resource "aws_acm_certificate_validation" "external" {
   certificate_arn = aws_acm_certificate.external.arn
   validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
 }
 
+# One route53 record required for each domain listed in the external certificate
 resource "aws_route53_record" "external_validation" {
   provider = aws.core-vpc
   for_each = {
@@ -28,12 +23,12 @@ resource "aws_route53_record" "external_validation" {
   zone_id         = data.aws_route53_zone.external.zone_id
 }
 
-// Create one Route 53 record for each entry in record names
+// Create one Route 53 record for each entry in the list of tribunals (assigned in platform_locals.tf)
 resource "aws_route53_record" "external" {
   provider = aws.core-vpc
-  count = length(var.record_names)
+  count = length(local.tribunal_names)
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "${var.record_names[count.index]}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "${local.tribunal_names[count.index]}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
 
   alias {
@@ -71,31 +66,3 @@ output "acm_certificate_validation_dns" {
 output "acm_certificate_validation_route53" {
   value = [for dvo in aws_acm_certificate.external.domain_validation_options : dvo.resource_record_value]
 }
-
-//// Route53 DNS records for certificate validation
-//resource "aws_acm_certificate_validation" "external" {
-//  certificate_arn         = aws_acm_certificate.external.arn
-//  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
-//}
-//
-//resource "aws_route53_record" "external_validation" {
-//  provider = aws.core-network-services
-//
-//  allow_overwrite = true
-//  name            = local.domain_name_main[0]
-//  records         = local.domain_record_main
-//  ttl             = 60
-//  type            = local.domain_type_main[0]
-//  zone_id         = data.aws_route53_zone.network-services.zone_id
-//}
-//
-//resource "aws_route53_record" "external_validation_subdomain" {
-//  provider = aws.core-vpc
-//
-//  allow_overwrite = true
-//  name            = local.domain_name_sub[0]
-//  records         = local.domain_record_sub
-//  ttl             = 60
-//  type            = local.domain_type_sub[0]
-//  zone_id         = data.aws_route53_zone.external.zone_id
-//}
--- terraform/environments/tribunals/platform_locals.tf
+++ terraform/environments/tribunals/platform_locals.tf
@@ -2,6 +2,11 @@ locals {
 
   application_name = "tribunals"
 
+  tribunal_names = [
+    "transportappeals",
+    "administrativeappeals"
+  ]
+
   environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
 
   # Stores modernisation platform account id for setting up the modernisation-platform provider
