436a5c7c6e2a5318486e2c34dfb525b33f7b3599
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-10-26 15:43:05 +0100

    Tipstaff: Add Bastion Config

--- terraform/environments/tipstaff/rds.tf
+++ terraform/environments/tipstaff/rds.tf
@@ -24,14 +24,6 @@ resource "aws_security_group" "postgresql_db_sc" {
   name        = "postgres_security_group"
   description = "control access to the database"
   vpc_id      = data.aws_vpc.shared.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    description = "MOJ Digital VPN access"
-    cidr_blocks = [local.application_data.accounts[local.environment].moj_ip]
-  }
   ingress {
     from_port       = 5432
     to_port         = 5432
@@ -45,6 +37,15 @@ resource "aws_security_group" "postgresql_db_sc" {
     protocol    = "tcp"
     description = "Allows Github Actions to access RDS"
     cidr_blocks = ["${jsondecode(data.http.myip.response_body)["ip"]}/32"]
+  }
+  ingress {
+    protocol    = "tcp"
+    description = "Allow PSQL traffic from bastion"
+    from_port   = 5432
+    to_port     = 5432
+    security_groups = [
+      module.bastion_linux.bastion_security_group
+    ]
   }
   egress {
     description = "allow all outbound traffic"
