46a2a756adff015c9d03b441373a490ef0d35085
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-10-26 17:04:03 +0100

    testing

--- terraform/environments/data-platform/iam.tf
+++ terraform/environments/data-platform/iam.tf
@@ -446,12 +446,6 @@ data "aws_iam_policy_document" "logs_s3_bucket_policy_document" {
         "bucket-owner-full-control"
       ]
     }
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "aws:SourceArn"
-
-    #   values = [aws_cloudtrail.data_s3_put_objects.arn]
-    # }
   }
 
   statement {
@@ -469,12 +463,6 @@ data "aws_iam_policy_document" "logs_s3_bucket_policy_document" {
 
     resources = [module.logs_s3_bucket.bucket.arn]
 
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "aws:SourceArn"
-
-    #   values = [aws_cloudtrail.data_s3_put_objects.arn]
-    # }
   }
 
   statement {
