2f8dbea3c7375991f6f0b0b119c9c4fa0d9983ab
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2023-10-26 10:56:24 +0100

    CC-2147: Update certs

--- terraform/environments/ccms-ebs-upgrade/r53.tf
+++ terraform/environments/ccms-ebs-upgrade/r53.tf
@@ -2,7 +2,7 @@
 resource "aws_route53_record" "ebsdb" {
   provider = aws.core-vpc
   zone_id  = data.aws_route53_zone.external.zone_id
-  name    = "ccms-ebs-db.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "ccms-ebs-db-upgrade.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
   ttl     = 300
   records = [aws_instance.ec2_oracle_ebs.private_ip]
