de03882e75b86cf4c5f69994b1ee95e31df74126
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2023-10-30 11:53:59 +0000

    CC-2147: Added Conc Tier Instance and SG

--- terraform/environments/ccms-ebs-upgrade/r53.tf
+++ terraform/environments/ccms-ebs-upgrade/r53.tf
@@ -16,5 +16,5 @@ resource "aws_route53_record" "ebsconc" {
   name    = "ccms-ebs-conc-upgrade.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.ec2_oracle_ebs.private_ip]
+  records = [aws_instance.ec2_oracle_conc.private_ip]
 }
