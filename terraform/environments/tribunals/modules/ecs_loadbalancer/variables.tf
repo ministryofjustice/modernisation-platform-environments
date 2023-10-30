50cadb3509ac59871aeedbc5fdd95f2c1538d943
Mark Butler <mark.butler@solirius.com>
2023-10-30 13:38:44 +0000

    Add tags_common variable for loadbalancer module

--- terraform/environments/tribunals/modules/ecs_loadbalancer/variables.tf
+++ terraform/environments/tribunals/modules/ecs_loadbalancer/variables.tf
@@ -2,3 +2,8 @@ variable "app_name" {
   type        = string
   description = "Name of the application"
 }
+
+variable "tags_common" {
+  type        = map(string)
+  description = "Common tags to be used by all resources"
+}
