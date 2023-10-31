e6b3f38adec7eb9db31e547c43d3b06d28bb1240
Robert Sweetman <robertiansweetman@gmail.com>
2023-10-31 09:13:24 +0000

    create common config for planetfm web instances (#3823)
    * create common config for planetfm web instances
    
    * formatting

--- terraform/environments/planetfm/locals_preproduction.tf
+++ terraform/environments/planetfm/locals_preproduction.tf
@@ -4,33 +4,23 @@ locals {
   # baseline config
   preproduction_config = {
     baseline_ec2_instances = {
-      pp-cafm-w-5-a = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name                      = "pp-cafm-w-5-a"
-          ami_owner                     = "self"
-          availability_zone             = "${local.region}a"
-          ebs_volumes_copy_all_from_ami = false
+      pp-cafm-w-5-a = merge(local.web_ec2, {
+        config = merge(local.web_ec2.config, {
+          ami_name          = "pp-cafm-w-5-a"
+          availability_zone = "${local.region}a"
         })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          instance_type           = "t3.large"
-          disable_api_termination = true
-          monitoring              = true
-          vpc_security_group_ids  = ["domain", "web", "jumpserver"]
-          tags = {
-            backup-plan = "daily-and-weekly"
-          }
+        instance = merge(local.web_ec2.instance, {
+          instance_type = "t3.large"
         })
-        ebs_volumes = {
+        ebs_volumes = merge(local.web_ec2.ebs_volumes, {
           "/dev/sda1" = { type = "gp3", size = 128 } # root volume
           "/dev/sdb"  = { type = "gp3", size = 100 }
-        }
-        tags = {
+        })
+        tags = merge(local.web_ec2.tags, {
           description = "copy of PPFWW0005 for planetfm ${local.environment}"
-          os-type     = "Windows"
           ami         = "pp-cafm-w-5-a"
-          component   = "web"
-        }
-      }
+        })
+      })
     }
   }
 }
---
+++ terraform/environments/planetfm/locals_web.tf
@@ -0,0 +1,25 @@
+locals {
+  web_ec2 = {
+    config = merge(module.baseline_presets.ec2_instance.config.default, {
+      ami_owner                     = "self"
+      ebs_volumes_copy_all_from_ami = false
+    })
+    instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+      disable_api_termination = true
+      monitoring              = true
+      vpc_security_group_ids  = ["domain", "web", "jumpserver"]
+      tags = {
+        backup-plan         = "daily-and-weekly"
+        instance-scheduling = "skip-scheduling"
+      }
+    })
+    ebs_volumes = {
+      "/dev/sdb" = { label = "app" }
+    }
+    tags = {
+      os-type   = "Windows"
+      component = "web"
+    }
+    route53_records = module.baseline_presets.ec2_instance.route53_records.internal_and_external
+  }
+}
