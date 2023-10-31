0d73c4fac2eda3231c3c75c59567911acd61daba
pav <pav.gainda@digital.justice.gov.uk>
2023-10-31 11:31:43 +0000

    Restore lb and use new bugfix branch

--- terraform/modules/baseline/lb.tf
+++ terraform/modules/baseline/lb.tf
@@ -97,7 +97,7 @@ module "lb" {
 
   for_each = var.lbs
 
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=v3.2.0"
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=bugfix/DSO-2309"
 
   providers = {
     aws.bucket-replication = aws
