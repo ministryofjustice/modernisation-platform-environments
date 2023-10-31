e61322e6328951f4e1979689e4393e8fa91a4eda
pav <pav.gainda@digital.justice.gov.uk>
2023-10-31 15:13:31 +0000

    Use main lb branch

--- terraform/modules/baseline/lb.tf
+++ terraform/modules/baseline/lb.tf
@@ -97,7 +97,7 @@ module "lb" {
 
   for_each = var.lbs
 
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=bugfix/DSO-2309"
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=v3.2.0"
 
   providers = {
     aws.bucket-replication = aws
