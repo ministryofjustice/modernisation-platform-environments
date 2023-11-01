c1b70dc29c81a0bc1cf184876afe2f835e7ae2de
IjazMoJ <134407207+IjazMoJ@users.noreply.github.com>
2023-11-01 14:15:55 +0000

    Csr/dsos 2258/terraform ami build (#3856)
    * preprod csr App 3 build
    
    * AZ corrected

--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -414,6 +414,39 @@ locals {
           create_external_record = true
         }
       }
+
+      pp-csr-a-3-a = {
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "pp-csr-a-3-a"
+          ami_owner                     = "self"
+          availability_zone             = "${local.region}a"
+          ebs_volumes_copy_all_from_ami = false
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          instance_type           = "m5.2xlarge"
+          disable_api_termination = true
+          monitoring              = true
+          vpc_security_group_ids  = ["domain", "app", "jumpserver"]
+          tags = {
+            backup-plan         = "daily-and-weekly"
+            instance-scheduling = "skip-scheduling"
+          }
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 128 } # root volume
+          "/dev/sdb"  = { type = "gp3", size = 56 }
+        }
+        tags = {
+          description = "copy of PPCAW00003 for csr ${local.environment}"
+          os-type     = "Windows"
+          ami         = "pp-csr-a-3-a"
+          component   = "app"
+        }
+        route53_records = {
+          create_internal_record = true
+          create_external_record = true
+        }
+      }
     }
 
     baseline_ec2_autoscaling_groups = {
