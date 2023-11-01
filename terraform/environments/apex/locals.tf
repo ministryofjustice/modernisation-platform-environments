6501c3cf97a7cfc921b4fb4f9c86420090ca2e34
Tariq Mahmood <tariq.mahmood@digital.justice.gov.uk>
2023-11-01 12:33:46 +0000

    testing lambda with new time

--- terraform/environments/apex/locals.tf
+++ terraform/environments/apex/locals.tf
@@ -5,7 +5,7 @@ locals {
   dbsnapshot_source_file = "dbsnapshot.js"
   deletesnapshot_source_file = "deletesnapshots.py"
   dbconnect_source_file = "dbconnect.js"
-  
+
   dbsnapshot_output_path = "dbsnapshot.zip"
   deletesnapshot_output_path = "deletesnapshots.zip"
   dbconnect_output_path = "dbconnect.zip"
@@ -13,7 +13,7 @@ locals {
   #Lambda Function creation
   snapshotDBFunctionname = "snapshotDBFunction"
   snapshotDBFunctionhandler = "snapshot/dbsnapshot.handler"
-  snapshotDBFunctionruntime = "nodejs14.x"
+  snapshotDBFunctionruntime = "nodejs18.x"
   snapshotDBFunctionfilename = "dbsnapshot.zip"
 
   deletesnapshotFunctionname = "deletesnapshotFunction"
@@ -23,12 +23,12 @@ locals {
 
   connectDBFunctionname = "connectDBFunction"
   connectDBFunctionhandler = "ssh/dbconnect.handler"
-  connectDBFunctionruntime = "nodejs14.x"
+  connectDBFunctionruntime = "nodejs18.x"
   connectDBFunctionfilename = "dbconnect.zip"
 
   #layer config
   s3layerkey = "nodejs.zip"
-  compatible_runtimes = "nodejs14.x"
+  compatible_runtimes = "nodejs18.x"
 
   application_test_url = "https://apex.laa-development.modernisation-platform.service.justice.gov.uk/apex/"
 }
