e6cab7d60f2f2adcb47bc757d56e61346de57ba4
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-11-08 11:15:09 +0000

    terraform fmt

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -17,7 +17,7 @@ locals {
     baseline_s3_buckets = {
       nomis-audit-archives = {
         custom_kms_key = module.environment.kms_keys["general"].arn
-        iam_policies = module.baseline_presets.s3_iam_policies
+        iam_policies   = module.baseline_presets.s3_iam_policies
       }
       nomis-db-backup-bucket = {
         custom_kms_key = module.environment.kms_keys["general"].arn
