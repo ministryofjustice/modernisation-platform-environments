49b05209062faa00620ce434556ac5c823631baa
Robert Sweetman <robertiansweetman@gmail.com>
2023-11-09 14:26:48 +0000

    add alarm defaults and templates for web, app and database instances (#3939)
    * add alarm defaults and templates for web, app and database instances
    
    * don't actually deploy alarms for now

--- terraform/environments/planetfm/locals_app.tf
+++ terraform/environments/planetfm/locals_app.tf
@@ -1,5 +1,5 @@
 locals {
-  web_ec2 = {
+  app_ec2 = {
     config = merge(module.baseline_presets.ec2_instance.config.default, {
       ami_owner                     = "self"
       ebs_volumes_copy_all_from_ami = false
@@ -8,21 +8,21 @@ locals {
       disable_api_termination = true
       disable_api_stop        = true
       monitoring              = true
-      vpc_security_group_ids  = ["domain", "web", "jumpserver"]
+      vpc_security_group_ids  = ["domain", "app", "jumpserver"]
       tags = {
         backup-plan         = "daily-and-weekly"
         instance-scheduling = "skip-scheduling"
       }
     })
-    cloudwatch_metric_alarms = local.web_ec2_cloudwatch_metric_alarms
+    cloudwatch_metric_alarms = local.app_ec2_cloudwatch_metric_alarms
     tags = {
       os-type   = "Windows"
-      component = "web"
+      component = "app"
     }
     route53_records = module.baseline_presets.ec2_instance.route53_records.internal_and_external
   }
 
-  web_ec2_cloudwatch_metric_alarms = merge(
+  app_ec2_cloudwatch_metric_alarms = merge(
     module.baseline_presets.cloudwatch_metric_alarms.ec2,
     module.baseline_presets.cloudwatch_metric_alarms.ec2_cwagent_windows
   )
--- terraform/environments/planetfm/locals_database.tf
+++ terraform/environments/planetfm/locals_database.tf
@@ -1,5 +1,6 @@
 locals {
-  web_ec2 = {
+
+  database_ec2 = {
     config = merge(module.baseline_presets.ec2_instance.config.default, {
       ami_owner                     = "self"
       ebs_volumes_copy_all_from_ami = false
@@ -8,21 +9,21 @@ locals {
       disable_api_termination = true
       disable_api_stop        = true
       monitoring              = true
-      vpc_security_group_ids  = ["domain", "web", "jumpserver"]
+      vpc_security_group_ids  = ["domain", "database", "jumpserver"]
       tags = {
         backup-plan         = "daily-and-weekly"
         instance-scheduling = "skip-scheduling"
       }
     })
-    cloudwatch_metric_alarms = local.web_ec2_cloudwatch_metric_alarms
+    cloudwatch_metric_alarms = local.database_ec2_cloudwatch_metric_alarms
     tags = {
       os-type   = "Windows"
-      component = "web"
+      component = "database"
     }
     route53_records = module.baseline_presets.ec2_instance.route53_records.internal_and_external
   }
 
-  web_ec2_cloudwatch_metric_alarms = merge(
+  database_ec2_cloudwatch_metric_alarms = merge(
     module.baseline_presets.cloudwatch_metric_alarms.ec2,
     module.baseline_presets.cloudwatch_metric_alarms.ec2_cwagent_windows
   )
