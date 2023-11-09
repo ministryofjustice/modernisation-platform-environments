5d1d67151669e9d674a6ca5fd571050cbfe22f9b
W <willg258@hotmail.co.uk>
2023-11-09 09:24:16 +0000

    combine-ssm-and-secrets in ec2 instance
    combine-ssm-and-secrets in ec2 instance

--- terraform/modules/baseline/ec2_instance.tf
+++ terraform/modules/baseline/ec2_instance.tf
@@ -3,7 +3,7 @@ module "ec2_instance" {
 
   for_each = var.ec2_instances
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance?ref=v2.3.0"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance?ref=combine-ssm-and-secrets"
 
   providers = {
     aws.core-vpc = aws.core-vpc
