ebc5bb01a4b9b7658018b921692c5c61fed83d7b
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2023-11-10 10:09:48 +0000

    deploying automation ssm docs into planet fm

--- terraform/environments/planetfm/ec2-common.tf
+++ terraform/environments/planetfm/ec2-common.tf
@@ -11,3 +11,45 @@ resource "aws_ssm_document" "windows_domain_join" {
     },
   )
 }
+
+resource "aws_ssm_document" "cloud_watch_agent" {
+  name            = "windows-cloudwatch-agent-config"
+  document_type   = "Command"
+  document_format = "YAML"
+  content         = file("./ssm-documents/windows-cloudwatch-agent-config.yaml")
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "windows-cloudwatch-agent-config"
+    },
+  )
+}
+
+resource "aws_ssm_document" "ami_build_command" {
+  name            = "ami-build-command"
+  document_type   = "Command"
+  document_format = "YAML"
+  content         = file("./ssm-documents/ami-build-command.yaml")
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "ami-build-command"
+    },
+  )
+}
+
+resource "aws_ssm_document" "ami_build_automation" {
+  name            = "ami-build-automation"
+  document_type   = "Automation"
+  document_format = "YAML"
+  content         = file("./ssm-documents/ami-build-automation.yaml")
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "ami-build-automation"
+    },
+  )
+}
