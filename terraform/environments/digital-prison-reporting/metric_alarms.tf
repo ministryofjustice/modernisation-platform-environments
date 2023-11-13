2e80a4a6ae217da13ed180154b1f10314183283e
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-11-13 18:12:04 +0000

    Shift Alarms control to dynamic vars

--- terraform/environments/digital-prison-reporting/metric_alarms.tf
+++ terraform/environments/digital-prison-reporting/metric_alarms.tf
@@ -5,14 +5,14 @@
 # Otherwise, the cluster is unhealthy. An unhealthy status can occur when the cluster database is under extremely heavy load or if there is a configuration problem with a database on the cluster.
 module "dpr_redshift_health_status_check" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_redshift_health_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_redshift_health_check ? true : false
 
   alarm_name          = "dpr-redshift-health-status"
   alarm_description   = "ATTENTION: DPR Redshift HealthStatus Monitor, Please investigate Redshift Errors !"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = 1 # Boolean
-  threshold           = 1
-  period              = 60
+  threshold           = local.thrld_redshift_health_check
+  period              = local.period_redshift_health_check
 
   namespace   = "AWS/Redshift"
   metric_name = "HealthStatus"
@@ -26,13 +26,13 @@ module "dpr_redshift_health_status_check" {
 # Alarm - "DMS Stop Monitor"
 module "dpr_dms_stoptask_check" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_stop_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_stop_check ? true : false
 
   alarm_name          = "dpr-dms-stop-task"
   alarm_description   = "ATTENTION: DPR DMS Replication Stop Monitor, Please investigate DMS Replication Task Errors !"
   comparison_operator = "GreaterThanThreshold"
-  threshold           = 0 # Boolean
-  period              = 30
+  threshold           = local.thrld_dms_stop_check # Boolean
+  period              = local.period_dms_stop_check
   evaluation_periods  = 1
 
   dimensions = {
@@ -52,13 +52,13 @@ module "dpr_dms_stoptask_check" {
 # Alarm - "DMS Start Monitor"
 module "dpr_dms_starttask_check" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_start_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_start_check ? true : false
 
   alarm_name          = "dpr-dms-start-task"
   alarm_description   = "ATTENTION: DPR DMS Replication Start Monitor, Please investigate DMS Replication Task Errors !"
   comparison_operator = "GreaterThanThreshold"
-  threshold           = 0 # Boolean
-  period              = 30
+  threshold           = local.thrld_dms_start_check # Boolean
+  period              = local.period_dms_start_check
   evaluation_periods  = 1
 
   dimensions = {
@@ -78,14 +78,14 @@ module "dpr_dms_starttask_check" {
 # Alarm - "DMS CPU Utilization Monitor"
 module "dpr_dms_cpu_utils_check" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_cpu_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_cpu_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-cpu-utilization"
   alarm_description   = "ATTENTION: DPR DMS Instance CPU Monitor, Please investigate High CPU Utilization for DMS Instance !"
   comparison_operator = "GreaterThanThreshold"
-  period              = 300
+  period              = local.period_dms_cpu_check
   evaluation_periods  = 1
-  threshold           = 80 # 80% CPU
+  threshold           = local.thrld_dms_cpu_check # 80% CPU
 
   dimensions = {
     "ReplicationInstanceIdentifier" = module.dms_nomis_ingestor.dms_instance_name
@@ -101,14 +101,14 @@ module "dpr_dms_cpu_utils_check" {
 # Alarm - "DMS FreeMemory Monitor"
 module "dpr_dms_free_memory_check" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_freemem_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_freemem_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-free-memory"
   alarm_description   = "ATTENTION: DPR DMS Instance FreeMemory Monitor, Please investigate FreeMemory is Below 1Gb DMS Instance !"
   comparison_operator = "LessThanThreshold"
-  period              = 300
+  period              = local.period_dms_freemem_check 
   evaluation_periods  = 1
-  threshold           = 1000000000 # 1Gb
+  threshold           = local.thrld_dms_freemem_check # 1Gb
 
   dimensions = {
     "ReplicationInstanceIdentifier" = module.dms_nomis_ingestor.dms_instance_name
@@ -124,14 +124,14 @@ module "dpr_dms_free_memory_check" {
 # Alarm - "DMS FreeableMemory Monitor"
 module "dpr_dms_freeable_memory_check" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_freeablemem_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_freeablemem_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-freeable-memory"
   alarm_description   = "ATTENTION: DPR DMS Instance Freeable Memory Monitor, Please investigate low FreeableMemory for Nomis DMS Instance !"
   comparison_operator = "LessThanOrEqualToThreshold"
-  period              = 300
+  period              = local.period_dms_freeablemem_check
   evaluation_periods  = 1
-  threshold           = 1000000000 # 1Gb
+  threshold           = local.thrld_dms_freeablemem_check # 1Gb
 
   dimensions = {
     "ReplicationInstanceIdentifier" = module.dms_nomis_ingestor.dms_instance_name
@@ -148,14 +148,14 @@ module "dpr_dms_freeable_memory_check" {
 # https://repost.aws/knowledge-center/dms-swap-files-consuming-space
 module "dpr_dms_swap_usage_check" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_swapusage_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_swapusage_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-swap-usage"
   alarm_description   = "ATTENTION: DPR DMS Instance SWAP Usage Monitor, Please investigate SWAP Usage is Above 0.75 Gb for DMS Instance!"
   comparison_operator = "GreaterThanThreshold"
-  period              = 300
+  period              = local.period__dms_swapusage_check 
   evaluation_periods  = 1
-  threshold           = 750000000 # 0.75Gb
+  threshold           = local.thrld__dms_swapusage_check # 0.75Gb
 
   dimensions = {
     "ReplicationInstanceIdentifier" = module.dms_nomis_ingestor.dms_instance_name
@@ -172,14 +172,14 @@ module "dpr_dms_swap_usage_check" {
 # https://repost.aws/knowledge-center/dms-swap-files-consuming-space
 module "dpr_dms_network_transmit_throughput" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_network_trans_tp_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_network_trans_tp_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-network-transmit-throughput"
   alarm_description   = "ATTENTION: DPR DMS Instance Network Throughput Monitor, Please investigate Network Transmit Throughput is below Threshold 1000 Bytes!"
   comparison_operator = "LessThanThreshold"
-  period              = 300
+  period              = local.period_dms_network_trans_tp_check
   evaluation_periods  = 1
-  threshold           = 10 # 10 Bytes
+  threshold           = local.thrld_dms_network_trans_tp_check # 10 Bytes
 
   namespace   = "AWS/DMS"
   metric_name = "NetworkTransmitThroughput"
@@ -192,14 +192,14 @@ module "dpr_dms_network_transmit_throughput" {
 # https://repost.aws/knowledge-center/dms-swap-files-consuming-space
 module "dpr_dms_network_receive_throughput" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_network_rec_tp_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_network_rec_tp_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-network-receive-throughput"
   alarm_description   = "ATTENTION: DPR DMS Instance Network Throughput Monitor, Please investigate Network Receive Throughput is below Threshold 10 Bytes!"
   comparison_operator = "LessThanThreshold"
-  period              = 300
+  period              = local.period_dms_network_rec_tp_check
   evaluation_periods  = 1
-  threshold           = 10 # 10 Bytes
+  threshold           = local.thrld_dms_network_rec_tp_check # 10 Bytes
 
   namespace   = "AWS/DMS"
   metric_name = "NetworkReceiveThroughput"
@@ -214,14 +214,14 @@ module "dpr_dms_network_receive_throughput" {
 # High CDCLatencySource means the process of capturing changes from source is delayed.
 module "dpr_dms_cdc_source_latency" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_cdc_src_lat_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_cdc_src_lat_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-cdc-source-latency"
   alarm_description   = "ATTENTION: P1 Incident: DPR DMS CDC Source Latency, Please investigate CDC Source Latency for Oracle Nomis is greater than 60 mins !"
   comparison_operator = "GreaterThanThreshold"
-  period              = 900
+  period              = local.period_dms_cdc_src_lat_check
   evaluation_periods  = 1
-  threshold           = 3600 # 60 mins
+  threshold           = local.thrld_dms_cdc_src_lat_check # 60 mins
 
   dimensions = {
     "ReplicationInstanceIdentifier" = module.dms_nomis_ingestor.dms_instance_name
@@ -240,14 +240,14 @@ module "dpr_dms_cdc_source_latency" {
 # Target latency is the difference between the replication instance server time and the oldest unconfirmed event id forwarded to a target component. 
 module "dpr_dms_cdc_target_latency" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_cdc_targ_lat_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_cdc_targ_lat_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-cdc-target-latency"
   alarm_description   = "ATTENTION: P1 Incident: DPR DMS CDC Target Latency, Please investigate CDC Target Latency for Oracle Nomis is greater than 60 mins !"
   comparison_operator = "GreaterThanThreshold"
-  period              = 900
+  period              = local.period_dms_cdc_targ_lat_check
   evaluation_periods  = 1
-  threshold           = 3600 # 60 mins
+  threshold           = local.thrld_dms_cdc_targ_lat_check # 60 mins
 
   dimensions = {
     "ReplicationInstanceIdentifier" = module.dms_nomis_ingestor.dms_instance_name
@@ -268,14 +268,14 @@ module "dpr_dms_cdc_target_latency" {
 # thus causing high target latency.
 module "dpr_dms_cdc_incoming_events" {
   source              = "./modules/cw_alarm"
-  create_metric_alarm = local.enable_dms_cdc_inc_events_check
+  create_metric_alarm = local.enable_cw_alarm && local.enable_dms_cdc_inc_events_check ? true : false
 
   alarm_name          = "dpr-dms-nomis-cdc-incoming-events"
   alarm_description   = "ATTENTION: P1 Incident: DPR DMS CDC Incoming Events Alert, Please investigate CDC Incoming Events are waiting to be applied for Oracle Nomis !"
   comparison_operator = "GreaterThanThreshold"
-  period              = 60
+  period              = local.period_dms_cdc_inc_events_check
   evaluation_periods  = 1
-  threshold           = 100 # 100 events 
+  threshold           = local.thrld_dms_cdc_inc_events_check # 100 events 
 
   dimensions = {
     "ReplicationInstanceIdentifier" = module.dms_nomis_ingestor.dms_instance_name
