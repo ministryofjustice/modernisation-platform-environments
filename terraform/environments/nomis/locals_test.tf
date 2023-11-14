f34260139fe4b67fdcdac25666424a240e0361e4
W <willg258@hotmail.co.uk>
2023-11-14 14:16:06 +0000

    Update locals_test.tf

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -644,7 +644,7 @@ locals {
         })
         tags = merge(local.database_ec2.tags, {
           nomis-environment   = "t2"
-          description         = "T2 NOMIS database"
+          description         = "Will's test server"
           oracle-sids         = "T2CNOM T2NDH T2TRDAT"
           instance-scheduling = "skip-scheduling"
         })
