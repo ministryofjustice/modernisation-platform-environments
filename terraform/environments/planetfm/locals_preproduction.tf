0ce794dd03e270e64e88c82317faff007da05957
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2023-11-14 17:02:51 +0000

    provisioning new app server and updating description tags

--- terraform/environments/planetfm/locals_preproduction.tf
+++ terraform/environments/planetfm/locals_preproduction.tf
@@ -24,11 +24,33 @@ locals {
         }
         cloudwatch_metric_alarms = {} # TODO: remove this later when @Dominic has added finished changing the alarms
         tags = merge(local.database_ec2.tags, {
-          description       = "copy of PPFDW0030 for planetfm ${local.environment}"
+          description       = "copy of PPFDW0030 SQL Server"
           app-config-status = "pending"
           ami               = "pp-cafm-db-a"
         })
       })
+
+      # app servers
+      pp-cafm-a-10-b = merge(local.app_ec2, {
+        config = merge(local.app_ec2.config, {
+          ami_name          = "pp-cafm-a-10-b"
+          availability_zone = "${local.region}b"
+        })
+        instance = merge(local.app_ec2.instance, {
+          instance_type = "t3.large"
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 128 } # root volume
+          "/dev/sdb"  = { type = "gp3", size = 100 }
+        }
+        cloudwatch_metric_alarms = {} # TODO: remove this later when @Dominic has added finished changing the alarms
+        tags = merge(local.app_ec2.tags, {
+          description       = "Migrated server PPFAW0010 PFME Licence Server"
+          ami               = "pp-cafm-a-10-b"
+          app-config-status = "pending"
+        })
+      })
+
 
       # web servers
       pp-cafm-w-5-a = merge(local.web_ec2, {
@@ -45,7 +67,7 @@ locals {
         }
         cloudwatch_metric_alarms = {} # TODO: remove this later when @Dominic has added finished changing the alarms
         tags = merge(local.web_ec2.tags, {
-          description       = "copy of PPFWW0005 for planetfm ${local.environment}"
+          description       = "Migrated server PPFWW0005 Web Portal Server"
           ami               = "pp-cafm-w-5-a"
           app-config-status = "pending"
         })
