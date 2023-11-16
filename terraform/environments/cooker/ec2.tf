80c846db953cd012ee123d788798fe1d002f24ee
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-11-16 14:15:24 +0000

    Added a security group to work?

--- terraform/environments/cooker/ec2.tf
+++ terraform/environments/cooker/ec2.tf
@@ -2,8 +2,18 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-0d729d2846a86a9e7"
   instance_type = "t2.micro"
-
+  subnet_id              = data.aws_subnet.private_subnets_a.id
+  security_groups = [aws_security_group.example_ec2_sg.id]
   tags = {
     Name = "ExampleAppServerInstance"
   }
+}
+
+resource "aws_security_group" "example_ec2_sg" {
+  name        = "example_ec2_sg"
+  description = "Controls access to EC2"
+  vpc_id      = data.aws_vpc.shared.id
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-example", local.application_name, local.environment)) }
+  )
 }
