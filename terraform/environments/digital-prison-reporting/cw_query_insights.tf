eafa6fcf470bb686139f061c2da3b425faa46339
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-11-17 11:18:44 +0000

    fix log group

--- terraform/environments/digital-prison-reporting/cw_query_insights.tf
+++ terraform/environments/digital-prison-reporting/cw_query_insights.tf
@@ -4,8 +4,8 @@ module "dpr_cw_insights_lambda_errors" {
   source              = "./modules/cw_insights"
   create_cw_insight   = local.enable_cw_insights
 
-  query_name          = "dpr-cw-insights-lambda-errors"
-  log_groups          = ["/aws/lambda"]
+  query_name          = "dpr-domain-builder-lambda-errors"
+  log_groups          = ["/aws/lambda/dpr-domain-builder-flyway-function"]
 
   query               = <<EOH
 filter @message like /(?i)(Exception|error|fail)/
