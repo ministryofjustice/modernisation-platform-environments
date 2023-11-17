5356253fe005be1614d087e1a1c233d1716dcd46
Ijaz Sultan <mohammed.ijazsultan@digital.justice.gov.uk>
2023-11-17 14:30:25 +0000

    Preprod 4 config added to terraform

--- terraform/environments/planetfm/locals_preproduction.tf
+++ terraform/environments/planetfm/locals_preproduction.tf
@@ -53,6 +53,26 @@ locals {
 
 
       # web servers
+      pp-cafm-w-4-b = merge(local.web_ec2, {
+        config = merge(local.web_ec2.config, {
+          ami_name          = "pp-cafm-w-4-b"
+          availability_zone = "${local.region}b"
+        })
+        instance = merge(local.web_ec2.instance, {
+          instance_type = "t3.large"
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 128 } # root volume
+          "/dev/sdb"  = { type = "gp3", size = 100 }
+        }
+        cloudwatch_metric_alarms = {} # TODO: remove this later when @Dominic has added finished changing the alarms
+        tags = merge(local.web_ec2.tags, {
+          description       = "Migrated server PPFWW0004 Web Portal Server"
+          ami               = "pp-cafm-w-4-b"
+          app-config-status = "pending"
+        })
+      })
+
       pp-cafm-w-5-a = merge(local.web_ec2, {
         config = merge(local.web_ec2.config, {
           ami_name          = "pp-cafm-w-5-a"
