b71376223fd6540ff13ba4e0ebe331f17c234fa7
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-11-21 12:54:12 +0000

    formatting

--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -595,7 +595,7 @@ locals {
           module.environment.subnet["private"]["eu-west-2b"].id,
         ]
         security_groups = ["load-balancer"]
-        access_logs = false
+        access_logs     = false
 
         instance_target_groups = {
           pp-csr-w-56-80 = {
