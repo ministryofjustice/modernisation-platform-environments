29c4c7f43b75078a8df52f0858ba80dbc0f99ff2
tanyamollercjse <tanya.moller@digital.justice.gov.uk>
2023-11-21 08:19:22 +0000

    test dev plan

--- terraform/environments/xhibit-portal/ingestion-load-balancer.tf
+++ terraform/environments/xhibit-portal/ingestion-load-balancer.tf
@@ -96,6 +96,188 @@ resource "aws_elb" "ingestion_lb" {
   )
 }
 
+# resource "aws_load_balancer_policy" "ingestion-ssl" {
+#   load_balancer_name = aws_elb.ingestion_lb.name
+#   policy_name        = "ingestion-lb-ssl"
+#   policy_type_name   = "SSLNegotiationPolicyType"
+
+#   policy_attribute {
+#     name  = "Protocol-TLSv1"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "Protocol-SSLv3"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "Protocol-TLSv1.1"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "Protocol-TLSv1.2"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-ECDSA-AES128-GCM-SHA256"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-RSA-AES128-GCM-SHA256"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-ECDSA-AES128-SHA256"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-RSA-AES128-SHA256"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-ECDSA-AES128-SHA"
+#     value = "true"
+#   }
+
+
+#   policy_attribute {
+#     name  = "ECDHE-RSA-AES128-SHA"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-ECDSA-AES256-GCM-SHA384"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-RSA-AES256-GCM-SHA384"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-ECDSA-AES256-SHA384"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-RSA-AES256-SHA384"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-RSA-AES256-SHA"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "ECDHE-ECDSA-AES256-SHA"
+#     value = "true"
+#   }
+
+#   policy_attribute {
+#     name  = "AES128-GCM-SHA256"
+#     value = "true"
+#   }
+# }
+
+
+# AES128-SHA256
+# AES128-SHA
+# AES256-GCM-SHA384
+# AES256-SHA256
+# AES256-SHA
+# DHE-DSS-AES128-SHA
+# CAMELLIA128-SHA
+# EDH-RSA-DES-CBC3-SHA
+# DES-CBC3-SHA
+# ECDHE-RSA-RC4-SHA
+# RC4-SHA
+# ECDHE-ECDSA-RC4-SHA
+# DHE-DSS-AES256-GCM-SHA384
+# DHE-RSA-AES256-GCM-SHA384
+# DHE-RSA-AES256-SHA256
+# DHE-DSS-AES256-SHA256
+# DHE-RSA-AES256-SHA
+# DHE-DSS-AES256-SHA
+# DHE-RSA-CAMELLIA256-SHA
+# DHE-DSS-CAMELLIA256-SHA
+# CAMELLIA256-SHA
+# EDH-DSS-DES-CBC3-SHA
+# DHE-DSS-AES128-GCM-SHA256
+# DHE-RSA-AES128-GCM-SHA256
+# DHE-RSA-AES128-SHA256
+# DHE-DSS-AES128-SHA256
+# DHE-RSA-CAMELLIA128-SHA
+# DHE-DSS-CAMELLIA128-SHA
+# ADH-AES128-GCM-SHA256
+# ADH-AES128-SHA
+# ADH-AES128-SHA256
+# ADH-AES256-GCM-SHA384
+# ADH-AES256-SHA
+# ADH-AES256-SHA256
+# ADH-CAMELLIA128-SHA
+# ADH-CAMELLIA256-SHA
+# ADH-DES-CBC3-SHA
+# ADH-DES-CBC-SHA
+# ADH-RC4-MD5
+# ADH-SEED-SHA
+# DES-CBC-SHA
+# DHE-DSS-SEED-SHA
+# DHE-RSA-SEED-SHA
+# EDH-DSS-DES-CBC-SHA
+# EDH-RSA-DES-CBC-SHA
+# IDEA-CBC-SHA
+# RC4-MD5
+# SEED-SHA
+# DES-CBC3-MD5
+# DES-CBC-MD5
+# RC2-CBC-MD5
+# PSK-AES256-CBC-SHA
+# PSK-3DES-EDE-CBC-SHA
+# KRB5-DES-CBC3-SHA
+# KRB5-DES-CBC3-MD5
+# PSK-AES128-CBC-SHA
+# PSK-RC4-SHA
+# KRB5-RC4-SHA
+# KRB5-RC4-MD5
+# KRB5-DES-CBC-SHA
+# KRB5-DES-CBC-MD5
+# EXP-EDH-RSA-DES-CBC-SHA
+# EXP-EDH-DSS-DES-CBC-SHA
+# EXP-ADH-DES-CBC-SHA
+# EXP-DES-CBC-SHA
+# EXP-RC2-CBC-MD5
+# EXP-KRB5-RC2-CBC-SHA
+# EXP-KRB5-DES-CBC-SHA
+# EXP-KRB5-RC2-CBC-MD5
+# EXP-KRB5-DES-CBC-MD5
+# EXP-ADH-RC4-MD5
+# EXP-RC4-MD5
+# EXP-KRB5-RC4-SHA
+# EXP-KRB5-RC4-MD5
+
+# resource "aws_load_balancer_listener_policy" "ingestion-listener-policies" {
+#   load_balancer_name = aws_elb.ingestion_lb.name
+#   load_balancer_port = 443
+
+#   policy_names = [
+#     aws_load_balancer_policy.ingestion-ssl.policy_name,
+#   ]
+# }
+
+
+
+
+
+
 data "aws_acm_certificate" "ingestion_lb_cert" {
   domain   = local.application_data.accounts[local.environment].public_dns_name_ingestion
   statuses = ["ISSUED"]
