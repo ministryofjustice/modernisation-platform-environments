6c5a7934f8e5592e59f44a3de172aeeb80deea8b
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-11-21 14:56:05 +0000

    set variable default

--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -595,7 +595,8 @@ locals {
           module.environment.subnet["private"]["eu-west-2b"].id,
         ]
         security_groups      = ["load-balancer"]
-        existing_bucket_name = "csr-nlb-log-bucket20231121131301186300000001"
+        existing_bucket_name = "csr-nlb-log-bucket"
+        access_logs = true
 
         instance_target_groups = {
           pp-csr-w-56-80 = {
