8b1f5e5de83514040fae820ecbbf948ecf9ca6ea
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-11-21 13:01:27 +0000

    Remove dev-nomis-db-1-a for now

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -276,25 +276,25 @@ locals {
     }
 
     baseline_ec2_instances = {
-      dev-nomis-db-1-a = merge(local.database_ec2, {
-        config = merge(local.database_ec2.config, {
-          ami_name          = "nomis_rhel_7_9_oracledb_11_2_release_2023-06-23T16-28-48.100Z"
-          availability_zone = "${local.region}a"
-        })
-        ebs_volumes = merge(local.database_ec2.ebs_volumes, {
-          "/dev/sdb" = { label = "app", size = 100 }
-          "/dev/sdc" = { label = "app", size = 100 }
-        })
-        ebs_volume_config = merge(local.database_ec2.ebs_volume_config, {
-          data  = { total_size = 500 }
-          flash = { total_size = 50 }
-        })
-        tags = merge(local.database_ec2.tags, {
-          nomis-environment   = "dev"
-          description         = "temporary DB to test DB restore"
-          oracle-sids         = ""
-        })
-      })
+      #dev-nomis-db-1-a = merge(local.database_ec2, {
+      #  config = merge(local.database_ec2.config, {
+      #    ami_name          = "nomis_rhel_7_9_oracledb_11_2_release_2023-06-23T16-28-48.100Z"
+      #    availability_zone = "${local.region}a"
+      #  })
+      #  ebs_volumes = merge(local.database_ec2.ebs_volumes, {
+      #    "/dev/sdb" = { label = "app", size = 100 }
+      #    "/dev/sdc" = { label = "app", size = 100 }
+      #  })
+      #  ebs_volume_config = merge(local.database_ec2.ebs_volume_config, {
+      #    data  = { total_size = 500 }
+      #    flash = { total_size = 50 }
+      #  })
+      #  tags = merge(local.database_ec2.tags, {
+      #    nomis-environment   = "dev"
+      #    description         = "temporary DB to test DB restore"
+      #    oracle-sids         = ""
+      #  })
+      #})
     }
 
     baseline_lbs = {
