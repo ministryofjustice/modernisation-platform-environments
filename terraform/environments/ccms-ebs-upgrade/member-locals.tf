0a754baed7bed05aef636c9379c384810d8ab6f9
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2023-11-21 08:52:33 +0000

    CC-2163: Creation of EBSApps Instances

--- terraform/environments/ccms-ebs-upgrade/member-locals.tf
+++ terraform/environments/ccms-ebs-upgrade/member-locals.tf
@@ -25,7 +25,7 @@ locals {
     data.aws_subnet.public_subnets_c.id
   ]
 
-  cert_opts    = aws_acm_certificate.external[0].domain_validation_options
-  cert_arn     = aws_acm_certificate.external[0].arn
+  cert_opts    = aws_acm_certificate.external.domain_validation_options
+  cert_arn     = aws_acm_certificate.external.arn
   cert_zone_id = data.aws_route53_zone.network-services.zone_id
 }
