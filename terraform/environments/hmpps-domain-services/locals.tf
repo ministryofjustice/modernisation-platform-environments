bd3943c42bf01bb725bfc7479dca73a80ffd53ab
W <willg258@hotmail.co.uk>
2023-11-22 17:00:01 +0000

    domain services secrets
    domain services secrets

--- terraform/environments/hmpps-domain-services/locals.tf
+++ terraform/environments/hmpps-domain-services/locals.tf
@@ -55,7 +55,17 @@ locals {
     }
   }
 
-  baseline_secretsmanager_secrets = {}
+  baseline_secretsmanager_secrets = {
+    "" = {
+      postfix = ""
+      secrets = {
+        account_ids                       = {}
+        ec2-user_pem                      = {}
+        environment_management_arn        = {}
+        modernisation_platform_account_id = {}
+      }
+    }
+  }
 
   baseline_security_groups = {
     private-dc = local.security_groups.private_dc
@@ -63,4 +73,5 @@ locals {
 
   baseline_sns_topics     = {}
   baseline_ssm_parameters = {}
+  
 }
