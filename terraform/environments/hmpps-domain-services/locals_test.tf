d947558c5398c26193eea9b2623ce932a6b1f470
pav <pav.gainda@digital.justice.gov.uk>
2023-11-23 14:52:21 +0000

    Deploy first windows instance

--- terraform/environments/hmpps-domain-services/locals_test.tf
+++ terraform/environments/hmpps-domain-services/locals_test.tf
@@ -46,6 +46,30 @@ locals {
           component   = "test"
           server-type = local.application_name
         }
+      },
+
+      rds-connection-broker-and-licensing-server = {
+        # ami has unwanted ephemeral device, don't copy all the ebs_volumess
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "hmpps_windows_server_2022_release_2023-*"
+          availability_zone             = null
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = base64encode(file("./templates/windows_server_2022-user-data.yaml"))
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private-dc"]
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        autoscaling_group     = module.baseline_presets.ec2_autoscaling_group.default
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Windows Server 2022 to hold RDS connection broker and licensing server"
+          os-type     = "Windows"
+          component   = "test"
+          server-type = "hmpps-windows_2022"
+        }
       }
     }
   }
