d4ff53b126e186fc094b039eb2aaecac2df98c96
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2023-11-24 15:09:15 +0000

    Removed some more whitespace in hope of passing check

--- terraform/environments/mlra/modules/ecs/main.tf
+++ terraform/environments/mlra/modules/ecs/main.tf
@@ -26,7 +26,7 @@ resource "aws_autoscaling_group" "cluster-scaling-group" {
   max_size              = var.ec2_max_size
   min_size              = var.ec2_min_size
   protect_from_scale_in = true
-  
+
   metrics_collection {
     granularity = "1Minute"
   }
