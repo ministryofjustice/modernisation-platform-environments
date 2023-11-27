c8d468903468f8cdfffa7a75ec5833140fd38448
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2023-11-27 15:39:10 +0000

    CC-2176: Creation of AccessGate Instances

--- terraform/environments/ccms-ebs-upgrade/certificates.tf
+++ terraform/environments/ccms-ebs-upgrade/certificates.tf
@@ -36,7 +36,7 @@ resource "aws_route53_record" "external_validation" {
     aws_instance.ec2_ebsapps
   ]
 
-  provider = aws.core-vpc
+  provider = aws.core-network-services
 
   for_each = {
     for dvo in local.cert_opts : dvo.domain_name => {
