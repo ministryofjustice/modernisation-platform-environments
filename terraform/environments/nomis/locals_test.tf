33c7e1e4ce2e8519665bbbbc1eb94ba777857a76
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-11-27 15:51:32 +0000

    test

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -239,11 +239,6 @@ locals {
           })
         })
 
-        secretsmanager_secrets = {
-          test4 = {}
-          test5 = {}
-        }
-
         tags = merge(local.weblogic_ec2.tags, {
           nomis-environment    = "t1"
           oracle-db-hostname-a = "t1nomis-a.test.nomis.service.justice.gov.uk"
