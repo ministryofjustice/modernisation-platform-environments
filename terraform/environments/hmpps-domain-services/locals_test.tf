71fc85fb78e2acd36d7a9cc5153479d2116952f0
pavmoj <142988272+pavmoj@users.noreply.github.com>
2023-11-28 09:47:34 +0000

    Add RDS components (#4159)
    * Add RDS components and user data

--- terraform/environments/hmpps-domain-services/locals_test.tf
+++ terraform/environments/hmpps-domain-services/locals_test.tf
@@ -46,7 +46,148 @@ locals {
           component   = "test"
           server-type = local.application_name
         }
+      },
+
+      rds-connection-broker = {
+        # ami has unwanted ephemeral device, don't copy all the ebs_volumess
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "hmpps_windows_server_2022_release_2023-*"
+          availability_zone             = null
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = base64encode(templatefile("./templates/rds.yaml.tftpl",{
+            rds_hostname = "RDSConnectionBroker"
+          }))
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private-dc"]
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 0 # set to 0 while testing
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Windows Server 2022 host for RDS connection broker role"
+          os-type     = "Windows"
+          component   = "RDS Connection Broker"
+          server-type = "hmpps-windows_2022"
+        }
+      },
+
+      rds-licensing = {
+        # ami has unwanted ephemeral device, don't copy all the ebs_volumess
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "hmpps_windows_server_2022_release_2023-*"
+          availability_zone             = null
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = base64encode(templatefile("./templates/rds.yaml.tftpl",{
+            rds_hostname = "RDSLicensing"
+          }))
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private-dc"]
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 0 # set to 0 while testing
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Windows Server 2022 host for RDS licensing role"
+          os-type     = "Windows"
+          component   = "RDS Licensing"
+          server-type = "hmpps-windows_2022"
+        }
+      },
+
+      rds-web-access = {
+        # ami has unwanted ephemeral device, don't copy all the ebs_volumess
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "hmpps_windows_server_2022_release_2023-*"
+          availability_zone             = null
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = base64encode(templatefile("./templates/rds.yaml.tftpl",{
+            rds_hostname = "RDSWebAccess"
+          }))
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private-dc"]
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 0 # set to 0 while testing
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Windows Server 2022 host for RDS web access role"
+          os-type     = "Windows"
+          component   = "RDS Web Access"
+          server-type = "hmpps-windows_2022"
+        }
+      },
+
+      rds-gateway = {
+        # ami has unwanted ephemeral device, don't copy all the ebs_volumess
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "hmpps_windows_server_2022_release_2023-*"
+          availability_zone             = null
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = base64encode(templatefile("./templates/rds.yaml.tftpl",{
+            rds_hostname = "RDSGateway"
+          }))
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private-dc"]
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 0 # set to 0 while testing
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Windows Server 2022 host for RDS gateway"
+          os-type     = "Windows"
+          component   = "RDS Gateway"
+          server-type = "hmpps-windows_2022"
+        }
+      },
+
+      rds-session-host = {
+        # ami has unwanted ephemeral device, don't copy all the ebs_volumess
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "hmpps_windows_server_2022_release_2023-*"
+          availability_zone             = null
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = base64encode(templatefile("./templates/rds.yaml.tftpl",{
+            rds_hostname = "RDSSessionHost"
+          }))
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private-dc"]
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 0 # set to 0 while testing
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Windows Server 2022 host for RDS session host"
+          os-type     = "Windows"
+          component   = "RDS Session Host"
+          server-type = "hmpps-windows_2022"
+        }
       }
     }
   }
 }
+
