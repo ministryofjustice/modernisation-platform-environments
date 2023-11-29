c3c951d067606fe3c0d38d91c3f64a8a07f3a526
Robert Sweetman <robertiansweetman@gmail.com>
2023-11-29 10:01:31 +0000

    add test env NS records and preprod dns zone (#4174)

--- terraform/environments/planetfm/locals_production.tf
+++ terraform/environments/planetfm/locals_production.tf
@@ -4,7 +4,11 @@ locals {
   # baseline config
   production_config = {
     baseline_route53_zones = {
-      "planetfm.service.justice.gov.uk" = {}
+      "planetfm.service.justice.gov.uk" = {
+        records = [
+          { name = "test", type = "NS", ttl = "86400", records = ["ns-1128.awsdns-13.org", "ns-2027.awsdns-61.co.uk", "ns-854.awsdns-42.net", "ns-90.awsdns-11.com"] },
+        ]
+      }
     }
   }
 }
