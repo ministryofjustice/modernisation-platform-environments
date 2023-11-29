69c43acf72b27b66f5f5761093416c65c4687804
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-11-29 10:33:10 +0000

    add NS records for pp subdomain

--- terraform/environments/planetfm/locals_production.tf
+++ terraform/environments/planetfm/locals_production.tf
@@ -7,6 +7,7 @@ locals {
       "planetfm.service.justice.gov.uk" = {
         records = [
           { name = "test", type = "NS", ttl = "86400", records = ["ns-1128.awsdns-13.org", "ns-2027.awsdns-61.co.uk", "ns-854.awsdns-42.net", "ns-90.awsdns-11.com"] },
+          { name = "pp", type = "NS", ttl = "86400", records = ["ns-1407.awsdns-47.org", "ns-1645.awsdns-13.co.uk", "ns-63.awsdns-07.com", "ns-730.awsdns-27.net"] },
         ]
       }
     }
