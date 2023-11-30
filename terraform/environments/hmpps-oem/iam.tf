60b3c7ca1e462e6a61e114435214687158010a6c
Jon Quinn <1213631+jnq@users.noreply.github.com>
2023-11-30 10:10:56 +0000

    DSOS-2402: add a policy to allow ec2 instance discovery (#4189)
    * add a policy to allow ec2 instance discovery
    
    * defined as a *, not a resource level wildcard
    
    * add required permission
    
    * add S3 permissions for Ansible SSM connection
    
    * missed a permission
    
    * allow encrypt and decrypt with out general shared key

--- terraform/environments/hmpps-oem/iam.tf
+++ terraform/environments/hmpps-oem/iam.tf
@@ -31,6 +31,56 @@ data "aws_iam_policy_document" "db_refresher_role" {
   }
 }
 
+data "aws_iam_policy_document" "db_refresher" {
+  statement {
+    sid    = "DescribeInstances"
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeInstances",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+  statement {
+    sid    = "KMSAccess"
+    effect = "Allow"
+    actions = [
+      "kms:GenerateDataKey",
+      "kms:Decrypt",
+      "kms:Encrypt",
+    ]
+    resources = [
+      data.aws_kms_key.general_shared.arn,
+    ]
+  }
+  statement {
+    sid    = "S3ObjectAccess"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+    resources = [
+      "arn:aws:s3:::s3-bucket20230608132808605000000001/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "db_refresher" {
+  count       = local.is-development || local.is-test ? 1 : 0
+  name        = "db_refresher"
+  description = "Allows describing of EC2 instances"
+  policy      = data.aws_iam_policy_document.db_refresher.json
+}
+
+resource "aws_iam_role_policy_attachment" "db_refresher_ec2_describe" {
+  count      = local.is-development || local.is-test ? 1 : 0
+  role       = aws_iam_role.db_refresher[0].name
+  policy_arn = aws_iam_policy.db_refresher[0].arn
+}
+
 resource "aws_iam_role_policy_attachment" "db_refresher_ssm_access" {
   count      = local.is-development || local.is-test ? 1 : 0
   role       = aws_iam_role.db_refresher[0].name
