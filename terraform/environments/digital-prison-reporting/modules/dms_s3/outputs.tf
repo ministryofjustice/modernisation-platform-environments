6353ca31ebfaa6cb8635d712a8d9d888149e765a
koladeadewuyi-moj <136330532+koladeadewuyi-moj@users.noreply.github.com>
2023-11-30 12:23:47 +0000

    DPR2-209: Add step function and lambda (#4038)

--- terraform/environments/digital-prison-reporting/modules/dms_s3/outputs.tf
+++ terraform/environments/digital-prison-reporting/modules/dms_s3/outputs.tf
@@ -14,3 +14,7 @@ output "dms_s3_iam_policy_admin_arn" {
   description = "The IAM Policy (ARN) admin of the DMS to S3 target"
   value       = concat(aws_iam_policy.dms-operator-s3-policy.*.arn, [""])[0]
 }
+
+output "dms_replication_task_arn" {
+  value = var.enable_replication_task ? join("", aws_dms_replication_task.dms-replication.*.replication_task_arn) : ""
+}
