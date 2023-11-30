49195e607e04ea57de85de63e10d849dc18c2e0e
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-11-30 11:45:08 +0000

    Add ecs module and remove cluster

--- terraform/environments/cdpt-chaps/locals.tf
+++ terraform/environments/cdpt-chaps/locals.tf
@@ -1,1 +1,10 @@
-#### This file can be used to store locals specific to the member account ####
+locals {
+  app_data = jsondecode(file("./application_variables.json"))
+
+  task_definition = templatefile("task_definition.json", {
+    app_name            = local.application_name
+    ecr_url             = "${local.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/cdpt-chaps-ecr-repo"
+    docker_image_tag    = local.application_data.accounts[local.environment].docker_image_tag
+    region              = local.application_data.accounts[local.environment].region
+  })
+}
