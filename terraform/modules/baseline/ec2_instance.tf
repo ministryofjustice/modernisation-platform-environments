39263b4d8c3b6459c193c84a7b592e7a41d584bc
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2023-12-01 10:23:26 +0000

    new versions of modules

--- terraform/modules/baseline/ec2_instance.tf
+++ terraform/modules/baseline/ec2_instance.tf
@@ -3,7 +3,7 @@ module "ec2_instance" {
 
   for_each = var.ec2_instances
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance?ref=DSOS-2423-ssm-and-secrets-policy-fix"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance?ref=v2.4.1"
 
   providers = {
     aws.core-vpc = aws.core-vpc
