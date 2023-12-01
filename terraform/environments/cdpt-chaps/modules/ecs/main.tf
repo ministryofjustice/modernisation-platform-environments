81b66587a3d319d59015e55c393cc000f92f2b11
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-01 14:11:53 +0000

    Fix name

--- terraform/environments/cdpt-chaps/modules/ecs/main.tf
+++ terraform/environments/cdpt-chaps/modules/ecs/main.tf
@@ -464,7 +464,7 @@ resource "aws_appautoscaling_policy" "scaling_policy_down" {
 # Set up CloudWatch group and log stream and retain logs for 30 days
 resource "aws_cloudwatch_log_group" "cloudwatch_group" {
   #checkov:skip=CKV_AWS_158:Temporarily skip KMS encryption check while logging solution is being updated
-  name              = "${var.app_name}-ecs"
+  name              = "${var.app_name}-ecs-log-group"
   retention_in_days = 30
   tags = merge(
     var.tags_common,
