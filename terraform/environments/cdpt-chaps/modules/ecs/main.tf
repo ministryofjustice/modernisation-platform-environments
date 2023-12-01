fb507d1473c7855dcb795e1fb0db27d5667293f5
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-01 11:55:50 +0000

    Update task definition

--- terraform/environments/cdpt-chaps/modules/ecs/main.tf
+++ terraform/environments/cdpt-chaps/modules/ecs/main.tf
@@ -235,6 +235,12 @@ resource "aws_ecs_cluster_capacity_providers" "ecs_cluster" {
   cluster_name = aws_ecs_cluster.ecs_cluster.name
 
   capacity_providers = [aws_ecs_capacity_provider.capacity_provider.name]
+
+  default_capacity_provider_strategy {
+   base              = 1
+   weight            = 100
+   capacity_provider = aws_ecs_capacity_provider.capacity_provider.name
+ }
 }
 
 resource "aws_ecs_task_definition" "windows_ecs_task_definition" {
@@ -321,6 +327,13 @@ resource "aws_ecs_capacity_provider" "capacity_provider" {
 
   auto_scaling_group_provider {
     auto_scaling_group_arn = aws_autoscaling_group.cluster-scaling-group.arn
+
+    managed_scaling {
+     maximum_scaling_step_size = 1000
+     minimum_scaling_step_size = 1
+     status                    = "ENABLED"
+     target_capacity           = 3
+   }
   }
 
   tags = merge(
