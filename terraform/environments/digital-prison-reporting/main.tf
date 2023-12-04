8295bb51d041ab0b159d3ab49a43d66e2e5e34d4
koladeadewuyi-moj <136330532+koladeadewuyi-moj@users.noreply.github.com>
2023-12-04 15:11:06 +0000

    DPR2-245: Create schema registry bucket (#4224)

--- terraform/environments/digital-prison-reporting/main.tf
+++ terraform/environments/digital-prison-reporting/main.tf
@@ -491,6 +491,24 @@ module "s3_domain_bucket" {
   )
 }
 
+# Schema Registry Bucket
+module "s3_schema_registry_bucket" {
+  source                    = "./modules/s3_bucket"
+  create_s3                 = local.setup_buckets
+  name                      = "${local.project}-schema-registry-${local.env}"
+  custom_kms_key            = local.s3_kms_arn
+  create_notification_queue = false # For SQS Queue
+  enable_lifecycle          = true
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name          = "${local.project}-schema-registry-${local.env}"
+      Resource_Type = "S3 Bucket"
+    }
+  )
+}
+
 # Data Domain Configuration Bucket
 module "s3_domain_config_bucket" {
   source                    = "./modules/s3_bucket"
