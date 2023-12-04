54c5160e99141af449c928776378ce07d322dc12
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-12-04 17:21:00 +0000

    Add production configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/environment-configurations.tf
+++ terraform/environments/observability-platform/environment-configurations.tf
@@ -24,13 +24,19 @@ locals {
       ]
       data_platform_apps_tools_account_id = local.environment_management.account_ids["data-platform-apps-and-tools-development"]
     }
-    test = {
-      data_platform_apps_tools_account_id = local.environment_management.account_ids["data-platform-apps-and-tools-development"]
-    }
-    preproduction = {
-      data_platform_apps_tools_account_id = local.environment_management.account_ids["data-platform-apps-and-tools-development"]
-    }
     production = {
+      observability_platform_configuration = {
+        "data-platform" = {
+          sso_uuid = "a68242b4-b0a1-7085-25f4-dc60e4c122c0"
+          cloudwatch_accounts = [
+            "data-platform-production",
+            "data-platform-apps-and-tools-production"
+          ]
+          prometheus_accounts = [
+            "data-platform-apps-and-tools-production"
+          ]
+        }
+      }
       source_accounts = [
         local.environment_management.account_ids["data-platform-production"],
         local.environment_management.account_ids["data-platform-apps-and-tools-production"]
