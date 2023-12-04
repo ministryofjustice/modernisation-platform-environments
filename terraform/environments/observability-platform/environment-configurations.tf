7c82e50e52a29d9df3a0d11674bc4031d6389278
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-12-04 16:50:07 +0000

    Update data-platform SSO UUID
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/environment-configurations.tf
+++ terraform/environments/observability-platform/environment-configurations.tf
@@ -4,7 +4,7 @@ locals {
     development = {
       observability_platform_configuration = {
         "data-platform" = {
-          sso_uuid = "16a2d234-1031-70b5-2657-7f744c55e48f"
+          sso_uuid = "a68242b4-b0a1-7085-25f4-dc60e4c122c0"
           cloudwatch_accounts = [
             "data-platform-development",
             "data-platform-test",
