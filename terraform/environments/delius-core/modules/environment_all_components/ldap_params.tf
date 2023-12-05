c3effe3136cea2cffe9666b0beb132014aa35056
Andrew Moore <andrew@automationlogic.com>
2023-12-05 15:37:06 +0000

    NIT-965 change from SSM param to secret

--- terraform/environments/delius-core/modules/environment_all_components/ldap_params.tf
+++ terraform/environments/delius-core/modules/environment_all_components/ldap_params.tf
@@ -17,6 +17,20 @@ data "aws_secretsmanager_secret" "delius_core_ldap_credential" {
   name = aws_secretsmanager_secret.delius_core_ldap_credential.name
 }
 
+resource "aws_secretsmanager_secret" "delius_core_ldap_seed_uri" {
+  name = "${var.account_info.application_name}-${var.env_name}-openldap-seed-uri"
+}
+
+resource "aws_secretsmanager_secret_version" "delius_core_ldap_seed_uri" {
+  secret_id     = aws_secretsmanager_secret.delius_core_ldap_seed_uri.id
+  secret_string = "INITIAL_VALUE_OVERRIDDEN"
+  lifecycle {
+    ignore_changes = [
+      secret_string
+    ]
+  }
+}
+
 resource "aws_ssm_parameter" "delius_core_ldap_host" {
   name  = format("/%s-%s/LDAP_HOST", var.account_info.application_name, var.env_name)
   type  = "SecureString"
@@ -32,18 +46,6 @@ resource "aws_ssm_parameter" "delius_core_ldap_host" {
 resource "aws_ssm_parameter" "delius_core_ldap_principal" {
   name  = format("/%s-%s/LDAP_PRINCIPAL", var.account_info.application_name, var.env_name)
   type  = "SecureString"
-  value = "INITIAL_VALUE_OVERRIDDEN"
-  lifecycle {
-    ignore_changes = [
-      value
-    ]
-  }
-  tags = local.tags
-}
-
-resource "aws_ssm_parameter" "delius_core_ldap_seed_uri" {
-  name  = format("/%s-%s/LDAP_SEED_URI", var.account_info.application_name, var.env_name)
-  type  = "String"
   value = "INITIAL_VALUE_OVERRIDDEN"
   lifecycle {
     ignore_changes = [
