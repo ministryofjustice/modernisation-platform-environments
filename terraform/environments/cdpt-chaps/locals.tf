26b07c146ddd6854dde5755d044a9beca842ec78
alistair.curtis <Alistair.Curtis@justice.gov.uk>
2023-12-05 11:46:39 +0000

    remove app name locals

--- terraform/environments/cdpt-chaps/locals.tf
+++ terraform/environments/cdpt-chaps/locals.tf
@@ -1,10 +1,7 @@
 #### This file can be used to store locals specific to the member account ####
 
-
 locals {
 
 app_data = jsondecode(file("./application_variables.json"))
-
-application_name = "Chaps"
 
 }
