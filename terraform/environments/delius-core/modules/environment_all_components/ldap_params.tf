3755bd5a447c80b8ab5790c7e80e4b8c9e6bf178
Andrew Moore <andrew@automationlogic.com>
2023-12-05 14:41:38 +0000

    NIT-965 add SSM parameter for LDAP seed URI

--- terraform/environments/delius-core/modules/environment_all_components/ldap_params.tf
+++ terraform/environments/delius-core/modules/environment_all_components/ldap_params.tf
@@ -41,3 +41,14 @@ resource "aws_ssm_parameter" "delius_core_ldap_principal" {
   tags = local.tags
 }
 
+resource "aws_ssm_parameter" "delius_core_ldap_seed_uri" {
+  name  = format("/%s-%s/LDAP_SEED_URI", var.account_info.application_name, var.env_name)
+  type  = "String"
+  value = "INITIAL_VALUE_OVERRIDDEN"
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
+  tags = local.tags
+}
