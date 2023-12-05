3f911aa4ec2823b288d55108fca5026d74bee9f6
alistair.curtis <Alistair.Curtis@justice.gov.uk>
2023-12-05 11:36:30 +0000

    add RDS

--- terraform/environments/cdpt-chaps/locals.tf
+++ terraform/environments/cdpt-chaps/locals.tf
@@ -1,1 +1,10 @@
 #### This file can be used to store locals specific to the member account ####
+
+
+locals {
+
+app_data = jsondecode(file("./application_variables.json"))
+
+application_name = "Chaps"
+
+}
