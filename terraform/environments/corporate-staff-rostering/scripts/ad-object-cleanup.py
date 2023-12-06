1b41c21e302119e3dd419637cb3b4f8a42b1c69a
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2023-12-06 08:36:09 +0000

    added some comments

--- terraform/environments/corporate-staff-rostering/scripts/ad-object-cleanup.py
+++ terraform/environments/corporate-staff-rostering/scripts/ad-object-cleanup.py
@@ -1,10 +1,12 @@
-import boto3
+import boto3 
+# amazon SDK for python
 from ldap3 import Server, Connection, ALL
 
+# function to check for objects within active directory
 def check_ad_for_object(object):
     server = Server('MGMCW0002.azure.noms.root:389', get_info=ALL)
     username = 'azure\aws-lambda'
-    password = 'Aw5Servic3Acc0unt'
+    password = 'Aw5Servic3Acc0unt' # use secret ARN, pw declared whilst testing
     
     with Connection(server, user=username, password=password, auto_bind=True) as conn:
         search_base = 'ou=Managed-Windows-Servers, ou=Computers, dc=azure, dc=noms, dc=root'
@@ -17,13 +19,14 @@ def check_ad_for_object(object):
         else:
             print(f"The object {object} is not found in Active Directory - no further action taken.")
 
+# function to iterate through instance tags 
 def get_tag_value(tags, key):
     for tag in tags:
         if tag['Key'] == key:
             return tag['Value']
     return None
-            
-            
+
+# function to search active directory if an instance is stopped, final iteration will be state terminated                       
 def lambda_handler(event, context):
     if event['detail']['state'] == 'stopped':
         instance_id = event['detail']['instance_id']
@@ -42,6 +45,7 @@ def lambda_handler(event, context):
         else:
             print(f"The tag '{resource_key}' was not found for the instance.")
     
+    # 200 http response lambda run successful
     return {
         'statusCode': 200,
         'body': 'Active Directory search complete. Check logs for results'
