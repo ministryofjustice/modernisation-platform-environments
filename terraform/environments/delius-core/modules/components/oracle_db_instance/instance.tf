52a8bf81bacee91a0dfd1e46358efa225365f6c3
George Taylor <george.taylor@digital.justice.gov.uk>
2023-12-07 12:10:17 +0000

    move locals

--- terraform/environments/delius-core/modules/components/oracle_db_instance/instance.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_instance/instance.tf
@@ -41,8 +41,3 @@ resource "aws_instance" "db_ec2" {
     { database = local.database_tag }
   )
 }
-
-locals {
-  instance_name_index = var.db_type == "primary" ? var.db_count_index : var.db_count_index + 1
-  database_tag        = var.db_type == "primary" ? "delius_${var.db_type}db" : "delius_${var.db_type}db${var.db_count_index}"
-}
