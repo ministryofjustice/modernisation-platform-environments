bda9ff20aa87e99d20a2fcc132ca3aa8fb0ca0f2
alistair.curtis <Alistair.Curtis@justice.gov.uk>
2023-12-08 11:01:31 +0000

    add s3 access for RDS

--- terraform/environments/cdpt-chaps/locals.tf
+++ terraform/environments/cdpt-chaps/locals.tf
@@ -1,5 +1,8 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
+	
+	app_data = jsondecode(file("./application_variables.json"))
+
   domain_types = { for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
     name   = dvo.resource_record_name
     record = dvo.resource_record_value
