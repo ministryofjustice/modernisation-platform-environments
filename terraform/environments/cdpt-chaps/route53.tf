248be97e411c7cc40e404bbd1398213f9c79fa2e
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-08 16:52:10 +0000

    disable health check

--- terraform/environments/cdpt-chaps/route53.tf
+++ terraform/environments/cdpt-chaps/route53.tf
@@ -54,6 +54,6 @@ resource "aws_route53_record" "external" {
   alias {
     name                   = aws_lb.chaps_lb.dns_name
     zone_id                = aws_lb.chaps_lb.zone_id
-    evaluate_target_health = true
+    evaluate_target_health = false
   }
 }
