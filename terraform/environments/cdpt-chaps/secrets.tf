5243e6d49a33ee309b9893f5fc3cb294dc1ad8f0
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2023-12-11 16:22:47 +0000

    new secret name

--- terraform/environments/cdpt-chaps/secrets.tf
+++ terraform/environments/cdpt-chaps/secrets.tf
@@ -1,7 +1,7 @@
 #### This file can be used to store secrets specific to the member account ####
 
 resource "aws_secretsmanager_secret" "chaps_secret" {
-  name        = "chaps_secret"
+  name        = "chaps_secret1"
   description = "Simple secret created through Terraform"
 }
 
