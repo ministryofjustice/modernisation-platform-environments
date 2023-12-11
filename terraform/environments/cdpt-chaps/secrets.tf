6978e07de792f4e1f954483bf1bcd5380a79221e
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2023-12-11 12:04:28 +0000

    added secret

--- terraform/environments/cdpt-chaps/secrets.tf
+++ terraform/environments/cdpt-chaps/secrets.tf
@@ -1,1 +1,15 @@
 #### This file can be used to store secrets specific to the member account ####
+
+resource "aws_secretsmanager_secret" "chaps_secret" {
+  name        = "chaps_secret"
+  description = "Simple secret created through Terraform"
+}
+
+resource "random_password" "password" {
+
+}
+
+resource "aws_secretsmanager_secret_version" "chaps_secret" {
+  secret_id     = aws_secretsmanager_secret.chaps_secret.id
+  secret_string = random_password.password.result
+}
