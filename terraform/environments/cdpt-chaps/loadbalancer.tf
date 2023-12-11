790ab9f6269ed37fb6fc39b7c28770a4b9593c5f
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-11 14:27:43 +0000

    Change back to ip

--- terraform/environments/cdpt-chaps/loadbalancer.tf
+++ terraform/environments/cdpt-chaps/loadbalancer.tf
@@ -51,7 +51,7 @@ resource "aws_lb_target_group" "chaps_target_group" {
   port                 = 80
   protocol             = "HTTP"
   vpc_id               = data.aws_vpc.shared.id
-  target_type          = "instance"
+  target_type          = "ip"
   deregistration_delay = 30
 
   stickiness {
