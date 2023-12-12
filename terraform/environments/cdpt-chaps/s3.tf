4cd5ede70e156a86fbab8b17df48e00892bd9d60
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2023-12-12 16:43:12 +0000

    add application_data s3

--- terraform/environments/cdpt-chaps/s3.tf
+++ terraform/environments/cdpt-chaps/s3.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "chaps-db-backup-bucket" {
-	bucket = local.app_data.accounts[local.environment].s3_bucket_name
+	bucket = local.application_data.accounts[local.environment].s3_bucket_name
 }
 
