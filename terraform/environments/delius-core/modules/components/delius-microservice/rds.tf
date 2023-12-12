9894489701bb1c01708fee14250d91893986960b
George Taylor <george.taylor@digital.justice.gov.uk>
2023-12-12 12:49:57 +0000

    rds configuration

--- terraform/environments/delius-core/modules/components/delius-microservice/rds.tf
+++ terraform/environments/delius-core/modules/components/delius-microservice/rds.tf
@@ -1,101 +1,89 @@
-resource "aws_security_group" "database_security_group" {
+resource "aws_security_group" "db" {
   name        = "${var.name}-database-security-group"
   description = "controls access to db"
-  vpc_id      = var.shared_vpc_id
+  vpc_id      = var.account_config.shared_vpc_id
 
   ingress {
     protocol    = "tcp"
-    description = "Allow MSSQL traffic"
-    from_port   = 1433
-    to_port     = 1433
+    description = "Allow RDS traffic"
+    from_port   = var.rds_port
+    to_port     = var.rds_port
     security_groups = [
-      aws_security_group.jitbit.id,
-      module.bastion_linux.bastion_security_group
+      var.account_config.bastion.bastion_security_group
+      var.rds_ingress_security_groups
     ]
   }
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
-      Name = "${local.application_name}-database_security_group-security-group"
+      Name = "${var.name}-database_security_group"
     }
   )
 }
 
-resource "aws_db_subnet_group" "jitbit" {
-  name = "data-tier"
-  subnet_ids = [
-    data.aws_subnet.data_subnets_a.id,
-    data.aws_subnet.data_subnets_b.id,
-    data.aws_subnet.data_subnets_c.id
-  ]
-
-  tags = local.tags
-}
-
-locals {
-  skip_final_snapshot = try(local.application_data.accounts[local.environment].db_skip_final_snapshot, false)
+resource "aws_db_subnet_group" "this" {
+  name       = "data-tier"
+  subnet_ids = var.account_config.ordered_subnets.*.id
+  tags       = var.tags
 }
 
 resource "aws_db_instance" "jitbit" {
-  engine         = "sqlserver-se"
-  license_model  = "license-included"
-  engine_version = local.application_data.accounts[local.environment].db_engine_version
-  instance_class = local.application_data.accounts[local.environment].db_instance_class
-  identifier     = "${local.application_name}-${local.environment}-database"
-  username       = local.application_data.accounts[local.environment].db_user
+  engine         = var.rds_engine
+  license_model  = length(var.rds_license_model) > 0 ? var.rds_license_model : null
+  engine_version = var.rds_engine_version
+  instance_class = var.rds_instance_class
+  identifier     = "${var.name}-${var.env_name}-db"
+  username       = var.rds_username
 
   manage_master_user_password = true
 
-  snapshot_identifier = try(local.application_data.accounts[local.environment].db_snapshot_identifier, null)
+  snapshot_identifier = var.snapshot_identifier != null && length(var.snapshot_identifier) > 0 ? var.snapshot_identifier : null
 
   # tflint-ignore: aws_db_instance_default_parameter_group
-  parameter_group_name        = "default.sqlserver-se-15.0"
-  deletion_protection         = local.application_data.accounts[local.environment].db_deletion_protection
-  delete_automated_backups    = local.application_data.accounts[local.environment].db_delete_automated_backups
-  skip_final_snapshot         = local.skip_final_snapshot
-  final_snapshot_identifier   = !local.skip_final_snapshot ? "${local.application_name}-${local.environment}-database-final-snapshot" : null
-  allocated_storage           = local.application_data.accounts[local.environment].db_allocated_storage
-  max_allocated_storage       = local.application_data.accounts[local.environment].db_max_allocated_storage
-  storage_type                = local.application_data.accounts[local.environment].db_storage_type
-  maintenance_window          = local.application_data.accounts[local.environment].db_maintenance_window
-  auto_minor_version_upgrade  = local.application_data.accounts[local.environment].db_auto_minor_version_upgrade
-  allow_major_version_upgrade = local.application_data.accounts[local.environment].db_allow_major_version_upgrade
-  backup_window               = local.application_data.accounts[local.environment].db_backup_window
-  backup_retention_period     = local.application_data.accounts[local.environment].db_retention_period
-  #checkov:skip=CKV_AWS_133: "backup_retention enabled, can be edited it application_variables.json"
-  iam_database_authentication_enabled = local.application_data.accounts[local.environment].db_iam_database_authentication_enabled
-  #checkov:skip=CKV_AWS_161: "iam auth enabled, but optional"
-  db_subnet_group_name   = aws_db_subnet_group.jitbit.id
-  vpc_security_group_ids = [aws_security_group.database_security_group.id]
-  multi_az               = local.application_data.accounts[local.environment].db_multi_az
-  #checkov:skip=CKV_AWS_157: "multi-az enabled, but optional"
-  monitoring_interval = local.application_data.accounts[local.environment].db_monitoring_interval
-  monitoring_role_arn = local.application_data.accounts[local.environment].db_monitoring_interval == 0 ? "" : aws_iam_role.rds_enhanced_monitoring[0].arn
+  parameter_group_name                = var.rds_parameter_group_name
+  deletion_protection                 = var.rds_deletion_protection
+  delete_automated_backups            = var.rds_delete_automated_backups
+  skip_final_snapshot                 = var.rds_skip_final_snapshot
+  final_snapshot_identifier           = !var.rds_skip_final_snapshot ? "${var.name}-${var.env_name}-db-final-snapshot" : null
+  allocated_storage                   = var.rds_allocated_storage
+  max_allocated_storage               = var.rds_max_allocated_storage
+  storage_type                        = var.rds_storage_type
+  maintenance_window                  = var.rds_maintenance_window
+  auto_minor_version_upgrade          = var.rds_auto_minor_version_upgrade
+  allow_major_version_upgrade         = var.rds_auto_major_version_upgrade
+  backup_window                       = var.rds_backup_window
+  backup_retention_period             = var.rds_backup_retention_period
+  iam_database_authentication_enabled = var.rds_iam_database_authentication_enabled
+  db_subnet_group_name                = aws_db_subnet_group.this.id
+  vpc_security_group_ids              = [aws_security_group.db.id]
+  multi_az                            = var.rds_multi_az
+  monitoring_interval                 = var.rds_monitoring_interval
+  monitoring_role_arn                 = var.rds_monitoring_interval != null || var.rds_monitoring_interval != 0 ? aws_iam_role.rds_enhanced_monitoring[0].arn : null
   #checkov:skip=CKV_AWS_118: "enhanced monitoring is enabled, but optional"
   storage_encrypted               = true
-  performance_insights_enabled    = local.application_data.accounts[local.environment].db_performance_insights_enabled
-  performance_insights_kms_key_id = "" #tfsec:ignore:aws-rds-enable-performance-insights-encryption Left empty so that it will run, however should be populated with real key in scenario.
-  enabled_cloudwatch_logs_exports = local.application_data.accounts[local.environment].db_enabled_cloudwatch_logs_exports
-  tags = merge(local.tags,
-    { Name = lower(format("%s-%s-database", local.application_name, local.environment)) }
+  performance_insights_enabled    = var.rds_performance_insights_enabled
+  performance_insights_kms_key_id = var.rds_performance_insights_enabled ? var.account_config.kms_keys.general_shared : null
+  enabled_cloudwatch_logs_exports = var.rds_enabled_cloudwatch_logs_exports
+  tags = merge(var.tags,
+    { Name = lower(format("%s-%s-database", var.name, var.env_name)) }
   )
 }
 
 resource "aws_iam_role" "rds_enhanced_monitoring" {
   assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring[0].json
-  count              = local.application_data.accounts[local.environment].db_monitoring_interval == 0 ? 0 : 1
+  count              = var.rds_monitoring_interval != null || var.rds_monitoring_interval != 0 ? 1 : 0
   name_prefix        = "rds-enhanced-monitoring"
 }
 
 resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
-  count      = local.application_data.accounts[local.environment].db_monitoring_interval == 0 ? 0 : 1
+  count      = var.rds_monitoring_interval != null || var.rds_monitoring_interval != 0 ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
   role       = aws_iam_role.rds_enhanced_monitoring[0].name
 }
 
 data "aws_iam_policy_document" "rds_enhanced_monitoring" {
-  count = local.application_data.accounts[local.environment].db_monitoring_interval == 0 ? 0 : 1
+  count = var.rds_monitoring_interval != null || var.rds_monitoring_interval != 0 ? 1 : 0
 
   statement {
     actions = [
