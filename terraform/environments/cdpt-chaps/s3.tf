71bfe2e5c93fd60674261239e430681a4815c797
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2023-12-12 16:19:07 +0000

    remove acl

--- terraform/environments/cdpt-chaps/s3.tf
+++ terraform/environments/cdpt-chaps/s3.tf
@@ -2,7 +2,3 @@ resource "aws_s3_bucket" "chaps-db-backup-bucket" {
 	bucket = local.app_data.accounts[local.environment].s3_bucket_name
 }
 
-resource "aws_s3_bucket_acl" "chaps-db-backup-bucket-acl" {
-	bucket = aws_s3_bucket.chaps-db-backup-bucket.id
-	acl = "private"
-}
