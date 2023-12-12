d16d11aae319f8f94058dddd6b2f9806ec71c604
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2023-12-12 16:02:04 +0000

    create s3 buckets

--- terraform/environments/cdpt-chaps/s3.tf
+++ terraform/environments/cdpt-chaps/s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "chaps-db-backup-bucket" {
 	bucket = local.app_data.accounts[local.environment].s3_bucket_name
+}
+
+resource "aws_s3_bucket_acl" "chaps-db-backup-bucket-acl" {
+	bucket = aws_s3_bucket.chaps-db-backup-bucket.id
 	acl = "private"
 }
