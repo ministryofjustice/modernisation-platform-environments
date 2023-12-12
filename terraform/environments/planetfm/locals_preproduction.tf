aebf2e0a745ea7ac33751782cfc42a75991ad8e0
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-12-12 12:38:25 +0000

    add new sg to instance for testing

--- terraform/environments/planetfm/locals_preproduction.tf
+++ terraform/environments/planetfm/locals_preproduction.tf
@@ -78,7 +78,7 @@ locals {
         })
         instance = merge(local.defaults_web_ec2.instance, {
           instance_type = "t3.large"
-          # vpc_security_group_ids = concat(local.defaults_web_ec2.instance.vpc_security_group_ids, ["cafm_app_fixngo"]) TODO: Apply in subsequent PR
+          vpc_security_group_ids = concat(local.defaults_web_ec2.instance.vpc_security_group_ids, ["cafm_app_fixngo"])
         })
         ebs_volumes = {
           "/dev/sda1" = { type = "gp3", size = 128 } # root volume
