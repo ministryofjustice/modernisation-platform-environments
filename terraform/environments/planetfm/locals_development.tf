5318491c0be12a821ed868d9e493e97ad96197e8
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-12-13 17:46:03 +0000

    rebuild test LB

--- terraform/environments/planetfm/locals_development.tf
+++ terraform/environments/planetfm/locals_development.tf
@@ -4,25 +4,26 @@ locals {
   # baseline config
   development_config = {
 
-    # baseline_lbs = {
+    baseline_lbs = {
 
-    #   private = {
-    #     internal_lb                      = true
-    #     enable_delete_protection         = false
-    #     loadbalancer_type                = "application"
-    #     idle_timeout                     = 3600
-    #     security_groups                  = ["loadbalancer"]
-    #     subnets                          = module.environment.subnets["private"].ids
-    #     enable_cross_zone_load_balancing = true
-    #     access_logs                      = true #default value
-    #     log_schedule                     = "cron(0 * * * ? *)"
+      private = {
+        internal_lb                      = true
+        enable_delete_protection         = false
+        loadbalancer_type                = "application"
+        idle_timeout                     = 3600
+        security_groups                  = ["loadbalancer"]
+        subnets                          = module.environment.subnets["private"].ids
+        enable_cross_zone_load_balancing = true
+        access_logs                      = true #default value
+        log_schedule                     = "cron(0 * * * ? *)"
+        force_destroy_bucket             = true
+        # not required for testing in sandbox
+        instance_target_groups = {}
+        # not required for testing in sandbox
+        listeners = {}
 
-    #     instance_target_groups = {}
-
-    #     listeners = {}
-
-    #   }
-    # }
+      }
+    }
   }
 }
 
