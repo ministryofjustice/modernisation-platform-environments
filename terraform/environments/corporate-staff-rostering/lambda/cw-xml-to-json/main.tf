de5804abce271eabe8bbfd660beb2d17507703ee
Keir Williams <keir.williams@digital.justice.gov.uk>
2023-12-13 16:59:01 +0000

    add log_group name to subscription filter

--- terraform/environments/corporate-staff-rostering/lambda/cw-xml-to-json/main.tf
+++ terraform/environments/corporate-staff-rostering/lambda/cw-xml-to-json/main.tf
@@ -28,7 +28,7 @@ module "lambda_cw_logs_xml_to_json" {
 
 resource "aws_cloudwatch_log_subscription_filter" "cw_logs_xml_to_json" {
   name            = "cw-logs-xml-to-json-application-logs"
-  log_group_name  = null # TODO add log group name
-  filter_pattern  = ""   # TODO add filter pattern
+  log_group_name  = "cwagent-windows-application"
+  filter_pattern  = "" # TODO add filter pattern
   destination_arn = module.lambda_cw_logs_xml_to_json.lambda_function_arn
 }
