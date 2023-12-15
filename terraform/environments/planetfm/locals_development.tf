34e27c5c8e4e6bf01fa5a52f489c3a4ecceaee37
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-12-15 10:30:30 +0000

    don't use custom key

--- terraform/environments/planetfm/locals_development.tf
+++ terraform/environments/planetfm/locals_development.tf
@@ -6,7 +6,7 @@ locals {
 
     baseline_s3_buckets = {
       public-lb-logs-bucket = {
-        custom_kms_key = module.environment.kms_keys["general"].arn
+        # custom_kms_key = module.environment.kms_keys["general"].arn
         iam_policies   = module.baseline_presets.s3_iam_policies
       }
     }
