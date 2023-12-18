5379303f422cb61c66e82e826df5051847a32423
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-18 17:40:49 +0000

    Use scaling group name

--- terraform/environments/performance-hub/module/ecs/main.tf
+++ terraform/environments/performance-hub/module/ecs/main.tf
@@ -239,6 +239,8 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 
 resource "aws_ecs_cluster_capacity_providers" "ecs_cluster" {
   cluster_name = aws_ecs_cluster.ecs_cluster.name
+
+  capacity_providers = [aws_autoscaling_group.cluster-scaling-group.name]
 }
 
 resource "aws_ecs_task_definition" "windows_ecs_task_definition" {
