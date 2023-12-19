fa658b877f9f72795c1a569451819481e28015d7
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-19 15:54:59 +0000

    reduce memory requirement

--- terraform/environments/cdpt-chaps/ecs.tf
+++ terraform/environments/cdpt-chaps/ecs.tf
@@ -73,7 +73,7 @@ resource "aws_ecs_task_definition" "chaps_task_definition" {
       name      = "${local.application_name}-container"
       image     = "${local.ecr_url}:${local.application_data.accounts[local.environment].docker_image_tag}"
       cpu       = 1024
-      memory    = 1024
+      memory    = 512
       essential = true
       portMappings = [
         {
