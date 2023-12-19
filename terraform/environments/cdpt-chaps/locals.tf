8bcdbe89b459adc4157a7101df40a929f6120886
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-19 11:30:30 +0000

    Change user data

--- terraform/environments/cdpt-chaps/locals.tf
+++ terraform/environments/cdpt-chaps/locals.tf
@@ -15,7 +15,7 @@ locals {
 
   ecr_url = "${local.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/cdpt-chaps-ecr-repo"
 
-  user_data = base64encode(templatefile("user_data.sh", {
-    app_name = local.application_name
+  user_data = base64encode(templatefile("user_data.txt", {
+    cluster_name = "${local.application_name}-ecs-cluster"
   }))
 }
