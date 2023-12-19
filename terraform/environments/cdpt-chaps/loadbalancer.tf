534c5547f9b91493c878ec62d3df8ad4479257f6
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-19 17:18:04 +0000

    Update name

--- terraform/environments/cdpt-chaps/loadbalancer.tf
+++ terraform/environments/cdpt-chaps/loadbalancer.tf
@@ -36,7 +36,7 @@ resource "aws_lb" "chaps_lb" {
 }
 
 resource "aws_lb_target_group" "chaps_target_group" {
-  name                 = "chaps-tg"
+  name                 = "chaps-target-group"
   port                 = 80
   protocol             = "HTTP"
   vpc_id               = data.aws_vpc.shared.id
