d0d038edfbcd10ccb92b7811e38bbeada0b6223b
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-12-19 16:43:51 +0000

    apply nlb bucket to nlb

--- terraform/environments/planetfm/locals_development.tf
+++ terraform/environments/planetfm/locals_development.tf
@@ -171,9 +171,9 @@ locals {
         security_groups                  = ["loadbalancer"]
         subnets                          = module.environment.subnets["private"].ids
         enable_cross_zone_load_balancing = true
-        access_logs                      = false #default value is true
+        access_logs                      = true #default value is true
         force_destroy_bucket             = true
-        # existing_bucket_name             = "network-lb-logs-bucket20231219101122706700000001"
+        existing_bucket_name             = "network-lb-logs-bucket20231219101122706700000001"
         # not required for testing in sandbox
         instance_target_groups = {
           network-lb-target-group = {
