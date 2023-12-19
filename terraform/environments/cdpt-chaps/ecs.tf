b0091024d96bf48da60827341650719d50c85f3c
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-19 14:26:24 +0000

    Use variables

--- terraform/environments/cdpt-chaps/ecs.tf
+++ terraform/environments/cdpt-chaps/ecs.tf
@@ -158,9 +158,9 @@ resource "aws_ecs_cluster_capacity_providers" "cdpt-chaps" {
 resource "aws_autoscaling_group" "cluster-scaling-group" {
   vpc_zone_identifier       = sort(data.aws_subnets.shared-private.ids)
   name                      = "${local.application_name}-cluster-scaling-group"
-  desired_capacity          = 2
-  max_size                  = 3
-  min_size                  = 1
+  desired_capacity          = local.application_data.accounts[local.environment].ecs_desired_capacity
+  max_size                  = local.application_data.accounts[local.environment].max_size
+  min_size                  = local.application_data.accounts[local.environment].min_size
   health_check_grace_period = 300
 
   launch_template {
