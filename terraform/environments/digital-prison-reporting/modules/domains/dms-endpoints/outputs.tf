bdec46f937566294946d2a0b692e18ccb1f2b63a
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-12-20 21:29:00 +0000

    Fix Outputs.tf

--- terraform/environments/digital-prison-reporting/modules/domains/dms-endpoints/outputs.tf
+++ terraform/environments/digital-prison-reporting/modules/domains/dms-endpoints/outputs.tf
@@ -1,7 +1,12 @@
 output "dms_target_endpoint_arn" {
-  value = var.setup_dms_endpoints && setup_dms_s3_endpoint ? join("", aws_dms_endpoint.dms-s3-target-endpoint.*.endpoint_arn): ""
+  value = var.setup_dms_endpoints && var.setup_dms_s3_endpoint ? join("", aws_dms_endpoint.dms-s3-target-endpoint.*.endpoint_arn): ""
 }
 
 output "dms_source_endpoint_arn" {
-  value = var.setup_dms_endpoints && setup_dms_nomis_endpoint ? join("", aws_dms_endpoint.dms-s3-target-source.*.endpoint_arn): ""
+  value = var.setup_dms_endpoints && var.setup_dms_nomis_endpoint ? join("", aws_dms_endpoint.dms-s3-target-source.*.endpoint_arn): ""
+}
+
+output "dms_s3_iam_policy_admin_arn" {
+  description = "The IAM Policy (ARN) admin of the DMS to S3 target"
+  value       = concat(aws_iam_policy.dms-operator-s3-policy.*.arn, [""])[0]
 }
