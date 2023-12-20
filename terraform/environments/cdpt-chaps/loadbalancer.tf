24b43a3c77e9c73d55ea9fe1a8c7afb94c0bfdfa
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-20 15:50:44 +0000

    Use port 8080

--- terraform/environments/cdpt-chaps/loadbalancer.tf
+++ terraform/environments/cdpt-chaps/loadbalancer.tf
@@ -4,14 +4,6 @@ resource "aws_security_group" "chaps_lb_sc" {
   vpc_id      = data.aws_vpc.shared.id
 
   ingress {
-    description = "allow access on HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "allow access on HTTPS"
     from_port   = 443
     to_port     = 443
