526225d89f2c751aa0dfed485ed7cd9f4dc11fda
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-12-21 14:12:49 +0000

    Add port 80 ingress

--- terraform/environments/cdpt-chaps/loadbalancer.tf
+++ terraform/environments/cdpt-chaps/loadbalancer.tf
@@ -4,6 +4,14 @@ resource "aws_security_group" "chaps_lb_sc" {
   vpc_id      = data.aws_vpc.shared.id
 
   ingress {
+    description = "allow access on HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "allow access on HTTPS"
     from_port   = 443
     to_port     = 443
@@ -15,7 +23,7 @@ resource "aws_security_group" "chaps_lb_sc" {
     description = "Open all outbound ports"
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
