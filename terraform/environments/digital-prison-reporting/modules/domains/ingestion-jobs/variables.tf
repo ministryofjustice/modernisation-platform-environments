3b44e0890b0173248266fdc85f565cddb2786470
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-12-21 13:55:04 +0000

    Fix Vars

--- terraform/environments/digital-prison-reporting/modules/domains/ingestion-jobs/variables.tf
+++ terraform/environments/digital-prison-reporting/modules/domains/ingestion-jobs/variables.tf
@@ -125,11 +125,212 @@ variable "glue_batch_create_role" {
   description = "(Optional) Create AWS IAM role associated with the job."
 }
 
-variable "glue_cdc_script_version" {
+
+# CDC
+
+## Glue job CDC
+
+variable "glue_cdc_arguments" {
+  type    = map(any)
+  default = {}
 }
 
-variable "setup_cdc_job" {}
-variable "glue_cdc_create_role" {}
-variable "glue_cdc_create_sec_conf" {}
-variable "glue_cdc_job_worker_type" {}
-variable "glue_cdc_job_num_workers" {}
+variable "setup_cdc_job" {
+  description = "Enable CDC Job, True or False"
+  type        = bool
+  default     = false  
+}
+
+variable "glue_cdc_job_name" {
+  description = "Name of the Glue CDC Job"
+  default     = ""  
+}
+
+variable "glue_cdc_job_short_name" {
+  description = "Name of the Glue CDC Job"
+  default     = ""  
+}
+
+variable "glue_cdc_description" {
+  description = "Job Description"
+  default     = ""  
+}
+
+variable "glue_cdc_create_sec_conf" {
+  type        = bool
+  default     = false
+  description = "(Optional) Create AWS Glue Security Configuration associated with the job."
+}
+
+variable "glue_cdc_log_group_retention_in_days" {
+  type        = number
+  default     = 1
+  description = "(Optional) The default number of days log events retained in the glue job log group."
+}
+
+
+variable "glue_cdc_language" {
+  type        = string
+  default     = "python"
+  description = "(Optional) The script programming language."
+
+  validation {
+    condition     = contains(["scala", "python"], var.glue_cdc_language)
+    error_message = "Accepts a value of 'scala' or 'python'."
+  }
+}
+
+variable "glue_cdc_temp_dir" {
+  type        = string
+  default     = null
+  description = "(Optional) Specifies an Amazon S3 path to a bucket that can be used as a temporary directory for the job."
+}
+
+variable "glue_cdc_checkpoint_dir" {
+  type        = string
+  default     = null
+  description = "(Optional) Specifies an Amazon S3 path to a bucket that can be used as a Checkoint directory for the job."
+}
+
+variable "glue_cdc_spark_event_logs" {
+  type        = string
+  default     = null
+  description = "(Optional) Specifies an Amazon S3 path to a bucket that can be used as a Spark Event Logs directory for the job."
+}
+
+variable "glue_cdc_script_location" {
+  type        = string
+  description = "(Optional) Specifies the S3 path to a script that executes a job."
+  default     = ""
+}
+
+variable "glue_cdc_enable_cont_log_filter" {
+  type        = bool
+  default     = true
+  description = "(Optional) Specifies a standard filter or no filter when you create or edit a job enabled for continuous logging."
+}
+
+variable "s3_kms_arn" {
+  type        = string
+  default     = ""
+  description = "(Optional) The ARN of the kMS Key associated to S3"
+}
+
+variable "glue_cdc_execution_class" {
+  default     = "STANDARD"
+  description = "Execution CLass Standard or FLex"
+}
+
+variable "glue_cdc_job_worker_type" {
+  type        = string
+  default     = "G.1X"
+  description = "(Optional) The type of predefined worker that is allocated when a job runs."
+
+  validation {
+    condition     = contains(["Standard", "G.1X", "G.2X"], var.glue_cdc_job_worker_type)
+    error_message = "Accepts a value of Standard, G.1X, or G.2X."
+  }
+}
+
+variable "glue_cdc_job_num_workers" {
+  type        = number
+  default     = 2
+  description = "(Optional) The number of workers of a defined workerType that are allocated when a job runs."
+}
+
+variable "glue_cdc_additional_policies" {
+  type        = string
+  default     = ""
+  description = "(Optional) The list of Policies used for this job."
+}
+
+variable "glue_cdc_max_concurrent" {
+  type        = number
+  default     = 1
+  description = "(Optional) The maximum number of concurrent runs allowed for a job."
+}
+
+variable "account_region" {
+  description = "Current AWS Region."
+  default     = "eu-west-2"
+}
+
+variable "account_id" {
+  description = "AWS Account ID."
+  default     = ""
+}
+
+variable "glue_cdc_create_role" {
+  type        = bool
+  default     = false
+  description = "(Optional) Create AWS IAM role associated with the job."
+}
+
+# Lambda
+variable "setup_step_function_notification_lambda" {
+  description = "Enable Step Function Notification Lambda, True or False ?"
+  type        = bool
+  default     = false
+}
+
+variable "step_function_notification_lambda" {
+  description = "Name for Notification Lambda Name"
+  type        = string
+  default     = ""
+}
+
+variable "s3_file_transfer_lambda_code_s3_bucket" {
+  description = "S3 File Transfer Lambda Code Bucket ID"
+  type        = string
+  default     = ""
+}
+
+variable "reporting_lambda_code_s3_key"{
+  description = "S3 File Transfer Lambda Code Bucket KEY"
+  type        = string
+  default     = ""  
+}
+
+variable "step_function_notification_lambda_handler" {
+  description = "Notification Lambda Handler"
+  type        = string
+  default     = "uk.gov.justice.digital.lambda.StepFunctionDMSNotificationLambda::handleRequest" 
+}
+
+variable "step_function_notification_lambda_runtime" {
+  description = "Lambda Runtime"
+  type        = string
+  default     = "java11"    
+}
+
+variable "step_function_notification_lambda_policies" {
+  description = "An List of Notification Lambda Policies"
+  type        = list(string)
+  default     = []  
+}
+
+variable "step_function_notification_lambda_tracing" {
+  description = "Lambda Tracing"
+  type        = string
+  default     = "Active"
+}
+
+variable "step_function_notification_lambda_trigger" {
+  description = "Name for Notification Lambda Trigger Name"
+  type        = string
+  default     = ""
+}
+
+variable "lambda_subnet_ids" {
+  description = "Lambda Subnet ID's"
+  type        = list(string)
+  default     = []
+}
+
+variable "lambda_security_group_ids" {
+  description = "Lambda Security Group ID's"
+  type        = list(string)
+  default     = []
+}
+
+variable "glue_cdc_script_version" {}
