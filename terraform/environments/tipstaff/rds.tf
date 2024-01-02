1508675b6b6fda46566ee3c08b5b0dd4bff9a269
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-01-02 09:27:09 +0000

    Tipstaff: update temp IP for RDS access

--- terraform/environments/tipstaff/rds.tf
+++ terraform/environments/tipstaff/rds.tf
@@ -52,8 +52,8 @@ resource "aws_security_group" "postgresql_db_sc" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    description = "temp access for Matt"
-    cidr_blocks = ["84.66.63.74/32"]
+    description = "temp access"
+    cidr_blocks = ["82.25.16.167"]
   }
 
   egress {
