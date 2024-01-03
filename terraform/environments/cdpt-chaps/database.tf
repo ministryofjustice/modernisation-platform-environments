f15b81bcc2a79515fced5bc33820e28cbdcabb0c
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2024-01-03 11:30:23 +0000

    fixed typo

--- terraform/environments/cdpt-chaps/database.tf
+++ terraform/environments/cdpt-chaps/database.tf
@@ -12,7 +12,7 @@ resource "aws_db_instance" "database" {
   username               = local.application_data.accounts[local.environment].db_user
   password               = data.aws_secretsmanager_secret_version.db_password.secret_string
   vpc_security_group_ids = [aws_security_group.db.id]
-  depends_on             = [aws_security_group.id]
+  depends_on             = [aws_security_group.db]
   snapshot_identifier    = "arn:aws:rds:eu-west-2:613903586696:snapshot:dev-modplatform-snapshot"
   skip_final_snapshot    = true
 }
