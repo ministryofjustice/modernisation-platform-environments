66345a8f005ddd2d56dbfab6ceeb65a3c6a6f519
roncitrus <Alistair.Curtis@digital.justice.gov.uk>
2024-01-03 10:16:50 +0000

    remove data definition

--- terraform/environments/cdpt-chaps/database.tf
+++ terraform/environments/cdpt-chaps/database.tf
@@ -52,7 +52,6 @@ data "aws_secretsmanager_secret_version" "db_password" {
   secret_id = data.aws_secretsmanager_secret.db_password.id
 }
 
-
 #------------------------------------------------------------------------------
 # KMS setup for RDS
 #------------------------------------------------------------------------------
