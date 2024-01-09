6585a4bf7bcc292de4b096234fa013c2f6ead787
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2024-01-09 15:22:05 +0000

    adding kms key and attaching it to cloudwatch log group

--- terraform/environments/maat/api-cw.tf
+++ terraform/environments/maat/api-cw.tf
@@ -1,7 +1,11 @@
 ######################################
 # ECS CLOUDWATCH GROUP
 ######################################
+resource "aws_kms_key" "cloudwatch_logs_key" {
+  description             = "KMS key to be used for encrypting the CloudWatch logs in the Log Groups"
+}
 resource "aws_cloudwatch_log_group" "maat_api_ecs_cw_group" {
   name              = "${local.application_name}-ECS"
   retention_in_days = 90
+  kms_key_id = aws_kms_key.cloudwatch_logs_key.arn
 }
