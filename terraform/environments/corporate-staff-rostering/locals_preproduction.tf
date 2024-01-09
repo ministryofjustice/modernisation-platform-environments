657dddebee1331a94a579aef89caca2a52b67b85
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-01-09 18:02:47 +0000

    destroy s3 buckets for nlb logs

--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -9,62 +9,6 @@ locals {
         custom_kms_key = module.environment.kms_keys["general"].arn
         iam_policies   = module.baseline_presets.s3_iam_policies
       }
-      nlb-logs-bucket = {
-        force_destroy = true
-        sse_algorithm = "AES256"
-        iam_policies  = module.baseline_presets.s3_iam_policies
-        bucket_policy_v2 = [
-          {
-            effect = "Allow"
-            actions = [
-              "s3:PutObject"
-            ]
-            principals = {
-              identifiers = ["delivery.logs.amazonaws.com"]
-              type        = "Service"
-            }
-            conditions = [
-              {
-                test     = "StringEquals"
-                variable = "s3:x-amz-acl"
-                values   = ["bucket-owner-full-control"]
-              },
-              {
-                test     = "StringEquals"
-                variable = "aws:SourceAccount"
-                values   = [module.environment.account_id]
-              },
-              {
-                test     = "ArnLike"
-                variable = "aws:SourceArn"
-                values   = ["arn:aws:logs:${module.environment.region}:${module.environment.account_id}:*"]
-              }
-            ]
-          },
-          {
-            effect = "Allow"
-            actions = [
-              "s3:GetBucketAcl"
-            ]
-            principals = {
-              identifiers = ["delivery.logs.amazonaws.com"]
-              type        = "Service"
-            }
-            conditions = [
-              {
-                test     = "StringEquals"
-                variable = "aws:SourceAccount"
-                values   = [module.environment.account_id]
-              },
-              {
-                test     = "ArnLike"
-                variable = "aws:SourceArn"
-                values   = ["arn:aws:logs:${module.environment.region}:${module.environment.account_id}:*"]
-              }
-            ]
-          }
-        ]
-      }
     }
 
     baseline_secretsmanager_secrets = {
