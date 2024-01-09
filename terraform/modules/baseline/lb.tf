95dda3072a6ceb6eb99ba89940b5fc000e167dc1
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-01-09 12:32:16 +0000

    use new LB module - this redeploys the athena part of the lb access_logs

--- terraform/modules/baseline/lb.tf
+++ terraform/modules/baseline/lb.tf
@@ -110,7 +110,7 @@ module "lb" {
 
   for_each = var.lbs
 
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=815c21be487a6e4f8b3d8d556dedb58bbfa0c5d0" # latest 05/01/2024 - not yet released
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=6f59e1ce47df66bc63ee9720b7c58993d1ee64ee" # latest 09/01/2024 - not yet released
 
   providers = {
     aws.bucket-replication = aws
