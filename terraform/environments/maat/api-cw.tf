38facc3e1d96ce58b445b713b1541cd6b25158f1
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2024-01-09 15:31:15 +0000

    changing resource attribute used from aws_kms_key resource

--- terraform/environments/maat/api-cw.tf
+++ terraform/environments/maat/api-cw.tf
@@ -7,5 +7,5 @@ resource "aws_kms_key" "cloudwatch_logs_key" {
 resource "aws_cloudwatch_log_group" "maat_api_ecs_cw_group" {
   name              = "${local.application_name}-ECS"
   retention_in_days = 90
-  kms_key_id = aws_kms_key.cloudwatch_logs_key.arn
+  kms_key_id = aws_kms_key.cloudwatch_logs_key.key_id
 }
