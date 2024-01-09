fe966924f4d52a6026149e781806bdd68519e9a7
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-01-09 16:28:45 +0000

    comment out acl permissions

--- terraform/environments/electronic-monitoring-data/s3.tf
+++ terraform/environments/electronic-monitoring-data/s3.tf
@@ -1,28 +1,39 @@
 #------------------------------------------------------------------------------
-# S3 bucket for bucket access logs
+# S3 bucket for bucket action logs
 #------------------------------------------------------------------------------
 
 resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket"
+  bucket_prefix = "em-data-store-logs-"
 }
 
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
-}
+#resource "aws_s3_bucket_acl" "log_bucket_acl" {
+#  bucket = aws_s3_bucket.log_bucket.id
+#  acl    = "log-delivery-write"
+#}
 
 #------------------------------------------------------------------------------
-# S3 bucket for Capita
+# S3 bucket for landing Capita data
+#
+# Lifecycle management not implemented for this bucket as everything will be
+# moved to a different bucket once landed.
 #------------------------------------------------------------------------------
+
+resource "random_string" "capita_random_string" {
+  length  = 10
+  lower   = true
+  upper   = false
+  numeric = true
+  special = false
+}
 
 resource "aws_s3_bucket" "capita_landing_bucket" {
-  bucket_prefix = "capita"
+  bucket = "capita-${random_string.capita_random_string.result}"
 }
 
-resource "aws_s3_bucket_acl" "capita_landing_bucket" {
-  bucket = aws_s3_bucket.capita_landing_bucket.id
-  acl    = "private"
-}
+#resource "aws_s3_bucket_acl" "capita_landing_bucket" {
+#  bucket = aws_s3_bucket.capita_landing_bucket.id
+#  acl    = "private"
+#}
 
 resource "aws_s3_bucket_versioning" "capita_landing_bucket" {
   bucket = aws_s3_bucket.capita_landing_bucket.id
