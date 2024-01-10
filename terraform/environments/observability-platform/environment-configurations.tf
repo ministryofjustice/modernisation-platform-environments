aef30fccb725edb6a2d94f2426b79c2b08de31d0
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-01-10 16:00:23 +0000

    :wrench: Update sso_uid

--- terraform/environments/observability-platform/environment-configurations.tf
+++ terraform/environments/observability-platform/environment-configurations.tf
@@ -16,11 +16,11 @@ locals {
           ]
         }
         "digital-studio-operations" = {
-          sso_uuid = "a68242b4-b0a1-7085-25f4-dc60e4c122c0"
-            cloudwatch_accounts = [
-              "nomis-test",
-              "oasys-test"
-            ]
+          sso_uuid = "9c6710dd7f-120a1f73-34c1-447a-b34c-6cdc2cd64b5e"
+          cloudwatch_accounts = [
+            "nomis-test",
+            "oasys-test"
+          ]
         }
       }
     }
