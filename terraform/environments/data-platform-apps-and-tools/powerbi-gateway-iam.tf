b1c9b2cb961a21689a760a41aa711d63c541de53
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-01-10 17:24:05 +0000

    Policy SID

--- terraform/environments/data-platform-apps-and-tools/powerbi-gateway-iam.tf
+++ terraform/environments/data-platform-apps-and-tools/powerbi-gateway-iam.tf
@@ -1,17 +1,16 @@
 data "aws_iam_policy_document" "powerbi_gateway_data_access" {
   statement {
-    sid = "local.environment_configuration.powerbi_gateway_ec2.instance_name"
+    sid = "PowerBIAssumeDataRoles"
 
     actions = [
       "sts:AssumeRole",
     ]
-    resources = formatlist("arn:aws:iam::%s:root", local.environment_configuration.powerbi_target_accounts)
+    resources = formatlist("arn:aws:iam::%s:role/alpha_*", local.environment_configuration.powerbi_target_accounts)
   }
 }
 
 
 resource "aws_iam_policy" "powerbi_gateway_data_access" {
-  name   = "DataPlatformAppsToolsPowerBIPolicy"
-  path   = "/"
+  name   = local.environment_configuration.powerbi_gateway_ec2.instance_name
   policy = data.aws_iam_policy_document.powerbi_gateway_data_access.json
 }
