127d30a451e2cfd4b0b521b4efb486a7d9c575d1
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2024-01-10 10:30:22 +0000

    moving params and keys to different files and adding key policy

--- terraform/environments/maat/api-cw.tf
+++ terraform/environments/maat/api-cw.tf
@@ -1,11 +1,8 @@
 ######################################
 # ECS CLOUDWATCH GROUP
 ######################################
-resource "aws_kms_key" "cloudwatch_logs_key" {
-  description             = "KMS key to be used for encrypting the CloudWatch logs in the Log Groups"
-}
 resource "aws_cloudwatch_log_group" "maat_api_ecs_cw_group" {
   name              = "${local.application_name}-ECS"
   retention_in_days = 90
-  kms_key_id = aws_kms_key.cloudwatch_logs_key.key_id
+  kms_key_id = aws_kms_key.cloudwatch_logs_key.arn
 }
