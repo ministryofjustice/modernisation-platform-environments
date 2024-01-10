0d341c7fcbbbc96bc963b644eeb7c9c2d39c08bf
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-01-10 17:22:04 +0000

    Ports.

--- terraform/environments/data-platform-apps-and-tools/powerbi-gateway-security-group.tf
+++ terraform/environments/data-platform-apps-and-tools/powerbi-gateway-security-group.tf
@@ -4,6 +4,7 @@ resource "aws_security_group" "powerbi_gateway" {
   description = local.environment_configuration.powerbi_gateway_ec2.instance_name
   vpc_id      = data.aws_vpc.shared.id
 
+  # https://learn.microsoft.com/en-us/data-integration/gateway/service-gateway-communication#ports
   egress {
     from_port   = 443
     to_port     = 443
@@ -11,11 +12,18 @@ resource "aws_security_group" "powerbi_gateway" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
+  egress {
+    from_port   = 5671
+    to_port     = 5672
     protocol    = "tcp"
-    cidr_blocks = [local.environment_configuration.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 9352
+    to_port     = 9354
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = local.tags
