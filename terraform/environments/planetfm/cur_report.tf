448ee53375f6c64b21ebc6f24bd3e923eb2aa14d
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-01-11 12:08:57 +0000

    add athena resources

--- terraform/environments/planetfm/cur_report.tf
+++ terraform/environments/planetfm/cur_report.tf
@@ -5,13 +5,13 @@ resource "aws_cur_report_definition" "cur_planetfm" {
   format                     = "Parquet"
   compression                = "Parquet"
   additional_schema_elements = ["RESOURCES", "SPLIT_COST_ALLOCATION_DATA"]
-  s3_bucket                  = module.s3-bucket.bucket.id
+  s3_bucket                  = module.csr-report-bucket.bucket.id
   s3_region                  = "eu-west-2"
   additional_artifacts       = ["ATHENA"]
   report_versioning          = "OVERWRITE_REPORT" 
 }
 
-module "s3-bucket" {
+module "csr-report-bucket" {
     source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.1.0"
 
     bucket_prefix = "planetfm"
@@ -19,6 +19,10 @@ module "s3-bucket" {
     bucket_policy = [data.aws_iam_policy_document.cur_bucket_policy.json]
     force_destroy  = true
     replication_enabled = false
+    sse_algorithm = "AES256"
+    providers = {
+        aws.bucket-replication = aws
+    }
 
     tags = merge(local.tags, {
         Name = lower(format("cur-report-bucket-%s-%s", local.application_name, local.environment))
@@ -29,7 +33,7 @@ data "aws_iam_policy_document" "cur_bucket_policy" {
     statement {
         sid       = "EnsureBucketOwnedByAccountForCURDelivery"
         effect    = "Allow"
-        resources = [module.s3-bucket.bucket.arn]
+        resources = [module.csr-report-bucket.bucket.arn]
 
         actions = [
         "s3:GetBucketAcl",
@@ -57,7 +61,7 @@ data "aws_iam_policy_document" "cur_bucket_policy" {
     statement {
         sid       = "GrantAccessToDeliverCURFiles"
         effect    = "Allow"
-        resources = ["${module.s3-bucket.bucket.arn}/*"]
+        resources = ["${module.csr-report-bucket.bucket.arn}/*"]
         actions   = ["s3:PutObject"]
 
         condition {
@@ -75,6 +79,47 @@ data "aws_iam_policy_document" "cur_bucket_policy" {
         principals {
         type        = "Service"
         identifiers = ["billingreports.amazonaws.com"]
+        }
+    }
+}
+
+resource "aws_athena_database" "cur" {
+    name = "cur"
+    bucket = module.csr-report-bucket.bucket.id
+    encryption_configuration {
+        encryption_option = "SSE_S3"
+    }
+}
+
+resource "aws_athena_workgroup" "cur" {
+    name = "cur"
+    configuration {
+        enforce_workgroup_configuration = true
+        publish_cloudwatch_metrics_enabled = true
+
+        engine_version {
+            selected_engine_version = "Athena engine version 3"    
+        }
+        result_configuration {
+            output_location = "s3://${module.csr-report-bucket.bucket.id}/cur/"
+        }
+    }
+}
+
+resource "aws_glue_catalog_table" "cur" {
+    name = "cur-table"
+    database_name = aws_athena_database.cur.name
+    table_type = "EXTERNAL_TABLE"
+
+    storage_descriptor {
+        input_format = "org.apache.hadoop.mapred.TextInputFormat"
+        output_format = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+        location = "s3://${module.csr-report-bucket.bucket.id}/"
+        ser_de_info {
+            serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+            parameters = {
+            "serialization.format" = "1"
+            }
         }
     }
 }
