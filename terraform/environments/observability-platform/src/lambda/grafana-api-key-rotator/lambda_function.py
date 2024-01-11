26b601bcd0112e7c229ade4258d444309a721018
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-01-11 21:28:43 +0000

    ran through black and pycodestyle
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/src/lambda/grafana-api-key-rotator/lambda_function.py
+++ terraform/environments/observability-platform/src/lambda/grafana-api-key-rotator/lambda_function.py
@@ -3,62 +3,59 @@ import botocore.exceptions
 import os
 
 
-grafana_client = boto3.client('grafana')
-secretsmanager_client = boto3.client('secretsmanager')
-workspace_api_key_name = os.environ['WORKSPACE_API_KEY_NAME']
-workspace_api_key_ttl = os.environ.get('WORKSPACE_API_KEY_TTL') or 1209600 # 1209600 is 14 days
-workspace_id = os.environ['WORKSPACE_ID']
-secret_id = os.environ['SECRET_ID']
+grafana_client = boto3.client("grafana")
+secretsmanager_client = boto3.client("secretsmanager")
+workspace_api_key_name = os.environ["WORKSPACE_API_KEY_NAME"]
+workspace_api_key_ttl = (
+    os.environ.get("WORKSPACE_API_KEY_TTL") or 1209600
+)  # 1209600 is 14 days
+workspace_id = os.environ["WORKSPACE_ID"]
+secret_id = os.environ["SECRET_ID"]
 
 
 def lambda_handler(event, context):
     try:
         delete_workspace_api_key = grafana_client.delete_workspace_api_key(
-            keyName=workspace_api_key_name,
-            workspaceId=workspace_id
+            keyName=workspace_api_key_name, workspaceId=workspace_id
         )
     except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+        if e.response["Error"]["Code"] == "ResourceNotFoundException":
             pass
         else:
             print(e)
             return {
-                'statusCode': 500,
-                'body': 'Failed to delete Grafana API key'
-            }
+                "statusCode": 500,
+                "body": "Failed to delete Grafana API key",
+            }  # noqa E501
 
     try:
         create_workspace_api_key = grafana_client.create_workspace_api_key(
             keyName=workspace_api_key_name,
-            keyRole='ADMIN',
+            keyRole="ADMIN",
             secondsToLive=workspace_api_key_ttl,
-            workspaceId=workspace_id
+            workspaceId=workspace_id,
         )
 
     except Exception as e:
         print(e)
-        return {
-            'statusCode': 500,
-            'body': 'Failed to create Grafana API key'
-        }
+        return {"statusCode": 500, "body": "Failed to create Grafana API key"}
 
-    api_key = create_workspace_api_key['key']
+    api_key = create_workspace_api_key["key"]
 
-    try: 
+    try:
         update_secret = secretsmanager_client.update_secret(
-            SecretId=secret_id,
-            SecretString=api_key
+            SecretId=secret_id, SecretString=api_key
         )
 
     except Exception as e:
         print(e)
         return {
-            'statusCode': 500,
-            'body': 'Failed to update AWS Secrets Manager secret'
+            "statusCode": 500,
+            "body": "Failed to update AWS Secrets Manager secret",
         }
 
     else:
         return {
-            'statusCode': 200,
-            'body': 'Successfully updated AWS Secrets Manager secret'
+            "statusCode": 200,
+            "body": "Successfully updated AWS Secrets Manager secret",
         }
