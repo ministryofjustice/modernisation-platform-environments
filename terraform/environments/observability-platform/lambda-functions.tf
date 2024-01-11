37ba7dd96273b5f98b20b9dc401967fe485a2dfe
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-01-11 16:03:11 +0000

    Add lambda function module Add WIP main.py
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/environments/observability-platform/lambda-functions.tf
@@ -0,0 +1,37 @@
+module "grafana_api_key_rotator" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 6.0"
+
+  publish = true
+
+  function_name = "grafana-api-key-rotator"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.12"
+
+  source_path = "${path.module}/src/lambda/grafana-api-key-rotator"
+
+  environment_variables = {
+    WORKSPACE_ID = module.managed_grafana.workspace_id
+    SECRET_ID    = aws_secretsmanager_secret.grafana_api_key.id
+  }
+
+  policy_statements = {
+    "amazonmanagedgrafana" = {
+      sid    = "AmazonManagedGrafana"
+      effect = "Allow"
+      actions = [
+        "grafana:CreateWorkspaceApiKey",
+        "grafana:DeleteWorkspaceApiKey"
+      ]
+      resources = [module.managed_grafana.workspace_arn]
+    }
+    "secretsmanager" = {
+      sid       = "SecretsManager"
+      effect    = "Allow"
+      actions   = ["secretsmanager:UpdateSecret"]
+      resources = [aws_secretsmanager_secret.grafana_api_key.arn]
+    }
+  }
+}
