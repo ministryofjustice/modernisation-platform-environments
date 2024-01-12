9d478d7e6962f121b0438431700aecf54175af5f
George Taylor <george.taylor@digital.justice.gov.uk>
2024-01-12 15:26:24 +0000

    add cronjob

--- terraform/environments/delius-core/modules/components/oracle_db_instance/templates/concatenated_user_data.sh
+++ terraform/environments/delius-core/modules/components/oracle_db_instance/templates/concatenated_user_data.sh
@@ -1,3 +1,15 @@
 ${default}
 
 ${ssh_setup}
+
+###########################################
+## SCHEDULE SCRIPTS AND SECURITY UPDATES ##
+###########################################
+
+cat > ~/mycron << EOF
+# */15 * * * * /usr/bin/bastion/sync_users
+# 0 0 * * * yum -y update --security
+*/10 * * * * /usr/bin/bastion/sync_s3
+EOF
+crontab ~/mycron
+rm ~/mycron
