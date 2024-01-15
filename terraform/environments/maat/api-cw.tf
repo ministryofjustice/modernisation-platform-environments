723f6fe245d10d8f59c1751c702a99b359a00978
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2024-01-15 17:01:15 +0000

    moving variables to separate file

--- terraform/environments/maat/api-cw.tf
+++ terraform/environments/maat/api-cw.tf
@@ -1,46 +1,6 @@
 ######################################
 # ECS CLOUDWATCH GROUP
 ######################################
-variable "ecs_cpu_alarm_threshold" {
-  type = number
-  default = 75
-}
-variable "ecs_memory_alarm_threshold" {
-  type = number
-  default = 75
-}
-variable "alb_target_response_time_threshold" {
-  type = number
-  default = 10
-}
-variable "alb_target_response_time_threshold_maximum" {
-  type = number
-  default = 60
-}
-variable "alb_unhealthy_alarm_threshold" {
-  type = number
-  default = 0
-}
-variable "alb_rejected_alarm_threshold" {
-  type = number
-  default = 10
-}
-variable "alb_target_5xx_alarm_threshold" {
-  type = number
-  default = 10
-}
-variable "alb_5xx_alarm_threshold" {
-  type = number
-  default = 10
-}
-variable "alb_target_4xx_alarm_threshold" {
-  type = number
-  default = 10
-}
-variable "alb_4xx_alarm_threshold" {
-  type = number
-  default = 10
-}
 resource "aws_cloudwatch_log_group" "maat_api_ecs_cw_group" {
   name              = "${local.application_name}-ECS"
   retention_in_days = 90
