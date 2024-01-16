1c9a927d289cd990f1e63bf71d93b9e2705f278f
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-01-16 22:56:34 +0000

    ELM-1313 add test user permissions

--- terraform/environments/electronic-monitoring-data/transfer_server_capita.tf
+++ terraform/environments/electronic-monitoring-data/transfer_server_capita.tf
@@ -15,12 +15,12 @@ resource "aws_eip" "capita_eip" {
 #------------------------------------------------------------------------------
 
 resource "aws_security_group" "capita_security_group" {
-  name        = "capita_allowed_inbound_IPs"
+  name        = "capita_inbound_ips"
   description = "Allowed IP addresses from Capita"
   vpc_id      = data.aws_vpc.shared.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_1" {
+resource "aws_vpc_security_group_ingress_rule" "capita_ip_1" {
   security_group_id = aws_security_group.capita_security_group.id
 
   cidr_ipv4   = "82.203.33.112/28"
@@ -29,7 +29,7 @@ resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_1" {
   to_port     = 0
 }
 
-resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_2" {
+resource "aws_vpc_security_group_ingress_rule" "capita_ip_2" {
   security_group_id = aws_security_group.capita_security_group.id
 
   cidr_ipv4   = "82.203.33.128/28"
@@ -38,7 +38,7 @@ resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_2" {
   to_port     = 0
 }
 
-resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_3" {
+resource "aws_vpc_security_group_ingress_rule" "capita_ip_3" {
   security_group_id = aws_security_group.capita_security_group.id
 
   cidr_ipv4   = "85.115.52.0/24"
@@ -47,7 +47,7 @@ resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_3" {
   to_port     = 0
 }
 
-resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_4" {
+resource "aws_vpc_security_group_ingress_rule" "capita_ip_4" {
   security_group_id = aws_security_group.capita_security_group.id
 
   cidr_ipv4   = "85.115.53.0/24"
@@ -56,7 +56,7 @@ resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_4" {
   to_port     = 0
 }
 
-resource "aws_vpc_security_group_ingress_rule" "capita_allowed_ip_5" {
+resource "aws_vpc_security_group_ingress_rule" "capita_ip_5" {
   security_group_id = aws_security_group.capita_security_group.id
 
   cidr_ipv4   = "85.115.54.0/24"
@@ -80,7 +80,7 @@ resource "aws_transfer_server" "capita_transfer_server" {
     vpc_id                 = data.aws_vpc.shared.id
     subnet_ids             = [data.aws_subnet.public_subnets_b.id]
     address_allocation_ids = [aws_eip.capita_eip.id]
-    security_group_ids     = [aws_security_group.capita_security_group.id]
+    security_group_ids     = [aws_security_group.capita_security_group.id, aws_security_group.test_security_group]
   }
 
   domain = "S3"
@@ -97,44 +97,44 @@ resource "aws_transfer_server" "capita_transfer_server" {
 # bucket.
 #------------------------------------------------------------------------------
 
-resource "aws_transfer_user" "capita_transfer_user" {
-  server_id = aws_transfer_server.capita_transfer_server.id
-  user_name = "capita_supplier"
-  role      = aws_iam_role.capita_transfer_user_iam_role.arn
-}
+# resource "aws_transfer_user" "capita_transfer_user" {
+#   server_id = aws_transfer_server.capita_transfer_server.id
+#   user_name = "capita"
+#   role      = aws_iam_role.capita_transfer_user_iam_role.arn
+# }
 
-data "aws_iam_policy_document" "capita_assume_role" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "capita_assume_role" {
+#   statement {
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["transfer.amazonaws.com"]
-    }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["transfer.amazonaws.com"]
+#     }
 
-    actions = ["sts:AssumeRole"]
-  }
-}
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
 
-resource "aws_iam_role" "capita_transfer_user_iam_role" {
-  name                = "capita-transfer-user-iam-role"
-  assume_role_policy  = data.aws_iam_policy_document.capita_assume_role.json
-}
+# resource "aws_iam_role" "capita_transfer_user_iam_role" {
+#   name                = "capita-transfer-user-iam-role"
+#   assume_role_policy  = data.aws_iam_policy_document.capita_assume_role.json
+# }
 
-data "aws_iam_policy_document" "capita_transfer_user_iam_policy_document" {
-  statement {
-    sid       = "AllowPutAccesstoCapitaS3"
-    effect    = "Allow"
-    actions   = ["s3:PutObject"]
-    resources = [aws_s3_bucket.capita_landing_bucket.arn]
-  }
-}
+# data "aws_iam_policy_document" "capita_transfer_user_iam_policy_document" {
+#   statement {
+#     sid       = "AllowPutAccesstoCapitaS3"
+#     effect    = "Allow"
+#     actions   = ["s3:PutObject"]
+#     resources = [aws_s3_bucket.capita_landing_bucket.arn]
+#   }
+# }
 
-resource "aws_iam_role_policy" "capita_transfer_user_iam_policy" {
-  name   = "capita-transfer-user-iam-policy"
-  role   = aws_iam_role.capita_transfer_user_iam_role.id
-  policy = data.aws_iam_policy_document.capita_transfer_user_iam_policy_document.json
-}
+# resource "aws_iam_role_policy" "capita_transfer_user_iam_policy" {
+#   name   = "capita-transfer-user-iam-policy"
+#   role   = aws_iam_role.capita_transfer_user_iam_role.id
+#   policy = data.aws_iam_policy_document.capita_transfer_user_iam_policy_document.json
+# }
 
 #------------------------------------------------------------------------------
 # AWS transfer ssh key
@@ -142,8 +142,8 @@ resource "aws_iam_role_policy" "capita_transfer_user_iam_policy" {
 # Set the public ssh key for the supplier user profile to access SFTP server.
 #------------------------------------------------------------------------------
 
-resource "aws_transfer_ssh_key" "capita_ssh_key" {
-  server_id = aws_transfer_server.capita_transfer_server.id
-  user_name = aws_transfer_user.capita_transfer_user.user_name
-  body      = "... PUBLIC SSH key ..."
-}
+# resource "aws_transfer_ssh_key" "capita_ssh_key" {
+#   server_id = aws_transfer_server.capita_transfer_server.id
+#   user_name = aws_transfer_user.capita_transfer_user.user_name
+#   body      = "... PUBLIC SSH key ..."
+# }
