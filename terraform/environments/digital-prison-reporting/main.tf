59cde687fb4c8bbe537e049b6e71625b094c1bf9
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2024-01-16 22:14:31 +0000

    DPR2-46: Create temp reload bucket

--- terraform/environments/digital-prison-reporting/main.tf
+++ terraform/environments/digital-prison-reporting/main.tf
@@ -592,6 +592,25 @@ module "s3_working_bucket" {
     }
   )
 }
+
+# S3 Temp Reload Bucket
+module "s3_temp_reload_bucket" {
+  source                    = "./modules/s3_bucket"
+  create_s3                 = local.setup_buckets
+  name                      = "${local.project}-temp-reload-${local.env}"
+  custom_kms_key            = local.s3_kms_arn
+  create_notification_queue = false # For SQS Queue
+  enable_lifecycle          = true
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name          = "${local.project}-temp-reload-${local.env}"
+      Resource_Type = "S3 Bucket"
+    }
+  )
+}
+
 ##########################
 # Data Domain Components # 
 ##########################
