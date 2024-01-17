98b5161ca8d950a559ef06a24e6ac801ceaf5cf9
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2024-01-17 17:00:45 +0000

    fixing tf code syntax and referencing correct attributes etc

--- terraform/environments/maat/api-gw.tf
+++ terraform/environments/maat/api-gw.tf
@@ -1,18 +1,19 @@
 # Variables
-variable "pAppName" {}
-variable "pApiScope1" {}
-variable "pApiStageName" {}
-variable "pAppApiGatewayFQDN" {}
-variable "pAppCertificateArn" {}
+locals {
+    api_scope = local.application_data.accounts[local.environment].api_scope
+    api_gateway_fqdn = local.application_data.accounts[local.environment].api_gateway_fqdn
+    api_stage_name = "v1"
+    certificate_arn = local.application_data.accounts[local.environment].certificate_arn
+}
 
 # API Gateway configuration
 resource "aws_apigatewayv2_vpc_link" "maat_api_gateway_vpc_link" {
   name             = "${local.application_name}_VPC_Link"
   security_group_ids = [aws_security_group.maat_api_gw_sg]
-  subnet_ids        = [var.env-AppPrivateSubnetA, var.env-AppPrivateSubnetB, var.env-AppPrivateSubnetC]
+  subnet_ids        = [data.aws_subnet.private_subnets_a, data.aws_subnet.private_subnets_b, data.aws_subnet.private_subnets_c]
 }
 
-resource "aws_logs_log_group" "maat_api_gateway_cloudwatch_log_group" {
+resource "aws_cloudwatch_log_group" "maat_api_gateway_cloudwatch_log_group" {
   name = "${local.application_name}-API-Gateway"
   retention_in_days = 90
 }
@@ -34,136 +35,136 @@ resource "aws_apigatewayv2_integration" "maat_api_integration" {
   payload_format_version = "1.0"
 }
 
-resource "aws_apigatewayv2_route" "ApiRoute1" {
+resource "aws_apigatewayv2_route" "maat_api_route1" {
   api_id               = aws_apigatewayv2_api.maat_api_gateway.id
   route_key            = "ANY /link/validate"
   authorization_type   = "JWT"
-  authorization_scopes = [local.application_name/var.pApiScope1]
-  authorizer_id        = aws_apigatewayv2_authorizer.ApiAuthorizer.id
-  target               = "/integrations/${aws_apigatewayv2_integration.ApiIntegration.id}"
-  depends_on           = [aws_apigatewayv2_integration.ApiIntegration]
+  authorization_scopes = [local.application_name/local.api_scope]
+  authorizer_id        = aws_apigatewayv2_authorizer.maat_api_authorizer.id
+  target               = "/integrations/${aws_apigatewayv2_integration.maat_api_integration.id}"
+  depends_on           = [aws_apigatewayv2_integration.maat_api_integration]
 }
 
-resource "aws_apigatewayv2_route" "ApiRouteCrimeMeansAssessment" {
-  api_id               = aws_apigatewayv2_api.ApiGateway.id
+resource "aws_apigatewayv2_route" "maat_api_route_crime_means_assessment" {
+  api_id               = aws_apigatewayv2_api.maat_api_gateway.id
   route_key            = "ANY /api/internal/v1/{proxy+}"
   authorization_type   = "JWT"
-  authorization_scopes = [local.application_name/var.pApiScope1]
-  authorizer_id        = aws_apigatewayv2_authorizer.ApiAuthorizer.id
-  target               = "/integrations/${aws_apigatewayv2_integration.ApiIntegration.id}"
+  authorization_scopes = [local.application_name/local.api_scope]
+  authorizer_id        = aws_apigatewayv2_authorizer.maat_api_authorizer.id
+  target               = "/integrations/${aws_apigatewayv2_integration.maat_api_integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "ApiRouteCrownCourtProceeding" {
+resource "aws_apigatewayv2_route" "maat_api_route_crown_court_proceeding" {
   api_id               = aws_apigatewayv2_api.ApiGateway.id
   route_key            = "ANY /api/internal/v1/crowncourtproceeding"
   authorization_type   = "JWT"
-  authorization_scopes = [local.application_name/var.pApiScope1]
-  authorizer_id        = aws_apigatewayv2_authorizer.ApiAuthorizer.id
-  target               = "/integrations/${aws_apigatewayv2_integration.ApiIntegration.id}"
+  authorization_scopes = [local.application_name/local.api_scope]
+  authorizer_id        = aws_apigatewayv2_authorizer.maat_api_authorizer.id
+  target               = "/integrations/${aws_apigatewayv2_integration.maat_api_integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "ApiRouteEformStaging" {
-  api_id               = aws_apigatewayv2_api.ApiGateway.id
+resource "aws_apigatewayv2_route" "maat_api_route_eform_staging" {
+  api_id               = aws_apigatewayv2_api.maat_api_gateway.id
   route_key            = "ANY /api/eform/{proxy+}"
   authorization_type   = "JWT"
-  authorization_scopes = [local.application_name/var.pApiScope1]
-  authorizer_id        = aws_apigatewayv2_authorizer.ApiAuthorizerForATSAndCAA.id
-  target               = "/integrations/${aws_apigatewayv2_integration.ApiIntegration.id}"
+  authorization_scopes = [local.application_name/local.api_scope]
+  authorizer_id        = aws_apigatewayv2_authorizer.maat_api_authorizer_for_ats_and_caa.id
+  target               = "/integrations/${aws_apigatewayv2_integration.maat_api_integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "ApiRouteDCESService" {
-  api_id               = aws_apigatewayv2_api.ApiGateway.id
+resource "aws_apigatewayv2_route" "maat_api_route_dces_service" {
+  api_id               = aws_apigatewayv2_api.maat_api_gateway.id
   route_key            = "ANY /api/internal/v1/debt-collection-enforcement/{proxy+}"
   authorization_type   = "JWT"
-  authorization_scopes = [local.application_name/var.pApiScope1]
-  authorizer_id        = aws_apigatewayv2_authorizer.ApiAuthorizerForDces.id
-  target               = "/integrations/${aws_apigatewayv2_integration.ApiIntegration.id}"
+  authorization_scopes = [local.application_name/local.api_scope]
+  authorizer_id        = aws_apigatewayv2_authorizer.maat_api_authorizer_for_dces.id
+  target               = "/integrations/${aws_apigatewayv2_integration.maat_api_integration.id}"
 }
 
-resource "aws_apigatewayv2_authorizer" "ApiAuthorizer" {
+resource "aws_apigatewayv2_authorizer" "maat_api_authorizer" {
   name              = "${local.application_name}_Authorizer"
-  api_id            = aws_apigatewayv2_api.ApiGateway.id
+  api_id            = aws_apigatewayv2_api.maat_api_gateway.id
   authorizer_type   = "JWT"
   identity_sources   = ["$request.header.Authorization"]
 
   jwt_configuration {
     audience = [
-      var.CognitoUserPoolClient,
-      var.CognitoUserPoolClientCda,
-      var.CognitoUserPoolClientCrimeMeansAssessment,
-      var.CognitoUserPoolClientCrownCourtProceeding,
-      var.CognitoUserPoolClientCrownCourtContribution,
-      var.CognitoUserPoolClientCrimeEvidence,
-      var.CognitoUserPoolCrimeHardshipService,
-      var.CognitoUserPoolMAATOrchestrationService,
-      var.CognitoUserPoolClientCrimeValidationService,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_default,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_cda,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_cma,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_ccp,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_ccc,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_ce,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_chs,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_maatos,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_cvs
     ]
-    issuer   = aws_cognito_user_pool.ProviderURL
+    issuer   = aws_cognito_user_pool.maat_api_cognito_user_pool.endpoint
   }
 }
 
-resource "aws_apigatewayv2_authorizer" "ApiAuthorizerForDces" {
-  name              = "${var.pAppName}_DCES_Authorizer"
-  api_id            = aws_apigatewayv2_api.ApiGateway.id
+resource "aws_apigatewayv2_authorizer" "maat_api_authorizer_for_dces" {
+  name              = "${local.application_name}_DCES_Authorizer"
+  api_id            = aws_apigatewayv2_api.maat_api_gateway.id
   authorizer_type   = "JWT"
 
 
   jwt_configuration {
     audience = [
-      var.CognitoUserPoolClientDcesReportService,
-      var.CognitoUserPoolClientDcesDrcReportService,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_dcrs,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_dirs
     ]
-    issuer   = aws_cognito_user_pool.ProviderURL
+    issuer   = aws_cognito_user_pool.maat_api_cognito_user_pool.endpoint
   }
 }
 
-resource "aws_apigatewayv2_authorizer" "ApiAuthorizerForATSAndCAA" {
-  name              = "${var.pAppName}_ATS_And_CAA_Authorizer"
-  api_id            = aws_apigatewayv2_api.ApiGateway.id
+resource "aws_apigatewayv2_authorizer" "maat_api_authorizer_for_ats_and_caa" {
+  name              = "${local.application_name}_ATS_And_CAA_Authorizer"
+  api_id            = aws_apigatewayv2_api.maat_api_gateway.id
   authorizer_type   = "JWT"
   identity_sources   = ["$request.header.Authorization"]
 
   jwt_configuration {
     audience = [
-      var.CognitoUserPoolClientCrimeApplyAdaptor,
-      var.CognitoUserPoolClientATSService,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_caa,
+      aws_cognito_user_pool_client.maat_api_cognito_pool_client_ats
     ]
-    issuer   = aws_cognito_user_pool.ProviderURL
+    issuer   = aws_cognito_user_pool.maat_api_cognito_user_pool.endpoint
   }
 }
 
-resource "aws_apigatewayv2_stage" "ApiStage" {
-  name = var.pApiStageName
-  description = "${var.pAppName} ${var.pApiStageName} Stage"
-  api_id     = aws_apigatewayv2_api.ApiGateway.id
+resource "aws_apigatewayv2_stage" "maat_api_stage" {
+  name = local.api_stage_name
+  description = "${local.application_name} ${local.api_stage_name} Stage"
+  api_id     = aws_apigatewayv2_api.maat_api_gateway.id
   auto_deploy = true
 
   access_log_settings {
-    destination_arn = aws_logs_log_group.ApiGatewayCloudwatchLogGroup.arn
+    destination_arn = aws_cloudwatch_log_group.maat_api_gateway_cloudwatch_log_group.arn
     format = "{\"requestId\":\"$context.requestId\",\"extendedRequestId\":\"$context.extendedRequestId\",\"ip\":\"$context.identity.sourceIp\",\"clientId\":\"$context.authorizer.claims.client_id\",\"requestTime\":\"$context.requestTime\",\"routeKey\":\"$context.routeKey\",\"status\":\"$context.status\"}"
   }
 }
 
-resource "aws_apigatewayv2_domain_name" "ApiExternalDomainName" {
+resource "aws_apigatewayv2_domain_name" "maat_api_external_domain_name" {
+  domain_name = local.api_gateway_fqdn
   domain_name_configuration {
     endpoint_type = "Regional"
-    certificate_arn = var.pAppCertificateArn
-    certificate_name = "-"
+    certificate_arn = local.certificate_arn
+    security_policy = "TLS_1_2"
   }
 }
 
-resource "aws_route53_record" "ApiExternalDNSRecord" {
+resource "aws_route53_record" "maat_api_external_dns_record" {
   zone_id = data.aws_route53_zone.dns.HostedZoneId
-  comment        = "Domain CNAME record for External Api Gateway"
-  name           = var.pAppApiGatewayFQDN
+  name           = local.api_gateway_fqdn
   type           = "CNAME"
   ttl            = "60"
-  records        = [aws_apigatewayv2_domain_name.ApiExternalDomainName.regional_domain_name]
+  records        = [aws_apigatewayv2_domain_name.maat_api_external_domain_name.regional_domain_name]
 }
 
-resource "aws_apigatewayv2_api_mapping" "ApiMapping" {
-  domain_name = aws_apigatewayv2_domain_name.ApiExternalDomainName.domain_name
-  api_id      = aws_apigatewayv2_api.ApiGateway.id
-  stage       = aws_apigatewayv2_stage.ApiStage.stage_name
-  depends_on  = [aws_apigatewayv2_domain_name.ApiExternalDomainName]
+resource "aws_apigatewayv2_api_mapping" "maat_api_mapping" {
+  domain_name = aws_apigatewayv2_domain_name.maat_api_external_domain_name.domain_name
+  api_id      = aws_apigatewayv2_api.maat_api_gateway.id
+  stage       = aws_apigatewayv2_stage.maat_api_stage.stage_name
+  depends_on  = [aws_apigatewayv2_domain_name.maat_api_external_domain_name]
 }
