14aa4be277681b59a2e721c021c74fe69b1b78c7
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-01-18 15:56:22 +0000

    remove old secrets

--- terraform/environments/hmpps-domain-services/locals_domain.tf
+++ terraform/environments/hmpps-domain-services/locals_domain.tf
@@ -28,21 +28,21 @@ locals {
   domain_secretsmanager_secrets = {
     recovery_window_in_days = 0
     secrets = {
-      passwords = {
-        description = "domain passwords only accessible by this account"
-      }
+      # passwords = {
+      #   description = "domain passwords only accessible by this account"
+      # }
       shared-passwords = {
         description = "domain passwords shared with other accounts"
         policy = [
           local.domain_secret_policy_read,
         ]
       }
-      shared-config = {
-        description = "domain related config shared with other accounts"
-        policy = [
-          local.domain_secret_policy_read,
-        ]
-      }
+      # shared-config = {
+      #   description = "domain related config shared with other accounts"
+      #   policy = [
+      #     local.domain_secret_policy_read,
+      #   ]
+      # }
     }
   }
 }
