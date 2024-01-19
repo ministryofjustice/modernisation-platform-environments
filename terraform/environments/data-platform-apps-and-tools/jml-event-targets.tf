6e1b873ea3e6377a1366098d348f5ba5cbe97379
murad-ali-MoJ <murad.ali@justice.gov.uk>
2024-01-19 05:40:16 +0000

    event trigger sorted

--- terraform/environments/data-platform-apps-and-tools/jml-event-targets.tf
+++ terraform/environments/data-platform-apps-and-tools/jml-event-targets.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_target" "jml_lambda_trigger" {
   rule      = aws_cloudwatch_event_rule.jml_lambda_trigger.name
   target_id = "jml-lambda-trigger"
-  arn       = module.data_product_jml_lambda_execution.lambda_function_arn
+  arn       = module.data_platform_jml_extract.lambda_function_arn
 }
