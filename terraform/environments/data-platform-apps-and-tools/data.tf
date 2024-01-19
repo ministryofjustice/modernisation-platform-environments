8382ee42be95159792531ece1fd91ee016cf21b9
murad-ali-MoJ <murad.ali@justice.gov.uk>
2024-01-19 14:54:09 +0000

    add index 1

--- terraform/environments/data-platform-apps-and-tools/data.tf
+++ terraform/environments/data-platform-apps-and-tools/data.tf
@@ -88,7 +88,7 @@ data "aws_secretsmanager_secret_version" "openmetadata_entra_id_tenant_id" {
 ##################################################
 
 data "aws_secretsmanager_secret_version" "govuk_notify_api_key" {
-  secret_id = aws_secretsmanager_secret.govuk_notify_api_key[count.index].id
+  secret_id = aws_secretsmanager_secret.govuk_notify_api_key[1].id
 }
 
 data "aws_secretsmanager_secret_version" "email_secret" {
