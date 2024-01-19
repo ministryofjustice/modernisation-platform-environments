dda3bcd67f058d9c15a13120cf8ae120bba9f7a8
dms1981 <david.sibley@digital.justice.gov.uk>
2024-01-19 08:45:31 +0000

    Update terraform/environments/data-platform-compute/vpc.tf
    Co-authored-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-compute/vpc.tf
+++ terraform/environments/data-platform-compute/vpc.tf
@@ -87,7 +87,7 @@ module "vpc_endpoints" {
   }
 }
 
-resource "aws_security_group" "vpc-endpoints" {
+resource "aws_security_group" "vpc_endpoints" {
   description = "Security Group for controlling all VPC endpoint traffic"
   name        = format("%s-vpc-endpoint-sg", local.application_name)
   vpc_id      = module.vpc.vpc_id
