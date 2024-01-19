b969214af941dbdf6b02bfa3e16b9af9f74f56f6
murad-ali-MoJ <murad.ali@justice.gov.uk>
2024-01-19 06:05:30 +0000

    image added as arn

--- terraform/environments/data-platform-apps-and-tools/jml-event-targets.tf
+++ terraform/environments/data-platform-apps-and-tools/jml-event-targets.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_target" "jml_lambda_trigger" {
   rule      = aws_cloudwatch_event_rule.jml_lambda_trigger.name
   target_id = "jml-lambda-trigger"
-  arn       = module.jml_extract.data_platform_jml_extract.lambda_function_arn
+  arn       = "374269020027.dkr.ecr.eu-west-2.amazonaws.com/data-platform-jml-extract-lambda-ecr-repo:1.0.1"
 }
