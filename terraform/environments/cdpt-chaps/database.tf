feaa391b77da8dae360423a70d4068abdcb80e00
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-01-19 15:32:47 +0000

    add final snapshot

--- terraform/environments/cdpt-chaps/database.tf
+++ terraform/environments/cdpt-chaps/database.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "database" {
   password               = data.aws_secretsmanager_secret_version.db_password.secret_string
   vpc_security_group_ids = [aws_security_group.db.id]
   depends_on             = [aws_security_group.db]
-  snapshot_identifier    = "arn:aws:rds:eu-west-2:613903586696:snapshot:cdpt-dev-staging-snapshot-9-1-24"
+  # snapshot_identifier    = "arn:aws:rds:eu-west-2:613903586696:snapshot:chaps-prod-snapshot-2024-01-19"
   db_subnet_group_name   = aws_db_subnet_group.db.id
   final_snapshot_identifier = "${local.application_data.accounts[local.environment].db_instance_identifier}-db-snapshot"
   publicly_accessible    = true
