ffc709b516b64ed1b822f987f48716d26e90d1ee
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-01-22 14:35:38 +0000

    move resources
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/modules/grafana/team/variables.tf
+++ terraform/environments/observability-platform/modules/grafana/team/variables.tf
@@ -7,43 +7,8 @@ variable "identity_centre_team" {
 }
 
 variable "aws_accounts" {
-  type = map(any)
-}
-
-data "grafana_data_source" "cloudwatch" {
-  for_each = var.aws_accounts
-
-  name = "${each.key}-cloudwatch"
-}
-
-resource "grafana_data_source_permission" "cloudwatch" {
-  for_each = var.aws_accounts
-
-  datasource_id = data.grafana_data_source.cloudwatch[each.key].id
-
-  permissions {
-    team_id    = grafana_team.this.id
-    permission = "Query"
-  }
-}
-
-data "grafana_data_source" "xray" {
-  for_each = {
-    for name, account in var.aws_accounts : name => account if account.xray_enabled
-  }
-
-  name = "${each.key}-xray"
-}
-
-resource "grafana_data_source_permission" "xray" {
-  for_each = {
-    for name, account in var.aws_accounts : name => account if account.xray_enabled
-  }
-
-  datasource_id = data.grafana_data_source.xray[each.key].id
-
-  permissions {
-    team_id    = grafana_team.this.id
-    permission = "Query"
-  }
+  type = map(object({
+    cloudwatch_enabled = optional(bool)
+    xray_enabled       = optional(bool)
+  }))
 }
