96bb759d9aeea419b79a7f92e139defa93846ddf
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-01-22 11:03:36 +0000

    removing unnecessary permission

--- terraform/modules/baseline_presets/iam_policies.tf
+++ terraform/modules/baseline_presets/iam_policies.tf
@@ -36,7 +36,6 @@ locals {
     var.options.enable_shared_s3 ? local.iam_policy_statements_ec2.S3ReadSharedWrite : [],
     var.options.enable_ec2_reduced_ssm_policy ? local.iam_policy_statements_ec2.SSMManagedInstanceCoreReduced : [],
     var.options.enable_ec2_oracle_enterprise_managed_server ? local.iam_policy_statements_ec2.OracleEnterpriseManagedServer : [],
-    var.options.enable_hmpps_domain ? local.iam_policy_statements_ec2.HmppsDomainSecrets : [],
     var.options.iam_policy_statements_ec2_default
   ])
 
