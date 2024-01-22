af5e1f2c0567da3799cdabf5f35f8c22891a7288
George Taylor <george.taylor@digital.justice.gov.uk>
2024-01-22 17:16:46 +0000

    remove old resources

--- terraform/environments/delius-core/modules/environment_all_components/outputs.tf
+++ terraform/environments/delius-core/modules/environment_all_components/outputs.tf
@@ -6,9 +6,6 @@ output "ldap_efs_location" {
   value = module.efs.ldap_efs_location
 }
 
-output "ldap_efs_security_group_id" {
-  value = aws_security_group.ldap.id
-}
 
 output "acm_domains" {
   value = aws_acm_certificate.external
