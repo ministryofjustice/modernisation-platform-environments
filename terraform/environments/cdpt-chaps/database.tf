4064ec597dd1c70cc98202b5c426eec8f4aef883
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-01-22 14:08:59 +0000

    rename final snapshot

--- terraform/environments/cdpt-chaps/database.tf
+++ terraform/environments/cdpt-chaps/database.tf
@@ -3,18 +3,18 @@
 #------------------------------------------------------------------------------
 
 resource "aws_db_instance" "database" {
-  allocated_storage      = local.application_data.accounts[local.environment].db_allocated_storage
-  storage_type           = "gp2"
-  engine                 = "sqlserver-web"
-  engine_version         = "14.00.3381.3.v1"
-  instance_class         = local.application_data.accounts[local.environment].db_instance_class
-  identifier             = local.application_data.accounts[local.environment].db_instance_identifier
-  username               = local.application_data.accounts[local.environment].db_user
-  password               = aws_secretsmanager_secret_version.db_password.secret_string
-  vpc_security_group_ids = [aws_security_group.db.id]
-  depends_on             = [aws_security_group.db]
-  snapshot_identifier    = local.application_data.accounts[local.environment].db_snapshot_identifier
-  db_subnet_group_name   = aws_db_subnet_group.db.id
+  allocated_storage         = local.application_data.accounts[local.environment].db_allocated_storage
+  storage_type              = "gp2"
+  engine                    = "sqlserver-web"
+  engine_version            = "14.00.3381.3.v1"
+  instance_class            = local.application_data.accounts[local.environment].db_instance_class
+  identifier                = local.application_data.accounts[local.environment].db_instance_identifier
+  username                  = local.application_data.accounts[local.environment].db_user
+  password                  = aws_secretsmanager_secret_version.db_password.secret_string
+  vpc_security_group_ids    = [aws_security_group.db.id]
+  depends_on                = [aws_security_group.db]
+  snapshot_identifier       = local.application_data.accounts[local.environment].db_snapshot_identifier
+  db_subnet_group_name      = aws_db_subnet_group.db.id
   final_snapshot_identifier = "final-snapshot"
   publicly_accessible       = true
 }
