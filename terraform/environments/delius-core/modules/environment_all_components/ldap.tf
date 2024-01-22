cf7090682c2bd4dcea6eb9e3fb1dafb6968206e4
George Taylor <george.taylor@digital.justice.gov.uk>
2024-01-22 17:04:59 +0000

    patch

--- terraform/environments/delius-core/modules/environment_all_components/ldap.tf
+++ terraform/environments/delius-core/modules/environment_all_components/ldap.tf
@@ -38,7 +38,7 @@ module "nlb" {
   account_info   = var.account_info
 }
 
-module "ldap_backups" {
+module "ldap" {
 
   source = "../components/ldap"
 
@@ -49,32 +49,15 @@ module "ldap_backups" {
     aws.core-network-services = aws.core-network-services
   }
 
-  env_name       = var.env_name
-  app_name       = var.app_name
-  account_config = var.account_config
-  account_info   = var.account_info
-  ldap_config    = var.ldap_config
-  platform_vars  = var.platform_vars
-  tags           = local.tags
-}
-
-module "ldap_datasync" {
-
-  source = "../components/ldap"
-  providers = {
-    aws                       = aws
-    aws.bucket-replication    = aws
-    aws.core-vpc              = aws.core-vpc
-    aws.core-network-services = aws.core-network-services
-  }
-
-  env_name       = var.env_name
-  app_name       = var.app_name
-  ldap_config    = var.ldap_config
-  account_config = var.account_config
-  account_info   = var.account_info
-  tags           = local.tags
-  platform_vars  = var.platform_vars
+  env_name                     = var.env_name
+  app_name                     = var.app_name
+  account_config               = var.account_config
+  account_info                 = var.account_info
+  environment_config           = var.environment_config
+  ldap_config                  = var.ldap_config
+  platform_vars                = var.platform_vars
+  tags                         = local.tags
+  efs_datasync_destination_arn = module.efs.ldap_efs_location
 }
 
 # module "ldap_params" {
