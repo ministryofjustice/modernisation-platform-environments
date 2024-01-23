15679f7b348bdb0e90ce543d51f5bf5359bf1cc1
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-01-23 12:23:41 +0000

    exclude special characters from password

--- terraform/environments/cdpt-chaps/secrets.tf
+++ terraform/environments/cdpt-chaps/secrets.tf
@@ -5,8 +5,8 @@ resource "aws_secretsmanager_secret" "db_password" {
 }
 
 resource "random_password" "password_long" {
-  length = 32
-  override_special = "!@#$%&*()-_=+[]{}<>:?@/ "
+  length  = 32
+  special = false
 }
 
 resource "aws_secretsmanager_secret_version" "db_password" {
