811ab87fae6999860e3db8040f296eeb630e4fe2
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2024-01-23 15:08:44 +0000

    Remove hyphen from state name

--- terraform/environments/digital-prison-reporting/modules/domains/reload-pipeline/pipeline.tf
+++ terraform/environments/digital-prison-reporting/modules/domains/reload-pipeline/pipeline.tf
@@ -22,9 +22,9 @@ module "reload_pipeline" {
               "--dpr.stop.glue.instance.job.name" : var.glue_reporting_hub_cdc_jobname
             }
           },
-          "Next" : "Copy Data To Temp-Reload Bucket"
+          "Next" : "Copy Data To Temp Reload Bucket"
         },
-        "Copy Data to Temp-Reload Bucket" : {
+        "Copy Data to Temp Reload Bucket" : {
           "Type" : "Task",
           "Resource" : "arn:aws:states:::glue:startJobRun.sync",
           "Parameters" : {
@@ -158,15 +158,15 @@ module "reload_pipeline" {
               "--dpr.config.key" : var.domain
             }
           },
-          "Next" : "Empty Temp-Reload Bucket Data"
+          "Next" : "Empty Temp Reload Bucket Data"
         },
-        "Empty Temp-Reload Bucket Data" : {
+        "Empty Temp Reload Bucket Data" : {
           "Type" : "Task",
           "Resource" : "arn:aws:states:::glue:startJobRun.sync",
           "Parameters" : {
             "JobName" : var.glue_s3_data_deletion_job,
             "Arguments" : {
-              "--dpr.file.deletion.buckets" : "${var.s3_temp_reload_bucket_id}"
+              "--dpr.file.deletion.buckets" : var.s3_temp_reload_bucket_id
               "--dpr.config.key" : var.domain
             }
           },
