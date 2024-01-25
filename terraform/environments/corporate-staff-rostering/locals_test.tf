e891615bc6903a7b4c95170db38b9f3f918ae8d7
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-01-25 12:34:28 +0000

    remove test instance

--- terraform/environments/corporate-staff-rostering/locals_test.tf
+++ terraform/environments/corporate-staff-rostering/locals_test.tf
@@ -4,31 +4,7 @@ locals {
   # baseline config
   test_config = {
 
-    baseline_ec2_instances = {
-      test-2022 = {
-        config = merge(module.baseline_presets.ec2_instance.config.default, {
-          ami_name                      = "hmpps_windows_server_2022_release_2024-01-24T14-27-49.703Z"
-          ami_owner                     = "self"
-          ebs_volumes_copy_all_from_ami = false
-          instance_profile_policies     = concat(module.baseline_presets.ec2_instance.config.default.instance_profile_policies, ["CSRWebServerPolicy"])
-          user_data_raw                 = base64encode(file("./templates/user-data.yaml"))
-        })
-        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
-          vpc_security_group_ids = ["app", "domain", "jumpserver"]
-          instance_type          = "t3.medium"
-        })
-        cloudwatch_metric_alarms = local.ec2_cloudwatch_metric_alarms.windows
-        ebs_volumes = {
-          "/dev/sda1" = { type = "gp3", size = 192 } # minimum size has to be 128 due to snapshot sizes
-        }
-        tags = {
-          description = "Test AWS AMI Windows Server 2022"
-          os-type     = "Windows"
-          component   = "appserver"
-          server-type = "test-server"
-        }
-      }
-    }
+    baseline_ec2_instances = {}
 
     baseline_ec2_autoscaling_groups = {}
 
