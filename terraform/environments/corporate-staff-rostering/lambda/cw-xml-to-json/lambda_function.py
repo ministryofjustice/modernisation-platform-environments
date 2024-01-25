601cd688a235fd02850a0e954b1494b982346d97
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-01-25 17:43:27 +0000

    add missing property ref

--- terraform/environments/corporate-staff-rostering/lambda/cw-xml-to-json/lambda_function.py
+++ terraform/environments/corporate-staff-rostering/lambda/cw-xml-to-json/lambda_function.py
@@ -64,7 +64,7 @@ def lambda_handler(event, context):
     create_log_stream(dest_log_group, dest_log_stream)
 
     for log_event in log_data["logEvents"]:
-        new_log_message = xml_to_dict(log_event)
+        new_log_message = xml_to_dict(log_event["message"])
 
         logs_client.put_log_events(
             logGroupName=dest_log_group,
