866aa6e3d83ba803d823e980bf56da2016096e00
Ijaz Sultan <mohammed.ijazsultan@digital.justice.gov.uk>
2024-01-29 16:24:07 +0000

    dev machine provision

--- terraform/environments/corporate-staff-rostering/locals_development.tf
+++ terraform/environments/corporate-staff-rostering/locals_development.tf
@@ -66,6 +66,31 @@ locals {
           server-type = "test-server"
         }
       }
+
+      dev-win-2022 = {
+        # ami has unwanted ephemeral device, don't copy all the ebs_volumess
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name                      = "hmpps_windows_server_2022_release_2023-*"
+          availability_zone             = null
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = base64encode(file("./templates/windows_server_2022-user-data.yaml"))
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["rds-ec2s"]
+        })
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 1
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = {
+          description = "Windows Server 2022 for connecting to Azure domain"
+          os-type     = "Windows"
+          component   = "test"
+        }
+      }
     }
 
     baseline_ec2_instances = {
