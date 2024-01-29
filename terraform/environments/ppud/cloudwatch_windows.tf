1d51e64044fbba0e7a43191c016191309e971f60
Buckingham <XC01857@lumen.com>
2024-01-29 11:57:11 +0000

    Update_290124_4

--- terraform/environments/ppud/cloudwatch_windows.tf
+++ terraform/environments/ppud/cloudwatch_windows.tf
@@ -199,7 +199,7 @@ resource "aws_cloudwatch_log_group" "SQL-Server-Logs" {
   retention_in_days = 365
 }
 
-resource "aws_cloudwatch_log_group" "Windows_Defender-Logs" {
+resource "aws_cloudwatch_log_group" "Windows-Defender-Logs" {
   count             = local.is-production == true ? 1 : 0
   name              = "Windows-Defender-Logs"
   retention_in_days = 365
