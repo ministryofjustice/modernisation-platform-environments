835eb313758021dfd5d4358ecd665cdaa648b07f
George Taylor <george.taylor@digital.justice.gov.uk>
2024-01-30 16:57:18 +0000

    image builder policies

---
+++ terraform/environments/delius-core/image_builder.tf
@@ -0,0 +1,83 @@
+data "aws_iam_policy_document" "image_builder_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["core-shared-services-production"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "image_builder_assume_role" {
+  policy = data.aws_iam_policy_document.image_builder_assume_role.json
+}
+
+resource "aws_iam_role" "EC2ImageBuilderDistributionCrossAccountRole" {
+  name               = "ImageBuilderLaunchTemplateRole"
+  assume_role_policy = aws_iam_policy.image_builder_assume_role.arn
+}
+
+resource "aws_iam_policy_attachment" "image_builder_template" {
+  name       = "Ec2ImageBuilderCrossAccountDistributionAccess"
+  roles      = [aws_iam_role.EC2ImageBuilderDistributionCrossAccountRole.name]
+  policy_arn = "arn:aws:iam::aws:policy/Ec2ImageBuilderCrossAccountDistributionAccess"
+}
+
+data "aws_iam_policy_document" "image_builder_kms" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:RevokeGrant"
+    ]
+    resources = [
+      data.aws_kms_key.general_shared.arn,
+      data.aws_kms_key.ebs_shared.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "image_builder_kms" {
+  policy = data.aws_iam_policy_document.image_builder_kms.json
+}
+
+resource "aws_iam_policy_attachment" "image_builder_kms" {
+  name       = "BusinessUnitKmsCmkPolicy"
+  roles      = [aws_iam_role.EC2ImageBuilderDistributionCrossAccountRole.name]
+  policy_arn = aws_iam_policy.image_builder_kms.arn
+}
+
+data "aws_iam_policy_document" "ImageBuilderLaunchTemplatePolicy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:CreateLaunchTemplateVersion",
+      "ec2:ModifyLaunchTemplate",
+      "ec2:DescribeLaunchTemplates"
+    ]
+    resources = [
+      "*"
+    ]
+    condition {
+      test     = "StringEquals"
+      values   = ["EC2 Image Builder"]
+      variable = "aws:ResourceTag/CreatedBy"
+    }
+  }
+}
+
+resource "aws_iam_policy" "ImageBuilderLaunchTemplatePolicy" {
+  policy = data.aws_iam_policy_document.image_builder_kms.json
+}
+
+resource "aws_iam_policy_attachment" "ImageBuilderLaunchTemplatePolicy" {
+  name       = "ImageBuilderLaunchTemplatePolicy"
+  roles      = [aws_iam_role.EC2ImageBuilderDistributionCrossAccountRole.name]
+  policy_arn = aws_iam_policy.ImageBuilderLaunchTemplatePolicy.arn
+}
