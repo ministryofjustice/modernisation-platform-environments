96dae22e7f3417731c45471ca5ac0f1093583025
Silviana Horga <silviana.horga@digital.justice.gov.uk>
2024-01-30 15:02:23 +0000

    Comment out AD related SGs

--- terraform/modules/fsx/security-groups-fsx-integration.tf
+++ terraform/modules/fsx/security-groups-fsx-integration.tf
@@ -58,22 +58,22 @@ resource "aws_security_group_rule" "fsx_integration_sg_egress_to_fsx_sg" {
   description              = "egress ALL traffic to FSx Security Group"
 }
 
-resource "aws_security_group_rule" "fsx_integration_sg_ingress_from_ad_sg" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = -1
-  source_security_group_id = var.fsx.active_directory_security_group_id
-  security_group_id        = aws_security_group.fsx_integration.id
-  description              = "ingress ALL traffic from AD Security Group"
-}
-
-resource "aws_security_group_rule" "fsx_integration_sg_egress_to_ad_sg" {
-  type                     = "egress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = -1
-  source_security_group_id = var.fsx.active_directory_security_group_id
-  security_group_id        = aws_security_group.fsx_integration.id
-  description              = "egress ALL traffic to AD Security Group"
-}
+#resource "aws_security_group_rule" "fsx_integration_sg_ingress_from_ad_sg" {
+#  type                     = "ingress"
+#  from_port                = 0
+#  to_port                  = 0
+#  protocol                 = -1
+#  source_security_group_id = var.fsx.active_directory_security_group_id
+#  security_group_id        = aws_security_group.fsx_integration.id
+#  description              = "ingress ALL traffic from AD Security Group"
+#}
+#
+#resource "aws_security_group_rule" "fsx_integration_sg_egress_to_ad_sg" {
+#  type                     = "egress"
+#  from_port                = 0
+#  to_port                  = 0
+#  protocol                 = -1
+#  source_security_group_id = var.fsx.active_directory_security_group_id
+#  security_group_id        = aws_security_group.fsx_integration.id
+#  description              = "egress ALL traffic to AD Security Group"
+#}
