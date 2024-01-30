7712c20346dad5dac268bca55d976ef07fb35649
Silviana Horga <silviana.horga@digital.justice.gov.uk>
2024-01-30 14:32:19 +0000

    Resolve vars error

--- terraform/modules/fsx/locals.tf
+++ terraform/modules/fsx/locals.tf
@@ -1,19 +1,19 @@
 locals {
   common = {
-    environment_name = var.environment_name
-    subnet_ids       = var.subnet_ids
-    tags             = var.tags
-    vpc_id           = var.vpc_id
-    region           = var.region
+    environment_name = var.common.environment_name
+    subnet_ids       = var.common.subnet_ids
+    tags             = var.common.tags
+    vpc_id           = var.common.vpc_id
+    region           = var.common.region
   }
   fsx = {
     automatic_backup_retention_days    = 7
-    common_name                        = var.environment_name
+    common_name                        = var.fsx.environment_name
     copy_tags_to_backups               = false
     daily_automatic_backup_start_time  = "03:00"
     deployment_type                    = "MULTI_AZ_1"
-    filesystem_name                    = var.environment_name
-    storage_capacity                   = var.storage_capacity
-    throughput_capacity                = var.throughput_capacity
+    filesystem_name                    = var.fsx.environment_name
+    storage_capacity                   = var.fsx.storage_capacity
+    throughput_capacity                = var.fsx.throughput_capacity
   }
 }
