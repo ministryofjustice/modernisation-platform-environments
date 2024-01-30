60937091c91f7a1d8c990283dd9f3760ab7dc404
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-01-30 11:56:31 +0000

    LAWS-3608: Reverting to what it was before
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/maat/locals.tf
+++ terraform/environments/maat/locals.tf
@@ -18,26 +18,4 @@ locals {
   domain_record_sub  = [for k, v in local.domain_types : v.record if k != "modernisation-platform.service.justice.gov.uk"]
   domain_type_main   = [for k, v in local.domain_types : v.type if k == "modernisation-platform.service.justice.gov.uk"]
   domain_type_sub    = [for k, v in local.domain_types : v.type if k != "modernisation-platform.service.justice.gov.uk"]
-
-  task_definition = templatefile("td.json", 
-    {
-    docker_image_tag            = local.application_data.accounts[local.environment].docker_image_tag
-    region                      = local.application_data.accounts[local.environment].region
-    sentry_env                  = local.environment
-    maat_orch_base_url          = local.application_data.accounts[local.environment].maat_orch_base_url
-    maat_ccp_base_url           = local.application_data.accounts[local.environment].maat_ccp_base_url
-    maat_orch_oauth_url         = local.application_data.accounts[local.environment].maat_orch_oauth_url
-    maat_ccc_oauth_url          = local.application_data.accounts[local.environment].maat_ccc_oauth_url
-    maat_cma_endpoint_auth_url  = local.application_data.accounts[local.environment].maat_cma_endpoint_auth_url
-    maat_ccp_endpoint_auth_url  = local.application_data.accounts[local.environment].maat_ccp_endpoint_auth_url
-    maat_db_url                 = local.application_data.accounts[local.environment].maat_db_url
-    maat_ccc_base_url           = local.application_data.accounts[local.environment].maat_ccc_base_url
-    maat_caa_oauth_url          = local.application_data.accounts[local.environment].maat_caa_oauth_url
-    maat_bc_endpoint_url        = local.application_data.accounts[local.environment].maat_bc_endpoint_url
-    maat_mlra_url               = local.application_data.accounts[local.environment].maat_mlra_url
-    maat_caa_base_url           = local.application_data.accounts[local.environment].maat_caa_base_url
-    maat_cma_url                = local.application_data.accounts[local.environment].maat_cma_url
-    ecr_url                     = "${local.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/maat"
-    }
-  )  
 }
