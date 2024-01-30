2ab67e1c361812be56df4bf8369c93545c96cbfa
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-01-30 11:52:45 +0000

    LAWS-3608: Checking if encoding is the problem
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/maat/maat-ecs.tf
+++ terraform/environments/maat/maat-ecs.tf
@@ -259,26 +259,8 @@ resource "aws_ecs_task_definition" "maat_task_definition" {
   requires_compatibilities = ["EC2"]
   execution_role_arn       = aws_iam_role.ec2_instance_role.arn
   task_role_arn            = aws_iam_role.ec2_instance_role.arn
-
-  container_definitions = templatefile("td.json", {
-    docker_image_tag            = local.application_data.accounts[local.environment].docker_image_tag
-    region                      = local.application_data.accounts[local.environment].region
-    # sentry_env                  = local.environment
-    maat_orch_base_url          = local.application_data.accounts[local.environment].maat_orch_base_url
-    maat_ccp_base_url           = local.application_data.accounts[local.environment].maat_ccp_base_url
-    maat_orch_oauth_url         = local.application_data.accounts[local.environment].maat_orch_oauth_url
-    maat_ccc_oauth_url          = local.application_data.accounts[local.environment].maat_ccc_oauth_url
-    maat_cma_endpoint_auth_url  = local.application_data.accounts[local.environment].maat_cma_endpoint_auth_url
-    maat_ccp_endpoint_auth_url  = local.application_data.accounts[local.environment].maat_ccp_endpoint_auth_url
-    maat_db_url                 = local.application_data.accounts[local.environment].maat_db_url
-    maat_ccc_base_url           = local.application_data.accounts[local.environment].maat_ccc_base_url
-    maat_caa_oauth_url          = local.application_data.accounts[local.environment].maat_caa_oauth_url
-    maat_bc_endpoint_url        = local.application_data.accounts[local.environment].maat_bc_endpoint_url
-    maat_mlra_url               = local.application_data.accounts[local.environment].maat_mlra_url
-    maat_caa_base_url           = local.application_data.accounts[local.environment].maat_caa_base_url
-    maat_cma_url                = local.application_data.accounts[local.environment].maat_cma_url
-    ecr_url                     = "${local.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/maat"
-  })
+  
+  container_definitions = local.task_definition
 
   tags = merge(
     local.tags,
