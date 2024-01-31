d3e1f5530d08494180dd77a954ed19f33d33ee6b
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2024-01-31 14:25:42 +0000

    Up memory limit to account for filesize

--- terraform/environments/data-platform-apps-and-tools/lambda-functions.tf
+++ terraform/environments/data-platform-apps-and-tools/lambda-functions.tf
@@ -11,6 +11,7 @@ module "jml_extract_lambda" {
   function_name = "data_platform_jml_extract"
   description   = "Generates a JML report and sends it to JMLv4"
   package_type  = "Image"
+  memory_size   = 512
   image_uri     = "374269020027.dkr.ecr.eu-west-2.amazonaws.com/data-platform-jml-extract-lambda-ecr-repo:1.0.3"
 
   environment_variables = {
