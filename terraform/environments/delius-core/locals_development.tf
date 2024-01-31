183b619ca98eac6f14a84674a9b961b609634fff
George Taylor <george.taylor@digital.justice.gov.uk>
2024-01-31 18:06:09 +0000

    refactor after merge of NIT-1052-delius-core-setup-test-environment-account

--- terraform/environments/delius-core/locals_development.tf
+++ terraform/environments/delius-core/locals_development.tf
@@ -223,13 +223,13 @@ locals {
   ]
 
   gdpr_config_dev = {
-    api_image_tag = try(local.gdpr_config_lower_environments.api_image_tag, "REPLACE")
-    ui_image_tag = try(local.gdpr_config_lower_environments.ui_image_tag, "REPLACE")
+    api_image_tag = "REPLACE"
+    ui_image_tag  = "REPLACE"
   }
 
   merge_config_dev = {
-    api_image_tag = try(local.merge_config_lower_environments.api_image_tag, "REPLACE")
-    ui_image_tag = try(local.merge_config_lower_environments.ui_image_tag, "REPLACE")
+    api_image_tag = "REPLACE"
+    ui_image_tag  = "REPLACE"
   }
 
   weblogic_config_dev = {
