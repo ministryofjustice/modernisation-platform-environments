a4f97503df28d5766669cc3b83ec655d7e7d1007
Seb Norris <sebastian@automationlogic.com>
2024-02-01 15:54:05 +0000

    pass in https listener

--- terraform/environments/delius-core/modules/environment_all_components/merge_ui_service.tf
+++ terraform/environments/delius-core/modules/environment_all_components/merge_ui_service.tf
@@ -17,6 +17,7 @@ module "merge_ui_service" {
   ingress_security_groups = []
   tags = var.tags
   microservice_lb_arn = aws_lb.delius_core_frontend.arn
+  microservice_lb_https_listener_arn = aws_lb_listener.listener_https.arn
   platform_vars = var.platform_vars
   container_image = "${var.platform_vars.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/delius-core-merge-ui-ecr-repo:${var.merge_config.ui_image_tag}"
   account_config = var.account_config
