9b3033225a15657bb97ebbea5c6732473fa47141
Buckingham <XC01857@lumen.com>
2024-02-01 11:26:28 +0000

    Update_010224_2

--- terraform/environments/ppud/sns.tf
+++ terraform/environments/ppud/sns.tf
@@ -19,20 +19,20 @@ resource "aws_sns_topic_subscription" "cw_subscription" {
 
 ##### Cloud Watch UAT ######
 
-resource "aws_sns_topic" "cw_alerts" {
+resource "aws_sns_topic" "cw_uat_alerts" {
   count = local.is-preproduction == true ? 1 : 0
   name  = "ppud-uat-cw-alerts"
 }
 
-resource "aws_sns_topic_policy" "sns_policy" {
+resource "aws_sns_topic_policy" "sns_uat_policy" {
   count  = local.is-preproduction == true ? 1 : 0
-  arn    = aws_sns_topic.cw_alerts[0].arn
+  arn    = aws_sns_topic.cw_uat_alerts[0].arn
   policy = data.aws_iam_policy_document.sns_topic_policy_ec2cw[0].json
 }
 
-resource "aws_sns_topic_subscription" "cw_subscription" {
+resource "aws_sns_topic_subscription" "cw_uat_subscription" {
   count     = local.is-preproduction == true ? 1 : 0
-  topic_arn = aws_sns_topic.cw_alerts[0].arn
+  topic_arn = aws_sns_topic.cw_uat_alerts[0].arn
   protocol  = "email"
   endpoint  = "PPUDAlerts@colt.net"
 }
