9aaf63002a388f6dcb1973a8d1e9b95c87093a6e
Robert Sweetman <robertiansweetman@gmail.com>
2024-02-01 16:49:59 +0000

    burn down pd-rds-1-b, apply domain join automatically and move fwd (#4800)
    * burn down pd-rds-1-b, apply domain join automatically and move fwd
    
    * fix path issue
    
    * swap lb target group

--- terraform/environments/hmpps-domain-services/locals_production.tf
+++ terraform/environments/hmpps-domain-services/locals_production.tf
@@ -83,9 +83,11 @@ locals {
           description = "Remote Desktop Gateway for hmpp.noms.root domain"
         })
       })
-      pd-rds-1-b = merge(local.rds_ec2_instance, {
+      pd-rds-1-a = merge(local.rds_ec2_instance, {
         config = merge(local.rds_ec2_instance.config, {
-          availability_zone = "eu-west-2b"
+          availability_zone         = "eu-west-2a"
+          user_data_raw             = base64encode(file("./templates/user-data-domain-join.yaml"))
+          instance_profile_policies = concat(local.rds_ec2_instance.config.instance_profile_policies, ["SSMPolicy"])
         })
         instance = merge(local.rds_ec2_instance.instance, {
           instance_type = "t3.large"
@@ -107,7 +109,7 @@ locals {
           })
           pd-rds-1-https = merge(local.rds_target_groups.https, {
             attachments = [
-              { ec2_instance_name = "pd-rds-1-b" },
+              { ec2_instance_name = "pd-rds-1-a" },
             ]
           })
         }
