234fac798ba6b17f9c3129f78597312c9bcc19ec
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-02-02 10:53:18 +0000

    missing locals

--- terraform/environments/cdpt-ifs/locals.tf
+++ terraform/environments/cdpt-ifs/locals.tf
@@ -1,3 +1,6 @@
 #### This file can be used to store locals specific to the member account ####
+locals {
 
 ecr_url = "${local.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/cdpt-ifs-ecr-repo"
+
+}
