6a5e44a7fd161572a9377dbbfe0e739af07f6abb
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-02-02 10:49:22 +0000

    task definition

--- terraform/environments/cdpt-ifs/ecs.tf
+++ terraform/environments/cdpt-ifs/ecs.tf
@@ -1,3 +1,63 @@
+data "aws_ecs_task_definition" "task_definition" {
+  task_definition = aws_ecs_task_definition.ifs_task_definition.family
+  depends_on      = [aws_ecs_task_definition.ifs_task_definition]
+}
+
+resource "aws_ecs_task_definition" "ifs_task_definition" {
+  family                   = "ifsFamily"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.app_execution.arn
+  task_role_arn            = aws_iam_role.app_task.arn
+  container_definitions = jsonencode([
+    {
+      name      = "${local.application_name}-container"
+      image     = "${local.ecr_url}:${local.application_data.accounts[local.environment].environment_name}"
+      cpu       = 1024
+      memory    = 1024
+      essential = true
+      portMappings = [
+        {
+          containerPort = local.application_data.accounts[local.environment].container_port
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group         = "${local.application_name}-ecs",
+          awslogs-region        = "eu-west-2",
+          awslogs-stream-prefix = local.application_name
+        }
+      }
+      environment = [
+#        {
+#          name  = "RDS_HOSTNAME"
+#          value = "${aws_db_instance.database.address}"
+#        },
+#        {
+#          name  = "RDS_USERNAME"
+#          value = "${aws_db_instance.database.username}"
+#        },
+#        {
+#          name  = "DB_NAME"
+#          value = "${local.application_data.accounts[local.environment].db_name}"
+#        },
+        {
+         name  = "CLIENT_ID"
+          value = "${local.application_data.accounts[local.environment].client_id}"
+        }
+      ]
+#      secrets = [
+#        {
+#          name : "RDS_PASSWORD",
+#          valueFrom : aws_secretsmanager_secret_version.db_password.arn
+#        }
+#      ]
+    }
+  ])
+}
+
 
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "${local.application_name}-ecs-cluster"
--- terraform/environments/cdpt-ifs/locals.tf
+++ terraform/environments/cdpt-ifs/locals.tf
@@ -1,1 +1,3 @@
 #### This file can be used to store locals specific to the member account ####
+
+ecr_url = "${local.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/cdpt-ifs-ecr-repo"
