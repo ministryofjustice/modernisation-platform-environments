49caecb217421f06606145add225f34148ba7406
George Taylor <george.taylor@digital.justice.gov.uk>
2024-02-02 18:35:27 +0000

    pass in platform vars and account config

--- terraform/environments/delius-core/modules/components/oracle_db_shared/locals.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/locals.tf
@@ -1,6 +1,6 @@
 locals {
-    secret_prefix           = join("-", [lookup(var.tags, "environment-name", null), lookup(var.tags, "delius-environment", null), lookup(var.tags, "application", null)])
-    dba_secret_name         = "${local.secret_prefix}-dba-passwords"
-    application_secret_name =  "${local.secret_prefix}-application-passwords"
-    oem_account_id          = module.environment.cross_account_secret_account_ids.hmpps_oem
+  secret_prefix           = join("-", [lookup(var.tags, "environment-name", null), lookup(var.tags, "delius-environment", null), lookup(var.tags, "application", null)])
+  dba_secret_name         = "${local.secret_prefix}-dba-passwords"
+  application_secret_name = "${local.secret_prefix}-application-passwords"
+  oem_account_id          = module.environment.cross_account_secret_account_ids.hmpps_oem
 }
