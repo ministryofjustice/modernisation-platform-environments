094919f10234476171c75945433e336897d3bcc8
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-02-05 16:43:25 +0000

    csr: define log groups for ssm docs (#4859)

--- terraform/environments/corporate-staff-rostering/locals.tf
+++ terraform/environments/corporate-staff-rostering/locals.tf
@@ -36,11 +36,14 @@ locals {
   }
 
   baseline_acm_certificates = {}
-  baseline_cloudwatch_log_groups = {
-    cwagent-windows-application = {
-      retention_in_days = 30
+  baseline_cloudwatch_log_groups = merge(
+    local.ssm_doc_cloudwatch_log_groups,
+    {
+      cwagent-windows-application = {
+        retention_in_days = 30
+      }
     }
-  }
+  )
   baseline_cloudwatch_log_metric_filters = {}
   baseline_ec2_autoscaling_groups        = {}
   baseline_ec2_instances                 = {}
