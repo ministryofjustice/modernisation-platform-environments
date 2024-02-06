5aa1fa5b4f673b987f2ce2219f5d3cbbfeb2ee64
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-02-06 17:11:48 +0000

    fix: wrap key in double quotes

--- terraform/environments/corporate-staff-rostering/lambda/cw-xml-to-json/lambda_function.py
+++ terraform/environments/corporate-staff-rostering/lambda/cw-xml-to-json/lambda_function.py
@@ -64,7 +64,7 @@ def lambda_handler(event, context):
     create_log_stream(dest_log_group, dest_log_stream)
 
     for log_event in log_data["logEvents"]:
-        new_log_message = {"_": {instanceId: log_data["logStream"]}} | xml_to_dict(
+        new_log_message = {"_": {"instanceId": log_data["logStream"]}} | xml_to_dict(
             log_event["message"]
         )
 
