c5a91cd891e74f009539056fbc30e9443c8e55bb
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-02-06 11:15:19 +0000

    ELM-1463 Refactor sftp server user code generation (#4847)
    * Refactor sftp server user code generation
    
    Now instead of hardcoding in a dev account and a user account, the
    landing_zone module takes a list of maps where each map is a unique user
    give access to the server and contains all details needed
    
    * Update module names
    
    * ensure security groups get unique names
    
    Previous code meant that passing e.g "dev" as an account to create meant
    terraform would be trying to create 3 identically names resourcess for
    different servers
    
    * fix typo of doom
    
    * Update user_accounts variable type
    
    * will a-z variables.tf file
    
    * Remove whitespace
    
    * ensure generate unique iam role names
    
    previous code meant it was creating duplicate named roles for dev on different servers

---
+++ terraform/environments/electronic-monitoring-data/modules/landing_zone/landing_zone_user/variables.tf
@@ -0,0 +1,22 @@
+variable "landing_bucket" {
+  description = "The landing bucket that data is placed in"
+}
+
+variable "ssh_keys" {
+  description = "The public ssh key for the SFTP server"
+  type        = list(string)
+}
+
+variable "supplier" {
+  description = "The name of the supplier the SFTP server is for"
+  type        = string
+}
+
+variable "transfer_server" {
+  description = "The SFTP server"
+}
+
+variable "user_name" {
+  description = "The user name for the SFTP server account"
+  type        = string
+}
