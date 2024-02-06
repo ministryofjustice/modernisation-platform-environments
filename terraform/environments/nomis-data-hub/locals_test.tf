fd1b22b6e766f13c413fccd4318d8553865c85bf
Antony Gowland <36728441+antonygowland@users.noreply.github.com>
2024-02-06 14:04:03 +0000

    DSOS-2525:remove the old test vms (#4873)

--- terraform/environments/nomis-data-hub/locals_test.tf
+++ terraform/environments/nomis-data-hub/locals_test.tf
@@ -2,27 +2,11 @@ locals {
   test_config = {
 
     baseline_secretsmanager_secrets = {
-      "/ndh/t1"   = local.ndh_secretsmanager_secrets
-      "/ndh/t2"   = local.ndh_secretsmanager_secrets
-      "/ndh/test" = local.ndh_secretsmanager_secrets
+      "/ndh/t1" = local.ndh_secretsmanager_secrets
+      "/ndh/t2" = local.ndh_secretsmanager_secrets
     }
 
     baseline_iam_policies = {
-      Ec2TestPolicy = {
-        description = "Permissions required for Test EC2s"
-        statements = [
-          {
-            effect = "Allow"
-            actions = [
-              "secretsmanager:GetSecretValue",
-            ]
-            resources = [
-              "arn:aws:secretsmanager:*:*:secret:/ndh/test/*",
-            ]
-          }
-        ]
-      }
-
       Ec2t1Policy = {
         description = "Permissions required for t1 EC2s"
         statements = [
@@ -58,30 +42,6 @@ locals {
 
       test-management-server-2022 = merge(local.management_server_2022, {
         tags = merge(local.management_server_2022.tags, {
-          nomis-data-hub-environment = "test"
-        })
-      })
-
-      test-ndh-app-a = merge(local.ndh_app_a, {
-        config = merge(local.ndh_app_a.config, {
-          instance_profile_policies = concat(local.ndh_app_a.config.instance_profile_policies, [
-            "Ec2TestPolicy",
-          ])
-        })
-        tags = merge(local.ndh_app_a.tags, {
-          os-type                    = "Linux"
-          nomis-data-hub-environment = "test"
-        })
-      })
-
-      test-ndh-ems-a = merge(local.ndh_ems_a, {
-        config = merge(local.ndh_ems_a.config, {
-          instance_profile_policies = concat(local.ndh_ems_a.config.instance_profile_policies, [
-            "Ec2TestPolicy",
-          ])
-        })
-        tags = merge(local.ndh_ems_a.tags, {
-          os-type                    = "Linux"
           nomis-data-hub-environment = "test"
         })
       })
