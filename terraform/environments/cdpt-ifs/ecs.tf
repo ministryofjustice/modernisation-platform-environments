867436a62869f3e5ab1dc61aa42d071be15e82b0
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-02-06 12:52:10 +0000

    add ecs service security group

--- terraform/environments/cdpt-ifs/ecs.tf
+++ terraform/environments/cdpt-ifs/ecs.tf
@@ -438,3 +438,24 @@ resource "aws_autoscaling_group" "cluster-scaling-group" {
     }
   }
 }
+
+
+resource "aws_security_group" "ecs_service" {
+  name_prefix = "ecs-service-sg-"
+  vpc_id      = data.aws_vpc.shared.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    description     = "Allow traffic on port 80 from load balancer"
+    security_groups = [aws_security_group.ifs_lb_sc.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
