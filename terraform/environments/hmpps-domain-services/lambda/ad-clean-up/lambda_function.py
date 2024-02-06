e05f6411c0184dd30d4b6da721599a37cc770485
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2024-02-06 17:13:57 +0000

    all variables added for python function

--- terraform/environments/hmpps-domain-services/lambda/ad-clean-up/lambda_function.py
+++ terraform/environments/hmpps-domain-services/lambda/ad-clean-up/lambda_function.py
@@ -1,16 +1,10 @@
 import json
 import boto3 
-from ldap3 import Server, Connection, ALL
+from ldap3 import Server, Connection, SUBTREE
 
 # checks for objects within active directory
-def check_ad_for_object(hostname, domain_fqdn, domain_name):
-    '''
-    This function takes the following variables:
-    hostname
-    domain_fqdn
-    domain_account 
-    '''
-    
+def check_ad_for_object(hostname, domain_fqdn, domain_name, search_base):
+
     # create a secrets manager connection
     secrets_manager = boto3.client('secretsmanager')
     
@@ -21,31 +15,29 @@ def check_ad_for_object(hostname, domain_fqdn, domain_name):
 
     # parse the JSON format secret data
     secret_json = json.loads(secret_data)
-    ad_password = secret_json['aws-lambda'] # update
+    ad_password = secret_json.get('svc_join_domain')
     
     # domain connection details
     domain_controller = Server(f'{domain_fqdn}:389')
     ad_username = rf'{domain_name}\aws-lambda'
     
     with Connection(Server, user=ad_username, password=ad_password, auto_bind=True) as conn:
-        search_base = 'ou=Managed-Windows-Servers,ou=Computers,dc=azure,dc=noms,dc=root'
+        ad_search = search_base
         search_filter = f'(sAMAccountName={hostname})'
-        
-        search_result = conn.search(search_base, search_filter)
+        # subtree for recursive search through defined OU
+        search_result = conn.search(ad_search, search_filter, SUBTREE)
         print(search_result)
-        status = 1 # default to "fail"
 
         if conn.entries:
             # Get the distinguished name (DN) of the found object
             object_dn = conn.entries[0].entry_dn
             print(object_dn)
-            print(f"The object {hostname} is present in Active Directory and will be deleted.")
-            # action removed during testing
-            # conn.delete(object_dn) 
-            status = 0
+            print(f"The object {object_dn} is present in Active Directory and will be deleted...")
+            # conn.delete(object_dn) # action removed during testing
+            return 0 # success status
         else:
-            print(f"The object {hostname} is not found in Active Directory - no further action taken.")
-        return status
+            print(f"The terminated server object {hostname} was not found in Active Directory - no further action taken.")
+            return 1 # object not found status
 
 # function to iterate through instance tags
 def get_tag_value(tags, key):
@@ -58,12 +50,16 @@ def get_tag_value(tags, key):
 def determine_domain(environment_tag):
     domain_info = {}
     if "development" in environment_tag.split('-') or "test" in environment_tag.split('-'):
+        domain_info['domain_type'] = "dev/test"
         domain_info['domain_name'] = "azure"
         domain_info['domain_fqdn'] = "azure.noms.root"
+        domain_info['search_base'] = "ou=Managed-Windows-Servers,ou=Computers,dc=azure,dc=noms,dc=root"
         # domain_info['account'] = "hmpps-domain-services-test"
     elif "preproduction" in environment_tag.split('-') or "production" in environment_tag.split('-'):
+        domain_info['domain_type'] = "preprod/prod"
         domain_info['domain_name'] = "hmpp"
         domain_info['domain_fqdn'] = "azure.hmpp.root"
+        domain_info['search_base'] = "ou=MEMBER_SERVERS,dc=azure,dc=hmpp,dc=root"
         # domain_info['account'] = "hmpps-domain-services-production"
     else:
         print("Unexpected environment-name tag. Aborting lambda function...")
@@ -81,24 +77,27 @@ def lambda_handler(event, context):
         response = ec2.describe_instances(InstanceIds=[instance_id])
         # return the tags associated with the terminated instance
         tags = response['Reservations'][0]['Instances'][0]['Tags']
-        
-        # terminated instance server-name value
+        # terminated instance server-name value, same as hostname
         resource_name = 'server-name'
-        hostname = get_tag_value(tags, resource_name)        
         
-        # terminated instance environment-name value
+        # obtain the hostame for the terminated server
+        hostname = get_tag_value(tags, resource_name)
+        print(f"Server hostname is: {hostname}")        
+        
+        # obtain terminated instance environment-name value
         resource_environment = 'environment-name'
         environment_tag = get_tag_value(tags, resource_environment)
 
-        # determine correct domain variables
+        # determine appropriate domain variables
         domain = determine_domain(environment_tag)
-        print("Domain address: {}".format(domain['domain_fqdn']))
+        print("Domain address: {}".format(domain['domain_type']))
 
-        # pass hostname and domain fqdn into AD function
+        # pass hostname and domain variables into AD oject deletion function
         if hostname is not None and domain is not None:
-            check_ad_for_object(hostname, domain['domain_fqdn'], domain['domain_name'])
+            check_ad_for_object(hostname, domain['domain_fqdn'], domain['domain_name'], domain['search_base'])
+            print(f"The Active Directory object {hostname} has been deleted.")
         else:
-            print(f"The tag '{resource_name}' was not found for the instance.")
+            print(f"The '{resource_name}' tag was not found for the terminated instance.")
     
     # 200 http response lambda run successful
     return {
