70ff81bdf7be406988c144b8ecd51580ecc92c10
wullub <william@automationlogic.com>
2024-02-07 16:26:51 +0000

    Update locals_development.tf

--- terraform/environments/oasys/locals_development.tf
+++ terraform/environments/oasys/locals_development.tf
@@ -4,7 +4,7 @@ locals {
   development_baseline_presets_options = {
     sns_topics = {
       pagerduty_integrations = {
-        dso_pagerduty               = "oasys_nonprod_alarms"
+        # dso_pagerduty               = "oasys_nonprod_alarms"
     #     dba_pagerduty               = "hmpps_shef_dba_non_prod"
     #     dba_high_priority_pagerduty = "hmpps_shef_dba_non_prod"
       }
