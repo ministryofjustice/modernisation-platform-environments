14d4a92536f3a45cf5ae1648a6cb4e64fb6f260a
George Taylor <george.taylor@digital.justice.gov.uk>
2024-02-07 15:35:47 +0000

    Update bcs.tf

--- terraform/environments/delius-mis/bcs.tf
+++ terraform/environments/delius-mis/bcs.tf
@@ -8,24 +8,20 @@ locals {
   bcs_instance_count = 3
 
   bcs_instance_ebs_volumes_config = {
-    encrypted  = true
-    kms_key_id = data.aws
+    "/dev/xvdf" = { label = "data", size = 300 }
+
   }
 
-  bcs_instance_volumes = {
-    root = {
-      device_name = "/dev/sda1"
-      volume_size = 8
-      volume_type = "gp2"
-    }
+  bcs_instance_ebs_volumes = {
     data = {
-      device_name = "/dev/sdb"
-      volume_size = 100
-      volume_type = "gp2"
+      iops       = 1000
+      throughput = 125
+      type       = "gp3"
+      total_size = 500
     }
   }
 
-  bcs_
+  bcs_instance_profile_policies = []
 
   bcs_instance_config = {
     associate_public_ip_address  = false
@@ -33,6 +29,7 @@ locals {
     disable_api_stop             = false
     instance_type                = "t3.micro"
     metadata_endpoint_enabled    = "enabled"
+    key_name                     = null
     metadata_options_http_tokens = "required"
     monitoring                   = true
     ebs_block_device_inline      = true
@@ -56,7 +53,7 @@ module "instance" {
     aws.core-vpc = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
   }
 
-  name = "${local.application_name}-bcs-{count.index + 1}"
+  name = "${local.application_name}-bcs-${count.index + 1}"
 
   ami_name                      = "delius-core-db"
   ami_owner                     = "self"
@@ -64,7 +61,7 @@ module "instance" {
   ebs_kms_key_id                = data.aws_kms_key.ebs_shared.id
   ebs_volumes_copy_all_from_ami = true
   ebs_volume_config             = local.bcs_instance_ebs_volumes_config
-  ebs_volumes                   = var.bcs_instance_ebs_volumes
+  ebs_volumes                   = local.bcs_instance_ebs_volumes
   ebs_volume_tags               = local.tags
   route53_records = {
     create_internal_record = false
@@ -73,15 +70,14 @@ module "instance" {
   iam_resource_names_prefix = "instance-bcs${count.index + 1}-"
   instance_profile_policies = local.bcs_instance_profile_policies
 
-  user_data_raw = base64encode(var.user_data)
+  user_data_raw = base64encode("")
 
   business_unit     = var.networking[0].business-unit
   application_name  = local.application_name
   environment       = terraform.workspace
   region            = "eu-west-2"
   availability_zone = "eu-west-2a"
-  subnet_id         = data.aws_subnets.shared-private[0].id
-  tags = local.tags }
-  )
+  subnet_id         = data.aws_subnets.shared-private.ids[0]
+  tags              = local.tags
   #  cloudwatch_metric_alarms = {}
 }
