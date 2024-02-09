640d2cbcd3adb48067ac7d007b5b760d93de4eee
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2024-02-09 11:35:58 +0000

    LAWS-3627: Add artifacts S3 bucket

---
+++ terraform/environments/maat/artifacts.tf
@@ -0,0 +1,55 @@
+# Terraform module which creates S3 Bucket resources for Load Balancer Access Logs on AWS.
+
+module "artifacts-s3" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.1.0"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  bucket_prefix       = "${local.application_name}-build-artifacts"
+  bucket_policy       = [data.aws_iam_policy_document.arfitacts.json]
+  replication_enabled = false
+  versioning_enabled  = true
+  force_destroy       = true
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      expiration = {
+        days = 31
+      }
+
+      noncurrent_version_expiration = {
+        days = 31
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
+data "aws_iam_policy_document" "arfitacts" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${module.artifacts-s3.bucket.arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.env_account_id}:role/modernisation-platform-oidc-cicd"]
+    }
+  }
+
+}
+
