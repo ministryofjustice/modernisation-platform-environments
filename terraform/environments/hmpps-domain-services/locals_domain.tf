0d6b6a460cf8c1c123ba068e0faf2fb4f6b73ac0
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2024-02-09 09:19:07 +0000

    updating iam locals policies with testing arns

--- terraform/environments/hmpps-domain-services/locals_domain.tf
+++ terraform/environments/hmpps-domain-services/locals_domain.tf
@@ -9,7 +9,9 @@ locals {
       "arn:aws:iam::${module.environment.account_ids.corporate-staff-rostering-test}:role/EC2HmppsDomainSecretsRole",
       "arn:aws:iam::${module.environment.account_ids.planetfm-development}:role/EC2HmppsDomainSecretsRole",
       "arn:aws:iam::${module.environment.account_ids.planetfm-test}:role/EC2HmppsDomainSecretsRole",
-      "arn:aws:iam::${module.environment.account_ids.corporate-staff-rostering-development}:role/LambdaFunctionADObjectCleanUp", # to be added after lambda deployed in CSR
+      "arn:aws:iam::${module.environment.account_ids.corporate-staff-rostering-test}:role/LambdaFunctionADObjectCleanUp", # testing
+      "arn:aws:iam::${module.environment.account_ids.hmpps-domain-services-development}:role/LambdaFunctionADObjectCleanUp", # testing
+
     ]
     preproduction = []
     production = [
