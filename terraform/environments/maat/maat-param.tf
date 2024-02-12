e1f397d743aab148cd1c031e3b7b03f0b8cc4f9c
Vladimirs Kovalovs <vladimirs.kovalovs@digital.justice.gov.uk>
2024-02-12 17:24:15 +0000

    LAWS-3623: initial commit

---
+++ terraform/environments/maat/maat-param.tf
@@ -0,0 +1,244 @@
+######################################
+# PARAMETER STORE SECRETS
+######################################
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_bc_client_user_id" {
+  name  = "/maat/APP_BC_CLIENT_USER_ID"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_ccc_endpoint" {
+  name  = "/maat/APP_CCC_ENDPOINT"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "maat_app_cma_oauth_scope" {
+  name  = "/maat/APP_CMA_OAUTH_SCOPE"
+  type  = "SecureString"
+  value = "replace in console"
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
