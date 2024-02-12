62abba62c6f140360b5e9a7e03b4bebe5f5230b2
George Taylor <george.taylor@digital.justice.gov.uk>
2024-02-12 18:12:48 +0000

    Update main.tf

--- terraform/environments/delius-core/modules/ecs_policies/main.tf
+++ terraform/environments/delius-core/modules/ecs_policies/main.tf
@@ -96,7 +96,7 @@ data "aws_iam_policy_document" "ecs_task_exec" {
 }
 
 resource "aws_iam_role" "task_exec" {
-  name               = "${var.env_name}-${var.service_name}-task-exec"
+  name               = "${var.env_name}-${var.service_name}-ecs-task-exec"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_exec.json
   tags               = var.tags
 }
