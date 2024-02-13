fd79e9b1580584902b48a27eac2885e385382210
mikereiddigital <mike.reid@digital.justice.gov.uk>
2024-02-13 15:51:35 +0000

    Builds example without the "environment" module.

--- terraform/environments/cooker/main.tf
+++ terraform/environments/cooker/main.tf
@@ -30,6 +30,7 @@ locals {
   }
 
   ec2_test = {
+
     tags = {
       component = "test"
     }
@@ -56,11 +57,11 @@ locals {
 
     # user can manually increase the desired capacity to 1 via CLI/console
     # to create an instance
-    autoscaling_group = {
-      desired_capacity = 0
-      max_size         = 2
-      min_size         = 0
-    }
+    # autoscaling_group = {
+    #   desired_capacity = 0
+    #   max_size         = 2
+    #   min_size         = 0
+    # }
 
     ec2_test_instances = {
       # Remove data.aws_kms_key from cmk.tf once the NDH servers are removed
@@ -101,7 +102,32 @@ locals {
         availability_zone = "eu-west-2b"
       }
     }
+  }
 
+  # This local provides a list of ingress rules for the ec2 security groups.
+
+    example_ec2_sg_ingress_rules = {
+
+      TCP_22 = {
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+      }
+
+      TCP_443 = {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+      }
+  }
+
+      example_ec2_sg_egress_rules = {
+
+      TCP_ALL = {
+        from_port = 1
+        to_port = 65000
+        protocol = "TCP"
+      }
   }
 
   # create list of common managed policies that can be attached to ec2 instance profiles
@@ -126,7 +152,7 @@ module "ec2_test_instance" {
   ami_owner                     = try(each.value.ami_owner, "core-shared-services-production")
   instance                      = merge(local.instance, lookup(each.value, "instance", { disable_api_stop = false }))
   ebs_volumes_copy_all_from_ami = try(each.value.ebs_volumes_copy_all_from_ami, true)
-  ebs_kms_key_id                = data.aws_kms_key.ebs_shared.arn
+  ebs_kms_key_id                = "" # module.environment.kms_keys["ebs"].arn
   ebs_volume_config             = lookup(each.value, "ebs_volume_config", {})
   ebs_volumes                   = lookup(each.value, "ebs_volumes", {})
   ssm_parameters_prefix         = lookup(each.value, "ssm_parameters_prefix", "test/")
@@ -156,7 +182,7 @@ resource "aws_security_group" "example_ec2_sg" {
 }
 
 resource "aws_security_group_rule" "ingress_traffic" {
-  for_each          = local.application_data.example_ec2_sg_rules
+  for_each          = local.example_ec2_sg_ingress_rules
   description       = format("Traffic for %s %d", each.value.protocol, each.value.from_port)
   from_port         = each.value.from_port
   protocol          = each.value.protocol
@@ -167,7 +193,7 @@ resource "aws_security_group_rule" "ingress_traffic" {
 }
 
 resource "aws_security_group_rule" "egress_traffic" {
-  for_each                 = local.application_data.example_ec2_sg_rules
+  for_each                 = local.example_ec2_sg_egress_rules
   description              = format("Outbound traffic for %s %d", each.value.protocol, each.value.from_port)
   from_port                = each.value.from_port
   protocol                 = each.value.protocol
@@ -187,32 +213,6 @@ resource "aws_security_group_rule" "ingress_traffic_icmp_from_cp" {
   cidr_blocks       = ["172.20.0.0/16"] //Cloud Platform
 }
 
-#  Build EC2 "example-ec2"
-resource "aws_instance" "develop" {
-  #checkov:skip=CKV2_AWS_41:"IAM role is not implemented for this example EC2. SSH/AWS keys are not used either."
-  # Specify the instance type and ami to be used (this is the Amazon free tier option)
-  instance_type          = local.application_data.accounts[local.environment].instance_type
-  ami                    = local.application_data.accounts[local.environment].ami_image_id
-  vpc_security_group_ids = [aws_security_group.example_ec2_sg.id]
-  subnet_id              = data.aws_subnet.private_subnets_a.id
-  monitoring             = true
-  ebs_optimized          = true
-
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-  # Increase the volume size of the root volume
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = 20
-    encrypted   = true
-  }
-  tags = merge(local.tags,
-    { Name = lower(format("ec2-%s-%s-example", local.application_name, local.environment)) }
-  )
-  depends_on = [aws_security_group.example_ec2_sg]
-}
 
 # create single managed policy
 resource "aws_iam_policy" "ec2_common_policy" {
@@ -280,26 +280,7 @@ resource "aws_kms_key" "ec2" {
   )
 }
 
-resource "aws_ebs_volume" "ebs_volume" {
-  availability_zone = "${local.application_data.accounts[local.environment].region}a"
-  type              = "gp3"
-  size              = 50
-  throughput        = 200
-  encrypted         = true
-  kms_key_id        = aws_kms_key.ec2.arn
-  tags = {
-    Name = "ebs-data-volume"
-  }
 
-  depends_on = [aws_instance.develop, aws_kms_key.ec2]
-}
-
-# Attach to the EC2
-resource "aws_volume_attachment" "mountvolumetoec2" {
-  device_name = "/dev/sdb"
-  instance_id = aws_instance.develop.id
-  volume_id   = aws_ebs_volume.ebs_volume.id
-}
 
 #### This file can be used to store data specific to the member account ####
 data "aws_iam_policy_document" "ebs-kms" {
