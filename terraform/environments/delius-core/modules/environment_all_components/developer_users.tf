efb2681672fbd07f52c474ce438f730d37394b0c
George Taylor <george.taylor@digital.justice.gov.uk>
2024-02-13 17:59:08 +0000

    snapshot id

--- terraform/environments/delius-core/modules/environment_all_components/developer_users.tf
+++ terraform/environments/delius-core/modules/environment_all_components/developer_users.tf
@@ -1,4 +1,4 @@
 module "developer_iam_user" {
   source        = "../helpers/developer_iam_users"
-  ecr_push_user = var.environment_config.ecr_push_user
+  ecr_push_user = var.environment_config.developer_ecr_push_user
 }
