db7668de8d14b2050342a82e01d77e6ebe970b54
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-02-13 15:44:45 +0000

    fix

--- terraform/environments/hmpps-domain-services/ad-fixngo.tf
+++ terraform/environments/hmpps-domain-services/ad-fixngo.tf
@@ -113,7 +113,7 @@ locals {
         managed_policy_arns = [
           "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
           "ad-fixngo-ec2-policy",
-          "ad-fixngo-devtest-secrets-policy",
+          "ad-fixngo-nonlive-secrets-policy",
         ]
       }
       ad-fixngo-ec2-live-role = {
@@ -605,9 +605,9 @@ resource "aws_iam_role" "ad_fixngo" {
     }
   )
 
-  managed_policy_arns = []
-  #  for key_or_arn in each.value.managed_policy_arns : try(aws_iam_policy.ad_fixngo[key_or_arn], key_or_arn)
-  # ]
+  managed_policy_arns = [
+    for key_or_arn in each.value.managed_policy_arns : try(aws_iam_policy.ad_fixngo[key_or_arn].arn, key_or_arn)
+  ]
 
   tags = merge(local.tags, local.ad_fixngo.tags, {
     Name = each.key
