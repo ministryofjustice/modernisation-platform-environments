5efa38c6507b0e16e902d0572001b1a8747a512a
W <willg258@hotmail.co.uk>
2024-02-13 11:58:56 +0000

    ..

--- terraform/environments/oasys/locals_preproduction.tf
+++ terraform/environments/oasys/locals_preproduction.tf
@@ -180,10 +180,11 @@ locals {
         # and put the wildcard in the san
         domain_name = "pp.oasys.service.justice.gov.uk"
         subject_alternate_names = [
+          "pp-int.oasys.service.justice.gov.uk",
           "bridge-pp-oasys.az.justice.gov.uk",
           "pp-oasys.az.justice.gov.uk",
           "*.pp-oasys.az.justice.gov.uk",
-          "*.oasys.service.justice.gov.uk",
+          "db.pp.oasys.service.justice.gov.uk",
           "db.pp.onr.oasys.service.justice.gov.uk",
         ]
         external_validation_records_created = false
