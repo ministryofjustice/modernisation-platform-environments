009fa54a55db398a697370316a2d790c3f292f2b
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-02-13 17:26:31 +0000

    fix

--- terraform/environments/hmpps-domain-services/ad-fixngo.tf
+++ terraform/environments/hmpps-domain-services/ad-fixngo.tf
@@ -27,45 +27,45 @@ locals {
       # rdlic        = remote desktop licensing server
       # a/b/c suffix = availability zone
 
-      # ad-hmpp-dc-a = {
-      #   ami_name                  = "hmpps_windows_server_2022_release_2024-02-02T00-00-04.569Z"
-      #   az_index                  = 0 # zone a
-      #   iam_instance_profile_role = "ad-fixngo-ec2-live-role"
-      #   instance_type             = "t3.large"
-      #   private_ip                = null
-      #   vpc_name                  = "live_data"
-      #   vpc_security_group_name   = "ad_hmpp_dc_sg"
-      #   tags = {
-      #     server-type = "DomainController"
-      #     description = "domain controller for FixNGo azure.hmpp.root domain"
-      #   }
-      # }
-      # ad-hmpp-dc-b = {
-      #   ami_name                  = "hmpps_windows_server_2022_release_2024-02-02T00-00-04.569Z"
-      #   az_index                  = 1 # zone b
-      #   iam_instance_profile_role = "ad-fixngo-ec2-live-role"
-      #   instance_type             = "t3.large"
-      #   private_ip                = "TODO"
-      #   vpc_name                  = "live_data"
-      #   vpc_security_group_name   = "ad_hmpp_dc_sg"
-      #   tags = {
-      #     server-type = "DomainController"
-      #     description = "domain controller for FixNGo azure.hmpp.root domain"
-      #   }
-      # }
-      # ad-hmpp-rdlic = {
-      #   ami_name                  = "hmpps_windows_server_2022_release_2024-02-02T00-00-04.569Z"
-      #   az_index                  = 2 # zone c
-      #   iam_instance_profile_role = "ad-fixngo-ec2-live-role"
-      #   instance_type             = "t3.medium"
-      #   private_ip                = null
-      #   vpc_name                  = "live_data"
-      #   vpc_security_group_name   = "ad_hmpp_rdlic_sg"
-      #   tags = {
-      #     server-type = "RDLicensing"
-      #     description = "remote desktop licensing server for FixNGo azure.hmpp.root domain"
-      #   }
-      # }
+      ad-hmpp-dc-a = {
+        ami_name                  = "hmpps_windows_server_2022_release_2024-02-02T00-00-04.569Z"
+        az_index                  = 0 # zone a
+        iam_instance_profile_role = "ad-fixngo-ec2-live-role"
+        instance_type             = "t3.large"
+        private_ip                = null
+        vpc_name                  = "live_data"
+        vpc_security_group_name   = "ad_hmpp_dc_sg"
+        tags = {
+          server-type = "DomainController"
+          description = "domain controller for FixNGo azure.hmpp.root domain"
+        }
+      }
+      ad-hmpp-dc-b = {
+        ami_name                  = "hmpps_windows_server_2022_release_2024-02-02T00-00-04.569Z"
+        az_index                  = 1 # zone b
+        iam_instance_profile_role = "ad-fixngo-ec2-live-role"
+        instance_type             = "t3.large"
+        private_ip                = null
+        vpc_name                  = "live_data"
+        vpc_security_group_name   = "ad_hmpp_dc_sg"
+        tags = {
+          server-type = "DomainController"
+          description = "domain controller for FixNGo azure.hmpp.root domain"
+        }
+      }
+      ad-hmpp-rdlic = {
+        ami_name                  = "hmpps_windows_server_2022_release_2024-02-02T00-00-04.569Z"
+        az_index                  = 2 # zone c
+        iam_instance_profile_role = "ad-fixngo-ec2-live-role"
+        instance_type             = "t3.medium"
+        private_ip                = null
+        vpc_name                  = "live_data"
+        vpc_security_group_name   = "ad_hmpp_rdlic_sg"
+        tags = {
+          server-type = "RDLicensing"
+          description = "remote desktop licensing server for FixNGo azure.hmpp.root domain"
+        }
+      }
 
       ad-azure-dc-a = {
         ami_name                  = "hmpps_windows_server_2022_release_2024-02-02T00-00-04.569Z"
