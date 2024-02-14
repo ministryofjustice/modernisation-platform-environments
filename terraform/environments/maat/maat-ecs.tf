173e0ab89086a9f8458a0b3760250418dc94d453
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2024-02-14 15:41:13 +0000

    adding ecs tasks role

--- terraform/environments/maat/maat-ecs.tf
+++ terraform/environments/maat/maat-ecs.tf
@@ -63,32 +63,6 @@ resource "aws_iam_policy" "maat_ec2_instance_role_policy" {
   })
 }
 
-resource "aws_iam_policy" "maat_ec2_instance_role_policy_access_params" {
-  name = "${local.application_name}-ec2-instance-role-policy-access-params"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "ssm:GetParameters"
-        Resource = [
-          "arn:aws:ssm:${local.env_account_region}:${local.env_account_id}:parameter/maat/*"
-        ]
-      }
-    ]
-  })
-
-  tags = {
-    Name = "${local.application_name}-ec2-instance-role-policy-access-params"
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "mmaat_ec2_instance_role_policy_attachment_access_params" {
-  role       = aws_iam_role.maat_ec2_instance_role.name
-  policy_arn = aws_iam_policy.maat_ec2_instance_role_policy_access_params.arn
-}
-
 resource "aws_iam_role_policy_attachment" "maat_ec2_instance_role_policy_attachment" {
   role       = aws_iam_role.maat_ec2_instance_role.name
   policy_arn = aws_iam_policy.maat_ec2_instance_role_policy.arn
@@ -453,6 +427,57 @@ resource "aws_iam_role_policy_attachment" "maat_ecs_autoscaling_role_policy_atta
   policy_arn = aws_iam_policy.maat_ecs_autoscaling_role_policy.arn
 }
 
+resource "aws_iam_role" "maat_ecs_tasks_role" {
+  name = "${local.application_name}-ecs-tasks-role"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${local.application_name}-ecs-tasks-role"
+    }
+  )
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+}
+
+EOF
+}
+
+resource "aws_iam_policy" "maat_ecs_policy_access_params" {
+  name = "${local.application_name}-ecs-policy-access-params"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "ssm:GetParameters"
+        Resource = [
+          "arn:aws:ssm:${local.env_account_region}:${local.env_account_id}:parameter/maat/*"
+        ]
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${local.application_name}-ecs-policy-access-params"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "maat_ec2_instance_role_policy_attachment_access_params" {
+  role       = aws_iam_role.maat_ecs_tasks_role.name
+  policy_arn = aws_iam_policy.maat_ecs_policy_access_params.arn
+}
 #### ECS TASK DEFINITION -------
 
 resource "aws_ecs_task_definition" "maat_ecs_task_definition" {
