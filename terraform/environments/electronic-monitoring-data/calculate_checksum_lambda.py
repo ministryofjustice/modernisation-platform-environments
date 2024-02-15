5d525a3d3e33d521caaeab6f502a279f47785796
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-02-15 08:59:17 +0000

    ELM-1524 Implement event driven lambda for calculating checksums (#5043)
    * Rename s3.tf to data_store.tf
    
    * Add s3 batch job and lambda function to data store bucket
    
    * Add lambda function python code to analyse zip files
    
    * change from s3 batch to s3 events for lambda function
    
    * update resource bucket reference
    
    * limit the event triggers to prevent recursion
    
    * Update lambda function to not do a copy action.
    
    * Update both lambda functions
    
    * Add invoke summarise_zip lambda into terraform
    
    * Update event trigger
    
    * update lambda function names
    
    * update lambda function
    
    * update lambda name
    
    * Taking out zip summary lambda as cannot invoke from same event as another lambda
    
    * Temporary patch to get all lambdas working
    
    * update lambda function code

---
+++ terraform/environments/electronic-monitoring-data/calculate_checksum_lambda.py
@@ -0,0 +1,159 @@
+"""Lambda function to add Checksum metadata to files.
+
+This function adds two tags to files that contain the SHA256 checksum as well
+as the BAse64 encoded SHA256 checksum (this is what AWS displays by default).
+
+To calculate a files SHA256 checksum locally in the command line (i.e. to
+compare) use:
+```
+shasum -a 256 path/to/file
+```
+
+and:
+
+```
+shasum -a 256 path/to/file | cut -f1 -d\ | xxd -r -p | base64
+```
+
+to encode it to Base64.
+"""
+import base64
+import boto3
+import hashlib
+
+import io
+import json
+import zipfile
+
+
+s3_client = boto3.client('s3')
+
+
+def handler(event, context):
+    print(event)
+
+    event_type = event['Records'][0]['eventName']
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+
+    print(f'{object_key = } added to {bucket = } via {event_type = }')
+
+    # Generate the SHA256 checksum of the object.
+    hash_value = generate_sha256_checksum(bucket, object_key)
+    
+    # Print the SHA256 checksum to CloudWatch logs.
+    print(f'SHA256 checksum of {object_key}: {hash_value}')
+    
+    hash_64 = convert_hash_to_base64(hash_value=hash_value)
+
+    # Print the Base 64 encoded SHA256 checksum to CloudWatch logs.
+    print(f'Base 64 SHA256 checksum of {object_key}: {hash_64}')
+
+    # Retrieve existing tags for the object
+    response = s3_client.get_object_tagging(
+        Bucket=bucket,
+        Key=object_key
+    )
+
+    additional_tags = {
+        'SHA-256 checksum': hash_value,
+        'Base64 SHA-256 checksum': hash_64,
+    }
+    
+    # Merge existing tags with additional tags
+    existing_tags = response.get('TagSet', [])
+    existing_tags.extend([
+        {'Key': key, 'Value': value}
+        for key, value in additional_tags.items()
+    ])
+    
+    # Update tags for the object
+    s3_client.put_object_tagging(
+        Bucket=bucket,
+        Key=object_key,
+        Tagging={
+            'TagSet': existing_tags
+        }
+    )
+
+    print(f'Added tags = {list(additional_tags.keys())} to {object_key = }')
+
+    # TEMPORARILY ALSO ADDING THE ZIP SUMMARY CODE HERE.
+
+    # Check if the object key ends with '.zip'
+    if object_key.endswith('.zip'):
+        print(f"Summarising '{object_key = }' as has '.zip' extension")
+
+        # Read the contents of the zip file from S3
+        response = s3_client.get_object(
+            Bucket=bucket,
+            Key=object_key
+        )
+        
+        # Read the zip file content into memory
+        zip_content = response['Body'].read()
+        
+        # Extract files from the zip
+        with zipfile.ZipFile(io.BytesIO(zip_content)) as zip_ref:
+            # List all files in the zip
+            file_list = zip_ref.namelist()
+            
+            # Total number of files
+            total_files = len(file_list)
+            
+            # Directory structure dictionary
+            directory_structure = {}
+            
+            # Read each file's content and build directory structure
+            for file_name in file_list:
+                parts = file_name.split('/')
+                current_dict = directory_structure
+                
+                # Traverse directory structure and create dictionary entries
+                for part in parts[:-1]:
+                    if part not in current_dict:
+                        current_dict[part] = {}
+                    current_dict = current_dict[part]
+            
+            print(f'\n\nJSON directory structure:\n{directory_structure}')
+
+            print(f'\n\n Total files in {object_key}: {total_files}')
+
+            # Writing the JSON file with the information
+            json_data = {
+                'total_objects': total_files,
+                'directory_structure': directory_structure
+            }
+            json_content = json.dumps(json_data)
+
+            # Saving JSON content to a new file with .json extension
+            new_object_key = object_key + '.info.json'
+
+            s3_client.put_object(
+                Bucket=bucket, 
+                Key=new_object_key,
+                Body=json_content.encode('utf-8')
+            )
+
+            print(f'Zip info saved to {new_object_key}')
+
+    return None
+
+
+def generate_sha256_checksum(bucket_name, object_key):
+    # Retrieve the object data from S3
+    response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    object_data = response['Body'].read()
+    
+    # Calculate the SHA256 checksum
+    sha256_hash = hashlib.sha256(object_data).hexdigest()
+
+    return sha256_hash
+
+
+def convert_hash_to_base64(hash_value):
+    # Convert hexdigest to bytes
+    binary_data = bytes.fromhex(hash_value)
+
+    # Encode bytes to base64
+    return base64.b64encode(binary_data).decode()
---
+++ terraform/environments/electronic-monitoring-data/data_store.tf
@@ -0,0 +1,231 @@
+#------------------------------------------------------------------------------
+# S3 bucket for data store logs
+#------------------------------------------------------------------------------
+
+module "data_store_log_bucket" {
+  source = "./modules/s3_log_bucket"
+
+  source_bucket = aws_s3_bucket.data_store
+  account_id    = data.aws_caller_identity.current.account_id
+}
+
+#------------------------------------------------------------------------------
+# S3 bucket for landed data (internal facing)
+#------------------------------------------------------------------------------
+
+resource "aws_s3_bucket" "data_store" {
+  bucket_prefix = "em-data-store-"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data_store" {
+  bucket = aws_s3_bucket.data_store.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "data_store" {
+  bucket                  = aws_s3_bucket.data_store.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "data_store" {
+  bucket = aws_s3_bucket.data_store.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_policy" "data_store" {
+  bucket = aws_s3_bucket.data_store.id
+  policy = data.aws_iam_policy_document.data_store.json
+}
+
+data "aws_iam_policy_document" "data_store" {
+  statement {
+    sid = "EnforceTLSv12orHigher"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.data_store.arn,
+      "${aws_s3_bucket.data_store.arn}/*"
+    ]
+    condition {
+      test     = "NumericLessThan"
+      variable = "s3:TlsVersion"
+      values   = [1.2]
+    }
+  }
+}
+
+resource "aws_s3_bucket_logging" "data_store" {
+  bucket = aws_s3_bucket.data_store.id
+
+  target_bucket = module.data_store_log_bucket.bucket_id
+  target_prefix = "log/"
+
+  target_object_key_format {
+    partitioned_prefix {
+      partition_date_source = "EventTime"
+    }
+  }
+}
+
+resource "aws_s3_bucket_notification" "data_store" {
+  bucket = aws_s3_bucket.data_store.id
+
+  # Only for copy events as those are events triggered by data being copied
+  # from landing bucket.
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.calculate_checksum_lambda.arn
+    events              = [
+      "s3:ObjectCreated:Copy"
+    ]
+  }
+
+  # Only for copy events as those are events triggered by data being copied
+  # from landing bucket.
+  # lambda_function {
+  #   lambda_function_arn = aws_lambda_function.summarise_zip_lambda.arn
+  #   events              = [
+  #     "s3:ObjectCreated:Copy"
+  #   ]
+  # }
+
+  depends_on = [
+    aws_lambda_permission.s3_allow_calculate_checksum_lambda,
+    # aws_lambda_permission.s3_allow_summarise_zip_lambda,
+  ]
+}
+
+#------------------------------------------------------------------------------
+# S3 lambda function to calculate data store file checksums
+#------------------------------------------------------------------------------
+
+variable "checksum_algorithm" {
+  description = "Select Checksum Algorithm. Default and recommended choice is SHA256, however CRC32, CRC32C, SHA1 are also available."
+  default     = "SHA256"
+}
+
+data "archive_file" "calculate_checksum_lambda" {
+  type        = "zip"
+  source_file = "calculate_checksum_lambda.py"
+  output_path = "calculate_checksum_lambda.zip"
+}
+
+resource "aws_lambda_function" "calculate_checksum_lambda" {
+  filename      = "calculate_checksum_lambda.zip"
+  function_name = "calculate-checksum-lambda"
+  role          = aws_iam_role.calculate_checksum_lambda.arn
+  handler       = "calculate_checksum_lambda.handler"
+  runtime       = "python3.9"
+  timeout       = 600
+
+  environment {
+    variables = {
+      Checksum = var.checksum_algorithm
+    }
+  }
+}
+
+resource "aws_iam_role" "calculate_checksum_lambda" {
+  name                = "calculate-checksum-lambda-iam-role"
+  assume_role_policy  = data.aws_iam_policy_document.lambda_assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+}
+
+data "aws_iam_policy_document" "calculate_checksum_lambda" {
+  statement {
+    sid = "S3Permissions"
+    effect  = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectTagging",
+      "s3:PutObjectAcl",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetObjectTagging",
+      "s3:GetObjectAcl",
+      "s3:GetObjectAttributes",
+      "s3:GetObjectVersionAttributes",
+      "s3:ListBucket"
+    ]
+    resources = ["${aws_s3_bucket.data_store.arn}/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "calculate_checksum_lambda" {
+  name   = "calculate_checksum-lambda-iam-policy"
+  role   = aws_iam_role.calculate_checksum_lambda.id
+  policy = data.aws_iam_policy_document.calculate_checksum_lambda.json
+}
+
+resource "aws_lambda_permission" "s3_allow_calculate_checksum_lambda" {
+  statement_id  = "AllowCalculateChecksumExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.calculate_checksum_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${aws_s3_bucket.data_store.arn}"
+}
+
+#------------------------------------------------------------------------------
+# S3 lambda function to perform zip file summary
+#------------------------------------------------------------------------------
+
+data "archive_file" "summarise_zip_lambda" {
+  type        = "zip"
+  source_file = "summarise_zip_lambda.py"
+  output_path = "summarise_zip_lambda.zip"
+}
+
+resource "aws_lambda_function" "summarise_zip_lambda" {
+  filename      = "summarise_zip_lambda.zip"
+  function_name = "summarise-zip-lambda"
+  role          = aws_iam_role.summarise_zip_lambda.arn
+  handler       = "summarise_zip_lambda.handler"
+  runtime       = "python3.9"
+  timeout       = 600
+}
+
+resource "aws_iam_role" "summarise_zip_lambda" {
+  name                = "summarise-zip-iam-role"
+  assume_role_policy  = data.aws_iam_policy_document.lambda_assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+}
+
+data "aws_iam_policy_document" "summarise_zip_lambda" {
+  statement {
+    sid = "S3Permissions"
+    effect  = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket"
+    ]
+    resources = ["${aws_s3_bucket.data_store.arn}/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "summarise_zip_lambda" {
+  name   = "summarise-zip-iam-policy"
+  role   = aws_iam_role.summarise_zip_lambda.id
+  policy = data.aws_iam_policy_document.summarise_zip_lambda.json
+}
+
+resource "aws_lambda_permission" "s3_allow_summarise_zip_lambda" {
+  statement_id  = "AllowSummariseZipExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.summarise_zip_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${aws_s3_bucket.data_store.arn}"
+}
