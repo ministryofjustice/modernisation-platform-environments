ff30b028af26f361b8023099fc64b2d96e459529
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-02-15 10:38:05 +0000

    improve logging from lambda

--- terraform/environments/corporate-staff-rostering/lambda/cw-xml-to-json/lambda_function.py
+++ terraform/environments/corporate-staff-rostering/lambda/cw-xml-to-json/lambda_function.py
@@ -28,6 +28,7 @@ def create_log_stream(log_group_name, log_stream_name):
 
         streams = response.get("logStreams", [])
         if not any(s["logStreamName"] == log_stream_name for s in streams):
+            print("Creating new log stream.")
             client.create_log_stream(
                 logGroupName=log_group_name, logStreamName=log_stream_name
             )
@@ -37,25 +38,15 @@ def create_log_stream(log_group_name, log_stream_name):
 
 
 def lambda_handler(event, context):
+    print("Processing log event.")
+
     logs_client = boto3.client("logs")
 
-    print("==>", "event")
-    print(event)
-    print("==>", "context")
-    print(context)
-    print("==>", 'event["awslogs"]["data"]')
-    print(event["awslogs"]["data"])
-
     compressed_payload = base64.b64decode(event["awslogs"]["data"])
-    print("==>", "compressed_payload")
-    print(compressed_payload)
-
     uncompressed_payload = zlib.decompress(compressed_payload, 16 + zlib.MAX_WBITS)
-    print("==>", "uncompressed_payload")
-    print(uncompressed_payload)
 
     log_data = json.loads(uncompressed_payload)
-    print("==>", "log_data")
+
     print(log_data)
 
     dest_log_group = DEST_LOG_GROUP
@@ -64,19 +55,22 @@ def lambda_handler(event, context):
     create_log_stream(dest_log_group, dest_log_stream)
 
     for log_event in log_data["logEvents"]:
-        new_log_message = {"_": {"sourceLogStream": log_data["logStream"]}} | xml_to_dict(
-            log_event["message"]
-        )
+        new_log_message = {
+            "_": {"sourceLogStream": log_data["logStream"]}
+        } | xml_to_dict(log_event["message"])
+
+        new_log_event = {
+            "timestamp": log_event["timestamp"],
+            "message": json.dumps(new_log_message),
+        }
+
+        print("Putting new log event.")
+        print(new_log_event)
 
         logs_client.put_log_events(
             logGroupName=dest_log_group,
             logStreamName=dest_log_stream,
-            logEvents=[
-                {
-                    "timestamp": log_event["timestamp"],
-                    "message": json.dumps(new_log_message),
-                }
-            ],
+            logEvents=[new_log_event],
         )
 
-    return {"statusCode": 200, "body": json.dumps("Log processing complete")}
+    return {"statusCode": 200, "body": json.dumps("Log processing complete.")}
