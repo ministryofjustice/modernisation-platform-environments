0a90155de212274815488f3fae7d4d23897ea5d0
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-02-15 10:08:36 +0000

    LAWS-3612: tags
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/maat/api-cw.tf
+++ terraform/environments/maat/api-cw.tf
@@ -5,6 +5,12 @@ resource "aws_cloudwatch_log_group" "maat_api_ecs_cw_group" {
   name              = "${local.application_name}-api-ECS"
   retention_in_days = 90
   kms_key_id        = aws_kms_key.cloudwatch_logs_key.arn
+  tags = merge(
+    local.tags,
+    {
+      Name = "${local.application_name}-api-ECS"
+    },
+  )
 }
 # ECS cluster alerting
 resource "aws_cloudwatch_metric_alarm" "maat_api_ecs_cpu_over_threshold" {
