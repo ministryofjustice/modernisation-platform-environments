9e01627a825ce41f1a1944248860c9fb932b9998
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2024-02-15 14:39:31 +0000

    removing ecs tasks role and attaching policies to ec2 instance role

--- terraform/environments/maat/maat-ecs.tf
+++ terraform/environments/maat/maat-ecs.tf
@@ -25,6 +25,13 @@ resource "aws_iam_role" "maat_ec2_instance_role" {
             },
             "Effect": "Allow"
         }
+        {
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "ecs-tasks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
     ]
 }
 EOF
@@ -427,32 +434,6 @@ resource "aws_iam_role_policy_attachment" "maat_ecs_autoscaling_role_policy_atta
   policy_arn = aws_iam_policy.maat_ecs_autoscaling_role_policy.arn
 }
 
-resource "aws_iam_role" "maat_ecs_tasks_role" {
-  name = "${local.application_name}-ecs-tasks-role"
-  tags = merge(
-    local.tags,
-    {
-      Name = "${local.application_name}-ecs-tasks-role"
-    }
-  )
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ecs-tasks.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-}
-
-EOF
-}
-
 resource "aws_iam_policy" "maat_ecs_policy_access_params" {
   name = "${local.application_name}-ecs-policy-access-params"
 
@@ -495,15 +476,15 @@ resource "aws_iam_policy" "maat_ecs_policy_access_params" {
 }
 
 resource "aws_iam_role_policy_attachment" "maat_ecs_tasks_role_policy_attachment_access_params" {
-  role       = aws_iam_role.maat_ecs_tasks_role.name
+  role       = aws_iam_role.maat_ec2_instance_role.name
   policy_arn = aws_iam_policy.maat_ecs_policy_access_params.arn
 }
 #### ECS TASK DEFINITION -------
 
 resource "aws_ecs_task_definition" "maat_ecs_task_definition" {
   family                   = "${local.application_name}-ecs-task-definition"
-  execution_role_arn       = aws_iam_role.maat_ecs_tasks_role.arn
-  task_role_arn            = aws_iam_role.maat_ecs_tasks_role.arn
+  execution_role_arn       = aws_iam_role.maat_ec2_instance_role.arn
+  task_role_arn            = aws_iam_role.maat_ec2_instance_role.arn
   
   container_definitions = templatefile("maat-task-definition.json", 
     {
