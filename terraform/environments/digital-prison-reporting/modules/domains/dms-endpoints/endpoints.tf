a2f1b8af9a845fb2ac457587523c2a4596c71710
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2024-02-21 00:15:35 +0000

    Rename setup_dms_nomis_endpoint to setup_dms_source_endpoint

--- terraform/environments/digital-prison-reporting/modules/domains/dms-endpoints/endpoints.tf
+++ terraform/environments/digital-prison-reporting/modules/domains/dms-endpoints/endpoints.tf
@@ -4,7 +4,7 @@ module "dms_endpoints" {
 
   setup_dms_endpoints      = var.setup_dms_endpoints
   setup_dms_iam            = var.setup_dms_iam
-  setup_dms_nomis_endpoint = var.setup_dms_nomis_endpoint
+  setup_dms_nomis_endpoint = var.setup_dms_source_endpoint
   setup_dms_s3_endpoint    = var.setup_dms_s3_endpoint
   source_engine_name       = var.source_engine_name
   dms_source_name          = var.dms_source_name
