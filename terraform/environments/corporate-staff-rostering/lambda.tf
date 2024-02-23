7175876d5c129de10854e4db2e39966fe069c757
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2024-02-23 13:18:26 +0000

    adding archive file data block for now..

--- terraform/environments/corporate-staff-rostering/lambda.tf
+++ terraform/environments/corporate-staff-rostering/lambda.tf
@@ -39,11 +39,11 @@ module "ad-clean-up-lambda" {
   )
 }
 
-# data "archive_file" "ad-cleanup-lambda" {
-#   type        = "zip"
-#   source_dir  = "lambda/ad-clean-up"
-#   output_path = "lambda/ad-clean-up-lambda-payload-test.zip"
-# }
+data "archive_file" "ad-cleanup-lambda" {
+  type        = "zip"
+  source_dir  = "lambda/ad-clean-up"
+  output_path = "lambda/ad-clean-up-lambda-payload-test.zip"
+}
 
 resource "aws_cloudwatch_event_rule" "ec2_state_change_terminated" {
   name        = "Ec2StateChangedTerminated"
