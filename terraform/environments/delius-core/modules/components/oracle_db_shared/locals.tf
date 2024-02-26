d21a7cd8f907162d671aab0c375ecd5c4d2385fd
ranbeersingh1 <ranbeersinghtradedata@hotmail.com>
2024-02-26 17:00:11 +0000

    Make access to S3 legacy bucket dynaic

--- terraform/environments/delius-core/modules/components/oracle_db_shared/locals.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/locals.tf
@@ -3,4 +3,6 @@ locals {
   dba_secret_name         = "${local.secret_prefix}-dba-passwords"
   application_secret_name = "${local.secret_prefix}-application-passwords"
   oem_account_id          = var.platform_vars.environment_management.account_ids[join("-", ["hmpps-oem", var.account_info.mp_environment])]
+  legacy_s3_bucket_env    = var.env_name == "dev" ? "dmd-mis-dev" : var.env_name == "test" ? "del-test" : var.env_name == "training" ? "dt-training" : null
+  legacy_s3_bucket        = local.legacy_s3_bucket_env != null ? "eu-west-2-${local.legacy_s3_bucket_env}-oracledb-backups" : null
 }
