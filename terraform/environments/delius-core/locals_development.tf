ddfd957fc473011cf343912bb819be6b799c53bf
George Taylor <george.taylor@digital.justice.gov.uk>
2024-02-26 17:29:56 +0000

    add migration vars

--- terraform/environments/delius-core/locals_development.tf
+++ terraform/environments/delius-core/locals_development.tf
@@ -4,11 +4,14 @@
 # tags demonstrate inheritance due to merges in the module
 locals {
   environment_config_dev = {
-    migration_environment_private_cidr = ["10.162.32.0/22", "10.162.36.0/22", "10.162.40.0/22"]
-    migration_environment_db_cidr      = ["10.162.44.0/24", "10.162.45.0/24", "10.162.46.0/25"]
-    legacy_engineering_vpc_cidr        = "10.161.98.0/25"
-    ec2_user_ssh_key                   = file("${path.module}/files/.ssh/${terraform.workspace}/ec2-user.pub")
-    homepage_path                      = "/"
+    migration_environment_private_cidr     = ["10.162.32.0/22", "10.162.36.0/22", "10.162.40.0/22"]
+    migration_environment_db_cidr          = ["10.162.44.0/24", "10.162.45.0/24", "10.162.46.0/25"]
+    migration_environment_full_name        = "dmd-mis-dev"
+    migration_environment_abbreviated_name = "dmd"
+    migration_environment_short_name       = "mis-dev"
+    legacy_engineering_vpc_cidr            = "10.161.98.0/25"
+    ec2_user_ssh_key                       = file("${path.module}/files/.ssh/${terraform.workspace}/ec2-user.pub")
+    homepage_path                          = "/"
   }
 
   ldap_config_dev = {
