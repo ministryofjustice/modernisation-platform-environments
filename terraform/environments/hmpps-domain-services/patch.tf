15989ad1c1d90f68b6f076bc33a12cc1b448038c
pavmoj <142988272+pavmoj@users.noreply.github.com>
2024-02-26 15:51:27 +0000

    Correct cron expression (#5175)
    Co-authored-by: pav <pav.gainda@version1.com>

--- terraform/environments/hmpps-domain-services/patch.tf
+++ terraform/environments/hmpps-domain-services/patch.tf
@@ -2,7 +2,7 @@ module "development" {
   source                  = "../../modules/patch_manager"
   application             = "hmpps-domain-services"
   environment             = "development"
-  schedule                = "cron(15 16 * * *)" # 4.15pm today
+  schedule                = "cron(15 16 ? * * *)" # 4.15pm today
   approved_patches        = ["KB5034682", "KB5034770"]
 }
 
