9d367eb853b4936b0d5b5abd07a186ef256408a5
W <willg258@hotmail.co.uk>
2024-02-27 15:02:24 +0000

    resize onr db
    resize onr db

--- terraform/environments/oasys/locals_preproduction.tf
+++ terraform/environments/oasys/locals_preproduction.tf
@@ -129,6 +129,9 @@ locals {
             "Ec2PreprodDatabasePolicy",
           ])
         })
+        instance = merge(local.database_a.instance, {
+          instance_type = "r6i.2xlarge"
+        })
         user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_ansible_no_tags, {
           args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_ansible_no_tags.args, {
             branch = "oracle_11g_oasys_patchset_addition"
