f977628f165d2292f9d8daa01dc49d41ac1fef33
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-02-28 16:56:18 +0000

    csr: add alarms for application log events (#5197)

--- terraform/environments/corporate-staff-rostering/locals_defaults.tf
+++ terraform/environments/corporate-staff-rostering/locals_defaults.tf
@@ -40,6 +40,7 @@ locals {
     web = merge(
       module.baseline_presets.cloudwatch_metric_alarms.ec2,
       module.baseline_presets.cloudwatch_metric_alarms.ec2_cwagent_windows,
+      local.application_log_metric_alarms.web,
       {
         instance-or-cloudwatch-agent-stopped = merge(module.baseline_presets.cloudwatch_metric_alarms_by_sns_topic["csr_pagerduty"].ec2_instance_or_cwagent_stopped_windows["instance-or-cloudwatch-agent-stopped"], {
           threshold           = "0"
@@ -53,6 +54,7 @@ locals {
     app = merge(
       module.baseline_presets.cloudwatch_metric_alarms_by_sns_topic["csr_pagerduty"].ec2,
       module.baseline_presets.cloudwatch_metric_alarms_by_sns_topic["csr_pagerduty"].ec2_cwagent_windows,
+      local.application_log_metric_alarms.app,
       {
         instance-or-cloudwatch-agent-stopped = merge(module.baseline_presets.cloudwatch_metric_alarms_by_sns_topic["csr_pagerduty"].ec2_instance_or_cwagent_stopped_windows["instance-or-cloudwatch-agent-stopped"], {
           threshold           = "0"
