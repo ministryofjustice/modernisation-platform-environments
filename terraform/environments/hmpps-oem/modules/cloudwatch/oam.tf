4527e550a2ee1d24bb08cfb04d0918ce08ae9c6e
Shajida Akthar <Shajida Akthar>
2024-02-29 15:32:12 +0000

    cloudwatch crossaccount role for nomis

---
+++ terraform/environments/hmpps-oem/modules/cloudwatch/oam.tf
@@ -0,0 +1,26 @@
+resource "aws_oam_sink" "monitoring_account_oam_sink" {
+  name = "HMPPSOemSink"
+}
+
+resource "aws_oam_sink_policy" "monitoring_account_oam_sink_policy" {
+  sink_identifier = aws_oam_sink.monitoring_account_oam_sink.id
+
+  policy = jsonencode ({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["oam:CreateLink", "oam:UpdateLink"]
+        Effect = "Allow"
+        Resource = "*"
+        Principal = {
+          "AWS"  = var.source_account_ids
+        }
+        Condition = {
+          "ForAllValues:StringEquals" = {
+            "oam:ResourceTypes" = ["AWS::CloudWatch::Metric"]
+          }
+        }
+      }
+    ]
+  })
+}
