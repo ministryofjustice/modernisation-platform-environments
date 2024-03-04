2dfe9a21869b29764e1e2a8a93e15cf4186464da
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-04 12:18:49 +0000

    Update sandbox_secrets.tf

--- terraform/environments/delius-jitbit/sandbox_secrets.tf
+++ terraform/environments/delius-jitbit/sandbox_secrets.tf
@@ -12,11 +12,12 @@ resource "aws_secretsmanager_secret" "db_app_connection_string_sandbox" {
   )
 }
 
+data "aws_secretsmanager_secret_version" "rds_password" {
+  secret_id = aws_db_instance.jitbit_sandbox[0].master_user_secret[0].secret_arn
+}
+
 resource "aws_secretsmanager_secret_version" "db_app_connection_string_sandbox" {
-  count = local.is-development ? 1 : 0
+  count         = local.is-development ? 1 : 0
   secret_id     = aws_secretsmanager_secret.db_app_connection_string_sandbox[0].id
-  secret_string = "user id=;data source=aws_db_instance.jitbit.address;initial catalog=;password="
-  lifecycle {
-    ignore_changes = [secret_string]
-  }
+  secret_string = "user id=${aws_db_instance.jitbit_sandbox[0].username};data source=${aws_db_instance.jitbit_sandbox[0].address};initial catalog=Office;password=${data.aws_secretsmanager_secret_version.rds_password.secret_string}"
 }
