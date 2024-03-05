ccafcb3fb04b46f3f084d3cec6e952db95ee7001
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-03-05 20:01:38 +0000

    ELM-1609 terraform code for generating DB instances for database backup restorations. (#5202)
    * initial commit for db instance
    
    * change to use public subnet
    
    * set availablity zone
    
    * Revert "set availablity zone"
    
    This reverts commit eae3e32672a1a4ddf42459352d6805d558519a43.
    
    * add sql server backup restore option group
    
    * add secret manager for db secrets
    
    * set sql-server version to 2022
    
    * update tags
    
    * terraform secret management
    
    * change secrets to resource
    
    * rename secret
    
    * use the secret string not arn for password

---
+++ terraform/environments/electronic-monitoring-data/server_backups.tf
@@ -0,0 +1,149 @@
+resource "aws_secretsmanager_secret" "db_password" {
+  name = "db_password"
+}
+
+resource "aws_secretsmanager_secret_version" "db_password" {
+  secret_id = aws_secretsmanager_secret.db_password.id
+  secret_string = random_password.random_password.result
+}
+
+resource "random_password" "random_password" {
+  length  = 32
+  special = false
+}
+
+#------------------------------------------------------------------------------
+resource "aws_db_instance" "database_2022" {
+#   count = local.is-production ? 1 : 0
+
+  identifier    = "database-v2022"
+  license_model = "license-included"
+  username      = "admin"
+  password      = aws_secretsmanager_secret_version.db_password.secret_string
+
+  engine         = "sqlserver-se"
+  engine_version = "16.00.4105.2.v1"
+  instance_class = "db.m5.large"
+
+  storage_type          = "gp2"
+  allocated_storage     = 100
+  max_allocated_storage = 5000
+  storage_encrypted     = true
+
+  multi_az = false
+
+  db_subnet_group_name   = aws_db_subnet_group.db.id
+  vpc_security_group_ids = [aws_security_group.db.id]
+  publicly_accessible    = true
+  port                   = 1433
+
+  auto_minor_version_upgrade = true
+  skip_final_snapshot        = true
+  maintenance_window         = "Mon:00:00-Mon:03:00"
+  deletion_protection        = false
+
+  option_group_name = aws_db_option_group.sqlserver_backup_restore_2022.name
+
+  iam_database_authentication_enabled = false
+
+  tags = local.tags
+}
+
+#------------------------------------------------------------------------------
+
+resource "aws_security_group" "db" {
+  name        = "database-security-group"
+  description = "Allow DB inbound traffic"
+  vpc_id      = data.aws_vpc.shared.id
+
+  tags = local.tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db_ipv4" {
+  security_group_id = aws_security_group.db.id
+  description       = "Default SQL Server port 1433"
+  ip_protocol       = "tcp"
+  from_port         = 1433
+  to_port           = 1433
+
+  # fy nhy
+  cidr_ipv4 = "46.69.144.146/32"
+}
+
+#------------------------------------------------------------------------------
+
+resource "aws_db_subnet_group" "db" {
+  name       = "db-subnet-group"
+  subnet_ids = data.aws_subnets.shared-public.ids
+  
+  tags = local.tags
+}
+
+#------------------------------------------------------------------------------
+
+resource "aws_db_option_group" "sqlserver_backup_restore_2022" {
+  name                     = "sqlserver-v2022"
+  option_group_description = "SQL server backup restoration using engine 16.x"
+  engine_name              = "sqlserver-se"
+  major_engine_version     = "16.00"
+
+  option {
+    option_name = "SQLSERVER_BACKUP_RESTORE"
+
+    option_settings {
+      name  = "IAM_ROLE_ARN"
+      value = aws_iam_role.s3_database_backups_role.arn
+    }
+  }
+
+  tags = local.tags
+}
+
+# #------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "rds-s3-access-policy" {
+  version = "2012-10-17"
+  statement {
+    sid    = ""
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "rds.amazonaws.com",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "s3_database_backups_role" {
+  name               = "s3-database-backups-role"
+  assume_role_policy = data.aws_iam_policy_document.rds-s3-access-policy.json
+
+  tags = local.tags
+}
+
+data "aws_iam_policy_document" "rds_data_store_access" {
+  statement {
+    sid    = "AllowReadDataStore"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = ["${aws_s3_bucket.data_store.arn}/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "this_transfer_workflow" {
+  role   = aws_iam_role.s3_database_backups_role.name
+  policy = data.aws_iam_policy_document.rds_data_store_access.json
+}
+
+#------------------------------------------------------------------------------
+
+
+
+
+
