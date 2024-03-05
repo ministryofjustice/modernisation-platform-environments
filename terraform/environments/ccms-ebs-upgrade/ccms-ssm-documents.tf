8cd42d172a9b6cf6dc43984bf35e22183364d2ab
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2024-03-05 16:56:53 +0000

    CC-2391: Reorganised SSM documents.

--- terraform/environments/ccms-ebs-upgrade/ccms-ssm-documents.tf
+++ terraform/environments/ccms-ebs-upgrade/ccms-ssm-documents.tf
@@ -1,7 +1,23 @@
-resource "aws_ssm_document" "service_actions" {
-  name            = "ServiceActions"
+resource "aws_ssm_document" "ebs_apps_service_start" {
+  name            = "EBS-Apps-Service-Start"
   document_type   = "Command"
   document_format = "YAML"
 
-  content = file("ccms-ssm-document-service-actions.yaml")
+  content = file("ccms-ssm-document-ebs-apps-service-start.yaml")
+}
+
+resource "aws_ssm_document" "ebs_apps_service_status" {
+  name            = "EBS-Apps-Service-Status"
+  document_type   = "Command"
+  document_format = "YAML"
+
+  content = file("ccms-ssm-document-ebs-apps-service-status.yaml")
+}
+
+resource "aws_ssm_document" "ebs_apps_service_stop" {
+  name            = "EBS-Apps-Service-Stop"
+  document_type   = "Command"
+  document_format = "YAML"
+
+  content = file("ccms-ssm-document-ebs-apps-service-stop.yaml")
 }
