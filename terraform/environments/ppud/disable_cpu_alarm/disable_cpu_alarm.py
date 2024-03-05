a9381f19583a72a8dae11f372c27eebc4e8bb3a6
Buckingham <XC01857@lumen.com>
2024-03-05 14:08:53 +0000

    Update_050324

---
+++ terraform/environments/ppud/disable_cpu_alarm/disable_cpu_alarm.py
@@ -0,0 +1,5 @@
+import boto3
+client = boto3.client('cloudwatch')
+
+def lambda_handler(event):
+    cloudwatch.disable_alarm_actions(AlarmNames=['CPU-High-i-029d2b17679dab982'])
--- terraform/environments/ppud/lambda.tf
+++ terraform/environments/ppud/lambda.tf
@@ -91,3 +91,105 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_start" {
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.start_instance[0].arn
 }
+
+
+#####################################
+# Create a ZIP of Python Applications
+#####################################
+
+data "archive_file" "zip_the_disable_alarm_code" {
+  count       = local.is-production == true ? 1 : 0
+  type        = "zip"
+  source_dir  = "${path.module}/disable_alarm/"
+  output_path = "${path.module}/disable_alarm/disable_cpu_alarm.zip"
+}
+
+data "archive_file" "zip_the_enable_alarm_code" {
+  count       = local.is-production == true ? 1 : 0
+  type        = "zip"
+  source_dir  = "${path.module}/enable_alarm/"
+  output_path = "${path.module}/enable_alarm/enable_cpu_alarm.zip"
+}
+
+
+########################################
+# EventBridge rules to Lambda functions
+########################################
+
+# Eventbridge Rule to Disable_CPU_Alarm
+
+resource "aws_cloudwatch_event_rule" "disable_cpu_alarm" {
+  count               = local.is-production == true ? 1 : 0
+  name                = "disable_cpu_alarm"
+  description         = "Runs Weekly every Saturday at 00:00am GMT"
+  schedule_expression = "cron(0 0 0 ? * SAT *)" # Time Zone is in UTC
+}
+
+resource "aws_cloudwatch_event_target" "trigger_lambda_disable_cpu_alarm" {
+  count     = local.is-production == true ? 1 : 0
+  rule      = aws_cloudwatch_event_rule.disable_cpu_alarm[0].name
+  target_id = "disable_cpu_alarm"
+  arn       = aws_lambda_function.terraform_lambda_disable_cpu_alarm[0].arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_disable_cpu_alarm" {
+  count         = local.is-production == true ? 1 : 0
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_lambda_disable_cpu_alarm[0].function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.disable_cpu_alarm[0].arn
+}
+
+# Eventbridge Rule to Enable_CPU_Alarm
+
+resource "aws_cloudwatch_event_rule" "enable_cpu_alarm" {
+  count               = local.is-production == true ? 1 : 0
+  name                = "enable_cpu_alarm"
+  description         = "Runs Weekly every Sunday at 08:00pm GMT"
+  schedule_expression = "cron(0 0 20 ? * SUN *)" # Time Zone is in UTC
+}
+
+resource "aws_cloudwatch_event_target" "trigger_lambda_enable_cpu_alarm" {
+  count     = local.is-production == true ? 1 : 0
+  rule      = aws_cloudwatch_event_rule.enable_cpu_alarm[0].name
+  target_id = "enable_cpu_alarm"
+  arn       = aws_lambda_function.terraform_lambda_enable_cpu_alarm[0].arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_enable_cpu_alarm" {
+  count         = local.is-production == true ? 1 : 0
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_lambda_enable_cpu_alarm[0].function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.enable_cpu_alarm[0].arn
+}
+
+################################################
+# Lambda Function to Disable and Enable CPU Alarms
+#################################################
+
+# Disable CPU Alarm
+
+resource "aws_lambda_function" "terraform_lambda_disable_cpu_alarm" {
+  count         = local.is-production == true ? 1 : 0
+  filename      = "${path.module}/disable_alarm/disable_cpu_alarm.zip"
+  function_name = "disable_cpu_alarm_function"
+  role          = aws_iam_role.lambda_role[0].arn
+  handler       = "disable_cpu_alarm.lambda_handler"
+  runtime       = "python3.9"
+  depends_on    = [aws_iam_role_policy_attachment.attach_lambda_policy_to_lambda_role]
+}
+
+# Enable CPU Alarm
+
+resource "aws_lambda_function" "terraform_lambda_enable_cpu_alarm" {
+  count         = local.is-production == true ? 1 : 0
+  filename      = "${path.module}/enable_alarm/enable_cpu_alarm.zip"
+  function_name = "enable_cpu_alarm_function"
+  role          = aws_iam_role.lambda_role[0].arn
+  handler       = "enable_cpu_alarm.lambda_handler"
+  runtime       = "python3.9"
+  depends_on    = [aws_iam_role_policy_attachment.attach_lambda_policy_to_lambda_role]
+}
