2383cce4263ec02a42fadbd50b20fa7b7afbd6a3
Buckingham <XC01857@lumen.com>
2024-03-06 12:05:58 +0000

    Update_060324_5

--- terraform/environments/ppud/disable_cpu_alarm/disable_cpu_alarm.py
+++ terraform/environments/ppud/disable_cpu_alarm/disable_cpu_alarm.py
@@ -1,6 +1,7 @@
-import boto3
-region = 'eu-west-2'
-cloudwatch = boto3.client('cloudwatch', region_name=region)
-
-def lambda_handler(event):
-    cloudwatch.disable_alarm_actions(AlarmNames=['CPU-High-i-029d2b17679dab982'])
+import boto3 
+cloudwatch = boto3.client('cloudwatch')
+     
+def lambda_handler(event, context):
+         response = cloudwatch.disable_alarm_actions(
+            AlarmNames=['CPU-High-i-029d2b17679dab982','CPU-High-i-00cbccc46d25e77c6'] 
+    )
