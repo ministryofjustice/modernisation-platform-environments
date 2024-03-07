60f0f194e949f9545fb33b139036b5e83d1b1a4a
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-07 17:30:02 +0000

    url schema

--- terraform/environments/delius-core/modules/delius_environment/pwm.tf
+++ terraform/environments/delius-core/modules/delius_environment/pwm.tf
@@ -62,7 +62,7 @@ module "pwm" {
       value = base64encode(templatefile("${path.module}/templates/PwmConfiguration.xml.tpl", {
         ldap_host_url = "ldap://${module.ldap.nlb_dns_name}:${var.ldap_config.port}"
         ldap_user     = module.ldap.delius_core_ldap_principal_arn
-        pwm_url       = "pwm.${var.env_name}.${var.account_config.dns_suffix}"
+        pwm_url       = "https://pwm.${var.env_name}.${var.account_config.dns_suffix}"
         # email_smtp_address = "smtp.${data.terraform_remote_state.vpc.outputs.private_zone_name}"
         email_smtp_address = "production-smtp-relay-70e032e2738d0a27.elb.eu-west-2.amazonaws.com"
         # email_from_address = "no-reply@${data.terraform_remote_state.vpc.outputs.public_zone_name}"
