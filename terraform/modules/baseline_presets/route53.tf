fb5e041faa4fdb72511220b6a1ef7849b87488b1
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-03-11 17:25:46 +0000

    fix

--- terraform/modules/baseline_presets/route53.tf
+++ terraform/modules/baseline_presets/route53.tf
@@ -19,8 +19,8 @@ locals {
     azure-fixngo-devtest-domain = {
       domain_name = "azure.noms.root"
       target_ips = flatten([
-        module.ip_addresses.azure_fixngo_ips.devtest_domain_controllers,
-        module.ip_addresses.mp_ips.ad_fixngo_azure_domain_controllers,
+        var.ip_addresses.azure_fixngo_ips.devtest_domain_controllers,
+        var.ip_addresses.mp_ips.ad_fixngo_azure_domain_controllers,
       ])
       rule_type = "FORWARD"
     }
