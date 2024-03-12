0c5cf2eb29b135f9fae9c30b2db6653999c74524
pavmoj <142988272+pavmoj@users.noreply.github.com>
2024-03-12 14:00:57 +0000

    Fix broken s3 bucket policy (#5322)
    * Updated patch module
    
    * Clean up
    
    * Fix broken policy error
    
    * Fix broken policy error
    
    ---------
    
    Co-authored-by: pav <pav.gainda@version1.com>

--- terraform/modules/patch_manager/s3.tf
+++ terraform/modules/patch_manager/s3.tf
@@ -26,10 +26,13 @@ data "aws_iam_policy_document" "bucket_policy_patch_access" {
 
     principals {
       identifiers = [
-        "arn:aws:iam::161282055413:role/ec2-instance*",
-        "arn:aws:iam::139351334100:role/ec2-instance*",
-        "arn:aws:iam::228371063224:role/ec2-instance*",
-        "arn:aws:iam::905761223702:role/ec2-instance*"
+        "arn:aws:iam::139351334100:role/ec2-instance-role-dev-win-2022",
+        "arn:aws:iam::161282055413:role/ec2-instance-role-test-win-2022",
+        "arn:aws:iam::228371063224:role/ec2-instance-role-pp-rdgw-1-a",
+        "arn:aws:iam::228371063224:role/ec2-instance-role-pp-rds-1-a",
+        "arn:aws:iam::905761223702:role/ec2-instance-role-pd-rdgw-1-a",
+        "arn:aws:iam::905761223702:role/ec2-instance-role-pd-rdgw-1-b",
+        "arn:aws:iam::905761223702:role/ec2-instance-role-pd-rds-1-a"
       ]
       type = "AWS"
     }
