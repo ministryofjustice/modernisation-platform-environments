57cd4556b568b3cfa0b0a1d231d156e881fcdff5
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2024-03-12 00:37:49 +0000

    Add SSM Agent and User

---
+++ terraform/environments/digital-prison-reporting/modules/compute_node/scripts/operational-db.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# update system packages
+yum update -y
+
+# enable repository to install postgresql
+amazon-linux-extras enable postgresql14
+
+# Install PostgreSQL server and initialize the database 
+# cluster for this server
+yum install postgresql-server postgresql-devel -y
+#/usr/bin/postgresql-setup --initdb
+sudo postgresql-setup initdb
+
+
+# Update PostgreSQL authentication config file
+# Add the following to the end of the file to allow client connections to all databases.
+echo -e "host    all          all            0.0.0.0/0  md5" >> /var/lib/pgsql/data/pg_hba.conf
+
+# OR
+# Backup PostgreSQL authentication config file
+#mv /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.bak
+
+# Create our new PostgreSQL authentication config file
+#cat <<'EOF' > /var/lib/pgsql/data/pg_hba.conf
+#${pg_hba_file}
+#EOF
+
+# Update the IPs of the address to listen from PostgreSQL config
+sed -i "59i listen_addresses = '*'" /var/lib/pgsql/data/postgresql.conf
+
+# Set Password for POSTGRES, Root user
+sudo -Hiu postgres psql -c "ALTER USER postgres PASSWORD '${POSTGRES_PASS}';"
+
+# Start the db service
+systemctl enable postgresql
+systemctl start postgresql
+systemctl status postgresql
---
+++ terraform/environments/digital-prison-reporting/operational_db.tf
@@ -0,0 +1,40 @@
+module "domain_builder_cli_agent" {
+  source = "./modules/compute_node"
+
+  enable_compute_node         = true
+  name                        = "${local.project}-operational-db-server-${local.env}"
+  description                 = "Operational DB Instance"
+  vpc                         = data.aws_vpc.shared.id
+  cidr                        = [data.aws_vpc.shared.cidr_block]
+  subnet_ids                  = data.aws_subnet.private_subnets_a.id
+  ec2_instance_type           = local.instance_type
+  ami_image_id                = local.image_id
+  aws_region                  = local.account_region
+  ec2_terminate_behavior      = "terminate"
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  monitoring                  = true
+  ebs_size                    = 20
+  ebs_encrypted               = true
+  ebs_delete_on_termination   = false
+  policies                    = ["arn:aws:iam::${local.account_id}:policy/${local.s3_read_access_policy}", "arn:aws:iam::${local.account_id}:policy/${local.kms_read_access_policy}", "arn:aws:iam::${local.account_id}:policy/${local.apigateway_get_policy}", ]
+  region                      = local.account_region
+  account                     = local.account_id
+  env                         = local.env
+  app_key                     = "operational-db"
+
+  env_vars = {
+    POSTGRES_PASS     = "postgres" # WEAK, WIP
+    ENV               = local.env
+  }
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name           = "${local.project}-operational-db-server-${local.env}"
+      Resource_Type  = "EC2 Instance"
+      Resource_Group = "operational-db"
+      Name           = "${local.project}-operational-db-server-${local.env}"
+    }
+  )
+}
