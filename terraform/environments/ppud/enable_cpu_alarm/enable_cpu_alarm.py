94a58cb54acfe97bc2af05f9cb793bfac8ff1849
Buckingham <XC01857@lumen.com>
2024-03-13 10:35:00 +0000

    Update_130324

--- terraform/environments/ppud/enable_cpu_alarm/enable_cpu_alarm.py
+++ terraform/environments/ppud/enable_cpu_alarm/enable_cpu_alarm.py
@@ -1,7 +1,7 @@
 import boto3 
-cloudwatch = boto3.client('cloudwatch')
+client = boto3.client('cloudwatch')
      
 def lambda_handler(event, context):
-         response = cloudwatch.enable_alarm_actions(
+         response = client.enable_alarm_actions(
             AlarmNames=['CPU-High-i-029d2b17679dab982','CPU-High-i-00cbccc46d25e77c6'] 
     )
