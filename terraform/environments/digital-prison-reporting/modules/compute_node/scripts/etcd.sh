7c4b34c790b8715456d37044f49f2b14e3ce05df
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-03-13 12:15:11 +0000

    DPR2-463: Rising wave dev deployment.

---
+++ terraform/environments/digital-prison-reporting/modules/compute_node/scripts/etcd.sh
@@ -0,0 +1,104 @@
+#!/bin/bash -xe
+# send script output to /tmp so we can debug boot failures
+# Ouput all log
+exec > >(tee /tmp/userdata.log|logger -t user-data-extra -s 2>/dev/console) 2>&1
+
+# Install AWS CLI Libs
+echo "Seup AWSCLI V2....."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+# Part of temporary service discovery
+# Remove the etcd host file so risingwave nodes won't use the old file on a redeploy
+aws s3 rm s3://dpr-working-development/rising-wave/hosts/risingwave_etcd.txt
+
+echo "assumeyes=1" >> /etc/yum.conf
+
+# Update all packages
+sudo yum -y update
+
+if grep ssm-user /etc/passwd &> /dev/null;
+then
+  echo "ssm-user already exists - skipping create"
+else
+  # Create the ssm-user using system defaults.
+  # See /etc/default/useradd
+  echo "ssm-user does not exist - creating"
+  sudo useradd ssm-user --create-home
+  echo "ssm-user created"
+
+  # TODO: Remove temporary NOPASSWD used for dev
+  cd /etc/sudoers.d
+  echo "ssm-user ALL=(ALL) NOPASSWD:ALL" > ssm-agent-users
+fi
+
+# install etcd
+
+sudo groupadd -f etcd
+sudo useradd -d /opt/etcd -s /bin/false -g etcd etcd
+cd ~
+
+ETCD_VER=v3.5.12
+
+# choose either URL
+GOOGLE_URL=https://storage.googleapis.com/etcd
+GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
+DOWNLOAD_URL=$GOOGLE_URL
+
+sudo rm -f /tmp/etcd-$ETCD_VER-linux-amd64.tar.gz
+sudo rm -rf /opt/etcd && mkdir -p /opt/etcd
+
+curl -L $DOWNLOAD_URL/$ETCD_VER/etcd-$ETCD_VER-linux-amd64.tar.gz -o /tmp/etcd-$ETCD_VER-linux-amd64.tar.gz
+tar xzvf /tmp/etcd-$ETCD_VER-linux-amd64.tar.gz -C /opt/etcd --strip-components=1
+rm -f /tmp/etcd-$ETCD_VER-linux-amd64.tar.gz
+
+sudo chown -R etcd:etcd /opt/etcd
+
+ETCD_HOST_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+ETCD_NAME=$(hostname -s)
+
+
+service_file_contents=`cat << EOF
+[Unit]
+Description=etcd service
+Documentation=https://github.com/coreos/etcd
+
+[Service]
+User=etcd
+Type=notify
+ExecStart=/opt/etcd/etcd \\
+ --name $ETCD_NAME \\
+ --data-dir /opt/etcd/data \\
+ --initial-advertise-peer-urls http://$ETCD_HOST_IP:2380 \\
+ --listen-peer-urls http://$ETCD_HOST_IP:2380 \\
+ --listen-client-urls http://$ETCD_HOST_IP:2379,http://127.0.0.1:2379 \\
+ --advertise-client-urls http://$ETCD_HOST_IP:2379 \\
+ --initial-cluster-token etcd-cluster-1 \\
+ --initial-cluster $ETCD_NAME=http://$ETCD_HOST_IP:2380 \\
+ --initial-cluster-state new \\
+ --heartbeat-interval 1000 \\
+ --election-timeout 5000
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF`
+
+echo "$service_file_contents" | sudo tee /lib/systemd/system/etcd.service > /dev/null
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable etcd
+sudo systemctl start etcd.service
+sudo systemctl status -l etcd.service
+
+# Part of temporary service discovery
+# Write a file to S3 for now and rely on timings
+hostname -s >risingwave_etcd.txt
+aws s3 cp ./risingwave_etcd.txt s3://dpr-working-development/rising-wave/hosts/risingwave_etcd.txt
+echo "Wrote etcd host file to s3 at $(date)"
+rm -f ./risingwave_etcd.txt
+
+echo "Finished setup"
---
+++ terraform/environments/digital-prison-reporting/rising-wave.tf
@@ -0,0 +1,297 @@
+module "rising_wave_etcd" {
+  source = "./modules/compute_node"
+
+  enable_compute_node         = true
+  name                        = "${local.project}-rising-wave-etcd-${local.env}"
+  description                 = "Rising Wave etcd dependency"
+  vpc                         = data.aws_vpc.shared.id
+  cidr                        = [data.aws_vpc.shared.cidr_block]
+  subnet_ids                  = data.aws_subnet.private_subnets_a.id
+  ec2_instance_type           = local.instance_type
+  ami_image_id                = local.image_id
+  aws_region                  = local.account_region
+  ec2_terminate_behavior      = "terminate"
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  monitoring                  = true
+  ebs_size                    = 20
+  ebs_encrypted               = true
+  ebs_delete_on_termination   = false
+  policies                    = [
+    "arn:aws:iam::${local.account_id}:policy/${local.s3_all_object_actions_policy}",
+    "arn:aws:iam::${local.account_id}:policy/${local.kms_read_access_policy}",
+  ]
+  region                      = local.account_region
+  account                     = local.account_id
+  env                         = local.env
+  app_key                     = "etcd"
+
+  ec2_sec_rules = {
+    "ETCD" = {
+      "from_port" = 2379,
+      "to_port"   = 2380,
+      "protocol"  = "TCP"
+    }
+  }
+
+  env_vars = {
+    ENV = local.env
+  }
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name           = "${local.project}-rising-wave-etcd-${local.env}"
+      Resource_Type  = "EC2 Instance"
+      Resource_Group = "rising-wave"
+      Jira           = "DPR2-463"
+    }
+  )
+
+  depends_on = [
+    aws_iam_policy.kms_read_access_policy,
+    aws_iam_policy.s3_all_object_actions_policy,
+  ]
+
+}
+
+# The central metadata management service
+module "rising_wave_meta_node" {
+  source = "./modules/compute_node"
+
+  enable_compute_node         = true
+  name                        = "${local.project}-rising-wave-meta-node-${local.env}"
+  description                 = "Rising Wave Meta node"
+  vpc                         = data.aws_vpc.shared.id
+  cidr                        = [data.aws_vpc.shared.cidr_block]
+  subnet_ids                  = data.aws_subnet.private_subnets_a.id
+  ec2_instance_type           = local.instance_type
+  ami_image_id                = local.image_id
+  aws_region                  = local.account_region
+  ec2_terminate_behavior      = "terminate"
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  monitoring                  = true
+  ebs_size                    = 20
+  ebs_encrypted               = true
+  ebs_delete_on_termination   = false
+  policies                    = [
+    "arn:aws:iam::${local.account_id}:policy/${local.s3_all_object_actions_policy}",
+    "arn:aws:iam::${local.account_id}:policy/${local.kms_read_access_policy}",
+  ]
+  region                      = local.account_region
+  account                     = local.account_id
+  env                         = local.env
+  app_key                     = "rising-wave"
+
+  ec2_sec_rules = {
+    "RISINGWAVE_META" = {
+      "from_port" = 5690,
+      "to_port"   = 5690,
+      "protocol"  = "TCP"
+    }
+    "RISINGWAVE_DASHBOARD" = {
+      "from_port" = 5691,
+      "to_port"   = 5691,
+      "protocol"  = "TCP"
+    }
+  }
+
+  env_vars = {
+    ENV                   = local.env
+    RISING_WAVE_NODE_TYPE = "meta"
+  }
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name           = "${local.project}-rising-wave-meta-node-${local.env}"
+      Resource_Type  = "EC2 Instance"
+      Resource_Group = "rising-wave"
+      Jira           = "DPR2-463"
+    }
+  )
+
+  depends_on = [
+    aws_iam_policy.kms_read_access_policy,
+    aws_iam_policy.s3_all_object_actions_policy,
+  ]
+
+}
+
+# The worker nodes that execute query plans and handles data ingestion and output
+module "rising_wave_compute_nodes" {
+  source = "./modules/compute_node"
+
+  enable_compute_node         = true
+  name                        = "${local.project}-rising-wave-compute-node-${local.env}"
+  description                 = "Rising Wave Compute Nodes"
+  vpc                         = data.aws_vpc.shared.id
+  cidr                        = [data.aws_vpc.shared.cidr_block]
+  subnet_ids                  = data.aws_subnet.private_subnets_a.id
+  ec2_instance_type           = local.instance_type
+  ami_image_id                = local.image_id
+  aws_region                  = local.account_region
+  ec2_terminate_behavior      = "terminate"
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  monitoring                  = true
+  ebs_size                    = 20
+  ebs_encrypted               = true
+  ebs_delete_on_termination   = false
+  policies                    = [
+    "arn:aws:iam::${local.account_id}:policy/${local.s3_all_object_actions_policy}",
+    "arn:aws:iam::${local.account_id}:policy/${local.kms_read_access_policy}",
+  ]
+  region                      = local.account_region
+  account                     = local.account_id
+  env                         = local.env
+  app_key                     = "rising-wave"
+
+  ec2_sec_rules = {
+    "RISINGWAVE_COMPUTE" = {
+      "from_port" = 5688,
+      "to_port"   = 5688,
+      "protocol"  = "TCP"
+    }
+  }
+
+  env_vars = {
+    ENV                   = local.env
+    RISING_WAVE_NODE_TYPE = "compute"
+  }
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name           = "${local.project}-rising-wave-compute-node-${local.env}"
+      Resource_Type  = "EC2 Instance"
+      Resource_Group = "rising-wave"
+      Jira           = "DPR2-463"
+    }
+  )
+
+  depends_on = [
+    aws_iam_policy.kms_read_access_policy,
+    aws_iam_policy.s3_all_object_actions_policy,
+  ]
+
+}
+
+# The stateless worker node that compacts data for the storage engine
+module "rising_wave_compactor_node" {
+  source = "./modules/compute_node"
+
+  enable_compute_node         = true
+  name                        = "${local.project}-rising-wave-compactor-node-${local.env}"
+  description                 = "Rising Wave Compactor Node"
+  vpc                         = data.aws_vpc.shared.id
+  cidr                        = [data.aws_vpc.shared.cidr_block]
+  subnet_ids                  = data.aws_subnet.private_subnets_a.id
+  ec2_instance_type           = local.instance_type
+  ami_image_id                = local.image_id
+  aws_region                  = local.account_region
+  ec2_terminate_behavior      = "terminate"
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  monitoring                  = true
+  ebs_size                    = 20
+  ebs_encrypted               = true
+  ebs_delete_on_termination   = false
+  policies                    = [
+    "arn:aws:iam::${local.account_id}:policy/${local.s3_all_object_actions_policy}",
+    "arn:aws:iam::${local.account_id}:policy/${local.kms_read_access_policy}",
+  ]
+  region                      = local.account_region
+  account                     = local.account_id
+  env                         = local.env
+  app_key                     = "rising-wave"
+
+  ec2_sec_rules = {
+    "RISINGWAVE_COMPACTOR" = {
+      "from_port" = 6660,
+      "to_port"   = 6660,
+      "protocol"  = "TCP"
+    }
+  }
+
+  env_vars = {
+    ENV                   = local.env
+    RISING_WAVE_NODE_TYPE = "compactor"
+  }
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name           = "${local.project}-rising-wave-compactor-node-${local.env}"
+      Resource_Type  = "EC2 Instance"
+      Resource_Group = "rising-wave"
+      Jira           = "DPR2-463"
+    }
+  )
+
+  depends_on = [
+    aws_iam_policy.kms_read_access_policy,
+    aws_iam_policy.s3_all_object_actions_policy,
+  ]
+
+}
+
+# The stateless proxy that parses SQL queries and performs planning and optimizations of query jobs
+module "rising_wave_frontend_node" {
+  source = "./modules/compute_node"
+
+  enable_compute_node         = true
+  name                        = "${local.project}-rising-wave-frontend-node-${local.env}"
+  description                 = "Rising Wave Frontend Node"
+  vpc                         = data.aws_vpc.shared.id
+  cidr                        = [data.aws_vpc.shared.cidr_block]
+  subnet_ids                  = data.aws_subnet.private_subnets_a.id
+  ec2_instance_type           = local.instance_type
+  ami_image_id                = local.image_id
+  aws_region                  = local.account_region
+  ec2_terminate_behavior      = "terminate"
+  associate_public_ip_address = false
+  ebs_optimized               = true
+  monitoring                  = true
+  ebs_size                    = 20
+  ebs_encrypted               = true
+  ebs_delete_on_termination   = false
+  policies                    = [
+    "arn:aws:iam::${local.account_id}:policy/${local.s3_all_object_actions_policy}",
+    "arn:aws:iam::${local.account_id}:policy/${local.kms_read_access_policy}",
+  ]
+  region                      = local.account_region
+  account                     = local.account_id
+  env                         = local.env
+  app_key                     = "rising-wave"
+
+  ec2_sec_rules = {
+    "RISINGWAVE_FRONTEND" = {
+      "from_port" = 4566,
+      "to_port"   = 4566,
+      "protocol"  = "TCP"
+    }
+  }
+
+  env_vars = {
+    ENV                   = local.env
+    RISING_WAVE_NODE_TYPE = "frontend"
+  }
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name           = "${local.project}-rising-wave-frontend-node-${local.env}"
+      Resource_Type  = "EC2 Instance"
+      Resource_Group = "rising-wave"
+      Jira           = "DPR2-463"
+    }
+  )
+
+  depends_on = [
+    aws_iam_policy.kms_read_access_policy,
+    aws_iam_policy.s3_all_object_actions_policy,
+  ]
+
+}
