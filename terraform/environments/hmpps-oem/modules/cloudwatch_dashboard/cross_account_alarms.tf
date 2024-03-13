d606b36152d7f6087402c4e03f8b1969e2671a25
Shajida Akthar <Shajida Akthar>
2024-03-13 00:34:08 +0000

    oasys alarms in hmpps-oem

--- terraform/environments/hmpps-oem/modules/cloudwatch_dashboard/cross_account_alarms.tf
+++ terraform/environments/hmpps-oem/modules/cloudwatch_dashboard/cross_account_alarms.tf
@@ -1,3 +1,4 @@
+# remove hardcoded values and modularise
 resource "aws_cloudwatch_metric_alarm" "nomis-cpu-util" {
     alarm_name = "nomis-cpu-util"
     comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -16,7 +17,31 @@ resource "aws_cloudwatch_metric_alarm" "nomis-cpu-util" {
             period = "60"
             stat = "Maximum"
             dimensions = {
-                InstanceId = "*"
+                InstanceId = "i-03389635247630d25"
+            }
+        }
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "oasys-cpu-util" {
+    alarm_name = "oasys-cpu-util"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods  = "15"
+    datapoints_to_alarm = "15"
+    threshold           = "95"
+    alarm_description   = "Triggers if the average cpu remains at 95% utilization or above for 15 minutes. See https://dsdmoj.atlassian.net/wiki/spaces/DSTT/pages/4326064583"
+
+    metric_query {
+        id = "m1"
+        account_id = "546088120047"
+        return_data = "true"
+        metric {
+            metric_name = "CPUUtilization"
+            namespace = "AWS/EC2"
+            period = "60"
+            stat = "Maximum"
+            dimensions = {
+                InstanceId = "i-0752b4bfbacf4c9e9"
             }
         }
     }
