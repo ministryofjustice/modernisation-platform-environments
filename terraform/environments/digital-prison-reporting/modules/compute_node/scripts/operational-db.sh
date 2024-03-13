faac3f66fd3cef350e7dbc9fbb20d6525ca417ea
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2024-03-13 23:35:02 +0000

    Add PG_IVM

--- terraform/environments/digital-prison-reporting/modules/compute_node/scripts/operational-db.sh
+++ terraform/environments/digital-prison-reporting/modules/compute_node/scripts/operational-db.sh
@@ -7,23 +7,13 @@ amazon-linux-extras enable postgresql14
 
 # Install PostgreSQL server and initialize the database 
 # cluster for this server
-yum install postgresql-server postgresql-devel -y
+yum install postgresql-server postgresql-devel postgresql-server-devel git -y
 #/usr/bin/postgresql-setup --initdb
 sudo postgresql-setup initdb
-
 
 # Update PostgreSQL authentication config file
 # Add the following to the end of the file to allow client connections to all databases.
 echo -e "host    all          all            0.0.0.0/0  md5" >> /var/lib/pgsql/data/pg_hba.conf
-
-# OR
-# Backup PostgreSQL authentication config file
-#mv /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.bak
-
-# Create our new PostgreSQL authentication config file
-#cat <<'EOF' > /var/lib/pgsql/data/pg_hba.conf
-#<<pg_hba_file>>
-#EOF
 
 # Update the IPs of the address to listen from PostgreSQL config
 sed -i "59i listen_addresses = '*'" /var/lib/pgsql/data/postgresql.conf
@@ -34,4 +24,13 @@ sudo -Hiu postgres psql -c "ALTER USER postgres PASSWORD '${POSTGRES_PASS}';"
 # Start the db service
 systemctl enable postgresql
 systemctl start postgresql
+
+# Setup PG_IVM
+git clone https://github.com/sraoss/pg_ivm.git
+cd pg_ivm; make install Makefile
+
+# Enable pg_ivm Extension
+sudo -Hiu postgres psql -c "CREATE EXTENSION pg_ivm;"
+
+# Check POSTGRES Service Status
 systemctl status postgresql
