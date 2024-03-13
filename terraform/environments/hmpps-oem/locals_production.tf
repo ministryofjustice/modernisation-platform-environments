b53688ee175868be5d9a61c354b97c0a56627ee2
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-03-13 10:35:06 +0000

    Oem: DSOS-2671: enable monitoring part2 (#5336)
    * Add SNS topic
    
    * add alarms
    
    * tweak oem alarms

--- terraform/environments/hmpps-oem/locals_production.tf
+++ terraform/environments/hmpps-oem/locals_production.tf
@@ -7,7 +7,13 @@ locals {
   }
 
   # baseline presets config
-  production_baseline_presets_options = {}
+  production_baseline_presets_options = {
+    sns_topics = {
+      pagerduty_integrations = {
+        dba_pagerduty = "hmpps_shef_dba_low_priority"
+      }
+    }
+  }
 
   # baseline config
   production_config = {
@@ -20,6 +26,10 @@ locals {
 
     baseline_ec2_instances = {
       prod-oem-a = merge(local.oem_ec2_default, {
+        cloudwatch_metric_alarms = merge(
+          local.oem_ec2_cloudwatch_metric_alarms.standard,
+          local.oem_ec2_cloudwatch_metric_alarms.backup,
+        )
         config = merge(local.oem_ec2_default.config, {
           availability_zone = "eu-west-2a"
         })
