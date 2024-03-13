040d38b65c20cb8cca9576225555c34c10b07da6
Fawaz Shafaat <fawaz.shafaat@digital.justice.gov.uk>
2024-03-13 16:38:16 +0000

    Added route to new domain

--- terraform/environments/cdpt-ifs/route53.tf
+++ terraform/environments/cdpt-ifs/route53.tf
@@ -2,10 +2,10 @@
 
 // ACM Public Certificate
 resource "aws_acm_certificate" "external" {
-  domain_name       = local.is-production ? "integratedfraudsystem.justice.gov.uk" : "modernisation-platform.service.justice.gov.uk"
+  domain_name       = local.is-production ? "integrated-fraud-system.service.justice.gov.uk" : "modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
-  subject_alternative_names = local.is-production ? ["integratedfraudsystem.justice.gov.uk"] : ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  subject_alternative_names = local.is-production ? ["integrated-fraud-system.service.justice.gov.uk"] : ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   tags = {
     Environment = local.environment
   }
@@ -66,7 +66,7 @@ resource "aws_route53_record" "external" {
 resource "aws_acm_certificate" "external_prod" {
   count = local.is-production ? 1 : 0
 
-  domain_name       = "integratedfraudsystem.justice.gov.uk"
+  domain_name       = "integrated-fraud-system.service.justice.gov.uk"
   validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
@@ -97,18 +97,18 @@ resource "aws_route53_record" "external_validation_prod" {
 }
 
 // Route53 DNS record for directing traffic to the service
-// resource "aws_route53_record" "external_prod" {
-//   count    = local.is-production ? 1 : 0
-//   provider = aws.core-network-services
+resource "aws_route53_record" "external_prod" {
+  count    = local.is-production ? 1 : 0
+  provider = aws.core-network-services
 
-//   zone_id = data.aws_route53_zone.application_zone.zone_id
-//   name    = "integratedfraudsystem.justice.gov.uk"
-//   type    = "A"
+  zone_id = data.aws_route53_zone.application_zone.zone_id
+  name    = "integrated-fraud-system.service.justice.gov.uk"
+  type    = "A"
 
-//   alias {
-//     name                   = aws_lb.ifs_lb.dns_name
-//     zone_id                = aws_lb.ifs_lb.zone_id
-//     evaluate_target_health = true
-//   }
-// }
+  alias {
+    name                   = aws_lb.ifs_lb.dns_name
+    zone_id                = aws_lb.ifs_lb.zone_id
+    evaluate_target_health = true
+  }
+}
 
