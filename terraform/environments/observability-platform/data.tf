5abafc9ebafe63367db5969d34e7b4c7d16b6e4e
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-03-14 19:30:55 +0000

    Getting names for migration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/data.tf
+++ terraform/environments/observability-platform/data.tf
@@ -6,27 +6,62 @@ data "aws_ssoadmin_instances" "main" {
   provider = aws.sso-readonly
 }
 
-data "aws_identitystore_group" "this" {
+data "aws_identitystore_group" "observability_platform" {
   provider = aws.sso-readonly
 
   identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
 
-  # This fails with the following error:
-  #   Error: reading AWS SSO Identity Store Group Data Source (d-XXXXXX): operation error identitystore: GetGroupId, https response error StatusCode: 400, RequestID: 059df12d-84ce-4803-9a6b-0d41624d749f, ResourceNotFoundException: Group not found
-  # alternate_identifier {
-  #   unique_attribute {
-  #     attribute_path  = "DisplayName"
-  #     attribute_value = "analytical-platform"
-  #   }
-  # }
-
-  # This is deprecated, but @dms1981 said it works...
   filter {
-    attribute_path  = "DisplayName"
-    attribute_value = "analytical-platform"
+    attribute_path  = "GroupId"
+    attribute_value = "16a2d234-1031-70b5-2657-7f744c55e48f"
   }
 }
 
-output "name" {
-  value = data.aws_identitystore_group.this.id
+output "observability_platform_display_name" {
+  value = data.aws_identitystore_group.observability_platform.display_name
+}
+
+data "aws_identitystore_group" "analytical_platform" {
+  provider = aws.sso-readonly
+
+  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
+
+  filter {
+    attribute_path  = "GroupId"
+    attribute_value = "9c6710dd7f-e2cdaf44-0510-48cd-8bb1-4b21552ae0f1"
+  }
+}
+
+output "analytical_platform_display_name" {
+  value = data.aws_identitystore_group.analytical_platform.display_name
+}
+
+data "aws_identitystore_group" "data_platform" {
+  provider = aws.sso-readonly
+
+  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
+
+  filter {
+    attribute_path  = "GroupId"
+    attribute_value = "a68242b4-b0a1-7085-25f4-dc60e4c122c0"
+  }
+}
+
+output "data_platform_display_name" {
+  value = data.aws_identitystore_group.data_platform.display_name
+}
+
+data "aws_identitystore_group" "dso" {
+  provider = aws.sso-readonly
+
+  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
+
+  filter {
+    attribute_path  = "GroupId"
+    attribute_value = "9c6710dd7f-120a1f73-34c1-447a-b34c-6cdc2cd64b5e"
+  }
+}
+
+output "dso_display_name" {
+  value = data.aws_identitystore_group.data_platform.display_name
 }
