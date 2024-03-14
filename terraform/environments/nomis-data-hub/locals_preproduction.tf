94779d03cff9fd2fb795a33fa64608fc7da1ad1e
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-03-14 10:58:56 +0000

    ndh: add preprod secrets (#5357)

--- terraform/environments/nomis-data-hub/locals_preproduction.tf
+++ terraform/environments/nomis-data-hub/locals_preproduction.tf
@@ -1,5 +1,26 @@
 locals {
   preproduction_config = {
+    baseline_secretsmanager_secrets = {
+      "/ndh/pp" = local.ndh_secretsmanager_secrets
+    }
+
+    baseline_iam_policies = {
+      Ec2ppPolicy = {
+        description = "Permissions required for PP EC2s"
+        statements = [
+          {
+            effect = "Allow"
+            actions = [
+              "secretsmanager:GetSecretValue",
+            ]
+            resources = [
+              "arn:aws:secretsmanager:*:*:secret:/ndh/pp/*",
+            ]
+          }
+        ]
+      }
+    }
+
     baseline_route53_zones = {
       "preproduction.ndh.nomis.service.justice.gov.uk" = {
         records = []
