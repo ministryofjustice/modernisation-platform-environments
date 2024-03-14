30441e39a9d371ccc74f093153136e307efa203d
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-03-14 15:16:34 +0000

    change module name

--- terraform/environments/tribunals/rds.tf
+++ terraform/environments/tribunals/rds.tf
@@ -83,7 +83,7 @@ resource "aws_security_group" "sqlserver_db_sc" {
     to_port         = 1433
     protocol        = "tcp"
     description     = "Allows Administrative Appeals ECS service to access RDS"
-    security_groups = [module.appeals-ecs.cluster_ec2_security_group_id]
+    security_groups = [module.appeals.cluster_ec2_security_group_id]
   }
   # ingress {
   #   from_port       = 1433
