f0ebc9f9e99658be6dcdb245c9b1f22d547ab248
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-03-14 17:06:04 +0000

    add missing ecs_security_group_id to transport module

--- terraform/environments/tribunals/main.tf
+++ terraform/environments/tribunals/main.tf
@@ -39,6 +39,7 @@ module "transport" {
   vpc_shared_id                     = data.aws_vpc.shared.id 
   subnets_shared_public_ids         = data.aws_subnets.shared-public.ids
   aws_acm_certificate_external      = aws_acm_certificate.external
+  ecs_security_group_id             = aws_security_group.ecs_service.id
 }
 
 module "appeals" {
