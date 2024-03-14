ed8a3680685b1c5eb35e205ede1131ecbfd7ad1b
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2024-03-14 11:08:42 +0000

    add_ndh_pp_vms

--- terraform/environments/nomis-data-hub/locals_preproduction.tf
+++ terraform/environments/nomis-data-hub/locals_preproduction.tf
@@ -21,6 +21,39 @@ locals {
       }
     }
 
+    baseline_ec2_instances = {
+
+      preprodution-management-server-2022 = merge(local.management_server_2022, {
+        tags = merge(local.management_server_2022.tags, {
+          nomis-data-hub-environment = "preprodution"
+        })
+      })
+
+      pp-ndh-app-a = merge(local.ndh_app_a, {
+        config = merge(local.ndh_app_a.config, {
+          instance_profile_policies = concat(local.ndh_app_a.config.instance_profile_policies, [
+            "Ec2t1Policy",
+          ])
+        })
+        tags = merge(local.ndh_app_a.tags, {
+          os-type                    = "Linux"
+          nomis-data-hub-environment = "pp"
+        })
+      })
+
+      pp-ndh-ems-a = merge(local.ndh_ems_a, {
+        config = merge(local.ndh_ems_a.config, {
+          instance_profile_policies = concat(local.ndh_ems_a.config.instance_profile_policies, [
+            "Ec2t1Policy",
+          ])
+        })
+        tags = merge(local.ndh_ems_a.tags, {
+          os-type                    = "Linux"
+          nomis-data-hub-environment = "pp"
+        })
+      })
+    }
+
     baseline_route53_zones = {
       "preproduction.ndh.nomis.service.justice.gov.uk" = {
         records = []
