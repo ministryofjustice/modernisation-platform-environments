231b26ab56fcd3b0a1ee01a2f20b3e8f595fe7ae
Robert Sweetman <robertiansweetman@gmail.com>
2024-03-15 13:48:01 +0000

    exclude rman backup alarms from the backup database deployment (#5386)

--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -68,6 +68,7 @@ locals {
             "Ec2PreprodDatabasePolicy",
           ])
         })
+        cloudwatch_metric_alarms = local.ec2_cloudwatch_metric_alarms.database # inc. ec2_instance_cwagent_collectd_oracle_db_backup alarms
         instance = merge(local.defaults_database_ec2.instance, {
           instance_type                = "r6i.xlarge"
           metadata_options_http_tokens = "optional" # the Oracle installer cannot accommodate a token
