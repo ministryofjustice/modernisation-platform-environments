6926973b3e58dfc9a9ef20cb9cc0b993e5443a17
matt-heery <matt.heery@digital.justice.gov.uk>
2024-03-15 11:55:57 +0000

    adding glue conn defined by tf

---
+++ terraform/environments/electronic-monitoring-data/rds_to_parquet.tf
@@ -0,0 +1,25 @@
+resource "aws_glue_crawler" "rds_to_parquet" {
+  database_name = aws_glue_catalog_database.rds_to_parquet.name
+  name          = "rds_to_parquet"
+  role          = aws_iam_role.rds_to_parquet.arn
+
+  jdbc_target {
+    connection_name = aws_glue_connection.rds_to_parquet.name
+    path            = "test"
+  }
+}
+
+resource "aws_glue_connection" "rds_to_parquet" {
+  connection_properties = {
+    JDBC_CONNECTION_URL = "jdbc:sqlserver:// ${aws_db_instance.database_2022.address}:${aws_db_instance.db.endpoint};database=${aws_db_instance.database_2022.db_name}"
+    PASSWORD            = "examplepassword"
+    USERNAME            = "exampleusername"
+  }
+
+  name = "rds_to_parquet"
+
+  physical_connection_requirements {
+    security_group_id_list = [aws_security_group.db.id]
+    subnet_id              = tolist(data.aws_subnets.shared-public.ids)[0]
+  }
+}
