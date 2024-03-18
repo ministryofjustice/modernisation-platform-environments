b09218a37656ab1a75024f4d2a14bad5d09e5c51
matt-heery <matt.heery@digital.justice.gov.uk>
2024-03-18 12:37:44 +0000

    lambda to get all table names from schema

---
+++ terraform/environments/electronic-monitoring-data/get_table_names.py
@@ -0,0 +1,31 @@
+import boto3
+
+
+def get_table_and_database_names():
+    client = boto3.client("glue")
+
+    response_get_databases = client.get_databases()
+
+    database_list = response_get_databases["DatabaseList"]
+    names = {}
+    for database_dict in database_list:
+
+        database_name = database_dict["Name"]
+        names[database_name] = []
+
+        response_get_tables = client.get_tables(databaseName=database_name)
+        table_list = response_get_tables["TableList"]
+
+        for table_dict in table_list:
+            names[database_name].append(table_dict["Name"])
+        return names
+
+
+def lambda_handler(event, context):
+    names = get_table_and_database_names()
+    args = [
+        {"database_name": database_name, "table_name": table_name}
+        for database_name in names
+        for table_name in names[database_name]
+    ]
+    return {"statusCode": 200, "arguments": args}
---
+++ terraform/environments/electronic-monitoring-data/iterate_over_tables.tf
@@ -0,0 +1,170 @@
+#------------------------------------------------------------------------------
+# Lambda function and IAM role definition
+#------------------------------------------------------------------------------
+data "archive_file" "get_table_names" {
+  type        = "zip"
+  source_file = "get_table_names.py"
+  output_path = "get_table_names.zip"
+}
+
+resource "aws_lambda_function" "get_table_names" {
+  filename      = "get_table_names.zip"
+  function_name = "get-all-table-names"
+  role          = aws_iam_role.lambda_execution.arn
+  handler       = "get_table_names.handler"
+  runtime       = "python3.12"
+  memory_size   = 4096
+  timeout       = 900
+  layers = [aws_lambda_layer_version.get_table_names.arn]
+}
+
+resource "aws_iam_role" "lambda_execution" {
+  name                = "ap-transfer-iam-role"
+  assume_role_policy  = data.aws_iam_policy_document.lambda_assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+}
+
+data "aws_iam_policy_document" "get_glue_table_names_policy" {
+    statement {
+      sid    = "VPCConfig"
+      effect = "Allow"
+      actions = [
+        "glue:GetTables"
+      ]
+      resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "get_table_names_lambda" {
+    name = "ap-transfer-iam-policy"
+    role = aws_iam_role.get_glue_table_names_policy.id
+    policy = data.aws_iam_policy_document.get_glue_table_names_policy.json
+}
+
+resource "aws_lambda_permission" "get_table_names_lambda" {
+  statement_id  = "GetTableNamesGlueSchema"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_table_names.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.rds_to_parquet.arn
+}
+
+#-----------------------------------------------------------------------------
+# Lambda layer for the above lambda
+#------------------------------------------------------------------------------
+
+
+#define variables
+locals {
+  layer_path        = "get_table_names_lambda_layer"
+  layer_zip_name    = "${local.layer_path}.zip"
+  requirements_name = "requirements.txt"
+  requirements_path = "${path.module}/${local.layer_path}/${local.requirements_name}"
+}
+
+# create zip file from requirements.txt. Triggers only when the file is updated
+resource "null_resource" "get_table_names_lambda_layer" {
+  triggers = {
+    requirements = filesha1(local.requirements_path)
+  }
+  # the command to install python and dependencies to the machine and zips
+  provisioner "local-exec" {
+    command = <<EOT
+      cd ${local.layer_path}
+      rm -rf python
+      mkdir python
+      pip install \
+      --platform manylinux2014_x86_64 \
+      --target=python \
+      --implementation cp \
+      --python-version 3.12 \
+      --only-binary=:all: --upgrade \
+      -r ${local.requirements_name}
+      zip -r ${local.layer_zip_name} python/
+    EOT
+  }
+}
+
+# create lambda layer from s3 object
+resource "aws_lambda_layer_version" "get_table_names_lambda_layer" {
+  s3_bucket           = aws_s3_bucket.rds_to_parquet.id
+  s3_key              = aws_s3_object.rds_to_parquet.key
+  layer_name          = local.layer_path
+  compatible_runtimes = ["python3.12"]
+  skip_destroy        = true
+  depends_on          = [aws_s3_object.lambda_layer_zip] # triggered only if the zip file is uploaded to the bucket
+}
+
+
+# upload zip file to s3
+resource "aws_s3_object" "lambda_layer_zip" {
+  bucket     = aws_s3_bucket.rds_to_parquet.id
+  key        = "lambda_layers/${local.layer_path}/${local.layer_zip_name}"
+  source     = "${local.layer_path}/${local.layer_zip_name}"
+  depends_on = [null_resource.get_table_names_lambda_layer] # triggered only if the zip file is created
+}
+
+
+
+
+
+# resource "aws_sfn_state_machine" "sfn_state_machine" {
+#   name     = "my-state-machine"
+#   role_arn = aws_iam_role.iam_for_sfn.arn
+#   publish  = true
+#   type     = "EXPRESS"
+#   definition = <<EOF
+# {
+#   "Comment": "Orchestration to run Glue job on all tables in a Glue schema",
+#   "StartAt": "StartCrawler",
+#   "States": {
+#     "StartCrawler": {
+#       "Type": "Task",
+#       "Next": "ListTables",
+#       "Parameters": {
+#         "Name": "MyData"
+#       },
+#       "Resource": "arn:aws:states:::aws-sdk:glue:startCrawler"
+#     },
+#     "ListTables": {
+#       "Type": "Task",
+#       "Resource": "arn:aws:lambda:REGION:ACCOUNT_ID:function:ListTablesLambdaFunction",
+#       "Next": "TriggerGlueJobs",
+#       "Catch": [
+#         {
+#           "ErrorEquals": [
+#             "States.ALL"
+#           ],
+#           "Next": "ErrorState"
+#         }
+#       ]
+#     },
+#     "TriggerGlueJobs": {
+#       "Type": "Map",
+#       "ItemsPath": "$.tableNames",
+#       "Iterator": {
+#         "StartAt": "Glue StartJobRun",
+#         "States": {
+#           "Glue StartJobRun": {
+#             "Type": "Task",
+#             "Resource": "arn:aws:states:::glue:startJobRun.sync",
+#             "Parameters": {
+#               "JobName": "myJobName"
+#             },
+#             "End": true
+#           }
+#         }
+#       },
+#       "Next": "Finish"
+#     },
+#     "Finish": {
+#       "Type": "Succeed"
+#     },
+#     "ErrorState": {
+#       "Type": "Fail"
+#     }
+#   }
+# }
+# EOF
+# }
+
