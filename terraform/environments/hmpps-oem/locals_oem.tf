813f3b28d5ec35d5e2f57d4dca371f46c8d8126b
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-03-18 11:22:58 +0000

    Allow access to Secrets for Ansible controller

--- terraform/environments/hmpps-oem/locals_oem.tf
+++ terraform/environments/hmpps-oem/locals_oem.tf
@@ -11,10 +11,12 @@ locals {
     contains(["development", "test"], local.environment) ? ["delius-core-${local.environment}"] : []
   ])
 
-  oem_share_secret_principal_ids = [
+  # EC2OracleEnterpriseManagementSecretsRole is used to allow access to OEM secrets from EC2 instances
+  # modernisation-platform-oidc-cicd is used to allow access to OEM secrets from Ansible controller 
+  oem_share_secret_principal_ids = flatten([
     for key, value in module.environment.account_ids :
-    "arn:aws:iam::${value}:role/EC2OracleEnterpriseManagementSecretsRole" if contains(local.oem_managed_applications, key)
-  ]
+      contains(local.oem_managed_applications, key) ? ["arn:aws:iam::${value}:role/EC2OracleEnterpriseManagementSecretsRole","arn:aws:iam::${value}:role/modernisation-platform-oidc-cicd"] : []
+  ])
 
   oem_secret_policy_write = {
     effect = "Allow"
