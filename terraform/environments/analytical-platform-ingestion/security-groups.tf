6c223aa3509324a567bf8f7dc28f64e7b97c3950
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-03-18 11:45:03 +0000

    Switch to environment-configuration HCL Split out resources into files Add public subnets and and NAT gateway
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/environments/analytical-platform-ingestion/security-groups.tf
@@ -0,0 +1,6 @@
+resource "aws_security_group" "vpc_endpoints" {
+  description = "Security Group for controlling all VPC endpoint traffic"
+  name        = format("%s-vpc-endpoint-sg", local.application_name)
+  vpc_id      = module.vpc.vpc_id
+  tags        = local.tags
+}
