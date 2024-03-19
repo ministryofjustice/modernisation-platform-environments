f3cadf59e229e76d12664b65a5c86d3cf0615371
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-19 20:25:23 +0000

    Update ecs_service.tf

--- terraform/environments/delius-mis/modules/nextcloud/ecs_service.tf
+++ terraform/environments/delius-mis/modules/nextcloud/ecs_service.tf
@@ -54,12 +54,12 @@ module "nextcloud_service" {
 
   create_rds               = true
   rds_engine               = "mariadb"
-  rds_engine_version       = "10.5"
+  rds_engine_version       = "10.6"
   rds_instance_class       = "db.t3.small"
-  rds_allocated_storage    = 20
+  rds_allocated_storage    = 500
   rds_username             = "misnextcloud"
   rds_port                 = 3306
-  rds_parameter_group_name = "default.mariadb10.5"
+  rds_parameter_group_name = "default.mariadb10.6"
   rds_license_model        = "general-public-license"
   snapshot_identifier      = "nextcloud-correct"
 
