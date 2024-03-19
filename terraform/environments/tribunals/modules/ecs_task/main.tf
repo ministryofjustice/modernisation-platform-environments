a19ff40afd89ddccd09dd0bd4d378e0ae469ed29
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-03-19 10:59:06 +0000

    add a folder for each tribunal in ecs task definition

--- terraform/environments/tribunals/modules/ecs_task/main.tf
+++ terraform/environments/tribunals/modules/ecs_task/main.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 
   volume {
     name = var.task_definition_volume
-    host_path = "D:/storage/tribunals"
+    host_path = "D:/storage/tribunals/${var.app_name}"
     docker_volume_configuration {
       driver = "local"
       scope = "shared"
