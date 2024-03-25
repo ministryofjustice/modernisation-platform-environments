84ac1f78b5fa9c5bd9bbb057aace23138ff97ec8
matt-heery <matt.heery@digital.justice.gov.uk>
2024-03-25 15:18:21 +0000

    vpc config

--- terraform/environments/electronic-monitoring-data/dms-setup.tf
+++ terraform/environments/electronic-monitoring-data/dms-setup.tf
@@ -1,9 +1,45 @@
+resource "aws_iam_role" "dms-access-for-endpoint" {
+  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
+  name               = "dms-access-for-endpoint"
+}
+
+resource "aws_iam_role_policy_attachment" "dms-access-for-endpoint-AmazonDMSRedshiftS3Role" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSRedshiftS3Role"
+  role       = aws_iam_role.dms-access-for-endpoint.name
+}
+
+resource "aws_iam_role" "dms-cloudwatch-logs-role" {
+  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
+  name               = "dms-cloudwatch-logs-role"
+}
+
+resource "aws_iam_role_policy_attachment" "dms-cloudwatch-logs-role-AmazonDMSCloudWatchLogsRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSCloudWatchLogsRole"
+  role       = aws_iam_role.dms-cloudwatch-logs-role.name
+}
+
+resource "aws_iam_role" "dms-vpc-role" {
+  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
+  name               = "dms-vpc-role"
+}
+
+resource "aws_iam_role_policy_attachment" "dms-vpc-role-AmazonDMSVPCManagementRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
+  role       = aws_iam_role.dms-vpc-role.name
+}
+
 resource "aws_dms_replication_instance" "rds_connection" {
     replication_instance_id = "rds-conn"
     replication_instance_class = "dms.r5.large"
     allocated_storage           = 1000
     engine_version              = "3.4.4"
+    replication_subnet_group_id = aws_dms_replication_subnet_group.rds_subnets.id
     vpc_security_group_ids = [aws_security_group.db.id]
+    depends_on = [
+    aws_iam_role_policy_attachment.dms-access-for-endpoint-AmazonDMSRedshiftS3Role,
+    aws_iam_role_policy_attachment.dms-cloudwatch-logs-role-AmazonDMSCloudWatchLogsRole,
+    aws_iam_role_policy_attachment.dms-vpc-role-AmazonDMSVPCManagementRole
+    ]
 }
 
 resource "aws_dms_replication_subnet_group" "rds_subnets" {
