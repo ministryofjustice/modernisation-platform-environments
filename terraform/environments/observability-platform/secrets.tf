aca03402a0392f0063d6390f7255e5ac3ed06953
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-03-25 15:54:15 +0000

    Add GitHub token secret Update Terraform dev container feature
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/secrets.tf
+++ terraform/environments/observability-platform/secrets.tf
@@ -1,3 +1,7 @@
 resource "aws_secretsmanager_secret" "grafana_api_key" {
   name = "grafana/api-key"
 }
+
+resource "aws_secretsmanager_secret" "github_token" {
+  name = "grafana/data-sources/github-token"
+}
