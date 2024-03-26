a2465e72710d09a10b7a70a3121e75d2bf176a84
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2024-03-26 16:00:52 +0000

    removing commented code

--- terraform/environments/corporate-staff-rostering/locals_preproduction.tf
+++ terraform/environments/corporate-staff-rostering/locals_preproduction.tf
@@ -1183,7 +1183,6 @@ locals {
               type              = "forward"
               target_group_name = "pp-csr-w-34-7770"
             }
-            #cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms_by_sns_topic["csr_pagerduty"].network_lb
             cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms.network_lb
 
           }
@@ -1195,7 +1194,6 @@ locals {
               type              = "forward"
               target_group_name = "pp-csr-w-34-7771"
             }
-            # cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms_by_sns_topic["csr_pagerduty"].network_lb
             cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms.network_lb
           }
           http-7780 = {
@@ -1206,7 +1204,6 @@ locals {
               type              = "forward"
               target_group_name = "pp-csr-w-34-7780"
             }
-            # cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms_by_sns_topic["csr_pagerduty"].network_lb
             cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms.network_lb
 
           }
@@ -1218,7 +1215,6 @@ locals {
               type              = "forward"
               target_group_name = "pp-csr-w-34-7781"
             }
-            # cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms_by_sns_topic["csr_pagerduty"].network_lb
             cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms.network_lb
           }
         }
