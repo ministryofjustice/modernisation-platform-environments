d8ba2ac7c5281aa0513ebc728de35c99ed909d5e
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-26 14:02:00 +0000

    Nit 1172 delius core update delius (#5476)
    * Update pwm.tf
    
    * reformat

--- terraform/environments/delius-core/modules/delius_environment/gdpr_api_service.tf
+++ terraform/environments/delius-core/modules/delius_environment/gdpr_api_service.tf
@@ -11,23 +11,14 @@ module "gdpr_api_service" {
       protocol      = "tcp"
   }]
   ecs_cluster_arn = module.ecs.ecs_cluster_arn
-  container_secrets = [
-    {
-      name      = "SPRING_DATASOURCE_PASSWORD"
-      valueFrom = aws_ssm_parameter.delius_core_gdpr_db_admin_password.arn
-      #value = "/${var.environment_name}/${var.project_name}/delius-gdpr-database/db/admin_password" # delete this
-    },
-    {
-      name      = "SPRING_SECOND-DATASOURCE_PASSWORD"
-      valueFrom = aws_ssm_parameter.delius_core_gdpr_db_pool_password.arn
-      #value = "/${var.environment_name}/${var.project_name}/delius-database/db/gdpr_pool_password" # delete
-    },
-    {
-      name      = "SECURITY_OAUTH2_CLIENT_CLIENT-SECRET"
-      valueFrom = aws_ssm_parameter.delius_core_gdpr_api_client_secret.arn
-      #value = "/${var.environment_name}/${var.project_name}/gdpr/api/client_secret" # delete
-    }
-  ]
+  container_secrets_default = {
+    "SPRING_DATASOURCE_PASSWORD" : aws_ssm_parameter.delius_core_gdpr_db_admin_password.arn,
+    "SPRING_SECOND-DATASOURCE_PASSWORD" : aws_ssm_parameter.delius_core_gdpr_db_pool_password.arn,
+    "SECURITY_OAUTH2_CLIENT_CLIENT-SECRET" : aws_ssm_parameter.delius_core_gdpr_api_client_secret.arn
+  }
+
+  container_secrets_env_specific = try(var.delius_microservice_configs.gdpr_api.container_secrets_env_specific, {})
+
   db_ingress_security_groups = []
   cluster_security_group_id  = aws_security_group.cluster.id
 
@@ -55,108 +46,24 @@ module "gdpr_api_service" {
   rds_backup_retention_period = var.delius_microservice_configs.merge_api.rds_backup_retention_period
   rds_backup_window           = var.delius_microservice_configs.merge_api.rds_backup_window
 
-  container_environment_vars = [
-    {
-      name  = "SERVER_SERVLET_CONTEXT_PATH"
-      value = "/gdpr/api/"
-    },
-    #    {
-    #      name  = "SPRING_DATASOURCE_JDBC_URL"
-    #      value = "jdbc:postgresql://${aws_db_instance.primary.endpoint}/${aws_db_instance.primary.name}"
-    #    },
-    #    {
-    #      name  = "SPRING_DATASOURCE_USERNAME"
-    #      value = aws_db_instance.primary.username
-    #    },
-    {
-      name  = "SPRING_DATASOURCE_DRIVER_CLASS_NAME"
-      value = "org.postgresql.Driver"
-    },
-    #    {
-    #      name  = "SPRING_SECOND_DATASOURCE_JDBC_URL"
-    #      value = data.terraform_remote_state.database.outputs.jdbc_failover_url
-    #    },
-    {
-      name  = "SPRING_SECOND_DATASOURCE_USERNAME"
-      value = "gdpr_pool"
-    },
-    {
-      name  = "SPRING_SECOND_DATASOURCE_TYPE"
-      value = "oracle.jdbc.pool.OracleDataSource"
-    },
-    {
-      name  = "SPRING_JPA_HIBERNATE_DDL_AUTO"
-      value = "update"
-    },
-    {
-      name  = "SPRING_BATCH_JOB_ENABLED"
-      value = "false"
-    },
-    {
-      name  = "SPRING_BATCH_INITIALIZE_SCHEMA"
-      value = "always"
-    },
-    {
-      name  = "ALFRESCO_DMS_PROTOCOL"
-      value = "https"
-    },
-    #    {
-    #      name  = "ALFRESCO_DMS_HOST"
-    #      value = "alfresco.${data.terraform_remote_state.vpc.outputs.public_zone_name}"
-    #    },
-    #    {
-    #      name  = "SCHEDULE_IDENTIFYDUPLICATES"
-    #      value = local.gdpr_config["cron_identifyduplicates"]
-    #    },
-    #    {
-    #      name  = "SCHEDULE_RETAINEDOFFENDERS"
-    #      value = local.gdpr_config["cron_retainedoffenders"]
-    #    },
-    #    {
-    #      name  = "SCHEDULE_RETAINEDOFFENDERSIICSA"
-    #      value = local.gdpr_config["cron_retainedoffendersiicsa"]
-    #    },
-    #    {
-    #      name  = "SCHEDULE_ELIGIBLEFORDELETION"
-    #      value = local.gdpr_config["cron_eligiblefordeletion"]
-    #    },
-    #    {
-    #      name  = "SCHEDULE_DELETEOFFENDERS"
-    #      value = local.gdpr_config["cron_deleteoffenders"]
-    #    },
-    #    {
-    #      name  = "SCHEDULE_DESTRUCTIONLOGCLEARING"
-    #      value = local.gdpr_config["cron_destructionlogclearing"]
-    #    },
-    #    {
-    #      name  = "SCHEDULE_ELIGIBLEFORDELETIONSOFTDELETED"
-    #      value = local.gdpr_config["cron_eligiblefordeletionsoftdeleted"]
-    #    },
-    {
-      name  = "SECURITY_OAUTH2_RESOURCE_ID"
-      value = "NDelius"
-    },
-    {
-      name  = "SECURITY_OAUTH2_CLIENT_CLIENT_ID"
-      value = "GDPR-API"
-    },
-    {
-      name  = "SECURITY_OAUTH2_RESOURCE_TOKEN_INFO_URI"
-      value = "http://usermanagement.ecs.cluster:8080/umt/oauth/check_token"
-    },
-    #    {
-    #      name  = "LOGGING_LEVEL_UK_GOV_JUSTICE"
-    #      value = local.gdpr_config["log_level"]
-    #    },
-    {
-      name  = "SPRING_FLYWAY_ENABLED"
-      value = "true"
-    },
-    {
-      name  = "SPRING_FLYWAY_LOCATIONS"
-      value = "classpath:/db"
-    }
-  ]
+  container_vars_default = {
+    "SERVER_SERVLET_CONTEXT_PATH" : "/gdpr/api/",
+    "SPRING_DATASOURCE_DRIVER_CLASS_NAME" : "org.postgresql.Driver",
+    "SPRING_SECOND_DATASOURCE_USERNAME" : "gdpr_pool",
+    "SPRING_SECOND_DATASOURCE_TYPE" : "oracle.jdbc.pool.OracleDataSource",
+    "SPRING_JPA_HIBERNATE_DDL_AUTO" : "update",
+    "SPRING_BATCH_JOB_ENABLED" : "false",
+    "SPRING_BATCH_INITIALIZE_SCHEMA" : "always",
+    "ALFRESCO_DMS_PROTOCOL" : "https",
+    "SECURITY_OAUTH2_RESOURCE_ID" : "NDelius",
+    "SECURITY_OAUTH2_CLIENT_CLIENT_ID" : "GDPR-API",
+    "SECURITY_OAUTH2_RESOURCE_TOKEN_INFO_URI" : "http://usermanagement.ecs.cluster:8080/umt/oauth/check_token",
+    "SPRING_FLYWAY_ENABLED" : "true",
+    "SPRING_FLYWAY_LOCATIONS" : "classpath:/db"
+  }
+
+  container_vars_env_specific = try(var.delius_microservice_configs.gdpr_api.container_vars_env_specific, {})
+
 
   providers = {
     aws          = aws
--- terraform/environments/delius-core/modules/delius_environment/newtech.tf
+++ terraform/environments/delius-core/modules/delius_environment/newtech.tf
@@ -6,6 +6,12 @@ module "newtech" {
   alb_security_group_id = aws_security_group.delius_frontend_alb_security_group.id
   env_name              = var.env_name
 
+  container_vars_default      = {}
+  container_vars_env_specific = try(var.delius_microservice_configs.newtech.container_vars_env_specific, {})
+
+  container_secrets_default      = {}
+  container_secrets_env_specific = try(var.delius_microservice_configs.newtech.container_secrets_env_specific, {})
+
   container_port_config = [
     {
       containerPort = var.delius_microservice_configs.newtech.container_port
@@ -14,7 +20,6 @@ module "newtech" {
   ]
 
   ecs_cluster_arn            = module.ecs.ecs_cluster_arn
-  container_secrets          = []
   db_ingress_security_groups = []
   cluster_security_group_id  = aws_security_group.cluster.id
 
@@ -23,13 +28,12 @@ module "newtech" {
   microservice_lb                    = aws_lb.delius_core_frontend
   microservice_lb_https_listener_arn = aws_lb_listener.listener_https.arn
 
-  alb_listener_rule_paths    = ["/newtech", "/newtech/*"]
-  platform_vars              = var.platform_vars
-  container_image            = "${var.platform_vars.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/delius-core-newtech-ecr-repo:${var.delius_microservice_configs.newtech.image_tag}"
-  account_config             = var.account_config
-  health_check_path          = "/newtech"
-  account_info               = var.account_info
-  container_environment_vars = []
+  alb_listener_rule_paths = ["/newtech", "/newtech/*"]
+  platform_vars           = var.platform_vars
+  container_image         = "${var.platform_vars.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/delius-core-newtech-ecr-repo:${var.delius_microservice_configs.newtech.image_tag}"
+  account_config          = var.account_config
+  health_check_path       = "/newtech"
+  account_info            = var.account_info
 
   providers = {
     aws          = aws
