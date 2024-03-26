6597c9a678bf575a3b525e3e29c71693f2529d76
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-03-26 11:12:41 +0000

    delete curserver

--- terraform/environments/pra-register/ecs.tf
+++ terraform/environments/pra-register/ecs.tf
@@ -153,10 +153,6 @@ resource "aws_ecs_task_definition" "pra_task_definition_dev" {
           value = "${local.application_data.accounts[local.environment].support_team}"
         },
         {
-          name  = "CurServer"
-          value = "${local.application_data.accounts[local.environment].curserver}"
-        },
-        {
           name  = "ida:ClientId"
           value = "${local.application_data.accounts[local.environment].client_id}"
         }
