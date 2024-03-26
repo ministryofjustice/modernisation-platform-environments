44477f8a666d1c25dd2bdfe39b1ea4e4b9839b15
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-26 14:02:00 +0000

    Nit 1172 delius core update delius (#5476)
    * Update pwm.tf
    
    * reformat

--- terraform/environments/delius-core/modules/helpers/delius_microservice/locals.tf
+++ terraform/environments/delius-core/modules/helpers/delius_microservice/locals.tf
@@ -8,20 +8,47 @@ locals {
     }
   }
 
-  rds_env_vars = var.rds_endpoint_environment_variable != "" ? [{
-    name  = var.rds_endpoint_environment_variable
-    value = aws_db_instance.this[0].endpoint
-  }] : []
+  calculated_container_secrets = merge(
+    var.container_secrets_default,
+    var.container_secrets_env_specific,
+    local.rds_secrets
+  )
 
-  rds_secrets = var.rds_password_secret_variable != "" ? [{
-    name  = var.rds_password_secret_variable
-    value = "${aws_db_instance.this[0].master_user_secret[0].secret_arn}:password:AWSCURRENT"
-  }] : []
+  calculated_container_secrets_list = flatten([
+    for key, value in local.calculated_container_secrets : [
+      {
+        name      = key
+        valueFrom = value
+      }
+    ]
+  ])
 
-  elasticache_env_vars = var.elasticache_endpoint_environment_variable != "" ? [{
-    name  = var.elasticache_endpoint_environment_variable
-    value = aws_elasticache_cluster.this[0].cluster_address
-  }] : []
+  calculated_container_vars = merge(
+    var.container_vars_default,
+    var.container_vars_env_specific,
+    local.rds_env_vars,
+    local.elasticache_env_vars
+  )
 
+  calculated_container_vars_list = flatten([
+    for key, value in local.calculated_container_vars : [
+      {
+        name  = key
+        value = value
+      }
+    ]
+  ])
+
+  rds_env_vars = var.rds_endpoint_environment_variable != "" ? {
+    var.rds_endpoint_environment_variable = aws_db_instance.this[0].endpoint
+  } : {}
+
+  rds_secrets = var.rds_password_secret_variable != "" ? {
+    var.rds_password_secret_variable = "${aws_db_instance.this[0].master_user_secret[0].secret_arn}:password:AWSCURRENT"
+  } : {}
+
+  elasticache_env_vars = var.elasticache_endpoint_environment_variable != "" ? {
+    var.elasticache_endpoint_environment_variable = aws_elasticache_cluster.this[0].cluster_address
+  } : {}
 
 }
