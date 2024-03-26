d9bbd8e2242fcbf0de09d741c754f30109e722cd
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-03-26 11:57:21 +0000

    set public ip to false for staging and prod ecs service

--- terraform/environments/ncas/ecs.tf
+++ terraform/environments/ncas/ecs.tf
@@ -182,7 +182,7 @@ resource "aws_ecs_service" "ncas_ecs_service" {
   network_configuration {
     subnets          = data.aws_subnets.shared-private.ids
     security_groups  = [aws_security_group.ecs_service.id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
 
   load_balancer {
