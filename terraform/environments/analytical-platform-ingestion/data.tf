83d31b40734b2df66cc51462215297fc0009366f
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-03-26 15:15:04 +0000

    Further configuration

--- terraform/environments/analytical-platform-ingestion/data.tf
+++ terraform/environments/analytical-platform-ingestion/data.tf
@@ -1,25 +1,2 @@
 #### This file can be used to store data specific to the member account ####
 data "aws_availability_zones" "available" {}
-
-data "aws_iam_policy_document" "s3_bold_egress_s3_policy" {
-  statement {
-    sid    = "ReplicationPermissions"
-    effect = "Allow"
-    principals { 
-      type = "AWS"
-      identifiers = ["arn:aws:iam::593291632749:role/service-role/source-account-IAM-role"]
-    }
-    actions = [
-      "s3:ReplicateDelete",
-      "s3:ReplicateObject",
-      "s3:ObjectOwnerOverrideToBucketOwner",
-      "s3:GetBucketVersioning",
-      "s3:PutBucketVersioning"
-    ]
-    resources = ["*"]
-    # resources = module.bold_egress_bucket.arn
-  }
-
-
-
-}
