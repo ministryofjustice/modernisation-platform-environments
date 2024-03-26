272d6f052894a61fe146e996c38aef89910b326f
Shajida Akthar <Shajida Akthar>
2024-03-26 17:11:01 +0000

    Environment module vars referenced in both cloudwatch modules

--- terraform/environments/oasys/main.tf
+++ terraform/environments/oasys/main.tf
@@ -146,6 +146,7 @@ module "baseline" {
 
 module "cross_account_cloudwatch" {
   source  = "../../modules/cross_account_cloudwatch"
+  environment  = module.environment
   options = merge(
     local.cloudwatch_monitoring_options,
     local.cloudwatch_local_environment_monitoring_options,
@@ -154,6 +155,7 @@ module "cross_account_cloudwatch" {
 
 module "cloudwatch" {
   source  = "../../modules/cloudwatch"
+  environment  = module.environment
   options = merge(
     local.cloudwatch_monitoring_options,
     local.cloudwatch_local_environment_monitoring_options,
