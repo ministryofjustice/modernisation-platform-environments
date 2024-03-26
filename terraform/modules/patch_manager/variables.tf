cf6de6880efdea536e99cb873e44d15cd88265ad
pav <pav.gainda@version1.com>
2024-03-26 11:12:56 +0000

    Correct validation syntax

--- terraform/modules/patch_manager/variables.tf
+++ terraform/modules/patch_manager/variables.tf
@@ -12,31 +12,34 @@ variable "schedule" {
 
 variable "predefined_baseline" {
   description = "Predefined baseline"
-  type    = string
+  type        = string
   validation {
     condition = contains([
       "AWS-WindowsPredefinedPatchBaseline-OS",
       "AWS-WindowsPredefinedPatchBaseline-OS-Applications",
       "AWS-RedHatDefaultPatchBaseline"
-    ], var.operating_system)
+    ], var.predefined_baseline)
     error_message = "Not a valid baseline"
   }
 }
 
 variable "operating_system" {
   description = "Operating system for baseline"
-  type = string
+  type        = string
   validation {
-    condition = contains(["WINDOWS", "REDHAT_ENTERPRISE_LINUX"], var.operating_system)
+    condition = contains([
+      "WINDOWS",
+      "REDHAT_ENTERPRISE_LINUX"
+    ], var.operating_system)
     error_message = "Not a valid operating system"
   }
 }
 
 variable "target_tag" {
   description = "Instance tag name and value to target for patching"
-  type = map(any)
+  type        = map(any)
 }
 
-locals  {
-  os  = replace(lower(var.operating_system),"_","-")
+locals {
+  os = replace(lower(var.operating_system), "_", "-")
 }
