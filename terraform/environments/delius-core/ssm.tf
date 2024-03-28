c59c6a69ca0a0697733f4433431933e3abc902ce
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-28 09:50:30 +0000

    Update ssm.tf

--- terraform/environments/delius-core/ssm.tf
+++ terraform/environments/delius-core/ssm.tf
@@ -26,3 +26,15 @@ resource "aws_ssm_parameter" "hmpps_bot_installation_id" {
   }
   tags = local.tags
 }
+
+resource "aws_ssm_parameter" "hmpps_bot_app_id" {
+  name  = "/github/hmpps_bot_app_id"
+  type  = "SecureString"
+  value = "INITIAL_VALUE_OVERRIDDEN"
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
+  tags = local.tags
+}
