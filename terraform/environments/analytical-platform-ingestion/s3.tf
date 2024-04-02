c74216f8442c057b4d1dd1deeef7c466dc951ba2
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-04-02 14:56:31 +0000

    :wrench: Edit permissions

--- terraform/environments/analytical-platform-ingestion/s3.tf
+++ terraform/environments/analytical-platform-ingestion/s3.tf
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "quarantine_bucket_policy" {
     effect = "Deny"
     not_principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${local.environment}:role/scan"]
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/scan"]
     }
     actions   = ["*"]
     resources = ["arn:aws:s3:::mojap-ingestion-${local.environment}-quarantine"]
