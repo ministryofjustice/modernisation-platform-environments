fe3d3427a96eeebf04b617badc297497643ddfe9
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-04-03 15:46:49 +0100

    remove ip address module duplication

--- terraform/modules/ip_addresses/modernisation_platform.tf
+++ terraform/modules/ip_addresses/modernisation_platform.tf
@@ -22,7 +22,7 @@ locals {
     ]
   }
 
-  mp_cidr = {
+  mp_cidr = merge({ for host, ip in local.mp_ip : host => "${ip}/32" }, {
     # Aggregate ranges
     development_test         = "10.26.0.0/16"
     preproduction_production = "10.27.0.0/16"
@@ -32,15 +32,7 @@ locals {
     hmpps-test          = "10.26.8.0/21"
     hmpps-preproduction = "10.27.0.0/21"
     hmpps-production    = "10.27.8.0/21"
-
-    # EC2s (azure.hmpp.root and azure.noms.root IPs)
-    ad-hmpp-dc-a   = "10.20.72.5/32"
-    ad-hmpp-dc-b   = "10.20.74.5/32"
-    ad-hmpp-rdlic  = "10.20.76.6/32"
-    ad-azure-dc-a  = "10.20.104.5/32"
-    ad-azure-dc-b  = "10.20.106.5/32"
-    ad-azure-rdlic = "10.20.108.6/32"
-  }
+  })
 
   mp_cidrs = {
 
