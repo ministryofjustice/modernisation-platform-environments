387f705cce7b3fe74e2fd32f269fe6a1c071e829
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2024-04-04 14:05:49 +0100

    CC-2437: Split Validation based on provider

--- terraform/environments/ccms-ebs/ccms-certificates.tf
+++ terraform/environments/ccms-ebs/ccms-certificates.tf
@@ -43,55 +43,6 @@ resource "aws_acm_certificate" "external-service" {
 ## Validation
 
 resource "aws_route53_record" "external_validation_core_network" {
-  count = local.is-production ? 0 : 1
-
-  depends_on = [
-    aws_instance.ec2_oracle_ebs,
-    aws_instance.ec2_ebsapps,
-    aws_instance.ec2_webgate,
-    aws_instance.ec2_accessgate
-  ]
-
-  provider = aws.core-network-services
-
-  allow_overwrite = true
-  name            = lookup(aws_acm_certificate.external[0].domain_validation_options, "domain_name", "modernisation-platform.service.justice.gov.uk").resource_record_name
-  records         = lookup(aws_acm_certificate.external[0].domain_validation_options, "domain_name", "modernisation-platform.service.justice.gov.uk").resource_record_value
-  type            = lookup(aws_acm_certificate.external[0].domain_validation_options, "domain_name", "modernisation-platform.service.justice.gov.uk").resource_record_type
-  # name            = aws_acm_certificate.external[0].domain_validation_options[0].resource_record_name
-  # records         = aws_acm_certificate.external[0].domain_validation_options[0].resource_record_value
-  ttl = 60
-  #  type            = aws_acm_certificate.external[0].domain_validation_options[0].resource_record_type
-  zone_id = local.cert_zone_id
-}
-
-
-resource "aws_route53_record" "external_validation_core_vpc" {
-  count = local.is-production ? 0 : 1
-
-  depends_on = [
-    aws_instance.ec2_oracle_ebs,
-    aws_instance.ec2_ebsapps,
-    aws_instance.ec2_webgate,
-    aws_instance.ec2_accessgate
-  ]
-
-  provider = aws.core-vpc
-
-  name            = lookup(aws_acm_certificate.external[0].domain_validation_options, "domain_name", "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk").resource_record_name
-  records         = lookup(aws_acm_certificate.external[0].domain_validation_options, "domain_name", "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk").resource_record_value
-  type            = lookup(aws_acm_certificate.external[0].domain_validation_options, "domain_name", "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk").resource_record_type
-
-#  name    = aws_acm_certificate.external[0].domain_validation_options[1].resource_record_name
-#  records = aws_acm_certificate.external[0].domain_validation_options[1].resource_record_value
-  ttl     = 60
-#  type    = aws_acm_certificate.external[0].domain_validation_options[1].resource_record_type
-  zone_id = local.cert_zone_id
-}
-
-resource "aws_route53_record" "external_validation" {
-#  count = local.is-production ? 1 : 0
-
   depends_on = [
     aws_instance.ec2_oracle_ebs,
     aws_instance.ec2_ebsapps,
@@ -102,12 +53,17 @@ resource "aws_route53_record" "external_validation" {
   provider = aws.core-network-services
 
   for_each = {
-    for dvo in local.cert_opts : dvo.domain_name => {
+    for dvo in local.cert_opts : (
+      dvo.domain_name == "modernisation-platform.service.justice.gov.uk" ||
+      dvo.domain_name == "ccms-ebs.service.justice.gov.uk" ||
+      dvo.domain_name == "*.ccms-ebs.service.justice.gov.uk"
+      ) => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
   }
+
   allow_overwrite = true
   name            = each.value.name
   records         = [each.value.record]
@@ -117,15 +73,72 @@ resource "aws_route53_record" "external_validation" {
 }
 
 
-resource "aws_acm_certificate_validation" "external" {
+resource "aws_route53_record" "external_validation_core_vpc" {
+  depends_on = [
+    aws_instance.ec2_oracle_ebs,
+    aws_instance.ec2_ebsapps,
+    aws_instance.ec2_webgate,
+    aws_instance.ec2_accessgate
+  ]
+
+  provider = aws.core-vpc
+
+  for_each = {
+    for dvo in local.cert_opts : dvo.domain_name == "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk" => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = local.cert_zone_id
+}
+
+
+# resource "aws_acm_certificate_validation" "external" {
+#   count = local.is-production ? 1 : 1
+
+#   depends_on = [
+#     aws_route53_record.external_validation
+#   ]
+
+#   certificate_arn         = local.cert_arn
+#   validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
+
+#   timeouts {
+#     create = "10m"
+#   }
+# }
+
+resource "aws_acm_certificate_validation" "external_core_network" {
   count = local.is-production ? 1 : 1
 
   depends_on = [
-    aws_route53_record.external_validation
+    aws_route53_record.external_validation_core_network
   ]
 
   certificate_arn         = local.cert_arn
-  validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.external_validation_core_network : record.fqdn]
+
+  timeouts {
+    create = "10m"
+  }
+}
+
+resource "aws_acm_certificate_validation" "external_core_vpc" {
+  count = local.is-production ? 1 : 1
+
+  depends_on = [
+    aws_route53_record.external_validation_core_vpc
+  ]
+
+  certificate_arn         = local.cert_arn
+  validation_record_fqdns = [for record in aws_route53_record.external_validation_core_vpc : record.fqdn]
 
   timeouts {
     create = "10m"
