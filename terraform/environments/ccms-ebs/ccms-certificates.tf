afcb8d44aee1c3b22164f08aff44be987ac14646
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2024-04-04 15:47:36 +0100

    CC-2437: Split Validation based on provider

--- terraform/environments/ccms-ebs/ccms-certificates.tf
+++ terraform/environments/ccms-ebs/ccms-certificates.tf
@@ -83,13 +83,13 @@ resource "aws_route53_record" "external_validation_core_vpc" {
 
   provider = aws.core-vpc
 
-  for_each = {
+  for_each = local.is-production ? {
     for dvo in local.cert_opts : dvo.domain_name == "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk" => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
-  }
+  } : {}
 
   allow_overwrite = true
   name            = each.value.name
