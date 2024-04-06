6edea4cea9f071833624daa28255815b6089cdb4
Craig Gilbertson <crvgilbertson@gmail.com>
2024-04-06 20:49:05 +0100

    Small renaming

--- terraform/environments/nomis-combined-reporting/locals_bip.tf
+++ terraform/environments/nomis-combined-reporting/locals_bip.tf
@@ -1,29 +1,29 @@
 locals {
 
-  bip_cms_secretsmanager_secrets = {
+  bip_secretsmanager_secrets = {
     secrets = {
-      passwords = { description = "BIP CMS Passwords" }
+      passwords = { description = "BIP Passwords" }
     }
   }
 
-  bip_cms_cloudwatch_metric_alarms = merge(
+  bip_cloudwatch_metric_alarms = merge(
     module.baseline_presets.cloudwatch_metric_alarms.ec2,
     module.baseline_presets.cloudwatch_metric_alarms.ec2_cwagent_linux,
     module.baseline_presets.cloudwatch_metric_alarms.ec2_instance_cwagent_collectd_service_status_os,
     # module.baseline_presets.cloudwatch_metric_alarms.ec2_instance_cwagent_collectd_service_status_app, # add in once there are custom services monitored
   )
 
-  bip_cms_cloudwatch_log_groups = {
-    cwagent-bip-cms-logs = {
+  bip_cloudwatch_log_groups = {
+    cwagent-bip-logs = {
       retention_in_days = 30
     }
   }
 
-  bip_cms_ec2_default = {
+  bip_ec2_default = {
 
     config = merge(module.baseline_presets.ec2_instance.config.default, {
       ami_name                  = "base_rhel_8_5_*"
-      iam_resource_names_prefix = "ec2-bip-cms"
+      iam_resource_names_prefix = "ec2-bip"
     })
     instance = merge(module.baseline_presets.ec2_instance.instance.default, {
       instance_type          = "t3.large"
@@ -41,11 +41,11 @@ locals {
     route53_records = module.baseline_presets.ec2_instance.route53_records.internal_and_external
 
     tags = {
-      description = "ncr bip cms webtier component"
+      description = "ncr bip mid-tier component"
       ami         = "base_rhel_8_5"
       os-type     = "Linux"
-      server-type = "ncr-bip-cms"
-      component   = "web"
+      server-type = "ncr-bip"
+      component   = "mid"
     }
   }
 }
