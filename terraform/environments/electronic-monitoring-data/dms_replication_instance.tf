d9e9581881d2a967be640907c9abe16eedeb4a5b
Madhu Kadiri <madhu.kadiri2@justice.gov.uk>
2024-04-08 10:03:36 +0100

    v8-dms-module-t3small-used

--- terraform/environments/electronic-monitoring-data/dms_replication_instance.tf
+++ terraform/environments/electronic-monitoring-data/dms_replication_instance.tf
@@ -19,7 +19,8 @@ resource "aws_dms_replication_subnet_group" "dms_replication_subnet_group" {
 # ==========================================================================
 
 # Create a new replication instance
-resource "aws_dms_replication_instance" "t3micro_dms_replication_instance" {
+
+resource "aws_dms_replication_instance" "t3small_dms_replication_instance" {
   allocated_storage          = 20
   apply_immediately          = true
   auto_minor_version_upgrade = true
@@ -29,7 +30,7 @@ resource "aws_dms_replication_instance" "t3micro_dms_replication_instance" {
   multi_az = false
   #   preferred_maintenance_window = "sun:10:30-sun:14:30"
   publicly_accessible         = false
-  replication_instance_class  = "dms.t3.micro"
+  replication_instance_class  = "dms.t3.small"
   replication_instance_id     = "t3micro-dms-replication-instance-tf"
   replication_subnet_group_id = aws_dms_replication_subnet_group.dms_replication_subnet_group.id
 
@@ -50,3 +51,34 @@ resource "aws_dms_replication_instance" "t3micro_dms_replication_instance" {
     aws_iam_role_policy_attachment.dms-vpc-role-AmazonDMSVPCManagementRole
   ]
 }
+# resource "aws_dms_replication_instance" "t3micro_dms_replication_instance" {
+#   allocated_storage          = 20
+#   apply_immediately          = true
+#   auto_minor_version_upgrade = true
+#   availability_zone          = "eu-west-2b"
+#   #   engine_version               = "3.5.1"
+#   #   kms_key_arn                  = "arn:aws:kms:eu-west-2:800964199911:key/b7f54acb-16a3-4958-9340-3bdf5f5842d8"
+#   multi_az = false
+#   #   preferred_maintenance_window = "sun:10:30-sun:14:30"
+#   publicly_accessible         = false
+#   replication_instance_class  = "dms.t3.micro"
+#   replication_instance_id     = "t3micro-dms-replication-instance-tf"
+#   replication_subnet_group_id = aws_dms_replication_subnet_group.dms_replication_subnet_group.id
+
+#   tags = merge(
+#     local.tags,
+#     {
+#       Resource_Type = "DMS Replication Instance",
+#     }
+#   )
+
+#   vpc_security_group_ids = [
+#     aws_security_group.dms_ri_security_group.id,
+#   ]
+
+#   depends_on = [
+#     aws_iam_role_policy_attachment.dms-endpoint-role,
+#     aws_iam_role_policy_attachment.dms-cloudwatch-logs-role-AmazonDMSCloudWatchLogsRole,
+#     aws_iam_role_policy_attachment.dms-vpc-role-AmazonDMSVPCManagementRole
+#   ]
+# }
