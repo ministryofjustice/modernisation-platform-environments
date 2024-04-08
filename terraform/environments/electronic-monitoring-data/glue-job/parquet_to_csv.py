59262525b066ddda4bdb0761b13d0c9c1a83888c
matt-heery <matt.heery@digital.justice.gov.uk>
2024-04-08 11:28:11 +0100

    using boto to read table names

--- terraform/environments/electronic-monitoring-data/glue-job/parquet_to_csv.py
+++ terraform/environments/electronic-monitoring-data/glue-job/parquet_to_csv.py
@@ -2,9 +2,9 @@ import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from py
 from awsglue.context import GlueContext
 from awsglue.job import Job
+import boto3
 
 # Set up Glue context and job
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
@@ -13,6 +13,7 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
+s3 = boto3.client("s3")
 
 # Source and destination buckets
 source_bucket = args["source_bucket"]
@@ -20,23 +21,45 @@ destination_bucket = args["destination_bucket"]
 source_path = "s3://{}/".format(source_bucket)
 destination_path = "s3://{}/".format(destination_bucket)
 
-# Maximum size for each CSV file (in bytes)
-max_csv_size = 5 * 1024 * 1024 * 1024  # 5GB
+
+def get_tables_from_s3_path(s3_client, bucket_name="dms-em-rds-output"):
+    # Pagination
+    paginator = s3_client.get_paginator("list_objects_v2")
+    response_iterator = paginator.paginate(Bucket=bucket_name)
+
+    for page in response_iterator:
+        keys = [obj["Key"] for obj in page.get("Contents", [])]
+
+    database_table_pairs = list(
+        set([(key.split("/")[0], key.split("/")[2]) for key in keys])
+    )
+
+    result = {}
+
+    for key, value in database_table_pairs:
+        if key in result:
+            result[key].append(value)
+        else:
+            result[key] = [value]
+
+    # Convert the lists to sets to remove duplicate table names
+    for key in result:
+        result[key] = list(set(result[key]))
+
+    return result
 
 
-# Get list of databases in the source bucket
-databases = glueContext.list_databases()
-
+databases = get_tables_from_s3_path(s3)
 # Iterate over each database
 for database in databases:
-    database_name = database["Name"]
+    database_name = database
 
     # Get list of tables in the current database
-    tables = glueContext.get_tables(database_name)
+    tables = databases[database]
 
     # Iterate over each table in the current database
     for table in tables:
-        table_name = table.name
+        table_name = table
 
         # Construct source and destination paths for the current table
         source_table_path = "{}/{}".format(source_path, table_name)
@@ -55,6 +78,8 @@ for database in databases:
         dataframe = datasource.toDF()
 
         # Write DataFrame to CSV format with size check and splitting
-        dataframe.write.option("header", "true").option("maxPartitionBytes", 5*1024*1024).csv(destination_path, mode="overwrite")
+        dataframe.write.option("header", "true").option(
+            "maxPartitionBytes", 5 * 1024 * 1024
+        ).csv(destination_path, mode="overwrite")
 
 job.commit()
