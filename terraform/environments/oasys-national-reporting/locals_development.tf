5b03f7906bdf00cf4b4661c190ff87ce5390160b
modernisation-platform-ci <modernisation-platform+github@digital.justice.gov.uk>
2024-04-08 04:53:22 +0000

    Updates from GitHub Actions Format Code workflow

--- terraform/environments/oasys-national-reporting/locals_development.tf
+++ terraform/environments/oasys-national-reporting/locals_development.tf
@@ -4,38 +4,38 @@ locals {
   development_config = {
 
     baseline_ec2_instances = {
-    #   dev-web-a = merge(local.defaults_web_ec2, 
-    #   {
-    #     config = merge(local.defaults_web_ec2.config, {
-    #       availability_zone = "${local.region}a"        
-    #     })
-    #     instance = merge(local.defaults_web_ec2.instance, {
-    #       instance_type = "t3.large"
-    #     })
-    #   })
-    #   dev-boe-a = merge(local.defaults_boe_ec2, 
-    #   {
-    #     config = merge(local.defaults_boe_ec2.config, {
-    #       availability_zone = "${local.region}a"        
-    #     })
-    #     instance = merge(local.defaults_boe_ec2.instance, {
-    #       instance_type = "t2.large"
-    #     })
-    #     user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
-    #       args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
-    #         branch = "main"
-    #       })
-    #     })    
-    #   })
-    #   dev-bods-a = merge(local.defaults_bods_ec2,
-    #   {
-    #     config = merge(local.defaults_bods_ec2.config, {
-    #       availability_zone = "${local.region}a"        
-    #     })
-    #     instance = merge(local.defaults_bods_ec2.instance, {    
-    #       instance_type = "t3.large"  
-    #     })
-    #   })
+      #   dev-web-a = merge(local.defaults_web_ec2, 
+      #   {
+      #     config = merge(local.defaults_web_ec2.config, {
+      #       availability_zone = "${local.region}a"        
+      #     })
+      #     instance = merge(local.defaults_web_ec2.instance, {
+      #       instance_type = "t3.large"
+      #     })
+      #   })
+      #   dev-boe-a = merge(local.defaults_boe_ec2, 
+      #   {
+      #     config = merge(local.defaults_boe_ec2.config, {
+      #       availability_zone = "${local.region}a"        
+      #     })
+      #     instance = merge(local.defaults_boe_ec2.instance, {
+      #       instance_type = "t2.large"
+      #     })
+      #     user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
+      #       args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
+      #         branch = "main"
+      #       })
+      #     })    
+      #   })
+      #   dev-bods-a = merge(local.defaults_bods_ec2,
+      #   {
+      #     config = merge(local.defaults_bods_ec2.config, {
+      #       availability_zone = "${local.region}a"        
+      #     })
+      #     instance = merge(local.defaults_bods_ec2.instance, {    
+      #       instance_type = "t3.large"  
+      #     })
+      #   })
     }
 
     baseline_ec2_autoscaling_groups = {
@@ -58,7 +58,7 @@ locals {
       })
       dev-boe-asg = merge(local.defaults_boe_ec2, {
         config = merge(local.defaults_boe_ec2.config, {
-          availability_zone = "${local.region}a"        
+          availability_zone = "${local.region}a"
         })
         instance = merge(local.defaults_boe_ec2.instance, {
           instance_type = "t2.large"
@@ -71,10 +71,10 @@ locals {
         autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
           desired_capacity = 0
         })
-        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours    
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
       })
       dev-bods-asg = merge(local.defaults_bods_ec2, {
-        config = merge(local.defaults_bods_ec2.config,{
+        config = merge(local.defaults_bods_ec2.config, {
           availability_zone = "${local.region}a"
         })
         instance = merge(local.defaults_bods_ec2.instance, {
