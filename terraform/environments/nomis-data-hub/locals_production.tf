529fa2fe6269a462abd9d54fa31a85b82cf829af
Antony Gowland <36728441+antonygowland@users.noreply.github.com>
2024-04-09 14:58:19 +0100

    Update locals_production.tf (#5636)

--- terraform/environments/nomis-data-hub/locals_production.tf
+++ terraform/environments/nomis-data-hub/locals_production.tf
@@ -3,7 +3,7 @@ locals {
     baseline_route53_zones = {
       "ndh.nomis.service.justice.gov.uk" = {
         records = [
-          { name = "preproduction", records = ["ns-1411.awsdns-48.org", "ns-885.awsdns-46.net", "ns-56.awsdns-07.com", "ns-1799.awsdns-32.co.uk"], type = "NS", ttl = "86400" },
+          { name = "preproduction", records = ["ns-175.awsdns-21.com", "ns-1492.awsdns-58.org", "ns-1647.awsdns-13.co.uk", "ns-535.awsdns-02.net"], type = "NS", ttl = "86400" },
           { name = "test", records = ["ns-498.awsdns-62.com", "ns-881.awsdns-46.net", "ns-1294.awsdns-33.org", "ns-1610.awsdns-09.co.uk"], type = "NS", ttl = "86400" }
         ]
       }
