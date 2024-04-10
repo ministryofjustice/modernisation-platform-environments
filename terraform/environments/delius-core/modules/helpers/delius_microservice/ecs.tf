96a38b198d07bf66d1a5b569bcf36b70ca52accf
Seb Norris <sebastian@automationlogic.com>
2024-04-10 15:34:56 +0100

    rebased module branch

--- terraform/environments/delius-core/modules/helpers/delius_microservice/ecs.tf
+++ terraform/environments/delius-core/modules/helpers/delius_microservice/ecs.tf
@@ -1,5 +1,5 @@
 module "container_definition" {
-  source                   = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//container?ref=3c764d9"
+  source                   = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//container?ref=711e9e4"
   name                     = var.name
   image                    = var.container_image
   memory                   = var.container_memory
@@ -30,7 +30,7 @@ module "ecs_policies" {
 }
 
 module "ecs_service" {
-  source                = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//service?ref=3c764d9"
+  source                = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//service?ref=711e9e4"
   container_definitions = module.container_definition.json_encoded_list
   cluster_arn           = var.ecs_cluster_arn
   name                  = var.name
