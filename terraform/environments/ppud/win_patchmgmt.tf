3e4f509e35ee7292306d5f46562bb271f768d6a7
Buckingham <XC01857@lumen.com>
2024-04-10 14:07:25 +0100

    Update_100424_6

--- terraform/environments/ppud/win_patchmgmt.tf
+++ terraform/environments/ppud/win_patchmgmt.tf
@@ -199,10 +199,8 @@ resource "aws_ssm_document" "perform_healthcheck_s3" {
 # Test Group
 
 resource "aws_ssm_patch_group" "win_patch_test_group" {
-  count       = local.is-development == true ? 1 : 0
-  baseline_id = aws_ssm_patch_baseline.windows_os_apps_test_baseline.id
-  patch_group = local.application_data.accounts[local.environment].test_patch_group
-
+    baseline_id = aws_ssm_patch_baseline.windows_os_apps_test_baseline.id
+    patch_group = local.application_data.accounts[local.environment].test_patch_group
 }
 
 # Create Windows Patch Baseline
