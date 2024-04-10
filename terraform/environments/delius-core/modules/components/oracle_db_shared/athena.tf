364dd4b8da2021b8f059bd73aa0ff8262ab6c616
George Taylor <george.taylor@digital.justice.gov.uk>
2024-04-10 01:34:38 +0100

    Update athena.tf

--- terraform/environments/delius-core/modules/components/oracle_db_shared/athena.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/athena.tf
@@ -71,7 +71,7 @@ resource "aws_glue_catalog_table" "this" {
 
 resource "aws_athena_database" "example" {
   name   = "athena_audit_db_${var.env_name}"
-  bucket = module.s3_bucket_athena_output[0].bucket.bucket
+  bucket = module.s3_bucket_athena_output.bucket.bucket
 }
 
 
@@ -92,7 +92,6 @@ resource "aws_athena_data_catalog" "example" {
 }
 
 module "s3_bucket_athena_output" {
-  count               = var.audit_main_account ? 1 : 0
   source              = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.0.0"
   bucket_name         = "delius-core-${var.env_name}-athena-output"
   versioning_enabled  = false
