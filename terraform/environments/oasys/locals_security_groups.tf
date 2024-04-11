9ade12aa61f6c7c896574c737860dc9355c11b1c
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-04-11 09:37:23 +0100

    fix

--- terraform/environments/oasys/locals_security_groups.tf
+++ terraform/environments/oasys/locals_security_groups.tf
@@ -51,9 +51,6 @@ locals {
       "10.40.40.0/24", # pp oasys
       "10.40.37.0/24", # pp prison nomis
       module.ip_addresses.azure_fixngo_cidrs.prod_jumpservers,
-      module.ip_addresses.moj_cidr.aws_data_engineering_dev,
-      module.ip_addresses.moj_cidr.aws_data_engineering_prod,
-      module.ip_addresses.moj_cidr.aws_data_engineering_stage,
     ])
     oracle_oem_agent = flatten([
       module.ip_addresses.azure_fixngo_cidrs.prod,
@@ -100,9 +97,10 @@ locals {
       "10.40.3.0/24", # prod prison nomis
       module.ip_addresses.azure_fixngo_cidrs.prod_jumpservers,
       module.ip_addresses.azure_fixngo_cidrs.prod,
-      module.ip_addresses.moj_cidr.aws_analytical_platform_aggregate,
       module.ip_addresses.azure_studio_hosting_cidrs.prod,
-      module.ip_addresses.azure_nomisapi_cidrs.prod,
+      module.ip_addresses.moj_cidr.aws_data_engineering_dev,
+      module.ip_addresses.moj_cidr.aws_data_engineering_prod,
+      module.ip_addresses.moj_cidr.aws_data_engineering_stage,
     ])
     oracle_oem_agent = flatten([
       module.ip_addresses.azure_fixngo_cidrs.prod,
