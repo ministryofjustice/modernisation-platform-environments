9d72598ec5635f5c41e08d3818b8e07aeec653d0
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2024-04-12 12:26:46 +0100

    LAWS-3850: Fixing access policy for Vault

--- terraform/environments/portal/backups.tf
+++ terraform/environments/portal/backups.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "portal" {
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${local.modernisation_platform_account_id}:root"]
+      identifiers = ["arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:root"]
     }
 
     actions = [
