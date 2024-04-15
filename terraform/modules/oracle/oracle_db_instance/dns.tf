ea4a70558cb40df378ec0daa2db6161df599392f
Seb Norris <sebastian@automationlogic.com>
2024-04-15 13:35:21 +0100

    copy delius core oracle files to new module

---
+++ terraform/modules/oracle/oracle_db_instance/dns.tf
@@ -0,0 +1,8 @@
+resource "aws_route53_record" "db_ec2_instance_internal" {
+  provider = aws.core-vpc
+  zone_id  = var.account_config.route53_inner_zone_info.zone_id
+  name     = var.db_type == "primary" ? "${var.env_name}-delius-db-${var.db_count_index}.delius.${var.account_config.route53_inner_zone_info.name}" : "${var.env_name}-delius-db-${var.db_count_index + 1}.delius.${var.account_config.route53_inner_zone_info.name}"
+  type     = "CNAME"
+  ttl      = 60
+  records  = [module.instance.aws_instance.private_dns]
+}
---
+++ terraform/modules/oracle/oracle_db_shared/backup_vault.tf
@@ -0,0 +1,10 @@
+resource "aws_backup_vault" "oracle_backup_vault" {
+  name        = "${var.env_name}-oracle-backup-vault"
+  kms_key_arn = var.account_config.kms_keys.general_shared
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "${var.env_name}-oracle-backup-vault"
+    },
+  )
+}
