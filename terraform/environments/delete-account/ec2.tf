b2cdbae601446015aee264e0bee072282ddd65d0
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-04-16 10:50:03 +0100

    ec2 fix

--- terraform/environments/delete-account/ec2.tf
+++ terraform/environments/delete-account/ec2.tf
@@ -1,28 +1,4 @@
-resource "aws_instance" "delete-account_test_instance" {
-  ami                         = local.application_data.accounts[local.environment].ec2amiid
-  associate_public_ip_address = false
-  availability_zone           = "eu-west-2a"
-  ebs_optimized               = true
-  instance_type               = local.application_data.accounts[local.environment].ec2instancetype
-  monitoring                  = true
-  subnet_id                   = data.aws_subnet.private_subnets_a.id
-
-
-  root_block_device {
-    delete_on_termination = false
-    encrypted             = true
-    volume_size           = 60
-    volume_type           = "gp2"
-    tags = merge(
-      local.tags,
-      { "Name" = "${local.application_name}test-ec2-root" },
-    )
-  }
-
-  tags = merge(
-    local.tags,
-    { "Name" = "${local.application_name} test server" },
-    { "instance-scheduling" = "skip-scheduling" },
-    { "snapshot-with-daily-7-day-retention" = "yes" }
-  )
+resource "aws_instance" "myec2" {
+  ami           = "ami-0648ea225c13e0729"
+  instance_type = "t2.micro"
 }
