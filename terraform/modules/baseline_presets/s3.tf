78e83c8e3d0c37b3e13c743a56fe9e7730e37235
W <willg258@hotmail.co.uk>
2024-04-18 17:28:32 +0100

    Update s3.tf

--- terraform/modules/baseline_presets/s3.tf
+++ terraform/modules/baseline_presets/s3.tf
@@ -26,7 +26,7 @@ locals {
 
     # If db_backup_s3 enabled, create db_backups in all environments.
     # Dev and Test can both access each other: Preprod can access prod but not vice-versa
-    var.options.db_backup_s3 && var.environment.environment == "production" && ! var.options.preprod_write_prod ? { "prod-${var.environment.application_name}-db-backup-bucket-" = {
+    var.options.db_backup_s3 && var.environment.environment == "production" && ! var.options.preprod_read_write_delete_prod ? { "prod-${var.environment.application_name}-db-backup-bucket-" = {
       bucket_policy_v2 = [
         local.s3_bucket_policies.PreprodReadOnlyAccessBucketPolicy
       ]
