a2e512fdde5a509345836b9c1f2ff60903b263f9
George Taylor <george.taylor@digital.justice.gov.uk>
2024-04-18 19:53:24 +0100

    Update ecs_monitoring.tf

--- terraform/environments/delius-core/modules/components/ldap/ecs_monitoring.tf
+++ terraform/environments/delius-core/modules/components/ldap/ecs_monitoring.tf
@@ -4,73 +4,47 @@ locals {
 }
 # Alarm for high CPU usage
 resource "aws_cloudwatch_metric_alarm" "ecs_cpu_over_threshold" {
-  alarm_name                = "ldap-${var.env_name}-ecs-cpu-threshold"
-  comparison_operator       = "GreaterThanUpperThreshold"
-  evaluation_periods        = "5"
-  threshold_metric_id       = "e1"
-  alarm_description         = "Triggers alarm if ECS CPU crosses a threshold"
-  insufficient_data_actions = []
-  alarm_actions             = [var.sns_topic_arn]
-  ok_actions                = [var.sns_topic_arn]
-  treat_missing_data        = "missing"
+  alarm_name          = "ldap-${var.env_name}-ecs-memory-threshold"
+  alarm_description   = "Triggers alarm if ECS CPU crosses a threshold"
+  namespace           = "AWS/ECS"
+  metric_name         = "CPUUtilization"
+  statistic           = "Average"
+  period              = "60"
+  evaluation_periods  = "5"
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
+  threshold           = "80"
+  treat_missing_data  = "missing"
+  comparison_operator = "GreaterThanThreshold"
 
-  metric_query {
-    id          = "e1"
-    expression  = "ANOMALY_DETECTION_BAND(m1,4)"
-    label       = "CPUUtilization (Expected)"
-    return_data = "true"
+  dimensions = {
+    ClusterName = local.cluster_name
+    ServiceName = local.cluster_name
   }
 
-  metric_query {
-    id          = "m1"
-    return_data = "true"
-    metric {
-      metric_name = "CpuUtilized"
-      namespace   = "AWS/ECS"
-      period      = "120"
-      stat        = "Average"
-
-      dimensions = {
-        ClusterName = local.cluster_name
-        ServiceName = local.cluster_name
-      }
-    }
-  }
+  tags = var.tags
 }
 
 # Alarm for high memory usage
 resource "aws_cloudwatch_metric_alarm" "memory_over_threshold" {
-  alarm_name                = "ldap-${var.env_name}-ecs-memory-threshold"
-  comparison_operator       = "GreaterThanUpperThreshold"
-  evaluation_periods        = "2"
-  threshold_metric_id       = "e1"
-  alarm_description         = "Triggers alarm if ECS memory crosses a threshold"
-  insufficient_data_actions = []
-  alarm_actions             = [var.sns_topic_arn]
-  ok_actions                = [var.sns_topic_arn]
-  treat_missing_data        = "missing"
+  alarm_name          = "ldap-${var.env_name}-ecs-memory-threshold"
+  alarm_description   = "Triggers alarm if ECS memory crosses a threshold"
+  namespace           = "AWS/ECS"
+  metric_name         = "MemoryUtilization"
+  statistic           = "Average"
+  period              = "60"
+  evaluation_periods  = "5"
+  alarm_actions       = [aws_sns_topic.jitbit_alerting.arn]
+  ok_actions          = [aws_sns_topic.jitbit_alerting.arn]
+  threshold           = "80"
+  treat_missing_data  = "missing"
+  comparison_operator = "GreaterThanThreshold"
 
-  metric_query {
-    id          = "e1"
-    expression  = "ANOMALY_DETECTION_BAND(m1,4)"
-    label       = "MemoryUtilization (Expected)"
-    return_data = "true"
+  dimensions = {
+    ClusterName = local.cluster_name
+    ServiceName = local.cluster_name
   }
 
-  metric_query {
-    id          = "m1"
-    return_data = "true"
-    metric {
-      metric_name = "MemoryUtilization"
-      namespace   = "AWS/ECS"
-      period      = "120"
-      stat        = "Average"
-
-      dimensions = {
-        ClusterName = local.cluster_name
-      }
-    }
-  }
 }
 
 resource "aws_cloudwatch_log_metric_filter" "error" {
