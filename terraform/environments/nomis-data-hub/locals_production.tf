e849e53f10c892b7aa144556d1440822f0e5e13f
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2024-04-18 09:51:12 +0100

    Create prod ndh vms

--- terraform/environments/nomis-data-hub/locals_production.tf
+++ terraform/environments/nomis-data-hub/locals_production.tf
@@ -11,5 +11,72 @@ locals {
     baseline_secretsmanager_secrets = {
       "/ndh/pd" = local.ndh_secretsmanager_secrets
     }
+    baseline_iam_policies = {
+      Ec2ppPolicy = {
+        description = "Permissions required for PD EC2s"
+        statements = [
+          {
+            effect = "Allow"
+            actions = [
+              "secretsmanager:GetSecretValue",
+            ]
+            resources = [
+              "arn:aws:secretsmanager:*:*:secret:/ndh/pd/*",
+            ]
+          }
+        ]
+      }
+    }
+    baseline_ec2_instances = {
+      prodution-management-server-2022 = merge(local.management_server_2022, {
+        tags = merge(local.management_server_2022.tags, {
+          nomis-data-hub-environment = "prodution"
+        })
+      })
+      pd-ndh-app-a = merge(local.ndh_app_a, {
+        config = merge(local.ndh_app_a.config, {
+          instance_profile_policies = concat(local.ndh_app_a.config.instance_profile_policies, [
+            "Ec2ppPolicy",
+          ])
+        })
+        tags = merge(local.ndh_app_a.tags, {
+          os-type                    = "Linux"
+          nomis-data-hub-environment = "pd"
+        })
+      })
+      pd-ndh-ems-a = merge(local.ndh_ems_a, {
+        config = merge(local.ndh_ems_a.config, {
+          instance_profile_policies = concat(local.ndh_ems_a.config.instance_profile_policies, [
+            "Ec2ppPolicy",
+          ])
+        })
+        tags = merge(local.ndh_ems_a.tags, {
+          os-type                    = "Linux"
+          nomis-data-hub-environment = "pd"
+        })
+      })
+      pd-ndh-app-b = merge(local.ndh_app_b, {
+        config = merge(local.ndh_app_b.config, {
+          instance_profile_policies = concat(local.ndh_app_b.config.instance_profile_policies, [
+            "Ec2ppPolicy",
+          ])
+        })
+        tags = merge(local.ndh_app_b.tags, {
+          os-type                    = "Linux"
+          nomis-data-hub-environment = "pd"
+        })
+      })
+      pd-ndh-ems-b = merge(local.ndh_ems_b, {
+        config = merge(local.ndh_ems_b.config, {
+          instance_profile_policies = concat(local.ndh_ems_b.config.instance_profile_policies, [
+            "Ec2ppPolicy",
+          ])
+        })
+        tags = merge(local.ndh_ems_b.tags, {
+          os-type                    = "Linux"
+          nomis-data-hub-environment = "pd"
+        })
+      })
+    }
   }
 }
