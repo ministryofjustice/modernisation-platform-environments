5ad2eb8682cf4b9eaf42ec378099b43bcdf6d190
Aaron Robinson <aaron.robinson@digital.justice.gov.uk>
2024-04-18 15:11:52 +0100

    Terraform init assume backend role

--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# This script runs terraform init with input set to false and no color outputs, suitable for running as part of a CI/CD pipeline.
-# You need to pass through a Terraform directory as an argument, e.g.
-# sh terraform-init.sh terraform/environments
+# This script runs terraform init with input set to false, no color outputs, and backend-config, suitable for running as part of a CI/CD pipeline.
+# You need to pass through a Terraform directory and backend config as arguments, e.g.
+# sh terraform-init.sh terraform/environments "assume_role={role_arn=\"arn:aws:iam::123456789012:role/github-actions\"}"
 
-if [ -z "$1" ]; then
-  echo "Unsure where to run terraform, exiting"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Unsure where to run terraform, exiting. (Usage: terraform-init.sh <terraform_directory> <backend_config>)"
   exit 1
 else
-  terraform -chdir="$1" init -input=false -no-color
+  terraform -chdir="$1" init -input=false -no-color -backend-config="$2"
 fi
