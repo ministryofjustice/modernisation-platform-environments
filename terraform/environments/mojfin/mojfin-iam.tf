7a52bbe4b37747e24fe9fde64573bbfb3f3d204f
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2024-04-18 10:52:39 +0100

    CC-2507: Fixed typos.

--- terraform/environments/mojfin/mojfin-iam.tf
+++ terraform/environments/mojfin/mojfin-iam.tf
@@ -5,29 +5,40 @@ resource "aws_iam_role" "mojfin_rds_oracle" {
   assume_role_policy = data.aws_iam_policy_document.mojfin_rds_oracle.json
 }
 
-data "aws_iam_policy_document" "mojfin_rds_oracle" {
+data "aws_iam_policy_document" "mojfin_rds_oracle_assume" {
   statement {
     effect = "Allow"
     actions = [
-      "s3:DeleteObject",
-      "s3:GetObject",
-      "s3:ListBucket",
-      "s3:PutObject"
+      "sts:AssumeRole"
     ]
     principals {
-      type        = "Service"
-      identifiers = ["rds.amazonaws.com"]
+      type = "Service"
+      identifiers = [
+        "rds.amazonaws.com"
+      ]
     }
-#   resources = [
-#     "${aws_s3_bucket.mojfin_rds_oracle.arn}/*"
-#   ]
+  }
+}
+
+data "aws_iam_policy_document" "mojfin_rds_oracle_s3" {
+  statement {
+    effect = "Allow"
+    actions = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject"
+    ]
+    resources = [
+          aws_s3_bucket.mojfin_rds_oracle.arn,
+          "${aws_s3_bucket.mojfin_rds_oracle.arn}/*"
+    ]
   }
 }
 
 resource "aws_iam_policy" "mojfin_rds_oracle" {
   name        = "mojfin_rds_oracle-${local.environment}"
   description = "Policy for Oracle RDS in ${local.environment}"
-  policy      = data.aws_iam_policy_document.mojfin_rds_oracle.json
+  policy = data.aws_iam_policy_document.mojfin_rds_oracle_s3.json
 }
 
 resource "aws_db_instance_role_association" "mojfin_rds_oracle" {
