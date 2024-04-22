25ad08f337e53ca1958ae01916b57f557ef96e1f
matt-heery <matt.heery@digital.justice.gov.uk>
2024-04-22 18:06:19 +0100

    proper addition

--- terraform/environments/electronic-monitoring-data/lambdas/manual_trigger_lambda.py
+++ terraform/environments/electronic-monitoring-data/lambdas/manual_trigger_lambda.py
@@ -1,24 +1,33 @@
 import boto3
 from logging import getLogger
 import sys
+import json
 
 logger = getLogger(__name__)
+AWS_REGION = "eu-west-2"
 
 # Initialize Boto3 client for S3
 s3 = boto3.client("s3")
 
 
-def invoke_lambda_for_file(file_key, lambda_function_name):
+def invoke_copy_lambda(
+    source_bucket_name, file_key, lambda_function_name, destination_bucket_name
+):
     """
-    Invoke the Lambda function for a given file.
+    Invoke the Lambda function to copy file from source to destination bucket.
     """
-    lambda_client = boto3.client("lambda", region_name="eu-west-2")
+    lambda_client = boto3.client("lambda", region_name=AWS_REGION)
+    payload = {
+        "source_bucket_name": source_bucket_name,
+        "file_key": file_key,
+        "destination_bucket_name": destination_bucket_name,
+    }
     response = lambda_client.invoke(
         FunctionName=lambda_function_name,
         InvocationType="Event",  # Asynchronous invocation
-        Payload='{"file_key": "' + file_key + '"}',  # Sending file key as payload
+        Payload=json.dumps(payload),
     )
-    print(f"Invoked Lambda '{lambda_function_name}' for file: {file_key}")
+    logger.info(f"Invoked Lambda '{lambda_function_name}' to copy file '{file_key}'")
 
 
 def process_s3_bucket(bucket_name, lambda_function_name):
