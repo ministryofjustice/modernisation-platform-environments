584da22cb7abfae2ad43fa2317ba45d6bdd6e5f4
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-04-22 15:35:37 +0100

    onr: change secret name from t1 to test
    It allows the Ansible to source the name from the default environment
    tags, which use "test" by default.

--- terraform/environments/oasys-national-reporting/locals_test.tf
+++ terraform/environments/oasys-national-reporting/locals_test.tf
@@ -4,7 +4,7 @@ locals {
   test_config = {
 
     baseline_secretsmanager_secrets = {
-      "/ec2/onr-web/t1" = local.web_secretsmanager_secrets
+      "/ec2/onr-web/test" = local.web_secretsmanager_secrets
 
       "/oracle/database/T3ONRAU"  = local.database_secretsmanager_secrets
       "/oracle/database/T3ONRBDS" = local.database_secretsmanager_secrets
