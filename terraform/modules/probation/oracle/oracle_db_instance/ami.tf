e5f39314a3b6cd2d31bef10396ae82c4da5994f8
Seb Norris <sebastian@automationlogic.com>
2024-04-24 13:58:48 +0100

    remove probation module for now

--- terraform/modules/probation/oracle/oracle_db_instance/ami.tf
+++
@@ -1,5 +0,0 @@
-data "aws_ami" "oracle_db" {
-  owners      = [var.db_ami.owner]
-  name_regex  = var.db_ami.name_regex
-  most_recent = true
-}
--- terraform/modules/probation/oracle/oracle_db_instance/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  instance_name_index = var.db_type == "primary" ? var.db_count_index : var.db_count_index + 1
-  database_tag        = var.db_type == "primary" ? "delius_${var.db_type}db" : "delius_${var.db_type}db${var.db_count_index}"
-}
