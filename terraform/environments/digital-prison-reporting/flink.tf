9cc631c9b6090837d6077f05d9396e3ee806a7a1
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-04-24 17:25:02 +0100

    DPR2-560: bump jar version

--- terraform/environments/digital-prison-reporting/flink.tf
+++ terraform/environments/digital-prison-reporting/flink.tf
@@ -91,7 +91,7 @@ resource "aws_kinesisanalyticsv2_application" "flink_spike_app" {
       code_content {
         s3_content_location {
           bucket_arn = "arn:aws:s3:::flink-demo-771283872747-eu-west-2-1713861000297-bucket"
-          file_key   = "flink-spike-0.1-SNAPSHOT-all.jar"
+          file_key   = "flink-spike-0.2-SNAPSHOT-all.jar"
         }
       }
 
