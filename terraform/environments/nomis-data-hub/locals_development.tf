4855b49c8db66da5047d3788ae1cb61edfc077c1
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-04-24 11:17:07 +0100

    add ASG

--- terraform/environments/nomis-data-hub/locals_development.tf
+++ terraform/environments/nomis-data-hub/locals_development.tf
@@ -51,5 +51,33 @@ locals {
         #}
       }
     }
+
+    baseline_ec2_autoscaling_groups = {
+      dev-base-rhel85 = {
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 1
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        config = merge(module.baseline_presets.ec2_instance.config.default, {
+          ami_name          = "base_rhel_8_5_*"
+          availability_zone = null
+        })
+        instance = merge(module.baseline_presets.ec2_instance.instance.default, {
+          vpc_security_group_ids = ["private"]
+        })
+        user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
+          args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
+            branch = "main"
+          })
+        })
+        tags = {
+          description = "For testing our base RHEL8.5 base image"
+          ami         = "base_rhel_8_5"
+          os-type     = "Linux"
+          component   = "test"
+          server-type = "base-rhel85"
+        }
+      }
+    }
   }
 }
