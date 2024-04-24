9549308258d7fcfb7e624f84573afeae19aa0618
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-04-24 11:03:04 +0100

    uncomment aws_route53_record

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -24,19 +24,19 @@ resource "aws_route53_record" "external_validation" {
 }
 
 # // Create one Route 53 record for each entry in the list of tribunals (assigned in platform_locals.tf)
-# resource "aws_route53_record" "external" {
-#   provider = aws.core-vpc
-#   count = length(local.tribunal_names)
-#   zone_id = data.aws_route53_zone.external.zone_id
-#   name    = "${local.tribunal_names[count.index]}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-#   type    = "A"
+resource "aws_route53_record" "external" {
+  provider = aws.core-vpc
+  count = length(local.tribunal_names)
+  zone_id = data.aws_route53_zone.external.zone_id
+  name    = "${local.tribunal_names[count.index]}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type    = "A"
 
-#   alias {
-#     name                   = module.ecs_loadbalancer.tribunals_lb.dns_name
-#     zone_id                = module.ecs_loadbalancer.tribunals_lb.zone_id
-#     evaluate_target_health = true
-#   }
-# }
+  alias {
+    name                   = module.ecs_loadbalancer.tribunals_lb.dns_name
+    zone_id                = module.ecs_loadbalancer.tribunals_lb.zone_id
+    evaluate_target_health = true
+  }
+}
 
 # resource "aws_route53_record" "external_appeals" {
 #   provider = aws.core-vpc 
