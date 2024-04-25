43543a2e2f0014fbc446cdef67c9580bbbbe223f
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-04-25 15:42:25 +0100

    re add tls policy to data store bucket

--- terraform/environments/electronic-monitoring-data/data_store.tf
+++ terraform/environments/electronic-monitoring-data/data_store.tf
@@ -45,47 +45,31 @@ resource "aws_s3_bucket_versioning" "data_store" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "data_store" {
-#   bucket = aws_s3_bucket.data_store.id
-#   policy = data.aws_iam_policy_document.data_store.json
-# }
+resource "aws_s3_bucket_policy" "data_store" {
+  bucket = aws_s3_bucket.data_store.id
+  policy = data.aws_iam_policy_document.data_store.json
+}
 
-# data "aws_iam_policy_document" "data_store" {
-  # statement {
-  #   sid = "ReadBucket"
-  #   principals {
-  #     type        = "AWS"
-  #     identifiers = ["*"]
-  #   }
-  #   effect  = "Allow"
-  #   actions = [
-  #     "s3:ListBucket",
-  #     "s3:GetObject"
-  #   ]
-  #   resources = [
-  #     aws_s3_bucket.data_store.arn,
-  #     "${aws_s3_bucket.data_store.arn}/*"
-  #   ]
-  # }
-  # statement {
-  #   sid = "EnforceTLSv12orHigher"
-  #   principals {
-  #     type        = "AWS"
-  #     identifiers = ["*"]
-  #   }
-  #   effect  = "Deny"
-  #   actions = ["s3:*"]
-  #   resources = [
-  #     aws_s3_bucket.data_store.arn,
-  #     "${aws_s3_bucket.data_store.arn}/*"
-  #   ]
-  #   condition {
-  #     test     = "NumericLessThan"
-  #     variable = "s3:TlsVersion"
-  #     values   = [1.2]
-  #   }
-  # }
-# }
+data "aws_iam_policy_document" "data_store" {
+  statement {
+    sid = "EnforceTLSv12orHigher"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.data_store.arn,
+      "${aws_s3_bucket.data_store.arn}/*"
+    ]
+    condition {
+      test     = "NumericLessThan"
+      variable = "s3:TlsVersion"
+      values   = [1.2]
+    }
+  }
+}
 
 resource "aws_s3_bucket_logging" "data_store" {
   bucket = aws_s3_bucket.data_store.id
