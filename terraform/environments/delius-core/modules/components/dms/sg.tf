e11c47af4bd6ca4eb8af0034f3a16797b04824d3
Ijaz Sultan <mohammed.ijazsultan@digital.justice.gov.uk>
2024-04-25 12:44:01 +0100

    module call added

--- terraform/environments/delius-core/modules/components/dms/sg.tf
+++ terraform/environments/delius-core/modules/components/dms/sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "dms" {
-  vpc_id = var.account_config.vpc_id
+  vpc_id = var.account_info.vpc_id
   name = "Modernisation-platform-DMS-security_group"
   description = "Security group for aws DMS service in Mod platformn"
 }
