dbcc2f0b5a55492f420c499d70f01f7f61a6e99d
George Taylor <george.taylor@digital.justice.gov.uk>
2024-04-25 15:20:31 +0100

    Update instance.tf

--- terraform/environments/delius-core/modules/components/dms/instance.tf
+++ terraform/environments/delius-core/modules/components/dms/instance.tf
@@ -3,14 +3,15 @@ resource "aws_dms_replication_instance" "test" {
   apply_immediately            = true
   auto_minor_version_upgrade   = true
   availability_zone            = data.aws_region.current.name
-  engine_version               = "3.1.4"
+  engine_version               = "3.5"
   kms_key_arn                  = var.account_config.kms_keys.general_shared
   multi_az                     = false
   preferred_maintenance_window = "wed:22:00-wed:23:30"
-  publicly_accessible          = true
+  publicly_accessible          = false
   replication_instance_class   = var.instance_class
   replication_instance_id      = "${var.env_name}-dms-instance"
   replication_subnet_group_id  = aws_dms_replication_subnet_group.this.id
+  network_type                 = "IPV4"
 
   tags = var.tags
 
