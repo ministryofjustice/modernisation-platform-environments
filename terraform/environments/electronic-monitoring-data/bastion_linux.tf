7259ce3db41679da03a0091c6caedf8da8b92b84
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-04-26 10:09:20 +0100

    add seperate instances for access needs

--- terraform/environments/electronic-monitoring-data/bastion_linux.tf
+++ terraform/environments/electronic-monitoring-data/bastion_linux.tf
@@ -32,6 +32,42 @@ module "rds_bastion" {
   subnet_set    = local.subnet_set
   environment   = local.environment
   region        = "eu-west-2"
+
+  # tags
+  tags_common = local.tags
+  tags_prefix = terraform.workspace
+}
+
+# tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
+module "s3_rds_bastion" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v4.2.1"
+
+  providers = {
+    aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
+    aws.share-tenant = aws          # The default provider (unaliased, `aws`) is the tenant
+  }
+
+  # s3 - used for logs and user ssh public keys
+  bucket_name = "s3-rds-bastion"
+
+  # public keys
+  public_key_data = local.public_key_data.keys[local.environment]
+
+  # logs
+  log_auto_clean       = "Enabled"
+  log_standard_ia_days = 30  # days before moving to IA storage
+  log_glacier_days     = 60  # days before moving to Glacier
+  log_expiry_days      = 180 # days before log expiration
+
+  # bastion
+  instance_name = "s3_rds_bastion"
+
+  allow_ssh_commands = true
+  app_name      = var.networking[0].application
+  business_unit = local.vpc_name
+  subnet_set    = local.subnet_set
+  environment   = local.environment
+  region        = "eu-west-2"
   volume_size   = 20
 
   # tags
@@ -39,8 +75,63 @@ module "rds_bastion" {
   tags_prefix = terraform.workspace
 }
 
+# # tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
+# module "s3_bastion" {
+#   source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v4.2.1"
+
+#   providers = {
+#     aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
+#     aws.share-tenant = aws          # The default provider (unaliased, `aws`) is the tenant
+#   }
+
+#   # s3 - used for logs and user ssh public keys
+#   bucket_name = "s3-bastion"
+
+#   # public keys
+#   public_key_data = local.public_key_data.keys[local.environment]
+
+#   # logs
+#   log_auto_clean       = "Enabled"
+#   log_standard_ia_days = 30  # days before moving to IA storage
+#   log_glacier_days     = 60  # days before moving to Glacier
+#   log_expiry_days      = 180 # days before log expiration
+
+#   # bastion
+#   instance_name = "s3_bastion"
+
+#   allow_ssh_commands = true
+#   app_name      = var.networking[0].application
+#   business_unit = local.vpc_name
+#   subnet_set    = local.subnet_set
+#   environment   = local.environment
+#   region        = "eu-west-2"
+#   volume_size   = 1000
+
+#   # tags
+#   tags_common = local.tags
+#   tags_prefix = terraform.workspace
+# }
+
+locals {
+  rds_access = [
+    module.s3_rds_bastion.bastion_security_group,
+    module.rds_bastion.bastion_security_group
+  ]
+  s3_access = [
+    module.s3_rds_bastion.bastion_iam_role.name,
+    # module.s3_bastion.bastion_iam_role.name
+  ]
+  all_instances = [
+    module.rds_bastion.bastion_iam_role.name,
+    module.s3_rds_bastion.bastion_iam_role.name,
+    # module.s3_bastion.bastion_iam_role.name
+  ]
+}
+
 resource "aws_vpc_security_group_egress_rule" "access_ms_sql_server" {
-  security_group_id = module.rds_bastion.bastion_security_group
+  for_each = { for idx, sg_id in local.rds_access : idx => sg_id }
+
+  security_group_id = each.value
   description       = "EC2 MSSQL Access"
   ip_protocol       = "tcp"
   from_port         = 1433
@@ -49,7 +140,9 @@ resource "aws_vpc_security_group_egress_rule" "access_ms_sql_server" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "vpc_access" {
-  security_group_id = module.rds_bastion.bastion_security_group
+  for_each = { for idx, sg_id in local.rds_access : idx => sg_id }
+
+  security_group_id = each.value
   description       = "Reach vpc endpoints"
   ip_protocol       = "tcp"
   from_port         = 443
@@ -58,12 +151,14 @@ resource "aws_vpc_security_group_egress_rule" "vpc_access" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "rds_via_vpc_access" {
+  for_each = { for idx, sg_id in local.rds_access : idx => sg_id }
+
   security_group_id = aws_security_group.db.id
   description       = "EC2 instance connection to RDS"
   ip_protocol       = "tcp"
   from_port         = 1433
   to_port           = 1433
-  referenced_security_group_id = module.rds_bastion.bastion_security_group
+  referenced_security_group_id = each.value
 }
 
 data "aws_iam_policy_document" "ec2_s3_policy" {
@@ -90,13 +185,15 @@ data "aws_iam_policy_document" "ec2_s3_policy" {
 }
 
 resource "aws_iam_role_policy" "ec2_s3_policy" {
-  name   = "ec2-s3-policy"
-  role   = module.rds_bastion.bastion_iam_role.name
+  for_each = { for idx, sg_id in local.rds_access : idx => sg_id }
+
+  name   = "ec2-s3-policy-${each.value}"
+  role   = each.value
   policy = data.aws_iam_policy_document.ec2_s3_policy.json
 }
 
 resource "aws_iam_policy_attachment" "ssm-attachments" {
   name       = "ssm-attach-instance-role"
-  roles      = [module.rds_bastion.bastion_iam_role.name]
+  roles      = local.all_instances
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
