3bd8312be8035c731dfa4f15bfad8f4cde9216bc
Shajida Akthar <Shajida Akthar>
2024-04-26 17:08:50 +0100

    get sink identifier

--- terraform/modules/cross_account_cloudwatch/locals.tf
+++ terraform/modules/cross_account_cloudwatch/locals.tf
@@ -1,3 +1,3 @@
 locals {
-    monitoring_account_sink_identifier = var.options.enable_cloudwatch_cross_account_sharing ? "${aws_oam_sink.monitoring_account_oam_sink[0].id}" : null
+    monitoring_account_sink_identifier = aws_oam_sink.monitoring_account_oam_sink[0].id
 }
