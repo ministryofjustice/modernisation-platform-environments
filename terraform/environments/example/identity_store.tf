117d38a6042ea53fa2b86d70165b5e5b3caadc5f
SteveLinden <stephen.linden@digital.justice.gov.uk>
2024-04-26 10:46:28 +0100

    Commenting out amended code for identity_store.tf as it  won't work

--- terraform/environments/example/identity_store.tf
+++ terraform/environments/example/identity_store.tf
@@ -2,14 +2,14 @@ data "aws_ssoadmin_instances" "example" {
   provider = aws.sso-readonly
 }
 
-data "aws_identitystore_group" "example" {
-  provider          = aws.sso-readonly
-  identity_store_id = tolist(data.aws_ssoadmin_instances.example.identity_store_ids)[0]
+# data "aws_identitystore_group" "example" {
+#   provider          = aws.sso-readonly
+#   identity_store_id = tolist(data.aws_ssoadmin_instances.example.identity_store_ids)[0]
 
-  alternate_identifier {
-    unique_attribute{
-    attribute_path  = "DisplayName"
-    attribute_value = "modernisation-platform"
-  }
-}
-}
+#   alternate_identifier {
+#     unique_attribute{
+#     attribute_path  = "DisplayName"
+#     attribute_value = "modernisation-platform"
+#   }
+# }
+# }
