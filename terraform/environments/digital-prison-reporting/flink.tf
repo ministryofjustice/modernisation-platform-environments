77894d7d53d3a63d587c299d63c37afd5453f82a
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-04-26 10:27:49 +0100

    DPR2-560: 2 flink jobs - master_complex and master_summary

--- terraform/environments/digital-prison-reporting/flink.tf
+++ terraform/environments/digital-prison-reporting/flink.tf
@@ -1,7 +1,12 @@
+locals {
+  flink_views_jar_file = "flink-spike-0.20-SNAPSHOT-all.jar"
+  flink_jar_bucket_arn = "arn:aws:s3:::flink-demo-771283872747-eu-west-2-1713861000297-bucket"
+}
+
 resource "aws_iam_role" "flink_role" {
-  name = "dpr-flink-spike-execution-role-development"
+  name               = "dpr-flink-spike-execution-role-development"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
@@ -17,7 +22,7 @@ resource "aws_iam_role" "flink_role" {
 }
 
 resource "aws_iam_policy" "flink_spike_additional_policy" {
-  name = "flink_spike_additional_policy"
+  name   = "flink_spike_additional_policy"
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -67,18 +72,18 @@ resource "aws_iam_policy" "flink_spike_additional_policy" {
         ]
       },
       {
-        "Action": "cloudwatch:PutMetricData",
-        "Resource": "*",
-        "Effect": "Allow"
+        "Action" : "cloudwatch:PutMetricData",
+        "Resource" : "*",
+        "Effect" : "Allow"
       },
       {
-        "Action": [
+        "Action" : [
           "logs:DescribeLogGroups",
           "logs:DescribeLogStreams",
           "logs:PutLogEvents"
         ],
-        "Resource": "*",
-        "Effect": "Allow"
+        "Resource" : "*",
+        "Effect" : "Allow"
       }
     ]
   })
@@ -99,18 +104,32 @@ resource "aws_iam_role_policy_attachment" "flink_spike_vpc_full_access_policy_at
   policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
 }
 
-resource "aws_cloudwatch_log_group" "flink_log_group" {
-  name              = "/aws/kinesis-analytics/flink-spike-app"
+resource "aws_security_group" "flink_allow_outbound" {
+  name        = "flink_allow_outbound"
+  description = "Allow all outbound traffic from the Flink application"
+  vpc_id      = data.aws_vpc.shared.id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "flink_master_summary_log_group" {
+  name              = "/aws/kinesis-analytics/flink-master-summary"
   retention_in_days = 7
 }
 
-resource "aws_cloudwatch_log_stream" "flink_log_stream" {
-  name           = "flink-spike-app"
-  log_group_name = aws_cloudwatch_log_group.flink_log_group.name
+resource "aws_cloudwatch_log_stream" "flink_master_summary_log_stream" {
+  name           = "flink_master_summary"
+  log_group_name = aws_cloudwatch_log_group.flink_master_summary_log_group.name
 }
 
-resource "aws_kinesisanalyticsv2_application" "flink_spike_app" {
-  name                   = "flink-spike"
+resource "aws_kinesisanalyticsv2_application" "master_summary" {
+  name                   = "flink-master_summary"
   runtime_environment    = "FLINK-1_18"
   service_execution_role = aws_iam_role.flink_role.arn
 
@@ -119,7 +138,7 @@ resource "aws_kinesisanalyticsv2_application" "flink_spike_app" {
       property_group {
         property_group_id = "sql"
         property_map = {
-          "viewName": "master_summary"
+          "viewName" : "master_summary"
         }
       }
     }
@@ -127,8 +146,8 @@ resource "aws_kinesisanalyticsv2_application" "flink_spike_app" {
     application_code_configuration {
       code_content {
         s3_content_location {
-          bucket_arn = "arn:aws:s3:::flink-demo-771283872747-eu-west-2-1713861000297-bucket"
-          file_key   = "flink-spike-0.19-SNAPSHOT-all.jar"
+          bucket_arn = local.flink_jar_bucket_arn
+          file_key   = local.flink_views_jar_file
         }
       }
 
@@ -161,20 +180,72 @@ resource "aws_kinesisanalyticsv2_application" "flink_spike_app" {
   }
 
   cloudwatch_logging_options {
-    log_stream_arn = aws_cloudwatch_log_stream.flink_log_stream.arn
+    log_stream_arn = aws_cloudwatch_log_stream.flink_master_summary_log_stream.arn
   }
 }
 
-resource "aws_security_group" "flink_allow_outbound" {
-  name        = "flink_allow_outbound"
-  description = "Allow all outbound traffic from the Flink application"
-  vpc_id      = data.aws_vpc.shared.id
+resource "aws_cloudwatch_log_group" "flink_master_complex_log_group" {
+  name              = "/aws/kinesis-analytics/flink-master-complex"
+  retention_in_days = 7
+}
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+resource "aws_cloudwatch_log_stream" "flink_master_complex_log_stream" {
+  name           = "master_complex"
+  log_group_name = aws_cloudwatch_log_group.flink_master_complex_log_group.name
+}
+
+resource "aws_kinesisanalyticsv2_application" "master_complex" {
+  name                   = "flink-master_complex"
+  runtime_environment    = "FLINK-1_18"
+  service_execution_role = aws_iam_role.flink_role.arn
+
+  application_configuration {
+    environment_properties {
+      property_group {
+        property_group_id = "sql"
+        property_map = {
+          "viewName" : "master_complex"
+        }
+      }
+    }
+
+    application_code_configuration {
+      code_content {
+        s3_content_location {
+          bucket_arn = local.flink_jar_bucket_arn
+          file_key   = local.flink_views_jar_file
+        }
+      }
+
+      code_content_type = "ZIPFILE"
+    }
+
+    flink_application_configuration {
+      checkpoint_configuration {
+        configuration_type = "DEFAULT"
+      }
+
+      monitoring_configuration {
+        configuration_type = "CUSTOM"
+        log_level          = "INFO"
+        metrics_level      = "APPLICATION"
+      }
+
+      parallelism_configuration {
+        auto_scaling_enabled = true
+        configuration_type   = "CUSTOM"
+        parallelism          = 4
+        parallelism_per_kpu  = 4
+      }
+    }
+
+    vpc_configuration {
+      security_group_ids = [aws_security_group.flink_allow_outbound.id]
+      subnet_ids         = [data.aws_subnet.private_subnets_a.id]
+    }
+  }
+
+  cloudwatch_logging_options {
+    log_stream_arn = aws_cloudwatch_log_stream.flink_master_complex_log_stream.arn
   }
 }
