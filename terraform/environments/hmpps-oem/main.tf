bce6529c0b537b6c41cc7c071bd30fdf40f60f7e
Shajida Akthar <Shajida Akthar>
2024-04-28 18:41:08 +0100

    Lookup monitoring_account_id and source_account_ids for oasys and nomis

--- terraform/environments/hmpps-oem/main.tf
+++ terraform/environments/hmpps-oem/main.tf
@@ -170,8 +170,8 @@ module "cross_account_cloudwatch" {
     local.cloudwatch_monitoring_options,
     local.cloudwatch_local_environment_monitoring_options,
   )
-  monitoring_account_id              = lookup(local.cloudwatch_local_environment_monitoring_options, "monitoring_account_id", "")
-  source_account_ids                 = lookup(local.cloudwatch_local_environment_monitoring_options, "source_account_ids", [])
+  monitoring_account_id = lookup(local.cloudwatch_local_environment_monitoring_options, "monitoring_account_id", "")
+  source_account_ids    = lookup(local.cloudwatch_local_environment_monitoring_options, "source_account_ids", [])
 }
 
 module "cloudwatch" {
