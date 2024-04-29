4d9eb323e17ee9827a2d142072c536e0a4b91639
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-04-29 10:36:57 +0100

    Add file share for preprod nomis combined reporting (#5883)

--- terraform/environments/nomis-combined-reporting/outputs.tf
+++ terraform/environments/nomis-combined-reporting/outputs.tf
@@ -1,3 +1,7 @@
+output "efs_dns_names" {
+  description = "EFS DNS names"
+  value       = { for key, value in module.baseline.efs : key => value.file_system.dns_name }
+}
 output "route53_zone_ns_records" {
   description = "NS records of created zones"
   value       = { for key, value in module.baseline.route53_zones : key => value.name_servers }
