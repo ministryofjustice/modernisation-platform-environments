9f51d7acf0f87259a68ee6660aff1a07e912199f
matt-heery <matt.heery@digital.justice.gov.uk>
2024-04-30 14:23:08 +0100

    minor refactoring

--- terraform/environments/electronic-monitoring-data/lambdas_layers.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_layers.tf
@@ -7,6 +7,7 @@ locals {
   layer_name        = "create_athena_external_tables_layer"
   requirements_name = "create_athena_external_tables_requirements.txt"
   requirements_path = "${local.layer_path}/${local.requirements_name}"
+  layer_zip_path    = "${local.lambda_path}/${local.layer_zip_name}"
 }
 
 resource "null_resource" "lambda_layer" {
@@ -26,10 +27,8 @@ resource "null_resource" "lambda_layer" {
 }
 
 resource "aws_lambda_layer_version" "create_external_athena_tables_layer" {
-    filename = local.layer_zip_name
-    layer_name = local.layer_name
-
+    filename            = local.layer_zip_name
+    layer_name          = local.layer_name
     compatible_runtimes = ["python3.12"]
-
-    source_code_hash = filesha1(local.layer_zip_name)
+    source_code_hash    = filesha1(local.layer_zip_name)
 }
