3abbaf58666746905a3b5b3f70a6041a5ec1c05a
matt-heery <matt.heery@digital.justice.gov.uk>
2024-04-30 15:57:20 +0100

    add layer to function

--- terraform/environments/electronic-monitoring-data/lambdas_main.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_main.tf
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "create_athena_external_tables" {
     function_name = "create_athena_external_tables"
     role = aws_iam_role.create_athena_external_tables_lambda.arn
     handler = "lambda.handler"
-
+    layers = [aws_lambda_layer_version.create_athena_external_tables_layer.arn]
     source_code_hash = data.archive_file.create_athena_external_tables.output_base64sha256
     depends_on    = [aws_cloudwatch_log_group.create_athena_external_tables_lambda]
 
