1eb043dc68a61165557965c47e8216f54bd0169e
matt-heery <matt.heery@digital.justice.gov.uk>
2024-04-30 16:38:53 +0100

    logging and correct handler name

--- terraform/environments/electronic-monitoring-data/lambdas_main.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_main.tf
@@ -19,7 +19,7 @@ resource "aws_lambda_function" "create_athena_external_tables" {
     filename = "${local.lambda_path}/create_athena_external_tables.zip"
     function_name = "create_athena_external_tables"
     role = aws_iam_role.create_athena_external_tables_lambda.arn
-    handler = "lambda.handler"
+    handler = "create_athena_external_tables.handler"
     layers = [aws_lambda_layer_version.create_external_athena_tables_layer.arn]
     source_code_hash = data.archive_file.create_athena_external_tables.output_base64sha256
     depends_on    = [aws_cloudwatch_log_group.create_athena_external_tables_lambda]
