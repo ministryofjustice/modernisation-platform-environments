4d7c83e7421cc5cab8e77fc67eb2f8c174fd7fee
matt-heery <matt.heery@digital.justice.gov.uk>
2024-05-01 14:52:37 +0100

    updating perms

--- terraform/environments/electronic-monitoring-data/lambdas_iam.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_iam.tf
@@ -16,3 +16,23 @@ resource "aws_iam_role_policy_attachment" "lambda_sqs_queue_access_execution" {
     role = aws_iam_role.create_athena_external_tables_lambda.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
 }
+
+resource "aws_iam_role_policy_attachment" "get_glue_connections" {
+    role = aws_iam_role.create_athena_external_tables_lambda.name
+    policy_arn = aws_iam_policy.get_glue_connections.arn
+}
+
+resource "aws_iam_policy" "get_glue_connections" {
+    name = "get_glue_connections"
+    policy = data.aws_iam_policy_document.get_glue_connections.json
+}
+
+data "aws_iam_policy_document" "get_glue_connections" {
+    statement {
+        effect = "Allow"
+        actions = [
+            "glue:GetConnection"
+        ]
+        resources = ["*"]
+    }
+}
