f517f5256a2dca442de06cb63b334041143bbf92
matt-heery <matt.heery@digital.justice.gov.uk>
2024-05-01 16:30:19 +0100

    get s3 and glue tables

--- terraform/environments/electronic-monitoring-data/lambdas_iam.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_iam.tf
@@ -17,22 +17,46 @@ resource "aws_iam_role_policy_attachment" "lambda_sqs_queue_access_execution" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "get_glue_connections" {
+resource "aws_iam_role_policy_attachment" "get_glue_connections_and_tables" {
     role = aws_iam_role.create_athena_external_tables_lambda.name
-    policy_arn = aws_iam_policy.get_glue_connections.arn
+    policy_arn = aws_iam_policy.get_glue_connections_and_tables.arn
 }
 
-resource "aws_iam_policy" "get_glue_connections" {
-    name = "get_glue_connections"
+resource "aws_iam_policy" "get_glue_connections_and_tables" {
+    name = "get_glue_connections_and_tables"
     policy = data.aws_iam_policy_document.get_glue_connections.json
 }
 
-data "aws_iam_policy_document" "get_glue_connections" {
+resource "aws_iam_role_policy_attachment" "get_s3_output" {
+    role = aws_iam_role.create_athena_external_tables_lambda.name
+    policy_arn = aws_iam_policy.get_s3_output.arn
+}
+
+resource "aws_iam_policy" "get_s3_output" {
+    name = "get_s3_output"
+    policy = data.aws_iam_policy_document.get_s3_output.json
+}
+
+
+data "aws_iam_policy_document" "get_glue_connections_and_tables" {
     statement {
         effect = "Allow"
         actions = [
-            "glue:GetConnection"
+            "glue:GetConnection",
+            "glue:GetTables"
         ]
         resources = ["*"]
+    }
+}
+
+data "aws_iam_policy_document" "get_s3_output" {
+    statement {
+        effect = "Allow"
+        actions = [
+            "s3:ListObjects"
+        ]
+        resources = [
+            "${aws_s3_bucket.dms_target_ep_s3_bucket}/*"
+        ]
     }
 }
