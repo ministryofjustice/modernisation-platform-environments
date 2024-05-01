06996349347368417d046a88689ceede6d971781
matt-heery <matt.heery@digital.justice.gov.uk>
2024-05-01 14:50:37 +0100

    rename

--- terraform/environments/electronic-monitoring-data/lambdas_security_groups.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_security_groups.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "lambda_db_security_group" {
   vpc_id      = data.aws_vpc.shared.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "dms_all_tcp_outbound" {
+resource "aws_vpc_security_group_egress_rule" "lambda_all_outbound" {
   security_group_id = aws_security_group.lambda_db_security_group.id
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "tcp"
@@ -16,7 +16,7 @@ resource "aws_vpc_security_group_egress_rule" "dms_all_tcp_outbound" {
   description = "DMS Terraform"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "dms_to_rds_sg_rule" {
+resource "aws_vpc_security_group_ingress_rule" "lambda_to_rds_sg_rule" {
   security_group_id = aws_security_group.db.id
 
   referenced_security_group_id = aws_security_group.lambda_db_security_group.id
