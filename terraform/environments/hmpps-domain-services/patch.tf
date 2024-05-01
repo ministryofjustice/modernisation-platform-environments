d89831ce6fad865506644e025f4e2441144bbd49
pav <pav.gainda@version1.com>
2024-05-01 15:53:08 +0100

    Test mod platform patch manager

--- terraform/environments/hmpps-domain-services/patch.tf
+++ terraform/environments/hmpps-domain-services/patch.tf
@@ -36,6 +36,24 @@ module "test" {
   ]
 }
 
+module "ssm-auto-patching" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref="
+  count  = local.environment == "development" ? 1 : 0
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  account_number             = local.environment_management.account_ids[terraform.workspace]
+  application_name           = local.application_name
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-patching"
+    },
+  )
+}
+
+
 #module "preproduction" {
 #  count               = local.is-preproduction == true ? 1 : 0
 #  source              = "../../modules/patch_manager"
