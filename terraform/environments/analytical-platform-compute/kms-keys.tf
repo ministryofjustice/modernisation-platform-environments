701cf49c2d07fe26703bf4b1c9c54579c4fa53bb
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-01 15:59:59 +0000

    Update key policy statement
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/environments/analytical-platform-compute/kms-keys.tf
+++ terraform/environments/analytical-platform-compute/kms-keys.tf
@@ -8,6 +8,33 @@ module "vpc_flow_logs_kms" {
   description             = "VPC flow logs KMS key"
   enable_default_policy   = true
   deletion_window_in_days = 7
+  key_statements = [
+    {
+      sid = "AllowCloudWatchLogs"
+      actions = [
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ]
+      resources = ["*"]
+      effect    = "Allow"
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+        }
+      ]
+      conditions = [
+        {
+          test     = "ArnEquals"
+          variable = "kms:EncryptionContext:aws:logs:arn"
+          values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/vpc/flow-log"]
+        }
+      ]
+    }
+  ]
 
   tags = local.tags
 }
