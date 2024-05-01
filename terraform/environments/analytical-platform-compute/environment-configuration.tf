7c05907ae00bcd9d246d15eae9a343124cecc521
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-01 13:42:51 +0000

    Add test environment
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/environments/analytical-platform-compute/environment-configuration.tf
+++ terraform/environments/analytical-platform-compute/environment-configuration.tf
@@ -12,6 +12,17 @@ locals {
       /* Observability Platform */
       observability_platform = "development"
     }
+    test = {
+      /* VPC */
+      vpc_cidr                   = "10.0.0.0/16"
+      vpc_private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+      vpc_public_subnets         = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+      vpc_enable_nat_gateway     = true
+      vpc_one_nat_gateway_per_az = true
+
+      /* Observability Platform */
+      observability_platform = "development"
+    }
     production = {
       /* VPC */
       vpc_cidr                   = "10.0.0.0/16"
