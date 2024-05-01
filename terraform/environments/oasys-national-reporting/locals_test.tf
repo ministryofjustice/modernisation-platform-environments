6f53fa560e4fb968461d2a121c5f6702d5b988b7
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-05-01 13:24:15 +0100

    add missing merges with defaults for BODS

--- terraform/environments/oasys-national-reporting/locals_test.tf
+++ terraform/environments/oasys-national-reporting/locals_test.tf
@@ -32,24 +32,23 @@ locals {
     }
 
     baseline_ec2_instances = {
-      tt-onr-bods-1-a = {
-        config = {
-          ami_name                      = "hmpps_windows_server_2019-release_*"
+      tt-onr-bods-1-a = merge(locals.default_bods_ec2, {
+        config = merge(locals.default_bods_ec2.config, {
           availability_zone             = "${local.region}a"
           ebs_volumes_copy_all_from_ami = false
-        }
-        instance = {
-          instance_type          = "t3.large"
-          vpc_security_group_ids = ["bods"]
-        }
+        })
+        instance = merge(locals.default_bods_ec2.instance, {
+          instance_type = "t3.large"
+        })
         user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-        ebs_volumes = {
+        # volumes are a direct copy of BODS in NCR
+        ebs_volumes = merge(locals.default_bods_ec2.ebs_volumes, {
           "/dev/sda1" = { type = "gp3", size = 100 }
           "/dev/sdb"  = { type = "gp3", size = 100 }
           "/dev/sdc"  = { type = "gp3", size = 100 }
           "/dev/sds"  = { type = "gp3", size = 100 }
-        }
-      }
+        })
+      })
       #   test-db = merge(local.defaults_onr_db_ec2 ,{
       #     config = merge(local.defaults_onr_db_ec2.config, {
       #       ami_name          = "hmpps_ol_8_5_oracledb_19c_release_2024-*"
