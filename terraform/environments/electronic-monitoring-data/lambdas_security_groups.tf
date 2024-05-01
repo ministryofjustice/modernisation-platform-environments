f222ceb74cffcaf346f02f88d5dd765c59dae766
matt-heery <matt.heery@digital.justice.gov.uk>
2024-05-01 16:07:23 +0100

    adding this to the correct rule

--- terraform/environments/electronic-monitoring-data/lambdas_security_groups.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_security_groups.tf
@@ -11,8 +11,8 @@ resource "aws_vpc_security_group_egress_rule" "lambda_all_outbound" {
   security_group_id = aws_security_group.lambda_db_security_group.id
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "tcp"
-  from_port   = 1433
-  to_port     = 1433
+  from_port   = 0
+  to_port     = 65535
   description = "DMS Terraform"
 }
 
@@ -21,7 +21,7 @@ resource "aws_vpc_security_group_ingress_rule" "lambda_to_rds_sg_rule" {
 
   referenced_security_group_id = aws_security_group.lambda_db_security_group.id
   ip_protocol                  = "tcp"
-  from_port                    = 0
-  to_port                      = 65535
+  from_port                    = 1433
+  to_port                      = 1433
   description                  = "Lambda RDS Access"
 }
