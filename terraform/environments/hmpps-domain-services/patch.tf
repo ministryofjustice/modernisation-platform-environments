9e0a89e4cd41070ce2467ef627ec6bdd3ec1367d
pavmoj <142988272+pavmoj@users.noreply.github.com>
2024-05-02 16:18:23 +0100

    Integrate mod platform patch manager module (#5925)
    * Test mod platform patch manager

--- terraform/environments/hmpps-domain-services/patch.tf
+++ terraform/environments/hmpps-domain-services/patch.tf
@@ -21,20 +21,25 @@ module "development" {
 }
 
 module "test" {
-  count               = local.is-test == true ? 1 : 0
-  source              = "../../modules/patch_manager"
-  application         = "hmpps-domain-services"
-  environment         = "test"
-  predefined_baseline = "AWS-WindowsPredefinedPatchBaseline-OS-Applications"
-  operating_system    = "WINDOWS"
-  schedule            = "cron(0 21 ? * WED#2 *)" # 2nd Weds @ 9pm
-  target_tag = {
-    "environment-name" = "hmpps-domain-services-test"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v1.0.0"
+  count  = local.is-test == true ? 1 : 0
+  providers = {
+    aws.bucket-replication = aws
   }
-  instance_roles = [
-    "arn:aws:iam::${module.environment.account_ids.hmpps-domain-services-test}:role/ec2-instance-role-test-win-2022",
-  ]
+
+  account_number   = module.environment.account_ids.hmpps-domain-services-test
+  application_name = local.application_name
+  approval_days    = "0"
+  patch_schedule   = "cron(0 21 ? * TUE#2 *)" # 2nd Tues @ 9pm
+  operating_system = "REDHAT_ENTERPRISE_LINUX"
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-patching"
+    },
+  )
 }
+
 
 #module "preproduction" {
 #  count               = local.is-preproduction == true ? 1 : 0
