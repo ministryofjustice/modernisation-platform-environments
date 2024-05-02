54e501c8947b06dd65fb084fb15e9ca7f31f08c1
matt-heery <matt.heery@digital.justice.gov.uk>
2024-05-02 11:36:35 +0100

    s3 bucket perms

--- terraform/environments/electronic-monitoring-data/glue_iam.tf
+++ terraform/environments/electronic-monitoring-data/glue_iam.tf
@@ -56,6 +56,25 @@ data "aws_iam_policy_document" "get_s3_output" {
             "s3:ListBucket"
         ]
         resources = [
+            aws_s3_bucket.glue-jobs.arn
+        ]
+    }
+        statement {
+        effect = "Allow"
+        actions = [
+            "s3:ListObjects",
+            "s3:GetObject"
+        ]
+        resources = [
+            "${aws_s3_bucket.glue-jobs.arn}/*"
+        ]
+    }
+    statement {
+        effect = "Allow"
+        actions = [
+            "s3:ListBucket"
+        ]
+        resources = [
             aws_s3_bucket.dms_target_ep_s3_bucket.arn
         ]
     }
