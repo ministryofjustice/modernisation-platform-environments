663ea4fc26a0618dc7db28aef1ce2508723437c6
pav <pav.gainda@version1.com>
2024-05-02 11:55:30 +0100

    Remove provider

--- terraform/environments/hmpps-domain-services/patch.tf
+++ terraform/environments/hmpps-domain-services/patch.tf
@@ -39,9 +39,9 @@
 module "development" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v1.0.0"
   count  = local.environment == "development" ? 1 : 0
-#   providers = {
-#     aws.bucket-replication = aws
-#   }
+  providers = {
+    aws.bucket-replication = aws
+  }
 
   account_number   = local.environment_management.account_ids[terraform.workspace]
   application_name = local.application_name
