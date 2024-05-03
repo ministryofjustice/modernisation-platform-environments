3d6a81bce9bbe6bbd9d99b5910b850819e03d71f
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2024-05-03 15:31:23 +0100

    LAWS-3878: Adding databse server

--- terraform/environments/contract-work-administration/locals.tf
+++ terraform/environments/contract-work-administration/locals.tf
@@ -1,4 +1,6 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
     application_name_short = "cwa"
+    nonprod_workspaces_local_cidr1 = "10.200.2.0/24"
+    nonprod_workspaces_local_cidr2 = "10.200.3.0/24"
 }
