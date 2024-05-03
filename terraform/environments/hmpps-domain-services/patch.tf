49644c69055daa4e9503141894dcdb932a256078
pav <pav.gainda@version1.com>
2024-05-03 16:26:14 +0100

    fix provider warning

--- terraform/environments/hmpps-domain-services/patch.tf
+++ terraform/environments/hmpps-domain-services/patch.tf
@@ -2,7 +2,7 @@ module "test" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
   count  = local.is-test == true ? 1 : 0
   providers = {
-    aws.bucket-replication = aws
+    source = "hashicorp/aws"
   }
 
   account_number   = local.environment_management.account_ids[terraform.workspace]
