390a6c24962344e9af2c6621df085e6fa01ab7e6
George Taylor <george.taylor@digital.justice.gov.uk>
2024-05-03 13:10:14 +0100

    add jitbit dev provided html template

--- terraform/environments/delius-jitbit/lambda/bounce_email_notification/bounce_email_notification.py
+++ terraform/environments/delius-jitbit/lambda/bounce_email_notification/bounce_email_notification.py
@@ -35,10 +35,22 @@ def handler(event, context):
 
     bounced_recipients_message = ""
     for bounced_recipient in bounced_recipients:
-        bounced_recipients_message += f"""Action: {bounced_recipient.get("action")} for {bounced_recipient.get("emailAddress")}\n
-                                        Status: {bounced_recipient.get("status")}\n
-                                        Diagnostic Code: {bounced_recipient.get("diagnosticCode")}\n
-                                        ---\n"""
+        bounced_recipients_message += f"""  <p>
+                                                Action: <strong>{bounced_recipient.get("action")}</strong>
+                                                <br>
+                                            </p>
+                                            <p>
+                                                Recipient: <strong>{bounced_recipient.get("emailAddress")}/strong>
+                                                <br>
+                                            </p>
+                                            <p>
+                                                Status: <strong>{bounced_recipient.get("status")}</strong>
+                                                <br>
+                                            </p>
+                                            <p>
+                                                Diagnostic Code: <strong>{bounced_recipient.get("diagnosticCode")}</strong>
+                                            </p>
+                                          """
 
     try:
         email = ses.send_email(
@@ -49,14 +61,65 @@ def handler(event, context):
                 "Simple": {
                     "Subject": {"Data": f"BOUNCE <{jitbit_ticket_id}>: {subject}"},
                     "Body": {
-                        "Text": {
+                        "Html": {
                             "Data": f"""
-                                    Ticket ID: {jitbit_ticket_id}\n
-                                    Feedback ID: {bounce.get('feedbackId')}\n
-                                    Timestamp: {bounce.get('timestamp')}\n
-                                    Bounced Recipients:\n
-                                        {bounced_recipients_message}\n
-                                    """
+                                    <table width="100%" style="background-color: rgb(250, 232, 205);">
+                                    <tbody>
+                                      <tr>
+                                        <td style="background-color: rgb(255, 50, 50);"></td>
+                                        <td>&nbsp;&nbsp;</td>
+                                        <td width="100%">
+                                          <br>
+                                          <strong>WARNING:</strong>
+                                          <br>
+                                          <p> An email notification from this ticket has failed to deliver to one or more recipients. This may be the result of a 'Reply' or a 'Forward Ticket By Email'. See the diagnostic information below for more details. We have provided a simple guide <a href="https://helpdesk.jitbit.cr.probation.service.justice.gov.uk/KB/View/976525-">HERE</a> which explains the actions that you need to take.
+                                            <br>
+                                          </p>
+                                        </td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                  <br>
+                                  <hr>
+                                  <br>
+                                  <strong>DIAGNOSTIC INFORMATION:</strong>
+                                  <br>
+                                  <br>
+                                  <table width="100%" style="background-color: rgb(240, 240, 240);">
+                                    <tbody>
+                                        <tr>
+                                            <td style="background-color: rgb(150, 150, 150);"></td>
+                                            <td>&nbsp;&nbsp;</td>
+                                            <td width="100%">
+                                              <br>
+                                              <p>Ticket ID: <strong>{jitbit_ticket_id}</strong>
+                                              </p>
+                                              <p>Feedback ID: <strong>010b018f3d5323b8-1afeb777-87b9-4433-8104-f2f265151de6-000000</strong>
+                                                <br>
+                                              </p>
+                                              <p>Timestamp: <strong>2024-05-03T07:20:10.035Z</strong>
+                                                <br>
+                                              </p>
+                                        </tr>
+                                        <tr>
+                                        <td style="background-color: rgb(150, 150, 150);"></td>
+                                            <td>&nbsp;&nbsp;</td>
+                                            <td>
+                                                <p><strong>Bounced Recipients:</strong></p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="background-color: rgb(150, 150, 150);"></td>
+                                            <td>&nbsp;&nbsp;</td>
+                                            <td width="100%">
+                                              {bounced_recipients_message}
+                                            </td>
+                                          </tr>
+                                    </tbody>
+                                  </table>
+                                  <br>
+                                  <br>
+                                """
                         }
                     },
                 }
