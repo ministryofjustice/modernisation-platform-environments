642b03f28040bbe954e7c12f9425c2b703170afd
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-05-03 17:09:25 +0100

    try to fix error sftp_host_port

--- terraform/environments/tribunals/modules/tribunal_ftp/outputs.tf
+++ terraform/environments/tribunals/modules/tribunal_ftp/outputs.tf
@@ -27,3 +27,8 @@ output "tribunals_lb_ftp" {
   description = "SFTP LB"
   value       = module.ecs_loadbalancer.tribunals_lb_ftp
 }
+
+output "sftp_host_port" {
+  description = "Port for SFTP"
+  value       = var.sftp_host_port
+}
