ee71c975349930df094e8f3cc9f73821bf4491a0
Jonathan Quinn <1213631+jnq@users.noreply.github.com>
2024-05-07 15:51:07 +0100

    try out an alarm, alarm if we have any failures in the last 6 hours

--- terraform/environments/hmpps-oem/cloudwatch.tf
+++ terraform/environments/hmpps-oem/cloudwatch.tf
@@ -3,15 +3,16 @@ locals {
 }
 
 resource "aws_cloudwatch_metric_alarm" "github_failed_actions" {
-  alarm_name = "github-actions-failures"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = 2
-  metric_name = "FailedWorkflowRuns"
-  namespace = "CustomMetrics"
-  period = 21600
-  statistic = "Average"
-  threshold = 0
-  alarm_description = "Alarm if the number of failed GitHub Actions runs is greater than 0"
+  alarm_name          = "github-actions-failures"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 72
+  threshold           = 1
+  datapoints_to_alarm = 1
+  statistic           = "Maximum"
+  period              = 300
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Alarm if the number of failed GitHub Actions runs is greater than 0"
+
   alarm_actions = [local.sns_topic_arn]
-  ok_actions = [local.sns_topic_arn]
+  ok_actions    = [local.sns_topic_arn]
 }
