d35197716639d4227b67700a4f98a9c1ab43253d
Keir Williams <keir.williams@digital.justice.gov.uk>
2024-05-07 10:31:03 +0100

    initial dev commit

--- terraform/environments/oasys-national-reporting/locals_development.tf
+++ terraform/environments/oasys-national-reporting/locals_development.tf
@@ -4,6 +4,24 @@ locals {
   development_config = {
 
     baseline_ec2_instances = {
+      tt-onr-bods-1-a = merge(local.defaults_bods_ec2, {
+        config = merge(local.defaults_bods_ec2.config, {
+          availability_zone             = "${local.region}a"
+          ebs_volumes_copy_all_from_ami = false
+          user_data_raw                 = module.baseline_presets.ec2_instance.user_data_raw["user-data-pwsh"]
+        })
+        instance = merge(local.defaults_bods_ec2.instance, {
+          instance_type = "t3.large"
+        })
+        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+        # volumes are a direct copy of BODS in NCR
+        ebs_volumes = merge(local.defaults_bods_ec2.ebs_volumes, {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+          "/dev/sdb"  = { type = "gp3", size = 100 }
+          "/dev/sdc"  = { type = "gp3", size = 100 }
+          "/dev/sds"  = { type = "gp3", size = 100 }
+        })
+      })
       #   dev-web-a = merge(local.defaults_web_ec2, 
       #   {
       #     config = merge(local.defaults_web_ec2.config, {
