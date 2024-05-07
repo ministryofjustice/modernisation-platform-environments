2d9bfc701d59bdc985d3d25c1df4c62364fb3fb1
pav <pav.gainda@version1.com>
2024-05-07 19:05:40 +0100

    Add remaining environments

--- terraform/environments/hmpps-domain-services/patch.tf
+++ terraform/environments/hmpps-domain-services/patch.tf
@@ -18,29 +18,84 @@ module "test" {
   )
 }
 
+module "development" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
+  count  = local.is-development == true ? 1 : 0
+  providers = {
+    aws.bucket-replication = aws
+  }
 
-#module "preproduction" {
-#  count               = local.is-preproduction == true ? 1 : 0
-#  source              = "../../modules/patch_manager"
-#  application         = "hmpps-domain-services"
-#  environment         = "preproduction"
-#  predefined_baseline = "AWS-WindowsPredefinedPatchBaseline-OS-Applications"
-#  operating_system    = "WINDOWS"
-#  schedule            = "cron(0 21 ? * WED#3 *)" # 3rd Weds @ 9pm
-#  target_tag = {
-#    "environment-name" = "hmpps-domain-services-preproduction"
-#  }
-#}
-#
-#module "production" {
-#  count               = local.is-production == true ? 1 : 0
-#  source              = "../../modules/patch_manager"
-#  application         = "hmpps-domain-services"
-#  environment         = "production"
-#  predefined_baseline = "AWS-WindowsPredefinedPatchBaseline-OS-Applications"
-#  operating_system    = "WINDOWS"
-#  schedule            = "cron(0 21 ? * THU#3 *)" # 3rd Thurs @ 9pm
-#  target_tag = {
-#    "environment-name" = "hmpps-domain-services-production"
-#  }
-#}
+  account_number   = local.environment_management.account_ids[terraform.workspace]
+  application_name = local.application_name
+  approval_days    = "0"
+  patch_schedule   = "cron(0 21 ? * TUE#2 *)" # 2nd Tues @ 9pm
+  operating_system = "REDHAT_ENTERPRISE_LINUX"
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-patching"
+    },
+  )
+}
+
+module "preproduction" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
+  count  = local.is-preproduction == true ? 1 : 0
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  account_number   = local.environment_management.account_ids[terraform.workspace]
+  application_name = local.application_name
+  approval_days    = "7"
+  patch_schedule   = "cron(0 21 ? * WED#2 *)" # 2nd Weds @ 9pm
+  operating_system = "WINDOWS"
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-patching"
+    },
+  )
+}
+
+module "production-eu-west-2a" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
+  count  = local.is-production == true ? 1 : 0
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  account_number   = local.environment_management.account_ids[terraform.workspace]
+  application_name = local.application_name
+  approval_days    = "14"
+  patch_schedule   = "cron(0 21 ? * THU#3 *)" # 3rd Thurs @ 9pm
+  operating_system = "WINDOWS"
+  patch_tag        = "eu-west-2a"
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-patching"
+    },
+  )
+}
+
+module "production-eu-west-2b" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
+  count  = local.is-production == true ? 1 : 0
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  account_number   = local.environment_management.account_ids[terraform.workspace]
+  application_name = local.application_name
+  approval_days    = "14"
+  patch_schedule   = "cron(0 21 ? * THU#4 *)" # 4th Thurs @ 9pm
+  operating_system = "WINDOWS"
+  patch_tag        = "eu-west-2b"
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-patching"
+    },
+  )
+}
