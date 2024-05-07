dab8d71c7e7e52123d5fd85d176a8d0c1ce4803d
George Taylor <george.taylor@digital.justice.gov.uk>
2024-05-07 16:01:45 +0100

    fix provider warnings

--- terraform/environments/delius-nextcloud/modules/components/nextcloud/ecs_service.tf
+++ terraform/environments/delius-nextcloud/modules/components/nextcloud/ecs_service.tf
@@ -113,7 +113,6 @@ module "nextcloud_service" {
   tags          = var.tags
 
   providers = {
-    aws          = aws
     aws.core-vpc = aws.core-vpc
     aws.core-network-services = aws.core-network-services
   }
