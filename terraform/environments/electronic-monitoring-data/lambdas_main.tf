08740518a89856934ef3c194c881cfd2abf6c38a
matt-heery <matt.heery@digital.justice.gov.uk>
2024-05-07 16:32:57 +0100

    mojap metadata

--- terraform/environments/electronic-monitoring-data/lambdas_main.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_main.tf
@@ -20,11 +20,15 @@ resource "aws_lambda_function" "create_athena_external_tables" {
     function_name = "create_athena_external_tables"
     role = aws_iam_role.create_athena_external_tables_lambda.arn
     handler = "create_athena_external_tables.handler"
-    layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:8"]
+    layers = [
+      "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:8",
+      aws_lambda_layer_version.mojap_metadata_layer.arn,
+      aws_lambda_layer_version.create_external_athena_tables_layer.arn
+      ]
     source_code_hash = data.archive_file.create_athena_external_tables.output_base64sha256
     depends_on    = [aws_cloudwatch_log_group.create_athena_external_tables_lambda]
     timeout = 200
-    runtime = "python3.12"
+    runtime = "python3.11"
     vpc_config {
       security_group_ids = [aws_security_group.lambda_db_security_group.id]
       subnet_ids = data.aws_subnets.shared-public.ids
@@ -32,8 +36,7 @@ resource "aws_lambda_function" "create_athena_external_tables" {
 
     environment {
       variables = {
-        RDS_ARN = aws_db_instance.database_2022.arn
-        SECRET_ARN = aws_secretsmanager_secret.db_password.arn
+        SECRET_NAME = aws_secretsmanager_secret.db_password.arn
       }
     }
 }
