54332180d3dd40d5f0f71cc21968c06fa078ccc0
Jonathan Quinn <1213631+jnq@users.noreply.github.com>
2024-05-08 10:18:17 +0100

    Try a search to span all our dimensions

--- terraform/environments/hmpps-oem/cloudwatch.tf
+++ terraform/environments/hmpps-oem/cloudwatch.tf
@@ -5,17 +5,14 @@ locals {
 resource "aws_cloudwatch_metric_alarm" "github_failed_actions" {
   alarm_name          = "github-actions-failures"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  metric_name         = "FailedWorkflowRuns"
-  namespace           = "CustomMetrics"
   evaluation_periods  = 1
   threshold           = 1
   datapoints_to_alarm = 1
-  statistic           = "Maximum"
-  period              = 600
-  treat_missing_data  = "notBreaching"
+  treat_missing_data  = "breaching"
   alarm_description   = "Alarm if the number of failed GitHub Actions runs is greater than 0"
-  dimensions = {
-    Repository = "dso-infra-azure-fixngo"
+  metric_query {
+    id = "m1"
+    expression = "SEARCH('{CustomMetrics,Repository} MetricName=\"FailedWorkflowRuns\"', 'Maximum', 600)"
   }
 
   alarm_actions = [local.sns_topic_arn]
