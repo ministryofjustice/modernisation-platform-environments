e1d0119ebfb4494713a4d30053ecde80e830168f
Jonathan Quinn <1213631+jnq@users.noreply.github.com>
2024-05-08 10:50:19 +0100

    search is unsupported in alarms

--- terraform/environments/hmpps-oem/cloudwatch.tf
+++ terraform/environments/hmpps-oem/cloudwatch.tf
@@ -5,15 +5,17 @@ locals {
 resource "aws_cloudwatch_metric_alarm" "github_failed_actions" {
   alarm_name          = "github-actions-failures"
   comparison_operator = "GreaterThanOrEqualToThreshold"
+  metric_name         = "FailedWorkflowRuns"
+  namespace           = "CustomMetrics"
   evaluation_periods  = 1
   threshold           = 1
   datapoints_to_alarm = 1
+  statistic           = "Maximum"
+  period              = 600
   treat_missing_data  = "breaching"
   alarm_description   = "Alarm if the number of failed GitHub Actions runs is greater than 0"
-  metric_query {
-    id         = "m1"
-    period = 600
-    expression = "SEARCH('{CustomMetrics,Repository} MetricName=\"FailedWorkflowRuns\"', 'Maximum', 600)"
+  dimensions = {
+    Repository = "dso-infra-azure-fixngo"
   }
 
   alarm_actions = [local.sns_topic_arn]
