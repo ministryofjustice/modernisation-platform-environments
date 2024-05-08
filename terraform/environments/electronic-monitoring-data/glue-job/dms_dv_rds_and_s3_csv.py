7f532e723f963400aa3d74c05891daf2db3f7818
Madhu Kadiri <madhu.kadiri2@justice.gov.uk>
2024-05-08 15:36:45 +0100

    glue-job-tf - v7

--- terraform/environments/electronic-monitoring-data/glue-job/dms_dv_rds_and_s3_csv.py
+++ terraform/environments/electronic-monitoring-data/glue-job/dms_dv_rds_and_s3_csv.py
@@ -177,7 +177,7 @@ def get_s3_csv_dataframe(in_csv_tbl_s3_folder_path, in_rds_df_schema):
 
 if __name__ == "__main__":
 
-    TARGET_TABLE_PATH = f'''s3://{PARQUET_TARGET_S3_BUCKET_NAME}/{PARQUET_TARGET_DB_NAME}/{PARQUET_TARGET_TBL_NAME}/'''
+    TARGET_TABLE_PATH = f'''s3://{PARQUET_TARGET_S3_BUCKET_NAME}/{PARQUET_TARGET_DB_NAME}/{PARQUET_TARGET_TBL_NAME}'''
 
     rds_sqlserver_db_tbl_list = get_rds_db_tbl_list(get_rds_database_list(args["rds_db_list"]))
 
@@ -240,7 +240,7 @@ if __name__ == "__main__":
             # print(f"No S3-csv folder path found for given {rds_db_name} - {rds_tbl_name}")
         
         if check_s3_path_if_exists(PARQUET_TARGET_S3_BUCKET_NAME, f'''{PARQUET_TARGET_DB_NAME}/{PARQUET_TARGET_TBL_NAME}/full_table_name={db_dbo_tbl}'''):
-            glueContext.purge_s3_path(TARGET_TABLE_PATH, options={"retentionPeriod": 0})
+            glueContext.purge_s3_path(f"""{TARGET_TABLE_PATH}/full_table_name={db_dbo_tbl}""", options={"retentionPeriod": 0})
 
     df_dv_output = df_dv_output.dropDuplicates()
     df_dv_output = df_dv_output.where("run_datetime is not null")
@@ -248,7 +248,7 @@ if __name__ == "__main__":
     dydf = DynamicFrame.fromDF(df_dv_output, glueContext, "final_spark_df")
     glueContext.write_dynamic_frame.from_options(frame=dydf, connection_type='s3', format='parquet',
                                                  connection_options={
-                                                     'path': TARGET_TABLE_PATH,
+                                                     'path': f"""{TARGET_TABLE_PATH}/""",
                                                      "partitionKeys": ["full_table_name"]},
                                                  format_options={
                                                      'useGlueParquetWriter': True,
