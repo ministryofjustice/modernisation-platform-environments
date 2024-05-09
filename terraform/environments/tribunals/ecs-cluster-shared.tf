cc03a6c9c0018010852c5b4d6e9d2e882cc26e5e
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-05-09 13:43:40 +0100

    add ingress rules for port 22 back in (for ecs SG)

--- terraform/environments/tribunals/ecs-cluster-shared.tf
+++ terraform/environments/tribunals/ecs-cluster-shared.tf
@@ -141,15 +141,15 @@ resource "aws_security_group" "ecs_service_sftp" {
   name_prefix = "ecs-service-sg-sftp-"
   vpc_id      = data.aws_vpc.shared.id
 
-  # ingress {
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   description = "Allow traffic on port 22 from network load balancer"
-  #   security_groups = [
-  #     module.charity_tribunal_decisions.tribunals_lb_sc_id_sftp, module.claims_management_decisions.tribunals_lb_sc_id_sftp
-  #   ]
-  # }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    description = "Allow traffic on port 22 from network load balancer"
+    security_groups = [
+      module.charity_tribunal_decisions.tribunals_lb_sc_id_sftp, module.claims_management_decisions.tribunals_lb_sc_id_sftp
+    ]
+  }
 
   ingress {
     from_port   = module.charity_tribunal_decisions.sftp_host_port
