fe3645718fbb5e0cb73f45e9450853323a13b2a8
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2024-05-10 11:31:10 +0100

    Correct wrong reference

--- terraform/environments/contract-work-administration/concurrent_manager.tf
+++ terraform/environments/contract-work-administration/concurrent_manager.tf
@@ -105,7 +105,7 @@ resource "aws_vpc_security_group_ingress_rule" "cm_self" {
 resource "aws_vpc_security_group_ingress_rule" "cm_app" {
   security_group_id = aws_security_group.concurrent_manager.id
   description       = "Access from the Application Server"
-  referenced_security_group_id         = aws_security_group.application.id
+  referenced_security_group_id         = aws_security_group.app.id
   from_port         = 1676
   ip_protocol       = "tcp"
   to_port           = 1676
