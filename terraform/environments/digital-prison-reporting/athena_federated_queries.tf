a915ea0f79f5a04ef4737ce0998c8979a409c6be
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-05-13 16:48:11 +0100

    DPR2-720 Fix typo

--- terraform/environments/digital-prison-reporting/athena_federated_queries.tf
+++ terraform/environments/digital-prison-reporting/athena_federated_queries.tf
@@ -8,16 +8,16 @@ module "athena_federated_query_connector_oracle" {
 
   # TODO Parameterise NOMIS IP
   #  Dev or Test for now
-  nomis_ip_address                = local.nomis_ip_address
-  spill_bucket_name               = module.s3_working_bucket.bucket_id
-  connector_jar_bucket_name       = module.s3_artifacts_store.bucket_id
-  oracle_connector_jar_bucket_key = "third-party/athena-connectors/athena-oracle-2024.18.2.jar"
-  nomis_credentials_secret_arn    = aws_secretsmanager_secret.nomis_athena_federated.arn
-  account_id                      = local.account_id
-  region                          = local.account_region
-  vpc_id                          = data.aws_vpc.shared.id
-  subnet_id                       = data.aws_subnet.private_subnets_a.id
-  connection_string_nomis         = local.connection_string_nomis
+  nomis_ip_address             = local.nomis_ip_address
+  spill_bucket_name            = module.s3_working_bucket.bucket_id
+  connector_jar_bucket_name    = module.s3_artifacts_store.bucket_id
+  connector_jar_bucket_key     = "third-party/athena-connectors/athena-oracle-2024.18.2.jar"
+  nomis_credentials_secret_arn = aws_secretsmanager_secret.nomis_athena_federated.arn
+  account_id                   = local.account_id
+  region                       = local.account_region
+  vpc_id                       = data.aws_vpc.shared.id
+  subnet_id                    = data.aws_subnet.private_subnets_a.id
+  connection_string_nomis      = local.connection_string_nomis
 }
 
 # Adds an Athena data source / catalog for NOMIS
