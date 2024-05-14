16a808c2ce94db9218dc3fa4fba58e9cf4cd213d
matt-heery <matt.heery@digital.justice.gov.uk>
2024-05-14 11:08:37 +0100

    replacing other name

--- terraform/environments/electronic-monitoring-data/modules/dms/db_migration_task.tf
+++ terraform/environments/electronic-monitoring-data/modules/dms/db_migration_task.tf
@@ -3,7 +3,7 @@ resource "aws_dms_replication_task" "dms_db_migration_task" {
   # cdc_start_time            = "1993-05-21T05:50:00Z"
   migration_type            = "full-load"
   replication_instance_arn  = var.dms_replication_instance_arn
-  replication_task_id       = "${replace(var.database_name, "_", "-")}-db-migration-task-tf"
+  replication_task_id       = "${replace(var.database_name, "_", "-")}-db-migration-task-${var.file_target_type}"
   replication_task_settings = var.rep_task_settings_filepath
   source_endpoint_arn       = aws_dms_endpoint.dms_rds_source.endpoint_arn
   table_mappings            = var.rep_task_table_mapping_filepath
