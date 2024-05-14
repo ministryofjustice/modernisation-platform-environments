ff41d1e6dc9d716080f49deea095fb3a73f30318
Buckingham <XC01857@lumen.com>
2024-05-14 14:55:42 +0100

    Update_IAM_Permissions_140524_2

--- terraform/environments/ppud/iam.tf
+++ terraform/environments/ppud/iam.tf
@@ -290,7 +290,8 @@ resource "aws_iam_policy" "iam_policy_for_lambda_terminate_cpu_process" {
         "ec2:DescribeInstances"
       ],
       "Resource": [
-      "arn:aws:cloudwatch:eu-west-2:075585660276:alarm:CPU-High-i-0b5c31ecda24ebc04"
+      "arn:aws:cloudwatch:eu-west-2:075585660276:alarm:CPU-High-i-0b5c31ecda24ebc04",
+      "arn:aws:ec2:eu-west-2:075585660276:instance/i-0b5c31ecda24ebc04"
       ]
    }
  ]
