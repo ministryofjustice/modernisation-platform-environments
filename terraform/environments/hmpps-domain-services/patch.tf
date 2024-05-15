1281dd74a36044f97273c98fd36e166ca0dedb08
pav <pav.gainda@version1.com>
2024-05-15 14:07:32 +0100

    Deploy production

--- terraform/environments/hmpps-domain-services/patch.tf
+++ terraform/environments/hmpps-domain-services/patch.tf
@@ -1,5 +1,5 @@
 module "test-2a" {
-  source = local.patch_module
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v3.0.0"
   count  = local.is-test == true ? 1 : 0
   providers = {
     aws.bucket-replication = aws
@@ -47,7 +47,7 @@ module "test-2c" {
 }
 
 module "development" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v3.0.0"
   count  = local.is-development == true ? 1 : 0
   providers = {
     aws.bucket-replication = aws
@@ -68,7 +68,7 @@ module "development" {
 }
 
 module "preproduction" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v3.0.0"
   count  = local.is-preproduction == true ? 1 : 0
   providers = {
     aws.bucket-replication = aws
@@ -88,46 +88,46 @@ module "preproduction" {
   )
 }
 
-# module "production-eu-west-2a" {
-#   source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
-#   count  = local.is-production == true ? 1 : 0
-#   providers = {
-#     aws.bucket-replication = aws
-#   }
-#
-#   account_number       = local.environment_management.account_ids[terraform.workspace]
-#   application_name     = local.application_name
-#   approval_days        = "14"
-#   patch_schedule       = "cron(0 21 ? * THU#3 *)" # 3rd Thurs @ 9pm
-#   operating_system     = "WINDOWS"
-#   patch_tag            = "eu-west-2a"
-#   patch_classification = ["SecurityUpdates", "CriticalUpdates"]
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "ssm-patching"
-#     },
-#   )
-# }
+module "production-eu-west-2a" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v3.0.0"
+  count  = local.is-production == true ? 1 : 0
+  providers = {
+    aws.bucket-replication = aws
+  }
 
-# module "production-eu-west-2b" {
-#   source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v2.0.0"
-#   count  = local.is-production == true ? 1 : 0
-#   providers = {
-#     aws.bucket-replication = aws
-#   }
-#
-#   account_number       = local.environment_management.account_ids[terraform.workspace]
-#   application_name     = local.application_name
-#   approval_days        = "14"
-#   patch_schedule       = "cron(0 21 ? * THU#4 *)" # 4th Thurs @ 9pm
-#   operating_system     = "WINDOWS"
-#   patch_tag            = "eu-west-2b"
-#   patch_classification = ["SecurityUpdates", "CriticalUpdates"]
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "ssm-patching"
-#     },
-#   )
-# }
+  account_number       = local.environment_management.account_ids[terraform.workspace]
+  application_name     = local.application_name
+  approval_days        = "14"
+  patch_schedule       = "cron(0 21 ? * THU#3 *)" # 3rd Thurs @ 9pm
+  operating_system     = "WINDOWS"
+  patch_tag            = "eu-west-2a"
+  patch_classification = ["SecurityUpdates", "CriticalUpdates"]
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-patching"
+    },
+  )
+}
+
+module "production-eu-west-2b" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v3.0.0"
+  count  = local.is-production == true ? 1 : 0
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  account_number       = local.environment_management.account_ids[terraform.workspace]
+  application_name     = local.application_name
+  approval_days        = "14"
+  patch_schedule       = "cron(0 21 ? * THU#4 *)" # 4th Thurs @ 9pm
+  operating_system     = "WINDOWS"
+  patch_tag            = "eu-west-2b"
+  patch_classification = ["SecurityUpdates", "CriticalUpdates"]
+  tags = merge(
+    local.tags,
+    {
+      Name = "ssm-patching"
+    },
+  )
+}
