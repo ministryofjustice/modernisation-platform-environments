f0aa86c086400a655ca9db5cea65a24b705b505b
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-05-15 12:19:20 +0100

    re-apply jumpserver locals

--- terraform/environments/oasys-national-reporting/locals_jumpserver.tf
+++ terraform/environments/oasys-national-reporting/locals_jumpserver.tf
@@ -4,11 +4,11 @@ locals {
     autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
     # ami has unwanted ephemeral device, don't copy all the ebs_volumes
     config = merge(module.baseline_presets.ec2_instance.config.default, {
-      ami_name                      = "base_windows_server_2012_r2_release_2024-05-15*"
+      ami_name                      = "base_windows_server_2012_r2_release_2024-*"
       ami_owner                     = "374269020027"
       availability_zone             = null
       ebs_volumes_copy_all_from_ami = false
-      # user_data_raw                 = module.baseline_presets.ec2_instance.user_data_raw["user-data-pwsh"]
+      user_data_raw                 = module.baseline_presets.ec2_instance.user_data_raw["user-data-pwsh"]
     })
     ebs_volumes = {
       "/dev/sda1" = { type = "gp3", size = 200 }
@@ -23,7 +23,7 @@ locals {
       os-type                = "Windows"
       component              = "test"
       server-type            = "OnrClient"
-      backup                 = "false" # no need to back this up as they are destroyed each night
+      backup                 = "false" # no need to backup as these shouldn't contain persistent data
     }
   }
 }
