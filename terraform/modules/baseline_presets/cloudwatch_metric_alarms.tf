ccefd9076684dced6b63f45ca8ef3622db915930
Shajida Akthar <Shajida Akthar>
2024-05-15 14:45:50 +0100

    update memory usage alarm to 15min

--- terraform/modules/baseline_presets/cloudwatch_metric_alarms.tf
+++ terraform/modules/baseline_presets/cloudwatch_metric_alarms.tf
@@ -117,8 +117,8 @@ locals {
       }
       high-memory-usage = {
         comparison_operator = "GreaterThanOrEqualToThreshold"
-        evaluation_periods  = "60"
-        datapoints_to_alarm = "60"
+        evaluation_periods  = "15"
+        datapoints_to_alarm = "15"
         metric_name         = "mem_used_percent"
         namespace           = "CWAgent"
         period              = "60"
