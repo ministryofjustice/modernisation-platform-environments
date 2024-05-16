3f65a9ba60bdd95acb3f222710616e04f6d35148
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-05-16 13:46:58 +0100

    comment out ec2 instance, run an asg first to test all the ansible

--- terraform/environments/oasys-national-reporting/locals_test.tf
+++ terraform/environments/oasys-national-reporting/locals_test.tf
@@ -50,21 +50,21 @@ locals {
           "/dev/sds"  = { type = "gp3", size = 100 }
         })
       })
-      t2-onr-boe-1-a = merge(local.defaults_boe_ec2, {
-        config = merge(local.defaults_boe_ec2.config, {
-          instance_profile_policies = setunion(local.defaults_boe_ec2.config.instance_profile_policies, [
-            "Ec2SecretPolicy",
-          ])
-          availability_zone = "${local.region}a"
-        })
-        instance = merge(local.defaults_boe_ec2.instance, {
-          instance_type = "m4.xlarge"
-        })
-        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
-        tags = merge(local.defaults_boe_ec2.tags, {
-          oasys-national-reporting-environment = "t2"
-        })
-      })
+      # t2-onr-boe-1-a = merge(local.defaults_boe_ec2, {
+      #   config = merge(local.defaults_boe_ec2.config, {
+      #     instance_profile_policies = setunion(local.defaults_boe_ec2.config.instance_profile_policies, [
+      #       "Ec2SecretPolicy",
+      #     ])
+      #     availability_zone = "${local.region}a"
+      #   })
+      #   instance = merge(local.defaults_boe_ec2.instance, {
+      #     instance_type = "m4.xlarge"
+      #   })
+      #   user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+      #   tags = merge(local.defaults_boe_ec2.tags, {
+      #     oasys-national-reporting-environment = "t2"
+      #   })
+      # })
     }
     baseline_ec2_autoscaling_groups = {
       test-web-asg = merge(local.defaults_web_ec2, {
@@ -87,29 +87,31 @@ locals {
         })
         autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
       })
-      # test-boe-asg = merge(local.defaults_boe_ec2, {
-      #   config = merge(local.defaults_boe_ec2.config, {
-      #     instance_profile_policies = setunion(local.defaults_boe_ec2.config.instance_profile_policies, [
-      #       "Ec2SecretPolicy",
-      #     ])
-      #     availability_zone = "${local.region}a"
-      #   })
-      #   instance = merge(local.defaults_boe_ec2.instance, {
-      #     instance_type = "m4.xlarge"
-      #   })
-      #   user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
-      #     args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
-      #       branch = "onr/DSOS-2682/onr-boe-install"
-      #     })
-      #   })
-      #   autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
-      #     desired_capacity = 0
-      #   })
-      #   autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
-      #   tags = merge(local.defaults_boe_ec2.tags, {
-      #     oasys-national-reporting-environment = "t2"
-      #   })
-      # })
+      # TODO: this is just for testing, remove when not needed
+      t2-test-boe-asg = merge(local.defaults_boe_ec2, {
+        config = merge(local.defaults_boe_ec2.config, {
+          instance_profile_policies = setunion(local.defaults_boe_ec2.config.instance_profile_policies, [
+            "Ec2SecretPolicy",
+          ])
+          availability_zone = "${local.region}a"
+        })
+        instance = merge(local.defaults_boe_ec2.instance, {
+          instance_type = "m4.xlarge"
+        })
+        user_data_cloud_init = module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible
+        # user_data_cloud_init = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible, {
+        #   args = merge(module.baseline_presets.ec2_instance.user_data_cloud_init.ssm_agent_and_ansible.args, {
+        #     branch = "onr/DSOS-2682/onr-boe-install"
+        #   })
+        # })
+        autoscaling_group = merge(module.baseline_presets.ec2_autoscaling_group.default, {
+          desired_capacity = 0
+        })
+        autoscaling_schedules = module.baseline_presets.ec2_autoscaling_schedules.working_hours
+        tags = merge(local.defaults_boe_ec2.tags, {
+          oasys-national-reporting-environment = "t2"
+        })
+      })
       test-bods-asg = merge(local.defaults_bods_ec2, {
         config = merge(local.defaults_bods_ec2.config, {
           availability_zone = "${local.region}a"
