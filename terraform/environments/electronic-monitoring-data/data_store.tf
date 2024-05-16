880785d5a785d853e8837e4c8fc0ed4ac499e4bd
Luke Williams <lawilliams1993@gmail.com>
2024-05-16 14:40:49 +0000

    change to test trivy ignore by id

--- terraform/environments/electronic-monitoring-data/data_store.tf
+++ terraform/environments/electronic-monitoring-data/data_store.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket" "data_store" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "data_store" {
   bucket = aws_s3_bucket.data_store.id
-
+  #tfsec:ignore:AVD-AWS-0132:exp:2024-07-01
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
