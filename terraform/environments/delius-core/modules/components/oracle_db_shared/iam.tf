ffa13d9f8d34362b0c3f14965f992dba46d4442e
ranbeersingh1 <ranbeersinghtradedata@hotmail.com>
2024-05-17 15:26:29 +0100

    Additional policy for mis to access Delius application secret

--- terraform/environments/delius-core/modules/components/oracle_db_shared/iam.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/iam.tf
@@ -138,7 +138,6 @@ resource "aws_iam_policy" "ec2_access_for_ansible" {
 #  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 #}
 
-
 data "aws_iam_policy_document" "db_access_to_secrets_manager" {
   statement {
     sid = "DbAccessToSecretsManager"
@@ -158,10 +157,28 @@ data "aws_iam_policy_document" "db_access_to_secrets_manager" {
   }
 }
 
+data "aws_iam_policy_document" "allow_access_to_delius_application_passwords" {
+  statement {
+    sid = "DbAccessToDeliusSecretsManager"
+    actions = ["secretsmanager:GetSecretValue"]
+    effect = "Allow"
+    resources = [
+      "arn:aws:secretsmanager:*:${local.delius_account_id}:secret:delius-core-${var.env_name}-oracle-db-application-passwords-*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "combined_policy_documents" {
+  source_policy_documents = flatten([
+    data.aws_iam_policy_document.db_access_to_secrets_manager.json,
+    var.db_suffix == "mis-db" ? [data.aws_iam_policy_document.allow_access_to_delius_application_passwords.json] : []])
+}
+
 resource "aws_iam_policy" "db_access_to_secrets_manager" {
   name   = "${var.account_info.application_name}-${var.env_name}-${var.db_suffix}-secrets-manager-access"
-  policy = data.aws_iam_policy_document.db_access_to_secrets_manager.json
+  policy = data.aws_iam_policy_document.combined_policy_documents.json
 }
+
 
 #resource "aws_iam_role_policy_attachment" "db_access_to_secrets_manager" {
 #  role       = aws_iam_role.db_ec2_instance_iam_role.name
