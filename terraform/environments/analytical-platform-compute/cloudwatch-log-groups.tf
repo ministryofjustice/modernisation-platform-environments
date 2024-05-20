19306b6ee557c9eb1a8749e15fdaf2d625b855ea
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-20 20:22:01 +0000

    Fix SCA
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/cloudwatch-log-groups.tf
+++ terraform/environments/analytical-platform-compute/cloudwatch-log-groups.tf
@@ -1,9 +1,12 @@
 module "eks_log_group" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
   source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
   version = "5.3.1"
 
-  name       = "/aws/eks/${local.our_vpc_name}/logs"
-  kms_key_id = module.eks_cluster_logs_kms.key_arn
+  name              = "/aws/eks/${local.our_vpc_name}/logs"
+  kms_key_id        = module.eks_cluster_logs_kms.key_arn
   retention_in_days = 400
 
   tags = local.tags
