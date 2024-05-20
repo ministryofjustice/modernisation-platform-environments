fbd8dd26561989e38b4d4a5dc680e0d4c2b73e58
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-20 19:04:39 +0000

    back out aws-for-fluent-bit
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/eks-pod-identities.tf
+++ terraform/environments/analytical-platform-compute/eks-pod-identities.tf
@@ -22,40 +22,40 @@ module "aws_cloudwatch_metrics_pod_identity" {
   tags = local.tags
 }
 
-module "aws_for_fluent_bit_pod_identity" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+# module "aws_for_fluent_bit_pod_identity" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+#   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/eks-pod-identity/aws"
-  version = "1.2.0"
+#   source  = "terraform-aws-modules/eks-pod-identity/aws"
+#   version = "1.2.0"
 
-  name = "aws-for-fluent-bit"
+#   name = "aws-for-fluent-bit"
 
-  policy_statements = [
-    {
-      sid = "AllowKMS"
-      actions = [
-        "kms:Encrypt*",
-        "kms:Decrypt*",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:Describe*"
-      ]
-      resources = [module.eks_cluster_logs_kms.key_arn]
-    }
-  ]
+#   policy_statements = [
+#     {
+#       sid = "AllowKMS"
+#       actions = [
+#         "kms:Encrypt*",
+#         "kms:Decrypt*",
+#         "kms:ReEncrypt*",
+#         "kms:GenerateDataKey*",
+#         "kms:Describe*"
+#       ]
+#       resources = [module.eks_cluster_logs_kms.key_arn]
+#     }
+#   ]
 
-  additional_policy_arns = {
-    CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-  }
+#   additional_policy_arns = {
+#     CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+#   }
 
-  associations = {
-    eks = {
-      cluster_name    = module.eks.cluster_name
-      namespace       = kubernetes_namespace.amazon_cloudwatch.metadata[0].name
-      service_account = "aws-for-fluent-bit"
-    }
-  }
+#   associations = {
+#     eks = {
+#       cluster_name    = module.eks.cluster_name
+#       namespace       = kubernetes_namespace.amazon_cloudwatch.metadata[0].name
+#       service_account = "aws-for-fluent-bit"
+#     }
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
