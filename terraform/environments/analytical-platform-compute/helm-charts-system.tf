05d1f20c11eb5a5bb4c3efd62a4c3dae71780a08
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-21 07:48:07 +0000

    Add AMP proxy
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/helm-charts-system.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-system.tf
@@ -1,3 +1,4 @@
+/* Policy */
 resource "helm_release" "kyverno" {
   name       = "kyverno"
   repository = "https://kyverno.github.io/kyverno"
@@ -12,6 +13,7 @@ resource "helm_release" "kyverno" {
   ]
 }
 
+/* AWS Observability */
 /*
   There is an ongoing issue with aws-cloudwatch-metrics as it doesn't properly support IMDSv2 (https://github.com/aws/amazon-cloudwatch-agent/issues/1101)
   Therefore for this to work properly, I've set hostNetwork to true in src/helm/amazon-cloudwatch-metrics/values.yml.tftpl
@@ -55,4 +57,22 @@ resource "helm_release" "aws_for_fluent_bit" {
   ]
 
   depends_on = [module.aws_for_fluent_bit_iam_role]
+}
+
+resource "helm_release" "amazon_prometheus_proxy" {
+  name       = "amazon-prometheus-proxy"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  version    = "25.21.0"
+  namespace  = kubernetes_namespace.aws_observability.metadata[0].name
+  values = [
+    templatefile(
+      "${path.module}/src/helm/amazon-prometheus-proxy/values.yml.tftpl",
+      {
+        aws_region       = data.aws_region.current.name
+        eks_role_arn     = module.amazon_prometheus_proxy_iam_role.iam_role_arn
+        amp_workspace_id = aws_prometheus_workspace.main.id
+      }
+    )
+  ]
 }
