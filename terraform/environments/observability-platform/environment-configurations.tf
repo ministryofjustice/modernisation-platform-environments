659361657089b057d88ea9cf13d2a389d0aa0726
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-21 12:28:52 +0000

    Add changes for remote AMP
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/environment-configurations.tf
+++ terraform/environments/observability-platform/environment-configurations.tf
@@ -9,9 +9,10 @@ locals {
           pagerduty_services   = ["observability-platform"]
           aws_accounts = {
             "observability-platform-development" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = true
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = true
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
           }
         },
@@ -19,19 +20,23 @@ locals {
           identity_centre_team = "analytical-platform"
           aws_accounts = {
             "analytical-platform-ingestion-development" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = false
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             },
             "analytical-platform-compute-development" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = true
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = true
+              amazon_prometheus_query_enabled = true
+              amazon_prometheus_workspace_id  = "ws-bfdd5d7a-5571-4686-bfd4-43ab07cf8d54ba"
+              xray_enabled                    = true
             },
             "analytical-platform-compute-test" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = true
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = true
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
           }
         },
@@ -39,24 +44,28 @@ locals {
           "identity_centre_team" = "data-platform"
           "aws_accounts" = {
             "data-platform-development" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = false
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
             "data-platform-test" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = false
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
             "data-platform-preproduction" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = false
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
             "data-platform-apps-and-tools-development" = {
-              cloudwatch_enabled        = true
-              "prometheus_push_enabled" = true
-              xray_enabled              = true
+              cloudwatch_enabled              = true
+              "prometheus_push_enabled"       = true
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
           }
         }
@@ -64,14 +73,16 @@ locals {
           "identity_centre_team" = "studio-webops"
           "aws_accounts" = {
             "nomis-test" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = false
-              xray_enabled            = false
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = false
             }
             "oasys-test" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = false
-              xray_enabled            = false
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = false
             }
           }
         }
