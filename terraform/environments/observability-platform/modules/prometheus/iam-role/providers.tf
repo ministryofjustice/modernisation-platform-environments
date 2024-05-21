197972f38ac08509f6da03ce5a598ddbe96216be
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-21 17:05:10 +0000

    SCA errors are so hard to read in the UI
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/modules/prometheus/iam-role/providers.tf
+++ terraform/environments/observability-platform/modules/prometheus/iam-role/providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    grafana = {
-      source  = "grafana/grafana"
-      version = "~> 2.0"
-    }
   }
   required_version = "~> 1.0"
 }
