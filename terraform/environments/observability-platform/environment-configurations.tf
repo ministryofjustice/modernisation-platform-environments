bed71677c66bf3ce90505c0439d1c0feabdbf42e
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-21 14:40:30 +0000

    Update prod
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/environment-configurations.tf
+++ terraform/environments/observability-platform/environment-configurations.tf
@@ -10,7 +10,7 @@ locals {
           aws_accounts = {
             "observability-platform-development" = {
               cloudwatch_enabled              = true
-              prometheus_push_enabled         = true
+              prometheus_push_enabled         = false
               amazon_prometheus_query_enabled = false
               xray_enabled                    = true
             }
@@ -27,15 +27,16 @@ locals {
             },
             "analytical-platform-compute-development" = {
               cloudwatch_enabled              = true
-              prometheus_push_enabled         = true
+              prometheus_push_enabled         = false
               amazon_prometheus_query_enabled = true
               amazon_prometheus_workspace_id  = "ws-bfdd5d7a-5571-4686-bfd4-43ab07cf8d54ba"
               xray_enabled                    = true
             },
             "analytical-platform-compute-test" = {
               cloudwatch_enabled              = true
-              prometheus_push_enabled         = true
-              amazon_prometheus_query_enabled = false
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = true
+              amazon_prometheus_workspace_id  = "ws-a9d7f576-58b7-4748-b4c1-b02bbdc54a2922"
               xray_enabled                    = true
             }
           }
@@ -96,9 +97,10 @@ locals {
           slack_channels       = ["observability-platform-production-alerts"]
           aws_accounts = {
             "observability-platform-production" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = true
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
           }
         },
@@ -106,14 +108,16 @@ locals {
           identity_centre_team = "analytical-platform"
           aws_accounts = {
             "analytical-platform-ingestion-production" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = false
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             },
             "analytical-platform-compute-production" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = true
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
           }
         },
@@ -121,14 +125,16 @@ locals {
           "identity_centre_team" = "data-platform"
           "aws_accounts" = {
             "data-platform-production" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = false
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
             "data-platform-apps-and-tools-production" = {
-              cloudwatch_enabled      = true
-              prometheus_push_enabled = true
-              xray_enabled            = true
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = true
             }
           }
         }
