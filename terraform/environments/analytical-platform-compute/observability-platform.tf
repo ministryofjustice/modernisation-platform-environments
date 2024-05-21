1532abf8db9d326845c25561a95252e7df1b9200
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-21 09:28:17 +0000

    Testing cross-account AMP access
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/observability-platform.tf
+++ terraform/environments/analytical-platform-compute/observability-platform.tf
@@ -2,11 +2,17 @@ module "observability_platform_tenant" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "ministryofjustice/observability-platform-tenant/aws"
-  version = "1.0.1"
+  # source  = "ministryofjustice/observability-platform-tenant/aws"
+  # version = "1.0.1"
+
+  source = "github.com/ministryofjustice/terraform-aws-observability-platform-tenant?ref=feat%2Fsupport-additional-polices"
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-${local.environment_configuration.observability_platform}"]
+  enable_prometheus                 = true
   enable_xray                       = true
+  additional_policies = {
+    managed_prometheus_kms_access = module.managed_prometheus_kms_access_iam_policy.arn
+  }
 
   tags = local.tags
 }
