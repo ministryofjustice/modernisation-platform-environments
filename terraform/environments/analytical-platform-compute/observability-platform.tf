9b22a9a6d46172cb1f773d77d5b454143c877445
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-21 17:13:57 +0100

    Update observability-platform.tf

--- terraform/environments/analytical-platform-compute/observability-platform.tf
+++ terraform/environments/analytical-platform-compute/observability-platform.tf
@@ -2,10 +2,8 @@ module "observability_platform_tenant" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  # source  = "ministryofjustice/observability-platform-tenant/aws"
-  # version = "1.0.1"
-
-  source = "github.com/ministryofjustice/terraform-aws-observability-platform-tenant?ref=feat%2Fsupport-additional-polices"
+  source  = "ministryofjustice/observability-platform-tenant/aws"
+  version = "1.2.0"
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-${local.environment_configuration.observability_platform}"]
   enable_prometheus                 = true
