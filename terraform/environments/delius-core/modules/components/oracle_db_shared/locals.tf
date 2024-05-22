0139b4395deff57cc4a283cfd903343437d4922c
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2024-05-22 14:53:30 +0100

    NIT-1267 adding condition to check whether mis-test environment exists or not

--- terraform/environments/delius-core/modules/components/oracle_db_shared/locals.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/locals.tf
@@ -58,7 +58,4 @@ locals {
   }
 
   oracle_backup_bucket_prefix = "${var.account_info.application_name}-${var.env_name}-oracle-${var.db_suffix}-backups"
-
-  has_mis_environment = lookup(var.environment_config, "has_mis_environment", false) != null
-
 }
