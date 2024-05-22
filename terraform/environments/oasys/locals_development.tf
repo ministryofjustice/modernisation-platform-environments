fe1e0bbfe56835b5b5466cf614f62132b5403b95
W <willg258@hotmail.co.uk>
2024-05-22 15:20:36 +0100

    Update locals_development.tf

--- terraform/environments/oasys/locals_development.tf
+++ terraform/environments/oasys/locals_development.tf
@@ -4,7 +4,15 @@ locals {
   # cloudwatch monitoring config
   development_cloudwatch_monitoring_options = {}
 
-  development_baseline_presets_options = {}
+  development_baseline_presets_options = {
+    sns_topics = {
+      pagerduty_integrations = {
+        dso_pagerduty               = "oasys_nonprod_alarms"
+        dba_pagerduty               = "hmpps_shef_dba_non_prod"
+        dba_high_priority_pagerduty = "hmpps_shef_dba_non_prod"
+      }
+    }
+  }
 
   development_config = {}
 }
