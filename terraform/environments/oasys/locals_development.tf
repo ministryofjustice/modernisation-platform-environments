68729d60ee2aa5f0c96efb1231637deabcad1bba
W <willg258@hotmail.co.uk>
2024-05-22 15:28:53 +0100

    Update locals_development.tf

--- terraform/environments/oasys/locals_development.tf
+++ terraform/environments/oasys/locals_development.tf
@@ -14,5 +14,26 @@ locals {
     }
   }
 
-  development_config = {}
+  development_config = {
+    baseline_cloudwatch_log_groups = {	
+      session-manager-logs = {	
+        retention_in_days = 1	
+      }	
+      cwagent-var-log-messages = {	
+        retention_in_days = 1	
+      }	
+      cwagent-var-log-secure = {	
+        retention_in_days = 1	
+      }	
+      cwagent-windows-system = {	
+        retention_in_days = 1	
+      }	
+      cwagent-oasys-autologoff = {	
+        retention_in_days = 1	
+      }	
+      cwagent-web-logs = {	
+        retention_in_days = 1	
+      }	
+    }
+  }
 }
