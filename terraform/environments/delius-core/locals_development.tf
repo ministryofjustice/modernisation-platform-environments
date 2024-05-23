3aae2d7a70cb1795240ddce92b46bebe8e33f49b
James MacBeth <56261600+jamesmacbeth-unilink@users.noreply.github.com>
2024-05-23 15:20:17 +0100

    DST-17030 - Update UMT development environment version to dev

--- terraform/environments/delius-core/locals_development.tf
+++ terraform/environments/delius-core/locals_development.tf
@@ -133,7 +133,7 @@ locals {
     }
 
     umt = {
-      image_tag                        = "5.7.6"
+      image_tag                        = "dev"
       container_port                   = 8080
       container_memory                 = 4096
       container_cpu                    = 1024
