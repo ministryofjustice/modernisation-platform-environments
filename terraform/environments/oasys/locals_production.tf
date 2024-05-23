36265c37f514a60567b30da5fa9b05ec70063f16
W <willg258@hotmail.co.uk>
2024-05-23 11:37:45 +0100

    oasys tls public
    oasys tls public

--- terraform/environments/oasys/locals_production.tf
+++ terraform/environments/oasys/locals_production.tf
@@ -434,7 +434,7 @@ locals {
           https = {
             port                      = 443
             protocol                  = "HTTPS"
-            ssl_policy                = "ELBSecurityPolicy-2016-08"
+            ssl_policy                = "ELBSecurityPolicy-TLS13-1-2-2021-06"
             certificate_names_or_arns = ["pd_oasys_cert"]
             default_action = {
               type = "fixed-response"
