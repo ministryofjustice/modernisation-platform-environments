5cc8bca1d4adc4b72a4b519566edbcd9c171c098
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-05-24 14:54:53 +0100

    try adding lb listener rules for /admin and /secure access

--- terraform/environments/tribunals/modules/ecs_loadbalancer/main.tf
+++ terraform/environments/tribunals/modules/ecs_loadbalancer/main.tf
@@ -165,3 +165,78 @@ resource "aws_lb_listener" "tribunals_lb_health" {
     target_group_arn = aws_lb_target_group.tribunals_target_group.arn
   }
 }
+
+resource "aws_lb_listener_rule" "admin_access" {
+  listener_arn = aws_lb_listener.tribunals_lb.arn
+  priority     = 1
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tribunals_target_group.arn
+  }
+  condition {
+    path_pattern {
+      values = ["*/Admin*", "*/admin*"]
+    }
+    http_header {
+      http_header_name = "X-Forwarded-For"
+      values           = ["194.33.19.0/24", "109.154.193.219/32", "82.1.119.170/32"]
+    }
+
+  }
+}
+
+resource "aws_lb_listener_rule" "secure_access" {
+  listener_arn = aws_lb_listener.tribunals_lb.arn
+  priority     = 2
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tribunals_target_group.arn
+  }
+  condition {
+    path_pattern {
+      values = ["*/Secure*", "*/secure*"]
+    }
+    http_header {
+      http_header_name = "X-Forwarded-For"
+      values           = ["194.33.19.0/24", "109.154.193.219/32", "82.1.119.170/32"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "admin_secure_fixed_response" {
+  listener_arn = aws_lb_listener.tribunals_lb.arn
+  priority     = 3
+  action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>Secure Page</h1> <h3>This area of the website now requires elevated security.</h3> <br> <h3>If you believe you should be able to access this page please send an email to: - dts-legacy-apps-support-team@hmcts.net</h3>"
+      status_code  = "403"
+    }
+  }
+  condition {
+    path_pattern {
+      values = ["/Admin*", "/admin*", "/Secure*", "/secure*"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "default_response" {
+  listener_arn = aws_lb_listener.tribunals_lb.arn
+  priority     = 999
+
+  action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/html"
+      message_body = ""
+      status_code  = "404"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/Admin*", "/admin*", "/Secure*", "/secure*"]
+    }
+  }
+}
