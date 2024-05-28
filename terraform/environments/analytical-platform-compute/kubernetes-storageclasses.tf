1fb7673483cadd7d5d23ed50fed57f9ba5e4e9bf
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-28 12:13:47 +0000

    Add gp3 storage class
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/environments/analytical-platform-compute/kubernetes-storageclasses.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_storage_class" "gp3" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  reclaim_policy         = "Delete"
+  allow_volume_expansion = "true"
+
+  parameters = {
+    type      = "gp3"
+    encrypted = "true"
+  }
+}
