68c6bc711f1d87201078c2bbf4272de025766514
George Taylor <george.taylor@digital.justice.gov.uk>
2024-05-28 18:35:53 +0100

    arn

--- terraform/environments/delius-core/modules/helpers/delius_microservice/locals.tf
+++ terraform/environments/delius-core/modules/helpers/delius_microservice/locals.tf
@@ -44,7 +44,7 @@ locals {
   } : {}
 
   rds_secrets = var.rds_password_secret_variable != "" ? {
-    (var.rds_password_secret_variable) = "${aws_db_instance.this[0].master_user_secret[0].secret_arn}:password:AWSCURRENT"
+    (var.rds_password_secret_variable) = aws_db_instance.this[0].master_user_secret[0].secret_arn
   } : {}
 
   elasticache_env_vars = var.elasticache_endpoint_environment_variable != "" ? {
