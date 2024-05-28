d9357a98662d0d9f0b26d9dbed698d257afd84f1
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-05-28 14:46:02 +0100

    fix

--- terraform/modules/cloudwatch_dashboard/main.tf
+++ terraform/modules/cloudwatch_dashboard/main.tf
@@ -1,27 +1,39 @@
 locals {
 
+  widget_group_header_height = [
+    for widget_group in var.widget_groups : lookup(widget_group, "header_markdown", null) == null ? 0 : 1
+  ]
+  widget_group_widgets_height = [
+    for widget_group in var.widget_groups : widget_group.height * floor((length(widget_group.widgets) + 1) / (24 / widget_group.width))
+  ]
+  widget_group_height = [
+    for i in range(length(var.widget_groups)) : local.widget_group_header_height[i] + local.widget_group_widgets_height[i]
+  ]
+  widget_group_y = [
+    for i in range(length(var.widget_groups)) : i == 0 ? 0 : sum(slice(local.widget_group_height, 0, i))
+  ]
+
   # add header widget and calculate x, y positions
-  # set y to 0 as AWS figures that out OK. 
   widgets = flatten([
-    for widget_group in var.widget_groups : [
-      lookup(widget_group, "header_markdown", null) == null ? [] : [{
+    for i in range(length(var.widget_groups)) : [
+      lookup(var.widget_groups[i], "header_markdown", null) == null ? [] : [{
         type   = "text"
         width  = 24
         height = 1
         x      = 0
-        y      = 0
+        y      = local.widget_group_y[i]
         properties = {
-          markdown   = widget_group.header_markdown
+          markdown   = var.widget_groups[i].header_markdown
           background = "solid"
         }
       }],
       [
-        for i in range(length(widget_group.widgets)) : merge(widget_group.widgets[i], {
-          width  = widget_group.width
-          height = widget_group.height
-          x      = i * widget_group.width % 24
-          y      = 0
-        }) if widget_group.widgets[i] != null
+        for j in range(length(var.widget_groups[i].widgets)) : merge(var.widget_groups[i].widgets[j], {
+          width  = var.widget_groups[i].width
+          height = var.widget_groups[i].height
+          x      = j * var.widget_groups[i].width % 24
+          y      = (floor(j * var.widget_groups[i].width / 24) * var.widget_groups[i].height) + local.widget_group_y[i] + local.widget_group_header_height[i]
+        }) if var.widget_groups[i].widgets[j] != null
       ]
     ]
   ])
