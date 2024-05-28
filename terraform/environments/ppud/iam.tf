cc131aee5328c671600d377bcea3334723370a45
Buckingham <XC01857@lumen.com>
2024-05-28 11:38:29 +0100

    Update_Lambda_Functions_280524_7

--- terraform/environments/ppud/iam.tf
+++ terraform/environments/ppud/iam.tf
@@ -372,6 +372,67 @@ resource "aws_iam_role_policy_attachment" "attach_lambda_policy_cloudwatch_invok
   policy_arn = aws_iam_policy.iam_policy_for_lambda_cloudwatch_invoke_lambda_uat[0].arn
 }
 
+###########################################################
+# IAM Role & Policy for Lambda Terminate CPU Process - PROD
+###########################################################
+
+resource "aws_iam_role" "lambda_role_cloudwatch_invoke_lambda_prod" {
+  count              = local.is-production == true ? 1 : 0
+  name               = "PPUD_Lambda_Function_Role_Cloudwatch_Invoke_Lambda_PROD"
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda_cloudwatch_invoke_lambda_prod" {
+  count       = local.is-production == true ? 1 : 0
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role_cloudwatch_invoke_lambda_prod"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role cloudwatch invoke lambda prod"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Effect": "Allow",
+     "Action": [
+        "ssm:SendCommand",
+        "ssm:GetCommandInvocation",
+        "ec2:DescribeInstances",
+        "lambda:InvokeAsync",
+        "lambda:InvokeFunction"
+      ],
+      "Resource": [
+      "arn:aws:ssm:eu-west-2:817985104434:*",
+      "arn:aws:cloudwatch:eu-west-2:817985104434:*",
+      "arn:aws:ssm:eu-west-2::document/AWS-RunPowerShellScript",
+      "arn:aws:lambda:eu-west-2:817985104434:*",
+      "arn:aws:ec2:eu-west-2:817985104434:*"
+      ]
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_lambda_policy_cloudwatch_invoke_lambda_to_lambda_role_cloudwatch_invoke_lambda_prod" {
+  count      = local.is-production == true ? 1 : 0
+  role       = aws_iam_role.lambda_role_cloudwatch_invoke_lambda_prod[0].name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda_cloudwatch_invoke_lambda_prod[0].arn
+}
+
 ###################
 # SNS IAM Policies
 ###################
