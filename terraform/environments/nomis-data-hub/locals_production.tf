13f73657721985674a67bca237aa4d00c331cd9b
AntonyGowland <36728441+AntonyGowland@users.noreply.github.com>
2024-05-28 12:34:15 +0100

    create dr vms

--- terraform/environments/nomis-data-hub/locals_production.tf
+++ terraform/environments/nomis-data-hub/locals_production.tf
@@ -62,30 +62,30 @@ locals {
           nomis-data-hub-environment = "pd"
         })
       })
-      #  dr-ndh-app-b = merge(local.ndh_app_a, {
-      #    config = merge(local.ndh_app_a.config, {
-      #      availability_zone = "eu-west-2b"
-      #      instance_profile_policies = concat(local.ndh_app_a.config.instance_profile_policies, [
-      #        "Ec2drPolicy",
-      #      ])
-      #    })
-      #    tags = merge(local.ndh_app_a.tags, {
-      #      os-type                    = "Linux"
-      #      nomis-data-hub-environment = "dr"
-      #    })
-      #  })
-      #  dr-ndh-ems-b = merge(local.ndh_ems_a, {
-      #    config = merge(local.ndh_ems_a.config, {
-      #      availability_zone = "eu-west-2b"
-      #      instance_profile_policies = concat(local.ndh_ems_a.config.instance_profile_policies, [
-      #        "Ec2drPolicy",
-      #      ])
-      #    })
-      #    tags = merge(local.ndh_ems_a.tags, {
-      #      os-type                    = "Linux"
-      #      nomis-data-hub-environment = "dr"
-      #    })
-      #  })
+      dr-ndh-app-b = merge(local.ndh_app_a, {
+        config = merge(local.ndh_app_a.config, {
+          availability_zone = "eu-west-2b"
+          instance_profile_policies = concat(local.ndh_app_a.config.instance_profile_policies, [
+            "Ec2drPolicy",
+          ])
+        })
+        tags = merge(local.ndh_app_a.tags, {
+          os-type                    = "Linux"
+          nomis-data-hub-environment = "dr"
+        })
+      })
+      dr-ndh-ems-b = merge(local.ndh_ems_a, {
+        config = merge(local.ndh_ems_a.config, {
+          availability_zone = "eu-west-2b"
+          instance_profile_policies = concat(local.ndh_ems_a.config.instance_profile_policies, [
+            "Ec2drPolicy",
+          ])
+        })
+        tags = merge(local.ndh_ems_a.tags, {
+          os-type                    = "Linux"
+          nomis-data-hub-environment = "dr"
+        })
+      })
     }
     #when changing the ems entries in prod or t2, also stop and start xtag to reconnect it.
     baseline_route53_zones = {
