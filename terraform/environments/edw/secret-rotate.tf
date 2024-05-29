0e174bbccb6a1fa4c33bc0e40a5d20dc162fcecc
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-05-29 15:01:39 +0100

    LAWS-3868 - attepting new role
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/edw/secret-rotate.tf
+++ terraform/environments/edw/secret-rotate.tf
@@ -151,7 +151,7 @@ resource "aws_iam_policy" "edw_lambda_function_execution_role_policy" { #tfsec:i
           "logs:PutLogEvents",
         ],
         Resource = [
-          "arn:aws:logs:eu-west-2:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/edw-edw-system-root-password-rotation:*",
+          "arn:aws:logs:eu-west-2:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/edw-edw-edw-system-root-password-rotation:*",
         ]
       },
       {
