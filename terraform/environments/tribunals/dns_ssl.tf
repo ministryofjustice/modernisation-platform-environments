0b50d355a939d97380e814c85f9af46cee481873
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-05-29 15:04:50 +0100

    revert to old method of validating cert

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -395,45 +395,6 @@ resource "aws_route53_record" "external_admin_appeals_sftp" {
 #   }
 # }
 
-output "acm_certificate_arn" {
-  value = aws_acm_certificate.external.arn
-}
-
-output "acm_certificate_validation_dns" {
-  value = [for dvo in aws_acm_certificate.external.domain_validation_options : dvo.resource_record_name]
-}
-
-output "acm_certificate_validation_route53" {
-  value = [for dvo in aws_acm_certificate.external.domain_validation_options : dvo.resource_record_value]
-}
-
-# ACM certificate validation
-# resource "aws_acm_certificate_validation" "external" {
-#   certificate_arn = aws_acm_certificate.external.arn
-#   validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
-# }
-
-# One route53 record required for each domain listed in the external certificate
-# resource "aws_route53_record" "external_validation" {
-#   provider = aws.core-vpc
-#   for_each = {
-#     for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
-
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.external.zone_id
-# }
-
-//////////////////////////////
-
 // ACM Public Certificate
 resource "aws_acm_certificate" "external" {
   domain_name       = "modernisation-platform.service.justice.gov.uk"
@@ -451,30 +412,67 @@ resource "aws_acm_certificate" "external" {
   }
 }
 
+# ACM certificate validation
 resource "aws_acm_certificate_validation" "external" {
-  certificate_arn         = aws_acm_certificate.external.arn
-  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
+  certificate_arn = aws_acm_certificate.external.arn
+  validation_record_fqdns = [for record in aws_route53_record.external_validation : record.fqdn]
 }
 
-// Route53 DNS records for certificate validation
+# One route53 record required for each domain listed in the external certificate
 resource "aws_route53_record" "external_validation" {
-  provider = aws.core-network-services
-
-  allow_overwrite = true
-  name            = local.domain_name_main[0]
-  records         = local.domain_record_main
-  ttl             = 60
-  type            = local.domain_type_main[0]
-  zone_id         = data.aws_route53_zone.network-services.zone_id
-}
-
-resource "aws_route53_record" "external_validation_subdomain" {
   provider = aws.core-vpc
+  for_each = {
+    for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
   allow_overwrite = true
-  name            = local.domain_name_sub[0]
-  records         = local.domain_record_sub
+  name            = each.value.name
+  records         = [each.value.record]
   ttl             = 60
-  type            = local.domain_type_sub[0]
+  type            = each.value.type
   zone_id         = data.aws_route53_zone.external.zone_id
 }
+
+output "acm_certificate_arn" {
+  value = aws_acm_certificate.external.arn
+}
+
+output "acm_certificate_validation_dns" {
+  value = [for dvo in aws_acm_certificate.external.domain_validation_options : dvo.resource_record_name]
+}
+
+output "acm_certificate_validation_route53" {
+  value = [for dvo in aws_acm_certificate.external.domain_validation_options : dvo.resource_record_value]
+}
+
+# resource "aws_acm_certificate_validation" "external" {
+#   certificate_arn         = aws_acm_certificate.external.arn
+#   validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
+# }
+
+# // Route53 DNS records for certificate validation
+# resource "aws_route53_record" "external_validation" {
+#   provider = aws.core-network-services
+
+#   allow_overwrite = true
+#   name            = local.domain_name_main[0]
+#   records         = local.domain_record_main
+#   ttl             = 60
+#   type            = local.domain_type_main[0]
+#   zone_id         = data.aws_route53_zone.network-services.zone_id
+# }
+
+# resource "aws_route53_record" "external_validation_subdomain" {
+#   provider = aws.core-vpc
+
+#   allow_overwrite = true
+#   name            = local.domain_name_sub[0]
+#   records         = local.domain_record_sub
+#   ttl             = 60
+#   type            = local.domain_type_sub[0]
+#   zone_id         = data.aws_route53_zone.external.zone_id
+# }
