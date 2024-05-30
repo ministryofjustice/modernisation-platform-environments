247829c0f0022c70b7074a5d2100de63b3566227
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-05-30 15:11:06 +0100

    comment out more references to cert

--- terraform/environments/tribunals/modules/ecs_loadbalancer/variables.tf
+++ terraform/environments/tribunals/modules/ecs_loadbalancer/variables.tf
@@ -17,8 +17,8 @@ variable "application_data" {
 variable "subnets_shared_public_ids" {
 }
 
-variable "aws_acm_certificate_external" {
-}
+# variable "aws_acm_certificate_external" {
+# }
 
 variable "is_ftp_app" {
   description = "Determines if it is an ftp app or not"
