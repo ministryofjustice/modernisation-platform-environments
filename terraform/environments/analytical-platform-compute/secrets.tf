542c71955b7f4ae26f2cbbf0f05d1b2aee813f78
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-30 15:55:10 +0000

    typo
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/secrets.tf
+++ terraform/environments/analytical-platform-compute/secrets.tf
@@ -1,10 +1,10 @@
-module "actions_runner_create_a_derived_table" {
+module "actions_runners_create_a_derived_table" {
   count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
 
   source  = "terraform-aws-modules/secrets-manager/aws"
   version = "1.1.2"
 
-  name        = "actions-runner/create-a-derived-table"
+  name        = "actions-runners/create-a-derived-table"
   description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/2208432"
 
   secret_string         = "CHANGEME"
