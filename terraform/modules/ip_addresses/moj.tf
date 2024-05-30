3bfbd84347ec89758f7cb99e3067ff69836b6cdd
George Taylor <george.taylor@digital.justice.gov.uk>
2024-05-30 17:45:03 +0100

    add ark egress IPs

--- terraform/modules/ip_addresses/moj.tf
+++ terraform/modules/ip_addresses/moj.tf
@@ -50,6 +50,15 @@ locals {
     palo_alto_primsa_access_third_party = "128.77.75.0/25"
     palo_alto_primsa_access_residents = "128.77.75.128/26"
 
+
+    ark_dc_external_internet = [
+      "195.59.75.0/24",
+      "194.33.192.0/25",
+      "194.33.193.0/25",
+      "194.33.196.0/25",
+      "194.33.197.0/25"
+    ]
+
     # for devices connected to Prison Networks
     vodafone_wan_nicts_aggregate = "10.80.0.0/12"
     # For users without an MOJ Official device, e.g. private prisons
@@ -77,7 +86,8 @@ locals {
       local.moj_cidr.mojo_arkc_internet_egress_exponential_e,
       local.moj_cidr.mojo_arkc_internet_egress_vodafone,
       local.moj_cidr.mojo_arkf_internet_egress_exponential_e,
-      local.moj_cidr.mojo_arkf_internet_egress_vodafone
+      local.moj_cidr.mojo_arkf_internet_egress_vodafone,
+      local.moj_cidr.ark_dc_external_internet,
     ])
 
     trusted_moj_enduser_internal = [
