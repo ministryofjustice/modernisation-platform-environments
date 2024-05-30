a88d0bc9e168c7a267489368a68a24521a2273ca
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-05-30 14:59:19 +0100

    try removing wildcard from certificate

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -5,7 +5,7 @@ resource "aws_acm_certificate" "external" {
   domain_name       = "modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
-  subject_alternative_names = ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   tags = {
     Environment = local.environment
   }
