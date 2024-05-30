6098dedebe6c3970aafefa21a5ed85120327592d
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2024-05-30 10:10:57 +0100

    NIT-1276 build infra in preproduction

--- terraform/environments/delius-core/locals_stage.tf
+++ terraform/environments/delius-core/locals_stage.tf
@@ -3,23 +3,21 @@
 # Sample data
 # tags demonstrate inheritance due to merges in the module
 locals {
-
-  environment_config_test = {
-    migration_environment_private_cidr     = ["10.162.8.0/22", "10.162.4.0/22", "10.162.0.0/22"]
-    migration_environment_db_cidr          = ["10.162.14.0/25", "10.162.13.0/24", "10.162.12.0/24"]
-    migration_environment_full_name        = "del-test"
+  environment_config_stage = {
+    migration_environment_private_cidr     = ["10.160.32.0/22", "10.160.36.0/22", "10.160.40.0/23"]
+    migration_environment_db_cidr          = ["10.160.42.0/23", "10.160.44.0/23", "10.160.46.0/23"]
+    migration_environment_full_name        = "del-stage"
     migration_environment_abbreviated_name = "del"
-    migration_environment_short_name       = "test"
+    migration_environment_short_name       = "stage"
     legacy_engineering_vpc_cidr            = "10.161.98.0/25"
-    ec2_user_ssh_key                       = file("${path.module}/files/.ssh/test/ec2-user.pub")
+    ec2_user_ssh_key                       = file("${path.module}/files/.ssh/stage/ec2-user.pub")
     homepage_path                          = "/"
-    has_mis_environment                    = false
   }
 
-  ldap_config_test = {
+  ldap_config_stage = {
     name                        = "ldap"
     encrypted                   = true
-    migration_source_account_id = "728765553488"
+    migration_source_account_id = "205048117103"
     migration_lambda_role       = "ldap-data-migration-lambda-role"
     efs_throughput_mode         = "bursting"
     efs_provisioned_throughput  = null
@@ -29,13 +27,15 @@ locals {
   }
 
 
-  db_config_test = {
+  db_config_stage = {
     instance_type  = "r6i.xlarge"
     ami_name_regex = "^delius_core_ol_8_5_oracle_db_19c_patch_2024-01-31T16-06-00.575Z"
+    standby_count  = 0
+
     instance_policies = {
       "business_unit_kms_key_access" = aws_iam_policy.business_unit_kms_key_access
     }
-    standby_count = 0
+
     ebs_volumes = {
       "/dev/sdb" = { label = "app", size = 200 } # /u01
       "/dev/sdc" = { label = "app", size = 100 } # /u02
@@ -70,7 +70,7 @@ locals {
     }
   }
 
-  delius_microservices_configs_test = {
+  delius_microservices_configs_stage = {
     gdpr_ui = {
       image_tag      = "REPLACE"
       container_port = 80
@@ -89,7 +89,7 @@ locals {
       rds_license_model           = "postgresql-license"
       rds_deletion_protection     = false
       rds_skip_final_snapshot     = true
-      snapshot_identifier         = "rds-1187-test-copy"
+      snapshot_identifier         = "REPLACE"
       rds_backup_retention_period = 1
       maintenance_window          = "Wed:21:00-Wed:23:00"
       rds_backup_window           = "19:00-21:00"
@@ -162,7 +162,7 @@ locals {
     }
   }
 
-  bastion_config_test = {
+  bastion_config_stage = {
     business_unit           = local.vpc_name
     subnet_set              = local.subnet_set
     environment             = local.environment
