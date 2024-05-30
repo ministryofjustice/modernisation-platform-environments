522d3318338b1ec184bc26dbad618f58f3d2b669
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-30 15:47:57 +0000

    Add actions-runner NS Add CaDeT secret
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/environments/analytical-platform-compute/secrets.tf
@@ -0,0 +1,19 @@
+module "actions_runner_create_a_derived_table" {
+  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
+
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.1.2"
+
+  name        = "actions-runner/create-a-derived-table"
+  description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/2208432"
+
+  secret_string         = "CHANGEME"
+  ignore_secret_changes = true
+
+  tags = merge(
+    local.tags,
+    {
+      "expiry-date" = "2024-10-26"
+    }
+  )
+}
