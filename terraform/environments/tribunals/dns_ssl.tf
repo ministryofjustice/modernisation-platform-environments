93fa3d9d9c67060bcc124451b0fe8bc4e3c9fbe6
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-05-30 16:57:51 +0100

    uncomment dns / cert creation

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -1,72 +1,58 @@
 // DEV + PRE-PRODUCTION DNS CONFIGURATION
 
 // ACM Public Certificate
-# resource "aws_acm_certificate" "external" {
-#   domain_name       = "modernisation-platform.service.justice.gov.uk"
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "external" {
+  domain_name       = "modernisation-platform.service.justice.gov.uk"
+  validation_method = "DNS"
 
-#   subject_alternative_names = ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-#   tags = {
-#     Environment = local.environment
-#   }
+  subject_alternative_names = ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  tags = {
+    Environment = local.environment
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_acm_certificate_validation" "external" {
-#   certificate_arn         = aws_acm_certificate.external.arn
-#   validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
-# }
+resource "aws_acm_certificate_validation" "external" {
+  certificate_arn         = aws_acm_certificate.external.arn
+  validation_record_fqdns = [local.domain_name_main[0], local.domain_name_sub[0]]
+}
 
-# // Route53 DNS records for certificate validation
-# resource "aws_route53_record" "external_validation" {
-#   provider = aws.core-network-services
+// Route53 DNS records for certificate validation
+resource "aws_route53_record" "external_validation" {
+  provider = aws.core-network-services
 
-#   allow_overwrite = true
-#   name            = local.domain_name_main[0]
-#   records         = local.domain_record_main
-#   ttl             = 60
-#   type            = local.domain_type_main[0]
-#   zone_id         = data.aws_route53_zone.network-services.zone_id
-# }
+  allow_overwrite = true
+  name            = local.domain_name_main[0]
+  records         = local.domain_record_main
+  ttl             = 60
+  type            = local.domain_type_main[0]
+  zone_id         = data.aws_route53_zone.network-services.zone_id
+}
 
-# resource "aws_route53_record" "external_validation_subdomain" {
-#   provider = aws.core-vpc
+resource "aws_route53_record" "external_validation_subdomain" {
+  provider = aws.core-vpc
 
-#   allow_overwrite = true
-#   name            = local.domain_name_sub[0]
-#   records         = local.domain_record_sub
-#   ttl             = 60
-#   type            = local.domain_type_sub[0]
-#   zone_id         = data.aws_route53_zone.external.zone_id
-# }
+  allow_overwrite = true
+  name            = local.domain_name_sub[0]
+  records         = local.domain_record_sub
+  ttl             = 60
+  type            = local.domain_type_sub[0]
+  zone_id         = data.aws_route53_zone.external.zone_id
+}
 
-# # // Create one Route 53 record for each entry in the list of tribunals (assigned in platform_locals.tf)
-# resource "aws_route53_record" "external_appeals" {
-#   provider = aws.core-vpc
-#   zone_id = data.aws_route53_zone.external.zone_id
-#   name    = "administrativeappeals.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-#   type    = "A"
+# // Create one Route 53 record for each entry in the list of tribunals (assigned in platform_locals.tf)
+resource "aws_route53_record" "external_appeals" {
+  provider = aws.core-vpc
+  zone_id = data.aws_route53_zone.external.zone_id
+  name    = "administrativeappeals.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type    = "A"
 
-#   alias {
-#     name                   = module.appeals.tribunals_lb.dns_name
-#     zone_id                = module.appeals.tribunals_lb.zone_id
-#     evaluate_target_health = true
-#   }
-# }
-
-# ////
-
-# output "acm_certificate_arn" {
-#   value = aws_acm_certificate.external.arn
-# }
-
-# output "acm_certificate_validation_dns" {
-#   value = [for dvo in aws_acm_certificate.external.domain_validation_options : dvo.resource_record_name]
-# }
-
-# output "acm_certificate_validation_route53" {
-#   value = [for dvo in aws_acm_certificate.external.domain_validation_options : dvo.resource_record_value]
-# }
+  alias {
+    name                   = module.appeals.tribunals_lb.dns_name
+    zone_id                = module.appeals.tribunals_lb.zone_id
+    evaluate_target_health = true
+  }
+}
