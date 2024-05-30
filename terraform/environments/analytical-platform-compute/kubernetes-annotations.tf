aafa86054371f94c7e35205b41d801603e97f73b
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-30 08:06:43 +0000

    mark gp2 as non default
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/environments/analytical-platform-compute/kubernetes-annotations.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_annotations" "gp2" {
+  api_version = "storage.k8s.io/v1"
+  kind        = "StorageClass"
+
+  metadata {
+    name = "gp2"
+  }
+
+  annotations = {
+    "storageclass.kubernetes.io/is-default-class" = "false"
+  }
+}
--- terraform/environments/analytical-platform-compute/kubernetes-storageclasses.tf
+++ terraform/environments/analytical-platform-compute/kubernetes-storageclasses.tf
@@ -9,9 +9,12 @@ resource "kubernetes_storage_class" "gp3" {
   storage_provisioner    = "ebs.csi.aws.com"
   reclaim_policy         = "Delete"
   allow_volume_expansion = "true"
+  volume_binding_mode    = "WaitForFirstConsumer"
 
   parameters = {
     type      = "gp3"
     encrypted = "true"
   }
+
+  depends_on = [kubernetes_annotations.gp2]
 }
