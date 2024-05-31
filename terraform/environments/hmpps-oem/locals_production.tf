95ed6b1eb3f0582ea8fa1b3d0eca19d449140a3c
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-05-31 12:15:58 +0100

    DSOS-2797: align hmpps-oem terraform with nomis (#6352)
    * DSOS-2797: align hmpps-oem terraform with nomis
    
    * Fix

--- terraform/environments/hmpps-oem/locals_production.tf
+++ terraform/environments/hmpps-oem/locals_production.tf
@@ -1,31 +1,20 @@
-# nomis-production environment settings
 locals {
 
-  # cloudwatch monitoring config
-  production_cloudwatch_monitoring_options = {
-    enable_cloudwatch_monitoring_account = false
-  }
-
-  # baseline presets config
-  production_baseline_presets_options = {
-    sns_topics = {
-      pagerduty_integrations = {
-        dba_pagerduty = "hmpps_shef_dba_low_priority"
-        dso_pagerduty = "nomis_alarms"
+  baseline_presets_production = {
+    options = {
+      sns_topics = {
+        pagerduty_integrations = {
+          dba_pagerduty = "hmpps_shef_dba_low_priority"
+          dso_pagerduty = "nomis_alarms"
+        }
       }
     }
   }
 
-  # baseline config
-  production_config = {
+  # please keep resources in alphabetical order
+  baseline_production = {
 
-    baseline_secretsmanager_secrets = {
-      "/oracle/oem"              = local.oem_secretsmanager_secrets
-      "/oracle/database/EMREP"   = local.oem_secretsmanager_secrets
-      "/oracle/database/PRCVCAT" = local.oem_secretsmanager_secrets
-    }
-
-    baseline_ec2_instances = {
+    ec2_instances = {
       prod-oem-a = merge(local.oem_ec2_default, {
         cloudwatch_metric_alarms = merge(
           local.oem_ec2_cloudwatch_metric_alarms.standard,
@@ -45,12 +34,18 @@ locals {
       })
     }
 
-    baseline_route53_zones = {
+    route53_zones = {
       "hmpps-production.modernisation-platform.service.justice.gov.uk" = {
         records = [
           { name = "oem.hmpps-oem", type = "CNAME", ttl = "300", records = ["prod-oem-a.hmpps-oem.hmpps-production.modernisation-platform.service.justice.gov.uk"] },
         ]
       }
+    }
+
+    secretsmanager_secrets = {
+      "/oracle/oem"              = local.oem_secretsmanager_secrets
+      "/oracle/database/EMREP"   = local.oem_secretsmanager_secrets
+      "/oracle/database/PRCVCAT" = local.oem_secretsmanager_secrets
     }
   }
 }
