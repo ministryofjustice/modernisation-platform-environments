5c39fe3a5aa3c0dfbc581848470ba35c55149fe7
George Taylor <george.taylor@digital.justice.gov.uk>
2024-05-31 12:35:02 +0100

    kms

--- terraform/environments/delius-nextcloud/modules/components/nextcloud/ecs_service.tf
+++ terraform/environments/delius-nextcloud/modules/components/nextcloud/ecs_service.tf
@@ -156,14 +156,23 @@ data "aws_iam_policy_document" "s3_bucket_config" {
 
 data "aws_iam_policy_document" "access_ldap_secret" {
   statement {
-    sid = "DbAccessToSecretsManager"
+    sid = "AccessToSecretsManager"
     actions = [
-      "secretsmanager:GetSecretValue",
-      "kms:Decrypt"
+      "secretsmanager:GetSecretValue"
     ]
     effect = "Allow"
     resources = [
       "arn:aws:secretsmanager:eu-west-2:${var.platform_vars.environment_management.account_ids[join("-", ["delius-core", var.account_info.mp_environment])]}:secret:ldap-admin-password*"
+    ]
+  }
+  statement {
+    sid = "kms_access"
+    actions = [
+      "kms:Decrypt"
+    ]
+    effect = "Allow"
+    resources = [
+      var.account_config.kms_keys.general_shared
     ]
   }
 }
