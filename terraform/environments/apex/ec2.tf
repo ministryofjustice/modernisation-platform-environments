744073e0027332cac90db55aac8234c5d7833109
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2024-05-31 17:19:04 +0100

    LAWS-3884: Readd rule to allow MP VPC access for OAS

--- terraform/environments/apex/ec2.tf
+++ terraform/environments/apex/ec2.tf
@@ -81,6 +81,15 @@ resource "aws_vpc_security_group_ingress_rule" "db_ecs" {
   to_port                      = 1521
 }
 
+resource "aws_vpc_security_group_ingress_rule" "db_mp_vpc" {
+  security_group_id            = aws_security_group.ec2.id
+  description                  = "Allow MP VPC (OAS) to access database instance"
+  referenced_security_group_id = data.aws_vpc.shared.cidr_block
+  from_port                    = 1521
+  ip_protocol                  = "tcp"
+  to_port                      = 1521
+}
+
 resource "aws_vpc_security_group_ingress_rule" "db_lambda" {
   security_group_id            = aws_security_group.ec2.id
   description                  = "Allow Lambda SSH access for backup snapshots"
