ced502742f591ddcc135a085e657d4d1409ce03f
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-06-03 17:16:19 +0100

    fix typos

--- terraform/environments/cdpt-chaps/route53.tf
+++ terraform/environments/cdpt-chaps/route53.tf
@@ -52,8 +52,8 @@ resource "aws_route53_record" "external" {
   type    = "A"
 
   alias {
-    name                   = module.lb_access_logs_enabled.load_balancer.arn.dns_name
-    zone_id                = module.lb_access_logs_enabled.load_balancer.arn.zone_id
+    name                   = module.lb_access_logs_enabled.load_balancer.dns_name
+    zone_id                = module.lb_access_logs_enabled.load_balancer.zone_id
     evaluate_target_health = true
   }
 }
