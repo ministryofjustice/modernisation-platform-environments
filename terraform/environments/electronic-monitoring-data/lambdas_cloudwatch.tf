a7d9ff528186faacd270d380a8857e367c0d49ed
matt-heery <matt.heery@digital.justice.gov.uk>
2024-06-03 11:18:52 +0100

    removing double cloudwatch

--- terraform/environments/electronic-monitoring-data/lambdas_cloudwatch.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_cloudwatch.tf
@@ -1,8 +1,3 @@
-resource "aws_cloudwatch_log_group" "get_metadata_from_rds_lambda" {
-  name              = "/aws/lambda/get_metadata_from_rds"
-  retention_in_days = 400
-}
-
 resource "aws_cloudwatch_log_group" "create_athena_external_table_lambda" {
   name              = "/aws/lambda/create_athena_external_table"
   retention_in_days = 400
