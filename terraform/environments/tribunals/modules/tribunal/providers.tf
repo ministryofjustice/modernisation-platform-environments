50ea1830a4d5863329f06e8302d9d0bbccb14c67
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-06-03 14:32:04 +0100

    add provider original-session

--- terraform/environments/tribunals/modules/tribunal/providers.tf
+++ terraform/environments/tribunals/modules/tribunal/providers.tf
@@ -2,6 +2,13 @@
 data "aws_caller_identity" "original_session" {
   provider = aws.original-session
 }
+
+# AWS provider for the original session which you connect with
+provider "aws" {
+  alias  = "original-session"
+  region = "eu-west-2"
+}
+
 provider "aws" {
   alias  = "core-vpc"
   region = "eu-west-2"
