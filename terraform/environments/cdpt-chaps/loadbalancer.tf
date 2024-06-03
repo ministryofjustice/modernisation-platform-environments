b50c6a69f0e8611e327205e23aab8c09732cb5f5
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-06-03 17:00:28 +0100

    use MP lb module

--- terraform/environments/cdpt-chaps/loadbalancer.tf
+++ terraform/environments/cdpt-chaps/loadbalancer.tf
@@ -1,37 +1,187 @@
-resource "aws_security_group" "chaps_lb_sc" {
-  name        = "load balancer security group"
-  description = "control access to the load balancer"
-  vpc_id      = data.aws_vpc.shared.id
+# resource "aws_security_group" "chaps_lb_sc" {
+#   name        = "load balancer security group"
+#   description = "control access to the load balancer"
+#   vpc_id      = data.aws_vpc.shared.id
 
-  ingress {
-    description = "allow access on HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["188.214.15.75/32", "192.168.5.101/32", "81.134.202.29/32", "79.152.189.104/32", "179.50.12.212/32", "188.172.252.34/32", "194.33.192.0/25", "194.33.193.0/25", "194.33.196.0/25", "194.33.197.0/25", "195.59.75.0/24", "201.33.21.5/32", "213.121.161.112/28", "52.67.148.55/32", "54.94.206.111/32", "178.248.34.42/32", "178.248.34.43/32", "178.248.34.44/32", "178.248.34.45/32", "178.248.34.46/32", "178.248.34.47/32", "89.32.121.144/32", "185.191.249.100/32", "2.138.20.8/32", "18.169.147.172/32", "35.176.93.186/32", "18.130.148.126/32", "35.176.148.126/32", "51.149.250.0/24", "51.149.249.0/29", "194.33.249.0/29", "51.149.249.32/29", "194.33.248.0/29", "20.49.214.199/32", "20.49.214.228/32", "20.26.11.71/32", "20.26.11.108/32", "128.77.75.128/26"]
+#   ingress {
+#     description = "allow access on HTTPS"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["188.214.15.75/32", "192.168.5.101/32", "81.134.202.29/32", "79.152.189.104/32", "179.50.12.212/32", "188.172.252.34/32", "194.33.192.0/25", "194.33.193.0/25", "194.33.196.0/25", "194.33.197.0/25", "195.59.75.0/24", "201.33.21.5/32", "213.121.161.112/28", "52.67.148.55/32", "54.94.206.111/32", "178.248.34.42/32", "178.248.34.43/32", "178.248.34.44/32", "178.248.34.45/32", "178.248.34.46/32", "178.248.34.47/32", "89.32.121.144/32", "185.191.249.100/32", "2.138.20.8/32", "18.169.147.172/32", "35.176.93.186/32", "18.130.148.126/32", "35.176.148.126/32", "51.149.250.0/24", "51.149.249.0/29", "194.33.249.0/29", "51.149.249.32/29", "194.33.248.0/29", "20.49.214.199/32", "20.49.214.228/32", "20.26.11.71/32", "20.26.11.108/32", "128.77.75.128/26"]
+#   }
+
+#   egress {
+#     description = "Open all outbound ports"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+# resource "aws_lb" "chaps_lb" {
+#   name               = "chaps-load-balancer"
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.chaps_lb_sc.id]
+#   subnets            = data.aws_subnets.shared-public.ids
+#   idle_timeout       = 60
+
+#   access_logs {
+#     bucket  = aws_s3_bucket.chaps_lb_logs.bucket
+#     prefix  = "chaps_lb"
+#     enabled = true
+#   }
+# }
+
+# resource "aws_lb_target_group" "chaps_target_group" {
+#   name                 = "chaps-target-group"
+#   port                 = 80
+#   protocol             = "HTTP"
+#   vpc_id               = data.aws_vpc.shared.id
+#   target_type          = "ip"
+#   deregistration_delay = 30
+
+#   stickiness {
+#     type            = "lb_cookie"
+#     cookie_duration = 86400
+#   }
+
+#   health_check {
+#     healthy_threshold   = "5"
+#     interval            = "120"
+#     protocol            = "HTTP"
+#     unhealthy_threshold = "2"
+#     matcher             = "200-499"
+#     timeout             = "5"
+#   }
+# }
+
+# resource "aws_lb_listener" "https_listener" {
+#   #checkov:skip=CKV_AWS_103
+#   depends_on = [aws_acm_certificate_validation.external]
+
+#   load_balancer_arn = aws_lb.chaps_lb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate.external.arn
+
+#   default_action {
+#     target_group_arn = aws_lb_target_group.chaps_target_group.id
+#     type             = "forward"
+#   }
+# }
+
+# resource "aws_s3_bucket" "chaps_lb_logs" {
+#   bucket = "chaps-lb-logs-bucket"
+# }
+
+# resource "aws_s3_bucket_versioning" "chaps_lb_logs_versioning" {
+#   bucket = aws_s3_bucket.chaps_lb_logs.bucket
+
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# resource "aws_s3_bucket_lifecycle_configuration" "chaps_lb_logs_lifecycle" {
+#   bucket = aws_s3_bucket.chaps_lb_logs.id
+
+#   rule {
+#     id = "log"
+#     status = "Enabled"
+
+#     expiration {
+#       days = 90
+#     }
+
+#     noncurrent_version_expiration {
+#       noncurrent_days = 90
+#     }
+#   }
+# }
+
+# resource "aws_iam_role" "lb_logging_role" {
+#   name = "lb-logging-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect    = "Allow",
+#         Principal = {
+#           Service = "elasticloadbalancing.amazonaws.com"
+#         },
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_policy" "lb_logging_policy" {
+#   name = "lb-logging-policy"
+#   policy = jsonencode({
+#     Version   = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "s3:PutObject",
+#           "s3:PutObjectAcl"
+#         ],
+#         Resource = "${aws_s3_bucket.chaps_lb_logs.arn}/*"
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "lb_logging_policy_attachment" {
+#   role       = aws_iam_role.lb_logging_role.name
+#   policy_arn = aws_iam_policy.lb_logging_policy.arn
+# }
+ 
+# resource "aws_s3_bucket_policy" "chaps_lb_logs_bucket_policy" {
+#   bucket = aws_s3_bucket.chaps_lb_logs.id
+
+#   policy = jsonencode({
+#     Version   = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect    = "Allow"
+#         Principal = {
+#           Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+#         }
+#         Action   = [
+#           "s3:PutObject",
+#           "s3:PutObjectAcl"
+#         ],
+#         Resource = "${aws_s3_bucket.chaps_lb_logs.arn}/*"
+#         Condition = {
+#           StringEquals = {
+#             "s3:x-amz-acl" = "bucket-owner-full-control"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
+#==========================================================================================
+
+module "lb_access_logs_enabled" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer?ref=6f59e1ce47df66bc63ee9720b7c58993d1ee64ee"
+  providers = {
+    aws.bucket-replication = aws
   }
-
-  egress {
-    description = "Open all outbound ports"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "chaps_lb" {
-  name               = "chaps-load-balancer"
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.chaps_lb_sc.id]
-  subnets            = data.aws_subnets.shared-public.ids
-  idle_timeout       = 60
-
-  access_logs {
-    bucket  = aws_s3_bucket.chaps_lb_logs.bucket
-    prefix  = "chaps_lb"
-    enabled = true
-  }
+  vpc_all                    = "${local.vpc_name}-${local.environment}"
+  force_destroy_bucket       = true # enables destruction of logging bucket
+  application_name           = local.application_name
+  public_subnets             = data.aws_subnets.shared-public.ids
+  loadbalancer_ingress_rules = local.loadbalancer_ingress_rules
+  loadbalancer_egress_rules  = local.loadbalancer_egress_rules
+  account_number             = local.environment_management.account_ids[terraform.workspace]
+  region                     = "eu-west-2"
+  enable_deletion_protection = false
+  idle_timeout               = 60
+  tags                       = {} 
 }
 
 resource "aws_lb_target_group" "chaps_target_group" {
@@ -48,119 +198,25 @@ resource "aws_lb_target_group" "chaps_target_group" {
   }
 
   health_check {
-    healthy_threshold   = "2"
-    interval            = "30"
-    unhealthy_threshold = "5"
+    healthy_threshold   = "5"
+    interval            = "120"
+    protocol            = "HTTP"
+    unhealthy_threshold = "2"
     matcher             = "200-499"
-    timeout             = "10"
-  }
-
-}
-
-resource "aws_lb_listener" "https_listener" {
-  #checkov:skip=CKV_AWS_103
-  depends_on = [aws_acm_certificate_validation.external]
-
-  load_balancer_arn = aws_lb.chaps_lb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate.external.arn
-
-  default_action {
-    target_group_arn = aws_lb_target_group.chaps_target_group.id
-    type             = "forward"
+    timeout             = "5"
   }
 }
 
-resource "aws_s3_bucket" "chaps_lb_logs" {
-  bucket = "chaps-lb-logs-bucket"
-}
+resource "aws_security_group" "chaps_lb_sc" {
+  name        = "load balancer security group"
+  description = "control access to the load balancer"
+  vpc_id      = data.aws_vpc.shared.id
 
-resource "aws_s3_bucket_versioning" "chaps_lb_logs_versioning" {
-  bucket = aws_s3_bucket.chaps_lb_logs.bucket
-
-  versioning_configuration {
-    status = "Enabled"
+  ingress {
+    description = "allow access on HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["188.214.15.75/32", "192.168.5.101/32", "81.134.202.29/32", "79.152.189.104/32", "179.50.12.212/32", "188.172.252.34/32", "194.33.192.0/25", "194.33.193.0/25", "194.33.196.0/25", "194.33.197.0/25", "195.59.75.0/24", "201.33.21.5/32", "213.121.161.112/28", "52.67.148.55/32", "54.94.206.111/32", "178.248.34.42/32", "178.248.34.43/32", "178.248.34.44/32", "178.248.34.45/32", "178.248.34.46/32", "178.248.34.47/32", "89.32.121.144/32", "185.191.249.100/32", "2.138.20.8/32", "18.169.147.172/32", "35.176.93.186/32", "18.130.148.126/32", "35.176.148.126/32", "51.149.250.0/24", "51.149.249.0/29", "194.33.249.0/29", "51.149.249.32/29", "194.33.248.0/29", "20.49.214.199/32", "20.49.214.228/32", "20.26.11.71/32", "20.26.11.108/32", "128.77.75.128/26"]
   }
-}
 
-resource "aws_s3_bucket_lifecycle_configuration" "chaps_lb_logs_lifecycle" {
-  bucket = aws_s3_bucket.chaps_lb_logs.id
-
-  rule {
-    id = "log"
-    status = "Enabled"
-
-    expiration {
-      days = 90
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = 90
-    }
-  }
-}
-
-resource "aws_iam_role" "lb_logging_role" {
-  name = "lb-logging-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = {
-          Service = "elasticloadbalancing.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "lb_logging_policy" {
-  name = "lb-logging-policy"
-  policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:PutObject",
-          "s3:PutObjectAcl"
-        ],
-        Resource = "${aws_s3_bucket.chaps_lb_logs.arn}/*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lb_logging_policy_attachment" {
-  role       = aws_iam_role.lb_logging_role.name
-  policy_arn = aws_iam_policy.lb_logging_policy.arn
-}
- 
-resource "aws_s3_bucket_policy" "chaps_lb_logs_bucket_policy" {
-  bucket = aws_s3_bucket.chaps_lb_logs.id
-
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
-        }
-        Action   = [
-          "s3:PutObject",
-          "s3:PutObjectAcl"
-        ],
-        Resource = "${aws_s3_bucket.chaps_lb_logs.arn}/*"
-        Condition = {
-          StringEquals = {
-            "s3:x-amz-acl" = "bucket-owner-full-control"
-          }
-        }
-      }
-    ]
-  })
-}
