807373997317541b83b809df3c2febc2e7c3ab24
matt-heery <matt.heery@digital.justice.gov.uk>
2024-06-03 17:13:38 +0100

    dependency

--- terraform/environments/electronic-monitoring-data/modules/lambdas/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/lambdas/main.tf
@@ -151,6 +151,7 @@ resource "aws_cloudwatch_log_group" "lambda_cloudwatch_group" {
   name              = "/aws/lambda/${var.function_name}"
   retention_in_days = 400
   kms_key_id = aws_kms_key.cloudwatch_env_key.id
+  depends_on = [aws_kms_key.lambda_env_key]
 }
 
 
