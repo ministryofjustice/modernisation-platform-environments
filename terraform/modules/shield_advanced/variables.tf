56c9caa9a276d381195220c1a32e2351f9c004ae
David Sibley <david.sibley@digital.justice.gov.uk>
2024-06-03 09:38:12 +0100

    removed orphaned variable

--- terraform/modules/shield_advanced/variables.tf
+++ terraform/modules/shield_advanced/variables.tf
@@ -3,12 +3,6 @@ variable "application_name" {
   description = "Name of application being protected."
 }
 
-variable "enable_proactive_engagement" {
-  type        = bool
-  default     = true
-  description = "Should AWS Shield proactive engagement be enabled?"
-}
-
 variable "monitored_resources" {
   type        = map(string)
   description = "A map of names to ARNs for resources to be included by AWS Shield."
