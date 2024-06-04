805b9c25c147dfdeec413fbb6fe43115465b4cb8
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2024-06-04 10:09:26 +0100

    Update Data API Cross Account Role

--- terraform/environments/digital-prison-reporting/cross-account.tf
+++ terraform/environments/digital-prison-reporting/cross-account.tf
@@ -32,128 +32,6 @@ resource "aws_iam_role" "redshift_dataapi_cross_role" {
   )
 }
 
-# Redshift DataAPI Policy Document
-data "aws_iam_policy_document" "redshift_dataapi" {
-  statement {
-    actions = [
-        "redshift-data:ListTables",
-        "redshift-data:DescribeTable",
-        "redshift-data:ListSchemas",
-        "redshift-data:ListDatabases",
-        "redshift-data:ExecuteStatement"
-    ]
-    resources = [
-      "arn:aws:redshift:${local.account_region}:${local.account_id}:cluster:*"
-    ]
-  }
-
-  statement {
-    actions = [
-        "redshift-data:GetStatementResult",
-        "redshift-data:DescribeStatement",
-        "redshift-data:ListStatements"
-    ]
-    resources = [
-      "*"
-    ]
-  }
-
-  statement {
-    actions = [
-        "secretsmanager:GetResourcePolicy",
-        "secretsmanager:GetSecretValue",
-        "secretsmanager:DescribeSecret",
-        "secretsmanager:ListSecretVersionIds"
-    ]
-    resources = [
-      "arn:aws:secretsmanager:${local.account_region}:${local.account_id}:secret:*"
-    ]
-  }
-
-  statement {
-    actions = [
-        "secretsmanager:ListSecrets"
-    ]
-    resources = [
-      "*"
-    ]
-  }    
-
-}
-
-# Athena API Policy Document
-data "aws_iam_policy_document" "athena_api" {
-  statement {
-    actions = [
-      "athena:GetDataCatalog",
-      "athena:GetQueryExecution",
-      "athena:GetQueryResults",
-      "athena:GetWorkGroup",
-      "athena:StartQueryExecution",
-      "athena:StopQueryExecution"
-    ]
-    resources = [
-      "arn:aws:athena:${local.account_region}:${local.account_id}:workgroup/primary"
-    ]
-  }
-
-  statement {
-    actions = [
-      "athena:ListWorkGroups"
-    ]
-    resources = [
-      "*"
-    ]
-  }
-
-  statement {
-    actions = [
-      "lambda:InvokeFunction"
-    ]
-    resources = [
-      "arn:aws:lambda:${local.account_region}:${local.account_id}:function:dpr-athena-federated-query-oracle-function"
-    ]
-  }
-
-  statement {
-    actions = [
-      "s3:AbortMultipartUpload",
-      "s3:GetBucketLocation",
-      "s3:GetObject",
-      "s3:ListBucket",
-      "s3:ListMultipartUploadParts",
-      "s3:PutObject"
-    ]
-    resources = [
-      "arn:aws:s3:::dpr-*/*"
-    ]
-  }
-
-  statement {
-    actions = [
-      "kms:GenerateDataKey"
-    ]
-    resources = [
-      "arn:aws:kms:*:771283872747:key/*"
-    ]
-  }
-
-}
-
-# Redshift DataAPI Policy
-resource "aws_iam_policy" "redshift_dataapi_cross_policy" {
-  name        = "${local.project}-redshift-data-api-cross-policy"
-  description = "Extra Policy for AWS Redshift"
-  policy      = data.aws_iam_policy_document.redshift_dataapi.json
-}
-
-# Athena API Policy
-resource "aws_iam_policy" "athena_api_cross_policy" {
-  name        = "${local.project}-athena-api-cross-policy"
-  description = "Extra Policy for AWS Athena"
-  policy      = data.aws_iam_policy_document.athena_api.json
-}
-
 # Redshift DataAPI Role/Policy Attachement
 resource "aws_iam_role_policy_attachment" "redshift_dataapi" {
   role       = aws_iam_role.redshift_dataapi_cross_role.name
@@ -164,4 +42,10 @@ resource "aws_iam_role_policy_attachment" "redshift_dataapi" {
 resource "aws_iam_role_policy_attachment" "athena_api" {
   role       = aws_iam_role.redshift_dataapi_cross_role.name
   policy_arn = aws_iam_policy.athena_api_cross_policy.arn
+}
+
+# S3 Read Write Policy Attachement
+resource "aws_iam_role_policy_attachment" "athena_api" {
+  role       = aws_iam_role.redshift_dataapi_cross_role.name
+  policy_arn = aws_iam_policy.s3_read_write_policy.arn
 }
