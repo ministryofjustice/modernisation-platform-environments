f7ac2cd196c5e5fd7aec28de3b43a7454cca0b67
George Taylor <george.taylor@digital.justice.gov.uk>
2024-06-05 00:23:12 +0100

    move secret

--- terraform/environments/delius-core/modules/components/ldap/secrets.tf
+++ terraform/environments/delius-core/modules/components/ldap/secrets.tf
@@ -61,3 +61,12 @@ resource "aws_ssm_parameter" "delius_core_ldap_bind_password" {
   }
   tags = var.tags
 }
+
+module "ldap_admin_password" {
+  source              = "../../helpers/secret"
+  name                = "ldap-admin-password"
+  description         = "LDAP Admin Password"
+  tags                = var.tags
+  kms_key_id          = var.account_config.kms_keys.general_shared
+  allowed_account_ids = [var.platform_vars.environment_management.account_ids[join("-", ["delius-nextcloud", var.account_info.mp_environment])]]
+}
