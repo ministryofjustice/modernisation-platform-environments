84ce24fbea599873277788345e245b394a9b73fc
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-06-05 11:59:53 +0100

    Tribunals preprod: add finance and tax tribunal

--- terraform/environments/tribunals/asg-shared.tf
+++ terraform/environments/tribunals/asg-shared.tf
@@ -226,7 +226,7 @@ resource "aws_security_group" "cluster_ec2" {
       module.care_standards.tribunals_lb_sc_id,
       module.cicap.tribunals_lb_sc_id,
       module.employment_appeals.tribunals_lb_sc_id,
-      # module.finance_and_tax.tribunals_lb_sc_id,
+      module.finance_and_tax.tribunals_lb_sc_id,
       # module.immigration_services.tribunals_lb_sc_id,
       # module.information_tribunal.tribunals_lb_sc_id,
       # module.lands_tribunal.tribunals_lb_sc_id,
