d3a0e4420b4ba8b26bdbd8ef9480dfe97746dc38
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-06-05 15:57:06 +0100

    Revert "delete cicap as it is currently broken"
    This reverts commit 12ea4d25ee622ac70c4d01a76a1a72271f78039a.

--- terraform/environments/tribunals/asg-shared.tf
+++ terraform/environments/tribunals/asg-shared.tf
@@ -224,7 +224,7 @@ resource "aws_security_group" "cluster_ec2" {
       module.appeals.tribunals_lb_sc_id,
       module.ahmlr.tribunals_lb_sc_id,
       module.care_standards.tribunals_lb_sc_id,
-      # module.cicap.tribunals_lb_sc_id,
+      module.cicap.tribunals_lb_sc_id,
       module.employment_appeals.tribunals_lb_sc_id,
       # module.finance_and_tax.tribunals_lb_sc_id,
       # module.immigration_services.tribunals_lb_sc_id,
