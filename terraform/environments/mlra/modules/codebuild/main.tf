840d35332ab1bd5f7908076d2c468a2ac79d062d
Alex Stone <alex.stone@digital.justice.gov.uk>
2024-06-05 11:59:01 +0100

    LASB-3291 Fix static analysis warning: Check: CKV_AWS_163: Ensure ECR image scanning on push is enabled

--- terraform/environments/mlra/modules/codebuild/main.tf
+++ terraform/environments/mlra/modules/codebuild/main.tf
@@ -54,7 +54,7 @@ resource "aws_ecr_repository" "local-ecr" {
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = false
+    scan_on_push = true
   }
 
   tags = merge(
