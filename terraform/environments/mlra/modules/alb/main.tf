cdd8fd7acd93bbc6085af66b0cf64142976b899a
Alex Stone <alex.stone@digital.justice.gov.uk>
2024-06-05 17:02:13 +0100

    LASB-3291 Fix static analysis warning: You should not use outdated/insecure TLS versions for encryption. You should be using TLS v1.2+.

--- terraform/environments/mlra/modules/alb/main.tf
+++ terraform/environments/mlra/modules/alb/main.tf
@@ -435,7 +435,7 @@ resource "aws_cloudfront_distribution" "external" {
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate.cloudfront.arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2018"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   logging_config {
