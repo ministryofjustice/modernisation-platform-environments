faf4f46169dd3e1c9264f4409582022de6e91093
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2024-06-05 13:27:13 +0100

    Add Analytics K8s Cluster ID to Secrets

--- terraform/environments/digital-prison-reporting/data.tf
+++ terraform/environments/digital-prison-reporting/data.tf
@@ -71,10 +71,17 @@ data "aws_secretsmanager_secret_version" "pagerduty_integration" {
 
 # Source Analytics DBT Secrets
 data "aws_secretsmanager_secret" "dbt_secrets" {
-  name = aws_secretsmanager_secret.dbt_secrets.id
+  name = aws_secretsmanager_secret.dbt_secrets[0].id
 
   depends_on = [aws_secretsmanager_secret_version.dbt_secrets]
 }
+
+data "aws_secretsmanager_secret_version" "dbt_secrets" {
+  secret_id = data.aws_secretsmanager_secret.dbt_secrets.id
+
+  depends_on = [aws_secretsmanager_secret.dbt_secrets]
+}
+
 
 # TLS Certificate for OIDC URL, DBT K8s Platform 
 data "tls_certificate" "dbt_analytics" {
