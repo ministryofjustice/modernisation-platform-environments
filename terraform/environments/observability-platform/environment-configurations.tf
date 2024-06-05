bd8ad0a8352222c0663df0fc1bb9a5f2b25f0f7d
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-06-05 15:18:22 +0100

    Onboard core-network-services-production

--- terraform/environments/observability-platform/environment-configurations.tf
+++ terraform/environments/observability-platform/environment-configurations.tf
@@ -92,6 +92,17 @@ locals {
               xray_enabled                    = true
             }
           }
+        },
+        "modernisation-platform" = {
+          identity_centre_team = "modernisation-platform"
+          aws_accounts = {
+            "core-network-services-production" = {
+              cloudwatch_enabled              = true
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = false
+            }
+          }
         }
       }
       grafana_api_key_rotator_version = "1.0.5"
