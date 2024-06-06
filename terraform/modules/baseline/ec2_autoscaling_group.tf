47ce831d9050785db62c3029534ca6eb104fb06a
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-06-06 15:45:08 +0100

    DSOS-2838: update ASG module version (#6460)

--- terraform/modules/baseline/ec2_autoscaling_group.tf
+++ terraform/modules/baseline/ec2_autoscaling_group.tf
@@ -15,7 +15,7 @@ module "ec2_autoscaling_group" {
 
   for_each = var.ec2_autoscaling_groups
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-autoscaling-group?ref=7aafc2df99aff8b4750163a4ca5509ac79dd86f3" # DSOS-2838/enable-auto-instance-refresh
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-autoscaling-group?ref=7174e5845bb089349080fe3fa093d74650a22ca9" # v2.5.5
 
   providers = {
     aws.core-vpc = aws.core-vpc
