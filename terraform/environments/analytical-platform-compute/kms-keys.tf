45eedc62665f8ad6eab9de63a6cef54070b5ec19
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-06-10 15:44:33 +0000

    Add KMS and S3
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/kms-keys.tf
+++ terraform/environments/analytical-platform-compute/kms-keys.tf
@@ -204,3 +204,19 @@ module "mlflow_rds_kms" {
 
   tags = local.tags
 }
+
+module "mlflow_s3_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.0.0"
+
+  aliases               = ["s3/mlflow"]
+  description           = "MLflow S3 KMS key"
+  enable_default_policy = true
+
+  deletion_window_in_days = 7
+
+  tags = local.tags
+}
