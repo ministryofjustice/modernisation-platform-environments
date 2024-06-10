1106795cd7175491d0c33c0a8a85fd033d837c2e
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-06-10 22:46:03 +0100

    fix vers
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/edw/event_rules.tf
+++ terraform/environments/edw/event_rules.tf
@@ -1,49 +1,41 @@
-resource "aws_cloudwatch_event_rule" "create_db_snapshots" {
+resource "aws_cloudwatch_event_rule" "snapshotDBFunctionmon_sun" {
 
-  name                = "${local.application_name}-${local.environment}-create-db-snapshots"
+  name                = "laa-createSnapshotRule-${local.application_name}-${local.environment}-mp"
   description         = "Daily snapshots of Oracle volumes"
-  schedule_expression = "cron(10 2 ? * MON-FRI *)"
-  tags = merge(
-    local.tags,
-    { Name = "${local.application_name}-${local.environment}-create-db-snapshots" }
-  )
+  schedule_expression = "cron(00 04 ? * MON-SUN *)"
+
 
 }
-
-resource "aws_lambda_permission" "create_db_snapshots" {
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_mon_sun" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.create_db_snapshots.function_name
+  function_name = aws_lambda_function.snapshotDBFunction.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.create_db_snapshots.arn
+  source_arn    = aws_cloudwatch_event_rule.snapshotDBFunctionmon_sun.arn
 }
 
-resource "aws_cloudwatch_event_target" "create_db_snapshots" {
-  rule  = aws_cloudwatch_event_rule.create_db_snapshots.name
-  arn   = aws_lambda_function.create_db_snapshots.arn
-  input = jsonencode({ "appname" : "${local.application_data.accounts[local.environment].database_ec2_name}" })
+resource "aws_cloudwatch_event_target" "snapshotDBFunctioncheck_mon_sun" {
+  rule  = aws_cloudwatch_event_rule.snapshotDBFunctionmon_sun.name
+  arn   = aws_lambda_function.snapshotDBFunction.arn
+  input = jsonencode({ "appname" : "apex Database Server" })
 }
 
-resource "aws_cloudwatch_event_rule" "delete_db_snapshots" {
-  name                = "${local.application_name}-${local.environment}-delete-db-snapshots"
+resource "aws_cloudwatch_event_rule" "deletesnapshotFunction_mon_fri" {
+  name                = "laa-deletesnapshotRule-${local.application_name}-${local.environment}-mp"
   description         = "Delete snapshots over 35 days old"
-  schedule_expression = "cron(10 2 ? * MON-FRI *)"
-
-  tags = merge(
-    local.tags,
-    { Name = "${local.application_name}-${local.environment}-delete-db-snapshots" }
-  )
+  schedule_expression = "cron(10 02 ? * MON-FRI *)"
 }
 
-resource "aws_lambda_permission" "delete_db_snapshots" {
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_mon_fri" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.delete_db_snapshots.function_name
+  function_name = aws_lambda_function.deletesnapshotFunction.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.delete_db_snapshots.arn
+  source_arn    = aws_cloudwatch_event_rule.deletesnapshotFunction_mon_fri.arn
 }
 
-resource "aws_cloudwatch_event_target" "delete_db_snapshots" {
-  rule  = aws_cloudwatch_event_rule.delete_db_snapshots.name
-  arn   = aws_lambda_function.delete_db_snapshots.arn
+resource "aws_cloudwatch_event_target" "deletesnapshotFunctioncheck_mon_fri" {
+  rule  = aws_cloudwatch_event_rule.deletesnapshotFunction_mon_fri.name
+  arn   = aws_lambda_function.deletesnapshotFunction.arn
+  input = jsonencode({ "appname" : "EDW Database Server" })
 }
