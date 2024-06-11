12a4b3f36f3c3b8ee8f42a23ea9a1ee36c5bdcc9
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-06-11 14:03:36 +0100

    restore db, no encryption

--- terraform/environments/cdpt-ifs/database.tf
+++ terraform/environments/cdpt-ifs/database.tf
@@ -17,7 +17,6 @@ resource "aws_db_instance" "database" {
   db_subnet_group_name      = aws_db_subnet_group.db.id
   final_snapshot_identifier = "final-snapshot-${formatdate("YYYYMMDDhhmmss", timestamp())}"
   publicly_accessible       = false
-  storage_encrypted         = true
 }
 
 resource "aws_db_subnet_group" "db" {
