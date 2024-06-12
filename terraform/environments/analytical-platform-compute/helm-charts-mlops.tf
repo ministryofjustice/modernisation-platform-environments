3293e54994934f430c5931ac05eef31a4ef41849
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-06-12 10:24:48 +0000

    ...
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/helm-charts-mlops.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-mlops.tf
@@ -7,7 +7,7 @@ resource "helm_release" "mlflow" {
   namespace  = kubernetes_namespace.mlflow.metadata[0].name
   values = [
     templatefile(
-      "${path.module}/src/helm/values/actions-runners/mlflow/values.yml.tftpl",
+      "${path.module}/src/helm/values/mlflow/values.yml.tftpl",
       {
         mlflow_hostname = "mlflow.${local.environment_configuration.route53_zone}"
         eks_role_arn    = module.mlflow_iam_role.iam_role_arn
