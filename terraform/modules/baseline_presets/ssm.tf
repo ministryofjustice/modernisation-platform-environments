cec735b96423fe15e7cf959c82d48614fb3e1b7b
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-06-12 14:01:15 +0100

    fix

--- terraform/modules/baseline_presets/ssm.tf
+++ terraform/modules/baseline_presets/ssm.tf
@@ -17,7 +17,7 @@ locals {
   ssm_documents_filter = flatten([
     var.options.enable_hmpps_domain ? ["ec2-ad-join-windows"] : [],
     var.options.enable_hmpps_domain ? ["ec2-ad-leave-windows"] : [],
-    var.options.enable_ec2_self_provision ? ["ec2-configuration-management.yaml"] : [],
+    var.options.enable_ec2_self_provision ? ["ec2-configuration-management"] : [],
   ])
 
   ssm_documents = {
