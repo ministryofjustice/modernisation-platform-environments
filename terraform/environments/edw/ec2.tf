d2493ddf7147e5163ccdf0b9de43571c022b1600
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-06-13 10:14:44 +0100

    LAWS-3843 - attempting route63 fix
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/edw/ec2.tf
+++ terraform/environments/edw/ec2.tf
@@ -75,14 +75,32 @@ resource "aws_iam_instance_profile" "edw_ec2_instance_profile" {
 
 ####### DB Instance #######
 
+
+# resource "aws_instance" "apex_db_instance" {
+#   ami                         = local.application_data.accounts[local.environment].ec2amiid
+#   associate_public_ip_address = false
+#   availability_zone           = "eu-west-2a"
+#   ebs_optimized               = true
+#   instance_type               = local.application_data.accounts[local.environment].ec2instancetype
+#   vpc_security_group_ids      = [aws_security_group.database.id]
+#   monitoring                  = true
+#   subnet_id                   = data.aws_subnet.data_subnets_a.id
+#   iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.id
+#   key_name                    = aws_key_pair.apex.key_name
+#   user_data_base64            = base64encode(local.database-instance-userdata)
+#   user_data_replace_on_change = true
+
+
+
 resource "aws_instance" "edw_db_instance" {
   ami                    = local.application_data.accounts[local.environment].edw_ec2_ami_id
+  availability_zone      = "eu-west-2a"
   instance_type          = local.application_data.accounts[local.environment].edw_ec2_instance_type
   iam_instance_profile   = aws_iam_instance_profile.edw_ec2_instance_profile.id
   # ADD AFTER BASTION
   # key_name               = local.application_data.accounts[local.environment].edw_ssh_key_name
   subnet_id              = data.aws_subnet.private_subnets_a.id
-  security_groups        = [aws_security_group.edw_db_security_group.id]
+  vpc_security_group_ids  = [aws_security_group.edw_db_security_group.id]
   user_data = base64encode(templatefile("edw-ec2-user-data.sh", {
     edw_app_name = local.application_data.accounts[local.environment].edw_AppName
     edw_dns_extension = local.application_data.accounts[local.environment].edw_dns_extension
@@ -286,8 +304,8 @@ resource "aws_security_group" "edw_db_security_group" {
 
 resource "aws_route53_record" "edw_internal_dns_record" {
   provider = aws.core-vpc
-  zone_id = data.aws_route53_zone.inner.zone_id
-  name    = "${local.application_name}.${data.aws_route53_zone.inner.name}"
+  zone_id = data.aws_route53_zone.external.zone_id
+  name    = "${local.application_name}.${data.aws_route53_zone.external.name}"
   type    = "A"
   ttl     = 900
   records = [aws_instance.edw_db_instance.private_ip]
