96fb1e40dd8432afdfe54f5362371a694112d503
matt-heery <matt.heery@digital.justice.gov.uk>
2024-06-14 16:27:27 +0100

    id to arn

--- terraform/environments/electronic-monitoring-data/step_functions_iam.tf
+++ terraform/environments/electronic-monitoring-data/step_functions_iam.tf
@@ -175,8 +175,8 @@ data "aws_iam_policy_document" "send_database_to_ap_athena_queries" {
     resources = [
       "arn:aws:s3:::em-athena-result-output",
       "arn:aws:s3:::em-athena-result-output/*",
-      "${aws_s3_bucket.dms_dv_parquet_s3_bucket.id}/*",
-      aws_s3_bucket.dms_dv_parquet_s3_bucket.id
+      "${aws_s3_bucket.dms_dv_parquet_s3_bucket.arn}/*",
+      aws_s3_bucket.dms_dv_parquet_s3_bucket.arn
     ]
   }
   statement {
