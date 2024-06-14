3d6fed5c92815fa669c4d162c10f41a83d78cc02
Andrew Moore <andrew.moore@digital.justice.gov.uk>
2024-06-14 10:33:13 +0100

    chore: Update AWS CloudWatch log group name in ldap/cloudwatch.tf

--- terraform/environments/delius-core/modules/components/ldap/cloudwatch.tf
+++ terraform/environments/delius-core/modules/components/ldap/cloudwatch.tf
@@ -4,6 +4,6 @@ resource "aws_cloudwatch_log_group" "ldap_ecs" {
 }
 
 resource "aws_cloudwatch_log_group" "ldap_automation" {
-  name              = "/ecs/ldap-automation"
+  name              = "/ecs/ldap-automation_${var.env_name}"
   retention_in_days = 5
 }
