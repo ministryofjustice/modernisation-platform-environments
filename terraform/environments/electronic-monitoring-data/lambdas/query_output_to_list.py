f6270e4cef6dfa9a0138217aa38775a70c14ba6e
matt-heery <matt.heery@digital.justice.gov.uk>
2024-06-14 16:21:15 +0100

    remove result set

--- terraform/environments/electronic-monitoring-data/lambdas/query_output_to_list.py
+++ terraform/environments/electronic-monitoring-data/lambdas/query_output_to_list.py
@@ -1,4 +1,4 @@
 def handler(event, context):
-    data = event["ResultSet"]["Rows"][1:]
+    data = event["Rows"][1:]
     output_list = [{row["Data"][0]["VarCharValue"]: row["Data"][1]["VarCharValue"]} for row in data]
     return output_list
