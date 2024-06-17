bdb901dcfe8e21baf0d976742518ac417488eb00
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2024-06-17 19:52:38 +0100

    DPR2-713: Rework replay pipeline to support AP integration

--- terraform/environments/digital-prison-reporting/modules/domains/replay-pipeline/pipeline.tf
+++ terraform/environments/digital-prison-reporting/modules/domains/replay-pipeline/pipeline.tf
@@ -10,8 +10,31 @@ module "replay_pipeline" {
   definition = jsonencode(
     {
       "Comment" : "Replay Pipeline Step Function",
-      "StartAt" : "Stop DMS Replication Task",
+      "StartAt" : "Deactivate Archive Trigger",
       "States" : {
+        "Deactivate Archive Trigger" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_trigger_activation_job,
+            "Arguments" : {
+              "--dpr.glue.trigger.name" : var.archive_job_trigger_name,
+              "--dpr.glue.trigger.activate" : "false"
+            }
+          },
+          "Next" : "Stop Archive Job"
+        },
+        "Stop Archive Job" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_stop_glue_instance_job,
+            "Arguments" : {
+              "--dpr.stop.glue.instance.job.name" : var.glue_archive_job
+            }
+          },
+          "Next" : "Stop DMS Replication Task"
+        },
         "Stop DMS Replication Task" : {
           "Type" : "Task",
           "Resource" : "arn:aws:states:::glue:startJobRun.sync",
@@ -19,6 +42,17 @@ module "replay_pipeline" {
             "JobName" : var.stop_dms_task_job,
             "Arguments" : {
               "--dpr.dms.replication.task.id" : var.replication_task_id
+            }
+          },
+          "Next" : "Check All Pending Files Have Been Processed"
+        },
+        "Check All Pending Files Have Been Processed" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun",
+          "Parameters" : {
+            "JobName" : var.glue_unprocessed_raw_files_check_job,
+            "Arguments" : {
+              "--dpr.orchestration.wait.interval.seconds" : "60"
             }
           },
           "Next" : "Stop Glue Streaming Job"
@@ -83,9 +117,24 @@ module "replay_pipeline" {
               "--dpr.config.key" : var.domain
             }
           },
-          "Next" : "Move Archived Data Back to Raw Bucket"
+          "Next" : "Archive Remaining Raw Files"
         },
-        "Move Archived Data Back to Raw Bucket" : {
+        "Archive Remaining Raw Files" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_s3_file_transfer_job,
+            "Arguments" : {
+              "--dpr.file.transfer.source.bucket" : var.s3_raw_bucket_id,
+              "--dpr.file.transfer.destination.bucket" : var.s3_raw_archive_bucket_id,
+              "--dpr.file.transfer.delete.copied.files" : "true",
+              "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
+              "--dpr.config.key" : var.domain
+            }
+          },
+          "Next" : "Copy Archived Data to Raw Bucket"
+        },
+        "Copy Archived Data to Raw Bucket" : {
           "Type" : "Task",
           "Resource" : "arn:aws:states:::glue:startJobRun.sync",
           "Parameters" : {
@@ -94,7 +143,7 @@ module "replay_pipeline" {
               "--dpr.file.transfer.source.bucket" : var.s3_raw_archive_bucket_id,
               "--dpr.file.transfer.destination.bucket" : var.s3_raw_bucket_id,
               "--dpr.file.transfer.retention.period.amount" : "0",
-              "--dpr.file.transfer.delete.copied.files" : "true",
+              "--dpr.file.transfer.delete.copied.files" : "false",
               "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
               "--dpr.config.key" : var.domain
             }
@@ -111,15 +160,39 @@ module "replay_pipeline" {
               "--dpr.config.key" : var.domain
             }
           },
-          "Next" : "Switch Hive Tables for Prisons to Curated"
+          "Next" : "Start Glue Streaming Job"
         },
-        "Switch Hive Tables for Prisons to Curated" : {
+        "Start Glue Streaming Job" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun",
+          "Parameters" : {
+            "JobName" : var.glue_reporting_hub_cdc_jobname,
+            "Arguments" : {
+              "--dpr.clean.cdc.checkpoint" : "true",
+              "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
+              "--dpr.config.key" : var.domain
+            }
+          },
+          "Next" : "Check All Files Have Been Replayed"
+        },
+        "Check All Files Have Been Replayed" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun",
+          "Parameters" : {
+            "JobName" : var.glue_unprocessed_raw_files_check_job,
+            "Arguments" : {
+              "--dpr.orchestration.wait.interval.seconds" : "60"
+            }
+          },
+          "Next" : "Truncate Raw Data"
+        },
+        "Truncate Raw Data" : {
           "Type" : "Task",
           "Resource" : "arn:aws:states:::glue:startJobRun.sync",
           "Parameters" : {
-            "JobName" : var.glue_switch_prisons_hive_data_location_job,
+            "JobName" : var.glue_s3_data_deletion_job,
             "Arguments" : {
-              "--dpr.prisons.data.switch.target.s3.path" : "s3://${var.s3_curated_bucket_id}",
+              "--dpr.file.deletion.buckets" : var.s3_raw_bucket_id,
               "--dpr.config.key" : var.domain
             }
           },
@@ -132,17 +205,28 @@ module "replay_pipeline" {
             "ReplicationTaskArn" : var.dms_replication_task_arn,
             "StartReplicationTaskType" : "resume-processing"
           },
-          "Next" : "Start Glue Streaming Job"
+          "Next" : "Switch Hive Tables for Prisons to Curated"
         },
-        "Start Glue Streaming Job" : {
+        "Switch Hive Tables for Prisons to Curated" : {
           "Type" : "Task",
-          "Resource" : "arn:aws:states:::glue:startJobRun",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
           "Parameters" : {
-            "JobName" : var.glue_reporting_hub_cdc_jobname,
+            "JobName" : var.glue_switch_prisons_hive_data_location_job,
             "Arguments" : {
-              "--dpr.clean.cdc.checkpoint" : "true",
-              "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
+              "--dpr.prisons.data.switch.target.s3.path" : "s3://${var.s3_curated_bucket_id}",
               "--dpr.config.key" : var.domain
+            }
+          },
+          "Next" : "Reactivate Archive Trigger"
+        },
+        "Reactivate Archive Trigger" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_trigger_activation_job,
+            "Arguments" : {
+              "--dpr.glue.trigger.name" : var.archive_job_trigger_name,
+              "--dpr.glue.trigger.activate" : "true"
             }
           },
           "Next" : "Empty Temp Reload Bucket Data"
