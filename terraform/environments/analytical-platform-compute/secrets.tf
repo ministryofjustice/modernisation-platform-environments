15543370ad2e8452a5fb033d25cc4a2658031e17
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-06-17 17:47:01 +0000

    remove conditional
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/secrets.tf
+++ terraform/environments/analytical-platform-compute/secrets.tf
@@ -25,8 +25,6 @@ module "ui_sentry_dsn_secret" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
-
   source  = "terraform-aws-modules/secrets-manager/aws"
   version = "1.1.2"
 
