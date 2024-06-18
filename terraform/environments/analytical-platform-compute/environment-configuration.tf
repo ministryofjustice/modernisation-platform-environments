cb0acc81b5553454a0027f5837984b912938ec50
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-06-18 21:49:08 +0000

    Bump EFS CSI
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/environment-configuration.tf
+++ terraform/environments/analytical-platform-compute/environment-configuration.tf
@@ -45,7 +45,7 @@ locals {
         coredns                = "v1.11.1-eksbuild.9"
         kube_proxy             = "v1.30.0-eksbuild.3"
         aws_ebs_csi_driver     = "v1.31.0-eksbuild.1"
-        aws_efs_csi_driver     = "v2.0.3-eksbuild.1"
+        aws_efs_csi_driver     = "v2.0.4-eksbuild.1"
         aws_guardduty_agent    = "v1.6.1-eksbuild.1"
         eks_pod_identity_agent = "v1.3.0-eksbuild.1"
         vpc_cni                = "v1.18.2-eksbuild.1"
