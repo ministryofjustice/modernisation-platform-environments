105dd91f20e686085188366c7e5268750264dea8
matt-heery <matt.heery@digital.justice.gov.uk>
2024-06-18 23:13:38 +0100

    use oidc not user access

--- terraform/environments/electronic-monitoring-data/ecr_iam.tf
+++ terraform/environments/electronic-monitoring-data/ecr_iam.tf
@@ -1,9 +1,49 @@
-# Create IAM user
-resource "aws_iam_user" "ecr_pusher" {
-  name = "ecr-pusher"
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
 }
 
-# Create the least privilege policy for ECR access
+locals {
+    branch = local.is-production ? "main": "*"
+}
+
+data "aws_iam_policy_document" "ecr_assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:ministryofjustice/electronic-monitoring-data-lambda-functions:ref:refs/heads/${local.branch}"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name               = "GitHubActionsECRRole"
+  assume_role_policy = data.aws_iam_policy_document.ecr_assume_role_policy.json
+}
+
 data "aws_iam_policy_document" "ecr_policy" {
   statement {
     actions = [
@@ -19,20 +59,13 @@ data "aws_iam_policy_document" "ecr_policy" {
   }
 }
 
-# Create the IAM policy
 resource "aws_iam_policy" "ecr_policy" {
   name        = "ECRPushPolicy"
   description = "Policy to allow pushing Docker images to ECR"
   policy      = data.aws_iam_policy_document.ecr_policy.json
 }
 
-# Attach the policy to the IAM user
-resource "aws_iam_user_policy_attachment" "ecr_pusher_policy_attachment" {
-  user       = aws_iam_user.ecr_pusher.name
+resource "aws_iam_role_policy_attachment" "gh_actions_policy_attachment" {
+  role       = aws_iam_role.github_actions_role.name
   policy_arn = aws_iam_policy.ecr_policy.arn
-}
-
-# Create access keys for the IAM user (optional)
-resource "aws_iam_access_key" "ecr_pusher_access_key" {
-  user = aws_iam_user.ecr_pusher.name
 }
