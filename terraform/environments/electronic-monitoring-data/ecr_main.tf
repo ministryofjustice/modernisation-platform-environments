52c1936efb0a8d41062da1fe74c64df022d59d0c
matt-heery <matt.heery@digital.justice.gov.uk>
2024-06-19 16:56:03 +0100

    two repos

--- terraform/environments/electronic-monitoring-data/ecr_main.tf
+++ terraform/environments/electronic-monitoring-data/ecr_main.tf
@@ -1,4 +1,9 @@
 module "ecr_lambda_repo" {
     source = "./modules/ecr"
+    ecr_name = "lambdas/update_log_table"
+}
+
+module "ecr_lambdas_repo" {
+    source = "./modules/ecr"
     ecr_name = "lambdas/lambda-functions-repo"
 }
