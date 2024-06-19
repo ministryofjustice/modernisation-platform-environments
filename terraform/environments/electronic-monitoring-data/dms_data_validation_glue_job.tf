b08f83736afa26dfc0764600cf633f726737a669
Madhu Kadiri <madhu.kadiri2@justice.gov.uk>
2024-06-19 14:18:00 +0100

    DV_GlueJobV3_Added

--- terraform/environments/electronic-monitoring-data/dms_data_validation_glue_job.tf
+++ terraform/environments/electronic-monitoring-data/dms_data_validation_glue_job.tf
@@ -59,6 +59,12 @@ resource "aws_s3_object" "dms_dv_glue_job_s3_object_v2" {
   etag   = filemd5("glue-job/dms_dv_rds_and_s3_parquet_write_v2.py")
 }
 
+resource "aws_s3_object" "dms_dv_glue_job_s3_object_v3" {
+  bucket = aws_s3_bucket.dms_dv_glue_job_s3_bucket.id
+  key    = "dms_dv_rds_and_s3_parquet_write_v3.py"
+  source = "glue-job/dms_dv_rds_and_s3_parquet_write_v3.py"
+  etag   = filemd5("glue-job/dms_dv_rds_and_s3_parquet_write_v3.py")
+}
 resource "aws_s3_object" "catalog_dv_table_glue_job_s3_object" {
   bucket = aws_s3_bucket.dms_dv_glue_job_s3_bucket.id
   key    = "create_or_replace_dv_table.py"
@@ -139,7 +145,7 @@ resource "aws_glue_job" "dms_dv_glue_job_v2" {
   role_arn          = aws_iam_role.dms_dv_glue_job_iam_role.arn
   glue_version      = "4.0"
   worker_type       = "G.1X"
-  number_of_workers = 5
+  number_of_workers = 8
   default_arguments = {
     "--script_bucket_name"               = "${aws_s3_bucket.dms_dv_glue_job_s3_bucket.id}"
     "--rds_db_host_ep"                   = split(":", aws_db_instance.database_2022.endpoint)[0]
@@ -182,6 +188,51 @@ resource "aws_glue_job" "dms_dv_glue_job_v2" {
 
 }
 
+resource "aws_glue_job" "dms_dv_glue_job_v3" {
+  name              = "dms-dv-glue-job-v3"
+  description       = "DMS Data Validation Glue-Job (PySpark)."
+  role_arn          = aws_iam_role.dms_dv_glue_job_iam_role.arn
+  glue_version      = "4.0"
+  worker_type       = "G.2X"
+  number_of_workers = 8
+  default_arguments = {
+    "--script_bucket_name"                 = "${aws_s3_bucket.dms_dv_glue_job_s3_bucket.id}"
+    "--rds_db_host_ep"                     = split(":", aws_db_instance.database_2022.endpoint)[0]
+    "--rds_db_pwd"                         = aws_db_instance.database_2022.password
+    "--rds_sqlserver_db"                   = ""
+    "--rds_sqlserver_db_schema"            = ""
+    "--rds_sqlserver_db_table"             = ""
+    "--rds_db_tbl_primary_key_list"        = ""
+    "--rds_df_trim_str_col_list"           = ""
+    "--repartition_factor"                 = 8
+    "--parquet_src_bucket_name"            = aws_s3_bucket.dms_target_ep_s3_bucket.id
+    "--parquet_output_bucket_name"         = aws_s3_bucket.dms_dv_parquet_s3_bucket.id
+    "--glue_catalog_db_name"               = "${aws_glue_catalog_database.dms_dv_glue_catalog_db.name}"
+    "--glue_catalog_tbl_name"              = "glue_df_output"
+    "--continuous-log-logGroup"            = "/aws-glue/jobs/${aws_cloudwatch_log_group.dms_dv_cw_log_group.name}"
+    "--enable-continuous-cloudwatch-log"   = "true"
+    "--enable-continuous-log-filter"       = "true"
+    "--enable-spark-ui"                    = "true"
+    "--spark-event-logs-path"              = "s3://${aws_s3_bucket.dms_dv_glue_job_s3_bucket.id}/spark_logs/"
+    "--enable-metrics"                     = "true"
+    "--enable-auto-scaling"                = "true"
+    "--conf"                               = "spark.memory.offHeap.enabled=true --conf spark.memory.offHeap.size=1g --conf spark.sql.adaptive.enabled=true --conf spark.sql.adaptive.coalescePartitions.enabled=true --conf spark.sql.adaptive.skewJoin.enabled=true --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=CORRECTED"
+  }
+
+  connections = ["${aws_glue_connection.glue_rds_sqlserver_db_connection.name}"]
+  command {
+    python_version  = "3"
+    script_location = "s3://${aws_s3_bucket.dms_dv_glue_job_s3_bucket.id}/dms_dv_rds_and_s3_parquet_write_v3.py"
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Resource_Type = "Glue-Job that processes data sourced from both RDS and S3",
+    }
+  )
+
+}
 
 resource "aws_glue_job" "catalog_dv_table_glue_job" {
   name              = "catalog-dv-table-glue-job"
