e15d13f16ca906772755d04b2ce35a124db1194f
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-06-20 14:20:41 +0100

    create to run a plan

--- terraform/environments/oasys-national-reporting/locals_development.tf
+++ terraform/environments/oasys-national-reporting/locals_development.tf
@@ -60,15 +60,15 @@ locals {
     }
 
     ec2_instances = {
-      #   dev-web-a = merge(local.defaults_web_ec2,
-      #   {
-      #     config = merge(local.defaults_web_ec2.config, {
-      #       availability_zone = "eu-west-2a"
-      #     })
-      #     instance = merge(local.defaults_web_ec2.instance, {
-      #       instance_type = "t3.large"
-      #     })
-      #   })
+        dev-web-a = merge(local.defaults_web_ec2,
+        {
+          config = merge(local.defaults_web_ec2.config, {
+            availability_zone = "eu-west-2a"
+          })
+          instance = merge(local.defaults_web_ec2.instance, {
+            instance_type = "t3.large"
+          })
+        })
       #   dev-boe-a = merge(local.defaults_boe_ec2,
       #   {
       #     config = merge(local.defaults_boe_ec2.config, {
