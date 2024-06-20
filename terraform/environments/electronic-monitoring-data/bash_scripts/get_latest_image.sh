a54b360bc908a32bbb5c047b962957d5ab8e8e84
matt-heery <matt.heery@digital.justice.gov.uk>
2024-06-20 11:44:20 +0100

    json output

--- terraform/environments/electronic-monitoring-data/bash_scripts/get_latest_image.sh
+++ terraform/environments/electronic-monitoring-data/bash_scripts/get_latest_image.sh
@@ -5,4 +5,10 @@ FUNCTION_NAME=$2
 
 LATEST_IMAGE=$(aws ecr describe-images --repository-name "$REPO_NAME" --query "sort_by(imageDetails,& imagePushedAt)[*].{ImageURI:imageUri,Tag:ImageTags[0]}" --output json | jq -r --arg FUNCTION_NAME "$FUNCTION_NAME" '[.[] | select(.Tag | startswith($FUNCTION_NAME))] | last | .ImageURI')
 
-echo $LATEST_IMAGE
+# Check if LATEST_IMAGE is empty
+if [ -z "$LATEST_IMAGE" ]; then
+  echo "{\"error\": \"No image found for function name $FUNCTION_NAME\"}"
+  exit 1
+fi
+
+echo "{\"latest_image_uri\": \"$LATEST_IMAGE\"}"
