13beb551dd84d050b97142b515b9d5c85091e907
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-06-20 10:13:30 +0100

    rename db secret

--- terraform/environments/cdpt-ifs/secrets.tf
+++ terraform/environments/cdpt-ifs/secrets.tf
@@ -7,7 +7,7 @@ resource "random_password" "password_long" {
   special = false
 }
 
-resource "aws_secretsmanager_secret_version" "db_password" {
+resource "aws_secretsmanager_secret_version" "dev_db_password" {
   secret_id     = aws_secretsmanager_secret.db_password.id
   secret_string = random_password.password_long.result
 }
