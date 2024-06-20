92281e31b98e4984aab21ddfadbd8aae340f5e1f
matt-heery <matt.heery@digital.justice.gov.uk>
2024-06-20 13:42:50 +0100

    edited bash script

--- terraform/environments/electronic-monitoring-data/bash_scripts/get_latest_image.sh
+++ terraform/environments/electronic-monitoring-data/bash_scripts/get_latest_image.sh
@@ -3,12 +3,7 @@
 REPO_NAME=$1
 FUNCTION_NAME=$2
 
-
-# Fetch image details from ECR
-IMAGE_DETAILS=$(aws ecr describe-images --repository-name "$REPO_NAME" --query "sort_by(imageDetails,& imagePushedAt)[*].{ImageURI:imageUri,Tag:ImageTags[0]}" --output json)
-
-# Filter images by function name and get the latest one
-LATEST_IMAGE=$(echo "$IMAGE_DETAILS" | jq -r --arg FUNCTION_NAME "$FUNCTION_NAME" '[.[] | select(.Tag != null and .Tag | startswith($FUNCTION_NAME))] | last | .ImageURI')
+LATEST_IMAGE=$(aws ecr describe-images --repository-name $REPO_NAME --query "imageDetails[?imageTags[?starts_with(@, '$FUNCTION_NAME')]] | sort_by(@, &imagePushedAt) | reverse(@) | [-1]" --output json | jq -r --arg "$FUNCTION_NAME" cd '.imageTags .[0]')
 
 # Check if LATEST_IMAGE is empty
 if [ -z "$LATEST_IMAGE" ]; then
