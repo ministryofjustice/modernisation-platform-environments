9ea4c39b09c7c021ace498230552148e61448388
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-06-20 10:58:01 +0100

    DPR2-893: Fix typo and add in initial attempt at glue connection security group rules

--- terraform/environments/digital-prison-reporting/operational_datastore.tf
+++ terraform/environments/digital-prison-reporting/operational_datastore.tf
@@ -1,16 +1,50 @@
 resource "aws_glue_connection" "glue_operational_datastore_connection" {
+  count           = (local.environment == "development" ? 1 : 0)
   name            = "${local.project}-operational-datastore-connection"
   connection_type = "JDBC"
 
   connection_properties = {
     # This will be replaced by the details for the real Operational Data Store
     JDBC_CONNECTION_URL = "jdbc:postgresql://dpr2-834-instance-1.cja8lnnvvipo.eu-west-2.rds.amazonaws.com:5432/postgres"
-    SECRET_ID           = data.aws_secretmanager_secret.operational_datastore.name
+    SECRET_ID           = data.aws_secretsmanager_secret.operational_datastore.name
   }
 
   physical_connection_requirements {
     availability_zone = data.aws_subnet.private_subnets_a.availability_zone
     security_group_id_list = [aws_security_group.glue_vpc_access_connection_sg[0].id]
     subnet_id         = data.aws_subnet.private_subnets_a.id
+  }
+}
+
+resource aws_security_group "glue_vpc_access_connection_sg" {
+  count       = (local.environment == "development" ? 1 : 0)
+  name        = "${local.project}-operational-datastore-connection_sg"
+  description = "Security group to allow glue access to Operational Datastore via JDBC Connection"
+  vpc_id      = data.aws_vpc.shared.id
+
+  # TODO Tighten these up if necessary once it works
+  # Allow all traffic in from this security group
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "-1"
+    self      = true
+    description = "Security Group can Ingress to itself on all ports - required by Glue"
+  }
+
+  # Allow all traffic out to this security group
+  egress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "-1"
+    self      = true
+  }
+
+  # Allow all traffic out
+  egress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
