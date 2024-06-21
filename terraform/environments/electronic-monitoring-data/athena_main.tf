24cf2130d1d80ca7badac7ad1f35054f88aa9744
Luke Williams <lawilliams1993@gmail.com>
2024-06-21 17:01:52 +0100

    add initial shell of workgroup

---
+++ terraform/environments/electronic-monitoring-data/athena_main.tf
@@ -0,0 +1,17 @@
+resource "aws_athena_workgroup" "default" {
+  name = "default"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://${module.athena-s3-bucket.bucket}/output/"
+
+      encryption_configuration {
+        encryption_option = "SSE_KMS"
+        kms_key_arn       = aws_kms_key.example.arn
+      }
+    }
+  }
+}
