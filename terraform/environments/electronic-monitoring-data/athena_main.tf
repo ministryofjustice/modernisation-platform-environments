3f292f3dc0ece6a4676161fb57a87b1971506a8a
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-06-21 22:32:45 +0100

    Update terraform/environments/electronic-monitoring-data/athena_main.tf

--- terraform/environments/electronic-monitoring-data/athena_main.tf
+++ terraform/environments/electronic-monitoring-data/athena_main.tf
@@ -50,7 +50,6 @@ resource "aws_athena_workgroup" "default" {
     enforce_workgroup_configuration    = true
     publish_cloudwatch_metrics_enabled = true
 
-    }
 
     result_configuration {
       output_location = "s3://${module.athena-s3-bucket.bucket.id}/output/"
