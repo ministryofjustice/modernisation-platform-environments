8d5b3c357248a1f1820c3e052a0ee5422c3e7fa4
George Taylor <george.taylor@digital.justice.gov.uk>
2024-06-24 16:16:30 +0100

    auto java opts

--- terraform/environments/delius-core/modules/delius_environment/pwm.tf
+++ terraform/environments/delius-core/modules/delius_environment/pwm.tf
@@ -86,7 +86,7 @@ module "pwm" {
       email_smtp_address = "email-smtp.eu-west-2.amazonaws.com"
     })),
     "SECURITY_KEY" = "${base64encode(uuid())}",
-    "JAVA_OPTS"    = "-Xmx1536m -Xms512m"
+    "JAVA_OPTS"    = "-Xmx${ceil(var.delius_microservice_configs.pwm.container_memory * 0.80)}m -Xms${floor(var.delius_microservice_configs.pwm.container_memory * 0.20)}m"
   }
   container_vars_env_specific            = try(var.delius_microservice_configs.pwm.container_vars_env_specific, {})
   ignore_changes_service_task_definition = false
