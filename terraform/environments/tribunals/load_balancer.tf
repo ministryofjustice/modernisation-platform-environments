e31e345f844d0cd4930b062c84a0f534d3ec789f
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-06-24 14:35:22 +0100

    delete depends_on, update the security group reference

--- terraform/environments/tribunals/load_balancer.tf
+++ terraform/environments/tribunals/load_balancer.tf
@@ -1,9 +1,8 @@
 resource "aws_lb" "tribunals_lb" {
   name                       = "tribunals-lb"
   load_balancer_type         = "application"
-  security_groups            = [module.appeals.tribunals_lb_sc_id]
+  security_groups            = [module.tribunal.tribunals_lb_sc_id]
   subnets                    = data.aws_subnets.shared-public.ids
   enable_deletion_protection = false
   internal                   = false
-  depends_on                 = [module.appeals.tribunals_lb_sc]
 }
