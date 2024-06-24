06a5f1708afa7e20f12f4ede90ae74addcfb2609
George Taylor <george.taylor@digital.justice.gov.uk>
2024-06-24 15:27:30 +0100

    raise mem+cpu

--- terraform/environments/delius-core/modules/delius_environment/pwm.tf
+++ terraform/environments/delius-core/modules/delius_environment/pwm.tf
@@ -85,7 +85,8 @@ module "pwm" {
       email_from_address = "no-reply@${aws_ses_domain_identity.pwm.domain}"
       email_smtp_address = "email-smtp.eu-west-2.amazonaws.com"
     })),
-    "SECURITY_KEY" = "${base64encode(uuid())}"
+    "SECURITY_KEY" = "${base64encode(uuid())}",
+    "JAVA_OPTS"    = "-Xmx1536m -Xms512m"
   }
   container_vars_env_specific            = try(var.delius_microservice_configs.pwm.container_vars_env_specific, {})
   ignore_changes_service_task_definition = true
