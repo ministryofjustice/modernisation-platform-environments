14cee9e96ae6363258a7df283e469b79485f4e99
Luke Williams <lawilliams1993@gmail.com>
2024-06-25 12:58:33 +0100

    attempted a change of encryption configuration due to KMS potentially causing problems

--- terraform/environments/electronic-monitoring-data/athena_main.tf
+++ terraform/environments/electronic-monitoring-data/athena_main.tf
@@ -1,62 +1,3 @@
-resource "aws_kms_key" "athena_workspace_result_encryption_key" {
-  description         = "KMS key for encrypting the default Athena Workspace's results"
-  enable_key_rotation = true
-
-  policy = jsonencode(
-    {
-      "Id": "key-default",
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "Enable IAM User Permissions",
-          "Effect": "Allow",
-          "Principal": {
-            "AWS": "arn:aws:iam::${local.env_account_id}:root"
-          },
-          "Action": "kms:*",
-          "Resource": "*"
-        },
-        {
-          "Sid": "Allow Athena use of the key",
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "athena.amazonaws.com"
-          },
-          "Action": [
-            "kms:Encrypt*",
-            "kms:Decrypt*",
-            "kms:ReEncrypt*",
-            "kms:GenerateDataKey*",
-            "kms:Describe*"
-          ],
-          "Resource": "*"
-        },
-        {
-          "Sid" : "Enable log service Permissions",
-          "Effect" : "Allow",
-          "Principal" : {
-            "Service" : "logs.eu-west-2.amazonaws.com"
-          },
-          "Action" : [
-            "kms:Encrypt*",
-            "kms:Decrypt*",
-            "kms:ReEncrypt*",
-            "kms:GenerateDataKey*",
-            "kms:Describe*"
-          ],
-          "Resource" : "*"
-        }
-      ]
-    }
-  )
-  tags = merge(
-    local.tags,
-    {
-      Resource_Type = "KMS key for query result encryption used with default Athena Workgroup",
-    }
-  )
-}
-
 resource "aws_athena_workgroup" "default" {
   name = format("%s-default", local.env_account_id)
   description = "A default Athena workgroup to set query limits and link to the default query location bucket: ${module.athena-s3-bucket.bucket.id}"
@@ -70,8 +11,7 @@ resource "aws_athena_workgroup" "default" {
       output_location = "s3://${module.athena-s3-bucket.bucket.id}/output/"
 
       encryption_configuration {
-        encryption_option = "SSE_KMS"
-        kms_key_arn       = aws_kms_key.athena_workspace_result_encryption_key.arn
+        encryption_option = "SSE_S3"
       }
       acl_configuration {
         s3_acl_option = "BUCKET_OWNER_FULL_CONTROL"
