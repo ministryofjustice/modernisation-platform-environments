ec84f4f41d016ad3f8b0a43d8d8af815243cfedf
Luke Williams <lawilliams1993@gmail.com>
2024-06-25 11:37:12 +0100

    fixed the issue in the jsonencode for wrong service allowance of KMS key

--- terraform/environments/electronic-monitoring-data/athena_main.tf
+++ terraform/environments/electronic-monitoring-data/athena_main.tf
@@ -17,10 +17,10 @@ resource "aws_kms_key" "athena_workspace_result_encryption_key" {
           "Resource": "*"
         },
         {
-          "Sid": "Enable log service Permissions",
+          "Sid": "Allow Athena use of the key",
           "Effect": "Allow",
           "Principal": {
-            "Service": "logs.eu-west-2.amazonaws.com"
+            "Service": "athena.amazonaws.com"
           },
           "Action": [
             "kms:Encrypt*",
@@ -42,8 +42,8 @@ resource "aws_kms_key" "athena_workspace_result_encryption_key" {
   )
 }
 
-resource "aws_athena_workgroup" "em_default" {
-  name = "em_default"
+resource "aws_athena_workgroup" "default" {
+  name = "default"
   description = "A default Athena workgroup to set query limits and link to the default query location bucket: ${module.athena-s3-bucket.bucket.id}"
 
   configuration {
