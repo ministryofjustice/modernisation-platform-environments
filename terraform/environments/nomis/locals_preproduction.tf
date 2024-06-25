ce6749489c5fbf6b2173a4c2738b84348bde84cf
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-06-25 16:32:50 +0100

    DSOS-2682: further scale down of weblogic servers

--- terraform/environments/nomis/locals_preproduction.tf
+++ terraform/environments/nomis/locals_preproduction.tf
@@ -71,8 +71,8 @@ locals {
       # ACTIVE (blue deployment)
       preprod-nomis-web-a = merge(local.ec2_autoscaling_groups.web, {
         autoscaling_group = merge(local.ec2_autoscaling_groups.web.autoscaling_group, {
-          desired_capacity = 2
-          max_size         = 2
+          desired_capacity = 1
+          max_size         = 1
         })
         cloudwatch_metric_alarms = local.cloudwatch_metric_alarms.web
         config = merge(local.ec2_autoscaling_groups.web.config, {
