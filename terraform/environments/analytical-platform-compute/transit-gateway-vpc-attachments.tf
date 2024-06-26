044f93665eb6235c1db66f7e8235ebd45d9bfc49
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-06-26 22:39:42 +0000

    Add ID whoops
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/transit-gateway-vpc-attachments.tf
+++ terraform/environments/analytical-platform-compute/transit-gateway-vpc-attachments.tf
@@ -1,5 +1,5 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "pttp" {
-  transit_gateway_id = data.aws_ec2_transit_gateway.pttp
+  transit_gateway_id = data.aws_ec2_transit_gateway.pttp.id
   vpc_id             = module.vpc.vpc_id
   subnet_ids         = module.vpc.private_subnets
 }
