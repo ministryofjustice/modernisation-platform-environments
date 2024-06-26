7c77f3d79e4208c1dadbc1645519af1613e98453
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-06-26 15:30:40 +0100

    DPR2-894: Add JDBC_DRIVER_CLASS_NAME to glue data connection for operational datastore.

--- terraform/environments/digital-prison-reporting/operational_datastore.tf
+++ terraform/environments/digital-prison-reporting/operational_datastore.tf
@@ -9,8 +9,9 @@ resource "aws_glue_connection" "glue_operational_datastore_connection" {
 
   connection_properties = {
     # This will be replaced by the details for the real Operational Data Store
-    JDBC_CONNECTION_URL = "jdbc:postgresql://dpr2-834-instance-1.cja8lnnvvipo.eu-west-2.rds.amazonaws.com:5432/postgres"
-    SECRET_ID           = data.aws_secretsmanager_secret.operational_datastore[0].name
+    JDBC_CONNECTION_URL    = "jdbc:postgresql://dpr2-834-instance-1.cja8lnnvvipo.eu-west-2.rds.amazonaws.com:5432/postgres"
+    JDBC_DRIVER_CLASS_NAME = "org.postgresql.Driver"
+    SECRET_ID              = data.aws_secretsmanager_secret.operational_datastore[0].name
   }
 
   physical_connection_requirements {
