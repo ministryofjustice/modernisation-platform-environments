b0043ddb63cbebca1588ede5fb8161b09e4d33cf
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2024-06-26 17:28:18 +0100

    DPR2-894: update local var name

--- terraform/environments/digital-prison-reporting/main.tf
+++ terraform/environments/digital-prison-reporting/main.tf
@@ -7,7 +7,7 @@ locals {
   glue_avro_registry           = split("/", module.glue_registry_avro.registry_name)
   shared_log4j_properties_path = "s3://${aws_s3_object.glue_job_shared_custom_log4j_properties.bucket}/${aws_s3_object.glue_job_shared_custom_log4j_properties.key}"
   # We only want to include the connection arg in the dev environment for now
-  glue_job_extra_args = (local.environment == "development" ? {
+  glue_batch_job_extra_args = (local.environment == "development" ? {
     "--dpr.operational.data.store.glue.connection.name" = aws_glue_connection.glue_operational_datastore_connection.name
   } : {})
 }
@@ -123,7 +123,7 @@ module "glue_reporting_hub_batch_job" {
     }
   )
 
-  arguments = merge(glue_job_extra_args, {
+  arguments = merge(glue_batch_job_extra_args, {
     "--extra-jars"                          = local.glue_jobs_latest_jar_location
     "--extra-files"                         = local.shared_log4j_properties_path
     "--class"                               = "uk.gov.justice.digital.job.DataHubBatchJob"
