617b751cbf52a9ecb8be9c39326ec0591376fbed
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-06-26 15:53:13 +0100

    add cicap

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -198,7 +198,7 @@ locals {
     appeals                     = module.appeals
     ahmlr                       = module.ahmlr
     care_standards              = module.care_standards
-    # cicap                       = module.cicap
+    cicap                       = module.cicap
     # employment_appeals          = module.employment_appeals
     # finance_and_tax             = module.finance_and_tax
     # immigration_services        = module.immigration_services
