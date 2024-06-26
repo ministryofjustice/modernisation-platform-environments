bbbfd4e1dea3b0e126eeb336443a51a7a8c7a15b
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-06-26 09:16:03 +0100

    revert last commit

--- terraform/environments/tribunals/ecs-cluster-shared.tf
+++ terraform/environments/tribunals/ecs-cluster-shared.tf
@@ -115,13 +115,13 @@ resource "aws_security_group" "ecs_service" {
   name_prefix = "ecs-service-sg-"
   vpc_id      = data.aws_vpc.shared.id
 
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   description = "Allow traffic on port 80 from load balancer"
-  #   security_groups = [aws_security_group.tribunals_lb_sc.id]
-  # }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "Allow traffic on port 80 from load balancer"
+    security_groups = [aws_security_group.tribunals_lb_sc.id]
+  }
 
   egress {
     from_port   = 0
