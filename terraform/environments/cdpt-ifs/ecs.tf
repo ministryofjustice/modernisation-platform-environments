65abdb26317ce00a825dd76d9c4d325dfa7a2d75
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-06-27 13:39:33 +0100

    revert old lb

--- terraform/environments/cdpt-ifs/ecs.tf
+++ terraform/environments/cdpt-ifs/ecs.tf
@@ -90,8 +90,6 @@ resource "aws_ecs_task_definition" "ifs_task_definition" {
   network_mode             = "awsvpc"
   execution_role_arn       = aws_iam_role.app_execution.arn
   task_role_arn            = aws_iam_role.app_task.arn
-  cpu                      = "2048"
-  memory                   = "2048"
   container_definitions = jsonencode([
     {
       name      = "${local.application_name}-container"
--- terraform/environments/cdpt-ifs/route53.tf
+++ terraform/environments/cdpt-ifs/route53.tf
@@ -52,10 +52,10 @@ resource "aws_route53_record" "external" {
   type    = "A"
 
   alias {
-    # name                   = aws_lb.ifs_lb.dns_name
-    # zone_id                = aws_lb.ifs_lb.zone_id
-    name                   = module.lb_access_logs_enabled.load_balancer.dns_name
-    zone_id                = module.lb_access_logs_enabled.load_balancer.zone_id
+    name                   = aws_lb.ifs_lb.dns_name
+    zone_id                = aws_lb.ifs_lb.zone_id
+    # name                   = module.lb_access_logs_enabled.load_balancer.dns_name
+    # zone_id                = module.lb_access_logs_enabled.load_balancer.zone_id
     evaluate_target_health = true
   }
 }
@@ -108,10 +108,10 @@ resource "aws_route53_record" "external_prod" {
   type    = "A"
 
   alias {
-    # name                   = aws_lb.ifs_lb.dns_name
-    # zone_id                = aws_lb.ifs_lb.zone_id
-    name                   = module.lb_access_logs_enabled.load_balancer.dns_name
-    zone_id                = module.lb_access_logs_enabled.load_balancer.zone_id
+    name                   = aws_lb.ifs_lb.dns_name
+    zone_id                = aws_lb.ifs_lb.zone_id
+    # name                   = module.lb_access_logs_enabled.load_balancer.dns_name
+    # zone_id                = module.lb_access_logs_enabled.load_balancer.zone_id
     evaluate_target_health = true
   }
 }
