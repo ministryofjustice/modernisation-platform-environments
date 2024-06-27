88227de86f18c5e3d0deeb3d38c0a96a8176bb3a
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-06-27 11:33:34 +0000

    :wrench: Add Airflow Action Runner

--- terraform/environments/analytical-platform-compute/helm-charts-actions-runners.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-actions-runners.tf
@@ -1,3 +1,5 @@
+/* create-a-derived-table */
+
 data "aws_secretsmanager_secret_version" "actions_runners_create_a_derived_table" {
   count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
 
@@ -47,6 +49,38 @@ resource "helm_release" "actions_runner_mojas_create_a_derived_table_dpr" {
         github_token         = data.aws_secretsmanager_secret_version.actions_runners_create_a_derived_table[0].secret_string
         github_runner_labels = "digital-prison-reporting"
         eks_role_arn         = "arn:aws:iam::972272129531:role/dpr-data-api-cross-account-role"
+      }
+    )
+  ]
+}
+
+/* Airflow */
+
+data "aws_secretsmanager_secret_version" "actions_runners_airflow" {
+  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
+
+  secret_id = module.actions_runners_airflow[0].secret_id
+}
+
+resource "helm_release" "actions_runner_mojas_airflow" {
+  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
+
+  /* https://github.com/ministryofjustice/analytical-platform-actions-runner */
+  name       = "actions-runner-mojas-airflow"
+  repository = "oci://ghcr.io/ministryofjustice/analytical-platform-charts"
+  version    = "2.317.0"
+  chart      = "actions-runner"
+  namespace  = kubernetes_namespace.actions_runners[0].metadata[0].name
+  values = [
+    templatefile(
+      "${path.module}/src/helm/values/actions-runners/airflow/values.yml.tftpl",
+      {
+        replicaCount         = 2
+        github_organisation  = "moj-analytical-services"
+        github_repository    = "airflow"
+        github_token         = data.aws_secretsmanager_secret_version.actions_runners_airflow[0].secret_string
+        github_runner_labels = "analytical-platform"
+        eks_role_arn         = "arn:aws:iam::593291632749:role/TBC"
       }
     )
   ]
