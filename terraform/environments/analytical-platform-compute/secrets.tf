2d5e6747d94726b48219bc9daf6c2f4e81227108
Gary <26419401+Gary-H9@users.noreply.github.com>
2024-06-27 14:33:19 +0100

    :wrench: Add Airflow Action Runner (#6772)

--- terraform/environments/analytical-platform-compute/secrets.tf
+++ terraform/environments/analytical-platform-compute/secrets.tf
@@ -21,6 +21,29 @@ module "actions_runners_create_a_derived_table_secret" {
   )
 }
 
+module "actions_runners_airflow" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
+
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.1.2"
+
+  name        = "actions-runners/airflow"
+  description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/3544241"
+
+  secret_string         = "CHANGEME"
+  ignore_secret_changes = true
+
+  tags = merge(
+    local.tags,
+    {
+      "expiry-date" = "2024-10-26"
+    }
+  )
+}
+
 module "ui_sentry_dsn_secret" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
