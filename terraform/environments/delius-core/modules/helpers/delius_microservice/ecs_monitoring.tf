7734fe40cc47e9e122ec764e852c9cc09ad54bed
George Taylor <george.taylor@digital.justice.gov.uk>
2024-06-27 17:54:09 +0100

    Update ecs_monitoring.tf

--- terraform/environments/delius-core/modules/helpers/delius_microservice/ecs_monitoring.tf
+++ terraform/environments/delius-core/modules/helpers/delius_microservice/ecs_monitoring.tf
@@ -74,36 +74,6 @@ resource "aws_cloudwatch_metric_alarm" "memory_over_threshold" {
   }
 }
 
-// log metric filter for error logs in container that contain the phrase "Error in Helpdesk"
-resource "aws_cloudwatch_log_metric_filter" "error" {
-  name           = "${var.name}-${var.env_name}-application-error"
-  pattern        = "Error in Helpdesk"
-  log_group_name = aws_cloudwatch_log_group.ecs.name
-
-  metric_transformation {
-    name          = "ErrorCount"
-    namespace     = "${var.name}Metrics"
-    value         = "1"
-    default_value = "0"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "high_error_volume" {
-  alarm_name          = "${var.name}-${var.env_name}-high-error-count"
-  alarm_description   = "Triggers alarm if there are more than 5 errors in the last 5 minutes"
-  namespace           = "${var.name}Metrics"
-  metric_name         = "ErrorCount"
-  statistic           = "Sum"
-  period              = "300"
-  evaluation_periods  = "1"
-  alarm_actions       = [var.sns_topic_arn]
-  ok_actions          = [var.sns_topic_arn]
-  threshold           = "10"
-  treat_missing_data  = "missing"
-  comparison_operator = "GreaterThanThreshold"
-}
-
-
 resource "aws_cloudwatch_log_metric_filter" "log_error_filter" {
   count          = var.log_error_pattern != "" ? 1 : 0
   log_group_name = aws_cloudwatch_log_group.ecs.name
