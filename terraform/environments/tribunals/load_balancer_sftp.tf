7c5c6a45874f04c5aa2e6093029aaa06724f286d
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-06-28 10:37:53 +0100

    attempt to create 10 NLB listeners mapping to 10 target groups for each sftp tribunal

--- terraform/environments/tribunals/load_balancer_sftp.tf
+++ terraform/environments/tribunals/load_balancer_sftp.tf
@@ -30,12 +30,16 @@ resource "aws_security_group" "tribunals_lb_sc_sftp" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "allow all traffic on port 10022"
-    from_port   = 10022
-    to_port     = 10022
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+//Will need ingress rules for all 10 sftp ports:
+  dynamic "ingress" {
+    for_each = var.sftp_services
+    content {
+      description = "allow all traffic on port ${ingress.value.sftp_port}"
+      from_port   = ingress.value.sftp_port
+      to_port     = ingress.value.sftp_port
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
@@ -48,50 +52,14 @@ resource "aws_security_group" "tribunals_lb_sc_sftp" {
 }
 
 resource "aws_lb_listener" "tribunals_lb_sftp" {
+  for_each          = var.sftp_services
   load_balancer_arn = aws_lb.tribunals_lb_sftp.arn
-  port              = 10022
+  port              = each.value.sftp_port
   protocol          = "TCP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.error_response_tg.arn
-  }
-}
-
-# This TG causes the network load balancer to drop connections or reset them, serving as an error response
-resource "aws_lb_target_group" "error_response_tg" {
-  name     = "error-response-tg"
-  port     = 12345
-  protocol = "TCP"
-  vpc_id   = data.aws_vpc.shared.id
-  target_type = "instance"
-
-  health_check {
-    enabled             = true
-    interval            = 30
-    protocol            = "TCP"
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    timeout             = 10
-    port                = "traffic-port"
-  }
-}
-
-resource "aws_lb_listener_rule" "tribunals_lb_rule_sftp" {
-  for_each = local.listener_header_to_target_group_sftp
-
-  listener_arn = aws_lb_listener.tribunals_lb.arn
-  priority     = index(keys(local.listener_header_to_target_group_sftp), each.key) + 1
-
-  action {
-    type             = "forward"
-    target_group_arn = each.value
-  }
-
-  condition {
-    host_header  {
-      values = ["*${each.key}.*"]
-    }
+    target_group_arn = each.aws_lb_target_group.tribunals_target_group_sftp.arn
   }
 }
 
