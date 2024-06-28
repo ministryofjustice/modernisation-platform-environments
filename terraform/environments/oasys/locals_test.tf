75fe086a38bfc7b617a695826348b70aa7f0f93f
W <willg258@hotmail.co.uk>
2024-06-28 16:44:45 +0100

    Update locals_test.tf

--- terraform/environments/oasys/locals_test.tf
+++ terraform/environments/oasys/locals_test.tf
@@ -154,6 +154,24 @@ locals {
           }
         ]
       }
+
+      Ec2AuditVaultPolicy = {
+        description = "Permissions required for Audit vault ec2"
+        statements = [
+          {
+            effect = "Allow"
+            actions = [
+              "s3:GetObject",
+              "s3:GetObjectTagging",
+              "s3:ListBucket",
+            ]
+            resources = [
+              "arn:aws:s3:::s3-bucket*",
+              "arn:aws:s3:::s3-bucket*/*",
+            ]
+          }
+        ]
+      }
     }
 
     ec2_autoscaling_groups = {
@@ -202,6 +220,11 @@ locals {
 
     ec2_instances = {
       audit-vault = merge(local.audit_vault, {
+        config = merge(local.audit_vault.config, {
+          instance_profile_policies = concat(local.audit_vault.config.instance_profile_policies, [
+            "Ec2AuditVaultPolicy",
+          ])
+        })
         ebs_volumes = {
           # "/dev/sdb" = { label = "app", snapshot_id = "snap-072a42704cb38f785", size = 300 }
           "/dev/sdb" = { label = "app", size = 300 }
