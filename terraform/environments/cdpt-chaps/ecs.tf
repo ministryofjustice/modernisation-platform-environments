f44897cb25b62a4f6a8b8a2add7223b109e3f637
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-07-01 14:55:38 +0100

    add SSM agent

--- terraform/environments/cdpt-chaps/ecs.tf
+++ terraform/environments/cdpt-chaps/ecs.tf
@@ -355,6 +355,11 @@ resource "aws_iam_role" "ec2_instance_role" {
 EOF
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ec2_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_role" "app_execution" {
   name = "execution-${var.networking[0].application}"
 
