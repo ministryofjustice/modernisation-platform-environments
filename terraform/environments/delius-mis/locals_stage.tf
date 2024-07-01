080d6aacbe688619406e29117c8380162ecd038c
Seb Norris <sebastian@automationlogic.com>
2024-07-01 15:55:47 +0100

    working plan for stage and pre

---
+++ terraform/environments/delius-mis/locals_stage.tf
@@ -0,0 +1,259 @@
+# Terraform configuration data for the stage environment in delius-mis preproduction account
+
+locals {
+  environment_config_stage = {
+    legacy_engineering_vpc_cidr            = "10.161.98.0/25"
+    legacy_counterpart_vpc_cidr            = "10.160.32.0/20"
+    legacy_ad_domain_name                  = "delius-mis-stage.local"
+    legacy_ad_ip_list                      = ["10.160.35.243", "10.160.38.128"]
+    ec2_user_ssh_key                       = file("${path.module}/files/.ssh/${terraform.workspace}/ec2-user.pub")
+    migration_environment_full_name        = "dmd-mis-stage"
+    migration_environment_abbreviated_name = "dmd"
+    migration_environment_short_name       = "mis-stage"
+  }
+
+  bastion_config_stage = {
+    extra_user_data_content = "yum install -y openldap-clients"
+  }
+
+  bcs_config_stage = {
+    instance_count = 1
+    ami_name       = "delius_mis_windows_server_patch_2024-02-07T11-03-13.202Z"
+    ebs_volumes = {
+      "/dev/sda1" = { label = "root", size = 150 }
+      "/dev/xvdf" = { label = "data", size = 300 }
+    }
+
+    ebs_volumes_config = {
+      data = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+      root = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+    }
+
+    instance_config = {
+      associate_public_ip_address  = false
+      disable_api_termination      = false
+      disable_api_stop             = false
+      instance_type                = "t3.xlarge"
+      metadata_endpoint_enabled    = "enabled"
+      key_name                     = null
+      metadata_options_http_tokens = "required"
+      monitoring                   = true
+      ebs_block_device_inline      = true
+
+      private_dns_name_options = {
+        enable_resource_name_dns_aaaa_record = false
+        enable_resource_name_dns_a_record    = true
+        hostname_type                        = "resource-name"
+      }
+
+      tags = merge(
+        local.tags,
+        { backup = true }
+      )
+    }
+  }
+
+  bps_config_stage = {
+    instance_count = 1
+    ami_name       = "delius_mis_windows_server_patch_2024-02-07T11-03-13.202Z"
+    ebs_volumes = {
+      "/dev/sda1" = { label = "root", size = 150 }
+      "/dev/xvdf" = { label = "data", size = 300 }
+    }
+
+    ebs_volumes_config = {
+      data = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+      root = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+    }
+
+    instance_config = {
+      associate_public_ip_address  = false
+      disable_api_termination      = false
+      disable_api_stop             = false
+      instance_type                = "t3.xlarge"
+      metadata_endpoint_enabled    = "enabled"
+      key_name                     = null
+      metadata_options_http_tokens = "required"
+      monitoring                   = true
+      ebs_block_device_inline      = true
+
+      private_dns_name_options = {
+        enable_resource_name_dns_aaaa_record = false
+        enable_resource_name_dns_a_record    = true
+        hostname_type                        = "resource-name"
+      }
+
+      tags = merge(
+        local.tags,
+        { backup = true }
+      )
+    }
+  }
+
+  bws_config_stage = {
+    instance_count = 1
+    ami_name       = "delius_mis_windows_server_patch_2024-02-07T11-03-13.202Z"
+    ebs_volumes = {
+      "/dev/sda1" = { label = "root", size = 150 }
+      "/dev/xvdf" = { label = "data", size = 300 }
+    }
+
+    ebs_volumes_config = {
+      data = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+      root = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+    }
+
+    instance_config = {
+      associate_public_ip_address  = false
+      disable_api_termination      = false
+      disable_api_stop             = false
+      instance_type                = "t3.xlarge"
+      metadata_endpoint_enabled    = "enabled"
+      key_name                     = null
+      metadata_options_http_tokens = "required"
+      monitoring                   = true
+      ebs_block_device_inline      = true
+
+      private_dns_name_options = {
+        enable_resource_name_dns_aaaa_record = false
+        enable_resource_name_dns_a_record    = true
+        hostname_type                        = "resource-name"
+      }
+
+      tags = merge(
+        local.tags,
+        { backup = true }
+      )
+    }
+  }
+
+  dis_config_stage = {
+    instance_count = 2
+    ami_name       = "delius_mis_windows_server_patch_2024-02-07T11-03-13.202Z"
+    ebs_volumes = {
+      "/dev/sda1" = { label = "root", size = 100 }
+      "/dev/xvdf" = { label = "data", size = 300 }
+    }
+
+    ebs_volumes_config = {
+      data = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+      root = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+    }
+
+    instance_config = {
+      associate_public_ip_address  = false
+      disable_api_termination      = false
+      disable_api_stop             = false
+      instance_type                = "t3.xlarge"
+      metadata_endpoint_enabled    = "enabled"
+      key_name                     = null
+      metadata_options_http_tokens = "required"
+      monitoring                   = true
+      ebs_block_device_inline      = true
+
+      private_dns_name_options = {
+        enable_resource_name_dns_aaaa_record = false
+        enable_resource_name_dns_a_record    = true
+        hostname_type                        = "resource-name"
+      }
+
+      tags = merge(
+        local.tags,
+        { backup = true }
+      )
+    }
+  }
+
+  # base config for each database
+  base_db_config_stage = {
+    instance_type  = "t3.large"
+    ami_name_regex = "^delius_core_ol_8_5_oracle_db_19c_patch_2024-01-31T16-06-00.575Z"
+
+    instance_policies = {
+      "business_unit_kms_key_access" = aws_iam_policy.business_unit_kms_key_access
+    }
+
+    ebs_volumes = {
+      "/dev/sdb" = { label = "app", size = 200 } # /u01
+      "/dev/sdc" = { label = "app", size = 100 } # /u02
+      "/dev/sde" = { label = "data" }            # DATA
+      "/dev/sdf" = { label = "flash" }           # FLASH
+      "/dev/sds" = { label = "swap" }
+    }
+    ebs_volume_config = {
+      app = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+      }
+      data = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+        total_size = 100
+      }
+      flash = {
+        iops       = 3000
+        throughput = 125
+        type       = "gp3"
+        total_size = 100
+      }
+    }
+    ansible_user_data_config = {
+      branch               = "main"
+      ansible_repo         = "modernisation-platform-configuration-management"
+      ansible_repo_basedir = "ansible"
+      ansible_args         = "oracle_19c_install"
+    }
+  }
+
+  # use slightly different config for each database
+  dsd_db_config_stage = local.base_db_config_stage
+
+  boe_db_config_stage = local.base_db_config_stage
+
+  mis_db_config_stage = merge(local.base_db_config_stage, {
+    ebs_volume_config = {
+      data = {
+        iops       = 5000
+        total_size = 500
+      }
+      flash = {
+        total_size = 500
+      }
+    }
+  })
+
+}
