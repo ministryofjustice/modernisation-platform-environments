7c24e47481bc5c20622d439cef65dbf9bea32334
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-07-01 14:26:38 +0000

    Rename TGW Add route module
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/data.tf
+++ terraform/environments/analytical-platform-compute/data.tf
@@ -4,33 +4,35 @@ data "aws_ssoadmin_instances" "main" {
   provider = aws.sso-readonly
 }
 
-data "aws_ec2_transit_gateway" "pttp" {
+data "aws_ec2_transit_gateway" "moj_tgw" {
   id = "tgw-026162f1ba39ce704"
 }
 
-# data "aws_ram_resource_share" "moj_tgw" {
-#   filter {
-#     name   = "resourceType"
-#     values = ["ec2:TransitGateway"]
-#   }
-# }
+/* TODO: Unhardcode aws_ec2_transit_gateway.moj_tgw
+data "aws_ram_resource_share" "moj_tgw" {
+  filter {
+    name   = "resourceType"
+    values = ["ec2:TransitGateway"]
+  }
+}
 
-# data "aws_arn" "moj_tgw" {
-#   arn = data.aws_ram_resource_share.moj_tgw.resource_arns[0]
-# }
+data "aws_arn" "moj_tgw" {
+  arn = data.aws_ram_resource_share.moj_tgw.resource_arns[0]
+}
 
-# TODO: revisit this to unhardcode the tgw ID above
-# data "aws_ram_resource_share" "tgw_moj" {
-#   name           = "tgw-moj"
-#   resource_owner = "OTHER-ACCOUNTS"
-# }
+TODO: revisit this to unhardcode the tgw ID above
+data "aws_ram_resource_share" "tgw_moj" {
+  name           = "tgw-moj"
+  resource_owner = "OTHER-ACCOUNTS"
+}
 
-# data "aws_ec2_transit_gateway" "pttp" {
-#   filter {
-#     name   = "owner-id"
-#     values = [data.aws_ram_resource_share.tgw_moj.resource_arns]
-#   }
-# }
+data "aws_ec2_transit_gateway" "pttp" {
+  filter {
+    name   = "owner-id"
+    values = [data.aws_ram_resource_share.tgw_moj.resource_arns]
+  }
+}
+*/
 
 data "aws_iam_roles" "eks_sso_access_role" {
   name_regex  = "AWSReservedSSO_${local.environment_configuration.eks_sso_access_role}_.*"
