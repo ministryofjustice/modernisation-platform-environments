906ad1eb9fdc5edc6144994b9cdf3e60a72aa8a4
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2024-07-02 16:34:56 +0100

    CC-2270: Added the 'backup_clone' volume as /dev/sdz.

--- terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_db.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_db.tf
@@ -184,7 +184,6 @@ resource "aws_ebs_volume" "backup" {
   availability_zone = "eu-west-2a"
   size              = local.application_data.accounts[local.environment].ebs_size_ebsdb_backup
   type              = local.application_data.accounts[local.environment].ebs_type_ebsdb_backup
-  snapshot_id       = length(local.application_data.accounts[local.environment].ebs_backup_snapshot_id) > 0 ? local.application_data.accounts[local.environment].ebs_backup_snapshot_id : null
   iops              = 3000
   encrypted         = true
   kms_key_id        = data.aws_kms_key.ebs_shared.key_id
@@ -197,6 +196,30 @@ resource "aws_ebs_volume" "backup" {
 resource "aws_volume_attachment" "backup_att" {
   device_name = "/dev/sdn"
   volume_id   = aws_ebs_volume.backup.id
+  instance_id = aws_instance.ec2_oracle_ebs.id
+}
+
+resource "aws_ebs_volume" "backup_clone" {
+  lifecycle {
+    ignore_changes = [kms_key_id]
+  }
+  availability_zone = "eu-west-2a"
+  count             = length(local.application_data.accounts[local.environment].ebs_backup_snapshot_id) > 0 ? 1 : 0
+  size              = local.application_data.accounts[local.environment].ebs_size_ebsdb_backup
+  type              = local.application_data.accounts[local.environment].ebs_type_ebsdb_backup
+  snapshot_id       = length(local.application_data.accounts[local.environment].ebs_backup_snapshot_id) > 0 ? local.application_data.accounts[local.environment].ebs_backup_snapshot_id : null
+  iops              = 3000
+  encrypted         = true
+  kms_key_id        = data.aws_kms_key.ebs_shared.key_id
+  tags = merge(local.tags,
+    { Name = lower(format("%s-%s", local.application_data.accounts[local.environment].instance_role_ebsdb, "backup")) },
+    { device-name = "/dev/sdz" }
+  )
+}
+
+resource "aws_volume_attachment" "backup_clone_att" {
+  device_name = "/dev/sdz"
+  volume_id   = aws_ebs_volume.backup_clone[0].id
   instance_id = aws_instance.ec2_oracle_ebs.id
 }
 
