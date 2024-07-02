6a178f784fd2ab53f5057ac78f644aa4c322ec12
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-07-02 11:49:45 +0100

    DSOS: iops tweaks for Nomis and CSR (#6822)
    * nomis: reduce iops and bandwidth
    
    * increase CSR prod iops

--- terraform/environments/nomis/locals_production.tf
+++ terraform/environments/nomis/locals_production.tf
@@ -176,10 +176,8 @@ locals {
           "/dev/sdc" = { label = "app", size = 1000 } # /u02
         })
         ebs_volume_config = merge(local.ec2_instances.db.ebs_volume_config, {
-          # data  = { total_size = 4000, iops = 9000, throughput = 250 }
-          # flash = { total_size = 1000, iops = 3000, throughput = 250 }
-          data  = { total_size = 4000, iops = 16000, throughput = 500 } # doubled for failover test. 16000 is the max iops
-          flash = { total_size = 1000, iops = 6000, throughput = 500 }
+          data  = { total_size = 4000, iops = 9000, throughput = 250 }
+          flash = { total_size = 1000, iops = 3000, throughput = 250 }
         })
         instance = merge(local.ec2_instances.db.instance, {
           disable_api_termination = true
