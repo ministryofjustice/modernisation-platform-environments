cc268d7ea06022142437ea88268ed5a2620c2c70
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-07-02 11:37:34 +0100

    add lb module

--- terraform/environments/cdpt-ifs/ecs.tf
+++ terraform/environments/cdpt-ifs/ecs.tf
@@ -473,7 +473,7 @@ resource "aws_security_group" "ecs_service" {
     to_port         = 80
     protocol        = "tcp"
     description     = "Allow traffic on port 80 from load balancer"
-    security_groups = [aws_security_group.ifs_lb_sc.id]
+    security_groups = [module.lb_access_logs_enabled.security_group.id]
   }
 
   egress {
