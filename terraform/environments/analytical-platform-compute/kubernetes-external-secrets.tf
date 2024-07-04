2c2793af9d601c0b39b3a5a550202466f3e424d2
jamesstottmoj <james.stott@digital.justice.gov.uk>
2024-07-04 15:19:35 +0100

    Testing not using module

--- terraform/environments/analytical-platform-compute/kubernetes-external-secrets.tf
+++ terraform/environments/analytical-platform-compute/kubernetes-external-secrets.tf
@@ -45,7 +45,7 @@ resource "kubernetes_manifest" "ui_azure_external_secret" {
       "data" = [
         {
           "remoteRef" = {
-            "key" = module.ui_azure_client_secret.secret_id
+            "key" = "a-test-string"
           }
           "secretKey" = "client-id"
         },
