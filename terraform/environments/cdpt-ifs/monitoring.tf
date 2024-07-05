94fd050e09b466cd78f6e38882c593a35df686c9
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-07-05 14:56:58 +0100

    add cw alarm with pagerduty integration

--- terraform/environments/cdpt-ifs/monitoring.tf
+++ terraform/environments/cdpt-ifs/monitoring.tf
@@ -1,43 +1,3 @@
-resource "aws_sns_topic" "lb_alarm_topic" {
-  name = "lb_alarm_topic"
-}
-
-module "slack_alerts_url" {
-  for_each = local.enable_slack_alerts ? { "enabled": "enabled" } : {}
-  source               = "./modules/baseline/secretsmanager"
-  name                 = "${local.application_name}-slack-alerts-url-${local.environment}"
-  description          = "IFS LoadBalancer slack alerts URL"
-  type                 = "MONO"
-  secret_value         = "http://Placeholder_webhook_URL"
-  ignore_secret_string = true
-
-  tags = merge(
-    local.all_tags,
-    {
-      Resource_Type  = "Secret"
-      Name           = "${local.application_name}-slack-alerts-url-${local.environment}"
-    }
-  )
-}
-
-data "aws_secretsmanager_secret" "slack_integration" {
-  for_each = local.enable_slack_alerts ? { "enabled": "enabled" } : {}
-  depends_on = [module.slack_alerts_url]
-  name       = "${local.project}-slack-alerts-url-${local.environment}"
-}
-
-data "aws_secretsmanager_secret_version" "slack_integration" {
-  for_each = local.enable_slack_alerts ? { "enabled": "enabled" } : {}
-  secret_id = data.aws_secretsmanager_secret.slack_integration[0].id
-}
-
-resource "aws_sns_topic_subscription" "slack_subscription" {
-  depends_on = [data.aws_secretsmanager_secret_version.slack_integration]
-  topic_arn = aws_sns_topic.lb_alarm_topic.arn
-  protocol  = "https"
-  endpoint  = data.aws_secretsmanager_secret_version.slack_integration["enabled"].secret_string
-}
-
 resource "aws_cloudwatch_metric_alarm" "lb_5xx_errors" {
   alarm_name          = "lb-5xx-errors"
   comparison_operator = "GreaterThanOrEqualToThreshold"
@@ -49,14 +9,40 @@ resource "aws_cloudwatch_metric_alarm" "lb_5xx_errors" {
   threshold           = "1"
   alarm_description   = "This metric monitors 5xx errors on the load balancer"
   alarm_actions       = [aws_sns_topic.lb_alarm_topic.arn]
-
   dimensions = {
     LoadBalancer = "${local.application_name}-lb"
   }
 }
 
+resource "aws_sns_topic" "lb_5xx_alarm_topic" {
+  name = "lb_5xx_alarm_topic"
+  kms_master_key_id = data.aws_kms_key.sns.id
+}
 
+# Pager duty integration
 
+# Get the map of pagerduty integration keys from the modernisation platform account
+data "aws_secretsmanager_secret" "pagerduty_integration_keys" {
+  provider = aws.modernisation-platform
+  name     = "pagerduty_integration_keys"
+}
+data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
+  provider  = aws.modernisation-platform
+  secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
+}
 
+# Add a local to get the keys
+locals {
+  pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
+}
 
+# link the sns topic to the service
+module "pagerduty_core_alerts" {
+  depends_on = [
+    aws_sns_topic.lb-5xx-errors
+  ]
+  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v2.0.0"
+  sns_topics                = [aws_sns_topic.lb_5xx_alarm_topic.name]
+  pagerduty_integration_key = local.pagerduty_integration_keys["cloudwatch_lb_alert"]
+}
 
