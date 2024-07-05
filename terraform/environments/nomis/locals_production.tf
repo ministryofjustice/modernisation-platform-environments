03f0828f416c1424763a9040a3c5781accfd7005
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-07-05 13:04:52 +0100

    nomis: DSOS: weblogic release testing

--- terraform/environments/nomis/locals_production.tf
+++ terraform/environments/nomis/locals_production.tf
@@ -72,7 +72,7 @@ locals {
         })
         user_data_cloud_init = merge(local.ec2_autoscaling_groups.web.user_data_cloud_init, {
           args = merge(local.ec2_autoscaling_groups.web.user_data_cloud_init.args, {
-            branch = "main"
+            branch = "86471c5730194674959e03fff043a6b4d2d1a92f" # DSOS-2838 memory fix
           })
         })
         tags = merge(local.ec2_autoscaling_groups.web.tags, {
