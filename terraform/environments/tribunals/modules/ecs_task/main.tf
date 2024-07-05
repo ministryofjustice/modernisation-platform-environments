cf31f550582dbdf28d68bf644a60363560bc71e5
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-07-05 16:17:20 +0100

    go back to default network_mode

--- terraform/environments/tribunals/modules/ecs_task/main.tf
+++ terraform/environments/tribunals/modules/ecs_task/main.tf
@@ -2,7 +2,6 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   family             = "${var.app_name}Family"
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
-  network_mode       = "awsvpc"
   requires_compatibilities = [
     "EC2",
   ]
