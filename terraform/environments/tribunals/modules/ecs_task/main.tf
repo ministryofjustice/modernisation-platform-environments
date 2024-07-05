1bc8416b7f31c32d9fb3be5202928ee0c4a3ff68
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-07-05 13:33:35 +0100

    add network_mode = bridge to the task definitoin (and watch it all burn)

--- terraform/environments/tribunals/modules/ecs_task/main.tf
+++ terraform/environments/tribunals/modules/ecs_task/main.tf
@@ -2,6 +2,8 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   family             = "${var.app_name}Family"
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+  //Set this to brige and disable public IP for ec2??
+  network_mode       = "bridge"
   requires_compatibilities = [
     "EC2",
   ]
