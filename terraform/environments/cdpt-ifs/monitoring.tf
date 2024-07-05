9c9c42ad28b1c940ea38debe6cc35d8aa1051137
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-07-05 12:26:25 +0100

    add cloudwatch alert using module

--- terraform/environments/cdpt-ifs/monitoring.tf
+++ terraform/environments/cdpt-ifs/monitoring.tf
@@ -2,10 +2,40 @@ resource "aws_sns_topic" "lb_alarm_topic" {
   name = "lb_alarm_topic"
 }
 
+module "slack_alerts_url" {
+  for_each = local.enable_slack_alerts ? { "enabled": "enabled" } : {}
+  source               = "./modules/secrets_manager"
+  name                 = "${local.application_name}-slack-alerts-url-${local.environment}"
+  description          = "IFS LoadBalancer slack alerts URL"
+  type                 = "MONO"
+  secret_value         = "http://Placeholder_webhook_URL"
+  ignore_secret_string = true
+
+  tags = merge(
+    local.all_tags,
+    {
+      Resource_Type  = "Secret"
+      Name           = "${local.application_name}-slack-alerts-url-${local.environment}"
+    }
+  )
+}
+
+data "aws_secretsmanager_secret" "slack_integration" {
+  for_each = local.enable_slack_alerts ? { "enabled": "enabled" } : {}
+  depends_on = [module.slack_alerts_url]
+  name       = "${local.project}-slack-alerts-url-${local.environment}"
+}
+
+data "aws_secretsmanager_secret_version" "slack_integration" {
+  for_each = local.enable_slack_alerts ? { "enabled": "enabled" } : {}
+  secret_id = data.aws_secretsmanager_secret.slack_integration[0].id
+}
+
 resource "aws_sns_topic_subscription" "slack_subscription" {
+  depends_on = [data.aws_secretsmanager_secret_version.slack_integration]
   topic_arn = aws_sns_topic.lb_alarm_topic.arn
   protocol  = "https"
-  endpoint  = "https://hooks.slack.com/services/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"
+  endpoint  = data.aws_secretsmanager_secret_version.slack_integration["enabled"].secret_string
 }
 
 resource "aws_cloudwatch_metric_alarm" "lb_5xx_errors" {
@@ -24,3 +54,9 @@ resource "aws_cloudwatch_metric_alarm" "lb_5xx_errors" {
     LoadBalancer = "${local.application_name}-lb"
   }
 }
+
+
+
+
+
+
