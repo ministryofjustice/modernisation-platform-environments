b8d547438713bef36420c297125eda78f4bd332a
bill-buchan <55987520+bill-buchan@users.noreply.github.com>
2024-07-08 09:44:47 +0100

    Update terraform/environments/delius-core/modules/components/dms/dms_db_endpoints.tf
    Co-authored-by: George Taylor <george.taylor@digital.justice.gov.uk>

--- terraform/environments/delius-core/modules/components/dms/dms_db_endpoints.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_db_endpoints.tf
@@ -18,9 +18,9 @@ resource "aws_dms_endpoint" "dms_audit_source_endpoint_db" {
 # In repository environments the dms_user_source_endpoint.read_database must be defined
 # The endpoint for user (USER_) is the Delius database.
 resource "aws_dms_endpoint" "dms_user_source_endpoint_db" {
-   count                           = var.dms_user_source_endpoint.read_database == null ? 0 : 1
-   database_name                   = var.dms_user_source_endpoint.read_database
-   endpoint_id                     = "user-data-from-${var.dms_user_source_endpoint.read_database}"
+   count                           = var.dms_config.user_source_endpoint.read_database == null ? 0 : 1
+   database_name                   = var.dms_config.user_source_endpoint.read_database
+   endpoint_id                     = "user-data-from-${var.dms_config.user_source_endpoint.read_database}"
    endpoint_type                   = "source"
    engine_name                     = "oracle"
    secrets_manager_access_role_arn = "arn:aws:iam::${local.delius_account_id}:role/DMSSecretsManagerAccessRole"
