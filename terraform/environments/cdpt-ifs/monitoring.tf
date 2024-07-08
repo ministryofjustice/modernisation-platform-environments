e58be5f7b60f7fef203f57ad2434f339e039c407
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-07-08 15:10:02 +0100

    use target 5xx error instead

--- terraform/environments/cdpt-ifs/monitoring.tf
+++ terraform/environments/cdpt-ifs/monitoring.tf
@@ -2,12 +2,12 @@ resource "aws_cloudwatch_metric_alarm" "lb_5xx_errors" {
   alarm_name          = "lb-5xx-errors"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
-  metric_name         = "HTTPCode_ELB_5XX_Count"
+  metric_name         = "HTTPCode_Target_5XX_Count"
   namespace           = "AWS/ApplicationELB"
   period              = "300"
   statistic           = "Sum"
   threshold           = "1"
-  alarm_description   = "This metric monitors 5xx errors on the load balancer"
+  alarm_description   = "This metric monitors 5xx errors on the targets behind the load balancer"
   alarm_actions       = [aws_sns_topic.lb_5xx_alarm_topic.arn]
   dimensions = {
     LoadBalancer = "${local.application_name}-lb"
