ff5ec27e5a8d834cd82403fb50a20567f9841735
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-07-08 14:54:09 +0100

    undo more changes

--- terraform/environments/tribunals/modules/tribunal/main.tf
+++ terraform/environments/tribunals/modules/tribunal/main.tf
@@ -166,22 +166,22 @@ resource "aws_ecr_repository" "app-ecr-repo" {
 ####################### ECS Task #########################################
 
 module "app_ecs_task" {
-  source                       = "../ecs_task"
-  app_name                     = local.app
-  task_definition_volume       = var.task_definition_volume
-  container_definition         = local.app_container_definition
-  tags_common                  = var.tags
-  appscaling_min_capacity      = var.appscaling_min_capacity
-  appscaling_max_capacity      = var.appscaling_max_capacity
-  ecs_scaling_cpu_threshold    = var.ecs_scaling_cpu_threshold
-  ecs_scaling_mem_threshold    = var.ecs_scaling_mem_threshold
-  app_count                    = var.app_count
-  server_port                  = var.server_port
-  cluster_id                   = var.cluster_id
-  cluster_name                 = var.cluster_name
-  is_ftp_app                   = var.is_ftp_app
-  lb_tg_arn                    = var.target_group_arns["${local.module_name}"]
-  sftp_lb_tg_arn               = ""
+  source                    = "../ecs_task"
+  app_name                  = local.app
+  task_definition_volume    = var.task_definition_volume
+  container_definition      = local.app_container_definition
+  tags_common               = var.tags
+  appscaling_min_capacity   = var.appscaling_min_capacity
+  appscaling_max_capacity   = var.appscaling_max_capacity
+  ecs_scaling_cpu_threshold = var.ecs_scaling_cpu_threshold
+  ecs_scaling_mem_threshold = var.ecs_scaling_mem_threshold
+  app_count                 = var.app_count
+  server_port               = var.server_port
+  cluster_id                = var.cluster_id
+  cluster_name              = var.cluster_name
+  is_ftp_app                = var.is_ftp_app
+  lb_tg_arn                 = var.target_group_arns["${local.module_name}"]
+  sftp_lb_tg_arn            = ""
 }
 
 ####################### Load Balancer #########################################
