9a580bf1645d9c19ca1c89c231557c83468592e8
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-10 14:39:02 +0100

    Add reverse direction SG

--- terraform/environments/delius-core/modules/components/dms/sg.tf
+++ terraform/environments/delius-core/modules/components/dms/sg.tf
@@ -19,7 +19,7 @@ resource "aws_vpc_security_group_egress_rule" "dms_instance_https_out" {
   )
 }
 
-resource "aws_vpc_security_group_egress_rule" "dms_db_conn" {
+resource "aws_vpc_security_group_egress_rule" "dms_db_conn_out" {
   security_group_id            = aws_security_group.dms.id
   description                  = "Allow outgoing communication between DMS and delius db instances"
   from_port                    = 1521
@@ -30,8 +30,32 @@ resource "aws_vpc_security_group_egress_rule" "dms_db_conn" {
     { Name = "oracle-out" }
   )
 }
+  
+resource "aws_vpc_security_group_ingress_rule" "dms_db_conn_in" {
+  security_group_id            = aws_security_group.dms.id
+  description                  = "Allow incoming communication between delius db instances and DMS"
+  from_port                    = 1521
+  to_port                      = 1521
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = var.db_ec2_sg_id
+  tags = merge(var.tags,
+    { Name = "oracle-in" }
+  )
+}
 
-resource "aws_vpc_security_group_ingress_rule" "db_dms_conn" {
+resource "aws_vpc_security_group_egress_rule" "db_dms_conn_out" {
+  security_group_id            = var.db_ec2_sg_id
+  description                  = "Allow outgoing communication between delius db instances and DMS"
+  from_port                    = 1521
+  to_port                      = 1521
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.dms.id
+  tags = merge(var.tags,
+    { Name = "dms-out" }
+  )
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db_dms_conn_in" {
   security_group_id            = var.db_ec2_sg_id
   description                  = "Allow incoming communication between DMS and delius db instances"
   from_port                    = 1521
