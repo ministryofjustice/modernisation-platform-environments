cefa18d984eb0f98405dce63355fcc35993b6590
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-10 11:01:04 +0100

    Add HTTPS Access for Secrets

--- terraform/environments/delius-core/modules/components/dms/sg.tf
+++ terraform/environments/delius-core/modules/components/dms/sg.tf
@@ -6,3 +6,15 @@ resource "aws_security_group" "dms" {
     create_before_destroy = true
   }
 }
+
+resource "aws_vpc_security_group_egress_rule" "dms_instance_https_out" {
+  security_group_id = aws_security_group.dms.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  description       = "Allow communication out on port 443, e.g. for SSM"
+  tags = merge(var.tags,
+    { Name = "https-out" }
+  )
+}
