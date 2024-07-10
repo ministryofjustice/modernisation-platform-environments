53a15e77c7ca85268c37f2570291c68f1203c24c
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-10 12:07:27 +0100

    Only need VPC access for Secrets

--- terraform/environments/delius-core/modules/components/dms/sg.tf
+++ terraform/environments/delius-core/modules/components/dms/sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "dms" {
 
 resource "aws_vpc_security_group_egress_rule" "dms_instance_https_out" {
   security_group_id = aws_security_group.dms.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.account_config.shared_vpc_cidr
   from_port         = 443
   to_port           = 443
   ip_protocol       = "tcp"
