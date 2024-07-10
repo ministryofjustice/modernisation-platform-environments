e24ddd18d9b92270e401a3d53adb71c031274fd6
matt-heery <matt.heery@digital.justice.gov.uk>
2024-07-10 12:33:26 +0100

    testing built in resource

--- terraform/environments/electronic-monitoring-data/ecs_main.tf
+++ terraform/environments/electronic-monitoring-data/ecs_main.tf
@@ -1,20 +1,26 @@
-module "dagster_test" {
-    source = "github.com/datarootsio/terraform-aws-ecs-dagster"
-
-    resource_prefix = "test-dlt"
-    resource_suffix = "env"
-
-    vpc             = data.aws_vpc.shared.id
-    public_subnet  = [
-      data.aws_subnet.public_subnets_a.id,
-      data.aws_subnet.public_subnets_b.id,
-      data.aws_subnet.public_subnets_c.id
-      ]
-
-    rds_password = data.aws_secretsmanager_random_password.test.random_password
+resource "dagster_deployment" "this" {
+  name              = "test-deploy"
+  settings_document = data.dagster_configuration_document.this.json
 }
 
-data "aws_secretsmanager_random_password" "test" {
-  password_length = 50
-  exclude_numbers = true
+data "dagster_configuration_document" "this" {
+  yaml_body = <<YAML
+run_queue:
+  max_concurrent_runs: 30
+  tag_concurrency_limits: []
+run_monitoring:
+  start_timeout_seconds: 1200
+  cancel_timeout_seconds: 1400
+  free_slots_after_run_end_seconds: 300
+run_retries:
+  max_retries: 0
+  retry_on_asset_or_op_failure: true
+sso_default_role: VIEWER
+non_isolated_runs:
+  max_concurrent_non_isolated_runs: 1
+auto_materialize:
+  run_tags: {}
+  respect_materialization_data_versions: false
+  use_sensors: false
+YAML
 }
