18c50bbd82c8d23a87f4f81dd2ce436ea6c64094
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-11 11:18:44 +0100

    Use Secrets Lookup for Password

--- terraform/environments/delius-core/modules/components/dms/dms_db_endpoints.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_db_endpoints.tf
@@ -20,7 +20,7 @@ resource "aws_dms_endpoint" "dms_audit_source_endpoint_db" {
    engine_name                     = "oracle"
    secrets_manager_access_role_arn = "arn:aws:iam::${local.delius_account_id}:role/DMSSecretsManagerAccessRole"
    secrets_manager_arn             = aws_secretsmanager_secret.dms_audit_source_endpoint_db.arn
-   extra_connection_attributes     = "ArchivedLogDestId=1;AdditionalArchivedLogDestId=32;asm_server=${join(".",[var.oracle_db_server_names[var.dms_config.audit_source_endpoint.read_host],var.account_config.route53_inner_zone_info.name])}:1521/+ASM;asm_user=delius_audit_dms_pool;UseBFile=true;UseLogminerReader=false;"
+   extra_connection_attributes     = "ArchivedLogDestId=1;AdditionalArchivedLogDestId=32;asm_server=${join(".",[var.oracle_db_server_names[var.dms_config.audit_source_endpoint.read_host],var.account_config.route53_inner_zone_info.name])}:1521/+ASM;asm_user=delius_audit_dms_pool;asm_password=${jsondecode(data.aws_secretsmanager_secret_version.delius_core_application_passwords.secret_string)["delius_audit_dms_pool"]};UseBFile=true;UseLogminerReader=false;"
 }
 
 # In repository environments the dms_user_source_endpoint.read_database must be defined
@@ -33,5 +33,5 @@ resource "aws_dms_endpoint" "dms_user_source_endpoint_db" {
    engine_name                     = "oracle"
    secrets_manager_access_role_arn = "arn:aws:iam::${local.delius_account_id}:role/DMSSecretsManagerAccessRole"
    secrets_manager_arn             = aws_secretsmanager_secret.dms_user_source_endpoint_db.arn
-   extra_connection_attributes     = "ArchivedLogDestId=1;AdditionalArchivedLogDestId=32;asm_server=${join(".",[var.oracle_db_server_names[var.dms_config.user_source_endpoint.read_host],var.account_config.route53_inner_zone_info.name])}:1521/+ASM;asm_user=delius_audit_dms_pool;UseBFile=true;UseLogminerReader=false;"
+   extra_connection_attributes     = "ArchivedLogDestId=1;AdditionalArchivedLogDestId=32;asm_server=${join(".",[var.oracle_db_server_names[var.dms_config.user_source_endpoint.read_host],var.account_config.route53_inner_zone_info.name])}:1521/+ASM;asm_user=delius_audit_dms_pool;asm_password=${jsondecode(data.aws_secretsmanager_secret_version.delius_core_application_passwords.secret_string)["delius_audit_dms_pool"]};UseBFile=true;UseLogminerReader=false;"
 }
