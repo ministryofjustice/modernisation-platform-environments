fa915cff4c3d785af548bd97b043a2d710f99313
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2024-07-11 10:29:10 +0100

    Add Usage Notes

--- terraform/environments/digital-prison-reporting/operational_datastore.tf
+++ terraform/environments/digital-prison-reporting/operational_datastore.tf
@@ -17,7 +17,6 @@ locals {
   )
 
   operational_db_credentials = jsondecode(data.aws_secretsmanager_secret_version.operational_db_secret_version.secret_string)
-  transfer_component_role_credentials = jsondecode(data.aws_secretsmanager_secret_version.transfer_component_role_secret_version.secret_string)
 }
 
 ################################################################################
@@ -108,24 +107,6 @@ module "aurora_operational_db" {
 
   tags = local.operational_db_tags
 }
-
-################################################################################
-# Operationa DB - Transfer Component Role/User
-################################################################################
-
-#module "transfer_component_role" {
-#  source = "./modules/rds/rds-pgres-role/"
-#
-#  setup_additional_users = false
-#  host                   = module.aurora_operational_db.rds_cluster_endpoints["static"]
-#  port                   = 5432
-#  database               = "postgres"
-#  db_username            = local.operational_db_credentials.username
-#  db_master_password     = local.operational_db_credentials.password
-#  db_password            = local.transfer_component_role_credentials.password
-#  rds_role_name          = local.transfer_component_role_credentials.username
-#  read_write_role        = true
-#}
 
 resource "aws_glue_connection" "glue_operational_datastore_connection" {
   count           = (local.environment == "development" ? 1 : 0)
