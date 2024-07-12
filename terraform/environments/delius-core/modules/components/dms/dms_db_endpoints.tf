3e87cbda54695bbd97093e4daa260c4f6bcbb502
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-12 12:24:36 +0100

    Add Oracle Port

--- terraform/environments/delius-core/modules/components/dms/dms_db_endpoints.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_db_endpoints.tf
@@ -24,16 +24,16 @@ data "aws_secretsmanager_secret_version" "delius_core_application_passwords" {
 }
 
 resource "aws_dms_endpoint" "dms_audit_source_endpoint_db" {
-  count                       = try(var.dms_config.audit_source_endpoint.read_database, null) == null ? 0 : 1
-  database_name               = var.dms_config.audit_source_endpoint.read_database
-  endpoint_id                 = "audit-data-from-${lower(var.dms_config.audit_source_endpoint.read_database)}"
-  endpoint_type               = "source"
-  engine_name                 = "oracle"
-  username                    = local.dms_audit_username
-  password                    = join(",", [jsondecode(data.aws_secretsmanager_secret_version.delius_core_application_passwords.secret_string)[local.dms_audit_username], jsondecode(data.aws_secretsmanager_secret_version.delius_core_application_passwords.secret_string)[local.dms_audit_username]])
-  server_name                 = join(".", [var.oracle_db_server_names[var.dms_config.audit_source_endpoint.read_host], var.account_config.route53_inner_zone_info.name])
-  port                        = local.oracle_port
-  extra_connection_attributes = "ArchivedLogDestId=1;AdditionalArchivedLogDestId=32;asm_server=${join(".", [var.oracle_db_server_names[var.dms_config.audit_source_endpoint.read_host], var.account_config.route53_inner_zone_info.name])}:${local.oracle_port}/+ASM;asm_user=${local.dms_audit_username};UseBFile=true;UseLogminerReader=false;"
+   count                           = try(var.dms_config.audit_source_endpoint.read_database, null) == null ? 0 : 1
+   database_name                   = var.dms_config.audit_source_endpoint.read_database
+   endpoint_id                     = "audit-data-from-${lower(var.dms_config.audit_source_endpoint.read_database)}"
+   endpoint_type                   = "source"
+   engine_name                     = "oracle"
+   username                        = local.dms_audit_username
+   password                        = join(",",[jsondecode(data.aws_secretsmanager_secret_version.delius_core_application_passwords.secret_string)[local.dms_audit_username],jsondecode(data.aws_secretsmanager_secret_version.delius_core_application_passwords.secret_string)[local.dms_audit_username]])
+   server_name                     = join(".",[var.oracle_db_server_names[var.dms_config.audit_source_endpoint.read_host],var.account_config.route53_inner_zone_info.name])
+   port                            = local.oracle_port
+   extra_connection_attributes     = "ArchivedLogDestId=1;AdditionalArchivedLogDestId=32;asm_server=${join(".",[var.oracle_db_server_names[var.dms_config.audit_source_endpoint.read_host],var.account_config.route53_inner_zone_info.name])}:${local.oracle_port}/+ASM;asm_user=${local.dms_audit_username};UseBFile=true;UseLogminerReader=false;"
 }
 
 # In repository environments the dms_user_source_endpoint.read_database must be defined
@@ -41,11 +41,12 @@ resource "aws_dms_endpoint" "dms_audit_source_endpoint_db" {
 resource "aws_dms_endpoint" "dms_user_source_endpoint_db" {
    count                           = try(var.dms_config.user_source_endpoint.read_database, null) == null ? 0 : 1
    database_name                   = var.dms_config.user_source_endpoint.read_database
-   endpoint_id                     = "user-data-from-${var.dms_config.user_source_endpoint.read_database}"
+   endpoint_id                     = "user-data-from-${lower(var.dms_config.user_source_endpoint.read_database)}"
    endpoint_type                   = "source"
    engine_name                     = "oracle"
    username                        = local.dms_audit_username
    password                        = join(",",[jsondecode(data.aws_secretsmanager_secret_version.delius_core_application_passwords.secret_string)[local.dms_audit_username],jsondecode(data.aws_secretsmanager_secret_version.delius_core_application_passwords.secret_string)[local.dms_audit_username]])
    server_name                     = join(".",[var.oracle_db_server_names[var.dms_config.user_source_endpoint.read_host],var.account_config.route53_inner_zone_info.name])
+   port                            = local.oracle_port
    extra_connection_attributes     = "ArchivedLogDestId=1;AdditionalArchivedLogDestId=32;asm_server=${join(".",[var.oracle_db_server_names[var.dms_config.user_source_endpoint.read_host],var.account_config.route53_inner_zone_info.name])}:1521/+ASM;asm_user=${local.dms_audit_username};UseBFile=true;UseLogminerReader=false;"
 }
