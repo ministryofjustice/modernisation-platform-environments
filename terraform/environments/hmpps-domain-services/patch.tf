3bbcd17119120793d863c41bc0d7b52620df77c5
pav <pav.gainda@digital.justice.gov.uk>
2024-07-12 17:17:29 +0100

    test new branch

--- terraform/environments/hmpps-domain-services/patch.tf
+++ terraform/environments/hmpps-domain-services/patch.tf
@@ -59,8 +59,7 @@ module "test-2c" {
 module "development" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   # This is an internal module so commit hashes are not needed
-#   source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v3.0.0"
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=ssm-patch-module-refactor"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v3.0.0"
 
   count  = local.is-development == true ? 1 : 0
   providers = {
@@ -71,7 +70,7 @@ module "development" {
   application_name = local.application_name
   approval_days    = "0"
   patch_schedule   = "cron(0 21 ? * TUE#2 *)" # 2nd Tues @ 9pm
-  operating_system = "WINDOWS"
+  operating_system = "REDHAT_ENTERPRISE_LINUX"
 
   tags = merge(
     local.tags,
