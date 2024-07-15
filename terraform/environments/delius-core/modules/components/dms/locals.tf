006de7da1f3f0dcbbf5aeed2c42b6fc97769750d
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-15 09:53:34 +0100

    Grant privileges on destination s3 bucket

--- terraform/environments/delius-core/modules/components/dms/locals.tf
+++ terraform/environments/delius-core/modules/components/dms/locals.tf
@@ -2,4 +2,9 @@ locals {
   delius_account_id = var.platform_vars.environment_management.account_ids[join("-", ["delius-core", var.account_info.mp_environment])]
   oracle_port = "1521"
   dms_audit_username = "delius_audit_dms_pool"
+  dms_s3_local_bucket_prefix = "${var.env_name}-dms-destination-bucket"
+  dms_s3_repository_bucket = {
+    prefix = try("${var.dms_config.audit_target_endpoint.write_environment}-dms-destination-bucket",null)
+    account_id = try(var.platform_vars.environment_management.account_ids[join("-", ["delius-core", var.dms_config.audit_target_endpoint.write_environment])],null)
+  }
 }
