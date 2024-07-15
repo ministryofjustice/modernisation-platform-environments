c4af25bd31e919a00fec20d66847990fb484ac44
Edward Duddy <edward.duddy@cica.gov.uk>
2024-07-15 17:22:58 +0100

    🐛 Add ebs block devices and snapshot ids to avoid instance replacement
    Define EBS block devices within instance resource. Add snapshot Ids to volumes to avoid instance replacement on each apply. Destroy additional volumes

--- terraform/environments/cica-tariff/locals.tf
+++ terraform/environments/cica-tariff/locals.tf
@@ -6,4 +6,39 @@ pubkey = {
 }
 cidr_cica_ss_a = "10.10.10.0/24"
 cidr_cica_ss_b = "10.10.110.0/24"
+
+#get snapshot IDs for each volume. Required to stop instance replacement on apply
+block_device_mapping_xvde = {
+    for mapping in data.aws_ami.shared_ami.block_device_mappings : "0" => mapping
+    if mapping.device_name == "xvde"
+
+  }
+snapshot_id_xvde = local.block_device_mapping_xvde[0].ebs.snapshot_id
+  
+block_device_mapping_xvdf = {
+    for mapping in data.aws_ami.shared_ami.block_device_mappings : "1" => mapping
+    if mapping.device_name == "xvdf"
+  }
+snapshot_id_xvdf = local.block_device_mapping_xvdf[1].ebs.snapshot_id
+
+block_device_mapping_xvdg = {
+    for mapping in data.aws_ami.shared_ami.block_device_mappings : "2" => mapping
+    if mapping.device_name == "xvdg"
+  }
+snapshot_id_xvdg = local.block_device_mapping_xvdg[2].ebs.snapshot_id
+
+block_device_mapping_xvdh = {
+    for mapping in data.aws_ami.shared_ami.block_device_mappings : "3" => mapping
+    if mapping.device_name == "xvdh"
+  }
+snapshot_id_xvdh = local.block_device_mapping_xvdh[3].ebs.snapshot_id
+
+block_device_mapping_xvdi = {
+    for mapping in data.aws_ami.shared_ami.block_device_mappings : "4" => mapping
+    if mapping.device_name == "xvdi"
+  }
+snapshot_id_xvdi = local.block_device_mapping_xvdi[4].ebs.snapshot_id
+
+block_device_mapping_merge = merge(local.block_device_mapping_xvde, local.block_device_mapping_xvdf, local.block_device_mapping_xvdg, local.block_device_mapping_xvdh, local.block_device_mapping_xvdi)
 }
+
