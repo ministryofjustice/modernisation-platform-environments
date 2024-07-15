6710e4f5b50dc02c97156f6ffb2285c4b4a9b386
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-15 13:56:52 +0100

    Quotes are required

--- terraform/environments/delius-core/modules/components/dms/locals.tf
+++ terraform/environments/delius-core/modules/components/dms/locals.tf
@@ -1,4 +1,5 @@
 locals {
+  account_id = data.aws_caller_identity.current.account_id
   delius_account_id = var.platform_vars.environment_management.account_ids[join("-", ["delius-core", var.account_info.mp_environment])]
   oracle_port = "1521"
   dms_audit_username = "delius_audit_dms_pool"
