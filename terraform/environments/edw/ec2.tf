540d62aa26563060349d4218bfd610c8de8adff2
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-07-16 16:42:02 +0100

    LAWS-3977 Update userdata
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/edw/ec2.tf
+++ terraform/environments/edw/ec2.tf
@@ -1,3 +1,257 @@
+locals {
+  db_userdata = <<EOF
+#!/bin/bash
+
+##### USERDATA #####
+
+####install missing package and hostname change
+yum -y install libXp.i386
+yum -y install sshpass
+echo "HOSTNAME="${local.application_data.accounts[local.environment].edw_AppName}"."${local.application_data.accounts[local.environment].edw_dns_extension}"" >> /etc/sysconfig/network
+
+#### configure aws timesync (external ntp source)
+AwsTimeSync(){
+    local RHEL=$1
+    local SOURCE=169.254.169.123
+
+    NtpD(){
+        local CONF=/etc/ntp.conf
+        sed -i 's/server \S/#server \S/g' $CONF && \
+        sed -i "20i\server $SOURCE prefer iburst" $CONF
+        /etc/init.d/ntpd status >/dev/null 2>&1 \
+            && /etc/init.d/ntpd restart || /etc/init.d/ntpd start
+        ntpq -p
+    }
+    ChronyD(){
+        local CONF=/etc/chrony.conf
+        sed -i 's/server \S/#server \S/g' $CONF && \
+        sed -i "7i\server $SOURCE prefer iburst" $CONF
+        systemctl status chronyd >/dev/null 2>&1 \
+            && systemctl restart chronyd || systemctl start chronyd
+        chronyc sources
+    }
+    case $RHEL in
+        5)
+            NtpD
+            ;;
+        7)
+            ChronyD
+            ;;
+    esac
+}
+AwsTimeSync $(cat /etc/redhat-release | cut -d. -f1 | awk '{print $NF}')
+
+####Install AWS cli
+mkdir -p /opt/aws/bin
+cd /root
+wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+easy_install --script-dir /opt/aws/bin aws-cfn-bootstrap-latest.tar.gz
+mkdir -p /run/cfn-init # Path to store cfn-init scripts
+
+####configure cfn-init variables
+export ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+export LOGS="${local.application_data.accounts[local.environment].edw_AppName}-EC2"
+export APPNAME="${local.application_data.accounts[local.environment].edw_AppName}"
+export ENV="${local.application_data.accounts[local.environment].edw_environment}"
+export BACKUPBUCKET="${local.application_data.accounts[local.environment].edw_s3_backup_bucket}"
+export SECRET=`/usr/local/bin/aws --region ${local.application_data.accounts[local.environment].edw_region} secretsmanager get-secret-value --secret-id $${terraform output -raw edw_db_secret} --query SecretString --output text`
+export host="$ip4 $APPNAME-$ENV $APPNAME.${local.application_data.accounts[local.environment].edw_dns_extension}"
+export host2="${local.application_data.accounts[local.environment].edw_cis_ip} cis.aws.${local.application_data.accounts[local.environment].edw_environment}.legalservices.gov.uk"
+export host3="${local.application_data.accounts[local.environment].edw_eric_ip} eric.aws.${local.application_data.accounts[local.environment].edw_environment}.legalservices.gov.uk"
+export host3="${local.application_data.accounts[local.environment].edw_ccms_ip} ccms.aws.${local.application_data.accounts[local.environment].edw_environment}.legalservices.gov.uk"
+echo $host >>/etc/hosts
+echo $host2 >>/etc/hosts
+echo $host3 >>/etc/hosts
+mkdir -p /home/oracle/scripts
+
+##### METADATA #####
+
+#### Install_aws_logging
+
+# Error handeling
+error_exit()
+{
+echo "$1" 1>&2
+exit 1
+}
+
+#Install AWS logs
+echo "---Install AWS logging"
+curl -O https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
+/usr/local/bin/python2.7 awslogs-agent-setup.py --no-proxy=NO_PROXY -n -r eu-west-2 -c /tmp/cwlogs/logstreams.conf || exit 2
+
+
+#### setup_file_systems
+
+# Create Oracle DBF file file system
+mkfs.ext4 /dev/xvdf
+mkdir -p /oracle/dbf
+echo "/dev/xvdf /oracle/dbf ext4 defaults 0 0" >> /etc/fstab
+
+# Create stage file system
+mkfs.ext4 /dev/xvdg
+mkdir -p /stage
+echo "/dev/xvdg /stage ext4 defaults 0 0" >> /etc/fstab
+
+# Create archive file system
+mkfs.ext4 /dev/xvdh
+mkdir -p /oracle/ar
+echo "/dev/xvdh /oracle/ar ext4 defaults 0 0" >> /etc/fstab
+
+#Create oracle_home
+mkfs.ext4 /dev/xvdi
+mkdir -p /oracle/software
+echo "/dev/xvdi /oracle/software ext4 defaults 0 0" >> /etc/fstab
+
+#Create temp_undo
+mkfs.ext4 /dev/xvdj
+mkdir -p /oracle/temp_undo
+echo "/dev/xvdj /oracle/temp_undo ext4 defaults 0 0" >> /etc/fstab
+
+# Mount all file systems in fstab
+mount -a
+chmod 777 /stage
+
+
+#### setup_oracle_db_software
+
+# Install wget / unzip
+yum install -y unzip
+
+groupadd dba
+groupadd oinstall
+useradd -d /home/oracle -g dba oracle
+
+#setup oracle user access
+cp -fr /home/ec2-user/.ssh /home/oracle/
+chown -R oracle:dba /home/oracle/.ssh
+
+# Unzip installers
+mkdir -p /stage/databases
+mkdir -p /stage/patches/1020
+unzip /repo/databases/10.2/installers/B24792-01_1of5.zip -d /stage/databases
+unzip /repo/databases/10.2/installers/B24792-01_2of5.zip -d /stage/databases
+unzip /repo/databases/10.2/installers/B24792-01_3of5.zip -d /stage/databases
+unzip /repo/databases/10.2/installers/B24792-01_4of5.zip -d /stage/databases
+unzip /repo/databases/10.2/installers/B24792-01_5of5.zip -d /stage/databases
+unzip /repo/databases/10.2/patches/db-patchset10204/p6810189_10204_Linux-x86-64.zip -d /stage/patches/10204
+
+# Create directories and set ownership
+mkdir -p /oracle/software/oraInventory
+mkdir -p /oracle/software/product
+mkdir -p /oracle/software/product/10.2.0
+mkdir -p /oracle/software/product/10.2.0_owb
+chown -R oracle:dba /oracle
+
+# Create swap space
+dd if=/dev/zero of=/swapfile bs=1024M count=9
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+# Run Oracle installer
+cp -f /repo/databases/10.2/templates/db-install-10g.rsp /run/cfn-init/db-install-10g.rsp
+chmod 777 /run/cfn-init/db-install-10g.rsp
+sed -i 's/{{ oracle_database_inventory_location }}/"\/oracle\/software\/oraInventory"/g' /run/cfn-init/db-install-10g.rsp
+sed -i 's/{{ oracle_database_oracle_home }}/"\/oracle\/software\/product\/10.2.0"/g'     /run/cfn-init/db-install-10g.rsp
+sed -i 's/{{ oracle_database_oracle_base }}/"\/oracle\/software\/product"/g'             /run/cfn-init/db-install-10g.rsp
+sed -i 's/{{ oracle_database_edition }}/"EE"/g'                                          /run/cfn-init/db-install-10g.rsp
+sed -i 's/{{ oracle_database_os_group }}/"dba"  /g'                                      /run/cfn-init/db-install-10g.rsp
+sed -i 's/{{ oracle_database_oracle_home_name }}/"db_home"/g'                            /run/cfn-init/db-install-10g.rsp
+
+
+# Run installer and post install
+export ORA_DISABLED_CVU_CHECKS=CHECK_RUN_LEVEL
+su oracle -c "/stage/databases/database/runInstaller -silent -waitforcompletion -ignoreSysPrereqs -ignorePrereq -responseFile /run/cfn-init/db-install-10g.rsp"
+
+/oracle/software/oraInventory/orainstRoot.sh -silent
+/oracle/software/product/10.2.0/root.sh -silent
+
+# Update oracle login script
+echo "export ORACLE_SID=EDW" >> /home/oracle/.bash_profile
+echo "export ORACLE_HOME=/oracle/software/product/10.2.0" >> /home/oracle/.bash_profile
+echo "export PATH=\$ORACLE_HOME/bin:\$PATH"           >> /home/oracle/.bash_profile
+
+# patch the database to 10.2.0.4
+cp -f /repo/databases/10.2/templates/patchset.rsp /home/oracle/patchset.rsp
+chown oracle:dba /home/oracle/patchset.rsp
+chmod 777 /home/oracle/patchset.rsp
+
+su oracle -c "/stage/patches/10204/Disk1/runInstaller -silent -responseFile /home/oracle/patchset.rsp"
+/oracle/software/product/10.2.0/root.sh -silent
+
+#### create_blank_database
+
+# Create a blank database
+cp  -f /repo/databases/10.2/templates/edw_warehouse.dbt /run/cfn-init/edw_warehouse.dbt
+chown oracle:dba /run/cfn-init/edw_warehouse.dbt
+chmod 777 /run/cfn-init/edw_warehouse.dbt
+
+su oracle -l -c "dbca -silent -createDatabase -templateName /run/cfn-init/edw_warehouse.dbt -gdbname $APPNAME -sid $APPNAME -responseFile NO_VALUE -characterSet WE8ISO8859P1 -sysPassword $SECRET -systemPassword $SECRET -databaseType DATA_WAREHOUSING  -datafileDestination "/oracle/dbf/" -MEMORYPERCENTAGE 70"
+
+# create listener
+cp -f /repo/databases/10.2/templates/netca.rsp /run/cfn-init/netca.rsp
+chmod 777 /run/cfn-init/netca.rsp
+su oracle -l -c "netca /silent /responseFile /run/cfn-init/netca.rsp"
+su oracle -l -c "lsnrctl start"
+
+mkdir -p /var/opt/oracle
+chown oracle:dba /var/opt/oracle
+cp /repo/edwcreate/passwds.sql /var/opt/oracle
+cp -r /repo/edwcreate /home/oracle
+chown -R oracle:dba /home/oracle/edwcreate
+chmod -R 777 /home/oracle/edwcreate
+chown oracle:dba /var/opt/oracle/passwds.sql
+chmod 777 /var/opt/oracle/passwds.sql
+su oracle -l -c "cp /home/oracle/edwcreate/tnsnames.ora /oracle/software/product/10.2.0/network/admin"
+sed -i "s/tst/${local.application_data.accounts[local.environment].edw_environment}/g" /oracle/software/product/10.2.0/network/admin/tnsnames.ora
+sed -i "s/EDW_SYS=welc0me/EDW_SYS=$SECRET/g" /var/opt/oracle/passwds.sql
+sed -i "s/EDW_SYSTEM=sysedw99/EDW_SYSTEM=$SECRET/g" /var/opt/oracle/passwds.sql
+
+chown -R oracle:dba /home/oracle/scripts/
+chmod -R 700 /home/oracle/scripts/
+chown oracle:dba /home/oracle
+chmod -R 777 /home/oracle
+
+#### Setup_owb
+
+# Create directories for OWB setup
+mkdir -p /stage/owb/owb101
+mkdir -p /stage/owb/owb104
+mkdir -p /stage/owb/owb105
+
+# Unzip OWB software packages
+unzip /repo/Software/OWB10/B30394-01_1of2.zip -d /stage/owb/owb101
+unzip /repo/Software/OWB10/B30394-01_2of2.zip -d /stage/owb/owb101
+unzip /repo/Software/OWB10/p7005587_10204_Linux-x86-64.zip -d /stage/owb/owb104
+unzip /repo/Software/OWB10/p8515097_10205_Linux-x86-64.zip -d /stage/owb/owb105
+
+# Copy response files to staging directory
+cp -f /repo/Software/OWB10/*.rsp /stage/owb/
+
+# Set permissions for staging directory
+chmod -R 777 /stage/owb/
+
+# Install OWB components
+su oracle -l -c "/stage/owb/owb101/Disk1/runInstaller -silent -ignoreSysPrereqs -ignorePrereq -waitforcompletion -responseFile /stage/owb/owb.rsp"
+/oracle/software/product/10.2.0_owb/root.sh -silent
+
+su oracle -l -c "/oracle/software/product/10.2.0/oui/bin/runInstaller -silent -waitforcompletion -responseFile /stage/owb/owb104.rsp"
+/oracle/software/product/10.2.0_owb/root.sh -silent
+
+su oracle -l -c "/oracle/software/product/10.2.0/oui/bin/runInstaller -silent -waitforcompletion -responseFile /stage/owb/owb105.rsp"
+/oracle/software/product/10.2.0_owb/root.sh -silent
+
+# Unzip additional files and configure environment
+unzip /repo/edwcreate/refresh.zip -d /stage
+unzip /repo/edwcreate/templates.zip -d /stage
+echo "export OMB_path=/oracle/software/product/10.2.0_owb/owb/bin/unix" >> /home/oracle/.bash_profile
+
+EOF
+
+}
+
+
 
 ####### EC2 Role #######
 resource "aws_iam_role" "edw_ec2_role" {
@@ -88,19 +342,9 @@ resource "aws_instance" "edw_db_instance" {
   key_name               = aws_key_pair.edw_ec2_key.key_name
   subnet_id              = data.aws_subnet.private_subnets_a.id
   vpc_security_group_ids = [aws_security_group.edw_db_security_group.id]
-  user_data = base64encode(templatefile("edw-ec2-user-data.sh", {
-    edw_app_name         = local.application_data.accounts[local.environment].edw_AppName
-    edw_dns_extension    = local.application_data.accounts[local.environment].edw_dns_extension
-    edw_environment      = local.application_data.accounts[local.environment].edw_environment
-    edw_region           = local.application_data.accounts[local.environment].edw_region
-    edw_ec2_role         = aws_iam_role.edw_ec2_role.name
-    edw_s3_backup_bucket = local.application_data.accounts[local.environment].edw_s3_backup_bucket
-    edw_cis_ip           = local.application_data.accounts[local.environment].edw_cis_ip
-    edw_eric_ip          = local.application_data.accounts[local.environment].edw_eric_ip
-    edw_ccms_ip          = local.application_data.accounts[local.environment].edw_ccms_ip
-  }))
-
-
+  user_data_base64            = base64encode(local.db_userdata)
+  user_data_replace_on_change = false
+  
   ebs_block_device {
     device_name = "/dev/sda1"
     volume_size = local.application_data.accounts[local.environment].edw_root_volume_size
