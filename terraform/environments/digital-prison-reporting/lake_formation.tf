ca125dc982c81b16a3b4b47a43f4776afbcb27bb
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-07-17 17:15:02 +0100

    Explanatory comments and more robust definition of lake formation admins

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -1,12 +1,15 @@
 resource "aws_lakeformation_data_lake_settings" "lake_formation" {
   admins = flatten([[for role in aws_iam_role.analytical_platform_share_role : role.arn], data.aws_iam_session_context.current.issuer_arn])
 
+  # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
   create_database_default_permissions {
+    # These settings should replicate current behaviour: LakeFormation is Ignored
     permissions = ["ALL"]
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
 
   create_table_default_permissions {
+    # These settings should replicate current behaviour: LakeFormation is Ignored
     permissions = ["ALL"]
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
