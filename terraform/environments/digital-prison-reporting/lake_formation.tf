e464e57b03e066062c92b8679d2429f21312068d
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-07-18 08:43:24 +0100

    Slight adjustment to admins declaration to handle non-sharing environments

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -1,5 +1,5 @@
 resource "aws_lakeformation_data_lake_settings" "lake_formation" {
-  admins = flatten([[for role in aws_iam_role.analytical_platform_share_role : role.arn], data.aws_iam_session_context.current.issuer_arn])
+  admins = flatten([[for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share].arn], data.aws_iam_session_context.current.issuer_arn])
 
   # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
   create_database_default_permissions {
