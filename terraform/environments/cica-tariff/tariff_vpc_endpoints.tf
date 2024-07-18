69d902c07ecf3bd8fe66cb0af53a8cfc438e6264
Edward Duddy <edward.duddy@cica.gov.uk>
2024-07-18 17:36:59 +0100

    💚 Fix type for subnet id
    Fix subnet ID types

--- terraform/environments/cica-tariff/tariff_vpc_endpoints.tf
+++ terraform/environments/cica-tariff/tariff_vpc_endpoints.tf
@@ -5,7 +5,7 @@ resource "aws_vpc_endpoint" "ssm" {
     vpc_id = data.aws_vpc.shared.id
     service_name = "com.amazonaws.eu-west-2.ssm"
     vpc_endpoint_type = "Interface"
-    subnet_ids = data.aws_subnets.shared-private
+    subnet_ids = data.aws_subnets.shared-private[*].id
     tags = merge(tomap({
     "Name"     = lower(format("ssm-%s-endpoint", local.application_name)),
     "hostname" = "${local.application_name}-app",
@@ -17,7 +17,7 @@ resource "aws_vpc_endpoint" "ec2messages" {
     vpc_id = data.aws_vpc.shared.id
     service_name = "com.amazonaws.eu-west-2.ec2messages"
     vpc_endpoint_type = "Interface"
-    subnet_ids = data.aws_subnets.shared-private
+    subnet_ids = data.aws_subnets.shared-private[*].id
     tags = merge(tomap({
     "Name"     = lower(format("ec2-messages-%s-endpoint", local.application_name)),
     "hostname" = "${local.application_name}-app",
@@ -31,7 +31,7 @@ resource "aws_vpc_endpoint" "ec2" {
     vpc_id = data.aws_vpc.shared.id
     service_name = "com.amazonaws.eu-west-2.ec2"
     vpc_endpoint_type = "Interface"
-    subnet_ids = data.aws_subnets.shared-private
+    subnet_ids = data.aws_subnets.shared-private[*].id
     tags = merge(tomap({
     "Name"     = lower(format("ec2-%s-endpoint", local.application_name)),
     "hostname" = "${local.application_name}-app",
@@ -42,7 +42,7 @@ resource "aws_vpc_endpoint" "ssm_messages" {
     vpc_id = data.aws_vpc.shared.id
     service_name = "com.amazonaws.eu-west-2.ssmmessages"
     vpc_endpoint_type = "Interface"
-    subnet_ids = data.aws_subnets.shared-private
+    subnet_ids = data.aws_subnets.shared-private[*].id
     tags = merge(tomap({
     "Name"     = lower(format("ssm-messages-%s-endpoint", local.application_name)),
     "hostname" = "${local.application_name}-app",
@@ -54,7 +54,7 @@ resource "aws_vpc_endpoint" "kms" {
     vpc_id = data.aws_vpc.shared.id
     service_name = "com.amazonaws.eu-west-2.kms"
     vpc_endpoint_type = "Interface"
-    subnet_ids = data.aws_subnets.shared-private
+    subnet_ids = data.aws_subnets.shared-private[*].id
     tags = merge(tomap({
     "Name"     = lower(format("kms-%s-endpoint", local.application_name)),
     "hostname" = "${local.application_name}-app",
@@ -66,7 +66,7 @@ resource "aws_vpc_endpoint" "logs" {
     vpc_id = data.aws_vpc.shared.id
     service_name = "com.amazonaws.eu-west-2.logs"
     vpc_endpoint_type = "Interface"
-    subnet_ids = data.aws_subnets.shared-private
+    subnet_ids = data.aws_subnets.shared-private[*].id
     tags = merge(tomap({
     "Name"     = lower(format("logs-%s-endpoint", local.application_name)),
     "hostname" = "${local.application_name}-app",
