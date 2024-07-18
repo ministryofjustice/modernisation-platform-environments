d5ed2bc0a473b5cc9f775cf018873b5af4d7bcd3
matt-heery <matt.heery@digital.justice.gov.uk>
2024-07-18 14:29:40 +0100

    renaming bucket

--- terraform/environments/electronic-monitoring-data/lambdas_main.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_main.tf
@@ -273,7 +273,7 @@ module "load_json_table" {
       DLT_PROJECT_DIR: "/tmp"
       DLT_DATA_DIR: "/tmp"
       DLT_PIPELINE_DIR: "/tmp"
-      BUCKET_URL = "s3://${aws_s3_bucket.data_store.id}/g4s/dev_access/2024-02-16/json"
+      BUCKET_URI = "s3://${module.json-directory-structure-bucket.bucket.id}"
       STANDARD_FILESYSTEM__QUERY_RESULT_BUCKET = "s3://${module.athena-s3-bucket.bucket.id}/output"
       SCHEMA_PATH = "s3://${module.metadata-s3-bucket.bucket.id}/dlt_schemas"
       }
