61685e0467e607fb30d268699e95e3b819fff74a
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-07-19 14:45:31 +0100

    Pulling out the getter iam actions and correcting service role arn

--- terraform/environments/digital-prison-reporting/policy.tf
+++ terraform/environments/digital-prison-reporting/policy.tf
@@ -760,13 +760,22 @@ data "aws_iam_policy_document" "analytical_platform_share_policy" {
     effect = "Allow"
     actions = [
       "iam:PutRolePolicy",
+    ]
+    resources = [
+      "arn:aws:iam::${local.current_account_id}:role/aws-service-role/lakeformation.amazonaws.com/AWSServiceRoleForLakeFormationDataAccess"
+    ]
+  }
+  # Needed for LakeFormationAdmin to check the presense of the Lake Formation Service Role
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:GetRolePolicy",
       "iam:GetRole"
     ]
     resources = [
-      "arn:aws:iam::${local.current_account_id}:role/*/AWSServiceRoleForLakeFormationDataAccess"
+      "*"
     ]
   }
-
   statement {
     effect = "Allow"
     actions = [
