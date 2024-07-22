e7edef61a8e16aa219c668d53c58eae61084097c
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-07-22 15:41:00 +0000

    :wrench: Define ollamate release

--- terraform/environments/analytical-platform-compute/helm-charts-applications.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-applications.tf
@@ -14,3 +14,20 @@ resource "helm_release" "ui" {
     )
   ]
 }
+
+resource "helm_release" "ollamate" {
+  /* https://github.com/ministryofjustice/analytical-platform-ollamate */
+  name       = "ui"
+  repository = "oci://ghcr.io/ministryofjustice/analytical-platform-charts"
+  version    = "0.0.0-rc1"
+  chart      = "analytical-platform-ollamate"
+  namespace  = kubernetes_namespace.ollamate.metadata[0].name
+  values = [
+    templatefile(
+      "${path.module}/src/helm/values/ollamate/values.yml.tftpl",
+      {
+        ollamate_hostname = "ollamate.${local.environment_configuration.route53_zone}"
+      }
+    )
+  ]
+}
