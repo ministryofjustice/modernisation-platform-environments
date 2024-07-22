5b7eccf4808356697bd6faf057a7da467af4bb84
Madhu Kadiri <madhu.kadiri2@justice.gov.uk>
2024-07-22 10:15:59 +0100

    cosmetic corrections-7

--- terraform/environments/electronic-monitoring-data/glue-job/rds_to_s3_parquet_migration.py
+++ terraform/environments/electronic-monitoring-data/glue-job/rds_to_s3_parquet_migration.py
@@ -741,6 +741,9 @@ if __name__ == "__main__":
             jdbc_read_partitions_num = int(rds_table_total_size_mb/1024)
         elif args.get("jdbc_read_2gb_partitions", "false") == "true":
             jdbc_read_partitions_num = int(int(rds_table_total_size_mb/1024)/2)
+        else:
+            raise ValueError(""">> When 'rds_table_total_size_mb' != 0, one of the 'jdbc_read_partition' size needs to be enabled ! <<""")
+            sys.exit(1)
     else:
         jdbc_read_partitions_num = int(args['default_jdbc_read_partition_num'])
     # ------------------------------
