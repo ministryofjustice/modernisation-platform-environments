c5241c6ec37205fd689c6a72dc2e08fe3d298de1
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-07-22 15:44:29 +0100

    onr: DSOS-2890: align terraform with nomis v3 (#7174)
    * add metric sharing
    
    * add dash

--- terraform/environments/oasys-national-reporting/locals_development.tf
+++ terraform/environments/oasys-national-reporting/locals_development.tf
@@ -2,6 +2,9 @@ locals {
 
   baseline_presets_development = {
     options = {
+      # disabling some features in development as the environment gets nuked
+      cloudwatch_metric_oam_links_ssm_parameters = []
+      cloudwatch_metric_oam_links                = []
     }
   }
 
