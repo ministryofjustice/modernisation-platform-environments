9b78429dd24eafddc14c2361e5dc334f72c3c979
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-07-23 11:18:29 +0100

    temp delete the rds (and any references to it), temp delete lambdas, make sure dbsubnetgroup is in a private subnet

--- terraform/environments/tribunals/rds.tf
+++ terraform/environments/tribunals/rds.tf
@@ -1,36 +1,31 @@
-resource "aws_db_instance" "rdsdb" {
-  allocated_storage = local.application_data.accounts[local.environment].allocated_storage
-  //db_name                 = DBName must be null for engine: sqlserver-se
-  storage_type   = local.application_data.accounts[local.environment].storage_type
-  identifier     = local.application_data.accounts[local.environment].db_identifier
-  engine         = local.application_data.accounts[local.environment].engine
-  engine_version = local.application_data.accounts[local.environment].engine_version
-  instance_class = local.application_data.accounts[local.environment].instance_class
-  username       = local.application_data.accounts[local.environment].username
-  password       = random_password.password.result
-  port           = 1433
+# resource "aws_db_instance" "rdsdb" {
+#   allocated_storage = local.application_data.accounts[local.environment].allocated_storage
+#   //db_name                 = DBName must be null for engine: sqlserver-se
+#   storage_type   = local.application_data.accounts[local.environment].storage_type
+#   identifier     = local.application_data.accounts[local.environment].db_identifier
+#   engine         = local.application_data.accounts[local.environment].engine
+#   engine_version = local.application_data.accounts[local.environment].engine_version
+#   instance_class = local.application_data.accounts[local.environment].instance_class
+#   username       = local.application_data.accounts[local.environment].username
+#   password       = random_password.password.result
+#   port           = 1433
 
-  skip_final_snapshot = true
+#   skip_final_snapshot = true
 
-  license_model       = "license-included"
-  publicly_accessible = false
+#   license_model       = "license-included"
+#   publicly_accessible = false
 
-  multi_az               = false
-  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.sqlserver_db_sc.id]
+#   multi_az               = false
+#   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+#   vpc_security_group_ids = [aws_security_group.sqlserver_db_sc.id]
 
-  tags = {
-    Name = "tribunals"
-  }
-}
+#   tags = {
+#     Name = "tribunals"
+#   }
+# }
 
 resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "dbsubnetgroup"
-  subnet_ids = data.aws_subnets.shared-private.ids
-}
-
-resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "rdssubnetgroup"
   subnet_ids = data.aws_subnets.shared-private.ids
 }
 
