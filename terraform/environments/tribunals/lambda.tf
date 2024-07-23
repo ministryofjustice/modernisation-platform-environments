749e627770e57dcb1414fed07df7aa17fb2a7e60
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-07-23 15:13:13 +0100

    Tribunals: move lambda_sg inbound rule to rds.tf

--- terraform/environments/tribunals/lambda.tf
+++ terraform/environments/tribunals/lambda.tf
@@ -174,12 +174,3 @@ resource "aws_security_group" "lambda_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group_rule" "allow_lambda_access_to_rds" {
-  type                     = "ingress"
-  from_port                = 1433
-  to_port                  = 1433
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.sqlserver_db_sc.id
-  source_security_group_id = aws_security_group.lambda_sg.id
-}
