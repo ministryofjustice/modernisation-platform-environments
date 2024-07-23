ea26f26d9454ae8c3901afbdffd6149cd7dea191
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-07-23 16:22:43 +0000

    Fix data location
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/data.tf
+++ terraform/environments/analytical-platform-compute/data.tf
@@ -1,5 +1,9 @@
 data "aws_availability_zones" "available" {}
 
+data "aws_iam_session_context" "current" {
+  arn = data.aws_caller_identity.current.arn
+}
+
 data "aws_ssoadmin_instances" "main" {
   provider = aws.sso-readonly
 }
