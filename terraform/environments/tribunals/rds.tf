e2d41ac49dc3fe792b37e566df65d1e7607d2402
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-07-23 10:54:41 +0100

    try setting the subnet group back to dbsubnetgroup

--- terraform/environments/tribunals/rds.tf
+++ terraform/environments/tribunals/rds.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "rdsdb" {
   publicly_accessible = false
 
   multi_az               = false
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.sqlserver_db_sc.id]
 
   tags = {
