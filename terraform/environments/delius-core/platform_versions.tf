539671d0792b3af1931d309d76fa17575d914b9a
David Sibley <david.sibley@digital.justice.gov.uk>
2024-07-23 15:29:04 +0100

    added external provider

--- terraform/environments/delius-core/platform_versions.tf
+++ terraform/environments/delius-core/platform_versions.tf
@@ -8,6 +8,10 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    external = {
+      source = "hashicorp/external"
+      version = "2.3.3"
+    }
   }
   required_version = "~> 1.0"
 }
