178908d2b83bd8dac7cffba00c0d98ef7382f7a0
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2024-07-23 13:58:13 +0100

    LAWS-3928 Allow Lambda ssh to database

--- terraform/environments/contract-work-administration/database.tf
+++ terraform/environments/contract-work-administration/database.tf
@@ -143,6 +143,15 @@ resource "aws_vpc_security_group_ingress_rule" "db_bastion_ssh" {
   to_port                      = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "db_lambda" {
+  security_group_id            = aws_security_group.database.id
+  description                  = "Allow Lambda SSH access for backup snapshots"
+  referenced_security_group_id = aws_security_group.backup_lambda.id
+  from_port                    = 22
+  ip_protocol                  = "tcp"
+  to_port                      = 22
+}
+
 resource "aws_vpc_security_group_ingress_rule" "db_workspaces_1" {
   security_group_id = aws_security_group.database.id
   description       = "DB access for Workspaces"
