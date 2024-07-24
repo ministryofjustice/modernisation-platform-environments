384f260cfa2fb4062fd9d4a40d54481e51b1636c
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-07-24 09:19:12 +0100

    align main.tf

--- terraform/environments/nomis-data-hub/locals_ec2_instances.tf
+++ terraform/environments/nomis-data-hub/locals_ec2_instances.tf
@@ -26,7 +26,6 @@ locals {
         instance_type                = "t3.xlarge"
         key_name                     = "ec2-user"
         metadata_options_http_tokens = "required"
-        monitoring                   = true
         vpc_security_group_ids       = ["ndh_app"]
         tags = {
           backup-plan = "daily-and-weekly"
@@ -77,7 +76,6 @@ locals {
         instance_type                = "t3.xlarge"
         key_name                     = "ec2-user"
         metadata_options_http_tokens = "required"
-        monitoring                   = true
         vpc_security_group_ids       = ["ndh_ems"]
         tags = {
           backup-plan = "daily-and-weekly"
