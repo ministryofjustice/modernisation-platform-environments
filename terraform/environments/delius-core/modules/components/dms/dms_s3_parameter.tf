75c3e19dab7b78e95b75001b285bea82a2292105
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-30 15:29:53 +0100

    Add new SSM Parameter for storing location of the DMS S3 target bucket

---
+++ terraform/environments/delius-core/modules/components/dms/dms_s3_parameter.tf
@@ -0,0 +1,11 @@
+# Since the DMS S3 bucket is generated with an arbitrary suffix, we need to store the name
+# in a well known location so that other accounts may find it.   Therefore we create an
+# SSM Parameter to store the bucket name.   It needs to be an Advanced Parameter so it
+# may be shared with the other accounts involved in the replication.
+resource "aws_ssm_parameter" "s3_bucket_dms_destination_name" {
+  name        = "s3-bucket-dms-destination-name"
+  description = "This parameter stores the location of the S3 Bucket for staging of DMS Replication data"
+  type        = "SecureString"
+  value       = "example-secure-value"
+  tier        = "Advanced"
+}
