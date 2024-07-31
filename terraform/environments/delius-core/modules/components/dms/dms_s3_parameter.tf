6efbb02861aab658bc192a6cc8e5917f90198810
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-31 11:55:58 +0100

    Add Resource Share for SSM Parameter

--- terraform/environments/delius-core/modules/components/dms/dms_s3_parameter.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_s3_parameter.tf
@@ -9,3 +9,23 @@ resource "aws_ssm_parameter" "s3_bucket_dms_destination_name" {
   value       = module.s3_bucket_dms_destination.bucket.bucket
   tier        = "Advanced"
 }
+
+# Create a resource share to allow sharing of the SSM parameter containing the bucket name
+resource "aws_ram_resource_share" "ssm_parameter_share_for_dms_bucket_name" {
+  name = "ssm-parameter-share-for-dms-bucket-name"
+}
+
+# Associate the SSM Parameter with this resource share
+resource "aws_ram_resource_association" "ssm_parameter_share_for_dms_bucket_name" {
+  resource_share_arn = aws_ram_resource_share.ssm_parameter_share_for_dms_bucket_name.arn
+  resource_arn       = aws_ssm_parameter.s3_bucket_dms_destination_name.arn
+}
+
+# We need to share the parameter with any remote clients associated with this account
+# and any remote repository associated with this account
+resource "aws_ram_principal_association" "ssm_parameter_share_for_dms_bucket_name" {
+  for_each           = toset(concat(var.dms_config.client_account_arns,local.dms_repository_account_id))
+  resource_share_arn = aws_ram_resource_share.ssm_parameter_share_for_dms_bucket_name.arn
+  principal          = each.value
+}
+
