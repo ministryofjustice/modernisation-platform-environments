4be0638525e2479cd1b525ae50bad3f90613cba9
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-07-31 12:13:30 +0100

    The repository account ID is a singleton list

--- terraform/environments/delius-core/modules/components/dms/locals.tf
+++ terraform/environments/delius-core/modules/components/dms/locals.tf
@@ -4,7 +4,7 @@ locals {
   oracle_port = "1521"
   dms_audit_username = "delius_audit_dms_pool"
   dms_s3_local_bucket_prefix = "${var.env_name}-dms-destination-bucket"
-  dms_repository_account_id = try(var.platform_vars.environment_management.account_ids[join("-", ["delius-core", var.dms_config.audit_target_endpoint.write_environment])],null)
+  dms_repository_account_id = nonsensitive(try(var.platform_vars.environment_management.account_ids[join("-", ["delius-core", var.dms_config.audit_target_endpoint.write_environment])],null))
   dms_s3_repository_bucket = {
     prefix = try("${var.dms_config.audit_target_endpoint.write_environment}-dms-destination-bucket",null)
     # account_id = try(var.platform_vars.environment_management.account_ids[join("-", ["delius-core", var.dms_config.audit_target_endpoint.write_environment])],null)
