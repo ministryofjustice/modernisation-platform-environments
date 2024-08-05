0943a51929356b8de59ccf5aa00880037e6e2214
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-08-05 15:32:43 +0100

    Require the bucket name

--- terraform/environments/delius-core/outputs.tf
+++ terraform/environments/delius-core/outputs.tf
@@ -16,3 +16,14 @@ output "dms_client_account_ids" {
   value = local.dms_client_account_ids
   sensitive = true
 }
+
+output "delius_account_names" {
+  value = local.delius_account_names
+}
+
+output "dms_s3_bucket_name" {
+ value = (
+   local.is-development ? module.environment_dev[0].dms_s3_bucket_name :
+     local.is-test ? module.environment_test[0].dms_s3_bucket_name :
+       local.is-preproduction ? module.environment_preprod[0].dms_s3_bucket_name : null )
+}
