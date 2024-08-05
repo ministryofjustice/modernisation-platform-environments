d50e5113973383370a4d9879e0f0a18daf8db1dc
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-08-05 18:37:55 +0100

    Do not create s3 endpoint if target role does not exist

--- terraform/environments/delius-core/modules/components/dms/dms_s3_cross_account_buckets.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_s3_cross_account_buckets.tf
@@ -15,9 +15,7 @@ locals {
     for bucket_map in local.dms_s3_bucket_list : bucket_map
   ]...)
 
-  dms_s3_name_to_account_id_map = [for account_name in var.delius_account_names : try(data.terraform_remote_state.get_dms_s3_bucket_info[account_name].outputs.aws_account_id,null) ]
-
-  dms_s3_role_exists = [for account_name in var.delius_account_names : try(data.terraform_remote_state.get_dms_s3_bucket_info[account_name].outputs.dms_s3_bucket_info.dms_s3_role_arn,null) == null ? false : true ]
+  dms_s3_role_exists = {for account_name in var.delius_account_names : account_name => try(data.terraform_remote_state.get_dms_s3_bucket_info[account_name].outputs.dms_s3_bucket_info.dms_s3_role_arn,null) == null ? false : true }
 
 }
 
