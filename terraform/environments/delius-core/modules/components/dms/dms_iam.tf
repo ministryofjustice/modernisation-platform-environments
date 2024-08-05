79867b3fa1df5810a71228388009f4eb0cf5e7a7
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-08-05 16:03:56 +0100

    DMS must assume local role

--- terraform/environments/delius-core/modules/components/dms/dms_iam.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_iam.tf
@@ -31,44 +31,89 @@ resource "aws_iam_role_policy_attachment" "dms-vpc-role-AmazonDMSVPCManagementRo
 
 # Create a Role to Allow any Audit Clients of this environment, or any Repository
 # for this environment to write to the DMS S3 bucket
+# resource "aws_iam_role" "dms_s3_writer_role" {
+#   count = length(local.dms_s3_writer_account_ids) > 0 ? 1 : 0
+#   name = local.dms_s3_writer_role_name
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       for principal in local.dms_s3_writer_account_ids:
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           AWS = "arn:aws:iam::${principal}:root"
+#         },
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
+
+
 resource "aws_iam_role" "dms_s3_writer_role" {
-  count = length(local.dms_s3_writer_account_ids) > 0 ? 1 : 0
   name = local.dms_s3_writer_role_name
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
-      for principal in local.dms_s3_writer_account_ids:
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::${principal}:root"
-        },
+          Service = "dms.amazonaws.com"
+        }
         Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-data "aws_iam_policy_document" "dms_s3_writer_policy_data"  {
- statement {
-    actions = [
-      "s3:PutObject",
-      "s3:PutObjectAcl"
+resource "aws_s3_bucket_policy" "dms_s3_bucket_policy" {
+  bucket = module.s3_bucket_dms_destination.bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      for account_id in local.dms_s3_writer_account_ids : {
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${account_id}:role/${local.dms_s3_writer_role_name}"
+        }
+        Action    = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource  = [
+          "${module.s3_bucket_dms_destination.bucket.arn}/*"
+        ]
+      }
     ]
-
-    resources = [ "arn:aws:s3:::${module.s3_bucket_dms_destination.bucket.bucket}" ]
-  }
+  })
 }
 
-resource "aws_iam_policy" "dms_s3_writer_policy" {
-  count       = length(local.dms_s3_writer_account_ids) > 0 ? 1 : 0
-  name        = "dms-s3-writer-policy"
-  description = "Policy to allow audit clients and repository putting data to the DMS S3 bucket"
-  policy      = data.aws_iam_policy_document.dms_s3_writer_policy_data.json
-}
 
-resource "aws_iam_role_policy_attachment" "dms_s3_writer_policy_attachment" {
-  count      = length(local.dms_s3_writer_account_ids) > 0 ? 1 : 0
-  role       = aws_iam_role.dms_s3_writer_role[0].name
-  policy_arn = aws_iam_policy.dms_s3_writer_policy[0].arn
-}
+
+
+
+
+# data "aws_iam_policy_document" "dms_s3_writer_policy_data"  {
+#  statement {
+#     actions = [
+#       "s3:PutObject",
+#       "s3:PutObjectAcl"
+#     ]
+
+#     resources = [ module.s3_bucket_dms_destination.bucket.arn ]
+#   }
+# }
+
+# resource "aws_iam_policy" "dms_s3_writer_policy" {
+#   count       = length(local.dms_s3_writer_account_ids) > 0 ? 1 : 0
+#   name        = "dms-s3-writer-policy"
+#   description = "Policy to allow audit clients and repository putting data to the DMS S3 bucket"
+#   policy      = data.aws_iam_policy_document.dms_s3_writer_policy_data.json
+# }
+
+# resource "aws_iam_role_policy_attachment" "dms_s3_writer_policy_attachment" {
+#   count      = length(local.dms_s3_writer_account_ids) > 0 ? 1 : 0
+#   role       = aws_iam_role.dms_s3_writer_role[0].name
+#   policy_arn = aws_iam_policy.dms_s3_writer_policy[0].arn
+# }
