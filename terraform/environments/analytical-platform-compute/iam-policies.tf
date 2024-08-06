adb1b3e4ea28a9b80624686e1015e506e257b940
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-08-06 16:14:15 +0100

    Remove headobject
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/iam-policies.tf
+++ terraform/environments/analytical-platform-compute/iam-policies.tf
@@ -133,8 +133,7 @@ data "aws_iam_policy_document" "mlflow" {
     actions = [
       "s3:PutObject",
       "s3:GetObject",
-      "s3:DeleteObject",
-      "s3:HeadObject"
+      "s3:DeleteObject"
     ]
     resources = [
       "${module.mlflow_bucket.s3_bucket_arn}/*",
