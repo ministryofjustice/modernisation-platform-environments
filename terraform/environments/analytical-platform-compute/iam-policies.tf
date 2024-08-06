54f199f4f0d45986655ca3073544743d158526e8
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-08-06 15:42:03 +0000

    Backport
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-compute/iam-policies.tf
+++ terraform/environments/analytical-platform-compute/iam-policies.tf
@@ -133,12 +133,11 @@ data "aws_iam_policy_document" "mlflow" {
     actions = [
       "s3:PutObject",
       "s3:GetObject",
-      "s3:DeleteObject",
-      "s3:ListBucket"
+      "s3:DeleteObject"
     ]
     resources = [
       "${module.mlflow_bucket.s3_bucket_arn}/*",
-      "arn:aws:s3:::${local.environment_configuration.mlflow_s3_bucket_name}"
+      "arn:aws:s3:::${local.environment_configuration.mlflow_s3_bucket_name}/*"
     ]
   }
 }
