72e6d98f43369e367a9d74a4ad94701154f3f760
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-08-06 11:28:39 +0100

    Merge role exists maps

--- terraform/environments/delius-core/modules/components/dms/dms_s3_cross_account_buckets.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_s3_cross_account_buckets.tf
@@ -15,9 +15,13 @@ locals {
     for bucket_map in local.dms_s3_bucket_list : bucket_map
   ]...)
 
-  dms_s3_existing_roles = [for account_name in var.delius_account_names : {
+  dms_s3_existing_roles_list = [for account_name in var.delius_account_names : {
                              for delius_environment_name in var.delius_environment_names : delius_environment_name => try(data.terraform_remote_state.get_dms_s3_bucket_info[account_name].outputs.dms_s3_bucket_info.dms_s3_role_arn[delius_environment_name],null) == null ? false : true
                             }
                           ]
+
+  dms_s3_cross_account_existing_roles = merge([
+    for role_exists_map in local.dms_s3_existing_roles_list : role_exists_map
+  ]...)  
 
 }
