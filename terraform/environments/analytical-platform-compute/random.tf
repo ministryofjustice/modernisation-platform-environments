17bdc3b0e60daf7c5b2f047b398ff32e42de0a01
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-08-12 12:41:12 +0100

    Removing ollamate

--- terraform/environments/analytical-platform-compute/random.tf
+++ terraform/environments/analytical-platform-compute/random.tf
@@ -22,13 +22,3 @@ resource "random_password" "ui_app_secrets" {
   length  = 32
   special = false
 }
-
-resource "random_password" "ollamate_rds" {
-  length  = 32
-  special = false
-}
-
-resource "random_password" "ollamate_app_secrets" {
-  length  = 32
-  special = false
-}
