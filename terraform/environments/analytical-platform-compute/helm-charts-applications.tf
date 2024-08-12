116a23f172fd07109014449a2320315c866dcc5a
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-08-12 12:42:47 +0100

    Removing ollamate

--- terraform/environments/analytical-platform-compute/helm-charts-applications.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-applications.tf
@@ -10,23 +10,7 @@ resource "helm_release" "ui" {
       "${path.module}/src/helm/values/ui/values.yml.tftpl",
       {
         ui_hostname = local.environment_configuration.ui_hostname
-      }
-    )
-  ]
-}
-
-resource "helm_release" "ollamate" {
-  /* https://github.com/ministryofjustice/analytical-platform-ollamate */
-  name       = "ollamate"
-  repository = "oci://ghcr.io/ministryofjustice/analytical-platform-charts"
-  version    = "0.0.1"
-  chart      = "analytical-platform-ollamate"
-  namespace  = kubernetes_namespace.ollamate.metadata[0].name
-  values = [
-    templatefile(
-      "${path.module}/src/helm/values/ollamate/values.yml.tftpl",
-      {
-        ollamate_hostname = "ollamate.${local.environment_configuration.route53_zone}"
+        eks_role_arn = module.lake_formation_share_role.
       }
     )
   ]
--- terraform/environments/analytical-platform-compute/kms-keys.tf
+++ terraform/environments/analytical-platform-compute/kms-keys.tf
@@ -339,19 +339,3 @@ module "ui_rds_kms" {
 
   tags = local.tags
 }
-
-module "ollamate_rds_kms" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
-
-  source  = "terraform-aws-modules/kms/aws"
-  version = "3.1.0"
-
-  aliases               = ["rds/ollamate"]
-  description           = "Ollamate RDS KMS key"
-  enable_default_policy = true
-
-  deletion_window_in_days = 7
-
-  tags = local.tags
-}
