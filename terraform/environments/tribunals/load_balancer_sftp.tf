15684d6c8c0a5e73c0b6b17d8e439440b8b1f416
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-08-13 14:42:24 +0100

    temporarily delete sftp LB, LB listener, LB target group attachment

--- terraform/environments/tribunals/load_balancer_sftp.tf
+++ terraform/environments/tribunals/load_balancer_sftp.tf
@@ -2,14 +2,14 @@ locals {
   target_group_arns_sftp = { for k, v in aws_lb_target_group.tribunals_target_group_sftp : k => v.arn }
 }
 
-resource "aws_lb" "tribunals_lb_sftp" {
-  name                       = "tribunals-sftp-lb"
-  load_balancer_type         = "network"
-  security_groups            = [aws_security_group.tribunals_lb_sc_sftp.id]
-  subnets                    = data.aws_subnets.shared-public.ids
-  enable_deletion_protection = false
-  internal                   = false
-}
+# resource "aws_lb" "tribunals_lb_sftp" {
+#   name                       = "tribunals-sftp-lb"
+#   load_balancer_type         = "network"
+#   security_groups            = [aws_security_group.tribunals_lb_sc_sftp.id]
+#   subnets                    = data.aws_subnets.shared-public.ids
+#   enable_deletion_protection = false
+#   internal                   = false
+# }
 
 resource "aws_security_group" "tribunals_lb_sc_sftp" {
   name        = "tribunals-load-balancer-sg-sftp"
@@ -35,17 +35,17 @@ resource "aws_security_group" "tribunals_lb_sc_sftp" {
   }
 }
 
-resource "aws_lb_listener" "tribunals_lb_sftp" {
-  for_each          = var.sftp_services
-  load_balancer_arn = aws_lb.tribunals_lb_sftp.arn
-  port              = each.value.sftp_port
-  protocol          = "TCP"
+# resource "aws_lb_listener" "tribunals_lb_sftp" {
+#   for_each          = var.sftp_services
+#   load_balancer_arn = aws_lb.tribunals_lb_sftp.arn
+#   port              = each.value.sftp_port
+#   protocol          = "TCP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tribunals_target_group_sftp[each.key].arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.tribunals_target_group_sftp[each.key].arn
+#   }
+# }
 
 resource "aws_lb_target_group" "tribunals_target_group_sftp" {
   for_each             = var.sftp_services
@@ -65,9 +65,9 @@ resource "aws_lb_target_group" "tribunals_target_group_sftp" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "tribunals_target_group_attachment_sftp" {
-  for_each         = aws_lb_target_group.tribunals_target_group_sftp
-  target_group_arn = each.value.arn
-  target_id        = element(data.aws_instances.tribunals_instance.ids, 0)
-  port             = each.value.port
-}
+# resource "aws_lb_target_group_attachment" "tribunals_target_group_attachment_sftp" {
+#   for_each         = aws_lb_target_group.tribunals_target_group_sftp
+#   target_group_arn = each.value.arn
+#   target_id        = element(data.aws_instances.tribunals_instance.ids, 0)
+#   port             = each.value.port
+# }
