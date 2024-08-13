eba14cde25938953d71151c9c20bd0173d4c8c3e
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-08-13 10:16:25 +0100

    Add test bucket for module logging

--- terraform/environments/electronic-monitoring-data/s3.tf
+++ terraform/environments/electronic-monitoring-data/s3.tf
@@ -95,9 +95,6 @@ module "metadata-s3-bucket" {
     aws.bucket-replication = aws
   }
 
-  log_bucket = module.s3-logging-bucket.bucket.id
-  log_prefix = "metadata-store/"
-
   lifecycle_rule = [
     {
       id      = "main"
@@ -459,4 +456,79 @@ module "json-directory-structure-bucket-logs" {
   source_bucket = module.json-directory-structure-bucket.bucket
   account_id    = data.aws_caller_identity.current.account_id
   local_tags    = local.tags
+}
+
+
+
+# ------------------------------------------------------------------------
+# testing Bucket
+# ------------------------------------------------------------------------
+
+module "data-store2-bucket" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=71132ac68fde5f545a66642159d780a36d69a89e"
+
+  bucket_prefix      = "testing-logs"
+  versioning_enabled = true
+
+  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
+  ownership_controls = "BucketOwnerEnforced"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below two variables and providers configuration are only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  # replication_role_arn                     = module.s3-bucket.role.arn
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
+  }
+
+  log_bucket = module.s3-logging-bucket.bucket.id
+  log_partition_date_source = "EventTime"
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = merge(local.tags)
 }
