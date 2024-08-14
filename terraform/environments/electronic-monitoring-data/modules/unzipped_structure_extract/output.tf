e6e693a71d70a59add168bb39de84e178a2128f9
matt-heery <matt.heery@digital.justice.gov.uk>
2024-08-14 13:27:27 +0100

    lambda!!!

--- terraform/environments/electronic-monitoring-data/modules/unzipped_structure_extract/output.tf
+++ terraform/environments/electronic-monitoring-data/modules/unzipped_structure_extract/output.tf
@@ -1,11 +1,11 @@
 output "lambda_function_name" {
-  value = module.this.function_name
+  value = module.this.lambda_function_name
 }
 
 output "lambda_function_arn" {
-  value = module.this.arn
+  value = module.this.lambda_function_arn
 }
 
 output "lambda_function_invoke_arn" {
-  value = module.this.invoke_arn
+  value = module.this.lambda_function_invoke_arn
 }
