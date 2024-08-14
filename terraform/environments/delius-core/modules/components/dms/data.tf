44578c272f71232d0a653cc36384a7b5b11d67ea
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-08-14 18:22:37 +0100

    Add User S3 Endpoint

--- terraform/environments/delius-core/modules/components/dms/data.tf
+++ terraform/environments/delius-core/modules/components/dms/data.tf
@@ -1,3 +1,11 @@
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
+
+data "aws_ec2_managed_prefix_list" "s3" {
+  name = "com.amazonaws.${data.aws_region.current.name}.s3"
+}
+
+data "aws_prefix_list" "s3" {
+  prefix_list_id = data.aws_ec2_managed_prefix_list.s3.id
+}
