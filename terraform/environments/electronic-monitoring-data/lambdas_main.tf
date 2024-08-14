7094e3b698508587a6dc87cb47c164c1f4242168
matt-heery <matt.heery@digital.justice.gov.uk>
2024-08-14 13:12:11 +0100

    two missing args

--- terraform/environments/electronic-monitoring-data/lambdas_main.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_main.tf
@@ -294,6 +294,8 @@ module "load_g4s_atrium_unstructured" {
   timeout                 = 900
   function_tag            = "v0.0.0-884806f"
   dataset_name            = "g4s_atrium_unstructured"
+  env_account_id          = local.env_account_id
+  core_shared_services_id = local.environment_management.account_ids["core-shared-services-production"]
   production_dev          = local.is-production ? "prod" : "dev"
   json_bucket_name        = module.json-directory-structure-bucket.bucket.id
   athena_bucket_name      = module.athena-s3-bucket.bucket.id
