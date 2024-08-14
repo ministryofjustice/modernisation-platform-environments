01d09b76a72bfee5efb3f44ba0ffb432c8e50ccd
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-08-14 10:10:58 +0100

    Add Security Group rule to allow access to S3 Endpoint

--- terraform/environments/delius-core/modules/components/dms/dms_s3_vpc_endpoint.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_s3_vpc_endpoint.tf
@@ -1,15 +1,25 @@
-# For AWS DMS version 3.4.7 and higher, DMS must access the source bucket through a VPC endpoint or a public route.
-# Ref: https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Target.S3.html#CHAP_Target.S3.Prerequisites
-data "aws_route_tables" "all_route_tables" {
-  filter {
-    name   = "vpc-id"
-    values = [var.account_info.vpc_id]
-  }
+# # For AWS DMS version 3.4.7 and higher, DMS must access the source bucket through a VPC endpoint or a public route.
+# # Ref: https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Target.S3.html#CHAP_Target.S3.Prerequisites
+# An S3 VPC already exists (supplied by MP using PrivateLink)
+# data "aws_route_tables" "all_route_tables" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [var.account_info.vpc_id]
+#   }
+# }
+
+# resource "aws_vpc_endpoint" "s3_bucket_dms_destination" {
+#   vpc_id            = var.account_info.vpc_id
+#   service_name      = "com.amazonaws.${var.account_info.region}.s3"
+#   vpc_endpoint_type = "Gateway"
+#   route_table_ids   = data.aws_route_tables.all_route_tables.ids
+# }
+
+
+data "aws_ec2_managed_prefix_list" "s3" {
+  name = "com.amazonaws.${data.aws_region.current.name}.s3"
 }
 
-resource "aws_vpc_endpoint" "s3_bucket_dms_destination" {
-  vpc_id            = var.account_info.vpc_id
-  service_name      = "com.amazonaws.${var.account_info.region}.s3"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids   = data.aws_route_tables.all_route_tables.ids
+data "aws_prefix_list" "s3" {
+  prefix_list_id = data.aws_ec2_managed_prefix_list.s3.id
 }
