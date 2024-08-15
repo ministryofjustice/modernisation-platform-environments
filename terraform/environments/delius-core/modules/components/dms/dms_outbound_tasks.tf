225faaaa5114222ce5eb0431307f6cd73a2052ba
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-08-15 14:00:49 +0100

    Add inbound endpoint

--- terraform/environments/delius-core/modules/components/dms/dms_outbound_tasks.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_outbound_tasks.tf
@@ -1,7 +1,7 @@
 # Audit outbound replication only happens in client environments
 resource "aws_dms_replication_task" "audited_interaction_outbound_replication" {
   count               = try(var.dms_config.audit_source_endpoint.read_database, null) == null ? 0 : 1
-  replication_task_id = "${var.env_name}-audited-interaction-replication-task-for-${lower(var.dms_config.audit_source_endpoint.read_database)}"
+  replication_task_id = "${var.env_name}-audited-interaction-outbound-replication-task-for-${lower(var.dms_config.audit_source_endpoint.read_database)}"
   migration_type      = "cdc" 
 
   # Even though we have the option to read from a standby database, we always record the name of the *primary*
@@ -12,11 +12,11 @@ resource "aws_dms_replication_task" "audited_interaction_outbound_replication" {
   # This allows us to resume after downtime on an endpoint but note that this means that errors will not be raised
   # and must be monitored independently.
   #
-  table_mappings      = templatefile("templates/audited_interaction_table_mapping.tmpl",{
+  table_mappings      = templatefile("templates/audited_interaction_outbound_table_mapping.tmpl",{
                            client_database = local.audit_source_primary
                         })
 
-  replication_task_settings = file("files/audited_interaction_settings.json")
+  replication_task_settings = file("files/audited_interaction_outbound_settings.json")
 
   source_endpoint_arn      = aws_dms_endpoint.dms_audit_source_endpoint_db[0].endpoint_arn
   target_endpoint_arn      = aws_dms_s3_endpoint.dms_audit_target_endpoint_s3[0].endpoint_arn
@@ -41,15 +41,15 @@ resource "aws_dms_replication_task" "audited_interaction_outbound_replication" {
 # This replicates records from the USER_ and PROBATION_AREA_USER tables
 resource "aws_dms_replication_task" "user_outbound_replication" {
   for_each            = toset(try(local.dms_s3_cross_account_client_environments[var.env_name],[]))
-  replication_task_id = "${var.env_name}-user-replication-task-for-${lower(var.dms_config.user_source_endpoint.read_database)}-to-${each.value}"
+  replication_task_id = "${var.env_name}-user-outbound-replication-task-for-${lower(var.dms_config.user_source_endpoint.read_database)}-to-${each.value}"
   # We do not support a full load since this would require a cascading delete of multiple
   # records; instead we only CDC user and probation records.  If this task is
   # restarted we should set the restart time accordingly to pick up only changes
   # to users and probation area records.
   migration_type      = "cdc" 
 
-  table_mappings            = file("files/user__table_mapping.json")
-  replication_task_settings = file("files/user__settings.json")
+  table_mappings            = file("files/user_outbound_table_mapping.json")
+  replication_task_settings = file("files/user_outbound_settings.json")
 
   source_endpoint_arn      = aws_dms_endpoint.dms_user_source_endpoint_db[0].endpoint_arn
   target_endpoint_arn      = aws_dms_s3_endpoint.dms_user_target_endpoint_s3[each.value].endpoint_arn
