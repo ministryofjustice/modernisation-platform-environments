cd0921f801a8e372089bbeab62ea1658139a9c29
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-08-19 13:43:30 +0100

    Testing if additional permissons to the deployment role allow management of groups in code

--- terraform/environments/analytical-platform-compute/quicksight.tf
+++ terraform/environments/analytical-platform-compute/quicksight.tf
@@ -3,7 +3,7 @@ resource "aws_quicksight_account_subscription" "subscription" {
   edition                          = "ENTERPRISE"
   authentication_method            = "IAM_IDENTITY_CENTER"
   iam_identity_center_instance_arn = tolist(data.aws_ssoadmin_instances.main.arns)[0]
-  admin_group                      = ["analytical-platform"]
-  author_group                     = ["analytical-platform"]
+  admin_group                      = ["analytical-platform", "entraid-aws-identitycenter-analytical-platform"]
+  author_group                     = ["analytical-platform", "entraid-aws-identitycenter-analytical-platform"]
   notification_email               = local.environment_configuration.quicksight_notification_email
 }
