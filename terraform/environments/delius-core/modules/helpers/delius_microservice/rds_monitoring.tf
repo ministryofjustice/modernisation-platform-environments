65499bcab93fddd7b7578831d5b9cd481ae3a741
George Taylor <george.taylor@digital.justice.gov.uk>
2024-08-19 16:17:37 +0100

    Update rds_monitoring.tf

--- terraform/environments/delius-core/modules/helpers/delius_microservice/rds_monitoring.tf
+++ terraform/environments/delius-core/modules/helpers/delius_microservice/rds_monitoring.tf
@@ -26,18 +26,17 @@ resource "aws_cloudwatch_metric_alarm" "rds_cpu_over_threshold" {
   )
 }
 
-resource "aws_cloudwatch_metric_alarm" "ram_over_threshold" {
+resource "aws_cloudwatch_metric_alarm" "rds_memory_over_threshold" {
   count              = var.create_rds ? 1 : 0
-  alarm_name         = "${var.name}-rds-ram-threshold"
-  alarm_description  = "Triggers alarm if RDS RAM crosses a threshold"
+  alarm_name         = "${var.name}-rds-memory-threshold"
+  alarm_description  = "Triggers alarm if RDS Memory crosses a threshold"
   namespace          = "AWS/RDS"
   metric_name        = "FreeableMemory"
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "10"
-  # add sns topic later
-  #  alarm_actions       = [aws_sns_topic.alerting.arn]
-  #  ok_actions          = [aws_sns_topic.alerting.arn]
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
   threshold           = "800000000"
   treat_missing_data  = "missing"
   comparison_operator = "LessThanThreshold"
@@ -54,7 +53,7 @@ resource "aws_cloudwatch_metric_alarm" "ram_over_threshold" {
   )
 }
 
-resource "aws_cloudwatch_metric_alarm" "read_latency_over_threshold" {
+resource "aws_cloudwatch_metric_alarm" "rds_read_latency_over_threshold" {
   count              = var.create_rds ? 1 : 0
   alarm_name         = "${var.name}-rds-read-latency-threshold"
   alarm_description  = "Triggers alarm if RDS read latency crosses a threshold"
@@ -63,9 +62,8 @@ resource "aws_cloudwatch_metric_alarm" "read_latency_over_threshold" {
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "5"
-  # add sns topic later
-  #  alarm_actions       = [aws_sns_topic.alerting.arn]
-  #  ok_actions          = [aws_sns_topic.alerting.arn]
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
   threshold           = "5"
   treat_missing_data  = "missing"
   comparison_operator = "GreaterThanThreshold"
@@ -82,7 +80,7 @@ resource "aws_cloudwatch_metric_alarm" "read_latency_over_threshold" {
   )
 }
 
-resource "aws_cloudwatch_metric_alarm" "write_latency_over_threshold" {
+resource "aws_cloudwatch_metric_alarm" "rds_write_latency_over_threshold" {
   count              = var.create_rds ? 1 : 0
   alarm_name         = "${var.name}-rds-write-latency-threshold"
   alarm_description  = "Triggers alarm if RDS write latency crosses a threshold"
@@ -91,9 +89,8 @@ resource "aws_cloudwatch_metric_alarm" "write_latency_over_threshold" {
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "5"
-  # add sns topic later
-  #  alarm_actions       = [aws_sns_topic.alerting.arn]
-  #  ok_actions          = [aws_sns_topic.alerting.arn]
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
   threshold           = "5"
   treat_missing_data  = "missing"
   comparison_operator = "GreaterThanThreshold"
@@ -110,7 +107,7 @@ resource "aws_cloudwatch_metric_alarm" "write_latency_over_threshold" {
   )
 }
 
-resource "aws_cloudwatch_metric_alarm" "db_connections_over_threshold" {
+resource "aws_cloudwatch_metric_alarm" "rds_connections_over_threshold" {
   count              = var.create_rds ? 1 : 0
   alarm_name         = "${var.name}-rds-db-connections-threshold"
   alarm_description  = "Triggers alarm if RDS database connections crosses a threshold"
@@ -119,9 +116,8 @@ resource "aws_cloudwatch_metric_alarm" "db_connections_over_threshold" {
   statistic          = "Average"
   period             = "60"
   evaluation_periods = "5"
-  # add sns topic later
-  #  alarm_actions       = [aws_sns_topic.alerting.arn]
-  #  ok_actions          = [aws_sns_topic.alerting.arn]
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
   threshold           = "100"
   treat_missing_data  = "missing"
   comparison_operator = "GreaterThanThreshold"
@@ -138,21 +134,48 @@ resource "aws_cloudwatch_metric_alarm" "db_connections_over_threshold" {
   )
 }
 
-resource "aws_cloudwatch_metric_alarm" "db_queue_depth_over_threshold" {
+resource "aws_cloudwatch_metric_alarm" "rds_allocated_storage_queue_depth_over_threshold" {
   count              = var.create_rds ? 1 : 0
-  alarm_name         = "${var.name}-rds-db-queue-depth-threshold"
+  alarm_name         = "${var.name}-rds-queue-depth-threshold"
   alarm_description  = "Triggers alarm if RDS database queue depth crosses a threshold"
   namespace          = "AWS/RDS"
   metric_name        = "DiskQueueDepth"
   statistic          = "Average"
   period             = "300"
   evaluation_periods = "5"
-  # add sns topic later
-  #  alarm_actions       = [aws_sns_topic.alerting.arn]
-  #  ok_actions          = [aws_sns_topic.alerting.arn]
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
   threshold           = "60"
   treat_missing_data  = "missing"
   comparison_operator = "GreaterThanThreshold"
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.this[0].identifier
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.name
+    }
+  )
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_freeable_memory_less_than_threshold" {
+  count               = var.create_rds ? 1 : 0
+  alarm_name          = "${var.name}-rds-freeable-memory-threshold"
+  alarm_description   = "Triggers alarm if RDS freeable memory crosses a threshold"
+  namespace           = "AWS/RDS"
+  metric_name         = "FreeableMemory"
+  statistic           = "Average"
+  period              = "60"
+  evaluation_periods  = "15"
+  datapoints_to_alarm = 15
+  alarm_actions       = [var.sns_topic_arn]
+  ok_actions          = [var.sns_topic_arn]
+  threshold           = "800000000"
+  treat_missing_data  = "missing"
+  comparison_operator = "LessThanThreshold"
 
   dimensions = {
     DBInstanceIdentifier = aws_db_instance.this[0].identifier
