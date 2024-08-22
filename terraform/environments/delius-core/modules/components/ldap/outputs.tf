81b8775f0f786227f97d6dcf2457fe5c70aee446
Seb Norris <sebastian@automationlogic.com>
2024-08-22 14:29:51 +0100

    add ldap container vars

--- terraform/environments/delius-core/modules/components/ldap/outputs.tf
+++ terraform/environments/delius-core/modules/components/ldap/outputs.tf
@@ -10,6 +10,14 @@ output "delius_core_ldap_bind_password_arn" {
   value = aws_ssm_parameter.delius_core_ldap_bind_password.arn
 }
 
+output "delius_core_ldap_seed_uri_arn" {
+  value = aws_ssm_parameter.delius_core_ldap_seed_uri.arn
+}
+
+output "delius_core_ldap_rbac_version_arn" {
+  value = aws_ssm_parameter.delius_core_ldap_rbac_version.arn
+}
+
 output "security_group_id" {
   value = aws_security_group.ldap.id
 }
--- terraform/environments/delius-core/modules/delius_environment/ldap.tf
+++ terraform/environments/delius-core/modules/delius_environment/ldap.tf
@@ -25,10 +25,7 @@ module "ldap" {
   enable_platform_backups = var.enable_platform_backups
 }
 
-
-
-
-module "ldap_microservice" {
+module "ldap_ecs" {
   source = "../helpers/delius_microservice"
 
   name                  = "ldap"
@@ -36,10 +33,20 @@ module "ldap_microservice" {
   alb_security_group_id = aws_security_group.delius_frontend_alb_security_group.id
   env_name              = var.env_name
 
-  container_vars_default      = {}
+  container_vars_default = {
+    "LDAP_HOST"          = "0.0.0.0",
+    "SLAPD_LOG_LEVEL"    = var.delius_microservice_configs.ldap.slapd_log_level,
+    "LDAP_PORT"          = "389",
+    "DELIUS_ENVIRONMENT" = "delius-core-${var.env_name}"
+  }
+
   container_vars_env_specific = try(var.delius_microservice_configs.ldap.container_vars_env_specific, {})
 
-  container_secrets_default      = {}
+  container_secrets_default      = {
+    "BIND_PASSWORD"         = module.ldap.delius_core_ldap_bind_password_arn,
+    "MIGRATION_S3_LOCATION" = module.ldap.delius_core_ldap_seed_uri_arn,
+    "RBAC_TAG"              = module.ldap.delius_core_ldap_rbac_version_arn
+  }
   container_secrets_env_specific = try(var.delius_microservice_configs.ldap.container_secrets_env_specific, {})
 
   desired_count = 0
@@ -61,7 +68,7 @@ module "ldap_microservice" {
   microservice_lb_https_listener_arn = aws_lb_listener.listener_https.arn
 
   platform_vars           = var.platform_vars
-  container_image         = "${var.platform_vars.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/delius-core-ldap:${var.delius_microservice_configs.ldap.image_tag}" # CHANGE ME
+  container_image         = "${var.platform_vars.environment_management.account_ids["core-shared-services-production"]}.dkr.ecr.eu-west-2.amazonaws.com/delius-core-openldap-ecr-repo:${var.delius_microservice_configs.ldap.image_tag}"
   account_config          = var.account_config
   health_check = {
     command     = ["CMD-SHELL", "ldapsearch -x -H ldap://localhost:389 -b '' -s base '(objectclass=*)' namingContexts"]
@@ -83,4 +90,5 @@ module "ldap_microservice" {
   sns_topic_arn           = aws_sns_topic.delius_core_alarms.arn
   frontend_lb_arn_suffix  = aws_lb.delius_core_frontend.arn_suffix
   enable_platform_backups = var.enable_platform_backups
+
 }
