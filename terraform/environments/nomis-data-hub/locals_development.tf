3ab4049c940e34c9599b36c091d7d4300aa7ad1d
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-08-27 09:39:46 +0100

    update pagerduty integration for test, preproduction and production env

--- terraform/environments/nomis-data-hub/locals_development.tf
+++ terraform/environments/nomis-data-hub/locals_development.tf
@@ -5,11 +5,6 @@ locals {
       # disabling some features in development as the environment gets nuked
       cloudwatch_metric_oam_links_ssm_parameters = []
       cloudwatch_metric_oam_links                = []
-      sns_topics = {
-        pagerduty_integrations = {
-          dso_pagerduty = "nomis_data_hub_nonprod_alarms"
-        }
-      }
     }
   }
 
