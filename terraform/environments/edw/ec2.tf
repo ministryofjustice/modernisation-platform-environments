aa88a90f23df4d21b31eafa9ac441c887d9ba6b1
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-08-27 13:20:56 +0100

    LAWS-3871 - update rules
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/edw/ec2.tf
+++ terraform/environments/edw/ec2.tf
@@ -358,7 +358,7 @@ chown oracle:dba /home/oracle/crecrontab.txt
 chmod 744 /home/oracle/crecrontab.txt
 su oracle -c "crontab /home/oracle/crecrontab.txt"
 
-# Set permissions for CDC scripts
+## Set permissions for CDC scripts
 chown oracle:dba /home/oracle/scripts/cdc_simple_health_check.sh
 chmod 744 /home/oracle/scripts/cdc_simple_health_check.sh
 
@@ -683,38 +683,6 @@ resource "aws_security_group" "edw_db_security_group" {
       Name = "${local.application_name}-security-group"
     }
   )
-
-  ingress {
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.shared.cidr_block]
-    description = "RDS env access"
-  }
-
-  ingress {
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = [local.application_data.accounts[local.environment].edw_management_cidr]
-    description = "RDS Workspace access"
-  }
-
-  ingress {
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = ["10.200.32.0/19"]
-    description = "RDS Appstream access"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "-"
-  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "db_bastion_ssh" {
@@ -734,6 +702,43 @@ resource "aws_vpc_security_group_ingress_rule" "db_lambda" {
   ip_protocol                  = "tcp"
   to_port                      = 22
 }
+
+resource "aws_vpc_security_group_ingress_rule" "RDS_Appstream" {
+  security_group_id            = aws_security_group.edw_db_security_group.id
+  description                  = "RDS Appstream access"
+  cidr_ipv4                    = ["10.200.32.0/19"]
+  from_port                    = 1521
+  ip_protocol                  = "tcp"
+  to_port                      = 1521
+}
+
+resource "aws_vpc_security_group_ingress_rule" "RDS_workstream" {
+  security_group_id            = aws_security_group.edw_db_security_group.id
+  description                  = "RDS Workspace access"
+  cidr_ipv4                    = [local.application_data.accounts[local.environment].edw_management_cidr]
+  from_port                    = 1521
+  ip_protocol                  = "tcp"
+  to_port                      = 1521
+}
+
+resource "aws_vpc_security_group_ingress_rule" "RDS_env" {
+  security_group_id            = aws_security_group.edw_db_security_group.id
+  description                  = "RDS env access"
+  cidr_ipv4                    = [data.aws_vpc.shared.cidr_block]
+  from_port                    = 1521
+  ip_protocol                  = "tcp"
+  to_port                      = 1521
+}
+
+resource "aws_vpc_security_group_egress_rule" "all_out" {
+  security_group_id            = aws_security_group.edw_db_security_group.id
+  description                  = "RDS env access"
+  cidr_ipv4                    = ["0.0.0.0/0"]
+  from_port                    = 0
+  ip_protocol                  = "-1"
+  to_port                      = 0
+}
+
 
 ###### DB DNS #######
 
