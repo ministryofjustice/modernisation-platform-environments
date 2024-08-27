cd4dd3df97f7318aa96da6f0e9080c76c76aff79
Robert Sweetman <robertiansweetman@gmail.com>
2024-08-27 15:26:27 +0100

    use pagerduty integration and remove defaults from development env (#7618)

--- terraform/environments/oasys/locals_development.tf
+++ terraform/environments/oasys/locals_development.tf
@@ -5,14 +5,6 @@ locals {
       # disabling some features in development as the environment gets nuked
       cloudwatch_metric_oam_links_ssm_parameters = []
       cloudwatch_metric_oam_links                = []
-
-      sns_topics = {
-        pagerduty_integrations = {
-          dso_pagerduty               = "oasys_nonprod_alarms"
-          dba_pagerduty               = "hmpps_shef_dba_non_prod"
-          dba_high_priority_pagerduty = "hmpps_shef_dba_non_prod"
-        }
-      }
     }
   }
 
