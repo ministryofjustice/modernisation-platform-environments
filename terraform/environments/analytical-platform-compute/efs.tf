071a9367462f2a088a8fc71cabe13d2eab790065
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-08-28 07:22:36 +0000

    Add KMS and EFS
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/environments/analytical-platform-compute/efs.tf
@@ -0,0 +1,51 @@
+module "actions_runner_cache_efs" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/efs/aws"
+  version = "1.6.3"
+
+  name          = "actions-runner-cache"
+  encrypted     = true
+  kms_key_arn   = module.actions_runner_cache_efs_kms.key_arn
+  attach_policy = false
+
+  enable_backup_policy = true
+
+  mount_targets = {
+    "private-subnets-0" = {
+      subnet_id = module.vpc.private_subnets[0]
+    }
+    "private-subnets-1" = {
+      subnet_id = module.vpc.private_subnets[1]
+    }
+    "private-subnets-2" = {
+      subnet_id = module.vpc.private_subnets[2]
+    }
+  }
+
+  security_group_vpc_id = module.vpc.vpc_id
+  security_group_rules = {
+    private-subnets = {
+      cidr_blocks = module.vpc.private_subnets_cidr_blocks
+    }
+  }
+
+  access_points = {
+    work = {
+      name = "work"
+      posix_user = {
+        gid = 10000
+        uid = 10000
+      }
+      root_directory = {
+        path = "/airflow-dags"
+        creation_info = {
+          owner_gid   = 10000
+          owner_uid   = 10000
+          permissions = "644"
+        }
+      }
+    }
+  }
+}
--- terraform/environments/analytical-platform-compute/kms-keys.tf
+++ terraform/environments/analytical-platform-compute/kms-keys.tf
@@ -339,3 +339,19 @@ module "ui_rds_kms" {
 
   tags = local.tags
 }
+
+module "actions_runner_cache_efs_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.1.0"
+
+  aliases               = ["efs/actions-runner-cache"]
+  description           = "Actions Runner Cache EFS KMS key"
+  enable_default_policy = true
+
+  deletion_window_in_days = 7
+
+  tags = local.tags
+}
