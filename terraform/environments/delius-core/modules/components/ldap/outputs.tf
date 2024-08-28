0bc1aa9be741cfcb5b8f6fbee32338402a6cf194
Seb Norris <sebastian@automationlogic.com>
2024-08-28 19:08:45 +0100

    params have moved

--- terraform/environments/delius-core/modules/components/ldap/outputs.tf
+++ terraform/environments/delius-core/modules/components/ldap/outputs.tf
@@ -2,14 +2,6 @@ output "nlb_dns_name" {
   value = module.nlb.dns_name
 }
 
-output "delius_core_ldap_principal_arn" {
-  value = aws_ssm_parameter.delius_core_ldap_principal.arn
-}
-
-output "delius_core_ldap_bind_password_arn" {
-  value = aws_ssm_parameter.delius_core_ldap_bind_password.arn
-}
-
 output "security_group_id" {
   value = aws_security_group.ldap.id
 }
