e284d1f564616f49f3b323dab8348c4bb860a85d
jodiejones-moj <jodie.jones@digital.justice.gov.uk>
2024-08-28 14:57:25 +0100

    Added creation of kali linux EC2 instance

---
+++ terraform/environments/panda-cyber-appsec-lab/ec2.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "kali_linux" {
+  ami           = "ami-07c1b39b7b3d2525d"
+  instance_type = "t2.micro"
+ 
+  subnet_ids         = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update && sudo apt -y install software-properties-common
+              sudo wget -q -O - https://archive.kali.org/archive-key.asc | sudo apt-key add -
+              sudo echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list.d/kali.list
+              sudo apt update && sudo apt -y install kali-linux-default
+              EOF
+
+  tags = {
+    Name = "Terraform-Kali-Linux"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
+
+
