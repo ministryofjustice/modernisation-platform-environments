c14e74ff825c1879461e99c57a37fea36d5fd9f5
matt-heery <matt.heery@digital.justice.gov.uk>
2024-08-28 16:16:07 +0100

    updating the api gateway code

--- terraform/environments/electronic-monitoring-data/api_gateway.tf
+++ terraform/environments/electronic-monitoring-data/api_gateway.tf
@@ -7,13 +7,13 @@ resource "aws_api_gateway_rest_api" "get_zipped_file" {
   description = "API Gateway to trigger Step Function to get a unzipped file from zipped store"
 }
 
-resource "aws_api_gateway_resource" "get_zipped_file_resource" {
+resource "aws_api_gateway_resource" "get_zipped_step_function_invoke" {
   rest_api_id = aws_api_gateway_rest_api.get_zipped_file.id
   parent_id   = aws_api_gateway_rest_api.get_zipped_file.root_resource_id
   path_part   = "execute"
 }
 
-resource "aws_api_gateway_method" "trigger_method" {
+resource "aws_api_gateway_method" "get_zipped_step_function_invoke" {
   rest_api_id   = aws_api_gateway_rest_api.get_zipped_file.id
   resource_id   = aws_api_gateway_resource.get_zipped_file_resource.id
   http_method   = "POST"
@@ -61,12 +61,12 @@ resource "aws_iam_role_policy_attachment" "get_zipped_gateway_trigger_step_funct
   policy_arn = aws_iam_policy.trigger_step_function_policy.arn
 }
 
-resource "aws_api_gateway_integration" "get_zipped_gateway_integration" {
+resource "aws_api_gateway_integration" "get_zipped_step_function_invoke" {
   rest_api_id             = aws_api_gateway_rest_api.get_zipped_file.id
-  resource_id             = aws_api_gateway_resource.get_zipped_file_resource.id
-  http_method             = aws_api_gateway_method.trigger_method.http_method
+  resource_id             = aws_api_gateway_resource.get_zipped_step_function_invoke.id
+  http_method             = aws_api_gateway_method.get_zipped_step_function_invoke.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = "arn:aws:apigateway:eu-west-2:states:action/StartExecution"
 
   credentials = aws_iam_role.get_zipped_gateway_role.arn
@@ -74,16 +74,33 @@ resource "aws_api_gateway_integration" "get_zipped_gateway_integration" {
   request_templates = {
     "application/json" = <<EOF
 {
-  "input": "{ \\"file_name\\": \\"$input.json('$.file_name')\\", \\"zip_file_name\\": \\"$input.json('$.zip_file_name')\\" }",
+  "input": "$util.escapeJavaScript($input.json('$'))",
   "stateMachineArn": "${module.get_zipped_file.arn}"
 }
 EOF
   }
 }
 
-resource "aws_api_gateway_deployment" "example_deployment" {
-  depends_on = [aws_api_gateway_integration.get_zipped_gateway_integration]
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.data_sharer.id
 
-  rest_api_id = aws_api_gateway_rest_api.get_zipped_file.id
-  stage_name  = "prod"
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+        # put some stuff once written
+        aws_api_gateway_resource.get_zipped_step_function_invoke,
+        aws_api_gateway_method.get_zipped_step_function_invoke,
+        aws_api_gateway_integration.get_zipped_step_function_invoke,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
