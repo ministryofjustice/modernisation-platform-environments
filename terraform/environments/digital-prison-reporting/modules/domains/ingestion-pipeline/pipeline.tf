3ff707d4576196870b6891f2530d8d3068d838b1
koladeadewuyi-moj <136330532+koladeadewuyi-moj@users.noreply.github.com>
2024-08-30 11:38:01 +0100

    DPR2-1053: Retain data availability during data ingest (#7640)

--- terraform/environments/digital-prison-reporting/modules/domains/ingestion-pipeline/pipeline.tf
+++ terraform/environments/digital-prison-reporting/modules/domains/ingestion-pipeline/pipeline.tf
@@ -68,15 +68,67 @@ module "data_ingestion_pipeline" {
               "--dpr.stop.glue.instance.job.name" : var.glue_reporting_hub_cdc_jobname
             }
           },
-          "Next" : "Empty All Data"
+          "Next" : "Update Hive Tables"
         },
-        "Empty All Data" : {
+        "Update Hive Tables" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_hive_table_creation_jobname,
+            "Arguments" : {
+              "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
+              "--dpr.config.key" : var.domain
+            }
+          },
+          "Next" : "Prepare Temp Reload Bucket Data"
+        },
+        "Prepare Temp Reload Bucket Data" : {
           "Type" : "Task",
           "Resource" : "arn:aws:states:::glue:startJobRun.sync",
           "Parameters" : {
             "JobName" : var.glue_s3_data_deletion_job,
             "Arguments" : {
-              "--dpr.file.deletion.buckets" : "${var.s3_raw_bucket_id},${var.s3_raw_archive_bucket_id},${var.s3_structured_bucket_id},${var.s3_curated_bucket_id},${var.s3_temp_reload_bucket_id}",
+              "--dpr.file.deletion.buckets" : var.s3_temp_reload_bucket_id,
+              "--dpr.config.key" : var.domain
+            }
+          },
+          "Next" : "Copy Curated Data to Temp-Reload Bucket"
+        },
+        "Copy Curated Data to Temp-Reload Bucket" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_s3_file_transfer_job,
+            "Arguments" : {
+              "--dpr.file.transfer.source.bucket" : var.s3_curated_bucket_id,
+              "--dpr.file.transfer.destination.bucket" : var.s3_temp_reload_bucket_id,
+              "--dpr.file.transfer.retention.period.amount" : "0",
+              "--dpr.file.transfer.delete.copied.files" : "false",
+              "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
+              "--dpr.config.key" : var.domain
+            }
+          },
+          "Next" : "Switch Hive Tables for Prisons to Temp-Reload Bucket"
+        },
+        "Switch Hive Tables for Prisons to Temp-Reload Bucket" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_switch_prisons_hive_data_location_job,
+            "Arguments" : {
+              "--dpr.prisons.data.switch.target.s3.path" : "s3://${var.s3_temp_reload_bucket_id}",
+              "--dpr.config.key" : var.domain
+            }
+          },
+          "Next" : "Empty Raw, Archive, Structured and Curated Data"
+        },
+        "Empty Raw, Archive, Structured and Curated Data" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_s3_data_deletion_job,
+            "Arguments" : {
+              "--dpr.file.deletion.buckets" : "${var.s3_raw_bucket_id},${var.s3_raw_archive_bucket_id},${var.s3_structured_bucket_id},${var.s3_curated_bucket_id}",
               "--dpr.config.key" : var.domain
             }
           },
@@ -144,18 +196,6 @@ module "data_ingestion_pipeline" {
               "--dpr.config.key" : var.domain
             }
           },
-          "Next" : "Create Hive Tables"
-        },
-        "Create Hive Tables" : {
-          "Type" : "Task",
-          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
-          "Parameters" : {
-            "JobName" : var.glue_hive_table_creation_jobname,
-            "Arguments" : {
-              "--dpr.config.s3.bucket" : var.s3_glue_bucket_id,
-              "--dpr.config.key" : var.domain
-            }
-          },
           "Next" : "Resume DMS Replication Task"
         },
         "Resume DMS Replication Task" : {
@@ -177,6 +217,18 @@ module "data_ingestion_pipeline" {
               "--dpr.config.key" : var.domain
             }
           },
+          "Next" : "Switch Hive Tables for Prisons to Curated"
+        },
+        "Switch Hive Tables for Prisons to Curated" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_switch_prisons_hive_data_location_job,
+            "Arguments" : {
+              "--dpr.prisons.data.switch.target.s3.path" : "s3://${var.s3_curated_bucket_id}",
+              "--dpr.config.key" : var.domain
+            }
+          },
           "Next" : "Reactivate Archive Trigger"
         },
         "Reactivate Archive Trigger" : {
@@ -187,6 +239,18 @@ module "data_ingestion_pipeline" {
             "Arguments" : {
               "--dpr.glue.trigger.name" : var.archive_job_trigger_name,
               "--dpr.glue.trigger.activate" : "true"
+            }
+          },
+          "Next" : "Empty Temp Reload Bucket Data"
+        },
+        "Empty Temp Reload Bucket Data" : {
+          "Type" : "Task",
+          "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+          "Parameters" : {
+            "JobName" : var.glue_s3_data_deletion_job,
+            "Arguments" : {
+              "--dpr.file.deletion.buckets" : var.s3_temp_reload_bucket_id,
+              "--dpr.config.key" : var.domain
             }
           },
           "End" : true
