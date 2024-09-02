6298644203a419fef33fac197b1cc50ac68a706e
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-09-02 08:54:27 +0100

    Setting Lake Formation admins in Ireland

--- terraform/environments/analytical-platform-compute/lakeformation-data-lake-settings.tf
+++ terraform/environments/analytical-platform-compute/lakeformation-data-lake-settings.tf
@@ -1,4 +1,13 @@
-resource "aws_lakeformation_data_lake_settings" "main" {
+resource "aws_lakeformation_data_lake_settings" "london" {
+  admins = [
+    data.aws_iam_session_context.current.issuer_arn,
+    module.lake_formation_share_role.iam_role_arn,
+    module.analytical_platform_ui_service_role.iam_role_arn
+  ]
+}
+
+resource "aws_lakeformation_data_lake_settings" "ireland" {
+  provider = aws.analytical-platform-compute-eu-west-1
   admins = [
     data.aws_iam_session_context.current.issuer_arn,
     module.lake_formation_share_role.iam_role_arn,
