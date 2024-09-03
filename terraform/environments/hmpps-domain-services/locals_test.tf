1b8c077def12f3ba7a3e2e14f02ca35b66a1a349
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-03 16:51:52 +0100

    test RDGateway build

--- terraform/environments/hmpps-domain-services/locals_test.tf
+++ terraform/environments/hmpps-domain-services/locals_test.tf
@@ -81,12 +81,17 @@ locals {
         })
         config = merge(local.ec2_autoscaling_groups.base_windows.config, {
           ami_name = "hmpps_windows_server_2022_release_2024-*"
+          user_data_raw = base64encode(templatefile(
+            "../../modules/baseline_presets/ec2-user-data/user-data-pwsh.yaml.tftpl", {
+              branch = "TM-153/remote-desktop-automation"
+            }
+          ))
         })
         ebs_volumes = {
           "/dev/sda1" = { type = "gp3", size = 100 }
         }
         instance = merge(local.ec2_autoscaling_groups.base_windows.instance, {
-          instance_type = "t3.medium"
+          instance_type = "t3.large"
         })
         tags = merge(local.ec2_autoscaling_groups.base_windows.tags, {
           description = "Windows Server 2022 instance for testing domain join and patching"
