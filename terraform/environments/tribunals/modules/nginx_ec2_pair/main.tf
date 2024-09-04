65450534a6ca35173bf9667cb1bd59693bef0299
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-09-04 16:56:35 +0100

    rebuild ec2, add logging to user data to debug creation of sites-enabled directory

--- terraform/environments/tribunals/modules/nginx_ec2_pair/main.tf
+++ terraform/environments/tribunals/modules/nginx_ec2_pair/main.tf
@@ -1,120 +1,151 @@
-# output "instance_ids" {
-#   value = tomap({
-#     for k, inst in aws_instance.nginx : k => inst.id
-#   })
-# }
+output "instance_ids" {
+  value = tomap({
+    for k, inst in aws_instance.nginx : k => inst.id
+  })
+}
 
-# variable "nginx_lb_sg_id" {
-#   type = string
-# }
+variable "nginx_lb_sg_id" {
+  type = string
+}
 
-# variable "vpc_shared_id" {
-#   type = string
-# }
+variable "vpc_shared_id" {
+  type = string
+}
 
-# variable "public_subnets_a_id" {
-# }
+variable "public_subnets_a_id" {
+}
 
-# variable "public_subnets_b_id" {
-# }
+variable "public_subnets_b_id" {
+}
 
-# variable "environment" {
-# }
+variable "environment" {
+}
 
-# data "aws_ami" "latest_linux" {
-#   most_recent = true
-#   owners = ["amazon"]
-#   filter {
-#     name   = "name"
-#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-#   }
-# }
+data "aws_ami" "latest_linux" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
 
-# resource "aws_instance" "nginx" {
-#   for_each = toset(["eu-west-2a", "eu-west-2b"])
+resource "aws_instance" "nginx" {
+  for_each = toset(["eu-west-2a", "eu-west-2b"])
 
-#   ami                         = data.aws_ami.latest_linux.id
-#   associate_public_ip_address = true
-#   subnet_id                   = each.key == "eu-west-2a" ? var.public_subnets_a_id : var.public_subnets_b_id
-#   instance_type               = "t2.micro"
-#   availability_zone           = each.value
-#   tags = {
-#     Name = "tribunals-nginx-${each.value}"
-#   }
-#   vpc_security_group_ids = [aws_security_group.allow_ssm.id]
-#   iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
-#   user_data              = <<-EOF
-#               #!/bin/bash
-#               ${file("${path.module}/scripts/install-nginx.sh")}
-#               aws s3 cp s3://${aws_s3_bucket.nginx_config.id}/sites-available /etc/nginx/sites-available --recursive
-#               sudo mkdir -p /etc/nginx/sites-enabled
-#               ${file("${path.module}/scripts/add-symbolic-links.sh")}
-#               ${file("${path.module}/scripts/restart-nginx.sh")}
-#               EOF
-# }
+  ami                         = data.aws_ami.latest_linux.id
+  associate_public_ip_address = true
+  subnet_id                   = each.key == "eu-west-2a" ? var.public_subnets_a_id : var.public_subnets_b_id
+  instance_type               = "t2.micro"
+  availability_zone           = each.value
+  tags = {
+    Name = "tribunals-nginx-${each.value}"
+  }
+  vpc_security_group_ids = [aws_security_group.allow_ssm.id]
+  iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
+    user_data = <<-EOF
+              #!/bin/bash
+              exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+              echo "Starting nginx setup"
 
-# resource "aws_security_group" "allow_ssm" {
-#   name        = "allow_ssm"
-#   description = "Allow SSM connection"
-#   vpc_id      = var.vpc_shared_id
+              echo "installing nginx"
+              sudo yum update -y &&
+              sudo amazon-linux-extras install nginx1 -y
 
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     security_groups = [
-#       var.nginx_lb_sg_id
-#     ]
-#   }
+              # Create necessary directories
+              echo "Creating nginx directories"
+              sudo mkdir -p /etc/nginx/sites-enabled || echo "Failed to create sites-enabled directory"
+              sudo mkdir -p /etc/nginx/sites-available || echo "Failed to create sites-available directory"
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+              # Check if directories were created
+              if [ -d "/etc/nginx/sites-enabled" ]; then
+                echo "sites-enabled directory exists"
+              else
+                echo "sites-enabled directory does not exist"
+              fi
 
-# resource "aws_s3_bucket" "nginx_config" {
-#   bucket = "tribunals-nginx-config-files-${var.environment}"
-# }
+              if [ -d "/etc/nginx/sites-available" ]; then
+                echo "sites-available directory exists"
+              else
+                echo "sites-available directory does not exist"
+              fi
 
-# resource "aws_s3_object" "sites_available" {
-#   for_each = fileset("${path.module}/sites-available", "*")
+              # Copy configuration files from S3
+              echo "Copying files from S3"
+              aws s3 cp s3://${aws_s3_bucket.nginx_config.id}/sites-available /etc/nginx/sites-available --recursive
 
-#   bucket = aws_s3_bucket.nginx_config.id
-#   key    = "sites-available/${each.value}"
-#   source = "${path.module}/sites-available/${each.value}"
-# }
+              # Run setup scripts
+              echo "Running add-symbolic-links.sh"
+              ${file("${path.module}/scripts/add-symbolic-links.sh")}
+              echo "Running restart-nginx.sh"
+              ${file("${path.module}/scripts/restart-nginx.sh")}
 
-# resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-#   role       = aws_iam_role.nginx_role.name
-# }
+              echo "Nginx setup completed"
+              EOF
+}
 
-# resource "aws_iam_role" "nginx_role" {
-#   name = "nginx-ssm-role"
+resource "aws_security_group" "allow_ssm" {
+  name        = "allow_ssm"
+  description = "Allow SSM connection"
+  vpc_id      = var.vpc_shared_id
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    security_groups = [
+      var.nginx_lb_sg_id
+    ]
+  }
 
-# resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-#   role       = aws_iam_role.nginx_role.name
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# resource "aws_iam_instance_profile" "nginx_profile" {
-#   name = "nginx-ssm-profile"
-#   role = aws_iam_role.nginx_role.name
-# }
+resource "aws_s3_bucket" "nginx_config" {
+  bucket = "tribunals-nginx-config-files-${var.environment}"
+}
+
+resource "aws_s3_object" "sites_available" {
+  for_each = fileset("${path.module}/sites-available", "*")
+
+  bucket = aws_s3_bucket.nginx_config.id
+  key    = "sites-available/${each.value}"
+  source = "${path.module}/sites-available/${each.value}"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  role       = aws_iam_role.nginx_role.name
+}
+
+resource "aws_iam_role" "nginx_role" {
+  name = "nginx-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.nginx_role.name
+}
+
+resource "aws_iam_instance_profile" "nginx_profile" {
+  name = "nginx-ssm-profile"
+  role = aws_iam_role.nginx_role.name
+}
