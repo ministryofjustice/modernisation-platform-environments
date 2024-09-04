1704d95346d35fa472cf4befdce2cea33d3b1059
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-04 09:12:19 +0100

    add target group

--- terraform/environments/hmpps-domain-services/locals_test.tf
+++ terraform/environments/hmpps-domain-services/locals_test.tf
@@ -77,7 +77,7 @@ locals {
       test-win-2022 = merge(local.ec2_autoscaling_groups.base_windows, {
         autoscaling_group = merge(local.ec2_autoscaling_groups.base_windows.autoscaling_group, {
           # clean up Computer and DNS entry from azure.noms.root domain before using
-          desired_capacity = 0
+          desired_capacity = 1
         })
         config = merge(local.ec2_autoscaling_groups.base_windows.config, {
           ami_name = "hmpps_windows_server_2022_release_2024-*"
@@ -93,6 +93,9 @@ locals {
         instance = merge(local.ec2_autoscaling_groups.base_windows.instance, {
           instance_type = "t3.large"
         })
+        lb_target_groups = {
+          http = local.lbs.public.instance_target_groups.http
+        }
         tags = merge(local.ec2_autoscaling_groups.base_windows.tags, {
           description = "Windows Server 2022 instance for testing domain join and patching"
           domain-name = "azure.noms.root"
