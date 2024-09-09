062971a09eec3e2c53e160047d40286a98016be8
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-09-09 13:30:40 +0100

    Add Audited Interaction Inbound Replication Task

--- terraform/environments/delius-core/modules/components/dms/dms_inbound_tasks.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_inbound_tasks.tf
@@ -54,3 +54,33 @@ resource "aws_dms_replication_task" "business_interaction_inbound_replication" {
   )
 
 }
+
+
+# Audited Interaction inbound replication only happens in the repository environments.
+# There is one replication task for each for the feeder client environments.
+resource "aws_dms_replication_task" "audited_interaction_inbound_replication" {
+  for_each            = toset(try(local.dms_s3_cross_account_client_environments[var.env_name],[]))
+  replication_task_id = "${var.env_name}-audited-interaction-replication-task-from-${each.value}"
+  migration_type      = "cdc" 
+
+  table_mappings            = file("files/audited_interaction_inbound_table_mapping.json")
+  replication_task_settings = file("files/audited_interaction_inbound_settings.json")
+ 
+  source_endpoint_arn      = aws_dms_s3_endpoint.dms_audit_source_endpoint_s3[each.value].endpoint_arn
+  target_endpoint_arn      = aws_dms_endpoint.dms_audit_target_endpoint_db[0].endpoint_arn
+  replication_instance_arn = aws_dms_replication_instance.dms_replication_instance.replication_instance_arn
+
+  tags = merge(
+    var.tags,
+    {
+      "name" = "Audited Interaction Replication from ${each.value} to ${var.env_name}"
+    },
+    {
+      "audit-client-environment" = "${each.value}"
+    },
+    {
+      "audit-repository-environment" = "${var.env_name}"
+    },
+  )
+
+}
