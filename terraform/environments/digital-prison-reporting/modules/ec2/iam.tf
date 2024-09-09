3b58c0e0d126fd1270e26cf6c45971b4d76eae17
SonyPimpale <pimpale@live.com>
2024-09-09 23:57:39 +0100

    modify description

--- terraform/environments/digital-prison-reporting/modules/ec2/iam.tf
+++ terraform/environments/digital-prison-reporting/modules/ec2/iam.tf
@@ -200,7 +200,7 @@ resource "aws_iam_policy" "generic" {
 data "aws_iam_policy_document" "generic" {
   #checkov:skip=CKV_AWS_111:"Ensure IAM policies does not allow write access without constraints"
   #checkov:skip=CKV_AWS_109:"Ensure IAM policies does not allow permissions management / resource exposure without constraints"
-  #checkov:skip=CKV_AWS_107:"Ensure IAM policies does not allow credentials exposure. Will be addressed as part of https://dsdmoj.atlassian.net/browse/DPR2-1083"
+  #checkov:skip=CKV_AWS_107:"Ensure IAM policies does not allow credentials exposure, will be addressed as part of https://dsdmoj.atlassian.net/browse/DPR2-1083"
   #checkov:skip=CKV_AWS_356:"Ensure no IAM policies documents allow "*" as a statement's resource for restrictable actions. Will be addressed as part of https://dsdmoj.atlassian.net/browse/DPR2-1083"
   statement {
     actions = [
