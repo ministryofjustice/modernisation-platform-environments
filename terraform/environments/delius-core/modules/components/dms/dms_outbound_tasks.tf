1c30f1d75bef31b31c1b76d42991719e7e86eddb
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-09-09 16:45:06 +0100

    Add new outbound replication task for AUDITED_INTERACTION_CHECKSUM

--- terraform/environments/delius-core/modules/components/dms/dms_outbound_tasks.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_outbound_tasks.tf
@@ -1,3 +1,8 @@
+  # We do not fail a replication task but keep retrying every 1800 seconds (RecoverableErrorStopRetryAfterThrottlingMax=false)
+  # This allows us to resume after downtime on an endpoint but note that this means that errors will not be raised
+  # and must be monitored independently.
+
+
 # Audit outbound replication only happens in client environments
 resource "aws_dms_replication_task" "audited_interaction_outbound_replication" {
   count               = try(var.dms_config.audit_source_endpoint.read_database, null) == null ? 0 : 1
@@ -106,4 +111,32 @@ resource "aws_dms_replication_task" "business_interaction_outbound_replication" 
 }
 
 
+# Audited Interaction Checksum outbound replication only happens in client environments.
+# This replicates records from the DELIUS_AUDIT_DMS_POOL.AUDITED_INTERACTION_CHECKSUM table.
+# This table is used to ensure accurate replication of audited interaction records.
+resource "aws_dms_replication_task" "audited_interaction_checksum_outbound_replication" {
+  count               = try(var.dms_config.audit_source_endpoint.read_database, null) == null ? 0 : 1
+  replication_task_id = "${var.env_name}-business-interaction-checksum-outbound-replication-task-for-${lower(var.dms_config.audit_source_endpoint.read_database)}"
+  migration_type      = "cdc" 
 
+  table_mappings            = file("files/audited_interaction_checksum_outbound_table_mapping.json")
+  replication_task_settings = file("files/audited_interaction_checksum_outbound_settings.json")
+
+  source_endpoint_arn      = aws_dms_endpoint.dms_audit_source_endpoint_db[0].endpoint_arn
+  target_endpoint_arn      = aws_dms_s3_endpoint.dms_audit_target_endpoint_s3[0].endpoint_arn
+  replication_instance_arn = aws_dms_replication_instance.dms_replication_instance.replication_instance_arn
+
+  tags = merge(
+    var.tags,
+    {
+      "name" = "Audited Interaction Checksum Replication from ${var.env_name} to ${var.dms_config.audit_target_endpoint.write_environment}"
+    },
+    {
+      "audit-client-environment" = "${var.env_name}"
+    },
+    {
+      "audit-repository-environment" = "${var.dms_config.audit_target_endpoint.write_environment}"
+    },
+  )
+
+}
