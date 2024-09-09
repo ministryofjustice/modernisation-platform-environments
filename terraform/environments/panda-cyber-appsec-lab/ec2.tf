5ada98651000fd4e151763ab2962e6306e3578d7
jodiejones-moj <jodie.jones@digital.justice.gov.uk>
2024-09-09 14:05:56 +0100

    Using creation of S3 bucket from mod platform code

--- terraform/environments/panda-cyber-appsec-lab/ec2.tf
+++ terraform/environments/panda-cyber-appsec-lab/ec2.tf
@@ -104,23 +104,71 @@ resource "aws_iam_instance_profile" "ssm_instance_profile" {
 }
 
 # Create S3 bucket
-resource "aws_s3_bucket" "panda_cyber_bucket" {
-  bucket = "panda-cyber-keys"
-  acl    = "private"
+module "s3-bucket" {
+    source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.0.0"
 
-  versioning {
-    enabled = true
+  bucket_prefix                            = "panda-cyber-keys-bucket"
+  versioning_enabled                       = true
+
+  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
+  ownership_controls = "BucketOwnerEnforced"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled                      = false
+  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
   }
-  
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
       }
     }
-  }
+  ]
+
+  tags                 = local.tags
 }
+
 
 
 
