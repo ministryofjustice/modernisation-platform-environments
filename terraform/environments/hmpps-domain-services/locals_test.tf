770b855362acb945724fc2599b5cce4fae995fce
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-10 09:35:15 +0100

    fix

--- terraform/environments/hmpps-domain-services/locals_test.tf
+++ terraform/environments/hmpps-domain-services/locals_test.tf
@@ -233,7 +233,7 @@ locals {
                 priority = 200
                 actions = [{
                   type              = "forward"
-                  target_group_name = "test-rds-2-https"
+                  target_group_name = "test-rds-2-a-http"
                 }]
                 conditions = [{
                   host_header = {
