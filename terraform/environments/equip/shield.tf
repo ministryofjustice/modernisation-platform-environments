21ea46dbb6b4e23bb5af5209620c66915552e622
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-11 08:43:24 +0100

    used for_each to scope import to production only

--- terraform/environments/equip/shield.tf
+++ terraform/environments/equip/shield.tf
@@ -22,6 +22,7 @@ module "shield" {
 }
 
 import {
+  for_each = local.is-production ? { "build" = true } : {}
   id = "06cebb43-c961-44f0-81e5-27f94e2159d4/FMManagedWebACLV2-shield_advanced_auto_remediate-1649415294385/REGIONAL"
   to = module.shield["build"].aws_wafv2_web_acl.main
 }
