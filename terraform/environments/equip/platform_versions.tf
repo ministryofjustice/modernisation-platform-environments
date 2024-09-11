2fcda1beff1708415ad859368dfd655ce52d9428
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-11 16:08:22 +0100

    Added some missing providers

--- terraform/environments/equip/platform_versions.tf
+++ terraform/environments/equip/platform_versions.tf
@@ -8,6 +8,14 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
   required_version = "~> 1.0"
 }
