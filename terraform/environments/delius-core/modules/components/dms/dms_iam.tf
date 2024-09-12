4e7a9cdd87b02e64fccf15efd5eb8165c93841a1
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-09-12 19:32:00 +0100

    Add Logging

--- terraform/environments/delius-core/modules/components/dms/dms_iam.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_iam.tf
@@ -251,3 +251,44 @@ resource "aws_iam_role_policy_attachment" "lambda_assume_role_attachment" {
 }
 
 
+resource "aws_iam_role" "api_gateway_cloudwatch_role" {
+  name = "APIGatewayCloudWatchLogsRole"
+  
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "apigateway.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "api_gateway_cloudwatch_policy" {
+  name = "APIGatewayCloudWatchLogsPolicy"
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_cloudwatch_role_attachment" {
+  role       = aws_iam_role.api_gateway_cloudwatch_role.name
+  policy_arn = aws_iam_policy.api_gateway_cloudwatch_policy.arn
+}
+
+
