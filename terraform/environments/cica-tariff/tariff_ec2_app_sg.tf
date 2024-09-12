8d0ccda9ca5ee17e308a0d588605e32024612df8
Edward Duddy <edward.duddy@cica.gov.uk>
2024-09-12 14:24:09 +0100

    🛂 Add uat to security group
    Add CICA UAT IPs to security group ingress

--- terraform/environments/cica-tariff/tariff_ec2_app_sg.tf
+++ terraform/environments/cica-tariff/tariff_ec2_app_sg.tf
@@ -24,14 +24,14 @@ resource "aws_security_group" "tariff_app_security_group" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b]
+    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b, local.cidr_cica_uat_a, local.cidr_cica_uat_b]
   }
 
   ingress {
     protocol    = "icmp"
     from_port   = -1
     to_port     = -1
-    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b]
+    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b, local.cidr_cica_uat_a, local.cidr_cica_uat_b]
   }
 
   ingress {
