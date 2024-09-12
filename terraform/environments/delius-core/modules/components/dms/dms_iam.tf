da9e92aa616c2c4b69feabeb42d28f384b121167
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-09-12 10:33:33 +0100

    merge

--- terraform/environments/delius-core/modules/components/dms/dms_iam.tf
+++ terraform/environments/delius-core/modules/components/dms/dms_iam.tf
@@ -29,27 +29,6 @@ resource "aws_iam_role_policy_attachment" "dms-vpc-role-AmazonDMSVPCManagementRo
   role       = aws_iam_role.dms-vpc-role.name
 }
 
-# Create a Role to Allow any Audit Clients of this environment, or any Repository
-# for this environment to write to the DMS S3 bucket
-# resource "aws_iam_role" "dms_s3_writer_role" {
-#   count = length(local.dms_s3_writer_account_ids) > 0 ? 1 : 0
-#   name = local.dms_s3_writer_role_name
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       for principal in local.dms_s3_writer_account_ids:
-#       {
-#         Effect = "Allow",
-#         Principal = {
-#           AWS = "arn:aws:iam::${principal}:root"
-#         },
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-#   })
-# }
-
-
 resource "aws_iam_role" "dms_s3_writer_role" {
   name = local.dms_s3_writer_role_name
   assume_role_policy = jsonencode({
@@ -110,7 +89,7 @@ resource "aws_iam_role" "dms_s3_reader_role" {
           Service = "dms.amazonaws.com"
         }
         Action = "sts:AssumeRole"
-      },
+      }
     ]
   })
 }
@@ -142,5 +121,91 @@ resource "aws_iam_policy" "dms_s3_bucket_reader_policy" {
 resource "aws_iam_role_policy_attachment" "dms_s3_bucket_reader_policy_attachment" {
   role       = aws_iam_role.dms_s3_reader_role.name
   policy_arn = aws_iam_policy.dms_s3_bucket_reader_policy.arn
-<<<<<<< HEAD
+}
+
+
+# The following role is used to allow listing of all buckets so that the DMS S3 Staging Bucket may be found.
+# This 1st version is used in client environments to allow the corresponding repository environment to list the local buckets.
+resource "aws_iam_role" "dms_s3_bucket_list_by_repository_role" {
+  count              = try(var.dms_config.user_target_endpoint.write_database, null) == null ? 0 : 1
+  name               = local.dms_s3_repository_list_by_repository_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+            AWS = "arn:aws:iam::${var.env_name_to_dms_config_map[var.dms_config.audit_target_endpoint.write_environment].account_id}:root"
+          }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+# Policy to list all buckets in the current environment to allow searching for the DMS S3 Staging bucket
+resource "aws_iam_policy" "dms_s3_bucket_list_by_repository_policy" {
+  count       = try(var.dms_config.user_target_endpoint.write_database, null) == null ? 0 : 1
+  name        =  "dms-s3-bucket-list-by-repository-policy"
+  description = "Policy to allow listing of S3 buckets by DMS Audit repository."
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "s3:ListAllMyBuckets",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dms_s3_bucket_list_by_repository_policy_attachment" {
+  count              = try(var.dms_config.user_target_endpoint.write_database, null) == null ? 0 : 1
+  role       = aws_iam_role.dms_s3_bucket_list_by_repository_role[0].name
+  policy_arn = aws_iam_policy.dms_s3_bucket_list_by_repository_policy[0].arn
+}
+
+
+# This 2nd version is used in repository environments to allow the corresponding client environments to list the local buckets.
+resource "aws_iam_role" "dms_s3_bucket_list_by_client_role" {
+     count  = length(local.client_account_ids) > 0 ? 1 : 0
+     name   = "dms-s3-bucket-list-by-client-role"
+     assume_role_policy = jsonencode({
+     Version = "2012-10-17"
+     Statement = [
+         {
+         Effect    = "Allow"
+         Principal = {
+            AWS = [for client_account_id in local.client_account_ids : "arn:aws:iam::${client_account_id}:root"]
+         }
+         Action    = "s3:AssumeRole"
+         }
+     ]
+   })
+}
+
+# Policy to list all buckets in the current environment to allow searching for the DMS S3 Staging bucket
+resource "aws_iam_policy" "dms_s3_bucket_list_by_client_policy" {
+  count       = length(local.client_account_ids) > 0 ? 1 : 0
+  name        = "dms-s3-bucket-list-by-client-policy"
+  description = "Policy to allow listing of S3 buckets by DMS Audit client."
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "s3:ListAllMyBuckets",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dms_s3_bucket_list_by_client_policy_attachment" {
+  count      = length(local.client_account_ids) > 0 ? 1 : 0
+  role       = aws_iam_role.dms_s3_bucket_list_by_client_role[0].name
+  policy_arn = aws_iam_policy.dms_s3_bucket_list_by_client_policy[0].arn
 }
