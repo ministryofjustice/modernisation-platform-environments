2267c08be45670ba9e69009c9598af787e9d1744
W <willg258@hotmail.co.uk>
2024-09-13 15:33:08 +0100

    Update ec2_autoscaling_group.tf

--- terraform/modules/baseline/ec2_autoscaling_group.tf
+++ terraform/modules/baseline/ec2_autoscaling_group.tf
@@ -15,7 +15,7 @@ module "ec2_autoscaling_group" {
 
   for_each = var.ec2_autoscaling_groups
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-autoscaling-group?ref=allow-update-of-random"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-autoscaling-group?ref=0111618bb1c7c52f59f11790b2f4b68a26b51cb3" # v2.6.1
 
   providers = {
     aws.core-vpc = aws.core-vpc
