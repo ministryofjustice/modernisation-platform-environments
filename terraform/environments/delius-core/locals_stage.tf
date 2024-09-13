e92b61884d51b7b0a78484fc61e6684e5b87c738
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-09-13 16:52:10 +0100

    Revert unwanted changes to EBS config

--- terraform/environments/delius-core/locals_stage.tf
+++ terraform/environments/delius-core/locals_stage.tf
@@ -23,8 +23,6 @@ locals {
     efs_provisioned_throughput  = null
     efs_backup_schedule         = "cron(0 19 * * ? *)",
     efs_backup_retention_period = "30"
-    efs_id                      = "replace_this"
-    efs_access_point_id         = "replace_this"
     port                        = 389
   }
 
@@ -41,8 +39,12 @@ locals {
     ebs_volumes = {
       "/dev/sdb" = { label = "app", size = 200 } # /u01
       "/dev/sdc" = { label = "app", size = 100 } # /u02
+      "/dev/sdd" = { label = "data" }            # DATA
       "/dev/sde" = { label = "data" }            # DATA
-      "/dev/sdf" = { label = "flash" }           # FLASH
+      "/dev/sdf" = { label = "data" }            # DATA
+      "/dev/sdg" = { label = "data" }            # DATA
+      "/dev/sdh" = { label = "data" }            # DATA
+      "/dev/sdi" = { label = "flash" }           # FLASH
       "/dev/sds" = { label = "swap" }
     }
     ebs_volume_config = {
@@ -53,15 +55,15 @@ locals {
       }
       data = {
         iops       = 3000
-        throughput = 125
+        throughput = 400
         type       = "gp3"
-        total_size = 500
+        total_size = 10000
       }
       flash = {
         iops       = 3000
-        throughput = 125
+        throughput = 400
         type       = "gp3"
-        total_size = 500
+        total_size = 1000
       }
     }
     ansible_user_data_config = {
@@ -92,12 +94,6 @@ locals {
       container_port   = 8080
       container_cpu    = 512
       container_memory = 1024
-    }
-
-    ldap = {
-      image_tag      = "replace_me"
-      container_port = 389
-      slapd_log_level = "replace_me"
     }
 
     pdf_creation = {
