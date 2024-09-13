5f96baefdbeab2d7412bfa38c91c36edfe4c7d11
George Taylor <george.taylor@digital.justice.gov.uk>
2024-09-13 12:46:58 +0100

    Changes to ldap ecs

--- terraform/environments/delius-core/modules/delius_environment/versions.tf
+++ terraform/environments/delius-core/modules/delius_environment/versions.tf
@@ -5,6 +5,10 @@ terraform {
       version               = "~> 5.0"
       configuration_aliases = [aws.bucket-replication, aws.core-vpc, aws.core-network-services]
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
   }
   required_version = ">= 1.0.1"
 }
