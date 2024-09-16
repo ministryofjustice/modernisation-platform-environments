8599df9f7551c5e1bb7f8ba30a4412dfd2536c93
Lee <lee_b250@hotmail.co.uk>
2024-09-16 12:18:27 +0100

    Amend file to use module

--- terraform/environments/electronic-monitoring-data/ems_s3_miscellaneous.tf
+++ terraform/environments/electronic-monitoring-data/ems_s3_miscellaneous.tf
@@ -1,54 +1,68 @@
-resource "aws_s3_bucket" "ems_mock_data_s3_bucket" {
-  bucket_prefix = "ems-mock-data-"
+module "ems-api-data-store" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=67165cb"
 
-  tags = merge(
-    local.tags,
+  bucket_prefix      = "ems-api-data-store-"
+  versioning_enabled = false
+
+  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
+  ownership_controls = "BucketOwnerEnforced"
+  acl                = "private"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
+  }
+
+  log_bucket = module.s3-logging-bucket.bucket.id
+  log_partition_date_source = "EventTime"
+
+  lifecycle_rule = [
     {
-      Resource_Type = "EMS semantic layer",
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 365
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 730
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 1000
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 30
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 90
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 365
+      }
     }
-  )
-}
+  ]
 
-resource "aws_s3_bucket_public_access_block" "ems_mock_data_s3_bucket" {
-  bucket                  = aws_s3_bucket.ems_mock_data_s3_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "ems_mock_data_s3_bucket" {
-  bucket = aws_s3_bucket.ems_mock_data_s3_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-data "aws_iam_policy_document" "ems_mock_data_s3_bucket" {
-  statement {
-    sid = "EnforceTLSv12orHigher"
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    effect  = "Deny"
-    actions = ["s3:*"]
-    resources = [
-      aws_s3_bucket.ems_mock_data_s3_bucket.arn,
-      "${aws_s3_bucket.ems_mock_data_s3_bucket.arn}/*"
-    ]
-    condition {
-      test     = "NumericLessThan"
-      variable = "s3:TlsVersion"
-      values   = [1.2]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "ems_mock_data_s3_bucket" {
-  bucket = aws_s3_bucket.ems_mock_data_s3_bucket.id
-  policy = data.aws_iam_policy_document.ems_mock_data_s3_bucket.json
+  tags = local.tags
 }
