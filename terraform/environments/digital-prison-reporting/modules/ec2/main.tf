56995811e0d7bb6fe9fafccca5aec11154e52279
SonyPimpale <pimpale@live.com>
2024-09-18 15:06:56 +0100

    Skip all allowing all egress traffic to public IPs

--- terraform/environments/digital-prison-reporting/modules/ec2/main.tf
+++ terraform/environments/digital-prison-reporting/modules/ec2/main.tf
@@ -59,6 +59,7 @@ resource "aws_security_group_rule" "ingress_traffic_from_security_groups" {
 resource "aws_security_group_rule" "egress_traffic" {
   #checkov:skip=CKV_AWS_23:"Ensure every security group and rule has a description"
   # tfsec:ignore:AVD-AWS-0097 - Ignoring the risk of allowing all egress traffic to public IPs
+  # tfsec:ignore:AVD-AWS-0104 - Security group rule allows egress to multiple public internet addresses.
 
   security_group_id = aws_security_group.ec2_sec_group.id
   type              = "egress"
