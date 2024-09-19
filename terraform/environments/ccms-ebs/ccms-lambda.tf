f8e43004f1bff33605c921e4b60be90460ae5179
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2024-09-19 16:02:39 +0100

    CC-2815: Creation of resources for payment load lambda

--- terraform/environments/ccms-ebs/ccms-lambda.tf
+++ terraform/environments/ccms-ebs/ccms-lambda.tf
@@ -90,7 +90,6 @@ resource "aws_s3_bucket_notification" "lambda_trigger" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda_function.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = ""
     filter_suffix       = ".xlsx"
   }
 
