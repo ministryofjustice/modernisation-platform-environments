007b9c702e8f1ac6f9bbda372fa0550f26886238
matt-heery <matt.heery@digital.justice.gov.uk>
2024-09-23 11:49:46 +0100

    adding script to compare checksums of buckets

---
+++ terraform/environments/electronic-monitoring-data/python_scripts/s3_checksum_comparison.py
@@ -0,0 +1,28 @@
+import boto3
+import typer
+
+app = typer.Typer()
+
+s3 = boto3.client("s3")
+
+
+def get_etags_all_objects(bucket_name: str):
+    "gets the metadata of all objects in an s3 bucket"
+    total_metadata = s3.list_objects_v2(bucket_name)
+    etags = {meta["Key"]: meta["ETag"] for meta in total_metadata["Contents"]}
+    return etags
+
+
+@app.command
+def compare_buckets(original_bucket: str, new_bucket: str):
+    original_bucket_tags = get_etags_all_objects(original_bucket)
+    new_bucket_tags = get_etags_all_objects(new_bucket)
+    if original_bucket_tags == new_bucket_tags:
+        print("Buckets are the same")
+    else:
+        bucket_tags_diff = set(original_bucket_tags) ^ set(new_bucket_tags)
+        print(str(bucket_tags_diff))
+
+
+if __name__ == "__main__":
+    app.run()
