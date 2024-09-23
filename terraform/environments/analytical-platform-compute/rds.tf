1feb52b3f918448f5eab6f1721d01637a80cd103
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-09-23 14:06:46 +0000

    :wrench: Maintenance: September Patching

--- terraform/environments/analytical-platform-compute/rds.tf
+++ terraform/environments/analytical-platform-compute/rds.tf
@@ -143,7 +143,7 @@ module "ui_rds" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/rds/aws"
-  version = "6.8.0"
+  version = "6.9.0"
 
   identifier = "ui"
 
--- terraform/environments/analytical-platform-compute/secrets.tf
+++ terraform/environments/analytical-platform-compute/secrets.tf
@@ -5,7 +5,7 @@ module "actions_runners_create_a_derived_table_secret" {
   count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
 
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "1.1.2"
+  version = "1.3.0"
 
   name        = "actions-runners/create-a-derived-table"
   description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/2208432"
@@ -28,7 +28,7 @@ module "actions_runners_airflow_secret" {
   count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
 
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "1.1.2"
+  version = "1.3.0"
 
   name        = "actions-runners/airflow"
   description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/3544241"
@@ -51,7 +51,7 @@ module "actions_runners_airflow_create_a_pipeline_secret" {
   count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
 
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "1.1.2"
+  version = "1.3.0"
 
   name        = "actions-runners/airflow-create-a-pipeline"
   description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/3733767"
@@ -72,7 +72,7 @@ module "ui_sentry_dsn_secret" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "1.1.2"
+  version = "1.3.0"
 
   name        = "ui/sentry-dsn"
   description = "Sentry DSN for Analytical Platform UI"
@@ -89,7 +89,7 @@ module "ui_azure_client_secret" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "1.1.2"
+  version = "1.3.0"
 
   name        = "ui/azure-client"
   description = "Azure client secret for Analytical Platform UI"
@@ -106,7 +106,7 @@ module "ui_azure_tenant_secret" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "1.1.2"
+  version = "1.3.0"
 
   name        = "ui/azure-tenant"
   description = "Azure tenant secret for Analytical Platform UI"
