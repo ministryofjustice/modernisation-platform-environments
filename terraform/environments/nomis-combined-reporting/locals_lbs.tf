9252d17181a474415f9a0545aacbc1679ec6a7f8
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-24 10:03:56 +0100

    fix

--- terraform/environments/nomis-combined-reporting/locals_lbs.tf
+++ terraform/environments/nomis-combined-reporting/locals_lbs.tf
@@ -47,6 +47,70 @@ locals {
         }
         https = {
           certificate_names_or_arns = ["nomis_combined_reporting_wildcard_cert"]
+          cloudwatch_metric_alarms  = module.baseline_presets.cloudwatch_metric_alarms.lb
+          port                      = 443
+          protocol                  = "HTTPS"
+          ssl_policy                = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+
+          default_action = {
+            type = "fixed-response"
+            fixed_response = {
+              content_type = "text/plain"
+              message_body = "Not implemented"
+              status_code  = "501"
+            }
+          }
+        }
+      }
+    }
+
+    public = {
+      # access_logs                    = true # enable this at some point
+      enable_cross_zone_load_balancing = true
+      enable_delete_protection         = false
+      force_destroy_bucket             = true
+      idle_timeout                     = 3600
+      internal_lb                      = false
+      security_groups                  = ["public-lb"]
+      subnets                          = module.environment.subnets["private"].ids
+
+      instance_target_groups = {
+        web = {
+          port     = 7777
+          protocol = "HTTP"
+          health_check = {
+            enabled             = true
+            healthy_threshold   = 3
+            interval            = 30
+            matcher             = "200-399"
+            path                = "/"
+            port                = 7777
+            timeout             = 5
+            unhealthy_threshold = 5
+          }
+          stickiness = {
+            enabled = true
+            type    = "lb_cookie"
+          }
+        }
+      }
+      listeners = {
+        http = {
+          port     = 80
+          protocol = "HTTP"
+
+          default_action = {
+            type = "redirect"
+            redirect = {
+              port        = 443
+              protocol    = "HTTPS"
+              status_code = "HTTP_301"
+            }
+          }
+        }
+        https = {
+          certificate_names_or_arns = ["nomis_combined_reporting_wildcard_cert"]
+          cloudwatch_metric_alarms  = module.baseline_presets.cloudwatch_metric_alarms.lb
           port                      = 443
           protocol                  = "HTTPS"
           ssl_policy                = "ELBSecurityPolicy-TLS13-1-2-2021-06"
