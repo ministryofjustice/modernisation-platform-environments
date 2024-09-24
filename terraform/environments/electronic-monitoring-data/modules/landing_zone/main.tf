c5d99bb9a8144e5f469a91a58a59de97d421f69e
matt-heery <matt.heery@digital.justice.gov.uk>
2024-09-24 10:18:12 +0100

    add provider

--- terraform/environments/electronic-monitoring-data/modules/landing_zone/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/landing_zone/main.tf
@@ -11,6 +11,14 @@ data "aws_iam_policy_document" "transfer_assume_role" {
   }
 }
 
+# AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
+provider "aws" {
+  region = "eu-west-2"
+  assume_role {
+    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
+  }
+}
+
 #------------------------------------------------------------------------------
 # S3 bucket for landing Supplier data
 #
