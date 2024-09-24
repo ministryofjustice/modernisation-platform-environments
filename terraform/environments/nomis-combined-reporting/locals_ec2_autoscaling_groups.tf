d9738c82d805fe91a4e1007c0f00b62cb418c735
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-09-24 10:00:16 +0100

    asgs

--- terraform/environments/nomis-combined-reporting/locals_ec2_autoscaling_groups.tf
+++ terraform/environments/nomis-combined-reporting/locals_ec2_autoscaling_groups.tf
@@ -2,6 +2,62 @@ locals {
 
   ec2_autoscaling_groups = {
 
+    bip_app = {
+      autoscaling_group = {
+        desired_capacity    = 1
+        max_size            = 1
+        force_delete        = true
+        vpc_zone_identifier = module.environment.subnets["private"].ids
+      }
+      config               = local.ec2_instances.bip_app.config
+      ebs_volumes          = local.ec2_instances.bip_app.ebs_volumes
+      instance             = local.ec2_instances.bip_app.instance
+      user_data_cloud_init = local.ec2_instances.bip_app.user_data_cloud_init
+      tags                 = local.ec2_instances.bip_app.tags
+    }
+
+    bip_cms = {
+      autoscaling_group = {
+        desired_capacity    = 1
+        max_size            = 1
+        force_delete        = true
+        vpc_zone_identifier = module.environment.subnets["private"].ids
+      }
+      config               = local.ec2_instances.bip_cms.config
+      ebs_volumes          = local.ec2_instances.bip_cms.ebs_volumes
+      instance             = local.ec2_instances.bip_cms.instance
+      user_data_cloud_init = local.ec2_instances.bip_cms.user_data_cloud_init
+      tags                 = local.ec2_instances.bip_cms.tags
+    }
+
+    bip_webadmin = {
+      autoscaling_group = {
+        desired_capacity    = 1
+        max_size            = 1
+        force_delete        = true
+        vpc_zone_identifier = module.environment.subnets["private"].ids
+      }
+      config               = local.ec2_instances.bip_webadmin.config
+      ebs_volumes          = local.ec2_instances.bip_webadmin.ebs_volumes
+      instance             = local.ec2_instances.bip_webadmin.instance
+      user_data_cloud_init = local.ec2_instances.bip_webadmin.user_data_cloud_init
+      tags                 = local.ec2_instances.bip_webadmin.tags
+    }
+
+    bip_web = {
+      autoscaling_group = {
+        desired_capacity    = 1
+        max_size            = 1
+        force_delete        = true
+        vpc_zone_identifier = module.environment.subnets["private"].ids
+      }
+      config               = local.ec2_instances.bip_web.config
+      ebs_volumes          = local.ec2_instances.bip_web.ebs_volumes
+      instance             = local.ec2_instances.bip_web.instance
+      user_data_cloud_init = local.ec2_instances.bip_web.user_data_cloud_init
+      tags                 = local.ec2_instances.bip_web.tags
+    }
+
     jumpserver = {
       autoscaling_group = {
         desired_capacity    = 1
