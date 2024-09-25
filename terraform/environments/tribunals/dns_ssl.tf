fd33c65a7c740e18985959b0b048ae47bebfc32e
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-09-25 10:51:42 +0100

    add *. back to subject_alternative_names

--- terraform/environments/tribunals/dns_ssl.tf
+++ terraform/environments/tribunals/dns_ssl.tf
@@ -2,7 +2,7 @@ resource "aws_acm_certificate" "external" {
   domain_name = local.is-production ? "*.decisions.tribunals.gov.uk" : "modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
-  subject_alternative_names = local.is-production ? null : ["${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  subject_alternative_names = local.is-production ? null : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   tags = {
     Environment = local.environment
   }
