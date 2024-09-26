cdc5e4e07017c41c726e8332d6792d331075fee0
SteveLinden <stephen.linden@digital.justice.gov.uk>
2024-09-26 13:54:27 +0100

    Amended integration key for Sprinkler

--- terraform/environments/sprinkler/monitoring.tf
+++ terraform/environments/sprinkler/monitoring.tf
@@ -47,5 +47,5 @@ module "pagerduty_core_alerts" {
   ]
   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=0179859e6fafc567843cd55c0b05d325d5012dc4" # v2.0.0
   sns_topics                = [aws_sns_topic.sprinkler_ddos_alarm.name]
-  pagerduty_integration_key = local.pagerduty_integration_keys["ddos_cloudwatch"]
+  pagerduty_integration_key = local.pagerduty_integration_keys["sprinkler_development"]
 }
