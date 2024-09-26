6d3dba59764dc6022e1d9ce44b965a4051fa5698
matt-heery <matt.heery@digital.justice.gov.uk>
2024-09-26 11:41:55 +0100

    remove self referential

--- terraform/environments/electronic-monitoring-data/api_gateway.tf
+++ terraform/environments/electronic-monitoring-data/api_gateway.tf
@@ -114,7 +114,7 @@ resource "aws_api_gateway_method_response" "response_200" {
   rest_api_id = aws_api_gateway_rest_api.get_zipped_file.id
   resource_id = aws_api_gateway_resource.get_zipped_step_function_invoke.id
   http_method = aws_api_gateway_method.get_zipped_step_function_invoke.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
+  status_code = "200"
 }
 
 resource "aws_api_gateway_usage_plan" "test_usage_plan" {
