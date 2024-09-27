146ca050f718b0c5f8e2b36a812c8654c5467c0a
Buckingham <XC01857@lumen.com>
2024-09-27 12:02:28 +0100

    Update_Lambda_Certificate_Expiry_270924

--- terraform/environments/ppud/lambda.tf
+++ terraform/environments/ppud/lambda.tf
@@ -391,3 +391,35 @@ data "archive_file" "zip_the_send_cpu_notification_code_prod" {
   output_path = "${path.module}/lambda_scripts/send_cpu_notification_prod.zip"
 }
 
+###########################################################
+# Lambda Function to check for certificate expiration - DEV
+###########################################################
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_certificates_expiry_dev" {
+  count         = local.is-development == true ? 1 : 0
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_lambda_func_certificate_expiry_dev[0].function_name
+  principal     = "lambda.alarms.cloudwatch.amazonaws.com"
+  source_arn    = "arn:aws:cloudwatch:eu-west-2:075585660276:alarm:*"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func_terminate_cpu_process_dev" {
+  count         = local.is-development == true ? 1 : 0
+  filename      = "${path.module}/lambda_scripts/certificate_expiry_dev.zip"
+  function_name = "certificate_expiry_dev"
+  role          = aws_iam_role.lambda_role_certificate_expiry_dev[0].arn
+  handler       = "certificate_expiry_dev.lambda_handler"
+  runtime       = "python3.8"
+  timeout       = 30
+  depends_on    = [aws_iam_role_policy_attachment.attach_lambda_policy_certificate_expiry_to_lambda_role_certificate_expiry_dev]
+}
+
+# Archive the zip file
+
+data "archive_file" "zip_the_certificate_expiry_dev" {
+  count       = local.is-development == true ? 1 : 0
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_scripts/"
+  output_path = "${path.module}/lambda_scripts/certificate_expiry_dev.zip"
+}
