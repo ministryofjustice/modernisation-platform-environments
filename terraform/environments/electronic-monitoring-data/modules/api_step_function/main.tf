0270fe6e2ec9355b5aae00bebf2863ba5cc054b2
matt-heery <matt.heery@digital.justice.gov.uk>
2024-09-27 11:23:12 +0100

    adding errored integration message

--- terraform/environments/electronic-monitoring-data/modules/api_step_function/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/api_step_function/main.tf
@@ -181,6 +181,17 @@ resource "aws_api_gateway_method_response" "response_200" {
   status_code = "200"
 }
 
+resource "aws_api_gateway_integration_response" "integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = "$input.json('$')"
+  }
+}
+
 # -------------------------------------------------------
 # Key set up
 # -------------------------------------------------------
@@ -239,7 +250,7 @@ resource "aws_api_gateway_method_settings" "settings" {
 resource "aws_cloudwatch_log_group" "api_gateway_logs" {
   for_each = { for stage in var.stages : stage.stage_name => stage }
   name = "/aws/apigateway/${var.api_name}-${each.key}"
-  retention_in_days = 30
+  retention_in_days = 400
 }
 
 
