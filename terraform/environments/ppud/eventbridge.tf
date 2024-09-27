17976d21ccf1587126aa8e3140163073585da1e5
Buckingham <XC01857@lumen.com>
2024-09-27 13:47:31 +0100

    Update_Eventbridge_Certificate_Approaching_Expiration_270924_2

--- terraform/environments/ppud/eventbridge.tf
+++ terraform/environments/ppud/eventbridge.tf
@@ -21,7 +21,7 @@ resource "aws_cloudwatch_event_target" "trigger_lambda_certificate_approaching_e
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_certificate_approaching_expiration_dev" {
   count         = local.is-development == true ? 1 : 0
-  statement_id  = "AllowExecutionFromCloudWatch"
+  statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.terraform_lambda_func_certificate_expiry_dev[0].function_name
   principal     = "events.amazonaws.com"
