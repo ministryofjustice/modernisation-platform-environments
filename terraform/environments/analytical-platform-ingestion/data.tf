e938a3b908b088f2325bd961cff68fd10647534e
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-09-30 16:33:06 +0000

    update ami and volume type
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/analytical-platform-ingestion/data.tf
+++ terraform/environments/analytical-platform-ingestion/data.tf
@@ -18,13 +18,6 @@ data "aws_secretsmanager_secret_version" "govuk_notify_templates" {
   secret_id = aws_secretsmanager_secret.govuk_notify_templates.id
 }
 
-data "aws_ami" "datasync" {
-  most_recent = true
-
-  owners = ["633936118553"] # Amazon
-
-  filter {
-    name   = "name"
-    values = ["aws-datasync-*"]
-  }
+data "aws_ssm_parameter" "datasync_ami" {
+  name = "/aws/service/datasync/ami"
 }
--- terraform/environments/analytical-platform-ingestion/ec2-instances.tf
+++ terraform/environments/analytical-platform-ingestion/ec2-instances.tf
@@ -5,7 +5,7 @@ module "datasync_instance" {
   version = "5.7.0"
 
   name                   = "${local.application_name}-${local.environment}-datasync"
-  ami                    = data.aws_ami.datasync.id
+  ami                    = data.aws_ssm_parameter.datasync_ami.value
   instance_type          = "m5.2xlarge"
   subnet_id              = element(module.connected_vpc.private_subnets, 0)
   vpc_security_group_ids = [module.datasync_security_group.security_group_id]
@@ -14,7 +14,7 @@ module "datasync_instance" {
     {
       encrypted   = true
       kms_key_id  = module.ec2_ebs_kms.key_arn
-      volume_type = "gp3"
+      volume_type = "gp2"
       volume_size = 80
     }
   ]
