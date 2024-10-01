11e1ed7f8ceea75c7f9337d05f5b3e1fbbb203d1
Seb Norris <sebastian.j.norris@gmail.com>
2024-10-01 11:29:18 +0100

    add tls port for ldap preprod

--- terraform/environments/delius-core/locals_preproduction.tf
+++ terraform/environments/delius-core/locals_preproduction.tf
@@ -25,6 +25,7 @@ locals {
     efs_backup_schedule         = "cron(0 19 * * ? *)",
     efs_backup_retention_period = "30"
     port                        = 389
+    tls_port                    = 636
     desired_count               = 0
   }
 
