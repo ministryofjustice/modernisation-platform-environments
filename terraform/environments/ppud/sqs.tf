843c339dcd62af83e62fbc38e65bf5baac43eaad
Buckingham <XC01857@lumen.com>
2024-10-02 13:50:41 +0100

    SQS_Deadletter_021024_2

--- terraform/environments/ppud/sqs.tf
+++ terraform/environments/ppud/sqs.tf
@@ -8,7 +8,7 @@ resource "aws_sqs_queue" "lambda_queue_prod" {
   max_message_size          = 2048
   receive_wait_time_seconds = 10
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.lambda_deadletter_queue_prod.arn
+    deadLetterTargetArn = aws_sqs_queue.lambda_deadletter_queue_prod[0].arn
     maxReceiveCount     = 4
   })
 }
@@ -20,10 +20,10 @@ resource "aws_sqs_queue" "lambda_deadletter_queue_prod" {
 
 resource "aws_sqs_queue_redrive_allow_policy" "lambda_queue_redrive_allow_policy" {
   count         = local.is-production == true ? 1 : 0
-  queue_url     = aws_sqs_queue.lambda_dead_letter_queue_prod.id
+  queue_url     = aws_sqs_queue.lambda_dead_letter_queue_prod[0].id
 
   redrive_allow_policy = jsonencode({
     redrivePermission = "byQueue",
-    sourceQueueArns   = [aws_sqs_queue.lambda_queue_prod.arn]
+    sourceQueueArns   = [aws_sqs_queue.lambda_queue_prod[0].arn]
   })
 }
