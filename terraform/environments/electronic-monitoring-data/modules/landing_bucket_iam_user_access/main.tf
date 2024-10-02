f6cf6fcec9227cd3006e481e22a06cacea5c7e31
matt-heery <matt.heery@digital.justice.gov.uk>
2024-10-02 12:18:26 +0100

    instigate secrets module

--- terraform/environments/electronic-monitoring-data/modules/landing_bucket_iam_user_access/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/landing_bucket_iam_user_access/main.tf
@@ -31,20 +31,24 @@ resource "aws_iam_access_key" "supplier" {
   user = aws_iam_user.supplier.name
 }
 
-resource "aws_secretsmanager_secret" "supplier" {
+module "secrets_manager" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  source = "terraform-aws-modules/secrets-manager/aws"
+
   name        = "iam-${aws_iam_user.supplier.name}"
   description = "IAM user access credentials for ${var.data_feed}-${var.order_type}"
+  secret_string = jsonencode({
+    key    = aws_iam_access_key.supplier.id,
+    secret = aws_iam_access_key.supplier.secret
+  })
+  enable_rotation     = true
+  rotation_lambda_arn = var.rotation_lambda_arn
+  rotation_rules = {
+    automatically_after_days = 100
+  }
   tags = merge(
     var.local_tags,
     { order_type = var.order_type },
     { data_feed = var.data_feed }
   )
-}
-
-resource "aws_secretsmanager_secret_version" "supplier" {
-  secret_id     = aws_secretsmanager_secret.supplier.id
-  secret_string = jsonencode({
-    key    = aws_iam_access_key.supplier.id,
-    secret = aws_iam_access_key.supplier.secret
-  })
 }
