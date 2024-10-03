d795852cec650fe8b4dc3501e28587c4f768f60d
matt-heery <matt.heery@digital.justice.gov.uk>
2024-10-03 10:30:00 +0100

    add put secret value

--- terraform/environments/electronic-monitoring-data/modules/landing_bucket_iam_user_access/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/landing_bucket_iam_user_access/main.tf
@@ -47,7 +47,8 @@ data "aws_iam_policy_document" "rotate_iam_keys" {
       "secretsmanager:GetSecretValue",
       "secretsmanager:ListSecrets",
       "secretsmanager:UpdateSecret",
-      "secretsmanager:ListSecretVersionIds"
+      "secretsmanager:ListSecretVersionIds",
+      "secretsmanager:PutSecretValue"
     ]
     resources = [module.secrets_manager.secret_arn]
   }
