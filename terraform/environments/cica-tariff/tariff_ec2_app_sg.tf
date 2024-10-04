7835741d67c85fa96536806c5f37383cfaeac566
Edward Duddy <edward.duddy@cica.gov.uk>
2024-10-04 11:00:25 +0100

    🔒️ Allow ingress from CICA
    Add ingress rules to allow traffic from CICA on-prem

--- terraform/environments/cica-tariff/tariff_ec2_app_sg.tf
+++ terraform/environments/cica-tariff/tariff_ec2_app_sg.tf
@@ -24,21 +24,21 @@ resource "aws_security_group" "tariff_app_security_group" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b, local.cidr_cica_uat_a, local.cidr_cica_uat_b]
+    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b, local.cidr_cica_uat_a, local.cidr_cica_uat_b, local.cidr_cica_onprem_uat]
   }
 
   ingress {
     protocol    = "icmp"
     from_port   = -1
     to_port     = -1
-    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b, local.cidr_cica_uat_a, local.cidr_cica_uat_b]
+    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b, local.cidr_cica_uat_a, local.cidr_cica_uat_b, local.cidr_cica_onprem_uat]
   }
 
   ingress {
     protocol    = "tcp"
     from_port   = 1521
     to_port     = 1521
-    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b]
+    cidr_blocks = [data.aws_vpc.shared.cidr_block, local.cidr_cica_ss_a, local.cidr_cica_ss_b, local.cidr_cica_uat_a, local.cidr_cica_uat_b, local.cidr_cica_onprem_uat]
 
   }
 
