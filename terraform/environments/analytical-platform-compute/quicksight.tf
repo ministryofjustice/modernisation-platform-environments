e4f2a1bf46388e6ba7648a54c5b59a8670ad3344
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-10-04 10:40:56 +0100

    Ignoreing author and admin groups as those are not managed in code

--- terraform/environments/analytical-platform-compute/quicksight.tf
+++ terraform/environments/analytical-platform-compute/quicksight.tf
@@ -6,4 +6,9 @@ resource "aws_quicksight_account_subscription" "subscription" {
   admin_group                      = ["analytical-platform"]
   author_group                     = ["analytical-platform"]
   notification_email               = local.environment_configuration.quicksight_notification_email
+  lifecycle {
+    ignore_changes = [
+      author_group, # not managed in code
+      admin_group
+    ]
 }
