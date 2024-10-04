5bc6d2b925b12670a96ab2c75b58a32c5b3f09b5
matt-heery <matt.heery@digital.justice.gov.uk>
2024-10-04 10:39:45 +0100

    no data eng in dev lol

--- terraform/environments/electronic-monitoring-data/analytical_platform_share.tf
+++ terraform/environments/electronic-monitoring-data/analytical_platform_share.tf
@@ -26,7 +26,7 @@ data "aws_iam_session_context" "current" {
 }
 
 data "aws_iam_roles" "data_engineering_roles" {
-  name_regex = "AWSReservedSSO_modernisation-platform-data-eng*"
+  name_regex = "AWSReservedSSO_modernisation-platform-4*"
 }
 
 ## DBT Analytics EKS Cluster Identifier
@@ -282,19 +282,20 @@ resource "aws_iam_role_policy_attachment" "analytical_platform_share_policy_atta
 }
 
 
-# resource "aws_lakeformation_data_lake_settings" "lake_formation" {
-#   admins = flatten([[for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn], data.aws_iam_session_context.current.issuer_arn, try(one(data.aws_iam_roles.data_engineering_roles.arns), [])])
+resource "aws_lakeformation_data_lake_settings" "lake_formation" {
+  admins = flatten([[for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn], data.aws_iam_session_context.current.issuer_arn, try(one(data.aws_iam_roles.data_engineering_roles.arns), [])])
 
-#   # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
-#   create_database_default_permissions {
-#     # These settings should replicate current behaviour: LakeFormation is Ignored
-#     permissions = ["ALL"]
-#     principal   = "IAM_ALLOWED_PRINCIPALS"
-#   }
+  # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
+  create_database_default_permissions {
+    # These settings should replicate current behaviour: LakeFormation is Ignored
+    permissions = ["ALL"]
+    principal   = "IAM_ALLOWED_PRINCIPALS"
+  }
 
-#   create_table_default_permissions {
-#     # These settings should replicate current behaviour: LakeFormation is Ignored
-#     permissions = ["ALL"]
-#     principal   = "IAM_ALLOWED_PRINCIPALS"
-#   }
-# }
+  create_table_default_permissions {
+    # These settings should replicate current behaviour: LakeFormation is Ignored
+    permissions = ["ALL"]
+    principal   = "IAM_ALLOWED_PRINCIPALS"
+  }
+}
+
