9f9640c93547a61596f9a10e448246a8cadee322
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-10-08 09:14:17 +0100

    TM: onr: secrets for bods db (#8095)
    * add preprod
    
    * add prod secrets
    
    * -

--- terraform/environments/oasys/locals_preproduction.tf
+++ terraform/environments/oasys/locals_preproduction.tf
@@ -299,6 +299,10 @@ locals {
       "/oracle/database/PPONRBDS" = local.secretsmanager_secrets.db
       "/oracle/database/PPMISTN2" = local.secretsmanager_secrets.db
       "/oracle/database/PPOASRP2" = local.secretsmanager_secrets.db
+      "/oracle/database/PPMISTN3" = local.secretsmanager_secrets.db # for AWS BODS testing
+      "/oracle/database/PPOASRP3" = local.secretsmanager_secrets.db # for AWS BODS testing
+      "/oracle/database/PPBOSYS"  = local.secretsmanager_secrets.db_bip
+      "/oracle/database/PPBOAUD"  = local.secretsmanager_secrets.db_bip
 
       # for azure, remove when migrated to aws db
       "/oracle/database/OASPROD" = local.secretsmanager_secrets.db_oasys
