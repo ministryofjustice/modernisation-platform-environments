6d139dd0d48d520df8cfb5b436e9aff8789764f7
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-10-08 17:04:45 +0100

    ncr: TM-534: add web server to preprod

--- terraform/environments/nomis-combined-reporting/locals_preproduction.tf
+++ terraform/environments/nomis-combined-reporting/locals_preproduction.tf
@@ -195,6 +195,18 @@ locals {
           nomis-combined-reporting-environment = "pp"
         })
       })
+
+      pp-ncr-web-1 = merge(local.ec2_instances.bip_web, {
+        config = merge(local.ec2_instances.bip_web.config, {
+          availability_zone = "eu-west-2a"
+          instance_profile_policies = concat(local.ec2_instances.bip_web.config.instance_profile_policies, [
+            "Ec2PPReportingPolicy",
+          ])
+        })
+        tags = merge(local.ec2_instances.bip_web.tags, {
+          nomis-combined-reporting-environment = "pp"
+        })
+      })
     }
 
     efs = {
@@ -293,6 +305,7 @@ locals {
           })
           pp-http-7777 = merge(local.lbs.public.instance_target_groups.http-7777, {
             attachments = [
+              { ec2_instance_name = "pp-ncr-web-1" },
             ]
           })
         }
