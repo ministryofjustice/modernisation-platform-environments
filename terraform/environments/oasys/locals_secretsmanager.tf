da7903d1ca66fd90167d2060edc1312ea22b2790
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-10-08 12:08:28 +0100

    share access to oasys-national-reporting-test account

--- terraform/environments/oasys/locals_secretsmanager.tf
+++ terraform/environments/oasys/locals_secretsmanager.tf
@@ -1,5 +1,14 @@
 locals {
 
+  domain_share_secret_principal_ids = {
+    development = []
+    test = [
+      "arn:aws:iam::${module.environment.account_ids.oasys-national-reporting-test}:policy/EC2SecretPolicy",
+    ]
+    preproduction = []
+    production    = []
+  }
+
   secretsmanager_secrets = {
 
     bip = {
