f3d4d8f3878039b3ad5d15478de1beb0bc99bea5
matt-heery <matt.heery@digital.justice.gov.uk>
2024-10-08 16:51:04 +0100

    tech debt-ing step functions

--- terraform/environments/electronic-monitoring-data/step_functions_iam.tf
+++ terraform/environments/electronic-monitoring-data/step_functions_iam.tf
@@ -27,128 +27,17 @@ data "aws_iam_policy_document" "lambda_invoke_policy" {
   }
 }
 
-
-resource "aws_iam_policy" "step_function_kms_policy" {
-  name        = "step-function-athena-layer-kms-policy"
-  description = "Policy for Lambda to use KMS key for athena-layer step function"
-
-  policy = data.aws_iam_policy_document.step_function_kms_policy.json
-}
-
-data "aws_iam_policy_document" "step_function_kms_policy" {
-  statement {
-    actions = [
-      "kms:Decrypt",
-      "kms:Encrypt",
-      "kms:GenerateDataKey"
-    ]
-    resources = [aws_kms_key.athena_layer_step_functions_log_key.arn]
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "step_function_kms_policy_policy_attachment" {
-  role       = aws_iam_role.step_functions_role.name
-  policy_arn = aws_iam_policy.step_function_kms_policy.arn
-}
-
-
-resource "aws_iam_policy" "step_function_log_policy" {
-  name        = "step-function-athena-layer-log-policy"
-  description = "Policy for Lambda to put logs for athena-layer step function"
-
-  policy = data.aws_iam_policy_document.step_function_logs_policy.json
-}
-
-
-data "aws_iam_policy_document" "step_function_logs_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogDelivery",
-      "logs:GetLogDelivery",
-      "logs:UpdateLogDelivery",
-      "logs:DeleteLogDelivery",
-      "logs:ListLogDeliveries",
-      "logs:PutResourcePolicy",
-      "logs:DescribeResourcePolicies",
-      "logs:DescribeLogGroups"
-    ]
-    resources = [
-      "*"
-    ]
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "step_function_log_policy_policy_attachment" {
-  role       = aws_iam_role.step_functions_role.name
-  policy_arn = aws_iam_policy.step_function_log_policy.arn
-}
-
-data "aws_iam_policy_document" "xray_policy" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "xray:PutTraceSegments",
-      "xray:PutTelemetryRecords",
-      "xray:GetSamplingRules",
-      "xray:GetSamplingTargets",
-    ]
-
-    resources = [
-      "*",
-    ]
-  }
-}
-
-data "aws_iam_policy_document" "assume_step_functions" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["states.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 resource "aws_iam_policy" "lambda_invoke_policy" {
   name        = "LambdaInvokePolicy"
   description = "Policy to allow invoking specific Lambda functions"
   policy      = data.aws_iam_policy_document.lambda_invoke_policy.json
 }
 
-resource "aws_iam_policy" "xray_policy" {
-  name        = "XRayPolicy"
-  description = "Policy to allow X-Ray actions"
-  policy      = data.aws_iam_policy_document.xray_policy.json
-}
-
-resource "aws_iam_role" "step_functions_role" {
-  name               = "StepFunctionsExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_step_functions.json
-}
-
-resource "aws_iam_role_policy_attachment" "attach_lambda_invoke_policy" {
-  role       = aws_iam_role.step_functions_role.name
-  policy_arn = aws_iam_policy.lambda_invoke_policy.arn
-}
-
-resource "aws_iam_role_policy_attachment" "attach_xray_policy" {
-  role       = aws_iam_role.step_functions_role.name
-  policy_arn = aws_iam_policy.xray_policy.arn
-}
-
-
 # --------------------------------
 # Send database to AP
 # -------------------------------- 
-resource "aws_iam_role" "send_database_to_ap" {
-  name               = "send_database_to_ap"
-  assume_role_policy = data.aws_iam_policy_document.assume_step_functions.json
-}
 
-data "aws_iam_policy_document" "send_database_to_ap_athena_queries" {
+data "aws_iam_policy_document" "send_database_to_ap" {
   statement {
     effect = "Allow"
 
@@ -193,9 +82,7 @@ data "aws_iam_policy_document" "send_database_to_ap_athena_queries" {
       "*"
     ]
   }
-}
 
-data "aws_iam_policy_document" "send_tables_to_ap_lambda_invoke_policy" {
   statement {
     effect = "Allow"
 
@@ -226,61 +113,28 @@ data "aws_iam_policy_document" "send_tables_to_ap_lambda_invoke_policy" {
   }
 }
 
-resource "aws_iam_policy" "send_database_to_ap_athena_queries" {
+resource "aws_iam_policy" "send_database_to_ap" {
   name        = "send_database_to_ap_athena_queries"
   description = "Policy to allow start and get specific Athena queries"
   policy      = data.aws_iam_policy_document.send_database_to_ap_athena_queries.json
 }
 
-resource "aws_iam_role_policy_attachment" "send_database_to_ap_athena_queries_attach_send_database_to_ap" {
-  role       = aws_iam_role.send_database_to_ap.name
-  policy_arn = aws_iam_policy.send_database_to_ap_athena_queries.arn
-}
+# ------------------------------------------
+# Unzip Files
+# ------------------------------------------
 
-resource "aws_iam_policy" "send_tables_to_ap_lambda_invoke_policy" {
-  name        = "send_tables_to_ap_lambda_invoke_policy"
-  description = "Policy to allow invoking specific Lambda functions"
-  policy      = data.aws_iam_policy_document.send_tables_to_ap_lambda_invoke_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "send_to_database_attach_lambda_invoke_policy" {
-  role       = aws_iam_role.send_database_to_ap.name
-  policy_arn = aws_iam_policy.send_tables_to_ap_lambda_invoke_policy.arn
-}
-
-data "aws_iam_policy_document" "send_database_to_ap_kms_policy" {
+data "aws_iam_policy_document" "trigger_unzip_lambda" {
   statement {
-    actions = [
-      "kms:Decrypt",
-      "kms:Encrypt",
-      "kms:GenerateDataKey"
+    effect  = "Allow"
+    actions = ["lambda:InvokeFunction"]
+    resources = [
+      module.unzip_single_file.lambda_function_arn,
+      module.unzipped_presigned_url.lambda_function_arn
     ]
-    resources = [aws_kms_key.send_database_to_ap_step_functions_log_key.arn]
   }
 }
 
-resource "aws_iam_policy" "send_database_to_ap_step_function_kms_policy" {
-  name        = "send-database-to-ap-kms-policy"
-  description = "Policy for Lambda to use KMS key for send_database_to_ap step function"
-
-  policy = data.aws_iam_policy_document.send_database_to_ap_kms_policy.json
+resource "aws_iam_policy" "trigger_unzip_lambda" {
+  name   = "trigger_unzip_lambda"
+  policy = data.aws_iam_policy_document.trigger_unzip_lambda.json
 }
-
-
-resource "aws_iam_role_policy_attachment" "send_to_database_step_function_kms_policy_policy_attachment" {
-  role       = aws_iam_role.send_database_to_ap.name
-  policy_arn = aws_iam_policy.send_database_to_ap_step_function_kms_policy.arn
-}
-
-
-resource "aws_iam_role_policy_attachment" "send_to_database_policy_attachment" {
-  role       = aws_iam_role.send_database_to_ap.name
-  policy_arn = aws_iam_policy.step_function_log_policy.arn
-}
-
-
-resource "aws_iam_role_policy_attachment" "send_to_database_x_ray_policy_attachment" {
-  role       = aws_iam_role.send_database_to_ap.name
-  policy_arn = aws_iam_policy.xray_policy.arn
-}
-
--- terraform/environments/electronic-monitoring-data/step_functions_main.tf
+++ terraform/environments/electronic-monitoring-data/step_functions_main.tf
@@ -1,239 +1,44 @@
-resource "aws_sfn_state_machine" "athena_layer" {
-  name     = "athena-layer"
-  role_arn = aws_iam_role.step_functions_role.arn
+# ------------------------------------------
+# Fake Athena Layer
+# ------------------------------------------
 
-  definition = jsonencode(
-    {
-      "StartAt" : "GetMetadataList",
-      "States" : {
-        "GetMetadataList" : {
-          "Type" : "Task",
-          "Resource" : "${module.get_metadata_from_rds_lambda.lambda_function_arn}",
-          "ResultPath" : "$.metadata_list",
-          "Next" : "LoopThroughMetadataList"
-        },
-        "LoopThroughMetadataList" : {
-          "Type" : "Map",
-          "ItemsPath" : "$.metadata_list.metadata_list",
-          "MaxConcurrency" : 4,
-          "Iterator" : {
-            "StartAt" : "CreateAthenaTable",
-            "States" : {
-              "CreateAthenaTable" : {
-                "Type" : "Task",
-                "Resource" : "${module.create_athena_table.lambda_function_arn}",
-                "ResultPath" : "$.result",
-                "End" : true
-              }
-            }
-          },
-          "End" : true
-        }
-      }
-    }
-  )
-
+module "athena_layer" {
+  source       = "./modules/step_function"
+  name         = "get_zipped_file"
+  iam_policies = tomap({ "lambda_invoke_policy" = aws_iam_policy.lambda_invoke_policy })
+  variable_dictionary = tomap({
+    get_metadata_lambda_arn = module.get_metadata_from_rds_lambda.lambda_function_arn
+    create_athena_table     = module.create_athena_table.lambda_function_arn
+  })
 }
 
-resource "aws_kms_key" "athena_layer_step_functions_log_key" {
-  description         = "KMS key for encrypting Step Functions logs for athena_layer"
-  enable_key_rotation = true
 
-  policy = jsonencode(
-    {
-      "Id" : "key-default",
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Sid" : "Enable IAM User Permissions",
-          "Effect" : "Allow",
-          "Principal" : {
-            "AWS" : "arn:aws:iam::${local.env_account_id}:root"
-          },
-          "Action" : "kms:*",
-          "Resource" : "*"
-        },
-        {
-          "Sid" : "Enable log service Permissions",
-          "Effect" : "Allow",
-          "Principal" : {
-            "Service" : "logs.eu-west-2.amazonaws.com"
-          },
-          "Action" : [
-            "kms:Encrypt*",
-            "kms:Decrypt*",
-            "kms:ReEncrypt*",
-            "kms:GenerateDataKey*",
-            "kms:Describe*"
-          ],
-          "Resource" : "*"
-        }
-      ]
-    }
-  )
-}
-
-resource "aws_cloudwatch_log_group" "athena_layer" {
-  name              = "/aws/vendedlogs/states/athena_layer"
-  retention_in_days = 400
-  kms_key_id        = aws_kms_key.athena_layer_step_functions_log_key.arn
-}
 # ------------------------------------------
 # Send Database to AP
 # ------------------------------------------
 
-resource "aws_sfn_state_machine" "send_database_to_ap" {
-  name     = "send_database_to_ap_layer"
-  role_arn = aws_iam_role.send_database_to_ap.arn
-
-  definition = jsonencode(
-    {
-      "StartAt" : "GetValidatedTableList",
-      "States" : {
-        "GetValidatedTableList" : {
-          "Type" : "Task",
-          "Resource" : "arn:aws:states:::athena:startQueryExecution.sync",
-          "Parameters" : {
-            "QueryString.$" : "States.Format('SELECT database_name, split(validation_msg, \\' - \\', 2)[1] as table_name FROM \"dms_data_validation\".\"glue_df_output\" WHERE validation_msg like \\'%Validated%\\' and database_name = \\'{}\\' and table_to_ap = \\'False\\'', $.db_name)",
-            "WorkGroup" : "${aws_athena_workgroup.default.name}",
-          },
-          "ResultPath" : "$.queryResult",
-          "Next" : "GetQueryResults"
-        },
-        "GetQueryResults" : {
-          "Type" : "Task",
-          "Resource" : "arn:aws:states:::athena:getQueryResults",
-          "Parameters" : {
-            "QueryExecutionId.$" : "$.queryResult.QueryExecution.QueryExecutionId"
-          },
-          "ResultPath" : "$.queryOutput",
-          "Next" : "QueryOutputToList"
-        },
-        "QueryOutputToList" : {
-          "Type" : "Task",
-          "Resource" : "${module.query_output_to_list.lambda_function_arn}",
-          "ResultPath" : "$.queryOutputList",
-          "Next" : "LoopThroughTables"
-        },
-        "LoopThroughTables" : {
-          "Type" : "Map",
-          "ItemsPath" : "$.queryOutputList",
-          "MaxConcurrency" : 4,
-          "Iterator" : {
-            "StartAt" : "GetTableFileNames",
-            "States" : {
-              "GetTableFileNames" : {
-                "Type" : "Task",
-                "Resource" : "${module.get_file_keys_for_table.lambda_function_arn}",
-                "ResultPath" : "$.fileKeys",
-                "Next" : "LoopThroughFileKeys"
-              },
-              "LoopThroughFileKeys" : {
-                "Type" : "Map",
-                "ItemsPath" : "$.fileKeys",
-                "MaxConcurrency" : 4,
-                "OutputPath" : "$[0].dbInfo",
-                "Iterator" : {
-                  "StartAt" : "SendTableToAp",
-                  "States" : {
-                    "SendTableToAp" : {
-                      "Type" : "Task",
-                      "Resource" : "${module.send_table_to_ap.lambda_function_arn}",
-                      "InputPath" : "$",
-                      "ResultPath" : "$.dbInfo",
-                      "End" : true
-                    }
-                  }
-                }
-                "Next" : "UpdateLogTable"
-              },
-              "UpdateLogTable" : {
-                "Type" : "Task",
-                "Resource" : "${module.update_log_table.lambda_function_arn}",
-                "End" : true
-              }
-            },
-          }
-          "Next" : "FixLogTable"
-        },
-        "FixLogTable" : {
-          "Type" : "Task",
-          "Resource" : "arn:aws:states:::athena:startQueryExecution.sync",
-          "Parameters" : {
-            "QueryString" : "MSCK REPAIR TABLE dms_data_validation.glue_df_output",
-            "WorkGroup" : "${aws_athena_workgroup.default.name}"
-          },
-          "End" : true
-        }
-      }
-    }
-  )
+module "send_database_to_ap" {
+  source       = "./modules/step_function"
+  name         = "send_database_to_ap"
+  iam_policies = tomap({ "send_database_to_ap" = aws_iam_policy.send_database_to_ap })
+  variable_dictionary = tomap({
+    athena_workgroup        = aws_athena_workgroup.default.name
+    query_output_to_list    = module.query_output_to_list.lambda_function_arn
+    get_file_keys_for_table = module.get_file_keys_for_table.lambda_function_arn
+    send_table_to_ap        = module.send_table_to_ap.lambda_function_arn
+    update_log_table        = module.update_log_table.lambda_function_arn
+  })
 }
 
-resource "aws_kms_key" "send_database_to_ap_step_functions_log_key" {
-  description         = "KMS key for encrypting Step Functions logs for send_database_to_ap"
-  enable_key_rotation = true
+# ------------------------------------------
+# Unzip Files
+# ------------------------------------------
 
-  policy = jsonencode(
-    {
-      "Id" : "key-default",
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Sid" : "Enable IAM User Permissions",
-          "Effect" : "Allow",
-          "Principal" : {
-            "AWS" : "arn:aws:iam::${local.env_account_id}:root"
-          },
-          "Action" : "kms:*",
-          "Resource" : "*"
-        },
-        {
-          "Sid" : "Enable log service Permissions",
-          "Effect" : "Allow",
-          "Principal" : {
-            "Service" : "logs.eu-west-2.amazonaws.com"
-          },
-          "Action" : [
-            "kms:Encrypt*",
-            "kms:Decrypt*",
-            "kms:ReEncrypt*",
-            "kms:GenerateDataKey*",
-            "kms:Describe*"
-          ],
-          "Resource" : "*"
-        }
-      ]
-    }
-  )
-}
-
-resource "aws_cloudwatch_log_group" "send_database_to_ap" {
-  name              = "/aws/vendedlogs/states/send_database_to_ap"
-  retention_in_days = 400
-  kms_key_id        = aws_kms_key.send_database_to_ap_step_functions_log_key.arn
-}
-
-data "aws_iam_policy_document" "trigger_unzip_lambda" {
-  statement {
-    effect  = "Allow"
-    actions = ["lambda:InvokeFunction"]
-    resources = [
-      module.unzip_single_file.lambda_function_arn,
-      module.unzipped_presigned_url.lambda_function_arn
-    ]
-  }
-}
-
-resource "aws_iam_policy" "trigger_unzip_lambda" {
-  name   = "trigger_unzip_lambda"
-  policy = data.aws_iam_policy_document.trigger_unzip_lambda.json
-}
 
 module "get_zipped_file" {
-  source         = "./modules/step_function"
-  name           = "get_zipped_file"
-  iam_policies   = tomap({ "trigger_unzip_lambda" = aws_iam_policy.trigger_unzip_lambda })
+  source       = "./modules/step_function"
+  name         = "get_zipped_file"
+  iam_policies = tomap({ "trigger_unzip_lambda" = aws_iam_policy.trigger_unzip_lambda })
   variable_dictionary = tomap(
     {
       "unzip_file_name"            = module.unzip_single_file.lambda_function_name,
