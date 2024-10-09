dcfc1ec14d4ebaebc53de86a04d59ca6ffaed6c6
Buckingham <XC01857@lumen.com>
2024-10-09 11:50:06 +0100

    Update_CW_Monitoring_091024_12

--- terraform/environments/ppud/cloudwatch_windows.tf
+++ terraform/environments/ppud/cloudwatch_windows.tf
@@ -10,14 +10,6 @@ data "aws_instances" "windows_tagged_instances" {
   }
 }
 
-# Data source for all Windows servers tagged as having E volumes
-data "aws_instances" "e_volume_tagged_instances" {
-  filter {
-    name   = "tag:e_volume"
-    values = ["yes"]
-  }
-}
-
 # Data source for ImageId and InstanceType for each instance
 data "aws_instance" "instance_details" {
   for_each    = toset(data.aws_instances.windows_tagged_instances.ids)
@@ -74,11 +66,12 @@ resource "aws_cloudwatch_metric_alarm" "low_disk_space_D_volume" {
   }
 }
 
-# Low Disk Alarm for all Windows instances with E Volumes
+# Low Disk Alarms for all Windows instances with E Volumes
+# There are currently 3 instances; RGVW021, RGVW022 and RGVW027
+# Each have different alert thresholds
 
-resource "aws_cloudwatch_metric_alarm" "low_disk_space_E_volume" {
-  for_each            = toset(data.aws_instances.e_volume_tagged_instances.ids)
-  alarm_name          = "Low-Disk-Space-E-Volume-${each.key}"
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_E_volume_rgvw021" {
+  alarm_name          = "Low-Disk-Space-E-Volume-i-080498c4c9d25e6bd"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "5"
   datapoints_to_alarm = "5"
@@ -91,10 +84,197 @@ resource "aws_cloudwatch_metric_alarm" "low_disk_space_E_volume" {
   alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
   alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
   dimensions = {
-    InstanceId = each.key
+    InstanceId = "i-080498c4c9d25e6bd"
     instance   = "E:"
-    ImageId    = data.aws_instance.instance_details[each.value].ami
-    InstanceType = data.aws_instance.instance_details[each.value].instance_type
+    ImageId    = "ami-05ddec53aa481cbc3"
+    InstanceType = "m5.2xlarge"
+    objectname = "LogicalDisk"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_E_volume_rgvw022" {
+  alarm_name          = "Low-Disk-Space-E-Volume-i-029d2b17679dab982"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "5"
+  metric_name         = "LogicalDisk % Free Space"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "0.5"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
+  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  dimensions = {
+    InstanceId = "i-029d2b17679dab982"
+    instance   = "E:"
+    ImageId    = "ami-02f8251c8cdf2464f"
+    InstanceType = "m5.xlarge"
+    objectname = "LogicalDisk"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_E_volume_rgvw027" {
+  alarm_name          = "Low-Disk-Space-E-Volume-i-00cbccc46d25e77c6"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "5"
+  metric_name         = "LogicalDisk % Free Space"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "1"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
+  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  dimensions = {
+    InstanceId = "i-00cbccc46d25e77c6"
+    instance   = "E:"
+    ImageId    = "ami-0e203fec985af6465"
+    InstanceType = "m5.xlarge"
+    objectname = "LogicalDisk"
+  }
+}
+
+# Low Disk Alarms for all Windows instances with F Volumes
+# There are currently 3 instances; RGVW021, RGVW022 and RGVW027
+# Each have different alert thresholds
+
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_F_volume_rgvw021" {
+  alarm_name          = "Low-Disk-Space-F-Volume-i-080498c4c9d25e6bd"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "5"
+  metric_name         = "LogicalDisk % Free Space"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "5"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
+  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  dimensions = {
+    InstanceId = "i-080498c4c9d25e6bd"
+    instance   = "F:"
+    ImageId    = "ami-05ddec53aa481cbc3"
+    InstanceType = "m5.2xlarge"
+    objectname = "LogicalDisk"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_F_volume_rgvw022" {
+  alarm_name          = "Low-Disk-Space-F-Volume-i-029d2b17679dab982"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "5"
+  metric_name         = "LogicalDisk % Free Space"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "1"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
+  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  dimensions = {
+    InstanceId = "i-029d2b17679dab982"
+    instance   = "E:"
+    ImageId    = "ami-02f8251c8cdf2464f"
+    InstanceType = "m5.xlarge"
+    objectname = "LogicalDisk"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_F_volume_rgvw027" {
+  alarm_name          = "Low-Disk-Space-F-Volume-i-00cbccc46d25e77c6"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "5"
+  metric_name         = "LogicalDisk % Free Space"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "2"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
+  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  dimensions = {
+    InstanceId = "i-00cbccc46d25e77c6"
+    instance   = "F:"
+    ImageId    = "ami-0e203fec985af6465"
+    InstanceType = "m5.xlarge"
+    objectname = "LogicalDisk"
+  }
+}
+
+# Low Disk Alarms for all Windows instances with G Volumes
+# There are currently 3 instances; RGVW021 and RGVW022
+# Each have different alert thresholds
+
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_G_volume_rgvw021" {
+  alarm_name          = "Low-Disk-Space-G-Volume-i-080498c4c9d25e6bd"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "5"
+  metric_name         = "LogicalDisk % Free Space"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "5"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
+  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  dimensions = {
+    InstanceId = "i-080498c4c9d25e6bd"
+    instance   = "G:"
+    ImageId    = "ami-05ddec53aa481cbc3"
+    InstanceType = "m5.2xlarge"
+    objectname = "LogicalDisk"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_G_volume_rgvw022" {
+  alarm_name          = "Low-Disk-Space-G-Volume-i-029d2b17679dab982"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "5"
+  metric_name         = "LogicalDisk % Free Space"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "2"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
+  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  dimensions = {
+    InstanceId = "i-029d2b17679dab982"
+    instance   = "G:"
+    ImageId    = "ami-02f8251c8cdf2464f"
+    InstanceType = "m5.xlarge"
+    objectname = "LogicalDisk"
+  }
+}
+
+# Low Disk Alarms for all Windows instances with H Volumes
+# There is currently only 1 instance RGVW027
+
+resource "aws_cloudwatch_metric_alarm" "low_disk_space_H_volume_rgvw027" {
+  alarm_name          = "Low-Disk-Space-H-Volume-i-00cbccc46d25e77c6"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  datapoints_to_alarm = "5"
+  metric_name         = "LogicalDisk % Free Space"
+  namespace           = "CWAgent"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "5"
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "This metric monitors the amount of free disk space on the instance. If the amount of free disk space falls below 5% for 5 minutes, the alarm will trigger"
+  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  dimensions = {
+    InstanceId = "i-00cbccc46d25e77c6"
+    instance   = "H:"
+    ImageId    = "ami-0e203fec985af6465"
+    InstanceType = "m5.xlarge"
     objectname = "LogicalDisk"
   }
 }
