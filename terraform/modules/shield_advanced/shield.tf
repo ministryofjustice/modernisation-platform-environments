bc4a0b1f3b454d32a94e97b0479e2befbfde293a
George Taylor <george.taylor@digital.justice.gov.uk>
2024-10-10 11:41:45 +0100

    fix: added count but forgot to add resource index

--- terraform/modules/shield_advanced/shield.tf
+++ terraform/modules/shield_advanced/shield.tf
@@ -99,6 +99,6 @@ resource "aws_cloudwatch_log_group" "waf" {
 
 resource "aws_wafv2_web_acl_logging_configuration" "waf" {
   count                   = var.enable_logging ? 1 : 0
-  log_destination_configs = [aws_cloudwatch_log_group.waf.arn]
-  resource_arn            = aws_wafv2_web_acl.this.arn
+  log_destination_configs = try([aws_cloudwatch_log_group.waf[0].arn], [])
+  resource_arn            = aws_wafv2_web_acl.main.arn
 }
