1e9268f6ec741f3e94f8633575ec166bbaeb3eed
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-10-11 16:25:12 +0100

    init of iam role module

--- terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
+++ terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
@@ -16,63 +16,13 @@ module "test_ap_airflow" {
   oidc_arn            = aws_iam_openid_connect_provider.analytical_platform_compute.arn
 }
 
-
-data "aws_iam_policy_document" "load_unstructured_atrium_database" {
-  statement {
-    sid    = "GetAtriumFiles"
-    effect = "Allow"
-    actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:DeleteObject",
-      "s3:GetObjectAttributes"
-    ]
-    resources = ["${module.s3-data-bucket.bucket.arn}/g4s/atrium_unstructured/*"]
-  }
-  statement {
-    sid       = "ListDataBucket"
-    effect    = "Allow"
-    actions   = ["s3:ListBucket"]
-    resources = [module.s3-data-bucket.bucket.arn]
-  }
-  statement {
-    sid    = "AthenaPermissionsForLoadAtriumUnstructured"
-    effect = "Allow"
-    actions = [
-      "athena:StartQueryExecution",
-      "athena:GetQueryExecution",
-      "athena:GetQueryResults",
-      "athena:StopQueryExecution"
-    ]
-    resources = [aws_athena_workgroup.default.arn]
-  }
-  statement {
-    sid    = "GluePermissionsForLoadAtriumUnstructured"
-    effect = "Allow"
-    actions = [
-      "glue:GetTable",
-      "glue:GetDatabase",
-      "glue:GetDatabases",
-      "glue:CreateTable",
-      "glue:DeleteTable",
-      "glue:CreateDatabase",
-      "glue:DeleteDatabase",
-      "glue:UpdateTable"
-    ]
-    resources = [
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/g4s_atrium_unstructured",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/g4s_atrium_unstructured/*"
-    ]
-  }
-}
-
 module "load_unstructured_atrium_database" {
-  count               = local.is-production ? 1 : 0
-  source              = "./modules/ap_airflow_iam_role"
-  role_name_suffix    = "load-unstructured-atrium-database"
-  role_description    = "Atrium database permissions"
-  iam_policy_document = data.aws_iam_policy_document.load_unstructured_atrium_database.json
-  secret_code         = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
-  oidc_arn            = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  count              = local.is-production ? 1 : 0
+  source             = "./modules/ap_airflow_load_data_iam_role"
+  name               = "unstructured-atrium-database"
+  database_name      = "g4s-atrium-unstructured"
+  path_to_data       = "/g4s/atrium_unstructured"
+  source_data_bucket = module.s3-data-bucket-bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
 }
---
+++ terraform/environments/electronic-monitoring-data/modules/ap_airflow_load_data_iam_role/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type        = string
+  description = "name of the pipeline"
+}
+
+variable "database_name" {
+  type        = string
+  description = "name of the database to load to"
+}
+
+variable "path_to_data" {
+  type        = string
+  description = "path to data in source bucket"
+  default     = ""
+}
+
+variable "source_data_bucket" {
+  type        = object(any)
+  description = "source of the data in s3"
+}
+
+
+variable "secret_code" {
+  type     = string
+  nullable = false
+}
+
+variable "oidc_arn" {
+  type     = string
+  nullable = false
+}
