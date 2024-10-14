d8706316a34cbdaad1330521c1713ee647801337
George Taylor <george.taylor@digital.justice.gov.uk>
2024-10-14 12:16:04 +0100

    Update eventbridge.tf

--- terraform/modules/fargate_graceful_retirement/eventbridge.tf
+++ terraform/modules/fargate_graceful_retirement/eventbridge.tf
@@ -2,20 +2,20 @@ resource "aws_cloudwatch_event_rule" "ecs_restart_rule" {
   name        = "ecs_task_retirement_rule"
   description = "Rule to catch AWS ECS Task Patching Retirement events"
   event_pattern = jsonencode({
-    "source": ["aws.health"],
-    "detail-type": ["AWS Health Event"],
-    "detail": {
-      "eventTypeCode": ["AWS_ECS_TASK_PATCHING_RETIREMENT"]
+    "source" : ["aws.health"],
+    "detail-type" : ["AWS Health Event"],
+    "detail" : {
+      "eventTypeCode" : ["AWS_ECS_TASK_PATCHING_RETIREMENT"]
     }
   })
 }
 
 resource "aws_cloudwatch_event_target" "ecs_restarts_target" {
-  rule      = aws_cloudwatch_event_rule.ecs_restart_rule.name
-  arn       = aws_lambda_function.ecs_restart_handler.arn
+  rule = aws_cloudwatch_event_rule.ecs_restart_rule.name
+  arn  = aws_lambda_function.ecs_restart_handler.arn
 }
 
 resource "aws_cloudwatch_event_target" "step_function_target" {
-  rule      = aws_cloudwatch_event_rule.ecs_restart_rule.name
-  arn       = aws_sfn_state_machine.ecs_restart_state_machine.arn
+  rule = aws_cloudwatch_event_rule.ecs_restart_rule.name
+  arn  = aws_sfn_state_machine.ecs_restart_state_machine.arn
 }
