6e5c6102e08883b061ca1dcf354aa5e2a1a30e12
Andrew Moore <andrew.moore@digital.justice.gov.uk>
2024-10-14 13:52:09 +0100

    fix: module refQ

--- terraform/environments/delius-mis/modules/mis_environment/cloudwatch_disable.tf
+++ terraform/environments/delius-mis/modules/mis_environment/cloudwatch_disable.tf
@@ -1,5 +1,5 @@
 module "cloudwatch_alarms_disable" {
-  source = "../modules/disable_alarms_lambda"
+  source = "../../../../modules/disable_alarms_lambda"
 
   lambda_function_name = "${var.account_info.application_name}-${var.env_name}-disable-alarms"
 
