5c756659cb5afb86aeeb7e9eb705427f34c04ed9
gwionap <32736223+gwionap@users.noreply.github.com>
2024-10-15 14:27:47 +0100

    Adding preprod runner and updating prod role

--- terraform/environments/analytical-platform-compute/helm-charts-actions-runners.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-actions-runners.tf
@@ -107,6 +107,29 @@ resource "helm_release" "actions_runner_mojas_create_a_derived_table_dpr" {
         github_repository    = "create-a-derived-table"
         github_token         = data.aws_secretsmanager_secret_version.actions_runners_create_a_derived_table[0].secret_string
         github_runner_labels = "digital-prison-reporting"
+        eks_role_arn         = "arn:aws:iam::004723187462:role/dpr-data-api-cross-account-role"
+      }
+    )
+  ]
+}
+
+resource "helm_release" "actions_runner_mojas_create_a_derived_table_dpr_pp" {
+  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
+
+  /* https://github.com/ministryofjustice/analytical-platform-actions-runner */
+  name       = "actions-runner-mojas-create-a-derived-table-dpr-pp"
+  repository = "oci://ghcr.io/ministryofjustice/analytical-platform-charts"
+  version    = "2.320.0-1"
+  chart      = "actions-runner"
+  namespace  = kubernetes_namespace.actions_runners[0].metadata[0].name
+  values = [
+    templatefile(
+      "${path.module}/src/helm/values/actions-runners/create-a-derived-table/values.yml.tftpl",
+      {
+        github_organisation  = "moj-analytical-services"
+        github_repository    = "create-a-derived-table"
+        github_token         = data.aws_secretsmanager_secret_version.actions_runners_create_a_derived_table[0].secret_string
+        github_runner_labels = "digital-prison-reporting-pp"
         eks_role_arn         = "arn:aws:iam::972272129531:role/dpr-data-api-cross-account-role"
       }
     )
