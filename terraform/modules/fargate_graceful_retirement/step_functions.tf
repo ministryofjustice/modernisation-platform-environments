e66b2f35d4bb757b8d28fa486368b4c5c2a49551
George Taylor <george.taylor@digital.justice.gov.uk>
2024-10-16 22:28:40 +0100

    typo

--- terraform/modules/fargate_graceful_retirement/step_functions.tf
+++ terraform/modules/fargate_graceful_retirement/step_functions.tf
@@ -5,8 +5,8 @@ resource "aws_iam_role" "step_function_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
           Service = "states.amazonaws.com"
         }
@@ -16,8 +16,8 @@ resource "aws_iam_role" "step_function_role" {
 }
 
 resource "aws_iam_role_policy" "step_function_policy" {
-  name   = "step_function_policy"
-  role   = aws_iam_role.step_function_role.id
+  name = "step_function_policy"
+  role = aws_iam_role.step_function_role.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -33,33 +33,33 @@ resource "aws_sfn_state_machine" "ecs_restart_state_machine" {
   name     = "ecs_restart_state_machine"
   role_arn = aws_iam_role.step_function_role.arn
 
- definition = jsonencode({
-    Comment: "State Machine to handle ECS Task Patching Retirement",
-    StartAt: "CalculateWaitTimestamp",
-    States: {
-      CalculateWaitTimestamp: {
-        Type: "Task",
-        Resource: "arn:aws:lambda:${aws_lambda_function.calculate_wait_time.arn}",
-        Parameters: {
-          "time.$": "$.time",            # Pass the event time from the input
-          "restart_time": var.restart_time
+  definition = jsonencode({
+    Comment : "State Machine to handle ECS Task Patching Retirement",
+    StartAt : "CalculateWaitTimestamp",
+    States : {
+      CalculateWaitTimestamp : {
+        Type : "Task",
+        Resource : aws_lambda_function.calculate_wait_time.arn,
+        Parameters : {
+          "time.$" : "$.time", # Pass the event time from the input
+          "restart_time" : var.restart_time
         },
-        ResultPath: "$.waitTimestamp",   # Store the result in $.waitTimestamp
-        Next: "WaitUntilRestartTime"
+        ResultPath : "$.waitTimestamp", # Store the result in $.waitTimestamp
+        Next : "WaitUntilRestartTime"
       },
-      WaitUntilRestartTime: {
-        Type: "Wait",
-        TimestampPath: "$.waitTimestamp.timestamp",  # Use the computed timestamp
-        Next: "InvokeLambdaFunction"
+      WaitUntilRestartTime : {
+        Type : "Wait",
+        TimestampPath : "$.waitTimestamp.timestamp", # Use the computed timestamp
+        Next : "InvokeLambdaFunction"
       },
-      InvokeLambdaFunction: {
-        Type: "Task",
-        Resource: "arn:aws:states:::lambda:invoke",
-        Parameters: {
-          "FunctionName": aws_lambda_function.ecs_restart_handler.arn,
-          "Payload.$": "$"
+      InvokeLambdaFunction : {
+        Type : "Task",
+        Resource : "arn:aws:states:::lambda:invoke",
+        Parameters : {
+          "FunctionName" : aws_lambda_function.ecs_restart_handler.arn,
+          "Payload.$" : "$"
         },
-        End: true
+        End : true
       }
     }
   })
