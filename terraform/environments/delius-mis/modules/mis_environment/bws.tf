fedcc6f9f915dfe45dd34ae975e64133b9c37ec9
Seb Norris <sebastian.j.norris@gmail.com>
2024-10-16 16:03:01 +0100

    var not local

--- terraform/environments/delius-mis/modules/mis_environment/bws.tf
+++ terraform/environments/delius-mis/modules/mis_environment/bws.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "bws" {
 }
 
 resource "aws_security_group_rule" "bws_outbound" {
-  for_each                 = { for port in local.domain_join_ports : port.port => port }
+  for_each                 = { for port in var.domain_join_ports : port.port => port }
   type                     = "egress"
   from_port                = each.value.port
   to_port                  = each.value.port
