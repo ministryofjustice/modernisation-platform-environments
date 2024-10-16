a3baae540a356464016aa27445341cf56428202b
George Taylor <george.taylor@digital.justice.gov.uk>
2024-10-17 00:57:08 +0100

    docs

--- terraform/modules/fargate_graceful_retirement/variables.tf
+++ terraform/modules/fargate_graceful_retirement/variables.tf
@@ -1,10 +1,11 @@
 variable "restart_time" {
-  description = "The time to wait until invoking the Lambda function (in RFC3339 format, e.g., 2023-11-13T14:30:00Z)"
+  description = "The time at which to restart the ECS task"
   type        = string
+  default     = "22:00"
 }
 
 variable "restart_day_of_the_week" {
-  description = "The day of the week to restart the ECS task (e.g., MONDAY)"
+  description = "The day of the week to restart the ECS task"
   type        = string
   default     = "WEDNESDAY"
   validation {
