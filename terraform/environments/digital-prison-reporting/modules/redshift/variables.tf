62b6b3d67d58a455b77f1b55c22191a713d7f3d5
tom-ogle-moj <142220790+tom-ogle-moj@users.noreply.github.com>
2024-10-18 16:53:00 +0100

    DPR2-1136: Add new reconciliation job parameters (#8334)
    * DPR2-1136: Configurenon-domain reconciliation job to fail job if reconciliation fails and to not write cloudwatch metrics.
    
    * DPR2-1136: Comments to ignore checkov checks as requested by Devops
    
    * DPR2-1136: tflint changes as requested by devops
    
    * DPR2-1136: Set type of redshift_ingress_sec_rules to map of map of any
    
    * DPR2-1136: More checkov ignores

--- terraform/environments/digital-prison-reporting/modules/redshift/variables.tf
+++ terraform/environments/digital-prison-reporting/modules/redshift/variables.tf
@@ -1,4 +1,6 @@
-variable "vpc" {}
+variable "vpc" {
+  type = string
+}
 
 variable "project_id" {
   type        = string
@@ -18,6 +20,7 @@ variable "cidr" {
 
 variable "redshift_ingress_sec_rules" {
   description = "A Map of map of security group Rules to associate with"
+  type        = map(map(any))
   default = {
     "redshift-tcp" = {
       "from_port" = 5439,
