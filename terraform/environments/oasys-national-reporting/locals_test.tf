9acee841d76fe156c60f08faf9d170e293c878ae
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-10-22 11:19:47 +0100

    use different user-data script for testing

--- terraform/environments/oasys-national-reporting/locals_test.tf
+++ terraform/environments/oasys-national-reporting/locals_test.tf
@@ -134,7 +134,7 @@ locals {
             "Ec2SecretPolicy",
           ])
           user_data_raw = base64encode(templatefile(
-            "./templates/user-data-onr-bods-pwsh.yaml.tftpl", {
+            "./templates/user-data-onr-bods-pwsh-drive-init.yaml.tftpl", {
               branch = "TM/TM-584/fix-path"
             }
           ))
