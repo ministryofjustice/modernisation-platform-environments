f0549fca6547e1196cc249d8429bd0665fa8e7d3
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2024-10-22 16:17:38 +0100

    put buckets back

--- terraform/environments/analytical-platform-compute/s3-buckets.tf
+++ terraform/environments/analytical-platform-compute/s3-buckets.tf
@@ -22,69 +22,69 @@ module "mlflow_bucket" {
   tags = local.tags
 }
 
-# module "mojap_derived_tables_replication_bucket" {
-#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-#   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+module "mojap_derived_tables_replication_bucket" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-#   source  = "terraform-aws-modules/s3-bucket/aws"
-#   version = "4.2.1"
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.2.1"
 
-#   bucket = "mojap-derived-tables-replication-${local.environment}"
+  bucket = "mojap-derived-tables-replication-${local.environment}"
 
-#   force_destroy = true
+  force_destroy = true
 
-#   object_lock_enabled = false
+  object_lock_enabled = false
 
-#   versioning = {
-#     status = "Disabled"
-#   }
+  versioning = {
+    status = "Disabled"
+  }
 
-#   server_side_encryption_configuration = {
-#     rule = {
-#       bucket_key_enabled = true
-#       apply_server_side_encryption_by_default = {
-#         kms_master_key_id = module.mojap_derived_tables_replication_s3_kms.key_arn
-#         sse_algorithm     = "aws:kms"
-#       }
-#     }
-#   }
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.mojap_derived_tables_replication_s3_kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
-#   logging = {
-#     target_bucket = module.apc_bucket_logs.s3_bucket_id
-#     target_prefix = "mojap-derived-tables-replication/"
-#   }
+  logging = {
+    target_bucket = module.apc_bucket_logs.s3_bucket_id
+    target_prefix = "mojap-derived-tables-replication/"
+  }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# module "apc_bucket_logs" {
-#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-#   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+module "apc_bucket_logs" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-#   source  = "terraform-aws-modules/s3-bucket/aws"
-#   version = "4.2.1"
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.2.1"
 
-#   bucket = "apc-bucket-logs-${local.environment}"
+  bucket = "apc-bucket-logs-${local.environment}"
 
-#   force_destroy = false
+  force_destroy = false
 
-#   object_lock_enabled = false
+  object_lock_enabled = false
 
-#   versioning = {
-#     status = "Disabled"
-#   }
+  versioning = {
+    status = "Disabled"
+  }
 
-#   server_side_encryption_configuration = {
-#     rule = {
-#       bucket_key_enabled = true
-#       apply_server_side_encryption_by_default = {
-#         kms_master_key_id = module.apc_bucket_logs_s3_kms.key_arn
-#         sse_algorithm     = "aws:kms"
-#       }
-#     }
-#   }
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.apc_bucket_logs_s3_kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
-#   tags = local.tags
+  tags = local.tags
 
-#   policy = data.aws_iam_policy_document.s3_server_access_logs_policy.json
-# }
+  policy = data.aws_iam_policy_document.s3_server_access_logs_policy.json
+}
