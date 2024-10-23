dcf096e4158af6b58f889bbdad6261a9cd69ab09
Buckingham <XC01857@lumen.com>
2024-10-23 12:03:38 +0100

    Update_231024_5

--- terraform/environments/ppud/s3.tf
+++ terraform/environments/ppud/s3.tf
@@ -564,19 +564,15 @@ resource "aws_s3_bucket_public_access_block" "moj-log-files-uat" {
   restrict_public_buckets = true
 }
 
-# Disabled S3 bucket notification pending further testing.
-
-/*
 resource "aws_s3_bucket_notification" "moj-log-files-uat" {
   count  = local.is-preproduction == true ? 1 : 0 
   bucket = aws_s3_bucket.moj-log-files-uat[0].id
   topic {
-    topic_arn = aws_sns_topic.cw_uat_alerts[0].arn
+    topic_arn = aws_sns_topic.s3_bucket_notifications_uat[0].arn
     events        = ["s3:ObjectCreated:*"]
     filter_prefix = "alb-logs/"
   }
 }
-*/
 
 resource "aws_s3_bucket_lifecycle_configuration" "moj-log-files-uat" {
   count  = local.is-preproduction == true ? 1 : 0
