85824c83c9d3da0051c4b0df3de909b8e15ec7db
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-10-23 14:46:14 +0000

    Actions Runner software doesn't work with app
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-compute/secrets.tf
+++ terraform/environments/analytical-platform-compute/secrets.tf
@@ -118,7 +118,7 @@ module "ui_azure_tenant_secret" {
   tags = local.tags
 }
 
-module "actions_runners_github_app_secret" {
+module "actions_runners_token_apc_self_hosted_runners_secret" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
@@ -127,14 +127,17 @@ module "actions_runners_github_app_secret" {
   source  = "terraform-aws-modules/secrets-manager/aws"
   version = "1.3.1"
 
-  name        = "actions-runners/github-app"
-  description = "https://github.com/organizations/moj-analytical-services/settings/apps/apc-self-hosted-runners"
+  name        = "actions-runners/token/apc-self-hosted-runners"
+  description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/2208432"
   kms_key_id  = module.common_secrets_manager_kms.key_arn
 
-  secret_string = jsonencode({
-    "change-me" = "CHANGEME"
-  })
+  secret_string         = "CHANGEME"
   ignore_secret_changes = true
 
-  tags = local.tags
+  tags = merge(
+    local.tags,
+    {
+      "expiry-date" = "2025-10-24"
+    }
+  )
 }
