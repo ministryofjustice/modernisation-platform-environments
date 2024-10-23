70620c007ba96fdc81d2020cbf229afc54820a13
Mark Roberts <mark.roberts@digital.justice.gov.uk>
2024-10-23 10:08:50 +0100

    Example with resources hashed out

--- terraform/environments/example/clean.sh
+++ terraform/environments/example/clean.sh
@@ -1,4 +1,8 @@
 rm -Rf .terraform
 rm .terraform.lock.hcl
-terraform init -backend-config=assume_role={role_arn=\"arn:aws:iam::946070829339:role/modernisation-account-terraform-state-member-access\"}
-terraform workspace select example-development
+echo "Account Number: $1"
+terraform init -backend-config=assume_role={role_arn=\"arn:aws:iam::$1:role/modernisation-account-terraform-state-member-access\"}
+terraform workspace list
+echo "Please select your workspace:"
+read workspace 
+terraform workspace select "$workspace"
