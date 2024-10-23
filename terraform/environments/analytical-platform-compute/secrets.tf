288d97334b91fb2afe36c960a60a78b76eed2403
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-10-23 14:54:35 +0000

    Update secret expiration date
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-compute/secrets.tf
+++ terraform/environments/analytical-platform-compute/secrets.tf
@@ -128,7 +128,7 @@ module "actions_runners_token_apc_self_hosted_runners_secret" {
   version = "1.3.1"
 
   name        = "actions-runners/token/apc-self-hosted-runners"
-  description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/2208432"
+  description = "moj-data-platform-robot: https://github.com/settings/personal-access-tokens/4281036"
   kms_key_id  = module.common_secrets_manager_kms.key_arn
 
   secret_string         = "CHANGEME"
@@ -137,7 +137,7 @@ module "actions_runners_token_apc_self_hosted_runners_secret" {
   tags = merge(
     local.tags,
     {
-      "expiry-date" = "2025-10-24"
+      "expiry-date" = "2025-10-23"
     }
   )
 }
