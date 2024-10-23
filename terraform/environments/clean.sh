c2644f9acd433f6259a19f5147a73724d3c61986
Mark Roberts <mark.roberts@digital.justice.gov.uk>
2024-10-23 10:19:39 +0100

    removed clean.sh completely

--- terraform/environments/clean.sh
+++
@@ -1,8 +0,0 @@
-rm -Rf .terraform
-rm .terraform.lock.hcl
-echo "Account Number: $1"
-terraform init -backend-config=assume_role={role_arn=\"arn:aws:iam::$1:role/modernisation-account-terraform-state-member-access\"}
-terraform workspace list
-echo "Please select your workspace:"
-read workspace 
-terraform workspace select "$workspace"
