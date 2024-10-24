620e597c277d16e4dda29a688fafd45430283d38
Madhu Kadiri <madhu.kadiri2@justice.gov.uk>
2024-10-24 16:30:18 +0100

    GluePySpark - Refactoring-v2410-8

--- terraform/environments/electronic-monitoring-data/dms_data_validation_glue_job.tf
+++ terraform/environments/electronic-monitoring-data/dms_data_validation_glue_job.tf
@@ -193,7 +193,7 @@ resource "aws_glue_job" "dms_dv_glue_job_v2" {
     "--parquet_output_bucket_name"        = module.s3-dms-data-validation-bucket.bucket.id
     "--glue_catalog_db_name"              = aws_glue_catalog_database.dms_dv_glue_catalog_db.name
     "--glue_catalog_tbl_name"             = "glue_df_output"
-    "--continuous-log-logGroup"           = "/aws-glue/jobs/${aws_cloudwatch_log_group.dms_dv_cw_log_group.name}"
+    "--continuous-log-logGroup"           = "/aws-glue/jobs/${aws_cloudwatch_log_group.dms_dv_cw_log_group_v2.name}"
     "--enable-continuous-cloudwatch-log"  = "true"
     "--enable-continuous-log-filter"      = "true"
     "--enable-metrics"                    = "true"
