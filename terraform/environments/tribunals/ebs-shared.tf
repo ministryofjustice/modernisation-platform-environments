aed0eeb29459cdb999aedd5f06af6b9cb66cc4e6
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-10-28 12:42:36 +0000

    destry ebs volumes

--- terraform/environments/tribunals/ebs-shared.tf
+++ terraform/environments/tribunals/ebs-shared.tf
@@ -1,28 +1,29 @@
-resource "aws_ebs_volume" "tribunals-ebs" {
-  availability_zone = "eu-west-2a"
-  type              = "gp2"
-  size              = 10
+# resource "aws_ebs_volume" "tribunals-ebs" {
+#   availability_zone = "eu-west-2a"
+#   type              = "gp2"
+#   size              = 10
 
-  tags = merge(
-    local.tags,
-    {
-      Name = "tribunals-all-storage"
-    }
-  )
-}
+#   tags = merge(
+#     local.tags,
+#     {
+#       Name = "tribunals-all-storage"
+#     }
+#   )
+# }
 
-resource "aws_ebs_volume" "tribunals-backup-ebs" {
-  availability_zone = "eu-west-2b"
-  type              = "gp2"
-  size              = 10
+# resource "aws_ebs_volume" "tribunals-backup-ebs" {
+#   availability_zone = "eu-west-2b"
+#   type              = "gp2"
+#   size              = 10
 
-  tags = merge(
-    local.tags,
-    {
-      Name = "tribunals-backup-storage"
-    }
-  )
-}
+#   tags = merge(
+#     local.tags,
+#     {
+#       Name = "tribunals-backup-storage"
+#     }
+#   )
+# }
+
 # resource "aws_volume_attachment" "tribunals-backup-ebs-attachment" {
 #   device_name = "/dev/sdf"
 #   volume_id   = aws_ebs_volume.tribunals-backup-ebs.id
