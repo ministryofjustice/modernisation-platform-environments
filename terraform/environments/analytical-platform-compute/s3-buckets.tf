4c4d42ea970dd9e7b6faac3c20652b72a0e2389a
Ant Fitzroy <101649764+AntFMoJ@users.noreply.github.com>
2024-10-28 17:12:15 +0000

    create logging bucket and kms keys in eu-west-1 (#8477)
    * create logging bucket in eu-west-1
    
    * add eu-west-2 suffix to original logging bucket
    
    * moved logging bucket module
    
    * corrected move blocks
    
    * add s3_server_access_logs_eu_west_1_policy
    
    * add eu-west-1 kms keys
    
    * update kms key aliases
    
    * fix kms module naming
    
    * fix error

--- terraform/environments/analytical-platform-compute/s3-buckets.tf
+++ terraform/environments/analytical-platform-compute/s3-buckets.tf
@@ -22,76 +22,76 @@ module "mlflow_bucket" {
   tags = local.tags
 }
 
-# data "aws_iam_policy_document" "s3_replication_policy" {
-#   #checkov:skip=CKV_AWS_356:resource "*" being applied to replication iam role only
-#   statement {
-#     sid    = "AllowLakeFormationPrincipalsReplication"
-#     effect = "Allow"
-#     actions = [
-#       "s3:ReplicateTags",
-#       "s3:ReplicateDelete",
-#       "s3:ReplicateObject"
-#     ]
-#     principals {
-#       type = "AWS"
-#       identifiers = [
-#         "arn:aws:iam::525294151996:role/service-role/s3replicate_role_for_lf-antfmoj-test",
-#         "arn:aws:iam::525294151996:role/service-role/s3crr_role_for_lf-antfmoj-test_1"
-#       ]
-#     }
-#     resources = ["arn:aws:s3:::mojap-compute-${local.environment}-derived-tables-replication/*"]
-#   }
-# }
+data "aws_iam_policy_document" "s3_replication_policy" {
+  #checkov:skip=CKV_AWS_356:resource "*" being applied to replication iam role only
+  statement {
+    sid    = "AllowLakeFormationPrincipalsReplication"
+    effect = "Allow"
+    actions = [
+      "s3:ReplicateTags",
+      "s3:ReplicateDelete",
+      "s3:ReplicateObject"
+    ]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::525294151996:role/service-role/s3replicate_role_for_lf-antfmoj-test",
+        "arn:aws:iam::525294151996:role/service-role/s3crr_role_for_lf-antfmoj-test_1"
+      ]
+    }
+    resources = ["arn:aws:s3:::mojap-compute-${local.environment}-derived-tables-replication/*"]
+  }
+}
 
-# module "mojap_derived_tables_replication_bucket" {
-#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-#   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+module "mojap_derived_tables_replication_bucket" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-#   source  = "terraform-aws-modules/s3-bucket/aws"
-#   version = "4.2.1"
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.2.1"
 
-#   providers = {
-#     aws = aws.analytical-platform-compute-eu-west-1
-#   }
+  providers = {
+    aws = aws.analytical-platform-compute-eu-west-1
+  }
 
-#   bucket = "mojap-compute-${local.environment}-derived-tables-replication"
+  bucket = "mojap-compute-${local.environment}-derived-tables-replication"
 
-#   force_destroy = true
+  force_destroy = true
 
-#   attach_policy = true
-#   policy        = data.aws_iam_policy_document.s3_replication_policy.json
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.s3_replication_policy.json
 
-#   object_lock_enabled = false
+  object_lock_enabled = false
 
-#   versioning = {
-#     status = "Enabled"
-#   }
+  versioning = {
+    status = "Enabled"
+  }
 
-#   server_side_encryption_configuration = {
-#     rule = {
-#       bucket_key_enabled = true
-#       apply_server_side_encryption_by_default = {
-#         kms_master_key_id = module.mojap_derived_tables_replication_s3_kms.key_arn
-#         sse_algorithm     = "aws:kms"
-#       }
-#     }
-#   }
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.mojap_derived_tables_replication_s3_kms_eu_west_1.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
-#   logging = {
-#     target_bucket = module.mojap_compute_logs_bucket.s3_bucket_id
-#     target_prefix = "mojap-derived-tables-replication/"
-#   }
+  logging = {
+    target_bucket = module.mojap_compute_logs_bucket_eu_west_1.s3_bucket_id
+    target_prefix = "mojap-derived-tables-replication/"
+  }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-data "aws_iam_policy_document" "s3_server_access_logs_policy" {
+data "aws_iam_policy_document" "s3_server_access_logs_eu_west_2_policy" {
   #checkov:skip=CKV_AWS_356:resource "*" limited by condition
   statement {
     sid       = "S3ServerAccessLogsPolicy"
     effect    = "Allow"
     actions   = ["s3:PutObject"]
-    resources = ["arn:aws:s3:::mojap-compute-${local.environment}-logs/*"]
+    resources = ["arn:aws:s3:::mojap-compute-${local.environment}-logs-eu-west-2/*"]
     principals {
       type        = "Service"
       identifiers = ["logging.s3.amazonaws.com"]
@@ -104,19 +104,19 @@ data "aws_iam_policy_document" "s3_server_access_logs_policy" {
   }
 }
 
-module "mojap_compute_logs_bucket" {
+module "mojap_compute_logs_bucket_eu_west_2" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.2.1"
 
-  bucket = "mojap-compute-${local.environment}-logs"
+  bucket = "mojap-compute-${local.environment}-logs-eu-west-2"
 
   force_destroy = false
 
   attach_policy = true
-  policy        = data.aws_iam_policy_document.s3_server_access_logs_policy.json
+  policy        = data.aws_iam_policy_document.s3_server_access_logs_eu_west_2_policy.json
 
   object_lock_enabled = false
 
@@ -128,11 +128,93 @@ module "mojap_compute_logs_bucket" {
     rule = {
       bucket_key_enabled = true
       apply_server_side_encryption_by_default = {
-        kms_master_key_id = module.mojap_compute_logs_s3_kms.key_arn
+        kms_master_key_id = module.mojap_compute_logs_s3_kms_eu_west_2.key_arn
         sse_algorithm     = "aws:kms"
       }
     }
   }
 
   tags = local.tags
+}
+
+data "aws_iam_policy_document" "s3_server_access_logs_eu_west_1_policy" {
+  #checkov:skip=CKV_AWS_356:resource "*" limited by condition
+  statement {
+    sid       = "S3ServerAccessLogsPolicy"
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::mojap-compute-${local.environment}-logs-eu-west-1/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["logging.s3.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+  }
+}
+
+module "mojap_compute_logs_bucket_eu_west_1" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.2.1"
+
+  providers = {
+    aws = aws.analytical-platform-compute-eu-west-1
+  }
+
+  bucket = "mojap-compute-${local.environment}-logs-eu-west-1"
+
+  force_destroy = false
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.s3_server_access_logs_eu_west_1_policy.json
+
+  object_lock_enabled = false
+
+  versioning = {
+    status = "Disabled"
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.mojap_compute_logs_s3_kms_eu_west_1.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = local.tags
+}
+
+moved {
+  from = module.mojap_compute_logs_bucket.aws_s3_bucket.this[0]
+  to   = module.mojap_compute_logs_bucket_eu_west_2.aws_s3_bucket.this[0]
+}
+moved {
+  from = module.mojap_compute_logs_bucket.aws_s3_bucket_policy.this[0]
+  to   = module.mojap_compute_logs_bucket_eu_west_2.aws_s3_bucket_policy.this[0]
+}
+moved {
+  from = module.mojap_compute_logs_bucket.aws_s3_bucket_public_access_block.this[0]
+  to   = module.mojap_compute_logs_bucket_eu_west_2.aws_s3_bucket_public_access_block.this[0]
+}
+moved {
+  from = module.mojap_compute_logs_bucket.aws_s3_bucket_server_side_encryption_configuration.this[0]
+  to   = module.mojap_compute_logs_bucket_eu_west_2.aws_s3_bucket_server_side_encryption_configuration.this[0]
+}
+moved {
+  from = module.mojap_compute_logs_bucket.aws_s3_bucket_versioning.this[0]
+  to   = module.mojap_compute_logs_bucket_eu_west_2.aws_s3_bucket_versioning.this[0]
+}
+
+moved {
+  from = aws_iam_policy_document.s3_server_access_logs_policy
+  to   = aws_iam_policy_document.s3_server_access_logs_eu_west_2_policy
 }
