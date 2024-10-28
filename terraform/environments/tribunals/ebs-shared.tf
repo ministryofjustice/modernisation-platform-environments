fa10913318ff3d16ba82d1adb1dd3b7c51df7199
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-10-28 12:48:25 +0000

    cancel deletion of original ebs volume

--- terraform/environments/tribunals/ebs-shared.tf
+++ terraform/environments/tribunals/ebs-shared.tf
@@ -1,15 +1,15 @@
-# resource "aws_ebs_volume" "tribunals-ebs" {
-#   availability_zone = "eu-west-2a"
-#   type              = "gp2"
-#   size              = 10
+resource "aws_ebs_volume" "tribunals-ebs" {
+  availability_zone = "eu-west-2a"
+  type              = "gp2"
+  size              = 10
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "tribunals-all-storage"
-#     }
-#   )
-# }
+  tags = merge(
+    local.tags,
+    {
+      Name = "tribunals-all-storage"
+    }
+  )
+}
 
 # resource "aws_ebs_volume" "tribunals-backup-ebs" {
 #   availability_zone = "eu-west-2b"
