d48b5aecffc3affa451574509375e24a20175008
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-10-28 10:25:35 +0000

    Add KMS and secret with placeholders
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-ingestion/secrets.tf
+++ terraform/environments/analytical-platform-ingestion/secrets.tf
@@ -19,3 +19,22 @@ resource "aws_secretsmanager_secret" "slack_token" {
   name       = "ingestion/slack-token"
   kms_key_id = module.slack_token_kms.key_arn
 }
+
+module "datasync_dom1_secret" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.3.1"
+
+  name        = "datasync/dom1"
+  kms_key_id  = module.datasync_credentials_kms.key_arn
+
+  ignore_secret_changes = true
+  secret_json           = jsonencode({
+    username = "CHANGEME"
+    password = "CHANGEME"
+  })
+
+  tags = local.tags
+}
