754e7f5e3fa5495a9ca0935822e33a532d0c14d7
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-10-28 10:44:33 +0000

    add depends_on to data call for aws_instances

--- terraform/environments/tribunals/load_balancer.tf
+++ terraform/environments/tribunals/load_balancer.tf
@@ -71,6 +71,10 @@ resource "aws_lb_target_group" "tribunals_target_group" {
 }
 
 data "aws_instances" "tribunals_instance" {
+  depends_on = [
+    aws_instance.tribunals-backup,
+    aws_autoscaling_group.tribunals-all-asg
+  ]
   filter {
     name   = "tag:Name"
     values = ["tribunals-instance", "tribunals-backup-instance"]
