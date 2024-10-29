909a449db05761ec27c3248bb783381ff51e8179
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-10-29 08:40:17 +0000

    Registering CeDeT bucket, defining another service role and adding it to LF admins

---
+++ terraform/environments/analytical-platform-compute/lakeformation-registrations.tf
@@ -0,0 +1,10 @@
+module "replicated_cadet_bucket" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+  source         = "github.com/ministryofjustice/terraform-aws-analytical-platform-lakeformation"
+  version        = "0.5.0"
+  data_locations = [module.mojap_compute_logs_bucket_eu_west_1.s3_bucket_arn]
+  register       = true
+  hybrid_mode    = false # will be managed exclusively in LakeFormation
+
+}
