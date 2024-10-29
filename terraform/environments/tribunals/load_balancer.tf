0c73d9afc725f56500b7a3b16ea2734545b6610c
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-10-29 11:42:54 +0000

    update data block for ec2 deletion

--- terraform/environments/tribunals/load_balancer.tf
+++ terraform/environments/tribunals/load_balancer.tf
@@ -70,14 +70,24 @@ resource "aws_lb_target_group" "tribunals_target_group" {
   }
 }
 
+# data "aws_instances" "tribunals_instance" {
+#   depends_on = [
+#     aws_instance.tribunals-backup,
+#     aws_autoscaling_group.tribunals-all-asg
+#   ]
+#   filter {
+#     name   = "tag:Name"
+#     values = ["tribunals-instance", "tribunals-backup-instance"]
+#   }
+# }
+
 data "aws_instances" "tribunals_instance" {
   depends_on = [
-    aws_instance.tribunals-backup,
     aws_autoscaling_group.tribunals-all-asg
   ]
   filter {
     name   = "tag:Name"
-    values = ["tribunals-instance", "tribunals-backup-instance"]
+    values = ["tribunals-instance"]
   }
 }
 
