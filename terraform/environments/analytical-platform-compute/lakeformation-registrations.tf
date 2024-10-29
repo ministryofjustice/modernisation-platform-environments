15b583d06067fe21ea615ad21bf6df08e009cecd
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-10-29 09:38:24 +0000

    Fixing the bucket ref

--- terraform/environments/analytical-platform-compute/lakeformation-registrations.tf
+++ terraform/environments/analytical-platform-compute/lakeformation-registrations.tf
@@ -3,7 +3,7 @@ module "replicated_cadet_bucket" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
   source = "github.com/ministryofjustice/terraform-aws-analytical-platform-lakeformation?ref=0.5.0"
   data_locations = [{
-    data_location = module.mojap_compute_logs_bucket_eu_west_1.s3_bucket_arn
+    data_location = module.mojap_derived_tables_replication_bucket.s3_bucket_arn
     register      = true
     share         = true
     hybrid_mode   = false # will be managed exclusively in LakeFormation
