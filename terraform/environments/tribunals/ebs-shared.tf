d4f4530a725cf7f802324e398b21dba496714a27
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-10-29 16:58:07 +0000

    rebuild ec2

--- terraform/environments/tribunals/ebs-shared.tf
+++ terraform/environments/tribunals/ebs-shared.tf
@@ -11,15 +11,15 @@ resource "aws_ebs_volume" "tribunals-ebs" {
   )
 }
 
-# resource "aws_ebs_volume" "tribunals-backup-ebs" {
-#   availability_zone = "eu-west-2b"
-#   type              = "gp2"
-#   size              = 10
+resource "aws_ebs_volume" "tribunals-backup-ebs" {
+  availability_zone = "eu-west-2b"
+  type              = "gp2"
+  size              = 10
 
-#   tags = merge(
-#     local.tags,
-#     {
-#       Name = "tribunals-backup-storage"
-#     }
-#   )
-# }
+  tags = merge(
+    local.tags,
+    {
+      Name = "tribunals-backup-storage"
+    }
+  )
+}
