b878e0525de09b3d8b81f5b84438a82b62a9e14c
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-10-31 16:01:20 +0000

    use cidr groups instead of sgs

--- terraform/environments/cdpt-chaps/ecs.tf
+++ terraform/environments/cdpt-chaps/ecs.tf
@@ -556,7 +556,7 @@ resource "aws_security_group" "ecs_service" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_service.id] # Allow traffic between the ECS services
+    cidr_blocks = [data.aws_vpc.shared.cidr_block]
   }
 
   egress {
