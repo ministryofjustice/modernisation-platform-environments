24173ca3825f042b39e4940d0d0fdc9aef584995
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-11-01 21:53:14 +0000

    add aws provider to module

--- terraform/environments/electronic-monitoring-data/modules/export_bucket_presigned_url/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/export_bucket_presigned_url/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+  }
+  required_version = "~> 1.0"
+}
+
 module "this-bucket" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=f759060"
 
