ac734e9a54755fe10c06c029ae5e12d0ae0dc82b
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-01 10:05:31 +0000

    fix outputs refs to task definitions

--- terraform/environments/cdpt-chaps/outputs.tf
+++ terraform/environments/cdpt-chaps/outputs.tf
@@ -1,3 +1,7 @@
-output "task_definition" {
-  value = data.aws_ecs_task_definition.task_definition
+output "chaps_task_definition" {
+  value = data.aws_ecs_task_definition.chaps_task_definition
+}
+
+output "chapsdotnet_task_definition" {
+  value = data.aws_ecs_task_definition.chapsdotnet_task_definition
 }
