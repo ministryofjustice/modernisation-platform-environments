73090d3b13a0d00cbf042120f7d264c7c9322b22
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-11-01 15:19:32 +0000

    re-add api gateway and step function

--- terraform/environments/electronic-monitoring-data/api_gateway.tf
+++ terraform/environments/electronic-monitoring-data/api_gateway.tf
@@ -1,27 +1,27 @@
-# module "get_zipped_file_api" {
-#   source           = "./modules/api_step_function"
-#   api_name         = "get_zipped_file"
-#   api_description  = "API to trigger step function that gets a zipped file out of storage"
-#   api_path         = "execute"
-#   step_function    = module.get_zipped_file
-#   api_key_required = true
-#   stages = [
-#     {
-#       stage_name             = "test",
-#       stage_description      = "API Stage for testing",
-#       burst_limit            = 200,
-#       rate_limit             = 2000,
-#       throttling_burst_limit = 200,
-#       throttling_rate_limit  = 2000
+module "get_zipped_file_api" {
+  source           = "./modules/api_step_function"
+  api_name         = "get_zipped_file"
+  api_description  = "API to trigger step function that gets a zipped file out of storage"
+  api_path         = "execute"
+  step_function    = module.get_zipped_file
+  api_key_required = true
+  stages = [
+    {
+      stage_name             = "test",
+      stage_description      = "API Stage for testing",
+      burst_limit            = 200,
+      rate_limit             = 2000,
+      throttling_burst_limit = 200,
+      throttling_rate_limit  = 2000
 
-#     }
-#   ]
-#   schema = {
-#     type = "object"
-#     properties = {
-#       file_name     = { type = "string" }
-#       zip_file_name = { type = "string" }
-#     }
-#     required = ["file_name", "zip_file_name"]
-#   }
-# }
+    }
+  ]
+  schema = {
+    type = "object"
+    properties = {
+      file_name     = { type = "string" }
+      zip_file_name = { type = "string" }
+    }
+    required = ["file_name", "zip_file_name"]
+  }
+}
