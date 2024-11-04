60042ed33b29c3c8775bbd4e8e318586fab2d018
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-11-04 11:22:41 +0000

    remove all lf setup

--- terraform/environments/electronic-monitoring-data/analytical_platform_share.tf
+++ terraform/environments/electronic-monitoring-data/analytical_platform_share.tf
@@ -328,11 +328,11 @@ resource "aws_iam_role_policy_attachment" "analytical_platform_share_policy_atta
 }
 
 
-resource "aws_lakeformation_data_lake_settings" "lake_formation" {
-  admins = flatten([[for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn], data.aws_iam_session_context.current.issuer_arn, try(one(data.aws_iam_roles.data_engineering_roles.arns), [])])
+# resource "aws_lakeformation_data_lake_settings" "lake_formation" {
+#   admins = flatten([[for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn], data.aws_iam_session_context.current.issuer_arn, try(one(data.aws_iam_roles.data_engineering_roles.arns), [])])
 
-  # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
-}
+#   # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
+# }
 
 
 # resource "aws_lakeformation_permissions" "example" {
