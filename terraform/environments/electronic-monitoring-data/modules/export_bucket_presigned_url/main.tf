2c8262025f41c41fac6f82fd57f8cafb14aaf16f
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-11-04 10:43:49 +0000

    remove count

--- terraform/environments/electronic-monitoring-data/modules/export_bucket_presigned_url/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/export_bucket_presigned_url/main.tf
@@ -61,15 +61,11 @@ module "this-bucket" {
 }
 
 resource "aws_s3_bucket_policy" "this" {
-  count = var.allowed_ips != null ? 1 : 0
-
   bucket = module.this-bucket.bucket.id
-  policy = data.aws_iam_policy_document.this[0].json
+  policy = data.aws_iam_policy_document.this.json
 }
 
 data "aws_iam_policy_document" "this" {
-  count = var.allowed_ips != null ? 1 : 0
-
   statement {
     sid     = "AllowedIPs"
     effect  = "Deny"
