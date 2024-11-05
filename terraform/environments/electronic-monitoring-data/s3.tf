455af1e222c1288c187a4b207a2d675fd8abdfb8
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-11-05 10:01:41 +0000

    update json structure name

--- terraform/environments/electronic-monitoring-data/s3.tf
+++ terraform/environments/electronic-monitoring-data/s3.tf
@@ -379,7 +379,7 @@ module "s3-dms-premigrate-assess-bucket" {
 module "s3-json-directory-structure-bucket" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=f759060"
 
-  bucket_prefix      = "${local.bucket_prefix}-json-directory-structure-"
+  bucket_prefix      = "${local.bucket_prefix}-json-dir-structure-"
   versioning_enabled = true
 
   # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
