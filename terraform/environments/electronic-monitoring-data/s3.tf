c2965a116e374fde6f4bebba4a0275e647b2d794
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-11-05 13:52:02 +0000

    add old json bucket back

--- terraform/environments/electronic-monitoring-data/s3.tf
+++ terraform/environments/electronic-monitoring-data/s3.tf
@@ -450,6 +450,80 @@ module "s3-json-directory-structure-bucket" {
   tags = local.tags
 }
 
+module "s3-json-directory-structure-old-bucket" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=f759060"
+
+  bucket_prefix      = "${local.bucket_prefix}-json-directory-structure-"
+  versioning_enabled = true
+
+  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
+  ownership_controls = "BucketOwnerEnforced"
+  acl                = "private"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
+  }
+
+  log_buckets = tomap({
+    "log_bucket_name" : module.s3-logging-bucket.bucket.id,
+    "log_bucket_arn" : module.s3-logging-bucket.bucket.arn,
+    "log_bucket_policy" : module.s3-logging-bucket.bucket_policy.policy,
+  })
+  log_prefix                = "logs/${local.bucket_prefix}-json-directory-structure/"
+  log_partition_date_source = "EventTime"
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 365
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 730
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 2190
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 365
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 730
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 2190
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
 # ------------------------------------------------------------------------
 # Main store bucket
 # ------------------------------------------------------------------------
