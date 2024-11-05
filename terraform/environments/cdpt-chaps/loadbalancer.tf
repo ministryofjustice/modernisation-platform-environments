dda8f8ee3755a0626a005ca473c8a0742f19ddf7
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-05 09:26:22 +0000

    change TG name prefix

--- terraform/environments/cdpt-chaps/loadbalancer.tf
+++ terraform/environments/cdpt-chaps/loadbalancer.tf
@@ -23,7 +23,7 @@ resource "random_string" "chaps_target_group_name" {
 }
 
   resource "aws_lb_target_group" "chapsdotnet_target_group" {
-    name = "chapsdotnet-target-group"
+    name_prefix = "chapsdotnet-target-group"
     port = 8080
     protocol = "HTTP"
     vpc_id = data.aws_vpc.shared.id 
