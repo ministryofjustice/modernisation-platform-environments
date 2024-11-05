069f3385951aa1587f11d85c08e7c433766f1159
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-11-05 16:59:56 +0000

    ignoring lf

--- terraform/environments/electronic-monitoring-data/analytical_platform_share.tf
+++ terraform/environments/electronic-monitoring-data/analytical_platform_share.tf
@@ -391,16 +391,16 @@ resource "aws_lakeformation_data_lake_settings" "lake_formation" {
   admins = flatten([[for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn], data.aws_iam_session_context.current.issuer_arn, try(one(data.aws_iam_roles.data_engineering_roles.arns), [])])
 
   # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
+  # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
+  create_database_default_permissions {
+    # These settings should replicate current behaviour: LakeFormation is Ignored
+    permissions = ["ALL"]
+    principal   = "IAM_ALLOWED_PRINCIPALS"
+  }
+
+  create_table_default_permissions {
+    # These settings should replicate current behaviour: LakeFormation is Ignored
+    permissions = ["ALL"]
+    principal   = "IAM_ALLOWED_PRINCIPALS"
+  }
 }
-
-
-# resource "aws_lakeformation_permissions" "example" {
-#   permissions = ["SELECT"]
-#   principal   = "arn:aws:iam::684969100054:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_AdministratorAccess_ab408ccf26c25b37"
-
-#   table_with_columns {
-#     database_name = "test_database"
-#     name          = "dummy_table_1"
-#     column_names  = ["id", "name"]
-#   }
-# }
