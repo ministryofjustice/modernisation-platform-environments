d0c739dc42a309008f2bd289c3976ef5a6bc9892
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-06 16:25:23 +0000

    adjust outputs

--- terraform/environments/cdpt-chaps/outputs.tf
+++ terraform/environments/cdpt-chaps/outputs.tf
@@ -3,7 +3,7 @@ output "chaps_task_definition" {
 }
 
 output "chapsdotnet_task_definition" {
-  value = aws_ecs_task_definition.chapsdotnet_task.arn
+  value = aws_ecs_task_definition.chapsdotnet_task[0].arn
 }
 
 output "debug_client_id" {
