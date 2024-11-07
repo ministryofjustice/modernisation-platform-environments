65937404629e2a8bc1344bd6ef491b89ce85a8ef
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-11-07 14:04:58 +0000

    create role for loading cap dw data (#8595)

--- terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
+++ terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
@@ -8,7 +8,7 @@ data "aws_iam_policy_document" "test_ap_airflow" {
 }
 
 module "test_ap_airflow" {
-  source              = "./modules/ap_airflow_iam_role"
+  source = "./modules/ap_airflow_iam_role"
 
   environment         = local.environment
   role_name_suffix    = "test-cross-account-access"
@@ -19,14 +19,27 @@ module "test_ap_airflow" {
 }
 
 module "load_unstructured_atrium_database" {
-  count              = local.is-production ? 1 : 0
-  source             = "./modules/ap_airflow_load_data_iam_role"
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
 
   name               = "unstructured-atrium-database"
   environment        = local.environment
   database_name      = "g4s-atrium-unstructured"
   path_to_data       = "/g4s/atrium_unstructured"
   source_data_bucket = module.s3-json-directory-structure-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+}
+
+module "load_cap_dw_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "load-cap-dw"
+  environment        = local.environment
+  database_name      = "g4s-cap-dw"
+  path_to_data       = "/g4s_cap_dw"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
   secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
   oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
 }
