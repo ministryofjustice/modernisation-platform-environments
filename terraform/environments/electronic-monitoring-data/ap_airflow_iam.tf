a2b68fb9ff752accdd980b72c176c56f7f46f285
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-11-07 14:25:21 +0000

    adding am role (#8599)

--- terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
+++ terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
@@ -43,3 +43,16 @@ module "load_cap_dw_database" {
   secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
   oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
 }
+
+module "load_alcohol_monitoring_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "alcohol-monitoring"
+  environment        = local.environment
+  database_name      = "capita-alcohol-monitoring"
+  path_to_data       = "/capita_alcohol_monitoring"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+}
