b264c2996f89de227a6f0399098a658644c8ac63
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-08 13:05:53 +0000

    remove unused variables

--- terraform/environments/cdpt-chaps/variables.tf
+++ terraform/environments/cdpt-chaps/variables.tf
@@ -3,18 +3,3 @@ variable "domain_name" {
   type        = string
   default     = "modernisation-platform.service.justice.gov.uk"
 }
-
-variable "zone_id" {
-  description = "The ID of the Route 53 Hosted Zone"
-  type        = string
-}
-
-variable "subdomain_name" {
-  description = "The subdomain to assign to the chaps EC2 instance (e.g., cdpt-chaps.hq-development)"
-  type        = string
-}
-
-variable "environment" {
-  description = "deployment environment"
-  type        = string
-}
