5171d8e4df6df714a2bee79a9331837d16e77df3
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-08 13:44:05 +0000

    fix typo

--- terraform/environments/cdpt-chaps/variables.tf
+++ terraform/environments/cdpt-chaps/variables.tf
@@ -1,5 +1,5 @@
 variable "domain_name" {
   description = "The domain name of the hosted zone"
   type        = string
-  default     = "modernisation-platform.service.justice.gov.uk"
+  default     = "modernisation-platform.service.justice.gov.uk."
 }
