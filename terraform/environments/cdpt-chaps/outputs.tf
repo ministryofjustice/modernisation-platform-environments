edd80e68b969092e6256d87c5d24d04d9c3e793c
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-11 13:50:20 +0000

    output ips

--- terraform/environments/cdpt-chaps/outputs.tf
+++ terraform/environments/cdpt-chaps/outputs.tf
@@ -15,3 +15,7 @@ output "chaps_instances_details" {
   description = "Details of the fetched chaps instances"
   value = data.aws_instances.chaps_instances
 }
+
+output "chaps_instances_ips" {
+  value = data.aws_instances.chaps_instances[*].private_ips
+}
