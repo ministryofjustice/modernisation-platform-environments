f9369aff4b6888ecbb9183f0edec242b3bc2de68
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-11-11 18:17:35 +0000

    Reset metric if the task starts

--- terraform/environments/delius-core/modules/components/dms/lambda/dms_replication_metric.py
+++ terraform/environments/delius-core/modules/components/dms/lambda/dms_replication_metric.py
@@ -1,17 +1,38 @@
 import boto3
+import json
 
 def lambda_handler(event, context):
+
     cloudwatch = boto3.client('cloudwatch')
-    cloudwatch.put_metric_data(
-        Namespace='CustomDMSMetrics',
-        MetricData=[
-            {
-                'MetricName': 'DMSReplicationEvent',
-                'Dimensions': [
-                    {'Name': 'Service', 'Value': 'DMS'}
-                ],
-                'Value': 1,  # Trigger threshold
-                'Unit': 'Count'
-            }
-        ]
-    )
+    for record in event['Records']:
+
+        message = json.loads(record['Sns']['Message'])
+
+        if message.get("EventType") == "replication-task-state-change" and message.get("status") == "STARTED":
+            cloudwatch.put_metric_data(
+                Namespace='CustomDMSMetrics',
+                MetricData=[
+                    {
+                        'MetricName': 'DMSReplicationFailure',
+                        'Dimensions': [
+                            {'Name': 'Service', 'Value': 'DMS'}
+                        ],
+                        'Value': 0,  # Reset Below Trigger threshold (Task Started)
+                        'Unit': 'Count'
+                    }
+                ]
+            )
+        elif message.get("EventType") == "failure":
+            cloudwatch.put_metric_data(
+                Namespace='CustomDMSMetrics',
+                MetricData=[
+                    {
+                        'MetricName': 'DMSReplicationFailure',
+                        'Dimensions': [
+                            {'Name': 'Service', 'Value': 'DMS'}
+                        ],
+                        'Value': 1,  # Trigger threshold (Task Failed)
+                        'Unit': 'Count'
+                    }
+                ]
+            )
