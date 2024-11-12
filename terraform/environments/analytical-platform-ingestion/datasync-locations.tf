2215ddc0d7760d05eeab44a40175ee2221017131
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-11-12 22:00:36 +0000

    Add DataSync locations
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ terraform/environments/analytical-platform-ingestion/datasync-locations.tf
@@ -0,0 +1,23 @@
+resource "aws_datasync_location_smb" "dom1_hq_pgo_shared_group_sis_case_management_investigations" {
+  server_hostname = "dom1.infra.int"
+  subdirectory    = "/data/hq/PGO/Shared/Group/SIS Case Management/Investigations"
+
+  user     = jsondecode(data.aws_secretsmanager_secret_version.datasync_dom1.secret_string)["username"]
+  password = jsondecode(data.aws_secretsmanager_secret_version.datasync_dom1.secret_string)["password"]
+
+  agent_arns = [aws_datasync_agent.main.arn]
+
+  tags = local.tags
+}
+
+resource "aws_datasync_location_smb" "dom1_hq_pgo_shared_group_sis_case_management_itas" {
+  server_hostname = "dom1.infra.int"
+  subdirectory    = "/data/hq/PGO/Shared/Group/SIS Case Management/ITAS"
+
+  user     = jsondecode(data.aws_secretsmanager_secret_version.datasync_dom1.secret_string)["username"]
+  password = jsondecode(data.aws_secretsmanager_secret_version.datasync_dom1.secret_string)["password"]
+
+  agent_arns = [aws_datasync_agent.main.arn]
+
+  tags = local.tags
+}
