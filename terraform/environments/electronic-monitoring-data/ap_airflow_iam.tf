6da42a480771d2e154a33db2460b48731d5f6716
Luke Williams <108728588+luke-a-williams@users.noreply.github.com>
2024-11-13 15:05:27 +0000

    Added the rest of the structured dataset modules

--- terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
+++ terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
@@ -33,6 +33,51 @@ module "load_alcohol_monitoring_database" {
   cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
 }
 
+module "load_orca_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "orca"
+  environment        = local.environment
+  database_name      = "capita-orca"
+  path_to_data       = "/capita_orca"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+}
+
+module "load_atrium_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "atrium"
+  environment        = local.environment
+  database_name      = "g4s-atrium"
+  path_to_data       = "/g4s_atrium"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+}
+
+module "load_atv_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "atv"
+  environment        = local.environment
+  database_name      = "g4s-atv"
+  path_to_data       = "/g4s_atv"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+}
+
 module "load_cap_dw_database" {
   count  = local.is-production ? 1 : 0
   source = "./modules/ap_airflow_load_data_iam_role"
@@ -71,6 +116,66 @@ module "load_fep_database" {
   environment        = local.environment
   database_name      = "g4s-fep"
   path_to_data       = "/g4s_fep"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+}
+
+module "load_rf_hours_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "rf-hours"
+  environment        = local.environment
+  database_name      = "g4s-rf-hours"
+  path_to_data       = "/g4s_rf_hours"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+}
+
+module "load_subject_history_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "subject-history"
+  environment        = local.environment
+  database_name      = "g4s-subject-history"
+  path_to_data       = "/g4s_subject_history"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+}
+
+module "load_tasking_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "tasking"
+  environment        = local.environment
+  database_name      = "g4s-tasking"
+  path_to_data       = "/g4s_tasking"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+}
+
+module "load_telephony_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "telephony"
+  environment        = local.environment
+  database_name      = "g4s-telephony"
+  path_to_data       = "/g4s_telephony"
   source_data_bucket = module.s3-dms-target-store-bucket.bucket
   secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
   oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
