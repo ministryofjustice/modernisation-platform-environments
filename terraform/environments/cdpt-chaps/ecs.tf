83fe2fd6bbb10e6816fdf4deb395b6cd50ea8a20
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-13 09:56:49 +0000

    revert sg config

--- terraform/environments/cdpt-chaps/ecs.tf
+++ terraform/environments/cdpt-chaps/ecs.tf
@@ -626,7 +626,7 @@ resource "aws_security_group" "chapsdotnet_service" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    security_groups = [aws_security_group.ecs_service.id]
+    cidr_blocks = [data.aws_vpc.shared.cidr_block]
   }
 
   egress {
