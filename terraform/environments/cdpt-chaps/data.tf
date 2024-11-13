819e1282529739a906902d71b84ca8a06a31a692
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-13 17:12:03 +0000

    undelete application_zone

--- terraform/environments/cdpt-chaps/data.tf
+++ terraform/environments/cdpt-chaps/data.tf
@@ -1,4 +1,10 @@
 #### This file can be used to store data specific to the member account ####
+data "aws_route53_zone" "application_zone" {
+  provider     = aws.core-network-services
+  name         = "correspondence-handling-and-processing.service.justice.gov.uk."
+  private_zone = false
+}
+
 data "aws_instances" "chaps_instances" {
   filter {
     name   = "tag:Name"
