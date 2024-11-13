262dea91b9a5d534eae63f6c09acb0eae2d52162
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-11-13 18:14:31 +0000

    Add task
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ terraform/environments/analytical-platform-ingestion/datasync-tasks.tf
@@ -0,0 +1,19 @@
+resource "aws_datasync_task" "dom1_hq_pgo_shared_group_sis_case_management_investigations" {
+  name                     = "dom1-hq-pgo-shared-group-sis-case-management-investigations"
+  source_location_arn      = aws_datasync_location_smb.dom1_hq_pgo_shared_group_sis_case_management_investigations.arn
+  destination_location_arn = aws_datasync_location_s3.dom1_hq_pgo_shared_group_sis_case_management_investigations.arn
+
+  task_report_config {
+    report_level         = "SUCCESSES_AND_ERRORS"
+    output_type          = "STANDARD"
+    s3_object_versioning = "INCLUDE"
+
+    s3_destination {
+      bucket_access_role_arn = module.datasync_iam_role.iam_role_arn
+      s3_bucket_arn          = module.datasync_bucket.s3_bucket_arn
+      subdirectory           = "reports/dom1/hq/pgo/shared/group/sis-case-management/investigations/"
+    }
+  }
+
+  tags = local.tags
+}
