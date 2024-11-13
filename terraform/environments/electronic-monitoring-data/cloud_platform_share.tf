8af8469068d35d079896f0e86a57b6f336db9351
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-11-13 09:43:13 +0000

    reference new ap-lakeformation build

--- terraform/environments/electronic-monitoring-data/cloud_platform_share.tf
+++ terraform/environments/electronic-monitoring-data/cloud_platform_share.tf
@@ -15,17 +15,20 @@ module "front_end_assumable_role" {
 module "share_api_data_marts" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
-  source = "github.com/ministryofjustice/terraform-aws-analytical-platform-lakeformation?ref=0.5.0"
+  source = "github.com/ministryofjustice/terraform-aws-analytical-platform-lakeformation?ref=de5032cf43ad4a45049650342d39a1a85171c776"
   data_locations = [{
     data_location = module.s3-create-a-derived-table-bucket.bucket.arn
     register      = true
     share         = true
     hybrid_mode   = false # will be managed exclusively in LakeFormation
+    principal     = module.front_end_assumable_role.role_arn
   }]
+
   databases_to_share = [{
-    name = "api_data_marts"
+    name      = "api_data_marts"
+    principal = module.front_end_assumable_role.role_arn
   }]
-  principal_arn = module.front_end_assumable_role.role_arn
+
   providers = {
     aws.source      = aws
     aws.destination = aws
