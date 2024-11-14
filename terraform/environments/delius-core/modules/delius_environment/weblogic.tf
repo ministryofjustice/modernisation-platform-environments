810d2cd297aa95217d60727923cff1b6356df4fb
George Taylor <george.taylor@digital.justice.gov.uk>
2024-11-14 15:51:18 +0000

    Revert "task defs"
    This reverts commit 6b379c0a779ad6af20e539a9ead0236643f0b3f5.

--- terraform/environments/delius-core/modules/delius_environment/weblogic.tf
+++ terraform/environments/delius-core/modules/delius_environment/weblogic.tf
@@ -30,8 +30,8 @@ module "weblogic" {
 
   cluster_security_group_id = aws_security_group.cluster.id
 
-  ignore_changes_service_task_definition = true
-  force_new_deployment                   = false
+  ignore_changes_service_task_definition = false
+  force_new_deployment                   = true
 
   providers = {
     aws.core-vpc              = aws.core-vpc
