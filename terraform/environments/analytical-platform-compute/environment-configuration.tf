38ae704acb7afa9f33319ffb27e1a8fcc4f8c4b7
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-11-14 20:30:39 +0000

    Update VPC CNI version

--- terraform/environments/analytical-platform-compute/environment-configuration.tf
+++ terraform/environments/analytical-platform-compute/environment-configuration.tf
@@ -35,7 +35,7 @@ locals {
         aws_efs_csi_driver     = "v2.0.9-eksbuild.1"
         aws_guardduty_agent    = "v1.7.1-eksbuild.2"
         eks_pod_identity_agent = "v1.3.2-eksbuild.2"
-        vpc_cni                = "v1.18.6-eksbuild.1"
+        vpc_cni                = "v1.19.0-eksbuild.1"
       }
 
       /* Data Engineering Airflow */
