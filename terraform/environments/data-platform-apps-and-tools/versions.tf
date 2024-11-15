97bf512aff3cc9aa7d703ad265ec8c6092eb382e
David Sibley <david.sibley@digital.justice.gov.uk>
2024-11-15 12:18:52 +0000

    renamed all platform_verisons.tf to versions.tf to allow customer management

---
+++ terraform/environments/data-platform-apps-and-tools/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+    http = {
+      version = "~> 3.0"
+      source  = "hashicorp/http"
+    }
+    kubernetes = {
+      version = "~> 2.0"
+      source  = "hashicorp/kubernetes"
+    }
+    helm = {
+      version = "~> 2.0"
+      source  = "hashicorp/helm"
+    }
+  }
+  required_version = "~> 1.0"
+}
---
+++ terraform/environments/sprinkler/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+    http = {
+      version = "~> 3.0"
+      source  = "hashicorp/http"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.2"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2"
+    }
+  }
+  required_version = "~> 1.0"
+}
