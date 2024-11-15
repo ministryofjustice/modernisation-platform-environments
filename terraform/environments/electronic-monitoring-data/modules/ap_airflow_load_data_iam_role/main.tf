c35ef2cc91fccc4e70bae2c96cd8337905cd8967
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-11-15 15:31:26 +0000

    get partitions

--- terraform/environments/electronic-monitoring-data/modules/ap_airflow_load_data_iam_role/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/ap_airflow_load_data_iam_role/main.tf
@@ -60,7 +60,9 @@ data "aws_iam_policy_document" "load_data" {
       "glue:DeleteTable",
       "glue:CreateDatabase",
       "glue:DeleteDatabase",
-      "glue:UpdateTable"
+      "glue:UpdateTable",
+      "glue:GetPartition",
+      "glue:GetPartitions"
     ]
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog",
