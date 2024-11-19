5843a67c20ec2b25ac8053f215b04873895311ba
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-11-19 10:45:56 +0000

    filter test

--- terraform/environments/analytical-platform-compute/lakeformation-data-lake-settings.tf
+++ terraform/environments/analytical-platform-compute/lakeformation-data-lake-settings.tf
@@ -1,22 +1,33 @@
 resource "aws_lakeformation_data_lake_settings" "london" {
-  admins = [
-    data.aws_iam_session_context.current.issuer_arn,
-    module.lake_formation_share_role.iam_role_arn,
-    module.analytical_platform_ui_service_role.iam_role_arn,
-    module.analytical_platform_data_eng_dba_service_role.iam_role_arn,
-    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.data_engineering_sso_role.names)}",
-    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.eks_sso_access_role.names)}"
-  ]
+  admins = concat(
+    [
+      data.aws_iam_session_context.current.issuer_arn,
+      module.lake_formation_share_role.iam_role_arn,
+      module.analytical_platform_ui_service_role.iam_role_arn,
+      module.analytical_platform_data_eng_dba_service_role.iam_role_arn,
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.data_engineering_sso_role.names)}",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.eks_sso_access_role.names)}"
+    ],
+    terraform.workspace == "analytical-platform-compute-development" ? [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.modernisation_platform_sandbox_role.names)}"
+    ] : []
+  )
 }
 
 resource "aws_lakeformation_data_lake_settings" "ireland" {
   provider = aws.analytical-platform-compute-eu-west-1
-  admins = [
-    data.aws_iam_session_context.current.issuer_arn,
-    module.lake_formation_share_role.iam_role_arn,
-    module.analytical_platform_ui_service_role.iam_role_arn,
-    module.analytical_platform_data_eng_dba_service_role.iam_role_arn,
-    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.data_engineering_sso_role.names)}",
-    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.eks_sso_access_role.names)}"
-  ]
+
+  admins = concat(
+    [
+      data.aws_iam_session_context.current.issuer_arn,
+      module.lake_formation_share_role.iam_role_arn,
+      module.analytical_platform_ui_service_role.iam_role_arn,
+      module.analytical_platform_data_eng_dba_service_role.iam_role_arn,
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.data_engineering_sso_role.names)}",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.eks_sso_access_role.names)}"
+    ],
+    terraform.workspace == "analytical-platform-compute-development" ? [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.modernisation_platform_sandbox_role.names)}"
+    ] : []
+  )
 }
