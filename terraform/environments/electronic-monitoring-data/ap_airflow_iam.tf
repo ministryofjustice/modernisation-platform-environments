48cc199b80c0f5a4aeedc6d70ecc1ea4c2106adb
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-11-20 15:27:42 +0000

    add iam role for fms database

--- terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
+++ terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
@@ -197,3 +197,19 @@ module "load_unstructured_atrium_database" {
   athena_dump_bucket = module.s3-athena-bucket.bucket
   cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
 }
+
+
+module "load_fms_database" {
+  count  = local.is-production ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  name               = "serco-fms-database"
+  environment        = local.environment
+  database_name      = "serco-fms"
+  path_to_data       = "/serco/fms"
+  source_data_bucket = module.s3-data-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+}
