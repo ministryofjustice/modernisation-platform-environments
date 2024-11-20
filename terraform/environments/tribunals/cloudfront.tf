b07c881aabf504d426b6609798ec4f3ef5e269a9
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-11-20 16:35:43 +0000

    Tribunals: fix aliases in cloudfront distribution

--- terraform/environments/tribunals/cloudfront.tf
+++ terraform/environments/tribunals/cloudfront.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_distribution" "tribunals_distribution" {
 
-  aliases = ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  aliases = local.is-production ? ["*.decisions.tribunals.gov.uk"] : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   origin {
     domain_name = aws_lb.tribunals_lb.dns_name
     origin_id   = "tribunalsOrigin"
