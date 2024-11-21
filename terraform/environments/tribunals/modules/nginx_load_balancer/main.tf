fba6b5a87f5204a6d289c01cc09dc1da0fa9c333
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-11-21 12:40:00 +0000

    try commenting out nginx lb listeners

--- terraform/environments/tribunals/modules/nginx_load_balancer/main.tf
+++ terraform/environments/tribunals/modules/nginx_load_balancer/main.tf
@@ -52,26 +52,26 @@ resource "aws_lb_target_group_attachment" "nginx_lb_tg_attachment" {
   port             = 80
 }
 
-resource "aws_lb_listener" "nginx_lb_listener" {
-  load_balancer_arn = aws_lb.nginx_lb.arn
-  port              = "8080"
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "nginx_lb_listener" {
+#   load_balancer_arn = aws_lb.nginx_lb.arn
+#   port              = "8080"
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.nginx_lb_tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.nginx_lb_tg.arn
+#   }
+# }
 
-resource "aws_lb_listener" "nginx_lb_listener_https" {
-  certificate_arn   = var.external_acm_cert_arn
-  load_balancer_arn = aws_lb.nginx_lb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+# resource "aws_lb_listener" "nginx_lb_listener_https" {
+#   certificate_arn   = var.external_acm_cert_arn
+#   load_balancer_arn = aws_lb.nginx_lb.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.nginx_lb_tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.nginx_lb_tg.arn
+#   }
+# }
