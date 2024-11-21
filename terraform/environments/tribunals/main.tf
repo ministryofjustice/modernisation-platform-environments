ed54efc416ab990f15fb64b570b634cb6474c784
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-11-21 12:31:41 +0000

    try setting http to listen on port 8080

--- terraform/environments/tribunals/main.tf
+++ terraform/environments/tribunals/main.tf
@@ -701,8 +701,8 @@ resource "aws_security_group" "nginx_lb_sg" {
   vpc_id      = data.aws_vpc.shared.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
--- terraform/environments/tribunals/modules/nginx_load_balancer/main.tf
+++ terraform/environments/tribunals/modules/nginx_load_balancer/main.tf
@@ -8,7 +8,7 @@ resource "aws_lb" "nginx_lb" {
 
 resource "aws_lb_target_group" "nginx_lb_tg" {
   name     = "tribunals-nginx"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_shared_id
   health_check {
@@ -54,7 +54,7 @@ resource "aws_lb_target_group_attachment" "nginx_lb_tg_attachment" {
 
 resource "aws_lb_listener" "nginx_lb_listener" {
   load_balancer_arn = aws_lb.nginx_lb.arn
-  port              = "80"
+  port              = "8080"
   protocol          = "HTTP"
 
   default_action {
