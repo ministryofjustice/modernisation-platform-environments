0dab71db987a338de1880fbe384d274c4ed2317f
khatraf <khatra.farah@digital.justice.gov.uk>
2024-11-25 11:23:14 +0000

    more buckets

--- terraform/environments/sprinkler/S3_guardduty.tf
+++ terraform/environments/sprinkler/S3_guardduty.tf
@@ -6,7 +6,7 @@ variable "buckets_to_protect" {
     To enable for all buckets, list all bucket names here.
   EOF
   type        = string
-  default     = "tests3malwarescanning,tests3malwarekf"
+  default     = "tests3scanningkf,macie-test-results-22032024,mytestbucket976858"
 }
 
 # Define the list of buckets to protect
