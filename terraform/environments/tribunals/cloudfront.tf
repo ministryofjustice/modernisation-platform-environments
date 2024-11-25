4d9fff8a2ceac5c6063231781dd18235cb978999
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2024-11-25 12:19:09 +0000

    undo linking nginx to cloudfront (got too complex)

--- terraform/environments/tribunals/cloudfront.tf
+++ terraform/environments/tribunals/cloudfront.tf
@@ -4,32 +4,10 @@ resource "aws_cloudfront_distribution" "tribunals_distribution" {
     "*.decisions.tribunals.gov.uk",
     "*.venues.tribunals.gov.uk",
     "*.reports.tribunals.gov.uk"
-  ] : [
-    "*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk",
-    "charity.tribunals.gov.uk",
-  ]
+  ] : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   origin {
     domain_name = aws_lb.tribunals_lb.dns_name
     origin_id   = "tribunalsOrigin"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-      origin_keepalive_timeout = 60
-      origin_read_timeout     = 60
-    }
-
-    custom_header {
-      name  = "X-Custom-Header"
-      value = "tribunals-origin"
-    }
-  }
-
-  origin {
-    domain_name = module.nginx_load_balancer[0].nginx_lb_dns_name
-    origin_id   = "nginxOrigin"
 
     custom_origin_config {
       http_port              = 80
@@ -62,24 +40,6 @@ resource "aws_cloudfront_distribution" "tribunals_distribution" {
     smooth_streaming       = false
   }
 
-  ordered_cache_behavior {
-    path_pattern     = "/*"
-    target_origin_id = "nginxOrigin"
-    
-    cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
-    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer.id
-
-    viewer_protocol_policy = "redirect-to-https"
-    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
-    cached_methods         = ["GET", "HEAD"]
-
-    # Only apply this behavior when the host header matches specific patterns
-    function_association {
-      event_type   = "viewer-request"
-      function_arn = aws_cloudfront_function.router.arn
-    }
-  }
-
   enabled         = true
   is_ipv6_enabled = true
   comment         = "CloudFront distribution for tribunals load balancer"
@@ -98,39 +58,6 @@ resource "aws_cloudfront_distribution" "tribunals_distribution" {
   }
 }
 
-# CloudFront function to route based on hostname
-resource "aws_cloudfront_function" "router" {
-  name    = "router"
-  runtime = "cloudfront-js-1.0"
-  code    = <<-EOT
-    function handler(event) {
-      var request = event.request;
-      var headers = request.headers;
-      var host = headers.host.value;
-      
-      // List of domains that should go to nginxOrigin
-      var nginxDomains = [
-        'charity.hmcts-development.modernisation-platform.service.justice.gov.uk'
-      ];
-      
-      // Check if the host matches any nginx domains
-      var useNginx = nginxDomains.some(function(domain) {
-        return host.endsWith(domain);
-      });
-      
-      // If not a nginx domain, skip this cache behavior
-      if (!useNginx) {
-        return {
-          statusCode: 403,
-          statusDescription: 'Forbidden'
-        };
-      }
-      
-      return request;
-    }
-  EOT
-}
-
 data "aws_cloudfront_cache_policy" "caching_disabled" {
   name = "Managed-CachingDisabled"
 }
@@ -142,15 +69,13 @@ data "aws_cloudfront_origin_request_policy" "all_viewer" {
 // Create a new certificate for the CloudFront distribution because it needs to be in us-east-1
 resource "aws_acm_certificate" "cloudfront" {
   provider                  = aws.us-east-1
-  domain_name               = local.is-production ? "*.decisions.tribunals.gov.uk" : "*.tribunals.gov.uk"
+  domain_name               = local.is-production ? "*.decisions.tribunals.gov.uk" : "modernisation-platform.service.justice.gov.uk"
   validation_method         = "DNS"
   subject_alternative_names = local.is-production ? [
-    "*.venues.tribunals.gov.uk",
-    "*.reports.tribunals.gov.uk"
-  ] : [
-    "*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk",
-    "charity.tribunals.gov.uk"
-  ]
+    "*.venues.tribunals.gov.uk", "*.reports.tribunals.gov.uk"
+    ] : [
+      "*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+    ]
   tags = {
     Environment = local.environment
   }
@@ -162,27 +87,6 @@ resource "aws_acm_certificate" "cloudfront" {
 resource "aws_acm_certificate_validation" "cloudfront_cert_validation" {
   provider        = aws.us-east-1
   certificate_arn = aws_acm_certificate.cloudfront.arn
-  validation_record_fqdns = [for record in aws_route53_record.cloudfront_cert_validation : record.fqdn]
-}
-
-// Route53 DNS records for certificate validation
-resource "aws_route53_record" "cloudfront_cert_validation" {
-  provider = aws.core-network-services
-
-  for_each = {
-    for dvo in aws_acm_certificate.cloudfront.domain_validation_options : dvo.domain_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.value]
-  ttl             = 300
-  type            = each.value.type
-  zone_id         = local.is-production ? data.aws_route53_zone.production_zone.zone_id : data.aws_route53_zone.network-services.zone_id
 }
 
 data "aws_ec2_managed_prefix_list" "cloudfront" {
