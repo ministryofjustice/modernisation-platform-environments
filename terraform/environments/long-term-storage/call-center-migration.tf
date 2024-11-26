3d504a6ec4a6c13cab74064e8a33072be676b81a
David Sibley <david.sibley@digital.justice.gov.uk>
2024-11-26 16:57:14 +0000

    first pass at creating a bucket with an aws_transfer frontend

--- terraform/environments/long-term-storage/call-center-migration.tf
+++ terraform/environments/long-term-storage/call-center-migration.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "call_center" {
 }
 
 resource "aws_transfer_server" "call_center" {
-  logging_role                = aws_iam_role.call_center_transfer_logging_role.arn
+  logging_role                = aws_iam_role.call_center_transfer_logging.arn
   structured_log_destinations = ["${aws_cloudwatch_log_group.call_center.arn}:*"]
   tags = merge(
     local.tags,
@@ -18,13 +18,36 @@ resource "aws_cloudwatch_log_group" "call_center" {
   tags              = local.tags
 }
 
-resource "aws_iam_role" "call_center_transfer_logging_role" {
-  name_prefix        = "call-center-migration"
-  assume_role_policy = data.aws_iam_policy_document.call_center_transfer_assume_role_policy.json
+resource "aws_iam_role" "call_center_transfer_logging" {
+  name_prefix        = "call-center-migration-logging"
+  assume_role_policy = data.aws_iam_policy_document.aws_transfer_assume_role_policy.json
   tags               = local.tags
 }
 
 resource "aws_iam_role_policy_attachments_exclusive" "call_center_transfer_logging_policy" {
   policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSTransferLoggingAccess"]
-  role_name   = aws_iam_role.call_center_transfer_logging_role.name
+  role_name   = aws_iam_role.call_center_transfer_logging.name
+}
+
+resource "aws_transfer_user" "call_center" {
+  role      = aws_iam_role.call_center_transfer_user.arn
+  server_id = aws_transfer_server.call_center.id
+  user_name = "call-center-migration"
+  tags      = local.tags
+}
+
+resource "aws_iam_role" "call_center_transfer_user" {
+  name_prefix        = "call-center-migration-user"
+  assume_role_policy = data.aws_iam_policy_document.aws_transfer_assume_role_policy.json
+  tags               = local.tags
+}
+
+resource "aws_iam_policy" "call_center_transfer_user" {
+  name_prefix = "call-center-migration-user"
+  policy      = data.aws_iam_policy_document.aws_transfer_user_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "call_center_transfer_user" {
+  policy_arn = aws_iam_policy.call_center_transfer_user.arn
+  role       = aws_iam_role.call_center_transfer_user.name
 }
--- terraform/environments/long-term-storage/data.tf
+++ terraform/environments/long-term-storage/data.tf
@@ -1,5 +1,5 @@
 #### This file can be used to store data specific to the member account ####
-data "aws_iam_policy_document" "call_center_transfer_assume_role_policy" {
+data "aws_iam_policy_document" "aws_transfer_assume_role_policy" {
   statement {
     effect = "Allow"
 
@@ -9,5 +9,13 @@ data "aws_iam_policy_document" "call_center_transfer_assume_role_policy" {
     }
 
     actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "aws_transfer_user_policy" {
+  statement {
+    actions   = ["s3:ListObjects"]
+    effect    = "Allow"
+    resources = [aws_s3_bucket.call_center.arn]
   }
 }
