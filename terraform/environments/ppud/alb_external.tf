8a86f3b671dd3c30d58604df0da668376be7d744
Buckingham <XC01857@lumen.com>
2024-11-26 13:22:06 +0000

    Update_261124_4

--- terraform/environments/ppud/alb_external.tf
+++ terraform/environments/ppud/alb_external.tf
@@ -145,7 +145,7 @@ resource "aws_lb_listener" "WAM-Front-End-Prod" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.WAM-Target-Group-Prod[0].arn
+    target_group_arn = aws_lb_target_group.WAM-Target-Group.arn
   }
 }
 
@@ -181,7 +181,7 @@ resource "aws_lb_target_group_attachment" "WAM-Portal-development" {
 
 resource "aws_lb_target_group_attachment" "WAM-Portal-preproduction" {
   count            = local.is-preproduction == true ? 1 : 0
-  target_group_arn = aws_lb_target_group.WAM-Target-Group-Preprod[0].arn
+  target_group_arn = aws_lb_target_group.WAM-Target-GroupPreprod[0].arn
   target_id        = aws_instance.s618358rgvw201[0].id
   port             = 80
 }
@@ -189,7 +189,7 @@ resource "aws_lb_target_group_attachment" "WAM-Portal-preproduction" {
 
 resource "aws_lb_target_group_attachment" "WAM-Portal-production" {
   count            = local.is-production == true ? 1 : 0
-  target_group_arn = aws_lb_target_group.WAM-Target-Group-Prod[0].arn
+  target_group_arn = aws_lb_target_group.WAM-Target-Group.arn
   target_id        = aws_instance.s618358rgvw204[0].id
   port             = 80
 }
