8870769f6ac46e1f6dc3beb9222d609471561510
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-11-26 15:34:24 +0000

    final deps

--- terraform/environments/electronic-monitoring-data/lambdas_secrets.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_secrets.tf
@@ -4,7 +4,7 @@ resource "aws_secretsmanager_secret" "db_glue_connection" {
 
 resource "aws_secretsmanager_secret_version" "db_glue_connection" {
   secret_id = aws_secretsmanager_secret.db_glue_connection.id
-  secret_string = local.create_rds_instance ? jsonencode(
+  secret_string = local.create_rds_instance == 1 ? jsonencode(
     {
       "host"     = "${aws_db_instance.database_2022[0].address},${aws_db_instance.database_2022[0].port}",
       "username" = aws_db_instance.database_2022[0].username,
