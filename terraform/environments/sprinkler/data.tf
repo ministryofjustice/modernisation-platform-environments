4a3f64596e34ee8e1cf3cf9a04fee5a6024be791
David Sibley <david.sibley@digital.justice.gov.uk>
2024-11-28 15:59:08 +0000

    removed leftovers from sprinkler

--- terraform/environments/sprinkler/data.tf
+++ terraform/environments/sprinkler/data.tf
@@ -11,24 +11,3 @@ data "aws_caller_identity" "core_network_services" {
 data "aws_caller_identity" "us_east_1" {
   provider = aws.us-east-1
 }
-
-data "aws_iam_policy_document" "aws_transfer_assume_role_policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["transfer.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-data "aws_iam_policy_document" "aws_transfer_user_policy" {
-  statement {
-    actions   = ["s3:ListObjects"]
-    effect    = "Allow"
-    resources = [aws_s3_bucket.call_center.arn]
-  }
-}
