94886f672158313a1fbaecc8a8ff968353e2356d
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-11-29 12:59:55 +0000

    style: correct resource name

--- terraform/environments/analytical-platform-compute/lakeformation-permissions.tf
+++ terraform/environments/analytical-platform-compute/lakeformation-permissions.tf
@@ -1,4 +1,4 @@
-resource "aws_lakeformation_permissions" "domain_table_permissions" {
+resource "aws_lakeformation_permissions" "metadata_copy_role_source_tag_super_permissions" {
   for_each = toset(["TABLE", "DATABASE"])
 
   principal   = module.copy_apdp_cadet_metadata_to_compute_assumable_role.iam_role_arn
