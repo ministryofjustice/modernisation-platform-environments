15fa2e35e9ef06201929e453875156fab1447a4b
Matthew Price <matthew.price2@digital.justice.gov.uk>
2024-11-29 14:06:19 +0000

    Update Lake Formation resource for development environment configuration

--- terraform/environments/electronic-monitoring-data/lake_formation.tf
+++ terraform/environments/electronic-monitoring-data/lake_formation.tf
@@ -12,7 +12,9 @@ data "aws_iam_roles" "modernisation_platform_sandbox_role" {
   path_prefix = "/aws-reserved/sso.amazonaws.com/"
 }
 
-resource "aws_lakeformation_data_lake_settings" "emds" {
+resource "aws_lakeformation_data_lake_settings" "emds_development" {
+  count = local.is-development ? 1 : 0
+
   admins = [
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.modernisation_platform_sandbox_role.names)}",
      data.aws_iam_role.github_actions_role.arn
