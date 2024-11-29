fb8df4b1fb682b14b5acfd3a82ab39b99cf812c9
David Sibley <david.sibley@digital.justice.gov.uk>
2024-11-29 08:39:14 +0000

    trimmed resources a little now that transfer users aren't in scope; we will pull files in and push files out. Wrote a first-pass bucket policy allow transfer service to access the bucket

--- terraform/environments/long-term-storage/call-centre-migration.tf
+++ terraform/environments/long-term-storage/call-centre-migration.tf
@@ -3,6 +3,11 @@ resource "aws_s3_bucket" "call_centre" {
   tags          = local.tags
 }
 
+resource "aws_s3_bucket_policy" "call_centre" {
+  bucket = aws_s3_bucket.call_centre.id
+  policy = data.aws_iam_policy_document.call_centre_bucket_policy.json
+}
+
 resource "aws_transfer_server" "call_centre" {
   logging_role                = aws_iam_role.call_centre_transfer_logging.arn
   structured_log_destinations = ["${aws_cloudwatch_log_group.call_centre.arn}:*"]
@@ -24,23 +29,7 @@ resource "aws_iam_role" "call_centre_transfer_logging" {
   tags               = local.tags
 }
 
-resource "aws_iam_role_policy_attachments_exclusive" "call_centre_transfer_logging_policy" {
+resource "aws_iam_role_policy_attachments_exclusive" "call_centre_transfer_logging" {
   policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSTransferLoggingAccess"]
   role_name   = aws_iam_role.call_centre_transfer_logging.name
-}
-
-resource "aws_iam_role" "call_centre_transfer_user" {
-  name_prefix        = "call-centre-migration-user"
-  assume_role_policy = data.aws_iam_policy_document.aws_transfer_assume_role_policy.json
-  tags               = local.tags
-}
-
-resource "aws_iam_policy" "call_centre_transfer_user" {
-  name_prefix = "call-centre-migration-user"
-  policy      = data.aws_iam_policy_document.aws_transfer_user_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "call_centre_transfer_user" {
-  policy_arn = aws_iam_policy.call_centre_transfer_user.arn
-  role       = aws_iam_role.call_centre_transfer_user.name
 }
