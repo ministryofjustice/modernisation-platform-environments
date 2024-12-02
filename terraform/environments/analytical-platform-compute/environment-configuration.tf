dcd21dc39ea4995db978984e4c5356c536804078
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-12-02 16:14:41 +0000

    Removing tags from dev

--- terraform/environments/analytical-platform-compute/environment-configuration.tf
+++ terraform/environments/analytical-platform-compute/environment-configuration.tf
@@ -44,10 +44,6 @@ locals {
       /* UI */
       ui_hostname = "development.analytical-platform.service.justice.gov.uk"
 
-      /* LF Domain Tags */
-      cadet_lf_tags = {
-        domain = ["bold", "civil", "courts", "general", "criminal_history", "development_sandpit", "electronic_monitoring", "finance", "interventions", "opg", "performance", "risk", "people", "prison", "probation", "victims", "victims_case_management"] # extracted from bucket paths
-      }
     }
     test = {
       /* VPC */
