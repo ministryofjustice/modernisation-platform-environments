837e313284ac5ca986875d00a7d20aeee2cbc60c
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-12-02 15:05:52 +0000

    add t2-onr-bods-1 instance

--- terraform/environments/oasys-national-reporting/locals_test.tf
+++ terraform/environments/oasys-national-reporting/locals_test.tf
@@ -151,23 +151,28 @@ locals {
 
     ec2_instances = {
 
-      # t2-onr-bods-1 = merge(local.ec2_instances.bods, {
-      #   config = merge(local.ec2_instances.bods.config, {
-      #     # ami_name          = ""
-      #     availability_zone = "eu-west-2a"
-      #     instance_profile_policies = concat(local.ec2_instances.bods.config.instance_profile_policies, [
-      #       "Ec2SecretPolicy",
-      #     ])
-      #   })
-      #   instance = merge(local.ec2_instances.bods.instance, {
-      #     instance_type = "m4.xlarge"
-      #   })
-      #   cloudwatch_metric_alarms = null
-      #   tags = merge(local.ec2_instances.bods.tags, {
-      #     oasys-national-reporting-environment = "t2"
-      #     domain-name                          = "azure.noms.root"
-      #   })
-      # })
+      t2-onr-bods-1 = merge(local.ec2_instances.bods, {
+        config = merge(local.ec2_instances.bods.config, {
+          # ami_name          = ""
+          availability_zone = "eu-west-2a"
+          instance_profile_policies = concat(local.ec2_instances.bods.config.instance_profile_policies, [
+            "Ec2SecretPolicy",
+          ])
+          user_data_raw = base64encode(templatefile(
+            "./templates/user-data-onr-bods-pwsh.yaml.tftpl", {
+              branch = "TM/TM-732/shared-drive-onr-bods"
+            }
+          ))
+        })
+        instance = merge(local.ec2_instances.bods.instance, {
+          instance_type = "m4.xlarge"
+        })
+        cloudwatch_metric_alarms = null
+        tags = merge(local.ec2_instances.bods.tags, {
+          oasys-national-reporting-environment = "t2"
+          domain-name                          = "azure.noms.root"
+        })
+      })
 
       # t2-onr-bods-2 = merge(local.ec2_instances.bods, {
       #   config = merge(local.ec2_instances.bods.config, {
@@ -175,14 +180,14 @@ locals {
       #     instance_profile_policies = concat(local.ec2_instances.bods.config.instance_profile_policies, [
       #       "Ec2SecretPolicy",
       #     ])
+      #     user-data-raw = base64encode(templatefile(
+      #       "./templates/user-data-onr-bods-pwsh.yaml.tftpl", {
+      #         branch = "TM/TM-660/onr-bods-second-server"
+      #       }))
       #   })
       #   instance = merge(local.ec2_instances.bods.instance, {
       #     instance_type = "m4.xlarge"
       #   })
-      #   user_data_raw = base64encode(templatefile(
-      #      "./templates/user-data-onr-bods-pwsh.yaml.tftpl", {
-      #      branch = "TM/TM-660/onr-bods-second-server"
-      #    }))
       #   cloudwatch_metric_alarms = null
       #   tags = merge(local.ec2_instances.bods.tags, {
       #     oasys-national-reporting-environment = "t2"
