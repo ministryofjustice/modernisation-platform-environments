e26667da362eff062d3105422fec8d62ee7e3148
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-02 21:56:02 +0000

    fix: add correct cidr blocks

--- terraform/environments/electronic-monitoring-data/lambdas_sg.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_sg.tf
@@ -13,12 +13,12 @@ resource "aws_security_group" "lambda_generic" {
 
 resource "aws_security_group_rule" "lambda_ingress_generic" {
   #checkov:skip=CKV_AWS_23: "Ensure every security group and rule has a description"
-  cidr_blocks       = [data.aws_vpc.dpr.cidr_block, ]
+  cidr_blocks       = [data.aws_vpc.shared.cidr_block, ]
   type              = "ingress"
   from_port         = 0
   to_port           = 65535
   protocol          = "tcp"
-  security_group_id = aws_security_group.lambda_generic[0].id
+  security_group_id = aws_security_group.lambda_generic.id
 }
 
 resource "aws_security_group_rule" "lambda_egress_generic" {
@@ -29,5 +29,5 @@ resource "aws_security_group_rule" "lambda_egress_generic" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lambda_generic[0].id
+  security_group_id = aws_security_group.lambda_generic.id
 }
