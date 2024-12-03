a493133e1b3a4aea07607acd6531cdd0e6c0dc2e
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-12-03 09:29:24 +0000

    fix

--- terraform/modules/efs/main.tf
+++ terraform/modules/efs/main.tf
@@ -68,7 +68,7 @@ resource "aws_efs_file_system" "this" {
 
 # disable automatic backups - use mod platform everything vault instead
 resource "aws_efs_backup_policy" "policy" {
-  file_system_id = aws_efs_file_system.fs.id
+  file_system_id = aws_efs_file_system.this.id
 
   backup_policy {
     status = "DISABLED"
