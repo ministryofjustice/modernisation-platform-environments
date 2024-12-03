577ff0e8efd35cb0e722f2df717550c95aecd643
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-03 11:47:52 +0000

    add versioning to lambdas module

--- terraform/environments/electronic-monitoring-data/modules/lambdas/versions.tf
+++ terraform/environments/electronic-monitoring-data/modules/lambdas/versions.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.0"
+    }
   }
   required_version = ">= 1.0.1"
 }
