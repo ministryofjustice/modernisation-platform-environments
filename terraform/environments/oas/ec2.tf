d586d9b8f95fb8d180b736c46cfc7377720e1aa1
Vladimirs Kovalovs <vladimirs.kovalovs@digital.justice.gov.uk>
2024-12-05 23:42:26 +0000

    [TM-805] minor change

--- terraform/environments/oas/ec2.tf
+++ terraform/environments/oas/ec2.tf
@@ -241,9 +241,8 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
 }
 
 resource "aws_iam_role" "ec2_instance_role" {
-  name                = "${local.application_name}-role"
-  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
-  assume_role_policy  = <<EOF
+  name               = "${local.application_name}-role"
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -257,6 +256,11 @@ resource "aws_iam_role" "ec2_instance_role" {
     ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_instance_role_attachment" {
+  role       = aws_iam_role.ec2_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy" "ec2_instance_policy" {
