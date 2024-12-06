88e04985b97a87fa23ade1037cef721722f1c367
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-12-06 14:26:03 +0000

    remove multi_az fsx share as not needed

--- terraform/environments/oasys-national-reporting/locals_test.tf
+++ terraform/environments/oasys-national-reporting/locals_test.tf
@@ -231,35 +231,35 @@ locals {
     }
 
     fsx_windows = {
-      # switch to using SINGLE_AZ_1 in T2 (cheaper), change settings in T2 servers then remove this
-      t2-bods-windows-share = {
-        preferred_availability_zone = "eu-west-2a"
-        deployment_type             = "MULTI_AZ_1"
-        security_groups             = ["bods"]
-        skip_final_backup           = true
-        storage_capacity            = 128
-        throughput_capacity         = 8
+      # retain commented out as a version of this (MULTI_AZ_1) fsx_share will be needed in production account
+      # t2-bods-windows-share = {
+      #   preferred_availability_zone = "eu-west-2a"
+      #   deployment_type             = "MULTI_AZ_1"
+      #   security_groups             = ["bods"]
+      #   skip_final_backup           = true
+      #   storage_capacity            = 128
+      #   throughput_capacity         = 8
 
-        subnets = [
-          {
-            name               = "private"
-            availability_zones = ["eu-west-2a", "eu-west-2b"]
-          }
-        ]
+      #   subnets = [
+      #     {
+      #       name               = "private"
+      #       availability_zones = ["eu-west-2a", "eu-west-2b"]
+      #     }
+      #   ]
 
-        self_managed_active_directory = {
-          dns_ips = [
-            module.ip_addresses.mp_ip.ad-azure-dc-a,
-            module.ip_addresses.mp_ip.ad-azure-dc-b,
-          ]
-          domain_name          = "azure.noms.root"
-          username             = "svc_join_domain"
-          password_secret_name = "/sap/bods/t2/passwords"
-        }
-        tags = {
-          backup = true
-        }
-      }
+      #   self_managed_active_directory = {
+      #     dns_ips = [
+      #       module.ip_addresses.mp_ip.ad-azure-dc-a,
+      #       module.ip_addresses.mp_ip.ad-azure-dc-b,
+      #     ]
+      #     domain_name          = "azure.noms.root"
+      #     username             = "svc_join_domain"
+      #     password_secret_name = "/sap/bods/t2/passwords"
+      #   }
+      #   tags = {
+      #     backup = true
+      #   }
+      # }
       t2-bods-win-share = {
         deployment_type     = "SINGLE_AZ_1"
         security_groups     = ["bods"]
