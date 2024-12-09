fbc298a3b935bc453d338fb82591e1cf0a908e05
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-12-09 14:20:25 +0000

    derp, commit doesn't exist in both

--- terraform/modules/baseline/ec2_instance.tf
+++ terraform/modules/baseline/ec2_instance.tf
@@ -4,7 +4,7 @@ module "ec2_instance" {
   for_each = var.ec2_instances
 
   # source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance?ref=ebf373aef70841d1c854689eb034b4e147be1709"
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance?ref=59ef74c18bcdc514c80e7b4ae9bb89e4e55f3169"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ec2-instance?ref=ebf373aef70841d1c854689eb034b4e147be1709"
 
   providers = {
     aws.core-vpc = aws.core-vpc
