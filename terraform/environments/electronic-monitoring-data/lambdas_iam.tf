ce3918ceed179ba28427537fe07f5f0478ed4d64
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-09 17:41:34 +0000

    Add s3:PutObject and s3:PutObjectTagging permissions to IAM policy

--- terraform/environments/electronic-monitoring-data/lambdas_iam.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_iam.tf
@@ -379,7 +379,8 @@ data "aws_iam_policy_document" "format_json_fms_data_policy_document" {
     actions = [
       "s3:GetObject",
       "s3:GetObjectTagging",
-
+      "s3:PutObject",
+      "s3:PutObjectTagging",
     ]
     resources = ["${module.s3-data-bucket.bucket.arn}/*"]
   }
