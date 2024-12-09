082332b2ec3d9623aaa506c93cce5904c3b5b601
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-12-09 13:32:52 +0000

    Add use of AWBackupDefaultServiceRole

---
+++ terraform/environments/delius-core/modules/delius_environment/backup_vault.tf
@@ -0,0 +1,27 @@
+# Allow the modernisation-platform-oidc-cicd role to assume the
+# AWS supplied role AWSBackupDefaultServiceRole which allows us
+# to write snapshots to the Backup Vault.   We use Backup Vault
+# to manage EC2 snapshots for Oracle hosts as these are only
+# created sporadically, e.g. ahead of a service release, and will
+# therefore not be continually overwritten.  Writing them to a
+# backup vault allows them to timeout without being overwritten.
+data "aws_iam_policy_document" "assume_AWSBackupDefaultServiceRole" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_info.id}:role/modernisation-platform-oidc-cicd"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "assume_AWSBackupDefaultServiceRole" {
+  name   = "AssumeAWSBackupDefaultServiceRolePolicy"
+  policy = data.aws_iam_policy_document.assume_AWSBackupDefaultServiceRole.json
+}
+
+resource "aws_iam_policy_attachment" "assume_AWSBackupDefaultServiceRole" {
+  name       = "AssumeAWSBackupDefaultServiceRoleAttachment"
+  roles      = ["arn:aws:iam::${var.account_info.id}::role/service-role/AWSBackupDefaultServiceRole"]
+  policy_arn = aws_iam_policy.assume_AWSBackupDefaultServiceRole.arn
+}
