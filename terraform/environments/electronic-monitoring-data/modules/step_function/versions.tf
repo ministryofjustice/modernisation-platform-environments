48ab1874539d4119588c3c4ee85d200e66ee9530
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-10 12:57:00 +0000

    Add random provider version to step function module

--- terraform/environments/electronic-monitoring-data/modules/step_function/versions.tf
+++ terraform/environments/electronic-monitoring-data/modules/step_function/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
   required_version = ">= 1.0.1"
 }
