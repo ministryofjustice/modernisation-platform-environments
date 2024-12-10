a7f031994d6d99fe20effe4e5c64a3d4173e5553
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-12-10 15:36:45 +0000

    Update to comments

--- terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
@@ -9,17 +9,14 @@ resource "aws_backup_vault" "oracle_backup_vault" {
   )
 }
 
-# Allow the modernisation-platform-oidc-cicd role to assume the
-# new role oracle_ec2_backup_service_role which allows us
-# to write snapshots to the Backup Vault.   We use Backup Vault
-# to manage EC2 snapshots for Oracle hosts as these are only
-# created sporadically, e.g. ahead of a service release, and will
-# therefore not be continually overwritten.  Writing them to a
+# Allow the AWSBackupDefaultServiceRolePolicy role to be passed to the modernisation-platform-oidc-cicd role.
+# We use Backup Vault to manage EC2 snapshots for Oracle hosts as these are only created sporadically, 
+# e.g. ahead of a service release, and will therefore not be continually overwritten.  Writing them to a
 # backup vault allows them to timeout without being overwritten.
-# The AWSBackupServiceRolePolicyForBackup is managed by AWS and
-# is documented at: https://docs.aws.amazon.com/aws-backup/latest/devguide/iam-service-roles.html
+# The AWSBackupDefaultServiceRole managed by AWS and is documented at: 
+# https://docs.aws.amazon.com/aws-backup/latest/devguide/iam-service-roles.html
 resource "aws_iam_policy" "oracle_ec2_snapshot_backup_pass_role_policy" {
-  description = "Allow iam:PassRole for oracle_ec2_snapshot_backup_role"
+  description = "Allow iam:PassRole for AWSBackupDefaultServiceRole"
 
   policy = jsonencode({
     Version = "2012-10-17",
