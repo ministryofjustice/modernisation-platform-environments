cf1f982a4b3f540f250acf7aad305741f2af3b8e
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-12-10 11:18:55 +0000

    Create new role for AWS Snapshots

--- terraform/environments/delius-core/modules/delius_environment/backup_vault.tf
+++ terraform/environments/delius-core/modules/delius_environment/backup_vault.tf
@@ -1,57 +1,39 @@
 # Allow the modernisation-platform-oidc-cicd role to assume the
-# AWS supplied role AWSBackupDefaultServiceRole which allows us
+# new role oracle_ec2_backup_service_role which allows us
 # to write snapshots to the Backup Vault.   We use Backup Vault
 # to manage EC2 snapshots for Oracle hosts as these are only
 # created sporadically, e.g. ahead of a service release, and will
 # therefore not be continually overwritten.  Writing them to a
 # backup vault allows them to timeout without being overwritten.
+# The AWSBackupServiceRolePolicyForBackup is managed by AWS and
+# is documented at: https://docs.aws.amazon.com/aws-backup/latest/devguide/iam-service-roles.html
 
-data "aws_iam_role" "AWSBackupDefaultServiceRole" {
-  name = "AWSBackupDefaultServiceRole"
+# New IAM role to allow writing of AWS EC2 snapshots of Oracle Hosts to Backup Vault
+resource "aws_iam_role" "oracle_ec2_backup_service_role" {
+  name = "oracle_ec2_backup_service_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "ForAnyValue:ArnLike": {
+          "aws:PrincipalArn": "arn:aws:iam::${var.account_info.id}:role/modernisation-platform-oidc-cicd"
+        }
+      }
+    }
+  ]
+}
+EOF
 }
 
-# Decode the existing assume role policy
-locals {
-  existing_statements = try(
-    jsondecode(data.aws_iam_role.AWSBackupDefaultServiceRole.assume_role_policy)["Statement"],
-    []
-  )
-  new_statement = {
-    Effect = "Allow",
-    Principal = {
-      AWS = "arn:aws:iam::${var.account_info.id}:role/modernisation-platform-oidc-cicd"
-    },
-    Action = "sts:AssumeRole"
-  }
-  combined_statements = concat(local.existing_statements, [local.new_statement])
+resource "aws_iam_role_policy_attachment" "oracle_ec2_backup_service_policy_attachment" {
+  role       = aws_iam_role.oracle_ec2_backup_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
 }
-
-# Create the combined policy
-data "aws_iam_policy_document" "merged_trust_policy" {
-  dynamic "statement" {
-    for_each = toset(local.combined_statements)
-
-       content {
-            effect    = statement.value["Effect"]
-            actions   = [statement.value["Action"]]
-            principals {
-                type        = keys(statement.value["Principal"])[0]
-                identifiers = toset([statement.value["Principal"][keys(statement.value["Principal"])[0]]])
-            }
-       }
-  }
-}
-resource "aws_iam_policy" "new_trust_policy" {
-  name        = "AWSBackupDefaultServiceRolePolicy"
-  description = "Updated trust policy for AWSBackupDefaultServiceRole"
-
-  policy = data.aws_iam_policy_document.merged_trust_policy.json
-}
-
-resource "aws_iam_policy_attachment" "attach_policy_to_AWSBackupDefaultServiceRole" {
-  name       = "attach-policy-to-AWSBackupDefaultServiceRole"
-  roles      = ["AWSBackupDefaultServiceRole"]
-  policy_arn = aws_iam_policy.new_trust_policy.arn
-}
-
-
