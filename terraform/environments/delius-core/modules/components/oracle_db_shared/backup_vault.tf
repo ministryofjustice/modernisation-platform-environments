7089bc120af304d20e9e49a9f0e0c5600639f379
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-12-10 15:17:25 +0000

    Pass the Backup Role

--- terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
@@ -9,35 +9,6 @@ resource "aws_backup_vault" "oracle_backup_vault" {
   )
 }
 
-resource "aws_backup_vault_policy" "oracle_backup_vault_policy" {
-  backup_vault_name = aws_backup_vault.oracle_backup_vault.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement: [
-      {
-        Sid: "AllowRoleAccess",
-        Effect: "Allow",
-        Principal: {
-          AWS: "${aws_iam_role.oracle_ec2_snapshot_backup_role.arn}"
-        },
-        Action: [
-          "backup:CopyIntoBackupVault",
-          "backup:DeleteRecoveryPoint",
-          "backup:DescribeBackupVault",
-          "backup:DescribeRecoveryPoint",
-          "backup:GetBackupVaultAccessPolicy",
-          "backup:GetRecoveryPointRestoreMetadata",
-          "backup:ListRecoveryPointsByBackupVault",
-          "backup:PutBackupVaultAccessPolicy",
-          "backup:StartRestoreJob"
-        ],
-        Resource: "${aws_backup_vault.oracle_backup_vault.arn}"
-      }
-    ]
-  })
-}
-
 # Allow the modernisation-platform-oidc-cicd role to assume the
 # new role oracle_ec2_backup_service_role which allows us
 # to write snapshots to the Backup Vault.   We use Backup Vault
@@ -47,42 +18,6 @@ resource "aws_backup_vault_policy" "oracle_backup_vault_policy" {
 # backup vault allows them to timeout without being overwritten.
 # The AWSBackupServiceRolePolicyForBackup is managed by AWS and
 # is documented at: https://docs.aws.amazon.com/aws-backup/latest/devguide/iam-service-roles.html
-
-# New IAM role to allow writing of AWS EC2 snapshots of Oracle Hosts to Backup Vault
-resource "aws_iam_role" "oracle_ec2_snapshot_backup_role" {
-  name = "oracle-ec2-snapshot-backup-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "sts:AssumeRole",
-      "Condition": {
-        "ForAnyValue:ArnLike": {
-          "aws:PrincipalArn": "arn:aws:iam::${var.account_info.id}:role/modernisation-platform-oidc-cicd"
-        }
-      }
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "oracle_ec2_snapshot_backup_role_policy_attachment" {
-  role       = aws_iam_role.oracle_ec2_snapshot_backup_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
-}
-
-resource "aws_iam_role_policy_attachment" "oracle_ec2_snapshot_backup_operator_policy_attachment" {
-  role       = aws_iam_role.oracle_ec2_snapshot_backup_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSBackupOperatorAccess"
-}
-
 resource "aws_iam_policy" "oracle_ec2_snapshot_backup_pass_role_policy" {
   description = "Allow iam:PassRole for oracle_ec2_snapshot_backup_role"
 
@@ -92,7 +27,7 @@ resource "aws_iam_policy" "oracle_ec2_snapshot_backup_pass_role_policy" {
       {
         Effect   = "Allow",
         Action   = "iam:PassRole",
-        Resource = aws_iam_role.oracle_ec2_snapshot_backup_role.arn
+        Resource = "arn:aws:iam::${var.account_info.id}:role/AWSBackupDefaultServiceRole"
       }
     ]
   })
