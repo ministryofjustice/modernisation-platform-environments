100388d0fdbb1082b5224db05920860e188aaa3f
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-12 09:46:17 +0000

    right placement

--- terraform/environments/electronic-monitoring-data/s3_sns.tf
+++ terraform/environments/electronic-monitoring-data/s3_sns.tf
@@ -30,8 +30,8 @@ resource "aws_s3_bucket_notification" "historic_data_store" {
 }
 
 # sns topic to allow multiple lambdas to be triggered off of it
+#trivy:ignore:AVD-AWS-0136
 resource "aws_sns_topic" "historic_s3_events" {
-  #trivy:ignore:AVD-AWS-0136
   name              = "${module.s3-data-bucket.bucket.id}-historic-object-created-topic"
   kms_master_key_id = "alias/aws/sns"
 }
@@ -85,8 +85,8 @@ resource "aws_s3_bucket_notification" "live_serco_fms_data_store" {
 }
 
 # sns topic to allow multiple lambdas to be triggered off of it
+#trivy:ignore:AVD-AWS-0136
 resource "aws_sns_topic" "live_serco_fms_s3_events" {
-  #trivy:ignore:AVD-AWS-0136
   name              = "${module.s3-data-bucket.bucket.id}-live-object-created-topic"
   kms_master_key_id = "alias/aws/sns"
 }
