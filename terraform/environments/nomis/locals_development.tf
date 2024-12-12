62c83e02ffd60ec73cdd38a710b59c5745f3dc1f
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-12-12 16:07:08 +0000

    -

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -141,7 +141,7 @@ locals {
         })
         user_data_cloud_init = merge(local.ec2_autoscaling_groups.web12.user_data_cloud_init, {
           args = merge(local.ec2_autoscaling_groups.web12.user_data_cloud_init.args, {
-            branch = "TM-626/nomis/weblogic-12-v1"
+            branch = "main"
           })
         })
         tags = merge(local.ec2_autoscaling_groups.web12.tags, {
