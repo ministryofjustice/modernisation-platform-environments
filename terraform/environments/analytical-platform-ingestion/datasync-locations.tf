efb1907f7138b0a3015932d51803988e098b5995
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-12-16 16:30:10 +0000

    Update locations
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/environments/analytical-platform-ingestion/datasync-locations.tf
+++ terraform/environments/analytical-platform-ingestion/datasync-locations.tf
@@ -10,8 +10,8 @@ resource "aws_datasync_location_s3" "mojap_datasync_s3" {
 }
 
 resource "aws_datasync_location_smb" "dom1_hq_pgo_shared_group_sis_case_management_investigations" {
-  server_hostname = "dom1.infra.int"
-  subdirectory    = "/data/hq/PGO/Shared/Group/SIS Case Management/Investigations/"
+  server_hostname = "eucw4171nas002.dom1.infra.int"
+  subdirectory    = "/mojshared002$/FITS_3635/Shared/Group/SIS Case Management/Investigations/"
 
   user     = jsondecode(data.aws_secretsmanager_secret_version.datasync_dom1.secret_string)["username"]
   password = jsondecode(data.aws_secretsmanager_secret_version.datasync_dom1.secret_string)["password"]
@@ -22,8 +22,8 @@ resource "aws_datasync_location_smb" "dom1_hq_pgo_shared_group_sis_case_manageme
 }
 
 resource "aws_datasync_location_smb" "dom1_hq_pgo_shared_group_sis_case_management_itas" {
-  server_hostname = "dom1.infra.int"
-  subdirectory    = "/data/hq/PGO/Shared/Group/SIS Case Management/ITAS/"
+  server_hostname = "eucw4171nas002.dom1.infra.int"
+  subdirectory    = "/mojshared002$/FITS_3635/Shared/Group/SIS Case Management/ITAS/"
 
   user     = jsondecode(data.aws_secretsmanager_secret_version.datasync_dom1.secret_string)["username"]
   password = jsondecode(data.aws_secretsmanager_secret_version.datasync_dom1.secret_string)["password"]
