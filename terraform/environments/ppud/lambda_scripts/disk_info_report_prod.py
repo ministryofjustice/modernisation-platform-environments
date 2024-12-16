2bcb7ca2df47354ff03f68ce90150a900b28eb3f
Buckingham <XC01857@lumen.com>
2024-12-16 09:09:08 +0000

    Update_161224_1

--- terraform/environments/ppud/lambda_scripts/disk_info_report_prod.py
+++ terraform/environments/ppud/lambda_scripts/disk_info_report_prod.py
@@ -19,7 +19,7 @@ from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 # Configuration
 CURRENT_DATE = datetime.now().strftime('%a %d %b %Y')
 SENDER = "donotreply@cjsm.secure-email.ppud.justice.gov.uk"
-RECIPIENTS = ["nick.buckingham@colt.net", "gabriella.browning@colt.net", "pankaj.pant@colt.net", "david.savage@colt.net"]
+RECIPIENTS = ["nick.buckingham@colt.net", "gabriela.browning@colt.net", "pankaj.pant@colt.net", "david.savage@colt.net"]
 SUBJECT = f'AWS PPUD Disk Information Report - {CURRENT_DATE}'
 AWS_REGION = 'eu-west-2'
 bucket_name = 'moj-lambda-layers-prod'
--- terraform/environments/ppud/versions.tf
+++ terraform/environments/ppud/versions.tf
@@ -8,6 +8,14 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    random = {
+      version = "~> 3.0"
+      source  = "hashicorp/random"
+    }
+    archive = {
+      version = "~> 2.0"
+      source  = "hashicorp/archive"
+    }
   }
   required_version = "~> 1.0"
 }
