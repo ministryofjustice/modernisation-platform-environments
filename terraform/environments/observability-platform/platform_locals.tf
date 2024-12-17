4acb74022300d08bd4770ff2d0a49851fc02955c
Aaron Robinson <aaron.robinson@digital.justice.gov.uk>
2024-12-17 18:46:11 +0000

    Merge MP account ID into environmnet_management

--- terraform/environments/observability-platform/platform_locals.tf
+++ terraform/environments/observability-platform/platform_locals.tf
@@ -2,7 +2,16 @@ locals {
 
   application_name = "observability-platform"
 
-  environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
+  # environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
+
+  environment_management = merge(
+    jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string),
+    { account_ids = merge(
+        jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string).account_ids,
+        { "modernisation_platform" = data.aws_ssm_parameter.modernisation_platform_account_id.value }
+      )
+    }
+  )
 
   # Stores modernisation platform account id for setting up the modernisation-platform provider
   modernisation_platform_account_id = data.aws_ssm_parameter.modernisation_platform_account_id.value
