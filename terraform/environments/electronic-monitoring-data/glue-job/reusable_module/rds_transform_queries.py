cdb4ed51bfe92fd591bc5550e42c75a7f071972c
Madhu Kadiri <madhu.kadiri2@justice.gov.uk>
2024-12-18 14:10:26 +0000

    partitioned_parquet_output_data_validation - 1812 - 3

--- terraform/environments/electronic-monitoring-data/glue-job/reusable_module/rds_transform_queries.py
+++ terraform/environments/electronic-monitoring-data/glue-job/reusable_module/rds_transform_queries.py
@@ -67,7 +67,7 @@ class SQLServer_Extract_Transform:
         'AuditDateTime': """
         CAST(
         CASE 
-             WHEN RIGHT(FORMAT(AuditDateTime, 'fff'), 2) = '00' THEN 
+            WHEN RIGHT(FORMAT(AuditDateTime, 'fff'), 2) = '00' THEN 
                 FORMAT(AuditDateTime, 'yyyy-MM-dd HH:mm:ss.f')
             WHEN RIGHT(FORMAT(AuditDateTime, 'fff'), 1) = '0' THEN 
                 FORMAT(AuditDateTime, 'yyyy-MM-dd HH:mm:ss.ff')
