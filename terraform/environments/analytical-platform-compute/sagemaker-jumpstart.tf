7ca54e51323bfc50c6d432341b6b86b3e8228ab6
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2024-12-18 11:39:39 +0000

    feat: add sagemaker_bucket and bucket policy

--- terraform/environments/analytical-platform-compute/sagemaker-jumpstart.tf
+++ terraform/environments/analytical-platform-compute/sagemaker-jumpstart.tf
@@ -48,3 +48,55 @@ resource "aws_iam_role" "sagemaker_execution_role" {
 
   tags = local.tags
 }
+
+module "sagemaker_bucket" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  count   = terraform.workspace == "analytical-platform-compute-development" ? 1 : 0 # Creates IAM role if not provided
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.2.2"
+
+  bucket = "mojap-compute-sagemaker-jumpstart-${local.environment}"
+
+  force_destroy = true
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.sagemaker_bucket_policy[0].json
+
+  object_lock_enabled = false
+
+  versioning = {
+    status = "Disabled"
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = merge(
+    local.tags,
+    { "backup" = "false" }
+  )
+}
+
+data "aws_iam_policy_document" "sagemaker_bucket_policy" {
+  #checkov:skip=CKV_AWS_356:resource "*" limited by condition
+  count = terraform.workspace == "analytical-platform-compute-development" ? 1 : 0 # Creates IAM role if not provided
+  statement {
+    sid     = "BroadS3Access"
+    effect  = "Allow"
+    actions = ["s3:*"]
+    resources = ["arn:aws:s3:::mojap-compute-sagemaker-jumpstart-${local.environment}/*",
+    "arn:aws:s3:::mojap-compute-sagemaker-jumpstart-${local.environment}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.sagemaker_execution_role[0].arn]
+    }
+  }
+}
