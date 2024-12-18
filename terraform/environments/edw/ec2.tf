10845279396632996c84ff705437511283590cdd
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2024-12-18 16:45:25 +0000

    TM-818 Fixing scripts download not working from S3

--- terraform/environments/edw/ec2.tf
+++ terraform/environments/edw/ec2.tf
@@ -495,7 +495,8 @@ resource "aws_iam_policy" "edw_ec2_role_policy" {
                 "s3:PutObjectAcl",
                 "s3:GetObject",
                 "s3:GetObjectAcl",
-                "s3:DeleteObject"
+                "s3:DeleteObject",
+                "s3:ListBucket"
             ],
             "Resource": [
                 "arn:aws:s3:::${aws_s3_bucket.scripts.id}/*.sh",
