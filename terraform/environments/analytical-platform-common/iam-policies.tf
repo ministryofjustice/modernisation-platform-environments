36084b26a59dbdbfed73fe60bd459cd199732e84
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-12-18 14:08:48 +0000

    Basic ECR repository permissions
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ terraform/environments/analytical-platform-common/iam-policies.tf
@@ -0,0 +1,29 @@
+data "aws_iam_policy_document" "mlflow_access" {
+  statement {
+    sid    = "AllowECRRepositoryPermissions"
+    effect = "Allow"
+    actions = [
+      "ecr:CreateRepository",
+      "ecr:GetRepositoryPolicy",
+      "ecr:SetRepositoryPolicy"
+    ]
+    resources = ["arn:aws:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/*"]
+  }
+  statement {
+    sid       = "DenyECRRepositoryPermissions"
+    effect    = "Deny"
+    actions   = ["ecr:DeleteRepository"]
+    resources = ["arn:aws:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/*"]
+  }
+}
+
+module "ecr_iam_policy" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.48.0"
+
+  name_prefix = "ecr-access"
+
+  policy = data.aws_iam_policy_document.ecr_access.json
+}
---
+++ terraform/environments/analytical-platform-common/iam-role.tf
@@ -0,0 +1,17 @@
+module "ecr_access_iam_role" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+  version = "5.48.0"
+
+  name = "ecr-access"
+
+  subjects = [
+    "ministrofjustice/*",
+    "moj-analytical-services/*"
+  ]
+
+  policies = {
+    ecr = module.ecr_iam_policy.arn
+  }
+}
