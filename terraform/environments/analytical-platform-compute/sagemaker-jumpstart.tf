b449771f59b0bbe115371e7c2e9dd8caff6bb2db
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2024-12-18 09:47:49 +0000

    feat:create sagemaker-_execution_role

---
+++ terraform/environments/analytical-platform-compute/sagemaker-jumpstart.tf
@@ -0,0 +1,50 @@
+# image
+# model id
+
+
+resource "aws_iam_role" "sagemaker_execution_role" {
+  count = terraform.workspace == "analytical-platform-compute-development" ? 1 : 0 # Creates IAM role if not provided
+  name  = "poc-sagemaker-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "sagemaker.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "terraform-inferences-policy"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow",
+          Action = [
+            "cloudwatch:PutMetricData",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "logs:CreateLogGroup",
+            "logs:DescribeLogStreams",
+            "s3:GetObject",
+            "s3:PutObject",
+            "s3:ListBucket",
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage"
+          ],
+          Resource = "*"
+        }
+      ]
+    })
+
+  }
+
+  tags = local.tags
+}
