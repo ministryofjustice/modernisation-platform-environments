7bf66dd387eb58752335bacb50c307f472e75f68
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-12-19 10:31:34 +0000

    Allow EC2 instance roles assume privilege to use backup default service role

--- terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
@@ -8,3 +8,31 @@ resource "aws_backup_vault" "oracle_backup_vault" {
     },
   )
 }
+
+# Allow the AWSBackupDefaultServiceRole role to be passed to the instance roles.
+# We use Backup Vault to manage EC2 snapshots for Oracle hosts as these are only created sporadically, 
+# e.g. ahead of a service release, and will therefore not be continually overwritten.  Writing them to a
+# backup vault allows them to timeout without being overwritten.
+# The AWSBackupDefaultServiceRole managed by AWS and is documented at: 
+# https://docs.aws.amazon.com/aws-backup/latest/devguide/iam-service-roles.html
+resource "aws_iam_policy" "oracle_ec2_snapshot_backup_role_policy" {
+  description = "Allow iam:PassRole for AWSBackupDefaultServiceRole"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "iam:AssumeRole",
+        Resource = "arn:aws:iam::${var.account_info.id}:role/service-role/AWSBackupDefaultServiceRole"
+      }
+    ]
+  })
+}
+
+# Allow Access To AWSBackupDefaultServiceRole From EC2 Instance Roles
+resource "aws_iam_policy_attachment" "oracle_ec2_snapshot_backup_role_policy_attachment" {
+  name       = "oracle-ec2-snapshot-backup-role-policy-attachment"
+  roles      = var.instance_roles
+  policy_arn = aws_iam_policy.oracle_ec2_snapshot_backup_role_policy.arn
+}
