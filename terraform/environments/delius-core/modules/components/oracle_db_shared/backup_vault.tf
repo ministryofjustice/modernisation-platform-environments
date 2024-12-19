1ee4c6c57e023f886135bef371beac05fca0fa3b
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-12-19 15:13:47 +0000

    Provide meaningful name for the policy

--- terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
@@ -16,6 +16,7 @@ resource "aws_backup_vault" "oracle_backup_vault" {
 # The AWSBackupDefaultServiceRole managed by AWS and is documented at: 
 # https://docs.aws.amazon.com/aws-backup/latest/devguide/iam-service-roles.html
 resource "aws_iam_policy" "oracle_ec2_snapshot_backup_role_policy" {
+  name = "oracle-ec2-snapshot-backup-role-policy"
   description = "Allow iam:AssumeRole for AWSBackupDefaultServiceRole"
 
   policy = jsonencode({
