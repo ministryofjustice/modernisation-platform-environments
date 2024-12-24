d7bf56893984846641be33974c06d796f467740a
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-24 10:28:57 +0000

    fix: add acutal permissions and reference cols correctly

--- terraform/environments/electronic-monitoring-data/lake_formation_cloud_platform.tf
+++ terraform/environments/electronic-monitoring-data/lake_formation_cloud_platform.tf
@@ -8,10 +8,22 @@ resource "aws_lakeformation_data_cells_filter" "filter_fms_current" {
     name             = "filter-fms-current"
     table_catalog_id = data.aws_caller_identity.current.account_id
     table_name       = "account"
-
+    column_names     = ["*"]
 
     row_filter {
       filter_expression = "select * from account where __current=true"
     }
+  }
+}
+
+
+resource "aws_lakeformation_permissions" "share_fms_with_cp" {
+  principal   = module.cmt_front_end_assumable_role.iam_role_arn
+  permissions = ["SELECT"]
+  data_cells_filter {
+    database_name    = "staged_fms_${local.env_}dbt"
+    table_name       = "account"
+    table_catalog_id = data.aws_caller_identity.current.account_id
+    name             = aws_lakeformation_data_cells_filter.filter_fms_current.name
   }
 }
