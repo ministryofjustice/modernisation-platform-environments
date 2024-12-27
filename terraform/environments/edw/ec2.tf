96bda81469fe8bf824545195499cde282c363019
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-12-27 23:34:27 +0000

    remove lambda rule
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/environments/edw/ec2.tf
+++ terraform/environments/edw/ec2.tf
@@ -721,15 +721,6 @@ resource "aws_vpc_security_group_ingress_rule" "db_bastion_ssh" {
   to_port                      = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "db_lambda" {
-  security_group_id            = aws_security_group.edw_db_security_group.id
-  description                  = "Allow Lambda SSH access for backup snapshots"
-  referenced_security_group_id = aws_security_group.backup_lambda.id
-  from_port                    = 22
-  ip_protocol                  = "tcp"
-  to_port                      = 22
-}
-
 resource "aws_vpc_security_group_ingress_rule" "RDS_Appstream" {
   security_group_id = aws_security_group.edw_db_security_group.id
   description       = "RDS Appstream access"
