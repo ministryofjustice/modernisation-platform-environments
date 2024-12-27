940756e45d5a64d04bc29c14ffb4b3483c94a6a5
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-27 14:01:52 +0000

    change ref

--- terraform/environments/electronic-monitoring-data/lake_formation_cloud_platform.tf
+++ terraform/environments/electronic-monitoring-data/lake_formation_cloud_platform.tf
@@ -2,13 +2,17 @@ locals {
   env_ = "${local.environment_shorthand}_"
 }
 
+resource "aws_lakeformation_resource" "data_bucket" {
+  arn = module.s3-create-a-derived-bucket.bucket.arn
+}
+
 module "share_current_version" {
   source = "./modules/lakeformation"
   table_filters = {
     "account" = "__current=true"
   }
-  role_arn               = module.cmt_front_end_assumable_role.iam_role_arn
-  database_name          = "staged_fms_${local.env_}dbt"
-  data_engineer_role_arn = try(one(data.aws_iam_roles.data_engineering_roles.arns))
-  data_bucket            = module.s3-create-a-derived-table-bucket.bucket
+  role_arn                = module.cmt_front_end_assumable_role.iam_role_arn
+  database_name           = "staged_fms_${local.env_}dbt"
+  data_engineer_role_arn  = try(one(data.aws_iam_roles.data_engineering_roles.arns))
+  data_bucket_lf_resource = aws_lakeformation_resource.data_bucket.arn
 }
