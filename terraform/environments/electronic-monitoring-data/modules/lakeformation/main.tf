dcbf095c2e263d0588a755f5bb7ce87d778e7bd5
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-30 13:19:43 +0000

    update module to allow for no data filter

--- terraform/environments/electronic-monitoring-data/modules/lakeformation/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/lakeformation/main.tf
@@ -63,10 +63,20 @@ resource "aws_lakeformation_permissions" "share_filtered_data_with_role" {
   for_each    = tomap(var.table_filters)
   principal   = var.role_arn
   permissions = ["DESCRIBE", "SELECT"]
-  data_cells_filter {
-    database_name    = var.database_name
-    table_name       = each.key
-    table_catalog_id = data.aws_caller_identity.current.account_id
-    name             = aws_lakeformation_data_cells_filter.data_filter[each.key].table_data[0].name
+  dynamic "data_cells_filter" {
+    for_each = each.value != "" ? [each.value] : []
+    content {
+      database_name    = var.database_name
+      table_name       = each.key
+      table_catalog_id = data.aws_caller_identity.current.account_id
+      name             = aws_lakeformation_data_cells_filter.data_filter[each.key].table_data[0].name
+    }
+  }
+  dynamic "table" {
+    for_each = each.value == "" ? [each.value] : []
+    content {
+      database_name = var.database_name
+      name          = each.key
+    }
   }
 }
