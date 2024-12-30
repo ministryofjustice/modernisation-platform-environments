6803876c308e1a50dd9f8dba6961107356e67be0
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-12-30 14:02:49 +0000

    add module

--- terraform/environments/electronic-monitoring-data/modules/analytical-platform-lakeformation/variables.tf
+++ terraform/environments/electronic-monitoring-data/modules/analytical-platform-lakeformation/variables.tf
@@ -13,11 +13,9 @@ variable "data_locations" {
 variable "databases_to_share" {
   description = "A list of databases to share"
   type = list(object({
-    name             = string
-    source_database  = string
-    source_table     = string
-    excluded_columns = list(string)
-    row_filter       = string
-    permissions      = list(string)
+    source_database = string
+    source_table    = string
+    row_filter      = string
+    permissions     = list(string)
   }))
 }
