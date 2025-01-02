39d11ad6cf2b3840eae2d740f4bd06c9e5328bd9
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-01-02 14:19:11 +0000

    update names and add AM tables infra

--- terraform/environments/electronic-monitoring-data/lake_formation_cloud_platform.tf
+++ terraform/environments/electronic-monitoring-data/lake_formation_cloud_platform.tf
@@ -1,6 +1,7 @@
 locals {
   env_          = "${local.environment_shorthand}_"
   cap_dw_tables = local.is-production ? ["contact_history", "equipment_details", "event_history", "incident", "order_details", "services", "suspension_of_visits", "violations", "visit_details"] : []
+  am_tables     = local.is-production ? ["am_contact_history", "am_equipment_details", "am_incident", "am_order_details", "am_services", "am_visit_details"] : []
 }
 
 resource "aws_lakeformation_resource" "data_bucket" {
