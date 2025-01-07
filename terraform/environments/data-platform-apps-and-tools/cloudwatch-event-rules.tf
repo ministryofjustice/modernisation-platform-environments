55cba8c5bb8cf6f8372335fa13dd3c1642daf3e4
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-01-07 16:13:15 +0000

    Remove JML Lambda and dependencies

--- terraform/environments/data-platform-apps-and-tools/cloudwatch-event-rules.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_cloudwatch_event_rule" "jml_lambda_trigger" {
-  count = terraform.workspace == "data-platform-apps-and-tools-production" ? 1 : 0
-
-  name                = "jml-lambda-trigger"
-  schedule_expression = "cron(0 2 1 * ? *)"
-}
