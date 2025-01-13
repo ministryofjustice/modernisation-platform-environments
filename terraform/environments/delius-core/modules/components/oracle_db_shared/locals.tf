fad61feb13157c9d1363615ca6c2a4d63ad12296
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2025-01-13 14:37:40 +0000

    TM-883 set up production env

--- terraform/environments/delius-core/modules/components/oracle_db_shared/locals.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/locals.tf
@@ -30,10 +30,10 @@ locals {
       # "target_account_id"  = var.platform_vars.environment_management.account_ids["delius-core-production"]
       # "target_environment" = "prod"
     },
-    #     "prod" = {
-    #       "source_account_id"  = var.platform_vars.environment_management.account_ids["delius-core-preproduction"]
-    #       "source_environment" = "preprod"
-    #     }
+    "prod" = {
+      #       "source_account_id"  = var.platform_vars.environment_management.account_ids["delius-core-preproduction"]
+      #       "source_environment" = "preprod"
+    }
   }
 
   oracle_duplicate_map = {
@@ -53,10 +53,10 @@ locals {
       # "target_account_id"  = var.platform_vars.environment_management.account_ids["delius-core-production"]
       # "target_environment" = "prod"
     },
-    #     "prod" = {
-    #       "source_account_id"  = var.platform_vars.environment_management.account_ids["delius-core-preproduction"]
-    #       "source_environment" = "preprod"
-    #     }
+    "prod" = {
+      #       "source_account_id"  = var.platform_vars.environment_management.account_ids["delius-core-preproduction"]
+      #       "source_environment" = "preprod"
+    }
   }
 
   oracle_backup_bucket_prefix = "${var.account_info.application_name}-${var.env_name}-oracle-${var.db_suffix}-backups"
