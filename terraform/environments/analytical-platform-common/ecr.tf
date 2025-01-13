f9b1ee03fbfbd786f0a5976e43e050e1fa714763
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-01-13 16:33:44 +0000

    :wrench: Tighten up permissions

--- terraform/environments/analytical-platform-common/ecr.tf
+++ terraform/environments/analytical-platform-common/ecr.tf
@@ -8,4 +8,23 @@ module "jml_ecr" {
    version = "2.3.0"
 
   repository_name = "analytical-platform-jml-report"
+
+  repository_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::593291632749:role/data_platform_jml_extract"
+        }
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+      }
+    ]
+  })
+
+  tags = local.tags
 }
