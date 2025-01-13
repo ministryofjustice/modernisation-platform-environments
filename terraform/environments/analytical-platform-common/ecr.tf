ccecd3ff837b2d5971bb4fb10912226fce0a47b9
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-01-13 14:20:39 +0000

    :wrench: ECR - first pass

---
+++ terraform/environments/analytical-platform-common/ecr.tf
@@ -0,0 +1,26 @@
+# This ECR is used to store the image built by in https://github.com/ministryofjustice/analytical-platform-jml-report/releases
+
+module "jml-ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "jml-report-ecr"
+
+  # repository_lambda_read_access_arns = []
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
