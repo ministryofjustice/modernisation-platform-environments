7366ef87f927a7e23dec5aca7b1b77486a0f1741
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-01-15 16:37:46 +0000

    🔧 Fix Cloud Trail Integration By Providing Manually Created ARN

--- terraform/environments/operations-engineering/audit_log_streaming_github.tf
+++ terraform/environments/operations-engineering/audit_log_streaming_github.tf
@@ -2,27 +2,6 @@ module "github-cloudtrail-auditlog" {
   source                          = "github.com/ministryofjustice/operations-engineering-cloudtrail-lake-github-audit-log-terraform-module?ref=main"
   create_github_auditlog_s3bucket = true
   github_auditlog_s3bucket        = "github-audit-log-landing"
-  cloudtrail_lake_channel_arn     = aws_cloudtrail_channel.github_channel.arn
+  cloudtrail_lake_channel_arn     = "arn:aws:cloudtrail:eu-west-2:211125434264:channel/810d471f-21e9-4552-b839-9e334f7fbe51"
   github_audit_allow_list         = ".*"
-}
-
-resource "aws_cloudtrail_event_data_store" "github_audit_logs" {
-  name                           = "github-audit-logs-store"
-  retention_period               = 90
-  termination_protection_enabled = true
-
-  advanced_event_selector {
-    name = "GitHubAuditLogs"
-    field_selector {
-      field  = "eventSource"
-      equals = ["GitHub"]
-    }
-  }
-}
-
-resource "aws_cloudtrail_channel" "github_channel" {
-  name                    = "github-audit-log-channel"
-  source                  = "Github"
-  destinations            = [aws_cloudtrail_event_data_store.github_audit_logs.arn]
-  advanced_event_selector = aws_cloudtrail_event_data_store.github_audit_logs.advanced_event_selector
 }
