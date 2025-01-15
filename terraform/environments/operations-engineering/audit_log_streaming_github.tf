0517da54b2f18dbbfd601ba4fa4ed6cf01c575f1
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-01-15 17:46:59 +0000

    📌 Pin Module Version

--- terraform/environments/operations-engineering/audit_log_streaming_github.tf
+++ terraform/environments/operations-engineering/audit_log_streaming_github.tf
@@ -10,7 +10,7 @@ data "external" "build_lambdas" {
 }
 
 module "github-cloudtrail-auditlog" {
-  source                          = "github.com/ministryofjustice/operations-engineering-cloudtrail-lake-github-audit-log-terraform-module?ref=main"
+  source                          = "github.com/ministryofjustice/operations-engineering-cloudtrail-lake-github-audit-log-terraform-module?ref=299e5774acd66d86909e8a77017ee420ff79028e" # v1.0.0
   create_github_auditlog_s3bucket = true
   github_auditlog_s3bucket        = "github-audit-log-landing"
   cloudtrail_lake_channel_arn     = "arn:aws:cloudtrail:eu-west-2:211125434264:channel/810d471f-21e9-4552-b839-9e334f7fbe51"
