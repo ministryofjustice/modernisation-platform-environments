7ba214565be37b98edef5e2cf8d6b5a6a332dede
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-01-15 17:16:49 +0000

    🔧 Build Lambda Functions on The Fly

--- terraform/environments/operations-engineering/audit_log_streaming_github.tf
+++ terraform/environments/operations-engineering/audit_log_streaming_github.tf
@@ -1,7 +1,21 @@
+data "external" "build_lambdas" {
+  program = [
+    "bash", "-c",
+    <<EOT
+      cd .terraform/modules/github-cloudtrail-auditlog &&
+      make all > /dev/null 2>&1 &&
+      echo '{"status": "success"}'
+    EOT
+  ]
+}
+
 module "github-cloudtrail-auditlog" {
   source                          = "github.com/ministryofjustice/operations-engineering-cloudtrail-lake-github-audit-log-terraform-module?ref=main"
   create_github_auditlog_s3bucket = true
   github_auditlog_s3bucket        = "github-audit-log-landing"
   cloudtrail_lake_channel_arn     = "arn:aws:cloudtrail:eu-west-2:211125434264:channel/810d471f-21e9-4552-b839-9e334f7fbe51"
   github_audit_allow_list         = ".*"
+
+  # Ensure the module waits for Lambdas to be built
+  depends_on = [data.external.build_lambdas]
 }
