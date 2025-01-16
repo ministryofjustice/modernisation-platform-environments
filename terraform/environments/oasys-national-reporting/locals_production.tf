c938da104afcd0e1fce90d684efd57e8d1259ed2
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-01-16 12:15:39 +0000

    destroy pd-onr-bods-1 as drives failed to be provisioned

--- terraform/environments/oasys-national-reporting/locals_production.tf
+++ terraform/environments/oasys-national-reporting/locals_production.tf
@@ -38,29 +38,29 @@ locals {
     }
 
     ec2_instances = {
-      pd-onr-bods-1 = merge(local.ec2_instances.bods, {
-        config = merge(local.ec2_instances.bods.config, {
-          ami_name          = "hmpps_windows_server_2019_release_2025-01-02T00-00-37.501Z"
-          availability_zone = "eu-west-2a"
-          user_data_raw = base64encode(templatefile(
-            "./templates/user-data-onr-bods-pwsh.yaml.tftpl", {
-              branch = "main"
-            }
-          ))
-          instance_profile_policies = concat(local.ec2_instances.bods.config.instance_profile_policies, [
-            "Ec2SecretPolicy",
-          ])
-        })
-        instance = merge(local.ec2_instances.bods.instance, {
-          instance_type           = "r6i.2xlarge"
-          disable_api_termination = true
-        })
-        tags = merge(local.ec2_instances.bods.tags, {
-          oasys-national-reporting-environment = "pd"
-          domain-name                          = "azure.hmpp.root"
-        })
-        cloudwatch_metric_alarms = null # <= REMOVE THIS LATER
-      })
+      # pd-onr-bods-1 = merge(local.ec2_instances.bods, {
+      #   config = merge(local.ec2_instances.bods.config, {
+      #     ami_name          = "hmpps_windows_server_2019_release_2025-01-02T00-00-37.501Z"
+      #     availability_zone = "eu-west-2a"
+      #     user_data_raw = base64encode(templatefile(
+      #       "./templates/user-data-onr-bods-pwsh.yaml.tftpl", {
+      #         branch = "main"
+      #       }
+      #     ))
+      #     instance_profile_policies = concat(local.ec2_instances.bods.config.instance_profile_policies, [
+      #       "Ec2SecretPolicy",
+      #     ])
+      #   })
+      #   instance = merge(local.ec2_instances.bods.instance, {
+      #     instance_type           = "r6i.2xlarge"
+      #     disable_api_termination = true
+      #   })
+      #   tags = merge(local.ec2_instances.bods.tags, {
+      #     oasys-national-reporting-environment = "pd"
+      #     domain-name                          = "azure.hmpp.root"
+      #   })
+      #   cloudwatch_metric_alarms = null # <= REMOVE THIS LATER
+      # })
     }
 
     fsx_windows = {
