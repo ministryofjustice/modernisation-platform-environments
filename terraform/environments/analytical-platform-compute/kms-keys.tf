778c67966dfee18348e1cf33f316a1a3214f622a
Marcus Aspin <maspin@unilink.com>
2025-01-17 10:21:02 +0000

    PI-2526 Remove SageMaker endpoint KMS key + bump instance type in dev… (#9381)
    PI-2526 Remove SageMaker endpoint KMS key + bump instance type in dev/preprod
    
    The instance type in dev/preprod was too slow to generate embeddings in any reasonable time for testing. Hopefully this helps, but if not we can drop it back down and test in production initially.
    
    Previously:
    * ml.t2.medium: 2 vCPU x 4GB - $0.062/hr
    Now:
    * ml.m6g.large: 2 vCPU x 8GB - $0.10656/hr

--- terraform/environments/analytical-platform-compute/kms-keys.tf
+++ terraform/environments/analytical-platform-compute/kms-keys.tf
@@ -465,19 +465,3 @@ module "actions_runner_cache_efs_kms" {
 
   tags = local.tags
 }
-
-module "sagemaker_kms" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
-
-  source  = "terraform-aws-modules/kms/aws"
-  version = "3.1.1"
-
-  aliases               = ["sagemaker"]
-  description           = "KMS key for SageMaker encryption-at-rest"
-  enable_default_policy = true
-
-  deletion_window_in_days = 7
-
-  tags = local.tags
-}
