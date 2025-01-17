b978bd2e967f70625d6bf0dbcc2113d4f2032086
Robert Sweetman <robertiansweetman@gmail.com>
2025-01-17 13:39:41 +0000

    :rocket: deploy pd-onr-bods-2 (#9384)

--- terraform/environments/oasys-national-reporting/locals_production.tf
+++ terraform/environments/oasys-national-reporting/locals_production.tf
@@ -56,6 +56,26 @@ locals {
         })
         cloudwatch_metric_alarms = null # <= REMOVE THIS LATER
       })
+
+      pd-onr-bods-2 = merge(local.ec2_instances.bods, {
+        config = merge(local.ec2_instances.bods.config, {
+          ami_name          = "hmpps_windows_server_2019_release_2025-01-02T00-00-37.501Z"
+          availability_zone = "eu-west-2b"
+          instance_profile_policies = concat(local.ec2_instances.bods.config.instance_profile_policies, [
+            "Ec2SecretPolicy",
+          ])
+        })
+        instance = merge(local.ec2_instances.bods.instance, {
+          instance_type           = "r6i.2xlarge"
+          disable_api_termination = true
+        })
+        tags = merge(local.ec2_instances.bods.tags, {
+          oasys-national-reporting-environment = "pd"
+          domain-name                          = "azure.hmpp.root"
+        })
+        cloudwatch_metric_alarms = null # <= REMOVE THIS LATER
+      })
+
     }
 
     fsx_windows = {
