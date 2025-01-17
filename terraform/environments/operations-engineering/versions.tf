26292c75a3e682dec63083d1e2b2837bfd09e863
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-01-17 19:21:50 +0000

    ✨ Add `awscc` Provider

--- terraform/environments/operations-engineering/versions.tf
+++ terraform/environments/operations-engineering/versions.tf
@@ -8,6 +8,20 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 1.0"
+    }
   }
   required_version = "~> 1.0"
 }
+
+provider "awscc" {
+  assume_role = {
+    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess",
+    # session_name = "SESSION_NAME"
+    # external_id  = "EXTERNAL_ID"
+  }
+}
+
+
