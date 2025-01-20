0d09b9be6ff618ab5f7c55d94351b60f8b02dd7e
Gary <26419401+Gary-H9@users.noreply.github.com>
2025-01-20 16:44:39 +0000

    Remove unnecessary code

--- terraform/environments/analytical-platform-common/ecr-repositories.tf
+++ terraform/environments/analytical-platform-common/ecr-repositories.tf
@@ -1,26 +1,3 @@
-data "aws_iam_policy_document" "analytical_platform_jml_report_ecr_repository" {
-  statement {
-    sid    = "LambdaECRImageRetrievalPolicy"
-    effect = "Allow"
-    actions = [
-      "ecr:BatchGetImage",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:SetRepositoryPolicy",
-      "ecr:DeleteRepositoryPolicy",
-      "ecr:GetRepositoryPolicy"
-    ]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-    condition {
-      test     = "StringLike"
-      variable = "aws:sourceArn"
-      values   = ["arn:aws:lambda:${data.aws_region.current.name}:${local.environment_management.account_ids["analytical-platform-data-production"]}:function:analytical-platform-jml-report"]
-    }
-  }
-}
-
 module "analytical_platform_jml_report_ecr_repository" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
