75d2e1ace6935cc7598f25c606edc1b1c47e5c39
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-01-20 17:35:44 +0000

    Fix zone_id (hopefully)
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/environments/analytical-platform-compute/acm.tf
+++ terraform/environments/analytical-platform-compute/acm.tf
@@ -2,7 +2,7 @@ module "mwaa_certificate" {
   source  = "terraform-aws-modules/acm/aws"
   version = "5.1.1"
 
-  zone_id     = module.route53_zones.zone_ids[local.environment_configuration.route53_zone]
+  zone_id     = module.route53_zones.route53_zone_zone_id[local.environment_configuration.route53_zone]
   domain_name = "airflow.${local.environment_configuration.route53_zone}"
 
   validation_method = "DNS"
