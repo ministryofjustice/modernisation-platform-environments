767064130cf68fc0fac5346d10db4ac4d63c7aac
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-01-23 14:34:35 +0000

    Remove NLB for Observer

--- terraform/environments/delius-core/modules/helpers/delius_microservice/load_balancing.tf
+++ terraform/environments/delius-core/modules/helpers/delius_microservice/load_balancing.tf
@@ -89,6 +89,7 @@ resource "aws_route53_record" "alb_r53_record" {
 # NLB for service interconnectivity
 
 resource "aws_lb" "delius_microservices" {
+  count                      = length(var.container_port_config) == 0 ? 0 : 1
   name                       = "${var.name}-${var.env_name}-service-nlb"
   internal                   = true
   load_balancer_type         = "network"
@@ -138,7 +139,7 @@ resource "aws_lb_target_group" "service" {
 resource "aws_lb_listener" "services" {
   for_each = toset([for _, v in var.container_port_config : tostring(v.containerPort)])
 
-  load_balancer_arn = aws_lb.delius_microservices.arn
+  load_balancer_arn = aws_lb.delius_microservices[0].arn
   port              = each.value
   protocol          = "TCP"
 
@@ -149,14 +150,15 @@ resource "aws_lb_listener" "services" {
 }
 
 resource "aws_route53_record" "services_nlb_r53_record" {
+  count    = length(var.container_port_config) == 0 ? 0 : 1
   provider = aws.core-vpc
   zone_id  = var.account_config.route53_inner_zone.zone_id
   name     = "${var.name}.service.${var.env_name}"
   type     = "A"
   alias {
     evaluate_target_health = false
-    name                   = aws_lb.delius_microservices.dns_name
-    zone_id                = aws_lb.delius_microservices.zone_id
+    name                   = aws_lb.delius_microservices[0].dns_name
+    zone_id                = aws_lb.delius_microservices[0].zone_id
   }
 }
 
