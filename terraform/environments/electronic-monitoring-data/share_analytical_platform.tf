21bf3d0c481729f50796ea4c4070fb9ee80e789c
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-01-24 16:22:46 +0000

    Enhance database grant logic to include live feed databases and suffix handling

--- terraform/environments/electronic-monitoring-data/share_analytical_platform.tf
+++ terraform/environments/electronic-monitoring-data/share_analytical_platform.tf
@@ -4,11 +4,14 @@ locals {
   dbt_k8s_secrets_placeholder = {
     oidc_cluster_identifier = "placeholder2"
   }
-  admin_roles       = local.is-development ? "sandbox" : "data-eng"
-  suffix            = local.is-production ? "" : "-test"
-  prod_dbs_to_grant = local.is-production ? ["am_stg", "cap_dw_stg", "emd_historic_int", "historic_api_mart", "historic_api_mart_mock"] : []
-  dev_dbs_to_grant  = local.is-production ? [for db in local.prod_dbs_to_grant : "${db}_historic_dev_dbt"] : []
-  dbs_to_grant      = toset(flatten([local.prod_dbs_to_grant, local.dev_dbs_to_grant]))
+  dbt_suffix             = local.is-production ? "" : "${local.environment_shorthand}_dbt"
+  admin_roles            = local.is-development ? "sandbox" : "data-eng"
+  suffix                 = local.is-production ? "" : "-test"
+  live_feed_dbs          = ["serco_fms", "allied_mdss", "staged_fms", "preprocessed_fms"]
+  prod_dbs_to_grant      = local.is-production ? ["am_stg", "cap_dw_stg", "emd_historic_int", "historic_api_mart", "historic_api_mart_mock"] : []
+  dev_dbs_to_grant       = local.is-production ? [for db in local.prod_dbs_to_grant : "${db}_historic_dev_dbt"] : []
+  live_feed_dbs_to_grant = [for db in local.live_feed_dbs : "${db}${local.dbt_suffix}"]
+  dbs_to_grant           = toset(flatten([local.prod_dbs_to_grant, local.dev_dbs_to_grant, local.live_feed_dbs_to_grant]))
 }
 
 # Source Analytics DBT Secrets
