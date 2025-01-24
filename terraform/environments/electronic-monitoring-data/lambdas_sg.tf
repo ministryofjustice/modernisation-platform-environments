9176b0f7cdf7c1d3e070eff8fc27be48497c5878
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-01-24 14:55:12 +0000

    refactor: remove unused VPC endpoint data sources and security group rules for Secrets Manager and Execute API

--- terraform/environments/electronic-monitoring-data/lambdas_sg.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_sg.tf
@@ -19,23 +19,6 @@ data "aws_vpc_endpoint" "s3" {
   }
 }
 
-data "aws_vpc_endpoint" "secrets_manager" {
-  provider     = aws.core-vpc
-  service_name = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
-  vpc_id       = data.aws_vpc.shared.id
-  tags = {
-    Name = "${var.networking[0].business-unit}-${local.environment}-com.amazonaws.${data.aws_region.current.name}.secretsmanager"
-  }
-}
-
-data "aws_vpc_endpoint" "execute_api" {
-  provider     = aws.core-vpc
-  service_name = "com.amazonaws.${data.aws_region.current.name}.execute-api"
-  vpc_id       = data.aws_vpc.shared.id
-  tags = {
-    Name = "${var.networking[0].business-unit}-${local.environment}-com.amazonaws.${data.aws_region.current.name}.execute-api"
-  }
-}
 data "aws_prefix_list" "s3" {
   name = "com.amazonaws.${data.aws_region.current.name}.s3"
 }
@@ -78,24 +61,4 @@ resource "aws_security_group_rule" "lambda_egress_s3" {
   protocol          = "tcp"
   prefix_list_ids   = [data.aws_prefix_list.s3.id]
   security_group_id = aws_security_group.lambda_generic.id
-}
-
-resource "aws_security_group_rule" "lambda_egress_secrets_manager" {
-  type              = "egress"
-  description       = "allow secrets manager"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.lambda_generic.id
-  prefix_list_ids   = [data.aws_vpc_endpoint.secrets_manager.service_name]
-}
-
-resource "aws_security_group_rule" "lambda_egress_iam" {
-  type              = "egress"
-  description       = "allow iam"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.lambda_generic.id
-  prefix_list_ids   = [data.aws_vpc_endpoint.execute_api.service_name]
 }
