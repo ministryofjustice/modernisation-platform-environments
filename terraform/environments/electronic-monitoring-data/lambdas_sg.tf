a8dd4b0ac2f40d5cac1040063a45619eca97ad5a
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-01-24 14:24:49 +0000

    refactor: convert VPC endpoint resources to data sources for Secrets Manager and Execute API

--- terraform/environments/electronic-monitoring-data/lambdas_sg.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_sg.tf
@@ -19,24 +19,23 @@ data "aws_vpc_endpoint" "s3" {
   }
 }
 
-resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id              = data.aws_vpc.shared.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = data.aws_subnets.shared-public.ids
-  security_group_ids  = [aws_security_group.lambda_generic.id]
-  private_dns_enabled = true
+data "aws_vpc_endpoint" "secrets_manager" {
+  provider     = aws.core-vpc
+  service_name = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
+  vpc_id       = data.aws_vpc.shared.id
+  tags = {
+    Name = "${var.networking[0].business-unit}-${local.environment}-com.amazonaws.${data.aws_region.current.name}.secretsmanager"
+  }
 }
 
-resource "aws_vpc_endpoint" "iam" {
-  vpc_id              = data.aws_vpc.shared.id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.iam"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = data.aws_subnets.shared-public.ids
-  security_group_ids  = [aws_security_group.lambda_generic.id]
-  private_dns_enabled = true
+data "aws_vpc_endpoint" "execute_api" {
+  provider     = aws.core-vpc
+  service_name = "com.amazonaws.${data.aws_region.current.name}.execute-api"
+  vpc_id       = data.aws_vpc.shared.id
+  tags = {
+    Name = "${var.networking[0].business-unit}-${local.environment}-com.amazonaws.${data.aws_region.current.name}.execute-api"
+  }
 }
-
 data "aws_prefix_list" "s3" {
   name = "com.amazonaws.${data.aws_region.current.name}.s3"
 }
@@ -88,7 +87,7 @@ resource "aws_security_group_rule" "lambda_egress_secrets_manager" {
   to_port           = 443
   protocol          = "tcp"
   security_group_id = aws_security_group.lambda_generic.id
-  prefix_list_ids   = [aws_vpc_endpoint.secretsmanager.prefix_list_id]
+  prefix_list_ids   = [data.aws_vpc_endpoint.secrets_manager.id]
 }
 
 resource "aws_security_group_rule" "lambda_egress_iam" {
@@ -98,5 +97,5 @@ resource "aws_security_group_rule" "lambda_egress_iam" {
   to_port           = 443
   protocol          = "tcp"
   security_group_id = aws_security_group.lambda_generic.id
-  prefix_list_ids   = [aws_vpc_endpoint.iam.prefix_list_id]
+  prefix_list_ids   = [data.aws_vpc_endpoint.execute_api.id]
 }
