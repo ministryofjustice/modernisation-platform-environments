00776d910837ac4148752fbe8876870e65a3bf96
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-01-27 12:26:56 +0000

    Update local settings
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-compute/src/airflow/local-settings/test/airflow_local_settings.py
+++ terraform/environments/analytical-platform-compute/src/airflow/local-settings/test/airflow_local_settings.py
@@ -1,8 +1,5 @@
 from airflow.www.utils import UIAlert
 
-# This is desribed;
-# here https://airflow.apache.org/docs/apache-airflow/2.4.3/howto/customize-ui.html#add-custom-alert-messages-on-the-dashboard
-# and here https://github.com/apache/airflow/blob/main/airflow/www/utils.py#L889
 DASHBOARD_UIALERTS = [
     UIAlert(
         'Analytical Platform Airflow Service',
