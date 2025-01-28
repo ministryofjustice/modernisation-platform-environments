10cb1e2b6eb43448a73a362fb129b9bb0b0a5099
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-01-28 13:17:17 +0000

    Add imports for preprocessed and staged FMS test DBT databases

---
+++ terraform/environments/electronic-monitoring-data/imports.tf
@@ -0,0 +1,9 @@
+import {
+  to = "module.share_dbs_with_roles[0].aws_glue_catalog_database.cadt_databases[\"preprocessed_fms_test_dbt\"]"
+  id = "preprocessed_fms_test_dbt"
+}
+
+import {
+  to = "module.share_dbs_with_roles[0].aws_glue_catalog_database.cadt_databases[\"staged_fms_test_dbt\"]"
+  id = "staged_fms_test_dbt"
+}
