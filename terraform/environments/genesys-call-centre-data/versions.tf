9efc318c24af59008c250d4a00a61d90ae4e3c47
ebubeabara-moj <ebubechukwu.obara@justice.gov.uk>
2025-01-30 10:16:11 +0000

    adding the terraform IaC for genesys staging bucket

--- terraform/environments/genesys-call-centre-data/versions.tf
+++ terraform/environments/genesys-call-centre-data/versions.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_providers {
-    aws = {
-      version               = "~> 5.0"
-      source                = "hashicorp/aws"
-      configuration_aliases = [aws.bucket-replication]
-    }
-    http = {
-      version = "~> 3.0"
-      source  = "hashicorp/http"
-    }
-  }
-  required_version = "~> 1.0"
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       version               = "~> 5.0"
+#       source                = "hashicorp/aws"
+#       configuration_aliases = [aws.bucket-replication]
+#     }
+#     http = {
+#       version = "~> 3.0"
+#       source  = "hashicorp/http"
+#     }
+#   }
+#   required_version = "~> 1.0"
+# }
