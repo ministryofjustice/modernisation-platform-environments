835c02d5cf7674e4ca91573ac722582f31aac25f
David Teeling <david.teeling1@justice.gov.uk>
2025-01-30 14:32:41 +0000

    Deploy to dev

---
+++ terraform/environments/delius-core/modules/components/oracle_db_instance/cloudwatch.tf
@@ -0,0 +1,43 @@
+locals {
+  instance_name = "${var.account_info.application_name}-${var.env_name}-${var.db_suffix}-${local.instance_name_index}"
+  alarm_name    = "${local.instance_name}-instance-status-check-failed"
+}
+
+resource "aws_cloudwatch_log_group" "ec2_status_check_log_group" {
+  name              = "/metrics/${var.env_name}/${local.alarm_name}"
+  retention_in_days = 0 # Retain indefinitely
+}
+
+resource "aws_cloudwatch_event_rule" "ec2_status_check_failed_event" {
+  name        = local.alarm_name
+  description = "Rule to capture EC2 instance status check failures"
+  event_pattern = jsonencode({
+    "source" : ["aws.cloudwatch"],
+    "detail-type" : ["CloudWatch Alarm State Change"],
+    "detail" : {
+      "state" : ["ALARM"],
+      "alarmName" : ["${local.alarm_name}"]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "ec2_status_check_failed_target" {
+  rule      = aws_cloudwatch_event_rule.ec2_status_check_failed_event.name
+  arn       = aws_cloudwatch_log_group.ec2_status_check_log_group.arn
+  target_id = local.alarm_name
+}
+
+resource "aws_cloudwatch_log_resource_policy" "log_group_policy" {
+  policy_name = local.alarm_name
+  policy_document = jsonencode({
+    Version = "2012-10-17",
+    Statement: [{
+      Action    = "logs:PutLogEvents",
+      Effect    = "Allow",
+      Principal = {
+        Service = "events.amazonaws.com"
+      },
+      Resource = aws_cloudwatch_log_group.ec2_status_check_log_group.arn
+    }]
+  })
+}
---
+++ terraform/environments/delius-core/modules/delius_environment/cloudwatch.tf
@@ -0,0 +1,67 @@
+# IAM Role for EventBridge to assume and write logs to the Log Groups
+resource "aws_iam_role" "eventbridge_to_logs_role" {
+  name = "${var.env_name}-eventbridge-to-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement: [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "events.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "eventbridge_logs_policy" {
+  name        = "${var.env_name}-eventbridge-to-logs-policy"
+  description = "Policy to allow EventBridge to write logs to CloudWatch Log Groups"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement: [{
+      Action   = "logs:PutLogEvents",
+      Effect   = "Allow",
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_logs_policy_attachment" {
+  policy_arn = aws_iam_policy.eventbridge_logs_policy.arn
+  role       = aws_iam_role.eventbridge_to_logs_role.name
+}
+
+data "aws_iam_policy_document" "route53-query-logging-policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:log-group:/aws/route53/*"]
+
+    principals {
+      identifiers = ["route53.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_resource_policy" "log_group_policy" {
+  policy_name = "${var.env_name}-eventbridge-to-logs-policy"
+  policy_document = jsonencode({
+    Version = "2012-10-17",
+    Statement: [{
+        Actions = [
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+        ],
+        Effect    = "Allow",
+        Principal = {
+            Service = "events.amazonaws.com"
+        },
+        Resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:log-group:/metrics/${var.env_name}/*"]
+    }]
+  })
+}
