daf0b51c710fb882bde7b4d504b438e0386d04d5
Seb Norris <sebastian.j.norris@gmail.com>
2025-01-31 15:03:26 +0000

    testing deploy of acm pca cert

--- terraform/environments/delius-core/modules/components/oracle_db_instance/cert.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_instance/cert.tf
@@ -4,22 +4,35 @@ data "aws_ram_resource_share" "shared_ca" {
   resource_owner = "OTHER-ACCOUNTS"
 }
 
+output "shared_ram_shares" {
+  value = data.aws_ram_resource_share.shared_ca
+}
+
+
 data "aws_acmpca_certificate_authority" "shared_ca" {
-  arn = data.aws_ram_resource_share.shared_ca.resources[0]
+  arn = data.aws_ram_resource_share.shared_ca.resource_arns[0]
+}
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+}
+
+
+resource "tls_cert_request" "csr" {
+  private_key_pem = tls_private_key.key.private_key_pem
+
+  subject {
+    common_name = "example"
+  }
 }
 
 resource "aws_acmpca_certificate" "oracle_cert" {
   certificate_authority_arn = data.aws_acmpca_certificate_authority.shared_ca.arn
-
-  csr = filebase64("${path.module}/oracle-csr.pem") # Path to your CSR file
-
-  signing_algorithm = "SHA256WITHRSA"
+  certificate_signing_request = tls_cert_request.csr.cert_request_pem
+  signing_algorithm           = "SHA256WITHRSA"
   validity {
-    type  = "DAYS"
-    value = 365
-  }
-
-  tags = {
-    Name = "OracleCertificate"
+    type  = "YEARS"
+    value = 1
   }
 }
+
