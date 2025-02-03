ab524fc50b7493bc40d82b96fae157516abfa668
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2025-02-03 16:28:48 +0000

    Add datahub assume role and bucket policy

---
+++ terraform/environments/digital-prison-reporting/share_data_catalogue_iam.tf
@@ -0,0 +1,63 @@
+
+data "aws_iam_policy_document" "datahub_read_cadet_bucket" {
+  statement {
+    sid    = "datahubReadCaDeTBucket"
+    effect = "Allow"
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:Describe*"
+    ]
+    resources = [
+      module.s3_structured_historical_bucket.bucket.arn
+    ]
+    condition {
+      test = "StringLike"
+      variable = "s3:prefix"
+      values = [
+        "data/${local.environment}/run_artefacts/*"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "datahub_read_cadet_bucket" {
+  name   = "datahub_read_CaDeT_bucket"
+  policy = data.aws_iam_policy_document.datahub_read_cadet_bucket.json
+}
+
+# Allow Github actions to assume a role via OIDC.
+# So that scheduled jobs in the data-catalogue repo can access the CaDeT bucket.
+data "aws_iam_policy_document" "datahub_ingestion_github_actions" {
+  # checkov:skip=CKV_AWS_358: Bug in the linter
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${local.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      values   = ["sts.amazonaws.com"]
+      variable = "token.actions.githubusercontent.com:aud"
+    }
+    condition {
+      test     = "StringLike"
+      values   = ["repo:ministryofjustice/data-catalogue:*"]
+      variable = "token.actions.githubusercontent.com:sub"
+    }
+  }
+}
+
+resource "aws_iam_role" "datahub_ingestion_github_actions" {
+  name                 = "datahub-ingestion-github-actions"
+  assume_role_policy   = data.aws_iam_policy_document.datahub_ingestion_github_actions.json
+  max_session_duration = 14400
+}
+
+resource "aws_iam_role_policy_attachment" "datahub_ingestion_github_actions" {
+  policy_arn = aws_iam_policy.datahub_read_cadet_bucket.arn
+  role       = aws_iam_role.datahub_ingestion_github_actions.name
+}
