4bfe601a887724336a4cb5c367fe8ce3ffab17c3
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-02-03 12:16:03 +0000

    Add ClusterRole and ClusterRoleBinding Update EKS access with new group
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ terraform/environments/analytical-platform-compute/kubernetes-cluster-roles.tf
@@ -0,0 +1,11 @@
+resource "kubernetes_cluster_role" "mwaa_external_secrets" {
+  metadata {
+    name = "mwaa-external-secrets"
+  }
+
+  rule {
+    api_groups = ["apiextensions.k8s.io"]
+    resources  = ["customresourcedefinitions"]
+    verbs      = ["list"]
+  }
+}
