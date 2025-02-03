8f04474f422dd23a75b43398dd5a91fa06b986c2
Seb Norris <sebastian.j.norris@gmail.com>
2025-02-03 15:18:52 +0000

    test different approach

---
+++ terraform/environments/delius-core/modules/delius_environment/cert.tf
@@ -0,0 +1,18 @@
+data "aws_ram_resource_share" "shared_ca" {
+  name = var.shared_ca_name
+  resource_owner = "OTHER-ACCOUNTS"
+}
+
+data "aws_acmpca_certificate_authority" "shared_ca" {
+  arn = data.aws_ram_resource_share.shared_ca.resource_arns[0]
+}
+
+resource "aws_acm_certificate" "cert" {
+  certificate_authority_arn = data.aws_acmpca_certificate_authority.shared_ca.arn
+  domain_name       = "delius-core-dev-db-1.delius-core.hmpps-development.modernisation-platform.service.justice.gov.uk"
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
