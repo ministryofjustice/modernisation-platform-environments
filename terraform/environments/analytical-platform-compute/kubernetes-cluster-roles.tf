b7a772651e5562c04acd89d2fd82aa4143c3ab58
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-02-03 12:52:52 +0000

    Add new role Add new role binding
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-compute/kubernetes-cluster-roles.tf
+++ terraform/environments/analytical-platform-compute/kubernetes-cluster-roles.tf
@@ -2,7 +2,6 @@ resource "kubernetes_cluster_role" "mwaa_external_secrets" {
   metadata {
     name = "mwaa-external-secrets"
   }
-
   rule {
     api_groups = ["apiextensions.k8s.io"]
     resources  = ["customresourcedefinitions"]
