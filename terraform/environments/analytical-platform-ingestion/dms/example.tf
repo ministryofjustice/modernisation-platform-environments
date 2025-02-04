ab0b1f897c71be9d80334f472d9cf4e7d0f4f226
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-02-04 14:43:16 +0000

    :wrench: Example S3 bucket

---
+++ terraform/environments/analytical-platform-ingestion/dms/example.tf
@@ -0,0 +1,10 @@
+module "example_bucket" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.3.0"
+
+  bucket = "example-bucket-123123123"
+
+  force_destroy = true
+}
