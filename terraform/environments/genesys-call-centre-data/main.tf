82fe6970a1e57adada97a5c6ed71a6d95529d402
ebubeabara-moj <ebubechukwu.obara@justice.gov.uk>
2025-02-04 10:58:37 +0000

    adding the terraform IaC for genesys staging bucket

--- terraform/environments/genesys-call-centre-data/main.tf
+++ terraform/environments/genesys-call-centre-data/main.tf
@@ -132,7 +132,7 @@ resource "aws_s3_bucket_logging" "default" {
 # AWS S3 bucket Public Access Block (Call Centre Staging)
 resource "aws_s3_bucket_public_access_block" "default" {
   bucket                  = aws_s3_bucket.default.id
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
@@ -141,7 +141,7 @@ resource "aws_s3_bucket_public_access_block" "default" {
 resource "aws_s3_bucket_public_access_block" "replication" {
   count                   = var.replication_enabled ? 1 : 0
   bucket                  = aws_s3_bucket.replication[count.index].id
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
