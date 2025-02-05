edcf4fc833be265c8aeb0bef4c8ea408f7d5ed16
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2025-02-05 12:44:45 +0000

    TM-943 test ldap config

--- terraform/environments/delius-core/locals_production.tf
+++ terraform/environments/delius-core/locals_production.tf
@@ -106,7 +106,7 @@ locals {
     }
 
     ldap = {
-      image_tag        = "6.0.3-latest"
+      image_tag        = "6.1.4-13156639844-1"
       container_port   = 389
       slapd_log_level  = "conns,config,stats,stats2"
       container_cpu    = 16384
