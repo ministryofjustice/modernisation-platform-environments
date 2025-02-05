6c86481353b25a3bb92471504030386d4d3e59d3
Vladimirs Kovalovs <vladimirs.kovalovs@digital.justice.gov.uk>
2025-02-05 14:31:49 +0000

    [TM-915] added some code for ec2 app instance

--- terraform/environments/laa-stabilisation-cdc-poc/cwa-poc2/app_servers.tf
+++ terraform/environments/laa-stabilisation-cdc-poc/cwa-poc2/app_servers.tf
@@ -185,7 +185,7 @@ resource "aws_instance" "app1" {
   monitoring                  = true
   vpc_security_group_ids      = [aws_security_group.cwa_poc2_app.id]
   subnet_id                   = data.aws_subnet.private_subnets_a.id
-  iam_instance_profile        = aws_iam_instance_profile.cwa.id
+  iam_instance_profile        = aws_iam_instance_profile.cwa_poc2.id
   key_name                    = aws_key_pair.cwa.key_name
   user_data_base64            = base64encode(local.app_userdata)
   user_data_replace_on_change = true
@@ -214,13 +214,13 @@ resource "aws_instance" "app1" {
 
 resource "aws_instance" "app2" {
   count                       = contains(["development2", "testing"], local.environment) ? 0 : 1
-  ami                         = local.application_data.accounts[local.environment].app_ami_id
+  ami                         = local.application_data.accounts[local.environment].cwa_poc2_app_ami_id
   availability_zone           = "eu-west-2a"
-  instance_type               = local.application_data.accounts[local.environment].app_instance_type
+  instance_type               = local.application_data.accounts[local.environment].cwa_poc2_app_instance_type
   monitoring                  = true
-  vpc_security_group_ids      = [aws_security_group.app.id]
+  vpc_security_group_ids      = [aws_security_group.cwa_poc2_app.id]
   subnet_id                   = data.aws_subnet.private_subnets_a.id
-  iam_instance_profile        = aws_iam_instance_profile.cwa.id
+  iam_instance_profile        = aws_iam_instance_profile.cwa_poc2.id
   key_name                    = aws_key_pair.cwa.key_name
   user_data_base64            = base64encode(local.app_userdata)
   user_data_replace_on_change = false
@@ -270,7 +270,7 @@ resource "aws_vpc_security_group_egress_rule" "app_outbound" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_bastion_ssh" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "SSH from the Bastion"
   referenced_security_group_id = module.bastion_linux.bastion_security_group
   from_port                    = 22
@@ -279,43 +279,43 @@ resource "aws_vpc_security_group_ingress_rule" "app_bastion_ssh" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_self_1" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from itself"
-  referenced_security_group_id = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.cwa_poc2_app.id
   from_port                    = 8050
   ip_protocol                  = "tcp"
   to_port                      = 8050
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_self_2" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from itself"
-  referenced_security_group_id = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.cwa_poc2_app.id
   from_port                    = 9050
   ip_protocol                  = "tcp"
   to_port                      = 9050
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_self_3" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from itself"
-  referenced_security_group_id = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.cwa_poc2_app.id
   from_port                    = 8250
   ip_protocol                  = "tcp"
   to_port                      = 8250
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_self_4" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from itself"
-  referenced_security_group_id = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.cwa_poc2_app.id
   from_port                    = 1676
   ip_protocol                  = "tcp"
   to_port                      = 1676
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_cm_1" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from Concurrent Manager"
   referenced_security_group_id = aws_security_group.concurrent_manager.id
   from_port                    = 9050
@@ -324,7 +324,7 @@ resource "aws_vpc_security_group_ingress_rule" "app_cm_1" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_cm_2" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from Concurrent Manager"
   referenced_security_group_id = aws_security_group.concurrent_manager.id
   from_port                    = 8250
@@ -333,7 +333,7 @@ resource "aws_vpc_security_group_ingress_rule" "app_cm_2" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_cm_3" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from Concurrent Manager"
   referenced_security_group_id = aws_security_group.concurrent_manager.id
   from_port                    = 1676
@@ -342,7 +342,7 @@ resource "aws_vpc_security_group_ingress_rule" "app_cm_3" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_cm_4" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from Concurrent Manager"
   referenced_security_group_id = aws_security_group.concurrent_manager.id
   from_port                    = 8050
@@ -351,7 +351,7 @@ resource "aws_vpc_security_group_ingress_rule" "app_cm_4" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_db_1" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from Database server"
   referenced_security_group_id = aws_security_group.database.id
   from_port                    = 8250
@@ -360,7 +360,7 @@ resource "aws_vpc_security_group_ingress_rule" "app_db_1" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_db_2" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from Database server"
   referenced_security_group_id = aws_security_group.database.id
   from_port                    = 9050
@@ -369,7 +369,7 @@ resource "aws_vpc_security_group_ingress_rule" "app_db_2" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "app_alb" {
-  security_group_id            = aws_security_group.app.id
+  security_group_id            = aws_security_group.cwa_poc2_app.id
   description                  = "Access from CWA ALB"
   referenced_security_group_id = aws_security_group.external_lb.id
   from_port                    = 8050
@@ -385,11 +385,11 @@ resource "aws_vpc_security_group_ingress_rule" "app_alb" {
 
 resource "aws_ebs_volume" "app1" {
   availability_zone = "eu-west-2a"
-  size              = local.application_data.accounts[local.environment].ebs_app_size
+  size              = local.application_data.accounts[local.environment].cwa_poc2_ebs_app_size
   type              = "gp2"
   encrypted         = true
   kms_key_id        = data.aws_kms_key.ebs_shared.key_id
-  snapshot_id       = local.application_data.accounts[local.environment].app_snapshot_id # This is used for when data is being migrated
+  snapshot_id       = local.application_data.accounts[local.environment].cwa_poc2_app_snapshot_id # This is used for when data is being migrated
 
   lifecycle {
     replace_triggered_by = [
