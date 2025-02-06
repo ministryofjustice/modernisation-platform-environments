153b9645ee6f58a6a9608ad7f02a906b9b1db09a
Omar Francis <omar.francis@gmail.com>
2025-02-06 11:09:33 +0000

    updating dms_vpc_role

--- terraform/environments/analytical-platform-ingestion/dms/iam-roles.tf
+++ terraform/environments/analytical-platform-ingestion/dms/iam-roles.tf
@@ -1,15 +1,17 @@
 # IAM Role for DMS VPC Access
 resource "aws_iam_role" "dms_vpc_role" {
+  name        = "dms-vpc-role"
+  description = "Allows DMS to manage VPC"
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "dms.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
+        Effect = "Allow"
+        Principal = {
+          Service = "dms.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
     ]
   })
 }
