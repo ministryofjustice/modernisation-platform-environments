9d18629025a66d44eadb52b6af99334e8771f5f4
ebubeabara-moj <ebubechukwu.obara@justice.gov.uk>
2025-02-06 15:50:47 +0000

    adding terraform for s3 buckets

---
+++ terraform/environments/genesys-call-centre-data/modules/s3/main.tf
@@ -0,0 +1,254 @@
+#trivy:ignore:AVD-AWS-0102
+# module "s3_staging" {
+#   source = "../.."
+#   providers = {
+#     aws.bucket-replication = aws.bucket-replication
+#   }
+# }
+
+
+# AWS S3 Bucket
+resource "aws_s3_bucket" "default" {
+  #checkov:skip=CKV_AWS_144: "Replication handled in replication configuration resource"
+  #checkov:skip=CKV_AWS_18: "Logging handled in logging configuration resource"
+  #checkov:skip=CKV_AWS_21: "Versioning handled in Versioning configuration resource"
+  #checkov:skip=CKV_AWS_145: "Encryption handled in encryption configuration resource"
+  #checkov:skip=CKV2_AWS_61:
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket" "replication" {
+  #checkov:skip=CKV_AWS_144: "Replication not required on replication bucket"
+  #checkov:skip=CKV_AWS_18: "Logging handled in logging configuration resource"
+  #checkov:skip=CKV_AWS_21: "Versioning handled in versioning configuration resource"
+  #checkov:skip=CKV_AWS_145: "Encryption handled in encryption configuration resource"
+  count         = var.replication_enabled ? 1 : 0
+  provider      = aws.bucket-replication
+  bucket        = var.bucket_name != null ? "${var.bucket_name}-replication" : null
+  bucket_prefix = var.bucket_prefix != null ? "${var.bucket_prefix}-replication" : null
+  force_destroy = var.force_destroy
+}
+
+# Event Notifications for S3 buckets
+resource "aws_s3_bucket_notification" "default" {
+  count  = var.replication_enabled && var.notification_enabled ? 1 : 0
+  bucket = aws_s3_bucket.default.id
+
+  topic {
+    topic_arn = var.notification_sns_arn
+    events    = var.notification_events
+  }
+}
+
+resource "aws_s3_bucket_notification" "replication" {
+  count  = var.replication_enabled ? 1 : 0
+  bucket = aws_s3_bucket.replication[count.index].id
+
+  topic {
+    topic_arn = var.notification_sns_arn
+    events    = var.notification_events
+  }
+}
+
+# tfsec:ignore:aws-s3-encryption-customer-key
+#tfsec:ignore:avd-aws-0132 S3 encryption should use Custom Managed Keys, KMS is acceptable compromise 
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  #checkov:skip=CKV2_AWS_67: "Ensure AWS S3 bucket encrypted with Customer Managed Key (CMK) has regular rotation"
+  bucket = aws_s3_bucket.default.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = var.sse_algorithm
+      kms_master_key_id = (var.custom_kms_key != "") ? var.custom_kms_key : ""
+    }
+  }
+}
+
+# Enable Versioning on S3 Buckets
+resource "aws_s3_bucket_versioning" "default" {
+  bucket = aws_s3_bucket.default.id
+  versioning_configuration {
+    status = var.versioning_enabled
+  }
+}
+
+# Configure bucket lifecycle rules
+resource "aws_s3_bucket_lifecycle_configuration" "replication" {
+  #checkov:skip=CKV_AWS_300: "Ensure S3 lifecycle configuration sets period for aborting failed uploads"
+  count    = var.replication_enabled ? 1 : 0
+  provider = aws.bucket-replication
+  bucket   = aws_s3_bucket.replication[count.index].id
+  rule {
+    id     = "main"
+    status = "Enabled"
+
+    transition {
+      days          = 90
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 365
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 730
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 90
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 365
+      storage_class   = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 730
+    }
+  }
+}
+
+# AWS S3 Bucket Logging
+resource "aws_s3_bucket_logging" "default" {
+  count = var.log_buckets != null ? 1 : 0
+
+  bucket        = aws_s3_bucket.default.id
+  target_bucket = var.log_bucket_name
+  target_prefix = var.log_prefix
+
+  dynamic "target_object_key_format" {
+    for_each = (var.log_partition_date_source != "None") ? [1] : []
+    content {
+      partitioned_prefix {
+        partition_date_source = var.log_partition_date_source
+      }
+    }
+  }
+}
+
+# AWS S3 bucket Public Access Block (Call Centre Staging)
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket                  = aws_s3_bucket.default.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "replication" {
+  count                   = var.replication_enabled ? 1 : 0
+  bucket                  = aws_s3_bucket.replication[count.index].id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# S3 bucket replication: role
+resource "aws_iam_role" "replication" {
+  provider           = aws.bucket-replication
+  count              = var.replication_enabled ? 1 : 0
+  name               = "AWSS3BucketReplication${var.suffix_name}"
+  assume_role_policy = data.aws_iam_policy_document.s3-assume-role-policy.json
+  # tags               = var.tags
+}
+
+# S3 bucket replication: assume role policy
+data "aws_iam_policy_document" "s3-assume-role-policy" {
+  version = var.json_encode_decode_version
+  statement {
+    effect  = var.moj_aws_iam_policy_document_statement_effect
+    actions = var.moj_aws_iam_policy_document_statement_actions
+
+    principals {
+      type        = var.moj_aws_iam_policy_document_principals_type
+      identifiers = var.moj_aws_iam_policy_document_principals_identifiers
+    }
+  }
+}
+
+# AWS S3 Bucket cross-region replication
+resource "aws_s3_bucket_replication_configuration" "default" {
+  for_each = var.replication_enabled ? toset(["run"]) : []
+  bucket   = aws_s3_bucket.default.id
+  role     = aws_iam_role.replication[0].arn
+  rule {
+    id       = var.moj_aws_s3_bucket_replication_configuration_rule_id
+    status   = var.replication_enabled ? "Enabled" : "Disabled"
+    priority = 0
+
+    destination {
+      # bucket        = var.replication_enabled ? aws_s3_bucket.replication[0].arn : aws_s3_bucket.replication[0].arn
+      bucket        = aws_s3_bucket.default.arn
+      storage_class = var.moj_aws_s3_bucket_replication_configuration_rule_destination_storage_class
+      encryption_configuration {
+        replica_kms_key_id = (var.custom_replication_kms_key != "") ? var.custom_replication_kms_key : "arn:aws:kms:${var.replication_region}:${data.aws_caller_identity.current.account_id}:alias/aws/s3"
+      }
+    }
+
+    source_selection_criteria {
+      sse_kms_encrypted_objects {
+        status = (var.replication_enabled != false) ? "Enabled" : "Disabled"
+      }
+    }
+  }
+  depends_on = [
+    aws_s3_bucket_versioning.default
+  ]
+}
+
+# AWS S3 Bucket Policy (Call Centre Staging)
+resource "aws_s3_bucket_policy" "default" {
+  bucket = var.bucket_name
+  policy = jsonencode({
+    Version = var.json_encode_decode_version,
+    Statement = [
+      {
+        Sid    = var.moj_aws_s3_bucket_policy_statement_sid,
+        Effect = var.moj_aws_s3_bucket_policy_statement_effect,
+        Principal = {
+          Service : var.moj_aws_s3_bucket_policy_statement_principal_service
+        },
+        Action   = var.moj_aws_s3_bucket_policy_statement_action,
+        Resource = "arn:aws:s3:::${aws_s3_bucket.default.id}/*"
+      },
+      {
+        Sid    = var.bt_genesys_aws_s3_bucket_policy_statement_sid,
+        Effect = var.bt_genesys_aws_s3_bucket_policy_statement_effect,
+        Principal = {
+          AWS = var.bt_genesys_aws_s3_bucket_policy_statement_principal_aws
+        },
+        Action   = var.bt_genesys_aws_s3_bucket_policy_statement_action,
+        Resource = "arn:aws:s3:::${aws_s3_bucket.default.id}/*"
+      }
+    ]
+  })
+}
+
+# AWS KMS Key (Call Centre Staging)
+resource "aws_kms_key" "s3" {
+  #checkov:skip=CKV_AWS_7
+  description = var.aws_kms_key_s3_description
+  key_usage   = var.aws_kms_key_s3_key_usage
+  policy = jsonencode({
+    Version = var.json_encode_decode_version,
+    Statement = [
+      {
+        Sid    = var.aws_kms_key_s3_policy_statement_sid,
+        Effect = var.aws_kms_key_s3_policy_statement_effect,
+        Principal = {
+          Service = var.aws_kms_key_s3_policy_statement_principal_service
+        },
+        Action   = var.aws_kms_key_s3_policy_statement_action,
+        Resource = var.aws_kms_key_s3_policy_statement_resource
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
+
+
