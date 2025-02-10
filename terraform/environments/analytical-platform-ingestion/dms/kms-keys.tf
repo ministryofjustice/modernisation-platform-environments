c5ac6592fa06869e54443182b196d4b1dad161b0
Gary <26419401+Gary-H9@users.noreply.github.com>
2025-02-10 14:25:04 +0000

    🪣 Add CICA DMS Ingress S3 bucket (#9590)
    * :wrench: Create KMS key
    
    * :wrench: Add S3 bucket
    
    * testing
    
    * Move KMS key into component
    
    * Add newline
    
    * :wrench: Specify production environment
    
    * :wrench: Update description
    
    * :wrench: Specify production environment

---
+++ terraform/environments/analytical-platform-ingestion/dms/kms-keys.tf
@@ -0,0 +1,28 @@
+module "s3_cica_dms_egress_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.1.0"
+
+  aliases               = ["s3/cica-dms-egress"]
+  description           = "Used in the CICA DMS Egress Solution"
+  enable_default_policy = true
+  key_statements = [
+    {
+      sid = "AllowAnalyticalPlatformDataProduction"
+      actions = [
+        "kms:Encrypt",
+        "kms:GenerateDataKey"
+      ]
+      resources = ["*"]
+      effect    = "Allow"
+      principals = [
+        {
+          type        = "AWS"
+          identifiers = ["arn:aws:iam::593291632749:role/mojap-data-production-cica-dms-egress-production"]
+        }
+      ]
+    }
+  ]
+  deletion_window_in_days = 7
+}
