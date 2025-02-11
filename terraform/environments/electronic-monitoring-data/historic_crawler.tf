77003e5e52e51e8a16a1b6a43fad2bf1aca1bf3b
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-02-11 12:06:13 +0000

    ✨ Add AWS Glue Crawler and IAM Role for Historic API Mart

---
+++ terraform/environments/electronic-monitoring-data/historic_crawler.tf
@@ -0,0 +1,65 @@
+resource "aws_glue_crawler" "iceberg_crawler" {
+  database_name = "historic_api_mart"
+  name          = "historic_api_mart_crawler"
+  role          = aws_iam_role.glue_role.arn
+
+  s3_target {
+    path = "s3://emds-test-cadt/data/test/models/domain_name=historic/database_name=historic_api_mart_mock/"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableGroupingPolicy = "CombineCompatibleSchemas"
+    }
+  })
+}
+
+# IAM role for the crawler
+resource "aws_iam_role" "glue_role" {
+  name = "historic_api_mart_crawler_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach necessary policies to the role
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+# Custom policy for S3 access
+resource "aws_iam_role_policy" "s3_access" {
+  name = "s3_access"
+  role = aws_iam_role.glue_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::emds-test-cadt/*",
+          "arn:aws:s3:::emds-test-cadt"
+        ]
+      }
+    ]
+  })
+}
