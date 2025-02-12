8d6bf75189d21936983e43f7d9550da34f57432e
Matthew Price <matthew.price2@digital.justice.gov.uk>
2025-02-12 10:15:01 +0000

    Remove S3 logging bucket module and associated variables, outputs, and resources

--- terraform/environments/electronic-monitoring-data/modules/s3-logging-bucket/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "logging_bucket_id" {
-  description = "The ID of the logging S3 bucket"
-  value       = aws_s3_bucket.logging_bucket.id
-}
-
-output "logging_bucket_arn" {
-  description = "The ARN of the logging S3 bucket"
-  value       = aws_s3_bucket.logging_bucket.arn
-}
