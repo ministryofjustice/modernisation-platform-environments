dc5b92a6e5ef9579b3dfca8bd6e04e65a24760e7
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-02-12 16:37:28 +0000

    Fix data filter ID reference in Lake Formation permissions and update output to handle multiple data filters

--- terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
+++ terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
@@ -5,6 +5,6 @@ resource "aws_lakeformation_permissions" "grant_test_databases" {
     database_name    = "staged_fms_test_dbt"
     table_name       = "account"
     table_catalog_id = data.aws_caller_identity.current.account_id
-    name             = module.share_current_version.data_filter_id
+    name             = module.share_current_version[0].data_filter_id
   }
 }
