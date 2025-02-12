c3da54e0928e42d7a40cd2a103ae4742dd6eae8a
Seb Norris <sebastian.j.norris@gmail.com>
2025-02-12 13:12:46 +0000

    move cert creation to oracle instance module

--- terraform/environments/delius-core/modules/components/oracle_db_instance/cert.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_instance/cert.tf
@@ -1,33 +1,19 @@
+data "aws_ram_resource_share" "shared_ca" {
+  name = var.shared_ca_name
+  resource_owner = "OTHER-ACCOUNTS"
+}
 
-# data "aws_ram_resource_share" "shared_ca" {
-#   name = var.shared_ca_name
-#   resource_owner = "OTHER-ACCOUNTS"
-# }
+data "aws_acmpca_certificate_authority" "shared_ca" {
+  arn = data.aws_ram_resource_share.shared_ca.resource_arns[0]
+}
 
-# data "aws_acmpca_certificate_authority" "shared_ca" {
-#   arn = data.aws_ram_resource_share.shared_ca.resource_arns[0]
-# }
+resource "aws_acm_certificate" "cert" {
+  certificate_authority_arn = data.aws_acmpca_certificate_authority.shared_ca.arn
+  domain_name = "modernisation-platform.service.justice.gov.uk"
+  subject_alternative_names = ["delius-core-dev-db-1.${var.account_config.dns_suffix}"]
 
-# resource "tls_private_key" "key" {
-#   algorithm = "RSA"
-# }
-
-# resource "tls_cert_request" "csr" {
-#   private_key_pem = tls_private_key.key.private_key_pem
-
-#   subject {
-#     common_name = "example"
-#   }
-# }
-
-# resource "aws_acmpca_certificate" "oracle_cert" {
-#   certificate_authority_arn = data.aws_acmpca_certificate_authority.shared_ca.arn
-#   certificate_signing_request = tls_cert_request.csr.cert_request_pem
-#   signing_algorithm           = "SHA256WITHRSA"
-#   validity {
-#     type  = "YEARS"
-#     value = 1
-#   }
-# }
-
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
