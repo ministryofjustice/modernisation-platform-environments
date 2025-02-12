ab6fa0d0f66e18f87533a003218e4f922f6d4bbd
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-02-12 17:02:11 +0000

    Fix data filter ID reference to access the first element and update output value for Lake Formation data filters

--- terraform/environments/electronic-monitoring-data/modules/lakeformation_w_data_filter/outputs.tf
+++ terraform/environments/electronic-monitoring-data/modules/lakeformation_w_data_filter/outputs.tf
@@ -1,3 +1,3 @@
 output "data_filter_id" {
-  value = aws_lakeformation_data_cells_filter.data_filter[*].id
+  value = values(aws_lakeformation_data_cells_filter.data_filter)[*].table_data[0].name
 }
