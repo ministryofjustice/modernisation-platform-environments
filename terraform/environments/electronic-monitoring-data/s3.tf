92ae2ef10afbbb82789452166d4e2456cfad2900
Matthew Price <matthew.price2@digital.justice.gov.uk>
2025-02-12 13:30:18 +0000

    Comment out export bucket ARN reference for clarity in S3 bucket outputs

--- terraform/environments/electronic-monitoring-data/s3.tf
+++ terraform/environments/electronic-monitoring-data/s3.tf
@@ -27,7 +27,7 @@ locals {
     # [module.s3-mdss-general-landing-bucket.bucket.id, module.s3-mdss-general-landing-bucket.bucket.arn],
     # [module.s3-mdss-ho-landing-bucket.bucket.id, module.s3-mdss-ho-landing-bucket.bucket.arn],
     # [module.s3-mdss-specials-landing-bucket.bucket.id, module.s3-mdss-specials-landing-bucket.bucket.arn],
-    [module.s3-p1-export-bucket.bucket_id, module.s3-p1-export-bucket.bucket_arn],
+    # [module.s3-p1-export-bucket.bucket_id, module.s3-p1-export-bucket.bucket_arn],
     # [module.s3-serco-export-bucket.bucket.id, module.s3-serco-export-bucket.bucket.arn],
     [module.s3-received-files-bucket.bucket.id, module.s3-received-files-bucket.bucket.arn],
     [module.s3-quarantine-files-bucket.bucket.id, module.s3-quarantine-files-bucket.bucket.arn],
