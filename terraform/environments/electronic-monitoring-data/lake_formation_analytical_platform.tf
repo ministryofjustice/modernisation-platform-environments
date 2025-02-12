0783b8da533ec8baa99ac011a34014265d0031f8
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-02-12 16:22:40 +0000

    Add Lake Formation permissions and data filter output for analytical platform

---
+++ terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
@@ -0,0 +1,10 @@
+resource "aws_lakeformation_permissions" "grant_test_databases" {
+  principal   = "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/alpha_user_matt-heery"
+  permissions = ["SELECT"]
+  data_cells_filter {
+    database_name    = "staged_fms_test_dbt"
+    table_name       = "account"
+    table_catalog_id = data.aws_caller_identity.current.account_id
+    name             = module.share_current_version.data_filter_id
+  }
+}
---
+++ terraform/environments/electronic-monitoring-data/modules/lakeformation_w_data_filter/outputs.tf
@@ -0,0 +1,3 @@
+output "data_filter_id" {
+  value = aws_lakeformation_data_cells_filter.data_filter.id
+}
