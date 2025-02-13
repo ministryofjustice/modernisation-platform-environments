e23d1b3d217357003b45b829c4f842a926536d2f
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-02-13 11:04:55 +0000

    Add S3 bucket permissions and update Lake Formation permissions to include DESCRIBE

--- terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
+++ terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
@@ -1,10 +1,21 @@
 resource "aws_lakeformation_permissions" "grant_test_databases" {
+  count       = local.is-test ? 1 : 0
   principal   = "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/alpha_user_matt-heery"
-  permissions = ["SELECT"]
+  permissions = ["SELECT", "DESCRIBE"]
   data_cells_filter {
     database_name    = "staged_fms_test_dbt"
     table_name       = "account"
     table_catalog_id = data.aws_caller_identity.current.account_id
     name             = module.share_current_version[0].data_filter_id[0]
+  }
+}
+
+resource "aws_lakeformation_permissions" "s3_bucket_permissions" {
+  principal = "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/alpha_user_matt-heery"
+
+  permissions = ["DATA_LOCATION_ACCESS"]
+
+  data_location {
+    arn = aws_lakeformation_resource.data_bucket.arn
   }
 }
