c85585c590901a6d7aa2c10ff870bc789dae5d52
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-02-13 11:20:24 +0000

    Add Lake Formation permissions for DESCRIBE on test database

--- terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
+++ terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
@@ -1,12 +1,22 @@
 resource "aws_lakeformation_permissions" "grant_test_databases" {
   count       = local.is-test ? 1 : 0
   principal   = "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/alpha_user_matt-heery"
-  permissions = ["SELECT", "DESCRIBE"]
+  permissions = ["SELECT"]
   data_cells_filter {
     database_name    = "staged_fms_test_dbt"
     table_name       = "account"
     table_catalog_id = data.aws_caller_identity.current.account_id
     name             = module.share_current_version[0].data_filter_id[0]
+  }
+}
+
+resource "aws_lakeformation_permissions" "grant_test_databases" {
+  count       = local.is-test ? 1 : 0
+  principal   = "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/alpha_user_matt-heery"
+  permissions = ["DESCRIBE"]
+  table {
+    database_name = "staged_fms_test_dbt"
+    name          = "account"
   }
 }
 
