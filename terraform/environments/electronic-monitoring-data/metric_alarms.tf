78a900fb8e93d6806a6a5030353219f6f0ab4a84
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-02-17 18:16:41 +0000

    Add CloudWatch metrics and log group for S3 file arrivals monitoring

--- terraform/environments/electronic-monitoring-data/metric_alarms.tf
+++ terraform/environments/electronic-monitoring-data/metric_alarms.tf
@@ -31,6 +31,47 @@ resource "aws_sns_topic" "fms_land_bucket_count" {
   kms_master_key_id = "alias/aws/sns"
 }
 
+resource "aws_cloudwatch_log_metric_filter" "s3_file_arrivals" {
+  count = local.is-development ? 0 : 1
+
+  name           = "fms-land-file-arrivals"
+  pattern        = "[timestamp, requestParameters.bucketName, eventName=PutObject]"
+  log_group_name = aws_cloudwatch_log_group.s3_events.name
+
+  metric_transformation {
+    name          = "FileArrivals"
+    namespace     = "Custom/S3"
+    value         = "1"
+    default_value = "0"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "s3_events" {
+  count = local.is-development ? 0 : 1
+
+  name              = "/aws/s3/fms-landing"
+  retention_in_days = 30
+}
+
+resource "aws_cloudtrail" "s3_events" {
+  count = local.is-development ? 0 : 1
+
+  name                          = "fms-landing-file-monitoring"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_logs.id
+  include_global_service_events = false
+  is_multi_region_trail         = false
+
+  event_selector {
+    read_write_type           = "WriteOnly"
+    include_management_events = false
+
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["${module.s3-fms-general-landing-bucket.bucket_arn}/"]
+    }
+  }
+}
+
 # Alarm - "Detect when no files land in fms bucket within 24 hours"
 module "files_in_fms_land_bucket_alarm" {
   count = local.is-development ? 0 : 1
@@ -46,8 +87,8 @@ module "files_in_fms_land_bucket_alarm" {
   period              = 90000
   unit                = "Count"
 
-  namespace   = "AWS/S3"
-  metric_name = "NumberOfObjects"
+  namespace   = "Custom/S3"
+  metric_name = "FileArrivals"
   statistic   = "Sum"
 
   dimensions = {
