e947f3d47e7b010f40fcb953b140fb650374db5f
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2025-02-19 12:00:40 +0000

    Allow configuring specific versions of scheduled dataset lambda per environment

--- terraform/environments/digital-prison-reporting/locals.tf
+++ terraform/environments/digital-prison-reporting/locals.tf
@@ -191,7 +191,7 @@ locals {
   lambda_scheduled_dataset_tracing        = "Active"
   lambda_scheduled_dataset_handler        = "uk.gov.justice.digital.hmpps.scheduled.lambda.ReportSchedulerLambda::handleRequest"
   lambda_scheduled_dataset_code_s3_bucket = module.s3_artifacts_store.bucket_id
-  lambda_scheduled_dataset_jar_version    = "vLatest"
+  lambda_scheduled_dataset_jar_version    = local.application_data.accounts[local.environment].scheduled_dataset_lambda_version
   lambda_scheduled_dataset_code_s3_key = (
     local.env == "production" || local.env == "preproduction"
     ? "build-artifacts/hmpps-dpr-scheduled-dataset-lambda/jars/hmpps-dpr-scheduled-dataset-lambda-${local.lambda_scheduled_dataset_jar_version}.rel-all.jar"
