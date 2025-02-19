81ac19c65199e792fbaaadaf8c7b1ce774ca68bd
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-02-19 16:54:13 +0000

    Add aws_ses_domain_identity_verification
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-compute/ses.tf
+++ terraform/environments/analytical-platform-compute/ses.tf
@@ -1,3 +1,9 @@
 resource "aws_ses_domain_identity" "main" {
   domain = local.environment_configuration.route53_zone
 }
+
+resource "aws_ses_domain_identity_verification" "main" {
+  domain = aws_ses_domain_identity.main.domain
+
+  depends_on = [module.route53_records.aws_route53_record.this["_amazonses TXT"]]
+}
