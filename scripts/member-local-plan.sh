d90acadcd4158b0c9698a14c6418956d655506ec
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-02-26 11:54:53 +0000

    Update dev container README Update script to accept components
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- scripts/member-local-plan.sh
+++ scripts/member-local-plan.sh
@@ -3,7 +3,13 @@
 # This is a convenience script for https://user-guide.modernisation-platform.service.justice.gov.uk/user-guide/running-terraform-plan-locally.html#running-terraform-plan-locally
 # Run it from within the environment directory, e.g. terraform/environments/cooker
 
-ENVIRONMENT=$(basename ${PWD})
+if [[ -z "${REMOTE_CONTAINERS}" ]]; then
+  echo "This script is intended to be run from within the development container"
+  exit 1
+fi
+
+COMPONENT="${PWD#/workspaces/modernisation-platform-environments/terraform/environments/}"
+ACCOUNT="${COMPONENT%%/*}"
 STAGE="development"
 ROLE="modernisation-platform-developer"
 APPLY="false"
@@ -18,17 +24,22 @@ done
 
 ###
 
-echo "Account: ${ENVIRONMENT}"
+if [[ "${ACCOUNT}" == "${COMPONENT}" ]]; then
+  COMPONENT="root"
+fi
+
+echo "Account: ${ACCOUNT}"
+echo "Component: ${COMPONENT}"
 echo "Stage: ${STAGE}"
 echo "Role: ${ROLE}"
 echo "Apply: ${APPLY}"
 
-aws-sso exec --profile ${ENVIRONMENT}-${STAGE}:${ROLE} -- terraform init
+aws-sso exec --profile ${ACCOUNT}-${STAGE}:${ROLE} -- terraform init
 
-aws-sso exec --profile ${ENVIRONMENT}-${STAGE}:${ROLE} -- terraform workspace select ${ENVIRONMENT}-${STAGE}
+aws-sso exec --profile ${ACCOUNT}-${STAGE}:${ROLE} -- terraform workspace select ${ACCOUNT}-${STAGE}
 
-aws-sso exec --profile ${ENVIRONMENT}-${STAGE}:${ROLE} -- terraform plan
+aws-sso exec --profile ${ACCOUNT}-${STAGE}:${ROLE} -- terraform plan
 
 if [[ "${APPLY}" == "true" ]]; then
-  aws-sso exec --profile ${ENVIRONMENT}-${STAGE}:${ROLE} -- terraform apply
+  aws-sso exec --profile ${ACCOUNT}-${STAGE}:${ROLE} -- terraform apply
 fi
