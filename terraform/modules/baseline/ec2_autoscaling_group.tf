7b6f8ff4cb45300c3eee68b80f3af5fb4b3e2316
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-02-27 12:02:42 +0000

    formatting

--- terraform/modules/baseline/ec2_autoscaling_group.tf
+++ terraform/modules/baseline/ec2_autoscaling_group.tf
@@ -37,18 +37,18 @@ module "ec2_autoscaling_group" {
     ]
   })
 
-  availability_zone               = each.value.config.availability_zone
-  subnet_ids                      = each.value.config.availability_zone == null ? var.environment.subnets[each.value.config.subnet_name].ids : [var.environment.subnet[each.value.config.subnet_name][each.value.config.availability_zone].id]
-  ebs_volumes_copy_all_from_ami   = each.value.config.ebs_volumes_copy_all_from_ami
-  ebs_kms_key_id                  = coalesce(each.value.config.ebs_kms_key_id, var.environment.kms_keys["ebs"].arn)
-  ebs_volume_config               = each.value.ebs_volume_config
-  ebs_volume_tags                 = each.value.ebs_volume_tags
-  ebs_volumes                     = each.value.ebs_volumes
-  user_data_raw                   = each.value.config.user_data_raw
-  user_data_cloud_init            = each.value.user_data_cloud_init
-  ssm_parameters_prefix           = each.value.config.ssm_parameters_prefix
-  secretsmanager_secrets_prefix   = each.value.config.secretsmanager_secrets_prefix
-  iam_resource_names_prefix       = each.value.config.iam_resource_names_prefix
+  availability_zone             = each.value.config.availability_zone
+  subnet_ids                    = each.value.config.availability_zone == null ? var.environment.subnets[each.value.config.subnet_name].ids : [var.environment.subnet[each.value.config.subnet_name][each.value.config.availability_zone].id]
+  ebs_volumes_copy_all_from_ami = each.value.config.ebs_volumes_copy_all_from_ami
+  ebs_kms_key_id                = coalesce(each.value.config.ebs_kms_key_id, var.environment.kms_keys["ebs"].arn)
+  ebs_volume_config             = each.value.ebs_volume_config
+  ebs_volume_tags               = each.value.ebs_volume_tags
+  ebs_volumes                   = each.value.ebs_volumes
+  user_data_raw                 = each.value.config.user_data_raw
+  user_data_cloud_init          = each.value.user_data_cloud_init
+  ssm_parameters_prefix         = each.value.config.ssm_parameters_prefix
+  secretsmanager_secrets_prefix = each.value.config.secretsmanager_secrets_prefix
+  iam_resource_names_prefix     = each.value.config.iam_resource_names_prefix
 
   # add KMS Key Ids if they are referenced by name
   ssm_parameters = each.value.ssm_parameters == null ? null : {
