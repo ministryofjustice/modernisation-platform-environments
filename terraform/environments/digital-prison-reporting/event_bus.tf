2b5c9d7bd44bbcf13fe1d331d93c992c190c9844
djcostin <daniel.costin@gmail.com>
2025-03-03 14:48:15 +0000

    DPR2-1715 add tags to event_bus.tf

--- terraform/environments/digital-prison-reporting/event_bus.tf
+++ terraform/environments/digital-prison-reporting/event_bus.tf
@@ -2,4 +2,13 @@
 module "create_event_bus_dpd" {
   source = "./modules/eventbridge"
   dpr_event_bus_name = local.event_bus_dpr
+
+  tags = merge(
+    local.all_tags,
+    {
+      Name           = local.event_bus_dpr
+      Jira           = "DPR2-1715"
+      Resource_Group = "Front-End"
+    }
+  )
 }
--- terraform/environments/digital-prison-reporting/modules/eventbridge/variables.tf
+++ terraform/environments/digital-prison-reporting/modules/eventbridge/variables.tf
@@ -3,3 +3,9 @@ variable "dpr_event_bus_name" {
   description = "Name of the custom event bus"
   type        = string
 }
+
+variable "tags" {
+  description = "Additional tags to apply to the log group."
+  type        = map(any)
+  default     = {}
+}
