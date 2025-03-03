20b085f76726bbd7ae3ee840b8e111f133177e01
Greg Whiting <greg.whiting@necsws.com>
2025-03-03 11:35:09 +0000

    fix for each on ses.tf toset

--- terraform/environments/youth-justice-app-framework/ses.tf
+++ terraform/environments/youth-justice-app-framework/ses.tf
@@ -1,5 +1,5 @@
 module "ses" {
-  for_each              = local.application_data.accounts[local.environment].ses_domain_identities
+  for_each              = toset(local.application_data.accounts[local.environment].ses_domain_identities)
   source                = "./modules/ses"
   project_name          = local.project_name
   environment           = local.environment
