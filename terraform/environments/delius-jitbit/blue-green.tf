f7371795a921f07bde66e1cae0fa2fc8af6f8bc3
Seb Norris <sebastian.j.norris@gmail.com>
2025-03-04 16:41:09 +0000

    temporarily remove blue-green

--- terraform/environments/delius-jitbit/blue-green.tf
+++ terraform/environments/delius-jitbit/blue-green.tf
@@ -42,115 +42,115 @@ module "ecs_green" {
   tags = local.tags
 }
 
-resource "aws_lb_target_group" "blue" {
+# resource "aws_lb_target_group" "blue" {
 
-  name                 = "${local.application_name}-blue"
-  port                 = local.app_port
-  protocol             = "HTTP"
-  vpc_id               = data.aws_vpc.shared.id
-  target_type          = "ip"
-  deregistration_delay = 30
+#   name                 = "${local.application_name}-blue"
+#   port                 = local.app_port
+#   protocol             = "HTTP"
+#   vpc_id               = data.aws_vpc.shared.id
+#   target_type          = "ip"
+#   deregistration_delay = 30
 
-  stickiness {
-    type = "lb_cookie"
-  }
+#   stickiness {
+#     type = "lb_cookie"
+#   }
 
-  health_check {
-    path                = "/User/Login?ReturnUrl=%2f"
-    healthy_threshold   = "5"
-    interval            = "120"
-    protocol            = "HTTP"
-    unhealthy_threshold = "2"
-    matcher             = "200-499"
-    timeout             = "5"
-  }
+#   health_check {
+#     path                = "/User/Login?ReturnUrl=%2f"
+#     healthy_threshold   = "5"
+#     interval            = "120"
+#     protocol            = "HTTP"
+#     unhealthy_threshold = "2"
+#     matcher             = "200-499"
+#     timeout             = "5"
+#   }
 
-  tags = merge(
-    local.tags,
-    {
-      Name = local.application_name
-    }
-  )
-}
+#   tags = merge(
+#     local.tags,
+#     {
+#       Name = local.application_name
+#     }
+#   )
+# }
 
-resource "aws_lb_target_group" "green" {
+# resource "aws_lb_target_group" "green" {
 
-  name                 = "${local.application_name}-green"
-  port                 = local.app_port
-  protocol             = "HTTP"
-  vpc_id               = data.aws_vpc.shared.id
-  target_type          = "ip"
-  deregistration_delay = 30
+#   name                 = "${local.application_name}-green"
+#   port                 = local.app_port
+#   protocol             = "HTTP"
+#   vpc_id               = data.aws_vpc.shared.id
+#   target_type          = "ip"
+#   deregistration_delay = 30
 
-  stickiness {
-    type = "lb_cookie"
-  }
+#   stickiness {
+#     type = "lb_cookie"
+#   }
 
-  health_check {
-    path                = "/User/Login?ReturnUrl=%2f"
-    healthy_threshold   = "5"
-    interval            = "120"
-    protocol            = "HTTP"
-    unhealthy_threshold = "2"
-    matcher             = "200-499"
-    timeout             = "5"
-  }
+#   health_check {
+#     path                = "/User/Login?ReturnUrl=%2f"
+#     healthy_threshold   = "5"
+#     interval            = "120"
+#     protocol            = "HTTP"
+#     unhealthy_threshold = "2"
+#     matcher             = "200-499"
+#     timeout             = "5"
+#   }
 
-  tags = merge(
-    local.tags,
-    {
-      Name = local.application_name
-    }
-  )
-}
+#   tags = merge(
+#     local.tags,
+#     {
+#       Name = local.application_name
+#     }
+#   )
+# }
 
-resource "aws_lb_listener_rule" "blue_green" {
-  count        = local.is-development ? 1 : 0
-  listener_arn = aws_lb_listener.listener.arn
-  priority     = 20
+# resource "aws_lb_listener_rule" "blue_green" {
+#   count        = local.is-development ? 1 : 0
+#   listener_arn = aws_lb_listener.listener.arn
+#   priority     = 20
 
-  action {
-    target_group_arn = data.aws_ssm_parameter.blue_green.value == "blue" ? aws_lb_target_group.blue.arn : aws_lb_target_group.green.arn
-    type             = "forward"
-  }
+#   action {
+#     target_group_arn = data.aws_ssm_parameter.blue_green.value == "blue" ? aws_lb_target_group.blue.arn : aws_lb_target_group.green.arn
+#     type             = "forward"
+#   }
 
-  condition {
-    host_header {
-      values = [local.blue_green_url]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = [local.blue_green_url]
+#     }
+#   }
+# }
 
-resource "aws_lb_listener_rule" "blue" {
-  count        = local.is-development ? 1 : 0
-  listener_arn = aws_lb_listener.listener.arn
-  priority     = 21
+# resource "aws_lb_listener_rule" "blue" {
+#   count        = local.is-development ? 1 : 0
+#   listener_arn = aws_lb_listener.listener.arn
+#   priority     = 21
 
-  action {
-    target_group_arn = aws_lb_target_group.blue.arn
-    type             = "forward"
-  }
+#   action {
+#     target_group_arn = aws_lb_target_group.blue.arn
+#     type             = "forward"
+#   }
 
-  condition {
-    host_header {
-      values = [local.blue_green_url]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = [local.blue_green_url]
+#     }
+#   }
+# }
 
-resource "aws_lb_listener_rule" "green" {
-  count        = local.is-development ? 1 : 0
-  listener_arn = aws_lb_listener.listener.arn
-  priority     = 22
+# resource "aws_lb_listener_rule" "green" {
+#   count        = local.is-development ? 1 : 0
+#   listener_arn = aws_lb_listener.listener.arn
+#   priority     = 22
 
-  action {
-    target_group_arn = aws_lb_target_group.green.arn
-    type             = "forward"
-  }
+#   action {
+#     target_group_arn = aws_lb_target_group.green.arn
+#     type             = "forward"
+#   }
 
-  condition {
-    host_header {
-      values = [local.blue_green_url]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = [local.blue_green_url]
+#     }
+#   }
+# }
