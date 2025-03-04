8d11225c2a4abaff14b4062f2a49a90389d1a0a5
djcostin <daniel.costin@gmail.com>
2025-03-04 16:13:16 +0000

    DPR2-1715 fix event bridge arn

--- terraform/environments/digital-prison-reporting/locals.tf
+++ terraform/environments/digital-prison-reporting/locals.tf
@@ -200,8 +200,7 @@ locals {
     "arn:aws:iam::${local.account_id}:policy/${local.s3_read_access_policy}",
     "arn:aws:iam::${local.account_id}:policy/${local.kms_read_access_policy}",
     aws_iam_policy.redshift_dataapi_cross_policy.arn,
-    aws_iam_policy.dpd_table_read_policy.arn,
-    aws_iam_policy.dpr_event_bus_write_events_policy.arn
+    aws_iam_policy.dpd_table_read_policy.arn
   ]
   lambda_scheduled_dataset_secret_arn          = module.datamart.credential_secret_arn
   lambda_scheduled_dataset_cluster_id          = module.datamart.cluster_id
--- terraform/environments/digital-prison-reporting/modules/eventbridge/outputs.tf
+++ terraform/environments/digital-prison-reporting/modules/eventbridge/outputs.tf
@@ -1,4 +1,4 @@
 output "event_bus_arn" {
   description = "ARN of the Custom Event Bus"
-  value       = aws_cloudwatch_event_bus.this.*.arn
+  value       = aws_cloudwatch_event_bus.this.arn
 }
