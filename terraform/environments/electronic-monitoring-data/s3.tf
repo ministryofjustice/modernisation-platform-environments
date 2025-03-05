79c01ef0cd0d4058a46abef2bd2bf940281d4994
Matthew Price <matthew.price2@digital.justice.gov.uk>
2025-03-05 10:10:24 +0000

    Refactor S3 bucket logging configuration to use a list of objects for bucket IDs and ARNs

--- terraform/environments/electronic-monitoring-data/s3.tf
+++ terraform/environments/electronic-monitoring-data/s3.tf
@@ -14,33 +14,30 @@ locals {
     }
   }
 
-  buckets_to_log = tomap(
-    # { bucket id: bucket arn }
-    {
-      "${module.s3-metadata-bucket.bucket.id}" : module.s3-metadata-bucket.bucket.arn,
-      "${module.s3-athena-bucket.bucket.id}" : module.s3-athena-bucket.bucket.arn,
-      "${module.s3-unzipped-files-bucket.bucket.id}" : module.s3-unzipped-files-bucket.bucket.arn,
-      "${module.s3-dms-premigrate-assess-bucket.bucket.id}" : module.s3-dms-premigrate-assess-bucket.bucket.arn,
-      "${module.s3-json-directory-structure-bucket.bucket.id}" : module.s3-json-directory-structure-bucket.bucket.arn,
-      "${module.s3-data-bucket.bucket.id}" : module.s3-data-bucket.bucket.arn,
-      "${module.s3-fms-general-landing-bucket.bucket_id}" : module.s3-fms-general-landing-bucket.bucket_arn,
-      "${module.s3-fms-ho-landing-bucket.bucket_id}" : module.s3-fms-ho-landing-bucket.bucket_arn,
-      "${module.s3-fms-specials-landing-bucket.bucket_id}" : module.s3-fms-specials-landing-bucket.bucket_arn,
-      "${module.s3-mdss-general-landing-bucket.bucket_id}" : module.s3-mdss-general-landing-bucket.bucket_arn,
-      "${module.s3-mdss-ho-landing-bucket.bucket_id}" : module.s3-mdss-ho-landing-bucket.bucket_arn,
-      "${module.s3-mdss-specials-landing-bucket.bucket_id}" : module.s3-mdss-specials-landing-bucket.bucket_arn,
-      "${module.s3-p1-export-bucket.bucket_id}" : module.s3-p1-export-bucket.bucket_arn,
-      "${module.s3-serco-export-bucket.bucket_id}" : module.s3-serco-export-bucket.bucket_arn,
-      "${module.s3-received-files-bucket.bucket.id}" : module.s3-received-files-bucket.bucket.arn,
-      "${module.s3-quarantine-files-bucket.bucket.id}" : module.s3-quarantine-files-bucket.bucket.arn,
-      "${module.s3-clamav-definitions-bucket.bucket.id}" : module.s3-clamav-definitions-bucket.bucket.arn,
-      "${module.s3-dms-data-validation-bucket.bucket.id}" : module.s3-dms-data-validation-bucket.bucket.arn,
-      "${module.s3-glue-job-script-bucket.bucket.id}" : module.s3-glue-job-script-bucket.bucket.arn,
-      "${module.s3-dms-target-store-bucket.bucket.id}" : module.s3-dms-target-store-bucket.bucket.arn,
-      "${module.s3-create-a-derived-table-bucket.bucket.id}" : module.s3-create-a-derived-table-bucket.bucket.arn,
-      "${module.s3-raw-formatted-data-bucket.bucket.id}" : module.s3-raw-formatted-data-bucket.bucket.arn,
-    }
-  )
+  buckets_to_log = [
+    { id = module.s3-metadata-bucket.bucket.id,                 arn = module.s3-metadata-bucket.bucket.arn },
+    { id = module.s3-athena-bucket.bucket.id,                   arn = module.s3-athena-bucket.bucket.arn },
+    { id = module.s3-unzipped-files-bucket.bucket.id,           arn = module.s3-unzipped-files-bucket.bucket.arn },
+    { id = module.s3-dms-premigrate-assess-bucket.bucket.id,    arn = module.s3-dms-premigrate-assess-bucket.bucket.arn },
+    { id = module.s3-json-directory-structure-bucket.bucket.id, arn = module.s3-json-directory-structure-bucket.bucket.arn },
+    { id = module.s3-data-bucket.bucket.id,                     arn = module.s3-data-bucket.bucket.arn },
+    { id = module.s3-fms-general-landing-bucket.bucket_id,      arn = module.s3-fms-general-landing-bucket.bucket_arn },
+    { id = module.s3-fms-ho-landing-bucket.bucket_id,           arn = module.s3-fms-ho-landing-bucket.bucket_arn },
+    { id = module.s3-fms-specials-landing-bucket.bucket_id,     arn = module.s3-fms-specials-landing-bucket.bucket_arn },
+    { id = module.s3-mdss-general-landing-bucket.bucket_id,     arn = module.s3-mdss-general-landing-bucket.bucket_arn },
+    { id = module.s3-mdss-ho-landing-bucket.bucket_id,          arn = module.s3-mdss-ho-landing-bucket.bucket_arn },
+    { id = module.s3-mdss-specials-landing-bucket.bucket_id,    arn = module.s3-mdss-specials-landing-bucket.bucket_arn },
+    { id = module.s3-p1-export-bucket.bucket_id,                arn = module.s3-p1-export-bucket.bucket_arn },
+    { id = module.s3-serco-export-bucket.bucket_id,             arn = module.s3-serco-export-bucket.bucket_arn },
+    { id = module.s3-received-files-bucket.bucket.id,           arn = module.s3-received-files-bucket.bucket.arn },
+    { id = module.s3-quarantine-files-bucket.bucket.id,         arn = module.s3-quarantine-files-bucket.bucket.arn },
+    { id = module.s3-clamav-definitions-bucket.bucket.id,       arn = module.s3-clamav-definitions-bucket.bucket.arn },
+    { id = module.s3-dms-data-validation-bucket.bucket.id,      arn = module.s3-dms-data-validation-bucket.bucket.arn },
+    { id = module.s3-glue-job-script-bucket.bucket.id,          arn = module.s3-glue-job-script-bucket.bucket.arn },
+    { id = module.s3-dms-target-store-bucket.bucket.id,         arn = module.s3-dms-target-store-bucket.bucket.arn },
+    { id = module.s3-create-a-derived-table-bucket.bucket.id,   arn = module.s3-create-a-derived-table-bucket.bucket.arn },
+    { id = module.s3-raw-formatted-data-bucket.bucket.id,       arn = module.s3-raw-formatted-data-bucket.bucket.arn }
+  ]
 }
 
 # ------------------------------------------------------------------------
@@ -133,7 +130,7 @@ data "aws_iam_policy_document" "log_bucket_policy" {
     condition {
       test     = "ArnLike"
       variable = "aws:SourceArn"
-      values   = [for bucket_arn in local.buckets_to_log : bucket_arn]
+      values   = [for bucket in local.buckets_to_log : bucket.arn]
     }
 
     condition {
@@ -145,9 +142,9 @@ data "aws_iam_policy_document" "log_bucket_policy" {
 }
 
 resource "aws_s3_bucket_logging" "s3_buckets_logging" {
-  for_each = local.buckets_to_log
+  for_each = { for bucket in local.buckets_to_log : bucket.id => bucket }
 
-  bucket = each.key
+  bucket = each.value
 
   target_bucket = module.s3-logging-bucket.bucket.id
   target_prefix = "logs/"
