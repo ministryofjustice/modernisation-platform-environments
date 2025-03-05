69fcaece69e31130f8875a15b88c933078e3cf78
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2025-03-05 09:54:19 +0000

    dev

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -31,6 +31,16 @@ locals {
           description = "wildcard cert for nomis development domains"
         }
       }
+      nomis_wildcard_cert_v2 = {
+        cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms.acm
+        domain_name              = "*.development.nomis.service.justice.gov.uk"
+        subject_alternate_names = [
+          "*.nomis.hmpps-development.modernisation-platform.service.justice.gov.uk",
+        ]
+        tags = {
+          description = "wildcard cert for nomis development domains"
+        }
+      }
     }
 
     cloudwatch_dashboards = {
