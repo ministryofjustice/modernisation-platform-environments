917f1a691bc218607218262ec865f30cf18214b0
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2025-03-05 15:54:43 +0000

    CC-1947: Reorganised efs_security_group.

--- terraform/environments/ccms-ebs-upgrade/efs.tf
+++ terraform/environments/ccms-ebs-upgrade/efs.tf
@@ -31,33 +31,73 @@ resource "aws_efs_mount_target" "mount_c" {
   ]
 }
 
-resource "aws_security_group" "efs-security-group" {
-  name_prefix = "efs-security-group"
+resource "aws_security_group" "efs_security_group" {
+  name        = "efs-security-group"
   description = "allow inbound access from ebsdb and ebsconc"
   vpc_id      = data.aws_vpc.shared.id
-
-  # Allow inbound access from container instances	
-  ingress {
-    protocol  = "tcp"
-    from_port = 2049
-    to_port   = 2049
-    cidr_blocks = [
-      data.aws_subnet.data_subnets_a.cidr_block,
-      data.aws_subnet.data_subnets_b.cidr_block,
-      data.aws_subnet.data_subnets_c.cidr_block,
-    ]
-  }
-
-  egress {
-    protocol  = "-1"
-    from_port = 0
-    to_port   = 0
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
 
   tags = merge(local.tags,
     { Name = lower(format("sg-%s-%s-efs", local.application_name, local.environment)) }
   )
 }
+
+resource "aws_vpc_security_group_ingress_rule" "efs_security_group_ingress" {
+  description       = "Allow ingress traffic to EFS"
+  security_group_id = aws_security_group.efs_security_group.id
+  cidr_ipv4 = [
+    data.aws_subnet.data_subnets_a.cidr_block,
+    data.aws_subnet.data_subnets_b.cidr_block,
+    data.aws_subnet.data_subnets_c.cidr_block
+  ]
+  from_port   = 2049
+  ip_protocol = "tcp"
+  to_port     = 2049
+
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-efs", local.application_name, local.environment)) }
+  )
+}
+
+resource "aws_vpc_security_group_ingress_rule" "efs_security_group_egress" {
+  description       = "Allow egress traffic from EFS"
+  security_group_id = aws_security_group.efs_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = -1
+  to_port           = 0
+
+  tags = merge(local.tags,
+    { Name = lower(format("sg-%s-%s-efs", local.application_name, local.environment)) }
+  )
+}
+
+# resource "aws_security_group" "efs-security-group" {
+#   name_prefix = "efs-security-group"
+#   description = "allow inbound access from ebsdb and ebsconc"
+#   vpc_id      = data.aws_vpc.shared.id
+# 
+#   # Allow inbound access from container instances	
+#   ingress {
+#     protocol  = "tcp"
+#     from_port = 2049
+#     to_port   = 2049
+#     cidr_blocks = [
+#       data.aws_subnet.data_subnets_a.cidr_block,
+#       data.aws_subnet.data_subnets_b.cidr_block,
+#       data.aws_subnet.data_subnets_c.cidr_block,
+#     ]
+#   }
+# 
+#   egress {
+#     protocol  = "-1"
+#     from_port = 0
+#     to_port   = 0
+#     cidr_blocks = [
+#       "0.0.0.0/0",
+#     ]
+#   }
+# 
+#   tags = merge(local.tags,
+#     { Name = lower(format("sg-%s-%s-efs", local.application_name, local.environment)) }
+#   )
+# }
