4f3e7ba33b8a3aac6434f3eea3afbb08807351d4
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-03-05 10:40:21 +0000

    TM-1042: nomis: cert fix v1 (#9833)
    * TM-1042: nomis: add new nomis wildcard cert
    
    * dev

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -32,6 +32,18 @@ locals {
         tags = {
           description = "wildcard cert for nomis test domains"
         }
+      },
+      nomis_wildcard_cert_v2 = {
+        cloudwatch_metric_alarms            = module.baseline_presets.cloudwatch_metric_alarms.acm
+        domain_name                         = "*.test.nomis.service.justice.gov.uk"
+        external_validation_records_created = false
+        subject_alternate_names = [
+          "*.nomis.hmpps-test.modernisation-platform.service.justice.gov.uk",
+          "*.hmpp-azdt.justice.gov.uk",
+        ]
+        tags = {
+          description = "wildcard cert for nomis test domains"
+        }
       }
     }
 
