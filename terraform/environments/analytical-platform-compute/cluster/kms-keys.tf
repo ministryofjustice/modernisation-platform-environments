eaecdd88289fc2a7d0b56b6a232d388c75151ad0
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-03-06 09:14:03 +0000

    move mlfow code

--- terraform/environments/analytical-platform-compute/cluster/kms-keys.tf
+++ terraform/environments/analytical-platform-compute/cluster/kms-keys.tf
@@ -212,3 +212,19 @@ module "karpenter_sqs_kms" {
 
   tags = local.tags
 }
+
+module "mlflow_s3_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.1.1"
+
+  aliases               = ["s3/mlflow"]
+  description           = "MLflow S3 KMS key"
+  enable_default_policy = true
+
+  deletion_window_in_days = 7
+
+  tags = local.tags
+}
