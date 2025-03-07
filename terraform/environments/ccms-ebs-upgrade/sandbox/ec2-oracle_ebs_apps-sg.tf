7b5aee9f16dc8ec7c0f8ca76e057edcb2c252d4a
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-03-07 14:04:10 +0000

    CC-3203: Test run of pipeline with 1 instance

---
+++ terraform/environments/ccms-ebs-upgrade/sandbox/ec2-oracle_ebs_apps-sg.tf
@@ -0,0 +1,31 @@
+# Sandbox Security Group
+
+resource "aws_security_group" "ec2_sg_sandbox" {
+  name        = "ec2_sg_sandbox"
+  description = "SG for Sandbox EC2 instances"
+  vpc_id      = data.aws_vpc.shared.id
+  tags = merge(local.tags,
+    { Name = lower(format("ec2-sg-%s", local.component_name)) }
+  )
+}
+
+# INGRESS Rules
+
+resource "aws_vpc_security_group_ingress_rule" "sandbox_ingress" {
+  security_group_id = aws_security_group.ec2_sg_sandbox.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "tcp"
+  self              = true
+}
+
+
+# INGRESS Rules
+
+resource "aws_vpc_security_group_egress_rule" "sandbox_egress" {
+  security_group_id = aws_security_group.ec2_sg_sandbox.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "tcp"
+  self              = true
+}
