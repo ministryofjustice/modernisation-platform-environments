b4631813a5314b4cb40c25512eaf4c63a0cc0e35
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2025-03-07 10:40:28 +0000

    observability platform configuration

--- terraform/environments/observability-platform/environment-configurations.tf
+++ terraform/environments/observability-platform/environment-configurations.tf
@@ -16,6 +16,24 @@ locals {
               athena_enabled                  = false
             }
           }
+        },
+        "test" = {
+          identity_centre_team = "modernisation-platform"
+          aws_accounts = {
+            "cooker-development" = {
+              cloudwatch_enabled              = false
+              prometheus_push_enabled         = false
+              amazon_prometheus_query_enabled = false
+              xray_enabled                    = false
+              athena_enabled                  = true
+              athena_config = {
+                primary = {
+                  database  = "grafana_db"
+                  workgroup = "grafana-dashboard"
+                }
+              }
+            }
+          }
         }
       }
       grafana_version                 = "10.4"
