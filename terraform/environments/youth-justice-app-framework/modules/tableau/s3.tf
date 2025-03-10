c84a94f7a7efcc36ffeae60562368502940f16e8
David Seekins <david.seekins@necsws.com>
2025-03-10 14:50:57 +0000

    Corrected alb s3 log bucket permissions

--- terraform/environments/youth-justice-app-framework/modules/tableau/s3.tf
+++ terraform/environments/youth-justice-app-framework/modules/tableau/s3.tf
@@ -1,13 +1,29 @@
-module "s3" {
-  source = "../s3"
+module "log_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
 
-  environment_name = "${var.project_name}-${var.environment}"
+  bucket = "${var.project_name}-${var.environment}-tableau-alb-logs"
+  acl    = "log-delivery-write"
 
-  bucket_name = [local.alb_access_logs_bucket_name_suffix]
+  # For example only
+  force_destroy = true
 
-  project_name = var.project_name
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
 
-  tags = var.tags
+  attach_elb_log_delivery_policy = true # Required for ALB logs
+  attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
 
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  attach_deny_insecure_transport_policy = false #todo probably should be true but matching yjaf atm
+  attach_require_latest_tls_policy      = false #todo same here
+  #todo cloudtrail dataevents for this bucket required
+  tags = local.all_tags
 }
-
