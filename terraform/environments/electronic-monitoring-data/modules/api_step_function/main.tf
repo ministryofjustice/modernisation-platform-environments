a7ca20b6679af17a891801769dc58b258a3cdea8
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-03-10 12:53:51 +0000

    Remove tfsec ignore comment for public access on API Gateway method

--- terraform/environments/electronic-monitoring-data/modules/api_step_function/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/api_step_function/main.tf
@@ -23,7 +23,6 @@ resource "aws_api_gateway_resource" "resource" {
   path_part   = var.api_path
 }
 
-# tfsec:ignore:aws-api-gateway-no-public-access
 resource "aws_api_gateway_method" "method" {
   rest_api_id          = aws_api_gateway_rest_api.api_gateway.id
   resource_id          = aws_api_gateway_resource.resource.id
