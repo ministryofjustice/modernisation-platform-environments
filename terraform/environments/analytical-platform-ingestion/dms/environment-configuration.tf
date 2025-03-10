898246e7500fd47dd7d58f7c09e24116a2da80d3
Toby Petty <tobypetty1@gmail.com>
2025-03-10 17:50:29 +0000

    Initial commit of DMS module invocation with initial parameters

---
+++ terraform/environments/analytical-platform-ingestion/dms/environment-configuration.tf
@@ -0,0 +1,15 @@
+locals {
+  environment_configuration = local.environment_configurations[local.environment]
+  environment_configurations = {
+    development = {
+      /* VPC */
+      connected_vpc_cidr            = "10.26.128.0/23"
+      connected_vpc_private_subnets = ["10.26.128.0/26", "10.26.128.64/26", "10.26.128.128/26"]
+    }
+    production = {
+      /* VPC */
+      connected_vpc_cidr            = "10.27.128.0/23"
+      connected_vpc_private_subnets = ["10.27.128.0/26", "10.27.128.64/26", "10.27.128.128/26"]
+    }
+  }
+}
