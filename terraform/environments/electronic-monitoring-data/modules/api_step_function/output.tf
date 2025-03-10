7c59a9cab94f59458b07357cbbecc7dd900b1580
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-03-10 17:50:43 +0000

    Update API Gateway outputs and IAM policy resource ARNs for improved access management

--- terraform/environments/electronic-monitoring-data/modules/api_step_function/output.tf
+++ terraform/environments/electronic-monitoring-data/modules/api_step_function/output.tf
@@ -1,3 +1,7 @@
-output "api_gateway_resource_id" {
-  value = aws_api_gateway_resource.resource.id
+output "api_gateway_id" {
+  value = aws_api_gateway_rest_api.api_gateway.id
+}
+
+output "api_gateway_stage_name" {
+  value = aws_api_gateway_deployment.deployment.stage_name
 }
