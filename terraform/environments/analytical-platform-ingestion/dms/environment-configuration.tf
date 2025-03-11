73286a5039b64f12eafbcb22223bf678c32e067c
Toby Petty <tobypetty1@gmail.com>
2025-03-11 17:01:03 +0000

    Add in environment-specific CIDR range for ingress
    Taken from [200~https://github.com/ministryofjustice/modernisation-platform/blob/c05a3c08b1386b57394bb71995a2189b068d243e/environments-networks/cica-development.json#L5
    
    and https://github.com/ministryofjustice/modernisation-platform/blob/c05a3c08b1386b57394bb71995a2189b068d243e/environments-networks/cica-production.json#L5

--- terraform/environments/analytical-platform-ingestion/dms/environment-configuration.tf
+++ terraform/environments/analytical-platform-ingestion/dms/environment-configuration.tf
@@ -5,11 +5,13 @@ locals {
       /* VPC */
       connected_vpc_cidr            = "10.26.128.0/23"
       connected_vpc_private_subnets = ["10.26.128.0/26", "10.26.128.64/26", "10.26.128.128/26"]
+      tariff_cidr                   = "10.26.32.0/21"
     }
     production = {
       /* VPC */
       connected_vpc_cidr            = "10.27.128.0/23"
       connected_vpc_private_subnets = ["10.27.128.0/26", "10.27.128.64/26", "10.27.128.128/26"]
+      tariff_cidr                   = "10.27.80.0/21"
     }
   }
 }
