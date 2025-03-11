c1f0038da7d8f537f83800f60ef485cae26ae2ca
Keir Williams <keir.williams@digital.justice.gov.uk>
2025-03-11 10:33:33 +0000

    add sns topic and event target

--- terraform/environments/nomis/aws_health.tf
+++ terraform/environments/nomis/aws_health.tf
@@ -9,3 +9,18 @@ resource "aws_cloudwatch_event_rule" "aws_health" {
     ]
   })
 }
+
+resource "aws_cloudwatch_event_target" "sns" {
+  count = local.is-development ? 1 : 0
+
+  rule      = aws_cloudwatch_event_rule.aws_health[0].name
+  target_id = "SendToSNS"
+  arn       = aws_sns_topic.aws_health[0].arn
+}
+
+
+resource "aws_sns_topic" "aws_health" {
+  count = local.is-development ? 1 : 0
+
+  name = "aws-health"
+}
