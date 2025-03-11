896e01e3e971b3838b9e96ba04507417673f7e0a
David Seekins <david.seekins@necsws.com>
2025-03-11 09:01:01 +0000

    s3 parameter correction

--- terraform/environments/youth-justice-app-framework/modules/redshift/s3.tf
+++ terraform/environments/youth-justice-app-framework/modules/redshift/s3.tf
@@ -1,7 +1,7 @@
 module "s3" {
   source = "../s3"
 
-  project_name = var.environment
+  project_name = var.project_name
   environment  = var.environment
 
   transfer_bucket_name = ["redshift-yjb-reporting", "redshift-ycs-reporting"]
