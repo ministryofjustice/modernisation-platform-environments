ececfeb66f2998a58a0a5f10270b7fe013d26d0b
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-03-13 17:08:45 +0000

    :wrench: Common: Terraform Module Patching

--- terraform/environments/analytical-platform-common/iam-policies.tf
+++ terraform/environments/analytical-platform-common/iam-policies.tf
@@ -71,7 +71,7 @@ module "ecr_access_iam_policy" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "5.52.2"
+  version = "5.54.0"
 
   name_prefix = "ecr-access"
 
