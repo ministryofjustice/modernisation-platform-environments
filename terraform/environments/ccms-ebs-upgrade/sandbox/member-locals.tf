87ea9d56eda66069091402d2591efb6618ba7703
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-03-14 15:35:33 +0000

    CC-3203: Create EBS Apps Load Balancer and Certs

--- terraform/environments/ccms-ebs-upgrade/sandbox/member-locals.tf
+++ terraform/environments/ccms-ebs-upgrade/sandbox/member-locals.tf
@@ -4,8 +4,6 @@ locals {
   logging_bucket_name        = "ccms-ebs-${local.component_name}-logging"
   rsync_bucket_name          = "ccms-ebs-${local.component_name}-dbbackup"
   lb_log_prefix_ebsapp       = "${local.component_name}-ebsapps-lb"
-  lb_log_prefix_wgate        = "${local.component_name}-wgate-lb"
-  lb_log_prefix_wgate_public = "${local.component_name}-wgate-lb-public"
 
   data_subnets = [
     data.aws_subnet.data_subnets_a.id,
@@ -25,7 +23,7 @@ locals {
     data.aws_subnet.public_subnets_c.id
   ]
 
-  # cert_opts    = aws_acm_certificate.external.domain_validation_options
-  # cert_arn     = aws_acm_certificate.external.arn
+  cert_opts    = aws_acm_certificate.external.domain_validation_options
+  cert_arn     = aws_acm_certificate.external.arn
   cert_zone_id = data.aws_route53_zone.external.zone_id
 }
