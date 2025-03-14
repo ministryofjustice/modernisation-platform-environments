672798b8edda6b9ee478868226513cb093adefd4
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2025-03-14 12:10:44 +0000

    Initial commit for new subnets

--- terraform/environments/youth-justice-networking/vpc.tf
+++ terraform/environments/youth-justice-networking/vpc.tf
@@ -6,8 +6,10 @@ module "vpc" {
   name            = "${local.application_name}-${local.environment}"
   azs             = local.availability_zones
   cidr            = local.application_data.accounts[local.environment].vpc_cidr
-  private_subnets = local.private_subnets
-
+  private_subnets = concat(
+    values(aws_subnet.vSRX01_subnets)[*].cidr_block,
+    values(aws_subnet.vSRX02_subnets)[*].cidr_block
+  )
   # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
   enable_flow_log                      = true
   create_flow_log_cloudwatch_log_group = true
@@ -23,7 +25,10 @@ module "vpc_endpoints" {
   source = "github.com/terraform-aws-modules/terraform-aws-vpc//modules/vpc-endpoints?ref=25322b6b6be69db6cca7f167d7b0e5327156a595" # v5.8.1
 
   security_group_ids = [aws_security_group.vpc_endpoints.id]
-  subnet_ids         = module.vpc.private_subnets
+  subnet_ids         = concat(
+    values(aws_subnet.vSRX01_subnets)[*].id,
+    values(aws_subnet.vSRX02_subnets)[*].id
+  )
   vpc_id             = module.vpc.vpc_id
 
   endpoints = {
@@ -71,4 +76,39 @@ resource "aws_security_group_rule" "allow_all_vpc" {
   security_group_id = aws_security_group.vpc_endpoints.id
   to_port           = 65535
   type              = "ingress"
+}
+
+
+
+resource "aws_subnet" "vSRX01_subnets" {
+  for_each = {
+    "vSRX01 Management Range"    = "10.100.105.0/24"
+    "vSRX01 PSK External Range"  = "10.100.110.0/24"
+    "vSRX01 Cert External Range" = "10.100.115.0/24"
+    "vSRX01 Internal Range"      = "10.100.120.0/24"
+    "Juniper Management & KMS Server Range" = "10.100.50.0/24"
+  }
+
+  vpc_id                  = module.vpc.vpc_id
+  cidr_block              = each.value
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = false
+
+  tags = local.tags
+}
+
+resource "aws_subnet" "vSRX02_subnets" {
+  for_each = {
+    "vSRX02 Management Range"    = "10.100.205.0/24"
+    "vSRX02 PSK External Range"  = "10.100.210.0/24"
+    "vSRX02 Cert External Range" = "10.100.215.0/24"
+    "vSRX02 Internal Range"      = "10.100.220.0/24"
+  }
+
+  vpc_id                  = module.vpc.vpc_id
+  cidr_block              = each.value
+  availability_zone       = "eu-west-2b"
+  map_public_ip_on_launch = false
+
+  tags = local.tags
 }
