c1e836322916d8a78915fd1890f84a92ed3d3d02
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-03-18 13:26:45 +0000

    Add DESCRIBE permission to Lake Formation account table and database access

--- terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
+++ terraform/environments/electronic-monitoring-data/lake_formation_analytical_platform.tf
@@ -1,13 +1,14 @@
 resource "aws_lakeformation_permissions" "grant_account_table_filter" {
   count       = local.is-test ? 1 : 0
   principal   = local.environment_management.account_ids["analytical-platform-data-production"]
-  permissions = ["SELECT"]
+  permissions = ["SELECT", "DESCRIBE"]
   data_cells_filter {
     database_name    = "staged_fms_test_dbt"
     table_name       = "account"
     table_catalog_id = data.aws_caller_identity.current.account_id
     name             = module.share_current_version[0].data_filter_id[0]
   }
+  permissions_with_grant_option = ["SELECT", "DESCRIBE"]
 }
 
 resource "aws_lakeformation_permissions" "grant_account_table" {
@@ -18,6 +19,7 @@ resource "aws_lakeformation_permissions" "grant_account_table" {
     database_name = "staged_fms_test_dbt"
     name          = "account"
   }
+  permissions_with_grant_option = ["DESCRIBE"]
 }
 
 resource "aws_lakeformation_permissions" "grant_account_database" {
@@ -27,6 +29,7 @@ resource "aws_lakeformation_permissions" "grant_account_database" {
   database {
     name = "staged_fms_test_dbt"
   }
+  permissions_with_grant_option = ["DESCRIBE"]
 }
 
 resource "aws_lakeformation_permissions" "s3_bucket_permissions_for_ap" {
