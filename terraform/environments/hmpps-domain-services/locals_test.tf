88a45eaebaba8ac60a864be565645b607ca9a424
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-03-18 11:42:52 +0000

    test instance, make sure it can be moved to correct OU

--- terraform/environments/hmpps-domain-services/locals_test.tf
+++ terraform/environments/hmpps-domain-services/locals_test.tf
@@ -130,16 +130,21 @@ locals {
       })
 
       # testing only do not use
-      # t2-jump2022-2 = merge(local.ec2_instances.jumpserver, {
-      #   config = merge(local.ec2_instances.jumpserver.config, {
-      #     ami_name          = "hmpps_windows_server_2022_release_2025-*"
-      #     availability_zone = "eu-west-2b"
-      #   })
-      #   tags = merge(local.ec2_instances.jumpserver.tags, {
-      #     domain-name = "azure.noms.root"
-      #   })
-      #   cloudwatch_metric_alarms = null
-      # })
+      t2-jump2022-2 = merge(local.ec2_instances.jumpserver, {
+        config = merge(local.ec2_instances.jumpserver.config, {
+          ami_name          = "hmpps_windows_server_2022_release_2025-*"
+          availability_zone = "eu-west-2b"
+          user_data_raw = base64encode(templatefile(
+            "../../modules/baseline_presets/ec2-user-data/user-data-pwsh.yaml.tftpl", {
+              branch = "TM/TM-1080/move-rds-instances-to-their-own-ou"
+            }
+          ))
+        })
+        tags = merge(local.ec2_instances.jumpserver.tags, {
+          domain-name = "azure.noms.root"
+        })
+        cloudwatch_metric_alarms = null
+      })
     }
 
     fsx_windows = {
