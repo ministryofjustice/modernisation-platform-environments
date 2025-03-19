8001838fa80bedb93ba9fe2b1f2f1c0b60defa52
Toby Petty <tobypetty1@gmail.com>
2025-03-19 14:49:45 +0000

    Remove pip_tmp_dir reference that points to non existent path
    I think this was taken from the example here https://github.com/terraform-aws-modules/terraform-aws-lambda/blob/84dfbfddf9483bc56afa0aff516177c03652f0c7/examples/build-package/main.tf#L40 where such a path does exist within source control

--- terraform/environments/analytical-platform-ingestion/modules/dms/metadata_generator.tf
+++ terraform/environments/analytical-platform-ingestion/modules/dms/metadata_generator.tf
@@ -165,7 +165,6 @@ module "metadata_generator" {
 
   source_path = [{
     path             = "${path.module}/lambda-functions/metadata_generator/main.py"
-    pip_tmp_dir      = "${path.module}/lambda-functions/metadata_generator/fixtures"
     pip_requirements = "${path.module}/lambda-functions/metadata_generator/requirements.txt"
   }]
 
