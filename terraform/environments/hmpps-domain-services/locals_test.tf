93e7bd93832787e79ef7a28d69d3cb100bc1a786
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-03-19 15:05:00 +0000

    redeploy test-rds-2-b

--- terraform/environments/hmpps-domain-services/locals_test.tf
+++ terraform/environments/hmpps-domain-services/locals_test.tf
@@ -146,24 +146,24 @@ locals {
         cloudwatch_metric_alarms = null
       })
       # testing only do not use
-      # test-rds-2-b = merge(local.ec2_instances.rds, {
-      #   config = merge(local.ec2_instances.rds.config, {
-      #     ami_name          = "hmpps_windows_server_2022_release_2025-*"
-      #     availability_zone = "eu-west-2b"
-      #     user_data_raw = base64encode(templatefile(
-      #       "../../modules/baseline_presets/ec2-user-data/user-data-pwsh.yaml.tftpl", {
-      #         branch = "TM/TM-1080/move-rds-instances-to-their-own-ou"
-      #       }
-      #     ))
-      #     instance_profile_policies = concat(local.ec2_instances.rds.config.instance_profile_policies, [
-      #       "Ec2SecretPolicy"]
-      #     )
-      #   })
-      #   tags = merge(local.ec2_instances.rds.tags, {
-      #     domain-name = "azure.noms.root"
-      #   })
-      #   cloudwatch_metric_alarms = null
-      # })
+      test-rds-2-b = merge(local.ec2_instances.rds, {
+        config = merge(local.ec2_instances.rds.config, {
+          ami_name          = "hmpps_windows_server_2022_release_2025-*"
+          availability_zone = "eu-west-2b"
+          user_data_raw = base64encode(templatefile(
+            "../../modules/baseline_presets/ec2-user-data/user-data-pwsh.yaml.tftpl", {
+              branch = "TM/TM-1080/move-rds-instances-to-their-own-ou"
+            }
+          ))
+          instance_profile_policies = concat(local.ec2_instances.rds.config.instance_profile_policies, [
+            "Ec2SecretPolicy"]
+          )
+        })
+        tags = merge(local.ec2_instances.rds.tags, {
+          domain-name = "azure.noms.root"
+        })
+        cloudwatch_metric_alarms = null
+      })
     }
 
     fsx_windows = {
