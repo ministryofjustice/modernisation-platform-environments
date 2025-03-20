5d8a6cdd23030251288715c98701b8fe209d6173
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-03-20 17:35:29 +0000

    CC-3203: Create EBSDB Instance

--- terraform/environments/ccms-ebs-upgrade/sandbox/ec2-oracle_ebs_db.tf
+++ terraform/environments/ccms-ebs-upgrade/sandbox/ec2-oracle_ebs_db.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "ec2_oracle_ebs" {
       user_data_replace_on_change
     ]
   }
-  user_data_replace_on_change = false
+  user_data_replace_on_change = true
   user_data = base64encode(templatefile("./templates/ec2_user_data_ebs.sh", {
     environment = "${local.environment}"
     hostname    = "ebs"
