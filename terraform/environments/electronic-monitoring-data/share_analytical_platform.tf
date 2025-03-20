c552003ee26e6dc3aea6827833b181720ad7e131
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-03-20 14:40:25 +0000

    Remove default permissions for database and table creation in Lake Formation settings

--- terraform/environments/electronic-monitoring-data/share_analytical_platform.tf
+++ terraform/environments/electronic-monitoring-data/share_analytical_platform.tf
@@ -551,20 +551,6 @@ resource "aws_lakeformation_data_lake_settings" "lake_formation" {
     try(one(data.aws_iam_roles.data_engineering_roles.arns),
     [])]
   )
-
-  # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
-  # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
-  create_database_default_permissions {
-    # These settings should replicate current behaviour: LakeFormation is Ignored
-    permissions = ["ALL"]
-    principal   = "IAM_ALLOWED_PRINCIPALS"
-  }
-
-  create_table_default_permissions {
-    # These settings should replicate current behaviour: LakeFormation is Ignored
-    permissions = ["ALL"]
-    principal   = "IAM_ALLOWED_PRINCIPALS"
-  }
 }
 
 module "share_dbs_with_roles" {
