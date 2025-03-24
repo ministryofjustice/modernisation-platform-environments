ce2a688df98c4c9de5f4059dc475eede01d4dbdb
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-03-24 15:52:22 +0000

    Add type definitions for variables in cross-account Lake Formation data filter module and create versions.tf file

--- terraform/environments/electronic-monitoring-data/modules/cross_account_lf_data_filter/variables.tf
+++ terraform/environments/electronic-monitoring-data/modules/cross_account_lf_data_filter/variables.tf
@@ -1,23 +1,29 @@
 variable "destination_account_id" {
   description = "The account ID of the destination account"
+  type        = string
 }
 
 variable "destination_account_role_arn" {
   description = "The ARN of the role in the destination account"
+  type        = string
 }
 
 variable "database_name" {
   description = "The name of the database"
+  type        = string
 }
 
 variable "table_name" {
   description = "The name of the table"
+  type        = string
 }
 
 variable "table_filter" {
-  description = " SQL filter"
+  description = "SQL filter"
+  type        = string
 }
 
 variable "data_bucket_lf_arn" {
   description = "The ARN of the data bucket in the destination account"
+  type        = string
 }
