5d28e1e41950e1d54d8e61aa727a45f45b42bc80
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2025-03-24 11:17:50 +0000

    add s3 infra

---
+++ terraform/environments/operations-engineering/s3.tf
@@ -0,0 +1,19 @@
+module "cur_v2_hourly" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.3.0"
+
+  bucket = "cur-v2-hourly"
+
+  force_destroy = true
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.cur_s3_kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
