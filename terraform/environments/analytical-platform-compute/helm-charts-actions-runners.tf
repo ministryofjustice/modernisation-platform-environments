605520ac2431597d29dc03c15836a7ab3efbeee0
Jacob Woffenden <jacob@woffenden.io>
2025-03-24 15:04:45 +0000

    Add DE Sandbox runners

--- terraform/environments/analytical-platform-compute/helm-charts-actions-runners.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-actions-runners.tf
@@ -103,6 +103,58 @@ resource "helm_release" "actions_runner_mojas_create_a_derived_table_non_spot" {
   }
 }
 
+resource "helm_release" "actions_runner_mojas_create_a_derived_table_sandbox_a" {
+  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
+
+  /* https://github.com/ministryofjustice/analytical-platform-actions-runner */
+  name       = "actions-runner-mojas-create-a-derived-table-sandbox-a"
+  repository = "oci://ghcr.io/ministryofjustice/analytical-platform-charts"
+  version    = "2.322.0"
+  chart      = "actions-runner"
+  namespace  = kubernetes_namespace.actions_runners[0].metadata[0].name
+  values = [
+    templatefile(
+      "${path.module}/src/helm/values/actions-runners/create-a-derived-table/values.yml.tftpl",
+      {
+        github_app_application_id  = jsondecode(data.aws_secretsmanager_secret_version.actions_runners_token_apc_self_hosted_runners_github_app[0].secret_string)["app_id"]
+        github_app_installation_id = jsondecode(data.aws_secretsmanager_secret_version.actions_runners_token_apc_self_hosted_runners_github_app[0].secret_string)["installation_id"]
+        github_organisation        = "moj-analytical-services"
+        github_repository          = "create-a-derived-table"
+        github_runner_labels       = "analytical-platform-sandbox-a"
+        eks_role_arn               = "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-engineering-sandboxa"]}:role/create-a-derived-table"
+      }
+    )
+  ]
+}
+
+resource "helm_release" "actions_runner_mojas_create_a_derived_table_sandbox_a_non_spot" {
+  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
+
+  /* https://github.com/ministryofjustice/analytical-platform-actions-runner */
+  name       = "actions-runner-mojas-create-a-derived-table-sandbox-a-non-spot"
+  repository = "oci://ghcr.io/ministryofjustice/analytical-platform-charts"
+  version    = "2.322.0"
+  chart      = "actions-runner"
+  namespace  = kubernetes_namespace.actions_runners[0].metadata[0].name
+  values = [
+    templatefile(
+      "${path.module}/src/helm/values/actions-runners/create-a-derived-table/values.yml.tftpl",
+      {
+        github_app_application_id  = jsondecode(data.aws_secretsmanager_secret_version.actions_runners_token_apc_self_hosted_runners_github_app[0].secret_string)["app_id"]
+        github_app_installation_id = jsondecode(data.aws_secretsmanager_secret_version.actions_runners_token_apc_self_hosted_runners_github_app[0].secret_string)["installation_id"]
+        github_organisation        = "moj-analytical-services"
+        github_repository          = "create-a-derived-table"
+        github_runner_labels       = "analytical-platform-sandbox-a-non-spot"
+        eks_role_arn               = "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/create-a-derived-table"
+      }
+    )
+  ]
+  set {
+    name  = "ephemeral.karpenter.nodePool"
+    value = "general-on-demand"
+  }
+}
+
 resource "helm_release" "actions_runner_mojas_create_a_derived_table_dpr" {
   count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
 
