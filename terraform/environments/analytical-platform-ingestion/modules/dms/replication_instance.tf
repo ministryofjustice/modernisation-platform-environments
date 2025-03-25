372e1388dc59f42a5584f7cba4f6b28fae47f323
Toby Petty <tobypetty1@gmail.com>
2025-03-25 11:12:25 +0000

    Apply infrastructure changes immediately without waiting for maintainance window, parameterise maintainance window

--- terraform/environments/analytical-platform-ingestion/modules/dms/replication_instance.tf
+++ terraform/environments/analytical-platform-ingestion/modules/dms/replication_instance.tf
@@ -57,12 +57,13 @@ resource "aws_dms_replication_subnet_group" "replication_subnet_group" {
 
 resource "aws_dms_replication_instance" "instance" {
   allocated_storage            = var.dms_replication_instance.allocated_storage
+  apply_immediately            = var.dms_replication_instance.apply_immediately
   auto_minor_version_upgrade   = true
   availability_zone            = var.dms_replication_instance.availability_zone
   engine_version               = var.dms_replication_instance.engine_version
   kms_key_arn                  = var.dms_replication_instance.kms_key_arn
   multi_az                     = var.dms_replication_instance.multi_az
-  preferred_maintenance_window = "sun:10:30-sun:14:30"
+  preferred_maintenance_window = var.dms_replication_instance.preferred_maintenance_window
   publicly_accessible          = false
   replication_instance_class   = var.dms_replication_instance.replication_instance_class
   replication_instance_id      = var.dms_replication_instance.replication_instance_id
