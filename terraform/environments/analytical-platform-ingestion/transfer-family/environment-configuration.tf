25d566a15e5946c9056ba229854fd897406a5ce3
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-03-25 18:02:04 +0000

    Move transfer server related resources to component

--- terraform/environments/analytical-platform-ingestion/transfer-family/environment-configuration.tf
+++ terraform/environments/analytical-platform-ingestion/transfer-family/environment-configuration.tf
@@ -5,14 +5,31 @@ locals {
       /* VPC */
       isolated_vpc_public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
       isolated_vpc_private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+      isolated_vpc_id              = "vpc-0902ef7a6188ef9a0"
+
+      /* Transfer Server */
+      transfer_server_hostname = "sftp.development.transfer.analytical-platform.service.justice.gov.uk"
+      transfer_server_sftp_users = {
+
+      }
+      transfer_server_sftp_users_with_egress = {
+
+      }
     }
     production = {
       /* VPC */
-      isolated_vpc_public_subnets   = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
-      isolated_vpc_private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-      connected_vpc_cidr            = "10.27.128.0/23"
-      connected_vpc_private_subnets = ["10.27.128.0/26", "10.27.128.64/26", "10.27.128.128/26"]
-      tariff_cidr                   = "10.27.80.0/21"
+      isolated_vpc_public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+      isolated_vpc_private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+      isolated_vpc_cidr            = ""
+
+      /* Transfer Server */
+      transfer_server_hostname = "sftp.development.transfer.analytical-platform.service.justice.gov.uk"
+      transfer_server_sftp_users = {
+
+      }
+      transfer_server_sftp_users_with_egress = {
+
+      }
     }
   }
 }
