e07508daa87b2cfdf6d0292700f28a71d09696b2
Mark Butler <mgb777@outlook.com>
2025-03-25 10:33:58 +0000

    Fine tune scaling up and down in 2 steps Additional SCA analysis issues

--- terraform/environments/dacp/rds.tf
+++ terraform/environments/dacp/rds.tf
@@ -3,23 +3,26 @@ resource "aws_db_instance" "dacp_db" {
   #checkov:skip=CKV_AWS_118: "Ensure that enhanced monitoring is enabled for Amazon RDS instances" - false error
   #checkov:skip=CKV_AWS_157: "Ensure that RDS instances have Multi-AZ enabled"
   #checkov:skip=CKV_AWS_354: "Ensure RDS Performance Insights are encrypted using KMS CMKs"
-  count                       = local.is-development ? 0 : 1
-  allocated_storage           = local.application_data.accounts[local.environment].allocated_storage
-  db_name                     = local.application_data.accounts[local.environment].db_name
-  storage_type                = local.application_data.accounts[local.environment].storage_type
-  engine                      = local.application_data.accounts[local.environment].engine
-  identifier                  = local.application_data.accounts[local.environment].identifier
-  engine_version              = local.application_data.accounts[local.environment].engine_version
-  instance_class              = local.application_data.accounts[local.environment].instance_class
-  username                    = local.application_data.accounts[local.environment].db_username
-  password                    = random_password.password.result
-  skip_final_snapshot         = true
-  publicly_accessible         = false
-  vpc_security_group_ids      = [aws_security_group.postgresql_db_sc[0].id]
-  db_subnet_group_name        = aws_db_subnet_group.dbsubnetgroup.name
-  allow_major_version_upgrade = true
-  ca_cert_identifier          = "rds-ca-rsa2048-g1"
-  apply_immediately           = true
+  count                           = local.is-development ? 0 : 1
+  allocated_storage               = local.application_data.accounts[local.environment].allocated_storage
+  db_name                         = local.application_data.accounts[local.environment].db_name
+  storage_type                    = local.application_data.accounts[local.environment].storage_type
+  engine                          = local.application_data.accounts[local.environment].engine
+  identifier                      = local.application_data.accounts[local.environment].identifier
+  engine_version                  = local.application_data.accounts[local.environment].engine_version
+  instance_class                  = local.application_data.accounts[local.environment].instance_class
+  username                        = local.application_data.accounts[local.environment].db_username
+  password                        = random_password.password.result
+  skip_final_snapshot             = true
+  publicly_accessible             = false
+  vpc_security_group_ids          = [aws_security_group.postgresql_db_sc[0].id]
+  db_subnet_group_name            = aws_db_subnet_group.dbsubnetgroup.name
+  allow_major_version_upgrade     = false
+  auto_minor_version_upgrade      = true
+  ca_cert_identifier              = "rds-ca-rsa2048-g1"
+  apply_immediately               = true
+  copy_tags_to_snapshot           = true
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 }
 
 resource "aws_db_subnet_group" "dbsubnetgroup" {
@@ -68,21 +71,23 @@ resource "aws_db_instance" "dacp_db_dev" {
   #checkov:skip=CKV_AWS_129: "Ensure that respective logs of Amazon Relational Database Service (Amazon RDS) are enabled"
   #checkov:skip=CKV_AWS_157: "Ensure that RDS instances have Multi-AZ enabled"
   #checkov:skip=CKV_AWS_353: "Ensure that RDS instances have performance insights enabled"
-  count                       = local.is-development ? 1 : 0
-  allocated_storage           = local.application_data.accounts[local.environment].allocated_storage
-  db_name                     = local.application_data.accounts[local.environment].db_name
-  storage_type                = local.application_data.accounts[local.environment].storage_type
-  engine                      = local.application_data.accounts[local.environment].engine
-  identifier                  = local.application_data.accounts[local.environment].identifier
-  engine_version              = local.application_data.accounts[local.environment].engine_version
-  instance_class              = local.application_data.accounts[local.environment].instance_class
-  username                    = local.application_data.accounts[local.environment].db_username
-  password                    = random_password.password.result
-  skip_final_snapshot         = true
-  publicly_accessible         = true
-  vpc_security_group_ids      = [aws_security_group.postgresql_db_sc_dev[0].id]
-  db_subnet_group_name        = aws_db_subnet_group.dbsubnetgroup.name
-  allow_major_version_upgrade = true
+  count                          = local.is-development ? 1 : 0
+  allocated_storage               = local.application_data.accounts[local.environment].allocated_storage
+  db_name                         = local.application_data.accounts[local.environment].db_name
+  storage_type                    = local.application_data.accounts[local.environment].storage_type
+  engine                          = local.application_data.accounts[local.environment].engine
+  identifier                      = local.application_data.accounts[local.environment].identifier
+  engine_version                  = local.application_data.accounts[local.environment].engine_version
+  instance_class                  = local.application_data.accounts[local.environment].instance_class
+  username                        = local.application_data.accounts[local.environment].db_username
+  password                        = random_password.password.result
+  skip_final_snapshot             = true
+  publicly_accessible             = true
+  vpc_security_group_ids          = [aws_security_group.postgresql_db_sc_dev[0].id]
+  db_subnet_group_name            = aws_db_subnet_group.dbsubnetgroup.name
+  allow_major_version_upgrade     = false
+  auto_minor_version_upgrade      = true
+  copy_tags_to_snapshot           = true
 }
 
 resource "aws_security_group" "postgresql_db_sc_dev" {
