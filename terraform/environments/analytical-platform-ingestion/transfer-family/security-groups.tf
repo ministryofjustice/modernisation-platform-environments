ca519eaf59cfbb9b139fb5d06d2d42ce614bdc1c
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-03-26 16:49:29 +0000

    Change SG description

--- terraform/environments/analytical-platform-ingestion/transfer-family/security-groups.tf
+++ terraform/environments/analytical-platform-ingestion/transfer-family/security-groups.tf
@@ -1,6 +1,16 @@
 resource "aws_security_group" "transfer_server" {
   description = "Security Group for Transfer Server"
   name        = "transfer-family-server"
-  vpc_id      = local.environment_configuration.isolated_vpc_id
+  vpc_id      = data.aws_vpc.isolated.id
   tags        = local.tags
+}
+
+resource "aws_security_group_rule" "this" {
+  description       = "Allow inbound SFTP traffic to Transfer Server"
+  type              = "ingress"
+  from_port         = 2222
+  to_port           = 2222
+  protocol          = "tcp"
+  cidr_blocks       = local.all_cidr_blocks
+  security_group_id = resource.aws_security_group.transfer_server.id
 }
