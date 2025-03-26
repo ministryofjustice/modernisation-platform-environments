02fa3a8c4bfd9b801b8a0332a20909fb7c26fabe
Timi Olubusi <timiolubusi@icloud.com>
2025-03-26 10:31:45 +0000

    Removing the application_data type due to error

--- terraform/environments/tribunals/modules/tribunal_ftp/variables.tf
+++ terraform/environments/tribunals/modules/tribunal_ftp/variables.tf
@@ -72,27 +72,6 @@ variable "documents_location" {
 }
 
 variable "application_data" {
-  type = map(object({
-      allocated_storage         = string
-      storage_type              = string
-      db_identifier             = string
-      engine                    = string
-      engine_version            = string
-      instance_class            = string
-      username                  = string
-      dms_source_db             = string
-      curserver                 = string
-      support_team              = string
-      support_email             = string
-      server_port_1             = string
-      task_definition_volume    = string
-      server_port               = number
-      app_count                 = number
-      appscaling_min_capacity   = number
-      appscaling_max_capacity   = number
-      ecs_scaling_cpu_threshold = number
-      ecs_scaling_mem_threshold = number
-    }))
 }
 
 variable "tags" {
