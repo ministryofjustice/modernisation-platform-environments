c94ad83d76e61ba6cd824cd16d35beb65d4a1a42
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-03-26 09:33:00 +0000

    Add transfer server to component

--- terraform/environments/analytical-platform-ingestion/transfer-family/transfer-servers.tf
+++ terraform/environments/analytical-platform-ingestion/transfer-family/transfer-servers.tf
@@ -1,31 +1,33 @@
 
-# resource "aws_transfer_server" "this" {
-#   protocols              = ["SFTP"]
-#   identity_provider_type = "SERVICE_MANAGED"
-#   domain                 = "S3"
+resource "aws_transfer_server" "this" {
+  protocols              = ["SFTP"]
+  identity_provider_type = "SERVICE_MANAGED"
+  domain                 = "S3"
 
-#   endpoint_type = "VPC"
-#   endpoint_details {
-#     vpc_id     = module.isolated_vpc.vpc_id
-#     subnet_ids = module.isolated_vpc.public_subnets
-#     address_allocation_ids = [
-#       aws_eip.transfer_server[0].id,
-#       aws_eip.transfer_server[1].id,
-#       aws_eip.transfer_server[2].id,
-#     ]
-#     security_group_ids = [
-#       aws_security_group.transfer_server.id
-#     ]
-#   }
+  endpoint_type = "VPC"
+  endpoint_details {
+    # vpc_id     = module.isolated_vpc.vpc_id
+    # subnet_ids = module.isolated_vpc.public_subnets
+    vpc_id     = local.environment_configuration.isolated_vpc_id
+    subnet_ids = local.environment_configuration.isolated_vpc_public_subnets
+    address_allocation_ids = [
+      aws_eip.transfer_server[0].id,
+      aws_eip.transfer_server[1].id,
+      aws_eip.transfer_server[2].id,
+    ]
+    security_group_ids = [
+      aws_security_group.transfer_server.id
+    ]
+  }
 
-#   security_policy_name = "TransferSecurityPolicy-2024-01"
+  security_policy_name = "TransferSecurityPolicy-2024-01"
 
-#   logging_role                = module.transfer_server_iam_role.iam_role_arn
-#   structured_log_destinations = ["${module.transfer_structured_logs.cloudwatch_log_group_arn}:*"]
-# }
+  logging_role                = module.transfer_server_iam_role.iam_role_arn
+  structured_log_destinations = ["${module.transfer_structured_logs.cloudwatch_log_group_arn}:*"]
+}
 
-# resource "aws_transfer_tag" "this" {
-#   resource_arn = aws_transfer_server.this.arn
-#   key          = "aws:transfer:customHostname"
-#   value        = local.environment_configuration.transfer_server_hostname
-# }
+resource "aws_transfer_tag" "this" {
+  resource_arn = aws_transfer_server.this.arn
+  key          = "aws:transfer:customHostname"
+  value        = local.environment_configuration.transfer_server_hostname
+}
