79712e96146ca28e960e7ce4434e9032912e2939
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-03-26 12:23:01 +0000

    Testing Egress User

--- terraform/environments/analytical-platform-ingestion/transfer-family/transfer-users.tf
+++ terraform/environments/analytical-platform-ingestion/transfer-family/transfer-users.tf
@@ -14,20 +14,20 @@ module "sftp_users" {
   supplier_data_kms_key          = module.supplier_data_kms.key_arn
 }
 
-# module "sftp_users_with_egress" {
-#   for_each = local.environment_configuration.transfer_server_sftp_users_with_egress
+module "sftp_users_with_egress" {
+  for_each = local.environment_configuration.transfer_server_sftp_users_with_egress
 
-#   source = "./modules/transfer-family/user-with-egress"
+  source = "./modules/transfer-family/user-with-egress"
 
-#   name        = each.key
-#   ssh_key     = each.value.ssh_key
-#   cidr_blocks = each.value.cidr_blocks
+  name        = each.key
+  ssh_key     = each.value.ssh_key
+  cidr_blocks = each.value.cidr_blocks
 
-#   transfer_server                = aws_transfer_server.this.id
-#   transfer_server_security_group = aws_security_group.transfer_server.id
-#   landing_bucket                 = module.landing_bucket.s3_bucket_id
-#   landing_bucket_kms_key         = module.s3_landing_kms.key_arn
-#   egress_bucket                  = each.value.egress_bucket
-#   egress_bucket_kms_key          = each.value.egress_bucket_kms_key
-#   supplier_data_kms_key          = module.supplier_data_kms.key_arn
-# }
+  transfer_server                = aws_transfer_server.this.id
+  transfer_server_security_group = aws_security_group.transfer_server.id
+  landing_bucket                 = module.transfer_landing_bucket.s3_bucket_id
+  landing_bucket_kms_key         = module.s3_transfer_landing_kms.key_arn
+  egress_bucket                  = each.value.egress_bucket
+  egress_bucket_kms_key          = each.value.egress_bucket_kms_key
+  supplier_data_kms_key          = module.supplier_data_kms.key_arn
+}
