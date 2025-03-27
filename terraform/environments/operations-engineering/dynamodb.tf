746d4ff67fb45cc11b08108022715c9add8306f9
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2025-03-27 11:22:58 +0000

    s3 and dynamodb tf state infra

---
+++ terraform/environments/operations-engineering/dynamodb.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "github_repos_state_lock" {
+  name         = "github_repos_state_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  ttl {
+    attribute_name = "Expires"
+    enabled        = true
+  }
+}
--- terraform/environments/operations-engineering/s3.tf
+++ terraform/environments/operations-engineering/s3.tf
@@ -1,3 +1,5 @@
+# CUR v2 bucket
+
 data "aws_iam_policy_document" "s3_root_account_write_policy" {
   #checkov:skip=CKV_AWS_356:resource "*" limited by condition
   statement {
@@ -21,6 +23,30 @@ module "cur_v2_hourly" {
   bucket = "cur-v2-hourly"
 
   force_destroy = true
+  
+  attach_deny_insecure_transport_policy = true
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.cur_s3_kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+# GitHub repositories Terraform state bucket
+
+module "github_repos_state_bucket" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.3.0"
+
+  bucket = "github_repos_state_bucket"
+
+  force_destroy = true
   attach_policy = true
   policy = data.aws_iam_policy_document.s3_root_account_write_policy.json
   
@@ -29,7 +55,7 @@ module "cur_v2_hourly" {
   server_side_encryption_configuration = {
     rule = {
       apply_server_side_encryption_by_default = {
-        kms_master_key_id = module.cur_s3_kms.key_arn
+        kms_master_key_id = module.github_repos_s3_kms.key_arn
         sse_algorithm     = "aws:kms"
       }
     }
