d87e8f94659b25992832373b9d266f5ec8b8574f
Toby Petty <tobypetty1@gmail.com>
2025-03-27 10:52:31 +0000

    Expose the ARNs of the lambda functions within the DMS module

--- terraform/environments/analytical-platform-ingestion/modules/dms/outputs.tf
+++ terraform/environments/analytical-platform-ingestion/modules/dms/outputs.tf
@@ -9,3 +9,14 @@ output "dms_source_role_arn" {
   description = "The ARN for the AWS role created for the DMS source endpoint"
   sensitive = true
 }
+
+output "metadata_generator_lambda_arn" {
+  value = module.metadata_generator.lambda_function_arn
+  description = "The ARN for the metadata_generator AWS Lambda function"
+}
+
+output "validation_lambda_arn" {
+  value = module.validation_lambda_function.lambda_function_arn
+  description = "The ARN for the validation AWS Lambda function"
+}
+
