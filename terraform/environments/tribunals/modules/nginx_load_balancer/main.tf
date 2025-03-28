a1423c7e4fba7fe3162b2bb78027d5e1cb7e2818
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2025-03-28 16:13:34 +0000

    add required_providers to a few places

--- terraform/environments/tribunals/modules/nginx_load_balancer/main.tf
+++ terraform/environments/tribunals/modules/nginx_load_balancer/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.0.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
 }
 
 # tfsec:ignore:aws-elb-alb-not-public
