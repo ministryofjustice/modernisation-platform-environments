aa278319a94dbbf79879bc1a3b75fff84f0199c4
ttipler <thomas.tipler@necsws.com>
2025-03-31 15:44:13 +0100

    update

--- terraform/environments/youth-justice-app-framework/modules/yjsm/main.tf
+++ terraform/environments/youth-justice-app-framework/modules/yjsm/main.tf
@@ -1,5 +1,6 @@
 #create keypair for ec2 instances
 module "key_pair" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   source  = "terraform-aws-modules/key-pair/aws"
   version = "2.0.3"
 
