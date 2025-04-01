fcea6753c43efc49246123f56ea39673ad2fb6cc
Toby Petty <tobypetty1@gmail.com>
2025-04-01 18:05:36 +0100

    Add eventbridge scheduler to schedule dms full-load for tariff

---
+++ terraform/environments/analytical-platform-ingestion/dms/eventbridge-scheduler.tf
@@ -0,0 +1,19 @@
+resource "aws_scheduler_schedule" "dms_nightly_full_load" {
+  name = "tariff-dms-nightly-full-load"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(30 0 * * ? *)" # 12:30 every day, to capture submissions up till midnight for monthly reporting
+
+  target {
+    arn      = module.cica_dms_tariff_dms_implementation.dms_full_load_task_arn
+    role_arn = aws_iam_role.eventbridge_dms_full_load_task_role.arn
+
+    input = jsonencode({
+      ReplicationTaskArn       = module.cica_dms_tariff_dms_implementation.dms_full_load_task_arn
+      StartReplicationTaskType = "restart-processing"
+    })
+  }
+}
