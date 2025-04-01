e03e703de2a13a29de25af37eedab20290fc94f7
Toby Petty <tobypetty1@gmail.com>
2025-04-01 18:04:13 +0100

    Expose full-load dms task arn from dms module

--- terraform/environments/analytical-platform-ingestion/modules/dms/outputs.tf
+++ terraform/environments/analytical-platform-ingestion/modules/dms/outputs.tf
@@ -10,6 +10,11 @@ output "dms_source_role_arn" {
   sensitive = true
 }
 
+output "dms_full_load_task_arn" {
+  value = aws_dms_replication_task.full_load_replication_task.replication_task_arn
+  description = "The ARN for the AWS DMS full-load task ARN"
+}
+
 output "metadata_generator_lambda_arn" {
   value = module.metadata_generator.lambda_function_arn
   description = "The ARN for the metadata_generator AWS Lambda function"
