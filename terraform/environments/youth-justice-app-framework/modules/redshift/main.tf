de031ce2bf6204942538e406a2765bf17189bf3c
David Seekins <david.seekins@necsws.com>
2025-04-01 14:13:13 +0100

    Resdshift Changes

--- terraform/environments/youth-justice-app-framework/modules/redshift/main.tf
+++ terraform/environments/youth-justice-app-framework/modules/redshift/main.tf
@@ -7,11 +7,14 @@ resource "aws_redshiftserverless_namespace" "default" {
     aws_iam_role.redshift.arn, aws_iam_role.ycs-team.arn, aws_iam_role.yjb-moj-team.arn, aws_iam_role.yjb-team.arn
   ]
 
-  kms_key_id = var.kms_key_arn
+  #kms_key_id = var.kms_key_arn
+
+  log_exports = ["userlog", "connectionlog", "useractivitylog"]
 
   tags = merge(local.all_tags,
     { Name = local.namespace_name }
   )
+
 }
 
 # Create the Redshift Serverless Workgroup
