3951fa370a9499d29054bfd433733b2e608ccb3a
ranbeersingh1 <43067999+ranbeersingh1@users.noreply.github.com>
2025-04-02 16:08:00 +0100

    Allow Oracle statistics bucket access from test to dev

--- terraform/environments/delius-core/modules/components/oracle_db_shared/s3.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/s3.tf
@@ -133,7 +133,7 @@ data "aws_iam_policy_document" "oracledb_backup_bucket_access" {
 
 
 data "aws_iam_policy_document" "oracle_remote_statistics_bucket_access" {
-  count = lookup(local.oracle_statistics_map[var.env_name], "source_id", null) != null ? 1 : 0
+  count = lookup(local.oracle_statistics_map[var.env_name], "source_account_id", null) != null ? 1 : 0
   statement {
     sid    = "allowAccessToListOracleStatistics${title(local.oracle_statistics_map[var.env_name]["source_environment"])}Bucket"
     effect = "Allow"
