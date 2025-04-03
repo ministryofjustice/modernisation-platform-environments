65937d5b8cdce14fdcca65394b885fba78c1afbf
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-04-03 10:38:41 +0100

    deploy jumpserver

--- terraform/environments/hmpps-domain-services/locals_test.tf
+++ terraform/environments/hmpps-domain-services/locals_test.tf
@@ -146,24 +146,24 @@ locals {
         cloudwatch_metric_alarms = null
       })
       # testing only do not use
-      test-rds-2-b = merge(local.ec2_instances.rds, {
-        config = merge(local.ec2_instances.rds.config, {
-          ami_name          = "hmpps_windows_server_2022_release_2025-*"
-          availability_zone = "eu-west-2b"
-          user_data_raw = base64encode(templatefile(
-            "../../modules/baseline_presets/ec2-user-data/user-data-pwsh.yaml.tftpl", {
-              branch = "TM/TM-1080/move-rds-instances-to-their-own-ou"
-            }
-          ))
-          instance_profile_policies = concat(local.ec2_instances.rds.config.instance_profile_policies, [
-            "Ec2SecretPolicy"]
-          )
-        })
-        tags = merge(local.ec2_instances.rds.tags, {
-          domain-name = "azure.noms.root"
-        })
-        cloudwatch_metric_alarms = null
-      })
+      # test-rds-2-b = merge(local.ec2_instances.rds, {
+      #   config = merge(local.ec2_instances.rds.config, {
+      #     ami_name          = "hmpps_windows_server_2022_release_2025-*"
+      #     availability_zone = "eu-west-2b"
+      #     user_data_raw = base64encode(templatefile(
+      #       "../../modules/baseline_presets/ec2-user-data/user-data-pwsh.yaml.tftpl", {
+      #         branch = "TM/TM-1080/move-rds-instances-to-their-own-ou"
+      #       }
+      #     ))
+      #     instance_profile_policies = concat(local.ec2_instances.rds.config.instance_profile_policies, [
+      #       "Ec2SecretPolicy"]
+      #     )
+      #   })
+      #   tags = merge(local.ec2_instances.rds.tags, {
+      #     domain-name = "azure.noms.root"
+      #   })
+      #   cloudwatch_metric_alarms = null
+      # })
     }
 
     fsx_windows = {
@@ -217,19 +217,19 @@ locals {
               { ec2_instance_name = "test-rdgw-1-a" },
             ]
           })
-          test-rds-1-https = merge(local.lbs.public.instance_target_groups.https, {
-            attachments = [
-              { ec2_instance_name = "test-rds-2-b" },
-            ]
-          })
+          # test-rds-1-https = merge(local.lbs.public.instance_target_groups.https, {
+          #   attachments = [
+          #     { ec2_instance_name = "test-rds-2-b" },
+          #   ]
+          # })
         }
         listeners = merge(local.lbs.public.listeners, {
           https = merge(local.lbs.public.listeners.https, {
             alarm_target_group_names = [
               "test-rdgw-1-http",
-              "test-rds-1-https",
+#              "test-rds-1-https",
             ]
-            certificate_names_or_arns = ["remote_desktop_wildcard_cert"]
+#            certificate_names_or_arns = ["remote_desktop_wildcard_cert"]
             rules = {
               test-rdgw-1-http = {
                 priority = 100
@@ -246,20 +246,20 @@ locals {
                   }
                 }]
               }
-              test-rds-1-https = {
-                priority = 200
-                actions = [{
-                  type              = "forward"
-                  target_group_name = "test-rds-1-https"
-                }]
-                conditions = [{
-                  host_header = {
-                    values = [
-                      "rdweb1.test.hmpps-domain.service.justice.gov.uk"
-                    ]
-                  }
-                }]                
-              }
+              # test-rds-1-https = {
+              #   priority = 200
+              #   actions = [{
+              #     type              = "forward"
+              #     target_group_name = "test-rds-1-https"
+              #   }]
+              #   conditions = [{
+              #     host_header = {
+              #       values = [
+              #         "rdweb1.test.hmpps-domain.service.justice.gov.uk"
+              #       ]
+              #     }
+              #   }]                
+              # }
             }
           })
         })
@@ -277,7 +277,7 @@ locals {
       "test.hmpps-domain.service.justice.gov.uk" = {
         lb_alias_records = [
           { name = "rdgateway1", type = "A", lbs_map_key = "public" },
-          { name = "rdweb1", type = "A", lbs_map_key = "public" },
+          # { name = "rdweb1", type = "A", lbs_map_key = "public" },
         ]
       }
     }
