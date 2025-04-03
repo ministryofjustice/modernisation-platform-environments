351bd72cf651fbb56d4b54ed7d112d1f9ea2fe04
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2025-04-03 10:05:33 +0100

    revert changes

--- terraform/environments/analytical-platform-compute/quicksight/platform_backend.tf
+++ terraform/environments/analytical-platform-compute/quicksight/platform_backend.tf
@@ -9,6 +9,4 @@ terraform {
     key                  = "terraform.tfstate"
     region               = "eu-west-2"
     use_lockfile         = true
-    workspace_key_prefix = "environments/members/analytical-platform-compute/quicksight" # This will store the object as environments/members/analytical-platform-compute/${workspace}/terraform.tfstate
-  }
-}
+    workspace_key_prefix = "environments/members/analytical-platform-compute/quicksight" # This will store the object as environments/members/<application>/<component>/${workspace}/terraform.tfstate
--- terraform/environments/analytical-platform-compute/quicksight/platform_locals.tf
+++ terraform/environments/analytical-platform-compute/quicksight/platform_locals.tf
@@ -1,6 +1,7 @@
 locals {
 
   application_name = "analytical-platform-compute"
+  component_name   = "quicksight"
 
   environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
 
