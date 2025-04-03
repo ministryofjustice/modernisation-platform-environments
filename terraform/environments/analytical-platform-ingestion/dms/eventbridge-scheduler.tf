bc1f3b2efb93b940218081eccbd9609233e4f9de
Toby Petty <tobypetty1@gmail.com>
2025-04-03 12:17:41 +0100

    Fix (hopefully) the universal target configuration for event bridge scheduler
    This stack overflow post was very helpful https://stackoverflow.com/questions/76653967/how-to-schedule-a-start-of-dms-task-by-aws-eventbridge-via-cloudformation-templa

--- terraform/environments/analytical-platform-ingestion/dms/eventbridge-scheduler.tf
+++ terraform/environments/analytical-platform-ingestion/dms/eventbridge-scheduler.tf
@@ -8,12 +8,12 @@ resource "aws_scheduler_schedule" "dms_nightly_full_load" {
   schedule_expression = "cron(30 0 * * ? *)" # 12:30 every day, to capture submissions up till midnight for monthly reporting
 
   target {
-    arn      = module.cica_dms_tariff_dms_implementation.dms_full_load_task_arn
+    arn      = "arn:aws:scheduler:::aws-sdk:databasemigration:startReplicationTask"
     role_arn = aws_iam_role.eventbridge_dms_full_load_task_role.arn
 
     input = jsonencode({
       ReplicationTaskArn       = module.cica_dms_tariff_dms_implementation.dms_full_load_task_arn
-      StartReplicationTaskType = "restart-processing"
+      StartReplicationTaskType = "start-replication"
     })
   }
 }
