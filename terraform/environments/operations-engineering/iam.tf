71d2b923305e4ac83cba7a6b23dc95191a215571
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-04-03 15:20:41 +0100

    auth0 backend specific policy and policy doc

--- terraform/environments/operations-engineering/iam.tf
+++ terraform/environments/operations-engineering/iam.tf
@@ -42,7 +42,7 @@ resource "aws_iam_role" "auth0_tfstate_role" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role_policy_document.json
 }
 
-data "aws_iam_policy_document" "s3_access_policy_document" {
+data "aws_iam_policy_document" "auth0_s3_access_policy_document" {
   version = "2012-10-17"
 
   statement {
@@ -61,12 +61,12 @@ data "aws_iam_policy_document" "s3_access_policy_document" {
   }
 }
 
-resource "aws_iam_policy" "s3_access_policy" {
+resource "aws_iam_policy" "auth0_s3_access_policy" {
   name        = "auth0_role_prod_s3_access_policy"
-  policy      = data.aws_iam_policy_document.s3_access_policy_document.json
+  policy      = data.aws_iam_policy_document.auth0_s3_access_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "auth0_role_perms_attachment" {
   role       = aws_iam_role.auth0_tfstate_role.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
+  policy_arn = aws_iam_policy.auth0_s3_access_policy.arn
 }
