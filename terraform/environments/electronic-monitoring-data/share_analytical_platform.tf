9fd0776f982560bb21be02c62820da4f072989e9
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-04-07 12:22:14 +0100

    copy and make new cadt role

--- terraform/environments/electronic-monitoring-data/share_analytical_platform.tf
+++ terraform/environments/electronic-monitoring-data/share_analytical_platform.tf
@@ -147,6 +147,20 @@ data "aws_iam_policy_document" "dataapi_cross_assume" {
       variable = "oidc.eks.eu-west-2.amazonaws.com/id/${jsondecode(data.aws_secretsmanager_secret_version.dbt_secrets.secret_string)["oidc_cluster_identifier"]}:aud"
     }
   }
+}
+
+data "aws_iam_policy_document" "airflow_cadt_cross_assume" {
+  #checkov:skip=CKV_AWS_110:Ensure IAM policies does not allow privilege escalation
+  #checkov:skip=CKV_AWS_358:OIDC trust policies only allows actions from a specific known organization Already
+  #checkov:skip=CKV_AWS_107:Ensure IAM policies does not allow credentials exposure
+  #checkov:skip=CKV_AWS_111:Ensure IAM policies does not allow write access without constraints
+  #checkov:skip=CKV_AWS_356
+  #checkov:skip=CKV_AWS_109
+  #checkov:skip=CKV_AWS_1
+  #checkov:skip=CKV_AWS_283
+  #checkov:skip=CKV_AWS_49
+  #checkov:skip=CKV_AWS_108
+
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -167,6 +181,27 @@ data "aws_iam_policy_document" "dataapi_cross_assume" {
     }
   }
 }
+
+resource "aws_iam_role" "airflow_cadt_cross_assume_role" {
+  #checkov:skip=CKV_AWS_61:Ensure IAM policies does not allow data exfiltration
+  #checkov:skip=CKV_AWS_60:Ensure IAM role allows only specific services or principals to assume it
+  #checkov:skip=CKV_AWS_274:Disallow IAM roles, users, and groups from using the AWS AdministratorAccess policy
+
+  name                  = "airflow-dev-cadet-deploy-emds${local.suffix}"
+  description           = "Role to use for Airflow CADT"
+  assume_role_policy    = data.aws_iam_policy_document.airflow_cadt_cross_assume.json
+  force_detach_policies = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name           = "airflow-dev-cadet-deploy-emds${local.suffix}"
+      Resource_Type  = "iam"
+      Resource_Group = "AP"
+    }
+  )
+}
+
 
 # Role used in create a derived table 
 resource "aws_iam_role" "dataapi_cross_role" {
