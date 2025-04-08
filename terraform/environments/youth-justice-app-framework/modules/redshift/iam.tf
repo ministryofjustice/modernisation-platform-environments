b7e7a00f47d39643f3900f5abb85a06d2351637b
David Seekins <david.seekins@necsws.com>
2025-04-08 08:39:45 +0100

    Enable assess to decrypt redshift secrets credentials

--- terraform/environments/youth-justice-app-framework/modules/redshift/iam.tf
+++ terraform/environments/youth-justice-app-framework/modules/redshift/iam.tf
@@ -42,7 +42,8 @@ resource "aws_iam_policy" "rds-aurora-postgres-secret" {
           "secretsmanager:GetResourcePolicy",
           "secretsmanager:GetSecretValue",
           "secretsmanager:DescribeSecret",
-          "secretsmanager:ListSecretVersionIds"
+          "secretsmanager:ListSecretVersionIds",
+          
         ],
         "Resource" : [
           var.rds_redshift_secret_arn
@@ -56,6 +57,14 @@ resource "aws_iam_policy" "rds-aurora-postgres-secret" {
           "secretsmanager:ListSecrets"
         ],
         "Resource" : "*"
+      },
+      {
+        "Sid" : "AccessSecretKey",
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:Decrypt"
+        ],
+        "Resource" : var.kms_key_arn
       }
     ]
   })
