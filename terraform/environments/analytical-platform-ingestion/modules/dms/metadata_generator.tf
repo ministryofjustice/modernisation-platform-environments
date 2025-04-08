b647202a8bb225ee29bda13d482890bfe8605e86
Toby Petty <tobypetty1@gmail.com>
2025-04-08 16:01:52 +0100

    Add custom kms key for internal DMS buckets
    This might require regenerating the existing buckets in dev
    
    Use this KMS key for raw_hist unless specified outside module, then assume KMS being managed externally too

--- terraform/environments/analytical-platform-ingestion/modules/dms/metadata_generator.tf
+++ terraform/environments/analytical-platform-ingestion/modules/dms/metadata_generator.tf
@@ -40,7 +40,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "validation_metada
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = module.bucket_kms.key_arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
