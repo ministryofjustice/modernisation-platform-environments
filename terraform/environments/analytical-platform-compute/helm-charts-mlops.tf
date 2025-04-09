eac137ad7214754088d4d94c99ef3eee17696c19
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-04-09 13:12:16 +0100

    Update mlflow helm chart version

--- terraform/environments/analytical-platform-compute/helm-charts-mlops.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-mlops.tf
@@ -2,7 +2,7 @@ resource "helm_release" "mlflow" {
   /* https://github.com/ministryofjustice/analytical-platform-mlflow */
   name       = "mlflow"
   repository = "oci://ghcr.io/ministryofjustice/analytical-platform-charts"
-  version    = "2.21.3-rc5"
+  version    = "2.21.3-rc6"
   chart      = "mlflow"
   namespace  = kubernetes_namespace.mlflow.metadata[0].name
   values = [
