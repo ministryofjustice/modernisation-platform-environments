5f49069cea35900569c681ea86638ecbd0150ada
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2025-04-09 15:49:32 +0100

    CC-3346: Fixed typos.

--- terraform/environments/ccms-ebs-upgrade/sandbox/s3.tf
+++ terraform/environments/ccms-ebs-upgrade/sandbox/s3.tf
@@ -292,17 +292,18 @@ resource "aws_s3_bucket_public_access_block" "ccms_ebs_shared" {
 }
 
 # Development
+
 moved {
-  from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-upgrade-development-logging"]
-  to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-upgrade-development-logging"]
+  from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-sandbox-logging"]
+  to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-sandbox-logging"]
 }
 
 moved {
-  from = module.s3-bucket-artefacts.aws_s3_bucket_logging.default["ccms-ebs-upgrade-development-logging"]
-  to   = module.s3-bucket-artefacts.aws_s3_bucket_logging.default_single_name["ccms-ebs-upgrade-development-logging"]
+  from = module.s3-bucket-artefacts.aws_s3_bucket_logging.default["ccms-ebs-sandbox-logging"]
+  to   = module.s3-bucket-artefacts.aws_s3_bucket_logging.default_single_name["ccms-ebs-sandbox-logging"]
 }
 
 moved {
-  from = module.s3-bucket-dbbackup.aws_s3_bucket_logging.default["ccms-ebs-upgrade-development-logging"]
-  to   = module.s3-bucket-dbbackup.aws_s3_bucket_logging.default_single_name["ccms-ebs-upgrade-development-logging"]
+  from = module.s3-bucket-dbbackup.aws_s3_bucket_logging.default["ccms-ebs-sandbox-logging"]
+  to   = module.s3-bucket-dbbackup.aws_s3_bucket_logging.default_single_name["ccms-ebs-sandbox-logging"]
 }
