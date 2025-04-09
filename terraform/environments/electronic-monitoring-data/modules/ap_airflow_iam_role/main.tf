1056e868ec9c84291a8562893859063df16b7845
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-04-09 17:52:13 +0100

    sort out mwaa

--- terraform/environments/electronic-monitoring-data/modules/ap_airflow_iam_role/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/ap_airflow_iam_role/main.tf
@@ -5,8 +5,8 @@ locals {
     "test"          = "test"
     "development"   = "dev"
   }
-  mwaa      = var.new_airflow ? "mwaa:" : ""
-  role_name = var.new_airflow ? "airflow-${local.account_map[var.environment]}-electronic-monitoring-data-store-${var.role_name_suffix}" : "airflow-${local.account_map[var.environment]}-${var.role_name_suffix}"
+  mwaa      = var.new_airflow ? "mwaa:airflow-${local.account_map[var.environment]}-electronic-monitoring-data-store-${var.role_name_suffix}" : local.role_name
+  role_name = "airflow-${local.account_map[var.environment]}-${var.role_name_suffix}"
 }
 
 # --------------------------------------------
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "oidc_assume_role_policy" {
     }
     condition {
       test     = "StringEquals"
-      values   = ["system:serviceaccount:airflow:${local.mwaa}${local.role_name}"]
+      values   = ["system:serviceaccount:airflow:${local.mwaa}"]
       variable = "oidc.eks.eu-west-2.amazonaws.com/id/${var.secret_code}:sub"
     }
     condition {
