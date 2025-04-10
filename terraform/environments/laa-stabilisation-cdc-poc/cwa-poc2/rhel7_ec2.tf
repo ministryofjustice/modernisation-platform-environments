4aca8e74d29bdca1b8f95141c7f29d700b7ff3f3
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2025-04-10 14:11:41 +0100

    TM-1180 Fixing code for rhel7 as it is now in module 3

--- terraform/environments/laa-stabilisation-cdc-poc/cwa-poc2/rhel7_ec2.tf
+++ terraform/environments/laa-stabilisation-cdc-poc/cwa-poc2/rhel7_ec2.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "rhel7_instance_1" {
   instance_type               = local.application_data.accounts[local.environment].rhel7_instance_type
   monitoring                  = true
   vpc_security_group_ids      = [aws_security_group.rhel7_instance.id]
-  subnet_id                   = data.aws_subnet.private_subnets_a.id
+  subnet_id                   = var.data_subnet_a_id
   iam_instance_profile        = aws_iam_instance_profile.rhel7.id
   user_data_base64            = base64encode(local.rhel7_userdata)
   user_data_replace_on_change = false
