34331ed5225c89a977b931d312123a8f5f05489f
ttipler <thomas.tipler@necsws.com>
2025-04-10 11:38:38 +0100

    undo ses change

--- terraform/environments/youth-justice-app-framework/modules/ses/providers.tf
+++ terraform/environments/youth-justice-app-framework/modules/ses/providers.tf
@@ -8,15 +8,6 @@ terraform {
       source  = "hashicorp/null"
       version = "3.2.2"
     }
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2.3"
-    }
   }
   required_version = ">= 1.0.1"
 }
-
-provider "null" {}
-
-provider "external" {}
-
