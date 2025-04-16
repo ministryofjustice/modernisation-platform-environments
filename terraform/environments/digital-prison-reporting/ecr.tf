6f31a097f2bc02b4a7bd9172018810c9e9bafe2d
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2025-04-16 11:26:47 +0100

    DPR2-1499: Scan on push, image tag immutability and kms encryption

--- terraform/environments/digital-prison-reporting/ecr.tf
+++ terraform/environments/digital-prison-reporting/ecr.tf
@@ -1,10 +1,14 @@
 # Used to store our custom clamav S3 scanner image for File Transfer In/Push
 resource "aws_ecr_repository" "file_transfer_in_clamav_scanner" {
   name                 = "${local.project}-images-${local.env}/hmpps-data-hub-clamav-s3-scanner"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = false
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
   }
 
   tags = merge(
