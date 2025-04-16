ac1cf2d02ba6805f44fae1576be04baea7da02ce
Tom Ogle <tom.ogle@digital.justice.gov.uk>
2025-04-16 11:08:51 +0100

    DPR2-1499: Update comment

--- terraform/environments/digital-prison-reporting/ecr.tf
+++ terraform/environments/digital-prison-reporting/ecr.tf
@@ -1,3 +1,4 @@
+# Used to store our custom clamav S3 scanner image for File Transfer In/Push
 resource "aws_ecr_repository" "file_transfer_in_clamav_scanner" {
   name                 = "${local.project}-images-${local.env}/hmpps-data-hub-clamav-s3-scanner"
   image_tag_mutability = "MUTABLE"
