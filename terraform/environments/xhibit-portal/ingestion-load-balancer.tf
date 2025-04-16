f6704e5443142e093c337745cf302bf83be26732
Zoltan Paldi <161834373+zoltan-paldi@users.noreply.github.com>
2025-04-16 14:40:59 +0100

    chore(xhibit-portal): add Appsec-CJSE for the ITHC

--- terraform/environments/xhibit-portal/ingestion-load-balancer.tf
+++ terraform/environments/xhibit-portal/ingestion-load-balancer.tf
@@ -24,7 +24,8 @@ resource "aws_security_group_rule" "ingestion_lb_allow_web_users" {
   to_port           = 443
   protocol          = "TCP"
   cidr_blocks = [
-    "10.182.60.51/32",   # NLE CGI proxy 
+    "10.182.60.51/32",   # NLE CGI proxy
+    "80.195.27.199",     # Appsec-CJSE - Krupal ITHC
     "195.59.75.151/32",  # New proxy IPs from Prashanth for testing ingestion NLE DEV
     "195.59.75.152/32",  # New proxy IPs from Prashanth for testing ingestion NLE DEV
     "194.33.192.0/24",   # New proxy IPs from Prashanth for testing ingestion LE PROD
