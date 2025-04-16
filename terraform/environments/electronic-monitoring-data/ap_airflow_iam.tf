1a8e74dabf880c5e14603c96bab678eda67a3d72
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-04-16 09:24:19 +0100

    refactor: what was i thinking

--- terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
+++ terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
@@ -342,24 +342,25 @@ module "load_unstructured_atrium_database" {
 }
 
 
-# module "load_fms" {
-#   count  = local.is-development ? 0 : 1
-#   source = "./modules/ap_airflow_load_data_iam_role"
+module "load_fms" {
+  count  = local.is-development ? 0 : 1
+  source = "./modules/ap_airflow_load_data_iam_role"
 
-#   data_bucket_lf_resource = aws_lakeformation_resource.data_bucket.arn
-#   de_role_arn             = try(one(data.aws_iam_roles.data_engineering_roles.arns))
+  data_bucket_lf_resource = aws_lakeformation_resource.data_bucket.arn
+  de_role_arn             = try(one(data.aws_iam_roles.data_engineering_roles.arns))
 
-#   name               = "fms"
-#   environment        = local.environment
-#   database_name      = "serco-fms"
-#   path_to_data       = "/serco/fms"
-#   source_data_bucket = module.s3-raw-formatted-data-bucket.bucket
-#   secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
-#   oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
-#   athena_dump_bucket = module.s3-athena-bucket.bucket
-#   cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
-#   db_exists          = true
-# }
+  name               = "fms"
+  environment        = local.environment
+  database_name      = "serco-fms"
+  path_to_data       = "/serco/fms"
+  source_data_bucket = module.s3-raw-formatted-data-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+  db_exists          = true
+  new_airflow        = true
+}
 
 
 module "load_mdss" {
@@ -500,24 +501,4 @@ module "full_reload_fms" {
   db_exists          = true
   new_airflow        = true
   full_reload        = true
-}
-
-module "load_fms" {
-  count  = local.is-development ? 0 : 1
-  source = "./modules/ap_airflow_load_data_iam_role"
-
-  data_bucket_lf_resource = aws_lakeformation_resource.data_bucket.arn
-  de_role_arn             = try(one(data.aws_iam_roles.data_engineering_roles.arns))
-
-  name               = "fms"
-  environment        = local.environment
-  database_name      = "serco-fms"
-  path_to_data       = "/serco/fms"
-  source_data_bucket = module.s3-raw-formatted-data-bucket.bucket
-  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
-  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
-  athena_dump_bucket = module.s3-athena-bucket.bucket
-  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
-  db_exists          = true
-  new_airflow        = true
 }
