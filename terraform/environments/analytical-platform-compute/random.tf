8f7ca625c5cf99295822d66f5e8146a872cec1fd
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-04-23 10:23:06 +0000

    Exclude compute-test for dashboard_service_secret_key

--- terraform/environments/analytical-platform-compute/random.tf
+++ terraform/environments/analytical-platform-compute/random.tf
@@ -31,4 +31,5 @@ resource "random_password" "ui_app_secrets" {
 resource "random_password" "dashboard_service_secret_key" {
   length  = 32
   special = false
+  count   = terraform.workspace == "analytical-platform-compute-test" ? 0 : 1
 }
