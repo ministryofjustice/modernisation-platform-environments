238d4e1e7c72ae5518b8848c23a23b77e0a6c9a7
Toby Petty <tobypetty1@gmail.com>
2025-04-24 16:46:21 +0100

    Update tariff variable names

--- terraform/environments/analytical-platform-ingestion/dms/dms.tf
+++ terraform/environments/analytical-platform-ingestion/dms/dms.tf
@@ -1,6 +1,8 @@
 locals {
-  short_resource_name = "tariff-${local.environment}"
-  resource_name       = "cica-ap-${local.short_resource_name}"
+    tariff = {
+        short_resource_name = "tariff-${local.environment}"
+        resource_name = "cica-ap-tariff-${local.environment}"
+    }
 }
 
 module "cica_dms_tariff_dms_implementation" {
@@ -9,34 +11,34 @@ module "cica_dms_tariff_dms_implementation" {
   vpc_id      = data.aws_vpc.connected_vpc.id
   environment = local.environment
 
-  db = local.short_resource_name
+  db          = local.tariff.short_resource_name
 
   dms_replication_instance = {
-    replication_instance_id    = local.resource_name
-    subnet_cidrs               = local.environment_configuration.connected_vpc_private_subnets
-    subnet_group_name          = local.resource_name
-    allocated_storage          = 20
-    availability_zone          = data.aws_availability_zones.available.names[0]
-    engine_version             = "3.5.4"
-    kms_key_arn                = module.cica_dms_credentials_kms.key_arn
-    multi_az                   = false
-    replication_instance_class = "dms.t3.large"
-    inbound_cidr               = local.environment_configuration.tariff_cidr
-    apply_immediately          = true
+      replication_instance_id    = local.tariff.resource_name
+      subnet_cidrs               = local.environment_configuration.connected_vpc_private_subnets
+      subnet_group_name          = local.tariff.resource_name
+      allocated_storage          = 20
+      availability_zone          = data.aws_availability_zones.available.names[0]
+      engine_version             = "3.5.4"
+      kms_key_arn                = module.cica_dms_credentials_kms.key_arn
+      multi_az                   = false
+      replication_instance_class = "dms.t3.large"
+      inbound_cidr               = local.environment_configuration.tariff_cidr
+      apply_immediately          = true
   }
   dms_source = {
-    engine_name             = "oracle"
-    secrets_manager_arn     = module.cica_dms_tariff_database_credentials.secret_arn
-    secrets_manager_kms_arn = module.cica_dms_credentials_kms.key_arn
-    sid                     = local.environment_configuration.source_database_sid
-    cdc_start_time          = "2025-03-10T12:00:00Z"
+      engine_name                 = "oracle"
+      secrets_manager_arn         = module.cica_dms_tariff_database_credentials.secret_arn
+      secrets_manager_kms_arn     = module.cica_dms_credentials_kms.key_arn
+      sid                         = local.environment_configuration.source_database_sid
+      cdc_start_time              = "2025-03-10T12:00:00Z"
   }
   dms_target_prefix = "cica_tariff"
   replication_task_id = {
-    full_load = "${local.resource_name}-full-load"
+    full_load = "${local.tariff.resource_name}-full-load"
   }
-  dms_mapping_rules = "./metadata/cica_tariff.json"
-  output_bucket     = module.cica_dms_ingress_bucket.s3_bucket_id
+  dms_mapping_rules     = "./metadata/cica_tariff.json"
+  output_bucket         = module.cica_dms_ingress_bucket.s3_bucket_id
 
   tags = local.tags
 
