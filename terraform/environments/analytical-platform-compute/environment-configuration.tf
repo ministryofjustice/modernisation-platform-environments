f090d337fa3ea4d17d81340a3d67afc3dfa497fe
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-04-25 16:14:40 +0100

    Add AUTH0_DOMAIN as an env var

--- terraform/environments/analytical-platform-compute/environment-configuration.tf
+++ terraform/environments/analytical-platform-compute/environment-configuration.tf
@@ -42,6 +42,7 @@ locals {
       ui_hostname = "development.analytical-platform.service.justice.gov.uk"
 
       /* Dashboard Service */
+      dashboard_service_auth0_domain = "dev-analytics-moj.eu.auth0.com"
       dashboard_service_hostname = "dashboards.development.analytical-platform.service.justice.gov.uk"
 
       /* MWAA */
@@ -147,6 +148,7 @@ locals {
       ui_hostname = "analytical-platform.service.justice.gov.uk"
 
       /* Dashboard Service */
+      dashboard_service_auth0_domain = "alpha-analytics-moj.eu.auth0.com"
       dashboard_service_hostname = "dashboards.analytical-platform.service.justice.gov.uk"
 
       /* MWAA */
--- terraform/environments/analytical-platform-compute/helm-charts-dashboard-service.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-dashboard-service.tf
@@ -12,6 +12,7 @@ resource "helm_release" "dashboard_service" {
       "${path.module}/src/helm/values/dashboard-service/values.yml.tftpl",
       {
         dashboard_service_app_env = local.environment,
+        dashboard_service_auth0_domain = local.environment_configuration.dashboard_service_auth0_domain,
         dashboard_service_django_settings_module = "dashboard_service.settings.${local.environment}"
         dashboard_service_hostname = local.environment_configuration.dashboard_service_hostname,
       }
