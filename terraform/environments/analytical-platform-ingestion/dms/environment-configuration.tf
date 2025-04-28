5b2f5607c166a1bbe6902d91fe57f92c5a4b3d82
rm-madetech <russell.mclean@madetech.com>
2025-04-28 17:22:14 +0100

    inconsequential change to workflow trigger changes from previous PR (#10414)

--- terraform/environments/analytical-platform-ingestion/dms/environment-configuration.tf
+++ terraform/environments/analytical-platform-ingestion/dms/environment-configuration.tf
@@ -2,6 +2,7 @@ locals {
   environment_configuration = local.environment_configurations[local.environment]
   environment_configurations = {
     development = {
+
       /* VPC */
       connected_vpc_cidr            = "10.26.128.0/23"
       connected_vpc_private_subnets = ["10.26.128.0/26", "10.26.128.64/26", "10.26.128.128/26"]
@@ -13,6 +14,7 @@ locals {
       ap_data_glue_catalog_role = "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/mojap-data-production-dms-ingress-development"
     }
     production = {
+
       /* VPC */
       connected_vpc_cidr            = "10.27.128.0/23"
       connected_vpc_private_subnets = ["10.27.128.0/26", "10.27.128.64/26", "10.27.128.128/26"]
