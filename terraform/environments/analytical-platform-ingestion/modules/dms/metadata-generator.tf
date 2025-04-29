654dbb32c413aaf5da46a95903e36b14b9c92058
Russell McLean <russell.mclean@madetech.com>
2025-04-29 16:13:09 +0100

    force metadata lambda to build with x86 packages

--- terraform/environments/analytical-platform-ingestion/modules/dms/metadata-generator.tf
+++ terraform/environments/analytical-platform-ingestion/modules/dms/metadata-generator.tf
@@ -222,7 +222,11 @@ module "metadata_generator" {
 
   source_path = [{
     path             = "${path.module}/lambda-functions/metadata_generator/main.py"
-    pip_requirements = "${path.module}/lambda-functions/metadata_generator/requirements.txt"
+    commands = [
+      "pip3.12 install --platform=manylinux2014_x86_64 --only-binary=:all: --no-compile --target=. -r requirements.txt",
+      "rm -rf pyarrow/tests numpy/tests *.dist-info", # Exclude tests and dist-info directories from the deployment package
+      ":zip",
+    ]
   }]
 
   tags = var.tags
