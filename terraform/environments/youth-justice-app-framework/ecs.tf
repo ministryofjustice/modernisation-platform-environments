c088963a12b3ca5208aed8a9d49f33ea5142bf19
ttipler <thomas.tipler@necsws.com>
2025-04-29 10:46:33 +0100

    ecs cluster size update

--- terraform/environments/youth-justice-app-framework/ecs.tf
+++ terraform/environments/youth-justice-app-framework/ecs.tf
@@ -19,9 +19,9 @@ module "ecs" {
   #ECS details
   cluster_name         = "yjaf-cluster"
   ec2_instance_type    = "m5.xlarge"
-  ec2_min_size         = 16
-  ec2_max_size         = 16
-  ec2_desired_capacity = 16
+  ec2_min_size         = 11
+  ec2_max_size         = 11
+  ec2_desired_capacity = 11
   nameserver           = join(".", [split(".", data.aws_vpc.shared.cidr_block)[0], split(".", data.aws_vpc.shared.cidr_block)[1], "0", "2"]) #eg "10.23.0.2"
 
   spot_overrides = [
