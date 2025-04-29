eaa4fd8bd5717586abf57d8a68c1f1079fff2a11
matt heery <matt.heery@justice.gov.uk>
2025-04-29 14:31:34 +0100

    feat: add data engineer to all roles

--- terraform/environments/electronic-monitoring-data/share_analytical_platform_data.tf
+++ terraform/environments/electronic-monitoring-data/share_analytical_platform_data.tf
@@ -1,12 +1,17 @@
 locals {
+  principals_to_share_with = [
+    local.environment_management.account_ids["analytical-platform-data-production"],
+    "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_modernisation-platform-data-eng_499410b42334a7d7"
+  ]
   tables_to_share_ap = {
     "derived": ["visits"],
   }
 }
 
 resource "aws_lakeformation_permissions" "share_cadt_bucket" {
-  # provider = aws.eu_west_1
-  principal = local.environment_management.account_ids["analytical-platform-data-production"]
+  for_each = toset(local.principals_to_share_with)
+
+  principal = each.value.principal
   permissions = ["DATA_LOCATION_ACCESS"]
   permissions_with_grant_option = ["DATA_LOCATION_ACCESS"]
 
@@ -16,16 +21,30 @@ resource "aws_lakeformation_permissions" "share_cadt_bucket" {
 }
 
 resource "aws_lakeformation_permissions" "share_table_with_ap" {
-  # provider = aws.eu_west_1
-  for_each                      = {for item in flatten([
-    for database_name, tables in local.tables_to_share_ap : [
-      for table_name in tables : {
-        database_name = database_name
-        table_name    = table_name
-      }
-    ]
-  ]): "${item.database_name}.${item.table_name}" => item}
-  principal                     = local.environment_management.account_ids["analytical-platform-data-production"]
+  for_each = {
+    for pair in flatten([
+      for principal in local.principals_to_share_with : [
+        for item in flatten([
+          for database_name, tables in local.tables_to_share_ap : [
+            for table_name in tables : {
+              database_name = database_name
+              table_name    = table_name
+              principal     = principal
+            }
+          ]
+        ]) : {
+          key = "${principal}:${item.database_name}.${item.table_name}"
+          value = {
+            database_name = item.database_name
+            table_name    = item.table_name
+            principal     = principal
+          }
+        }
+      ]
+    ]) : pair.key => pair.value
+  }
+
+  principal                     = each.value.principal
   permissions                   = ["DESCRIBE"]
   permissions_with_grant_option = ["DESCRIBE"]
   table {
@@ -35,16 +54,30 @@ resource "aws_lakeformation_permissions" "share_table_with_ap" {
 }
 
 resource "aws_lakeformation_permissions" "share_database_with_ap" {
-  # provider = aws.eu_west_1
-  for_each                      = {for item in flatten([
-    for database_name, tables in local.tables_to_share_ap : [
-      for table_name in tables : {
-        database_name = database_name
-        table_name    = table_name
-      }
-    ]
-  ]): "${item.database_name}.${item.table_name}" => item}
-  principal                     = local.environment_management.account_ids["analytical-platform-data-production"]
+  for_each = {
+    for pair in flatten([
+      for principal in local.principals_to_share_with : [
+        for item in flatten([
+          for database_name, tables in local.tables_to_share_ap : [
+            for table_name in tables : {
+              database_name = database_name
+              table_name    = table_name
+              principal     = principal
+            }
+          ]
+        ]) : {
+          key = "${principal}:${item.database_name}.${item.table_name}"
+          value = {
+            database_name = item.database_name
+            table_name    = item.table_name
+            principal     = principal
+          }
+        }
+      ]
+    ]) : pair.key => pair.value
+  }
+
+  principal                     = each.value.principal
   permissions                   = ["DESCRIBE"]
   permissions_with_grant_option = ["DESCRIBE"]
   database {
