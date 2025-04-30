8abb4ec05fabd89a709fb466ffdfb5803f1da339
ttipler <thomas.tipler@necsws.com>
2025-04-30 12:37:25 +0100

    add aws config to datadog

--- terraform/environments/youth-justice-app-framework/modules/datadog/aws-config.tf
+++ terraform/environments/youth-justice-app-framework/modules/datadog/aws-config.tf
@@ -1,17 +1,13 @@
-module "config_changes_datadog" {
-  source  = "DataDog/config-changes-datadog/aws"
-  version = "1.0.0"
-  datadog_api_key_secret_arn = aws_secretsmanager_secret.datadog_api.arn
-  dd_destination_url         = "https://cloudplatform-intake.datadoghq.eu/api/v2/cloudchanges?dd-protocol=aws-kinesis-firehose"
-
-  # Optional: You can provide overrides if needed
-  # sns_topic_name             = "my-config-topic"
-  # iam_role_name              = "datadog-sns-role"
+module "datadog-aws-config" {
+  source                       = "DataDog/config-changes-datadog/aws"
+  version                      = "1.0.0"
+  dd_api_key_secret_arn        =  aws_secretsmanager_secret.datadog_api.arn
+  dd_integration_role_name     = "datadog-awsconfig-integration-role"
+  dd_destination_url           = "https://cloudplatform-intake.datadoghq.eu/api/v2/cloudchanges?dd-protocol=aws-kinesis-firehose"
+  sns_topic_name               = "aws-config-topic"
+  s3_bucket_name               = "awsconfig"
+  failed_events_s3_bucket_name = "awsconfig-failed-events"
+  tags = {
+    "team" = "AWS"
+  }
 }
-
-resource "aws_config_delivery_channel" "main" {
-  name           = "default"
-  s3_bucket_name = "aws-config-snapshot-bucket"
-  sns_topic_arn  = module.config_changes_datadog.sns_topic_arn
-}
-
