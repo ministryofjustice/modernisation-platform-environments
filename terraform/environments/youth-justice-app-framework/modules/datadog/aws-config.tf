4adc5f14baa070dd2fb83e15ca0d8b1c190f5912
ttipler <thomas.tipler@necsws.com>
2025-04-30 13:00:56 +0100

    fix datadog module

--- terraform/environments/youth-justice-app-framework/modules/datadog/aws-config.tf
+++ terraform/environments/youth-justice-app-framework/modules/datadog/aws-config.tf
@@ -2,11 +2,11 @@ module "datadog-aws-config" {
   source                       = "DataDog/config-changes-datadog/aws"
   version                      = "1.0.0"
   dd_api_key_secret_arn        =  aws_secretsmanager_secret.datadog_api.arn
-  dd_integration_role_name     = "datadog-awsconfig-integration-role"
+  dd_integration_role_name     = "DatadogAWSIntegrationRole"
   dd_destination_url           = "https://cloudplatform-intake.datadoghq.eu/api/v2/cloudchanges?dd-protocol=aws-kinesis-firehose"
   sns_topic_name               = "aws-config-topic"
-  s3_bucket_name               = "awsconfig"
-  failed_events_s3_bucket_name = "awsconfig-failed-events"
+  s3_bucket_name               = "yjaf-${var.environment}-awsconfig-datadog"
+  failed_events_s3_bucket_name = "yjaf-${var.environment}-awsconfig-failed-events"
   tags = {
     "team" = "AWS"
   }
