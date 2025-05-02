392efc87316b54d90a3cadd9c41545291143dfab
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2025-05-02 12:25:36 +0100

    tidy data.tf and move src/airflow files

--- terraform/environments/analytical-platform-compute/airflow/data.tf
+++ terraform/environments/analytical-platform-compute/airflow/data.tf
@@ -42,52 +42,6 @@ data "aws_subnet" "shared_private_subnets_c" {
   }
 }
 
-data "aws_ssoadmin_instances" "main" {
-  provider = aws.sso-readonly
-}
-
-data "aws_ec2_transit_gateway" "moj_tgw" {
-  id = "tgw-026162f1ba39ce704"
-}
-
-data "aws_iam_roles" "eks_sso_access_role" {
-  name_regex  = "AWSReservedSSO_${local.environment_configuration.eks_sso_access_role}_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
-}
-
-data "aws_iam_roles" "data_engineering_sso_role" {
-  name_regex  = "AWSReservedSSO_modernisation-platform-data-eng_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
-}
-
-data "aws_iam_roles" "platform_engineer_admin_sso_role" {
-  name_regex  = "AWSReservedSSO_platform-engineer-admin_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
-}
-
-data "http" "prometheus_operator_crds" {
-  for_each = {
-    alertmanagerconfigs = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml"
-    alertmanagers       = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml"
-    podmonitors         = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml"
-    probes              = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml"
-    prometheus_agents   = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusagents.yaml"
-    prometheuses        = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml"
-    prometheusrules     = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml"
-    scrapeconfigs       = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_scrapeconfigs.yaml"
-    servicemonitors     = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml"
-    thanosrulers        = "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${local.prometheus_operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml"
-  }
-
-  url = each.value
-}
-
-data "aws_secretsmanager_secret_version" "actions_runners_token_apc_self_hosted_runners_github_app" {
-  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
-
-  secret_id = module.actions_runners_token_apc_self_hosted_runners_github_app[0].secret_id
-}
-
 data "aws_vpc_endpoint" "mwaa_webserver" {
   service_name = aws_mwaa_environment.main.webserver_vpc_endpoint_service
 }
