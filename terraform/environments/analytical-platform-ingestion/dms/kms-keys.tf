f0b283ed722838f1dd0f43d3050bf289fa30b26d
Toby Petty <tobypetty1@gmail.com>
2025-05-02 12:30:52 +0100

    Add seperate tempus eventbridge roles

--- terraform/environments/analytical-platform-ingestion/dms/kms-keys.tf
+++ terraform/environments/analytical-platform-ingestion/dms/kms-keys.tf
@@ -63,8 +63,12 @@ module "cica_dms_eventscheduler_kms" {
 
   # Grants
   grants = {
-    dms_source = {
-      grantee_principal = module.eventbridge_dms_full_load_task_role.iam_role_arn
+    tariff_dms_source = {
+      grantee_principal = module.tariff_eventbridge_dms_full_load_task_role.iam_role_arn
+      operations        = ["Encrypt", "Decrypt", "GenerateDataKey"]
+    }
+    tempus_dms_source = {
+      grantee_principal = module.tempus_eventbridge_dms_full_load_task_role.iam_role_arn
       operations        = ["Encrypt", "Decrypt", "GenerateDataKey"]
     }
   }
