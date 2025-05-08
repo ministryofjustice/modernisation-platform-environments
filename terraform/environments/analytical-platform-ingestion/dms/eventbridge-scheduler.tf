d764315bbf33a9f68d35c3767e0cbf8b0212b4df
Toby Petty <tobypetty1@gmail.com>
2025-05-08 10:52:09 +0100

    Spread out the task start times to avoid/minimise overlap
    Overlap has the potential for memory starvation, observed in the dev environment against uat databases, where prod databases are ~2-12x the size of uat databases

--- terraform/environments/analytical-platform-ingestion/dms/eventbridge-scheduler.tf
+++ terraform/environments/analytical-platform-ingestion/dms/eventbridge-scheduler.tf
@@ -1,3 +1,29 @@
+locals {
+  # We stagger these jobs as they share compute resource
+  # We want all these to be finished by the time the `Deploy dbt prod daily` job starts at ~4am
+  # All jobs need to start post-midnight to make sure any entries from the previous day are used
+  cron_timings = {
+    tempus = {
+      SPPFinishedJobs = {
+        cron_string = "cron(0 3 * * ? *)" # 3:00am every day
+        replication_task_arn = module.cica_dms_tempus_dms_implementation["SPPFinishedJobs"].dms_full_load_task_arn
+      }
+      SPPProcessPlatform = {
+        cron_string = "cron(0 2 * * ? *)" # 2am every day
+        replication_task_arn = module.cica_dms_tempus_dms_implementation["SPPProcessPlatform"].dms_full_load_task_arn
+      }
+      CaseWork = {
+        cron_string = "cron(0 1 * * ? *)" # 1:00am every day
+        replication_task_arn = module.cica_dms_tempus_dms_implementation["CaseWork"].dms_full_load_task_arn
+      }
+    }
+    tariff = {
+      cron_string = "cron(10 0 * * ? *)" # 12:10am every day
+      replication_task_arn = module.cica_dms_tariff_dms_implementation.dms_full_load_task_arn
+    }
+  }
+}
+
 resource "aws_scheduler_schedule" "tariff_dms_nightly_full_load" {
   name       = "tariff-dms-nightly-full-load"
   group_name = aws_scheduler_schedule_group.tariff_dms_nightly_full_load.name
@@ -6,7 +32,7 @@ resource "aws_scheduler_schedule" "tariff_dms_nightly_full_load" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(30 0 * * ? *)" # 12:30am every day, to capture submissions up till midnight for monthly reporting
+  schedule_expression = local.cron_timings.tariff.cron_string
   kms_key_arn         = module.cica_dms_eventscheduler_kms.key_arn
 
   target {
@@ -14,7 +40,7 @@ resource "aws_scheduler_schedule" "tariff_dms_nightly_full_load" {
     role_arn = module.tariff_eventbridge_dms_full_load_task_role.iam_role_arn
 
     input = jsonencode({
-      ReplicationTaskArn       = module.cica_dms_tariff_dms_implementation.dms_full_load_task_arn
+      ReplicationTaskArn       = local.cron_timings.tariff.replication_task_arn
       StartReplicationTaskType = "reload-target"
     })
   }
@@ -26,7 +52,7 @@ resource "aws_scheduler_schedule_group" "tariff_dms_nightly_full_load" {
 }
 
 resource "aws_scheduler_schedule" "tempus_dms_nightly_full_load" {
-  for_each   = module.cica_dms_tempus_dms_implementation
+  for_each   = local.cron_timings.tempus
   name       = "tempus-${each.key}-dms-nightly-full-load"
   group_name = aws_scheduler_schedule_group.tempus_dms_nightly_full_load.name
 
@@ -34,7 +60,7 @@ resource "aws_scheduler_schedule" "tempus_dms_nightly_full_load" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(30 0 * * ? *)" # 12:30am every day, to capture submissions up till midnight for monthly reporting
+  schedule_expression = each.value.cron_string
   kms_key_arn         = module.cica_dms_eventscheduler_kms.key_arn
 
   target {
@@ -42,7 +68,7 @@ resource "aws_scheduler_schedule" "tempus_dms_nightly_full_load" {
     role_arn = module.tempus_eventbridge_dms_full_load_task_role.iam_role_arn
 
     input = jsonencode({
-      ReplicationTaskArn       = each.value.dms_full_load_task_arn
+      ReplicationTaskArn       = each.value.replication_task_arn
       StartReplicationTaskType = "reload-target"
     })
   }
