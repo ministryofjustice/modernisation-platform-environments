c807dd7d5845af73ed20796631a74b81c2f1aad4
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-05-08 15:06:26 +0100

    amend root files

--- terraform/environments/analytical-platform-compute/data.tf
+++ terraform/environments/analytical-platform-compute/data.tf
@@ -88,10 +88,7 @@ data "aws_secretsmanager_secret_version" "actions_runners_token_apc_self_hosted_
   secret_id = module.actions_runners_token_apc_self_hosted_runners_github_app[0].secret_id
 }
 
-data "aws_vpc_endpoint" "mwaa_webserver" {
-  service_name = aws_mwaa_environment.main.webserver_vpc_endpoint_service
-}
-
-data "dns_a_record_set" "mwaa_webserver_vpc_endpoint" {
-  host = data.aws_vpc_endpoint.mwaa_webserver.dns_entry[0].dns_name
+# Application Load Balancer
+data "aws_lb" "mwaa_alb" {
+  name = "mwaa"
 }
