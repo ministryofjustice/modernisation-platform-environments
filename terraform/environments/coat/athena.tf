278a5fdef8c1ab264fcc364354d5a862175241a5
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2025-05-08 11:41:18 +0100

    added athena workgroup

---
+++ terraform/environments/coat/athena.tf
@@ -0,0 +1,14 @@
+resource "aws_athena_workgroup" "coat_cur_report" {
+  name = "coat_cur_report"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://coat-${local.environment}-cur-v2-hourly/athena-results/"
+    }
+
+    enforce_workgroup_configuration = true
+    publish_cloudwatch_metrics_enabled = true
+  }
+
+  tags = local.tags
+}
