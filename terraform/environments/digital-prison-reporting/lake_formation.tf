84405a9f25950e574d7f858cedaa00f278d0d551
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-05-08 09:14:15 +0100

    dmet-prisons-318: add comments back

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -23,12 +23,15 @@ resource "aws_lakeformation_data_lake_settings" "lake_formation" {
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.modernisation_platform_sandbox_role.names)}"
   ])
 
+  # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
   create_database_default_permissions {
+    # These settings should replicate current behaviour: LakeFormation is Ignored
     permissions = ["ALL"]
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
 
   create_table_default_permissions {
+    # These settings should replicate current behaviour: LakeFormation is Ignored
     permissions = ["ALL"]
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
