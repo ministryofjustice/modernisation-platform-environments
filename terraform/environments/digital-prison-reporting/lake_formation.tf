679025dedfd9013e0051fc016d8f889b3f8de731
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-05-08 09:20:02 +0100

    dmet-prisons-318: removing old permissions

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -10,24 +10,19 @@ data "aws_iam_roles" "modernisation_platform_sandbox_role" {
   path_prefix = "/aws-reserved/sso.amazonaws.com/"
 }
 
+
 resource "aws_lakeformation_data_lake_settings" "lake_formation" {
-  admins = flatten([
-    [for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn],
-    data.aws_iam_session_context.current.issuer_arn,
-    try(one(data.aws_iam_roles.data_engineering_roles.arns), []),
+  admins = [
     data.aws_iam_role.github_actions_role.arn,
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.modernisation_platform_sandbox_role.names)}"
-  ])
+  ]
 
-  # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
   create_database_default_permissions {
-    # These settings should replicate current behaviour: LakeFormation is Ignored
     permissions = ["ALL"]
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
 
   create_table_default_permissions {
-    # These settings should replicate current behaviour: LakeFormation is Ignored
     permissions = ["ALL"]
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
