ed4e3d828e31b59bace916b56240159d7626f09a
sony-pimpale-moj <sony.pimpale@digital.justice.gov.uk>
2025-05-08 11:26:49 +0100

    Suppressing AVD-AWS-0345

--- terraform/environments/digital-prison-reporting/modules/dms_dps/iam.tf
+++ terraform/environments/digital-prison-reporting/modules/dms_dps/iam.tf
@@ -26,8 +26,7 @@ resource "aws_iam_role_policy" "dmskinesispolicy" {
   #checkov:skip=CKV_AWS_355: "Ensure no IAM policies documents allow "*" as a statement's resource for restrictable actions"
   #checkov:skip=CKV_AWS_289: "Ensure IAM policies does not allow permissions management / resource exposure without constraints"
   #checkov:skip=CKV_AWS_288: "Ensure IAM policies does not allow data exfiltration"
-  
-
+  #trivy:ignore:AVD-AWS-0345: test policy for development
   name = "dms-${var.short_name}-kinesis-policy"
   role = aws_iam_role.dms-kinesis-role.id
 
