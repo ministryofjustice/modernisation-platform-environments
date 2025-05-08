3612d150c168af927096249737fc6905bc2c43be
matt heery <matt.heery@justice.gov.uk>
2025-05-08 15:33:28 +0100

    fix: add glue catalog settings

---
+++ terraform/environments/electronic-monitoring-data/glue_catalog_settings.tf
@@ -0,0 +1,19 @@
+resource "aws_glue_resource_policy" "standard_policy" {
+  policy = data.aws_iam_policy_document.glue-policy.json
+}
+
+data "aws_iam_policy_document" "glue-policy" {
+    statement {
+      effect = "Allow"
+      principals {
+        type = "AWS"
+        identifiers = ["arn:aws:iam::${local.environment_management["analytical-platform-data-production"]}:root"]
+      }
+      actions = "glue:*"
+      resources = [
+        "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/*/*",
+        "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/*",
+        "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog"
+      ]
+    }
+}
