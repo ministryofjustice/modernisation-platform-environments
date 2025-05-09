61be97635846efda3da2f93f6863f5dc68e533e0
David Teeling <david.teeling1@justice.gov.uk>
2025-05-09 17:05:50 +0100

    Add certs and ecs cluster

--- terraform/environments/delius-alfresco/modules/alfresco/alfresco.tf
+++ terraform/environments/delius-alfresco/modules/alfresco/alfresco.tf
@@ -67,7 +67,7 @@ module "alfresco_sfs_ecs" {
   }
 
   # ecs_cluster_arn           = data.aws_ecs_cluster.delius_core.arn
-  ecs_cluster_arn           = "arn:aws:ecs:eu-west-2:326912278139:cluster/delius-core-poc-cluster"
+  ecs_cluster_arn           = module.ecs.ecs_cluster_arn
   cluster_security_group_id = aws_security_group.cluster.id
 
   # bastion_sg_id = data.aws_security_group.bastion_core.arn
@@ -206,62 +206,5 @@ data "aws_iam_policy_document" "alfresco_efs_access_policy" {
       "arn:aws:elasticfilesystem:eu-west-2:326912278139:file-system/fs-021c2025ae87537b0"
     ]
     effect = "Allow"
-  }
-}
-
-resource "aws_security_group" "alfresco_sfs_alb" {
-  name        = "${terraform.workspace}-alf-sfs-alb"
-  description = "controls access to and from alfresco sfs load balancer"
-  vpc_id      = var.account_config.shared_vpc_id
-  tags        = var.tags
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "alfresco_sfs_alb" {
-  for_each          = toset([var.account_info.cp_cidr, var.account_config.shared_vpc_cidr])
-  security_group_id = aws_security_group.alfresco_sfs_alb.id
-  description       = "Access into alb over https"
-  from_port         = "443"
-  to_port           = "443"
-  ip_protocol       = "tcp"
-  cidr_ipv4         = each.key
-}
-
-resource "aws_vpc_security_group_egress_rule" "alfresco_sfs_alb" {
-  security_group_id = aws_security_group.alfresco_sfs_alb.id
-  description       = "egress from alb to ecs cluster"
-  ip_protocol       = "-1"
-  cidr_ipv4         = var.account_config.shared_vpc_cidr
-}
-
-# internal application load balancer
-resource "aws_lb" "alfresco_sfs" {
-  name               = "${var.app_name}-${var.env_name}-alf-sfs-alb"
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alfresco_sfs_alb.id]
-  subnets            = var.account_config.private_subnet_ids
-
-  enable_deletion_protection = false
-  drop_invalid_header_fields = true
-}
-
-
-resource "aws_lb_listener" "alfresco_sfs_listener_https" {
-  load_balancer_arn = aws_lb.alfresco_sfs.id
-  port              = 443
-  protocol          = "HTTPS"
-  # certificate_arn   = data.aws_acm_certificate.mp_service.arn
-  certificate_arn   = "arn:aws:acm:eu-west-2:326912278139:certificate/a9d57e87-4be8-4c82-b0c8-1ac64fc1c972"
-  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      status_code  = "404"
-    }
   }
 }
