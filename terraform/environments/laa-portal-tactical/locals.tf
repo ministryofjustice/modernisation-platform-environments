ebb9477c83203630a0e43e74dc31a845c0f0038a
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2025-05-10 10:13:23 +0100

    Add in IDM and OIM servers

--- terraform/environments/laa-portal-tactical/locals.tf
+++ terraform/environments/laa-portal-tactical/locals.tf
@@ -4,15 +4,8 @@ locals {
   private_subnets    = ["10.206.4.0/24", "10.206.5.0/24", "10.206.6.0/24"]
   public_subnets     = ["10.206.7.0/24", "10.206.8.0/24", "10.206.9.0/24"]
   database_subnets    = ["10.206.1.0/24", "10.206.2.0/24", "10.206.3.0/24"]
-  first-cidr              = "10.202.0.0/20"
-  second-cidr             = "10.200.0.0/20"
-  third-cidr              = "10.200.16.0/20"
-  prd-cidr                = "10.200.16.0/20"
-  aws_region              = "eu-west-2"
   nonprod_workspaces_cidr = "10.200.0.0/20"
   prod_workspaces_cidr    = "10.200.16.0/20"
-  redc_cidr               = "172.16.0.0/20"
-  atos_cidr               = "10.0.0.0/8"
 
   # Temp local variable for environments where we wish to build out the EBS to be transfered to EFS
   ebs_conditional = ["testing", "preproduction", "production"]
