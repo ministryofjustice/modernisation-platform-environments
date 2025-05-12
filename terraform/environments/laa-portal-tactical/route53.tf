707c17fc8c81ea1bb4c73f6192d2f2306e448201
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2025-05-12 16:00:56 +0100

    Turning hosted zone private 2

--- terraform/environments/laa-portal-tactical/route53.tf
+++ terraform/environments/laa-portal-tactical/route53.tf
@@ -5,9 +5,9 @@ resource "aws_route53_zone" "private_uat" {
     vpc_id = module.vpc.vpc_id
   }
 
-  lifecycle {
-    ignore_changes = [vpc]
-  }
+#   lifecycle {
+#     ignore_changes = [vpc]
+#   }
 }
 
 # resource "aws_route53_zone_association" "private_uat" {
