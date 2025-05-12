2492ce1b484e822e3cf8d7c03e830fbf0cf1ca65
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2025-05-12 16:08:37 +0100

    Rebuild hosted zone

--- terraform/environments/laa-portal-tactical/route53.tf
+++ terraform/environments/laa-portal-tactical/route53.tf
@@ -1,32 +1,32 @@
-resource "aws_route53_zone" "private_uat" {
-  name = "aws.uat.legalservices.gov.uk"
+# resource "aws_route53_zone" "private_uat" {
+#   name = "aws.uat.legalservices.gov.uk"
 
-  vpc {
-    vpc_id = module.vpc.vpc_id
-  }
-
-#   lifecycle {
-#     ignore_changes = [vpc]
+#   vpc {
+#     vpc_id = module.vpc.vpc_id
 #   }
-}
 
-# resource "aws_route53_zone_association" "private_uat" {
-#   zone_id = aws_route53_zone.private_uat.zone_id
-#   vpc_id  = module.vpc.vpc_id
+# #   lifecycle {
+# #     ignore_changes = [vpc]
+# #   }
 # }
 
-resource "aws_route53_record" "iadb" {
-  zone_id  = aws_route53_zone.private_uat.zone_id
-  name     = "db-portal-iadb.${aws_route53_zone.private_uat.name}" # db-portal-iadb.aws.[env].legalservices.gov.uk
-  type     = "CNAME"
-  ttl      = 60
-  records  = [aws_db_instance.iadb.address]
-}
+# # resource "aws_route53_zone_association" "private_uat" {
+# #   zone_id = aws_route53_zone.private_uat.zone_id
+# #   vpc_id  = module.vpc.vpc_id
+# # }
 
-resource "aws_route53_record" "igdb" {
-  zone_id  = aws_route53_zone.private_uat.zone_id
-  name     = "db-portal-igdb.${aws_route53_zone.private_uat.name}" # db-portal-igdb.aws.dev.legalservices.gov.uk
-  type     = "CNAME"
-  ttl      = 60
-  records  = [aws_db_instance.igdb.address]
-}
+# resource "aws_route53_record" "iadb" {
+#   zone_id  = aws_route53_zone.private_uat.zone_id
+#   name     = "db-portal-iadb.${aws_route53_zone.private_uat.name}" # db-portal-iadb.aws.[env].legalservices.gov.uk
+#   type     = "CNAME"
+#   ttl      = 60
+#   records  = [aws_db_instance.iadb.address]
+# }
+
+# resource "aws_route53_record" "igdb" {
+#   zone_id  = aws_route53_zone.private_uat.zone_id
+#   name     = "db-portal-igdb.${aws_route53_zone.private_uat.name}" # db-portal-igdb.aws.dev.legalservices.gov.uk
+#   type     = "CNAME"
+#   ttl      = 60
+#   records  = [aws_db_instance.igdb.address]
+# }
