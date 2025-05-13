564ac409344db1e24c760f696ec20f9e86f2e0e1
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2025-05-13 11:22:06 +0100

    remove data calls not needed for airflow

--- terraform/environments/analytical-platform-compute/airflow/data.tf
+++ terraform/environments/analytical-platform-compute/airflow/data.tf
@@ -4,44 +4,6 @@ data "aws_iam_session_context" "current" {
   arn = data.aws_caller_identity.current.arn
 }
 
-# Shared VPC and Subnets
-data "aws_vpc" "shared" {
-  tags = {
-    "Name" = "${var.networking[0].business-unit}-${local.environment}"
-  }
-}
-
-data "aws_subnets" "shared_private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.shared.id]
-  }
-  tags = {
-    Name = "${var.networking[0].business-unit}-${local.environment}-${var.networking[0].set}-private*"
-  }
-}
-
-data "aws_subnet" "shared_private_subnets_a" {
-  vpc_id = data.aws_vpc.shared.id
-  tags = {
-    "Name" = "${var.networking[0].business-unit}-${local.environment}-${var.networking[0].set}-private-${data.aws_region.current.name}a"
-  }
-}
-
-data "aws_subnet" "shared_private_subnets_b" {
-  vpc_id = data.aws_vpc.shared.id
-  tags = {
-    "Name" = "${var.networking[0].business-unit}-${local.environment}-${var.networking[0].set}-private-${data.aws_region.current.name}b"
-  }
-}
-
-data "aws_subnet" "shared_private_subnets_c" {
-  vpc_id = data.aws_vpc.shared.id
-  tags = {
-    "Name" = "${var.networking[0].business-unit}-${local.environment}-${var.networking[0].set}-private-${data.aws_region.current.name}c"
-  }
-}
-
 data "aws_vpc_endpoint" "mwaa_webserver" {
   service_name = aws_mwaa_environment.main.webserver_vpc_endpoint_service
 }
@@ -81,28 +43,6 @@ data "aws_subnets" "apc_private_subnets" {
 data "aws_acm_certificate" "certificate" {
   domain   = local.environment_configuration.route53_zone
   statuses = ["ISSUED"]
-}
-
-# Kubernetes
-data "kubernetes_namespace" "actions_runner" {
-  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
-
-  metadata {
-    name = "actions-runner"
-  }
-}
-
-# Secrets manager
-data "aws_secretsmanager_secret" "actions_runners_token_apc_self_hosted_runners_secret" {
-  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
-
-  name = "actions-runners/token/apc-self-hosted-runners"
-}
-
-data "aws_secretsmanager_secret_version" "actions_runners_token_apc_self_hosted_runners_github_app" {
-  count = terraform.workspace == "analytical-platform-compute-production" ? 1 : 0
-
-  secret_id = data.aws_secretsmanager_secret.actions_runners_token_apc_self_hosted_runners_secret[0].id
 }
 
 # EKS
--- terraform/environments/analytical-platform-compute/airflow/locals.tf
+++ terraform/environments/analytical-platform-compute/airflow/locals.tf
@@ -1,5 +1,4 @@
 locals {
-  route53_zone     = "compute.development.analytical-platform.service.justice.gov.uk"
   eks_cluster_name = "${local.application_name}-${local.environment}"
 
   /* Environment Configuration */
