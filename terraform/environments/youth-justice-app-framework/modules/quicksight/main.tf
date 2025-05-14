b7c8b672b29b7ae1aae176882c2be3c7743ce78a
davidseekins <161700927+davidseekins@users.noreply.github.com>
2025-05-14 17:02:48 +0100

    Change quicksite vpc connection id

--- terraform/environments/youth-justice-app-framework/modules/quicksight/main.tf
+++ terraform/environments/youth-justice-app-framework/modules/quicksight/main.tf
@@ -1,6 +1,6 @@
 resource "aws_quicksight_vpc_connection" "local" {
-  name               = "${var.project_name}-${var.environment}-3"
-  vpc_connection_id  = "${var.project_name}-${var.environment}-3"
+  name               = "${var.project_name}-${var.environment}"
+  vpc_connection_id  = "${var.project_name}-${var.environment}-4"
   role_arn           =  aws_iam_role.vpc_connection_role.arn
   security_group_ids = [module.quicksight_sg.security_group_id]
   subnet_ids         = var.database_subnet_ids
