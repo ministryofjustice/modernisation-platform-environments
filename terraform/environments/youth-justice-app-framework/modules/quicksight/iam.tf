1bf35bde976e0d992b20207f2f714a1da5ad3725
David Seekins <david.seekins@necsws.com>
2025-05-15 13:53:08 +0100

    Create VPC Permissions

--- terraform/environments/youth-justice-app-framework/modules/quicksight/iam.tf
+++ terraform/environments/youth-justice-app-framework/modules/quicksight/iam.tf
@@ -18,3 +18,27 @@ resource "aws_iam_role" "vpc_connection_role" {
   })
 }
 
+resource "aws_iam_policy" "qs_vpc" {
+  name        = "QuickSightVPCConnectionRolePolicy"
+  policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "ec2:CreateNetworkInterface",
+            "ec2:ModifyNetworkInterfaceAttribute",
+            "ec2:DeleteNetworkInterface",
+            "ec2:DescribeSubnets",
+            "ec2:DescribeSecurityGroups"
+          ]
+          Resource = ["*"]
+        }
+      ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "qs_vpc" {
+  role       = aws_iam_role.vpc_connection_role.name
+  policy_arn = aws_iam_policy.qs_vpc.arn
+}
