884c37ffbfb9700a6cb411af4f4d077d12530226
Rich Green <richard.green@digital.justice.gov.uk>
2025-05-16 11:17:03 +0100

    add test bucket acl

--- terraform/environments/sprinkler/modules/test/main.tf
+++ terraform/environments/sprinkler/modules/test/main.tf
@@ -2,3 +2,17 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket_prefix = var.bucket_prefix
   tags          = var.tags
 }
+
+resource "aws_s3_bucket_ownership_controls" "test_bucket" {
+  bucket = aws_s3_bucket.test_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.test_bucket.id
+  acl    = "private"
+}
