65e87d25ce725086b29de57b1dfe37f132f9f9e7
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2025-05-16 09:42:05 +0100

    DPR2-1906 update lambda name

--- terraform/environments/digital-prison-reporting/locals.tf
+++ terraform/environments/digital-prison-reporting/locals.tf
@@ -243,13 +243,13 @@ locals {
 
   # Multiphase Query Manager Lambda
   lambda_multiphase_query_enabled         = local.application_data.accounts[local.environment].enable_multiphase_query_lambda
-  lambda_multiphase_query_name            = "${local.project}-multiphase-query-manager"
+  lambda_multiphase_query_name            = "${local.project}-multiphase-query"
   lambda_multiphase_query_runtime         = "java21"
   lambda_multiphase_query_tracing         = "Active"
   lambda_multiphase_query_handler         = "uk.gov.justice.digital.hmpps.multiphasequery.ManageAthenaAsyncQueries::handleRequest"
   lambda_multiphase_query_code_s3_bucket  = module.s3_artifacts_store.bucket_id
   lambda_multiphase_query_jar_version     = local.application_data.accounts[local.environment].multiphase_query_lambda_version
-  lambda_multiphase_query_code_s3_key     = "build-artifacts/hmpps-dpr-multiphase-query-manager-lambda/jars/hmpps-dpr-multiphase-query-manager-lambda-${local.lambda_multiphase_query_jar_version}-all.jar"
+  lambda_multiphase_query_code_s3_key     = "build-artifacts/hmpps-dpr-multiphase-query-lambda/jars/hmpps-dpr-multiphase-query-manager-lambda-${local.lambda_multiphase_query_jar_version}-all.jar"
   lambda_multiphase_query_policies        = [
     "arn:aws:iam::${local.account_id}:policy/${local.s3_read_access_policy}",
     "arn:aws:iam::${local.account_id}:policy/${local.kms_read_access_policy}",
