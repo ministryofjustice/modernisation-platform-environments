d3e21a33a8871956472dc75c02eb95de2750069e
Rich Green <richard.green@digital.justice.gov.uk>
2025-05-16 11:45:05 +0100

    update depends_on ref

--- terraform/environments/sprinkler/modules/test/main.tf
+++ terraform/environments/sprinkler/modules/test/main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_ownership_controls" "test_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "test_bucket" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
+  depends_on = [aws_s3_bucket_ownership_controls.test_bucket]
 
   bucket = aws_s3_bucket.test_bucket.id
   acl    = "private"
