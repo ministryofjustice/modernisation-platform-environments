a7716d58ad86fcd3349355e29fbdb8d878364dfa
vikasbsuresh-moj <vikas.suresh@justice.gov.uk>
2025-05-20 16:38:55 +0100

    fix: remove tags from local.tf

--- terraform/environments/laa-ccms-soa/locals.tf
+++ terraform/environments/laa-ccms-soa/locals.tf
@@ -1,17 +1,1 @@
 #### This file can be used to store locals specific to the member account ####
-
-
-### local varibales for SOA application ####
-locals {
-  tags = {
-    business-unit          = "LAA"
-    application            = "CCMS SOA"
-    environment-name       = terraform.workspace
-    owner                  = "laa-ccms-members@digital.justice.gov.uk"
-    infrastructure-support = "laa-role-sre@digital.justice.gov.uk"
-    is-production          = terraform.workspace == "production" ? "true" : "false"
-    source-code            = "https://github.com/ministryofjustice/modernisation-platform-environments/tree/main/terraform/environments/laa-ccms-soa"
-    iac-tool               = "terraform"
-  }
-}
-
