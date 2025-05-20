20947258d3ef91fc24436ae0319a0936c64fa25e
Julien Nioche <julien.nioche@digital.justice.gov.uk>
2025-05-20 16:46:34 +0100

    better way to attach policies?
    Signed-off-by: Julien Nioche <julien.nioche@digital.justice.gov.uk>

--- terraform/environments/coat/observability_platform.tf
+++ terraform/environments/coat/observability_platform.tf
@@ -4,65 +4,63 @@ module "observability_platform_tenant" {
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-production"]
 
+  additional_policies = {
+    additional_athena_policy = aws_iam_policy.grafana_athena_full_access_policy.arn
+  }
+
   tags = local.tags
 }
 
+
 resource "aws_iam_policy" "grafana_athena_full_access_policy" {
-
-  # checkov:skip=CKV_AWS_290: "Ensure IAM policies does not allow write access without constraints"
-  # checkov:skip=CKV_AWS_355: "Ensure no IAM policies documents allow "*" as a statement's resource for restrictable actions"
-
-  name = "grafana_athena_full_access_policy"
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "athena:*"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "glue:GetDatabase",
-                "glue:GetDatabases",
-                "glue:GetTable",
-                "glue:GetTables",
-                "glue:GetPartition",
-                "glue:GetPartitions",
-                "glue:BatchGetPartition"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetBucketLocation",
-                "s3:GetObject",
-                "s3:ListBucket",
-                "s3:ListBucketMultipartUploads",
-                "s3:ListMultipartUploadParts",
-                "s3:AbortMultipartUpload",
-                "s3:CreateBucket",
-                "s3:PutObject",
-                "s3:PutBucketPublicAccessBlock"
-            ],
-            "Resource": [ 
-              "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly/*",
-              "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly"
-            ]
-        }
-    ]
-})
+  name   = "grafana_athena_full_access_policy"
+  policy = data.aws_iam_policy_document.grafana_athena_full_access_policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "observability_platform_role_grafana_athena_full_access_attachment" {
-  role       = "observability-platform"
-  policy_arn = aws_iam_policy.grafana_athena_full_access_policy.arn
+data "aws_iam_policy_document" "grafana_athena_full_access_policy" {  statement {
+    effect = "Allow"
+
+    actions = [
+      "athena:*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "glue:GetDatabase",
+      "glue:GetDatabases",
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetPartition",
+      "glue:GetPartitions",
+      "glue:BatchGetPartition"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload",
+      "s3:CreateBucket",
+      "s3:PutObject",
+      "s3:PutBucketPublicAccessBlock"
+    ]
+
+    resources = [
+      "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly",
+      "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly/*"
+    ]
+  }
 }
