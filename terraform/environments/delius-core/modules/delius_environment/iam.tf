7c089e0dff7cf370ea435ac3ed224ec6635f2eea
David Teeling <david.teeling1@justice.gov.uk>
2025-05-21 14:07:11 +0100

    Test

--- terraform/environments/delius-core/modules/delius_environment/iam.tf
+++ terraform/environments/delius-core/modules/delius_environment/iam.tf
@@ -1,4 +1,5 @@
 resource "aws_iam_role" "aws_backup_default_service_role" {
+  count  = var.env_name != "poc" ? 1 : 0
   name = "AWSBackupDefaultServiceRole"
 
   assume_role_policy = jsonencode({
@@ -16,11 +17,13 @@ resource "aws_iam_role" "aws_backup_default_service_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "backup_policy" {
-  role       = aws_iam_role.aws_backup_default_service_role.name
+  count  = var.env_name != "poc" ? 1 : 0
+  role       = aws_iam_role.aws_backup_default_service_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
 }
 
 resource "aws_iam_role_policy_attachment" "restore_policy" {
-  role       = aws_iam_role.aws_backup_default_service_role.name
+  count  = var.env_name != "poc" ? 1 : 0
+  role       = aws_iam_role.aws_backup_default_service_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForRestores"
 }
