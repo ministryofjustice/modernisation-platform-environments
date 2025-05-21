d7aa7b8a7f71986755d6ecb4a5f957ac24048714
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-05-21 16:13:40 +0100

    feat: initial infra standup

---
+++ terraform/environments/laa-ccms-soa/loadbalancer.tf
@@ -0,0 +1,143 @@
+#--NEED TO CONFIGURE ACCESS LOGS!!! - AW
+
+#--Admin
+resource "aws_lb" "admin" {
+  name               = "${local.application_data.accounts[local.environment].app_name}-admin-lb"
+  load_balancer_type = "application"
+  internal           = true
+  subnets            = data.aws_subnets.shared-private.ids
+  security_groups    = [aws_security_group.alb_admin.id]
+}
+
+resource "aws_lb_target_group" "admin" {
+  name                 = "${local.application_data.accounts[local.environment].app_name}-admin-target-group"
+  port                 = local.application_data.accounts[local.environment].admin_server_port
+  protocol             = "HTTP"
+  vpc_id               = data.aws_vpc.shared.id
+  target_type          = "instance"
+  deregistration_delay = 30
+
+  #--Need to find out the applications HTTP endpoints - AW
+  /*   health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "TCP"
+    unhealthy_threshold = "3"
+  } */
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener" "admin80" {
+  load_balancer_arn = aws_lb.admin.id
+  port              = 80 #--Don't know why HTTP is being listened, is this a redirect? Why? - Revist. AW
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.admin.id
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "admin443" {
+  load_balancer_arn = aws_lb.admin.id
+  port              = 443
+  protocol          = "TLS"
+
+  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn = local.application_data.accounts[local.environment].admin_loadbalancer_certificate_arn #--NEED TO POPULATE, MAKE A DUMMY CERT FOR NOW
+
+  default_action {
+    target_group_arn = aws_lb_target_group.admin.id
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "admin7001" {
+  load_balancer_arn = aws_lb.admin.id
+  port              = 7001
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.admin.id
+    type             = "forward"
+  }
+}
+
+#--Managed
+resource "aws_lb" "managed" {
+  name               = "${local.application_data.accounts[local.environment].app_name}-managed-api-lb"
+  load_balancer_type = "application"
+  internal           = true
+  subnets            = data.aws_subnets.shared-private.ids
+  security_groups    = [aws_security_group.alb_managed.id]
+}
+
+resource "aws_lb_target_group" "managed" {
+  name        = "${local.application_data.accounts[local.environment].app_name}-managed-target-group"
+  port        = local.application_data.accounts[local.environment].managed_server_port
+  protocol    = "TCP"
+  vpc_id      = data.aws_vpc.shared.id
+  target_type = "instance"
+
+  #--Need to find out the applications HTTP endpoints - AW
+  /*   health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "TCP"
+    unhealthy_threshold = "3"
+  } */
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener" "managed443" {
+  load_balancer_arn = aws_lb.managed.id
+  port              = 443
+  protocol          = "TLS"
+
+  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn = local.application_data.accounts[local.environment].managed_loadbalancer_certificate_arn
+
+  default_action {
+    target_group_arn = aws_lb_target_group.managed.id
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "managed80" {
+  load_balancer_arn = aws_lb.managed.id
+  port              = 80 #--Don't know why HTTP is being listened, is this a redirect? Why? - Revist. AW
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.managed.id
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "managed8001" {
+  load_balancer_arn = aws_lb.managed.id
+  port              = 8001
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.managed.id
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "managed7777" {
+  load_balancer_arn = aws_lb.managed.id
+  port              = 7777
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.managed.id
+    type             = "forward"
+  }
+}
---
+++ terraform/environments/laa-ccms-soa/security.tf
@@ -0,0 +1,214 @@
+#--ALB Admin
+resource "aws_security_group" "alb_admin" {
+  name        = "${local.application_data.accounts[local.environment].app_name}_alb_admin"
+  description = "Controls Traffic for SOA Admin Application"
+  vpc_id      = data.aws_vpc.shared.id
+}
+
+resource "aws_security_group_rule" "alb_admin_ingress_80" {
+  security_group_id = aws_security_group.alb_admin.id
+  type              = "ingress"
+  description       = "Admin HTTP" #--Why?
+  protocol          = "TCP"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "alb_admin_ingress_443" {
+  security_group_id = aws_security_group.alb_admin.id
+  type              = "ingress"
+  description       = "Admin HTTPS"
+  protocol          = "TCP"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "alb_admin_ingress_7001" {
+  security_group_id = aws_security_group.alb_admin.id
+  type              = "ingress"
+  description       = "Admin Weblogic" #--Maybe?
+  protocol          = "TCP"
+  from_port         = 7001
+  to_port           = 7001
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "alb_admin_egress_all" {
+  security_group_id = aws_security_group.alb_admin.id
+  type              = "egress"
+  description       = "All"
+  protocol          = "TCP"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW
+}
+
+#--Managed
+resource "aws_security_group" "alb_managed" {
+  name        = "${local.application_data.accounts[local.environment].app_name}_alb_managed"
+  description = "Controls Traffic for SOA Managed Application"
+  vpc_id      = data.aws_vpc.shared.id
+}
+
+resource "aws_security_group_rule" "alb_managed_ingress_80" {
+  security_group_id = aws_security_group.alb_managed.id
+  type              = "ingress"
+  description       = "Managed HTTP" #--Why?
+  protocol          = "TCP"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "alb_managed_ingress_443" {
+  security_group_id = aws_security_group.alb_managed.id
+  type              = "ingress"
+  description       = "Managed HTTPS"
+  protocol          = "TCP"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "alb_managed_ingress_7001" {
+  security_group_id = aws_security_group.alb_managed.id
+  type              = "ingress"
+  description       = "Managed Weblogic" #--Maybe?
+  protocol          = "TCP"
+  from_port         = 7001
+  to_port           = 7001
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "alb_managed_egress_all" {
+  security_group_id = aws_security_group.alb_managed.id
+  type              = "egress"
+  description       = "All"
+  protocol          = "TCP"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW
+}
+
+#--ECS Tasks Admin
+resource "aws_security_group" "ecs_tasks_admin" {
+  name_prefix = "${local.application_data.accounts[local.environment].app_name}_ecs_tasks_admin"
+  description = "SOA Admin - Controls Traffic Between VPC and ECS Control Plane"
+  vpc_id      = data.aws_vpc.shared.id
+}
+
+resource "aws_security_group_rule" "ecs_tasks_admin_server" {
+  security_group_id = aws_security_group.ecs_tasks_admin.id
+  type              = "ingress"
+  description       = "SOA Admin Server" #--Why?
+  protocol          = "TCP"
+  from_port         = local.application_data.accounts[local.environment].admin_server_port
+  to_port           = local.application_data.accounts[local.environment].admin_server_port
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+#--ECS Tasks Managed
+resource "aws_security_group" "ecs_tasks_managed" {
+  name_prefix = "${local.application_data.accounts[local.environment].app_name}_ecs_tasks_managed"
+  description = "SOA Managed - Controls Traffic Between VPC and ECS Control Plane"
+  vpc_id      = data.aws_vpc.shared.id
+}
+
+resource "aws_security_group_rule" "ecs_tasks_managed_server" {
+  security_group_id = aws_security_group.ecs_tasks_admin.id
+  type              = "ingress"
+  description       = "SOA Managed Server"
+  protocol          = "TCP"
+  from_port         = local.application_data.accounts[local.environment].managed_server_port
+  to_port           = local.application_data.accounts[local.environment].managed_server_port
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "ecs_tasks_managed_7" {
+  security_group_id = aws_security_group.ecs_tasks_admin.id
+  type              = "ingress"
+  description       = "SOA Managed Application" #--Why?
+  protocol          = "TCP"
+  from_port         = 7
+  to_port           = 7
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "ecs_tasks_managed_7574_tcp" {
+  security_group_id = aws_security_group.ecs_tasks_admin.id
+  type              = "ingress"
+  description       = "SOA Managed Application" #--Why?
+  protocol          = "TCP"
+  from_port         = 7574
+  to_port           = 7574
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "ecs_tasks_managed_8088_8089" {
+  security_group_id = aws_security_group.ecs_tasks_admin.id
+  type              = "ingress"
+  description       = "SOA Managed Application" #--Why?
+  protocol          = "TCP"
+  from_port         = 8088
+  to_port           = 8089
+  cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
+}
+
+resource "aws_security_group_rule" "ecs_tasks_managed_egress_all" {
+  security_group_id = aws_security_group.ecs_tasks_managed.id
+  type              = "egress"
+  description       = "All"
+  protocol          = "TCP"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
+}
+
+#--Cluster EC2 Instances
+resource "aws_security_group" "cluster_ec2" {
+  name        = "${local.application_data.accounts[local.environment].app_name}-cluster-ec2-security-group"
+  description = "controls access to the cluster ec2 instance"
+  vpc_id      = data.aws_vpc.shared.id
+}
+
+resource "aws_security_group_rule" "cluster_ec2_ingress_1521" {
+  security_group_id = aws_security_group.cluster_ec2.id
+  type              = "ingress"
+  description       = "Application Traffic"
+  protocol          = "TCP" #--What?
+  from_port         = 1521
+  to_port           = 1521
+  cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
+}
+
+resource "aws_security_group_rule" "cluster_ec2_ingress_3872" {
+  security_group_id = aws_security_group.cluster_ec2.id
+  type              = "ingress"
+  description       = "Application Traffic"
+  protocol          = "TCP" #--What?
+  from_port         = 3872
+  to_port           = 3872
+  cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
+}
+
+resource "aws_security_group_rule" "cluster_ec2_ingress_4903" {
+  security_group_id = aws_security_group.cluster_ec2.id
+  type              = "ingress"
+  description       = "Application Traffic"
+  protocol          = "TCP" #--What?
+  from_port         = 4903
+  to_port           = 4903
+  cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
+}
+
+resource "aws_security_group_rule" "cluster_ec2_egress_all" {
+  security_group_id = aws_security_group.cluster_ec2.id
+  type              = "egress"
+  description       = "All Egress"
+  protocol          = "TCP"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
+}
