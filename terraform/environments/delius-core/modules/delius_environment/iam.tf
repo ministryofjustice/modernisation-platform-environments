9611cd9ed9f8ec20b2ac7d5af8f85673dce57ce2
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-05-22 16:00:48 +0100

    Add Backup Actions to the default Backup Role

--- terraform/environments/delius-core/modules/delius_environment/iam.tf
+++ terraform/environments/delius-core/modules/delius_environment/iam.tf
@@ -28,3 +28,25 @@ resource "aws_iam_role_policy_attachment" "restore_policy" {
   role       = aws_iam_role.aws_backup_default_service_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForRestores"
 }
+
+data "aws_iam_policy_document" "backup_actions_policy_document" {
+  statement {
+    effect = "Allow"
+    actions = ["backup:ListBackupVaults",
+      "backup:StartBackupJob",
+      "backup:DescribeBackupJob",
+      "ec2:DescribeSnapshots"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "backup_actions_policy" {
+  name   = "backup_actions_policy"
+  policy = data.aws_iam_policy_document.backup_actions_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "backup_actions_policy_attachment" {
+  count      = contains(["poc", "stage"], var.env_name) ? 0 : 1
+  role       = aws_iam_role.aws_backup_default_service_role[0].name
+  policy_arn = aws_iam_policy.backup_actions_policy.arn
+}
