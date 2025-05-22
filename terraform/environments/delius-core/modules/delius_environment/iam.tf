01b0a373604fc220ab40b984e6e4a32564bbd340
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-05-22 16:17:42 +0100

    Handle multiple environment accounts

--- terraform/environments/delius-core/modules/delius_environment/iam.tf
+++ terraform/environments/delius-core/modules/delius_environment/iam.tf
@@ -30,7 +30,6 @@ resource "aws_iam_role_policy_attachment" "restore_policy" {
 }
 
 data "aws_iam_policy_document" "backup_actions_policy_document" {
-  count      = contains(["poc", "stage"], var.env_name) ? 0 : 1
   statement {
     effect = "Allow"
     actions = ["backup:ListBackupVaults",
@@ -42,13 +41,11 @@ data "aws_iam_policy_document" "backup_actions_policy_document" {
 }
 
 resource "aws_iam_policy" "backup_actions_policy" {
-  count  = contains(["poc", "stage"], var.env_name) ? 0 : 1
-  name   = "backup_actions_policy"
+  name   = "${var.env_name}-backup_actions_policy"
   policy = data.aws_iam_policy_document.backup_actions_policy_document[0].json
 }
 
 resource "aws_iam_role_policy_attachment" "backup_actions_policy_attachment" {
-  count      = contains(["poc", "stage"], var.env_name) ? 0 : 1
-  role       = aws_iam_role.aws_backup_default_service_role[0].name
-  policy_arn = aws_iam_policy.backup_actions_policy[0].arn
+  role       = aws_iam_role.aws_backup_default_service_role.name
+  policy_arn = aws_iam_policy.backup_actions_policy.arn
 }
