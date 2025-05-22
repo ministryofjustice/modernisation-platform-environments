7b78b8c37f1b68056054360810f8f9ac70d2734b
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-05-22 16:44:11 +0100

    Overwrite previous version

--- terraform/environments/delius-core/modules/delius_environment/iam.tf
+++ terraform/environments/delius-core/modules/delius_environment/iam.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role_policy_attachment" "restore_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForRestores"
 }
 
-data "aws_iam_policy_document" "backup_actions_policy_document" {
+data "aws_iam_policy_document" "backup_action_policy_document" {
   statement {
     effect = "Allow"
     actions = ["backup:ListBackupVaults",
@@ -40,14 +40,14 @@ data "aws_iam_policy_document" "backup_actions_policy_document" {
   }
 }
 
-resource "aws_iam_policy" "backup_actions_policy" {
+resource "aws_iam_policy" "backup_action_policy" {
   count  = contains(["poc", "stage"], var.env_name) ? 0 : 1
-  name   = "backup_actions_policy"
-  policy = data.aws_iam_policy_document.backup_actions_policy_document.json
+  name   = "backup_action_policy"
+  policy = data.aws_iam_policy_document.backup_action_policy_document.json
 }
 
-resource "aws_iam_role_policy_attachment" "backup_actions_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "backup_action_policy_attachment" {
   count      = contains(["poc", "stage"], var.env_name) ? 0 : 1
   role       = aws_iam_role.aws_backup_default_service_role[0].name
-  policy_arn = aws_iam_policy.backup_actions_policy[0].arn
+  policy_arn = aws_iam_policy.backup_action_policy[0].arn
 }
