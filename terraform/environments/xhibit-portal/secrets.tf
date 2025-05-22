513884e052eeaf1af771c368e44964d78d983377
ep-93 <109581241+ep-93@users.noreply.github.com>
2025-05-22 10:29:37 +0100

    Update secrets.tf

--- terraform/environments/xhibit-portal/secrets.tf
+++ terraform/environments/xhibit-portal/secrets.tf
@@ -1,1 +1,15 @@
-#### This file can be used to store secrets specific to the member account ####
+resource "aws_secretsmanager_secret" "ip_block_list" {
+  name        = "ip_block_list"
+  description = "Secret for storing IP block list"
+  tags = merge(
+    local.tags
+  )
+}
+
+resource "aws_secretsmanager_secret_version" "ip_block_list_version" {
+  secret_id     = aws_secretsmanager_secret.ip_block_list.id
+  secret_string = "{}"  # Initialize as empty, populate later
+  tags = merge(
+    local.tags
+  )
+}
