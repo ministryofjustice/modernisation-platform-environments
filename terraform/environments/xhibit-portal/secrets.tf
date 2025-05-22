f7246fb720256c612aafaf493d2c47e146c6e9c4
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2025-05-22 14:20:27 +0100

    Sorted vulerabilities

--- terraform/environments/xhibit-portal/secrets.tf
+++ terraform/environments/xhibit-portal/secrets.tf
@@ -1,5 +1,6 @@
 resource "aws_secretsmanager_secret" "ip_block_list" {
 # checkov:skip=CKV2_AWS_57:Auto rotation not possible
+# checkov:skip=CKV_AWS_149: Default encryption is fine
   name        = "ip_block_list"
   description = "Secret for storing IP block list"
   tags = merge(
