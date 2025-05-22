4aeebf130a245b1ede29bed5210599f949dbdc32
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2025-05-22 12:19:08 +0100

    Added KMS key

--- terraform/environments/xhibit-portal/secrets.tf
+++ terraform/environments/xhibit-portal/secrets.tf
@@ -2,6 +2,7 @@ resource "aws_secretsmanager_secret" "ip_block_list" {
 # checkov:skip=CKV2_AWS_57:Auto rotation not possible
   name        = "ip_block_list"
   description = "Secret for storing IP block list"
+  kms_key_id = data.aws_kms_key.general_shared
   tags = merge(
     local.tags
   )
@@ -10,4 +11,5 @@ resource "aws_secretsmanager_secret" "ip_block_list" {
 resource "aws_secretsmanager_secret_version" "ip_block_list_version" {
   secret_id     = aws_secretsmanager_secret.ip_block_list.id
   secret_string = "{}"  # Initialize as empty, populate later
+  
 }
