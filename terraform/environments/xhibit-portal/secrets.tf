70bef4f18e87a93bae655e76b3e191a75eb74cab
ep-93 <109581241+ep-93@users.noreply.github.com>
2025-05-22 14:00:43 +0100

    Update secrets.tf

--- terraform/environments/xhibit-portal/secrets.tf
+++ terraform/environments/xhibit-portal/secrets.tf
@@ -2,7 +2,6 @@ resource "aws_secretsmanager_secret" "ip_block_list" {
 # checkov:skip=CKV2_AWS_57:Auto rotation not possible
   name        = "ip_block_list"
   description = "Secret for storing IP block list"
-  kms_key_id = data.aws_kms_key.general_shared.arn
   tags = merge(
     local.tags
   )
