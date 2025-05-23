16d05c81007aaefdb8bd0374820a2017f40fe415
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2025-05-23 11:42:20 +0100

    fixing resource duplicate name

--- terraform/environments/maat/maat-ecs.tf
+++ terraform/environments/maat/maat-ecs.tf
@@ -240,7 +240,7 @@ resource "aws_security_group_rule" "maat_sg_rule_outbound" {
   security_group_id = aws_security_group.maat_ecs_security_group.id
 }
 
-resource "aws_security_group_rule" "maat_sg_rule_outbound" {
+resource "aws_security_group_rule" "maat_to_maatdb_sg_rule_outbound" {
   type              = "egress"
   from_port         = 1521
   to_port           = 1521
