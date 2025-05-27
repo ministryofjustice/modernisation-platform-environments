fe1dd5f0b30c4804a5f7d72e08e7ed7c70034456
sony-pimpale-moj <sony.pimpale@digital.justice.gov.uk>
2025-05-27 09:41:27 +0100

    Updated Script to pass username and password

--- terraform/environments/digital-prison-reporting/modules/compute_node_windows/scripts/probation-discovery.ps1
+++ terraform/environments/digital-prison-reporting/modules/compute_node_windows/scripts/probation-discovery.ps1
@@ -1,9 +1,19 @@
+<powershell>
 # Example PowerShell startup script
 Write-Output "Bootstrapping Windows EC2 instance..."
 
+# Install AWS CLI
 Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.msi" -OutFile "C:\Windows\Temp\AWSCLIV2.msi"
 Start-Process "msiexec.exe" -ArgumentList "/i C:\Windows\Temp\AWSCLIV2.msi /qn" -Wait
 
-aws --version
+# (Optional) Check version
+& "C:\Program Files\Amazon\AWSCLIV2\aws.exe" --version
 
-aws s3 cp s3://dpr-artifact-store-development/third-party/PowerBI/PBIDesktopSetup_x64.exe C:\Windows\Temp\file.txt
+# Download file from S3
+& "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp s3://dpr-artifact-store-development/third-party/PowerBI/PBIDesktopSetup_x64.exe C:\Windows\Temp\PBIDesktopSetup_x64.exe
+
+# Set Administrator password directly
+net user Administrator "MySecurePassword123"
+</powershell>
+
+
