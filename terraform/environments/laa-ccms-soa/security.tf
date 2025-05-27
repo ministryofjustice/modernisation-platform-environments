dc8578b93b80585cb8947ea7255590f5c489398a
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-05-27 12:52:46 +0100

    fix: revert last change

--- terraform/environments/laa-ccms-soa/security.tf
+++ terraform/environments/laa-ccms-soa/security.tf
@@ -161,6 +161,8 @@ resource "aws_security_group_rule" "ecs_tasks_managed_egress_all" {
   type              = "egress"
   description       = "All"
   protocol          = -1
+  from_port         = 0
+  to_port           = 0
   cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
 }
 
@@ -206,6 +208,8 @@ resource "aws_security_group_rule" "cluster_ec2_egress_all" {
   type              = "egress"
   description       = "All Egress"
   protocol          = -1
+  from_port         = 0
+  to_port           = 0
   cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
 }
 
@@ -232,6 +236,8 @@ resource "aws_security_group_rule" "soa_db_egress_all" {
   type              = "egress"
   description       = "All Egress"
   protocol          = -1
+  from_port         = 0
+  to_port           = 0
   cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
 }
 
@@ -257,6 +263,8 @@ resource "aws_security_group_rule" "tds_db_egress_all" {
   type              = "egress"
   description       = "All Egress"
   protocol          = -1
+  from_port         = 0
+  to_port           = 0
   cidr_blocks       = ["0.0.0.0/0"] #--Tighten - AW.
 }
 
