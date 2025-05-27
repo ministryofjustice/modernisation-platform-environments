995cfc3b72ca4ca4675f6417159352b256766252
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-05-27 11:32:07 +0100

    Reinstate missing privileges

--- terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
+++ terraform/environments/delius-core/modules/components/oracle_db_shared/backup_vault.tf
@@ -31,6 +31,8 @@ data "aws_iam_policy_document" "oracle_ec2_snapshot_backup_role_policy_document"
       effect  = "Allow"
       actions = [
         "backup:StartBackupJob",
+        "backup:ListBackupVaults",
+        "backup:DescribeBackupJob",
         "ec2:DescribeInstances",
         "ec2:DescribeVolumes",
         "ec2:CreateSnapshot",
