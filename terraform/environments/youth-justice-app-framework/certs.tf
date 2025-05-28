b5e4f773aeb025bbcea696b15d06480bec2c622e
Greg Whiting <Gregory.Whiting@devtech.vocalink.co.uk>
2025-05-28 21:53:28 +0100

    fix secret resourtce

--- terraform/environments/youth-justice-app-framework/certs.tf
+++ terraform/environments/youth-justice-app-framework/certs.tf
@@ -5,7 +5,7 @@ module "certs" {
   project_name = local.project_name
 
   r53_zone_id    = module.public_dns_zone.aws_route53_zone_id
-  domain_name    = "yjaf-${local.application_data.accounts[local.environment].domain_name}"
+  domain_name    = "yjaf.${local.application_data.accounts[local.environment].domain_name}"
   validate_certs = local.application_data.accounts[local.environment].validate_certs
 
   tags = local.tags
--- terraform/environments/youth-justice-app-framework/cloudfront.tf
+++ terraform/environments/youth-justice-app-framework/cloudfront.tf
@@ -6,7 +6,7 @@ module "cloudfront_yjaf" {
     aws.us-east-1 = aws.us-east-1
   }
 
-  cloudfront_alias               = "yjaf-${local.application_data.accounts[local.environment].domain_name}"
+  cloudfront_alias               = "yjaf.${local.application_data.accounts[local.environment].domain_name}"
   alb_dns                        = module.external_alb.dns_name
   waf_web_acl_arn                = module.waf-cf.waf_arn
   r53_zone_id                    = module.public_dns_zone.aws_route53_zone_id
