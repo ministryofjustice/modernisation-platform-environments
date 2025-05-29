54f6067d8d9e5f45c62a97f9adf3e57bbe1b5d84
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2025-05-29 15:27:17 +0100

    Add new S3 bucket for storing XDR agent installer
    Created a new S3 bucket using the modernisation platform Terraform S3 module to store the XDR agent installer.
    
    Bucket prefix is based on the application name
    
    Versioning is disabled and force_destroy is off for safety
    
    Includes a lifecycle rule to automatically delete objects after 90 days
    
    Tags are applied using the shared local tags variable
    
    This bucket will support secure hosting of agent install files for ECS-based services.

---
+++ terraform/environments/maat/s3_xdr_agent_bucket.tf
@@ -0,0 +1,35 @@
+module "xdr-agent-s3" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v8.2.1"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  bucket_prefix       = "${local.application_name}-xdr-agent"
+  replication_enabled = false
+  versioning_enabled  = false
+  force_destroy       = false
+
+  lifecycle_rule = [
+    {
+      id      = "xdr-agent-cleanup"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "xdr"
+        autoclean = "true"
+      }
+
+      expiration = {
+        days = 90
+      }
+
+      noncurrent_version_expiration = {
+        days = 90
+      }
+    }
+  ]
+
+  tags = local.tags
+}
