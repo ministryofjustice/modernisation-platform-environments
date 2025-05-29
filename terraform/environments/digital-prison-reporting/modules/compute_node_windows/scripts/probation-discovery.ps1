c9a71fd4c0328b41c276e758c2a02ef572ab70e3
sony-pimpale-moj <sony.pimpale@digital.justice.gov.uk>
2025-05-29 03:29:03 +0100

    updated the script

--- terraform/environments/digital-prison-reporting/modules/compute_node_windows/scripts/probation-discovery.ps1
+++ terraform/environments/digital-prison-reporting/modules/compute_node_windows/scripts/probation-discovery.ps1
@@ -5,8 +5,16 @@ Write-Output "🔧 Bootstrapping Windows EC2 instance..."
 Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.msi" -OutFile "C:\Windows\Temp\AWSCLIV2.msi"
 Start-Process "msiexec.exe" -ArgumentList "/i C:\Windows\Temp\AWSCLIV2.msi /qn" -Wait
 
+# Persist AWS CLI in system PATH
+$awsCliPath = "C:\Program Files\Amazon\AWSCLIV2"
+$currentPath = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine)
+
+if ($currentPath -notlike "*$awsCliPath*") {
+  [Environment]::SetEnvironmentVariable("Path", "$currentPath;$awsCliPath", [EnvironmentVariableTarget]::Machine)
+}
+
 # Fetch RDP Secret from AWS Secrets Manager
-$secretJson = & "C:\Program Files\Amazon\AWSCLIV2\aws.exe" secretsmanager get-secret-value `
+$secretJson = & "$awsCliPath\aws.exe" secretsmanager get-secret-value `
   --secret-id "compute/dpr-windows-rdp-credentials" `
   --query SecretString `
   --output text `
@@ -27,12 +35,12 @@ Set-Service -Name TermService -StartupType Automatic
 Start-Service -Name TermService
 
 # Download Power BI installer
-& "C:\Program Files\Amazon\AWSCLIV2\aws.exe" s3 cp `
+& "$awsCliPath\aws.exe" s3 cp `
   s3://dpr-artifact-store-development/third-party/PowerBI/PBIDesktopSetup_x64.exe `
   C:\Windows\Temp\PBIDesktopSetup_x64.exe
 
 # Install Power BI silently
 Start-Process -FilePath "C:\Windows\Temp\PBIDesktopSetup_x64.exe" -ArgumentList "/quiet /norestart" -Wait
 
-Write-Output "Bootstrap complete."
+Write-Output "✅ Bootstrap complete."
 </powershell>
