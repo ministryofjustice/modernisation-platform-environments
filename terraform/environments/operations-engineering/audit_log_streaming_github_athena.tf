25dd607ede0e6d31d523bcdfec8bfa03b3ebb6be
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-05-29 20:38:02 +0100

    ✨ Add Athena Working Group

--- terraform/environments/operations-engineering/audit_log_streaming_github_athena.tf
+++ terraform/environments/operations-engineering/audit_log_streaming_github_athena.tf
@@ -7,3 +7,23 @@ module "github_audit_log_athena_results" {
   }
   tags = local.tags
 }
+
+resource "aws_athena_workgroup" "github_auditlog" {
+  name = "github-auditlog-wg"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${module.github_audit_log_athena_results.bucket.id}/results/"
+    }
+
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+    bytes_scanned_cutoff_per_query     = "1000000000" # 1GB, adjust as needed
+    requester_pays_enabled             = false
+  }
+
+  state = "ENABLED"
+
+  tags = local.tags
+}
+
