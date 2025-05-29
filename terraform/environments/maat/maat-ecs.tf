de9124dfcfb1227d33ce111fff5e5d181caba8f6
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2025-05-29 15:43:54 +0100

    Grant S3 access to EC2 instance role
    Added s3:GetObject permissions to the EC2 instance IAM policy to allow downloading of the XDR/EDR agent from the S3 bucket created for build artifacts. This will enable the user data script to retrieve the installer during ECS instance initialisation.

--- terraform/environments/maat/maat-ecs.tf
+++ terraform/environments/maat/maat-ecs.tf
@@ -66,6 +66,16 @@ resource "aws_iam_policy" "maat_ec2_instance_role_policy" {
         ]
         Resource = "*"
       },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject"
+        ],
+        Resource = [
+          "arn:aws:s3:::${module.xdr-agent-s3.bucket_name}",
+          "arn:aws:s3:::${module.xdr-agent-s3.bucket_name}/*"
+        ]
+      }
     ]
   })
 }
