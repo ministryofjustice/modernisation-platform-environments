9d448fd2c59b89839e5acc35f567ca7ef5e99e6d
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2025-05-29 14:42:01 +0100

    test commit

--- terraform/environments/coat/s3.tf
+++ terraform/environments/coat/s3.tf
@@ -1,4 +1,5 @@
 # CUR Reports
+# Test
 module "cur_s3_kms" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
