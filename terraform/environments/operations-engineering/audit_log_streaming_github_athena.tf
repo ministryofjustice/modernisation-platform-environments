34c9b017de6133947dcd0933920f98df98e02d40
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-05-29 21:22:01 +0100

    ✨ Add Glue Crawler

--- terraform/environments/operations-engineering/audit_log_streaming_github_athena.tf
+++ terraform/environments/operations-engineering/audit_log_streaming_github_athena.tf
@@ -35,3 +35,79 @@ resource "aws_glue_catalog_database" "github_auditlog" {
   tags = local.tags
 }
 
+# IAM Role that allows Glue to read from S3 and write to the catalog
+resource "aws_iam_role" "glue_github_auditlog_crawler" {
+  name = "glue-github-auditlog-crawler-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "glue.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Minimal policy: S3 read + Glue write
+resource "aws_iam_role_policy" "glue_github_auditlog_policy" {
+  role = aws_iam_role.glue_github_auditlog_crawler.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "arn:aws:s3:::${module.github-cloudtrail-auditlog.github_auditlog_s3bucket}",
+          "arn:aws:s3:::${module.github-cloudtrail-auditlog.github_auditlog_s3bucket}/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "glue:CreateTable",
+          "glue:GetTable",
+          "glue:UpdateTable",
+          "glue:GetDatabase",
+          "glue:UpdateDatabase"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Glue Crawler
+resource "aws_glue_crawler" "github_auditlog" {
+  name = "github-auditlog-crawler"
+
+  role = aws_iam_role.glue_github_auditlog_crawler.arn
+
+  database_name = aws_glue_catalog_database.github_auditlog.name
+
+  s3_target {
+    path = "s3://${module.github-cloudtrail-auditlog.github_auditlog_s3bucket}/"
+  }
+
+  # Configuration to optimize schema inference and control partitions
+  configuration = jsonencode({
+    Version = 1.0,
+    Grouping = {
+      TableGroupingPolicy = "CombineCompatibleSchemas"
+    },
+    CrawlerOutput = {
+      Partitions = {
+        AddOrUpdateBehavior = "InheritFromTable"
+      }
+    }
+  })
+
+  tags = local.tags
+}
