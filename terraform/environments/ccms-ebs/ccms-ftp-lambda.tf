93208045217cdfd5807aedb05800df89d111723f
vikasbsuresh-moj <vikas.suresh@justice.gov.uk>
2025-05-29 17:00:28 +0100

    feat: adding ftp lambda module

---
+++ terraform/environments/ccms-ebs/ccms-ftp-lambda.tf
@@ -0,0 +1,60 @@
+
+### secrets for ftp user and password
+resource "aws_secretsmanager_secret" "ftp_password" {
+  name        = lower(format("laa-ccms-ebs-ftp-password-%s",local.environment))
+  description = "A secret for storing ftp server password"
+}
+
+resource "aws_secretsmanager_secret" "ftp_user" {
+  name        = lower(format("laa-ccms-ebs-ftp-user-%s",local.environment))
+  description = "A secret for storing ftp server user"
+}
+
+
+
+
+#### bucket for laa-ccms-inbound for storing files from lambda
+resource "aws_s3_bucket" "inbound_bucket" {
+  bucket = lower(format("laa-ccms-inbound-%s-mp",local.environment))  # ccms inbound bucket
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_bucket_encryption" {
+  bucket = aws_s3_bucket.inbound_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+#LAA-ftp-allpay-inbound-ccms
+module "ftp_lambda" {
+  source              = "./modules/ftp-lambda"
+  lambda_name         = lower(format("LAA-ftp-allpay-inbound-ccms-%s",local.environment))
+  vpc_id              = data.aws_vpc.shared.id
+  subnet_ids          = [data.aws_subnet.private_subnets_a.id, data.aws_subnet.private_subnets_b.id,data.aws_subnet.private_subnets_c.id]
+  ftp_host            = "sftp.allpay.cloud"
+  ftp_port            = "22"
+  ftp_protocol        = "FTPS"
+  ftp_transfer_type   = "SFTP_DOWNLOAD"
+  ftp_file_types      = ""
+  ftp_local_path      = "CCMS_PRD_Allpay/Inbound/"
+  ftp_remote_path     = "/Outbound/"
+  ftp_require_ssl     = "NO"
+  ftp_insecure        = "NO"
+  ftp_ca_cert         = ""
+  ftp_cert            = ""
+  ftp_key             = ""
+  ftp_key_type        = ""
+  ftp_user            = ""
+  ftp_password_path   = "/secure/path"
+  ftp_file_remove     = "YES"
+  ftp_cron            = "cron(0 10 * * ? *)"
+  ftp_bucket          = aws_s3_bucket.inbound_bucket.id
+  sns_topic_sev5      = "arn:aws:sns:region:account:sev5"
+  sns_topic_ops       = "arn:aws:sns:region:account:ops"
+  ssh_key_path        = ""
+}
---
+++ terraform/environments/ccms-ebs/modules/ftp-lambda/main.tf
@@ -0,0 +1,149 @@
+## sg for ftp
+resource "aws_security_group" "ftp_sg" {
+  name        = "${var.lambda_name}-sg"
+  description = "Allow outbound connection"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+## lambda role for ftp
+resource "aws_iam_role" "ftp_lambda_role" {
+  name = "${var.lambda_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.ftp_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "ftp_policy" {
+  name = "${var.lambda_name}-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["ec2:CreateNetworkInterface", "ec2:DescribeNetworkInterfaces", "ec2:DeleteNetworkInterface"],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action   = ["s3:*"],
+        Effect   = "Allow",
+        Resource = [
+          "arn:aws:s3:::${var.ftp_bucket}",
+          "arn:aws:s3:::${var.ftp_bucket}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ftp_lambda_policy_attach" {
+  role       = aws_iam_role.ftp_lambda_role.name
+  policy_arn = aws_iam_policy.ftp_policy.arn
+}
+### lambda layer for python dependencies
+resource "aws_lambda_layer_version" "ftp_layer" {
+  layer_name          = "ftpclientlibs"
+  compatible_runtimes = ["python3.7"]
+  filename            = "ftpclientlibs.zip"
+  source_code_hash = filebase64sha256("./lambda/ftpclientlibs.zip")
+}
+#### lambda function for ftp inbound
+resource "aws_lambda_function" "ftp_lambda" {
+  function_name = var.lambda_name
+  role          = aws_iam_role.ftp_lambda_role.arn
+  handler       = "ftpclient.lambda_handler"
+  runtime       = "python3.7"
+  timeout       = 300
+  memory_size   = 256
+  filename         = "ftp-client.zip"
+  source_code_hash = filebase64sha256("./lambda/ftp-client.zip")
+  layers        = [aws_lambda_layer_version.ftp_layer.arn]
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = [aws_security_group.ftp_sg.id]
+  }
+
+  environment {
+    variables = {
+      HOST         = var.ftp_host
+      PORT         = var.ftp_port
+      PROTOCOL     = var.ftp_protocol
+      FILETYPES    = var.ftp_file_types
+      TRANSFERTYPE = var.ftp_transfer_type
+      LOCALPATH    = var.ftp_local_path
+      REMOTEPATH   = var.ftp_remote_path
+      REQUIRE_SSL  = var.ftp_require_ssl
+      INSECURE     = var.ftp_insecure
+      CA_CERT      = var.ftp_ca_cert
+      CERT         = var.ftp_cert
+      KEY          = var.ftp_key
+      KEY_TYPE     = var.ftp_key_type
+      USER         = var.ftp_user
+      PASSWORD     = ""
+      SSH_KEY      = ""
+      S3BUCKET     = var.ftp_bucket
+      FILEREMOVE   = var.ftp_file_remove
+    }
+  }
+}
+### cw rule for schedule
+resource "aws_cloudwatch_event_rule" "ftp_schedule" {
+  name                = "${var.lambda_name}-schedule"
+  schedule_expression = var.ftp_cron
+}
+### cw event lambda target
+resource "aws_cloudwatch_event_target" "ftp_target" {
+  rule      = aws_cloudwatch_event_rule.ftp_schedule.name
+  target_id = "ftp-lambda"
+  arn       = aws_lambda_function.ftp_lambda.arn
+}
+
+### attaching lambda iam role to inbound bucket
+resource "aws_lambda_permission" "ftp_permission" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ftp_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.ftp_schedule.arn
+}
+
+
+### crating cw metric alarm
+resource "aws_cloudwatch_metric_alarm" "ftp_errors" {
+  alarm_name          = "${var.lambda_name}-Errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 0
+  treat_missing_data  = "ignore"
+
+  dimensions = {
+    FunctionName = aws_lambda_function.ftp_lambda.function_name
+  }
+
+  alarm_description = "Errors occurred in Lambda"
+  alarm_actions     = [var.sns_topic_sev5, var.sns_topic_ops]
+  ok_actions        = [var.sns_topic_sev5, var.sns_topic_ops]
+}
