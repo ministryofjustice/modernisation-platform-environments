dbba9c58ef5fc3a24894ea02d6b1c64ea7291b24
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2025-05-30 12:42:30 +0100

    adding new rules to dynamic block

--- terraform/environments/mlra/modules/ecs/main.tf
+++ terraform/environments/mlra/modules/ecs/main.tf
@@ -119,28 +119,6 @@ resource "aws_security_group" "cluster_ec2" {
   )
 }
 
-
-resource "aws_security_group_rule" "mlra_sg_rule_outbound" {
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "This rule is needed for the ECS agent to reach the ECS API endpoints"
-  security_group_id = aws_security_group.cluster_ec2.id
-}
-
-# Specific Security Group Rule for Access to MAATDB
-resource "aws_security_group_rule" "mlra_to_maatdb_sg_rule_outbound" {
-  type              = "egress"
-  from_port         = 1521
-  to_port           = 1521
-  protocol          = "tcp"
-  description       = "This rule is needed for the MLRA to connect to MAATDB"
-  security_group_id = aws_security_group.cluster_ec2.id
-  source_security_group_id = var.maatdb_rds_sec_group_id
-}
-
 # always use the recommended ECS optimized linux 2 base image; used to obtain its AMI ID
 data "aws_ssm_parameter" "ecs_optimized_ami_1" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
