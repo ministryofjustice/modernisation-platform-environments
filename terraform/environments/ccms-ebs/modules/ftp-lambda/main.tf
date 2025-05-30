5f8bdeaa626b17d261821c0ab39e1a168ebe3586
vikasbsuresh-moj <vikas.suresh@justice.gov.uk>
2025-05-30 17:07:10 +0100

    fix: fix zip file path

--- terraform/environments/ccms-ebs/modules/ftp-lambda/main.tf
+++ terraform/environments/ccms-ebs/modules/ftp-lambda/main.tf
@@ -63,7 +63,7 @@ resource "aws_lambda_layer_version" "ftp_layer" {
   layer_name          = "ftpclientlibs"
   compatible_runtimes = ["python3.7"]
   filename            = "ftpclientlibs.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda/ftpclientlibs.zip")
+  source_code_hash = filebase64sha256("${path.module}/ftp-lambda/lambda/ftpclientlibs.zip")
 }
 #### lambda function for ftp inbound
 resource "aws_lambda_function" "ftp_lambda" {
@@ -74,7 +74,7 @@ resource "aws_lambda_function" "ftp_lambda" {
   timeout       = 300
   memory_size   = 256
   filename         = "ftp-client.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda/ftp-client.zip")
+  source_code_hash = filebase64sha256("${path.module}/ftp-lambda/lambda/ftp-client.zip")
   layers        = [aws_lambda_layer_version.ftp_layer.arn]
 
   vpc_config {
