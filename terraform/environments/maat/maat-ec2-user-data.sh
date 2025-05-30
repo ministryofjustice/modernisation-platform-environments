83ad8db13a4e6105c7921fa07a5a68512f971951
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2025-05-30 13:59:32 +0100

    Switch to passing in variables explicitly via Terraform
    Updated the user_data script to receive required variables (e.g. XDR_TAR,
    XDR_BUCKET) through the Terraform `templatefile()` vars map instead of
    relying on in-script bash variable definitions. This resolves issues with
    Terraform interpreting or failing to render undefined variables during
    template evaluation.

--- terraform/environments/maat/maat-ec2-user-data.sh
+++ terraform/environments/maat/maat-ec2-user-data.sh
@@ -28,24 +28,21 @@ sudo systemctl start awslogsd
 sudo systemctl enable awslogsd.service
 
 # Install XDR agent stored in S3 bucket
-XDR_DIR="/tmp/cortex-agent"
-XDR_TAR="/tmp/cortex-agent.tar.gz"
+aws s3 cp "s3://${XDR_BUCKET}/cortex-agent.tar.gz" ${XDR_TAR}
 
-aws s3 cp "s3://${xdr_bucket}/cortex-agent.tar.gz" \${XDR_TAR}
+if [[ -f ${XDR_TAR} ]]; then
+  mkdir -p ${XDR_DIR}
+  tar -xzf ${XDR_TAR} -C ${XDR_DIR}
 
-if [[ -f \${XDR_TAR} ]]; then
-  mkdir -p \${XDR_DIR}
-  tar -xzf \${XDR_TAR} -C \${XDR_DIR}
-
-  if [[ -f \${XDR_DIR}/cortex.conf ]]; then
+  if [[ -f ${XDR_DIR}/cortex.conf ]]; then
     sudo mkdir -p /etc/panw
-    sudo cp \${XDR_DIR}/cortex.conf /etc/panw/
+    sudo cp ${XDR_DIR}/cortex.conf /etc/panw/
   else
     echo "Missing cortex.conf in extracted archive" >&2
     exit 1
   fi
 
-  sudo yum install -y \${XDR_DIR}/*.rpm
+  sudo yum install -y ${XDR_DIR}/*.rpm
 else
   echo "XDR agent download failed" >&2
   exit 1
