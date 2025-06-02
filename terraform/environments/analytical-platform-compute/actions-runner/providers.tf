92651c4930d5359c89f0e543eabb988aea02b896
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-06-02 14:32:37 +0100

    amend script location

--- terraform/environments/analytical-platform-compute/actions-runner/providers.tf
+++ terraform/environments/analytical-platform-compute/actions-runner/providers.tf
@@ -1,12 +1,3 @@
-# AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
-provider "aws" {
-  alias  = "analytical-platform-compute-eu-west-1"
-  region = "eu-west-1"
-  assume_role {
-    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
-  }
-}
-
 # Provider for interacting with the EKS cluster
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.apc_cluster.endpoint
@@ -14,19 +5,6 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "bash"
-    args        = ["scripts/eks-authentication.sh", local.environment_management.account_ids[terraform.workspace], data.aws_eks_cluster.apc_cluster.name]
-  }
-}
-
-# Provider for interacting with the EKS cluster using Helm
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.apc_cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.apc_cluster.certificate_authority[0].data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "bash"
-      args        = ["scripts/eks-authentication.sh", local.environment_management.account_ids[terraform.workspace], data.aws_eks_cluster.apc_cluster.name]
-    }
+    args        = ["../scripts/eks-authentication.sh", local.environment_management.account_ids[terraform.workspace], data.aws_eks_cluster.apc_cluster.name]
   }
 }
