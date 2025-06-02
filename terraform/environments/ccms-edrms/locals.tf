4c3e2683925e5530de09831934ebb8894bdf3b3f
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-06-02 12:20:22 +0100

    initial build of edrms

--- terraform/environments/ccms-edrms/locals.tf
+++ terraform/environments/ccms-edrms/locals.tf
@@ -1,1 +1,13 @@
-#### This file can be used to store locals specific to the member account ####
+locals {
+  data_subnets_cidr_blocks = [
+    data.aws_subnet.data_subnets_a.cidr_block,
+    data.aws_subnet.data_subnets_b.cidr_block,
+    data.aws_subnet.data_subnets_c.cidr_block
+  ]
+
+  private_subnets_cidr_blocks = [
+    data.aws_subnet.private_subnets_a.cidr_block,
+    data.aws_subnet.private_subnets_b.cidr_block,
+    data.aws_subnet.private_subnets_c.cidr_block
+  ]
+}
--- terraform/environments/ccms-edrms/secrets.tf
+++ terraform/environments/ccms-edrms/secrets.tf
@@ -1,1 +1,6 @@
 #### This file can be used to store secrets specific to the member account ####
+
+resource "aws_secretsmanager_secret" "spring_datasource_password" {
+  name        = "ccms/edrms/datasource"
+  description = "SOA TDS database password for CCMS EDRMS application"
+}
