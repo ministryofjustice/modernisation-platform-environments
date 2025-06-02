41a4d2132b01db0c3d9b984508857bf2bf955853
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2025-06-02 12:05:13 +0100

    checkov updates

--- terraform/environments/maat/maat-ext-lb.tf
+++ terraform/environments/maat/maat-ext-lb.tf
@@ -132,7 +132,7 @@ resource "aws_lb" "external" {
   subnets                    = [data.aws_subnet.public_subnets_a.id, data.aws_subnet.public_subnets_b.id, data.aws_subnet.public_subnets_c.id]
   enable_deletion_protection = local.application_data.accounts[local.environment].ext_lb_enable_deletion_protection
   idle_timeout               = local.external_lb_idle_timeout
-  drop_invalid_header_fields = false
+  drop_invalid_header_fields = true
 
   access_logs {
     bucket  = local.existing_bucket_name != "" ? local.existing_bucket_name : module.lb-s3-access-logs[0].bucket.id
