bdbbedc0d57509d0425c9cee4041e72cba3905a6
Luke Williams <108728588+luke-a-williams@users.noreply.github.com>
2025-06-03 18:15:09 +0100

    removal of self referencing ingress/egress rule within RDS security group

--- terraform/environments/electronic-monitoring-data/server_backups.tf
+++ terraform/environments/electronic-monitoring-data/server_backups.tf
@@ -129,16 +129,6 @@ resource "aws_vpc_security_group_ingress_rule" "db_ipv4_lb" {
   cidr_ipv4 = "209.35.83.77/32"
 }
 
-resource "aws_vpc_security_group_egress_rule" "db_outbound" {
-
-  security_group_id            = aws_security_group.db.id
-  referenced_security_group_id = aws_security_group.db.id
-  ip_protocol                  = "tcp"
-  from_port                    = 0
-  to_port                      = 65535
-  description                  = "RDS Database -----[All ports]-----+ RDS Database"
-}
-
 resource "aws_db_subnet_group" "db" {
   name       = "db-subnet-group"
   subnet_ids = data.aws_subnets.shared-public.ids
