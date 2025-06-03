17c7c09779b354bdfe7508654bd93c249d07661d
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-06-03 16:34:07 +0100

    s3 buckets removed

--- terraform/environments/operations-engineering/s3.tf
+++ terraform/environments/operations-engineering/s3.tf
@@ -1,48 +1,48 @@
 # CUR v2 bucket
 
-module "cur_v2_hourly" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+# module "cur_v2_hourly" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.3.0"
+#   source  = "terraform-aws-modules/s3-bucket/aws"
+#   version = "4.3.0"
 
-  bucket = "cur-v2-hourly"
+#   bucket = "cur-v2-hourly"
 
-  force_destroy = true
+#   force_destroy = true
 
-  attach_deny_insecure_transport_policy = true
+#   attach_deny_insecure_transport_policy = true
 
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = module.cur_s3_kms.key_arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-}
+#   server_side_encryption_configuration = {
+#     rule = {
+#       apply_server_side_encryption_by_default = {
+#         kms_master_key_id = module.cur_s3_kms.key_arn
+#         sse_algorithm     = "aws:kms"
+#       }
+#     }
+#   }
+# }
 
-module "gpx_output_bucket" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+# module "gpx_output_bucket" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.3.0"
+#   source  = "terraform-aws-modules/s3-bucket/aws"
+#   version = "4.3.0"
 
-  bucket = "gpx-cur-output-bucket"
+#   bucket = "gpx-cur-output-bucket"
 
-  force_destroy = true
+#   force_destroy = true
 
-  attach_deny_insecure_transport_policy = true
+#   attach_deny_insecure_transport_policy = true
 
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = module.gpx_s3_kms.key_arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-}
+#   server_side_encryption_configuration = {
+#     rule = {
+#       apply_server_side_encryption_by_default = {
+#         kms_master_key_id = module.gpx_s3_kms.key_arn
+#         sse_algorithm     = "aws:kms"
+#       }
+#     }
+#   }
+# }
 
 # GitHub repositories Terraform state bucket
 
