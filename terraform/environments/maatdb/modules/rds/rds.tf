df4337f0b8fb65d9c2b4e03c911c051a6e494d3f
mikereiddigital <mike.reid@justice.gov.uk>
2025-06-03 11:45:12 +0100

    Further testing

--- terraform/environments/maatdb/modules/rds/rds.tf
+++ terraform/environments/maatdb/modules/rds/rds.tf
@@ -13,8 +13,8 @@ locals {
   rds_sg_group_ids = compact([
     aws_security_group.cloud_platform_sec_group.id,
     aws_security_group.bastion_sec_group.id,
-    aws_security_group.vpc_sec_group.id,
-    aws_security_group.mlra_ecs_sec_group.id
+    length(aws_security_group.vpc_sec_group) > 0 ? aws_security_group.vpc_sec_group[0].id : "",
+    length(aws_security_group.mlra_ecs_sec_group) > 0 ? aws_security_group.mlra_ecs_sec_group[0].id : ""
   ])
 
 }
