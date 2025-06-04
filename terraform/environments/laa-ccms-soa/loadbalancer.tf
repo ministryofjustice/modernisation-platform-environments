a8c9b02083302b4e2d002dbdd4b53657e988ab73
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-06-04 09:54:21 +0100

    feaat: recreate albs as nlbs, teardown tgs and listeners

--- terraform/environments/laa-ccms-soa/loadbalancer.tf
+++ terraform/environments/laa-ccms-soa/loadbalancer.tf
@@ -9,7 +9,7 @@ resource "aws_lb" "admin" {
   security_groups    = [aws_security_group.alb_admin.id]
 }
 
-resource "aws_lb_target_group" "admin" {
+/* resource "aws_lb_target_group" "admin" {
   name                 = "${local.application_data.accounts[local.environment].app_name}-admin-target-group"
   port                 = local.application_data.accounts[local.environment].admin_server_port
   protocol             = "TCP"
@@ -17,7 +17,7 @@ resource "aws_lb_target_group" "admin" {
   target_type          = "ip"
   deregistration_delay = 30
 
-    health_check {
+  health_check {
     healthy_threshold   = "3"
     interval            = "30"
     protocol            = "TCP"
@@ -49,21 +49,6 @@ resource "aws_lb_listener" "admin_server_port" {
     target_group_arn = aws_lb_target_group.admin.id
     type             = "forward"
   }
-}
-
-#--Don't think an HTTPS listener is actually needed. Disabling. AW
-/* resource "aws_lb_listener" "admin443" {
-  load_balancer_arn = aws_lb.admin.id
-  port              = 443
-  protocol          = "TLS"
-
-  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn = local.application_data.accounts[local.environment].admin_loadbalancer_certificate_arn #--NEED TO POPULATE, MAKE A DUMMY CERT FOR NOW
-
-  default_action {
-    target_group_arn = aws_lb_target_group.admin.id
-    type             = "forward"
-  }
 } */
 
 #--Managed
@@ -75,14 +60,14 @@ resource "aws_lb" "managed" {
   security_groups    = [aws_security_group.alb_managed.id]
 }
 
-resource "aws_lb_target_group" "managed" {
+/* resource "aws_lb_target_group" "managed" {
   name        = "${local.application_data.accounts[local.environment].app_name}-managed-target-group"
   port        = local.application_data.accounts[local.environment].managed_server_port
   protocol    = "TCP"
   vpc_id      = data.aws_vpc.shared.id
   target_type = "ip"
 
-    health_check {
+  health_check {
     healthy_threshold   = "3"
     interval            = "30"
     protocol            = "TCP"
@@ -109,21 +94,6 @@ resource "aws_lb_listener" "managed_server_port" {
   load_balancer_arn = aws_lb.managed.id
   port              = local.application_data.accounts[local.environment].managed_server_port
   protocol          = "TCP"
-
-  default_action {
-    target_group_arn = aws_lb_target_group.managed.id
-    type             = "forward"
-  }
-}
-
-#--Don't think an HTTPS listener is actually needed. Disabling. AW
-/* resource "aws_lb_listener" "managed443" {
-  load_balancer_arn = aws_lb.managed.id
-  port              = 443
-  protocol          = "HTTPS"
-
-  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn = local.application_data.accounts[local.environment].managed_loadbalancer_certificate_arn
 
   default_action {
     target_group_arn = aws_lb_target_group.managed.id
