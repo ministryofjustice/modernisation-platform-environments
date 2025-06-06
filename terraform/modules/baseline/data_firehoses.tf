9481ca2e481e053e427dbfa602764eb7db178fd8
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-06-06 12:03:04 +0100

    -

--- terraform/modules/baseline/data_firehoses.tf
+++ terraform/modules/baseline/data_firehoses.tf
@@ -8,7 +8,7 @@ locals {
 module "data_firehose" {
   for_each = local.data_firehoses
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-data-firehose?ref=dc2fbd0e5f36415037db149d31dc5be99adf1862"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-data-firehose?ref=1851b164e43db853ff45bb4393ec76b89cb0edbf"
 
   cloudwatch_log_group_names = each.value.cloudwatch_log_group_names
   destination_http_endpoint  = aws_ssm_parameter.placeholder[each.value.destination_http_endpoint_ssm_parameter_name].value
