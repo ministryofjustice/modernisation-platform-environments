82155d0d284fa1057337078b12bed3d30f628743
ttipler <thomas.tipler@necsws.com>
2025-06-09 12:49:40 +0100

    fix ses issue

--- terraform/environments/youth-justice-app-framework/modules/ses/r53.tf
+++ terraform/environments/youth-justice-app-framework/modules/ses/r53.tf
@@ -1,27 +1,9 @@
 
 
-### WORK AROUND AND NEEDS TO BE REMOVED ONCE WE FIX THE R53 ISSUE 
-### ORIGINAL BLOCK BELOW
-### data "aws_route53_zone" "zones" {
-###  for_each = { for k, v in var.ses_domain_identities : k => v if v.create_records }
-###  name     = "${each.value.identity}."
-###   }
-
-data "aws_route53_zone" "zone_id_override" {
-  for_each = {
-    for k, v in var.ses_domain_identities :
-    k => v
-    if v.create_records && var.environment == "production" && v.identity == "production.yjbservices.yjb.gov.uk"
-  }
-  zone_id       = "Z03007883SPT4Z4CG736D"
-  private_zone  = false
-}
-
 data "aws_route53_zone" "zones" {
   for_each = { for k, v in var.ses_domain_identities : k => v if v.create_records }
   name     = "${each.value.identity}."
 }
-
 
 resource "aws_route53_record" "ses_verification" {
   for_each = { for k, v in var.ses_domain_identities : k => v if v.create_records }
