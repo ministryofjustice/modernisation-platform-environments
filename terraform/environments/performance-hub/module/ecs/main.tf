6edcd177d0e0dca043e238f735f872f3a1d7fc9d
Jeremy Collins <jeremy.collins@digital.justice.gov.uk>
2025-06-09 07:46:57 +0100

    Update win AMI from 2019 to 2022

--- terraform/environments/performance-hub/module/ecs/main.tf
+++ terraform/environments/performance-hub/module/ecs/main.tf
@@ -100,7 +100,7 @@ resource "aws_security_group" "cluster_ec2" {
 
 resource "aws_launch_template" "ec2-launch-template" {
   name_prefix   = "${var.app_name}-ec2-launch-template"
-  image_id      = "resolve:ssm:/aws/service/ami-windows-latest/Windows_Server-2019-English-Full-ECS_Optimized/image_id"
+  image_id      = "resolve:ssm:/aws/service/ami-windows-latest/Windows_Server-2022-English-Full-ECS_Optimized/image_id"
   instance_type = var.instance_type
   key_name      = var.key_name
   ebs_optimized = true
