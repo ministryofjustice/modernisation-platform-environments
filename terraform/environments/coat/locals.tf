6add4c2aa6ef171562ba07a817251980b6a6dbc1
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-06-10 14:09:12 +0100

    s3 cross role update

--- terraform/environments/coat/locals.tf
+++ terraform/environments/coat/locals.tf
@@ -1,6 +1,15 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
-  prod_environment  = "production"
-  dev_environment   = "development"
+
+  env_account_id = local.environment_management.account_ids[terraform.workspace]
+
+  prod_environment     = "production"
+  dev_environment      = "development"
+  coat_prod_account_id = local.environment_management.account_ids["coat-production"]  #"279191903737"
+  coat_dev_account_id  = local.environment_management.account_ids["coat-development"] #"082282578003"
+
   cross_environment = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" ? local.dev_environment : local.prod_environment
+
+  cross_env_account_id = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" ? local.coat_dev_account_id : local.coat_prod_account_id
+
 }
