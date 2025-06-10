9e8bfefa113695a8a14e36807052301a456fa78a
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-06-10 11:53:50 +0100

    s3 cross account added

--- terraform/environments/coat/locals.tf
+++ terraform/environments/coat/locals.tf
@@ -1,6 +1,8 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
-  prod_environment  = "production"
-  dev_environment   = "development"
-  cross_environment = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" ? local.dev_environment : local.prod_environment
+  prod_environment    = "production"
+  dev_environment     = "development"
+  coat_production_id  = "279191903737"
+  coat_development_id = "082282578003"
+  cross_environment   = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" ? local.dev_environment : local.prod_environment
 }
