8452459c7a149e9e8e307096f24dded7f8148d04
vikasbsuresh-moj <vikas.suresh@justice.gov.uk>
2025-06-11 12:17:12 +0100

    feat: adding xerox secret and s3 bucket with soa role

--- terraform/environments/ccms-ebs/ccms-ftp-lambda.tf
+++ terraform/environments/ccms-ebs/ccms-ftp-lambda.tf
@@ -14,7 +14,8 @@ locals {
     "LAA-ftp-tdx-inbound-ccms-exceptionnotif",
     "LAA-ftp-eckoh-inbound-ccms",
     "LAA-ftp-1stlocate-ccms-inbound",
-    "LAA-ftp-rossendales-nct-inbound-product"
+    "LAA-ftp-rossendales-nct-inbound-product",
+    "LAA-ftp-xerox-outbound"
   ]
   base_buckets = ["laa-ccms-inbound", "laa-ccms-outbound", "laa-ccms-ftp-lambda"]
 
@@ -31,128 +32,128 @@ resource "aws_secretsmanager_secret" "secrets" {
   name = "${each.value}-${local.environment}"
 }
 
-# data "aws_secretsmanager_secret_version" "secrets" {
-#   for_each  = toset(local.secret_names)
-#   secret_id = "${each.value}-${local.environment}"
-# }
+data "aws_secretsmanager_secret_version" "secrets" {
+  for_each  = toset(local.secret_names)
+  secret_id = "${each.value}-${local.environment}"
+}
 
-# resource "aws_s3_bucket" "buckets" {
-#   for_each = toset(local.bucket_names)
+resource "aws_s3_bucket" "buckets" {
+  for_each = toset(local.bucket_names)
 
-#   bucket = each.value
+  bucket = each.value
 
-#   tags = {
-#     Name        = each.value
-#     Environment = local.environment
-#   }
-# }
+  tags = {
+    Name        = each.value
+    Environment = local.environment
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
-#   for_each = aws_s3_bucket.buckets
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+  for_each = aws_s3_bucket.buckets
 
-#   bucket = each.value.id
+  bucket = each.value.id
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "s3_versioning" {
-#   for_each = aws_s3_bucket.buckets
-#   bucket = each.value.id
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  for_each = aws_s3_bucket.buckets
+  bucket = each.value.id
 
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
-
-
-# resource "aws_s3_bucket_policy" "inbound_bucket_policy" {
-#   bucket = aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].bucket
-
-#   policy = jsonencode({
-#     "Version": "2012-10-17",
-#     "Id": "AccessFromMP",
-#     "Statement": [
-#         {
-#             "Sid": "Access_for_ccms-ebs_and_soa",
-#             "Effect": "Allow",
-#             "Principal": {
-#                 "AWS": [
-#                     "arn:aws:iam::${local.application_data.accounts[local.environment].soa_account_id}:role/ccms-soa-ec2-instance-role",
-#                     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role_stsassume_oracle_base"
-#                 ]
-#             },
-#             "Action": [
-#                 "s3:PutObject",
-#                 "s3:GetObject",
-#                 "s3:ListBucket",
-#                 "s3:DeleteObject"
-#             ],
-#             "Resource": [
-#                 aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].arn,
-#                 "${aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].arn}/*"
-#             ]
-#         }
-#     ]
-#    }
-#   )
-# }
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 
-# resource "aws_s3_bucket_policy" "outbound_bucket_policy" {
-#   bucket = aws_s3_bucket.buckets["laa-ccms-outbound-${local.environment}-mp"].bucket
+resource "aws_s3_bucket_policy" "inbound_bucket_policy" {
+  bucket = aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].bucket
 
-#   policy = jsonencode({
-#     "Version": "2012-10-17",
-#     "Id": "AccessFromMP",
-#     "Statement": [
-#         {
-#             "Sid": "Access_for_ccms-ebs_and_soa",
-#             "Effect": "Allow",
-#             "Principal": {
-#                 "AWS": [
-#                     "arn:aws:iam::${local.application_data.accounts[local.environment].soa_account_id}:role/ccms-soa-ec2-instance-role",
-#                     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role_stsassume_oracle_base"
-#                 ]
-#             },
-#             "Action": [
-#                 "s3:PutObject",
-#                 "s3:GetObject",
-#                 "s3:ListBucket",
-#                 "s3:DeleteObject"
-#             ],
-#             "Resource": [
-#                 aws_s3_bucket.buckets["laa-ccms-outbound-${local.environment}-mp"].arn,
-#                 "${aws_s3_bucket.buckets["laa-ccms-outbound-${local.environment}-mp"].arn}/*"
-#             ]
-#         }
-#     ]
-#    }
-#   )
-# }
-
-
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Id": "AccessFromMP",
+    "Statement": [
+        {
+            "Sid": "Access_for_ccms-ebs_and_soa",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::${local.application_data.accounts[local.environment].soa_account_id}:role/ccms-soa-ec2-instance-role",
+                    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role_stsassume_oracle_base"
+                ]
+            },
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:ListBucket",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].arn,
+                "${aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].arn}/*"
+            ]
+        }
+    ]
+   }
+  )
+}
 
 
-# locals {
-#   secrets_map = {
-#     for name in local.secret_names :
-#     name => jsondecode(data.aws_secretsmanager_secret_version.secrets[name].secret_string)
-#   }
+resource "aws_s3_bucket_policy" "outbound_bucket_policy" {
+  bucket = aws_s3_bucket.buckets["laa-ccms-outbound-${local.environment}-mp"].bucket
 
-#   # Optionally extract just user/password maps
-#   credentials_map = {
-#     for name, creds in local.secrets_map :
-#     name => {
-#       user     = creds.USER
-#       password = creds.PASSWORD
-#       ssh_key  = creds.SSH_KEY
-#     }
-#   }
-# }
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Id": "AccessFromMP",
+    "Statement": [
+        {
+            "Sid": "Access_for_ccms-ebs_and_soa",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::${local.application_data.accounts[local.environment].soa_account_id}:role/ccms-soa-ec2-instance-role",
+                    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/role_stsassume_oracle_base"
+                ]
+            },
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:ListBucket",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                aws_s3_bucket.buckets["laa-ccms-outbound-${local.environment}-mp"].arn,
+                "${aws_s3_bucket.buckets["laa-ccms-outbound-${local.environment}-mp"].arn}/*"
+            ]
+        }
+    ]
+   }
+  )
+}
+
+
+
+
+locals {
+  secrets_map = {
+    for name in local.secret_names :
+    name => jsondecode(data.aws_secretsmanager_secret_version.secrets[name].secret_string)
+  }
+
+  # Optionally extract just user/password maps
+  credentials_map = {
+    for name, creds in local.secrets_map :
+    name => {
+      user     = creds.USER
+      password = creds.PASSWORD
+      ssh_key  = creds.SSH_KEY
+    }
+  }
+}
 
 
 # resource "aws_s3_object" "ftp_lambda_layer" {
@@ -204,33 +205,33 @@ resource "aws_secretsmanager_secret" "secrets" {
 # }
 
 
-# #LAA-ftp-allpay-outbound-ccms
-# module "allpay_ftp_lambda_outbound" {
+#LAA-xerox-outbound-ccms
+# module "LAA-ftp-xerox-cis-pay-outbound" {
 #   source              = "./modules/ftp-lambda"
-#   lambda_name         = lower(format("LAA-ftp-allpay-outbound-ccms-%s",local.environment))
+#   lambda_name         = lower(format("LAA-ftp-xerox-cis-pay-outbound-%s",local.environment))
 #   vpc_id              = data.aws_vpc.shared.id
-#   subnet_ids          = [data.aws_subnet.private_subnets_a.id, data.aws_subnet.private_subnets_b.id,data.aws_subnet.private_subnets_c.id]
-#   ftp_host            = "sftp.allpay.cloud"
+#   subnet_ids          = [data.aws_subnet.public_subnets_a.id, data.aws_subnet.public_subnets_b.id,data.aws_subnet.public_subnets_c.id]
+#   ftp_host            = "cmseft.services.xerox.com"
 #   ftp_port            = "22"
-#   ftp_protocol        = "FTPS"
+#   ftp_protocol        = "SFTP"
 #   ftp_transfer_type   = "SFTP_UPLOAD"
 #   ftp_file_types      = ""
-#   ftp_local_path      = "CCMS_PRD_Allpay/Outbound/"
-#   ftp_remote_path     = "/Inbound/"
+#   ftp_local_path      = "CIS_PAY_DST/Outbound/"
+#   ftp_remote_path     = "/Test/Outbound/PETER/"
 #   ftp_require_ssl     = "NO"
 #   ftp_insecure        = "NO"
 #   ftp_ca_cert         = ""
 #   ftp_cert            = ""
 #   ftp_key             = ""
 #   ftp_key_type        = ""
-#   ftp_user            = local.credentials_map["LAA-ftp-allpay-inbound-ccms"].user
-#   ftp_password_path   = local.credentials_map["LAA-ftp-allpay-inbound-ccms"].password
+#   ftp_user            = local.credentials_map["LAA-ftp-rossendales-ccms-csv-inbound"].user
+#   ftp_password_path   = local.credentials_map["LAA-ftp-rossendales-ccms-csv-inbound"].password
 #   ftp_file_remove     = "YES"
 #   ftp_cron            = "cron(0 10 * * ? *)"
 #   ftp_bucket          = aws_s3_bucket.buckets["laa-ccms-outbound-${local.environment}-mp"].bucket
 #   sns_topic_sev5      = ""
 #   sns_topic_ops       = ""
-#   ssh_key_path        = local.credentials_map["LAA-ftp-allpay-inbound-ccms"].ssh_key
+#   ssh_key_path        = local.credentials_map["LAA-ftp-rossendales-ccms-csv-inbound"].ssh_key
 #   env                 = local.environment
 #   s3_bucket_ftp       = aws_s3_bucket.buckets["laa-ccms-ftp-lambda-${local.environment}-mp"].bucket
 #   s3_object_ftp_client= aws_s3_object.ftp_client.key
