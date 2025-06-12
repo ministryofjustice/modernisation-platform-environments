039476118573bd48313da048a192ba1766fe2e20
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-06-12 16:32:55 +0100

    Create RDS Database for EDRMS

--- terraform/environments/ccms-edrms/secrets.tf
+++ terraform/environments/ccms-edrms/secrets.tf
@@ -2,5 +2,9 @@
 
 resource "aws_secretsmanager_secret" "spring_datasource_password" {
   name        = "ccms/edrms/datasource"
-  description = "SOA TDS database password for CCMS EDRMS application"
+  description = "EDRMS TDS database password for CCMS EDRMS application"
+}
+
+data "aws_secretsmanager_secret_version" "spring_datasource_password" {
+  secret_id = aws_secretsmanager_secret.spring_datasource_password.id
 }
