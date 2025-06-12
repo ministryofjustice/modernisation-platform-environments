5b086fd36da32608fb86df138fa687905f3cdf22
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-06-12 15:24:34 +0100

    test: valid verification method

--- terraform/environments/laa-ccms-soa/cert.tf
+++ terraform/environments/laa-ccms-soa/cert.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "admin" {
   domain_name               = aws_route53_record.admin.fqdn
   certificate_authority_arn = local.application_data.accounts[local.environment].certificate_authority_arn
-  validation_method         = "NONE"
+  validation_method         = "DNS"
   lifecycle {
     create_before_destroy = true
   }
@@ -10,7 +10,7 @@ resource "aws_acm_certificate" "admin" {
 resource "aws_acm_certificate" "managed" {
   domain_name               = aws_route53_record.managed.fqdn
   certificate_authority_arn = local.application_data.accounts[local.environment].certificate_authority_arn
-  validation_method         = "NONE"
+  validation_method         = "DNS"
   lifecycle {
     create_before_destroy = true
   }
