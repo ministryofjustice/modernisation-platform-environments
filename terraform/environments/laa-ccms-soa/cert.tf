35e04743da044468259e84369c1f6b0f52f5cbec
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-06-12 16:05:32 +0100

    test: add secondary dns and csr values

--- terraform/environments/laa-ccms-soa/cert.tf
+++ terraform/environments/laa-ccms-soa/cert.tf
@@ -1,5 +1,6 @@
 resource "aws_acm_certificate" "admin" {
-  domain_name               = aws_route53_record.admin.fqdn
+  domain_name               = aws_route53_record.admin_internal.fqdn
+  subject_alternative_names = [aws_route53_record.admin.fqdn]
   #certificate_authority_arn = local.application_data.accounts[local.environment].certificate_authority_arn
   validation_method         = "DNS"
   lifecycle {
@@ -8,7 +9,8 @@ resource "aws_acm_certificate" "admin" {
 }
 
 resource "aws_acm_certificate" "managed" {
-  domain_name               = aws_route53_record.managed.fqdn
+  domain_name               = aws_route53_record.managed_internal.fqdn
+  subject_alternative_names = [aws_route53_record.managed.fqdn]
   #certificate_authority_arn = local.application_data.accounts[local.environment].certificate_authority_arn
   validation_method         = "DNS"
   lifecycle {
--- terraform/environments/laa-ccms-soa/dns.tf
+++ terraform/environments/laa-ccms-soa/dns.tf
@@ -11,10 +11,34 @@ resource "aws_route53_record" "admin" {
   }
 }
 
+resource "aws_route53_record" "admin_internal" {
+  provider = aws.core-vpc
+  zone_id  = data.aws_route53_zone.inner.zone_id
+  name     = local.application_data.accounts[local.environment].admin_hostname
+  type     = "A"
+  alias {
+    name                   = aws_lb.admin.dns_name
+    zone_id                = aws_lb.admin.zone_id
+    evaluate_target_health = false
+  }
+}
+
 #--Managed
 resource "aws_route53_record" "managed" {
   provider = aws.core-vpc
   zone_id  = data.aws_route53_zone.external.zone_id
+  name     = local.application_data.accounts[local.environment].managed_hostname
+  type     = "A"
+  alias {
+    name                   = aws_lb.managed.dns_name
+    zone_id                = aws_lb.managed.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "managed_internal" {
+  provider = aws.core-vpc
+  zone_id  = data.aws_route53_zone.inner.zone_id
   name     = local.application_data.accounts[local.environment].managed_hostname
   type     = "A"
   alias {
