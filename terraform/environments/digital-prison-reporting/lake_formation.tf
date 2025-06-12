ccb23da3772128b808c8310826c7571abc5512e1
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-12 16:07:06 +0100

    dmet-prisons: add developers to LF admins

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -2,7 +2,12 @@ resource "aws_lakeformation_data_lake_settings" "lake_formation" {
   admins = flatten([
     [for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn],
     data.aws_iam_session_context.current.issuer_arn,
+
+    # Make Data engineer/developer role a LF admin
     try(one(data.aws_iam_roles.data_engineering_roles.arns), []),
+    try(one(data.aws_iam_roles.aws_iam_roles.developer_roles.arns), []),
+
+    # Make the cross-account runner used by create-a-derived table LF admin
     aws_iam_role.dataapi_cross_role.arn
     ]
   )
@@ -20,6 +25,8 @@ resource "aws_lakeformation_data_lake_settings" "lake_formation" {
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
 }
+
+
 
 # Create the 'domain' tag with values
 resource "aws_lakeformation_lf_tag" "domain_tag" {
@@ -52,6 +59,16 @@ resource "aws_lakeformation_permissions" "sensitive_grant" {
   lf_tag {
     key    = aws_lakeformation_lf_tag.sensitive_tag.key
     values = aws_lakeformation_lf_tag.sensitive_tag.values
+  }
+}
+
+resource "aws_lakeformation_permissions" "developer_roles_get_data_access" {
+  for_each    = toset(data.aws_iam_roles.developer_roles.arns)
+  principal   = each.value
+  permissions = ["DATA_LOCATION_ACCESS"]
+
+  data_location {
+    arn = "arn:aws:s3:::${local.project}-*" // Adjust this ARN as needed for your data locations
   }
 }
 
