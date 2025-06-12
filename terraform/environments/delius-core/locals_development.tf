61c72f098cc33cfe02b7e6915c39f78847520dcb
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-06-12 15:14:18 +0100

    Disable schedule change

--- terraform/environments/delius-core/locals_development.tf
+++ terraform/environments/delius-core/locals_development.tf
@@ -146,8 +146,8 @@ locals {
     is-production = false
     # Times must be specified in UTC
     disable_latency_alarms = {
-      start_time      = "11:00"
-      end_time        = "11:30"
+      start_time      = "14:30"
+      end_time        = "15:00"
       disable_weekend = true
     }
   }
