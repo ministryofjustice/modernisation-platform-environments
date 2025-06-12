8f773dfcf5e9e05490f16060ae3aa599990e569f
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-12 12:04:11 +0100

    dmet-prisons-352: add permissions to associate, describe and grant

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -30,3 +30,25 @@ resource "aws_lakeformation_lf_tag" "sensitive_tag" {
   key    = "sensitive"
   values = ["true", "false", "data_linking"]
 }
+
+resource "aws_lakeformation_permissions" "domain_grant" {
+  principal   = aws_iam_role.dataapi_cross_role.arn
+  permissions = ["DESCRIBE", "ASSOCIATE", "GRANT_WITH_LF_TAG_EXPRESSION"]
+
+  lf_tag {
+    key    = aws_lakeformation_lf_tag.domain_tag.key
+    values = aws_lakeformation_lf_tag.domain_tag.values
+  }
+}
+
+resource "aws_lakeformation_permissions" "sensitive_grant" {
+  principal   = aws_iam_role.dataapi_cross_role.arn
+  permissions = ["DESCRIBE", "ASSOCIATE", "GRANT_WITH_LF_TAG_EXPRESSION"]
+
+  lf_tag {
+    key    = aws_lakeformation_lf_tag.sensitive_tag.key
+    values = aws_lakeformation_lf_tag.sensitive_tag.values
+  }
+}
+
+
