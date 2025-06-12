de64440ad98a2fea8673452d02faebbf96566532
Ratna-Kurapati-MoJ <Ratna.Kurapati@justice.gov.uk>
2025-06-12 17:22:25 +0100

    S3 bucket policy and permissions (#11065)
    * S3 bucket policy and permissions

--- terraform/environments/coat/locals.tf
+++ terraform/environments/coat/locals.tf
@@ -8,7 +8,4 @@ locals {
   coat_prod_account_id = "279191903737" #local.environment_management.account_ids["coat-production"] 
   coat_dev_account_id  = "082282578003" #local.environment_management.account_ids["coat-development"]
 
-  cross_environment    = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" ? local.dev_environment : local.prod_environment
-  cross_env_account_id = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" ? local.coat_dev_account_id : local.coat_prod_account_id
-
 }
