87aa355bb61854532e48cf4e9c7af4f7d990300c
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-06-13 10:51:51 +0100

    mp dev role added to policy

--- terraform/environments/coat/data.tf
+++ terraform/environments/coat/data.tf
@@ -1,5 +1,6 @@
 #### This file can be used to store data specific to the member account ####
 
 data "aws_iam_role" "moj_mp_dev_role" {
-  name = local.mp_dev_role
+  count = local.is-production ? 1 : 0
+  name  = local.mp_dev_role
 }
