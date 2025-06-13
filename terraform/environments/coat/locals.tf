7588891de1d2120920300b4fe9accd9525c7661f
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-06-13 10:54:22 +0100

    mp dev role added to policy

--- terraform/environments/coat/locals.tf
+++ terraform/environments/coat/locals.tf
@@ -3,10 +3,12 @@ locals {
 
   env_account_id = local.environment_management.account_ids[terraform.workspace]
 
-  prod_environment     = "production"
-  dev_environment      = "development"
+  prod_environment = "production"
+  dev_environment  = "development"
+
   coat_prod_account_id = "279191903737" #local.environment_management.account_ids["coat-production"] 
   coat_dev_account_id  = "082282578003" #local.environment_management.account_ids["coat-development"]
-  mp_dev_role          = "AWSReservedSSO_modernisation-platform-developer_cd1b8f85b1611d20"
+
+  mp_dev_role = "AWSReservedSSO_modernisation-platform-developer_cd1b8f85b1611d20"
 
 }
