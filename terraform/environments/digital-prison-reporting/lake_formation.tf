8f96cab055a2211f8d184f6a4050c6e17eea5e24
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-13 11:00:16 +0100

    dmet-prisons-393: more changes to logic in flatten

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -7,7 +7,7 @@ resource "aws_lakeformation_data_lake_settings" "lake_formation" {
     try(one(data.aws_iam_roles.data_engineering_roles.arns), []),
 
     # Make Developer role a LF admin
-    length(data.aws_iam_roles.developer_roles.arns) > 0 ? one(data.aws_iam_roles.developer_roles.arns) : null,
+    length(data.aws_iam_roles.developer_roles.arns) > 0 ? [one(data.aws_iam_roles.developer_roles.arns)] : [],
 
 
     # Make the cross-account runner used by create-a-derived table LF admin
