b50b8896856124ea76a80cc94f38af15aa48888c
Ratna Kurapati <ratna.kurapati@justice.gov.uk>
2025-06-13 10:32:40 +0100

    mp dev role added to policy

--- terraform/environments/coat/data.tf
+++ terraform/environments/coat/data.tf
@@ -1,1 +1,5 @@
 #### This file can be used to store data specific to the member account ####
+
+data "aws_iam_role" "moj_mp_dev_role" {
+  name = local.mp_dev_role
+}
--- terraform/environments/coat/locals.tf
+++ terraform/environments/coat/locals.tf
@@ -7,5 +7,6 @@ locals {
   dev_environment      = "development"
   coat_prod_account_id = "279191903737" #local.environment_management.account_ids["coat-production"] 
   coat_dev_account_id  = "082282578003" #local.environment_management.account_ids["coat-development"]
+  mp_dev_role          = "AWSReservedSSO_modernisation-platform-developer_cd1b8f85b1611d20"
 
 }
