fbde3a5eb2831ebeca521c3c26aa3dccb2c96219
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-06-16 11:10:31 +0100

    Add new SGs for EC2s

--- terraform/environments/hmpps-domain-services/locals_security_groups.tf
+++ terraform/environments/hmpps-domain-services/locals_security_groups.tf
@@ -17,7 +17,6 @@ locals {
     ])
     rd_session_hosts = flatten([
       module.ip_addresses.mp_cidr[module.environment.vpc_name],
-      module.ip_addresses.azure_fixngo_cidrs.devtest,
     ])
   }
   security_group_cidrs_preprod_prod = {
@@ -37,7 +36,6 @@ locals {
     ])
     rd_session_hosts = flatten([
       module.ip_addresses.mp_cidr[module.environment.vpc_name],
-      module.ip_addresses.azure_fixngo_cidrs.prod,
     ])
   }
   security_group_cidrs_by_environment = {
@@ -49,6 +47,133 @@ locals {
   security_group_cidrs = local.security_group_cidrs_by_environment[local.environment]
 
   security_groups = {
+
+    jumpserver = {
+      description = "Security group for jumpservers"
+      ingress = {
+        all-from-self = {
+          description = "Allow all ingress to self"
+          from_port   = 0
+          to_port     = 0
+          protocol    = -1
+          self        = true
+        }
+        rpc-from-rds = {
+          description     = "Allow RPC from remote desktop connection broker"
+          from_port       = 135
+          to_port         = 135
+          protocol        = "TCP"
+          security_groups = ["rds"]
+        }
+        smb-from-rds = {
+          description     = "Allow SMB from remote desktop connection broker"
+          from_port       = 445
+          to_port         = 445
+          protocol        = "TCP"
+          security_groups = ["rds"]
+        }
+        winrm-from-rds = {
+          description     = "Allow WinRM from remote desktop connection broker"
+          from_port       = 5985
+          to_port         = 5986
+          protocol        = "TCP"
+          security_groups = ["rds"]
+        }
+        rpc-dynamic-from-rds = {
+          description     = "Allow RPC dynamic ports from remote desktop connection broker"
+          from_port       = 49152
+          to_port         = 65535
+          protocol        = "TCP"
+          security_groups = ["rds"]
+        }
+      }
+      egress = {
+        all = {
+          description     = "Allow all egress"
+          from_port       = 0
+          to_port         = 0
+          protocol        = "-1"
+          cidr_blocks     = ["0.0.0.0/0"]
+          security_groups = []
+        }
+      }
+    }
+    rdgw = {
+      description = "Security group for Remote Desktop Gateways"
+      ingress = {
+        all-from-self = {
+          description = "Allow all ingress to self"
+          from_port   = 0
+          to_port     = 0
+          protocol    = -1
+          self        = true
+        }
+        http-from-lb = {
+          description = "Allow http ingress"
+          from_port   = 80
+          to_port     = 80
+          protocol    = "TCP"
+          security_groups = [
+            "public-lb", "public-lb-2"
+          ]
+        }
+        https-from-lb = {
+          description = "Allow https ingress"
+          from_port   = 443
+          to_port     = 443
+          protocol    = "TCP"
+          security_groups = [
+            "public-lb", "public-lb-2"
+          ]
+        }
+      }
+    }
+
+    rds = {
+      description = "Security group for Remote Desktop Services (ConnectionBroker and RDWeb)"
+      ingress = {
+        all-from-self = {
+          description = "Allow all ingress to self"
+          from_port   = 0
+          to_port     = 0
+          protocol    = -1
+          self        = true
+        }
+        http-from-lb = {
+          description = "Allow http ingress"
+          from_port   = 80
+          to_port     = 80
+          protocol    = "TCP"
+          security_groups = [
+            "public-lb", "public-lb-2"
+          ]
+        }
+        rpc-from-rdsessionhosts = {
+          description = "Allow RPC from remote desktop session hosts"
+          from_port   = 135
+          to_port     = 135
+          protocol    = "TCP"
+          cidr_blocks = local.security_group_cidrs.rd_session_hosts
+        }
+        https-from-lb = {
+          description = "Allow https ingress"
+          from_port   = 443
+          to_port     = 443
+          protocol    = "TCP"
+          security_groups = [
+            "public-lb", "public-lb-2"
+          ]
+        }
+        rpc-dynamic-from-rdsessionhost = {
+          description = "Allow RPC dynamic ports from remote desktop session hosts"
+          from_port   = 49152
+          to_port     = 65535
+          protocol    = "TCP"
+          cidr_blocks = local.security_group_cidrs.rd_session_hosts
+        }
+      }
+    }
+
     rds-ec2s = {
       description = "Security group for Remote Desktop Service EC2s"
       ingress = {
