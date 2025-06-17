da4a1d07b08969827cd66d0fc6b2d470c44ad6c6
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-06-17 14:52:42 +0100

    add baseline SGs

--- terraform/environments/nomis/locals.tf
+++ terraform/environments/nomis/locals.tf
@@ -28,6 +28,7 @@ locals {
       enable_business_unit_kms_cmks               = true
       enable_ec2_cloud_watch_agent                = true
       enable_ec2_oracle_enterprise_managed_server = true
+      enable_ec2_security_groups                  = true
       enable_ec2_self_provision                   = true
       enable_ec2_session_manager_cloudwatch_logs  = true
       enable_ec2_ssm_agent_update                 = true
