d6cde37b81d10ae1324567e1f50d04a6a2d5b537
Ratna-Kurapati-MoJ <Ratna.Kurapati@justice.gov.uk>
2025-06-17 10:38:18 +0100

    Cros account (#11098)
    * kms key with cross role
    
    * dev s3 bucket added in prod role

--- terraform/environments/coat/s3.tf
+++ terraform/environments/coat/s3.tf
@@ -25,7 +25,7 @@ module "cur_s3_kms" {
       effect    = "Allow"
       principals = [
         {
-          type        = "AWS"
+          type = "AWS"
           identifiers = [
             "arn:aws:iam::${local.environment_management.aws_organizations_root_account_id}:role/moj-cur-reports-v2-hourly-replication-role",
             "arn:aws:iam::${local.coat_prod_account_id}:role/moj-coat-${local.prod_environment}-cur-reports-cross-role"
