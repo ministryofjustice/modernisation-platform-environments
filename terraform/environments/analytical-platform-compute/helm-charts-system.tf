ba4c4f98d8b58f05ef073787e2028f90f401d4b3
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-06-17 16:39:12 +0000

    Version upgrades

--- terraform/environments/analytical-platform-compute/helm-charts-system.tf
+++ terraform/environments/analytical-platform-compute/helm-charts-system.tf
@@ -209,7 +209,7 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "v1.17.2"
+  version    = "v1.18.0"
   namespace  = kubernetes_namespace.cert_manager.metadata[0].name
   values = [
     templatefile(
