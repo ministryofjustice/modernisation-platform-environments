df94f8372007a66243810c74259e04fee7eba2ea
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-06-18 16:21:31 +0100

    add lb dash

--- terraform/environments/oasys-national-reporting/locals.tf
+++ terraform/environments/oasys-national-reporting/locals.tf
@@ -21,7 +21,7 @@ locals {
   baseline_presets_all_environments = {
     options = {
       cloudwatch_dashboard_default_widget_groups = [
-        # "lb",
+        "lb",
         "ec2",
         "ec2_linux",
         "ec2_instance_linux",
