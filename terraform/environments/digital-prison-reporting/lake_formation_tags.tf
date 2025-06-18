9329f6fc3991c759397a594d023d7c91c753e9a1
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-18 18:29:12 +0100

    dmet-prisons-391: add database level permissions for single user

--- terraform/environments/digital-prison-reporting/lake_formation_tags.tf
+++ terraform/environments/digital-prison-reporting/lake_formation_tags.tf
@@ -17,6 +17,18 @@ resource "aws_lakeformation_permissions" "lf_data_location_alpha" {
   }
 }
 
+resource "aws_lakeformation_permissions" "grant_database_access_by_tag" {
+  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
+  permissions = ["DESCRIBE"]
+
+  lf_tag_policy {
+    resource_type = "DATABASE"
+    expression {
+      key    = aws_lakeformation_lf_tag.domain_tag.key
+      values = ["prisons"]
+    }
+  }
+}
 resource "aws_lakeformation_permissions" "grant_table_access_by_tag" {
   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
   permissions = ["DESCRIBE"] 
