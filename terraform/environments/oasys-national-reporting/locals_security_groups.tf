fc5d7541b48f186244002b75c18f834d1c34cb9c
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-06-18 16:56:47 +0100

    SG tweaks

--- terraform/environments/oasys-national-reporting/locals_security_groups.tf
+++ terraform/environments/oasys-national-reporting/locals_security_groups.tf
@@ -14,14 +14,13 @@ locals {
       module.ip_addresses.azure_fixngo_cidrs.internet_egress,
       module.ip_addresses.mp_cidrs.non_live_eu_west_nat,
     ])
-    noms_core = module.ip_addresses.azure_fixngo_cidrs.devtest_core
     oasys_db = flatten([
       module.ip_addresses.mp_cidr[module.environment.vpc_name],
       module.ip_addresses.azure_fixngo_cidrs.devtest_oasys_db,
     ])
-    rdp = flatten([
+    cms_ingress = flatten([
+      module.ip_addresses.azure_fixngo_cidrs.devtest,
       module.ip_addresses.mp_cidr[module.environment.vpc_name],
-      module.ip_addresses.azure_fixngo_cidrs.devtest
     ])
   }
 
@@ -40,14 +39,13 @@ locals {
       module.ip_addresses.azure_fixngo_cidrs.internet_egress,
       module.ip_addresses.mp_cidrs.live_eu_west_nat,
     ])
-    noms_core = module.ip_addresses.azure_fixngo_cidrs.prod_core
     oasys_db = flatten([
       module.ip_addresses.mp_cidr[module.environment.vpc_name],
       module.ip_addresses.azure_fixngo_cidrs.prod_oasys_db,
     ])
-    rdp = flatten([
+    cms_ingress = flatten([
+      module.ip_addresses.azure_fixngo_cidrs.prod,
       module.ip_addresses.mp_cidr[module.environment.vpc_name],
-      module.ip_addresses.azure_fixngo_cidrs.prod
     ])
   }
 
@@ -142,13 +140,6 @@ locals {
     bip-web = {
       description = "Security group for bip web tier"
       ingress = {
-        all-within-subnet = {
-          description = "Allow all ingress to self"
-          from_port   = 0
-          to_port     = 0
-          protocol    = -1
-          self        = true
-        }
         http7010 = {
           description     = "Allow http7010 ingress"
           from_port       = 7010
@@ -182,27 +173,10 @@ locals {
           security_groups = ["public-lb", "public-lb-2"]
         }
       }
-      egress = {
-        all = {
-          description     = "Allow all egress"
-          from_port       = 0
-          to_port         = 0
-          protocol        = "-1"
-          cidr_blocks     = ["0.0.0.0/0"]
-          security_groups = []
-        }
-      }
     }
     bip-app = {
       description = "Security group for bip application tier"
       ingress = {
-        all-within-subnet = {
-          description = "Allow all ingress to self"
-          from_port   = 0
-          to_port     = 0
-          protocol    = -1
-          self        = true
-        }
         all-from-web = {
           description     = "Allow all ingress from web"
           from_port       = 0
@@ -215,17 +189,7 @@ locals {
           from_port   = 6400
           to_port     = 6500
           protocol    = "tcp"
-          cidr_blocks = ["10.0.0.0/8"] # added for testing, remove later
-        }
-      }
-      egress = {
-        all = {
-          description     = "Allow all egress"
-          from_port       = 0
-          to_port         = 0
-          protocol        = "-1"
-          cidr_blocks     = ["0.0.0.0/0"]
-          security_groups = []
+          cidr_blocks = local.security_group_cidrs.cms_ingress
         }
       }
     }
@@ -237,78 +201,17 @@ locals {
     bods = {
       description = "Security group for BODS servers"
       ingress = {
-        all-from-self = {
-          description = "Allow all ingress to self"
-          from_port   = 0
-          to_port     = 0
-          protocol    = -1
-          self        = true
-        }
-        rdp_3389_tcp = {
-          description = "3389: rdp tcp"
-          from_port   = 3389
-          to_port     = 3389
-          protocol    = "TCP"
-          cidr_blocks = local.security_group_cidrs.rdp
-        }
-        http_6400 = {
-          description = "6400: boe cms"
-          from_port   = 6400
-          to_port     = 6400
-          protocol    = "TCP"
-        }
-        http_6410_6500 = {
-          description = "6410-6500: boe sia"
-          from_port   = 6410
-          to_port     = 6500
-          protocol    = "TCP"
-        }
         http_28080 = {
           description     = "28080: bods tomcat http"
           from_port       = 28080
           to_port         = 28080
           protocol        = "TCP"
-          security_groups = ["public-lb"] # TODO check?
-        }
-      }
-      egress = {
-        all = {
-          description = "Allow all traffic outbound"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
+          security_groups = ["public-lb", "public-lb-2"]
         }
       }
     }
     onr_db = {
       description = "Security group for ONR DB server"
-      ingress = {
-        all-from-self = {
-          description     = "Allow all ingress to self"
-          from_port       = 0
-          to_port         = 0
-          protocol        = -1
-          self            = true
-          security_groups = ["boe", "bods"]
-        }
-        onr_db_oem_agent = {
-          description = "3872: Oracle OEM agent"
-          from_port   = 3872
-          to_port     = 3872
-          protocol    = "TCP"
-          cidr_blocks = local.security_group_cidrs.noms_core
-        }
-      }
-      egress = {
-        all = {
-          description = "Allow all traffic outbound"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      }
     }
     oasys_db = {
       description = "Allow traffic in from Oasys db servers"
@@ -368,38 +271,6 @@ locals {
     }
     oasys_db_onr_db = {
       description = "Allow traffic from Oasys db servers to ONR DB server"
-      ingress = {
-        all-from-self = {
-          description = "Allow all ingress to self"
-          from_port   = 0
-          to_port     = 0
-          protocol    = -1
-          self        = true
-        }
-        oasys_db_onr_db_1521 = {
-          description = "1521: TCP Oracle DB access ingress from Oasys db servers to ONR DB server"
-          from_port   = 1521
-          to_port     = 1521
-          protocol    = "TCP"
-          cidr_blocks = local.security_group_cidrs.oasys_db
-        }
-        oasys_db_onr_db_7443 = {
-          description = "7443: TCP Oracle DB access ingress from Oasys db servers to ONR DB server"
-          from_port   = 7443
-          to_port     = 7443
-          protocol    = "TCP"
-          cidr_blocks = local.security_group_cidrs.oasys_db
-        }
-      }
-      egress = {
-        all = {
-          description = "Allow all traffic outbound"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      }
     }
     private-jumpserver = {
       description = "Security group for jumpservers"
@@ -407,13 +278,6 @@ locals {
     win-bip = {
       description = "Security group for Temporary Windows BIP server"
       ingress = {
-        all-from-self = {
-          description = "Allow all ingress to self"
-          from_port   = 0
-          to_port     = 0
-          protocol    = -1
-          self        = true
-        }
         oasys_db_onr_db_1521 = {
           description = "1521: TCP Oracle DB access ingress from Oasys db servers to ONR DB server"
           from_port   = 1521
@@ -427,23 +291,6 @@ locals {
           to_port     = 7443
           protocol    = "TCP"
           cidr_blocks = local.security_group_cidrs.oasys_db
-        }
-        rdp_3389_tcp = {
-          description = "3389: rdp tcp"
-          from_port   = 3389
-          to_port     = 3389
-          protocol    = "TCP"
-          cidr_blocks = local.security_group_cidrs.rdp
-        }
-      }
-      egress = {
-        all = {
-          description     = "Allow all egress"
-          from_port       = 0
-          to_port         = 0
-          protocol        = "-1"
-          cidr_blocks     = ["0.0.0.0/0"]
-          security_groups = []
         }
       }
     }
