d90adf072858b045494f97495ec53684829318fe
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-18 15:41:56 +0100

    dmet-prisons-remove policy

--- terraform/environments/digital-prison-reporting/lake_formation_tags.tf
+++ terraform/environments/digital-prison-reporting/lake_formation_tags.tf
@@ -1,25 +1,3 @@
-resource "aws_glue_resource_policy" "lakeformation_cross_account_sharing" {
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "LakeFormationCrossAccountSharing",
-        Effect = "Allow",
-        Principal = {
-          Service = "ram.amazonaws.com"
-        },
-        Action = "glue:ShareResource",
-        Resource = [
-          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog",
-          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/dpr_ap_integration_test_tag_dev_dbt",
-          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/dpr_ap_integration_test_tag_dev_dbt/*"
-        ]
-      }
-    ]
-  })
-  enable_hybrid = "TRUE"
-}
-
 resource "aws_lakeformation_permissions" "grant_tag_to_consumer" {
   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
   permissions = ["DESCRIBE", "ASSOCIATE"]
