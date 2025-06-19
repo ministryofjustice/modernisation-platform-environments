e7c60cd7ec036554a636db1afec0da7c43585ad9
Andy Welsh <200072691+andywelshmoj@users.noreply.github.com>
2025-06-19 10:45:31 +0100

    fix: bad SAN on cert

--- terraform/environments/laa-ccms-soa/cert.tf
+++ terraform/environments/laa-ccms-soa/cert.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "soa" {
   domain_name               = trim(data.aws_route53_zone.external.name, ".") #--Remove the trailing dot from the zone name
-  subject_alternative_names = [aws_route53_record.admin.fqdn, aws_route53_record.admin.fqdn]
+  subject_alternative_names = [aws_route53_record.admin.fqdn, aws_route53_record.managed.fqdn]
   validation_method         = "DNS"
   lifecycle {
     create_before_destroy = true
