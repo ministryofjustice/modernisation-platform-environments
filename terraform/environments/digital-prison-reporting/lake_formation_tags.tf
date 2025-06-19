3ebb973f787c0e6b58cd08cce57e4d710b6921b4
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-19 14:51:23 +0100

    Revert "dmet-prisons-391-remove"
    This reverts commit f6e5f30ed34ed1dd97795108ca96cd6f273c94b4.

---
+++ terraform/environments/digital-prison-reporting/lake_formation_tags.tf
@@ -0,0 +1,55 @@
+# Need to give database the lf_tag - cannot be done by DBT
+resource "aws_lakeformation_resource_lf_tag" "tag_database_with_domain" {
+  database {
+    name = "dpr_ap_integration_test_tag_dev_dbt"  
+  }
+
+  lf_tag {
+    key   = aws_lakeformation_lf_tag.domain_tag.key
+    value = "prisons"
+  }
+}
+
+resource "aws_lakeformation_permissions" "grant_tag_to_consumer" {
+  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
+  permissions = ["DESCRIBE", "ASSOCIATE"]
+
+  lf_tag {
+    key    = aws_lakeformation_lf_tag.domain_tag.key
+    values = ["prisons"]
+  }
+}
+
+resource "aws_lakeformation_permissions" "lf_data_location_alpha" {
+  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
+  permissions = ["DATA_LOCATION_ACCESS"]
+
+  data_location {
+    arn = "arn:aws:s3:::dpr-structured-historical-development"
+  }
+}
+
+resource "aws_lakeformation_permissions" "grant_database_access_by_tag" {
+  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
+  permissions = ["DESCRIBE"]
+
+  lf_tag_policy {
+    resource_type = "DATABASE"
+    expression {
+      key    = aws_lakeformation_lf_tag.domain_tag.key
+      values = ["prisons"]
+    }
+  }
+}
+resource "aws_lakeformation_permissions" "grant_table_access_by_tag" {
+  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
+  permissions = ["DESCRIBE"] 
+
+  lf_tag_policy {
+    resource_type = "TABLE"
+    expression {
+      key    = aws_lakeformation_lf_tag.domain_tag.key
+      values = ["prisons"]
+    }
+  }
+}
