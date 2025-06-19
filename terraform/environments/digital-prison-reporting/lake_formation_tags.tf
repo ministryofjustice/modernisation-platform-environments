8675804ed7ea7d89e107fb23737c347cc166e341
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-19 16:42:09 +0100

    dmet-prisons-391 - update comments

--- terraform/environments/digital-prison-reporting/lake_formation_tags.tf
+++ terraform/environments/digital-prison-reporting/lake_formation_tags.tf
@@ -1,7 +1,7 @@
 # Need to give database the lf_tag - cannot be done by DBT
 resource "aws_lakeformation_resource_lf_tag" "tag_database_with_domain" {
   database {
-    name = "dpr_ap_integration_test_tag_dev_dbt"  
+    name = "dpr_ap_integration_test_tag_dev_dbt"
   }
 
   lf_tag {
@@ -9,7 +9,7 @@ resource "aws_lakeformation_resource_lf_tag" "tag_database_with_domain" {
     value = "prisons"
   }
 }
-
+# Alpha user describe/associate on domain = prisons (can add more later)
 resource "aws_lakeformation_permissions" "grant_tag_to_consumer" {
   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
   permissions = ["DESCRIBE", "ASSOCIATE"]
@@ -20,6 +20,7 @@ resource "aws_lakeformation_permissions" "grant_tag_to_consumer" {
   }
 }
 
+# Alpha user data location
 resource "aws_lakeformation_permissions" "lf_data_location_alpha" {
   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
   permissions = ["DATA_LOCATION_ACCESS"]
@@ -29,6 +30,7 @@ resource "aws_lakeformation_permissions" "lf_data_location_alpha" {
   }
 }
 
+# Grant external account database access by tag (all values)
 resource "aws_lakeformation_permissions" "grant_database_access_by_tag" {
   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
   permissions = ["DESCRIBE"]
@@ -37,19 +39,21 @@ resource "aws_lakeformation_permissions" "grant_database_access_by_tag" {
     resource_type = "DATABASE"
     expression {
       key    = aws_lakeformation_lf_tag.domain_tag.key
-      values = ["prisons"]
+      values = ["prisons", "probation", "electronic-monitoring"]
     }
   }
 }
+
+# Grant external account table access by tag (all values)
 resource "aws_lakeformation_permissions" "grant_table_access_by_tag" {
   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
-  permissions = ["DESCRIBE"] 
+  permissions = ["DESCRIBE"]
 
   lf_tag_policy {
     resource_type = "TABLE"
     expression {
       key    = aws_lakeformation_lf_tag.domain_tag.key
-      values = ["prisons"]
+      values = ["prisons", "probation", "electronic-monitoring"]
     }
   }
 }
