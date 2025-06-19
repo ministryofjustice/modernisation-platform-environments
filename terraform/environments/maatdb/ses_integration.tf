aacba6cf9581618b77a4d87a56c5b685c607e56b
mikereiddigital <mike.reid@justice.gov.uk>
2025-06-19 09:58:06 +0100

    Add block public access acls.

--- terraform/environments/maatdb/ses_integration.tf
+++ terraform/environments/maatdb/ses_integration.tf
@@ -153,6 +153,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "ses_incoming_emai
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket = aws_s3_bucket.ses_incoming_email.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
 resource "aws_s3_bucket_policy" "allow_ses" {
   bucket = aws_s3_bucket.ses_incoming_email.id
 
