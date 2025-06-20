572989fe9ccab0e3e51916962cf3332404a51a83
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2025-06-20 18:18:49 +0100

    moved arn to locals from variable

--- terraform/modules/shield_advanced/shield.tf
+++ terraform/modules/shield_advanced/shield.tf
@@ -28,6 +28,12 @@ locals {
   }
 }
 
+locals {
+  environment_management_accounts = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
+  core_logging_account_id         = local.environment_management_accounts["core-logging-production"]
+  firehose_stream_arn             = "arn:aws:firehose:${var.region}:${local.core_logging_account_id}:deliverystream/waf-logs-to-s3"
+}
+
 resource "aws_shield_drt_access_role_arn_association" "main" {
   role_arn = data.aws_iam_role.srt_access.arn
 }
@@ -105,7 +111,7 @@ resource "aws_wafv2_web_acl_logging_configuration" "waf" {
 
 # IAM role for CloudWatch Logs to Firehose
 resource "aws_iam_role" "cwl_to_firehose" {
-  count = var.enable_logging && var.firehose_stream_arn != null ? 1 : 0
+  count = var.enable_logging && local.firehose_stream_arn != null ? 1 : 0
   name  = "CWLtoKinesisFirehoseRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -120,24 +126,24 @@ resource "aws_iam_role" "cwl_to_firehose" {
 }
 
 resource "aws_iam_role_policy" "cwl_to_firehose_policy" {
-  count = var.enable_logging && var.firehose_stream_arn != null ? 1 : 0
+  count = var.enable_logging && local.firehose_stream_arn != null ? 1 : 0
   role  = aws_iam_role.cwl_to_firehose[0].id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
       Action = "firehose:PutRecord"
-      Resource = var.firehose_stream_arn
+      Resource = local.firehose_stream_arn
     }]
   })
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "waf_to_firehose" {
-  count           = var.enable_logging && var.firehose_stream_arn != null ? 1 : 0
+  count           = var.enable_logging && local.firehose_stream_arn != null ? 1 : 0
   name            = "waf-to-firehose"
   log_group_name  = aws_cloudwatch_log_group.waf[0].name
   filter_pattern  = "{$.action = * }"
-  destination_arn = var.firehose_stream_arn
+  destination_arn = local.firehose_stream_arn
   role_arn        = aws_iam_role.cwl_to_firehose[0].arn
   depends_on      = [aws_iam_role_policy.cwl_to_firehose_policy]
 }
