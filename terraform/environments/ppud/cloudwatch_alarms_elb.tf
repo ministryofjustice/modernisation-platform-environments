80358aabdac1a4cf39dd0952b4c94f9361db91b2
Buckingham <XC01857@lumen.com>
2025-06-23 09:58:11 +0100

    Update_230625_1

--- terraform/environments/ppud/cloudwatch_alarms_elb.tf
+++ terraform/environments/ppud/cloudwatch_alarms_elb.tf
@@ -14,11 +14,11 @@ resource "aws_cloudwatch_metric_alarm" "high_target_response_time_wam_elb" {
   datapoints_to_alarm = "1"
   metric_name         = "TargetResponseTime"
   namespace           = "AWS/ApplicationELB"
-  period              = "60"
+  period              = "3600"
   statistic           = "Average"
-  threshold           = "0.01"
+  threshold           = "1"
   treat_missing_data  = "notBreaching"
-  alarm_description   = "This metric monitors the target response time of the WAM load balancer. If the target response time averages over 0.01 for 1 minute, the alarm will trigger."
+  alarm_description   = "This metric monitors the target response time of the WAM load balancer. If the target response time averages over 1 second for 60 minutes, the alarm will trigger."
   alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
   dimensions = {
     LoadBalancer      = "app/WAM-ALB-PROD/bfc963544454bdde"
