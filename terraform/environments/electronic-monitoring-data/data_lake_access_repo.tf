adb6babb62f9fe33b97ddd04a708e14c7d7e8cbd
matt heery <matt.heery@justice.gov.uk>
2025-06-24 15:34:18 +0100

    fix: rename resource

--- terraform/environments/electronic-monitoring-data/data_lake_access_repo.tf
+++ terraform/environments/electronic-monitoring-data/data_lake_access_repo.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "cloud_formation_access" {
     }
 }
 
-resource "aws_iam_role_policy" "analytical_platform_share_policy_attachment" {
+resource "aws_iam_role_policy" "analytical_platform_cloud_formation_policy_attachment" {
   for_each = local.analytical_platform_share
 
   name   = "${each.value.target_account_name}-cloud-formation-policy"
