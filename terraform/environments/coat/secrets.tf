9872df9af5d694ee05b73b66bd854b33cb68ea39
Stefan Hristov <stefan.hristov@justice.gov.uk>
2025-06-25 13:55:17 +0100

    Adding dates for clarity

--- terraform/environments/coat/secrets.tf
+++ terraform/environments/coat/secrets.tf
@@ -1,7 +1,7 @@
 #### This file can be used to store secrets specific to the member account ####
 resource "aws_ssm_parameter" "gbp_exchange_rate_up_to_april" {
   # checkov:skip=CKV_AWS_337: Standard KMS is fine
-  name  = "/currency_rates/up_to_april/gbp"
+  name  = "/currency_rates/up_to_april_30th/gbp"
   type  = "SecureString"
   value = "[]" # or use a dummy like '[]'
 
@@ -12,7 +12,7 @@ resource "aws_ssm_parameter" "gbp_exchange_rate_up_to_april" {
 
 resource "aws_ssm_parameter" "gbp_exchange_rate_april_to_may" {
   # checkov:skip=CKV_AWS_337: Standard KMS is fine
-  name  = "/currency_rates/april_to_may/gbp"
+  name  = "/currency_rates/30th_april_to_may_1st/gbp"
   type  = "SecureString"
   value = "[]" # or use a dummy like '[]'
 
@@ -23,7 +23,7 @@ resource "aws_ssm_parameter" "gbp_exchange_rate_april_to_may" {
 
 resource "aws_ssm_parameter" "gbp_exchange_rate_may" {
   # checkov:skip=CKV_AWS_337: Standard KMS is fine
-  name  = "/currency_rates/may/gbp"
+  name  = "/currency_rates/since_may_1st/gbp"
   type  = "SecureString"
   value = "[]" # or use a dummy like '[]'
 
