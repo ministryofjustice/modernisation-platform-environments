ec70cc838c4ab67c4799359267bbc08c7973c4aa
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-06-25 17:27:07 +0100

    change to arn

--- terraform/environments/delius-mis/modules/mis_environment/iam.tf
+++ terraform/environments/delius-mis/modules/mis_environment/iam.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "secrets_manager" {
     resources = [
       aws_secretsmanager_secret.ad_admin_password.arn,
       "arn:aws:secretsmanager:*:*:secret:NDMIS_DFI_SERVICEACCOUNTS_DEV-*",
-      "delius-mis-dev-oracle-dsd-db-application-passwords"
+      "arn:aws:secretsmanager:*:*:secret:delius-mis-dev-oracle-mis-db-application-passwords-*"
     ]
   }
 }
