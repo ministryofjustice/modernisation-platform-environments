5f9a5036be31dcbe0876035daf7d3c0c02d86a4c
Stefan Hristov <stefan.hristov@justice.gov.uk>
2025-06-25 13:32:07 +0100

    Adding 3 params to be populated via console but managed via Terraform

--- terraform/environments/coat/secrets.tf
+++ terraform/environments/coat/secrets.tf
@@ -1,1 +1,33 @@
 #### This file can be used to store secrets specific to the member account ####
+resource "aws_ssm_parameter" "gbp_exchange_rate_up_to_april" {
+  # checkov:skip=CKV_AWS_337: Standard KMS is fine
+  name  = "/currency_rates/up_to_april/gbp"
+  type  = "SecureString"
+  value = "[]" # or use a dummy like '[]'
+
+  lifecycle {
+    ignore_changes = [value] # This is critical, so its not overwritten 
+  }
+}
+
+resource "aws_ssm_parameter" "gbp_exchange_rate_april_to_may" {
+  # checkov:skip=CKV_AWS_337: Standard KMS is fine
+  name  = "/currency_rates/april_to_may/gbp"
+  type  = "SecureString"
+  value = "[]" # or use a dummy like '[]'
+
+  lifecycle {
+    ignore_changes = [value] # This is critical, so its not overwritten 
+  }
+}
+
+resource "aws_ssm_parameter" "gbp_exchange_rate_may" {
+  # checkov:skip=CKV_AWS_337: Standard KMS is fine
+  name  = "/currency_rates/may/gbp"
+  type  = "SecureString"
+  value = "[]" # or use a dummy like '[]'
+
+  lifecycle {
+    ignore_changes = [value] # This is critical, so its not overwritten 
+  }
+}
