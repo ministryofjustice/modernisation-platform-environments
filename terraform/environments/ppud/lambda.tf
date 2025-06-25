f82200796e3cfda5a214bc8295a94fc3252d73f3
Buckingham <XC01857@lumen.com>
2025-06-25 15:01:06 +0100

    Update_250625_15

--- terraform/environments/ppud/lambda.tf
+++ terraform/environments/ppud/lambda.tf
@@ -148,11 +148,11 @@ resource "aws_lambda_function" "terraform_lambda_func_securityhub_report_dev" {
   s3_bucket                      = "moj-infrastructure-dev"
   s3_key                         = "lambda/functions/securityhub_report_dev.zip"
   function_name                  = "securityhub_report_dev"
-  role                           = aws_iam_role.lambda_role_securityhub_get_data_dev[0].arn
+  role                           = aws_iam_role.lambda_role_get_securityhub_data_dev[0].arn
   handler                        = "securityhub_report_dev.lambda_handler"
   runtime                        = "python3.12"
   timeout                        = 300
-  depends_on                     = [aws_iam_role_policy_attachment.attach_lambda_policy_securityhub_get_data_to_lambda_role_securityhub_get_data_dev]
+  depends_on                     = [aws_iam_role_policy_attachment.attach_lambda_policies_get_securityhub_data_dev]
   reserved_concurrent_executions = 5
   dead_letter_config {
     target_arn = aws_sqs_queue.lambda_queue_dev[0].arn
