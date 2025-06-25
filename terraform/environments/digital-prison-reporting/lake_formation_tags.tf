2a86372638f2a8c117241e754d2219aeabf76370
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-25 15:21:04 +0100

    dmet-prisons-391; remove db level permissions

--- terraform/environments/digital-prison-reporting/lake_formation_tags.tf
+++ terraform/environments/digital-prison-reporting/lake_formation_tags.tf
@@ -14,19 +14,6 @@ resource "aws_lakeformation_resource_lf_tag" "tag_database_with_domain" {
 # Data Engineering SSO Role
 ###########################
 
-resource "aws_lakeformation_permissions" "grant_tag_policy_db_de_role" {
-  principal   = "arn:aws:iam::593291632749:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_modernisation-platform-data-eng_499410b42334a7d7"
-  permissions = ["DESCRIBE", "ASSOCIATE"]
-
-  lf_tag_policy {
-    resource_type = "DATABASE"
-    expression {
-      key    = aws_lakeformation_lf_tag.domain_tag.key
-      values = ["prisons"]
-    }
-  }
-}
-
 resource "aws_lakeformation_permissions" "grant_tag_policy_table_de_role" {
   principal   = "arn:aws:iam::593291632749:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_modernisation-platform-data-eng_499410b42334a7d7"
   permissions = ["DESCRIBE", "ASSOCIATE"]
@@ -53,19 +40,6 @@ resource "aws_lakeformation_permissions" "grant_tag_access_de_role" {
 ###########################
 # External Account Permissions
 ###########################
-
-resource "aws_lakeformation_permissions" "grant_tag_policy_db_external_account" {
-  principal   = "593291632749"
-  permissions = ["DESCRIBE", "ASSOCIATE"]
-
-  lf_tag_policy {
-    resource_type = "DATABASE"
-    expression {
-      key    = aws_lakeformation_lf_tag.domain_tag.key
-      values = ["prisons"]
-    }
-  }
-}
 
 resource "aws_lakeformation_permissions" "grant_tag_policy_table_external_account" {
   principal   = "593291632749"
