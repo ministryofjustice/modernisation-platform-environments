50f9f45aea6b30afb13d74730eb3871328f61075
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-06-27 11:54:05 +0100

    added more S3 sub resource

---
+++ terraform/environments/property-cafm-data-migration/s3.tf
@@ -0,0 +1,117 @@
+#######################################################################################################
+# S3 Buckets, acls, versioning, lifestyle configs, logging, notifications, public access & IAM policies
+#######################################################################################################
+
+#######################################################################################################
+# Production Environment 
+#######################################################################################################
+
+# S3 Bucket for Files copying between the CAFM Environments
+
+resource "aws_s3_bucket" "CAFM" {
+  # checkov:skip=CKV_AWS_145: "S3 bucket is not public facing, does not contain any sensitive information and does not need encryption"
+  # checkov:skip=CKV_AWS_62: "S3 bucket event notification is not required"
+  # checkov:skip=CKV2_AWS_62: "S3 bucket event notification is not required"
+  # checkov:skip=CKV_AWS_144: "CAFM has a UK Sovereignty requirement so cross region replication is prohibited"
+  # checkov:skip=CKV_AWS_18: "S3 bucket logging is not required"
+  bucket = "property-datahub-landing-${local.environment}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${local.application_name}-CAFM-S3"
+    }
+  )
+}
+
+resource "aws_s3_bucket_acl" "CAFM_ACL" {
+  bucket = aws_s3_bucket.CAFM.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "CAFM" {
+  bucket = aws_s3_bucket.CAFM.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "CAFM" {
+  # checkov:skip=CKV_AWS_300: "S3 bucket has a set period for aborting failed uploads, this is a false positive finding"
+  bucket = aws_s3_bucket.CAFM.id
+  rule {
+    id     = "tf-s3-lifecycle"
+    status = "Enabled"
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "STANDARD_IA"
+    }
+  }
+}
+
+# S3 block public access
+resource "aws_s3_bucket_public_access_block" "CAFM" {
+  bucket = aws_s3_bucket.CAFM.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# S3 bucket policy
+
+resource "aws_s3_bucket_policy" "CAFM" {
+  bucket = aws_s3_bucket.CAFM.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid" : "RequireSSLRequests",
+        "Effect" : "Deny",
+        "Principal" : "*",
+        "Action" : "s3:*",
+        "Resource" : [
+          aws_s3_bucket.CAFM.arn,
+          "${aws_s3_bucket.CAFM.arn}/*"
+        ],
+        "Condition" : {
+          "Bool" : {
+            "aws:SecureTransport" : "false"
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::${local.environment_management.account_ids["property-cafm-data-migration-development"]}:role/developer",
+            "arn:aws:iam::${local.environment_management.account_ids["property-cafm-data-migration-development"]}:role/sandbox",
+            "arn:aws:iam::${local.environment_management.account_ids["property-cafm-data-migration-preproduction"]}:role/developer",
+            "arn:aws:iam::${local.environment_management.account_ids["property-cafm-data-migration-preproduction"]}:role/migration",
+            "arn:aws:iam::${local.environment_management.account_ids["property-cafm-data-migration-production"]}:role/developer",
+            "arn:aws:iam::${local.environment_management.account_ids["property-cafm-data-migration-production"]}:role/migration"
+          ]
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.CAFM.arn}/*"
+      }
+    ]
+  })
+}
