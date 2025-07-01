7109365b1ed8480308b3606fe518292e9538550b
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-07-01 17:33:22 +0100

    cloudwatch log group fixed

--- terraform/environments/property-cafm-data-migration/transfer-servers.tf
+++ terraform/environments/property-cafm-data-migration/transfer-servers.tf
@@ -20,7 +20,7 @@ resource "aws_transfer_server" "this" {
   security_policy_name = "TransferSecurityPolicy-2024-01"
 
   logging_role                = module.transfer_family_service_role.iam_role_arn
-  structured_log_destinations = ["${module.transfer_structured_logs.cloudwatch_log_group_arn}:*"]
+  structured_log_destinations = ["${aws_cloudwatch_log_group.cloudwatch_log_group_arn}:*"]
 }
 
 resource "aws_transfer_tag" "this" {
