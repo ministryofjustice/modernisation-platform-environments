4a8344d536b162440ee9cb17391441a6e202e76f
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2025-07-01 15:11:48 +0100

    updated variables.tf

--- terraform/modules/shield_advanced/variables.tf
+++ terraform/modules/shield_advanced/variables.tf
@@ -21,7 +21,7 @@ variable "waf_acl_rules" {
 
 variable "enable_logging" {
   type        = bool
-  default     = false
+  default     = true
   description = "Enable logging for the WAF ACL."
 }
 
@@ -29,4 +29,10 @@ variable "log_retention_in_days" {
   type        = number
   default     = 60
   description = "Number of days to retain logs in CloudWatch Logs."
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
 }
