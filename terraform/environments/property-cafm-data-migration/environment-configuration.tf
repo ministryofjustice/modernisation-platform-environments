515e0cf7f01d468715f4462dfa66130a2b211c05
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-07-02 16:03:37 +0100

    removed test egres user

--- terraform/environments/property-cafm-data-migration/environment-configuration.tf
+++ terraform/environments/property-cafm-data-migration/environment-configuration.tf
@@ -10,12 +10,6 @@ locals {
           cidr_blocks = ["1.2.3.4/32"]
         }
       }
-      transfer_server_sftp_users_with_egress = {
-        "gary-test-egress" = {
-          ssh_key               = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpNyYzfbqmy4QkViLRyASRqxrEK7G34o3Bc6Jdp8vK555/oBuAUXUxfavtenZnxuxdxsrBYBSCBFcU4+igeXN/nN2kVfaUlt1xBZBCRUaajinhmt+3CLbr8bWmHR/5vL/DhxHH+j/+gDH5A244XN/ybZQvCGX/ilgKiae8s0tiOZD2hmX0fhRTCohQFG/DIu06gqKIyxUQoHBoBJxjzaDvjqioJgqmD9893DN+Gx1KozmaQWHM+0f7iK1UFp8BkdeFBVkj8TOfx60o/EmAjWQ/U+WSHblaXo0nI+LQKZYkW52uTEnfSkbkyvs/vj8E8+vagwYi0noyTVmb5qReSuk1kyuqEP2ycKIaWKt+Z4LnwxHm7KO51SMMeBgpiFHaUTQWXZHYuU2aXVfFIgJkCtHdEjG7Qe2P8K5XU5rG+CrQ/Y9PxPrKQHk+2nox9dLfCWo2Eho1N85z9/rA7A0oNwsHkjWAl3k87lWdpg7y3VNLzqsMNF4M4HjpQV60MH73dUU= essex-police@kpvmshift04app.netr.ecis.police.uk"
-          cidr_blocks           = ["1.1.2.3/32"]
-        }
-      }
     }
     production = {
       /* Transfer Server */
