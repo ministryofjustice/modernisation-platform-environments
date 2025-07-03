d4aa1cdc83c324c56c709fd69ff78e5415e94ad8
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-07-03 12:59:14 +0100

    Create SNS Topic and Chatbot configuration

--- terraform/environments/ccms-edrms/chatbot.tf
+++ terraform/environments/ccms-edrms/chatbot.tf
@@ -6,8 +6,9 @@ module "template" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot"
 
   slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
-  sns_topic_arns   = aws_sns_topic.cloudwatch_slack.arn
+  sns_topic_arns   = ["arn:aws:sns:eu-west-2:${local.environment_management.account_ids[terraform.workspace]}:cloudwatch-slack-alerts"]
   tags             = local.tags
   application_name = local.application_name
 
 }
+
