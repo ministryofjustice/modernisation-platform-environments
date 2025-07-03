6aa78be9925e52fbc68f2f5c016c5bbe28c9547f
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-07-03 15:35:30 +0100

    Create CW Alarm

---
+++ terraform/environments/ccms-edrms/cloudwatch.tf
@@ -0,0 +1,23 @@
+# CloudWatch Alarms for EDRMS Container Count
+
+resource "aws_cloudwatch_metric_alarm" "container_count" {
+  alarm_name                = "${local.application_name}-ecs-task-count"
+  alarm_description         = "This alarm fires if the number of EDRMS ECS tasks is less than or equal to the threshold"
+  comparison_operator       = "LessThanThreshold"
+  metric_name               = "DesiredTaskCount"
+  namespace                 = "ECS/ContainerInsights"
+  statistic                 = "Average"
+  period                    = 300
+  evaluation_periods        = 1
+  datapoints_to_alarm       = 1
+  threshold                 = local.application_data.accounts[local.environment].app_count
+  treat_missing_data        = "missing"
+  alarm_actions             = [aws_sns_topic.cloudwatch_slack.arn]
+  ok_actions                = [aws_sns_topic.cloudwatch_slack.arn]
+  insufficient_data_actions = []
+
+  dimensions = {
+    ServiceName = local.application_name
+    ClusterName = "${local.application_name}-cluster"
+  }
+}
