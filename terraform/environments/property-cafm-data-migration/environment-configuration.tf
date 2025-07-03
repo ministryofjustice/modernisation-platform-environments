0f52daa539e596520368ccdd2ddcecc1084d602e
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-07-03 11:45:35 +0100

    added a sftp test user

--- terraform/environments/property-cafm-data-migration/environment-configuration.tf
+++ terraform/environments/property-cafm-data-migration/environment-configuration.tf
@@ -30,7 +30,12 @@ locals {
 
       /* Transfer Server */
       transfer_server_hostname = "sftp.development.ingestion.cafm-migration.service.justice.gov.uk"
-
+      transfer_server_sftp_users = {
+        "test_user" = {
+          ssh_key     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCWz7ue/saomMAKrVgo6FifjpGQfl7B4fs2s/MJa2jhpBVWXk9tquGDXp1/Yfk4C7FIneGKfh8fWHz9FPS+u6h3a9hMW8d/5onNuSr9S6T2mN7ydZQzGez5qyG2vNFLyip3ls6mQjIpXSo2aow7+3Y2lbDe8UamiYNVgvvWB+hVl5RJjcaReDDbi0xwdjGjep0LcvgAyKa8evmcEbFVkrLhWyc30xn1+OesqPWSpoIb/IlBDFxCqR46GW/zlOldEIatONhXWgvJ6dS5T1YmHsE4U0Py3BV8O5zvc+XRYjr/3w9LOwmTHS1xbzlhNBjO1o6O9hSBsowBjsWLL5aNWcdBH0DiWfIWkoq9Fy8VEAa/T5v7GCaKvDs9pGBpjQSQsWyKXbwP0Z2RGyU2CSGVzMM6gzrjaxanOK9QbLOqCpTSSIYWfokt+MNrHcQU+9mBTjq20URF7RW6tsM8GvzGRNk0hlkX3ueq86uLpQzRctGBTjN74qBba0WbauIcSl4OIrc+NEwjaFTmuIs0NIG5aoAop8WHOC8cxFAST2XjMF30eEh6/W9Gh0uPor4L5tUqJ/JuI2wcfYLuk1KLDcOUVin79QficX93zbaTPNXWW052ct50B0KnCmZyvQORwOH8gBFgkFe5MO/bqevG9Xpof/QvpCLKEON/fBAW4bEdIIv5qw=="
+          cidr_blocks = ["51.11.176.157/32"]
+        }
+      }
       /* DataSync */
       datasync_instance_private_ip = "10.26.128.5"
     }
