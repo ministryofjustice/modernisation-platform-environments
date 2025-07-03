bea6354733aea02f029f69baea84b3914d9bfe48
Jono Basford <jono.basford@justice.gov.uk>
2025-07-03 18:30:53 +0100

    Added extra attributes to dms_spike_target_endpoint

--- terraform/environments/electronic-monitoring-data/modules/dms_spike/endpoints.tf
+++ terraform/environments/electronic-monitoring-data/modules/dms_spike/endpoints.tf
@@ -19,5 +19,6 @@ resource "aws_dms_s3_endpoint" "dms_spike_target_endpoint" {
   timestamp_column_name            = "dms_spike_timestamp"
   enable_statistics                = true
   parquet_timestamp_in_millisecond = true
-  service_access_role_arn             = aws_iam_role.dms_spike_s3_access_role.arn
+  service_access_role_arn          = aws_iam_role.dms_spike_s3_access_role.arn
+  add_column_name                  = true
 }
