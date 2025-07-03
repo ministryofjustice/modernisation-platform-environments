debeab024fe86dca46458b59083929ebceb06680
Jono Basford <jono.basford@justice.gov.uk>
2025-07-03 18:13:19 +0100

    More Build Fixes

--- terraform/environments/electronic-monitoring-data/dms_spike_main.tf
+++ terraform/environments/electronic-monitoring-data/dms_spike_main.tf
@@ -4,20 +4,20 @@ module "dms_rds_spike" {
   source = "./modules/dms_spike"
 
   # Instance Configuration
-  dms_instance_id    = "mysql-rds-spike-${var.local.environment}"
-  rds_instance_arn   = data.aws_db_instance.database_2022.arn
-  dms_subnet_id      = tolist(data.aws_db_subnet_group.db.subnet_ids)
-  dms_security_group = data.aws_security_group.dms_ri_security_group.id
+  dms_instance_id    = "mysql-rds-spike-${local.environment}"
+  rds_instance_arn   = aws_db_instance.database_2022.arn
+  dms_subnet_id      = tolist(aws_db_subnet_group.db.subnet_ids)
+  dms_security_group = aws_security_group.dms_ri_security_group[0].id
 
   # Connection details
   engine_name = "sqlserver"
-  username    = data.aws_db_instance.database_2022.username
-  password    = data.aws_db_instance.database_2022.password
+  username    = aws_db_instance.database_2022.username
+  password    = aws_db_instance.database_2022.password
   server_name = split(":", data.aws_db_instance.database_2022.endpoint)[0]
-  port        = data.aws_db_instance.database_2022.port
+  port        = aws_db_instance.database_2022.port
 
   # RDS Data Source and target details
-  s3_bucket_name  = "emds-${var.local.environment}-data-20240917144025201600000001"
+  s3_bucket_name  = "emds-${local.environment}-data-20240917144025201600000001"
   table_mappings = file("table_mappings/lcm_archive_2019.json")
   database_name  = "lcm_archive_2019"
 }
