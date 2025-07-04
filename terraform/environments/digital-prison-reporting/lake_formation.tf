9251536c263b81472792a43f8511c1dc691c5c62
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-07-04 21:19:06 +0100

    dmet-prison-add tag policy

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -87,3 +87,22 @@ resource "aws_lakeformation_permissions" "de_role_db_describe" {
     name = "dpr_ap_integration_test_newtag_dev_dbt"
   }
 }
+
+resource "aws_lakeformation_permissions" "de_role_prisons_and_non_sensitive" {
+  principal   = "arn:aws:iam::593291632749:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_modernisation-platform-data-eng_499410b42334a7d7"
+  permissions = ["DESCRIBE", "SELECT"]
+
+  lf_tag_policy {
+    resource_type = "TABLE"
+
+    expression {
+      key    = "domain"
+      values = ["prisons"]
+    }
+
+    expression {
+      key    = "sensitive"
+      values = ["false", "true", "data_linking"]
+    }
+  }
+}
