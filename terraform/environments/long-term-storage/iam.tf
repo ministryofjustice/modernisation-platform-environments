7cebab807df425653308ca847094f7b75a917c9d
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2025-07-07 15:21:14 +0100

    skipped Checkov secret encryption check

--- terraform/environments/long-term-storage/iam.tf
+++ terraform/environments/long-term-storage/iam.tf
@@ -36,6 +36,7 @@ resource "aws_iam_user_policy_attachment" "s3_user_policy_attach" {
 
 resource "aws_secretsmanager_secret" "s3_user_secret" {
   # checkov:skip=CKV2_AWS_57:Auto rotation not possible
+  # checkov:skip=CKV_AWS_149:No requirement currently to encrypt this secret with customer-managed KMS key
   name        = "s3-user-credentials"
   description = "Access and secret key for S3 IAM user"
 }
