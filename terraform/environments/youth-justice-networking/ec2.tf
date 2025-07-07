48356410ca1e540c39b50741c0a36bf2b83c7dbf
Rich Green <richard.green@digital.justice.gov.uk>
2025-07-07 13:54:09 +0100

    update ignores

--- terraform/environments/youth-justice-networking/ec2.tf
+++ terraform/environments/youth-justice-networking/ec2.tf
@@ -3,10 +3,9 @@ resource "aws_security_group" "external_sg" {
   #checkov:skip=CKV_AWS_24: "vSRX instances are configured as network firewalls with DDOS protection mitigating the risk of open SG rules"
   #checkov:skip=CKV_AWS_25:
   #checkov:skip=CKV_AWS_260:
-  #checkov:skip=CKV_AWS_277: 
-  #checkov:skip=382:
-  #trivy:ignore:avd-aws-0107:
-
+  #checkov:skip=CKV_AWS_277:
+  #checkov:skip=CKV_AWS_382:
+  #trivy:ignore:AVD-AWS-0107:
   name        = "YJBJuniperEXT-SG"
   description = "External Interface Juniper Security Group"
   vpc_id      = module.vpc.vpc_id
