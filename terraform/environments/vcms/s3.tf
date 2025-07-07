dd392456ad8ccea386fc12a22c69bef1c0662e30
Seb Norris <sebastian.j.norris@gmail.com>
2025-07-07 14:34:18 +0100

    add hyphen to bucket prefix

--- terraform/environments/vcms/s3.tf
+++ terraform/environments/vcms/s3.tf
@@ -5,7 +5,7 @@ module "general_bucket" {
 
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v8.2.1"
 
-  bucket_prefix      = "${local.application_name}-${local.environment}"
+  bucket_prefix      = "${local.application_name}-${local.environment}-"
   versioning_enabled = false
 
   providers = {
