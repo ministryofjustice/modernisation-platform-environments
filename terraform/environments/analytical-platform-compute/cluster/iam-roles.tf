de67703615fac30e0debb5702269afe045be8b4c
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-07-07 14:51:05 +0000

    :wrench: Moving IAM Role resource to iam-roles.tf

--- terraform/environments/analytical-platform-compute/cluster/iam-roles.tf
+++ terraform/environments/analytical-platform-compute/cluster/iam-roles.tf
@@ -225,3 +225,24 @@ module "amazon_prometheus_proxy_iam_role" {
 
   tags = local.tags
 }
+
+module "vpc_cni_iam_role" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.58.0"
+
+  role_name_prefix      = "vpc-cni"
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
+  }
+
+  tags = local.tags
+}
