2859610af1b99cc158fdbb72d5b83470a4d7d36a
Rich Green <richard.green@digital.justice.gov.uk>
2025-07-07 11:00:40 +0100

    encrypt cw log groups with kms

--- terraform/environments/youth-justice-networking/logging.tf
+++ terraform/environments/youth-justice-networking/logging.tf
@@ -13,6 +13,7 @@ resource "aws_cloudwatch_log_group" "yjaf_logs" {
   for_each          = local.log_groups
   name              = each.key
   retention_in_days = each.value
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
 }
 
 resource "aws_iam_role" "yjb_juniper_ec2_role" {
@@ -30,7 +31,66 @@ resource "aws_iam_role" "yjb_juniper_ec2_role" {
   })
 }
 
-# CReate instance profile for syslog server
+resource "aws_kms_key" "cloudwatch_logs" {
+  description         = "KMS key for CloudWatch log group encryption"
+  enable_key_rotation = true
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "Allow account use of the key"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow CloudWatch Logs use of the key"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.${data.aws_region.current.name}.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "kms:EncryptionContext:aws:logs:arn" = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_kms_alias" "cloudwatch_logs" {
+  name          = "alias/cloudwatch-logs"
+  target_key_id = aws_kms_key.cloudwatch_logs.key_id
+}
+
+resource "aws_cloudwatch_log_group" "yjaf_logs" {
+  for_each          = local.log_groups
+  name              = each.key
+  retention_in_days = each.value
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
+}
+
+
+# Create instance profile for syslog server
 
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.yjb_juniper_ec2_role.name
