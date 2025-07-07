e4d15272e4fc6ebd9b2807d487c3f44451ceae8c
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-07 15:41:28 +0100

    chore: remove webhook secret

--- terraform/environments/laa-ccms-soa/secrets.tf
+++ terraform/environments/laa-ccms-soa/secrets.tf
@@ -40,12 +40,3 @@ resource "aws_secretsmanager_secret" "soa_deploy_ssh_key" {
   name        = "ccms/soa/deploy-github-ssh-key"
   description = "Github SSH Deploy Key"
 }
-
-resource "aws_secretsmanager_secret" "alerting_webhook_url" {
-  name        = "ccms/soa/alerting_webhook_url"
-  description = "Alerting Slack Webook URL"
-}
-
-data "aws_secretsmanager_secret_version" "alerting_webhook_url" {
-  secret_id = aws_secretsmanager_secret.alerting_webhook_url.id
-}
