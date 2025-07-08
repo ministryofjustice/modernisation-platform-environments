6f618727f107d99ce1dcaeee0a9bcb4c01514fbd
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-07-08 09:28:20 +0100

    update lb vars

--- terraform/modules/baseline/lb.tf
+++ terraform/modules/baseline/lb.tf
@@ -110,7 +110,7 @@ module "lb" {
 
   for_each = var.lbs
 
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=f803c4706d2f064c7877bb5f5a89d8db7e201613"
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer.git?ref=f6fe0d9c4fcc20561864cff292c6ef2d3a0f80b3"
 
   providers = {
     aws.bucket-replication = aws
@@ -129,7 +129,7 @@ module "lb" {
   enable_cross_zone_load_balancing = each.value.enable_cross_zone_load_balancing
   dns_record_client_routing_policy = each.value.dns_record_client_routing_policy
   s3_versioning                    = each.value.s3_versioning
-  s3_notification_sqs_queues       = each.value.s3_notification_sqs_queues
+  s3_notification_queues           = each.value.s3_notification_queues
   access_logs_lifecycle_rule       = each.value.access_logs_lifecycle_rule
 
   existing_bucket_name = try(module.s3_bucket[each.value.existing_bucket_name].bucket.id, each.value.existing_bucket_name)
