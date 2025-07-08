67ab42cb5e87967960c57cd0c7f16b7eb4015246
Jono Basford <jono.basford@justice.gov.uk>
2025-07-08 09:49:28 +0100

    Updated replication task settings

--- terraform/environments/electronic-monitoring-data/modules/dms_spike/replication_task.tf
+++ terraform/environments/electronic-monitoring-data/modules/dms_spike/replication_task.tf
@@ -9,14 +9,14 @@ resource "aws_dms_replication_task" "dms_spike_replication_task" {
   replication_task_settings = jsonencode(
     {
       Logging = {
-        "EnableLogging" = true,
-      }
-      "ValidationSettings" = {
-        "EnableValidation" = true,
-        "ValidationMode" = "table-level",
-        "ThreadCount" = 2,
-        "TableFailureMaxCount" = 5,
-        "RecordFailureDelayLimitInMinutes" = 5,
+        EnableLogging = true
+      },
+      ValidationSettings = {
+        EnableValidation = true,
+        ValidationMode = "table-level",
+        ThreadCount = 2,
+        TableFailureMaxCount = 5,
+        RecordFailureDelayLimitInMinutes = 5,
       }
     }
   )
