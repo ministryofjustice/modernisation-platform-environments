db201a0a35c6444ec5e49e93bd2877c9485de685
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-07-09 17:23:54 +0000

    Add XCom sidecar container configuration to Airflow local settings
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-compute/airflow/src/airflow/local-settings/development/airflow_local_settings.py
+++ terraform/environments/analytical-platform-compute/airflow/src/airflow/local-settings/development/airflow_local_settings.py
@@ -1,4 +1,7 @@
 from airflow.www.utils import UIAlert
+from airflow.providers.cncf.kubernetes.utils.xcom_sidecar import PodDefaults
+from kubernetes.client import models as k8s_models
+
 
 DASHBOARD_UIALERTS = [
     UIAlert(
@@ -7,3 +10,14 @@ DASHBOARD_UIALERTS = [
         html=True,
     )
 ]
+
+# Configure the XCom sidecar container
+PodDefaults.SIDECAR_CONTAINER.image = "ghcr.io/ministryofjustice/analytical-platform-airflow-xcom-sidecar:1.0.0-rc1@sha256:4378d3e223747478b63c3fb2a262e201e12b0ddc997c8e62f2eecae365b28021"
+
+PodDefaults.SIDECAR_CONTAINER.security_context=k8s_models.V1SecurityContext(
+    allow_privilege_escalation=False,
+    privileged=False,
+    run_as_non_root=True,
+    seccomp_profile=k8s_models.V1SeccompProfile(type="RuntimeDefault"),
+    capabilities=k8s_models.V1Capabilities(drop=["ALL"]),
+)
