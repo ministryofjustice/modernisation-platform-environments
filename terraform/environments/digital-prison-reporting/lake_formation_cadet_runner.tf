7f56c7e735806cfa4dac88f90d3c799190533dea
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-07-10 15:50:32 +0100

    dmet-prisons: add DPR DE role

--- terraform/environments/digital-prison-reporting/lake_formation_cadet_runner.tf
+++ terraform/environments/digital-prison-reporting/lake_formation_cadet_runner.tf
@@ -5,28 +5,42 @@ locals {
     "dpr_ap_integration_test_notag_dev_dbt"
   ]
 
-
+  principals = [
+    aws_iam_role.dataapi_cross_role.arn,
+    "arn:aws:iam::771283872747:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_modernisation-platform-data-eng_a2da3e45320e1580"
+  ]
 }
 
-resource "aws_lakeformation_permissions" "dataapi_cross_role_db_access" {
-  for_each = toset(local.databases_for_db_access)
-
-  principal   = aws_iam_role.dataapi_cross_role.arn
-  permissions = ["ALL"]
-
-  database {
-    name = each.key
+# Cartesian product: for each (database, principal) combination
+locals {
+  database_principal_pairs = {
+    for pair in setproduct(local.databases_for_db_access, local.principals) :
+    "${pair[0]}::${pair[1]}" => {
+      database  = pair[0]
+      principal = pair[1]
+    }
   }
 }
 
-resource "aws_lakeformation_permissions" "dataapi_cross_role_table_access" {
-  for_each = toset(local.databases_for_db_access)
+resource "aws_lakeformation_permissions" "role_db_access_internal" {
+  for_each = local.database_principal_pairs
 
-  principal   = aws_iam_role.dataapi_cross_role.arn
+  principal   = each.value.principal
+  permissions = ["ALL"]
+
+  database {
+    name = each.value.database
+  }
+}
+
+resource "aws_lakeformation_permissions" "role_table_access_internal" {
+  for_each = local.database_principal_pairs
+
+  principal   = each.value.principal
   permissions = ["ALL"]
 
   table {
-    database_name = each.key
+    database_name = each.value.database
     wildcard      = true
   }
 }
