1837b7e49af13eb0f956fdc06f1b755650c03ca8
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-10 13:55:57 +0100

    chore: enabled rds logging

--- terraform/environments/ccms-edrms/logs.tf
+++ terraform/environments/ccms-edrms/logs.tf
@@ -12,3 +12,19 @@ resource "aws_cloudwatch_log_stream" "log_stream_edrms" {
   name           = "${local.application_name}-log-stream"
   log_group_name = aws_cloudwatch_log_group.log_group_edrms.name
 }
+
+#--RDS
+resource "aws_cloudwatch_log_group" "rds_alert" {
+  name              = "/aws/rds/instance/oracle-db/alert"
+  retention_in_days = local.application_data.accounts[local.environment].db_log_retention_days
+}
+
+resource "aws_cloudwatch_log_group" "rds_audit" {
+  name              = "/aws/rds/instance/oracle-db/audit"
+  retention_in_days = local.application_data.accounts[local.environment].db_log_retention_days
+}
+
+resource "aws_cloudwatch_log_group" "rds_listener" {
+  name              = "/aws/rds/instance/oracle-db/listener"
+  retention_in_days = local.application_data.accounts[local.environment].db_log_retention_days
+}
