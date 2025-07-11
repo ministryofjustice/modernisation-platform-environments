add34a202ecba4d09830c82874f123d8e904e590
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-07-11 14:17:43 +0100

    endpoints module added

--- terraform/environments/property-cafm-data-migration/main.tf
+++ terraform/environments/property-cafm-data-migration/main.tf
@@ -1,5 +1,5 @@
 module "rds_export" {
-  source = "github.com/ministryofjustice/terraform-rds-export?ref=sql-backup-restore"
+  source = "github.com/ministryofjustice/terraform-rds-export?ref=04916a52ce20590674198fc48456dad95d7dee75"
 
   # Replace the kms_key_arn, name, vpc_id and (database_subnet_ids in a list)
   kms_key_arn = aws_kms_key.sns_kms.arn
@@ -15,29 +15,39 @@ module "rds_export" {
   }
 }
 
-# S3 Gateway Endpoint
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = module.vpc.vpc_id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids   = module.vpc.private_route_table_ids
 
-  tags = {
-    Name = "${local.application_name}-s3-endpoint"
+module "endpoints" {
+  # Commit has for v5.21.0
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc//modules/vpc-endpoints?ref=507193ee659f6f0ecdd4a75107e59e2a6c1ac3cc"
+
+  vpc_id                     = module.vpc.vpc_id
+  create_security_group      = true
+  security_group_description = "Managed by Terraform"
+  security_group_tags        = { Name : "eu-west-1-dev" }
+  security_group_rules = {
+    ingress_https = {
+      description = "HTTPS from VPC"
+      cidr_blocks = [module.vpc.vpc_cidr_block]
+    }
   }
-}
+  endpoints = {
 
-# Secrets Manager Interface Endpoint
-resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id             = module.vpc.vpc_id
-  service_name       = "com.amazonaws.${data.aws_region.current.name}.secretsmanager"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
-  security_group_ids = [module.vpc.default_security_group_id] # or your custom SG
+    s3 = {
+      service_type    = "Gateway" # gateway endpoint
+      service         = "s3"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags            = { Name = "s3-eu-west-1-dev" }
+    }
 
-  private_dns_enabled = true
-
-  tags = {
-    Name = "${local.application_name}-secretsmanager-endpoint"
+    secrets_manager = {
+      service             = "secretsmanager"
+      service_type        = "Interface"
+      subnet_ids          = module.vpc.private_subnets
+      private_dns_enabled = true
+      tags                = { Name = "secretsmanager-eu-west-1-dev" }
+    }
   }
+
+  tags = merge(var.tags, { network = "Private" })
+
 }
