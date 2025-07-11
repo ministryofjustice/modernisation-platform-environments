84fdb1cb5055fca80dc8aa6a1ef8c60a66bc8021
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2025-07-11 15:09:33 +0100

    Module calling edited

--- terraform/environments/xhibit-portal/xhibit-waf.tf
+++ terraform/environments/xhibit-portal/xhibit-waf.tf
@@ -1,13 +1,13 @@
 module "waf" {
   source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-waf?ref=v0.0.1"
   enable_ddos_protection = true
-  ddos_rate_limit        = 5000
-  block_non_uk_traffic   = true
+  ddos_rate_limit        = 3000
+  block_non_uk_traffic   = false
   associated_resource_arns = [aws_lb.waf_lb.arn]
 
   managed_rule_actions = {
     AWSManagedRulesKnownBadInputsRuleSet = false
-    AWSManagedRulesCommonRuleSet         = true
+    AWSManagedRulesCommonRuleSet         = false
     AWSManagedRulesSQLiRuleSet           = false
     AWSManagedRulesLinuxRuleSet          = false
     AWSManagedRulesAnonymousIpList       = false
