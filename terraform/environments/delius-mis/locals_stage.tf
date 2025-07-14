94699a67524864b40d4ee81ba9378574d2cd39da
Robert Sweetman <robertiansweetman@gmail.com>
2025-07-14 14:53:56 +0100

    deploy new instance name (#11477)
    * deploy new instance name
    
    * remove duplicate data ref
    
    * auto will be deleted so put in here
    
    * remove dis instances
    
    * delete instance
    
    * build instance
    
    * add access to more secrets
    
    * fix bug in user-data init
    
    * delete instance
    
    * build instance
    
    * change to arn
    
    * add secrets permissions, use real hostnames
    
    * use EC2 tags
    
    * add param
    
    * destroy instance
    
    * deploy instance
    
    * delete instance
    
    * redeploy for testing
    
    * fix naming and rename
    
    * fix var name
    
    * delete instance
    
    * redeploy and check IPS install params
    
    * re-order user data init and add debug
    
    * delete
    
    * add missing cidr var
    
    * test move to OU
    
    * burn down instance
    
    * deploy instance
    
    * change ami and destroy instance
    
    * deploy new ami
    
    * use correct ami name format
    
    * delete instance
    
    * delete steps that are now in the ami
    
    * deploy instance and retest
    
    * delete instance
    
    * add winrar to packages and path
    
    * deploy again
    
    * change branch to main, delete auto instance, fix defaults
    
    * use latest ami (dis) and set backup true for (dfi)
    
    * fix instance count

--- terraform/environments/delius-mis/locals_stage.tf
+++ terraform/environments/delius-mis/locals_stage.tf
@@ -10,6 +10,7 @@ locals {
     migration_environment_abbreviated_name = "del"
     migration_environment_short_name       = "stage"
     migration_environment_private_cidr     = ["10.160.32.0/22", "10.160.36.0/22", "10.160.40.0/22"]
+    migration_environment_db_cidr          = ["10.162.110.0/25", "10.162.108.0/24", "10.162.109.0/24"]
     cloudwatch_alarm_schedule              = true
     cloudwatch_alarm_disable_time          = "20:45"
     cloudwatch_alarm_enable_time           = "06:15"
