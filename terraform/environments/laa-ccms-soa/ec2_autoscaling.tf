697133da51e2f532bf9fc71f83ddd4ea59110da6
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-14 10:35:02 +0100

    fix: asg syntax error

--- terraform/environments/laa-ccms-soa/ec2_autoscaling.tf
+++ terraform/environments/laa-ccms-soa/ec2_autoscaling.tf
@@ -12,10 +12,11 @@ resource "aws_autoscaling_group" "cluster-scaling-group-managed" {
     id      = aws_launch_template.ec2-launch-template-managed.id
     version = "$Latest"
   }
-  tags = merge(
-    local.tags,
-    { instance-scheduling = "skip-scheduling" }
-  )
+  tag {
+    key                 = "instance-scheduling"
+    value               = "skip-scheduling"
+    propagate_at_launch = true
+  }
   depends_on = [
     aws_efs_file_system.storage,
     aws_db_instance.soa_db
@@ -34,10 +35,11 @@ resource "aws_autoscaling_group" "cluster-scaling-group-admin" {
     id      = aws_launch_template.ec2-launch-template-admin.id
     version = "$Latest"
   }
-  tags = merge(
-    local.tags,
-    { instance-scheduling = "skip-scheduling" }
-  )
+  tag {
+    key                 = "instance-scheduling"
+    value               = "skip-scheduling"
+    propagate_at_launch = true
+  }
   depends_on = [
     aws_efs_file_system.storage,
     aws_db_instance.soa_db
