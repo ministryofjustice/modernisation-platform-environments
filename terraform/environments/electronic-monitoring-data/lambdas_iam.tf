087b009db4470197472fb8cbb8ba6f142b1e1d9f
matt heery <matt.heery@justice.gov.uk>
2025-07-15 00:04:41 +0100

    fix: ***

--- terraform/environments/electronic-monitoring-data/lambdas_iam.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_iam.tf
@@ -510,6 +510,8 @@ data "aws_iam_policy_document" "zero_etl" {
     effect = "Allow"
     actions = [
       "glue:*Integration*",
+      "glue:*Integration",
+      "glue:Integration*"
     ]
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:integration/*",
