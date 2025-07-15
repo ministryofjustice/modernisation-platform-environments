8775ebc53716e6e39d44cbcc2aa632f95f6d1f9c
matt heery <matt.heery@justice.gov.uk>
2025-07-15 15:30:00 +0100

    fix: allow root to access glue

--- terraform/environments/electronic-monitoring-data/glue_catalog_settings.tf
+++ terraform/environments/electronic-monitoring-data/glue_catalog_settings.tf
@@ -27,11 +27,24 @@ data "aws_iam_policy_document" "glue-policy" {
     actions = [
       "glue:AuthorizeInboundIntegration",
       "glue:CreateInboundIntegration",
-      "glue:CreateIntegration"
+      "glue:CreateIntegration",
     ]
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog",
       "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = ["glue:*"]
+    resources = [
+      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/*/*",
+      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/*",
+      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog"
     ]
   }
 }
