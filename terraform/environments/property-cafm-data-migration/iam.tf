aceb2b1d437ec41a6bdcdbe3c28a5a443d6c4dcd
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-07-15 10:55:55 +0100

    fixed Inappropriate value for attribute

--- terraform/environments/property-cafm-data-migration/iam.tf
+++ terraform/environments/property-cafm-data-migration/iam.tf
@@ -27,7 +27,7 @@ data "aws_iam_policy_document" "sftp_access" {
       "transfer:List*",
       "transfer:SendWorkflowStepState"
     ]
-    resources = "arn:aws:s3:::${aws_s3_bucket.CAFM.bucket}/*"
+    resources = ["arn:aws:s3:::${aws_s3_bucket.CAFM.bucket}/*"]
   }
 }
 
