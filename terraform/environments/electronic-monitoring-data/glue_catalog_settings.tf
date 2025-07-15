2d12accd4f9a511c29b3eda62065b4f9f7e77827
matt heery <matt.heery@justice.gov.uk>
2025-07-15 15:45:09 +0100

    fix: all glue properties

--- terraform/environments/electronic-monitoring-data/glue_catalog_settings.tf
+++ terraform/environments/electronic-monitoring-data/glue_catalog_settings.tf
@@ -43,9 +43,7 @@ data "aws_iam_policy_document" "glue-policy" {
     }
     actions = ["glue:*"]
     resources = [
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/*/*",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/*",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog"
+      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
     ]
   }
 }
