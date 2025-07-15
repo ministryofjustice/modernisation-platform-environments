95824c1023ce50855aa1f8e01d3d087499814fe6
matt heery <matt.heery@justice.gov.uk>
2025-07-15 12:17:02 +0100

    fix: add create inbound int

--- terraform/environments/electronic-monitoring-data/glue_catalog_settings.tf
+++ terraform/environments/electronic-monitoring-data/glue_catalog_settings.tf
@@ -26,7 +26,8 @@ data "aws_iam_policy_document" "glue-policy" {
     }
     actions = [
       "glue:AuthorizeInboundIntegration",
-      "glue:CreateIntegration",
+      "glue:CreateInboundIntegration",
+      "glue:CreateIntegration"
     ]
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog",
