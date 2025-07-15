6952b72c28af8d8d73cd1f4b5754aeaf1ae9c86f
vikasbsuresh-moj <vikas.suresh@justice.gov.uk>
2025-07-15 23:38:48 +0100

    fix: added awscliv2 in unserdata

--- terraform/environments/ccms-ebs/templates/ec2_user_data_ftp.sh
+++ terraform/environments/ccms-ebs/templates/ec2_user_data_ftp.sh
@@ -7,9 +7,9 @@ amazon-linux-extras install -y epel
 yum install -y wget unzip vsftpd jq s3fs-fuse amazon-cloudwatch-agent
 # yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
 
 # wget https://s3.amazonaws.com/amazoncloudwatch-agent/oracle_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 # rpm -U ./amazon-cloudwatch-agent.rpm
