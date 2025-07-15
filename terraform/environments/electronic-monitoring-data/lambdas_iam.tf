2185528b8a4c415c55b3b8bb744df1962f545635
matt heery <matt.heery@justice.gov.uk>
2025-07-15 15:02:38 +0100

    fix: use *

--- terraform/environments/electronic-monitoring-data/lambdas_iam.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_iam.tf
@@ -517,10 +517,7 @@ data "aws_iam_policy_document" "zero_etl" {
       "glue:UpdateIntegrationResourceProperty"
     ]
     resources = [
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:integration/*",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:connection/*",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/*",
-      "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog",
+      "*"
     ]
   }
   statement {
