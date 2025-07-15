b34d4ee444611ecc7c964b3ea82e6544694f7bb0
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-07-15 16:22:21 +0100

    dmet-prisons/dl remove domain/sensitive grants

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -44,26 +44,5 @@ resource "aws_lakeformation_permissions" "data_location_access_working" {
   }
 }
 
-# Domain tag: Now grant the permissions to the CaDeT cross account role
-resource "aws_lakeformation_permissions" "domain_grant" {
-  principal   = aws_iam_role.dataapi_cross_role.arn
-  permissions = ["DESCRIBE", "ASSOCIATE", "GRANT_WITH_LF_TAG_EXPRESSION"]
-
-  lf_tag {
-    key    = aws_lakeformation_lf_tag.domain_tag.key
-    values = aws_lakeformation_lf_tag.domain_tag.values
-  }
-}
-
-# Sensitive tag: Now grant the permissions to the CaDeT cross account role
-resource "aws_lakeformation_permissions" "sensitive_grant" {
-  principal   = aws_iam_role.dataapi_cross_role.arn
-  permissions = ["DESCRIBE", "ASSOCIATE", "GRANT_WITH_LF_TAG_EXPRESSION"]
-
-  lf_tag {
-    key    = aws_lakeformation_lf_tag.sensitive_tag.key
-    values = aws_lakeformation_lf_tag.sensitive_tag.values
-  }
-}
 
 
