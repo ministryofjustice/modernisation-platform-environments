e369bf94534c378ce6fa743da26ac9c8d0e9f5c4
Zoltan Paldi <161834373+zoltan-paldi@users.noreply.github.com>
2025-07-16 14:01:07 +0100

    chore(waf): fix remove extra comma from array #XPM-347

--- terraform/environments/xhibit-portal/xhibit-waf.tf
+++ terraform/environments/xhibit-portal/xhibit-waf.tf
@@ -34,7 +34,7 @@ resource "aws_wafv2_ip_set" "prtg_waf_ip_set" {
     "35.176.93.186/32",  // MoJ VPN Gateway Proxies
     "172.10.10.188/32",  // V1 Digital Wifi
     "194.62.186.170/32", // V1 VPN Gateway Proxies
-    "66.155.16.68/32",   // Southampton BEL Wifi
+    "66.155.16.68/32"   // Southampton BEL Wifi
   ]
 
   tags = merge(local.tags,
