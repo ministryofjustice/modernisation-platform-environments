41b0d3b912f70645911b5784a5657c2cd150f7dd
ttipler <thomas.tipler@necsws.com>
2025-07-16 10:30:26 +0100

    add sns for ses bounce

--- terraform/environments/youth-justice-app-framework/modules/ses/sns.tf
+++ terraform/environments/youth-justice-app-framework/modules/ses/sns.tf
@@ -1,3 +1,30 @@
 resource "aws_sns_topic" "ses_notifications" {
   name = "ses-bounce-complaint-topic"
 }
+
+
+resource "aws_sns_topic_policy" "allow_ses_publish" {
+  arn = aws_sns_topic.ses_notifications.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowSESPublish",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ses.amazonaws.com"
+        },
+        Action    = "SNS:Publish",
+        Resource  = aws_sns_topic.ses_notifications.arn,
+        Condition = {
+          StringEquals = {
+            "AWS:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+        }
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
