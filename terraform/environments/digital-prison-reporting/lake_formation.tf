e08de2e3f1618eea1c3712bd588f0b01cadba5e3
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-07-17 20:16:00 +0100

    dmet-prisons/dl/remove defaults (entire block)

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -1,22 +1,22 @@
-resource "aws_lakeformation_data_lake_settings" "lake_formation" {
-  admins = flatten([
-    [for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn],
-    data.aws_iam_session_context.current.issuer_arn,
+# resource "aws_lakeformation_data_lake_settings" "lake_formation" {
+#   admins = flatten([
+#     [for share in local.analytical_platform_share : aws_iam_role.analytical_platform_share_role[share.target_account_name].arn],
+#     data.aws_iam_session_context.current.issuer_arn,
 
-    # Make Data engineer role a LF admin
-    try(one(data.aws_iam_roles.data_engineering_roles.arns), []),
+#     # Make Data engineer role a LF admin
+#     try(one(data.aws_iam_roles.data_engineering_roles.arns), []),
 
-    # Make Developer role a LF admin
-    # As cannot give them the permissions (lakeformation:GetDataAccess)
-    length(data.aws_iam_roles.developer_roles.arns) > 0 ? [one(data.aws_iam_roles.developer_roles.arns)] : [],
+#     # Make Developer role a LF admin
+#     # As cannot give them the permissions (lakeformation:GetDataAccess)
+#     length(data.aws_iam_roles.developer_roles.arns) > 0 ? [one(data.aws_iam_roles.developer_roles.arns)] : [],
 
-    ]
-  )
+#     ]
+#   )
 
-  parameters = {
-    "CROSS_ACCOUNT_VERSION" = "4"
-  }
-}
+#   parameters = {
+#     "CROSS_ACCOUNT_VERSION" = "4"
+#   }
+# }
 
 # Give the cadet cross-account role LF data access
 resource "aws_iam_role_policy_attachment" "dataapi_cross_role_lake_formation_data_access" {
