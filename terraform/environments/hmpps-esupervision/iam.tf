46aa30f104fc7660c7c1118140157aa121d43135
Lee Kitching <lee.kitching@justice.gov.uk>
2025-07-17 13:58:46 +0100

    Add IAM user and access policies for S3 and Rekognition
    Add IAM user to HMPPS esupervision environments. Add a policy
    granting the user access to the rekognition uploads bucket and
    attach the managed read-only policy for rekognition.

---
+++ terraform/environments/hmpps-esupervision/iam.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_user" "rekognition_user" {
+  name = "rekognition"
+}
+
+data "aws_iam_policy_document" "rekognition_s3_policy_document" {
+  statement {
+    sid = "LocateUserBuckets"
+    effect = "Allow"
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation"
+    ]
+    resources = ["arn:aws:s3:::*"]
+  }
+
+  statement {
+    sid = "RekognitionBucketList"
+    effect = "Allow"
+    actions = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.rekognition_bucket.arn]
+  }
+
+  statement {
+    sid = "RekognitionBucketCRUDOperations"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    resources = ["${aws_s3_bucket.rekognition_bucket.arn}/*"]
+  }
+}
+
+resource "aws_iam_policy" "rekognition_s3_policy" {
+  name = "rekognition_uploads_bucket_access"
+  policy = data.aws_iam_policy_document.rekognition_s3_policy_document.json
+}
+
+data "aws_iam_policy" "rekognition_read" {
+  name = "AmazonRekognitionReadOnlyAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "rekognition_rekognition" {
+  user = aws_iam_user.rekognition_user.name
+  policy_arn = data.aws_iam_policy.rekognition_read
+}
+
+resource "aws_iam_user_policy_attachment" "rekognition_s3" {
+  user = aws_iam_user.rekognition_user.name
+  policy_arn = aws_iam_policy.rekognition_s3_policy.arn
+}
