3a610f21eb0f5af33ab292c87968be894b45ec69
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-07-17 11:13:54 +0100

    dmet-prisons/dl104 - set to null with a principal

--- terraform/environments/digital-prison-reporting/lake_formation.tf
+++ terraform/environments/digital-prison-reporting/lake_formation.tf
@@ -16,14 +16,14 @@ resource "aws_lakeformation_data_lake_settings" "lake_formation" {
   # ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_data_lake_settings#principal
   create_database_default_permissions {
     # These settings should replicate current behaviour: LakeFormation is Ignored
-    permissions = ["ALL"]
-    principal   = "IAM_ALLOWED_PRINCIPALS"
+    permissions = []
+    principal   = ""
   }
 
   create_table_default_permissions {
     # These settings should replicate current behaviour: LakeFormation is Ignored
-    permissions = ["ALL"]
-    principal   = "IAM_ALLOWED_PRINCIPALS"
+    permissions = []
+    principal   = ""
   }
 
   parameters = {
