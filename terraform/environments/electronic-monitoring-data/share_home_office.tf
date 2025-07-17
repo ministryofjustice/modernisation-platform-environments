565856c7fffa0242b21079f5ee0eecc42d9dc037
matt heery <matt.heery@justice.gov.uk>
2025-07-17 16:37:10 +0100

    fix: add cross gov share

---
+++ terraform/environments/electronic-monitoring-data/share_home_office.tf
@@ -0,0 +1,32 @@
+data "aws_secretsmanager_secret_version" "home_office_account_id" {
+  secret_id = aws_secretsmanager_secret.home_office_account_id.id
+}
+
+locals {
+    ho_role_arn = "arn:aws:iam::${data.aws_secretsmanager_secret_version.home_office_account_id.secret_string}:role/DACC-DataScience-TL"
+}
+
+resource "aws_lakeformation_permissions" "home_office_share_bucket" {
+  principal   = local.ho_role_arn
+  permissions = ["DATA_LOCATION_ACCESS"]
+  data_location {
+    arn = aws_lakeformation_resource.data_bucket.arn
+  }
+}
+
+resource "aws_lakeformation_permissions" "home_office_share_database" {
+  principal   = local.ho_role_arn
+  permissions = ["DESCRIBE"]
+  database {
+    name = "g4s_gps"
+  }
+}
+
+resource "aws_lakeformation_permissions" "home_office_share_table" {
+  principal   = local.ho_role_arn
+  permissions = ["SELECT"]
+  table {
+    database_name = "g4s_gps"
+    name          = "ho_subject_positions"
+  }
+}
