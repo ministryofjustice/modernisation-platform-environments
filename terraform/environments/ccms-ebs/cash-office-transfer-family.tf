270f7267b04889bff7c81c51cb87cdeaaa1c6ad2
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-18 11:50:48 +0100

    fix: linting

--- terraform/environments/ccms-ebs/cash-office-transfer-family.tf
+++ terraform/environments/ccms-ebs/cash-office-transfer-family.tf
@@ -1,8 +1,8 @@
 module "transfer_family" {
-  source           = "./modules/transfer-family"
-  aws_account_id   = data.aws_caller_identity.current.account_id
-  app_name         = local.application_name
-  bucket_name      = aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].bucket
-  aws_identity_centre_store_arn = local.application_data.accounts[local.environment].cash_office_idp_arn
-  aws_identity_centre_group_id = local.application_data.accounts[local.environment].cash_office_sso_group_id
+  source                           = "./modules/transfer-family"
+  aws_account_id                   = data.aws_caller_identity.current.account_id
+  app_name                         = local.application_name
+  bucket_name                      = aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].bucket
+  aws_identity_centre_store_arn    = local.application_data.accounts[local.environment].cash_office_idp_arn
+  aws_identity_centre_sso_group_id = local.application_data.accounts[local.environment].cash_office_sso_group_id
 }
