e05aad08e9df5508e85f0ec92357aa99ff233b28
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-21 16:04:23 +0100

    test: module count

--- terraform/environments/ccms-ebs/imports.tf
+++ terraform/environments/ccms-ebs/imports.tf
@@ -1,9 +1,9 @@
 import {
-  to = module.transfer_family.awscc_transfer_web_app.this
+  to = module.transfer_family[0].awscc_transfer_web_app.this
   id = "arn:aws:transfer:eu-west-2:767123802783:webapp/webapp-1089f07c01fc4216b"
 }
 
 import {
-  to = module.transfer_family.aws_s3control_access_grant.this
+  to = module.transfer_family[0].aws_s3control_access_grant.this
   id = "767123802783,c64272c4-30a1-7039-8ffd-af791143da2e"
 }
