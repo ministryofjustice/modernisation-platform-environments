c1967f991202fb54b413ffb32307954ea7970c47
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-22 12:24:56 +0100

    test: recreate instance

--- terraform/environments/ccms-ebs/modules/transfer-family/grant.tf
+++ terraform/environments/ccms-ebs/modules/transfer-family/grant.tf
@@ -4,11 +4,12 @@ resource "aws_s3control_access_grants_instance" "this" {
 }
 
 resource "aws_s3control_access_grants_location" "this" {
+  depends_on     = [aws_s3control_access_grants_instance.this]
   iam_role_arn   = aws_iam_role.s3.arn
   location_scope = "s3://${var.bucket_name}"
 }
 
-resource "aws_s3control_access_grant" "this" {
+/* resource "aws_s3control_access_grant" "this" {
   depends_on                = [aws_s3control_access_grants_instance.this]
   permission                = "READWRITE"
   access_grants_location_id = aws_s3control_access_grants_location.this.id
@@ -16,4 +17,4 @@ resource "aws_s3control_access_grant" "this" {
     grantee_type       = "DIRECTORY_GROUP"
     grantee_identifier = var.aws_identity_centre_sso_group_id
   }
-}
+} */
