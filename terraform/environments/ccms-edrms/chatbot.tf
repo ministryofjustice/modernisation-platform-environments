2d8c221c927d15573fa487f1e3a0354110c3b1d0
Aaron Robinson <aaron.robinson1@justice.gov.uk>
2025-07-22 10:06:47 +0100

    remove ref

--- terraform/environments/ccms-edrms/chatbot.tf
+++ terraform/environments/ccms-edrms/chatbot.tf
@@ -3,7 +3,7 @@
 
 module "template" {
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=@73280f80ce8a4557cec3a76ee56eb913452ca9aa" # v2.0.0
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot@73280f80ce8a4557cec3a76ee56eb913452ca9aa" # v2.0.0
 
   slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
   sns_topic_arns   = ["arn:aws:sns:eu-west-2:${local.environment_management.account_ids[terraform.workspace]}:cloudwatch-slack-alerts"]
