312304c344ab7fe2ceef74d1e6139597c0dcb7c4
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-22 09:43:05 +0100

    fix: remove inline imports

--- terraform/environments/ccms-ebs/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = module.transfer_family[0].awscc_transfer_web_app.this
-  id = "arn:aws:transfer:eu-west-2:767123802783:webapp/webapp-1089f07c01fc4216b"
-}
-
-import {
-  to = module.transfer_family[0].aws_s3control_access_grant.this
-  id = "767123802783,bf39ec08-45cb-4003-b9ea-ed3f8c40175d"
-}
