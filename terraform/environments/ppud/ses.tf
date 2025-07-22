a4099e2dfa667f2e9a610b013c342a930b25c9ae
Buckingham <XC01857@lumen.com>
2025-07-22 08:27:53 +0100

    Update_220725_2

--- terraform/environments/ppud/ses.tf
+++ terraform/environments/ppud/ses.tf
@@ -1,3 +1,37 @@
+####################################################################
+# SES domain identities, verifications, config sets and dkim configs
+####################################################################
+
+###########################
+# Preproduction Environment
+###########################
+
+resource "aws_ses_configuration_set" "ses_events_configuration_set_uat" {
+  count  = local.is-preproduction == false ? 1 : 0
+  name   = "ses-events-configuration-set-uat"
+}
+
+resource "aws_ses_event_destination" "ses_delivery_events" {
+  count                  = local.is-preproduction == false ? 1 : 0
+  name                   = "delivery-events"
+  configuration_set_name = aws_ses_configuration_set.ses_events_configuration_set_uat[0].name
+  enabled                = true
+
+  matching_types = [
+    "delivery",
+    "bounce",
+    "complaint"
+  ]
+
+  sns_destination {
+    topic_arn = aws_sns_topic.ses_logging_uat[0].arn
+  }
+}
+
+########################
+# Production Environment
+########################
+
 resource "aws_ses_domain_identity" "ppud" {
   count  = local.is-production == false ? 1 : 0
   domain = local.application_data.accounts[local.environment].SES_domain
