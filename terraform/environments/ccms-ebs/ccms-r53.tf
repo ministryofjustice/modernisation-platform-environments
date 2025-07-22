fc81f099f1d80e8e9cabbd54302cd062267455e9
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-22 10:41:37 +0100

    chore: dns error

--- terraform/environments/ccms-ebs/ccms-r53.tf
+++ terraform/environments/ccms-ebs/ccms-r53.tf
@@ -289,7 +289,7 @@ resource "aws_route53_record" "nlb_testing" {
   provider = aws.core-vpc
   zone_id  = data.aws_route53_zone.external.zone_id
   name     = "ccms-nlb.modernisation-platform.service.justice.gov.uk"
-  type     = "A"
+  type     = "CNAME"
   ttl      = 300
   records  = ["ccms-ebs-db-nlb-4a3f6f3bd578eb48.elb.eu-west-2.amazonaws.com"]
 }
