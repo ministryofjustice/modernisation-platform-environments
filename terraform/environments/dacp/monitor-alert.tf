744b7d0438c96da74b23cce83d6172177456f51a
Timi Olubusi <timiolubusi@icloud.com>
2025-07-23 17:09:55 +0100

    Revert "Removing all ecs tf"
    This reverts commit 07e9c556a803f2da435874094633217bce52c072.

--- terraform/environments/dacp/monitor-alert.tf
+++ terraform/environments/dacp/monitor-alert.tf
@@ -18,60 +18,60 @@ resource "aws_sns_topic_subscription" "email_subscription" {
 }
 
 # Define the metrics in ContainerInsights (for ECS Fargate)
-# resource "aws_cloudwatch_metric_alarm" "ecs_service_high_ram_alarm" {
-#   alarm_name          = "ecs_service_high_ram_alarm"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = 3
-#   metric_name         = "MemoryUtilized"
-#   namespace           = "ECS/ContainerInsights"
-#   period              = 60
-#   statistic           = "Average"
-#   threshold           = 2500
-#   alarm_description   = "DACP ECS scaling up as memory has exceeded the threshold"
-#   alarm_actions = [
-#     aws_appautoscaling_policy.scale_up_amber.arn,
-#     aws_sns_topic.dacp_utilisation_alarm.arn,
-#     aws_sns_topic.email_topic.arn
-#   ]
-#   dimensions = {
-#     ClusterName = "dacp_cluster"
-#     ServiceName = var.networking[0].application
-#   }
-# }
+resource "aws_cloudwatch_metric_alarm" "ecs_service_high_ram_alarm" {
+  alarm_name          = "ecs_service_high_ram_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 3
+  metric_name         = "MemoryUtilized"
+  namespace           = "ECS/ContainerInsights"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 2500
+  alarm_description   = "DACP ECS scaling up as memory has exceeded the threshold"
+  alarm_actions = [
+    aws_appautoscaling_policy.scale_up_amber.arn,
+    aws_sns_topic.dacp_utilisation_alarm.arn,
+    aws_sns_topic.email_topic.arn
+  ]
+  dimensions = {
+    ClusterName = "dacp_cluster"
+    ServiceName = var.networking[0].application
+  }
+}
 
-# resource "aws_cloudwatch_metric_alarm" "ecs_service_normal_ram_alarm" {
-#   alarm_name          = "ecs_service_normal_ram_alarm"
-#   comparison_operator = "LessThanThreshold"
-#   evaluation_periods  = 3
-#   metric_name         = "MemoryUtilized"
-#   namespace           = "ECS/ContainerInsights"
-#   period              = 60
-#   statistic           = "Average"
-#   threshold           = 2500
-#   alarm_description   = "DACP ECS scaling down as memory has returned to normal levels"
-#   alarm_actions = [
-#     aws_appautoscaling_policy.scale_down_amber.arn,
-#     aws_sns_topic.dacp_utilisation_alarm.arn,
-#     aws_sns_topic.email_topic.arn
-#   ]
-#   dimensions = {
-#     ClusterName = "dacp_cluster"
-#     ServiceName = var.networking[0].application
-#   }
-# }
+resource "aws_cloudwatch_metric_alarm" "ecs_service_normal_ram_alarm" {
+  alarm_name          = "ecs_service_normal_ram_alarm"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 3
+  metric_name         = "MemoryUtilized"
+  namespace           = "ECS/ContainerInsights"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 2500
+  alarm_description   = "DACP ECS scaling down as memory has returned to normal levels"
+  alarm_actions = [
+    aws_appautoscaling_policy.scale_down_amber.arn,
+    aws_sns_topic.dacp_utilisation_alarm.arn,
+    aws_sns_topic.email_topic.arn
+  ]
+  dimensions = {
+    ClusterName = "dacp_cluster"
+    ServiceName = var.networking[0].application
+  }
+}
 
-# resource "aws_cloudwatch_metric_alarm" "ecs_cpu_alarm" {
-#   alarm_name          = "ecs-cpu-utilization-alarm"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = "2"
-#   metric_name         = "CpuUtilized"
-#   namespace           = "ECS/ContainerInsights"
-#   period              = "120"
-#   statistic           = "Average"
-#   threshold           = "100"
-#   alarm_description   = "This metric checks if CPU utilization is high - threshold set to 80%"
-#   alarm_actions       = [aws_sns_topic.dacp_utilisation_alarm.arn]
-#   dimensions = {
-#     ClusterName = aws_ecs_cluster.dacp_cluster.name
-#   }
-# }
+resource "aws_cloudwatch_metric_alarm" "ecs_cpu_alarm" {
+  alarm_name          = "ecs-cpu-utilization-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CpuUtilized"
+  namespace           = "ECS/ContainerInsights"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "100"
+  alarm_description   = "This metric checks if CPU utilization is high - threshold set to 80%"
+  alarm_actions       = [aws_sns_topic.dacp_utilisation_alarm.arn]
+  dimensions = {
+    ClusterName = aws_ecs_cluster.dacp_cluster.name
+  }
+}
