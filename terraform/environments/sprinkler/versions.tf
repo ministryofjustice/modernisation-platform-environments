c537180d80440842d7bcf545028805c654f77395
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2025-07-23 12:32:27 +0100

    Test

--- terraform/environments/sprinkler/versions.tf
+++ terraform/environments/sprinkler/versions.tf
@@ -8,6 +8,10 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    null = {
+      version = "~> 3.2"
+      source  = "hashicorp/null"
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.6.2"
