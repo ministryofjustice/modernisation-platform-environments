1fd7a8b12955a07a01619f7c3b48d70a789a6287
matt heery <matt.heery@justice.gov.uk>
2025-07-23 15:38:32 +0100

    fix: add get tables

--- terraform/environments/electronic-monitoring-data/servicenow.tf
+++ terraform/environments/electronic-monitoring-data/servicenow.tf
@@ -95,6 +95,7 @@ data "aws_iam_policy_document" "zero_etl_target" {
     actions = [
       "glue:CreateTable",
       "glue:GetTable",
+      "glue:GetTables",
       "glue:UpdateTable",
       "glue:GetTableVersion",
       "glue:GetTableVersions",
@@ -216,21 +217,4 @@ resource "aws_lakeformation_permissions" "zero_etl_snow_table_access" {
 resource "aws_glue_catalog_database" "servicenow" {
   name = "servicenow${local.underscore_env}"
   location_uri = "s3://${module.s3-create-a-derived-table-bucket.bucket.id}/zero-etl/servicenow${local.underscore_env}/"
-}
-
-resource "aws_lakeformation_permissions" "zero_etl_snow_table_access_source" {
-  principal = aws_iam_role.zero_etl_snow_source.arn
-  permissions = ["ALL"]
-  table {
-    database_name = aws_glue_catalog_database.servicenow.name
-    wildcard      = true
-  }
-}
-
-resource "aws_lakeformation_permissions" "zero_etl_snow_db_access_source" {
-  principal   = aws_iam_role.zero_etl_snow_source.arn
-  permissions = ["ALL"]
-  database {
-    name = aws_glue_catalog_database.servicenow.name
-  }
 }
