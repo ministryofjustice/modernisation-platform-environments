c8b08bcf45e979c17366913b571a6b6e7b4a9e22
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2025-07-25 11:41:09 +0100

    populating files with draft resource scripts

---
+++ terraform/environments/laa-enterprise-service-bus/maat_lambda.tf
@@ -0,0 +1,18 @@
+resource "aws_lambda_function" "maat_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "nodejs16.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
