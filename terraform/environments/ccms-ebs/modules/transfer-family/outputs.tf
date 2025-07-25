bb22b4dc0314b6bec8de1350fa57ccf799147105
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-25 16:13:07 +0100

    test: add output

--- terraform/environments/ccms-ebs/modules/transfer-family/outputs.tf
+++ terraform/environments/ccms-ebs/modules/transfer-family/outputs.tf
@@ -2,3 +2,8 @@ output "grant_iam_role_arn" {
   description = "ARN for Grant Location IAM Role"
   value       = aws_iam_role.s3.arn
 }
+
+output "transfer_iam_role_arn" {
+  description = "ARN for Transfer IAM Role"
+  value       = aws_iam_role.transfer.arn
+}
