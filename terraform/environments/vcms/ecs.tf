9e698795d0951086b3fdbb9bcf3d190fda346ee6
Seb Norris <sebastian.j.norris@gmail.com>
2025-07-28 16:03:10 +0100

    update module version

--- terraform/environments/vcms/ecs.tf
+++ terraform/environments/vcms/ecs.tf
@@ -1,7 +1,6 @@
 module "ecs" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//cluster?ref=v2.0.1"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//cluster?ref=v6.0.0"
 
-  environment = local.environment
   name        = local.application_name
 
   tags = local.tags
