5b3bb744f559cca2409504f83b3edf975d60a1f5
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-28 16:46:18 +0100

    chore: ignore lifecycle changes

--- terraform/environments/ccms-ebs/webapp.tf
+++ terraform/environments/ccms-ebs/webapp.tf
@@ -4,4 +4,7 @@ resource "awscc_transfer_web_app" "demo" {
         instance_arn = "arn:aws:sso:::instance/ssoins-7535d9af4f41fb26"
         role         = "arn:aws:iam::767123802783:role/ccms-ebs-cashoffice-transfer"
     }
+    lifecycle {
+        ignore_changes = all
+    }
 }
