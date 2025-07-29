9bca3a879e2e95181839626d022b899dc0044d05
Vladimirs Kovalovs <vladimirs.kovalovs@digital.justice.gov.uk>
2025-07-29 14:04:24 +0100

    [HUB-32-bucket] lambda-layer bucker deployment

--- terraform/environments/laa-enterprise-service-bus/cwa_iam.tf
+++ terraform/environments/laa-enterprise-service-bus/cwa_iam.tf
@@ -1,51 +1,51 @@
-resource "aws_iam_role" "cwa_lambda_role" {
-  name = "${local.application_name}-cwa-lambda-role"
-  tags = merge(
-    local.tags,
-    {
-      Name = "${local.application_name}-cwa-lambda-role"
-    }
-  )
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+# resource "aws_iam_role" "cwa_lambda_role" {
+#   name = "${local.application_name}-cwa-lambda-role"
+#   tags = merge(
+#     local.tags,
+#     {
+#       Name = "${local.application_name}-cwa-lambda-role"
+#     }
+#   )
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "lambda.amazonaws.com"
+#         },
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_policy" "cwa_lambda_role_policy" {
-  name = "${local.application_name}-cwa-lambda-policy"
+# resource "aws_iam_policy" "cwa_lambda_role_policy" {
+#   name = "${local.application_name}-cwa-lambda-policy"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup"
-        ],
-        Resource = "arn:aws:logs:*:*:*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "arn:aws:logs:*:*:log-group:/aws/lambda/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "logs:CreateLogGroup"
+#         ],
+#         Resource = "arn:aws:logs:*:*:*"
+#       },
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents"
+#         ],
+#         Resource = "arn:aws:logs:*:*:log-group:/aws/lambda/*"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "cwa_lambda_role_policy_attachment" {
-  role       = aws_iam_role.cwa_lambda_role.name
-  policy_arn = aws_iam_policy.cwa_lambda_role_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "cwa_lambda_role_policy_attachment" {
+#   role       = aws_iam_role.cwa_lambda_role.name
+#   policy_arn = aws_iam_policy.cwa_lambda_role_policy.arn
+# }
