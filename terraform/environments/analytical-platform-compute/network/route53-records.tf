756f366cb7689669dc39068df3900a1207f65b4f
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-07-30 15:04:01 +0000

    Revery to module but commented out
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-compute/network/route53-records.tf
+++ terraform/environments/analytical-platform-compute/network/route53-records.tf
@@ -1,39 +1,42 @@
-resource "aws_route53_record" "airflow" {
-  zone_id = data.aws_route53_zone.network_services.zone_id
-  name    = "airflow"
-  type    = "CNAME"
-  ttl     = 300
-  records = [data.aws_lb.mwaa_alb.dns_name]
-}
+# module "route53_records" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+#   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-resource "aws_route53_record" "ses_verification" {
-  zone_id = data.aws_route53_zone.network_services.zone_id
-  name    = "_amazonses"
-  type    = "TXT"
-  ttl     = 600
-  records = [aws_ses_domain_identity.main.verification_token]
-}
+#   source  = "terraform-aws-modules/route53/aws//modules/records"
+#   version = "5.0.0"
 
-resource "aws_route53_record" "dkim_0" {
-  zone_id = data.aws_route53_zone.network_services.zone_id
-  name    = "${aws_ses_domain_dkim.main.dkim_tokens[0]}._domainkey"
-  type    = "CNAME"
-  ttl     = 300
-  records = ["${aws_ses_domain_dkim.main.dkim_tokens[0]}.dkim.amazonses.com"]
-}
+#   zone_id = module.route53_zones.route53_zone_zone_id[local.environment_configuration.route53_zone]
 
-resource "aws_route53_record" "dkim_1" {
-  zone_id = data.aws_route53_zone.network_services.zone_id
-  name    = "${aws_ses_domain_dkim.main.dkim_tokens[1]}._domainkey"
-  type    = "CNAME"
-  ttl     = 300
-  records = ["${aws_ses_domain_dkim.main.dkim_tokens[1]}.dkim.amazonses.com"]
-}
-
-resource "aws_route53_record" "dkim_2" {
-  zone_id = data.aws_route53_zone.network_services.zone_id
-  name    = "${aws_ses_domain_dkim.main.dkim_tokens[2]}._domainkey"
-  type    = "CNAME"
-  ttl     = 300
-  records = ["${aws_ses_domain_dkim.main.dkim_tokens[2]}.dkim.amazonses.com"]
-}
+#   records = [
+#     {
+#       name    = "airflow"
+#       type    = "CNAME"
+#       ttl     = 300
+#       records = [data.aws_lb.mwaa_alb.dns_name]
+#     },
+#     {
+#       name    = "_amazonses"
+#       type    = "TXT"
+#       ttl     = 600
+#       records = [aws_ses_domain_identity.main.verification_token]
+#     },
+#     {
+#       name    = "${aws_ses_domain_dkim.main.dkim_tokens[0]}._domainkey"
+#       type    = "CNAME"
+#       ttl     = 300
+#       records = ["${aws_ses_domain_dkim.main.dkim_tokens[0]}.dkim.amazonses.com"]
+#     },
+#     {
+#       name    = "${aws_ses_domain_dkim.main.dkim_tokens[1]}._domainkey"
+#       type    = "CNAME"
+#       ttl     = 300
+#       records = ["${aws_ses_domain_dkim.main.dkim_tokens[1]}.dkim.amazonses.com"]
+#     },
+#     {
+#       name    = "${aws_ses_domain_dkim.main.dkim_tokens[2]}._domainkey"
+#       type    = "CNAME"
+#       ttl     = 300
+#       records = ["${aws_ses_domain_dkim.main.dkim_tokens[2]}.dkim.amazonses.com"]
+#     }
+#   ]
+# }
--- terraform/environments/analytical-platform-compute/network/ses.tf
+++ terraform/environments/analytical-platform-compute/network/ses.tf
@@ -5,13 +5,7 @@ resource "aws_ses_domain_identity" "main" {
 resource "aws_ses_domain_identity_verification" "main" {
   domain = aws_ses_domain_identity.main.domain
 
-  # depends_on = [module.route53_records]
-  depends_on = [
-    aws_route53_record.ses_verification,
-    aws_route53_record.dkim_0,
-    aws_route53_record.dkim_1,
-    aws_route53_record.dkim_2
-  ]
+  # depends_on = [module.route53_records] # uncomment after moving module.route53_records
 }
 
 resource "aws_ses_domain_dkim" "main" {
