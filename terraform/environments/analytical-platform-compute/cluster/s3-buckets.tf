afd5ec3662ce212ea5709f83fd939d5d7cf822d8
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2025-07-30 15:31:29 +0100

    work in progress

---
+++ terraform/environments/analytical-platform-compute/cluster/s3-buckets.tf
@@ -0,0 +1,13 @@
+# Add kms key to this bucket
+module "velero_s3_bucket" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.1.0"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+
+  bucket_prefix = "mojap-compute-${local.environment}-velero"
+
+  tags = local.tags
+}
