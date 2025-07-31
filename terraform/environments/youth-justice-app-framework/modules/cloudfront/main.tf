5055126fa4f8370f9dca42b7383653c97e63daac
ttipler <thomas.tipler@necsws.com>
2025-07-31 09:35:35 +0100

    cloudfront security headers

--- terraform/environments/youth-justice-app-framework/modules/cloudfront/main.tf
+++ terraform/environments/youth-justice-app-framework/modules/cloudfront/main.tf
@@ -185,10 +185,21 @@ resource "aws_cloudfront_response_headers_policy" "strict_transport_security" {
       preload                    = false    # Matches unchecked preload
       include_subdomains         = false    # Matches unchecked includeSubDomains
     }
+    content_type_options {
+      override                   = true
+    }
+    frame_options{
+      override                   = true
+      frame_option               = "DENY"
+    }
+    xss_protection {
+      override                   = true
+      protection                 = true
+      mode_block                 = true
+    }
     content_security_policy {
       override = true
       content_security_policy = "default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://public.tableau.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' https://www.gov.uk; object-src 'none';"
-
     }
   }
 }
