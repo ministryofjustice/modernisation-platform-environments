46e3d7d5595aef3b8d5db85c511bec07ca492961
matt heery <matt.heery@justice.gov.uk>
2025-07-31 16:55:06 +0100

    fix: remove ho share

--- terraform/environments/electronic-monitoring-data/share_home_office.tf
+++ terraform/environments/electronic-monitoring-data/share_home_office.tf
@@ -9,58 +9,58 @@ locals {
   ho_admin_arn = "arn:aws:iam::${local.ho_acct_id}:role/HO-FullAdmin"
 }
 
-resource "aws_lakeformation_permissions" "home_office_share_bucket" {
-  count       = local.is-production ? 1 : 0
-  principal   = local.ho_role_arn
-  permissions = ["DATA_LOCATION_ACCESS"]
-  data_location {
-    arn = aws_lakeformation_resource.data_bucket.arn
-  }
-}
+# resource "aws_lakeformation_permissions" "home_office_share_bucket" {
+#   count       = local.is-production ? 1 : 0
+#   principal   = local.ho_role_arn
+#   permissions = ["DATA_LOCATION_ACCESS"]
+#   data_location {
+#     arn = aws_lakeformation_resource.data_bucket.arn
+#   }
+# }
 
-resource "aws_lakeformation_permissions" "home_office_share_database" {
-  count       = local.is-production ? 1 : 0
-  principal   = local.ho_role_arn
-  permissions = ["DESCRIBE"]
-  database {
-    name = "g4s_gps"
-  }
-}
+# resource "aws_lakeformation_permissions" "home_office_share_database" {
+#   count       = local.is-production ? 1 : 0
+#   principal   = local.ho_role_arn
+#   permissions = ["DESCRIBE"]
+#   database {
+#     name = "g4s_gps"
+#   }
+# }
 
-resource "aws_lakeformation_permissions" "home_office_share_table" {
-  count       = local.is-production ? 1 : 0
-  principal   = local.ho_role_arn
-  permissions = ["SELECT"]
-  table {
-    database_name = "g4s_gps"
-    name          = "ho_subject_positions"
-  }
-}
+# resource "aws_lakeformation_permissions" "home_office_share_table" {
+#   count       = local.is-production ? 1 : 0
+#   principal   = local.ho_role_arn
+#   permissions = ["SELECT"]
+#   table {
+#     database_name = "g4s_gps"
+#     name          = "ho_subject_positions"
+#   }
+# }
 
-resource "aws_lakeformation_permissions" "home_office_admin_share_bucket" {
-  count       = local.is-production ? 1 : 0
-  principal   = local.ho_admin_arn
-  permissions = ["DATA_LOCATION_ACCESS"]
-  data_location {
-    arn = aws_lakeformation_resource.data_bucket.arn
-  }
-}
+# resource "aws_lakeformation_permissions" "home_office_admin_share_bucket" {
+#   count       = local.is-production ? 1 : 0
+#   principal   = local.ho_admin_arn
+#   permissions = ["DATA_LOCATION_ACCESS"]
+#   data_location {
+#     arn = aws_lakeformation_resource.data_bucket.arn
+#   }
+# }
 
-resource "aws_lakeformation_permissions" "home_office_admin_share_database" {
-  count       = local.is-production ? 1 : 0
-  principal   = local.ho_admin_arn
-  permissions = ["DESCRIBE"]
-  database {
-    name = "g4s_gps"
-  }
-}
+# resource "aws_lakeformation_permissions" "home_office_admin_share_database" {
+#   count       = local.is-production ? 1 : 0
+#   principal   = local.ho_admin_arn
+#   permissions = ["DESCRIBE"]
+#   database {
+#     name = "g4s_gps"
+#   }
+# }
 
-resource "aws_lakeformation_permissions" "home_office_admin_share_table" {
-  count       = local.is-production ? 1 : 0
-  principal   = local.ho_admin_arn
-  permissions = ["SELECT"]
-  table {
-    database_name = "g4s_gps"
-    name          = "ho_subject_positions"
-  }
-}
+# resource "aws_lakeformation_permissions" "home_office_admin_share_table" {
+#   count       = local.is-production ? 1 : 0
+#   principal   = local.ho_admin_arn
+#   permissions = ["SELECT"]
+#   table {
+#     database_name = "g4s_gps"
+#     name          = "ho_subject_positions"
+#   }
+# }
