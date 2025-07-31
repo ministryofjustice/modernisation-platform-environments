447f96771bb9152e50d89ea7d8a16e9cf6ccaacf
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2025-07-31 10:43:14 +0100

    updating resource name and syntax

--- terraform/environments/contract-work-administration/database.tf
+++ terraform/environments/contract-work-administration/database.tf
@@ -269,12 +269,12 @@ resource "aws_vpc_security_group_ingress_rule" "db_workspace_winscp" {
   to_port           = 22
 }
 
-resource "aws_security_group_ingress_rule" "allow_https_from_external_sg" {
-  source_security_group_id = data.aws_security_group.hub20_dev_cwa_lambda_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow_https_from_external_sg" {
+  referenced_security_group_id = data.aws_security_group.hub20_dev_cwa_lambda_sg.id
   security_group_id        = aws_security_group.database.id
   description              = "Allow SSH Access from HUB2.0 CWA Lambda"
   from_port                = 22
-  protocol                 = "tcp"
+  ip_protocol                 = "tcp"
   to_port                  = 22
   count                    = local.environment == "development" ? 1 : 0
 }
