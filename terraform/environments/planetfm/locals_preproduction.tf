55a22a55687f2321ffa4f598976a743dfdd624b0
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-07-31 10:44:13 +0100

    TM-1440: planetfm: upgrade preprod app 11-a part 3

--- terraform/environments/planetfm/locals_preproduction.tf
+++ terraform/environments/planetfm/locals_preproduction.tf
@@ -42,9 +42,9 @@ locals {
           availability_zone = "eu-west-2a"
         })
         ebs_volumes = {
-          "/dev/sda1" = { type = "gp3", size = 128 }                                       # root volume
-          "xvde"      = { type = "gp3", size = 6, snapshot_id = "snap-04435aa8246764616" } # Windows 2022 English Installation Media
-          "xvdf"      = { type = "gp3", size = 6, snapshot_id = "snap-040a13a16f7ffb223" } # Windows 2019 English Installation Media
+          "/dev/sda1" = { type = "gp3", size = 128 } # root volume
+          # "xvde"      = { type = "gp3", size = 6, snapshot_id = "snap-04435aa8246764616" } # Windows 2022 English Installation Media
+          "xvdf" = { type = "gp3", size = 6, snapshot_id = "snap-040a13a16f7ffb223" } # Windows 2019 English Installation Media
         }
         instance = merge(local.ec2_instances.app.instance, {
           disable_api_termination = true
