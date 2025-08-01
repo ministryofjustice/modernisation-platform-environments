8f20034923baef8df4d1fa61934b7530f6fca083
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-08-01 10:47:27 +0100

    deploy full size instance mis-db-config-stage

--- terraform/environments/delius-mis/locals_stage.tf
+++ terraform/environments/delius-mis/locals_stage.tf
@@ -337,12 +337,10 @@ locals {
 
   # MIS DB config
   mis_db_config_stage = {
-    instance_type = "m7i.large" # set to same size as dev for testing only
-    # instance_type  = "r7i.4xlarge"
-    instance_count = 0
+    instance_type  = "r7i.4xlarge" # manually turn off when not in use to save costs
+    instance_count = 1
     # most recent 8_5 image, ami builder needs fixing after this
     ami_name_regex = "^delius_core_ol_8_5_oracle_db_19c_patch_2025-03-02T00-00-34.442Z"
-    # ami_name_regex = "^delius_core_ol_8_5_oracle_db_19c_patch_2024-01-31T16-06-00.575Z"
 
     instance_policies = {
       "business_unit_kms_key_access" = aws_iam_policy.business_unit_kms_key_access
@@ -372,15 +370,13 @@ locals {
         iops       = 5000
         throughput = 500
         type       = "gp3"
-        # total_size = 6000 <- revert back when deploying properly
-        total_size = 500
+        total_size = 6000
       }
       flash = {
         iops       = 3000
         throughput = 500
         type       = "gp3"
-        # total_size = 4000 <- revert back when deploying properly
-        total_size = 500
+        total_size = 4000
       }
     }
     ansible_user_data_config = {
