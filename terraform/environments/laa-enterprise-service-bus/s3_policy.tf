b86393c03d5bb5c77f5b88c7b783f162e26d9557
Vladimirs Kovalovs <vladimirs.kovalovs@digital.justice.gov.uk>
2025-08-01 17:28:48 +0100

    [HUB-67-s3policy-v2] changed ccms permission to data bucket and redone cwa lambda sg

--- terraform/environments/laa-enterprise-service-bus/s3_policy.tf
+++ terraform/environments/laa-enterprise-service-bus/s3_policy.tf
@@ -13,10 +13,8 @@ resource "aws_s3_bucket_policy" "data_cross_account_access" {
           AWS = "arn:aws:iam::767123802783:role/role_stsassume_oracle_base"
         },
         Action = [
-          "s3:PutObject",
           "s3:ListBucket",
-          "s3:GetObject",
-          "s3:DeleteObject"
+          "s3:GetObject"
         ],
         Resource = [
           "arn:aws:s3:::${aws_s3_bucket.data.bucket}",
