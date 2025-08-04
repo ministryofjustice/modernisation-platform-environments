70851de41fcea5e750df2518a22b7af5351f303b
Vladimirs Kovalovs <vladimirs.kovalovs@digital.justice.gov.uk>
2025-08-04 14:52:15 +0100

    [HUB-67-s3policy-v2] new SG for lambda

--- terraform/environments/laa-enterprise-service-bus/cwa_extract_lambda.tf
+++ terraform/environments/laa-enterprise-service-bus/cwa_extract_lambda.tf
@@ -20,11 +20,22 @@ resource "aws_security_group" "cwa_extract" {
   description = "CWA Extract Lambda Security Group"
   vpc_id      = data.aws_vpc.shared.id
 
+  tags = merge(
+    local.tags,
+    { Name = "${local.application_name_short}-${local.environment}-cwa-extract-lambda-security-group" }
+  )
+}
+
+resource "aws_security_group" "cwa_extract_new" {
+  name        = "${local.application_name_short}-${local.environment}-cwa-extract-lambda-security-group-new"
+  description = "CWA Extract Lambda Security Group New"
+  vpc_id      = data.aws_vpc.shared.id
+
   revoke_rules_on_delete = true
 
   tags = merge(
     local.tags,
-    { Name = "${local.application_name_short}-${local.environment}-cwa-extract-lambda-security-group" }
+    { Name = "${local.application_name_short}-${local.environment}-cwa-extract-lambda-security-group-new" }
   )
 }
 
@@ -34,7 +45,7 @@ resource "aws_security_group_rule" "cwa_extract_egress_ssh" {
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = [local.application_data.accounts[local.environment].cwa_database_ip]
-  security_group_id = aws_security_group.cwa_extract.id
+  security_group_id = aws_security_group.cwa_extract_new.id
   description       = "Outbound SSH Access to CWA DB"
 }
 
@@ -44,7 +55,7 @@ resource "aws_security_group_rule" "cwa_extract_egress_oracle" {
   to_port           = 1521
   protocol          = "tcp"
   cidr_blocks       = [local.application_data.accounts[local.environment].cwa_database_ip]
-  security_group_id = aws_security_group.cwa_extract.id
+  security_group_id = aws_security_group.cwa_extract_new.id
   description       = "Outbound 1521 Access to CWA DB"
 }
 
@@ -54,7 +65,7 @@ resource "aws_security_group_rule" "cwa_extract_egress_https" {
   to_port                  = 443
   protocol                 = "tcp"
   source_security_group_id = local.application_data.accounts[local.environment].vpc_endpoint_sg
-  security_group_id        = aws_security_group.cwa_extract.id
+  security_group_id        = aws_security_group.cwa_extract_new.id
   description              = "Outbound 443 to LAA VPC Endpoint SG"
 }
 
