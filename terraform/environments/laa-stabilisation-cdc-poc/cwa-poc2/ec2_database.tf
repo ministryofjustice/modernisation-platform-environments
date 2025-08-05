16febf614f105f794077e8dffdc42398d566a5fa
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2025-08-05 16:05:06 +0100

    reverting change

--- terraform/environments/laa-stabilisation-cdc-poc/cwa-poc2/ec2_database.tf
+++ terraform/environments/laa-stabilisation-cdc-poc/cwa-poc2/ec2_database.tf
@@ -256,16 +256,6 @@ resource "aws_vpc_security_group_ingress_rule" "db_workspace_ssh" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "hub_20_ssh" {
-  referenced_security_group_id = var.application_data.accounts[local.environment].hub20_cwa_lambda_sg
-  security_group_id            = aws_security_group.cwa_poc2_database.id
-  description                  = "Allow SSH Access from HUB2.0 CWA Lambda"
-  from_port                    = 22
-  ip_protocol                  = "tcp"
-  to_port                      = 22
-  count                        = local.environment == "development" ? 1 : 0
-}
-
 resource "aws_vpc_security_group_ingress_rule" "db_workspaces_1" {
   security_group_id = aws_security_group.cwa_poc2_database.id
   description       = "DB access for Workspaces"
