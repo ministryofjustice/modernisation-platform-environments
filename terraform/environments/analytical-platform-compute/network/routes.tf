cffd926226fd91659e4c6faa94675011db48b321
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-08-06 11:26:15 +0100

    amend routes back to original to fix create-destroy issue

--- terraform/environments/analytical-platform-compute/network/routes.tf
+++ terraform/environments/analytical-platform-compute/network/routes.tf
@@ -1,15 +1,5 @@
-# module "transit_gateway_routes" {
-#   for_each = toset(module.vpc.private_route_table_ids)
-
-#   source = "./modules/routes"
-
-#   route_table_id          = each.value
-#   destination_cidr_blocks = local.environment_configuration.transit_gateway_routes
-#   transit_gateway_id      = data.aws_ec2_transit_gateway.moj_tgw.id
-# }
-
 module "transit_gateway_routes" {
-  for_each = local.route_table_ids_map
+  for_each = toset(module.vpc.private_route_table_ids)
 
   source = "./modules/routes"
 
