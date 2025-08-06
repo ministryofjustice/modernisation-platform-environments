223203cec269154e30cc3d06274ea8ae9b2edaaa
matt heery <matt.heery@justice.gov.uk>
2025-08-06 15:09:21 +0100

    feat: add bucket for udf and lambda function for udf

---
+++ terraform/environments/electronic-monitoring-data/h3_utf_lambda.tf
@@ -0,0 +1,16 @@
+module "h3_lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "h3-udf"
+  description   = "Athena udf for Uber h3 hexes"
+  handler       = "com.aws.athena.udf.h3.H3AthenaHandler"
+  runtime       = "java11"
+  architectures	= ["x86_64"]
+  create_package = false
+  memory_size   = 4096
+
+  s3_existing_package = {
+    bucket = module.s3-lambda-store-bucket.bucket.id
+    key    = "h3_uf/binary/aws-h3-athena-udf-1.0-SNAPSHOT.jar"
+  }
+}
