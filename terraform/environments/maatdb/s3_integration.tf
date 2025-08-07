f1e7f21af9f4b3a8f1897fbe2dcc7cfcb8d70ea7
mikereiddigital <mike.reid@justice.gov.uk>
2025-08-07 08:47:52 +0100

    issue with dynamic statement corrupting policy

--- terraform/environments/maatdb/s3_integration.tf
+++ terraform/environments/maatdb/s3_integration.tf
@@ -163,33 +163,33 @@ data "aws_iam_policy_document" "bucket_policy" {
     }
   }
 
-  dynamic "statement" {
-    for_each = (
-      local.build_transfer && length(aws_iam_role.transfer_role) > 0 && each.key == "inbound" ? [1] : []
-    )
+  # dynamic "statement" {
+  #   for_each = (
+  #     local.build_transfer && length(aws_iam_role.transfer_role) > 0 && each.key == "inbound" ? [1] : []
+  #   )
 
-    content {
-      sid    = "AllowTransferUserAccess"
-      effect = "Allow"
+  #   content {
+  #     sid    = "AllowTransferUserAccess"
+  #     effect = "Allow"
 
-      principals {
-        type        = "AWS"
-        identifiers = [aws_iam_role.transfer_role[0].arn]
-      }
+  #     principals {
+  #       type        = "AWS"
+  #       identifiers = [aws_iam_role.transfer_role[0].arn]
+  #     }
 
-      actions = [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject",
-        "s3:ListBucket"
-      ]
+  #     actions = [
+  #       "s3:GetObject",
+  #       "s3:PutObject",
+  #       "s3:DeleteObject",
+  #       "s3:ListBucket"
+  #     ]
 
-      resources = [
-        each.value.bucket.arn,
-        "${each.value.bucket.arn}/*"
-      ]
-    }
-  }
+  #     resources = [
+  #       each.value.bucket.arn,
+  #       "${each.value.bucket.arn}/*"
+  #     ]
+  #   }
+  # }
 
 
 }
