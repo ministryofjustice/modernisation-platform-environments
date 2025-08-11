2674ef0b933ee8a143ab8b4e5db12ccc6178e9fc
mnasr-moj <mohamed.nasr@digital.justice.gov.uk>
2025-08-11 11:09:44 +0100

    updating lambda timeout

--- terraform/environments/laa-enterprise-service-bus/cwa_extract_lambda.tf
+++ terraform/environments/laa-enterprise-service-bus/cwa_extract_lambda.tf
@@ -67,7 +67,7 @@ resource "aws_lambda_function" "cwa_extract" {
   handler          = "lambda_function.lambda_handler"
   filename         = "lambda/cwa_extract_lambda/cwa_lambda.zip"
   source_code_hash = filebase64sha256("lambda/cwa_extract_lambda/cwa_lambda.zip")
-  timeout          = 900
+  timeout          = 300
   memory_size      = 128
   runtime          = "python3.10"
 
