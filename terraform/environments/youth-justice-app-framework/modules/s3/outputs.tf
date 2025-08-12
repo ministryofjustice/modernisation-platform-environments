a32c5007f0cf8aa79dfccbfb9797fed94855fa08
David Seekins <david.seekins@necsws.com>
2025-08-12 10:39:36 +0100

    With outut description.

--- terraform/environments/youth-justice-app-framework/modules/s3/outputs.tf
+++ terraform/environments/youth-justice-app-framework/modules/s3/outputs.tf
@@ -1,4 +1,5 @@
 output "aws_s3_bucket_arn" {
+  description = "Map of bucket names (as specified, without the enviroment prefix) to the ARN of the created buckets."
   value = {for k,v in aws_s3_bucket.default:
     trimprefix(k,"${local.environment_name}-") =>
        aws_s3_bucket.default.*.arn
