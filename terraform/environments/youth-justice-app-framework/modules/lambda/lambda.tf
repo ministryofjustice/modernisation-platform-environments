2bd75007c6457109a065f8b40518f2cd78d86620
Greg <greg.whiting@necsws.com>
2025-08-12 13:12:26 +0100

    testing new ecr

--- terraform/environments/youth-justice-app-framework/modules/lambda/lambda.tf
+++ terraform/environments/youth-justice-app-framework/modules/lambda/lambda.tf
@@ -42,6 +42,7 @@ resource "aws_lambda_function" "main" {
 }
 
 resource "aws_cloudwatch_log_group" "log_group" {
+  #checkov:skip=CKV_AWS_158: "Ensure that CloudWatch Log Group is encrypted by KMS" - not required
   count             = var.lambda.log_group != null ? 1 : 0
   name_prefix       = var.lambda.log_group.name
   retention_in_days = 365
