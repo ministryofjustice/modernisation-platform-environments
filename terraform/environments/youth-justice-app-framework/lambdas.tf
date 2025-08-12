dead5ce8836137865ccb108715739f1987dd40b3
Greg <greg.whiting@necsws.com>
2025-08-12 10:42:09 +0100

    testing new ecr

--- terraform/environments/youth-justice-app-framework/lambdas.tf
+++ terraform/environments/youth-justice-app-framework/lambdas.tf
@@ -70,7 +70,7 @@ module "serverlessrepo-lambda-canary" {
   environment                     = local.environment
   lambda_role                     = local.serverlessrepo-lambda-canary-role
   lambda                          = local.serverlessrepo-lambda-canary
-  cloudwatch_log_group_kms_key_id = module.kms.kms_key_id
+  cloudwatch_log_group_kms_key_id = module.kms.key_id
 }
 
 #ESB to Int load balancer
