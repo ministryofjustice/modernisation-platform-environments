dfe885bb40f856ed0752c9d5e0a1dfdf9c814b00
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-08-13 13:27:34 +0100

    add missing param

--- terraform/environments/delius-mis/locals_development.tf
+++ terraform/environments/delius-mis/locals_development.tf
@@ -353,7 +353,9 @@ locals {
     }
   })
 
-  dfi_report_bucket_config = {}
+  dfi_report_bucket_config = {
+    bucket_policy_enabled = true
+  }
 
   fsx_config_dev = {
     storage_capacity     = 100
