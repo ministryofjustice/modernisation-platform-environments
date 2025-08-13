c4aa09bdc155519ec40b4bfab8d2b6d59dc6571a
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-08-13 10:43:25 +0100

    CC-3794: revert back to tls

--- terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_db-nlb.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_db-nlb.tf
@@ -41,7 +41,9 @@ resource "aws_lb_target_group_attachment" "ebsdb" {
 resource "aws_lb_listener" "ebsdbnlb_listener" {
   load_balancer_arn = aws_lb.ebsdb_nlb.id
   port              = 443
-  protocol          = "TCP"
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
+  certificate_arn   = aws_acm_certificate_validation.external.certificate_arn
   default_action {
     target_group_arn = aws_lb_target_group.ebsdb_nlb.id
     type             = "forward"
