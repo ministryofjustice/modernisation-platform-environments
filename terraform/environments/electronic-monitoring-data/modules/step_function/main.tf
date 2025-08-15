e297747ea72ef5818d76893f833b3177f859af5e
Jono Basford <jono.basford@justice.gov.uk>
2025-08-15 10:49:10 +0100

    Split log permissons statements

--- terraform/environments/electronic-monitoring-data/modules/step_function/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/step_function/main.tf
@@ -58,20 +58,26 @@ data "aws_iam_policy_document" "step_function_base_permissions" {
   statement {
     effect = "Allow"
     actions = [
-      "logs:CreateLogDelivery",
       "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:PutDestination",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogDelivery",
       "logs:GetLogDelivery",
       "logs:UpdateLogDelivery",
       "logs:DeleteLogDelivery",
       "logs:ListLogDeliveries",
       "logs:PutResourcePolicy",
       "logs:DescribeResourcePolicies",
-      "logs:DescribeLogGroups",
-      # "logs:PutDestination",
-      # "logs:PutDestinationPolicy",
-      "logs:PutLogEvents"
+      "logs:PutDestinationPolicy",
     ]
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"]
+    resources = ["*"]
   }
 }
 
