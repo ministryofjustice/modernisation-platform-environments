ebf82b55e8df388618fbd4535455c93d0b2fa375
ttipler <thomas.tipler@necsws.com>
2025-08-15 09:24:07 +0100

    add sbom s3 bucket

--- terraform/environments/youth-justice-app-framework/s3.tf
+++ terraform/environments/youth-justice-app-framework/s3.tf
@@ -29,7 +29,7 @@ module "s3" {
 
   archive_bucket_name = ["s3-bucket-access-logging", "redshift-yjb-reporting", "tf-webops-config-service", "tableau-alb-logs", "yjaf-ext-external-logs",
     "yjaf-int-internal-logs", "cloudfront-logs", "cloudtrail-logs", "guardduty-to-fallanx-archive", "tableau-backups",
-    "aws-glue-assets", "cloudtrail-logs"
+    "aws-glue-assets", "cloudtrail-logs", "sbom-application"
   ]
 
   transfer_bucket_name = ["bands", "bedunlock", "cmm", "cms", "incident", "mis", "reporting", "yjsm-artefact", "yjsm", "transfer",
