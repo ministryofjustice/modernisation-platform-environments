ae4eeb1a6dc20448e75a132b442fe96b2c78ebd7
Fani Foteva <fani.foteva@digital.justice.gov.uk>
2025-08-19 00:05:19 +0100

    fix error

--- terraform/environments/ccms-ebs/ssogen-ec2-sg.tf
+++ terraform/environments/ccms-ebs/ssogen-ec2-sg.tf
@@ -74,13 +74,12 @@ resource "aws_security_group" "ssogen_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow internal communication within VPC
   egress {
-    description = "Allow all outbound to internal network"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/8"]
+  description = "allow yum/http"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(local.tags, {
