8d7055e966714ef69523a042960e256def0b0b03
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-08-19 12:17:43 +0100

    -

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -65,7 +65,8 @@ locals {
         })
         user_data_cloud_init = merge(local.ec2_autoscaling_groups.web.user_data_cloud_init, {
           args = merge(local.ec2_autoscaling_groups.web.user_data_cloud_init.args, {
-            branch = "TM-1518/nomis-web-ansible-fix"
+            # branch = "TM-1518/nomis-web-ansible-fix" ## works...
+            branch = "f8ea577571c5ea360da9f7582f0ac1c11ae1268f" # 2025-06-26 tag_release_detail fix
           })
         })
         tags = merge(local.ec2_autoscaling_groups.web.tags, {
