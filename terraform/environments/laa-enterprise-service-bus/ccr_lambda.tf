3500cf71bf139cd4b827061b942c2534941383ab
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-08-20 15:49:48 +0100

    fixing duplicate issue

--- terraform/environments/laa-enterprise-service-bus/ccr_lambda.tf
+++ terraform/environments/laa-enterprise-service-bus/ccr_lambda.tf
@@ -2,36 +2,36 @@
 ### Lambda SG
 ######################################
 
-resource "aws_security_group" "maat_provider_load_sg" {
-  name        = "${local.application_name_short}-${local.environment}-maat-provider-load-lambda-security-group"
-  description = "MAAT Provider Lambda Security Group"
+resource "aws_security_group" "ccr_provider_load_sg" {
+  name        = "${local.application_name_short}-${local.environment}-ccr-provider-load-lambda-security-group"
+  description = "CCR Provider Lambda Security Group"
   vpc_id      = data.aws_vpc.shared.id
 
   revoke_rules_on_delete = true
 
   tags = merge(
     local.tags,
-    { Name = "${local.application_name_short}-${local.environment}-maat-provider-load-lambda-security-group" }
+    { Name = "${local.application_name_short}-${local.environment}-ccr-provider-load-lambda-security-group" }
   )
 }
 
-resource "aws_security_group_rule" "maat_provider_load_egress_oracle" {
+resource "aws_security_group_rule" "ccr_provider_load_egress_oracle" {
   type                     = "egress"
   from_port                = 1521
   to_port                  = 1521
   protocol                 = "tcp"
-  source_security_group_id = local.application_data.accounts[local.environment].maatdb_sg
-  security_group_id        = aws_security_group.maat_provider_load_sg.id
-  description              = "Outbound 1521 Access to MAAT DB"
+  cidr_blocks              = [local.application_data.accounts[local.environment].cloud_platform_cidr]
+  security_group_id        = aws_security_group.ccr_provider_load_sg.id
+  description              = "Outbound 1521 Access to Cloud Platform"
 }
 
-resource "aws_security_group_rule" "maat_provider_load_egress_https_sm" {
+resource "aws_security_group_rule" "ccr_provider_load_egress_https_sm" {
   type                     = "egress"
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
   source_security_group_id = local.application_data.accounts[local.environment].vpc_endpoint_sg
-  security_group_id        = aws_security_group.maat_provider_load_sg.id
+  security_group_id        = aws_security_group.ccr_provider_load_sg.id
   description              = "Outbound 443 to LAA VPC Endpoint SG"
 }
 
@@ -39,11 +39,11 @@ resource "aws_security_group_rule" "maat_provider_load_egress_https_sm" {
 ### Lambda Resources
 ######################################
 
-resource "aws_lambda_function" "maat_provider_load" {
+resource "aws_lambda_function" "ccr_provider_load" {
 
-  description      = "Connect to MAAT DB"
-  function_name    = "maat_provider_load_function"
-  role             = aws_iam_role.maat_provider_load_role.arn
+  description      = "Connect to ccr DB"
+  function_name    = "ccr_provider_load_function"
+  role             = aws_iam_role.ccr_provider_load_role.arn
   handler          = "lambda_function.lambda_handler"
   filename         = "lambda/provider_load_lambda/provider_load_package.zip"
   source_code_hash = filebase64sha256("lambda/provider_load_lambda/provider_load_package.zip")
@@ -57,15 +57,15 @@ resource "aws_lambda_function" "maat_provider_load" {
   ]
 
   vpc_config {
-    security_group_ids = [aws_security_group.maat_provider_load_sg.id]
+    security_group_ids = [aws_security_group.ccr_provider_load_sg.id]
     subnet_ids         = [data.aws_subnet.data_subnets_a.id]
   }
   
 
   environment {
     variables = {
-      DB_SECRET_NAME    = aws_secretsmanager_secret.maat_db_mp_credentials.name
-      PROCEDURE_SECRET_NAME = aws_secretsmanager_secret.maat_procedures_config.name
+      DB_SECRET_NAME    = aws_secretsmanager_secret.ccr_db_mp_credentials.name
+      PROCEDURE_SECRET_NAME = aws_secretsmanager_secret.ccr_procedures_config.name
       LD_LIBRARY_PATH   = "/opt/instantclient_12_2_linux"
       ORACLE_HOME       = "/opt/instantclient_12_2_linux"
     }
@@ -73,12 +73,12 @@ resource "aws_lambda_function" "maat_provider_load" {
 
   tags = merge(
     local.tags,
-    { Name = "${local.application_name_short}-${local.environment}-maat-provider-load" }
+    { Name = "${local.application_name_short}-${local.environment}-ccr-provider-load" }
   )
 }
 
-resource "aws_lambda_event_source_mapping" "maat_provider_q_trigger" {
-  event_source_arn = aws_sqs_queue.maat_provider_q.arn
-  function_name    = aws_lambda_function.maat_provider_load.arn
+resource "aws_lambda_event_source_mapping" "ccr_provider_q_trigger" {
+  event_source_arn = aws_sqs_queue.ccr_provider_q.arn
+  function_name    = aws_lambda_function.ccr_provider_load.arn
   batch_size       = 1
 }
