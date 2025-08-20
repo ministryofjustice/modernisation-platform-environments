dc302b601cd2edc87d0a397580d90bb8ab13e92a
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-08-20 11:43:01 +0100

    updating step function name

--- terraform/environments/laa-enterprise-service-bus/sfn.tf
+++ terraform/environments/laa-enterprise-service-bus/sfn.tf
@@ -1,5 +1,5 @@
 resource "aws_sfn_state_machine" "sfn_state_machine" {
-  name     = "my-state-machine"
+  name     = "cwa-step-function"
   role_arn = aws_iam_role.step_function_role.arn
 
   definition = jsonencode({
