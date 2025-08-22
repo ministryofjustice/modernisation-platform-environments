f712d06bab750eb9defa15059d9652e3bc0806c3
suma.challa <suma.challa@justice.gov.uk>
2025-08-22 10:57:16 +0100

    HUB-124 added latest zip files

--- terraform/environments/laa-enterprise-service-bus/cwa_extract_lambda.tf
+++ terraform/environments/laa-enterprise-service-bus/cwa_extract_lambda.tf
@@ -132,8 +132,6 @@ resource "aws_lambda_function" "cwa_extract_lambda" {
   environment {
     variables = {
       PROCEDURES_CONFIG = aws_secretsmanager_secret.cwa_procedures_config.name
-      # TARGET_BUCKET     = aws_s3_bucket.data.bucket
-      # SNS_TOPIC         = aws_sns_topic.priority_p1.arn
       DB_SECRET_NAME    = aws_secretsmanager_secret.cwa_db_secret.name
       LD_LIBRARY_PATH   = "/opt/instantclient_12_2_linux"
       ORACLE_HOME       = "/opt/instantclient_12_2_linux"
