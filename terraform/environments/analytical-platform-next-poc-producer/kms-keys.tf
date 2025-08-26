fc1a80b8d30b1a8b855829bfa807d22a299cf590
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-08-26 20:37:40 +0000

    refactor: comment out unused KMS key modules for clarity
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-next-poc-producer/kms-keys.tf
+++ terraform/environments/analytical-platform-next-poc-producer/kms-keys.tf
@@ -1,68 +1,68 @@
-module "glue_catalog_kms_key" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+# module "glue_catalog_kms_key" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+#   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/kms/aws"
-  version = "4.0.0"
+#   source  = "terraform-aws-modules/kms/aws"
+#   version = "4.0.0"
 
-  aliases               = ["glue/catalog"]
-  enable_default_policy = true
+#   aliases               = ["glue/catalog"]
+#   enable_default_policy = true
 
-  deletion_window_in_days = 7
-}
+#   deletion_window_in_days = 7
+# }
 
-module "glue_connections_kms_key" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+# module "glue_connections_kms_key" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+#   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/kms/aws"
-  version = "4.0.0"
+#   source  = "terraform-aws-modules/kms/aws"
+#   version = "4.0.0"
 
-  aliases               = ["glue/connections"]
-  enable_default_policy = true
+#   aliases               = ["glue/connections"]
+#   enable_default_policy = true
 
-  deletion_window_in_days = 7
-}
+#   deletion_window_in_days = 7
+# }
 
-module "glue_crawler_logs_kms_key" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+# module "glue_crawler_logs_kms_key" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+#   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/kms/aws"
-  version = "4.0.0"
+#   source  = "terraform-aws-modules/kms/aws"
+#   version = "4.0.0"
 
-  aliases               = ["glue/crawler/logs"]
-  enable_default_policy = true
-  key_statements = [
-    {
-      sid    = "AllowCloudWatchLogs"
-      effect = "Allow"
-      actions = [
-        "kms:Decrypt*",
-        "kms:Describe*",
-        "kms:Encrypt*",
-        "kms:GenerateDataKey*",
-        "kms:ReEncrypt*"
-      ]
-      resources = ["*"]
-      principals = [
-        {
-          type        = "Service"
-          identifiers = ["logs.${data.aws_region.current.region}.amazonaws.com"]
-        }
-      ]
-      conditions = [
-        {
-          test     = "ArnEquals"
-          variable = "kms:EncryptionContext:aws:logs:arn"
-          values   = ["arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws-glue/*"]
-        }
-      ]
-    }
-  ]
+#   aliases               = ["glue/crawler/logs"]
+#   enable_default_policy = true
+#   key_statements = [
+#     {
+#       sid    = "AllowCloudWatchLogs"
+#       effect = "Allow"
+#       actions = [
+#         "kms:Decrypt*",
+#         "kms:Describe*",
+#         "kms:Encrypt*",
+#         "kms:GenerateDataKey*",
+#         "kms:ReEncrypt*"
+#       ]
+#       resources = ["*"]
+#       principals = [
+#         {
+#           type        = "Service"
+#           identifiers = ["logs.${data.aws_region.current.region}.amazonaws.com"]
+#         }
+#       ]
+#       conditions = [
+#         {
+#           test     = "ArnEquals"
+#           variable = "kms:EncryptionContext:aws:logs:arn"
+#           values   = ["arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws-glue/*"]
+#         }
+#       ]
+#     }
+#   ]
 
-  deletion_window_in_days = 7
-}
+#   deletion_window_in_days = 7
+# }
 
 module "s3_mojap_next_poc_athena_query_kms_key" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
