e540fdf3bdaf7a3c5ece2d5d5074d5d64043fe3f
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-08-27 15:19:49 +0100

    add s3 bucket, route53 record and classic load balancer

--- terraform/environments/delius-mis/modules/mis_environment/s3.tf
+++ terraform/environments/delius-mis/modules/mis_environment/s3.tf
@@ -80,3 +80,80 @@ data "aws_iam_policy_document" "dfi_report_bucket_policy" {
     }
   }
 }
+
+module "s3_lb_logs_bucket" {
+  count  = var.lb_config != null ? 1 : 0
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v8.2.2"
+  providers = {
+    aws                    = aws
+    aws.bucket-replication = aws
+  }
+
+  bucket_prefix      = "delius-mis-${var.env_name}-lb-logs-"
+  versioning_enabled = false
+  bucket_policy      = var.lb_config.bucket_policy_enabled ? [data.aws_iam_policy_document.s3_lb_logs_bucket_policy[0].json] : []
+  force_destroy      = true
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+        }
+      ]
+
+      expiration = {
+        days = 365
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 365
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
+data "aws_iam_policy_document" "s3_lb_logs_bucket_policy" {
+  count = var.lb_config != null && var.lb_config.bucket_policy_enabled ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "${module.s3-lb-logs-bucket[0].bucket.arn}/*",
+      module.s3-lb-logs-bucket[0].bucket.arn
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "aws:PrincipalOrgPaths"
+      values   = ["${data.aws_organizations_organization.root_account.id}/*/${var.platform_vars.environment_management.modernisation_platform_organisation_unit_id}/*"]
+    }
+  }
+}
