169cd95bfdf41352dcc55aa1ad13ba1fba1e00b8
nbuckingham72 <nick.buckingham@colt.net>
2025-08-28 15:45:06 +0100

    Update_280825_2

--- terraform/environments/ppud/alb_external.tf
+++ terraform/environments/ppud/alb_external.tf
@@ -123,7 +123,7 @@ resource "aws_lb_listener" "WAM-Front-End-Preprod" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.WAM-Target-Group-Preprod[0].arn
+    target_group_arn = aws_lb_target_group.WAM-Target-Group-Preprod-2[0].arn
   }
 }
 
@@ -174,7 +174,7 @@ resource "aws_lb_target_group_attachment" "WAM-Portal-development" {
 
 resource "aws_lb_target_group_attachment" "WAM-Portal-preproduction" {
   count            = local.is-preproduction == true ? 1 : 0
-  target_group_arn = aws_lb_target_group.WAM-Target-Group-Preprod[0].arn
+  target_group_arn = aws_lb_target_group.WAM-Target-Group-Preprod-2[0].arn
   target_id        = aws_instance.s618358rgvw201[0].id
   port             = 80
 }
