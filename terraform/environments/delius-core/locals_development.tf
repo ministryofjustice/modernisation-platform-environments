a232731874372f3c9dac78e39025b9a6ade0c68c
David Teeling <david.teeling1@justice.gov.uk>
2025-08-29 15:28:23 +0100

    Weblogic task revision to v11

--- terraform/environments/delius-core/locals_development.tf
+++ terraform/environments/delius-core/locals_development.tf
@@ -83,7 +83,7 @@ locals {
       container_port           = 8080
       container_memory         = 4096
       container_cpu            = 2048
-      task_definition_revision = 9
+      task_definition_revision = 11
     }
 
     weblogic_eis = {
