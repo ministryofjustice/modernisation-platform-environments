082fed9ae5bdedc423a75a156e998af5f46ac285
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-02 15:03:39 +0000

    @michaeljcollinsuk take the wheel
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-next-poc-hub/glue-databases.tf
+++ terraform/environments/analytical-platform-next-poc-hub/glue-databases.tf
@@ -1,14 +1,14 @@
 # This registers the Glue Catalog Database that is shared via RAM
-resource "aws_glue_catalog_database" "producer_resource_link" {
-  name = "${local.producer_account_id}_${local.producer_database}"
+# resource "aws_glue_catalog_database" "producer_resource_link" {
+#   name = "${local.producer_account_id}_${local.producer_database}"
 
-  target_database {
-    catalog_id    = local.producer_account_id
-    database_name = local.producer_database
-    region        = data.aws_region.current.region
-  }
+#   target_database {
+#     catalog_id    = local.producer_account_id
+#     database_name = local.producer_database
+#     region        = data.aws_region.current.region
+#   }
 
-  lifecycle {
-    ignore_changes = [description]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [description]
+#   }
+# }
--- terraform/environments/analytical-platform-next-poc-hub/lakeformation-permissions.tf
+++ terraform/environments/analytical-platform-next-poc-hub/lakeformation-permissions.tf
@@ -1,34 +1,34 @@
-# Required Lake Formation permissions to query shared Glue database
-resource "aws_lakeformation_permissions" "jacobwoffenden_database_resource_link" {
-  principal                     = module.user_jacobwoffenden_iam_role.arn
-  permissions                   = ["DESCRIBE"]
-  permissions_with_grant_option = ["DESCRIBE"]
+# # Required Lake Formation permissions to query shared Glue database
+# resource "aws_lakeformation_permissions" "jacobwoffenden_database_resource_link" {
+#   principal                     = module.user_jacobwoffenden_iam_role.arn
+#   permissions                   = ["DESCRIBE"]
+#   permissions_with_grant_option = ["DESCRIBE"]
 
-  database {
-    catalog_id = data.aws_caller_identity.current.account_id
-    name       = aws_glue_catalog_database.producer_resource_link.name
-  }
-}
+#   database {
+#     catalog_id = data.aws_caller_identity.current.account_id
+#     name       = aws_glue_catalog_database.producer_resource_link.name
+#   }
+# }
 
-resource "aws_lakeformation_permissions" "jacobwoffenden_database" {
-  principal                     = module.user_jacobwoffenden_iam_role.arn
-  permissions                   = ["DESCRIBE"]
-  permissions_with_grant_option = ["DESCRIBE"]
+# resource "aws_lakeformation_permissions" "jacobwoffenden_database" {
+#   principal                     = module.user_jacobwoffenden_iam_role.arn
+#   permissions                   = ["DESCRIBE"]
+#   permissions_with_grant_option = ["DESCRIBE"]
 
-  database {
-    catalog_id = local.producer_account_id
-    name       = local.producer_database
-  }
-}
+#   database {
+#     catalog_id = local.producer_account_id
+#     name       = local.producer_database
+#   }
+# }
 
-resource "aws_lakeformation_permissions" "jacobwoffenden_database_tables" {
-  principal                     = module.user_jacobwoffenden_iam_role.arn
-  permissions                   = ["SELECT", "DESCRIBE"]
-  permissions_with_grant_option = ["SELECT", "DESCRIBE"]
+# resource "aws_lakeformation_permissions" "jacobwoffenden_database_tables" {
+#   principal                     = module.user_jacobwoffenden_iam_role.arn
+#   permissions                   = ["SELECT", "DESCRIBE"]
+#   permissions_with_grant_option = ["SELECT", "DESCRIBE"]
 
-  table {
-    catalog_id    = local.producer_account_id
-    database_name = local.producer_database
-    wildcard      = true
-  }
-}
+#   table {
+#     catalog_id    = local.producer_account_id
+#     database_name = local.producer_database
+#     wildcard      = true
+#   }
+# }
