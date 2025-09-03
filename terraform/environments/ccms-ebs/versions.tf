0ae24d0b8cdcea70e87ad07e85190876de542e75
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-09-03 10:05:57 +0100

    fixing the version.tf

--- terraform/environments/ccms-ebs/versions.tf
+++ terraform/environments/ccms-ebs/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
     aws = {
       version = "~> 5.0, != 5.86.0, != 5.99.0"
       source  = "hashicorp/aws"
@@ -8,13 +12,13 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.4"
-    }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.1.0"
     }
   }
   required_version = "~> 1.10"
