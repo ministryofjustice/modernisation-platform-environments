3be2a4f6499ba6d1ee50e4543c920cf33207f1cb
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-04 08:05:27 +0000

    refactor: remove retired Glue data catalog encryption settings and commented-out IAM policy statements
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-next-poc-producer/glue-crawlers.tf
+++ terraform/environments/analytical-platform-next-poc-producer/glue-crawlers.tf
@@ -2,7 +2,6 @@ resource "aws_glue_crawler" "data" {
   name          = "data"
   database_name = aws_glue_catalog_database.moj.name
   role          = module.glue_crawler_iam_role.name
-  #   security_configuration = aws_glue_security_configuration.main.name
 
   s3_target {
     path = "s3://${module.mojap_next_poc_data_s3_bucket.s3_bucket_id}/${local.data_folder_prefix}"
--- terraform/environments/analytical-platform-next-poc-producer/iam-policies.tf
+++ terraform/environments/analytical-platform-next-poc-producer/iam-policies.tf
@@ -1,25 +1,4 @@
 data "aws_iam_policy_document" "glue_crawler" {
-  #   statement {
-  #     # This statement is a workaround for a bug I found when using a KMS key in 'aws_glue_security_configuration.main.encryption_configuration.cloudwatch_encryption'
-  #     sid       = "AllowCloudWatchLogsActions"
-  #     effect    = "Allow"
-  #     actions   = ["logs:AssociateKmsKey"]
-  #     resources = ["arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws-glue/*"]
-  #   }
-  #   statement {
-  #     sid    = "AllowGlueKMSActions"
-  #     effect = "Allow"
-  #     actions = [
-  #       "kms:Decrypt",
-  #       "kms:Encrypt",
-  #       "kms:GenerateDataKey"
-  #     ]
-  #     resources = [
-  #       module.glue_catalog_kms_key.key_arn,
-  #       module.glue_connections_kms_key.key_arn, # TODO (@jacobwoffenden): Is this needed in the Glue crawler?
-  #       module.glue_crawler_logs_kms_key.key_arn
-  #     ]
-  #   }
   statement {
     sid       = "AllowS3KMSActions"
     effect    = "Allow"
