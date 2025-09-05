1804f1f4b83ec749d9edb281313a472bae49f7c1
Jono Basford <jono.basford@justice.gov.uk>
2025-09-05 17:33:32 +0100

    Moved IAM role and Event rule and target to the specific tf files

--- terraform/environments/electronic-monitoring-data/cloudwatch-event-rules.tf
+++ terraform/environments/electronic-monitoring-data/cloudwatch-event-rules.tf
@@ -2,3 +2,19 @@ resource "aws_cloudwatch_event_rule" "definition_update" {
   name                = "definition-update"
   schedule_expression = "cron(15 6 * * ? *)" # 06:15 every day
 }
+
+resource "aws_cloudwatch_event_rule" "dms_task_completed" {
+  count       = local.is-production || local.is-development ? 1 : 0
+  name        = "dms_validation_trigger_rule"
+  description = "Triggeres DMS validation Step Function"
+
+  event_pattern = jsonencode({
+    "source" : ["aws.dms"],
+    "detail-type" : ["DMS Replication Task State Change"],
+    "detail" : {
+      "eventId" : ["DMS-EVENT-0079"],
+      "eventType" : ["REPLICATION_TASK_STOPPED"]
+      "detailMessage" : ["Stop Reason FULL_LOAD_ONLY_FINISHED"]
+    }
+  })
+}
--- terraform/environments/electronic-monitoring-data/cloudwatch-event-targets.tf
+++ terraform/environments/electronic-monitoring-data/cloudwatch-event-targets.tf
@@ -3,3 +3,12 @@ resource "aws_cloudwatch_event_target" "definition_update" {
   target_id = "definition-update"
   arn       = module.virus_scan_definition_upload.lambda_function_arn
 }
+
+
+resource "aws_cloudwatch_event_target" "dms_validation_step_function_trigger" {
+  count      = local.is-production || local.is-development ? 1 : 0
+  rule       = aws_cloudwatch_event_rule.dms_task_completed.name
+  arn        = module.dms_validation_step_function[0].arn
+  role_arn   = aws_iam_role.dms_validation_event_bridge_invoke_sfn_role.arn
+  input_path = "$"
+}
