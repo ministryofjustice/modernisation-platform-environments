e39d0b9443829fd53464d860af1204f1b3c8c6d9
Seb Norris <sebastian.j.norris@gmail.com>
2025-09-09 13:29:24 +0100

    remove everythign

--- terraform/environments/vcms/locals.tf
+++ terraform/environments/vcms/locals.tf
@@ -33,43 +33,43 @@ locals {
   bastion_config = {}
   image_tag      = "initial-16447252449-1"
   app_port       = 80
-  internal_security_group_cidrs = distinct(flatten([
-    module.ip_addresses.moj_cidrs.trusted_moj_digital_staff_public,
-    module.ip_addresses.moj_cidrs.trusted_moj_enduser_internal,
-    module.ip_addresses.moj_cidrs.trusted_mojo_public,
-    module.ip_addresses.moj_cidr.ark_dc_external_internet,
-    module.ip_addresses.moj_cidr.vodafone_dia_networks,
-    module.ip_addresses.moj_cidr.palo_alto_prisma_access_corporate,
-    module.ip_addresses.moj_cidr.mojo_azure_landing_zone_egress,
-    [
-      # Route53 Healthcheck Access Cidrs
-      # London Region not support yet, so metrics are not yet publised, can be enabled at later stage for Route53 endpoint monitor
-      "15.177.0.0/18",     # GLOBAL Region
-      "54.251.31.128/26",  # ap-southeast-1 Region
-      "54.255.254.192/26", # ap-southeast-1 Region
-      "176.34.159.192/26", # eu-west-1 Region
-      "54.228.16.0/26",    # eu-west-1 Region
-      "107.23.255.0/26",   # us-east-1 Region
-      "54.243.31.192/26"   # us-east-1 Region
-    ]
-  ]))
+  # internal_security_group_cidrs = distinct(flatten([
+  #   module.ip_addresses.moj_cidrs.trusted_moj_digital_staff_public,
+  #   module.ip_addresses.moj_cidrs.trusted_moj_enduser_internal,
+  #   module.ip_addresses.moj_cidrs.trusted_mojo_public,
+  #   module.ip_addresses.moj_cidr.ark_dc_external_internet,
+  #   module.ip_addresses.moj_cidr.vodafone_dia_networks,
+  #   module.ip_addresses.moj_cidr.palo_alto_prisma_access_corporate,
+  #   module.ip_addresses.moj_cidr.mojo_azure_landing_zone_egress,
+  #   [
+  #     # Route53 Healthcheck Access Cidrs
+  #     # London Region not support yet, so metrics are not yet publised, can be enabled at later stage for Route53 endpoint monitor
+  #     "15.177.0.0/18",     # GLOBAL Region
+  #     "54.251.31.128/26",  # ap-southeast-1 Region
+  #     "54.255.254.192/26", # ap-southeast-1 Region
+  #     "176.34.159.192/26", # eu-west-1 Region
+  #     "54.228.16.0/26",    # eu-west-1 Region
+  #     "107.23.255.0/26",   # us-east-1 Region
+  #     "54.243.31.192/26"   # us-east-1 Region
+  #   ]
+  # ]))
   ipv6_cidr_blocks = []
 
-  domain_types = { for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-    }
-  }
+  #  domain_types = { for dvo in aws_acm_certificate.external.domain_validation_options : dvo.domain_name => {
+  #  name   = dvo.resource_record_name
+  #  record = dvo.resource_record_value
+  #  type   = dvo.resource_record_type
+  #  }
+  #}
 
   domain                  = local.is-production ? "vcms.probation.service.justice.gov.uk" : "modernisation-platform.service.justice.gov.uk"
-  domain_name_main        = [for k, v in local.domain_types : v.name if k == local.domain]
-  domain_record_main      = [for k, v in local.domain_types : v.record if k == local.domain]
-  domain_type_main        = [for k, v in local.domain_types : v.type if k == local.domain]
-  domain_name_sub         = [for k, v in local.domain_types : v.name if k == local.app_url]
-  domain_record_sub       = [for k, v in local.domain_types : v.record if k == local.app_url]
-  domain_type_sub         = [for k, v in local.domain_types : v.type if k == local.app_url]
-  validation_record_fqdns = local.is-development ? [local.domain_name_main[0], local.domain_name_sub[0]] : [local.domain_name_main[0], local.domain_name_sub[0]]
+  #domain_name_main        = [for k, v in local.domain_types : v.name if k == local.domain]
+  #domain_record_main      = [for k, v in local.domain_types : v.record if k == local.domain]
+  #domain_type_main        = [for k, v in local.domain_types : v.type if k == local.domain]
+  #domain_name_sub         = [for k, v in local.domain_types : v.name if k == local.app_url]
+  #domain_record_sub       = [for k, v in local.domain_types : v.record if k == local.app_url]
+  #domain_type_sub         = [for k, v in local.domain_types : v.type if k == local.app_url]
+  #validation_record_fqdns = local.is-development ? [local.domain_name_main[0], local.domain_name_sub[0]] : [local.domain_name_main[0], local.domain_name_sub[0]]
 
   app_url                       = "${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.${local.domain}"
   acm_subject_alternative_names = [local.app_url]
