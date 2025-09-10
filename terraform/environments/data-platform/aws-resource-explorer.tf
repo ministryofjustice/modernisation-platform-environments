579db895e023fca17c968d95a16c0a2d38fbcbd4
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-10 11:37:50 +0000

    Switch to default tags
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/aws-resource-explorer.tf
+++ terraform/environments/data-platform/aws-resource-explorer.tf
@@ -1,13 +1,9 @@
 resource "aws_resourceexplorer2_index" "eu_west_2" {
   type = "LOCAL"
-
-  tags = local.tags
 }
 
 resource "aws_resourceexplorer2_view" "eu_west_2" {
   name = "eu-west-2"
-
-  tags = local.tags
 
   depends_on = [aws_resourceexplorer2_index.eu_west_2]
 }
