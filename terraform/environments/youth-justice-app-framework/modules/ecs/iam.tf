fda4802886b6cd225ff0610d32689e6675fdfe23
Greg <greg.whiting@necsws.com>
2025-09-10 15:33:07 +0100

    initial commit for reducing ecs role permissions

--- terraform/environments/youth-justice-app-framework/modules/ecs/iam.tf
+++ terraform/environments/youth-justice-app-framework/modules/ecs/iam.tf
@@ -21,18 +21,6 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
 }
 
 #Todo refine this it is too permissive
-resource "aws_iam_role_policy_attachment" "ecs_task_secrets_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
-}
-
-#Todo refine this it is too permissive
-resource "aws_iam_role_policy_attachment" "ecs_task_s3_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-#Todo refine this it is too permissive
 resource "aws_iam_role_policy_attachment" "ecs_task_ses_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
