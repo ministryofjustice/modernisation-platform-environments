4151fcc779b71e6caeca0f920f72d8a75634b4de
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-11 14:00:46 +0000

    fix: update AWS provider version to 6.0 and required Terraform version to 1.0
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/versions.tf
+++ terraform/environments/data-platform/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 5.0, != 5.86.0"
+      version = "~> 6.0"
       source  = "hashicorp/aws"
     }
     http = {
@@ -9,5 +9,5 @@ terraform {
       source  = "hashicorp/http"
     }
   }
-  required_version = "~> 1.10"
+  required_version = "~> 1.0"
 }
