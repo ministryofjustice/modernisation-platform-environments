59a76b9e559e8345c62af76940535869f2f85281
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-12 15:01:33 +0000

    fix: update subnet values and security group configuration for workspaces web
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/laa-workspaces-web.tf
+++ terraform/environments/data-platform/laa-workspaces-web.tf
@@ -3,14 +3,14 @@
 data "aws_subnet" "private_aza" {
   filter {
     name   = "tag:Name"
-    values = ["platforms-test-general-private-eu-west-2a"]
+    values = ["${var.networking[0].business-unit}-${local.environment}-${var.networking[0].set}-private-eu-west-2a"]
   }
 }
 
 data "aws_subnet" "private_azc" {
   filter {
     name   = "tag:Name"
-    values = ["platforms-test-general-private-eu-west-2c"]
+    values = ["${var.networking[0].business-unit}-${local.environment}-${var.networking[0].set}-private-eu-west-2c"]
   }
 }
 
@@ -28,15 +28,8 @@ module "workspacesweb_security_group" {
 
   ingress_cidr_blocks = ["10.10.0.0/16"]
   ingress_rules       = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    }
-  ]
+  egress_cidr_blocks  = ["172.20.0.0/16"]
+  egress_rules        = ["https-443-tcp"]
 }
 
 ### NETWORK SETTINGS
@@ -50,6 +43,28 @@ resource "awscc_workspacesweb_network_settings" "main" {
   security_group_ids = [module.workspacesweb_security_group[0].security_group_id]
 }
 
+### IDENTITY PROVIDER
+
+/*
+
+This is currently commented out as the identity provider needs to be created to produce the ServiceProviderMetadata, which is upload to Entra ID
+which then produces the IDPMetadata which is required to create the identity provider
+ --- chicken and egg situation ---
+
+resource "awscc_workspacesweb_identity_provider" "main" {
+
+  count = local.environment == "test" ? 1 : 0
+
+  identity_provider_name = "laa-idp"
+  identity_provider_type = "SAML"
+  identity_provider_details = {
+    MetadataFile = "${path.module}/src/idp-metadata.xml"
+  }
+  portal_arn = awscc_workspacesweb_portal.main[0].portal_arn
+}
+
+*/
+
 
 ### WORKSPACES WEB PORTAL
 
@@ -59,4 +74,29 @@ resource "awscc_workspacesweb_portal" "main" {
 
   display_name         = "laa-workspaces-web"
   network_settings_arn = awscc_workspacesweb_network_settings.main[0].network_settings_arn
+  user_settings_arn    = awscc_workspacesweb_user_settings.main[0].user_settings_arn
+}
+
+### USER SETTINGS
+
+# Create a user settings configuration
+resource "awscc_workspacesweb_user_settings" "main" {
+
+  count = local.environment == "test" ? 1 : 0
+
+  # Required settings
+  copy_allowed     = "Enabled"
+  download_allowed = "Enabled"
+  paste_allowed    = "Enabled"
+  print_allowed    = "Disabled"
+  upload_allowed   = "Enabled"
+
+  # Optional settings
+  deep_link_allowed                  = "Enabled"
+  disconnect_timeout_in_minutes      = 60
+  idle_disconnect_timeout_in_minutes = 15
+
+  toolbar_configuration = {
+    hidden_toolbar_items = ["Microphone", "Webcam"] /* THESE NEED DOING MANUALLY 🤡 */
+  }
 }
