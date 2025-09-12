3467996fa8fbd18367b788cffe58826e3bd93738
Mark Butler <mark.butler@solirius.com>
2025-09-12 13:08:54 +0100

    Specify the preproduction launch template version to avoid the error collecting results issue.
    Revert launch_template to $latest

--- terraform/environments/tribunals/asg-shared.tf
+++ terraform/environments/tribunals/asg-shared.tf
@@ -275,7 +275,7 @@ resource "aws_instance" "tribunals_backup" {
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   launch_template {
     id      = aws_launch_template.tribunals-backup-lt.id
-    version = "4"
+    version = "$Latest"
   }
 
   ebs_optimized = true
