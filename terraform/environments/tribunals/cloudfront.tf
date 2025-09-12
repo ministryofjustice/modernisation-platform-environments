5765160e7a7021b1370aea8b3f97184bdad77107
Timi Olubusi <81569910+Tim97eng@users.noreply.github.com>
2025-09-12 17:53:29 +0100

    Revert "Rst 5550 nginx record migration (#12889)" (#12890)
    This reverts commit e179e7f9e71dcfd7434039ba7b776934d8f86664.

--- terraform/environments/tribunals/cloudfront.tf
+++ terraform/environments/tribunals/cloudfront.tf
@@ -15,13 +15,10 @@ resource "aws_cloudfront_distribution" "tribunals_distribution" {
   }
 
   aliases = local.is-production ? [
-  "*.decisions.tribunals.gov.uk",
-  "*.venues.tribunals.gov.uk",
-  "*.reports.tribunals.gov.uk",
-  "siac.tribunal.gov.uk"
-] : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
-
-
+    "*.decisions.tribunals.gov.uk",
+    "*.venues.tribunals.gov.uk",
+    "*.reports.tribunals.gov.uk"
+  ] : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   origin {
     domain_name = aws_lb.tribunals_lb.dns_name
     origin_id   = "tribunalsOrigin"
@@ -56,14 +53,6 @@ resource "aws_cloudfront_distribution" "tribunals_distribution" {
     min_ttl                = 0
     max_ttl                = 31536000
     smooth_streaming       = false
-
-    dynamic "function_association" {
-      for_each = local.is-development ? [] : [1]
-      content {
-        event_type   = "viewer-request"
-        function_arn = aws_cloudfront_function.redirect_function[0].arn
-      }
-    }
   }
 
   enabled         = true
@@ -97,7 +86,7 @@ resource "aws_acm_certificate" "cloudfront" {
   provider                  = aws.us-east-1
   domain_name               = local.is-production ? "*.decisions.tribunals.gov.uk" : "modernisation-platform.service.justice.gov.uk"
   validation_method         = "DNS"
-  subject_alternative_names = local.is-production ? ["*.venues.tribunals.gov.uk", "*.reports.tribunals.gov.uk", "siac.tribunals.gov.uk"] : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  subject_alternative_names = local.is-production ? ["*.venues.tribunals.gov.uk", "*.reports.tribunals.gov.uk"] : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   tags = {
     Environment = local.environment
   }
@@ -282,39 +271,4 @@ resource "aws_cloudfront_response_headers_policy" "security_headers_policy" {
       override   = true
     }
   }
-}
-
-resource "aws_cloudfront_function" "redirect_function" {
-  count = local.is-development ? 0 : 1
-  name    = "tribunals_redirect_function"
-  runtime = "cloudfront-js-2.0"
-  publish = true
-  code    = <<EOF
-  function handler(event) {
-    var request = event.request;
-    var host    = request.headers.host.value;
-    var uri     = request.uri;
-    // Redirect rules for siac.tribunals.gov.uk
-    if (host === "siac.tribunals.gov.uk") {
-      if (uri.toLowerCase() === "/outcomes2007onwards.htm") {
-        return {
-          statusCode: 301,
-          statusDescription: "Moved Permanently",
-          headers: {
-            "location": { "value": "https://siac.decisions.tribunals.gov.uk" }
-          }
-        };
-      }
-      return {
-        statusCode: 301,
-        statusDescription: "Moved Permanently",
-        headers: {
-          "location": { "value": "https://www.gov.uk/guidance/appeal-to-the-special-immigration-appeals-commission" }
-        }
-      };
-    }
-    // Default: Pass through to origin
-    return request;
-  }
-  EOF
 }
