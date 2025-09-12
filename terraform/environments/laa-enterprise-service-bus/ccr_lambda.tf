b57e2b8b90e31d94bdbe85ee0cbb728df62bb269
suma.challa <suma.challa@justice.gov.uk>
2025-09-12 10:29:00 +0100

    HUB-153 added latest lambda files with metrics, env variables

--- terraform/environments/laa-enterprise-service-bus/ccr_lambda.tf
+++ terraform/environments/laa-enterprise-service-bus/ccr_lambda.tf
@@ -69,7 +69,9 @@ resource "aws_lambda_function" "ccr_provider_load" {
       LD_LIBRARY_PATH        = "/opt/instantclient_12_2_linux"
       ORACLE_HOME            = "/opt/instantclient_12_2_linux"
       SERVICE_NAME           = "ccr-load-service"
-      NAMESPACE              = "CCRProviderLoadService"
+      NAMESPACE              = "HUB20-CCR-NS"
+      ENVIRONMENT            = local.environment
+      LOG_LEVEL              = "DEBUG"
       PURGE_LAMBDA_TIMESTAMP = aws_ssm_parameter.ccr_provider_load_timestamp.name
     }
   }
