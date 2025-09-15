0955d6ce34c2d49aa3a360a2a225adca51894d57
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2025-09-15 12:25:26 +0100

    description is added

--- terraform/environments/ccms-ebs/ccms-lambda.tf
+++ terraform/environments/ccms-ebs/ccms-lambda.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "lambda_security_group" {
 # hashicorp recommened Ingress rule of lambda_security_group
 resource "aws_vpc_security_group_ingress_rule" "lambda_ingress" {
   security_group_id = aws_security_group.lambda_security_group.id
-
+  description = "Allow FTP lambda inbound traffic"
   cidr_ipv4   = data.aws_vpc.shared.cidr_block
   from_port   = 1521
   ip_protocol = "tcp"
@@ -42,7 +42,7 @@ resource "aws_vpc_security_group_ingress_rule" "lambda_ingress" {
 # hashicorp recommened egress rule of lambda_security_group
 resource "aws_vpc_security_group_egress_rule" "lambda_egress" {
   security_group_id = aws_security_group.lambda_security_group.id
-
+  description = "Allow FTP lambdaall outbound traffic"
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "-1"
 }
