e8ff4729a5ded68b5b131060709fc41e72b49b75
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-09-15 10:08:02 +0100

    adding metric query

--- terraform/environments/laa-enterprise-service-bus/cloudwatch.tf
+++ terraform/environments/laa-enterprise-service-bus/cloudwatch.tf
@@ -13,14 +13,25 @@ resource "aws_cloudwatch_metric_alarm" "lambda_failures" {
 
   alarm_name          = "${each.value.servicename}-${local.environment}-InvocationFailureCount-Alarm"
   alarm_description   = "Alarm when ${each.value.servicename} lambda reports failures in ${local.environment}"
-  namespace           = each.value.namespace
   metric_name         = "InvocationFailureCount"
-  statistic           = "Sum"
   period              = 60
   evaluation_periods  = 1
   threshold           = 0
   comparison_operator = "GreaterThanThreshold"
   treat_missing_data  = "notBreaching"
+
+  metric_query {
+    id          = "m1"
+    label       = "AllFailureCounts"
+    return_data = true
+
+    metric {
+      namespace   = each.value.namespace
+      metric_name = "InvocationFailureCount"
+      stat        = "Sum"
+      period      = 60
+    }
+  }
 
   alarm_actions = [aws_sns_topic.hub2_alerts.arn]
 
