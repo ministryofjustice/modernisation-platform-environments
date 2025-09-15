100ce987ba7bafa0ff4ce30d42f4a57f0fb7fde7
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-09-15 10:32:27 +0100

    readding alarms

--- terraform/environments/laa-enterprise-service-bus/cloudwatch.tf
+++ terraform/environments/laa-enterprise-service-bus/cloudwatch.tf
@@ -7,6 +7,8 @@ resource "aws_cloudwatch_metric_alarm" "lambda_failures" {
     "cclf-load"         = { namespace = "HUB20-CCLF-NS",       servicename = "cclf-load-service" }
     "ccr-load"          = { namespace = "HUB20-CCR-NS",        servicename = "ccr-load-service" }
     "maat-load"         = { namespace = "HUB20-MAAT-NS",       servicename = "maat-load-service" }
+    "ccms-load"         = { namespace = "HUB20-CCMS-NS",       servicename = "ccms-load-service" }
+    "purge-lambda"      = { namespace = "HUB20-PURGE-DATA-NS", servicename = "purge-lambda-service" }
   }
 
   alarm_name          = "${each.value.servicename}-${local.environment}-InvocationFailureCount-Alarm"
