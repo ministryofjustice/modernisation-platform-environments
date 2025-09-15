24277d2e4e44e89620424d58c4631940d1ecd5d1
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-09-15 16:49:46 +0100

    Adding shared vpc lookup

---
+++ terraform/environments/ccms-ebs/workspaces-secure-browser/member-data.tf
@@ -0,0 +1,20 @@
+
+data "aws_subnet" "private_aza" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.networking[0].business-unit}-${local.environment}-${var.networking[0].set}-private-eu-west-2a"]
+  }
+}
+
+data "aws_subnet" "private_azc" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.networking[0].business-unit}-${local.environment}-${var.networking[0].set}-private-eu-west-2c"]
+  }
+}
+
+data "aws_vpc" "shared" {
+  tags = {
+    "Name" = "${var.networking[0].business-unit}-${local.environment}"
+  }
+}
