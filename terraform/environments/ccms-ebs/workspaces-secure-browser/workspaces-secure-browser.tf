e7012ce3962e559911e7f7a26c940d42fa582c72
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-09-16 08:36:48 +0100

    Module vars are hard

--- terraform/environments/ccms-ebs/workspaces-secure-browser/workspaces-secure-browser.tf
+++ terraform/environments/ccms-ebs/workspaces-secure-browser/workspaces-secure-browser.tf
@@ -14,7 +14,15 @@ module "workspacesweb_security_group" {
   egress_cidr_blocks = local.cloud_platform_ranges
   egress_rules       = ["https-443-tcp"]
 
-  egress_with_prefix_list_ids = [aws_ec2_managed_prefix_list.entra_saml_auth.id]
+  egress_with_prefix_list_ids = [
+    {
+      rule           = "azure-auth-endpoints"
+      prefix_list_id = aws_ec2_managed_prefix_list.entra_saml_auth.id
+      description    = "Entra SAML auth (AAD + AFD)"
+
+    }
+
+  ]
 
 }
 
