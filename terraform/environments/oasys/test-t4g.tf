4c6842e56a33c836f5c72b4ab4d6d0df57e40c59
SteveLinden <stephen.linden@justice.gov.uk>
2025-09-16 16:44:07 +0100

    Next try

--- terraform/environments/oasys/test-t4g.tf
+++ terraform/environments/oasys/test-t4g.tf
@@ -4,13 +4,10 @@ resource "aws_instance" "my_instance" {
   #checkov:skip=CKV_AWS_16: "Ensure all data stored in the RDS is securely encrypted at rest"
   #checkov:skip=CKV2_AWS_41: "Ensure an IAM role is attached to EC2 instance"
   #checkov:skip=CKV_AWS_8: "Ensure all data stored in the Launch configuration or instance Elastic Blocks Store is securely encrypted"
-  attachment {
-    instance     = aws_instance.example.id
-    device_index = 0
-  }
+
   count         = local.is-development ? 1 : 0
   ami           = "ami-08f714c552929eda9"
   instance_type = "t4g.micro"
-
+  tags          = {}
 
 }
