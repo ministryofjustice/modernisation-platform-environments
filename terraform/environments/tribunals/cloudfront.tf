9aacef35206bbe009f4b4dff574d092abb02744c
Mark Butler <42868152+mark-butler-solirius@users.noreply.github.com>
2025-09-16 17:01:17 +0100

    Add siac.tribunals.gov.uk as a SAN (#12936)

--- terraform/environments/tribunals/cloudfront.tf
+++ terraform/environments/tribunals/cloudfront.tf
@@ -92,7 +92,7 @@ resource "aws_acm_certificate" "cloudfront" {
   provider                  = aws.us-east-1
   domain_name               = local.is-production ? "*.decisions.tribunals.gov.uk" : "modernisation-platform.service.justice.gov.uk"
   validation_method         = "DNS"
-  subject_alternative_names = local.is-production ? ["*.venues.tribunals.gov.uk", "*.reports.tribunals.gov.uk"] : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
+  subject_alternative_names = local.is-production ? ["*.venues.tribunals.gov.uk", "*.reports.tribunals.gov.uk", "siac.tribunals.gov.uk"] : ["*.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"]
   tags = {
     Environment = local.environment
   }
