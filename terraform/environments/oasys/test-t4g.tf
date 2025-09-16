c94aa40253264bf2c6659cd89dad68e4998c4eec
SteveLinden <stephen.linden@justice.gov.uk>
2025-09-16 14:47:43 +0100

    Changed the AWS-8 to match above

--- terraform/environments/oasys/test-t4g.tf
+++ terraform/environments/oasys/test-t4g.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my_instance" {
   #checkov:skip=CKV_AWS_135: "Ensure that EC2 is EBS optimized"
   #checkov:skip=CKV_AWS_16: "Ensure all data stored in the RDS is securely encrypted at rest"
   #checkov:skip=CKV2_AWS_41: "Ensure an IAM role is attached to EC2 instance"
-  #checkov:skip==CKV_AWS_8: "Ensure all data stored in the Launch configuration or instance Elastic Blocks Store is securely encrypted"
+  #checkov:skip=CKV_AWS_8: "Ensure all data stored in the Launch configuration or instance Elastic Blocks Store is securely encrypted"
 
   count         = local.is-development ? 1 : 0
   ami           = "ami-08f714c552929eda9"
