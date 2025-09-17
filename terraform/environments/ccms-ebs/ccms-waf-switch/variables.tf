08e4b4b27413383e86ac9b415890988c01db0240
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2025-09-17 18:11:00 +0100

    files are moved to different folder

---
+++ terraform/environments/ccms-ebs/ccms-waf-switch/variables.tf
@@ -0,0 +1,26 @@
+#Variables
+variable "env" { 
+    type = string
+    default = "dev"
+}
+    
+variable "region" {
+    default = "eu-west-2"
+}
+
+variable "scope"{
+    default = "REGIONAL"
+}
+
+variable "web_acl_name" { 
+    type = string
+    default = "ebs_waf"
+}
+
+variable "web_acl_id" { 
+    type = string
+}
+
+variable "rule_name" {
+    default = "Allow" 
+}
