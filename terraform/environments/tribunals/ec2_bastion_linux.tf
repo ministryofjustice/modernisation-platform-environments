49a723d5913bc6d10908912ce1b5c799078b4dad
Timi Olubusi <timiolubusi@icloud.com>
2025-09-17 11:54:21 +0100

    reverting bastion version upgrade

--- terraform/environments/tribunals/ec2_bastion_linux.tf
+++ terraform/environments/tribunals/ec2_bastion_linux.tf
@@ -1,14 +1,16 @@
 module "bastion_linux" {
   # tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
   #checkov:skip=CKV_TF_1:"Using version tag v4.0.0 instead of commit hash for better readability and version tracking"
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v4.4.2"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v4.0.0"
 
   providers = {
     aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
     aws.share-tenant = aws          # The default provider (unaliased, `aws`) is the tenant
   }
   # s3 - used for logs and user ssh public keys
-  bucket_name          = "bastion"
+  bucket_name          = "bastion-example"
+  bucket_versioning    = true
+  bucket_force_destroy = true
   # public keys
   public_key_data = local.public_key_data.keys[local.environment]
   # logs
