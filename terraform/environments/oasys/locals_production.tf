3b0770741164bc807586b88ff48e9819d9df4ed6
wullub <13420301+wullub@users.noreply.github.com>
2025-09-17 17:10:39 +0100

    test production fs

--- terraform/environments/oasys/locals_production.tf
+++ terraform/environments/oasys/locals_production.tf
@@ -375,6 +375,41 @@ locals {
           oasys-environment = "trn"
         })
       })
+
+      fs-dns-test-linux = {
+        config = {
+          ami_name                      = "oasys_webserver_release_2023-07-02*"
+          availability_zone             = "eu-west-2a"
+          ebs_volumes_copy_all_from_ami = false
+          iam_resource_names_prefix     = "ec2-instance"
+          instance_profile_policies = [
+            "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+            "EC2Default",
+            "EC2S3BucketWriteAndDeleteAccessPolicy",
+            "ImageBuilderS3BucketWriteAndDeleteAccessPolicy"
+          ]
+          subnet_name = "private"
+        }
+        ebs_volumes = {
+          "/dev/sda1" = { type = "gp3", size = 100 }
+        }
+        instance = {
+          disable_api_termination      = false
+          instance_type                = "t3.medium"
+          key_name                     = "ec2-user"
+          metadata_options_http_tokens = "required"
+          vpc_security_group_ids = ["private_web"]
+        }
+        route53_records = {
+          create_external_record = false
+          create_internal_record = false
+        }
+        tags = {
+          backup      = "false" # disable mod platform backup since we use our own policies
+          os-type     = "Linux"
+          server-type = "fs-dns-test-linux"
+        }
+      }
     }
 
     iam_policies = {
