d150678dbd2207cfb0e074b7acf74eb7f60e3344
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-17 22:15:44 +0000

    feat: implement S3 access logs bucket policy with IAM permissions
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/aws-s3-access-logging.tf
+++ terraform/environments/data-platform/aws-s3-access-logging.tf
@@ -1,9 +1,19 @@
-module "s3_access_logs_kms_key" {
-  source = "github.com/terraform-aws-modules/terraform-aws-kms.git?ref=83e5418372a0716f6dae00ef04eaf42110f9f072" # v4.1.0
-
-  aliases                 = ["s3/mojdp-${local.environment}-s3-access-logs"]
-  enable_default_policy   = true
-  deletion_window_in_days = 7
+data "aws_iam_policy_document" "s3_access_logs_bucket_policy" {
+  statement {
+    sid       = "AllowS3ServerAccessLogs"
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::mojdp-${local.environment}-s3-access-logs/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["logging.s3.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+  }
 }
 
 module "s3_access_logs_s3_bucket" {
@@ -11,13 +21,27 @@ module "s3_access_logs_s3_bucket" {
 
   bucket = "mojdp-${local.environment}-s3-access-logs"
 
-  server_side_encryption_configuration = {
-    rule = {
-      bucket_key_enabled = true
-      apply_server_side_encryption_by_default = {
-        kms_master_key_id = module.s3_access_logs_kms_key.key_arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
+  force_destroy = false
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.s3_access_logs_bucket_policy.json
+
+  object_lock_enabled = false
+
+  versioning = {
+    status = "Disabled"
+  }
+}
+
+module "s3_access_logs_testing_s3_bucket" {
+  source = "github.com/terraform-aws-modules/terraform-aws-s3-bucket.git?ref=c375418373496865e2770ad8aabfaf849d4caee5" # v5.7.0
+
+  bucket = "mojdp-${local.environment}-s3-access-logs-testing"
+
+  force_destroy = true
+
+  logging = {
+    target_bucket = module.s3_access_logs_s3_bucket.s3_bucket_id
+    target_prefix = "${module.s3_access_logs_testing_s3_bucket.s3_bucket_id}/"
   }
 }
