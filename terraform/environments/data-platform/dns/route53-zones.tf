4b195a68e3d910c9b897b66aa741bf39c92ba55c
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-18 13:33:50 +0000

    refactor dns
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/dns/route53-zones.tf
+++ terraform/environments/data-platform/dns/route53-zones.tf
@@ -1,39 +1,7 @@
-module "development_zone" {
-  count = terraform.workspace == "data-platform-development" ? 1 : 0
-
+module "zone" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-route53.git//modules/zones?ref=f56825af8cb08bec2478e2f62b678e51986c1531" # v5.0.0
 
   zones = {
-    "${local.environment_configuration.route53_zone_name}" = {}
-  }
-}
-
-module "preproduction_zone" {
-  count = terraform.workspace == "data-platform-preproduction" ? 1 : 0
-
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-route53.git//modules/zones?ref=f56825af8cb08bec2478e2f62b678e51986c1531" # v5.0.0
-
-  zones = {
-    "${local.environment_configuration.route53_zone_name}" = {}
-  }
-}
-
-module "production_zone" {
-  count = terraform.workspace == "data-platform-production" ? 1 : 0
-
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-route53.git//modules/zones?ref=f56825af8cb08bec2478e2f62b678e51986c1531" # v5.0.0
-
-  zones = {
-    "${local.environment_configuration.route53_zone_name}" = {}
-  }
-}
-
-module "test_zone" {
-  count = terraform.workspace == "data-platform-test" ? 1 : 0
-
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-route53.git//modules/zones?ref=f56825af8cb08bec2478e2f62b678e51986c1531" # v5.0.0
-
-  zones = {
-    "${local.environment_configuration.route53_zone_name}" = {}
+    (local.environment_configuration.route53_zone_name) = {}
   }
 }
