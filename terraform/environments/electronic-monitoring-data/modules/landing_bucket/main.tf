571f5ac8e05c2c5463a161680469371ec6f792c2
matt heery <matt.heery@justice.gov.uk>
2025-09-18 16:18:45 +0100

    fix: add empty list

--- terraform/environments/electronic-monitoring-data/modules/landing_bucket/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/landing_bucket/main.tf
@@ -130,6 +130,7 @@ module "s3_to_lambda" {
   bucket               = module.this-bucket.bucket
   lambda_function_name = module.process_landing_bucket_files.lambda_function_name
   bucket_prefix        = var.local_bucket_prefix
+  s3_suffixes          = []
 }
 
 #-----------------------------------------------------------------------------------
