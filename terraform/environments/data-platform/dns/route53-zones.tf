11179728de028943d9f7cd89ba238092b3521e61
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-18 09:16:43 +0000

    feat: add Route 53 DNS records and zones modules for development, preproduction, production, and test environments
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ terraform/environments/data-platform/dns/route53-zones.tf
@@ -0,0 +1,39 @@
+module "development_zone" {
+  source = "github.com/terraform-aws-modules/terraform-aws-route53.git/modules/zones?ref=f56825af8cb08bec2478e2f62b678e51986c1531" # v5.0.0
+
+  count = terraform.workspace == "data-platform-development" ? 1 : 0
+
+  zones = {
+    "development.data-platform.service.justice.gov.uk" = {}
+  }
+}
+
+module "preproduction_zone" {
+  source = "github.com/terraform-aws-modules/terraform-aws-route53.git/modules/zones?ref=f56825af8cb08bec2478e2f62b678e51986c1531" # v5.0.0
+
+  count = terraform.workspace == "data-platform-preproduction" ? 1 : 0
+
+  zones = {
+    "preproduction.data-platform.service.justice.gov.uk" = {}
+  }
+}
+
+module "production_zone" {
+  source = "github.com/terraform-aws-modules/terraform-aws-route53.git/modules/zones?ref=f56825af8cb08bec2478e2f62b678e51986c1531" # v5.0.0
+
+  count = terraform.workspace == "data-platform-production" ? 1 : 0
+
+  zones = {
+    "data-platform.service.justice.gov.uk" = {}
+  }
+}
+
+module "test_zone" {
+  source = "github.com/terraform-aws-modules/terraform-aws-route53.git/modules/zones?ref=f56825af8cb08bec2478e2f62b678e51986c1531" # v5.0.0
+
+  count = terraform.workspace == "data-platform-test" ? 1 : 0
+
+  zones = {
+    "test.data-platform.service.justice.gov.uk" = {}
+  }
+}
