227a7f0b0d3eaae5447ad21fa2bc1d99e243e575
nbuckingham72 <nick.buckingham@colt.net>
2025-09-18 20:09:15 +0100

    Update_180925_1

--- terraform/environments/ppud/alb_external.tf
+++ terraform/environments/ppud/alb_external.tf
@@ -144,30 +144,6 @@ resource "aws_lb_listener" "WAM-Front-End-Prod" {
   }
 }
 
-/*
-resource "aws_lb_target_group" "WAM-Target-Group" {
-  name     = "WAM"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.shared.id
-
-  health_check {
-    enabled             = true
-    path                = "/"
-    interval            = 30
-    protocol            = "HTTP"
-    port                = 80
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    matcher             = "302"
-  }
-  tags = {
-    Name = "${var.networking[0].business-unit}-${local.environment}"
-  }
-}
-*/
-
 resource "aws_lb_target_group_attachment" "WAM-Portal-development" {
   count            = local.is-development == true ? 1 : 0
   target_group_arn = aws_lb_target_group.WAM-Target-Group-Dev[0].arn
@@ -177,17 +153,16 @@ resource "aws_lb_target_group_attachment" "WAM-Portal-development" {
 
 resource "aws_lb_target_group_attachment" "WAM-Portal-preproduction" {
   count            = local.is-preproduction == true ? 1 : 0
-  target_group_arn = aws_lb_target_group.WAM-Target-Group-Preprod-2[0].arn
+  target_group_arn = aws_lb_target_group.WAM-Target-Group-Preprod[0].arn
   target_id        = aws_instance.s618358rgvw201[0].id
   port             = 443
 }
-
 
 resource "aws_lb_target_group_attachment" "WAM-Portal-production" {
   count            = local.is-production == true ? 1 : 0
   target_group_arn = aws_lb_target_group.WAM-Target-Group-Prod[0].arn
   target_id        = aws_instance.s618358rgvw204[0].id
-  port             = 80
+  port             = 443
 }
 
 resource "aws_lb_target_group" "WAM-Target-Group-Dev" {
@@ -213,34 +188,9 @@ resource "aws_lb_target_group" "WAM-Target-Group-Dev" {
   }
 }
 
-/*
 resource "aws_lb_target_group" "WAM-Target-Group-Preprod" {
   count    = local.is-preproduction == true ? 1 : 0
   name     = "WAM-Preprod"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.shared.id
-
-  health_check {
-    enabled             = true
-    path                = "/"
-    interval            = 30
-    protocol            = "HTTP"
-    port                = 80
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    matcher             = "302"
-  }
-  tags = {
-    Name = "${var.networking[0].business-unit}-${local.environment}"
-  }
-}
-*/
-
-resource "aws_lb_target_group" "WAM-Target-Group-Preprod-2" {
-  count    = local.is-preproduction == true ? 1 : 0
-  name     = "WAM-Preprod-2"
   port     = 443
   protocol = "HTTPS"
   vpc_id   = data.aws_vpc.shared.id
@@ -264,16 +214,16 @@ resource "aws_lb_target_group" "WAM-Target-Group-Preprod-2" {
 resource "aws_lb_target_group" "WAM-Target-Group-Prod" {
   count    = local.is-production == true ? 1 : 0
   name     = "WAM-Prod"
-  port     = 80
-  protocol = "HTTP"
+  port     = 443
+  protocol = "HTTPS"
   vpc_id   = data.aws_vpc.shared.id
 
   health_check {
     enabled             = true
     path                = "/"
     interval            = 30
-    protocol            = "HTTP"
-    port                = 80
+    protocol            = "HTTPS"
+    port                = 443
     timeout             = 5
     healthy_threshold   = 5
     unhealthy_threshold = 2
