2e11474e8b112e244cb56cd71a9c76f83df2c717
Jono Basford <jono.basford@justice.gov.uk>
2025-09-19 16:51:37 +0100

    Added centurion ingestion IAM role

--- terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
+++ terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
@@ -568,3 +568,22 @@ module "full_reload_servicenow" {
   new_airflow = true
   full_reload = true
 }
+
+module "full_load_g4s_centurion" {
+  count  = local.is-production || local.is-development ? 1 : 0
+  source = "./modules/ap_airflow_load_data_iam_role"
+
+  data_bucket_lf_resource = aws_lakeformation_resource.data_bucket.arn
+  de_role_arn             = try(one(data.aws_iam_roles.mod_plat_roles.arns))
+
+  name               = "g4s-centurion"
+  environment        = local.environment
+  database_name      = "g4s-centurion"
+  path_to_data       = "g4s-centurion"
+  source_data_bucket = module.s3-dms-target-store-bucket.bucket
+  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
+  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
+  athena_dump_bucket = module.s3-athena-bucket.bucket
+  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
+  new_airflow        = true
+}
