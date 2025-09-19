29b482bea757b00232048be6d20c726b7e686f37
SteveLinden <stephen.linden@justice.gov.uk>
2025-09-19 15:27:17 +0100

    Change security group

--- terraform/environments/oasys/test-t4g.tf
+++ terraform/environments/oasys/test-t4g.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "my_t4_instance" {
   count                       = local.is-development ? 1 : 0
   ami                         = "ami-08f714c552929eda9"
   instance_type               = "t4g.micro"
-  vpc_security_group_ids      = ["subnet-072949a209fe05fb3"]
+  vpc_security_group_ids      = ["subnet-01d1b50ce2c2ce8d3"]
   associate_public_ip_address = false
   availability_zone           = "eu-west-2a"
   ebs_optimized               = true
