b340dad82a794b306a251f60b1af2e1dd6e4e179
nbuckingham72 <nick.buckingham@colt.net>
2025-09-22 15:27:16 +0100

    ACM_Update_1

--- terraform/environments/ppud/certificates.tf
+++ terraform/environments/ppud/certificates.tf
@@ -19,7 +19,7 @@ locals {
   } : {}
 }
 
-resource "aws_acm_certificate" "uat_certificates" {
+resource "aws_acm_certificate" "preprod_certificates" {
   for_each                  = local.is-preproduction ? local.preprod_domains : {}
   domain_name               = each.value
   validation_method         = "DNS"
@@ -34,8 +34,9 @@ resource "aws_acm_certificate" "uat_certificates" {
   }
 }
 
+/*
 resource "aws_acm_certificate_validation" "preprod_certificate_validation" {
-  for_each        = aws_acm_certificate.uat_certificates
+  for_each        = aws_acm_certificate.preprod_certificates
   certificate_arn = each.value.arn
 
   validation_record_fqdns = [
@@ -48,24 +49,25 @@ resource "aws_acm_certificate_validation" "preprod_certificate_validation" {
 }
 
 locals {
-  preprod_dns_records = local.is-preproduction ? merge(
-    [
-      for cert_key, cert in aws_acm_certificate.uat_certificates :
-      {
-        for option in cert.domain_validation_options :
-        "${cert_key}-${option.resource_record_name}" => {
-          name   = option.resource_record_name
-          type   = option.resource_record_type
-          record = option.resource_record_value
-        }
+  preprod_zone_ids = local.is-preproduction ? {
+    uat    = data.aws_route53_zone.uat[0].zone_id
+    wamuat = data.aws_route53_zone.wamuat[0].zone_id
+  } : {}
+
+  preprod_dns_records = local.is-preproduction ? merge([
+    for cert_key, cert in aws_acm_certificate.preprod_certificates : {
+      for option in cert.domain_validation_options : "${cert_key}-${option.resource_record_name}" => {
+        name    = option.resource_record_name
+        type    = option.resource_record_type
+        record  = option.resource_record_value
+        zone_id = local.preprod_zone_ids[cert_key]
       }
-    ]...
-  ) : {}
+    }
+  ]...) : {}
 }
 
 resource "aws_route53_record" "preprod_dns_record" {
   for_each = local.is-preproduction ? local.preprod_dns_records : {}
-
   provider = aws.core-network-services
 
   allow_overwrite = true
@@ -73,10 +75,9 @@ resource "aws_route53_record" "preprod_dns_record" {
   type    = each.value.type
   records = [each.value.record]
   ttl     = 60
-  # zone_id = var.shared_zone_id      # shared hosted zone
-  zone_id = each.value.zone.zone_id   # multiple zones
+  zone_id = each.value.zone_id
 }
-
+*/
 
 ########################
 # Production Environment
--- terraform/environments/ppud/data.tf
+++ terraform/environments/ppud/data.tf
@@ -1,6 +1,5 @@
 #### This file can be used to store data specific to the member account ####
 
-
 # ACM certificate for PPUD TEST ALB
 data "aws_acm_certificate" "PPUD_internaltest_cert" {
   count    = local.is-development == true ? 1 : 0
@@ -49,4 +48,18 @@ data "aws_acm_certificate" "WAM_PROD_ALB" {
   count    = local.is-production == true ? 1 : 0
   domain   = "wam.ppud.justice.gov.uk"
   statuses = ["ISSUED"]
+}
+
+# Route 53 Zones for Certificate Domain Validation
+
+data "aws_route53_zone" "uat" {
+  count        = local.is-preproduction ? 1 : 0
+  name         = "uat.ppud.justice.gov.uk."
+  private_zone = false
+}
+
+data "aws_route53_zone" "wamuat" {
+  count        = local.is-preproduction ? 1 : 0
+  name         = "wamuat.ppud.justice.gov.uk."
+  private_zone = false
 }
