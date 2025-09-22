dbc86fee2bcda253f56050bc9815a31801b242cd
matt heery <matt.heery@justice.gov.uk>
2025-09-22 15:14:55 +0100

    feat: add sqs queue for metadata lambda

--- terraform/environments/electronic-monitoring-data/lambda_triggers.tf
+++ terraform/environments/electronic-monitoring-data/lambda_triggers.tf
@@ -44,6 +44,12 @@ resource "aws_s3_bucket_notification" "historic_data_checksum" {
     filter_suffix = ".JSON"
     filter_prefix = "serco/fms"
   }
+  queue {
+    queue_arn     = module.process_fms_metadata.sqs_queue.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".JSON"
+    filter_prefix = "serco/fms"
+  }
 }
 
 module "format_json_fms_data_sqs" {
@@ -58,4 +64,20 @@ module "copy_mdss_data_sqs" {
   bucket               = module.s3-data-bucket.bucket
   lambda_function_name = module.copy_mdss_data.lambda_function_name
   bucket_prefix        = local.bucket_prefix
+}
+
+module process_fms_metadata_sqs" {
+  source               = "./modules/sqs_s3_lambda_trigger"
+  bucket               = module.s3-data-bucket.bucket
+  lambda_function_name = module.process_fms_metadata.lambda_function_name
+  bucket_prefix        = local.bucket_prefix
+}
+
+resource "aws_s3_bucket_notification" "virus_scan" {
+  bucket = module.s3-received-files-bucket.bucket
+
+  queue {
+    queue_arn = module.virus_scan_file.lambda_function_name
+    events    = ["s3:ObjectCreated:*"]
+  }
 }
