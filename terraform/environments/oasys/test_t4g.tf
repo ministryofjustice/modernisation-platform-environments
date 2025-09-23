fb9edaf0edd243c1dbc9f6fc32d57a9a8ba147f0
SteveLinden <stephen.linden@justice.gov.uk>
2025-09-23 16:21:10 +0100

    Re-ordered the checkov skips and added a couple more that were missing

--- terraform/environments/oasys/test_t4g.tf
+++ terraform/environments/oasys/test_t4g.tf
@@ -21,12 +21,12 @@ locals {
   }
 }
 resource "aws_instance" "my_t4_instance" {
+  #checkov:skip=CKV_AWS_8: "Ensure all data stored in the Launch configuration or instance Elastic Blocks Store is securely encrypted"
+  #checkov:skip=CKV_AWS_16: "Ensure all data stored in the RDS is securely encrypted at rest"
+  #checkov:skip=CKV_AWS_23: "Ensure every security group and rule has a description"
+  #checkov:skip=CKV2_AWS_41: "Ensure an IAM role is attached to EC2 instance"
   #checkov:skip=CKV_AWS_79: "Ensure Instance Metadata Service Version 1 is not enabled"
   #checkov:skip=CKV_AWS_135: "Ensure that EC2 is EBS optimized"
-  #checkov:skip=CKV_AWS_16: "Ensure all data stored in the RDS is securely encrypted at rest"
-  #checkov:skip=CKV2_AWS_41: "Ensure an IAM role is attached to EC2 instance"
-  #checkov:skip=CKV_AWS_8: "Ensure all data stored in the Launch configuration or instance Elastic Blocks Store is securely encrypted"
-  #checkov:skip=CKV_AWS_23: "Ensure every security group and rule has a description"
   #checkov:skip=CKV_AWS_382: "Ensure no security groups allow egress from 0.0.0.0:0 to port -1"
 
   count = local.is-development ? 1 : 0
