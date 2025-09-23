7d02c8654b4d9071137ca86480edfa8411b89ec0
SteveLinden <stephen.linden@justice.gov.uk>
2025-09-23 17:00:41 +0100

    Moved the items to the correct place

--- terraform/environments/oasys/test_t4g.tf
+++ terraform/environments/oasys/test_t4g.tf
@@ -1,4 +1,7 @@
 resource "aws_security_group" "test-sg-for-t4" {
+
+  #checkov:skip=CKV_AWS_23: "Ensure every security group and rule has a description"
+  #checkov:skip=CKV_AWS_382: "Ensure no security groups allow egress from 0.0.0.0:0 to port -1"
   vpc_id      = data.aws_vpc.shared.id
   name        = "test-sec-group"
   description = "Allow all outbound traffic"
@@ -23,11 +26,11 @@ locals {
 resource "aws_instance" "my_t4_instance" {
   #checkov:skip=CKV_AWS_8: "Ensure all data stored in the Launch configuration or instance Elastic Blocks Store is securely encrypted"
   #checkov:skip=CKV_AWS_16: "Ensure all data stored in the RDS is securely encrypted at rest"
-  #checkov:skip=CKV_AWS_23: "Ensure every security group and rule has a description"
   #checkov:skip=CKV2_AWS_41: "Ensure an IAM role is attached to EC2 instance"
   #checkov:skip=CKV_AWS_79: "Ensure Instance Metadata Service Version 1 is not enabled"
   #checkov:skip=CKV_AWS_135: "Ensure that EC2 is EBS optimized"
-  #checkov:skip=CKV_AWS_382: "Ensure no security groups allow egress from 0.0.0.0:0 to port -1"
+
+
 
   count = local.is-development ? 1 : 0
   ami   = "ami-03823b6730d398a3f"
