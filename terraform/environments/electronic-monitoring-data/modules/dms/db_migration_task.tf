2e70a3a0f61b60b16ca60d99fd19abb92eb85132
matt heery <matt.heery@justice.gov.uk>
2025-09-25 16:00:06 +0100

    fi: new task name

--- terraform/environments/electronic-monitoring-data/modules/dms/db_migration_task.tf
+++ terraform/environments/electronic-monitoring-data/modules/dms/db_migration_task.tf
@@ -3,7 +3,7 @@ resource "aws_dms_replication_task" "dms_db_migration_task" {
 
   migration_type            = "full-load"
   replication_instance_arn  = var.dms_replication_instance_arn
-  replication_task_id       = "${replace(var.database_name, "_", "-")}-db-migration-task-tf"
+  replication_task_id       = "${replace(var.database_name, "_", "-")}${replace(var.dump_number_suffix, "_", "-")}-db-migration-task-tf"
   replication_task_settings = var.rep_task_settings_filepath
   source_endpoint_arn       = aws_dms_endpoint.dms_rds_source.endpoint_arn
   table_mappings            = var.rep_task_table_mapping_filepath
@@ -22,7 +22,7 @@ resource "aws_dms_replication_task" "dms_db_migration_task_v2" {
 
   migration_type            = "full-load"
   replication_instance_arn  = var.dms_replication_instance_arn
-  replication_task_id       = "${replace(var.database_name, "_", "-")}-db-migration-task-v2-tf"
+  replication_task_id       = "${replace(var.database_name, "_", "-")}${replace(var.dump_number_suffix, "_", "-")}-db-migration-task-v2-tf"
   replication_task_settings = var.rep_task_settings_filepath
   source_endpoint_arn       = aws_dms_endpoint.dms_rds_source.endpoint_arn
   table_mappings            = trimspace(file("${path.module}/dms_${var.database_name}_task_transformations.json"))
