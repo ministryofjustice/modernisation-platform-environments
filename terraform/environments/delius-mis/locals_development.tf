3f0c9e2249f4e6a6a697102ec6a12db1e331dbd1
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-09-26 10:37:30 +0100

    fix dis drive init

--- terraform/environments/delius-mis/locals_development.tf
+++ terraform/environments/delius-mis/locals_development.tf
@@ -157,11 +157,11 @@ locals {
   }
 
   dis_config_dev = {
-    instance_count = 1
+    instance_count = 0
     ami_name       = "delius_mis_windows_server_patch_2025-*"
     ebs_volumes = {
       "/dev/sda1" = { label = "root", size = 100 }
-      "/dev/xvdf" = { label = "data", size = 300 }
+      "xvdd"      = { label = "data", size = 300 }
     }
 
     ebs_volumes_config = {
