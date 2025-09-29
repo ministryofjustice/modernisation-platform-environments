a0696a8703cb8b626346142f2a01552f27f0fe66
SteveLinden <stephen.linden@justice.gov.uk>
2025-09-29 11:26:39 +0100

    Amending t4g to xlarge

--- terraform/environments/oasys/test_t4g.tf
+++ terraform/environments/oasys/test_t4g.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "my_t4_instance" {
   count = local.is-development ? 1 : 0
 
   ami           = "ami-01e9af7b9c1dfb736"
-  instance_type = "t4g.micro"
+  instance_type = "t4g.xlarge"
 
   vpc_security_group_ids = [aws_security_group.test-sg-for-t4.id]
 
