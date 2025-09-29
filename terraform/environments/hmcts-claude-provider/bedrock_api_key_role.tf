a0f1a1e5b7c0091155ee20f888305b71d33f646e
Linus Norton <linusnorton@gmail.com>
2025-09-29 20:44:50 +0100

    Add key creator role

---
+++ terraform/environments/hmcts-claude-provider/bedrock_api_key_role.tf
@@ -0,0 +1,95 @@
+# Custom role for Bedrock API key creation that bypasses the common_policy deny
+resource "aws_iam_role" "bedrock_api_key_creator" {
+  name = "BedrockAPIKeyCreator"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:sts::313941174580:assumed-role/developer/*"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.tags
+}
+
+# Policy that allows creating Bedrock API key users
+resource "aws_iam_policy" "bedrock_api_key_creator_policy" {
+  name        = "BedrockAPIKeyCreatorPolicy"
+  description = "Allows creation and management of Bedrock API key users"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowBedrockAPIKeyUserManagement"
+        Effect = "Allow"
+        Action = [
+          "iam:CreateUser",
+          "iam:DeleteUser",
+          "iam:GetUser",
+          "iam:ListUsers",
+          "iam:TagUser",
+          "iam:UntagUser",
+          "iam:CreateAccessKey",
+          "iam:DeleteAccessKey",
+          "iam:ListAccessKeys",
+          "iam:GetAccessKeyLastUsed",
+          "iam:UpdateAccessKey",
+          "iam:PutUserPolicy",
+          "iam:DeleteUserPolicy",
+          "iam:GetUserPolicy",
+          "iam:ListUserPolicies",
+          "iam:AttachUserPolicy",
+          "iam:DetachUserPolicy",
+          "iam:ListAttachedUserPolicies"
+        ]
+        Resource = "arn:aws:iam::313941174580:user/BedrockAPIKey-*"
+      },
+      {
+        Sid    = "AllowBedrockAPIKeyOperations"
+        Effect = "Allow"
+        Action = [
+          "bedrock:CreateApiKey",
+          "bedrock:ListApiKeys",
+          "bedrock:DeleteApiKey",
+          "bedrock:GetApiKey"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowListingPolicies"
+        Effect = "Allow"
+        Action = [
+          "iam:ListPolicies",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "bedrock_api_key_creator_policy" {
+  role       = aws_iam_role.bedrock_api_key_creator.name
+  policy_arn = aws_iam_policy.bedrock_api_key_creator_policy.arn
+}
+
+# Also attach the Bedrock model access policy so this role can test the API keys
+resource "aws_iam_role_policy_attachment" "bedrock_api_key_creator_bedrock_access" {
+  role       = aws_iam_role.bedrock_api_key_creator.name
+  policy_arn = aws_iam_policy.bedrock_claude_policy.arn
+}
+
+# Output the role ARN for easy reference
+output "bedrock_api_key_creator_role_arn" {
+  value       = aws_iam_role.bedrock_api_key_creator.arn
+  description = "ARN of the role that can create Bedrock API keys. Assume this role to bypass the common_policy deny."
+}
