85feff6a4a0bd98efe603b4527838f7cb8439301
Linus Norton <linusnorton@gmail.com>
2025-09-29 20:27:05 +0100

    add more permissions so I can create a long term key

--- terraform/environments/hmcts-claude-provider/bedrock.tf
+++ terraform/environments/hmcts-claude-provider/bedrock.tf
@@ -49,3 +49,53 @@ resource "aws_iam_instance_profile" "bedrock_instance_profile" {
   name = "HMCTSClaudeBedrockInstanceProfile"
   role = aws_iam_role.bedrock_service_role.name
 }
+
+# Policy to allow creation of long-term Bedrock API keys
+resource "aws_iam_policy" "bedrock_api_key_creation" {
+  name        = "BedrockAPIKeyCreationPolicy"
+  description = "Allow creation of long-term Bedrock API keys"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowBedrockAPIKeyUserCreation"
+        Effect = "Allow"
+        Action = [
+          "iam:CreateUser",
+          "iam:CreateAccessKey",
+          "iam:PutUserPolicy",
+          "iam:AttachUserPolicy",
+          "iam:TagUser",
+          "iam:DeleteUser",
+          "iam:DeleteAccessKey",
+          "iam:DeleteUserPolicy",
+          "iam:DetachUserPolicy",
+          "iam:ListAccessKeys",
+          "iam:GetUser"
+        ]
+        Resource = "arn:aws:iam::*:user/BedrockAPIKey-*"
+      },
+      {
+        Sid    = "AllowBedrockAPIKeyOperations"
+        Effect = "Allow"
+        Action = [
+          "bedrock:CreateApiKey",
+          "bedrock:ListApiKeys",
+          "bedrock:DeleteApiKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the API key creation policy to the developer role
+data "aws_iam_role" "developer" {
+  name = "developer"
+}
+
+resource "aws_iam_role_policy_attachment" "developer_bedrock_api_keys" {
+  role       = data.aws_iam_role.developer.name
+  policy_arn = aws_iam_policy.bedrock_api_key_creation.arn
+}
