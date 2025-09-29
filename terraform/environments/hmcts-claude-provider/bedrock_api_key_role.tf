3876906af18aeef1110f34fbf34ba5cbbbe702ac
Linus Norton <linusnorton@gmail.com>
2025-09-29 20:56:33 +0100

    update role

--- terraform/environments/hmcts-claude-provider/bedrock_api_key_role.tf
+++ terraform/environments/hmcts-claude-provider/bedrock_api_key_role.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role" "bedrock_api_key_creator" {
       {
         Effect = "Allow"
         Principal = {
-          AWS = "arn:aws:sts::313941174580:assumed-role/developer/*"
+          AWS = "arn:aws:iam::313941174580:role/developer"
         }
         Action = "sts:AssumeRole"
       }
