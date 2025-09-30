29d700f98656c32434396ad1d71a6792c14110a4
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-30 20:36:59 +0000

    feat: add tags to cloud platform live IAM OIDC provider module
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/iam-oidc-providers.tf
+++ terraform/environments/data-platform/iam-oidc-providers.tf
@@ -10,4 +10,11 @@ module "cloud_platform_live_iam_oidc_provider" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-oidc-provider?ref=dc7a9f3bed20aaaba05d151b0789745070424b3a" # v6.2.1
 
   url = data.aws_secretsmanager_secret_version.cloud_platform_live_iam_oidc_provider[0].secret_string
+
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "cloud-platform-live"
+    }
+  )
 }
