9b88ae9d3c950b6c6eb2acc91b354ae64a46a84a
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-09-30 20:01:48 +0000

    rename bedrock file
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/llm-gateway/bedrock-inferences-profiles.tf
+++ terraform/environments/data-platform/llm-gateway/bedrock-inferences-profiles.tf
@@ -4,10 +4,10 @@ data "aws_bedrock_inference_profile" "main" {
   inference_profile_id = each.value
 }
 
-resource "aws_bedrock_inference_profile" "litellm" {
+resource "aws_bedrock_inference_profile" "main" {
   for_each = local.environment_configuration.bedrock_inference_profiles
 
-  name = "litellm-${each.key}"
+  name = "${local.component_name}-${each.key}"
 
   model_source {
     copy_from = data.aws_bedrock_inference_profile.main[each.key].inference_profile_arn
