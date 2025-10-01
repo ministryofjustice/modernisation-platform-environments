5519a50a47a08ad63bfe0ba199397f62d3a9749a
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-10-01 16:54:25 +0100

    changes

--- terraform/environments/ccms-oia/ecs_cluster.tf
+++ terraform/environments/ccms-oia/ecs_cluster.tf
@@ -1,19 +1,3 @@
-#######################################
-# ECS Cluster and Service for OIA
-#######################################
-
-# # Capacity Provider
-# resource "aws_ecs_capacity_provider" "capacity_provider" {
-#   name = "${local.application_name}-capacity-provider"
-
-#   auto_scaling_group_provider {
-#     auto_scaling_group_arn = aws_autoscaling_group.cluster_scaling_group.arn
-#   }
-
-#   tags = merge(local.tags,
-#     { Name = lower(format("%s-%s-cp", local.application_name, local.environment)) }
-#   )
-# }
 
 # ECS Cluster
 resource "aws_ecs_cluster" "main" {
