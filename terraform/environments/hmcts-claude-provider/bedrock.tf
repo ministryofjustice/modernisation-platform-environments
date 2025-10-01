76284c0578c77ea2227adea1e0dfca377a5161a3
Linus Norton <linusnorton@gmail.com>
2025-10-01 17:01:32 +0100

    try adding account number

--- terraform/environments/hmcts-claude-provider/bedrock.tf
+++ terraform/environments/hmcts-claude-provider/bedrock.tf
@@ -20,6 +20,8 @@ resource "aws_iam_policy" "bedrock_claude_policy" {
           "arn:aws:bedrock:eu-west-1::foundation-model/anthropic.claude-sonnet-4-20250514-v1:0",
           "arn:aws:bedrock:eu-west-1::inference-profile/eu.anthropic.claude-sonnet-4-20250514-v1:0",
           "arn:aws:bedrock:eu-west-1::inference-profile/eu.anthropic.claude-sonnet-4-5-20250929-v1:0",
+          "arn:aws:bedrock:eu-west-1:313941174580:inference-profile/eu.anthropic.claude-sonnet-4-20250514-v1:0",
+          "arn:aws:bedrock:eu-west-1:313941174580:inference-profile/eu.anthropic.claude-sonnet-4-5-20250929-v1:0",
           "arn:aws:bedrock:eu-west-1:313941174580:application-inference-profile/*"
         ]
       },
