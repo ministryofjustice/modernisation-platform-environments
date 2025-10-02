7f1e8320dfa4d50bbff831962f80e6a3713cf4c4
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-10-02 18:06:36 +0000

    Test SG ingress rule creation on deduplicated list of CIDR blocks

---
+++ terraform/environments/analytical-platform-ingestion/to_be_security_group_and_rules.tf
@@ -0,0 +1,40 @@
+
+resource "aws_security_group" "to_be_transfer_server" {
+  description = "To Be Security Group for Transfer Server"
+  name        = "to-be-transfer-server"
+  vpc_id      = module.isolated_vpc.vpc_id
+  tags        = local.tags
+}
+
+# # Security group rules for to-be-transfer-server security group
+# locals {
+#   cidr_blocks_distinct = toset(flatten([
+#     for k, v in local.environment_configuration.transfer_server_sftp_users : [
+#       for cidr_blocks in v.cidr_blocks : cidr_blocks
+#     ]
+#   ]))
+# }
+
+resource "aws_vpc_security_group_ingress_rule" "this" {
+  for_each = toset(flatten([
+    for k, v in local.environment_configuration.transfer_server_sftp_users : [
+      for cidr_blocks in v.cidr_blocks : cidr_blocks
+    ]
+  ]))
+  # description       = each.key # meaningless if users can share IP addesses
+  from_port         = 2222
+  ip_protocol       = "tcp"
+  to_port           = 2222
+  security_group_id = aws_security_group.to_be_transfer_server.id
+  cidr_ipv4         = each.value
+}
+
+# resource "aws_vpc_security_group_ingress_rule" "this" {
+#   for_each = local.cidr_blocks_distinct
+#   # description       = each.key # meaningless if users can share IP addesses
+#   from_port         = 2222
+#   ip_protocol       = "tcp"
+#   to_port           = 2222
+#   security_group_id = aws_security_group.to_be_transfer_server.id
+#   cidr_ipv4         = each.value
+# }
