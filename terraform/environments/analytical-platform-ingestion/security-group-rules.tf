c4e6bacb627ce01e499c5f89ed5f73f6f7b84672
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-10-03 14:22:41 +0000

    Phase 1 add new SG and ingress rules

--- terraform/environments/analytical-platform-ingestion/security-group-rules.tf
+++ terraform/environments/analytical-platform-ingestion/security-group-rules.tf
@@ -1,19 +1,3 @@
-locals {
-  transfer_server_cidr_blocks_distinct = setunion(
-    flatten([for k, v in local.environment_configuration.transfer_server_sftp_users : v.cidr_blocks]),
-    flatten([for k, v in local.environment_configuration.transfer_server_sftp_users_with_egress : v.cidr_blocks])
-  )
-}
-
-resource "aws_vpc_security_group_ingress_rule" "this" {
-  for_each = local.transfer_server_cidr_blocks_distinct
-  from_port         = 2222
-  ip_protocol       = "tcp"
-  to_port           = 2222
-  security_group_id = aws_security_group.transfer_server.id
-  cidr_ipv4         = each.value
-}
-
 resource "aws_security_group_rule" "connected_vpc_endpoints_allow_all_vpc" {
   cidr_blocks       = [module.connected_vpc.vpc_cidr_block]
   description       = "Allow all traffic in from VPC CIDR"
