3cc0971feb90593cd40538d5f5ad8d644b85faf6
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-10-03 12:23:41 +0100

    initial build of ccms service apaptor

--- terraform/environments/ccms-oia/service_adaptor_secrets.tf
+++ terraform/environments/ccms-oia/service_adaptor_secrets.tf
@@ -2,15 +2,15 @@
 # AWS Secrets Manager - Adaptor Secrets
 #######################################
 
-# Client OPA12Assess Security User Password
-resource "aws_secretsmanager_secret" "client_opa12assess_security_user_password" {
-  name        = "client_opa12assess_security_user_password"
-  description = "Client OPA12Assess Security User Password"
-}
+# # Client OPA12Assess Security User Password
+# resource "aws_secretsmanager_secret" "client_opa12assess_security_user_password" {
+#   name        = "client_opa12assess_security_user_password"
+#   description = "Client OPA12Assess Security User Password"
+# }
 
-data "aws_secretsmanager_secret_version" "client_opa12assess_security_user_password" {
-  secret_id = aws_secretsmanager_secret.client_opa12assess_security_user_password.id
-}
+# data "aws_secretsmanager_secret_version" "client_opa12assess_security_user_password" {
+#   secret_id = aws_secretsmanager_secret.client_opa12assess_security_user_password.id
+# }
 
 # Server OPA10Assess Security User Password
 resource "aws_secretsmanager_secret" "server_opa10assess_security_user_password" {
