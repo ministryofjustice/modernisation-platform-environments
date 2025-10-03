35cab61c2b176ecd5df5a3101542bd6a6738a5a2
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-10-03 16:19:47 +0000

    Created Redshift IdC role
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-next-poc-hub/iam-roles.tf
+++ terraform/environments/analytical-platform-next-poc-hub/iam-roles.tf
@@ -59,3 +59,54 @@ module "project_iam_roles" {
     user_policy = module.athena_access_policy.arn
   }
 }
+
+
+module "redshift_identity_centre_iam_role" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  // Based on the policy from https://aws.amazon.com/blogs/big-data/integrate-identity-provider-idp-with-amazon-redshift-query-editor-v2-and-sql-client-using-aws-iam-identity-center-for-seamless-single-sign-on/
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "6.2.1"
+
+  name = "redshift-idc"
+
+  trust_policy_permissions = {
+    TrustRoleAndServiceToAssume = {
+      actions = [
+        "sts:AssumeRole",
+        "sts:SetContext"
+      ]
+      principals = [{
+        type        = "Service"
+        identifiers = ["redshift.amazonaws.com"]
+      }]
+    }
+  }
+
+  create_inline_policy = true
+  inline_policy_permissions = {
+    IdentityCentreAccess = {
+      effect = "Allow"
+      actions = [
+        "sso:DescribeApplication",
+        "sso:DescribeInstance"
+      ]
+      resources = [
+        local.identity_centre_instance,
+        "arn:aws:sso::${local.environment_management.aws_organizations_root_account_id}:application/${local.identity_centre_instance_id}/*" // Leaving this as a wildcard for now
+      ]
+    }
+    RedShiftAccess = {
+      effect = "Allow"
+      actions = [
+        "redshift:DescribeQev2IdcApplications",
+        "redshift-serverless:ListNamespaces",
+        "redshift-serverless:ListWorkgroups",
+        "redshift-serverless:GetWorkgroup"
+      ]
+      resources = ["*"]
+    }
+  }
+}
--- terraform/environments/analytical-platform-next-poc-hub/locals.tf
+++ terraform/environments/analytical-platform-next-poc-hub/locals.tf
@@ -1,7 +1,9 @@
 locals {
-  athena_query_bucket_name = "mojap-next-poc-hub-athena-query"
-  producer_account_id      = local.environment_management.account_ids["analytical-platform-next-poc-producer-development"]
-  producer_database        = "moj"
+  identity_centre_instance    = tolist(data.aws_ssoadmin_instances.main.arns)[0]
+  identity_centre_instance_id = trimprefix(local.identity_centre_instance, "arn:aws:sso:::instance/")
+  athena_query_bucket_name    = "mojap-next-poc-hub-athena-query"
+  producer_account_id         = local.environment_management.account_ids["analytical-platform-next-poc-producer-development"]
+  producer_database           = "moj"
 
   users = [
     # slugified emails
