416d91941e069ff29959a277aa06fe9433ad4950
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-10-03 10:35:22 +0000

    tidy 🧹

--- terraform/environments/analytical-platform-ingestion/to_be_security_group_and_rules.tf
+++ terraform/environments/analytical-platform-ingestion/to_be_security_group_and_rules.tf
@@ -6,14 +6,6 @@ resource "aws_security_group" "to_be_transfer_server" {
   tags        = local.tags
 }
 
-# locals {
-#   # CIDR blocks for users and users-with-egress
-#   cidr_blocks_distinct = setunion(
-#     flatten([for k, v in local.environment_configuration.transfer_server_sftp_users : v.cidr_blocks]),
-#     flatten([for k, v in local.environment_configuration.transfer_server_sftp_users_with_egress : v.cidr_blocks])
-#   )
-# }
-
 resource "aws_vpc_security_group_ingress_rule" "this" {
   for_each = setunion(
     flatten([for k, v in local.environment_configuration.transfer_server_sftp_users : v.cidr_blocks]),
