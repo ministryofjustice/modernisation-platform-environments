d79f7ac520e9bd9155e1efaa89c9d8a0e6743948
Zoltan Paldi <161834373+zoltan-paldi@users.noreply.github.com>
2025-10-03 12:04:24 +0100

    chore(lambda): fix pipeline issue  #XPM-480

--- terraform/environments/xhibit-portal/lambda.tf
+++ terraform/environments/xhibit-portal/lambda.tf
@@ -159,6 +159,7 @@ resource "aws_lambda_function" "delete_old_ami" {
   # checkov:skip=CKV_AWS_116: "DLQ not required"
   # checkov:skip=CKV_AWS_272: "Ensure AWS Lambda function is configured to validate code-signing"
   # checkov:skip=CKV_AWS_363: "Ensure Lambda Runtime is not deprecated"
+  # checkov:skip=CKV_AWS_173: "Check encryption settings for Lambda environmental variable"
   filename         = "lambda/delete_old_ami.zip"
   function_name    = "delete_old_ami"
   role             = aws_iam_role.delete_snapshot_lambda.arn
