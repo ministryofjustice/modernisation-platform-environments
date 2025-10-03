40f1753c1f87cd015c425906932d0d74cd655456
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-10-03 11:32:18 +0100

    changes

--- terraform/environments/ccms-oia/service_adaptor_sg.tf
+++ terraform/environments/ccms-oia/service_adaptor_sg.tf
@@ -1,19 +1,19 @@
 #######################################
-# Connector Load Balancer Security Group
+# adaptor Load Balancer Security Group
 #######################################
 
-resource "aws_security_group" "connector_load_balancer" {
-  name_prefix = "${local.connector_app_name}-load-balancer-sg"
-  description = "Controls access to ${local.connector_app_name} lb"
+resource "aws_security_group" "adaptor_load_balancer" {
+  name_prefix = "${local.adaptor_app_name}-load-balancer-sg"
+  description = "Controls access to ${local.adaptor_app_name} lb"
   vpc_id      = data.aws_vpc.shared.id
 
   tags = merge(local.tags,
-    { Name = lower(format("%s-lb-sg", local.connector_app_name)) }
+    { Name = lower(format("%s-lb-sg", local.adaptor_app_name)) }
   )
 }
 
-resource "aws_security_group_rule" "connector_alb_ingress_443" {
-  security_group_id = aws_security_group.connector_load_balancer.id
+resource "aws_security_group_rule" "adaptor_alb_ingress_443" {
+  security_group_id = aws_security_group.adaptor_load_balancer.id
   type              = "ingress"
   description       = "HTTPS"
   protocol          = "TCP"
@@ -22,8 +22,8 @@ resource "aws_security_group_rule" "connector_alb_ingress_443" {
   cidr_blocks       = [data.aws_subnet.private_subnets_a.cidr_block, data.aws_subnet.private_subnets_b.cidr_block, data.aws_subnet.private_subnets_c.cidr_block]
 }
 
-resource "aws_security_group_rule" "connector_alb_egress_all" {
-  security_group_id = aws_security_group.connector_load_balancer.id
+resource "aws_security_group_rule" "adaptor_alb_egress_all" {
+  security_group_id = aws_security_group.adaptor_load_balancer.id
   type              = "egress"
   description       = "All outbound"
   protocol          = -1
@@ -36,28 +36,28 @@ resource "aws_security_group_rule" "connector_alb_egress_all" {
 # Container Security Group
 #######################################
 
-resource "aws_security_group" "ecs_tasks_connector" {
-  name_prefix = "${local.connector_app_name}-ecs-tasks-security-group"
-  description = "Controls access to ${local.connector_app_name} containers"
+resource "aws_security_group" "ecs_tasks_adaptor" {
+  name_prefix = "${local.adaptor_app_name}-ecs-tasks-security-group"
+  description = "Controls access to ${local.adaptor_app_name} containers"
   vpc_id      = data.aws_vpc.shared.id
 
   tags = merge(local.tags,
-    { Name = lower(format("%s-task-sg", local.connector_app_name)) }
+    { Name = lower(format("%s-task-sg", local.adaptor_app_name)) }
   )
 }
 
-resource "aws_security_group_rule" "ecs_tasks_connector_ingress" {
-  security_group_id        = aws_security_group.ecs_tasks_connector.id
+resource "aws_security_group_rule" "ecs_tasks_adaptor_ingress" {
+  security_group_id        = aws_security_group.ecs_tasks_adaptor.id
   type                     = "ingress"
-  description              = "Connector App Port"
+  description              = "adaptor App Port"
   protocol                 = "TCP"
-  from_port                = local.application_data.accounts[local.environment].connector_server_port
-  to_port                  = local.application_data.accounts[local.environment].connector_server_port
-  source_security_group_id = aws_security_group.connector_load_balancer.id
+  from_port                = local.application_data.accounts[local.environment].adaptor_server_port
+  to_port                  = local.application_data.accounts[local.environment].adaptor_server_port
+  source_security_group_id = aws_security_group.adaptor_load_balancer.id
 }
 
-resource "aws_security_group_rule" "ecs_tasks_connector_egress_all" {
-  security_group_id = aws_security_group.ecs_tasks_connector.id
+resource "aws_security_group_rule" "ecs_tasks_adaptor_egress_all" {
+  security_group_id = aws_security_group.ecs_tasks_adaptor.id
   type              = "egress"
   description       = "All outbound"
   protocol          = -1
