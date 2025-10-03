f4887d6558b52377f7297044fe7eb506dc9cfe78
Dave Kent <dave.kent@justice.gov.uk>
2025-10-03 15:37:56 +0100

    tm-1638-fixing-patchmanager-lambda-logs-new-commit-id

--- terraform/environments/hmpps-domain-services/patch-manager.tf
+++ terraform/environments/hmpps-domain-services/patch-manager.tf
@@ -1,7 +1,7 @@
 module "patch_manager" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions; this is an internal module so commit hashes are not needed
   # source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=3659b4c4d37d5d71bca0ae9f2760cf3541e8d291" # v4.0.1
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=4eca903f9b6f8b761add546c48768237877559d6" # v5.0.0 + my branch
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=230b4533d98221100c34a6d881e35776d1958dd9" # v5.0.0 + my branch
   providers = {
     aws.bucket-replication = aws
   }
