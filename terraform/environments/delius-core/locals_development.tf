b08a2183a3937e9ff90092bbb847f3fcc68d899d
David Teeling <david.teeling1@justice.gov.uk>
2025-10-03 10:21:09 +0100

    ldap cache warmup

--- terraform/environments/delius-core/locals_development.tf
+++ terraform/environments/delius-core/locals_development.tf
@@ -102,7 +102,7 @@ locals {
     }
 
     ldap = {
-      image_tag        = "6.1.3-latest"
+      image_tag        = "testing-18197314155-1"
       container_port   = 389
       slapd_log_level  = "stats"
       container_cpu    = 512
