c11465fcdaa118433193568d4e7d60bb41b97588
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2025-10-06 16:50:16 +0100

    add dis endpoint to lb and make it environment agnostic

--- terraform/environments/delius-mis/locals_development.tf
+++ terraform/environments/delius-mis/locals_development.tf
@@ -201,6 +201,13 @@ locals {
         }
       )
     }
+    # Load balancer configuration for DIS
+    lb_target_config = {
+      endpoint             = "ndl-dis"
+      port                 = 8080
+      health_check_path    = "/BOE/CMC/"
+      health_check_matcher = "200,302,301"
+    }
   }
   # automation test instance only - do not use
   auto_config_dev = {
@@ -290,6 +297,13 @@ locals {
         { backup = true
         }
       )
+    }
+    # Load balancer configuration for DFI
+    lb_target_config = {
+      endpoint             = "ndl-dfi"
+      port                 = 8080
+      health_check_path    = "/DataServices/"
+      health_check_matcher = "200,302,301"
     }
   }
 
