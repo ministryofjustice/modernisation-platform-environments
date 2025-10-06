1428886f4a59895c32faaf03e745daedab03c46f
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-10-06 15:19:28 +0100

    creating another duplicate secret for ccms

--- terraform/environments/laa-enterprise-service-bus/patch_secrets.tf
+++ terraform/environments/laa-enterprise-service-bus/patch_secrets.tf
@@ -1,9 +1,6 @@
 #####################################################################################
 ### Secrets used for CWA Extract Lambda Functions ###
 #####################################################################################
-
-
-
 resource "aws_secretsmanager_secret" "patch_cwa_db_secret" {
   count = local.environment == "test" ? 1 : 0
   name  = "patch-cwa-extract-lambda-db-secret-${local.environment}"
@@ -27,6 +24,16 @@ resource "aws_secretsmanager_secret" "patch_ccms_db_mp_credentials" {
     local.tags,
     {
       Name = "${local.application_name_short}-${local.environment}-patch-ccms-db-mp-credentials"
+    }
+  )
+}
+
+resource "aws_secretsmanager_secret" "patch_ccms_procedures_config" {
+  name = "patch-ccms-provider-lambda-procedures-config-${local.environment}"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${local.application_name_short}-${local.environment}-patch-ccms-provider-lambda-procedures-config"
     }
   )
 }
