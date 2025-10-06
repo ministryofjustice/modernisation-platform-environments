826e8ca9af8a38da6a8d3a35c64d05844052c3cf
matt heery <matt.heery@justice.gov.uk>
2025-10-06 17:21:25 +0100

    fix: update secrets

--- terraform/environments/electronic-monitoring-data/lambda_triggers.tf
+++ terraform/environments/electronic-monitoring-data/lambda_triggers.tf
@@ -100,3 +100,11 @@ resource "aws_cloudwatch_event_rule" "serco_secretsmanager_key_update" {
   })
 }
 
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = module.process_fms_metadata.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.serco_secretsmanager_key_update.arn
+}
+
