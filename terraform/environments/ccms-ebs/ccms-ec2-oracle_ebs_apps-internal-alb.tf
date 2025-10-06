c21aa5e4c712e4cae0741e51f993b49a053e913b
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-10-06 18:38:04 +0100

    Create EBSAPPs Internal ALB

--- terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_apps-internal-alb.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_apps-internal-alb.tf
@@ -45,7 +45,7 @@ resource "aws_lb_listener" "ebsapps_internal_listener" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = data.aws_acm_certificate.gandi_cert.arn
+  certificate_arn   = aws_acm_certificate.external.arn
 
   default_action {
     type             = "forward"
