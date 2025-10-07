a8902afce23361b0df625eb1bb4a206eb5e9e6f4
matt heery <matt.heery@justice.gov.uk>
2025-10-07 16:59:34 +0100

    fix: correct name for lambda func

--- terraform/environments/electronic-monitoring-data/lambda_triggers.tf
+++ terraform/environments/electronic-monitoring-data/lambda_triggers.tf
@@ -108,7 +108,7 @@ resource "aws_cloudwatch_event_target" "serco_secretsmanager_key_update" {
 resource "aws_lambda_permission" "allow_eventbridge" {
   statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
-  function_name = module.rotate_iam_key.lambda_function_name
+  function_name = module.generate_encrypted_key_serco.lambda_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.serco_secretsmanager_key_update.arn
 }
