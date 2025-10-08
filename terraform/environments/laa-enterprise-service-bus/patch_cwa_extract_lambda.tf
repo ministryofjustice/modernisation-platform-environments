ae8bc642b95e1bd550ba7b51ed9f67e55a00e817
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-10-08 17:59:54 +0100

    creating new lambda layer

--- terraform/environments/laa-enterprise-service-bus/patch_cwa_extract_lambda.tf
+++ terraform/environments/laa-enterprise-service-bus/patch_cwa_extract_lambda.tf
@@ -184,7 +184,7 @@ resource "aws_lambda_function" "oracledb_patch_cwa_extract_lambda" {
   runtime          = "python3.10"
 
   layers = [
-    aws_lambda_layer_version.lambda_layer_oracle_python.arn,
+    aws_lambda_layer_version.oracledb_lambda_layer_python.arn,
     "arn:aws:lambda:eu-west-2:017000801446:layer:AWSLambdaPowertoolsPython:2"
   ]
 
