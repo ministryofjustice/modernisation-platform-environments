9e10a68eaf081679a7a7341735d0f15bdf9acb28
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-10-08 10:49:41 +0100

    remove webgate alb

--- terraform/environments/ccms-ebs/ccms-ec2-oracle_webgate-internal-alb.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-oracle_webgate-internal-alb.tf
@@ -1,24 +1,24 @@
-resource "aws_lb" "webgate_lb" {
-  count              = local.is-production ? 1 : 1
-  name               = lower(format("lb-%s-%s-wgate", local.application_name, local.environment))
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg_webgate_lb.id]
-  subnets            = data.aws_subnets.shared-private.ids
+# resource "aws_lb" "webgate_lb" {
+#   count              = local.is-production ? 1 : 1
+#   name               = lower(format("lb-%s-%s-wgate", local.application_name, local.environment))
+#   internal           = true
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.sg_webgate_lb.id]
+#   subnets            = data.aws_subnets.shared-private.ids
 
-  drop_invalid_header_fields = true
-  enable_deletion_protection = false
+#   drop_invalid_header_fields = true
+#   enable_deletion_protection = false
 
-  access_logs {
-    bucket  = module.s3-bucket-logging.bucket.id
-    prefix  = local.lb_log_prefix_wgate
-    enabled = true
-  }
+#   access_logs {
+#     bucket  = module.s3-bucket-logging.bucket.id
+#     prefix  = local.lb_log_prefix_wgate
+#     enabled = true
+#   }
 
-  tags = merge(local.tags,
-    { Name = lower(format("lb-%s-%s-wgate", local.application_name, local.environment)) }
-  )
-}
+#   tags = merge(local.tags,
+#     { Name = lower(format("lb-%s-%s-wgate", local.application_name, local.environment)) }
+#   )
+# }
 
 # resource "aws_lb_target_group" "webgate_internal_tg" {
 #   name     = lower(format("tg-%s-wgate", local.application_name))
