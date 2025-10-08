060de3ae50a8fb6b0ad73914adbfe81343d13a45
Prem Basumatary <prem.basumatary@justice.gov.uk>
2025-10-08 13:37:09 +0100

    TM-1647 fix lambda function name update

--- terraform/environments/delius-core/modules/components/ldap/circuit_breaker/circuit_breaker.tf
+++ terraform/environments/delius-core/modules/components/ldap/circuit_breaker/circuit_breaker.tf
@@ -121,7 +121,7 @@ resource "aws_lambda_function" "circuit_breaker" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "${var.env_name}-ldap-circuit-breaker"
   role             = aws_iam_role.lambda_role.arn
-  handler          = "lambda_function.lambda_handler"
+  handler          = "circuit_breaker.lambda_handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.11"
 
