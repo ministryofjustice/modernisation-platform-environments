6746fd4ab32b08f53342502d381fdcbd7f707357
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-10-08 09:21:29 +0100

    remove delete protection for webgate alb

--- terraform/environments/ccms-ebs/ccms-ec2-oracle_webgate-internal-alb.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-oracle_webgate-internal-alb.tf
@@ -1,8 +1,9 @@
-resource "aws_lb" "webgate_alb_internal" {
-  name               = lower(format("lb-%s-wgate-internal", local.application_name))
+resource "aws_lb" "webgate_lb" {
+  count              = local.is-production ? 1 : 1
+  name               = lower(format("lb-%s-%s-wgate", local.application_name, local.environment))
   internal           = true
   load_balancer_type = "application"
-  # security_groups    = [aws_security_group.sg_webgate_internal_alb.id]
+  security_groups    = [aws_security_group.sg_webgate_lb.id]
   subnets            = data.aws_subnets.shared-private.ids
 
   drop_invalid_header_fields = true
@@ -10,12 +11,12 @@ resource "aws_lb" "webgate_alb_internal" {
 
   access_logs {
     bucket  = module.s3-bucket-logging.bucket.id
-    prefix  = local.lb_log_prefix_webgate_internal
+    prefix  = local.lb_log_prefix_wgate
     enabled = true
   }
 
   tags = merge(local.tags,
-    { Name = lower(format("lb-wgate-internal")) }
+    { Name = lower(format("lb-%s-%s-wgate", local.application_name, local.environment)) }
   )
 }
 
