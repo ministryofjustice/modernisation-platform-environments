142d3cda76da74e42ca538dc31260fd949d676f7
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-10-09 21:06:24 +0000

    fix: update GitHub token secret name to include component name
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/iac/secrets.tf
+++ terraform/environments/data-platform/iac/secrets.tf
@@ -3,7 +3,7 @@ module "github_token_secret" {
 
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=f7fef2d8f63f1595c3e2b0ee14a6810dc7bdb9af" # v2.0.0
 
-  name = "github/token"
+  name = "${local.component_name}/github/token"
 
   secret_string         = "CHANGEME"
   ignore_secret_changes = true
