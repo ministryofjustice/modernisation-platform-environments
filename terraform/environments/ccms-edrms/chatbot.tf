f628ecfb0fbf93ec936438836ed715f0a22adaed
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-09 16:56:59 +0100

    fix: version provider updated to 6.0 with template being 3.0.0

--- terraform/environments/ccms-edrms/chatbot.tf
+++ terraform/environments/ccms-edrms/chatbot.tf
@@ -5,8 +5,7 @@ module "template" {
 
   source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0
 
-  # slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
-  slack_channel_id = local.edrms_secret["alerts_slack_channel_id"]
+  slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
   sns_topic_arns   = ["arn:aws:sns:eu-west-2:${local.environment_management.account_ids[terraform.workspace]}:cloudwatch-slack-alerts"]
   tags             = local.tags
   application_name = local.application_name
