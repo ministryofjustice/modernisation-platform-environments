8c9c4678ffaafe8cabbdb9770eacc6420e1305ff
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2025-10-09 11:21:30 +0100

    added the awscc provider for secure browser

--- terraform/environments/ccms-ebs/workspaces-secure-browser/versions.tf
+++ terraform/environments/ccms-ebs/workspaces-secure-browser/versions.tf
@@ -4,6 +4,10 @@ terraform {
       version = "~> 6.0"
       source  = "hashicorp/aws"
     }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 1.0"
+    }
     dns = {
       version = "~> 3.0"
       source  = "hashicorp/dns"
