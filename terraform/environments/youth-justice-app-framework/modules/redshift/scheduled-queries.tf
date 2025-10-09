b1846c125d97917867d36015f8a52ca4099bf5f4
ttipler <thomas.tipler@necsws.com>
2025-10-09 14:45:05 +0100

    update for static code

--- terraform/environments/youth-justice-app-framework/modules/redshift/scheduled-queries.tf
+++ terraform/environments/youth-justice-app-framework/modules/redshift/scheduled-queries.tf
@@ -1,4 +1,10 @@
 resource "aws_lambda_function" "redshift_scheduler" {
+  #checkov:skip=CKV_AWS_50: "X-Ray tracing is enabled for Lambda" - could be implemented but not required
+  #checkov:skip=CKV_AWS_115: "Ensure that AWS Lambda function is configured for function-level concurrent execution limit" - seems unnecessary for this module, could be added as reserved_concurrent_executions = 100 or similar (smaller) number.
+  #checkov:skip=CKV_AWS_116: "Ensure that AWS Lambda function is configured for a Dead Letter Queue(DLQ)" - not required 
+  #checkov:skip=CKV_AWS_117: "Ensure that AWS Lambda function is configured inside a VPC" - irrelevant for this module
+  #checkov:skip=CKV_AWS_173: "Check encryption settings for Lambda environmental variable" - not required
+  #checkov:skip=CKV_AWS_272: "Ensure AWS Lambda function is configured to validate code-signing" - code signing is not implemented
   function_name = "redshift_scheduler"
   runtime       = "python3.11"
   handler       = "index.handler"
