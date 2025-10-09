d3c1890b918c75a9a07913a313307e2211e8fff9
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2025-10-09 12:51:06 +0100

    previous aws secret put back

--- terraform/environments/ccms-edrms/secrets.tf
+++ terraform/environments/ccms-edrms/secrets.tf
@@ -27,3 +27,24 @@ data "aws_secretsmanager_secret_version" "edrms_secret_version" {
   Individual per-key secret resources were removed so the code reads both
   keys from the single combined secret via locals (see locals.tf).
 */
+
+#### This file can be used to store secrets specific to the member account ####
+
+resource "aws_secretsmanager_secret" "spring_datasource_password" {
+  name        = "ccms/edrms/datasource"
+  description = "EDRMS TDS database password for CCMS EDRMS application"
+}
+
+data "aws_secretsmanager_secret_version" "spring_datasource_password" {
+  secret_id = aws_secretsmanager_secret.spring_datasource_password.id
+}
+
+# Slack Channel ID for Alerts
+resource "aws_secretsmanager_secret" "slack_channel_id" {
+  name        = "alerts_slack_channel_id"
+  description = "Slack Channel ID for EDRMS Alerts"
+}
+
+data "aws_secretsmanager_secret_version" "slack_channel_id" {
+  secret_id = aws_secretsmanager_secret.slack_channel_id.id
+}
