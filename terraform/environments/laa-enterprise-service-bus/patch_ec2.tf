24a5b8eb869fcfb99a10ccfed62810e79193f137
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-10-09 14:34:48 +0100

    Creating EC2 instance for debugging

--- terraform/environments/laa-enterprise-service-bus/patch_ec2.tf
+++ terraform/environments/laa-enterprise-service-bus/patch_ec2.tf
@@ -1,72 +1,73 @@
-# # Security Group allowing SSM (no inbound needed)
-# resource "aws_security_group" "ec2_debug_sg" {
-#   count       = local.environment == "test" ? 1 : 0
-#   name        = "${local.application_name_short}-${local.environment}-ec2-debug-security-group"
-#   description = "EC2 Debug Security Group"
-#   vpc_id      = data.aws_vpc.shared.id
+# Security Group allowing SSM (no inbound needed)
+resource "aws_security_group" "ec2_debug_sg" {
+  count       = local.environment == "test" ? 1 : 0
+  name        = "${local.application_name_short}-${local.environment}-ec2-debug-security-group"
+  description = "EC2 Debug Security Group"
+  vpc_id      = data.aws_vpc.shared.id
 
-#   revoke_rules_on_delete = true
+  revoke_rules_on_delete = true
 
-#   tags = merge(
-#     local.tags,
-#     { Name = "${local.application_name_short}-${local.environment}-patch-cwa-extract-lambda-security-group" }
-#   )
-# }
+  tags = merge(
+    local.tags,
+    { Name = "${local.application_name_short}-${local.environment}-patch-cwa-extract-lambda-security-group" }
+  )
+}
 
-# resource "aws_security_group_rule" "ec2_debug_egress_ecp" {
-#   count             = local.environment == "test" ? 1 : 0
-#   type              = "egress"
-#   from_port         = 2484
-#   to_port           = 2484
-#   protocol          = "tcp"
-#   cidr_blocks       = ["10.205.11.0/26", "10.205.11.64/26"] # Patch CCMS Database IP
-#   security_group_id = aws_security_group.patch_cwa_extract_sg[0].id
-#   description       = "Outbound 2484 Access to CWA DB Safe3 in ECP"
-# }
+resource "aws_security_group_rule" "ec2_debug_egress_ecp" {
+  count             = local.environment == "test" ? 1 : 0
+  type              = "egress"
+  from_port         = 2484
+  to_port           = 2484
+  protocol          = "tcp"
+  cidr_blocks       = ["10.205.11.0/26", "10.205.11.64/26"] # Patch CCMS Database IP
+  security_group_id = aws_security_group.patch_cwa_extract_sg[0].id
+  description       = "Outbound 2484 Access to CWA DB Safe3 in ECP"
+}
 
-# # IAM Role for SSM
-# resource "aws_iam_role" "ec2_debug_role" {
-#   count = local.environment == "test" ? 1 : 0
-#   name  = "ec2-debug-role"
+# IAM Role for SSM
+resource "aws_iam_role" "ec2_debug_role" {
+  count = local.environment == "test" ? 1 : 0
+  name  = "ec2-debug-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         },
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
 
-# # Attach SSM managed policy
-# resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
-#   count      = local.environment == "test" ? 1 : 0
-#   role       = aws_iam_role.ssm_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-# }
+# Attach SSM managed policy
+resource "aws_iam_role_policy_attachment" "ec2_debug_policy_attach" {
+  count      = local.environment == "test" ? 1 : 0
+  role       = aws_iam_role.ec2_debug_role[0].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
-# # IAM Instance Profile
-# resource "aws_iam_instance_profile" "ssm_instance_profile" {
-#   count = local.environment == "test" ? 1 : 0
-#   name  = "ssm-instance-profile"
-#   role  = aws_iam_role.ssm_role.name
-# }
+# IAM Instance Profile
+resource "aws_iam_instance_profile" "ec2_debug_instance_profile" {
+  count = local.environment == "test" ? 1 : 0
+  name  = "ec2-debug-profile"
+  role  = aws_iam_role.ec2_debug_role[0].name
+}
 
-# # EC2 Instance
-# resource "aws_instance" "ssm_instance" {
-#   ami                         = "" # Amazon Linux 2 in us-east-1, change region if needed
-#   instance_type               = "t3.micro"
-#   subnet_id                   = aws_subnet.main.id
-#   vpc_security_group_ids      = [aws_security_group.ssm_sg.id]
-#   iam_instance_profile        = aws_iam_instance_profile.ssm_instance_profile.name
-#   associate_public_ip_address = false
+# EC2 Instance
+resource "aws_instance" "ssm_instance" {
+  count                      = local.environment == "test" ? 1 : 0
+  ami                         = "ami-0336cdd409ab5eec4" # Amazon Linux 2 in us-east-1, change region if needed
+  instance_type               = "t3.micro"
+  subnet_id                   = data.aws_subnet.data_subnets_a.id
+  vpc_security_group_ids      = [aws_security_group.ec2_debug_sg[0].id]
+  iam_instance_profile        = aws_iam_instance_profile.ssm_instance_profile[0].name
+  associate_public_ip_address = false
 
-#   tags = {
-#     Name = "SSM-Instance"
-#   }
-# }
+  tags = {
+    Name = "EC2-Debug-Instance"
+  }
+}
