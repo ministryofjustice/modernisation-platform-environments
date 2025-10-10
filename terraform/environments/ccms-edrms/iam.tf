891e3acc9c05bb4d738261aa10cc891a34bce6b3
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-10 17:30:43 +0100

    fix: resource name corrected

--- terraform/environments/ccms-edrms/iam.tf
+++ terraform/environments/ccms-edrms/iam.tf
@@ -45,43 +45,19 @@ resource "aws_iam_policy" "ecs_secrets_policy" {
     {
       "Effect": "Allow",
       "Action": ["secretsmanager:GetSecretValue"],
-      "Resource": ["arn:aws:secretsmanager:eu-west-2:*:secret:edrms-secret-*"]
+      "Resource": ["arn:aws:secretsmanager:eu-west-2:*:secret:edrms-secret*"]
     }
   ]
 }
 EOF
 }
 
-# ECS Secrets Manager Policy
-resource "aws_iam_policy" "ecs_parameter_store_policy" {
-  name = "${local.application_name}-ecs_parameter_store_policy"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ssm:GetParameter",
-        "ssm:GetParameters"
-      ],
-      "Resource": "arn:aws:ssm:eu-west-2:876083391650:parameter/XXSOA"
-    }
-  ]
-}
-EOF
-}
 
 resource "aws_iam_role_policy_attachment" "ecs_secrets_policy_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.ecs_secrets_policy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_parameter_store_policy_attachment" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = aws_iam_policy.ecs_parameter_store_policy.arn
-}
 
 # EC2 Instance Role
 
