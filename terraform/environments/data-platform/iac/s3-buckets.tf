8505f943188b75413715a5135a842725627e80d8
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-10-14 11:24:02 +0000

    feat: enhance IAM roles and S3 bucket policies for data platform access
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/iac/s3-buckets.tf
+++ terraform/environments/data-platform/iac/s3-buckets.tf
@@ -1,6 +1,93 @@
 data "aws_iam_policy_document" "s3_bucket_policy" {
+  # Explicit deny for all principals except allowed ones
   statement {
-    sid    = "AllowDataPlatformEngineers"
+    sid     = "DenyAllExceptAllowedPrincipals"
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [
+      "arn:aws:s3:::mojdp-${local.environment}-${local.component_name}",
+      "arn:aws:s3:::mojdp-${local.environment}-${local.component_name}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "StringNotLike"
+      variable = "aws:PrincipalArn"
+      values = [
+        module.data_platform_access_iam_role[0].arn,
+        "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/*/AWSReservedSSO_platform-engineer-admin_*"
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalAccount"
+      values   = [local.environment_management.account_ids["data-platform-development"]]
+    }
+  }
+
+  # Allow bucket-level actions for data platform access role
+  statement {
+    sid    = "AllowDataPlatformAccessBucket"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketVersions"
+    ]
+    resources = ["arn:aws:s3:::mojdp-${local.environment}-${local.component_name}"]
+    principals {
+      type        = "AWS"
+      identifiers = [module.data_platform_access_iam_role[0].arn]
+    }
+  }
+
+  # Allow object-level actions for data platform access role
+  statement {
+    sid    = "AllowDataPlatformAccessObjects"
+    effect = "Allow"
+    actions = [
+      "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+    resources = ["arn:aws:s3:::mojdp-${local.environment}-${local.component_name}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [module.data_platform_access_iam_role[0].arn]
+    }
+  }
+
+  # Allow bucket-level actions for platform engineers
+  statement {
+    sid    = "AllowDataPlatformEngineersBucket"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketVersions"
+    ]
+    resources = ["arn:aws:s3:::mojdp-${local.environment}-${local.component_name}"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "aws:PrincipalArn"
+      values   = ["arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/*/AWSReservedSSO_platform-engineer-admin_*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalAccount"
+      values   = [local.environment_management.account_ids["data-platform-development"]]
+    }
+  }
+
+  # Allow object-level actions for platform engineers
+  statement {
+    sid    = "AllowDataPlatformEngineersObjects"
     effect = "Allow"
     actions = [
       "s3:DeleteObject",
