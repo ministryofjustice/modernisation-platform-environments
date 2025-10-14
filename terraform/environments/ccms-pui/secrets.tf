474a237cb81cf8f7ccd6a84c775693a105e8e63d
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-10-14 14:26:52 +0100

    remove individual secrets

--- terraform/environments/ccms-pui/secrets.tf
+++ terraform/environments/ccms-pui/secrets.tf
@@ -39,72 +39,72 @@ data "aws_secretsmanager_secret_version" "pui_secrets" {
   secret_id = aws_secretsmanager_secret.pui_secrets.id
 }
 
-# CCMS EBS Database Password
-resource "aws_secretsmanager_secret" "spring_datasource_password" {
-  name        = "spring_datasource_password"
-  description = "CCMS EBS Database Password for user XXCCMS_PUI"
-}
+# # CCMS EBS Database Password
+# resource "aws_secretsmanager_secret" "spring_datasource_password" {
+#   name        = "spring_datasource_password"
+#   description = "CCMS EBS Database Password for user XXCCMS_PUI"
+# }
 
-data "aws_secretsmanager_secret_version" "spring_datasource_password" {
-  secret_id = aws_secretsmanager_secret.spring_datasource_password.id
-}
+# data "aws_secretsmanager_secret_version" "spring_datasource_password" {
+#   secret_id = aws_secretsmanager_secret.spring_datasource_password.id
+# }
 
-# Portal Certificate - not required but using as placeholder
-resource "aws_secretsmanager_secret" "portal_certificate" {
-  name        = "portal_certificate"
-  description = "Portal Certificate"
-}
+# # Portal Certificate - not required but using as placeholder
+# resource "aws_secretsmanager_secret" "portal_certificate" {
+#   name        = "portal_certificate"
+#   description = "Portal Certificate"
+# }
 
-data "aws_secretsmanager_secret_version" "portal_certificate" {
-  secret_id = aws_secretsmanager_secret.portal_certificate.id
-}
+# data "aws_secretsmanager_secret_version" "portal_certificate" {
+#   secret_id = aws_secretsmanager_secret.portal_certificate.id
+# }
 
-# SP Certificate - not sure what this is for - placeholder
-resource "aws_secretsmanager_secret" "spcert" {
-  name        = "spcert"
-  description = "SP Certificate"
-}
+# # SP Certificate - not sure what this is for - placeholder
+# resource "aws_secretsmanager_secret" "spcert" {
+#   name        = "spcert"
+#   description = "SP Certificate"
+# }
 
-data "aws_secretsmanager_secret_version" "spcert" {
-  secret_id = aws_secretsmanager_secret.spcert.id
-}
+# data "aws_secretsmanager_secret_version" "spcert" {
+#   secret_id = aws_secretsmanager_secret.spcert.id
+# }
 
-# SP Private Key - not sure what this is for - placeholder
-resource "aws_secretsmanager_secret" "spprivatekey" {
-  name        = "spprivatekey"
-  description = "SP Private Key"
-}
+# # SP Private Key - not sure what this is for - placeholder
+# resource "aws_secretsmanager_secret" "spprivatekey" {
+#   name        = "spprivatekey"
+#   description = "SP Private Key"
+# }
 
-data "aws_secretsmanager_secret_version" "spprivatekey" {
-  secret_id = aws_secretsmanager_secret.spprivatekey.id
-}
+# data "aws_secretsmanager_secret_version" "spprivatekey" {
+#   secret_id = aws_secretsmanager_secret.spprivatekey.id
+# }
 
-# PostCode API Key - not sure what this is for - placeholder
-resource "aws_secretsmanager_secret" "postcodeApiKey" {
-  name        = "postcodeApiKey"
-  description = "Postcode API Key"
-}
+# # PostCode API Key - not sure what this is for - placeholder
+# resource "aws_secretsmanager_secret" "postcodeApiKey" {
+#   name        = "postcodeApiKey"
+#   description = "Postcode API Key"
+# }
 
-data "aws_secretsmanager_secret_version" "postcodeApiKey" {
-  secret_id = aws_secretsmanager_secret.postcodeApiKey.id
-}
+# data "aws_secretsmanager_secret_version" "postcodeApiKey" {
+#   secret_id = aws_secretsmanager_secret.postcodeApiKey.id
+# }
 
-# SOA SOAP Header User Password
-resource "aws_secretsmanager_secret" "ccms_soa_soapHeaderUserPassword" {
-  name        = "ccms_soa_soapHeaderUserPassword"
-  description = "SOA SOAP Header User Password"
-}
+# # SOA SOAP Header User Password
+# resource "aws_secretsmanager_secret" "ccms_soa_soapHeaderUserPassword" {
+#   name        = "ccms_soa_soapHeaderUserPassword"
+#   description = "SOA SOAP Header User Password"
+# }
 
-data "aws_secretsmanager_secret_version" "ccms_soa_soapHeaderUserPassword" {
-  secret_id = aws_secretsmanager_secret.ccms_soa_soapHeaderUserPassword.id
-}
+# data "aws_secretsmanager_secret_version" "ccms_soa_soapHeaderUserPassword" {
+#   secret_id = aws_secretsmanager_secret.ccms_soa_soapHeaderUserPassword.id
+# }
 
-# User Management API Access Token
-resource "aws_secretsmanager_secret" "user_management_api_access_token" {
-  name        = "user_management_api_access_token"
-  description = "User Management API Access Token"
-}
+# # User Management API Access Token
+# resource "aws_secretsmanager_secret" "user_management_api_access_token" {
+#   name        = "user_management_api_access_token"
+#   description = "User Management API Access Token"
+# }
 
-data "aws_secretsmanager_secret_version" "user_management_api_access_token" {
-  secret_id = aws_secretsmanager_secret.user_management_api_access_token.id
-}
+# data "aws_secretsmanager_secret_version" "user_management_api_access_token" {
+#   secret_id = aws_secretsmanager_secret.user_management_api_access_token.id
+# }
