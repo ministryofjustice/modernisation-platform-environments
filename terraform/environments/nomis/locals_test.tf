06a687df3deb774572ef5fb6656faa390b539548
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-10-14 15:54:12 +0100

    add 19c db test

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -279,6 +279,33 @@ locals {
         })
       })
 
+      t1-nomis-db19c-1-a = merge(local.ec2_instances.db19c, {
+        cloudwatch_metric_alarms = {}
+        config = merge(local.ec2_instances.db19c.config, {
+          availability_zone = "eu-west-2a"
+          instance_profile_policies = concat(local.ec2_instances.db19c.config.instance_profile_policies, [
+            "Ec2T1DatabasePolicy",
+          ])
+        })
+        ebs_volumes = merge(local.ec2_instances.db19c.ebs_volumes, {
+          "/dev/sdb" = { label = "app", size = 100 }
+          "/dev/sdc" = { label = "app", size = 100 }
+        })
+        ebs_volume_config = merge(local.ec2_instances.db19c.ebs_volume_config, {
+          data  = { total_size = 500 }
+          flash = { total_size = 50 }
+        })
+        instance = merge(local.ec2_instances.db19c.instance, {
+          disable_api_termination = true
+        })
+        tags = merge(local.ec2_instances.db19c.tags, {
+          description         = "T1 NOMIS database 19c"
+          instance-scheduling = "skip-scheduling"
+          nomis-environment   = "t1"
+          oracle-sids         = ""
+        })
+      })
+
       t1-nomis-db-2-a = merge(local.ec2_instances.db, {
         cloudwatch_metric_alarms = merge(
           local.cloudwatch_metric_alarms.db,
