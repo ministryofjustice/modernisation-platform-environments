a20d2929ea0fa7304f5c3d14f7e964a0d278935f
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-15 16:46:32 +0100

    chore: test wrong value again

--- terraform/environments/ccms-edrms/tds_database.tf
+++ terraform/environments/ccms-edrms/tds_database.tf
@@ -27,7 +27,8 @@ resource "aws_db_instance" "tds_db" {
   db_name                             = "EDRMSTDS"
   username                            = local.application_data.accounts[local.environment].tds_db_user
   # password                            = data.aws_secretsmanager_secret_version.spring_datasource_password.secret_string
-  password                            = jsondecode(data.aws_secretsmanager_secret_version.edrms_secret_version_current.secret_string)["spring_datasource_password"]
+  # password                            = jsondecode(data.aws_secretsmanager_secret_version.edrms_secret_version_current.secret_string)["spring_datasource_password"]
+  password                            = "${aws_secretsmanager_secret.edrms_secret.arn}:spring_datasource_password::"
   port                                = "1521"
   kms_key_id                          = data.aws_kms_key.rds_shared.arn
   storage_encrypted                   = true
