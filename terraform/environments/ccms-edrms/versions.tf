0072b88b3a95f9f24b826801770875f890a41981
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-15 17:27:38 +0100

    chore: reverting provider version

--- terraform/environments/ccms-edrms/versions.tf
+++ terraform/environments/ccms-edrms/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 6.0"
+      version = "~> 5.0"
       source  = "hashicorp/aws"
     }
     http = {
@@ -9,7 +9,7 @@ terraform {
       source  = "hashicorp/http"
     }
     template = {
-      version = ">= 2.2.0"
+      version = ">= 2.0.0"
       source  = "hashicorp/template"
     }
   }
