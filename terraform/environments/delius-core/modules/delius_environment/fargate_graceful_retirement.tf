90dd4da0fb4293a5e5231676fb56a5e463d0ca9f
Prem Basumatary <prem.basumatary@justice.gov.uk>
2025-10-16 11:36:02 +0100

    TM-1650 update time for testing

--- terraform/environments/delius-core/modules/delius_environment/fargate_graceful_retirement.tf
+++ terraform/environments/delius-core/modules/delius_environment/fargate_graceful_retirement.tf
@@ -1,7 +1,7 @@
 locals {
   # Restart schedules for envs, testing for dev only now
   fargate_restart_schedules = {
-    dev  = { day = "THURSDAY", time = "11:30" }
+    dev  = { day = "THURSDAY", time = "11:45" }
     poc  = { day = "MONDAY", time = "20:00" }
     test = { day = "TUESDAY", time = "22:00" }
   }
