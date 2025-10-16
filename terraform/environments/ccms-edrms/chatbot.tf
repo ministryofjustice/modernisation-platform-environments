d2562cd65740011ef5c34f7aa9ec2ed99d0b02ba
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-16 09:20:39 +0100

    chore: reinstating provider version

--- terraform/environments/ccms-edrms/chatbot.tf
+++ terraform/environments/ccms-edrms/chatbot.tf
@@ -3,9 +3,9 @@
 
 module "template" {
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=73280f80ce8a4557cec3a76ee56eb913452ca9aa" # v2.0.0
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0
 
-  slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
+  slack_channel_id = jsondecode(data.aws_secretsmanager_secret_version.edrms_secret_version_current.secret_string)["alerts_slack_channel_id"]
   sns_topic_arns   = ["arn:aws:sns:eu-west-2:${local.environment_management.account_ids[terraform.workspace]}:cloudwatch-slack-alerts"]
   tags             = local.tags
   application_name = local.application_name
