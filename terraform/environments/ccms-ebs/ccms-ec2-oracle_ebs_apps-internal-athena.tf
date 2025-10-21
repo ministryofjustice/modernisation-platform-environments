ae26c61cf55a7ce640ec98fe9c6494543830036d
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-21 22:43:52 +0100

    feat: adding athena for internal lb logs

--- terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_apps-internal-athena.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_apps-internal-athena.tf
@@ -4,13 +4,6 @@ data "aws_s3_bucket" "data_bucket" {
   bucket = "ccms-ebs-development-logging"
 }
 
-
-# Glue Catalog Database
-data "aws_glue_catalog_database" "athena_internal_lb_logs_db" {
-  name = "loadbalancer_access_logs"
-}
-
-
 # IAM Role for Glue Crawler
 resource "aws_iam_role" "glue_role" {
   name = "glue-crawler-role"
@@ -34,12 +27,16 @@ resource "aws_iam_role_policy_attachment" "glue_policy_attach" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
 
+# Glue Catalog Database
+resource "aws_glue_catalog_database" "logs_database" {
+  name = "internal_loadbalancer_access_logs"
+}
 
 # Glue Crawler
 resource "aws_glue_crawler" "internal_lb_logs_crawler" {
   name          = "internal-lb-logs-crawler"
   role          = aws_iam_role.glue_role.arn
-  database_name = data.aws_glue_catalog_database.athena_internal_lb_logs_db.name
+  database_name = aws.glue_catalog_database.logs_database.name
   table_prefix  = "internal_lb_logs"
 
   s3_target {
