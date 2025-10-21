aac5d220f1735545c0642c78e041fbcc6dab0496
Gary <26419401+Gary-H9@users.noreply.github.com>
2025-10-21 08:59:55 +0100

    space

--- terraform/environments/analytical-platform-ingestion/modules/transfer-family/user-with-egress/main.tf
+++ terraform/environments/analytical-platform-ingestion/modules/transfer-family/user-with-egress/main.tf
@@ -32,7 +32,6 @@ data "aws_iam_policy_document" "this" {
       "s3:PutObject",
       "s3:GetObject",
       "s3:DeleteObject"
-
     ]
     resources = ["arn:aws:s3:::${var.landing_bucket}/${var.name}/*"]
   }
