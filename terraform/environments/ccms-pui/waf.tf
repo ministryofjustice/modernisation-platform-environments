1d99d4cbdaa89baac1ca8256a04e9a8349be99ff
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-10-22 11:18:07 +0100

    update waf rules

--- terraform/environments/ccms-pui/waf.tf
+++ terraform/environments/ccms-pui/waf.tf
@@ -19,6 +19,9 @@ resource "aws_wafv2_ip_set" "pui_waf_ip_set" {
       local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_a,
       local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_b,
       local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_c,
+      "35.176.254.38/32", # Temp AWS PROD Workspace
+      "35.177.173.197/32", # Temp AWS PROD Workspace
+      "52.56.212.11/32" # Temp AWS PROD Workspace
     ]
   )
 
