a7fbf53a775c570cd40005b2c966808ac62b5d2d
nbuckingham72 <nick.buckingham@colt.net>
2025-10-22 13:04:19 +0100

    Update_221025_3

--- terraform/environments/ppud/waf.tf
+++ terraform/environments/ppud/waf.tf
@@ -11,7 +11,7 @@ locals {
 }
 
 module "waf" {
-  source                   = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-aws-waf?ref=b9cf6f92b142e80845ae30252aee2f84f57a71a9"
+  source                   = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-aws-waf.git?ref=feature/adding-custom-rules"
   enable_ddos_protection   = true
   ddos_rate_limit          = 150
   block_non_uk_traffic     = true
@@ -22,13 +22,13 @@ module "waf" {
     aws.modernisation-platform = aws.modernisation-platform
   }
 
- #additional_managed_rules = [
- #   {
- #     arn             = aws_wafv2_rule_group.wam_waf_acl.arn
- #     override_action = "none"   # respect the group's action (BLOCK). Use "count" to dry-run.
- #     priority        = 9        # unique; runs before managed rules at 10..15
- #   }
- # ]
+ additional_managed_rules = [
+    {
+      arn             = aws_wafv2_rule_group.wam_waf_acl.arn
+      override_action = "none"   # respect the group's action (BLOCK). Use "count" to dry-run.
+      priority        = 9        # unique; runs before managed rules at 10..15
+    }
+  ]
 
   managed_rule_actions = {
     AWSManagedRulesKnownBadInputsRuleSet = false
