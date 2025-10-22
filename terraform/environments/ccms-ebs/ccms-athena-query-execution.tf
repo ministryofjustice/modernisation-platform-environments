0cc4ab2ff6b6a660f1e2d88807308f8c248374ce
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-22 14:12:21 +0100

    chore: switching of role before local-exec

--- terraform/environments/ccms-ebs/ccms-athena-query-execution.tf
+++ terraform/environments/ccms-ebs/ccms-athena-query-execution.tf
@@ -6,6 +6,10 @@ resource "null_resource" "execute_create_table_queries" {
 
   provisioner "local-exec" {
     command = <<EOF
+CREDS=$(aws sts assume-role --role-arn arn:aws:iam::${data.aws_caller_identity.current.id}:role/MemberInfrastructureAccess --role-session-name github-actions-session)
+export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r '.Credentials.AccessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.Credentials.SecretAccessKey')
+export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.main_table_ebsapp_internal.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
