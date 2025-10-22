45a916a8eaf8855da82557b283c707114b34d58e
matt heery <matt.heery@justice.gov.uk>
2025-10-22 11:18:21 +0100

    fix: add _staging to lake formation dbs

--- terraform/environments/electronic-monitoring-data/modules/ap_airflow_load_data_iam_role/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/ap_airflow_load_data_iam_role/main.tf
@@ -163,7 +163,7 @@ module "ap_database_sharing" {
 
 module "share_dbs_with_roles" {
   source                  = "../lakeformation_database_share"
-  dbs_to_grant            = toset([local.snake-database])
+  dbs_to_grant            = toset([local.snake-database, "${local.snake-database}_staging"])
   data_bucket_lf_resource = var.data_bucket_lf_resource
   role_arn                = module.ap_database_sharing.iam_role.arn
   de_role_arn             = var.de_role_arn
