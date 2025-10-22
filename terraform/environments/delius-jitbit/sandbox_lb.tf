0e8c27c3fa18ef0ccc74cc016fcb8523f9800b72
David Teeling <david.teeling1@justice.gov.uk>
2025-10-22 16:35:36 +0100

    Create sandbox loadbalancer

--- terraform/environments/delius-jitbit/sandbox_lb.tf
+++ terraform/environments/delius-jitbit/sandbox_lb.tf
@@ -11,19 +11,81 @@ locals {
   )
 }
 
-resource "aws_lb_listener_rule" "listener_rule" {
+#tfsec:ignore:aws-elb-alb-not-public
+resource "aws_lb" "external_sandbox" {
+  # checkov:skip=CKV_AWS_91
+  # checkov:skip=CKV2_AWS_28
+  count = local.is-development ? 1 : 0
+
+  name               = "${local.application_name}-lb-sandbox"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+  subnets            = data.aws_subnets.shared-public.ids
+
+  enable_deletion_protection = true
+  drop_invalid_header_fields = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name = local.application_name
+    }
+  )
+}
+
+resource "aws_lb_listener" "listener_sandbox" {
+  count = local.is-development ? 1 : 0
+
+  load_balancer_arn = aws_lb.external_sandbox[0].id
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.external.arn
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+
+  default_action {
+    target_group_arn = local.active_target_group_arn
+    type             = "forward"
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = local.application_name
+    }
+  )
+}
+
+resource "aws_lb_listener_rule" "listener_rule_blue" {
   count        = local.is-development ? 1 : 0
-  listener_arn = aws_lb_listener.listener.arn
+  listener_arn = aws_lb_listener.listener_sandbox[0].arn
   priority     = 10
 
   action {
-    target_group_arn = local.active_target_group_arn
+    target_group_arn = aws_lb_target_group.target_group_fargate_sandbox_blue[0].arn
     type             = "forward"
   }
 
   condition {
     host_header {
-      values = [local.sandbox_app_url]
+      values = ["blue-${local.sandbox_app_url}"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "listener_rule_green" {
+  count        = local.is-development ? 1 : 0
+  listener_arn = aws_lb_listener.listener_sandbox[0].arn
+  priority     = 10
+
+  action {
+    target_group_arn = aws_lb_target_group.target_group_fargate_sandbox_green[0].arn
+    type             = "forward"
+  }
+
+  condition {
+    host_header {
+      values = ["green-${local.sandbox_app_url}"]
     }
   }
 }
