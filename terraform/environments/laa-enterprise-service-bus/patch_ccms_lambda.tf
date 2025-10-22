9b988c6f126d655800ae3a2cc78c788bb5c4ea1e
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-10-22 12:50:04 +0100

    removing dead letter config for dry run lambda too

--- terraform/environments/laa-enterprise-service-bus/patch_ccms_lambda.tf
+++ terraform/environments/laa-enterprise-service-bus/patch_ccms_lambda.tf
@@ -58,10 +58,6 @@ resource "aws_lambda_function" "patch_ccms_provider_load" {
     aws_lambda_layer_version.lambda_layer_oracle_python.arn
   ]
 
-  dead_letter_config {
-    target_arn = aws_sqs_queue.patch_ccms_provider_dlq[0].arn
-  }
-
   vpc_config {
     security_group_ids = [aws_security_group.patch_ccms_provider_load[0].id]
     subnet_ids         = [data.aws_subnet.data_subnets_a.id]
