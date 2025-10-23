b681e1fb10d75da4a9abfb9d427882913feca061
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-24 00:06:51 +0100

    fix: policy modified as per doc

--- terraform/environments/laa-ccms-soa/s3.tf
+++ terraform/environments/laa-ccms-soa/s3.tf
@@ -84,14 +84,28 @@ resource "aws_s3_bucket_policy" "lb_access_logs" {
 
   policy = jsonencode({
     Version = "2012-10-17"
+    Id = "AWSLogDeliveryWrite"
     Statement = [
       # Allow NLB (network) log delivery principal (elasticloadbalancing.amazonaws.com).
       # If the bucket is in a different account, set the aws:SourceAccount to the LB account id.
       {
         Sid = "AllowNLBLogDelivery"
         Effect = "Allow"
-        Principal = { Service = "elasticloadbalancing.amazonaws.com" }
-        Action    = [ "s3:PutObject", "s3:PutObjectAcl" ]
+        Principal = { Service = "delivery.logs.amazonaws.com" }
+        Action    = [ "s3:GetBucketAcl" ]
+        Resource  = "${module.s3-bucket-logging.bucket.arn}/*"
+        Condition = {
+          # If bucket is in the same account as the load balancer, this keeps it scoped to your account:
+          StringEquals = { "aws:SourceAccount" = data.aws_caller_identity.current.account_id }
+          # Restrict to network load balancers only (optional but recommended)
+          ArnLike = { "aws:SourceArn" = "arn:aws:elasticloadbalancing:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:loadbalancer/net/*" }
+        }
+      },
+      {
+        Sid = "AllowNLBLogDelivery"
+        Effect = "Allow"
+        Principal = { Service = "delivery.logs.amazonaws.com" }
+        Action    = [ "s3:PutObject" ]
         Resource  = "${module.s3-bucket-logging.bucket.arn}/*"
         Condition = {
           # If bucket is in the same account as the load balancer, this keeps it scoped to your account:
