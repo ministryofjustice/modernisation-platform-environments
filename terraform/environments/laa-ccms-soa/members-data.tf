baf2a77dcb9ca5fe67f7ca87f3a6f034ef80edaf
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-23 14:52:46 +0100

    feat: policy removed

--- terraform/environments/laa-ccms-soa/members-data.tf
+++ terraform/environments/laa-ccms-soa/members-data.tf
@@ -1,22 +1,22 @@
 ## S3 NOTIFICATIONS
-data "aws_iam_policy_document" "s3_topic_policy" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "s3_topic_policy" {
+#   statement {
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["s3.amazonaws.com"]
+#     }
 
-    actions   = ["SNS:Publish"]
-    resources = ["arn:aws:sns:*:*:s3-event-notification-topic"]
+#     actions   = ["SNS:Publish"]
+#     resources = ["arn:aws:sns:*:*:s3-event-notification-topic"]
 
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values = [
-        module.s3-bucket-logging.bucket.arn
-      ]
-    }
-  }
-}
+#     condition {
+#       test     = "ArnLike"
+#       variable = "aws:SourceArn"
+#       values = [
+#         module.s3-bucket-logging.bucket.arn
+#       ]
+#     }
+#   }
+# }
