cff252fa32ae74b41f9ef5b3ed8f8b9108d40327
ttipler <thomas.tipler@necsws.com>
2025-10-27 09:53:45 +0000

    WAF logging

--- terraform/environments/youth-justice-app-framework/modules/waf/logging.tf
+++ terraform/environments/youth-justice-app-framework/modules/waf/logging.tf
@@ -1,66 +1,70 @@
 resource "aws_cloudwatch_log_group" "waf_logs" {
-  name              = "/aws/waf/${var.waf_name}-logs"
+  name              = "aws-waf-logs-${var.waf_name}" # must start with aws-waf-logs-
   retention_in_days = 30
   tags              = local.tags
 }
 
+data "aws_iam_policy_document" "waf_logging" {
+  version = "2012-10-17"
 
-data "aws_iam_policy_document" "firehose_assume_role" {
   statement {
-    actions = ["sts:AssumeRole"]
+    effect = "Allow"
+
     principals {
       type        = "Service"
-      identifiers = ["firehose.amazonaws.com"]
+      identifiers = ["wafv2.amazonaws.com"]
+    }
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.waf_logs.arn}:*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [tostring(data.aws_caller_identity.current.account_id)]
     }
   }
 }
 
-resource "aws_iam_role" "firehose_role" {
-  name               = "${var.waf_name}-firehose-role"
-  assume_role_policy = data.aws_iam_policy_document.firehose_assume_role.json
-}
-
-data "aws_iam_policy_document" "firehose_policy" {
-  statement {
-    actions = [
-      "logs:PutLogEvents",
-      "logs:CreateLogStream",
-      "logs:DescribeLogGroups",
-      "logs:DescribeLogStreams"
-    ]
-    resources = ["${aws_cloudwatch_log_group.waf_logs.arn}:*"]
-  }
-}
-
-resource "aws_iam_role_policy" "firehose_policy" {
-  role   = aws_iam_role.firehose_role.id
-  policy = data.aws_iam_policy_document.firehose_policy.json
-}
-
-
-resource "aws_kinesis_firehose_delivery_stream" "waf_logs" {
-  name        = "${var.waf_name}-waf-logs"
-  destination = "cloudwatch_logs"
-
-  cloudwatch_logs_configuration {
-    log_group_name  = aws_cloudwatch_log_group.waf_logs.name
-    log_stream_name = "waf-logs-stream"
-  }
-
-  role_arn = aws_iam_role.firehose_role.arn
+resource "aws_cloudwatch_log_resource_policy" "waf_logs_policy" {
+  policy_name     = "waf-logs-policy-${var.waf_name}"
+  policy_document = data.aws_iam_policy_document.waf_logging.json
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "regional" {
   count = var.scope != "CLOUDFRONT" ? 1 : 0
 
   resource_arn            = aws_wafv2_web_acl.waf[0].arn
-  log_destination_configs = [aws_kinesis_firehose_delivery_stream.waf_logs.arn]
+  log_destination_configs = [aws_cloudwatch_log_group.waf_logs.arn]
 
   redacted_fields {
     single_header {
       name = "authorization"
     }
   }
+
+  depends_on = [aws_cloudwatch_log_resource_policy.waf_logs_policy]
+}
+
+
+#Must create log group in us-east-1
+resource "aws_cloudwatch_log_group" "waf_logs_cf" {
+  provider          = aws.us-east-1
+  name              = "aws-waf-logs-${var.waf_name}-cf"
+  retention_in_days = 30
+  tags              = local.tags
+}
+
+resource "aws_cloudwatch_log_resource_policy" "waf_logs_policy_cf" {
+  provider        = aws.us-east-1
+  policy_name     = "waf-logs-policy-${var.waf_name}-cf"
+  policy_document = data.aws_iam_policy_document.waf_logging.json
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "cloudfront" {
@@ -68,11 +72,15 @@ resource "aws_wafv2_web_acl_logging_configuration" "cloudfront" {
   provider = aws.us-east-1
 
   resource_arn            = aws_wafv2_web_acl.cf[0].arn
-  log_destination_configs = [aws_kinesis_firehose_delivery_stream.waf_logs.arn]
+  log_destination_configs = [aws_cloudwatch_log_group.waf_logs_cf.arn]
 
   redacted_fields {
     single_header {
       name = "authorization"
     }
   }
+
+  depends_on = [aws_cloudwatch_log_resource_policy.waf_logs_policy_cf]
 }
+
+data "aws_caller_identity" "current" {}
