d4469c53e96f7e25999ffea12a446eaea2fb8647
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2025-10-27 16:00:00 +0000

    add comment to KMS key

--- terraform/environments/analytical-platform-ingestion/environment-configuration.tf
+++ terraform/environments/analytical-platform-ingestion/environment-configuration.tf
@@ -105,7 +105,7 @@ locals {
       /* Target KMS */
       target_kms_keys = [
         "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["analytical-platform-data-production"]}:key/62503ba6-316e-473d-ae4b-042f8420dd07", # s3/mojap-data-production-shared-services-client-team-gov-29148
-        "arn:aws:kms:eu-west-2:437720536440:key/d4cd0acf-5b4f-461f-ba01-886f814afec5"
+        "arn:aws:kms:eu-west-2:437720536440:key/d4cd0acf-5b4f-461f-ba01-886f814afec5"                                                                        # s3/property-datahub-landing-production
       ]
       mojap_land_kms_key                  = "arn:aws:kms:eu-west-1:${local.environment_management.account_ids["analytical-platform-data-production"]}:key/2855ac30-4e14-482e-85ca-53258e01f64c"
       datasync_opg_target_bucket_kms      = "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["analytical-platform-data-production"]}:key/96eb04fe-8393-402c-b1f9-71fcece99e75"
