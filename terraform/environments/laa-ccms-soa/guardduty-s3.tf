aac30d42a642573fd316d9687dd08445d506035e
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-28 10:35:58 +0000

    feat: establish alert with slack channel first

--- terraform/environments/laa-ccms-soa/guardduty-s3.tf
+++ terraform/environments/laa-ccms-soa/guardduty-s3.tf
@@ -20,51 +20,10 @@ resource "aws_guardduty_malware_protection_plan" "s3_scan" {
   tags = merge(local.tags,
     { Name = lower(format("s3-%s-%s-logging", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
   )
+
+  depends_on = [ module.s3-bucket-logging ]
 }
 
-resource "aws_iam_role" "guardduty_s3_scan" {
-  name = "GuardDutyS3ScanRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "malware-protection-plan.guardduty.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "guardduty_s3_scan_policy" {
-  name        = "GuardDutyS3ScanPolicy"
-  description = "Allows GuardDuty to scan S3 bucket and tag objects"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowS3Access",
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:PutObjectTagging",
-          "s3:GetBucketLocation"
-        ],
-        Resource = [
-          "${module.s3-bucket-logging.bucket.arn}",
-          "${module.s3-bucket-logging.bucket.arn}/*"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_guardduty_policy" {
-  role       = aws_iam_role.guardduty_s3_scan.name
-  policy_arn = aws_iam_policy.guardduty_s3_scan_policy.arn
+data "aws_iam_role" "guardduty_s3_scan" {
+  name = "GuardDutyS3MalwareScanRole-0c025c41-d0be-4fe9-af40-bacce35b6021"
 }
--- terraform/environments/laa-ccms-soa/monitoring.tf
+++ terraform/environments/laa-ccms-soa/monitoring.tf
@@ -23,17 +23,3 @@ module "pagerduty_core_alerts" {
   sns_topics                = [aws_sns_topic.ddos_alarm.name]
   pagerduty_integration_key = local.pagerduty_integration_keys["ddos_cloudwatch"]
 }
-
-# Chatbot Configuration for CCMS EDRMS Cloudwatch Alerts
-
-
-module "template" {
-
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0
-
-  slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
-  sns_topic_arns   = ["arn:aws:sns:eu-west-2:${local.environment_management.account_ids[terraform.workspace]}:cloudwatch-slack-alerts"]
-  tags             = local.tags
-  application_name = local.application_name
-
-}
