18d18b67df2826d1c3aefda19911d499cd8c15b2
matt heery <matt.heery@justice.gov.uk>
2025-10-28 08:54:38 +0000

    chore: remove duplicate

--- terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
+++ terraform/environments/electronic-monitoring-data/ap_airflow_iam.tf
@@ -414,25 +414,6 @@ module "load_telephony_database" {
   cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
 }
 
-module "load_unstructured_atrium_database" {
-  count  = local.is-production ? 1 : 0
-  source = "./modules/ap_airflow_load_data_iam_role"
-
-  data_bucket_lf_resource = aws_lakeformation_resource.data_bucket.arn
-  de_role_arn             = try(one(data.aws_iam_roles.mod_plat_roles.arns))
-
-  name               = "unstructured-atrium"
-  environment        = local.environment
-  database_name      = "g4s-atrium-unstructured"
-  path_to_data       = "/load/g4s_atrium_unstructured/structure"
-  source_data_bucket = module.s3-json-directory-structure-bucket.bucket
-  secret_code        = jsondecode(data.aws_secretsmanager_secret_version.airflow_secret.secret_string)["oidc_cluster_identifier"]
-  oidc_arn           = aws_iam_openid_connect_provider.analytical_platform_compute.arn
-  athena_dump_bucket = module.s3-athena-bucket.bucket
-  cadt_bucket        = module.s3-create-a-derived-table-bucket.bucket
-}
-
-
 module "load_fms" {
   count  = local.is-development ? 0 : 1
   source = "./modules/ap_airflow_load_data_iam_role"
