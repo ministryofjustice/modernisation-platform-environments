9255754a00143af63f1d578bd05cb44a66a0af05
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2025-10-28 11:46:53 +0000

    chore:commented out pagerduty

--- terraform/environments/laa-ccms-soa/monitoring.tf
+++ terraform/environments/laa-ccms-soa/monitoring.tf
@@ -1,25 +1,25 @@
-## Pager duty integration
-# Get the map of pagerduty integration keys from the modernisation platform account
-data "aws_secretsmanager_secret" "pagerduty_integration_keys" {
-  provider = aws.modernisation-platform
-  name     = "pagerduty_integration_keys"
-}
-data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
-  provider  = aws.modernisation-platform
-  secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
-}
+# ## Pager duty integration
+# # Get the map of pagerduty integration keys from the modernisation platform account
+# data "aws_secretsmanager_secret" "pagerduty_integration_keys" {
+#   provider = aws.modernisation-platform
+#   name     = "pagerduty_integration_keys"
+# }
+# data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
+#   provider  = aws.modernisation-platform
+#   secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
+# }
 
-# Add a local to get the keys
-locals {
-  pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
-}
+# # Add a local to get the keys
+# locals {
+#   pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
+# }
 
-# link the sns topic to the service
-module "pagerduty_core_alerts" {
-  depends_on = [
-    aws_sns_topic.ddos_alarm
-  ]
-  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=d88bd90d490268896670a898edfaba24bba2f8ab" # v3.0.0
-  sns_topics                = [aws_sns_topic.ddos_alarm.name]
-  pagerduty_integration_key = local.pagerduty_integration_keys["ddos_cloudwatch"]
-}
+# # link the sns topic to the service
+# module "pagerduty_core_alerts" {
+#   depends_on = [
+#     aws_sns_topic.ddos_alarm
+#   ]
+#   source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=d88bd90d490268896670a898edfaba24bba2f8ab" # v3.0.0
+#   sns_topics                = [aws_sns_topic.ddos_alarm.name]
+#   pagerduty_integration_key = local.pagerduty_integration_keys["ddos_cloudwatch"]
+# }
