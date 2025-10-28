8d7bdc2cf928398bc68c8b646ec77385dabddda3
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-28 12:19:53 +0000

    fix: remove the detector

--- terraform/environments/laa-ccms-soa/guardduty-s3.tf
+++ terraform/environments/laa-ccms-soa/guardduty-s3.tf
@@ -1,11 +1,11 @@
-resource "aws_guardduty_detector" "main" {
-  enable = true
-  finding_publishing_frequency = "SIX_HOURS"
-   tags = merge(local.tags,
-    { Name = lower(format("s3-%s-%s-awsgaurdduty-detector", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
-  )
+# resource "aws_guardduty_detector" "main" {
+#   enable = true
+#   finding_publishing_frequency = "SIX_HOURS"
+#   #  tags = merge(local.tags,
+#   #   { Name = lower(format("s3-%s-%s-awsgaurdduty-detector", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
+#   # )
 
-}
+# }
 
 resource "aws_guardduty_malware_protection_plan" "s3_scan" {
   role = data.aws_iam_role.guardduty_s3_scan.arn
