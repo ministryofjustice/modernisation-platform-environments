3575a853a73d97d01a96977e413b83fec8de1a5e
harichintala1 <109067588+harichintala1@users.noreply.github.com>
2025-10-29 11:18:22 +0000

    Update platform_data.tf

--- terraform/environments/digital-prison-reporting/platform_data.tf
+++ terraform/environments/digital-prison-reporting/platform_data.tf
@@ -170,4 +170,4 @@ data "aws_iam_session_context" "whoami" {
 # Get the environments file from the main repository
 data "http" "environments_file" {
   url = "https://raw.githubusercontent.com/ministryofjustice/modernisation-platform/main/environments/${local.application_name}.json"
-} 
+}
