3a72224ee80581797b87a1719eff299437720833
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2025-10-30 12:52:16 +0000

    fixing the notification issue

--- terraform/environments/digital-prison-reporting/modules/notifications/email/main.tf
+++ terraform/environments/digital-prison-reporting/modules/notifications/email/main.tf
@@ -2,8 +2,4 @@ resource "aws_sns_topic_subscription" "email_alerts" {
   topic_arn = var.sns_topic_arn
   protocol  = "email"
   endpoint  = var.email_url
-
-   lifecycle {
-    ignore_changes = [tags]
-  }
 }
