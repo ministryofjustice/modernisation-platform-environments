7cdae3c22746e90dcc9074d7e4333c8fb99ed1b9
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-30 12:11:53 +0000

    fix: corrected the secret index

--- terraform/environments/ccms-oia/oia_secrets.tf
+++ terraform/environments/ccms-oia/oia_secrets.tf
@@ -10,15 +10,15 @@ resource "aws_secretsmanager_secret" "oia_secrets" {
 resource "aws_secretsmanager_secret_version" "oia_secrets" {
   secret_id = aws_secretsmanager_secret.oia_secrets.id
   secret_string = jsonencode({
-    "guardduty_slack_channel_id" = "",
-    "cloudwatch_slack_channel_id"    = ""
+    "guardduty_slack_channel_id"  = "",
+    "cloudwatch_slack_channel_id" = ""
   })
 
-  # lifecycle {
-  #   ignore_changes = [
-  #     secret_string
-  #   ]
-  # }
+  lifecycle {
+    ignore_changes = [
+      secret_string
+    ]
+  }
 }
 
 data "aws_secretsmanager_secret_version" "oia_secrets" {
