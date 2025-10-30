445da1fe638f502752d3057d88088f6e53c8bff0
julialawrence <julia.lawrence@justice.gov.uk>
2025-10-30 15:20:21 +0000

    Fixing KMS and ranges

--- terraform/environments/laa-pui-secure-browser/networking/environment-configuration.tf
+++ terraform/environments/laa-pui-secure-browser/networking/environment-configuration.tf
@@ -4,7 +4,7 @@ locals {
       /* VPC */
       vpc_cidr                   = "172.31.192.0/18"
       vpc_public_subnets         = ["172.31.192.0/27", "172.31.192.32/27"]  # Small subnets for NAT gateways only (32 IPs each)
-      vpc_intra_subnets          = ["172.31.192.64/27", "172.31.192.96/27"] # Dedicated subnets for VPC endpoints (32 IPs each)
+      vpc_intra_subnets          = ["172.31.193.0/24", "172.31.194.0/24"]    # Dedicated subnets for VPC endpoints (256 IPs each)
       vpc_private_subnets        = ["172.31.200.0/21", "172.31.208.0/21", "172.31.216.0/21", "172.31.224.0/21", "172.31.232.0/21"] # 2k IPs per subnet x 5 = 10k total
       vpc_enable_nat_gateway     = true
       vpc_one_nat_gateway_per_az = true
