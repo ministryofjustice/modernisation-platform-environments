6150c3fe0846729b644a675801256572d5760977
julialawrence <julia.lawrence@justice.gov.uk>
2025-10-30 16:24:27 +0000

    Fxing KMS key for secure browser vpc flow logs

--- terraform/environments/laa-pui-secure-browser/networking/kms.tf
+++ terraform/environments/laa-pui-secure-browser/networking/kms.tf
@@ -1,54 +1,42 @@
 module "vpc_flow_logs_kms" {
-  count = local.environment == "production" ? 1 : 0
-
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/kms/aws"
   version = "3.1.1"
 
-  description = "VPC Flow Logs KMS key"
-
-  key_administrators = [
-    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-  ]
-
-  key_service_roles_for_autoscaling = [
-    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
-  ]
-
+  aliases                 = ["vpc/vpc-flow-logs-secure-browser"]
+  description             = "VPC flow logs KMS key"
+  enable_default_policy   = true
+  deletion_window_in_days = 7
   key_statements = [
     {
-      sid = "Allow CloudWatch Logs"
+      sid = "AllowCloudWatchLogs"
       actions = [
-        "kms:Encrypt",
-        "kms:Decrypt",
+        "kms:Encrypt*",
+        "kms:Decrypt*",
         "kms:ReEncrypt*",
         "kms:GenerateDataKey*",
-        "kms:CreateGrant",
-        "kms:DescribeKey"
+        "kms:Describe*"
       ]
       resources = ["*"]
+      effect    = "Allow"
       principals = [
         {
-          type = "Service"
-          identifiers = [
-            "logs.${data.aws_region.current.name}.amazonaws.com"
-          ]
+          type        = "Service"
+          identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
         }
       ]
       conditions = [
         {
-          test     = "ArnLike"
+          test     = "ArnEquals"
           variable = "kms:EncryptionContext:aws:logs:arn"
-          values = [
-            "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${local.vpc_flow_log_cloudwatch_log_group_name_prefix}${local.vpc_flow_log_cloudwatch_log_group_name_suffix}"
-          ]
+          values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${local.vpc_flow_log_cloudwatch_log_group_name_prefix}*"]
         }
       ]
     }
   ]
 
-  aliases = ["vpc-flow-logs-secure-browser"]
-
   tags = local.tags
 }
+
