d1c3906683bb09474a655910e281cecad29da72b
julialawrence <julia.lawrence@justice.gov.uk>
2025-10-30 12:50:23 +0000

    Removing duplicate vars

--- terraform/environments/laa-pui-secure-browser/networking/locals.tf
+++ terraform/environments/laa-pui-secure-browser/networking/locals.tf
@@ -1,6 +1,5 @@
 locals {
-  /* VPC */
-  vpc_name                                            = "${local.application_name}-${local.environment}-secure-browser"
+  /* VPC Flow Logs */
   vpc_flow_log_cloudwatch_log_group_name_prefix       = "/aws/vpc-flow-log/"
   vpc_flow_log_cloudwatch_log_group_name_suffix       = local.vpc_name
   vpc_flow_log_cloudwatch_log_group_retention_in_days = 400
