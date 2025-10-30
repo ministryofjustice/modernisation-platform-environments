b8f7747b6618b26824915401ce9e027f6f746001
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-10-30 12:07:28 +0000

    fix: corrected the secret

--- terraform/environments/ccms-oia/alerting.tf
+++ terraform/environments/ccms-oia/alerting.tf
@@ -1,7 +1,7 @@
 #--Alerting Chatbot
 module "cloudwatch_chatbot" {
   source           = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0"
-  slack_channel_id = jsondecode(data.aws_secretsmanager_secret_version.oia_secrets.secret_string)["cloudwatch_slack_channel_id"]
+  slack_channel_id = jsondecode(data.aws_secretsmanager_secret_version.oia_secrets.secret_string)["alerts_slack_channel_id"]
   sns_topic_arns   = [aws_sns_topic.cloudwatch_alerts.arn]
   tags             = local.tags #--This doesn't seem to pass to anything in the module but is a mandatory var. Consider submitting a PR to the module. AW
   application_name = local.application_name
