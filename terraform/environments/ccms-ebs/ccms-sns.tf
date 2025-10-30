ff609f0e7f3255c008c4707c95d8626c42222264
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2025-10-30 16:52:22 +0000

    chore: guardduty files added

--- terraform/environments/ccms-ebs/ccms-sns.tf
+++ terraform/environments/ccms-ebs/ccms-sns.tf
@@ -72,3 +72,27 @@ resource "aws_sns_topic_subscription" "ddos_subscription" {
   protocol  = "email"
   endpoint  = aws_secretsmanager_secret_version.alerts_subscription_email.secret_string
 }
+
+#--Altering SNS
+resource "aws_sns_topic" "guardduty_alerts" {
+  name            = "${local.application_data.accounts[local.environment].app_name}-guardduty-alerts"
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultRequestPolicy": {
+      "headerContentType": "text/plain; charset=UTF-8"
+    }
+  }
+}
+EOF
+}
---
+++ terraform/environments/ccms-ebs/guardduty-s3.tf
@@ -0,0 +1,137 @@
+resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket1" {
+  role = data.aws_iam_role.guardduty_s3_scan.arn
+
+  protected_resource {
+    s3_bucket {
+      bucket_name     = module.s3-bucket.bucket.id
+    }
+  }
+
+  actions {
+    tagging {
+      status = "ENABLED"
+    }
+  }
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
+  )
+
+  depends_on = [ module.s3-bucket ]
+}
+
+resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket2" {
+  role = data.aws_iam_role.guardduty_s3_scan.arn
+
+  protected_resource {
+    s3_bucket {
+      bucket_name     = module.s3-bucket-logging.id
+    }
+  }
+
+  actions {
+    tagging {
+      status = "ENABLED"
+    }
+  }
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
+  )
+
+  depends_on = [ module.s3-bucket-logging ]
+}
+
+resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket3" {
+  role = data.aws_iam_role.guardduty_s3_scan.arn
+
+  protected_resource {
+    s3_bucket {
+      bucket_name     = module.s3-bucket-dbbackup.id
+    }
+  }
+
+  actions {
+    tagging {
+      status = "ENABLED"
+    }
+  }
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
+  )
+
+  depends_on = [ module.s3-bucket-dbbackup ]
+}
+
+resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket4" {
+  role = data.aws_iam_role.guardduty_s3_scan.arn
+
+  protected_resource {
+    s3_bucket {
+      bucket_name     = aws_s3_bucket.lambda_payment_load.bucket.id
+    }
+  }
+
+  actions {
+    tagging {
+      status = "ENABLED"
+    }
+  }
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
+  )
+
+  depends_on = [ aws_s3_bucket.lambda_payment_load ]
+}
+
+resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket5" {
+  for_each = aws_s3_bucket.buckets
+  role = data.aws_iam_role.guardduty_s3_scan.arn
+
+  protected_resource {
+    s3_bucket {
+      bucket_name     = each.value.id
+    }
+  }
+
+  actions {
+    tagging {
+      status = "ENABLED"
+    }
+  }
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
+  )
+
+  depends_on = [ each.value.id ]
+}
+
+resource "aws_guardduty_malware_protection_plan" "s3_scan_bucket6" {
+  role = data.aws_iam_role.guardduty_s3_scan.arn
+
+  protected_resource {
+    s3_bucket {
+      bucket_name     = aws_s3_bucket.red_button_data.bucket.id
+    }
+  }
+
+  actions {
+    tagging {
+      status = "ENABLED"
+    }
+  }
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-%s-%s-awsgaurdduty-mpp", "${local.application_data.accounts[local.environment].app_name}", local.environment)) }
+  )
+
+  depends_on = [ aws_s3_bucket.red_button_data ]
+
+}
+
+data "aws_iam_role" "guardduty_s3_scan" {
+  name = "GuardDutyS3MalwareProtectionRole"
+}
