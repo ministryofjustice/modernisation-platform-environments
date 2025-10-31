f5d11d3adc1c5dd411474d85cc636e2b614f5f26
David Teeling <david.teeling1@justice.gov.uk>
2025-10-31 12:29:01 +0000

    Move fargate module to delius-core

--- terraform/environments/delius-core-training/fargate_graceful_retirement.tf
+++ terraform/environments/delius-core-training/fargate_graceful_retirement.tf
@@ -1,7 +1,7 @@
 module "fargate_graceful_retirement" {
   count                   = local.environment == "development" ? 1 : 0
   environment             = local.environment
-  source                  = "../../modules/fargate_graceful_retirement"
+  source                  = "../delius-core/modules/components/fargate_graceful_retirement"
   restart_time            = "22:00"
   restart_day_of_the_week = "WEDNESDAY"
   debug_logging           = true
