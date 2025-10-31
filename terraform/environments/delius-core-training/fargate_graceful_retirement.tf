a67ce64ed7e6e236a857597086fe49eb2bfe7c7b
David Teeling <david.teeling1@justice.gov.uk>
2025-10-31 10:37:10 +0000

    Required var for fargate module

--- terraform/environments/delius-core-training/fargate_graceful_retirement.tf
+++ terraform/environments/delius-core-training/fargate_graceful_retirement.tf
@@ -1,5 +1,6 @@
 module "fargate_graceful_retirement" {
   count                   = local.environment == "development" ? 1 : 0
+  environment             = local.environment
   source                  = "../../modules/fargate_graceful_retirement"
   restart_time            = "22:00"
   restart_day_of_the_week = "WEDNESDAY"
