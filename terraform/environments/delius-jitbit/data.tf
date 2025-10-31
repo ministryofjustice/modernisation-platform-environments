06942fd094ab365ffb15c361bb7fd95f5a7b5c7a
David Teeling <david.teeling1@justice.gov.uk>
2025-10-31 10:22:09 +0000

    Fix Terraform deprecations

--- terraform/environments/delius-jitbit/data.tf
+++ terraform/environments/delius-jitbit/data.tf
@@ -1,1 +1,5 @@
 #### This file can be used to store data specific to the member account ####
+data "aws_ssm_parameter" "sandbox_active_deployment_colour" {
+  count = local.is-development ? 1 : 0
+  name  = "/delius-jitbit/sandbox-blue-green-active-colour"
+}
--- terraform/environments/delius-jitbit/ecs.tf
+++ terraform/environments/delius-jitbit/ecs.tf
@@ -1,7 +1,7 @@
 module "ecs" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//cluster?ref=v6.0.0"
 
-  name = local.application_name
+  name = "hmpps-${local.environment}-${local.application_name}"
 
   tags = local.tags
 }
