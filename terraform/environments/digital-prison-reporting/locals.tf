25db557a8b63a8d66282c5f0ba5be3917997bc3c
Tom Ogle <tom.ogle@justice.gov.uk>
2025-11-03 16:53:45 +0000

    DHS-469: Use updated jar

--- terraform/environments/digital-prison-reporting/locals.tf
+++ terraform/environments/digital-prison-reporting/locals.tf
@@ -72,7 +72,7 @@ locals {
 
   # Glue Job parameters
   glue_placeholder_script_location = "s3://${local.project}-artifact-store-${local.environment}/build-artifacts/digital-prison-reporting-jobs/scripts/digital-prison-reporting-jobs-vLatest.scala"
-  glue_jobs_latest_jar_location    = "s3://${local.project}-artifact-store-${local.environment}/build-artifacts/digital-prison-reporting-jobs/jars/digital-prison-reporting-jobs-v1.0.125-dev.18+DHS-469-glue-v5-upgrade.5c5d3c5f-all.jar"
+  glue_jobs_latest_jar_location    = "s3://${local.project}-artifact-store-${local.environment}/build-artifacts/digital-prison-reporting-jobs/jars/digital-prison-reporting-jobs-v1.0.125-dev.21+DHS-469-glue-v5-upgrade.573e4942-all.jar"
   glue_log_retention_in_days       = local.application_data.accounts[local.environment].glue_log_retention_in_days
 
   # Common Maintenance Job settings
