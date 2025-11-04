e8412540c3ba7c53c07d8f6bf86b53b7b8965fe8
Stuart James <stuart.james1@justice.gov.uk>
2025-11-04 16:28:58 +0000

    Update buckets to correct permissions for landing files

--- terraform/environments/property-cafm-data-migration/s3.tf
+++ terraform/environments/property-cafm-data-migration/s3.tf
@@ -6,25 +6,6 @@
 # Production Environment 
 #######################################################################################################
 
-# S3 Bucket for Files copying between the CAFM Environments
-
-resource "aws_s3_bucket" "CAFM" {
-  # checkov:skip=CKV_AWS_144: "S3 bucket has cross-region not required"
-  bucket = "property-datahub-landing-${local.environment}"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = merge(
-    local.tags,
-    {
-      Name = "${local.application_name}-CAFM-S3"
-    }
-  )
-}
-
-
 resource "aws_s3_bucket_versioning" "CAFM" {
   bucket = aws_s3_bucket.CAFM.id
   versioning_configuration {
@@ -310,6 +291,56 @@ resource "aws_s3_bucket_policy" "LOG" {
 ############################################
 # Buckets
 ############################################
+module "aws_s3_landing" {
+  source        = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=9facf9fc8f8b8e3f93ffbda822028534b9a75399"
+  bucket_prefix = "property-datahub-landing-${local.environment}"
+  bucket_policy = [jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowAnalyticalPlatformIngestionService"
+        Effect = "Allow"
+        Principal = {
+          AWS = ["arn:aws:iam::${local.environment_management.account_ids["analytical-platform-ingestion-development"]}:role/transfer",
+            "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-ingestion-production"]}:role/transfer"
+          ]
+        },
+        Action = [
+          "s3:DeleteObject",
+          "s3:GetObject",
+          "s3:GetObjectAcl",
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:PutObjectTagging"
+        ],
+        Resource = [
+          module.aws_s3_landing.bucket.arn,
+          "${module.aws_s3_landing.bucket.arn}/*"
+        ]
+      }
+    ]
+  })]
+
+  custom_kms_key     = aws_kms_key.shared_kms_key.arn
+  versioning_enabled = true
+
+  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
+  ownership_controls = "BucketOwnerEnforced"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
+  }
+
+  tags = local.tags
+}
 
 module "s3_bucket_logs" {
   source             = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=9facf9fc8f8b8e3f93ffbda822028534b9a75399"
@@ -368,186 +399,6 @@ module "s3_bucket_logs" {
       noncurrent_version_expiration = {
         days = 730
       }
-    }
-  ]
-
-  tags = local.tags
-}
-
-module "s3_planetfm_landing_bucket" {
-  source        = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=9facf9fc8f8b8e3f93ffbda822028534b9a75399"
-  bucket_prefix = "${local.account_name}-landing-planetfm-${local.environment_shorthand}-"
-  bucket_policy = [jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowAnalyticalPlatformIngestionService"
-        Effect = "Allow"
-        Principal = {
-          AWS = ["arn:aws:iam::${local.environment_management.account_ids["analytical-platform-ingestion-development"]}:role/transfer",
-            "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-ingestion-production"]}:role/transfer"
-          ]
-        },
-        Action = [
-          "s3:DeleteObject",
-          "s3:GetObject",
-          "s3:GetObjectAcl",
-          "s3:PutObject",
-          "s3:PutObjectAcl",
-          "s3:PutObjectTagging"
-        ],
-        Resource = [
-          module.s3_planetfm_landing_bucket.bucket.arn,
-          "${module.s3_planetfm_landing_bucket.bucket.arn}/*"
-        ]
-      }
-    ]
-  })]
-
-  custom_kms_key     = aws_kms_key.shared_kms_key.arn
-  versioning_enabled = true
-
-  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
-  ownership_controls = "BucketOwnerEnforced"
-
-  # Refer to the below section "Replication" before enabling replication
-  replication_enabled = false
-  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
-  # replication_region                       = "eu-west-2"
-  providers = {
-    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
-    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
-    # Platform team to add a new provider for the additional Region.
-    # Leave this provider block in even if you are not using replication
-    aws.bucket-replication = aws
-  }
-
-  lifecycle_rule = [
-    {
-      id      = "main"
-      enabled = "Enabled"
-      filter  = { prefix = "" }
-
-      tags = {
-        rule      = "log"
-        autoclean = "true"
-      }
-
-      transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
-
-      expiration = {
-        days = 730
-      }
-
-      noncurrent_version_transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
-
-      noncurrent_version_expiration = { days = 730 }
-    }
-  ]
-
-  tags = local.tags
-}
-
-module "s3_concept_landing_bucket" {
-  source        = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=9facf9fc8f8b8e3f93ffbda822028534b9a75399"
-  bucket_prefix = "${local.account_name}-landing-concept-${local.environment_shorthand}-"
-  bucket_policy = [jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowAnalyticalPlatformIngestionService"
-        Effect = "Allow"
-        Principal = {
-          AWS = ["arn:aws:iam::${local.environment_management.account_ids["analytical-platform-ingestion-development"]}:role/transfer",
-            "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-ingestion-production"]}:role/transfer"
-          ]
-        },
-        Action = [
-          "s3:DeleteObject",
-          "s3:GetObject",
-          "s3:GetObjectAcl",
-          "s3:PutObject",
-          "s3:PutObjectAcl",
-          "s3:PutObjectTagging"
-        ],
-        Resource = [
-          module.s3_concept_landing_bucket.bucket.arn,
-          "${module.s3_concept_landing_bucket.bucket.arn}/*"
-        ]
-      }
-    ]
-  })]
-
-  custom_kms_key     = aws_kms_key.shared_kms_key.arn
-  versioning_enabled = true
-
-  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
-  ownership_controls = "BucketOwnerEnforced"
-
-  # Refer to the below section "Replication" before enabling replication
-  replication_enabled = false
-  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
-  # replication_region                       = "eu-west-2"
-  providers = {
-    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
-    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
-    # Platform team to add a new provider for the additional Region.
-    # Leave this provider block in even if you are not using replication
-    aws.bucket-replication = aws
-  }
-
-  lifecycle_rule = [
-    {
-      id      = "main"
-      enabled = "Enabled"
-      filter  = { prefix = "" }
-
-      tags = {
-        rule      = "log"
-        autoclean = "true"
-      }
-
-      transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
-
-      expiration = {
-        days = 730
-      }
-
-      noncurrent_version_transition = [
-        {
-          days          = 90
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = 365
-          storage_class = "GLACIER"
-        }
-      ]
-
-      noncurrent_version_expiration = { days = 730 }
     }
   ]
 
