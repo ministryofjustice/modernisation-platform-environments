bf6fbadc2394f010dbb74d98fa2c1e62da49286a
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-11-04 11:23:14 +0000

    chore: another ip removed

--- terraform/environments/ccms-pui/waf.tf
+++ terraform/environments/ccms-pui/waf.tf
@@ -13,10 +13,10 @@ resource "aws_wafv2_ip_set" "pui_waf_ip_set" {
     "35.176.254.38/32",  # Temp AWS PROD Workspace
     "35.177.173.197/32", # Temp AWS PROD Workspace
     "52.56.212.11/32",   # Temp AWS PROD Workspace
-    "80.195.27.199/32",  # Krupal IP
     "35.179.83.235/32",  # Secure Browser
     "13.43.42.69/32",     # Secure Browser
-    jsondecode(data.aws_secretsmanager_secret_version.ip_secrets.secret_string)["ip_address_ko"]
+    jsondecode(data.aws_secretsmanager_secret_version.ip_secrets.secret_string)["ip_address_ko"],
+    jsondecode(data.aws_secretsmanager_secret_version.ip_secrets.secret_string)["ip_address_kb"]
   ]
 
   tags = merge(
