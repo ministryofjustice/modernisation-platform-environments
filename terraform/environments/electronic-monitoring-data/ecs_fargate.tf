09d1636a72d6fadb8b5ee57a99eb27231aec9636
matt heery <matt.heery@justice.gov.uk>
2025-11-05 10:29:47 +0000

    fix: remove redundant policy

--- terraform/environments/electronic-monitoring-data/ecs_fargate.tf
+++ terraform/environments/electronic-monitoring-data/ecs_fargate.tf
@@ -37,15 +37,11 @@ data "aws_iam_policy_document" "ecs_assume_policy" {
   }
 }
 
-
 resource "aws_iam_role" "ecs_execution_role" {
   name               = "ears-sars-app-execution-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_assume_policy.json
 }
-resource "aws_iam_policy" "ecs_execution_policy" {
-  name = "ears-sars-app-ecs-execution-role-policy"
-  policy = data.aws_iam_policy_document.ecs_execution_policy.json
-}
+
 resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_attach" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
