1ede45b992920783318ea2ac0488a296de92b272
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-11-06 13:36:23 +0000

    Missed one + correction

--- terraform/environments/analytical-platform-common/kms-keys.tf
+++ terraform/environments/analytical-platform-common/kms-keys.tf
@@ -35,7 +35,7 @@ module "secrets_manager_common_kms" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/kms/aws"
-  version = "4.0.0"
+  version = "4.1.1"
 
   aliases               = ["secretsmanager/common"]
   description           = "Secrets Manager Common KMS key"
