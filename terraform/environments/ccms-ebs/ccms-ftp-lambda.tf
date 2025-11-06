acdc5b69d188fa5e1e215d0eedd69618667ae4eb
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2025-11-06 15:40:49 +0000

    fix: bucket versioning fix

--- terraform/environments/ccms-ebs/ccms-ftp-lambda.tf
+++ terraform/environments/ccms-ebs/ccms-ftp-lambda.tf
@@ -63,25 +63,99 @@ locals {
   ftp_test_user_secret_value = jsondecode(data.aws_secretsmanager_secret_version.ftp_test_user_secret_value.secret_string)
 }
 
-resource "aws_s3_bucket" "buckets" {
+module "bucket" { #tfsec:ignore:aws-s3-enable-versioning
+  # v8.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/52a40b0dd18aaef0d7c5565d93cc8997aad79636
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=52a40b0dd18aaef0d7c5565d93cc8997aad79636"
   for_each = toset(local.bucket_names)
 
-  bucket = each.value
 
-  tags = merge(
+  bucket_name = each.value
+  sse_algorithm      = "AES256"
+  custom_kms_key     = ""
+  #  bucket_prefix      = "s3-bucket-example"
+  versioning_enabled = true
+  bucket_policy      = [data.aws_iam_policy_document.artefacts_s3_policy.json]
+
+  log_bucket = local.logging_bucket_name
+  log_prefix = "s3access/${each.value}"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below three variables and providers configuration are only relevant if 'replication_enabled' is set to true
+  replication_region = "eu-west-2"
+  # replication_role_arn                     = module.s3-bucket-replication-role.role.arn
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
     {
-      Name        = each.value
-      Environment = local.environment
-    },
-    {
-      "business-unit"          = "LAA",
-      "infrastructure-support" = "laa-role-sre@digital.justice.gov.uk",
-      "source-code"            = "https://github.com/ministryofjustice/modernisation-platform-environments"
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_standard
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_glacier
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_current
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_standard
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_glacier
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_noncurrent
+      }
+
+      abort_incomplete_multipart_upload_days = local.application_data.accounts[local.environment].s3_lifecycle_days_abort_incomplete_multipart_upload_days
     }
-  )
+  ]
 
-  # server access logging is configured via aws_s3_bucket_logging resource below
+  tags = merge(local.tags,
+    { Name = lower(format("s3-bucket-%s-%s", local.application_name, local.environment)) }
+  )
 }
+
+# resource "aws_s3_bucket" "buckets" {
+#   for_each = toset(local.bucket_names)
+
+#   bucket = each.value
+
+#   tags = merge(
+#     {
+#       Name        = each.value
+#       Environment = local.environment
+#     },
+#     {
+#       "business-unit"          = "LAA",
+#       "infrastructure-support" = "laa-role-sre@digital.justice.gov.uk",
+#       "source-code"            = "https://github.com/ministryofjustice/modernisation-platform-environments"
+#     }
+#   )
+# }
 
 # Server access logging: send access logs to the environment logging bucket
 resource "aws_s3_bucket_logging" "buckets_access_logging" {
--- terraform/environments/ccms-ebs/ccms-s3.tf
+++ terraform/environments/ccms-ebs/ccms-s3.tf
@@ -300,47 +300,195 @@ data "aws_iam_policy_document" "dbbackup_s3_policy" {
   }
 }
 
-resource "aws_s3_bucket" "ccms_ebs_shared" {
-  bucket = "${local.application_name}-${local.environment}-shared"
-}
+module "ccms_ebs_shared" { #tfsec:ignore:aws-s3-enable-versioning
+  # v8.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/52a40b0dd18aaef0d7c5565d93cc8997aad79636
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=52a40b0dd18aaef0d7c5565d93cc8997aad79636"
 
-resource "aws_s3_bucket_public_access_block" "ccms_ebs_shared" {
-  bucket                  = aws_s3_bucket.ccms_ebs_shared.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+  bucket_name = "${local.application_name}-${local.environment}-ccms_ebs_shared"
+  sse_algorithm      = "AES256"
+  custom_kms_key     = ""
+  #  bucket_prefix      = "s3-bucket-example"
+  versioning_enabled = true
+  bucket_policy      = [data.aws_iam_policy_document.artefacts_s3_policy.json]
 
-resource "aws_s3_bucket_versioning" "ccms_ebs_shared" {
-  bucket = aws_s3_bucket.ccms_ebs_shared.id
+  log_bucket = local.logging_bucket_name
+  log_prefix = "s3access/${local.application_name}-${local.environment}-ccms_ebs_shared"
 
-  versioning_configuration {
-    status = "Enabled"
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below three variables and providers configuration are only relevant if 'replication_enabled' is set to true
+  replication_region = "eu-west-2"
+  # replication_role_arn                     = module.s3-bucket-replication-role.role.arn
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
   }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_standard
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_glacier
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_current
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_standard
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_glacier
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_noncurrent
+      }
+
+      abort_incomplete_multipart_upload_days = local.application_data.accounts[local.environment].s3_lifecycle_days_abort_incomplete_multipart_upload_days
+    }
+  ]
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-bucket-%s-%s", local.application_name, local.environment)) }
+  )
+}
+
+# resource "aws_s3_bucket" "ccms_ebs_shared" {
+#   bucket = "${local.application_name}-${local.environment}-shared"
+# }
+
+# resource "aws_s3_bucket_public_access_block" "ccms_ebs_shared" {
+#   bucket                  = aws_s3_bucket.ccms_ebs_shared.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
+# resource "aws_s3_bucket_versioning" "ccms_ebs_shared" {
+#   bucket = aws_s3_bucket.ccms_ebs_shared.id
+
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+module "lambda_payment_load" { #tfsec:ignore:aws-s3-enable-versioning
+  # v8.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/52a40b0dd18aaef0d7c5565d93cc8997aad79636
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=52a40b0dd18aaef0d7c5565d93cc8997aad79636"
+
+  bucket_name = "${local.application_name}-${local.environment}-lambda_payment_load"
+  sse_algorithm      = "AES256"
+  custom_kms_key     = ""
+  #  bucket_prefix      = "s3-bucket-example"
+  versioning_enabled = true
+  bucket_policy      = [data.aws_iam_policy_document.artefacts_s3_policy.json]
+
+  log_bucket = local.logging_bucket_name
+  log_prefix = "s3access/${local.application_name}-${local.environment}-lambda_payment_load"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below three variables and providers configuration are only relevant if 'replication_enabled' is set to true
+  replication_region = "eu-west-2"
+  # replication_role_arn                     = module.s3-bucket-replication-role.role.arn
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_standard
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_glacier
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_current
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_standard
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_glacier
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_noncurrent
+      }
+
+      abort_incomplete_multipart_upload_days = local.application_data.accounts[local.environment].s3_lifecycle_days_abort_incomplete_multipart_upload_days
+    }
+  ]
+
+  tags = merge(local.tags,
+    { Name = lower(format("s3-bucket-%s-%s", local.application_name, local.environment)) }
+  )
 }
 
 # S3 Bucket for Payment Load
 
-resource "aws_s3_bucket" "lambda_payment_load" {
-  bucket = "${local.application_name}-${local.environment}-payment-load"
-}
+# resource "aws_s3_bucket" "lambda_payment_load" {
+#   bucket = "${local.application_name}-${local.environment}-payment-load"
+# }
 
-resource "aws_s3_bucket_public_access_block" "lambda_payment_load" {
-  bucket                  = aws_s3_bucket.lambda_payment_load.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+# resource "aws_s3_bucket_public_access_block" "lambda_payment_load" {
+#   bucket                  = aws_s3_bucket.lambda_payment_load.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-resource "aws_s3_bucket_versioning" "lambda_payment_load" {
-  bucket = aws_s3_bucket.lambda_payment_load.id
+# resource "aws_s3_bucket_versioning" "lambda_payment_load" {
+#   bucket = aws_s3_bucket.lambda_payment_load.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
 # Development
 moved {
