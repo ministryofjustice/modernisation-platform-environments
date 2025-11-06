a5a7ed4b2fb51e8d5a6a489155d0e3c12b754963
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-11-06 14:04:21 +0000

    move sb vpc to vars

--- terraform/environments/ccms-oia/opa_waf.tf
+++ terraform/environments/ccms-oia/opa_waf.tf
@@ -6,13 +6,12 @@ resource "aws_wafv2_ip_set" "opahub_waf_ip_set" {
   ip_address_version = "IPV4"
   description        = "List of trusted IP Addresses allowing access via WAF"
 
-  addresses = concat(
-    [
+  addresses = [
       local.application_data.accounts[local.environment].aws_workspace,
       data.aws_subnet.private_subnets_a.cidr_block,
       data.aws_subnet.private_subnets_b.cidr_block,
       data.aws_subnet.private_subnets_c.cidr_block,
-  ])
+  ]
 
   tags = merge(local.tags,
     { Name = lower(format("%s-ip-set", local.opa_app_name)) }
