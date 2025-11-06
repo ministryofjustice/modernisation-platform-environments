5efa56df24d5680840c5e2568a529acff5508457
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-11-06 13:25:38 +0000

    update WAF ips to internal for aws workspace

--- terraform/environments/ccms-oia/opa_waf.tf
+++ terraform/environments/ccms-oia/opa_waf.tf
@@ -8,23 +8,11 @@ resource "aws_wafv2_ip_set" "opahub_waf_ip_set" {
 
   addresses = concat(
     [
-      local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_a,
-      local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_b,
-      local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_c,
-      local.application_data.accounts[local.environment].mp_nat_gateway_a,
-      local.application_data.accounts[local.environment].mp_nat_gateway_b,
-      local.application_data.accounts[local.environment].mp_nat_gateway_c,
-      "35.176.254.38/32",  # Temp AWS PROD Workspace
-      "35.177.173.197/32", # Temp AWS PROD Workspace
-      "52.56.212.11/32"    # Temp AWS PROD Workspace
-    ],
-    local.environment == "development" ? [
+      local.application_data.accounts[local.environment].aws_workspace,
       data.aws_subnet.private_subnets_a.cidr_block,
       data.aws_subnet.private_subnets_b.cidr_block,
-      data.aws_subnet.private_subnets_c.cidr_block
-    ] : [],
-    local.environment == "production" ? ["172.31.192.0/18"] : [] # Secure Browser VPC
-  )
+      data.aws_subnet.private_subnets_c.cidr_block,
+  ])
 
   tags = merge(local.tags,
     { Name = lower(format("%s-ip-set", local.opa_app_name)) }
@@ -41,13 +29,11 @@ resource "aws_wafv2_ip_set" "opahub_waf_ip_set_web_determinations" {
   description        = "Trusted IPs for Web Determinations path"
 
   addresses = [
-    local.application_data.accounts[local.environment].mp_nat_gateway_a,
-    local.application_data.accounts[local.environment].mp_nat_gateway_b,
-    local.application_data.accounts[local.environment].mp_nat_gateway_c,
-    "89.45.177.118/32",   # Sahid
-    "172.31.192.0/18",    # Secure Browser VPC
-    jsondecode(data.aws_secretsmanager_secret_version.ip_secrets.secret_string)["ip_address_ko"],
-    jsondecode(data.aws_secretsmanager_secret_version.ip_secrets.secret_string)["ip_address_kb"]
+    local.application_data.accounts[local.environment].aws_workspace,
+    data.aws_subnet.private_subnets_a.cidr_block,
+    data.aws_subnet.private_subnets_b.cidr_block,
+    data.aws_subnet.private_subnets_c.cidr_block,
+    "172.31.192.0/18" # Secure Browser VPC
   ]
 
   tags = merge(local.tags,
--- terraform/environments/ccms-pui/waf.tf
+++ terraform/environments/ccms-pui/waf.tf
@@ -8,22 +8,12 @@ resource "aws_wafv2_ip_set" "pui_waf_ip_set" {
 
   addresses = concat(
     [
-      local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_a,
-      local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_b,
-      local.application_data.accounts[local.environment].lz_aws_workspace_public_nat_gateway_c,
-      "35.176.254.38/32",  # Temp AWS PROD Workspace
-      "35.177.173.197/32", # Temp AWS PROD Workspace
-      "52.56.212.11/32",   # Temp AWS PROD Workspace
-      jsondecode(data.aws_secretsmanager_secret_version.ip_secrets.secret_string)["ip_address_ko"],
-      jsondecode(data.aws_secretsmanager_secret_version.ip_secrets.secret_string)["ip_address_kb"]
-    ],
-    local.environment == "development" ? [
+      local.application_data.accounts[local.environment].aws_workspace,
       data.aws_subnet.private_subnets_a.cidr_block,
       data.aws_subnet.private_subnets_b.cidr_block,
-      data.aws_subnet.private_subnets_c.cidr_block
-    ] : [],
-    local.environment == "production" ? ["172.31.192.0/18"] : [] # Secure Browser VPC
-  )
+      data.aws_subnet.private_subnets_c.cidr_block,
+      local.environment == "production" ? ["172.31.192.0/18"] : [] # Secure Browser VPC
+  ], )
 
   tags = merge(
     local.tags,
