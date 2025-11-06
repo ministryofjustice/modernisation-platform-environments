1aa7b24e7045f063d7a7976d4720f0717ad423e1
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-11-06 14:03:08 +0000

    🤐 Add Airflow GitHub App Secret

--- terraform/environments/analytical-platform-common/secrets.tf
+++ terraform/environments/analytical-platform-common/secrets.tf
@@ -3,9 +3,30 @@ module "analytical_platform_compute_cluster_data_secret" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "1.3.1"
+  version = "2.0.1"
 
   name       = "analytical-platform-compute/cluster-data"
+  kms_key_id = module.secrets_manager_common_kms.key_arn
+
+  secret_string = jsonencode({
+    change_me = "CHANGEME"
+  })
+  ignore_secret_changes = true
+
+  tags = local.tags
+}
+
+
+}
+
+module "analytical_platform_airflow_github_app_secret" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "2.0.1"
+
+  name       = "github/airflow-github-app"
   kms_key_id = module.secrets_manager_common_kms.key_arn
 
   secret_string = jsonencode({
