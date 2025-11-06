8c71e885ed61d637cd37d5ed3d29f9c2b85f8049
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-11-06 16:36:59 +0000

    ccmsebs lb changes

---
+++ terraform/environments/ccms-ebs/ssogen-lb-sg.tf
@@ -0,0 +1,64 @@
+#########################################
+# SSOGEN Internal Load Balancer Security Group
+#########################################
+
+resource "aws_security_group" "sg_ssogen_internal_alb" {
+  name        = "ssogen_internal_alb"
+  description = "Inbound and outbound rules for SSOGEN Internal Load Balancer"
+  vpc_id      = data.aws_vpc.shared.id
+
+  tags = merge(local.tags,
+    { Name = lower(format("sg-ssogen-loadbalancer-internal")) }
+  )
+}
+
+#########################################
+# INGRESS RULES
+#########################################
+
+# Allow HTTPS (4443) from AWS Workspaces
+resource "aws_security_group_rule" "ingress_ssogen_internal_4443_workspaces" {
+  security_group_id = aws_security_group.sg_ssogen_internal_alb.id
+  type              = "ingress"
+  description       = "Allow HTTPS (4443) from AWS Workspaces CIDR"
+  protocol          = "tcp"
+  from_port         = 4443
+  to_port           = 4443
+  cidr_blocks       = [local.application_data.accounts[local.environment].lz_aws_workspace_nonprod_prod]
+}
+
+# Allow HTTPS (4443) from Mojo devices
+resource "aws_security_group_rule" "ingress_ssogen_internal_4443_mojo_devices" {
+  security_group_id = aws_security_group.sg_ssogen_internal_alb.id
+  type              = "ingress"
+  description       = "Allow HTTPS (4443) from Mojo device CIDRs"
+  protocol          = "tcp"
+  from_port         = 4443
+  to_port           = 4443
+  cidr_blocks       = [local.application_data.accounts[local.environment].mojo_devices]
+}
+
+#########################################
+# EGRESS RULES
+#########################################
+
+# Allow outbound HTTPS (4443) to backend EC2s within VPC
+resource "aws_security_group_rule" "egress_ssogen_internal_4443" {
+  security_group_id = aws_security_group.sg_ssogen_internal_alb.id
+  type              = "egress"
+  description       = "Allow outbound HTTPS (4443) to backend targets in VPC"
+  protocol          = "tcp"
+  from_port         = 4443
+  to_port           = 4443
+  cidr_blocks       = [data.aws_vpc.shared.cidr_block]
+}
+
+#########################################
+# NOTES
+#########################################
+# - This SG is attached to the SSOGEN internal ALB (aws_lb.ssogen_alb)
+# - ALB Listener runs on port 4443 with HTTPS
+# - Backend instances listen on 4443 using self-signed certs
+# - No public exposure (internal-only ALB and private subnets)
+# - Access limited to trusted internal CIDRs only
+#########################################
---
+++ terraform/environments/ccms-ebs/ssogen-load-balancer.tf
@@ -0,0 +1,66 @@
+resource "aws_lb" "ssogen_alb" {
+  name               = lower(format("lb-%s-ssogen-internal", local.application_name))
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_ssogen_internal_alb.id]
+  subnets            = data.aws_subnets.shared-private.ids
+
+  drop_invalid_header_fields = true
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = module.s3-bucket-logging.bucket.id
+    prefix  = local.lb_log_prefix_ssogen_internal
+    enabled = true
+  }
+
+  tags = merge(local.tags,
+    { Name = lower(format("lb-%s-ssogen-internal", local.application_name)) }
+  )
+}
+
+resource "aws_lb_target_group" "ssogen_internal_tg" {
+  name     = lower(format("tg-%s-ssogen", local.application_name))
+  port     = local.application_data.accounts[local.environment].tg_ssogen_apps_port
+  protocol = "HTTPS"
+  vpc_id   = data.aws_vpc.shared.id
+  target_type = "instance"
+  # deregistration_delay = 60
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  stickiness {
+    enabled         = true
+    type            = "lb_cookie"
+    cookie_duration = 3600
+  }
+}
+
+resource "aws_lb_listener" "ssogen_internal_listener" {
+  load_balancer_arn = aws_lb.ssogen_alb.arn
+  port              = "4443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.external.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ssogen_internal_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "ssogen_internal" {
+  count            = local.is_development ? local.application_data.accounts[local.environment].ssogen_no_instances : 0
+  target_group_arn = aws_lb_target_group.ssogen_internal_tg.arn
+  target_id        = aws_instance.ec2_ssogen[count.index].id
+  port             = local.application_data.accounts[local.environment].tg_ssogen_apps_port
+}
+
