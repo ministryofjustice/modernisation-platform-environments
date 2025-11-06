27857d5487c898f590341e65791268fddb353e44
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-11-06 14:19:34 +0000

    Update to add parameters

--- terraform/environments/analytical-platform-common/secrets.tf
+++ terraform/environments/analytical-platform-common/secrets.tf
@@ -16,20 +16,26 @@ module "analytical_platform_compute_cluster_data_secret" {
   tags = local.tags
 }
 
-module "analytical_platform_airflow_github_app_secret" {
+module "octo_github_app_secret" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/secrets-manager/aws"
   version = "2.0.1"
 
-  name       = "github/airflow-github-app"
-  kms_key_id = module.secrets_manager_common_kms.key_arn
+  name        = "github/airflow-github-app"
+  description = "https://github.com/ministryofjustice/analytical-platform-airflow"
 
   secret_string = jsonencode({
-    change_me = "CHANGEME"
+    app_id          = "CHANGEME"
+    client_id       = "CHANGEME"
+    installation_id = "CHANGEME"
+    private_key     = "CHANGEME"
   })
   ignore_secret_changes = true
 
-  tags = local.tags
+  tags = merge(
+    local.tags,
+    { "credential-expiration" = "none" }
+  )
 }
