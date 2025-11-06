c1a4b6a477026d63bac0b3ee8cbef2cbc26a31c1
nbuckingham72 <nick.buckingham@colt.net>
2025-11-06 11:37:12 +0000

    Update_061125_2

--- terraform/environments/ppud/waf.tf
+++ terraform/environments/ppud/waf.tf
@@ -11,7 +11,8 @@ locals {
 }
 
 module "waf" {
-  source                   = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-aws-waf?ref=b9cf6f92b142e80845ae30252aee2f84f57a71a9"
+  source                   = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-aws-waf"
+  name                     = "test"
   enable_ddos_protection   = true
   ddos_rate_limit          = 150
   block_non_uk_traffic     = true
@@ -22,13 +23,13 @@ module "waf" {
     aws.modernisation-platform = aws.modernisation-platform
   }
 
-  #additional_managed_rules = [
-  #   {
-  #     arn             = aws_wafv2_rule_group.wam_waf_acl.arn
-  #     override_action = "none"   # respect the group's action (BLOCK). Use "count" to dry-run.
-  #     priority        = 9        # unique; runs before managed rules at 10..15
-  #   }
-  # ]
+  additional_managed_rules = [
+     {
+       arn             = aws_wafv2_rule_group.wam_waf_acl.arn
+       override_action = "none"   # respect the group's action (BLOCK). Use "count" to dry-run.
+       priority        = 3       # unique; runs before managed rules at 10..15
+     }
+   ]
 
   managed_rule_actions = {
     AWSManagedRulesKnownBadInputsRuleSet = false
@@ -40,12 +41,12 @@ module "waf" {
   }
 
   managed_rule_priorities = {
-    AWSManagedRulesAnonymousIpList       = 40
-    AWSManagedRulesKnownBadInputsRuleSet = 50
-    AWSManagedRulesCommonRuleSet         = 60
-    AWSManagedRulesSQLiRuleSet           = 70
-    AWSManagedRulesLinuxRuleSet          = 80
-    AWSManagedRulesBotControlRuleSet     = 90
+    AWSManagedRulesAnonymousIpList       = 5
+    AWSManagedRulesKnownBadInputsRuleSet = 6
+    AWSManagedRulesCommonRuleSet         = 7
+    AWSManagedRulesSQLiRuleSet           = 8
+    AWSManagedRulesLinuxRuleSet          = 9
+    AWSManagedRulesBotControlRuleSet     = 10
   }
 
   core_logging_account_id = local.environment_management.account_ids["core-logging-production"]
@@ -155,131 +156,3 @@ resource "aws_wafv2_ip_set" "circle_ci_waf_ip_set" {
     { Name = lower(format("%s-circle-ci-waf-ip-set-%s", local.application_name, local.environment)) }
   )
 }
-
-/*
-# WebACL for WAM
-resource "aws_wafv2_web_acl" "wam_web_acl" {
-  # checkov:skip=CKV_AWS_192: "Ensure WAF prevents message lookup in Log4j2. See CVE-2021-44228 aka log4jshell"
-  count       = local.is-development == true ? 1 : 0
-  name        = "wam-waf-acl"
-  scope       = "REGIONAL"
-  description = "AWS WAF Web ACL for WAM"
-
-  default_action {
-    allow {}       // This allows UK traffic by default
-  }
-
-  rule {
-    name = "NCSC-WAF-IP-List"
-    priority = 10
-    action {
-      allow {}
-    }
-    statement {
-      ip_set_reference_statement {
-        arn = aws_wafv2_ip_set.ncsc_waf_ip_set.arn
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "wam-ncsc-waf-ip-list"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  rule {
-    name = "Circle-CI-WAF-IP-List"
-    priority = 20
-    action {
-      allow {}
-    }
-    statement {
-      ip_set_reference_statement {
-        arn = aws_wafv2_ip_set.circle_ci_waf_ip_set.arn
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "wam-circle-ci-waf-ip-list"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  rule {
-    name     = "Block-non-UK-Traffic"
-    priority = 30
-    action {
-      block {}
-    }
-    statement {
-      not_statement {
-        statement {
-          geo_match_statement {
-            country_codes = ["GB"]
-          }
-        }
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "wam-waf-block-non-uk"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  # AWS Managed Rule Group (in COUNT mode)
-  rule {
-    name     = "AWSManagedRulesKnownBadInputsRuleSet"
-    priority = 100
-
-    override_action {
-      count {}
-    }
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesKnownBadInputsRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "AWSManagedRulesKnownBadInputsRuleSet"
-      sampled_requests_enabled   = true
-    }
-  }
-  tags = merge(local.tags,
-    { Name = lower(format("%s-wam-waf-web-acl-%s", local.application_name, local.environment)) }
-  )
-  visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "wam-waf"
-    sampled_requests_enabled   = true
-  }
-}
-
-# ALB Attachment to WAF ACL
-resource "aws_wafv2_web_acl_association" "wam_alb_waf_association" {
-  count        = local.is-development == true ? 1 : 0
-  resource_arn = aws_lb.WAM-ALB.arn
-  web_acl_arn  = aws_wafv2_web_acl.wam_web_acl.arn
-}
-
-# Create CloudWatch log group for PRTG
-resource "aws_cloudwatch_log_group" "wam_waf_logs" {
-  # checkov:skip=CKV_AWS_158: "Ensure that CloudWatch Log Group is encrypted by KMS"
-  count             = local.is-development == true ? 1 : 0
-  name              = "aws-waf-logs-wam-waf"
-  retention_in_days = 365
-  tags = merge(local.tags,
-    { Name = lower(format("%s-wam-waf-logs-%s", local.application_name, local.environment)) }
-  )
-}
-
-# Send WebACL logs to CloudWatch
-resource "aws_wafv2_web_acl_logging_configuration" "wam_waf_logging" {
-  count                   = local.is-development == true ? 1 : 0
-  log_destination_configs = [aws_cloudwatch_log_group.wam_waf_logs[count.index].arn]
-# resource_arn            = aws_wafv2_web_acl.wam_web_acl[count.index].arn 
-  resource_arn            = module.waf.web_acl_arn
-}
-*/
