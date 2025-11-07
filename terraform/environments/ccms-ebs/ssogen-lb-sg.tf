55ff2b9cce2d1ecd457732834fb59179209139b8
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-11-07 14:07:12 +0000

    changes

--- terraform/environments/ccms-ebs/ssogen-lb-sg.tf
+++ terraform/environments/ccms-ebs/ssogen-lb-sg.tf
@@ -17,38 +17,27 @@ resource "aws_security_group" "sg_ssogen_internal_alb" {
 #########################################
 
 # Allow HTTPS (4443) from AWS Workspaces
-resource "aws_security_group_rule" "ingress_ssogen_internal_4443_workspaces" {
+resource "aws_vpc_security_group_ingress_rule" "ingress_ssogen_internal_4443_workspaces" {
   security_group_id = aws_security_group.sg_ssogen_internal_alb.id
-  type              = "ingress"
   description       = "Allow HTTPS (4443) from AWS Workspaces CIDR"
-  protocol          = "tcp"
-  from_port         = 443
-  to_port           = 443
-  cidr_blocks       = [local.application_data.accounts[local.environment].lz_aws_workspace_nonprod_prod]
+  from_port         = 4443
+  to_port           = 4443
+  ip_protocol       = "tcp"
+  cidr_ipv4         = local.application_data.accounts[local.environment].lz_aws_workspace_nonprod_prod
 }
-
 
 #########################################
 # EGRESS RULES
 #########################################
 
-# Allow outbound HTTPS (4443) to backend EC2s within VPC
-resource "aws_security_group_rule" "egress_ssogen_internal_4443" {
-  security_group_id = aws_security_group.sg_ssogen_internal_alb.id
-  type              = "egress"
-  description       = "Allow outbound HTTPS (4443) to backend targets in VPC"
-  protocol          = "tcp"
-  from_port         = 4443
-  to_port           = 4443
-  cidr_blocks       = [data.aws_vpc.shared.cidr_block]
+# Allow outbound HTTPS (4443) only to backend SSOGEN EC2s
+resource "aws_vpc_security_group_egress_rule" "egress_ssogen_internal_4443_backend" {
+  security_group_id            = aws_security_group.sg_ssogen_internal_alb.id
+  description                  = "Allow HTTPS (4443) to backend SSOGEN EC2s"
+  from_port                    = 4443
+  to_port                      = 4443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.ssogen_sg[0].id
 }
 
-#########################################
-# NOTES
-#########################################
-# - This SG is attached to the SSOGEN internal ALB (aws_lb.ssogen_alb)
-# - ALB Listener runs on port 4443 with HTTPS
-# - Backend instances listen on 4443 using self-signed certs
-# - No public exposure (internal-only ALB and private subnets)
-# - Access limited to trusted internal CIDRs only
-#########################################
+
