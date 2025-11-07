1ad4ed6fc1938effdc7791d00de4cce59a9f2830
Mark Butler <mark.butler@solirius.com>
2025-11-07 16:42:18 +0000

    Add parentheses to conditional

--- terraform/environments/tribunals/cloudfront-lambda-resources.tf
+++ terraform/environments/tribunals/cloudfront-lambda-resources.tf
@@ -100,9 +100,11 @@ resource "aws_lambda_permission" "allow_http_cloudfront" {
   function_name = aws_lambda_function.cloudfront_redirect_lambda.function_name
   principal     = "edgelambda.amazonaws.com"
   # Only set source_arn if distribution exists
-  source_arn = local.matching_distribution_id != null ?
+  source_arn = (
+    )local.matching_distribution_id != null ?
     "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${local.matching_distribution_id}" :
     null
+  )
   qualifier     = aws_lambda_function.cloudfront_redirect_lambda.version
 }
 
