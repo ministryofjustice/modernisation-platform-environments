d5c813c00125e701a7419941bd247c57e9f04317
Mark Butler <mark.butler@solirius.com>
2025-11-07 16:49:46 +0000

    typo

--- terraform/environments/tribunals/cloudfront-lambda-resources.tf
+++ terraform/environments/tribunals/cloudfront-lambda-resources.tf
@@ -101,7 +101,7 @@ resource "aws_lambda_permission" "allow_http_cloudfront" {
   principal     = "edgelambda.amazonaws.com"
   # Only set source_arn if distribution exists
   source_arn = (
-    )local.matching_distribution_id != null ?
+    local.matching_distribution_id != null ?
     "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${local.matching_distribution_id}" :
     null
   )
