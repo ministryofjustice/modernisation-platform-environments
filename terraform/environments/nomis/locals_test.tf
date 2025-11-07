1bdec5ba609dc2a29b35c084ef3446f73e307164
Dave Kent <dave.kent@justice.gov.uk>
2025-11-07 13:04:30 +0000

    nomis-tag-test

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -242,6 +242,9 @@ locals {
         })
         instance = merge(local.ec2_instances.db.instance, {
           disable_api_termination = true
+          tags = merge(local.ec2_instances.db.instance.tags, {
+            patch-manager = "manual"
+          })
         })
         tags = merge(local.ec2_instances.db.tags, {
           description         = "T1 NOMIS database"
