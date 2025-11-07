e305e93fe25d938d7e0c2454243ebe021fcd9a4d
nbuckingham72 <nick.buckingham@colt.net>
2025-11-07 13:54:21 +0000

    Update_071125_12

--- terraform/environments/ppud/lambda_functions.tf
+++ terraform/environments/ppud/lambda_functions.tf
@@ -227,6 +227,7 @@ locals {
 resource "aws_lambda_function" "lambda_functions" {
   for_each = local.lambda_instances_map
 
+  # checkov:skip=CKV_AWS_116: "Dead Letter queues to be enabled later"
   # checkov:skip=CKV_AWS_117: "PPUD Lambda functions do not require VPC access and can run in no-VPC mode"
   # checkov:skip=CKV_AWS_272: "PPUD Lambda code signing not required"
   
