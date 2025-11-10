c90bcf89bc8e4a1c66ef2d0244c1cf8c9ef97bab
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-11-10 12:59:21 +0000

    chore: made ec2 same as infra

--- terraform/environments/ccms-ebs/ssogen-ec2.tf
+++ terraform/environments/ccms-ebs/ssogen-ec2.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "ec2_ssogen" {
   key_name                    = aws_key_pair.ssogen[0].key_name
   vpc_security_group_ids      = [aws_security_group.ssogen_sg[0].id]
   subnet_id                   = local.private_subnets[count.index]
-  monitoring                  = true
+  # monitoring                  = true
   ebs_optimized               = false
   associate_public_ip_address = false
   iam_instance_profile        = aws_iam_instance_profile.ssogen_instance_profile[0].name
@@ -25,9 +25,16 @@ resource "aws_instance" "ec2_ssogen" {
     volume_size = 60
     volume_type = "gp2"
     encrypted   = true
+    tags = merge(
+      local.tags,
+      { Name = "ec2-ccms-ebs-development-ssogen-${count.index + 1}" },
+      { "instance-role" = local.application_data.accounts[local.environment].instance_role_ssogen },
+      { "instance-scheduling" = local.application_data.accounts[local.environment]["instance-scheduling"] },
+      { backup = "true" }
+    )
   }
 
-  user_data_replace_on_change = true
+  # user_data_replace_on_change = true
   user_data = base64encode(templatefile("./templates/ec2_user_data_ssogen.sh", {
     hostname = "ssogen-${count.index + 1}"
   }))
