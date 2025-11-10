3bd853fa3d6a7f29f5c9fb9be17fb962b44b7f5d
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2025-11-10 15:04:35 +0000

    corrected the name in waf event lambda permission

--- terraform/environments/ccms-ebs/ccms-waf-switch.tf
+++ terraform/environments/ccms-ebs/ccms-waf-switch.tf
@@ -130,7 +130,7 @@ resource "aws_lambda_permission" "waf_events_allow" {
 
 
 resource "aws_lambda_permission" "waf_events_block" {
-  statement_id  = "AllowEvents2130-${local.environment}"
+  statement_id  = "BlockEvents2130-${local.environment}"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.waf_toggle.arn
   principal     = "events.amazonaws.com"
