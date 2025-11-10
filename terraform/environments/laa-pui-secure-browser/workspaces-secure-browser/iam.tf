65da311af1642a53e217ff9f23eacdd3815a2348
David Sibley <david.sibley@digital.justice.gov.uk>
2025-11-10 17:01:15 +0000

    first pass on external role for XSIAM
    ensure external_id is not needlessly exposed in terraform output
    
    make resources safe through local.create_resources
    
    i will squash these

---
+++ terraform/environments/laa-pui-secure-browser/workspaces-secure-browser/iam.tf
@@ -0,0 +1,51 @@
+resource "random_uuid" "cortex" {}
+
+module "cortex_xsiam_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "~> 6.2"
+
+  count           = local.create_resources ? 1 : 0
+  name            = "cortex_xsiam"
+  use_name_prefix = true
+  description     = "Role utilised by Palo Alto Cortex XSIAM"
+
+  # Allow Cortex account root to assume role with ExternalId
+  trust_policy_permissions = {
+    CortexXSIAM = {
+      actions = ["sts:AssumeRole"]
+      principals = [{
+        type        = "AWS"
+        identifiers = ["arn:aws:iam::${aws_ssm_parameter.cortex_account_id.insecure_value}:root"]
+      }]
+      condition = [{
+        test     = "StringEquals"
+        variable = "sts:ExternalId"
+        values   = [sensitive(random_uuid.cortex[0].result)]
+      }]
+    }
+  }
+
+  create_inline_policy = true
+  inline_policy_permissions = {
+    SQSQueueReceiveMessages = {
+      effect = "Allow"
+      actions = [
+        "sqs:ChangeMessageVisibility",
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes",
+        "sqs:GetQueueUrl",
+        "sqs:ListQueues",
+      ]
+      resources = [module.sqs_s3_notifications[0].queue_arn]
+    }
+
+    S3GetLogs = {
+      effect    = "Allow"
+      actions   = ["s3:GetObject"]
+      resources = [module.s3_bucket_workspacesweb_session_logs[0].s3_bucket_arn]
+    }
+  }
+
+  tags = local.tags
+}
