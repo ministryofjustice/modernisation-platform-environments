f9b2c0bc1145e9ca859b71db02839e214a04ff69
wullub <13420301+wullub@users.noreply.github.com>
2025-11-11 17:09:53 +0000

    change userdata

--- terraform/modules/baseline/versions.tf
+++ terraform/modules/baseline/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      version               = "5.82.2"
+      version               = "~> 6.0"
       source                = "hashicorp/aws"
       configuration_aliases = [aws.core-vpc, aws.core-network-services, aws.us-east-1]
     }
