9787a5cef17ac0ae46e43d0a4e2f8948dd8a07f7
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2025-11-14 16:26:19 +0000

    CC-4027: Enable EFS backups.

--- terraform/environments/laa-oem/oem_ec2_db_efs.tf
+++ terraform/environments/laa-oem/oem_ec2_db_efs.tf
@@ -18,32 +18,10 @@ resource "aws_efs_mount_target" "oem_db_efs" {
   ]
 }
 
-resource "aws_security_group" "oem_db_efs_sg" {
-  name_prefix = "${local.application_name}-db-efs-sg-"
-  description = "Allow inbound access from instances"
-  vpc_id      = data.aws_vpc.shared.id
+resource "aws_efs_backup_policy" "policy" {
+  file_system_id = aws_efs_file_system.oem_db_efs.id
 
-  tags = merge(tomap(
-    { "Name" = "${local.application_name}-db-efs-sg" }
-  ), local.tags)
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 2049
-    to_port     = 2049
-    cidr_blocks = [data.aws_vpc.shared.cidr_block]
-  }
-
-  egress {
-    protocol  = "-1"
-    from_port = 0
-    to_port   = 0
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-
-  lifecycle {
-    create_before_destroy = true
+  backup_policy {
+    status = "ENABLED"
   }
 }
