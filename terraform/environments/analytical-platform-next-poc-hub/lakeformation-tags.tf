729b6db8e225e971ddca72db446089dd63d4f4ab
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-11-14 15:38:57 +0000

    refactor: remove unused AWS Lake Formation resource configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/analytical-platform-next-poc-hub/lakeformation-tags.tf
+++ terraform/environments/analytical-platform-next-poc-hub/lakeformation-tags.tf
@@ -1,14 +1,3 @@
 module "lakeformation_tags" {
   source = "../../modules/analytical-platform-next/lakeformation/tag-ontology"
 }
-
-resource "aws_lakeformation_resource_lf_tag" "test" {
-  database {
-    name = aws_glue_catalog_database.example.name
-  }
-
-  lf_tag {
-    key   = aws_lakeformation_lf_tag.example.key
-    value = "stowe"
-  }
-}
