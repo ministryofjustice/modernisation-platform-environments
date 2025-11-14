3f7bf371a1c66c9ec6690c467e8d2762102e32a4
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-11-14 11:13:16 +0000

    same for ONR prod

--- terraform/environments/oasys-national-reporting/locals_production.tf
+++ terraform/environments/oasys-national-reporting/locals_production.tf
@@ -1,8 +1,10 @@
 locals {
 
   lb_maintenance_message_production = {
-    maintenance_title   = "OASys National Reporting Maintenance Window"
-    maintenance_message = "OASys National Reporting is currently unavailable due to planned maintenance. Please try again later."
+    maintenance_title   = "OASys National Reporting Environment Not Started"
+    maintenance_message = "OASys National Reporting Production environment is powered down until we are ready for final configuration. Please contact <a href=\"https://moj.enterprise.slack.com/archives/C6D94J81E\">#ask-digital-studio-ops</a> slack channel if you need the environment starting."
+    # maintenance_title   = "OASys National Reporting Maintenance Window"
+    # maintenance_message = "OASys National Reporting is currently unavailable due to planned maintenance. Please try again later."
   }
 
   baseline_presets_production = {
@@ -326,7 +328,7 @@ locals {
                 }]
               }
               pd-onr-web-http-7777 = {
-                priority = 200
+                priority = 1200 # change priority to 200 when environment is powered on during day
                 actions = [{
                   type              = "forward"
                   target_group_name = "pd-onr-web-http-7777"
