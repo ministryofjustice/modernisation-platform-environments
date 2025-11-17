b958e3d15d28f0aea7d2ec950d4ca8dc6da6fdc1
Timi Olubusi <timiolubusi@icloud.com>
2025-11-17 17:42:18 +0000

    Setting major upgrade to false and minor to true

--- terraform/environments/pra-register/rds.tf
+++ terraform/environments/pra-register/rds.tf
@@ -12,7 +12,8 @@ resource "aws_db_instance" "pra_db" {
   publicly_accessible         = local.is-development ? true : false
   vpc_security_group_ids      = [aws_security_group.postgresql_db_sc.id]
   db_subnet_group_name        = aws_db_subnet_group.dbsubnetgroup.name
-  allow_major_version_upgrade = true
+  allow_major_version_upgrade = false
+  auto_minor_version_upgrade  = true
   ca_cert_identifier          = "rds-ca-rsa2048-g1"
   apply_immediately           = true
   maintenance_window          = local.is-production ? null : "tue:20:20-tue:20:50"
