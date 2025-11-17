0601849885062706b79ad20c8291f7fb4676b84f
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-11-17 14:06:36 +0000

    feat: log group subscription to slack

---
+++ terraform/environments/ccms-edrms/ccms-lambda-function-monitory.tf
@@ -0,0 +1,83 @@
+resource "aws_iam_role" "lambda_edrms_docs_exception_monitor_role" {
+  name = "${local.application_name}-${local.environment}-edrms_docs_exception_monitor_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+  tags = merge(local.tags, {
+    Name = "${local.application_name}-${local.environment}-edrms-docs-exception-monitor"
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "${local.application_name}-${local.environment}-edrms_docs_exception_monitor_policy"
+  role = aws_iam_role.lambda_edrms_docs_exception_monitor_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:GetLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sns:Publish"
+        ]
+        Resource = aws_sns_topic.cloudwatch_slack.arn
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "edrms_docs_exception_monitor" {
+  filename         = "./lambda/edrms_docs_exception.zip"
+  source_code_hash = filebase64sha256("./lambda/edrms_docs_exception.zip")
+  function_name    = "${local.application_name}-${local.environment}-edrms-docs-exception-monitor"
+  role             = aws_iam_role.lambda_edrms_docs_exception_monitor_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.13"
+  timeout          = 30
+  publish          = true
+
+  environment {
+    variables = {
+      LOG_GROUP_NAME      = aws_cloudwatch_log_group.log_group_edrms.name
+      SNS_TOPIC_ARN       = aws_sns_topic.certificate_expiration_alerts.arn
+    }
+  }
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  tags = merge(local.tags, {
+    Name = "${local.application_name}-${local.environment}-edrms-docs-exception-monitor"
+  })
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_invoke" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.edrms_docs_exception_monitor.function_name
+  principal     = "logs.amazonaws.com"
+  source_arn    = aws_cloudwatch_log_group.log_group_edrms.arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "edrms_docs_exception_filter" {
+  name            = "${local.application_name}-${local.environment}-edrms-docs-exception-filter"
+  log_group_name  = aws_cloudwatch_log_group.log_group_edrms.name
+  filter_pattern  = "\"EdrmsDocumentException\""
+  destination_arn = aws_lambda_function.edrms_docs_exception_monitor.arn
+}
