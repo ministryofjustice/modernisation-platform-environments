92347ab48894f0963e642553d18545cc264f0bdd
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-11-17 14:17:35 +0000

    WIP

--- terraform/environments/laa-cst-security-dashboard/random.tf
+++ terraform/environments/laa-cst-security-dashboard/random.tf
@@ -1,13 +1,9 @@
 resource "random_password" "cst_db" {
-  count = terraform.workspace == "laa-cst-security-dashboard" ? 1 : 0
-
   length  = 32
   special = false
 }
 
 resource "random_password" "cst_db_dev" {
-  count = terraform.workspace == "laa-cst-security-dashboard" ? 1 : 0
-
   length  = 32
   special = false
 }
