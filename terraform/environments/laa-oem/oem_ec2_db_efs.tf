21f649649c22da8e4d977f6da1570193f549dd76
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2025-11-17 13:07:36 +0000

    CC-4027: Branch with all EC2 resources and their dependencies removed.

--- terraform/environments/laa-oem/oem_ec2_db_efs.tf
+++ terraform/environments/laa-oem/oem_ec2_db_efs.tf
@@ -1,27 +1,27 @@
-resource "aws_efs_file_system" "oem_db_efs" {
-  encrypted        = true
-  kms_key_id       = data.aws_kms_key.ebs_shared.arn
-  performance_mode = "generalPurpose"
-  tags = merge(tomap({
-    "Name"                 = "${local.application_name}-db-efs"
-    "volume-attach-host"   = "db",
-    "volume-attach-device" = "efs://",
-    "volume-mount-path"    = "/opt/oem/backups"
-  }), local.tags)
-}
-
-resource "aws_efs_mount_target" "oem_db_efs" {
-  file_system_id = aws_efs_file_system.oem_db_efs.id
-  subnet_id      = data.aws_subnet.data_subnets_a.id
-  security_groups = [
-    aws_security_group.oem_db_efs_sg.id
-  ]
-}
-
-resource "aws_efs_backup_policy" "oem_db_efs_backup" {
-  file_system_id = aws_efs_file_system.oem_db_efs.id
-
-  backup_policy {
-    status = "ENABLED"
-  }
-}
+#resource "aws_efs_file_system" "oem_db_efs" {
+#  encrypted        = true
+#  kms_key_id       = data.aws_kms_key.ebs_shared.arn
+#  performance_mode = "generalPurpose"
+#  tags = merge(tomap({
+#    "Name"                 = "${local.application_name}-db-efs"
+#    "volume-attach-host"   = "db",
+#    "volume-attach-device" = "efs://",
+#    "volume-mount-path"    = "/opt/oem/backups"
+#  }), local.tags)
+#}
+#
+#resource "aws_efs_mount_target" "oem_db_efs" {
+#  file_system_id = aws_efs_file_system.oem_db_efs.id
+#  subnet_id      = data.aws_subnet.data_subnets_a.id
+#  security_groups = [
+#    aws_security_group.oem_db_efs_sg.id
+#  ]
+#}
+#
+#resource "aws_efs_backup_policy" "oem_db_efs_backup" {
+#  file_system_id = aws_efs_file_system.oem_db_efs.id
+#
+#  backup_policy {
+#    status = "ENABLED"
+#  }
+#}
