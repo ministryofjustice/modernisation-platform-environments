9dfd312bb5322f2bd52296be06bc80e3b993d07d
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2025-11-18 23:22:58 +0000

    fixing docker issue and cleanup

--- terraform/environments/ccms-ebs/ccms-ec2-smpt4dev.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-smpt4dev.tf
@@ -41,50 +41,14 @@ resource "aws_instance" "smtp4dev_mock_server" {
   iam_instance_profile        = aws_iam_instance_profile.smtp4dev_ec2_ssm_profile.name
 
  #place holder for user data changes
-  user_data_replace_on_change = false
+  user_data_replace_on_change = true
   user_data = base64encode(templatefile("./templates/ec2_user_data_smpt4dev.sh", {
     environment               = "${local.environment}"
   }))
-
-#   metadata_options {
-#     http_endpoint = "enabled"
-#     http_tokens   = "required"
-#   }
-
-  # Increase the volume size of the root volume
-#   root_block_device {
-#     volume_type = "gp3"
-#     volume_size = 50
-#     iops        = 3000
-#     encrypted   = true
-#     kms_key_id  = data.aws_kms_key.ebs_shared.key_id
-#     tags = merge(local.tags,
-#       { Name = lower(format("%s-%s", local.application_data.accounts[local.environment].instance_role_, "root")) },
-#       { device-name = "/dev/sda1" }
-#     )
-#   }
 
   tags = merge(local.tags,
     { Name = lower(format("ec2-%s-%s-smtp4dev", local.application_name, local.environment)) },
     { instance-scheduling = "skip-auto-start" },
     { backup = "true" }
   )
-
-#   depends_on = [aws_security_group.ec2_sg_mailrelay]
 }
-
-
-# Create Route53 record for SMTP4Dev EC2 instance
-# resource "aws_route53_record" "route53_record_smtp4dev" {
-#   provider = aws.core-vpc
-#   zone_id  = data.aws_route53_zone.internal.zone_id
-#   name     = "smtp4dev.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.internal"
-#   type     = "A"
-#   ttl      = "300"
-#   records  = [aws_instance.smtp4dev_mock_server.private_ip]
-# }
-
-# output "route53_record_smtp4dev" {
-#   description = "SMTP4Dev Route53 record"
-#   value       = aws_route53_record.route53_record_smtp4dev.fqdn
-# }
