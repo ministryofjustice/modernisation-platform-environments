eb3d78960c1f24f2e0b5e3f389509c5d86b4442b
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-11-18 17:00:33 +0000

    fix: permission for ecs log group

--- terraform/environments/ccms-edrms/ccms-lambda-function-edrms-docs-exception.tf
+++ terraform/environments/ccms-edrms/ccms-lambda-function-edrms-docs-exception.tf
@@ -43,7 +43,7 @@ resource "aws_iam_role_policy" "lambda_edrms_docs_exception_policy" {
           "logs:GetLogEvents",
           "logs:FilterLogEvents"
         ]
-        Resource = "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:${aws_cloudwatch_log_group.log_group_edrms.arn}:*"
+        Resource = "${aws_cloudwatch_log_group.log_group_edrms.arn}:*"
       }
     ]
   })
