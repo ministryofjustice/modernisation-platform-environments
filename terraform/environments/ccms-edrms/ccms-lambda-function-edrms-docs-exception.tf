0a9f20752e4a94649468e3b586229c751495eab6
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-11-18 15:38:38 +0000

    fix: reference to resource corrected

--- terraform/environments/ccms-edrms/ccms-lambda-function-edrms-docs-exception.tf
+++ terraform/environments/ccms-edrms/ccms-lambda-function-edrms-docs-exception.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role_policy" "lambda_edrms_docs_exception_policy" {
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ]
-        Resource = "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:function:${aws_lambda_function.edrms_docs_exception_monitor.function_name}"
+        Resource = "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:${aws_lambda_function.edrms_docs_exception_monitor.function_name}:*"
       }
     ]
   })
