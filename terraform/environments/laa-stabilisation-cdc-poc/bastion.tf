e2cf56c2ff00e6505dcd3e2ff04f2eedf287a5e6
Simon Harrison 1 <simon.harrison1@justice.gov.uk>
2025-11-19 12:32:10 +0000

    Use uppercase version V

--- terraform/environments/laa-stabilisation-cdc-poc/bastion.tf
+++ terraform/environments/laa-stabilisation-cdc-poc/bastion.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "bastion_linux" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=v4.5.0"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=V4.5.0"
 
   providers = {
     aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
