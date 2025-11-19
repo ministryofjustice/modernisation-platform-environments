e8a1272e22038bcb75ef5c1cdc9c739dc9e88951
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-11-19 15:43:36 +0000

    Fix linting

--- terraform/environments/laa-cst-security-dashboard/rds.tf
+++ terraform/environments/laa-cst-security-dashboard/rds.tf
@@ -23,20 +23,43 @@ resource "aws_db_subnet_group" "cst_database" {
 }
 
 resource "aws_db_instance" "cst_db" {
-  identifier              = "cst-postgres-db"
-  allocated_storage       = 20
-  instance_class          = "db.t3.micro"
-  engine                  = "postgres"
-  engine_version          = "16"
-  db_subnet_group_name    = aws_db_subnet_group.cst_database.name
-  username                = "postgresadmin"
-  password                = random_password.cst_db.result
-  publicly_accessible     = false
-  skip_final_snapshot     = true
-  deletion_protection     = true
-  backup_retention_period = 1
-  vpc_security_group_ids  = [aws_security_group.cst_rds_sc.id]
-  apply_immediately       = true
+  identifier                    = "cst-postgres-db"
+  allocated_storage             = 20
+  instance_class                = "db.t3.micro"
+  engine                        = "postgres"
+  engine_version                = "16"
+  db_subnet_group_name          = aws_db_subnet_group.cst_database.name
+  username                      = "postgresadmin"
+  password                      = random_password.cst_db.result
+  publicly_accessible           = false
+  skip_final_snapshot           = true
+  deletion_protection           = true
+  backup_retention_period       = 1
+  vpc_security_group_ids        = [aws_security_group.cst_rds_sc.id]
+  apply_immediately             = true
+
+  ## FIXES for checkov and PrismaCloud:
+  auto_minor_version_upgrade    = true        # CKV_AWS_226
+
+  multi_az                     = true        # CKV_AWS_157
+
+  monitoring_interval          = 60          # CKV_AWS_118
+
+  performance_insights_enabled = true        # CKV_AWS_353
+
+  iam_database_authentication_enabled = true # CKV_AWS_161
+
+  storage_encrypted            = true        # CKV_AWS_16
+
+  copy_tags_to_snapshot        = true        # CKV2_AWS_60
+
+  enabled_cloudwatch_logs_exports = [
+    "postgresql",               # CKV_AWS_129: PostgreSQL logs
+    "upgrade",
+    "audit"
+  ]
+
+  parameter_group_name = "default.postgres16"
 
   tags = {
     Name = "PostgresLatest"
