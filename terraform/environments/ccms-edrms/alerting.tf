3735c42cd75d9498c18ffd1084db011ea982b8ac
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-11-19 15:11:34 +0000

    fix: duplicate call removed

--- terraform/environments/ccms-edrms/alerting.tf
+++ terraform/environments/ccms-edrms/alerting.tf
@@ -7,13 +7,13 @@ module "guardduty_chatbot" {
   application_name = local.application_data.accounts[local.environment].app_name
 }
 
-module "cloudwatch_chatbot" {
-  source           = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0"
-  slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
-  sns_topic_arns   = [aws_sns_topic.cloudwatch_slack.arn]
-  tags             = local.tags #--This doesn't seem to pass to anything in the module but is a mandatory var. Consider submitting a PR to the module. AW
-  application_name = local.application_data.accounts[local.environment].app_name
-}
+# module "cloudwatch_chatbot" {
+#   source           = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0"
+#   slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
+#   sns_topic_arns   = [aws_sns_topic.cloudwatch_slack.arn]
+#   tags             = local.tags #--This doesn't seem to pass to anything in the module but is a mandatory var. Consider submitting a PR to the module. AW
+#   application_name = local.application_data.accounts[local.environment].app_name
+# }
 
 resource "aws_cloudwatch_event_rule" "guardduty" {
   name = "${local.application_name}-guardduty-findings"
