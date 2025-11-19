3400a999fbc3202ecbabdbf171d99fdc5e7f8a89
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-11-19 10:26:15 +0000

    UPdate versions file

--- terraform/environments/delius-iaps/versions.tf
+++ terraform/environments/delius-iaps/versions.tf
@@ -1,16 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 5.8, != 5.86.0"
+      version = "~> 6.16"
       source  = "hashicorp/aws"
     }
     http = {
       version = "~> 3.0"
       source  = "hashicorp/http"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
     }
   }
   required_version = "~> 1.10"
--- terraform/environments/laa-cst-security-dashboard/versions.tf
+++ terraform/environments/laa-cst-security-dashboard/versions.tf
@@ -1,13 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 6.0"
+      version = "~> 5.8, != 5.86.0"
       source  = "hashicorp/aws"
     }
     http = {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
-  required_version = "~> 1.0"
+  required_version = "~> 1.10"
 }
