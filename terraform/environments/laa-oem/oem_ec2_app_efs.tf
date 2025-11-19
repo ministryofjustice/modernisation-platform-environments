233a123aae86222b12dc7ae29dce3df15d407274
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2025-11-19 11:29:28 +0000

    CC-4027: Fixed typos.

--- terraform/environments/laa-oem/oem_ec2_app_efs.tf
+++ terraform/environments/laa-oem/oem_ec2_app_efs.tf
@@ -13,9 +13,7 @@ resource "aws_efs_file_system" "oem_app_efs" {
 resource "aws_efs_mount_target" "oem_app_efs" {
   file_system_id = aws_efs_file_system.oem_app_efs.id
   subnet_id      = data.aws_subnet.data_subnets_a.id
-  security_groups = [
-    aws_security_group.oem_app_efs_sg.id
-  ]
+# security_groups = [aws_security_group.oem_app_efs_sg.id]
 }
 
 resource "aws_efs_backup_policy" "oem_app_efs_backup" {
