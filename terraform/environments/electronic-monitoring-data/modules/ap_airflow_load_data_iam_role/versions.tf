514b30e23494fc7b5b46b4f5aa5524bdab0584bd
matt heery <matt.heery@justice.gov.uk>
2025-11-20 11:48:43 +0000

    fix: remove dot

--- terraform/environments/electronic-monitoring-data/modules/ap_airflow_load_data_iam_role/versions.tf
+++ terraform/environments/electronic-monitoring-data/modules/ap_airflow_load_data_iam_role/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~.>6.21, != 5.86.0"
+      version = "~>6.21, != 5.86.0"
     }
   }
   required_version = ">= 1.0.1"
