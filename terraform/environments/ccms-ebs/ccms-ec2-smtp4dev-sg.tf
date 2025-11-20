44b3857724a15b2e52d7edd8a292e5cee44326bd
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2025-11-20 11:36:42 +0000

    modfied as per the PR comments and suggestions

--- terraform/environments/ccms-ebs/ccms-ec2-smtp4dev-sg.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-smtp4dev-sg.tf
@@ -1,43 +1,56 @@
 resource "aws_security_group" "smtp4dev_mock_server_sg" {
+  count    = local.is-production ? 0 : 1
   name        = "smtp4dev_mock_server_sg"
   description = "Security group for smtp4dev mock server"
   vpc_id      = data.aws_vpc.shared.id
 
-  # Inbound rules
-  ingress {
-    description = "This rule is used for AWS Workspace vm"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.200.0.0/20"]
-  }
+  tags = merge(local.tags,
+      { Name = "smtp4dev_mock_server_sg" }
+  )
+}
 
-  ingress {
-    description = "This rule is used for smtp4dev for mail messages"
-    from_port   = 2525
-    to_port     = 2525
-    protocol    = "tcp"
-    cidr_blocks = ["10.26.60.223/32"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "smtp4dev_workspace_80_ingress_rule" {
+  count    = local.is-production ? 0 : 1
+  security_group_id = aws_security_group.smtp4dev_mock_server_sg[count.index].id
 
-  ingress {
-    description = "POP3 (Port 110)"
-    from_port   = 110
-    to_port     = 110
-    protocol    = "tcp"
-    cidr_blocks = ["10.26.60.223/32"]
-  }
+  description = "This rule is used for AWS Workspace vm"
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+  cidr_ipv4 = "10.200.0.0/20"
 
-  # Outbound rule
-  egress {
-    description = "HTTPS outbound (Port 443)"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+}
 
-  tags = {
-    Name = "smtp4dev_mock_server_sg"
-  }
+resource "aws_vpc_security_group_ingress_rule" "smtp4dev_ccmsebs_2525_ingress_rule" {
+  count    = local.is-production ? 0 : 1
+  security_group_id = aws_security_group.smtp4dev_mock_server_sg[count.index].id
+
+  description = "This rule is used for ccmsebs server"
+  ip_protocol = "tcp"
+  from_port   = 2525
+  to_port     = 2525
+  cidr_ipv4 = data.aws_subnet.data_subnets_a.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "smtp4dev_ccmsebs_110_ingress_rule" {
+  count    = local.is-production ? 0 : 1
+  security_group_id = aws_security_group.smtp4dev_mock_server_sg[count.index].id
+
+  description = "POP3 (Port 110)"
+  ip_protocol = "tcp"
+  from_port   = 110
+  to_port     = 110
+  cidr_ipv4 = data.aws_subnet.data_subnets_a.id
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "smtp4dev_http_egress_rule" {
+  count    = local.is-production ? 0 : 1
+  security_group_id = aws_security_group.smtp4dev_mock_server_sg[count.index].id
+
+  description = "HTTPS outbound (Port 443)"
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+  cidr_ipv4 = "0.0.0.0/0"
 }
