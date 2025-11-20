c52b122ef67ee30d6b65cb7a15a9ea4abad9c182
matt heery <matt.heery@justice.gov.uk>
2025-11-20 11:58:35 +0000

    fix: update zip bastion

--- terraform/environments/electronic-monitoring-data/bastion_linux.tf
+++ terraform/environments/electronic-monitoring-data/bastion_linux.tf
@@ -165,7 +165,7 @@ data "aws_iam_policy_document" "zip_s3_policy" {
 
 # tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
 module "zip_bastion" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=e4a3840d4b7b327228d1397bb684bc79cd4e76cb"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=1eaf3c9"
 
   providers = {
     aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
