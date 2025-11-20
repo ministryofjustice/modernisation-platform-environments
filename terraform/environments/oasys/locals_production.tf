348298a5580895333cfeeaa5ddd2b823a8deb452
wullub <13420301+wullub@users.noreply.github.com>
2025-11-20 16:54:08 +0000

    oasys prod bip resize

--- terraform/environments/oasys/locals_production.tf
+++ terraform/environments/oasys/locals_production.tf
@@ -376,9 +376,8 @@ locals {
           ])
         })
         instance = merge(local.ec2_instances.bip.instance, {
+          disable_api_termination      = true
           ami                          = "ami-0d206b8546ea2b68a" # to prevent instances being re-created due to recreated AMI
-          instance_type                = "r6i.4xlarge"           # this is massively over provisioned FIXME
-          metadata_options_http_tokens = "optional"              # accidentally set, remove this line when resizing
         })
         tags = merge(local.ec2_instances.bip.tags, {
           bip-db-hostname   = "ptctrn-oasys-db-a"
