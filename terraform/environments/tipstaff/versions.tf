7910a8f06d3c09ba5d286b385d0a8f6bf107e28e
Timi Olubusi <timiolubusi@icloud.com>
2025-11-21 17:25:43 +0000

    Selecting correct source for version

--- terraform/environments/tipstaff/versions.tf
+++ terraform/environments/tipstaff/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 5.0, != 5.86.0"
       source  = "hashicorp/aws"
+      version = "~> 5.0, != 5.86.0"
     }
     http = {
       version = "~> 3.0"
@@ -17,8 +17,8 @@ terraform {
       version = "~> 3.6"
     }
     github = {
-      version = "~> 4.28.0"
-      source  = "integrations/github"
+      source  = "hashicorp/github"
+      version = "~> 5.0"
     }
   }
   required_version = "~> 1.10"
