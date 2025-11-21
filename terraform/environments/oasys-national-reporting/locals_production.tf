8af804a7b17008590ea950d216d782cc07f5bc8e
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2025-11-21 12:01:13 +0000

    TM-1702: onr: improve dashboards (#14531)
    * remove default dashboard
    
    * -
    
    * add separate sections for BIP and BODS on dashboard

--- terraform/environments/oasys-national-reporting/locals_production.tf
+++ terraform/environments/oasys-national-reporting/locals_production.tf
@@ -37,6 +37,21 @@ locals {
       }
     }
 
+    cloudwatch_dashboards = {
+      "CloudWatch-Default" = {
+        periodOverride = "auto"
+        start          = "-PT6H"
+        widget_groups = [
+          module.baseline_presets.cloudwatch_dashboard_widget_groups.lb,
+          local.cloudwatch_dashboard_widget_groups.all_ec2,
+          local.cloudwatch_dashboard_widget_groups.cms,
+          local.cloudwatch_dashboard_widget_groups.web,
+          local.cloudwatch_dashboard_widget_groups.bods,
+          module.baseline_presets.cloudwatch_dashboard_widget_groups.ssm_command,
+        ]
+      }
+    }
+
     ec2_instances = {
       pd-onr-bods-1 = merge(local.ec2_instances.bods, {
         config = merge(local.ec2_instances.bods.config, {
