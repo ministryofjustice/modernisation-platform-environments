77e215f352c7c1a4bdd48646b9a962b5616626b7
Timi Olubusi <timiolubusi@icloud.com>
2025-11-24 17:42:09 +0000

    Fix for copied resource name and adding versions

--- terraform/environments/pra-register/ecs.tf
+++ terraform/environments/pra-register/ecs.tf
@@ -183,7 +183,7 @@ resource "aws_iam_role_policy" "app_execution" {
               "ecr:GetDownloadUrlForLayer",
               "ecr:BatchGetImage"
             ],
-            "Resource": "arn:aws:ecr:eu-west-2:${local.environment_management.account_ids[terraform.workspace]}:repository/${aws_ecr_repository.tipstaff_ecr_repo.name}",
+            "Resource": "arn:aws:ecr:eu-west-2:${local.environment_management.account_ids[terraform.workspace]}:repository/${aws_ecr_repository.pra_ecr_repo.name}",
             "Effect": "Allow"
       },
       {
