0d8269a3e4006bcf3eca7e28f5cae066fc6e8a17
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-11-24 17:33:23 +0000

    chore: refactor Route 53 records configuration and remove obsolete file
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/dns/route53-zones.tf
+++ terraform/environments/data-platform/dns/route53-zones.tf
@@ -1,5 +1,6 @@
 module "zone" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-route53.git?ref=6c5c0587f16701e8050d14d20b39d823534eec9a" # v6.1.1
 
-  name = local.environment_configuration.route53_zone_name
+  name    = local.environment_configuration.route53_zone_name
+  records = local.environment_configuration.route53_records
 }
