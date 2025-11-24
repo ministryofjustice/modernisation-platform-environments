fce42da77a81e432b5d788f4074a7f6fdc6f8f87
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2025-11-24 11:25:33 +0000

    chore: policy added

--- terraform/environments/ccms-ebs/member-data.tf
+++ terraform/environments/ccms-ebs/member-data.tf
@@ -75,32 +75,11 @@ data "aws_iam_policy_document" "sns_topic_policy_ec2cw" {
     principals {
       type = "Service"
       identifiers = [
-        "events.amazonaws.com",
+        "cloudwatch.amazonaws.com",
       ]
     }
   }
-  # policy_id = "SnsTopicId"
-  # statement {
-  #   sid = "statement1"
-  #   principals {
-  #     type        = "AWS"
-  #     identifiers = ["*"]
-  #   }
-  #   effect = "Allow"
-  #   actions = [
-  #     "SNS:GetTopicAttributes",
-  #     "SNS:SetTopicAttributes",
-  #     "SNS:AddPermission",
-  #     "SNS:DeleteTopic",
-  #     "SNS:Subscribe",
-  #     "SNS:ListSubscriptionsByTopic",
-  #     "SNS:Publish",
-  #     "SNS:Receive"
-  #   ]
-  #   resources = [
-  #     aws_sns_topic.cw_alerts.arn
-  #   ]
-  # }
+  
 }
 
 # data "aws_iam_policy_document" "sns_topic_policy_s3" {
