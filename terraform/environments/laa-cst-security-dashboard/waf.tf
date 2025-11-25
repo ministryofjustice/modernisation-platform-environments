97d16262496ab4ec5d6e20c27d70a497ac5f49c8
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-11-25 13:40:30 +0000

    Amend checkov fixes on RDS and WAF

--- terraform/environments/laa-cst-security-dashboard/waf.tf
+++ terraform/environments/laa-cst-security-dashboard/waf.tf
@@ -25,10 +25,30 @@ resource "aws_wafv2_web_acl" "basic" {
       sampled_requests_enabled   = true
     }
   }
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+  }
 
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name                = "basicWaf"
     sampled_requests_enabled   = true
   }
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "basic_waf_logging" {
+  log_destination_configs = [aws_cloudwatch_log_group.basic_waf_logs.arn]
+  resource_arn            = aws_wafv2_web_acl.basic_web_acl.arn
 }
