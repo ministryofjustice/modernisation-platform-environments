2983bb5a33a86e5ba1184a991eebccbf5e7fe832
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2025-11-25 17:30:01 +0000

    Clean-up the unwanted resources

--- terraform/environments/ccms-ebs/ccms-ftp-lambda.tf
+++ terraform/environments/ccms-ebs/ccms-ftp-lambda.tf
@@ -112,22 +112,6 @@ resource "aws_s3_bucket_versioning" "s3_versioning" {
   }
 }
 
-# resource "aws_s3_bucket_lifecycle_configuration" "inbound_bucket_lifecycle_delete_noncurrent_versions" {
-
-#   bucket = aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].id
-
-#   rule {
-#     id     = "delete-noncurrent-versions-after-5-days"
-#     status = "Enabled"
- 
-#     # No filter → applies to whole bucket
-#     filter {}
- 
-#     noncurrent_version_expiration {
-#       noncurrent_days = 5
-#     }
-#   }
-# }
 
 resource "aws_s3_bucket_lifecycle_configuration" "outbound_bucket_lifecycle_delete_noncurrent_versions" {
 
@@ -210,24 +194,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "inbound_bucket_lifecycle" {
   }
 }
 
-# resource "aws_s3_bucket_lifecycle_configuration" "inbound_bucket_lifecycle_archive_folder" {
-
-#   bucket = aws_s3_bucket.buckets["laa-ccms-inbound-${local.environment}-mp"].id
-  
-#   rule {
-#     id     = "delete-archive-folder-file-after-5-days"
-#     status = "Enabled"
- 
-#     # No filter → applies to whole bucket
-#     filter {
-#       prefix = "archive/"
-#     }
- 
-#     expiration {
-#       days = 5   # delete objects 5 days after creation
-#     }
-#   }
-# }
 
 #--Dynamic blocks for transfer family policy in production only
 data "aws_iam_policy_document" "inbound_bucket_policy" {
