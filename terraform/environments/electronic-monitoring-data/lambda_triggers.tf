c7602e73d200dc75345f3336137c681b3ea94b13
Lucy Astley-Jones <lucy.astleyjones@justice.gov.uk>
2025-11-26 14:25:51 +0000

    add event mapping + encryption

--- terraform/environments/electronic-monitoring-data/lambda_triggers.tf
+++ terraform/environments/electronic-monitoring-data/lambda_triggers.tf
@@ -216,3 +216,12 @@ resource "aws_sqs_queue" "clean_mdss_load_queue" {
   visibility_timeout_seconds = 30
   message_retention_seconds = 432000 # 5 days
 }
+
+resource "aws_lambda_event_source_mapping" "trigger_cleanup" {
+  event_source_arn = aws_sqs_queue.clean_mdss_load_queue.arn
+  function_name    = module.clean_after_mdss_load.lambda_function_name
+  batch_size       = 10
+  scaling_config {
+    maximum_concurrency = 1000
+  }
+}
