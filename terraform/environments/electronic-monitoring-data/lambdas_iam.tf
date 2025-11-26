b714520ea32e4e587da5cc383641ea5f81d6fe2f
Lucy Astley-Jones <lucy.astleyjones@justice.gov.uk>
2025-11-26 15:11:09 +0000

    missing comma

--- terraform/environments/electronic-monitoring-data/lambdas_iam.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_iam.tf
@@ -1038,7 +1038,7 @@ data "aws_iam_policy_document" "clean_after_mdss_load_lambda_role_policy_documen
     effect = "Allow"
     actions = [
       "glue:GetTable",
-      "glue:GetTables"
+      "glue:GetTables",
       "glue:GetDatabase",
       "glue:GetDatabases",
       "glue:DeleteTable",
