cce7481d3137ddf7cabd077e4c8d37f4044bc5b3
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-11-26 10:48:22 +0000

    Add additional s3 bucket information

--- terraform/environments/laa-cst-security-dashboard/s3.tf
+++ terraform/environments/laa-cst-security-dashboard/s3.tf
@@ -1,3 +1,53 @@
+module "s3_cst_docs" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v9.0.0"
+
+  bucket_name        = "${local.application_name}-docs-${local.environment}"
+  versioning_enabled = true
+  ownership_controls = "BucketOwnerEnforced"
+
+  lifecycle_rule = [
+    {
+      id      = "cst_docs_lifecycle"
+      enabled = "Enabled"
+      prefix  = ""
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+        },
+        {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_transition = [
+        {
+          days          = 30
+          storage_class = "STANDARD_IA"
+        },
+        {
+          days          = 90
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 365
+      }
+    }
+  ]
+
+  tags = merge(local.tags,
+    { Name = lower(format("%s-docs-%s", local.application_name, local.environment)) }
+  )
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+}
+
 # S3 for Load Balancer access logs
 
 # S3 Bucket - Logging
