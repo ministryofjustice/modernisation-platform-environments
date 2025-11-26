9d3527dd66091f741989ca1d4e066ab3eda945bd
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-11-26 13:15:17 +0000

    Update WAF to module

--- terraform/environments/laa-cst-security-dashboard/waf.tf
+++ terraform/environments/laa-cst-security-dashboard/waf.tf
@@ -1,70 +1,42 @@
-# modules/waf.tf
 
-resource "aws_wafv2_web_acl" "cst_waf" {
-  name        = "cst-waf"
-  scope       = "REGIONAL"
-  default_action { 
-    block {} 
+module "waf" {
+  source                   = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-aws-waf"
+  enable_pagerduty_integration = true
+  enable_ddos_protection = true
+  ddos_rate_limit        = 5000
+  block_non_uk_traffic   = false
+  associated_resource_arns = [aws_lb.waf_lb.arn]
+  managed_rule_actions = {
+    AWSManagedRulesKnownBadInputsRuleSet = true
+    AWSManagedRulesCommonRuleSet         = true
+    AWSManagedRulesSQLiRuleSet           = true
+    AWSManagedRulesLinuxRuleSet          = false
+    AWSManagedRulesAnonymousIpList       = false
+    AWSManagedRulesBotControlRuleSet     = false
   }
+  
+  core_logging_account_id = local.environment_management.account_ids["core-logging-production"]
 
-  rule {
-    name     = "AWSManagedRulesCommonRuleSet"
-    priority = 1
-    override_action { 
-        count {} 
-    }
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "commonRuleSet"
-      sampled_requests_enabled   = true
-    }
-  }
-  rule {
-    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
-    priority = 2
-
-    override_action {
-      none {}
-    }
-
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesKnownBadInputsRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "BadInputRuleSet"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "cstWaf"
-    sampled_requests_enabled   = true
-  }
+  application_name = local.application_name        
+  tags             = local.tags
 }
 
-# WAF Logging to CloudWatch
-resource "aws_cloudwatch_log_group" "cst_waf_logs" {
-  name              = "aws-waf-logs-${local.application_name}"
-  retention_in_days = 365
-  kms_key_id = var.kms_key_id
 
-  tags = merge(local.tags,
-    { Name = lower(format("%s-waf-logs", local.application_name)) }
-  )
-}
 
-resource "aws_wafv2_web_acl_logging_configuration" "cst_waf_logging" {
-  log_destination_configs = [aws_cloudwatch_log_group.cst_waf_logs.arn]
-  resource_arn            = aws_wafv2_web_acl.cst_waf.arn
+  additional_managed_rules = [
+  {
+    name            = "AWSManagedRulesPHPRuleSet"
+    vendor_name     = "AWS"
+    override_action = "count"
+  },
+  {
+    name        = "AWSManagedRulesUnixRuleSet"
+    vendor_name = "AWS"
+    override_action = "count"
+  }
+]
+
+  application_name = local.application_name        
+  tags             = local.tags
+
 }
