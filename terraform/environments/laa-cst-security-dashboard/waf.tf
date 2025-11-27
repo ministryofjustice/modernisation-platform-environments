646386a439ef6e6d452e207d1f4cca3377de61b1
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-11-27 11:52:37 +0000

    readd WAF

---
+++ terraform/environments/laa-cst-security-dashboard/waf.tf
@@ -0,0 +1,35 @@
+
+module "waf" {
+  source                   = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-aws-waf"
+  enable_pagerduty_integration = true
+  enable_ddos_protection = true
+  ddos_rate_limit        = 5000
+  block_non_uk_traffic   = false
+  associated_resource_arns = [aws_lb.waf_lb.arn]
+  managed_rule_actions = {
+    AWSManagedRulesKnownBadInputsRuleSet = false
+    AWSManagedRulesCommonRuleSet         = false
+    AWSManagedRulesSQLiRuleSet           = false
+    AWSManagedRulesLinuxRuleSet          = false
+    AWSManagedRulesAnonymousIpList       = false
+    AWSManagedRulesBotControlRuleSet     = false
+  }
+
+  core_logging_account_id = local.environment_management.account_ids["core-logging-production"]
+
+  application_name = local.application_name
+  tags             = local.tags
+
+  additional_managed_rules = [
+  {
+    name            = "AWSManagedRulesPHPRuleSet"
+    vendor_name     = "AWS"
+    override_action = "count"
+  },
+  {
+    name        = "AWSManagedRulesUnixRuleSet"
+    vendor_name = "AWS"
+    override_action = "count"
+  }
+]
+}
