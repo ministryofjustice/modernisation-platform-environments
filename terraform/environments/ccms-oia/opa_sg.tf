ffdcbaadbed8404eb089fedc824a057966ad33f3
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2025-11-28 16:37:24 +0000

    build oia in test

--- terraform/environments/ccms-oia/opa_sg.tf
+++ terraform/environments/ccms-oia/opa_sg.tf
@@ -11,14 +11,14 @@ resource "aws_security_group" "opahub_load_balancer" {
 }
 
 # # Temp only for first install - to be removed after
-resource "aws_vpc_security_group_ingress_rule" "alb_ingress_7001_all" {
-  security_group_id = aws_security_group.opahub_load_balancer.id
-  cidr_ipv4         = local.application_data.accounts[local.environment].aws_workspace
-  ip_protocol       = "tcp"
-  from_port         = 7001
-  to_port           = 7001
-  description       = "7001 from anywhere (WAF controlled)"
-}
+# resource "aws_vpc_security_group_ingress_rule" "alb_ingress_7001_all" {
+#   security_group_id = aws_security_group.opahub_load_balancer.id
+#   cidr_ipv4         = local.application_data.accounts[local.environment].aws_workspace
+#   ip_protocol       = "tcp"
+#   from_port         = 7001
+#   to_port           = 7001
+#   description       = "7001 from anywhere (WAF controlled)"
+# }
 resource "aws_vpc_security_group_ingress_rule" "alb_ingress_443_all" {
   security_group_id = aws_security_group.opahub_load_balancer.id
   cidr_ipv4         = "0.0.0.0/0"
