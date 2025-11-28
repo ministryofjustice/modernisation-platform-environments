f60bcfc9386b88ef8f65a8f4f9d8af183bb0d45e
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-11-28 16:46:17 +0000

    fix: secret reference

--- terraform/environments/ccms-ebs/ccms-secrets.tf
+++ terraform/environments/ccms-ebs/ccms-secrets.tf
@@ -66,7 +66,7 @@ resource "aws_secretsmanager_secret" "ebs_cw_alerts_secrets" {
 }
 
 resource "aws_secretsmanager_secret_version" "ebs_cw_alerts_secrets" {
-  secret_id = aws_secretsmanager_secret.edrms_docs_exception_secrets.id
+  secret_id = aws_secretsmanager_secret.ebs_cw_alerts_secrets.id
   secret_string = jsonencode({
     "slack_channel_webhook" = ""
   })
