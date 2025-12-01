171e0933388ca195fd5e05d1b012c8c616d64ea8
ghill26 <George.Hill2@justice.gov.uk>
2025-12-01 14:16:33 +0000

    dev-nomis-weblogic-12: Set branch to main

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -166,7 +166,7 @@ locals {
         })
         user_data_cloud_init = merge(local.ec2_autoscaling_groups.web12.user_data_cloud_init, {
           args = merge(local.ec2_autoscaling_groups.web12.user_data_cloud_init.args, {
-            branch = "TM-1756"
+            branch = "main"
           })
         })
         tags = merge(local.ec2_autoscaling_groups.web12.tags, {
