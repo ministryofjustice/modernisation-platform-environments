e9ac46d7ab327605ab5ec6083cafd6ed8a3383ba
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-12-02 17:12:28 +0000

    fix: hash problem with lambda directory contents

--- terraform/environments/ccms-ebs/ccms-lambda-cloudwatch-sns.tf
+++ terraform/environments/ccms-ebs/ccms-lambda-cloudwatch-sns.tf
@@ -72,7 +72,7 @@ resource "aws_lambda_layer_version" "lambda_cloudwatch_sns_layer" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir= "${path.module}/lambda/cloudwatch_alarm_slack_integration"
+  source_dir = "${path.module}/lambda/cloudwatch_alarm_slack_integration"
   output_path = "${path.module}/lambda/cloudwatch_alarm_slack_integration.zip"
 }
 
