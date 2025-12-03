6f6b70af1073a336448c4b3ff366062e59cf79b6
mnasr-moj <mohamed.nasr@justice.gov.uk>
2025-12-03 10:17:56 +0000

    adding safe01 nlb subnets to patch lambda sg

--- terraform/environments/laa-enterprise-service-bus/patch_cwa_extract_lambda.tf
+++ terraform/environments/laa-enterprise-service-bus/patch_cwa_extract_lambda.tf
@@ -48,6 +48,17 @@ resource "aws_security_group_rule" "patch_cwa_extract_egress_safe02" {
   description       = "CWA Prod - Safe02 DB"
 }
 
+resource "aws_security_group_rule" "patch_cwa_extract_egress_safe01" {
+  count             = local.environment == "test" ? 1 : 0
+  type              = "egress"
+  from_port         = 2484
+  to_port           = 2484
+  protocol          = "tcp"
+  cidr_blocks       = ["10.205.14.0/26", "10.205.14.0/26"] # CWA ECP SAFE01 Loadbalancer subnets
+  security_group_id = aws_security_group.patch_cwa_extract_sg[0].id
+  description       = "CWA Prod - Safe01 DB"
+}
+
 resource "aws_security_group_rule" "patch_cwa_extract_egress_https_endpoint" {
   count                    = local.environment == "test" ? 1 : 0
   type                     = "egress"
