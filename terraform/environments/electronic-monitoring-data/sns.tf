9b04d7a9a5dc06a1b728c21f2a9eb0cd51d1c4fc
Khristiania <khristiania.raihan@justice.gov.uk>
2025-12-05 16:53:08 +0000

    updated permission

--- terraform/environments/electronic-monitoring-data/sns.tf
+++ terraform/environments/electronic-monitoring-data/sns.tf
@@ -5,6 +5,7 @@ resource "aws_sns_topic" "emds_alerts" {
 data "aws_iam_policy_document" "emds_alerts_topic_policy" {
   version = "2012-10-17"
 
+  # Allow CloudWatch alarms to publish
   statement {
     sid    = "AllowCloudWatchToPublish"
     effect = "Allow"
@@ -13,14 +14,35 @@ data "aws_iam_policy_document" "emds_alerts_topic_policy" {
       "sns:Publish",
     ]
 
-    resources = [
-      aws_sns_topic.emds_alerts.arn
-    ]
+    resources = [aws_sns_topic.emds_alerts.arn]
 
     principals {
       type = "Service"
       identifiers = [
         "cloudwatch.amazonaws.com"
+      ]
+    }
+  }
+
+  # Allow AWS Chatbot HTTPS endpoint ingestion
+  statement {
+    sid    = "AllowChatbotToConsume"
+    effect = "Allow"
+
+    actions = [
+      "sns:Subscribe",
+      "sns:Receive",
+      "sns:Publish"
+    ]
+
+    resources = [aws_sns_topic.emds_alerts.arn]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "sns.amazonaws.com",
+        "events.amazonaws.com",
+        "chatbot.amazonaws.com"
       ]
     }
   }
