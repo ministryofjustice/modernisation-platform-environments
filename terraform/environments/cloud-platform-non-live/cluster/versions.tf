52bd2b5cfe349a621e50cd8b343f3126a6ebf638
Ky <ky.truong@digital.justice.gov.uk>
2025-12-05 16:27:19 +0000

    adding providers

--- terraform/environments/cloud-platform-non-live/cluster/versions.tf
+++ terraform/environments/cloud-platform-non-live/cluster/versions.tf
@@ -16,6 +16,18 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    time = {
+      version = "~> 0.10"
+      source  = "hashicorp/time"
+    }
+    tls = {
+      version = "~> 4.0"
+      source  = "hashicorp/tls"
+    }
+    null = {
+      version = "~> 3.0"
+      source  = "hashicorp/null"
+    }
   }
   required_version = "~> 1.0"
 }
