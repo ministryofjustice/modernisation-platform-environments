de188b6a633821c86dfee8722c13af5c055d5a8d
Vladimirs Kovalovs <vladimirs.kovalovs1@justice.gov.uk>
2025-12-05 11:02:15 +0000

    [STB-3272] fixed tf plan error on route53

--- terraform/environments/oas/new-rds-route53.tf
+++ terraform/environments/oas/new-rds-route53.tf
@@ -2,6 +2,8 @@
 ### RDS Route53 Record
 ######################################
 resource "aws_route53_record" "oas-rds-new" {
+  count = local.environment == "preproduction" ? 1 : 0
+
   provider = aws.core-vpc
   zone_id  = data.aws_route53_zone.external.zone_id
   name     = "rds.${local.application_name}.${data.aws_route53_zone.external.name}"
