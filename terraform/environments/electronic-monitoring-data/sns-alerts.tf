c66ea73d2f5064afc353228a30e2f3e569fa867e
Khristiania <khristiania.raihan@justice.gov.uk>
2025-12-05 16:03:51 +0000

    adding missing SNS permission

--- terraform/environments/electronic-monitoring-data/sns-alerts.tf
+++ terraform/environments/electronic-monitoring-data/sns-alerts.tf
@@ -1,3 +1,4 @@
-resource "aws_sns_topic" "emds_alerts" {
-  name = "emds-alerts"
+resource "aws_sns_topic_policy" "emds_alerts" {
+  arn    = aws_sns_topic.emds_alerts.arn
+  policy = data.aws_iam_policy_document.emds_alerts_topic_policy.json
 }
---
+++ terraform/environments/electronic-monitoring-data/sns.tf
@@ -0,0 +1,23 @@
+data "aws_iam_policy_document" "emds_alerts_topic_policy" {
+  version = "2012-10-17"
+
+  statement {
+    sid    = "AllowCloudWatchToPublish"
+    effect = "Allow"
+
+    actions = [
+      "sns:Publish",
+    ]
+
+    resources = [
+      aws_sns_topic.emds_alerts.arn
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "cloudwatch.amazonaws.com"
+      ]
+    }
+  }
+}
