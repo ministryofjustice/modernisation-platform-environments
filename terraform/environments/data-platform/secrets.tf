f20f85dd32208b5b14966d1b04b2b5b97274cb71
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-12-08 18:31:24 +0000

    chore: update secrets manager module source version to v2.0.1
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/secrets.tf
+++ terraform/environments/data-platform/secrets.tf
@@ -1,7 +1,7 @@
 module "cloud_platform_live_secret" {
   count = terraform.workspace == "data-platform-development" ? 1 : 0
 
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=82029345dea22bc49989a6f46c5d8d8e555b84c9" # v2.0.0
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=82029345dea22bc49989a6f46c5d8d8e555b84c9" # v2.0.1
 
   name = "cloud-platform/live"
 
