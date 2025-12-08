a2db48531c694d0e28454fe101636331d77f9780
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-12-08 16:38:06 +0000

    shared s3 bucket

--- terraform/environments/laa-ccms-soa/member-locals.tf
+++ terraform/environments/laa-ccms-soa/member-locals.tf
@@ -14,4 +14,10 @@ locals {
   logging_bucket_name       = "${local.application_data.accounts[local.environment].app_name}-${local.environment}-logging"
   lb_log_prefix_soa_admin   = "${local.application_data.accounts[local.environment].app_name}-admin-lb"
   lb_log_prefix_soa_managed = "${local.application_data.accounts[local.environment].app_name}-managed-lb"
+
+  lambda_folder_name = [
+    "lambda_delivery",
+    "ccms-soa-edn-quiesced-layer"
+  ]
+
 }
