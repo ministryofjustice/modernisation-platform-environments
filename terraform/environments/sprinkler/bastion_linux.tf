749fe87813fc8c0800c024befe80251eaa7d5681
ep-93 <109581241+ep-93@users.noreply.github.com>
2025-12-08 08:35:26 +0000

    Update bastion_linux module source reference

--- terraform/environments/sprinkler/bastion_linux.tf
+++ terraform/environments/sprinkler/bastion_linux.tf
@@ -4,7 +4,7 @@ locals {
 
 # tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning
 module "bastion_linux" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=95ed3c3f454e2014a62990aacd5d68c64d026f11" # v4.2.1
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=9facf9fc8f8b8e3f93ffbda822028534b9a75399" # v9.0.0
 
   providers = {
     aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
