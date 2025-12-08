995065c2770f9f82aedb9b9c09bdb1eb03d86d07
jackstockley89 <jstock8904@gmail.com>
2025-12-08 13:09:08 +0000

    feat(TF): add eks module

--- terraform/environments/cloud-platform-non-live/cluster/eks.tf
+++ terraform/environments/cloud-platform-non-live/cluster/eks.tf
@@ -1,1 +1,67 @@
-// Placeholder for EKS cluster module
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["${local.application_name}-${local.environment}"]
+  }
+}
+
+data "aws_subnets" "eks_private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["${local.application_name}-${local.environment}-private"]
+  }
+}
+
+data "aws_subnets" "eks_public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["${local.application_name}-${local.environment}-public"]
+  }
+}
+
+module "eks" {
+  count = contains(local.enabled_workspaces, terraform.workspace) ? 1 : 0
+  
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "non-live-${local.environment}"
+  kubernetes_version = "1.34"
+  vpc_id     = data.aws_vpc.selected.id
+  subnet_ids = concat(data.aws_subnets.eks_private.ids, data.aws_subnets.eks_public.ids)
+  enable_irsa = true
+
+
+  cloudwatch_log_group_retention_in_days = 30
+
+  eks_managed_node_groups = local.eks_managed_node_groups
+  
+  addons = {
+    coredns = {
+      enabled = true
+      version = "v1.12.4-eksbuild.1"
+    }
+    kube-proxy = {
+      enabled = true
+      version = "v1.34.1-eksbuild.2"
+    }
+    vpc-cni = {
+      enabled = true
+      version = "v1.20.4-eksbuild.1"
+    }
+  }
+
+
+
+
+  tags = local.tags
+
+}
