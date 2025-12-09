c7692f44b7cfa7c071191335a746e426d5583f8b
matt heery <matt.heery@justice.gov.uk>
2025-12-09 10:27:50 +0000

    fix: map

--- terraform/environments/electronic-monitoring-data/lambda_triggers.tf
+++ terraform/environments/electronic-monitoring-data/lambda_triggers.tf
@@ -45,9 +45,9 @@ resource "aws_s3_bucket_notification" "data_bucket_triggers" {
     filter_prefix = "serco/fms"
   }
   dynamic "queue" {
-    for_each = toset([module.load_historic_csv_sqs])
+    for_each = { for k, v in module.load_historic_csv_sqs : k => v }
     content {
-      queue_arn     = each.key.sqs_queue.arn
+      queue_arn     = each.value.sqs_queue.arn
       events        = ["s3:ObjectCreated:*"]
       filter_suffix = ".csv"
       filter_prefix = "g4s/lcm"
