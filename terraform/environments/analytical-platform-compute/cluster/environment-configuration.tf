5f5868b362f68420c013ef58267d512c1037e8f2
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-12-09 14:44:20 +0000

    asdf

--- terraform/environments/analytical-platform-compute/cluster/environment-configuration.tf
+++ terraform/environments/analytical-platform-compute/cluster/environment-configuration.tf
@@ -60,15 +60,15 @@ locals {
       eks_cluster_version = "1.33"
       eks_node_version    = "1.44.0-244cd3a5"
       eks_cluster_addon_versions = {
-        kube_proxy                        = "v1.33.3-eksbuild.4"
-        aws_efs_csi_driver                = "v2.1.10-eksbuild.1"
-        aws_network_flow_monitoring_agent = "v1.0.3-eksbuild.1"
-        eks_node_monitoring_agent         = "v1.4.0-eksbuild.2"
-        coredns                           = "v1.12.2-eksbuild.4"
-        eks_pod_identity_agent            = "v1.3.8-eksbuild.2"
-        aws_guardduty_agent               = "v1.10.0-eksbuild.2"
-        aws_ebs_csi_driver                = "v1.47.0-eksbuild.1"
-        vpc_cni                           = "v1.20.1-eksbuild.1"
+        kube_proxy                        = "v1.33.5-eksbuild.2"
+        aws_efs_csi_driver                = "v2.1.15-eksbuild.1"
+        aws_network_flow_monitoring_agent = "v1.1.1-eksbuild.1"
+        eks_node_monitoring_agent         = "v1.4.2-eksbuild.1"
+        coredns                           = "v1.12.4-eksbuild.1"
+        eks_pod_identity_agent            = "v1.3.10-eksbuild.1"
+        aws_guardduty_agent               = "v1.12.1-eksbuild.2"
+        aws_ebs_csi_driver                = "v1.53.0-eksbuild.1"
+        vpc_cni                           = "v1.20.5-eksbuild.1"
       }
 
       /* Data Engineering Airflow */
