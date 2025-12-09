a60da9c881e4e4f8cb8d38ed40af9febd93ff850
nbuckingham72 <nick.buckingham@colt.net>
2025-12-09 12:00:36 +0000

    Update_091225_3

--- terraform/environments/ppud/certificates.tf
+++ terraform/environments/ppud/certificates.tf
@@ -35,21 +35,6 @@ resource "aws_acm_certificate" "preprod_certificates" {
   }
 }
 
-# Output only needs to be enabled to view the CNAME records required for the justice.gov.uk DNS zone.
-/*
-output "preprod_cname_validation_records" {
-  value = local.is-preproduction ? {
-    for cert_key, cert in aws_acm_certificate.preprod_certificates : cert_key => [
-      for option in cert.domain_validation_options : {
-        name   = option.resource_record_name
-        type   = option.resource_record_type
-        value  = option.resource_record_value
-      }
-    ]
-  } : {}
-}
-*/
-
 resource "aws_acm_certificate_validation" "preprod_certificate_validation" {
   for_each        = local.is-preproduction ? aws_acm_certificate.preprod_certificates : {}
   certificate_arn = each.value.arn
@@ -92,16 +77,4 @@ resource "aws_acm_certificate_validation" "prod_certificate_validation" {
   validation_record_fqdns = [
     for option in each.value.domain_validation_options : option.resource_record_name
   ]
-}
-
-output "prod_cname_validation_records" {
-  value = local.is-production ? {
-    for cert_key, cert in aws_acm_certificate.prod_certificates : cert_key => [
-      for option in cert.domain_validation_options : {
-        name   = option.resource_record_name
-        type   = option.resource_record_type
-        value  = option.resource_record_value
-      }
-    ]
-  } : {}
 }
