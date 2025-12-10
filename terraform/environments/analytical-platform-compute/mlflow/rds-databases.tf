929025447cf600a4ad4d77d7db2f9f412500cd19
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-12-10 09:57:35 +0000

    :copilot: chore(terraform): update module versions in analytical-platform-compute/mlflow

--- terraform/environments/analytical-platform-compute/mlflow/rds-databases.tf
+++ terraform/environments/analytical-platform-compute/mlflow/rds-databases.tf
@@ -3,7 +3,7 @@ module "mlflow_auth_rds" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/rds/aws"
-  version = "6.12.0"
+  version = "6.13.1"
 
   identifier = "mlflow-auth"
 
@@ -73,7 +73,7 @@ module "mlflow_rds" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/rds/aws"
-  version = "6.12.0"
+  version = "6.13.1"
 
   identifier = "mlflow"
 
