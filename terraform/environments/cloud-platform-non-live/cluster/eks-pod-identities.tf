a0e61f14f13044a24bd1b1d1b7f06f88012d1dbc
Ky <ky.truong@digital.justice.gov.uk>
2025-12-11 14:45:24 +0000

    commenting out EKS to destroy

--- terraform/environments/cloud-platform-non-live/cluster/eks-pod-identities.tf
+++ terraform/environments/cloud-platform-non-live/cluster/eks-pod-identities.tf
@@ -1,21 +1,21 @@
-module "aws_vpc_cni_pod_identity" {
+# module "aws_vpc_cni_pod_identity" {
 
-  source  = "terraform-aws-modules/eks-pod-identity/aws"
-  version = "2.5.0"
+#   source  = "terraform-aws-modules/eks-pod-identity/aws"
+#   version = "2.5.0"
 
-  name = "aws-vpc-cni"
+#   name = "aws-vpc-cni"
 
-  attach_aws_vpc_cni_policy = true
-  aws_vpc_cni_enable_ipv4   = true
-  aws_vpc_cni_enable_ipv6   = true
+#   attach_aws_vpc_cni_policy = true
+#   aws_vpc_cni_enable_ipv4   = true
+#   aws_vpc_cni_enable_ipv6   = true
 
-  associations = {
-    this = {
-      cluster_name    = module.eks[0].cluster_name
-      namespace       = "kube-system"
-      service_account = "aws-node"
-    }
-  }
+#   associations = {
+#     this = {
+#       cluster_name    = module.eks[0].cluster_name
+#       namespace       = "kube-system"
+#       service_account = "aws-node"
+#     }
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
