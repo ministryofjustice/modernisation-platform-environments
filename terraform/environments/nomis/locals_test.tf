10bf29a0d18f85e2f73d6b901ebe1ed81a6b995a
Dave Kent <dave.kent@justice.gov.uk>
2025-12-12 16:07:13 +0000

    tm-1791-decomm-azure-dns-zones-nomis-test

--- terraform/environments/nomis/locals_test.tf
+++ terraform/environments/nomis/locals_test.tf
@@ -31,6 +31,16 @@ locals {
           description = "wildcard cert for nomis test domains"
         }
       }
+      nomis_wildcard_cert_v3 = {
+        cloudwatch_metric_alarms = module.baseline_presets.cloudwatch_metric_alarms.acm
+        domain_name              = "*.test.nomis.service.justice.gov.uk"
+        subject_alternate_names = [
+          "*.nomis.hmpps-test.modernisation-platform.service.justice.gov.uk",
+        ]
+        tags = {
+          description = "wildcard cert for nomis test domains"
+        }
+      }      
     }
 
     cloudwatch_dashboards = {
