5bb8b2d7985b561c5ddf8169d273893fcae48987
nbuckingham72 <nick.buckingham@colt.net>
2025-12-12 07:48:12 +0000

    Update_121225_1

--- terraform/environments/ppud/cloudwatch_alarms_elb.tf
+++ terraform/environments/ppud/cloudwatch_alarms_elb.tf
@@ -19,7 +19,7 @@ resource "aws_cloudwatch_metric_alarm" "high_target_response_time_wam_elb" {
   threshold           = "2"
   treat_missing_data  = "notBreaching"
   alarm_description   = "This metric monitors the target response time of the WAM load balancer. If the target response time averages over 1 second for 60 minutes, the alarm will trigger."
-  alarm_actions       = [aws_sns_topic.cw_alerts[0].arn]
+  alarm_actions       = [aws_sns_topic.cw_std_and_sms_alerts[0].arn]
   dimensions = {
     LoadBalancer     = "app/WAM-ALB-PROD/bfc963544454bdde"
     AvailabilityZone = "eu-west-2a"
