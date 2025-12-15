760fb22eea10346865244a052d614a8d7235c23f
Ky <ky.truong@digital.justice.gov.uk>
2025-12-15 15:24:59 +0000

    security group for VPC endpoints

--- terraform/environments/cloud-platform-non-live/network/vpc-endpoints.tf
+++ terraform/environments/cloud-platform-non-live/network/vpc-endpoints.tf
@@ -1,8 +1,25 @@
+module "vpc_endpoints_security_group" {
+
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+
+  name        = "${module.vpc.name}-vpc-endpoints"
+  description = "VPC endpoints security group"
+
+  vpc_id = module.vpc.vpc_id
+
+  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
+  ingress_rules       = ["https-443-tcp"]
+
+  tags = local.tags
+}
+
 module "vpc_vpc-endpoints" {
   source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
   version = "6.5.1"
 
   vpc_id = module.vpc.vpc_id
+  security_group_ids = [module.vpc_endpoints_security_group.security_group_id]
 
   endpoints = {
 
