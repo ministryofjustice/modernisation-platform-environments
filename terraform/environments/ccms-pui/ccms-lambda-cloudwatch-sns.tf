2391b7d173dcd6b7596d0a17942d1df8f3c8cbd4
“Sanjay <sanjay.baranwal@justice.gov.uk>
2025-12-15 15:58:07 +0000

    fix: athena region deprecation and statement fixed

--- terraform/environments/ccms-pui/ccms-lambda-cloudwatch-sns.tf
+++ terraform/environments/ccms-pui/ccms-lambda-cloudwatch-sns.tf
@@ -113,7 +113,7 @@ resource "aws_lambda_function" "cloudwatch_sns" {
 }
 
 resource "aws_lambda_permission" "allow_sns_invoke_guardduty" {
-  statement_id  = "AllowExecutionFromSNS"
+  statement_id  = "AllowExecutionFromGuardDutySNS"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.cloudwatch_sns.function_name
   principal     = "sns.amazonaws.com"
@@ -121,7 +121,7 @@ resource "aws_lambda_permission" "allow_sns_invoke_guardduty" {
 }
 
 resource "aws_lambda_permission" "allow_sns_invoke_cloudwatch" {
-  statement_id  = "AllowExecutionFromSNS"
+  statement_id  = "AllowExecutionFromCloudWatchSNS"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.cloudwatch_sns.function_name
   principal     = "sns.amazonaws.com"
--- terraform/environments/laa-ccms-soa/athena-query-execution.tf
+++ terraform/environments/laa-ccms-soa/athena-query-execution.tf
@@ -15,22 +15,22 @@ aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.main_table_admin.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
   --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
-  --region ${data.aws_region.current.name}
+  --region ${data.aws_region.current.region}
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.main_table_managed.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
   --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
-  --region ${data.aws_region.current.name}
+  --region ${data.aws_region.current.region}
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.tls_requests_admin.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
   --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
-  --region ${data.aws_region.current.name}
+  --region ${data.aws_region.current.region}
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.tls_requests_managed.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
   --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
-  --region ${data.aws_region.current.name}
+  --region ${data.aws_region.current.region}
 EOF
   }
 
