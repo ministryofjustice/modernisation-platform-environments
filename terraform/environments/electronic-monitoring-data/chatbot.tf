d7d5d81614acc11a9b24deaa0b282c781b60ab81
Khristiania <khristiania.raihan@justice.gov.uk>
2025-12-15 15:25:40 +0000

    justice digital workspace

--- terraform/environments/electronic-monitoring-data/chatbot.tf
+++ terraform/environments/electronic-monitoring-data/chatbot.tf
@@ -3,7 +3,7 @@ module "chatbot_alerts" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0
 
   slack_channel_id = "C09C3P43UNP"
-  slack_team_id    = "T01067J1X9Q" #This is wrong and needs to be Justice Digital
+  slack_team_id    = "T02DYEB3A"
   sns_topic_arns   = [aws_sns_topic.emds_alerts.arn]
   tags             = local.tags
   application_name = local.application_name
