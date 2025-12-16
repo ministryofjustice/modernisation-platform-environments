59d5cd32b6daee2789bc9ce65f44aa639647aff1
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-12-16 15:00:06 +0000

    Back to dashboard service 0.2.16

--- terraform/environments/analytical-platform-compute/dashboard-service/helm-charts-dashboard-service.tf
+++ terraform/environments/analytical-platform-compute/dashboard-service/helm-charts-dashboard-service.tf
@@ -4,7 +4,7 @@ resource "helm_release" "dashboard_service" {
   /* https://github.com/ministryofjustice/analytical-platform-dashboard-service */
   name       = "dashboard-service"
   repository = "oci://ghcr.io/ministryofjustice/analytical-platform-charts"
-  version    = "0.2.15"
+  version    = "0.2.16"
   chart      = "dashboard-service"
   namespace  = kubernetes_namespace.dashboard_service[0].metadata[0].name
   values = [
