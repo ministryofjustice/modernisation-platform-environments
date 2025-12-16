a8bdd2f762f82d7638a424efd6b98287694544bb
Dave Kent <dave.kent@justice.gov.uk>
2025-12-16 11:00:09 +0000

    tm-1791-decomm-azure-dns-zones-hmpps-domain-services-dev

--- terraform/environments/hmpps-domain-services/locals_development.tf
+++ terraform/environments/hmpps-domain-services/locals_development.tf
@@ -14,20 +14,30 @@ locals {
   baseline_development = {
 
     acm_certificates = {
+      # remote_desktop_wildcard_cert = {
+      #   cloudwatch_metric_alarms            = module.baseline_presets.cloudwatch_metric_alarms.acm
+      #   domain_name                         = "modernisation-platform.service.justice.gov.uk"
+      #   external_validation_records_created = true
+      #   subject_alternate_names = [
+      #     "*.hmpps-domain-services.hmpps-development.modernisation-platform.service.justice.gov.uk",
+      #     "*.development.hmpps-domain.service.justice.gov.uk",
+      #     "hmppgw2.justice.gov.uk",
+      #     "*.hmppgw2.justice.gov.uk",
+      #   ]
+      #   tags = {
+      #     description = "wildcard cert for hmpps domain load balancer"
+      #   }
+      # }
       remote_desktop_wildcard_cert = {
         cloudwatch_metric_alarms            = module.baseline_presets.cloudwatch_metric_alarms.acm
-        domain_name                         = "modernisation-platform.service.justice.gov.uk"
-        external_validation_records_created = true
+        domain_name                         = "*.development.hmpps-domain.service.justice.gov.uk"
         subject_alternate_names = [
           "*.hmpps-domain-services.hmpps-development.modernisation-platform.service.justice.gov.uk",
-          "*.development.hmpps-domain.service.justice.gov.uk",
-          "hmppgw2.justice.gov.uk",
-          "*.hmppgw2.justice.gov.uk",
         ]
         tags = {
           description = "wildcard cert for hmpps domain load balancer"
         }
-      }
+      }      
     }
 
     ec2_autoscaling_groups = {
@@ -125,7 +135,7 @@ locals {
       maintenance_window_cutoff   = 1 # 2 for prod
       patch_classifications = {
         # REDHAT_ENTERPRISE_LINUX = ["Security", "Bugfix"] # Linux Options=(Security,Bugfix,Enhancement,Recommended,Newpackage)
-        WINDOWS = ["SecurityUpdates", "CriticalUpdates"]
+        WINDOWS = ["SecurityUpdates", "CriticalUpdates", "UpdateRollups"] # Windows Options=CriticalUpdates,SecurityUpdates,DefinitionUpdates,Drivers,FeaturePacks,ServicePacks,Tools,UpdateRollups,Updates,Upgrades
       }
     }
 
