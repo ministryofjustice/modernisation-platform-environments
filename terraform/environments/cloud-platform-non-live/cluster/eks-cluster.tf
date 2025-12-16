fb9e65c1714de5bdc6b7b6bb6f6a22e83325be96
Ky <ky.truong@digital.justice.gov.uk>
2025-12-16 14:27:22 +0000

    csi-ebs addon

--- terraform/environments/cloud-platform-non-live/cluster/eks-cluster.tf
+++ terraform/environments/cloud-platform-non-live/cluster/eks-cluster.tf
@@ -62,6 +62,9 @@ module "eks" {
     }
     aws-guardduty-agent = {
     }
+
+    aws-ebs-csi-driver = {  
+    }
     
   }
 
--- terraform/environments/cloud-platform-non-live/cluster/eks-pod-identities.tf
+++ terraform/environments/cloud-platform-non-live/cluster/eks-pod-identities.tf
@@ -18,3 +18,22 @@ module "aws_vpc_cni_pod_identity" {
 
   tags = local.tags
 }
+
+module "aws_ebs_csi_pod_identity" {
+  source = "terraform-aws-modules/eks-pod-identity/aws"
+
+  name = "aws-ebs-csi"
+
+  attach_aws_ebs_csi_policy = true
+  # aws_ebs_csi_kms_arns      = ["arn:aws:kms:*:*:key/1234abcd-12ab-34cd-56ef-1234567890ab"]
+
+  associations = {
+    this = {
+      cluster_name    = module.eks[0].cluster_name
+      namespace       = "kube-system"
+      service_account = "ebs-csi-controller-sa"
+    }
+  }
+
+  tags = local.tags
+}
