7f292a7d1f9266730d6965b5f32bb08651360c63
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2025-12-17 10:22:04 +0000

    creds

--- terraform/environments/laa-ccms-soa/soa_database.tf
+++ terraform/environments/laa-ccms-soa/soa_database.tf
@@ -22,38 +22,46 @@ resource "aws_db_option_group" "soa_oracle_19" {
     version     = "1.0"
   }
 
-option {
-  option_name = "OEM_AGENT"
+  # -----------------------------
+  # OEM Agent Option (Updated)
+  # -----------------------------
+  option {
+    option_name = "OEM_AGENT"
 
-  vpc_security_group_memberships = [
-    "${aws_security_group.soa_db.id}"
-  ]
+    vpc_security_group_memberships = [
+      "${aws_security_group.soa_db.id}"
+    ]
 
-  option_settings {
-    name  = "OMS_HOST"
-    value = "laa-oem-app.laa-development.modernisation-platform.service.justice.gov.uk"
+    option_settings {
+      name  = "OMS_HOST"
+      value = "laa-oem-app.laa-development.modernisation-platform.service.justice.gov.uk"
+    }
+
+    option_settings {
+      name  = "AGENT_PORT"
+      value = "3872"
+    }
+
+    option_settings {
+      name  = "EM_UPLOAD_PORT"
+      value = "4903"
+    }
+
+    option_settings {
+      name  = "AGENT_REGISTRATION_USERNAME"
+      value = jsondecode(data.aws_secretsmanager_secret_version.oem_agent_credentials.secret_string).username
+    }
+
+    option_settings {
+      name  = "AGENT_REGISTRATION_PASSWORD"
+      value = jsondecode(data.aws_secretsmanager_secret_version.oem_agent_credentials.secret_string).password
+    }
+
+    option_settings {
+      name  = "AGENT_PASSWORD"
+      value = jsondecode(data.aws_secretsmanager_secret_version.oem_agent_credentials.secret_string).password
+    }
   }
-
-  option_settings {
-    name  = "AGENT_PORT"
-    value = "3872"
-  }
-
-  option_settings {
-    name  = "EM_UPLOAD_PORT"
-    value = "4903"
-  }
-
-  option_settings {
-    name  = "AGENT_REGISTRATION_USERNAME"
-    value = jsondecode(data.aws_secretsmanager_secret_version.oem_agent_credentials.secret_string).username
-  }
-
-  option_settings {
-    name  = "AGENT_REGISTRATION_PASSWORD"
-    value = jsondecode(data.aws_secretsmanager_secret_version.oem_agent_credentials.secret_string).password
-  }
-}
 
   lifecycle {
     create_before_destroy = true
@@ -63,7 +71,7 @@ option {
 resource "aws_db_instance" "soa_db" {
   identifier                          = "soa-db"
   allocated_storage                   = local.application_data.accounts[local.environment].soa_db_storage_gb
-  auto_minor_version_upgrade          = local.application_data.accounts[local.environment].soa_db_minor_version_upgrade_allowed #--This needs to be set to true if using a JVM in the above option group
+  auto_minor_version_upgrade          = local.application_data.accounts[local.environment].soa_db_minor_version_upgrade_allowed
   storage_type                        = "gp2"
   engine                              = "oracle-ee"
   engine_version                      = local.application_data.accounts[local.environment].soa_db_version
