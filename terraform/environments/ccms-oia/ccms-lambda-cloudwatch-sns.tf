3942da3899ef34a1409a6167160aa0df8e02e224
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2025-12-17 14:05:10 +0000

    chore: guardduty webhook added

--- terraform/environments/ccms-oia/ccms-lambda-cloudwatch-sns.tf
+++ terraform/environments/ccms-oia/ccms-lambda-cloudwatch-sns.tf
@@ -126,5 +126,5 @@ resource "aws_lambda_permission" "allow_sns_invoke_guardduty" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.cloudwatch_sns.function_name
   principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.cloudwatch_alerts.arn
+  source_arn    = aws_sns_topic.guardduty_alerts.arn
 }
