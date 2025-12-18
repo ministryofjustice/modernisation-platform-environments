d50918182037c2ef584e5a75816538dd682c47a6
Ramya Manuneethi <ramya.manuneethi@opencastsoftware.com>
2025-12-18 14:39:04 +0000

    added archive

--- terraform/environments/ccms-oia/versions.tf
+++ terraform/environments/ccms-oia/versions.tf
@@ -12,6 +12,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.7.2"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.7.1"
+    }
   }
   required_version = "~> 1.0"
 }
