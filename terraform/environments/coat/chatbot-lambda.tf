5d26dfcba3929013c7aec623b60ec8596d012a73
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2025-12-19 12:33:45 +0000

    add null resource to build lambda

--- terraform/environments/coat/chatbot-lambda.tf
+++ terraform/environments/coat/chatbot-lambda.tf
@@ -27,6 +27,21 @@ resource "aws_lambda_function" "rag_lambda" {
   tags = local.tags
 }
 
+resource "null_resource" "build_lambda_zip" {
+
+  triggers = {
+    script_hash = filesha256("${path.module}/lambdas/rag-lambda/rag-lambda.py")
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      cd ${path.module}/lambdas/rag-lambda
+
+      zip -r rag-lambda.zip .
+    EOT
+  }
+}
+
 data "archive_file" "rag_lambda" {
   type        = "zip"
   source_dir  = "${path.module}/lambdas/rag-lambda/"
