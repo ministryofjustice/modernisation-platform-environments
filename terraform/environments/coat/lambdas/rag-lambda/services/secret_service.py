89efb473b3f88137f7c42cb89cf174df853140b7
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2025-12-19 18:03:33 +0000

    newline

--- terraform/environments/coat/lambdas/rag-lambda/services/secret_service.py
+++ terraform/environments/coat/lambdas/rag-lambda/services/secret_service.py
@@ -5,10 +5,11 @@ class SecretService:
     def __init__(self) -> None:
         self.client = boto3.client("secretsmanager")
 
+
     def get_secret(self, secret_name):
 
         response = self.client.get_secret_value(SecretId=secret_name)
 
         secret = response.get("SecretString", "")
-        
+
         return secret
