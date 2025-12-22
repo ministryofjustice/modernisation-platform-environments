223cbd96ca52abcd8082be39a729205108d15fd6
mikereiddigital <mike.reid@justice.gov.uk>
2025-12-22 09:12:55 +0000

    Adds missing time provider to sprinkler to fix a conflict caused by prior use of the env where that provider was added via a non-main branch but never cleared up after.

--- terraform/environments/sprinkler/versions.tf
+++ terraform/environments/sprinkler/versions.tf
@@ -20,6 +20,10 @@ terraform {
       version = "~> 3.2"
       source  = "hashicorp/null"
     }
+    time = {
+      version = "~> 0.9"
+      source  = "hashicorp/time"
+    }
   }
   required_version = "~> 1.10"
 }
