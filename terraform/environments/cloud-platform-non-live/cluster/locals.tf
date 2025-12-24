a8b3dd2a53eaf0562ed21198147785a51527be31
jackstockley89 <jstock8904@gmail.com>
2025-12-24 15:09:52 +0000

    feat(TF): add route53 and cluster node security groups

--- terraform/environments/cloud-platform-non-live/cluster/locals.tf
+++ terraform/environments/cloud-platform-non-live/cluster/locals.tf
@@ -2,4 +2,5 @@ locals {
   /* worksapce specific configurations */
   enabled_workspaces        = []
   environment_configuration = local.environment_configurations[local.environment]
+  is_prod_cluster           = local.environment == "production"
 }
