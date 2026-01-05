9d8919591ebc6aeb9d3d4304fe2b9589fb54e0a2
matt-heery <116661071+matt-heery@users.noreply.github.com>
2026-01-05 11:32:10 +0000

    Apply suggestions from code review

--- terraform/environments/electronic-monitoring-data/share_analytical_platform_compute.tf
+++ terraform/environments/electronic-monitoring-data/share_analytical_platform_compute.tf
@@ -48,7 +48,7 @@ locals {
   prod_dbs_to_grant = local.is-production ? [
     "am_stg",
     "buddi_stg",
-    "buddi_buddi_historic_dev",
+    "buddi_buddi,
     "buddi_buddi_prod",
     "cap_dw_stg",
     "curated_emsys_mvp",
