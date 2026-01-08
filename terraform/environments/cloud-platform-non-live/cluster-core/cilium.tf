4a7bdb64ed663961c595d596252b0c64270870e2
jackstockley89 <jstock8904@gmail.com>
2026-01-08 15:34:01 +0000

    feat(TF): add count for core components for control over what gets applied to which cluster

--- terraform/environments/cloud-platform-non-live/cluster-core/cilium.tf
+++ terraform/environments/cloud-platform-non-live/cluster-core/cilium.tf
@@ -1,3 +1,4 @@
 module "cilium" {
+  count = local.environment_configuration.enable_culium ? 1 : 0
   source = "github.com/ministryofjustice/cloud-platform-terraform-cilium?ref=1.1.0" # use the latest release
 }
--- terraform/environments/cloud-platform-non-live/cluster-core/locals.tf
+++ terraform/environments/cloud-platform-non-live/cluster-core/locals.tf
@@ -1,7 +1,7 @@
 locals {
   /* worksapce specific configurations */
   enabled_workspaces        = ["development_cluster"]
-  # environment_configuration = local.environment_configurations[local.cluster_environment]
+  environment_configuration = local.environment_configurations[local.cluster_environment]
   # below are replaced for dev clusters
   cp_vpc_name         = "${local.application_name}-${local.environment}" # replaced with "cp-date-time" or custom name
   cluster_name        = local.environment                                # replaced with "cp-date-time" or custom name
