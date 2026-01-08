3d88ffe3f5babdee7adc28a7303db379b4864607
David Elliott <david.elliott@digital.justice.gov.uk>
2026-01-08 16:47:02 +0000

    Amend to use workspaces for dev clusters
    Use terraform.workspace to determine things like cluster and environment instead of replacing with the script.
    
    Amend the script to use workspace_key_prefix which will use the workspace name for the folder instead of key.
    Delete unneeded replacements.

--- terraform/environments/cloud-platform-non-live/scripts/dev-cluster-files.sh
+++ terraform/environments/cloud-platform-non-live/scripts/dev-cluster-files.sh
@@ -27,38 +27,11 @@ for folder in "${FOLDERS[@]}"; do
         echo "Copying ${folder} to ${TMP_DIR}/"
         cp -r "${BASE_DIR}/${folder}" "$TMP_DIR/"
         
-        # Modify network/locals.tf if this is the network folder
-        if [ "$folder" = "network" ] && [ -f "${TMP_DIR}/network/locals.tf" ]; then
-            echo "Updating network/locals.tf with cluster-specific values..."
-            sed -i.bak "s/cluster_environment = .*/cluster_environment = \"development_cluster\"/" "${TMP_DIR}/network/locals.tf"
-            sed -i.bak "s|cp_vpc_name.*= .*|cp_vpc_name         = \"${CLUSTER_NAME}\"|" "${TMP_DIR}/network/locals.tf"
-            rm -f "${TMP_DIR}/network/locals.tf.bak"
-        fi
-        
-        # Modify cluster/locals.tf if this is the cluster folder
-        if [ "$folder" = "cluster" ] && [ -f "${TMP_DIR}/cluster/locals.tf" ]; then
-            echo "Updating cluster/locals.tf with cluster-specific values..."
-            sed -i.bak "s/cluster_environment = .*/cluster_environment = \"development_cluster\"/" "${TMP_DIR}/cluster/locals.tf"
-            sed -i.bak "s|cp_vpc_name.*= .*|cp_vpc_name         = \"${CLUSTER_NAME}\"|" "${TMP_DIR}/cluster/locals.tf"
-            sed -i.bak "s|cluster_name.*= .*|cluster_name         = \"${CLUSTER_NAME}\"|" "${TMP_DIR}/cluster/locals.tf"
-            rm -f "${TMP_DIR}/cluster/locals.tf.bak"
-        fi
-
-        # Modify cluster/locals.tf if this is the cluster folder
-        if [ "$folder" = "cluster-core" ] && [ -f "${TMP_DIR}/cluster-core/locals.tf" ]; then
-            echo "Updating cluster-core/locals.tf with cluster-specific values..."
-            sed -i.bak "s/cluster_environment = .*/cluster_environment = \"development_cluster\"/" "${TMP_DIR}/cluster-core/locals.tf"
-            sed -i.bak "s|cp_vpc_name.*= .*|cp_vpc_name         = \"${CLUSTER_NAME}\"|" "${TMP_DIR}/cluster-core/locals.tf"
-            sed -i.bak "s|cluster_name.*= .*|cluster_name         = \"${CLUSTER_NAME}\"|" "${TMP_DIR}/cluster-core/locals.tf"
-            rm -f "${TMP_DIR}/cluster-core/locals.tf.bak"
-        fi
-        
         # Modify platform_backend.tf if it exists in the folder
         if [ -f "${TMP_DIR}/${folder}/platform_backend.tf" ]; then
             echo "Updating ${folder}/platform_backend.tf with cluster-specific values..."
             sed -i.bak "s/bucket *= *\".*\"/bucket               = \"development-clusters-terraform-state20251223114024319400000001\"/" "${TMP_DIR}/${folder}/platform_backend.tf"
-            sed -i.bak "s|key *= *\".*\"|key                  = \"${CLUSTER_NAME}/${folder}/terraform.tfstate\"|" "${TMP_DIR}/${folder}/platform_backend.tf"
-            sed -i.bak "/workspace_key_prefix/d" "${TMP_DIR}/${folder}/platform_backend.tf"
+            sed -i.bak "s|workspace_key_prefix *= *\".*\"|workspace_key_prefix = \"${folder}\"|" "${TMP_DIR}/${folder}/platform_backend.tf"
             rm -f "${TMP_DIR}/${folder}/platform_backend.tf.bak"
         fi
     else
