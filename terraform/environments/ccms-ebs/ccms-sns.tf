a8d1f563525dae19ea69ffc98f8bcb252d8344f0
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2026-01-08 12:58:04 +0000

    fix

--- terraform/environments/ccms-ebs/ccms-sns.tf
+++ terraform/environments/ccms-ebs/ccms-sns.tf
@@ -1,18 +1,18 @@
-#### Secret for support email address ###
-resource "aws_secretsmanager_secret" "support_email_account" {
-  name                    = "support_email_account"
-  description             = "email address of the support account for cw alerts"
-  recovery_window_in_days = local.is-production ? 30 : 0
-}
+# #### Secret for support email address ###
+# resource "aws_secretsmanager_secret" "support_email_account" {
+#   name                    = "support_email_account"
+#   description             = "email address of the support account for cw alerts"
+#   recovery_window_in_days = local.is-production ? 30 : 0
+# }
 
-# Use a default dummy address just for creation. Will require to be populated manually.
-resource "aws_secretsmanager_secret_version" "support_email_account" {
-  secret_id     = aws_secretsmanager_secret.support_email_account.id
-  secret_string = "default@email.com"
-  lifecycle {
-    ignore_changes = [secret_string]
-  }
-}
+# # Use a default dummy address just for creation. Will require to be populated manually.
+# resource "aws_secretsmanager_secret_version" "support_email_account" {
+#   secret_id     = aws_secretsmanager_secret.support_email_account.id
+#   secret_string = "default@email.com"
+#   lifecycle {
+#     ignore_changes = [secret_string]
+#   }
+# }
 
 resource "aws_secretsmanager_secret" "alerts_subscription_email" {
   name                    = "alerts_subscription_email"
@@ -116,10 +116,19 @@ resource "aws_sns_topic" "s3_topic" {
   )
 }
 
+# S3 SNS -> Lambda (Slack) instead of email
 resource "aws_sns_topic_subscription" "s3_subscription" {
   topic_arn = aws_sns_topic.s3_topic.arn
-  protocol  = "email"
-  endpoint  = aws_secretsmanager_secret_version.alerts_subscription_email.secret_string
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.cloudwatch_sns.arn
+}
+
+resource "aws_lambda_permission" "allow_s3_sns_invoke" {
+  statement_id  = "AllowExecutionFromS3SNSTopic"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cloudwatch_sns.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.s3_topic.arn
 }
 
 resource "aws_sns_topic" "ddos_alarm" {
@@ -130,10 +139,19 @@ resource "aws_sns_topic" "ddos_alarm" {
   )
 }
 
+# DDoS SNS -> Lambda (Slack) instead of email
 resource "aws_sns_topic_subscription" "ddos_subscription" {
   topic_arn = aws_sns_topic.ddos_alarm.arn
-  protocol  = "email"
-  endpoint  = aws_secretsmanager_secret_version.alerts_subscription_email.secret_string
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.cloudwatch_sns.arn
+}
+
+resource "aws_lambda_permission" "allow_ddos_sns_invoke" {
+  statement_id  = "AllowExecutionFromDDoSSNSTopic"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cloudwatch_sns.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.ddos_alarm.arn
 }
 
 resource "aws_sns_topic" "guardduty_alerts" {
