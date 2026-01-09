ed9862cf80aa30e36aba4c3285d92f34b9a667db
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-01-09 11:55:04 +0000

    refactor(network): simplify environment configurations by removing unnecessary line breaks
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/network/environment-configuration.tf
+++ terraform/environments/data-platform/network/environment-configuration.tf
@@ -1,14 +1,10 @@
 locals {
   environment_configuration = local.environment_configurations[local.environment]
   environment_configurations = {
-    development = {
-    }
-    test = {
-    }
-    preproduction = {
-    }
-    production = {
-    }
+    development   = {}
+    test          = {}
+    preproduction = {}
+    production    = {}
   }
 
   network_configuration = yamldecode(file("${path.module}/configuration/network.yml"))["environment"][local.environment]
