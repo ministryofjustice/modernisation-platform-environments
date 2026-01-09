be9dd053effb0f260c5cb694a1ce441ae29a9496
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2026-01-09 09:32:13 +0000

    fix

--- terraform/environments/ccms-ebs/alerting.tf
+++ terraform/environments/ccms-ebs/alerting.tf
@@ -1,32 +1,17 @@
-#--Alerting Chatbot
-module "guardduty_chatbot" {
-  source           = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=73280f80ce8a4557cec3a76ee56eb913452ca9aa" # v2.0.0"
-  slack_channel_id = data.aws_secretsmanager_secret_version.guardduty_slack_channel_id.secret_string
-  sns_topic_arns   = [aws_sns_topic.guardduty_alerts.arn]
-  tags             = local.tags #--This doesn't seem to pass to anything in the module but is a mandatory var. Consider submitting a PR to the module. AW
-  application_name = local.application_name
-}
-
-resource "aws_sns_topic_policy" "guarduty_default" {
-  arn    = aws_sns_topic.guardduty_alerts.arn
-  policy = data.aws_iam_policy_document.guardduty_alerting_sns.json
-}
-
-resource "aws_sns_topic_subscription" "guardduty_alerts" {
-  topic_arn = aws_sns_topic.guardduty_alerts.arn
-  protocol  = "https"
-  endpoint  = "https://global.sns-api.chatbot.amazonaws.com"
-}
+#### GuardDuty alerting – send all findings to the cw_alerts SNS topic
+#### (Lambda subscribed to cw_alerts will forward to Slack)
 
 resource "aws_cloudwatch_event_rule" "guardduty" {
-  name = "${local.application_name}-guardduty-findings"
+  name = "${local.application_name}-${local.environment}-guardduty-findings"
+
   event_pattern = jsonencode({
-    "source" : ["aws.guardduty"],
+    "source"      : ["aws.guardduty"],
     "detail-type" : ["GuardDuty Finding"]
   })
 }
 
-resource "aws_cloudwatch_event_target" "guardduty_to_sns" {
-  rule = aws_cloudwatch_event_rule.guardduty.name
-  arn  = aws_sns_topic.guardduty_alerts.arn
+resource "aws_cloudwatch_event_target" "guardduty_to_cw_alerts" {
+  rule      = aws_cloudwatch_event_rule.guardduty.name
+  target_id = "send-guardduty-findings-to-cw-alerts"
+  arn       = aws_sns_topic.cw_alerts.arn
 }
