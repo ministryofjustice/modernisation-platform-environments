4cab2cff59ccd1a67f0f6b0c303f0ab228a2f70d
Tom Ogle <tom.ogle@justice.gov.uk>
2026-01-13 16:52:23 +0000

    DHS-556: Add Spark log filter metrics for errors, warn, exceptions.

--- terraform/environments/digital-prison-reporting/modules/glue_job/variables.tf
+++ terraform/environments/digital-prison-reporting/modules/glue_job/variables.tf
@@ -266,6 +266,10 @@ variable "log_group_retention_in_days" {
   description = "(Optional) The default number of days log events retained in the glue job log group."
 }
 
+variable "custom_metric_namespace" {
+  type        = string
+  description = "Namespace for custom metrics related to Glue, e.g. log filter metrics that parse the logs for errors"
+}
 
 variable "job_language" {
   type        = string
