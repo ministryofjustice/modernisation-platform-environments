edb47482492101ba4ff2a7889dc92bd49746d4f7
Vladimirs Kovalovs <vladimirs.kovalovs1@justice.gov.uk>
2026-01-14 15:38:15 +0000

    [STB-3305-v6] small change

--- terraform/environments/oas/new-acm.tf
+++ terraform/environments/oas/new-acm.tf
@@ -23,11 +23,11 @@ data "aws_route53_zone" "modernisation_platform" {
 ### ACM CERTIFICATE FOR LOAD BALANCER ###
 ##############################################
 # ACM Public Certificate for test and preproduction environments
-# Using wildcard on parent domain (48 chars) to cover laa-preproduction subdomain
+# Test uses zone-specific wildcard (57 chars) - preproduction uses parent wildcard (48 chars)
 resource "aws_acm_certificate" "external" {
   count = contains(["test", "preproduction"], local.environment) ? 1 : 0
 
-  domain_name       = "*.modernisation-platform.service.justice.gov.uk"
+  domain_name       = local.environment == "test" ? "*.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk" : "*.modernisation-platform.service.justice.gov.uk"
   validation_method = "DNS"
 
   tags = merge(
@@ -41,16 +41,17 @@ resource "aws_acm_certificate" "external" {
 }
 
 # Route53 DNS records for certificate validation
+# Test validates in laa-test zone, preproduction validates in parent zone
 resource "aws_route53_record" "external_validation" {
   for_each = local.domain_types
-  provider = aws.core-network-services
+  provider = local.environment == "test" ? aws.core-vpc : aws.core-network-services
 
   allow_overwrite = true
   name            = each.value.name
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.modernisation_platform.zone_id
+  zone_id         = local.environment == "test" ? data.aws_route53_zone.external.zone_id : data.aws_route53_zone.modernisation_platform.zone_id
 }
 
 # ACM Certificate Validation
