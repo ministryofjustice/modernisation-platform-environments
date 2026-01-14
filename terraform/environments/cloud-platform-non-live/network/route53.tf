c1315ce6f3670b857e08b9a522df58c711724d43
jackstockley89 <jstock8904@gmail.com>
2026-01-14 17:02:59 +0000

    feat(TF): remove RAM

--- terraform/environments/cloud-platform-non-live/network/route53.tf
+++ terraform/environments/cloud-platform-non-live/network/route53.tf
@@ -2,3 +2,7 @@ resource "aws_route53_zone" "temp_cloud_platform_justice_gov_uk" {
   count = terraform.workspace == "cloud-platform-non-live-production" ? 1 : 0
   name = "temp.cloud-platform.service.justice.gov.uk."
 }
+
+resource "aws_route53_zone" "account_zone" {
+  name = trimprefix(terraform.workspace, "cloud-platform-") + ".temp.cloud-platform.service.justice.gov.uk"
+}
