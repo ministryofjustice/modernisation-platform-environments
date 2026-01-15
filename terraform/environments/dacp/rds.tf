af45f080986bbcbac2563519e90f041386adda61
Timi Olubusi <timiolubusi@icloud.com>
2026-01-15 15:30:28 +0000

    Removing index thats been removed

--- terraform/environments/dacp/rds.tf
+++ terraform/environments/dacp/rds.tf
@@ -17,7 +17,7 @@ resource "aws_db_instance" "dacp_db" {
   password                        = random_password.password.result
   skip_final_snapshot             = true
   publicly_accessible             = local.is-development ? true : false
-  vpc_security_group_ids          = [aws_security_group.postgresql_db_sc[0].id]
+  vpc_security_group_ids          = [aws_security_group.postgresql_db_sc.id]
   db_subnet_group_name            = aws_db_subnet_group.dbsubnetgroup.name
   allow_major_version_upgrade     = false
   auto_minor_version_upgrade      = true
