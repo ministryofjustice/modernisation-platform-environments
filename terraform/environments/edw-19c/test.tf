0ff1d5c622ae8066a5912ab2dd66013685f9d090
Muhammad-MOJ <MuhammadImran.Khan@justice.gov.uk>
2026-01-16 12:11:40 +0000

    test

---
+++ terraform/environments/edw-19c/test.tf
@@ -0,0 +1,9 @@
+
+resource "aws_instance" "london_ec2" {
+  ami           = "ami-0d37e07bd4ff37148" # Amazon Linux 2 (eu-west-2)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "london-ec2"
+  }
+}
