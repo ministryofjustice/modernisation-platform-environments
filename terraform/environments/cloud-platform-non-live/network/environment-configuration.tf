c994255fb948785046f38538b43a25336a76e2e5
jackstockley89 <jstock8904@gmail.com>
2026-01-19 13:46:26 +0000

    feat(TF): ignore development_cluster

--- terraform/environments/cloud-platform-non-live/network/environment-configuration.tf
+++ terraform/environments/cloud-platform-non-live/network/environment-configuration.tf
@@ -28,13 +28,13 @@ locals {
       ]
     }
     production = {
-      account_subdomain_name = aws_route53_zone.account_zone[0].name
-      ns_records = [
+      account_subdomain_name = length(aws_route53_zone.account_zone) > 0 ? aws_route53_zone.account_zone[0].name : "production.temp.cloud-platform.service.justice.gov.uk"
+      ns_records = length(aws_route53_zone.account_zone) > 0 ? [
         aws_route53_zone.account_zone[0].name_servers[0],
         aws_route53_zone.account_zone[0].name_servers[1],
         aws_route53_zone.account_zone[0].name_servers[2],
         aws_route53_zone.account_zone[0].name_servers[3],
-      ]
+      ] : []
     }
   }
 }
