723ace87f91baaab2b44770868e31cf6165c53c6
Ky <ky.truong@digital.justice.gov.uk>
2026-01-19 16:13:28 +0000

    adding zone for devlopement test clusters

--- terraform/environments/cloud-platform-non-live/cluster/route53.tf
+++ terraform/environments/cloud-platform-non-live/cluster/route53.tf
@@ -1,23 +1,29 @@
-# resource "aws_route53_zone" "cluster_zone" {
-#   name = "cluster" + trimprefix(terraform.workspace, "cloud-platform-") + ".temp.cloud-platform.service.justice.gov.uk"
-#   force_destroy = true
+## Only create Route53 zone in development clusters
 
-#   tags = {
-#     Environment = terraform.workspace
-#     ManagedBy   = "Terraform"
-#   }
-# }
+resource "aws_route53_zone" "development_cluster_zone" {
+  count = contains(locals.mp_environments, terraform.workspace) ? 0 : 1
+  
+  name = terraform.workspace + ".temp.cloud-platform.service.justice.gov.uk"
+  force_destroy = true
 
-# resource "aws_route53_record" "parent_zone_cluster_ns" {
-#   zone_id = data.aws_route53_zone.shared_parent_zone.zone_id
-#   name    = aws_route53_zone.cluster_zone.name
-#   type    = "NS"
-#   ttl     = "30"
+  tags = {
+    Environment = terraform.workspace
+    ManagedBy   = "Terraform"
+  }
+}
 
-#   records = [
-#     aws_route53_zone.cluster_zone.name_servers[0],
-#     aws_route53_zone.cluster_zone.name_servers[1],
-#     aws_route53_zone.cluster_zone.name_servers[2],
-#     aws_route53_zone.cluster_zone.name_servers[3],
-#   ]
-# }
+resource "aws_route53_record" "development_hosted_zone_ns" {
+  count = contains(locals.mp_environments, terraform.workspace) ? 0 : 1
+  
+  zone_id = data.aws_route53_zone.development_hosted_zone.zone_id
+  name    = aws_route53_zone.development_cluster_zone[0].name
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    aws_route53_zone.development_cluster_zone[0].name_servers[0],
+    aws_route53_zone.development_cluster_zone[0].name_servers[1],
+    aws_route53_zone.development_cluster_zone[0].name_servers[2],
+    aws_route53_zone.development_cluster_zone[0].name_servers[3],
+  ]
+}
