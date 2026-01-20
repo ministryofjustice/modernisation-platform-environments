7841cedb2ecb5c53108c40fdd403585bc8a891f2
jackstockley89 <jstock8904@gmail.com>
2026-01-20 12:28:49 +0000

    feat(TF): delegate temp.cloud-platform... to the other accounts from production

--- terraform/environments/cloud-platform-non-live/environment-configuration.tf
+++ terraform/environments/cloud-platform-non-live/environment-configuration.tf
@@ -1,40 +1,40 @@
-# locals {
-#   environment_configurations = {
-#     development = {
-#       account_subdomain_name = "non-live-development.temp.cloud-platform.service.justice.gov.uk"
-#       ns_records = [
-#         "ns-1509.awsdns-60.org.",
-#         "ns-205.awsdns-25.com.",
-#         "ns-1550.awsdns-01.co.uk.",
-#         "ns-899.awsdns-48.net."
-#       ]
-#     }
-#     test = {
-#       account_subdomain_name = "non-live-test.temp.cloud-platform.service.justice.gov.uk"
-#       ns_records = [
-#         "ns-792.awsdns-35.net.",
-#         "ns-110.awsdns-13.com.",
-#         "ns-1592.awsdns-07.co.uk.",
-#         "ns-1245.awsdns-27.org."
-#       ]
-#     }
-#     preproduction = {
-#       account_subdomain_name = "non-live-preproduction.temp.cloud-platform.service.justice.gov.uk"
-#       ns_records = [
-#         "ns-1801.awsdns-33.co.uk.",
-#         "ns-728.awsdns-27.net.",
-#         "ns-1172.awsdns-18.org.",
-#         "ns-454.awsdns-56.com."
-#       ]
-#     }
-#     production = {
-#       account_subdomain_name = length(aws_route53_zone.account_zone) > 0 ? aws_route53_zone.account_zone[0].name : "production.temp.cloud-platform.service.justice.gov.uk"
-#       ns_records = length(aws_route53_zone.account_zone) > 0 ? [
-#         aws_route53_zone.account_zone[0].name_servers[0],
-#         aws_route53_zone.account_zone[0].name_servers[1],
-#         aws_route53_zone.account_zone[0].name_servers[2],
-#         aws_route53_zone.account_zone[0].name_servers[3],
-#       ] : []
-#     }
-#   }
-# }
+locals {
+  environment_configurations = {
+    development = {
+      account_subdomain_name = "non-live-development.temp.cloud-platform.service.justice.gov.uk"
+      ns_records = [
+        "ns-1750.awsdns-26.co.uk.",
+        "ns-329.awsdns-41.com.",
+        "ns-1065.awsdns-05.org.",
+        "ns-911.awsdns-49.net."
+      ]
+    }
+    test = {
+      account_subdomain_name = "non-live-test.temp.cloud-platform.service.justice.gov.uk"
+      ns_records = [
+        "ns-81.awsdns-10.com.",
+        "ns-1469.awsdns-55.org.",
+        "ns-1962.awsdns-53.co.uk.",
+        "ns-965.awsdns-56.net."
+      ]
+    }
+    preproduction = {
+      account_subdomain_name = "non-live-preproduction.temp.cloud-platform.service.justice.gov.uk"
+      ns_records = [
+        "ns-2030.awsdns-61.co.uk.",
+        "ns-771.awsdns-32.net.",
+        "ns-355.awsdns-44.com.",
+        "ns-1457.awsdns-54.org."
+      ]
+    }
+    production = {
+      account_subdomain_name = length(aws_route53_zone.account_zone) > 0 ? aws_route53_zone.account_zone[0].name : "production.temp.cloud-platform.service.justice.gov.uk"
+      ns_records = length(aws_route53_zone.account_zone) > 0 ? [
+        aws_route53_zone.account_zone[0].name_servers[0],
+        aws_route53_zone.account_zone[0].name_servers[1],
+        aws_route53_zone.account_zone[0].name_servers[2],
+        aws_route53_zone.account_zone[0].name_servers[3],
+      ] : []
+    }
+  }
+}
