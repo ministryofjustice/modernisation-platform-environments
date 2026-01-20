59e5bbfce78352f8523bd730163b433a337bdca0
jackstockley89 <jstock8904@gmail.com>
2026-01-20 17:47:36 +0000

    feat(TF): readonly for production to other accounts for the ns_record lookup when building out the hosted zones

--- terraform/environments/cloud-platform-non-live/data.tf
+++ terraform/environments/cloud-platform-non-live/data.tf
@@ -1,2 +1,23 @@
 #### This file can be used to store data specific to the member account ####
 data "aws_availability_zones" "available" {}
+
+# Data sources to fetch hosted zone information from each environment account
+# These fetch the NS records dynamically from the account zones
+
+data "aws_route53_zone" "development_account_zone" {
+  count    = terraform.workspace == "cloud-platform-non-live-production" ? 1 : 0
+  provider = aws.cloud-platform-non-live-development
+  name     = "non-live-development.temp.cloud-platform.service.justice.gov.uk"
+}
+
+data "aws_route53_zone" "test_account_zone" {
+  count    = terraform.workspace == "cloud-platform-non-live-production" ? 1 : 0
+  provider = aws.cloud-platform-non-live-test
+  name     = "non-live-test.temp.cloud-platform.service.justice.gov.uk"
+}
+
+data "aws_route53_zone" "preproduction_account_zone" {
+  count    = terraform.workspace == "cloud-platform-non-live-production" ? 1 : 0
+  provider = aws.cloud-platform-non-live-preproduction
+  name     = "non-live-preproduction.temp.cloud-platform.service.justice.gov.uk"
+}
---
+++ terraform/environments/cloud-platform-non-live/hosted_zones_read_role.tf
@@ -0,0 +1,37 @@
+# IAM role to allow production account to read hosted zones for account delegation
+data "aws_iam_policy_document" "cross_account_hosted_zones_assume_role" {
+  statement {
+    sid    = "AllowProductionAccountAssume"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.environment_management.account_ids["cloud-platform-live-production"]}:root"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "cross_account_hosted_zones_policy" {
+  statement {
+    sid    = "ReadRoute53HostedZones"
+    effect = "Allow"
+    actions = [
+      "route53:GetHostedZone",
+      "route53:ListHostedZones",
+      "route53:ListResourceRecordSets"
+    ]
+    resources = ["${local.environment}.${local.base_domain}."]
+  }
+}
+
+resource "aws_iam_role" "cross_account_hosted_zones_read" {
+  name               = "cross-account-hosted-zones-read"
+  description        = "Allows production account to read hosted zones for account delegation NS records"
+  assume_role_policy = data.aws_iam_policy_document.cross_account_hosted_zones_assume_role.json
+}
+
+resource "aws_iam_role_policy" "cross_account_hosted_zones_read" {
+  name   = "hosted-zones-read-policy"
+  role   = aws_iam_role.cross_account_hosted_zones_read.id
+  policy = data.aws_iam_policy_document.cross_account_hosted_zones_policy.json
+}
