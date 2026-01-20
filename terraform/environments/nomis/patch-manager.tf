77bc3aef210830a62e823fce1fc424651866bacc
Dave Kent <dave.kent@justice.gov.uk>
2026-01-20 14:39:11 +0000

    tm-1833-nomis-patch-manager-deployment-2

--- terraform/environments/nomis/patch-manager.tf
+++ terraform/environments/nomis/patch-manager.tf
@@ -4,7 +4,7 @@ module "patch_manager" {
   providers = {
     aws.bucket-replication = aws
   }
-  daily_definition_update = true
+  daily_definition_update = false
   account_number          = local.environment_management.account_ids[terraform.workspace] # Required, Account number of current environment, (string)
   application_name        = local.application_name                                        # Required, Name of application, (string)
   environment             = local.environment
@@ -14,10 +14,11 @@ module "patch_manager" {
     preproduction = 10
     production    = 14
   }
-  patch_schedules             = local.baseline_environment_specific.patch_manager.patch_schedules
-  maintenance_window_cutoff   = local.baseline_environment_specific.patch_manager.maintenance_window_cutoff
-  maintenance_window_duration = local.baseline_environment_specific.patch_manager.maintenance_window_duration
-  patch_classifications       = local.baseline_environment_specific.patch_manager.patch_classifications
+  patch_schedules             = local.baseline_all_environments.patch_manager.patch_schedules
+  patch_tag_key               = local.baseline_all_environments.patch_manager.patch_tag_key
+  maintenance_window_cutoff   = local.baseline_all_environments.patch_manager.maintenance_window_cutoff
+  maintenance_window_duration = local.baseline_all_environments.patch_manager.maintenance_window_duration
+  patch_classifications       = local.baseline_all_environments.patch_manager.patch_classifications
   simple_patching             = true # Optional, Use AWS-RunPatchBaseline directly, instead of AWS-PatchInstanceWithRollback which is a wrapper that adds orchestration and prolific lambda logs.
   tags                        = merge(local.tags, { name = "ssm-patching-module" }, )
 }
