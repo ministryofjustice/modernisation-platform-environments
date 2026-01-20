a4d8ffa7e81ff68f4290a225d1653c35071cf026
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-01-20 21:07:25 +0000

    chore: add PagerDuty API key secret module and update IAM role references
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/iac/secrets.tf
+++ terraform/environments/data-platform/iac/secrets.tf
@@ -42,12 +42,12 @@ module "github_app_secret" {
   )
 }
 
-module "slack_token_secret" {
+module "pagerduty_api_key_secret" {
   count = terraform.workspace == "data-platform-development" ? 1 : 0
 
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=82029345dea22bc49989a6f46c5d8d8e555b84c9" # v2.0.1
 
-  name        = "pagerduty/token"
+  name = "pagerduty/api-key"
 
   secret_string         = "CHANGEME"
   ignore_secret_changes = true
