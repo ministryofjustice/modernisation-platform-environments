dac3763984288d0ac7f262cb1529006c6982dc82
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2026-01-20 15:00:22 +0000

    remove chatbot

--- terraform/environments/ccms-edrms/chatbot.tf
+++ terraform/environments/ccms-edrms/chatbot.tf
@@ -1,13 +1,13 @@
 # Chatbot Configuration for CCMS EDRMS Cloudwatch Alerts
 
 
-module "template" {
+# module "template" {
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0
+#   source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=0ec33c7bfde5649af3c23d0834ea85c849edf3ac" # v3.0.0
 
-  slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
-  sns_topic_arns   = [aws_sns_topic.cloudwatch_slack.arn]
-  tags             = local.tags
-  application_name = local.application_name
+#   slack_channel_id = data.aws_secretsmanager_secret_version.slack_channel_id.secret_string
+#   sns_topic_arns   = [aws_sns_topic.cloudwatch_slack.arn]
+#   tags             = local.tags
+#   application_name = local.application_name
 
-}
+# }
