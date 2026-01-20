75f3baf2e76771da8c17156e0a1cf5a1e7fdea0c
matt heery <matt.heery@justice.gov.uk>
2026-01-20 13:03:46 +0000

    fix: add string for all

--- terraform/environments/electronic-monitoring-data/modules/landing_bucket/variables.tf
+++ terraform/environments/electronic-monitoring-data/modules/landing_bucket/variables.tf
@@ -81,8 +81,19 @@ variable "cross_account_id" {
 variable "replication_details" {
   description = "kms key and name of bucket to replicate data to"
   type        = object({
-    bucket  = string
-    kms_key = string
+    fms_general_bucket = string
+    fms_general_kms_id = string
+    fms_ho_bucket = string
+    fms_ho_kms_id = string
+    fms_specials_bucket = string
+    fms_specials_kms_id = string
+    mdss_general_bucket = string
+    mdss_general_kms_id = string
+    mdss_specials_bucket = string
+    mdss_specials_kms_id = string
+    mdss_ho_bucket = string
+    mdss_ho_kms_id = string
+    account_id = string
   })
   default     = null
   nullable    = true
