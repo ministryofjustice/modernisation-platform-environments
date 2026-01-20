b10f5b3ac066b3301f8c5f8fd811d91fb3194ec2
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-01-20 10:29:53 +0000

    removed other engineers rebase changes

--- terraform/environments/laa-ccms-soa/secrets.tf
+++ terraform/environments/laa-ccms-soa/secrets.tf
@@ -87,16 +87,3 @@ resource "aws_secretsmanager_secret_version" "ccms_soa_quiesced_secrets_version"
   }
 }
 
-##########################################################
-# OEM Agent Credentials Secret (added)
-##########################################################
-
-data "aws_secretsmanager_secret" "oem_agent_credentials" {
-  name = "ccms/soa/oem_agent_credentials"
-}
-
-data "aws_secretsmanager_secret_version" "oem_agent_credentials" {
-  secret_id = data.aws_secretsmanager_secret.oem_agent_credentials.id
-}
-
-
