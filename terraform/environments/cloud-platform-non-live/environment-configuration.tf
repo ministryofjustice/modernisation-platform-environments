90558bca9f28915c0489eea7057e2a8f9588b1fb
jackstockley89 <jstock8904@gmail.com>
2026-01-21 17:39:53 +0000

    feat(TF): update policy hosted zone resource

--- terraform/environments/cloud-platform-non-live/environment-configuration.tf
+++ terraform/environments/cloud-platform-non-live/environment-configuration.tf
@@ -1,34 +1,31 @@
 locals {
   environment_configurations = {
     development = {
-      account_subdomain_name = "non-live-development.${local.base_domain}"
-      # Dynamically fetch NS records from development account when running in production workspace
-      ns_records = terraform.workspace == "cloud-platform-non-live-production" && length(data.aws_route53_zone.development_account_zone) > 0 ? [
-        data.aws_route53_zone.development_account_zone[0].name_servers[0],
-        data.aws_route53_zone.development_account_zone[0].name_servers[1],
-        data.aws_route53_zone.development_account_zone[0].name_servers[2],
-        data.aws_route53_zone.development_account_zone[0].name_servers[3],
-      ] : []
+      account_subdomain_name = "non-live-development.temp.cloud-platform.service.justice.gov.uk"
+      ns_records = [
+        "ns-1750.awsdns-26.co.uk.",
+        "ns-329.awsdns-41.com.",
+        "ns-1065.awsdns-05.org.",
+        "ns-911.awsdns-49.net."
+      ]
     }
     test = {
-      account_subdomain_name = "non-live-test.${local.base_domain}"
-      # Dynamically fetch NS records from test account when running in production workspace
-      ns_records = terraform.workspace == "cloud-platform-non-live-production" && length(data.aws_route53_zone.test_account_zone) > 0 ? [
-        data.aws_route53_zone.test_account_zone[0].name_servers[0],
-        data.aws_route53_zone.test_account_zone[0].name_servers[1],
-        data.aws_route53_zone.test_account_zone[0].name_servers[2],
-        data.aws_route53_zone.test_account_zone[0].name_servers[3],
-      ] : []
+      account_subdomain_name = "non-live-test.temp.cloud-platform.service.justice.gov.uk"
+      ns_records = [
+        "ns-81.awsdns-10.com.",
+        "ns-1469.awsdns-55.org.",
+        "ns-1962.awsdns-53.co.uk.",
+        "ns-965.awsdns-56.net."
+      ]
     }
     preproduction = {
-      account_subdomain_name = "non-live-preproduction.${local.base_domain}"
-      # Dynamically fetch NS records from preproduction account when running in production workspace
-      ns_records = terraform.workspace == "cloud-platform-non-live-production" && length(data.aws_route53_zone.preproduction_account_zone) > 0 ? [
-        data.aws_route53_zone.preproduction_account_zone[0].name_servers[0],
-        data.aws_route53_zone.preproduction_account_zone[0].name_servers[1],
-        data.aws_route53_zone.preproduction_account_zone[0].name_servers[2],
-        data.aws_route53_zone.preproduction_account_zone[0].name_servers[3],
-      ] : []
+      account_subdomain_name = "non-live-preproduction.temp.cloud-platform.service.justice.gov.uk"
+      ns_records = [
+        "ns-2030.awsdns-61.co.uk.",
+        "ns-771.awsdns-32.net.",
+        "ns-355.awsdns-44.com.",
+        "ns-1457.awsdns-54.org."
+      ]
     }
     production = {
       account_subdomain_name = aws_route53_zone.account_zone.name
@@ -41,3 +38,48 @@ locals {
     }
   }
 }
+
+
+# locals {
+#   environment_configurations = {
+#     development = {
+#       account_subdomain_name = "non-live-development.${local.base_domain}"
+#       # Dynamically fetch NS records from development account when running in production workspace
+#       ns_records = terraform.workspace == "cloud-platform-non-live-production" && length(data.aws_route53_zone.development_account_zone) > 0 ? [
+#         data.aws_route53_zone.development_account_zone[0].name_servers[0],
+#         data.aws_route53_zone.development_account_zone[0].name_servers[1],
+#         data.aws_route53_zone.development_account_zone[0].name_servers[2],
+#         data.aws_route53_zone.development_account_zone[0].name_servers[3],
+#       ] : []
+#     }
+#     test = {
+#       account_subdomain_name = "non-live-test.${local.base_domain}"
+#       # Dynamically fetch NS records from test account when running in production workspace
+#       ns_records = terraform.workspace == "cloud-platform-non-live-production" && length(data.aws_route53_zone.test_account_zone) > 0 ? [
+#         data.aws_route53_zone.test_account_zone[0].name_servers[0],
+#         data.aws_route53_zone.test_account_zone[0].name_servers[1],
+#         data.aws_route53_zone.test_account_zone[0].name_servers[2],
+#         data.aws_route53_zone.test_account_zone[0].name_servers[3],
+#       ] : []
+#     }
+#     preproduction = {
+#       account_subdomain_name = "non-live-preproduction.${local.base_domain}"
+#       # Dynamically fetch NS records from preproduction account when running in production workspace
+#       ns_records = terraform.workspace == "cloud-platform-non-live-production" && length(data.aws_route53_zone.preproduction_account_zone) > 0 ? [
+#         data.aws_route53_zone.preproduction_account_zone[0].name_servers[0],
+#         data.aws_route53_zone.preproduction_account_zone[0].name_servers[1],
+#         data.aws_route53_zone.preproduction_account_zone[0].name_servers[2],
+#         data.aws_route53_zone.preproduction_account_zone[0].name_servers[3],
+#       ] : []
+#     }
+#     production = {
+#       account_subdomain_name = aws_route53_zone.account_zone.name
+#       ns_records = length(aws_route53_zone.account_zone) > 0 ? [
+#         aws_route53_zone.account_zone.name_servers[0],
+#         aws_route53_zone.account_zone.name_servers[1],
+#         aws_route53_zone.account_zone.name_servers[2],
+#         aws_route53_zone.account_zone.name_servers[3],
+#       ] : []
+#     }
+#   }
+# }
--- terraform/environments/cloud-platform-non-live/hosted_zones_read_role.tf
+++ terraform/environments/cloud-platform-non-live/hosted_zones_read_role.tf
@@ -5,7 +5,7 @@ data "aws_iam_policy_document" "cross_account_hosted_zones_assume_role" {
     effect = "Allow"
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${local.environment_management.account_ids["cloud-platform-live-production"]}:root"]
+      identifiers = ["arn:aws:iam::${local.environment_management.account_ids["cloud-platform-non-live-production"]}:role/github-actions"]
     }
     actions = ["sts:AssumeRole"]
   }
@@ -13,14 +13,20 @@ data "aws_iam_policy_document" "cross_account_hosted_zones_assume_role" {
 
 data "aws_iam_policy_document" "cross_account_hosted_zones_policy" {
   statement {
-    sid    = "ReadRoute53HostedZones"
+    sid       = "ListAllHostedZones"
+    effect    = "Allow"
+    actions   = ["route53:ListHostedZones"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ReadSpecificHostedZone"
     effect = "Allow"
     actions = [
       "route53:GetHostedZone",
-      "route53:ListHostedZones",
       "route53:ListResourceRecordSets"
     ]
-    resources = ["non-live-${local.environment}.${local.base_domain}/*"]
+    resources = ["arn:aws:route53:::hostedzone/${aws_route53_zone.account_zone.zone_id}"]
   }
 }
 
@@ -36,4 +42,32 @@ resource "aws_iam_role_policy" "cross_account_hosted_zones_read" {
   name   = "hosted-zones-read-policy"
   role   = aws_iam_role.cross_account_hosted_zones_read[0].id
   policy = data.aws_iam_policy_document.cross_account_hosted_zones_policy.json
+}
+
+
+# Policy to allow github-actions role in production to assume cross-account hosted zones read roles
+data "aws_iam_policy_document" "github_actions_assume_hosted_zones_role" {
+  count = terraform.workspace == "cloud-platform-non-live-production" ? 1 : 0
+  statement {
+    sid     = "AssumeHostedZonesReadRoles"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    resources = [
+      "arn:aws:iam::${local.environment_management.account_ids["cloud-platform-non-live-development"]}:role/cross-account-hosted-zones-read",
+      "arn:aws:iam::${local.environment_management.account_ids["cloud-platform-non-live-test"]}:role/cross-account-hosted-zones-read",
+      "arn:aws:iam::${local.environment_management.account_ids["cloud-platform-non-live-preproduction"]}:role/cross-account-hosted-zones-read"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "github_actions_assume_hosted_zones_role" {
+  count  = terraform.workspace == "cloud-platform-non-live-production" ? 1 : 0
+  name   = "github-actions-assume-hosted-zones-read"
+  policy = data.aws_iam_policy_document.github_actions_assume_hosted_zones_role[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_assume_hosted_zones_role" {
+  count      = terraform.workspace == "cloud-platform-non-live-production" ? 1 : 0
+  role       = "github-actions"
+  policy_arn = aws_iam_policy.github_actions_assume_hosted_zones_role[0].arn
 }
