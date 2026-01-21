28ba6c901f4c51bb4262aa5db73130b5814b90c5
matt heery <matt.heery@justice.gov.uk>
2026-01-21 11:03:35 +0000

    feat: remove standard storage class, just copy original bucket

--- terraform/environments/electronic-monitoring-data/modules/landing_bucket/cross_account_copy.tf
+++ terraform/environments/electronic-monitoring-data/modules/landing_bucket/cross_account_copy.tf
@@ -99,7 +99,6 @@ resource "aws_s3_bucket_replication_configuration" "default" {
     destination {
       account       = var.replication_details["account_id"]
       bucket        = local.replication_enabled ? "arn:aws:s3:::${var.replication_details["${var.data_feed}_${var.order_type}_bucket"]}" : ""
-      storage_class = "STANDARD"
       encryption_configuration {
         replica_kms_key_id = local.replication_enabled != "" ? "arn:aws:kms:eu-west-2:${var.replication_details["account_id"]}:key/${var.replication_details["${var.data_feed}_${var.order_type}_kms_id"]}" : ""
       }
