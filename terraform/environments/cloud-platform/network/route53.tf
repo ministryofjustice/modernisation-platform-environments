a1f0c37025afa66df5df887d618d125c36b91642
David Elliott <david.elliott@digital.justice.gov.uk>
2026-01-22 09:33:37 +0000

    Add new route 53 code

--- terraform/environments/cloud-platform/network/route53.tf
+++ terraform/environments/cloud-platform/network/route53.tf
@@ -1,17 +1,29 @@
-resource "aws_route53_zone" "temp_cloud_platform_justice_gov_uk" {
-  count = terraform.workspace == "cloud-platform-production" ? 1 : 0
-  name  = "temp.cloud-platform.service.justice.gov.uk."
+## Only create Route53 zone in development clusters
+
+resource "aws_route53_zone" "development_cluster_zone" {
+  count = contains(local.mp_environments, terraform.workspace) ? 0 : 1
+
+  name          = "${terraform.workspace}.development.temp.cloud-platform.service.justice.gov.uk"
+  force_destroy = true
+
+  tags = {
+    Environment = terraform.workspace
+    ManagedBy   = "Terraform"
+  }
 }
 
-resource "aws_route53_zone" "account_zone" {
-  name = "${trimprefix(terraform.workspace, "cloud-platform-")}.temp.cloud-platform.service.justice.gov.uk"
-}
+resource "aws_route53_record" "development_cluster_hosted_zone_ns" {
+  count = contains(local.mp_environments, terraform.workspace) ? 0 : 1
 
-resource "aws_route53_record" "account_delegation" {
-  for_each = terraform.workspace == "cloud-platform-production" ? local.environment_configurations : {}
-  zone_id  = aws_route53_zone.temp_cloud_platform_justice_gov_uk[0].zone_id
-  name     = each.value.account_subdomain_name
-  type     = "NS"
-  ttl      = 300
-  records  = each.value.ns_records
+  zone_id = data.aws_route53_zone.account_hosted_zone.zone_id
+  name    = aws_route53_zone.development_cluster_zone[0].name
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    aws_route53_zone.development_cluster_zone[0].name_servers[0],
+    aws_route53_zone.development_cluster_zone[0].name_servers[1],
+    aws_route53_zone.development_cluster_zone[0].name_servers[2],
+    aws_route53_zone.development_cluster_zone[0].name_servers[3],
+  ]
 }
