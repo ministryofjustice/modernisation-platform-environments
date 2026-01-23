b5ec8c3e28a3adbfe86a6e9a737b3eb45c56ebaf
Jacob Woffenden <jacob@woffenden.io>
2026-01-23 17:39:29 +0000

    chore: update permissions

--- terraform/environments/data-platform/iac/iam-roles.tf
+++ terraform/environments/data-platform/iac/iam-roles.tf
@@ -40,14 +40,27 @@ module "data_platform_access_iam_role" {
       ]
       resources = ["${module.s3_bucket[0].s3_bucket_arn}/data-platform-access/*"]
     }
-    SecretsManagerAccess = {
+    SecretsManagerReadAccess = {
       effect  = "Allow"
       actions = ["secretsmanager:GetSecretValue"]
       resources = [
         module.entra_secret[0].secret_arn,
         module.github_app_secret[0].secret_arn,
         module.pagerduty_api_key_secret[0].secret_arn,
-        module.slack_token_secret[0].secret_arn,
+        module.slack_token_secret[0].secret_arn
+      ]
+    }
+    SecretsManagerWriteAccess = {
+      effect  = "Allow"
+      actions = [
+        "secretsmanager:CreateSecret",
+        "secretsmanager:DeleteSecret,
+        "secretsmanager:DescribeSecret",
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:PutSecretValue",
+        "secretsmanager:UpdateSecret"
+      ]
+      resources = [
         /* Secrets Managed by Data Platform Access */
         "arn:aws:secretsmanager:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:secret:pagerduty/*" # PagerDuty
       ]
