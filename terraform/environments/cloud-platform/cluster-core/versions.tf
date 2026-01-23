53f8c72e7d2f2fcfdc95504eba9aa8acda12197c
David Elliott <david.elliott@digital.justice.gov.uk>
2026-01-23 16:12:13 +0000

    Add existing changes to new cp app (#15133)
    * Add existing changes to new cp app
    
    * Change to new envs
    
    * Add new workflows
    
    * Remove unneeded vpc and add cluster components to workflow
    
    * Fix needing to use update kubeconfig command
    
    The new provider configuration and kubectl provider means we no longer need a separate authentication step in the pipeline.
    
    * Remove unneeded local
    
    * Fix environments in configuration
    
    * Add new route 53 code
    
    * Comment out until confirm dev cluster working
    
    * Add permission to required role

---
+++ terraform/environments/cloud-platform/cluster-core/versions.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 6.0"
+      source  = "hashicorp/aws"
+    }
+    dns = {
+      version = "~> 3.0"
+      source  = "hashicorp/dns"
+    }
+    external = {
+      version = "~> 2.0"
+      source  = "hashicorp/external"
+    }
+    http = {
+      version = "~> 3.0"
+      source  = "hashicorp/http"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "> 2.0"
+    }
+  }
+  required_version = "~> 1.0"
+}
---
+++ terraform/environments/cloud-platform/cluster/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  mp_environments = [
+    "cloud-platform-development",
+    "cloud-platform-preproduction",
+    "cloud-platform-nonlive",
+    "cloud-platform-live",
+  ]
+  environment_configuration = local.environment_configurations[local.cluster_environment]
+  enabled_workspaces        = ["development_cluster", "development"]
+  cp_vpc_name               = terraform.workspace
+  cluster_name              = contains(local.mp_environments, terraform.workspace) ? local.environment : terraform.workspace
+  cluster_environment       = contains(local.mp_environments, terraform.workspace) ? local.environment : "development_cluster"
+}
