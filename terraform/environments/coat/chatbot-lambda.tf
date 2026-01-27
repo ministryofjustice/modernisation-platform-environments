57a192b2556fc9b2f197e2b8760a0aff1ab88111
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-01-27 13:56:01 +0000

    comment out lambda build

--- terraform/environments/coat/chatbot-lambda.tf
+++ terraform/environments/coat/chatbot-lambda.tf
@@ -1,31 +1,31 @@
-# # Lambda
+# Lambda
 
-# resource "aws_lambda_function" "rag_lambda" {
-#   #checkov:skip=CKV_AWS_173:No sensitive information stored in Lambda environment variables
-#   #checkov:skip=CKV_AWS_117:This Lambda doesn't need VPC
-#   #checkov:skip=CKV_AWS_116:Queue it self has DLQ so Lambda fail should redrive to DLQ
-#   #checkov:skip=CKV_AWS_272:Doesn't need code signing
+resource "aws_lambda_function" "rag_lambda" {
+  #checkov:skip=CKV_AWS_173:No sensitive information stored in Lambda environment variables
+  #checkov:skip=CKV_AWS_117:This Lambda doesn't need VPC
+  #checkov:skip=CKV_AWS_116:Queue it self has DLQ so Lambda fail should redrive to DLQ
+  #checkov:skip=CKV_AWS_272:Doesn't need code signing
 
-#   function_name = "RAGLambdaFunction"
-#   description   = "Recieve NL request from user, use Bedrock to create SQL from NL, and use query to extract data from Athena"
+  function_name = "RAGLambdaFunction"
+  description   = "Recieve NL request from user, use Bedrock to create SQL from NL, and use query to extract data from Athena"
 
-#   role    = aws_iam_role.rag_lambda_role.arn
-#   runtime = "python3.12"
-#   timeout = 30
+  role    = aws_iam_role.rag_lambda_role.arn
+  runtime = "python3.12"
+  timeout = 30
 
-#   handler          = "rag-lambda.lambda_handler"
-#   package_type     = "Zip"
-#   filename         = "${path.module}/lambdas/rag-lambda/rag-lambda.zip"
-#   source_code_hash = data.archive_file.rag_lambda.output_base64sha256
+  handler          = "rag-lambda.lambda_handler"
+  package_type     = "Zip"
+  filename         = "${path.module}/lambdas/rag-lambda/rag-lambda.zip"
+  source_code_hash = data.archive_file.rag_lambda.output_base64sha256
 
-#   reserved_concurrent_executions = 10
+  reserved_concurrent_executions = 10
 
-#   tracing_config {
-#     mode = "PassThrough"
-#   }
+  tracing_config {
+    mode = "PassThrough"
+  }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 # resource "null_resource" "build_lambda_zip" {
 
@@ -48,112 +48,112 @@
 #   output_path = "${path.module}/lambdas/rag-lambda.zip"
 # }
 
-# # Logs
+# Logs
 
-# resource "aws_cloudwatch_log_group" "rag_lambda_log_group" {
-#   name              = "/aws/lambda/${aws_lambda_function.rag_lambda.function_name}"
-#   retention_in_days = 120
-# }
+resource "aws_cloudwatch_log_group" "rag_lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.rag_lambda.function_name}"
+  retention_in_days = 120
+}
 
-# # IAM role
+# IAM role
 
-# resource "aws_iam_role" "rag_lambda_role" {
-#   name        = "RAGLambdaFunctionRole"
-#   description = "RAG Lambda Function IAM Role"
+resource "aws_iam_role" "rag_lambda_role" {
+  name        = "RAGLambdaFunctionRole"
+  description = "RAG Lambda Function IAM Role"
 
-#   assume_role_policy = data.aws_iam_policy_document.rag_lambda_function_assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.rag_lambda_function_assume_role.json
 
-#   inline_policy {
-#     name   = "rag_lambda_policy"
-#     policy = data.aws_iam_policy_document.rag_lambda_function_role.json
-#   }
+  inline_policy {
+    name   = "rag_lambda_policy"
+    policy = data.aws_iam_policy_document.rag_lambda_function_role.json
+  }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# data "aws_iam_policy_document" "rag_lambda_function_assume_role" {
-#   statement {
-#     effect  = "Allow"
-#     actions = ["sts:AssumeRole"]
+data "aws_iam_policy_document" "rag_lambda_function_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["lambda.amazonaws.com"]
-#     }
-#   }
-# }
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
 
-# data "aws_iam_policy_document" "rag_lambda_function_role" {
+data "aws_iam_policy_document" "rag_lambda_function_role" {
 
-#   statement {
-#     sid    = "AllowToWriteCloudWatchLog"
-#     effect = "Allow"
+  statement {
+    sid    = "AllowToWriteCloudWatchLog"
+    effect = "Allow"
 
-#     actions = [
-#       "logs:CreateLogStream",
-#       "logs:PutLogEvents"
-#     ]
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
 
-#     resources = ["arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/RAGLambdaFunction:*"]
-#   }
+    resources = ["arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/RAGLambdaFunction:*"]
+  }
 
-#   statement {
-#     sid    = "AllowS3Access"
-#     effect = "Allow"
+  statement {
+    sid    = "AllowS3Access"
+    effect = "Allow"
 
-#     actions = [
-#       "s3:GetObject",
-#       "s3:PutObject",
-#       "s3:ListBucket"
-#     ]
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket"
+    ]
 
-#     resources = [
-#       "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly/",
-#       "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly/*",
-#     ]
-#   }
+    resources = [
+      "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly/",
+      "arn:aws:s3:::coat-${local.environment}-cur-v2-hourly/*",
+    ]
+  }
 
-#   statement {
-#     sid    = "AllowAthenaQueries"
-#     effect = "Allow"
+  statement {
+    sid    = "AllowAthenaQueries"
+    effect = "Allow"
 
-#     actions = [
-#       "athena:StartQueryExecution",
-#       "athena:GetQueryExecution",
-#       "athena:GetQueryResults",
-#       "athena:StopQueryExecution"
-#     ]
+    actions = [
+      "athena:StartQueryExecution",
+      "athena:GetQueryExecution",
+      "athena:GetQueryResults",
+      "athena:StopQueryExecution"
+    ]
 
-#     resources = ["*"]
-#   }
+    resources = ["*"]
+  }
 
-#   statement {
-#     sid    = "AllowGlueCatalogRead"
-#     effect = "Allow"
+  statement {
+    sid    = "AllowGlueCatalogRead"
+    effect = "Allow"
 
-#     actions = [
-#       "glue:GetDatabase",
-#       "glue:GetDatabases",
-#       "glue:GetTable",
-#       "glue:GetTables",
-#       "glue:GetPartition",
-#       "glue:GetPartitions"
-#     ]
+    actions = [
+      "glue:GetDatabase",
+      "glue:GetDatabases",
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetPartition",
+      "glue:GetPartitions"
+    ]
 
-#     resources = ["*"]
-#   }
+    resources = ["*"]
+  }
 
-#   statement {
-#     sid    = "AllowInvokeBedrockModels"
-#     effect = "Allow"
+  statement {
+    sid    = "AllowInvokeBedrockModels"
+    effect = "Allow"
 
-#     actions = [
-#       "bedrock:InvokeModel",
-#       "bedrock:InvokeModelWithResponseStream"
-#     ]
+    actions = [
+      "bedrock:InvokeModel",
+      "bedrock:InvokeModelWithResponseStream"
+    ]
 
-#     resources = [
-#       "arn:aws:bedrock:*::foundation-model/*"
-#     ]
-#   }
-# }
+    resources = [
+      "arn:aws:bedrock:*::foundation-model/*"
+    ]
+  }
+}
