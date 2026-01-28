a6d739fb6b6c2a7c6bd3453df27a87be7bced6c0
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-01-28 11:22:19 +0000

    fixing terraform index errors

--- terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-ccms-lambda-cloudwatch-sns.tf
+++ terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-ccms-lambda-cloudwatch-sns.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_cloudwatch_sns_role" {
-  name = "${local.application_name}-${local.environment}-${local.component_name}-lambda_cloudwatch_sns_role"
+  name = "${local.component_name}-lambda_cloudwatch_sns_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
