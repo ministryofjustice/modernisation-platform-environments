8b5eb6e2256903a49772b50a09610ec5205402dc
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2026-01-28 11:11:34 +0000

    remove artefacts s3 bucket

--- terraform/environments/ccms-ebs/member-locals.tf
+++ terraform/environments/ccms-ebs/member-locals.tf
@@ -1,6 +1,6 @@
 #### This file can be used to store locals specific to the member account ####
 locals {
-  artefact_bucket_name           = "${local.application_name}-${local.environment}-artefacts"
+  # artefact_bucket_name           = "${local.application_name}-${local.environment}-artefacts"
   logging_bucket_name            = "${local.application_name}-${local.environment}-logging"
   rsync_bucket_name              = "${local.application_name}-${local.environment}-dbbackup"
   lb_log_prefix_ebsapp           = "ebsapps-lb"
