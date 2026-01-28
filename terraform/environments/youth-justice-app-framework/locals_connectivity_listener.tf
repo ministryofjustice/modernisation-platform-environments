c71375c40ace589524ea6062e0aded0bc7f298d7
ttipler <thomas.tipler@necsws.com>
2026-01-28 10:26:22 +0000

    fix

--- terraform/environments/youth-justice-app-framework/locals_connectivity_listener.tf
+++ terraform/environments/youth-justice-app-framework/locals_connectivity_listener.tf
@@ -1,20 +1,11 @@
+# connectivity_listeners.tf
 locals {
-  connectivity_tg_arn = lookup(
-    var.existing_target_groups,
-    "connectivity-target-group-1",
-    null
-  )
-
   connectivity_listeners = {
     connectivity_listener = {
       port                                 = 8080
       protocol                             = "HTTP"
       routing_http_response_server_enabled = true
-
-      # Default forward uses the existing target group ARN
-      forward = {
-        target_group_arn = local.connectivity_tg_arn
-      }
+      # remove default forward
 
       rules = {
         connectivity = {
@@ -22,33 +13,23 @@ locals {
           actions = [
             {
               type             = "forward"
-              target_group_arn = local.connectivity_tg_arn
+              target_group_key = "connectivity-target-group-1"
             }
           ]
           conditions = [
-            {
-              path_pattern = {
-                values = ["/api/v1/connectivity*", "/cjse/*"]
-              }
-            }
+            { path_pattern = { values = ["/api/v1/connectivity*", "/cjse/*"] } }
           ]
         },
-
         connectivity_health = {
-          priority = 23
+          priority = 2
           actions = [
             {
               type             = "forward"
-              target_group_arn = local.connectivity_tg_arn
+              target_group_key = "connectivity-target-group-1"
             }
           ]
           conditions = [
-            {
-              http_header = {
-                http_header_name = "service-health"
-                values           = ["connectivity"]
-              }
-            }
+            { http_header = { http_header_name = "service-health", values = ["connectivity"] } }
           ]
         }
       }
