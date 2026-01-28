46f5c52708dd1d1541e25a40cc47e707bb01263d
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-01-28 15:47:20 +0000

    remove providers

--- terraform/environments/coat/versions.tf
+++ terraform/environments/coat/versions.tf
@@ -8,14 +8,6 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.0"
-    }
   }
   required_version = "~> 1.0"
 }
