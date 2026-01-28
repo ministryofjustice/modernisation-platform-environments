7f5cc1904ffb5829f1565faa32154d7c2807ccad
Srikanth Thummala <srikanth.thummala@justice.gov.uk>
2026-01-28 11:11:34 +0000

    remove artefacts s3 bucket

--- terraform/environments/ccms-ebs/ccms-s3.tf
+++ terraform/environments/ccms-ebs/ccms-s3.tf
@@ -1,101 +1,106 @@
-# S3 Bucket - Artefacts
-module "s3-bucket" { #tfsec:ignore:aws-s3-enable-versioning
-  # v8.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/52a40b0dd18aaef0d7c5565d93cc8997aad79636
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=52a40b0dd18aaef0d7c5565d93cc8997aad79636"
+# ---------------------------------------------
+# S3 Bucket - Artefacts (DISABLED)
+# ---------------------------------------------
 
-  bucket_name = local.artefact_bucket_name
-  #  bucket_prefix      = "s3-bucket-example"
-  versioning_enabled = true
-  bucket_policy      = [data.aws_iam_policy_document.artefacts_s3_policy.json]
+# module "s3-bucket" { #tfsec:ignore:aws-s3-enable-versioning
+#   # v8.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/52a40b0dd18aaef0d7c5565d93cc8997aad79636
+#   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=52a40b0dd18aaef0d7c5565d93cc8997aad79636"
+#
+#   bucket_name = local.artefact_bucket_name
+#   #  bucket_prefix      = "s3-bucket-example"
+#   versioning_enabled = true
+#   bucket_policy      = [data.aws_iam_policy_document.artefacts_s3_policy.json]
+#
+#   log_bucket = local.logging_bucket_name
+#   log_prefix = "s3access/${local.artefact_bucket_name}"
+#
+#   # Refer to the below section "Replication" before enabling replication
+#   replication_enabled = false
+#   # Below three variables and providers configuration are only relevant if 'replication_enabled' is set to true
+#   replication_region = "eu-west-2"
+#   # replication_role_arn                     = module.s3-bucket-replication-role.role.arn
+#   providers = {
+#     # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+#     # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+#     # Platform team to add a new provider for the additional Region.
+#     aws.bucket-replication = aws
+#   }
+#
+#   lifecycle_rule = [
+#     {
+#       id      = "main"
+#       enabled = "Enabled"
+#       prefix  = ""
+#
+#       tags = {
+#         rule      = "log"
+#         autoclean = "true"
+#       }
+#
+#       transition = [
+#         {
+#           days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_standard
+#           storage_class = "STANDARD_IA"
+#         }, {
+#           days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_glacier
+#           storage_class = "GLACIER"
+#         }
+#       ]
+#
+#       expiration = {
+#         days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_current
+#       }
+#
+#       noncurrent_version_transition = [
+#         {
+#           days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_standard
+#           storage_class = "STANDARD_IA"
+#         }, {
+#           days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_glacier
+#           storage_class = "GLACIER"
+#         }
+#       ]
+#
+#       noncurrent_version_expiration = {
+#         days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_noncurrent
+#       }
+#
+#       abort_incomplete_multipart_upload_days = local.application_data.accounts[local.environment].s3_lifecycle_days_abort_incomplete_multipart_upload_days
+#     }
+#   ]
+#
+#   tags = merge(local.tags,
+#     { Name = lower(format("s3-bucket-%s-%s", local.application_name, local.environment)) }
+#   )
+# }
+#
+# resource "aws_s3_bucket_notification" "artefact_bucket_notification" {
+#   bucket      = module.s3-bucket.bucket.id
+#   eventbridge = true
+#   topic {
+#     topic_arn     = aws_sns_topic.s3_topic.arn
+#     events        = ["s3:ObjectCreated:*"]
+#     filter_suffix = ".log"
+#   }
+# }
+#
+# data "aws_iam_policy_document" "artefacts_s3_policy" {
+#   statement {
+#     principals {
+#       type = "AWS"
+#       identifiers = [
+#         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/developer",
+#         "arn:aws:iam::${local.environment_management.account_ids["core-shared-services-production"]}:root"
+#       ]
+#     }
+#     actions   = ["s3:GetObject"]
+#     resources = ["arn:aws:s3:::${local.artefact_bucket_name}/*"]
+#   }
+# }
 
-  log_bucket = local.logging_bucket_name
-  log_prefix = "s3access/${local.artefact_bucket_name}"
-
-  # Refer to the below section "Replication" before enabling replication
-  replication_enabled = false
-  # Below three variables and providers configuration are only relevant if 'replication_enabled' is set to true
-  replication_region = "eu-west-2"
-  # replication_role_arn                     = module.s3-bucket-replication-role.role.arn
-  providers = {
-    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
-    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
-    # Platform team to add a new provider for the additional Region.
-    aws.bucket-replication = aws
-  }
-
-  lifecycle_rule = [
-    {
-      id      = "main"
-      enabled = "Enabled"
-      prefix  = ""
-
-      tags = {
-        rule      = "log"
-        autoclean = "true"
-      }
-
-      transition = [
-        {
-          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_standard
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_glacier
-          storage_class = "GLACIER"
-        }
-      ]
-
-      expiration = {
-        days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_current
-      }
-
-      noncurrent_version_transition = [
-        {
-          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_standard
-          storage_class = "STANDARD_IA"
-          }, {
-          days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_glacier
-          storage_class = "GLACIER"
-        }
-      ]
-
-      noncurrent_version_expiration = {
-        days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_noncurrent
-      }
-
-      abort_incomplete_multipart_upload_days = local.application_data.accounts[local.environment].s3_lifecycle_days_abort_incomplete_multipart_upload_days
-    }
-  ]
-
-  tags = merge(local.tags,
-    { Name = lower(format("s3-bucket-%s-%s", local.application_name, local.environment)) }
-  )
-}
-
-resource "aws_s3_bucket_notification" "artefact_bucket_notification" {
-  bucket      = module.s3-bucket.bucket.id
-  eventbridge = true
-  topic {
-    topic_arn     = aws_sns_topic.s3_topic.arn
-    events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
-  }
-}
-
-data "aws_iam_policy_document" "artefacts_s3_policy" {
-  statement {
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/developer",
-        "arn:aws:iam::${local.environment_management.account_ids["core-shared-services-production"]}:root"
-      ]
-    }
-    actions   = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::${local.artefact_bucket_name}/*"]
-  }
-}
-
+# ---------------------------------------------
 # S3 Bucket - Logging
+# ---------------------------------------------
 module "s3-bucket-logging" {
   # v8.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/52a40b0dd18aaef0d7c5565d93cc8997aad79636
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=52a40b0dd18aaef0d7c5565d93cc8997aad79636"
@@ -219,7 +224,9 @@ data "aws_iam_policy_document" "logging_s3_policy" {
   }
 }
 
+# ---------------------------------------------
 # S3 Bucket - R-sync
+# ---------------------------------------------
 module "s3-bucket-dbbackup" {
   # v8.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/52a40b0dd18aaef0d7c5565d93cc8997aad79636
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=52a40b0dd18aaef0d7c5565d93cc8997aad79636"
@@ -237,9 +244,6 @@ module "s3-bucket-dbbackup" {
   replication_region = "eu-west-2"
   # replication_role_arn                     = module.s3-bucket-replication-role.role.arn
   providers = {
-    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
-    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
-    # Platform team to add a new provider for the additional Region.
     aws.bucket-replication = aws
   }
 
@@ -316,6 +320,7 @@ data "aws_iam_policy_document" "dbbackup_s3_policy" {
     resources = ["${module.s3-bucket-dbbackup.bucket.arn}/*"]
   }
 }
+
 #For shared bucket lifecycle rule is not needed as it host lambda application source code
 resource "aws_s3_bucket" "ccms_ebs_shared" {
   bucket = "${local.application_name}-${local.environment}-shared"
@@ -325,9 +330,7 @@ resource "aws_s3_bucket" "ccms_ebs_shared" {
       Name = "${local.application_name}-${local.environment}-shared"
     }
   )
-
 }
-
 
 resource "aws_s3_object" "folder" {
   bucket = aws_s3_bucket.ccms_ebs_shared.bucket
@@ -337,7 +340,6 @@ resource "aws_s3_object" "folder" {
   }
 
   key = each.value
-
 }
 
 resource "aws_s3_bucket_public_access_block" "ccms_ebs_shared" {
@@ -356,10 +358,7 @@ resource "aws_s3_bucket_versioning" "ccms_ebs_shared" {
   }
 }
 
-
-
 # S3 Bucket for Payment Load
-
 resource "aws_s3_bucket" "lambda_payment_load" {
   bucket = "${local.application_name}-${local.environment}-payment-load"
 
@@ -388,10 +387,8 @@ resource "aws_s3_bucket_versioning" "lambda_payment_load" {
 
 # Lifecycle configuration: expire current objects and noncurrent versions after 30 days
 resource "aws_s3_bucket_lifecycle_configuration" "lambda_payment_load_lifecycle" {
-
   bucket = aws_s3_bucket.lambda_payment_load.id
 
-  # One lifecycle rule per prefix
   rule {
     id     = "expire-${aws_s3_bucket.lambda_payment_load.id}-${local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_current}d"
     status = "Enabled"
@@ -413,6 +410,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "lambda_payment_load_lifecycle"
       noncurrent_days = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_glacier
       storage_class   = "GLACIER"
     }
+
     noncurrent_version_expiration {
       noncurrent_days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_noncurrent
     }
@@ -420,16 +418,18 @@ resource "aws_s3_bucket_lifecycle_configuration" "lambda_payment_load_lifecycle"
     abort_incomplete_multipart_upload {
       days_after_initiation = local.application_data.accounts[local.environment].s3_lifecycle_days_abort_incomplete_multipart_upload_days
     }
-
   }
-
 }
 
-# Development
-moved {
-  from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-development-logging"]
-  to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-development-logging"]
-}
+# ---------------------------------------------
+# MOVED blocks
+# ---------------------------------------------
+
+# Development (DISABLED for artefacts module)
+# moved {
+#   from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-development-logging"]
+#   to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-development-logging"]
+# }
 
 moved {
   from = module.s3-bucket-dbbackup.aws_s3_bucket_logging.default["ccms-ebs-development-logging"]
@@ -441,11 +441,11 @@ moved {
   to   = module.s3-bucket-logging.aws_s3_bucket_logging.default_single_name["ccms-ebs-development-logging"]
 }
 
-# Test
-moved {
-  from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-test-logging"]
-  to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-test-logging"]
-}
+# Test (DISABLED for artefacts module)
+# moved {
+#   from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-test-logging"]
+#   to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-test-logging"]
+# }
 
 moved {
   from = module.s3-bucket-dbbackup.aws_s3_bucket_logging.default["ccms-ebs-test-logging"]
@@ -457,11 +457,11 @@ moved {
   to   = module.s3-bucket-logging.aws_s3_bucket_logging.default_single_name["ccms-ebs-test-logging"]
 }
 
-# Preproduction
-moved {
-  from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-preproduction-logging"]
-  to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-preproduction-logging"]
-}
+# Preproduction (DISABLED for artefacts module)
+# moved {
+#   from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-preproduction-logging"]
+#   to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-preproduction-logging"]
+# }
 
 moved {
   from = module.s3-bucket-dbbackup.aws_s3_bucket_logging.default["ccms-ebs-preproduction-logging"]
@@ -473,11 +473,11 @@ moved {
   to   = module.s3-bucket-logging.aws_s3_bucket_logging.default_single_name["ccms-ebs-preproduction-logging"]
 }
 
-# Production
-moved {
-  from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-production-logging"]
-  to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-production-logging"]
-}
+# Production (DISABLED for artefacts module)
+# moved {
+#   from = module.s3-bucket.aws_s3_bucket_logging.default["ccms-ebs-production-logging"]
+#   to   = module.s3-bucket.aws_s3_bucket_logging.default_single_name["ccms-ebs-production-logging"]
+# }
 
 moved {
   from = module.s3-bucket-dbbackup.aws_s3_bucket_logging.default["ccms-ebs-production-logging"]
