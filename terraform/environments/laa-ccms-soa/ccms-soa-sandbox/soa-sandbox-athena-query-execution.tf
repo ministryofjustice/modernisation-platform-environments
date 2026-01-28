f05ae63f138e172a7f44e865c472f8f44c63b962
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-01-28 11:05:29 +0000

    fix invalid name terraform error2

--- terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-athena-query-execution.tf
+++ terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-athena-query-execution.tf
@@ -14,22 +14,22 @@ export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.main_table_admin.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
-  --query-execution-context Database=${aws_athena_database.soa-sandbox-lb-access-logs.name} \
+  --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
   --region ${data.aws_region.current.region}
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.main_table_managed.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
-  --query-execution-context Database=${aws_athena_database.soa-sandbox-lb-access-logs.name} \
+  --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
   --region ${data.aws_region.current.region}
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.tls_requests_admin.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
-  --query-execution-context Database=${aws_athena_database.soa-sandbox-lb-access-logs.name} \
+  --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
   --region ${data.aws_region.current.region}
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.tls_requests_managed.id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.lb-access-logs.name} \
-  --query-execution-context Database=${aws_athena_database.soa-sandbox-lb-access-logs.name} \
+  --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
   --region ${data.aws_region.current.region}
 EOF
   }
