426a3cbb9af49faa8e30c1b83fa9d4fc8d385d01
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-01-28 15:24:40 +0000

    test removing null resource

--- terraform/environments/coat/chatbot-lambda.tf
+++ terraform/environments/coat/chatbot-lambda.tf
@@ -1,17 +1,17 @@
-resource "null_resource" "build_lambda_zip" {
+# resource "null_resource" "build_lambda_zip" {
 
-  triggers = {
-    script_hash = filesha256("${path.module}/lambdas/rag-lambda/rag-lambda.py")
-  }
+#   triggers = {
+#     script_hash = filesha256("${path.module}/lambdas/rag-lambda/rag-lambda.py")
+#   }
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      cd ${path.module}/lambdas/rag-lambda
+#   provisioner "local-exec" {
+#     command = <<-EOT
+#       cd ${path.module}/lambdas/rag-lambda
 
-      zip -r rag-lambda.zip .
-    EOT
-  }
-}
+#       zip -r rag-lambda.zip .
+#     EOT
+#   }
+# }
 
 data "archive_file" "rag_lambda" {
   type        = "zip"
