389a168578e5d0dd43e72b47c894a7ea5c89a745
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-01-28 11:06:41 +0000

    fix typo

--- terraform/environments/oasys/locals_production.tf
+++ terraform/environments/oasys/locals_production.tf
@@ -657,7 +657,7 @@ locals {
         access_logs_lifecycle_rule = [module.baseline_presets.s3_lifecycle_rules.general_purpose_one_year]
 
         s3_notification_queues = {
-          "cortex-xsiam-s3-public-alb-log-collection" = {
+          "cortex-xsiam-s3-private-alb-log-collection" = {
             events    = ["s3:ObjectCreated:*"]
             queue_arn = "cortex-xsiam-s3-alb-log-collection"
           }
