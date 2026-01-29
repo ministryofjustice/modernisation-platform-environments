4fe796b8f490abb1902d4af0cdae16693a001947
Timi Olubusi <timiolubusi@icloud.com>
2026-01-29 16:03:19 +0000

    Update observability_platform.tf

--- terraform/environments/wardship/observability_platform.tf
+++ terraform/environments/wardship/observability_platform.tf
@@ -1,8 +1,6 @@
 module "observability_platform_tenant" {
-
-  source = "github.com/ministryofjustice/terraform-aws-observability-platform-tenant?ref=fbbe5c8282786bcc0a00c969fe598e14f12eea9b" # v1.2.0
-
+  source                            = "ministryofjustice/observability-platform-tenant/aws"
+  version                           = "1.2.0"
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-production"]
-
-  tags = local.tags
+  tags                              = local.tags
 }
