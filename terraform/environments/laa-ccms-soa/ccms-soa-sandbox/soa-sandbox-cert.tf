b28703139ad93e7fdc86acc9a48f573c2df715a9
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-01-29 13:27:20 +0000

    uncommented required files and name changes

--- terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-cert.tf
+++ terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-cert.tf
@@ -1,14 +1,14 @@
 # #-- ACM Certificate for SOASANDBOX Load Balancers --#
-# resource "aws_acm_certificate" "soa" {
-#   domain_name               = trim(data.aws_route53_zone.external.name, ".")
-#   subject_alternative_names = [aws_route53_record.admin.fqdn, aws_route53_record.managed.fqdn]
-#   validation_method         = "DNS"
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+resource "aws_acm_certificate" "soa" {
+  domain_name               = trim(data.aws_route53_zone.external.name, ".")
+  subject_alternative_names = [aws_route53_record.admin.fqdn, aws_route53_record.managed.fqdn]
+  validation_method         = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_acm_certificate_validation" "soa" {
-#   certificate_arn         = aws_acm_certificate.soa.arn
-#   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "soa" {
+  certificate_arn         = aws_acm_certificate.soa.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+}
