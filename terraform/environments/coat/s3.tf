235ee6091c00da6d7b9cdd0a1b7502dec62b758b
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-01-29 16:21:24 +0000

    add application tag to s3 test

--- terraform/environments/coat/s3.tf
+++ terraform/environments/coat/s3.tf
@@ -41,6 +41,7 @@ module "cur_v2_hourly" {
   tags = merge(
     { "owner" = "CloudOptimisationAndAccountability@justice.gov.uk" },
     { "business-unit" = "lalala" },
+    { "application" = "COAT" },
     { "is-production" = true },
     { "owner" = "COAT" },
     { "service-area" = "lalalalala" },
