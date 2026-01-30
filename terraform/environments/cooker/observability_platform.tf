8553ed9d6150dc50ec7d0e977f773ca269b2c48b
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-01-30 11:49:57 +0000

    Update observability_platform.tf

--- terraform/environments/cooker/observability_platform.tf
+++ terraform/environments/cooker/observability_platform.tf
@@ -1,8 +1,13 @@
 module "observability_platform_tenant" {
-  source = "ministryofjustice/observability-platform-tenant/aws"
+  source  = "ministryofjustice/observability-platform-tenant/aws"
   version = "9.9.9"
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-production"]
-
   tags = local.tags
+
+  enable_health_signal_reader_role = true
+
+  observability_platform_health_signal_assumer_arns = [
+    "arn:aws:iam::${local.environment_management.account_ids["observability-platform-production"]}:role/op-production-health-signals-lambda"
+  ]
 }
