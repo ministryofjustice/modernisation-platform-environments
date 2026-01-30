e151efd26dc1fa16a5037bd9f79609c9eadac6a7
Timi Olubusi <timiolubusi@icloud.com>
2026-01-30 10:02:46 +0000

    Update observability_platform.tf

--- terraform/environments/ncas/observability_platform.tf
+++ terraform/environments/ncas/observability_platform.tf
@@ -1,6 +1,6 @@
 module "observability_platform_tenant" {
   source = "ministryofjustice/observability-platform-tenant/aws"
-  version = "2.0.0"
+  version = "1.2.0"
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-production"]
 
