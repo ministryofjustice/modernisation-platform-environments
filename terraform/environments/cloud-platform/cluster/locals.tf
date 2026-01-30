d25beba292ee7bf232c235a6ab259bb0da15d6cb
jackstockley89 <jstock8904@gmail.com>
2026-01-30 12:19:01 +0000

    feat(TF): delete cluster core

--- terraform/environments/cloud-platform/cluster/locals.tf
+++ terraform/environments/cloud-platform/cluster/locals.tf
@@ -6,7 +6,7 @@ locals {
     "cloud-platform-live",
   ]
   environment_configuration = local.environment_configurations[local.cluster_environment]
-  enabled_workspaces        = ["development_cluster"]
+  enabled_workspaces        = ["development_cluster","preproduction","nonlive","live"]
   cp_vpc_name               = terraform.workspace
   cluster_name              = contains(local.mp_environments, terraform.workspace) ? local.environment : terraform.workspace
   cluster_environment       = contains(local.mp_environments, terraform.workspace) ? local.environment : "development_cluster"
