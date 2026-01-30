c69ed8c24ab6757255ea26bc8a13673a85b5e276
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-01-30 13:03:14 +0000

    Refactor observability platform configuration

--- terraform/environments/cooker/observability_platform.tf
+++ terraform/environments/cooker/observability_platform.tf
@@ -3,11 +3,10 @@ module "observability_platform_tenant" {
   version = "9.9.9"
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-development"]
-  tags = local.tags
-
-  enable_health_signal_reader_role = true
-
+  
   observability_platform_health_signal_assumer_arns = [
     "arn:aws:iam::${local.environment_management.account_ids["observability-platform-development"]}:role/op-development-health-signals-lambda"
   ]
+
+  enable_health_signal_reader_role = true
 }
