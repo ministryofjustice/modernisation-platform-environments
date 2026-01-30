efedc6ce84149dd3717f80249f2ec1f91d334aab
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-01-30 12:14:05 +0000

    Change observability platform account to development

--- terraform/environments/cooker/observability_platform.tf
+++ terraform/environments/cooker/observability_platform.tf
@@ -2,12 +2,12 @@ module "observability_platform_tenant" {
   source  = "ministryofjustice/observability-platform-tenant/aws"
   version = "9.9.9"
 
-  observability_platform_account_id = local.environment_management.account_ids["observability-platform-production"]
+  observability_platform_account_id = local.environment_management.account_ids["observability-platform-development"]
   tags = local.tags
 
   enable_health_signal_reader_role = true
 
   observability_platform_health_signal_assumer_arns = [
-    "arn:aws:iam::${local.environment_management.account_ids["observability-platform-production"]}:role/op-production-health-signals-lambda"
+    "arn:aws:iam::${local.environment_management.account_ids["observability-platform-development"]}:role/op-production-health-signals-lambda"
   ]
 }
