a0b18ea63eac59ab5f43cbca598794c66a5983c7
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-01-30 15:11:45 +0000

    Downgrade observability platform module version to 9.9.8

--- terraform/environments/cooker/observability_platform.tf
+++ terraform/environments/cooker/observability_platform.tf
@@ -1,6 +1,6 @@
 module "observability_platform_tenant" {
   source  = "ministryofjustice/observability-platform-tenant/aws"
-  version = "9.9.9"
+  version = "9.9.8"
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-development"]
   
