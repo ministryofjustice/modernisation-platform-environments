f3601231b96973ea81ddc4b56dd870100a457bce
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-01-30 16:25:17 +0000

    commented the files to reduce the layers

--- terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-secrets.tf
+++ terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-secrets.tf
@@ -72,12 +72,14 @@ data "aws_secretsmanager_secret_version" "trust_store_password" {
 #   })
 # }
 
-# resource "aws_secretsmanager_secret_version" "ccms_soa_quiesced_secrets_version" {
+# resource "aws_secretsmanager_secret_version" "sandbox_ccms_soa_quiesced_secrets_version" {
 #   secret_id = aws_secretsmanager_secret.ccms_soa_quiesced_secrets.id
 
 #   secret_string = jsonencode({
 #     slack_channel_webhook           = "",
 #     slack_channel_webhook_guardduty = ""
+
+    
 #   })
 
 #   lifecycle {
