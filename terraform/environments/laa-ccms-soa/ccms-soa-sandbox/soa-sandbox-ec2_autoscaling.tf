86c272ae27a71bdc1c552f670861f020d17cb394
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-02-02 16:25:42 +0000

    removed the commented lines

--- terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-ec2_autoscaling.tf
+++ terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-ec2_autoscaling.tf
@@ -171,8 +171,6 @@ data "template_file" "launch-template-managed" {
     cluster_name       = "${local.component_name}-${local.environment}-cluster"
     efs_id             = aws_efs_file_system.storage.id
     server             = "managed"
-    inbound_bucket     = ""
-    outbound_bucket    = ""
     deploy_environment = local.environment
   }
 }
@@ -183,8 +181,6 @@ data "template_file" "launch-template-admin" {
     cluster_name       = "soasandbox-${local.application_data.accounts[local.environment].app_name}-cluster"
     efs_id             = aws_efs_file_system.storage.id
     server             = "admin"
-    inbound_bucket     = ""
-    outbound_bucket    = ""
     deploy_environment = local.environment
   }
 }
