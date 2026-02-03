4fb6ef3291fb9a458399aa5c75017c20667a110b
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-02-03 15:55:02 +0000

    updated the variable name in json and commented unwanted in logs file

--- terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-logs.tf
+++ terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-logs.tf
@@ -26,29 +26,29 @@ resource "aws_cloudwatch_log_metric_filter" "soa_stuck_thread_admin" {
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "soa_benefit_checker_admin" {
-  name           = "SOABenefitCheckerAdmin"
-  pattern        = "\"<Error>\" \"benefitchecker\""
-  log_group_name = aws_cloudwatch_log_group.log_group_admin.name
+# resource "aws_cloudwatch_log_metric_filter" "soa_benefit_checker_admin" {
+#   name           = "SOABenefitCheckerAdmin"
+#   pattern        = "\"<Error>\" \"benefitchecker\""
+#   log_group_name = aws_cloudwatch_log_group.log_group_admin.name
 
-  metric_transformation {
-    name      = "SOABenefitCheckerAdmin"
-    namespace = "CCMS-SOA-APP"
-    value     = "1"
-  }
-}
+#   metric_transformation {
+#     name      = "SOABenefitCheckerAdmin"
+#     namespace = "CCMS-SOA-APP"
+#     value     = "1"
+#   }
+# }
 
-resource "aws_cloudwatch_log_metric_filter" "soa_benefit_checker_rollback_error_admin" {
-  name           = "SOABenefitCheckerRollbackErrorAdmin"
-  pattern        = "\"<Error>\" -\"benefitchecker\" -\"<ADFC-64007>\" -\"Transaction rolledback\" -\"transaction has been rolled back\""
-  log_group_name = aws_cloudwatch_log_group.log_group_admin.name
+# resource "aws_cloudwatch_log_metric_filter" "soa_benefit_checker_rollback_error_admin" {
+#   name           = "SOABenefitCheckerRollbackErrorAdmin"
+#   pattern        = "\"<Error>\" -\"benefitchecker\" -\"<ADFC-64007>\" -\"Transaction rolledback\" -\"transaction has been rolled back\""
+#   log_group_name = aws_cloudwatch_log_group.log_group_admin.name
 
-  metric_transformation {
-    name      = "SOABenefitCheckerRollbackErrorAdmin"
-    namespace = "CCMS-SOA-APP"
-    value     = "1"
-  }
-}
+#   metric_transformation {
+#     name      = "SOABenefitCheckerRollbackErrorAdmin"
+#     namespace = "CCMS-SOA-APP"
+#     value     = "1"
+#   }
+# }
 
 # #--Managed
 resource "aws_cloudwatch_log_group" "log_group_managed" {
@@ -78,29 +78,29 @@ resource "aws_cloudwatch_log_metric_filter" "soa_stuck_thread_managed" {
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "soa_benefit_checker_managed" {
-  name           = "SOABenefitCheckerManaged"
-  pattern        = "\"<Error>\" \"benefitchecker\""
-  log_group_name = aws_cloudwatch_log_group.log_group_managed.name
+# resource "aws_cloudwatch_log_metric_filter" "soa_benefit_checker_managed" {
+#   name           = "SOABenefitCheckerManaged"
+#   pattern        = "\"<Error>\" \"benefitchecker\""
+#   log_group_name = aws_cloudwatch_log_group.log_group_managed.name
 
-  metric_transformation {
-    name      = "SOABenefitCheckerManaged"
-    namespace = "CCMS-SOA-APP"
-    value     = "1"
-  }
-}
+#   metric_transformation {
+#     name      = "SOABenefitCheckerManaged"
+#     namespace = "CCMS-SOA-APP"
+#     value     = "1"
+#   }
+# }
 
-resource "aws_cloudwatch_log_metric_filter" "soa_benefit_checker_rollback_error_managed" {
-  name           = "SOABenefitCheckerRollbackErrorManaged"
-  pattern        = "\"<Error>\" -\"benefitchecker\" -\"Transaction rolledback\" -\"transaction has been rolled back\""
-  log_group_name = aws_cloudwatch_log_group.log_group_managed.name
+# resource "aws_cloudwatch_log_metric_filter" "soa_benefit_checker_rollback_error_managed" {
+#   name           = "SOABenefitCheckerRollbackErrorManaged"
+#   pattern        = "\"<Error>\" -\"benefitchecker\" -\"Transaction rolledback\" -\"transaction has been rolled back\""
+#   log_group_name = aws_cloudwatch_log_group.log_group_managed.name
 
-  metric_transformation {
-    name      = "SOABenefitCheckerRollbackErrorManaged"
-    namespace = "CCMS-SOA-APP"
-    value     = "1"
-  }
-}
+#   metric_transformation {
+#     name      = "SOABenefitCheckerRollbackErrorManaged"
+#     namespace = "CCMS-SOA-APP"
+#     value     = "1"
+#   }
+# }
 
 # # resource "aws_cloudwatch_log_metric_filter" "soa_custom_checks_error_managed" {
 # #   name           = "SOACustomChecksManaged"
