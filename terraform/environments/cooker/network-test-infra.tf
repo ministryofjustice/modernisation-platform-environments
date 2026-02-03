e1c2ff839eaf146b8d59d94c7e45d147a1298e65
mikereiddigital <mike.reid@justice.gov.uk>
2026-02-03 11:06:32 +0000

    Testing

--- terraform/environments/cooker/network-test-infra.tf
+++ terraform/environments/cooker/network-test-infra.tf
@@ -157,25 +157,25 @@ resource "aws_security_group_rule" "web_server_2" {
   ]
 }
 
-resource "aws_security_group_rule" "web_server_3" {
+# resource "aws_security_group_rule" "web_server_3" {
 
-  security_group_id = aws_security_group.web_server.id
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+#   security_group_id = aws_security_group.web_server.id
+#   type              = "egress"
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
-resource "aws_security_group_rule" "web_server_4" {
+# resource "aws_security_group_rule" "web_server_4" {
 
-  security_group_id = aws_security_group.web_server.id
-  type              = "egress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+#   security_group_id = aws_security_group.web_server.id
+#   type              = "egress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
 
 resource "aws_security_group" "internal_server" {
