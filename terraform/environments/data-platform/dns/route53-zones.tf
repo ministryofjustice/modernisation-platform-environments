674b9774a430ea5b00e41fbd44800c5c54b1dbdf
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-02-06 17:09:45 +0000

    chore(data-platform): remove obsolete route53-zones.tf file
    This file was restructured in main as route53.tf with improved for_each loop.
    The module version update to v6.4.0 is already included in the new structure.

--- terraform/environments/data-platform/dns/route53-zones.tf
+++
@@ -1,6 +0,0 @@
-module "zone" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-route53.git?ref=b4c9c820abdff9357747865f65131d1fa4128edc" # v6.4.0
-
-  name    = local.environment_configuration.route53_zone_name
-  records = local.environment_configuration.route53_records
-}
