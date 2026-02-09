22cf9dcd24c8fa77de484e90fd6140ba36fb7209
David Seekins <david.seekins@necsws.com>
2026-02-09 14:30:11 +0000

    Create a 2nd Tableau Inbstance for a controlled upgrade

--- terraform/environments/youth-justice-app-framework/modules/tableau/main.tf
+++ terraform/environments/youth-justice-app-framework/modules/tableau/main.tf
@@ -3,52 +3,6 @@ resource "aws_iam_instance_profile" "tableau" {
   role = aws_iam_role.ec2_tableau_role.name
 }
 
-resource "aws_instance" "tableau" {
-  ami                     = data.aws_ami.app_ami.id
-  instance_type           = var.instance_type
-  subnet_id               = var.tableau_subnet_id
-  private_ip              = var.private_ip
-  vpc_security_group_ids  = [module.tableau_sg.security_group_id]
-  disable_api_termination = local.disable_api_termination
-
-  key_name             = module.key_pair.key_pair_name
-  iam_instance_profile = aws_iam_instance_profile.tableau.name
-
-  metadata_options {
-    http_tokens = "required"
-  }
-
-  ebs_optimized = true
-
-  user_data = (templatefile("${path.module}/tableau_init.sh.tftpl",
-    {
-      dd_api_key_secret_arn = var.datadog_api_key_arn,
-      instance_role         = "tableau"
-  }))
-
-  root_block_device {
-    delete_on_termination = local.delete_on_termination
-    encrypted             = true
-    volume_size           = var.instance_volume_size
-    tags = {
-      Name = "Tableau Server"
-    }
-  }
-
-  tags = merge(local.all_tags,
-    { "Name" = "Tableau Server" },
-    { "Build" = data.aws_ami.app_ami.name },
-    { "PatchSchedule" = var.patch_schedule },
-    { "OS" = "Linux" },
-    { "Owner" = "Devops" }
-  )
-
-  ## Create using the latest version of the ami but do not replace when a new version is repeased. 
-  lifecycle {
-    ignore_changes = [ami]
-  }
-
-}
 #trivy:ignore:AVD-AWS-0052 - needs testing in non-prod first
 #trivy:ignore:AVD-AWS-0053
 module "tableau-alb" {
