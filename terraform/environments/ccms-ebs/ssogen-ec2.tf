1130a8edd221200792575cb3deb1b31e1a69c13f
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-02-10 17:48:45 +0000

    fix: athena and cloudwatch changes

--- terraform/environments/ccms-ebs/ssogen-ec2.tf
+++ terraform/environments/ccms-ebs/ssogen-ec2.tf
@@ -2,55 +2,136 @@ locals {
   is_development = local.environment == "development"
 }
 
-resource "aws_instance" "ec2_ssogen" {
-  count = local.is_development ? local.application_data.accounts[local.environment].ssogen_no_instances : 0
+data "template_file" "launch-template" {
+  template = file("${path.module}/templates/ec2_user_data_ssogen.sh")
+  vars = {
+    cluster_name       = "${local.application_name}-cluster"
+    deploy_environment = local.environment
+  }
+}
 
-  instance_type          = local.application_data.accounts[local.environment].ec2_oracle_instance_type_ssogen
-  ami                    = local.application_data.accounts[local.environment]["ssogen_ami_id-${count.index + 1}"]
-  key_name               = aws_key_pair.ssogen[0].key_name
-  vpc_security_group_ids = [aws_security_group.ssogen_sg[0].id]
-  subnet_id              = local.private_subnets[count.index]
-  # monitoring                  = true
-  ebs_optimized               = false
-  associate_public_ip_address = false
-  iam_instance_profile        = aws_iam_instance_profile.ssogen_instance_profile[0].name
+resource "aws_launch_template" "ssogen-ec2-launch-template" {
+  name_prefix   = local.application_name
+  image_id      = local.application_data.accounts[local.environment].ami_image_id
+  instance_type = local.application_data.accounts[local.environment].ec2_oracle_instance_type_ssogen
+  # key_name      = var.key_name
+  ebs_optimized = true
 
-  lifecycle {
-    ignore_changes = [
-      user_data
-    ]
+  monitoring {
+    enabled = true
   }
 
-  root_block_device {
-    volume_size = 60
-    volume_type = "gp2"
-    encrypted   = true
-    tags = merge(
-      local.tags,
-      { Name = "ec2-ccms-ebs-development-ssogen-${count.index + 1}" },
-      { "instance-role" = local.application_data.accounts[local.environment].instance_role_ssogen },
-      { "instance-scheduling" = local.application_data.accounts[local.environment]["instance-scheduling"] },
-      { backup = "true" }
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_instance_profile.name
+  }
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.cluster_ec2.id]
+  }
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      delete_on_termination = true
+      encrypted             = false
+      volume_size           = 30
+      volume_type           = "gp2"
+      iops                  = 0
+    }
+  }
+
+  user_data = base64encode(data.template_file.launch-template.rendered)
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(local.tags,
+      { Name = lower(format("%s-%s-ecs-cluster", local.application_name, local.environment)) }
     )
   }
 
-  # user_data_replace_on_change = true
-  user_data = base64encode(templatefile("./templates/ec2_user_data_ssogen.sh", {
-    hostname = "ssogen-${count.index + 1}"
-  }))
-
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(local.tags,
+      { instance-scheduling = "skip-scheduling" }
+    )
   }
 
-  tags = merge(
-    local.tags,
-    { Name = "ec2-ccms-ebs-development-ssogen-${count.index + 1}" },
-    { "instance-role" = local.application_data.accounts[local.environment].instance_role_ssogen },
-    { "instance-scheduling" = local.application_data.accounts[local.environment]["instance-scheduling"] },
-    { backup = "true" }
+  tag_specifications {
+    resource_type = "volume"
+    tags = merge(local.tags,
+      { Name = lower(format("%s-%s-ecs-cluster", local.application_name, local.environment)) }
+    )
+  }
+
+  tags = merge(local.tags,
+    { Name = lower(format("%s-%s-ecs-cluster-template", local.application_name, local.environment)) }
   )
 
-  depends_on = [aws_security_group.ssogen_sg]
 }
+
+resource "aws_autoscaling_group" "cluster-scaling-group" {
+  name                = "${local.application_name}-ssogen-auto-scaling-group"
+  vpc_zone_identifier = data.aws_subnets.shared-private.ids
+  desired_capacity    = local.application_data.accounts[local.environment].ssogen_desired_capacity
+  max_size            = local.application_data.accounts[local.environment].ssogen_max_capacity
+  min_size            = local.application_data.accounts[local.environment].ssogen_min_capacity
+
+  launch_template {
+    id      = aws_launch_template.ec2-launch-template.id
+    version = "$Latest"
+  }
+
+}
+# resource "aws_instance" "ec2_ssogen" {
+#   count = local.is_development ? local.application_data.accounts[local.environment].ssogen_no_instances : 0
+
+#   instance_type          = local.application_data.accounts[local.environment].ec2_oracle_instance_type_ssogen
+#   ami                    = local.application_data.accounts[local.environment]["ssogen_ami_id-${count.index + 1}"]
+#   key_name               = aws_key_pair.ssogen[0].key_name
+#   vpc_security_group_ids = [aws_security_group.ssogen_sg[0].id]
+#   subnet_id              = local.private_subnets[count.index]
+#   # monitoring                  = true
+#   ebs_optimized               = false
+#   associate_public_ip_address = false
+#   iam_instance_profile        = aws_iam_instance_profile.ssogen_instance_profile[0].name
+
+#   lifecycle {
+#     ignore_changes = [
+#       user_data
+#     ]
+#   }
+
+#   root_block_device {
+#     volume_size = 60
+#     volume_type = "gp2"
+#     encrypted   = true
+#     tags = merge(
+#       local.tags,
+#       { Name = "ec2-ccms-ebs-development-ssogen-${count.index + 1}" },
+#       { "instance-role" = local.application_data.accounts[local.environment].instance_role_ssogen },
+#       { "instance-scheduling" = local.application_data.accounts[local.environment]["instance-scheduling"] },
+#       { backup = "true" }
+#     )
+#   }
+
+#   # user_data_replace_on_change = true
+#   user_data = base64encode(templatefile("./templates/ec2_user_data_ssogen.sh", {
+#     hostname = "ssogen-${count.index + 1}"
+#   }))
+
+#   metadata_options {
+#     http_endpoint = "enabled"
+#     http_tokens   = "required"
+#   }
+
+#   tags = merge(
+#     local.tags,
+#     { Name = "ec2-ccms-ebs-development-ssogen-${count.index + 1}" },
+#     { "instance-role" = local.application_data.accounts[local.environment].instance_role_ssogen },
+#     { "instance-scheduling" = local.application_data.accounts[local.environment]["instance-scheduling"] },
+#     { backup = "true" }
+#   )
+
+#   depends_on = [aws_security_group.ssogen_sg]
+# }
