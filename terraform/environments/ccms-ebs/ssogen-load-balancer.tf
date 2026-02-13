3f182eb388de9bad45fe7777e59d9306020526c1
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-02-13 14:16:35 +0000

    fix: disabled launch template

--- terraform/environments/ccms-ebs/ssogen-load-balancer.tf
+++ terraform/environments/ccms-ebs/ssogen-load-balancer.tf
@@ -66,38 +66,38 @@ resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_admin" {
   }
 }
 
-# resource "aws_lb_listener" "ssogen_internal_listener" {
-#   count             = local.is-development || local.is-test ? 1 : 0
-#   load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-#   certificate_arn   = aws_acm_certificate.external.arn
+resource "aws_lb_listener" "ssogen_internal_listener" {
+  count             = local.is-development || local.is-test ? 1 : 0
+  load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.external.arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_app[count.index].arn
-#   }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_app[count.index].arn
+  }
 
-#   depends_on = [aws_acm_certificate_validation.external_nonprod]
-# }
+  depends_on = [aws_acm_certificate_validation.external_nonprod]
+}
 
 
-# resource "aws_lb_listener" "ssogen_internal_console_listener" {
-#   count             = local.is-development || local.is-test ? 1 : 0
-#   load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
-#   port              = "7001"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-#   certificate_arn   = aws_acm_certificate.external.arn
+resource "aws_lb_listener" "ssogen_internal_console_listener" {
+  count             = local.is-development || local.is-test ? 1 : 0
+  load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
+  port              = "7001"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.external.arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_admin[count.index].arn
-#   }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_admin[count.index].arn
+  }
 
-#   depends_on = [aws_acm_certificate_validation.external_nonprod]
-# }
+  depends_on = [aws_acm_certificate_validation.external_nonprod]
+}
 
 # resource "aws_lb_target_group_attachment" "ssogen_internal" {
 #   count            = local.is-development ? local.application_data.accounts[local.environment].ssogen_no_instances : 0
