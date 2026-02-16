b83c514b861d010f0e880cff91a0b9518eb07518
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-02-16 23:51:30 +0000

    fix: delete lb, asg, lt

--- terraform/environments/ccms-ebs/ssogen-load-balancer.tf
+++ terraform/environments/ccms-ebs/ssogen-load-balancer.tf
@@ -1,103 +1,103 @@
-resource "aws_lb" "ssogen_alb" {
-  count              = local.is-development || local.is-test ? 1 : 0
-  name               = lower(format("lb-%s-internal", local.application_name_ssogen))
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg_ssogen_internal_alb[count.index].id]
-  subnets            = data.aws_subnets.shared-private.ids
+# resource "aws_lb" "ssogen_alb" {
+#   count              = local.is-development || local.is-test ? 1 : 0
+#   name               = lower(format("lb-%s-internal", local.application_name_ssogen))
+#   internal           = true
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.sg_ssogen_internal_alb[count.index].id]
+#   subnets            = data.aws_subnets.shared-private.ids
 
-  drop_invalid_header_fields = true
-  enable_deletion_protection = false
+#   drop_invalid_header_fields = true
+#   enable_deletion_protection = false
 
-  access_logs {
-    bucket  = module.s3-bucket-logging.bucket.id
-    prefix  = local.lb_log_prefix_ssogen_internal
-    enabled = true
-  }
+#   access_logs {
+#     bucket  = module.s3-bucket-logging.bucket.id
+#     prefix  = local.lb_log_prefix_ssogen_internal
+#     enabled = true
+#   }
 
-  tags = merge(local.tags,
-    { Name = lower(format("lb-%s-internal", local.application_name_ssogen)) }
-  )
-}
+#   tags = merge(local.tags,
+#     { Name = lower(format("lb-%s-internal", local.application_name_ssogen)) }
+#   )
+# }
 
-resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_app" {
-  count       = local.is-development || local.is-test ? 1 : 0
-  name        = lower(format("tg-%s-app", local.application_name_ssogen))
-  port        = local.application_data.accounts[local.environment].tg_ssogen_apps_port
-  protocol    = "HTTP"
-  vpc_id      = data.aws_vpc.shared.id
-  target_type = "instance"
-  # deregistration_delay = 60
-  health_check {
-    enabled             = true
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-  }
+# resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_app" {
+#   count       = local.is-development || local.is-test ? 1 : 0
+#   name        = lower(format("tg-%s-app", local.application_name_ssogen))
+#   port        = local.application_data.accounts[local.environment].tg_ssogen_apps_port
+#   protocol    = "HTTP"
+#   vpc_id      = data.aws_vpc.shared.id
+#   target_type = "instance"
+#   # deregistration_delay = 60
+#   health_check {
+#     enabled             = true
+#     path                = "/"
+#     protocol            = "HTTP"
+#     matcher             = "200"
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold   = 3
+#     unhealthy_threshold = 3
+#   }
 
-  stickiness {
-    enabled         = true
-    type            = "lb_cookie"
-    cookie_duration = 3600
-  }
-}
+#   stickiness {
+#     enabled         = true
+#     type            = "lb_cookie"
+#     cookie_duration = 3600
+#   }
+# }
 
-resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_console" { 
-  count       = local.is-development || local.is-test ? 1 : 0
-  name        = lower(format("tg-%s-console", local.application_name_ssogen))
-  port        = local.application_data.accounts[local.environment].tg_ssogen_admin_port
-  protocol    = "HTTP"
-  vpc_id      = data.aws_vpc.shared.id
-  target_type = "instance"
-  # deregistration_delay = 60
-  health_check {
-    enabled             = true
-    path                = "/weblogic/ready"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-  }
-}
+# resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_console" { 
+#   count       = local.is-development || local.is-test ? 1 : 0
+#   name        = lower(format("tg-%s-console", local.application_name_ssogen))
+#   port        = local.application_data.accounts[local.environment].tg_ssogen_admin_port
+#   protocol    = "HTTP"
+#   vpc_id      = data.aws_vpc.shared.id
+#   target_type = "instance"
+#   # deregistration_delay = 60
+#   health_check {
+#     enabled             = true
+#     path                = "/weblogic/ready"
+#     protocol            = "HTTP"
+#     matcher             = "200"
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold   = 3
+#     unhealthy_threshold = 3
+#   }
+# }
 
-resource "aws_lb_listener" "ssogen_internal_app_listener" {
-  count             = local.is-development || local.is-test ? 1 : 0
-  load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate.external.arn
+# resource "aws_lb_listener" "ssogen_internal_app_listener" {
+#   count             = local.is-development || local.is-test ? 1 : 0
+#   load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+#   certificate_arn   = aws_acm_certificate.external.arn
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_app[count.index].arn
-  }
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_app[count.index].arn
+#   }
 
-  depends_on = [aws_acm_certificate_validation.external_nonprod]
-}
+#   depends_on = [aws_acm_certificate_validation.external_nonprod]
+# }
 
 
-resource "aws_lb_listener" "ssogen_internal_console_listener" {
-  count             = local.is-development || local.is-test ? 1 : 0
-  load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
-  port              = "5443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate.external.arn
+# resource "aws_lb_listener" "ssogen_internal_console_listener" {
+#   count             = local.is-development || local.is-test ? 1 : 0
+#   load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
+#   port              = "5443"
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+#   certificate_arn   = aws_acm_certificate.external.arn
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_console[count.index].arn
-  }
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_console[count.index].arn
+#   }
 
-  depends_on = [aws_acm_certificate_validation.external_nonprod]
-}
+#   depends_on = [aws_acm_certificate_validation.external_nonprod]
+# }
 
 # resource "aws_lb_target_group_attachment" "ssogen_internal" {
 #   count            = local.is-development ? local.application_data.accounts[local.environment].ssogen_no_instances : 0
