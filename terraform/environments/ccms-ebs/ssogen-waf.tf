9b94826fa1db127ea4276d8d757dd551cdbb3850
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-02-16 14:29:44 +0000

    fix: count instance corrected

--- terraform/environments/ccms-ebs/ssogen-waf.tf
+++ terraform/environments/ccms-ebs/ssogen-waf.tf
@@ -106,6 +106,6 @@ resource "aws_cloudwatch_log_group" "ssogen_waf_logs" {
 
 resource "aws_wafv2_web_acl_logging_configuration" "ssogen_waf_logging" {
   count                   = local.is-development || local.is-test ? 1 : 0
-  log_destination_configs = [aws_cloudwatch_log_group.ssogen_waf_logs.arn]
+  log_destination_configs = [aws_cloudwatch_log_group.ssogen_waf_logs[count.index].arn]
   resource_arn            = aws_wafv2_web_acl.ssogen_web_acl[count.index].arn
 }
