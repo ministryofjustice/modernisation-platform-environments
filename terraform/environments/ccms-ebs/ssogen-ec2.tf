97f919f55bad7c32d91eea16932aba6a494aabb7
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-02-17 11:51:45 +0000

    fix: create a ssogen kms key

--- terraform/environments/ccms-ebs/ssogen-ec2.tf
+++ terraform/environments/ccms-ebs/ssogen-ec2.tf
@@ -39,7 +39,7 @@ resource "aws_launch_template" "ssogen-ec2-launch-template-primary" {
       volume_size           = 60
       volume_type           = "gp2"
       iops                  = 0
-      kms_key_id            = data.aws_kms_key.ssogen_kms_key[count.index].arn
+      kms_key_id            = aws_kms_key.ssogen_kms_key[count.index].arn
     }
   }
 
@@ -101,7 +101,7 @@ resource "aws_launch_template" "ssogen-ec2-launch-template-secondary" {
       volume_size           = 60
       volume_type           = "gp2"
       iops                  = 0
-      kms_key_id            = data.aws_kms_key.ssogen_kms_key[count.index].arn
+      kms_key_id            = aws_kms_key.ssogen_kms_key[count.index].arn
     }
   }
 
