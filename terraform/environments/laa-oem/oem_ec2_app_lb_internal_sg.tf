bee98e00c18e91053fdc93f4a86ee8aea8fc157d
Maciej Matysiak <103054339+mmgovuk@users.noreply.github.com>
2026-02-18 15:23:09 +0000

    CC-4027: Added manual changes to the code.

--- terraform/environments/laa-oem/oem_ec2_app_lb_internal_sg.tf
+++ terraform/environments/laa-oem/oem_ec2_app_lb_internal_sg.tf
@@ -8,17 +8,17 @@ resource "aws_security_group" "load_balancer_internal" {
   ), local.tags)
 }
 
-# Egress Rules for load_balancer_internal
-resource "aws_vpc_security_group_egress_rule" "lb_int_egress_all_0_0_cidr" {
-  security_group_id = aws_security_group.load_balancer_internal.id
-  description       = "Allow all outbound traffic"
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
-
-  tags = {
-    Name = "Allow all outbound traffic"
-  }
-}
+# # Egress Rules for load_balancer_internal
+# resource "aws_vpc_security_group_egress_rule" "lb_int_egress_all_0_0_cidr" {
+#   security_group_id = aws_security_group.load_balancer_internal.id
+#   description       = "Allow all outbound traffic"
+#   ip_protocol       = "-1"
+#   cidr_ipv4         = "0.0.0.0/0"
+# 
+#   tags = {
+#     Name = "Allow all outbound traffic"
+#   }
+# }
 
 # Ingress Rules for load_balancer_internal - HTTPS (443)
 resource "aws_vpc_security_group_ingress_rule" "lb_int_ingress_tcp_443_443_cidr_1" {
@@ -106,7 +106,7 @@ resource "aws_vpc_security_group_ingress_rule" "lb_int_ingress_tcp_3872_3872_cid
   ip_protocol       = "tcp"
   from_port         = 3872
   to_port           = 3872
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = data.aws_vpc.shared.cidr_block
 
   tags = {
     Name = "Oracle EM Console HTTP from anywhere"
@@ -159,7 +159,7 @@ resource "aws_vpc_security_group_ingress_rule" "lb_int_ingress_tcp_4903_4903_cid
   ip_protocol       = "tcp"
   from_port         = 4903
   to_port           = 4903
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = data.aws_vpc.shared.cidr_block
 
   tags = {
     Name = "Oracle EM Console HTTPS from anywhere"
@@ -252,7 +252,7 @@ resource "aws_vpc_security_group_ingress_rule" "lb_int_ingress_tcp_7803_7803_cid
   ip_protocol       = "tcp"
   from_port         = 7803
   to_port           = 7803
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = data.aws_vpc.shared.cidr_block
 
   tags = {
     Name = "Oracle EM port 7803 from anywhere"
--- terraform/environments/laa-oem/oem_ec2_app_lb_sg.tf
+++ terraform/environments/laa-oem/oem_ec2_app_lb_sg.tf
@@ -8,17 +8,17 @@ resource "aws_security_group" "load_balancer_security_group" {
   ), local.tags)
 }
 
-# Egress Rules for load_balancer_security_group
-resource "aws_vpc_security_group_egress_rule" "lb_sg_egress_all_0_0_cidr" {
-  security_group_id = aws_security_group.load_balancer_security_group.id
-  description       = "Allow all outbound traffic"
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
-
-  tags = {
-    Name = "Allow all outbound traffic"
-  }
-}
+# # Egress Rules for load_balancer_security_group
+# resource "aws_vpc_security_group_egress_rule" "lb_sg_egress_all_0_0_cidr" {
+#   security_group_id = aws_security_group.load_balancer_security_group.id
+#   description       = "Allow all outbound traffic"
+#   ip_protocol       = "-1"
+#   cidr_ipv4         = "0.0.0.0/0"
+# 
+#   tags = {
+#     Name = "Allow all outbound traffic"
+#   }
+# }
 
 # Ingress Rules for load_balancer_security_group - HTTPS (443)
 resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_443_443_cidr_1" {
@@ -100,18 +100,18 @@ resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_3872_3872_cidr
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_3872_3872_cidr_any" {
-  security_group_id = aws_security_group.load_balancer_security_group.id
-  description       = "Oracle EM Console HTTP from internet (public access)"
-  ip_protocol       = "tcp"
-  from_port         = 3872
-  to_port           = 3872
-  cidr_ipv4         = "0.0.0.0/0"
-
-  tags = {
-    Name = "Oracle EM Console HTTP from anywhere"
-  }
-}
+# resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_3872_3872_cidr_any" {
+#   security_group_id = aws_security_group.load_balancer_security_group.id
+#   description       = "Oracle EM Console HTTP from internet (public access)"
+#   ip_protocol       = "tcp"
+#   from_port         = 3872
+#   to_port           = 3872
+#   cidr_ipv4         = "0.0.0.0/0"
+# 
+#   tags = {
+#     Name = "Oracle EM Console HTTP from anywhere"
+#   }
+# }
 
 # Ingress Rules for load_balancer_security_group - Oracle EM Console HTTPS (4903)
 resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_4903_4903_cidr_1" {
@@ -153,18 +153,18 @@ resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_4903_4903_cidr
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_4903_4903_cidr_any" {
-  security_group_id = aws_security_group.load_balancer_security_group.id
-  description       = "Oracle EM Console HTTPS from internet (public access)"
-  ip_protocol       = "tcp"
-  from_port         = 4903
-  to_port           = 4903
-  cidr_ipv4         = "0.0.0.0/0"
-
-  tags = {
-    Name = "Oracle EM Console HTTPS from anywhere"
-  }
-}
+# resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_4903_4903_cidr_any" {
+#   security_group_id = aws_security_group.load_balancer_security_group.id
+#   description       = "Oracle EM Console HTTPS from internet (public access)"
+#   ip_protocol       = "tcp"
+#   from_port         = 4903
+#   to_port           = 4903
+#   cidr_ipv4         = "0.0.0.0/0"
+# 
+#   tags = {
+#     Name = "Oracle EM Console HTTPS from anywhere"
+#   }
+# }
 
 # Ingress Rules for load_balancer_security_group - Oracle EM OMS (7102)
 resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_7102_7102_cidr_1" {
@@ -246,15 +246,15 @@ resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_7803_7803_cidr
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_7803_7803_cidr_any" {
-  security_group_id = aws_security_group.load_balancer_security_group.id
-  description       = "Oracle EM additional port from internet (public access)"
-  ip_protocol       = "tcp"
-  from_port         = 7803
-  to_port           = 7803
-  cidr_ipv4         = "0.0.0.0/0"
-
-  tags = {
-    Name = "Oracle EM port 7803 from anywhere"
-  }
-}
+# resource "aws_vpc_security_group_ingress_rule" "lb_sg_ingress_tcp_7803_7803_cidr_any" {
+#   security_group_id = aws_security_group.load_balancer_security_group.id
+#   description       = "Oracle EM additional port from internet (public access)"
+#   ip_protocol       = "tcp"
+#   from_port         = 7803
+#   to_port           = 7803
+#   cidr_ipv4         = "0.0.0.0/0"
+# 
+#   tags = {
+#     Name = "Oracle EM port 7803 from anywhere"
+#   }
+# }
