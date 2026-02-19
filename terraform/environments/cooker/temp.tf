08d61a6f0979124f7c50d6f401b2291d9ed12adc
mikereiddigital <mike.reid@justice.gov.uk>
2026-02-19 13:37:25 +0000

    run

---
+++ terraform/environments/cooker/temp.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket_prefix = "mike-reid-testing-abc1234"
+  tags = {
+    Name        = "mike-reid-testing-abc1234"
+    Environment = "development"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logging" {
+  bucket   = aws_s3_bucket.bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
