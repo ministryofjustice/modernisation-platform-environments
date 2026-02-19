d7e8c998329d57521ec227a70b53d0fa0703ea37
Vladimirs Kovalovs <vladimirs.kovalovs1@justice.gov.uk>
2026-02-19 14:23:30 +0000

    [STB-3819] minor changes

--- terraform/environments/oas/new-alb.tf
+++ terraform/environments/oas/new-alb.tf
@@ -129,11 +129,50 @@ resource "aws_s3_bucket_lifecycle_configuration" "lb_access_logs" {
   bucket = aws_s3_bucket.lb_access_logs[0].id
 
   rule {
-    id     = "expire-old-logs"
+    id     = "main"
     status = "Enabled"
 
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
+    transition {
+      days          = 90
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 365
+      storage_class = "GLACIER"
+    }
+
     expiration {
-      days = 90
+      days = 730
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 90
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 365
+      storage_class   = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 730
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "lb_access_logs" {
+  count  = local.environment == "preproduction" ? 1 : 0
+  bucket = aws_s3_bucket.lb_access_logs[0].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
     }
   }
 }
@@ -148,6 +187,8 @@ resource "aws_s3_bucket_public_access_block" "lb_access_logs" {
   restrict_public_buckets = true
 }
 
+data "aws_elb_service_account" "default" {}
+
 resource "aws_s3_bucket_policy" "lb_access_logs" {
   count  = local.environment == "preproduction" ? 1 : 0
   bucket = aws_s3_bucket.lb_access_logs[0].id
@@ -156,21 +197,56 @@ resource "aws_s3_bucket_policy" "lb_access_logs" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "AWSLogDeliveryWrite"
-        Effect = "Allow"
+        Effect = "Deny"
         Principal = {
-          Service = "elasticloadbalancing.amazonaws.com"
+          AWS = "*"
         }
-        Action   = "s3:PutObject"
-        Resource = "${aws_s3_bucket.lb_access_logs[0].arn}/*"
+        Action = "s3:*"
+        Resource = [
+          aws_s3_bucket.lb_access_logs[0].arn,
+          "${aws_s3_bucket.lb_access_logs[0].arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
       },
       {
-        Sid    = "AWSLogDeliveryAclCheck"
         Effect = "Allow"
         Principal = {
-          Service = "elasticloadbalancing.amazonaws.com"
+          AWS = data.aws_elb_service_account.default.arn
         }
-        Action   = "s3:GetBucketAcl"
+        Action = "s3:PutObject"
+        Resource = [
+          "${aws_s3_bucket.lb_access_logs[0].arn}/${local.application_name}/AWSLogs/${local.environment_management.account_ids[terraform.workspace]}/*",
+          "${aws_s3_bucket.lb_access_logs[0].arn}/AWSLogs/${local.environment_management.account_ids[terraform.workspace]}/*"
+        ]
+      },
+      {
+        Sid = "AWSLogDeliveryWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = [
+          "${aws_s3_bucket.lb_access_logs[0].arn}/${local.application_name}/AWSLogs/${local.environment_management.account_ids[terraform.workspace]}/*",
+          "${aws_s3_bucket.lb_access_logs[0].arn}/AWSLogs/${local.environment_management.account_ids[terraform.workspace]}/*"
+        ]
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Sid = "AWSLogDeliveryAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action = "s3:GetBucketAcl"
         Resource = aws_s3_bucket.lb_access_logs[0].arn
       }
     ]
