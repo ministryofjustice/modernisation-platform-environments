a7b212d20da85ff392a35e4c0c1a6b9495f6681b
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-02-20 05:45:33 +0000

    fix: directory of cmake changed

--- terraform/environments/ccms-ebs/templates/ec2_user_data_ssogen.sh
+++ terraform/environments/ccms-ebs/templates/ec2_user_data_ssogen.sh
@@ -39,7 +39,7 @@ chmod 775 /oracle
 EFS_MOUNT_POINT=/SSOGEN
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 HOME=/root
-source"$HOME/.cargo/env"
+source "$HOME/.cargo/env"
 yum -y install git rpm-build make rust cargo openssl-devel gcc gcc-c++ cmake wget perl
 wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
