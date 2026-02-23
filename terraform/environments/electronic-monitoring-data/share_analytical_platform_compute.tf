fdf4d2300eb4131dd9ecb711c3ba0a85c626d4d5
matt heery <matt.heery@justice.gov.uk>
2026-02-23 14:39:55 +0000

    fix: add all dbt dbs to all envs

--- terraform/environments/electronic-monitoring-data/share_analytical_platform_compute.tf
+++ terraform/environments/electronic-monitoring-data/share_analytical_platform_compute.tf
@@ -48,7 +48,7 @@ locals {
     "scram_alcohol_monitoring",
   ] : local.is-development ? ["test"] : []
 
-  prod_dbs_to_grant = local.is-production ? [
+  prod_dbs_to_grant = [
     "am_stg",
     "buddi_stg",
     "buddi_buddi",
@@ -84,12 +84,13 @@ locals {
     "staged_emsys_mvp",
     "staged_emsys_tpims",
     "staged_scram_alcohol_monitoring",
-  ] : []
+  ]
 
   dev_dbs_to_grant       = local.is-production ? [for db in local.prod_dbs_to_grant : "${db}_historic_dev_dbt"] : []
+  prod_dbt_dbs_to_grant  = flatten([for db in local.prod_dbs_to_grant : "${db}${local.dbt_suffix}"], local.dev_dbs_to_grant)
   dbt_dbs_to_grant       = [for db in local.dbt_dbs : "${db}${local.dbt_suffix}"]
   live_feed_dbs_to_grant = [for db in local.live_feeds_dbs : "${db}${local.db_suffix}"]
-  dbs_to_grant           = toset(flatten([local.prod_dbs_to_grant, local.dev_dbs_to_grant, local.dbt_dbs_to_grant]))
+  dbs_to_grant           = toset(flatten([local.dev_dbs_to_grant, local.dbt_dbs_to_grant]))
 
 
   existing_dbs_to_grant = toset(flatten([local.live_feed_dbs_to_grant, local.historic_source_dbs]))
