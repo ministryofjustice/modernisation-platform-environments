7614e911a52662fcf7ac8cf5fe3710bba7079ffd
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-02-25 15:07:39 +0000

    fix: efs mountpoint fixed

--- terraform/environments/ccms-ebs/ssogen-r53.tf
+++ terraform/environments/ccms-ebs/ssogen-r53.tf
@@ -36,25 +36,25 @@ data "aws_instance" "ssogen_secondary_details" {
   }
 }
 
-  resource "aws_route53_record" "ssogen_primary" {
-    count    = local.is-development || local.is-test ? 1 : 0
-    provider = aws.core-vpc
-    zone_id  = data.aws_route53_zone.external.zone_id
-    #name    = "ccms-ebs-db.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-    name    = "ccms-${local.application_name_ssogen}-as1.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-    type    = "A"
-    ttl     = 300
-    records = [data.aws_instance.ssogen_primary_details[count.index].private_ip]
-  }
+resource "aws_route53_record" "ssogen_primary" {
+  count    = local.is-development || local.is-test ? 1 : 0
+  provider = aws.core-vpc
+  zone_id  = data.aws_route53_zone.external.zone_id
+  #name    = "ccms-ebs-db.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "ccms-${local.application_name_ssogen}-as1.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type    = "A"
+  ttl     = 300
+  records = [data.aws_instance.ssogen_primary_details[count.index].private_ip]
+}
 
-  resource "aws_route53_record" "ssogen_secondary" {
-    count    = local.is-development || local.is-test ? 1 : 0
-    provider = aws.core-vpc
-    zone_id  = data.aws_route53_zone.external.zone_id
-    #name    = "ccms-ebs-db.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-    name    = "ccms-${local.application_name_ssogen}-as2.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-    type    = "A"
-    ttl     = 300
-    records = [data.aws_instance.ssogen_secondary_details[count.index].private_ip]
-  }
+resource "aws_route53_record" "ssogen_secondary" {
+  count    = local.is-development || local.is-test ? 1 : 0
+  provider = aws.core-vpc
+  zone_id  = data.aws_route53_zone.external.zone_id
+  #name    = "ccms-ebs-db.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  name    = "ccms-${local.application_name_ssogen}-as2.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type    = "A"
+  ttl     = 300
+  records = [data.aws_instance.ssogen_secondary_details[count.index].private_ip]
+}
   
