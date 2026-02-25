bd9959fa156498dfa52cccdd09acb0e5e070eb41
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-02-25 13:23:33 +0000

    feat: update Prometheus module configuration and tidy up logging settings
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/cluster/prometheus.tf
+++ terraform/environments/data-platform/cluster/prometheus.tf
@@ -4,9 +4,10 @@ module "prometheus" {
   workspace_alias = local.eks_cluster_name
   kms_key_arn     = module.prometheus_kms_key.key_arn
 
+  #   retention_period_in_days = 365 # Defaults to 150 days, but this setting is linked to `limits_per_label_set` and I don't think we want that. If we want to set this, we need to use a separate aws_prometheus_workspace_configuration resource
+
   logging_configuration = {
     create_log_group = false
-    log_group_arn = "${module.prometheus_log_group.cloudwatch_log_group_arn}:*"
+    log_group_arn    = "${module.prometheus_log_group.cloudwatch_log_group_arn}:*"
   }
 }
-
