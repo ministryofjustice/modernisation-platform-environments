f1bea3eeb265ac0ae09e919bba27ebc979d78074
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2026-02-26 21:25:59 +0000

    added env config for sandbox airflow

--- terraform/environments/analytical-platform-compute/airflow/locals.tf
+++ terraform/environments/analytical-platform-compute/airflow/locals.tf
@@ -1,5 +1,7 @@
 locals {
   eks_cluster_name = "${local.application_name}-${local.environment}"
+  # Sandbox Airflow will use development networking resources.
+  networking_environment = local.environment == "sandbox" ? "development" : local.environment
 
   /* Environment Configuration */
   environment_configuration = local.environment_configurations[local.environment]
