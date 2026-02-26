dcf845d2498a47d6d58cb0a96553116ae63c6161
Vladimirs Kovalovs <vladimirs.kovalovs1@justice.gov.uk>
2026-02-26 09:56:36 +0000

    changes to oas lb

--- terraform/environments/oas/new-alb.tf
+++ terraform/environments/oas/new-alb.tf
@@ -462,7 +462,7 @@ resource "aws_lb_listener_rule" "em_9500_rule" {
   }
 }
 
-# HTTP Listener on port 9502 for Analytics and Data Visualization (redirects to HTTPS)
+# HTTP Listener on port 9502 for Analytics and Data Visualization
 resource "aws_lb_listener" "http_9502_listener" {
   count = local.environment == "preproduction" ? 1 : 0
 
@@ -471,11 +471,83 @@ resource "aws_lb_listener" "http_9502_listener" {
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.oas_analytics_target_group[0].arn
+  }
+}
+
+# Listener rule for /analytics on port 9502
+resource "aws_lb_listener_rule" "analytics_9502_rule" {
+  count = local.environment == "preproduction" ? 1 : 0
+
+  listener_arn = aws_lb_listener.http_9502_listener[0].arn
+  priority     = 200
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.oas_analytics_target_group[0].arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/analytics*"]
+    }
+  }
+}
+
+# Listener rule for /dv on port 9502
+resource "aws_lb_listener_rule" "dv_9502_rule" {
+  count = local.environment == "preproduction" ? 1 : 0
+
+  listener_arn = aws_lb_listener.http_9502_listener[0].arn
+  priority     = 210
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.oas_analytics_target_group[0].arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/dv*"]
+    }
+  }
+}
+
+# Listener rule for /bi-security-login on port 9502
+resource "aws_lb_listener_rule" "bi_security_login_9502_rule" {
+  count = local.environment == "preproduction" ? 1 : 0
+
+  listener_arn = aws_lb_listener.http_9502_listener[0].arn
+  priority     = 220
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.oas_analytics_target_group[0].arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/bi-security-login*"]
+    }
+  }
+}
+
+# Listener rule for /static on port 9502
+resource "aws_lb_listener_rule" "static_9502_rule" {
+  count = local.environment == "preproduction" ? 1 : 0
+
+  listener_arn = aws_lb_listener.http_9502_listener[0].arn
+  priority     = 230
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.oas_analytics_target_group[0].arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/static*"]
     }
   }
 }
