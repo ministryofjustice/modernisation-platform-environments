db5bdb01ad150c90a1d11a8d8873f2564dfdc6c0
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-02-27 08:50:29 +0000

    TM-1915: onr: revert test pointing legacy URL at ALB

--- terraform/environments/oasys/locals_preproduction.tf
+++ terraform/environments/oasys/locals_preproduction.tf
@@ -319,8 +319,7 @@ locals {
       }
       "pp-oasys.az.justice.gov.uk" = {
         records = [
-          # { name = "onr", type = "A", ttl = "300", records = ["10.40.40.210"] }
-          { name = "onr", type = "A", ttl = "300", records = ["13.43.251.60"] }
+          { name = "onr", type = "A", ttl = "300", records = ["10.40.40.210"] }
         ]
         lb_alias_records = [
           { name = "", type = "A", lbs_map_key = "private" }
