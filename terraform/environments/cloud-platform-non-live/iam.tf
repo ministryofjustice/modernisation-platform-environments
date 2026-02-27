9335b2aedd5c542318a655fe95d18d6438766dcb
Ewa Stempel <ewa.stempel@justice.gov.uk>
2026-02-27 12:46:23 +0000

    Testing IAM user creation

---
+++ terraform/environments/cloud-platform-non-live/iam.tf
@@ -0,0 +1,45 @@
+#### IAM resources for this member account ####
+
+variable "admin_iam_user_name" {
+  description = "Optional: create an IAM user with AdministratorAccess in this member account. Leave null to disable."
+  type        = string
+  default     = null
+}
+
+resource "aws_iam_user" "admin" {
+  count = var.admin_iam_user_name == null ? 0 : 1
+
+  name          = var.admin_iam_user_name
+  force_destroy = true
+}
+
+resource "aws_iam_user_policy_attachment" "admin" {
+  count = var.admin_iam_user_name == null ? 0 : 1
+
+  user       = aws_iam_user.admin[0].name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_access_key" "admin" {
+  count = var.admin_iam_user_name == null ? 0 : 1
+
+  user = aws_iam_user.admin[0].name
+}
+
+output "admin_iam_user_name" {
+  description = "IAM admin username (if created)."
+  value       = var.admin_iam_user_name
+}
+
+output "admin_iam_access_key_id" {
+  description = "Access key id for the created IAM admin user (if created)."
+  value       = try(aws_iam_access_key.admin[0].id, null)
+  sensitive   = true
+}
+
+output "admin_iam_secret_access_key" {
+  description = "Secret access key for the created IAM admin user (if created)."
+  value       = try(aws_iam_access_key.admin[0].secret, null)
+  sensitive   = true
+}
+
