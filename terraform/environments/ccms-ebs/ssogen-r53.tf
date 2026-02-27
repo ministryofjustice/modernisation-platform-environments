94de55d8c9680af446d50cb0804398dcdd7af9a5
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-02-27 15:06:00 +0000

    fix: ssogen primary ip added

--- terraform/environments/ccms-ebs/ssogen-r53.tf
+++ terraform/environments/ccms-ebs/ssogen-r53.tf
@@ -81,41 +81,41 @@ resource "aws_route53_record" "ssogen_admin_primary" {
   #name    = "ccms-ebs-db.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   name           = "ccms-${local.application_name_ssogen}-admin.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
   type           = "A"
-  set_identifier = "primary"
-  failover_routing_policy {
-    type = "PRIMARY"
-  }
-  health_check_id = aws_route53_health_check.primary_hc[count.index].id
+  # set_identifier = "primary"
+  # failover_routing_policy {
+  #   type = "PRIMARY"
+  # }
+  # health_check_id = aws_route53_health_check.primary_hc[count.index].id
   ttl             = 300
   records         = [data.aws_instance.ssogen_primary_details[count.index].private_ip]
 }
 
-resource "aws_route53_record" "ssogen_admin_secondary" {
-  count    = local.is-development || local.is-test ? 1 : 0
-  provider = aws.core-vpc
-  zone_id  = data.aws_route53_zone.external.zone_id
-  #name    = "ccms-ebs-db.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-  name           = "ccms-${local.application_name_ssogen}-admin.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-  type           = "A"
-  set_identifier = "secondary"
-  failover_routing_policy {
-    type = "SECONDARY"
-  }
-  health_check_id = aws_route53_health_check.secondary_hc[count.index].id
-  ttl             = 300
-  records         = [data.aws_instance.ssogen_secondary_details[count.index].private_ip]
-}
+# resource "aws_route53_record" "ssogen_admin_secondary" {
+#   count    = local.is-development || local.is-test ? 1 : 0
+#   provider = aws.core-vpc
+#   zone_id  = data.aws_route53_zone.external.zone_id
+#   #name    = "ccms-ebs-db.${var.networking[0].application}.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+#   name           = "ccms-${local.application_name_ssogen}-admin.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+#   type           = "A"
+#   # set_identifier = "secondary"
+#   # failover_routing_policy {
+#   #   type = "SECONDARY"
+#   # }
+#   health_check_id = aws_route53_health_check.secondary_hc[count.index].id
+#   ttl             = 300
+#   records         = [data.aws_instance.ssogen_secondary_details[count.index].private_ip]
+# }
 
-resource "aws_route53_health_check" "primary_hc" {
-  count = local.is-development || local.is-test ? 1 : 0
-  fqdn  = data.aws_instance.ssogen_primary_details[count.index].private_ip
-  port  = 7001
-  type  = "TCP"
-}
+# resource "aws_route53_health_check" "primary_hc" {
+#   count = local.is-development || local.is-test ? 1 : 0
+#   fqdn  = data.aws_instance.ssogen_primary_details[count.index].private_ip
+#   port  = 7001
+#   type  = "TCP"
+# }
 
-resource "aws_route53_health_check" "secondary_hc" {
-  count = local.is-development || local.is-test ? 1 : 0
-  fqdn  = data.aws_instance.ssogen_secondary_details[count.index].private_ip
-  port  = 7001
-  type  = "TCP"
-}
+# resource "aws_route53_health_check" "secondary_hc" {
+#   count = local.is-development || local.is-test ? 1 : 0
+#   fqdn  = data.aws_instance.ssogen_secondary_details[count.index].private_ip
+#   port  = 7001
+#   type  = "TCP"
+# }
