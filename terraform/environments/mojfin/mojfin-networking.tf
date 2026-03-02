658c292b24e60531525291d8d23f098f34ed0a95
mnasr-moj <mohamed.nasr@justice.gov.uk>
2026-03-02 12:50:42 +0000

    STB-3842, Giving MOJO devices access to MOJFIN Prod DB through Port 1521

--- terraform/environments/mojfin/mojfin-networking.tf
+++ terraform/environments/mojfin/mojfin-networking.tf
@@ -1,3 +1,36 @@
+locals {
+  mojfin_allowed_cidrs = local.environment == "production" ? [
+    "51.149.249.0/29",
+    "194.33.249.0/29",
+    "51.149.249.32/29",
+    "194.33.248.0/29",
+    "128.77.75.64/26",
+    "20.49.214.199/32",
+    "20.49.214.228/32",
+    "20.26.11.71/32",
+    "20.26.11.108/32",
+    "18.169.147.172/32",
+    "35.176.93.186/32",
+    "18.130.148.126/32",
+    "35.176.148.126/32",
+    "13.41.38.176/32",
+    "3.11.197.133/32",
+    "3.8.81.175/32",
+    "13.42.163.245/32",
+    "13.43.9.198/32",
+    "18.132.208.127/32",
+    "35.178.209.113/32",
+    "3.8.51.207/32",
+    "35.177.252.54/32",
+    "35.176.127.232/32",
+    "18.130.39.94/32",
+    "35.177.145.193/32",
+    "35.176.254.38/32",
+    "52.56.212.11/32",
+    "35.177.173.197/32",
+    "10.148.0.0/14",
+  ] : []
+}
 resource "aws_db_subnet_group" "mojfin" {
   name       = "${local.application_name}-${local.environment}-subnetgrp"
   subnet_ids = [data.aws_subnet.data_subnets_a.id, data.aws_subnet.data_subnets_b.id, data.aws_subnet.data_subnets_c.id]
@@ -97,8 +130,40 @@ resource "aws_security_group" "mojfin" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  dynamic "ingress" {
+    for_each = local.environment == "production" ? [1] : []
+    content {
+      description     = "Custom IP prefix list for MOJO devices - only applied in production"
+      from_port       = 1521
+      to_port         = 1521
+      protocol        = "tcp"
+      prefix_list_ids = [aws_ec2_managed_prefix_list.mojfin_allowed_ips.id]
+    }
+  }
+
   tags = merge(
     local.tags,
     { "Name" = "${local.application_name}-${local.environment}-mojfin" }
+  )
+}
+
+# managed prefix list containing the external addresses the mojfin SG should allow
+resource "aws_ec2_managed_prefix_list" "mojfin_allowed_ips" {
+  count          = local.environment == "production" ? 1 : 0
+  name           = "${local.application_name}-${local.environment}-mojo-allowed-ips"
+  address_family = "IPv4"
+  max_entries    = length(local.mojfin_allowed_cidrs)
+
+  dynamic "entry" {
+    for_each = local.mojfin_allowed_cidrs
+    content {
+      cidr        = entry.value
+      description = "allowed address"
+    }
+  }
+
+  tags = merge(
+    local.tags,
+    { Name = "${local.application_name}-${local.environment}-allowed-ips" }
   )
 }
