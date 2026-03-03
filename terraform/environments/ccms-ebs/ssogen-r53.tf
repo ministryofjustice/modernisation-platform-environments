7521822f2d27039c584034d1b7dabbfb61ce209b
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-03 09:55:07 +0000

    fix: open 7001 within ec2 instance

--- terraform/environments/ccms-ebs/ssogen-r53.tf
+++ terraform/environments/ccms-ebs/ssogen-r53.tf
@@ -56,35 +56,35 @@ resource "aws_route53_record" "ssogen_primary" {
   count    = local.is-development || local.is-test ? 1 : 0
   provider = aws.core-vpc
   zone_id  = data.aws_route53_zone.external.zone_id
-  name    = "ccms-${local.application_name_ssogen}-as1.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-  type    = "A"
-  ttl     = 300
-  records = [data.aws_instance.ssogen_primary_details[count.index].private_ip]
+  name     = "ccms-${local.application_name_ssogen}-as1.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type     = "A"
+  ttl      = 300
+  records  = [data.aws_instance.ssogen_primary_details[count.index].private_ip]
 }
 
 resource "aws_route53_record" "ssogen_secondary" {
   count    = local.is-development || local.is-test ? 1 : 0
   provider = aws.core-vpc
   zone_id  = data.aws_route53_zone.external.zone_id
-  name    = "ccms-${local.application_name_ssogen}-as2.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-  type    = "A"
-  ttl     = 300
-  records = [data.aws_instance.ssogen_secondary_details[count.index].private_ip]
+  name     = "ccms-${local.application_name_ssogen}-as2.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type     = "A"
+  ttl      = 300
+  records  = [data.aws_instance.ssogen_secondary_details[count.index].private_ip]
 }
 
 resource "aws_route53_record" "ssogen_admin_primary" {
   count    = local.is-development || local.is-test ? 1 : 0
   provider = aws.core-vpc
   zone_id  = data.aws_route53_zone.external.zone_id
-  name           = "ccms-${local.application_name_ssogen}-admin.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
-  type           = "A"
+  name     = "ccms-${local.application_name_ssogen}-admin.${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+  type     = "A"
   # set_identifier = "primary"
   # failover_routing_policy {
   #   type = "PRIMARY"
   # }
   # health_check_id = aws_route53_health_check.primary_hc[count.index].id
-  ttl             = 300
-  records         = [data.aws_instance.ssogen_primary_details[count.index].private_ip]
+  ttl     = 300
+  records = [data.aws_instance.ssogen_primary_details[count.index].private_ip]
 }
 
 # resource "aws_route53_record" "ssogen_admin_secondary" {
