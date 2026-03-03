9d32540204bfdc6eadf3ebd28948d010ad76b405
sourish-banerjee-laa <sourish.banerjee@justice.gov.uk>
2026-03-03 11:16:46 +0000

    add LB listner rule for /biinfer

--- terraform/environments/oas/new-alb.tf
+++ terraform/environments/oas/new-alb.tf
@@ -687,12 +687,31 @@ resource "aws_lb_listener_rule" "bi_security_login_https_rule" {
   }
 }
 
+# Listener rule for /biinfer on HTTPS
+resource "aws_lb_listener_rule" "/biinfer_login_https_rule" {
+  count = local.environment == "preproduction" ? 1 : 0
+
+  listener_arn = aws_lb_listener.https_listener[0].arn
+  priority     = 230
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.oas_analytics_target_group[0].arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/biinfer*"]
+    }
+  }
+}
+
 # Listener rule for /static on HTTPS
 resource "aws_lb_listener_rule" "static_https_rule" {
   count = local.environment == "preproduction" ? 1 : 0
 
   listener_arn = aws_lb_listener.https_listener[0].arn
-  priority     = 230
+  priority     = 240
 
   action {
     type             = "forward"
