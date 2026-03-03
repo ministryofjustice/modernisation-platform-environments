202b32ccb5ee54844e1cfe53a58f158ce3f9578c
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2026-03-03 14:05:13 +0000

    internal-airflow initial changes

---
+++ terraform/environments/analytical-platform-compute/internal-airflow/data.tf
@@ -0,0 +1,12 @@
+# data call for:
+# kms key for s3 bucket encryption
+# mwaa_execution_iam_role ?
+# airflow_requirements_object
+# airflow_kube_config_object
+# airflow_plugins_object
+# mwaa_ses_iam_user
+# mwaa_security_group
+# data.aws_subnet.apc_private_subnet_a.id
+# data.aws_subnet.apc_private_subnet_b.id
+# data "aws_eks_cluster" "apc_cluster"
+
