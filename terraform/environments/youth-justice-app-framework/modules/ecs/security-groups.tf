daa2a271a374e3b32805037dc56ebe32e2684da7
ttipler <thomas.tipler@necsws.com>
2026-03-04 17:27:13 +0000

    fix sg variable

--- terraform/environments/youth-justice-app-framework/modules/ecs/security-groups.tf
+++ terraform/environments/youth-justice-app-framework/modules/ecs/security-groups.tf
@@ -131,7 +131,7 @@ resource "aws_security_group_rule" "connectivity_alb_to_ecs_internal_rule" {
 
 #yjsm-hub-svc external to ecs
 resource "aws_security_group_rule" "yjsm_hub_svc_alb_to_ecs_external_rule" {
-  count                    = var.yjsm_hub_svc_alb_security_group_id != null ? 1 : 0
+  count                    = count = var.create_svc_pilot ? 1 : 0
   type                     = "ingress"
   from_port                = 8080
   to_port                  = 8080
