fe2bb457f739286a54ec879120ecf300994340b9
mikereiddigital <mike.reid@justice.gov.uk>
2026-03-04 14:19:27 +0000

    removes s3.tf

--- terraform/environments/cooker/s3.tf
+++
@@ -1,7 +0,0 @@
-data "aws_s3_bucket" "show_tell_a" {
-  bucket = "show-tell-4c1f14-replication-84a84d4e"
-}
-
-data "aws_s3_bucket" "show_tell_b" {
-  bucket = "show-tell-4c1f14-replication-locked-23476f3f"
-}
