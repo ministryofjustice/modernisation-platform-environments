7a3177c2c76d54c6871520bd8c3a941941c8d27b
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-04 22:02:01 +0000

    fix: export options enabled

--- terraform/environments/ccms-ebs/ccms-certificates.tf
+++ terraform/environments/ccms-ebs/ccms-certificates.tf
@@ -11,6 +11,9 @@ resource "aws_acm_certificate" "external" {
   domain_name               = local.primary_domain
   subject_alternative_names = local.subject_alternative_names
 
+  options {
+    export = ENABLED
+  }
   tags = merge(local.tags,
     { Environment = local.environment }
   )
