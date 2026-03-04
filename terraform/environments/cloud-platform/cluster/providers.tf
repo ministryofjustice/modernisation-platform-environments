050d151679f15082ebc88c180ef0baf5a10c5874
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-03-04 10:30:51 +0000

    Including helm provider

--- terraform/environments/cloud-platform/cluster/providers.tf
+++ terraform/environments/cloud-platform/cluster/providers.tf
@@ -10,13 +10,14 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes = {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
