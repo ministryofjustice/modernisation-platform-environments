f1cbfa81b14df330b34624be394a76e1641a4eab
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-04 21:37:32 +0000

    fix: disable ebsdbnlb and ebs app listener

--- terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_db-nlb.tf
+++ terraform/environments/ccms-ebs/ccms-ec2-oracle_ebs_db-nlb.tf
@@ -38,16 +38,16 @@ resource "aws_lb_target_group_attachment" "ebsdb" {
   port             = local.application_data.accounts[local.environment].tg_db_port
 }
 
-resource "aws_lb_listener" "ebsdbnlb_listener" {
-  load_balancer_arn = aws_lb.ebsdb_nlb.id
-  port              = 443
-  protocol          = "TLS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
-  certificate_arn   = aws_acm_certificate.external.arn
-  default_action {
-    target_group_arn = aws_lb_target_group.ebsdb_nlb.id
-    type             = "forward"
-  }
+# resource "aws_lb_listener" "ebsdbnlb_listener" {
+#   load_balancer_arn = aws_lb.ebsdb_nlb.id
+#   port              = 443
+#   protocol          = "TLS"
+#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
+#   certificate_arn   = aws_acm_certificate.external.arn
+#   default_action {
+#     target_group_arn = aws_lb_target_group.ebsdb_nlb.id
+#     type             = "forward"
+#   }
 
-  depends_on = [aws_acm_certificate_validation.external_nonprod, aws_acm_certificate_validation.external_prod]
-}
+#   depends_on = [aws_acm_certificate_validation.external_nonprod, aws_acm_certificate_validation.external_prod]
+# }
