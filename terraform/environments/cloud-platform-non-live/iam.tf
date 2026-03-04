5e431d2b30e5f97aefb6c0ea101bdbc4e243ffb3
ewastempel <ewa.stempel@justice.gov.uk>
2026-03-04 12:32:14 +0000

    Introducing tf destroy

--- terraform/environments/cloud-platform-non-live/iam.tf
+++ terraform/environments/cloud-platform-non-live/iam.tf
@@ -1,4 +1,4 @@
-#### IAM resources for this member account ####
+#### IAM resources for this member accounts ####
 resource "aws_iam_user" "admin" {
   count = local.application_data.accounts[local.environment].admin_iam_user_name == null ? 0 : 1
 
@@ -22,16 +22,4 @@ resource "aws_iam_access_key" "admin" {
 output "admin_iam_user_name" {
   description = "IAM admin username (if created)."
   value       = local.application_data.accounts[local.environment].admin_iam_user_name
-}
-
-output "admin_iam_access_key_id" {
-  description = "Access key id for the created IAM admin user (if created)."
-  value       = try(aws_iam_access_key.admin[0].id, null)
-  sensitive   = true
-}
-
-output "admin_iam_secret_access_key" {
-  description = "Secret access key for the created IAM admin user (if created)."
-  value       = try(aws_iam_access_key.admin[0].secret, null)
-  sensitive   = true
 }
