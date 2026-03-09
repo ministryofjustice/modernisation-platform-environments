d79b57ecce8cb571adb8b89fd41f38cb67961234
matthew-sharpe-mt <matthew.sharpe2@justice.gov.uk>
2026-03-09 10:53:13 +0000

    add 404 error support

--- terraform/environments/electronic-monitoring-data/modules/api_step_function/main.tf
+++ terraform/environments/electronic-monitoring-data/modules/api_step_function/main.tf
@@ -1,7 +1,6 @@
 locals {
   camel_case_api_name  = join("", [for word in split("_", var.api_name) : title(word)])
   sync                 = var.sfn_type == "express" ? "Sync" : ""
-  sfn_name             = trimsuffix(var.api_name, "_api")
   # Return a parsed response dependent on if sfn is standard or express type
   express_response  = <<EOF
 #set ($parsedPayload = $util.parseJson($input.json('$.output')))
@@ -171,7 +170,7 @@ resource "aws_api_gateway_integration" "status_integration" {
   request_templates = {
     "application/json" = <<EOF
 {
-  "executionArn": "arn:aws:states:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:execution:${local.sfn_name}:$input.params('execution_id')"
+  "executionArn": "${replace(var.step_function.arn, "stateMachine", "execution")}:$input.params('execution_id')"
 }
 EOF
   }
@@ -243,6 +242,13 @@ resource "aws_api_gateway_method_response" "status_200" {
   status_code = "200"
 }
 
+resource "aws_api_gateway_method_response" "status_404" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.execution_id.id
+  http_method = aws_api_gateway_method.get_status.http_method
+  status_code = "404"
+}
+
 resource "aws_api_gateway_integration_response" "integration_response_200" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   resource_id = aws_api_gateway_resource.resource.id
@@ -266,17 +272,28 @@ resource "aws_api_gateway_integration_response" "status_integration_response" {
   response_templates = {
     "application/json" = <<EOF
 #set($input = $input.path('$'))
-{
-  "execution_id": "$input.name",
-  "status": "$input.status",
-  "startDate": "$input.startDate",
-  #if($input.status == "SUCCEEDED")
-    "output": $input.output
-  #elseif($input.status == "FAILED")
-    "error": "$input.error",
-    "cause": "$input.cause"
-  #end
-}
+#if($input.get("__type").contains("Execution Does Not Exist"))
+    #set($context.responseOverride.status = 404)
+    {
+      "error": "NotFound",
+      "cause": "The Step Function execution with the provided ID does not exist.",
+      "execution_id": "$input.params('execution_id')"
+    }
+#else
+    {
+      "execution_id": "$input.name",
+      "status": "$input.status",
+      "startDate": "$input.startDate",
+      #if($input.status == "SUCCEEDED")
+        "output": $input.output
+      #elseif($input.status == "FAILED")
+        "error": "$input.error",
+        "cause": "$input.cause"
+      #else
+        "output": "Execution is currently $input.status"
+      #end
+    }
+#end
 EOF
   }
   depends_on = [
