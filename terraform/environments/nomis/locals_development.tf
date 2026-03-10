06df64b1f9026627c2aad881455af70aecedabf6
ghill26 <George.Hill2@justice.gov.uk>
2026-03-10 10:05:25 +0000

    TAGSAR: Set branch for testing

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -171,7 +171,7 @@ locals {
         })
         user_data_cloud_init = merge(local.ec2_autoscaling_groups.web12.user_data_cloud_init, {
           args = merge(local.ec2_autoscaling_groups.web12.user_data_cloud_init.args, {
-            branch = "main"
+            branch = "nomis-12-tagsar-s3"
           })
         })
         tags = merge(local.ec2_autoscaling_groups.web12.tags, {
