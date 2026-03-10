4ff1f9ea903e4c76b8532a0ec28548031b2825a1
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-03-10 12:01:16 +0000

    add kubectl provider

--- terraform/environments/cloud-platform/cluster/providers.tf
+++ terraform/environments/cloud-platform/cluster/providers.tf
@@ -23,15 +23,13 @@ provider "helm" {
 }
 
 provider "kubectl" {
-  kubernetes = {
-    host                   = module.eks[0].cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data)
+  host                   = module.eks[0].cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data)
 
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks[0].cluster_name]
-    }
+  exec = {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks[0].cluster_name]
   }
 }
