59ee063e3ff010caf284f66aec5c352ab60a02db
p-sin <philip.sinfield@justice.gov.uk>
2026-03-10 15:17:12 +0000

    Add more scan infra

--- terraform/environments/genesys-call-centre-data/lamdbas_triggers.tf
+++ terraform/environments/genesys-call-centre-data/lamdbas_triggers.tf
@@ -1,14 +1,14 @@
 module "virus_scan_file_sqs" {
   source               = "./modules/sqs_s3_lambda_trigger"
-  bucket               = module.s3-received-files-bucket.bucket
+  bucket               = module.s3_bucket_staging.bucket
   lambda_function_name = module.virus_scan_file.lambda_function_name
-  bucket_prefix        = local.bucket_prefix
+  bucket_prefix        = ""
   maximum_concurrency  = 1000
 }
 
 
 resource "aws_s3_bucket_notification" "virus_scan_file" {
-  bucket = module.s3-received-files-bucket.bucket.id
+  bucket = module.s3_bucket_staging.bucket.id
 
   queue {
     queue_arn = module.virus_scan_file_sqs.sqs_queue.arn
--- terraform/environments/genesys-call-centre-data/s3.tf
+++ terraform/environments/genesys-call-centre-data/s3.tf
@@ -41,19 +41,6 @@ module "s3_bucket_logs" {
   tags = local.tags
 }
 
-# tfsec:ignore:aws-s3-enable-logging - Logging is enabled on the bucket in the next resource
-module "s3_bucket_staging" {
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=11707a540d9ced11f8df4a8ed1547753dd3a0b7d"
-
-  providers = {
-    aws.bucket-replication = aws
-  }
-  bucket_prefix      = "call-centre-staging-"
-  versioning_enabled = true
-  ownership_controls = "BucketOwnerEnforced"
-
-  tags = local.tags
-}
 
 #tfsec:ignore:aws-s3-enable-logging - Logging is enabled on the bucket in the next resource
 module "s3_buckets" {
@@ -72,6 +59,42 @@ module "s3_buckets" {
   tags = local.tags
 }
 
+# tfsec:ignore:aws-s3-enable-logging - Logging is enabled on the bucket in the next resource
+module "s3_bucket_staging" {
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=11707a540d9ced11f8df4a8ed1547753dd3a0b7d"
+
+  providers = {
+    aws.bucket-replication = aws
+  }
+  bucket_prefix      = "call-centre-staging-"
+  versioning_enabled = true
+  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
+  ownership_controls = "BucketOwnerEnforced"
+  acl                = "private"
+
+  replication_enabled = false
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      expiration = {
+        days = 90
+      }
+    }
+  ]
+
+
+  tags = local.tags
+}
+
 # Enable bucket server logging for the staging bucket
 resource "aws_s3_bucket_logging" "staging_bucket_logging" {
   bucket = module.s3_bucket_staging.bucket.id
@@ -80,27 +103,87 @@ resource "aws_s3_bucket_logging" "staging_bucket_logging" {
   target_prefix = "call-centre-staging"
 }
 
-# Data source for the member-access IAM role
-# This is created by the mod platform team
-data "aws_iam_role" "guardduty_malware_protection_role" {
-  name = "GuardDutyS3MalwareProtectionRole"
+
+module "s3-quarantine-files-bucket" {
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=11707a540d9ced11f8df4a8ed1547753dd3a0b7d"
+
+  bucket_prefix      = "call-centre-quarantined-"
+  versioning_enabled = true
+
+  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
+  ownership_controls = "BucketOwnerEnforced"
+  acl                = "private"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      expiration = {
+        days = 90
+      }
+    }
+  ]
+
+  tags = local.tags
 }
 
-# Create guardduty malware protection plan for the staging bucket
-resource "aws_guardduty_malware_protection_plan" "s3_bucket_staging" {
-  role = data.aws_iam_role.guardduty_malware_protection_role.arn
+module "s3-clamav-definitions-bucket" {
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=11707a540d9ced11f8df4a8ed1547753dd3a0b7d"
 
-  protected_resource {
-    s3_bucket {
-      bucket_name = module.s3_bucket_staging.bucket.id
-    }
+  bucket_prefix      = "call-centre-clamav-definitions-"
+  versioning_enabled = true
+
+  # to disable ACLs in preference of BucketOwnership controls as per https://aws.amazon.com/blogs/aws/heads-up-amazon-s3-security-changes-are-coming-in-april-of-2023/ set:
+  ownership_controls = "BucketOwnerEnforced"
+  acl                = "private"
+
+  # Refer to the below section "Replication" before enabling replication
+  replication_enabled = false
+  # Below variable and providers configuration is only relevant if 'replication_enabled' is set to true
+  # replication_region                       = "eu-west-2"
+  providers = {
+    # Here we use the default provider Region for replication. Destination buckets can be within the same Region as the
+    # source bucket. On the other hand, if you need to enable cross-region replication, please contact the Modernisation
+    # Platform team to add a new provider for the additional Region.
+    # Leave this provider block in even if you are not using replication
+    aws.bucket-replication = aws
   }
 
-  actions {
-    tagging {
-      status = "ENABLED"
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      expiration = {
+        days = 90
+      }
     }
-  }
+  ]
 
   tags = local.tags
 }
