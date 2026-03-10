8fae389a24cfb7543448b1bd972a2a036bd3b527
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-03-10 11:52:20 +0000

    updating provider

--- terraform/environments/cloud-platform/cluster/providers.tf
+++ terraform/environments/cloud-platform/cluster/providers.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks[0].cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
