415cd773a30ba599c7b78c4ddae9886fbf7533b5
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-11 17:00:01 +0000

    fix: lambda function added for failover

--- terraform/environments/ccms-ebs/ssogen-topic.tf
+++ terraform/environments/ccms-ebs/ssogen-topic.tf
@@ -1,55 +1,55 @@
-# resource "aws_sns_topic" "ssogen_admin_dns_flip_topic" {
-#   name              = "ssogen-admin-dns-flip-topic"
-#   delivery_policy   = <<EOF
-# {
-#   "http": {
-#     "defaultHealthyRetryPolicy": {
-#       "minDelayTarget": 20,
-#       "maxDelayTarget": 20,
-#       "numRetries": 3,
-#       "numMaxDelayRetries": 0,
-#       "numNoDelayRetries": 0,
-#       "numMinDelayRetries": 0,
-#       "backoffFunction": "linear"
-#     },
-#     "disableSubscriptionOverrides": false,
-#     "defaultRequestPolicy": {
-#       "headerContentType": "text/plain; charset=UTF-8"
-#     }
-#   }
-# }
-# EOF
-#   policy            = data.aws_iam_policy_document.ssogen_admin_dns_flip_topic_policy.json
-#   kms_master_key_id = aws_kms_key.ssogen_kms_key[count.index].arn
-#   tags = merge(local.tags,
-#     { Name = "ssogen-admin-dns-flip-topic" }
-#   )
-# }
+resource "aws_sns_topic" "ssogen_admin_dns_flip_topic" {
+  name              = "ssogen-admin-dns-flip-topic"
+  delivery_policy   = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultRequestPolicy": {
+      "headerContentType": "text/plain; charset=UTF-8"
+    }
+  }
+}
+EOF
+  policy            = data.aws_iam_policy_document.ssogen_admin_dns_flip_topic_policy.json
+  kms_master_key_id = aws_kms_key.ssogen_kms_key[count.index].arn
+  tags = merge(local.tags,
+    { Name = "ssogen-admin-dns-flip-topic" }
+  )
+}
 
-# # S3 SNS -> Lambda (Slack) instead of email
-# resource "aws_sns_topic_subscription" "ssogen_admin_dns_flip_subscription" {
-#   topic_arn = aws_sns_topic.ssogen_admin_dns_flip_topic.arn
-#   protocol  = "lambda"
-#   endpoint  = aws_lambda_function.ssogen_admin_dns_flip.arn
-# }
+# S3 SNS -> Lambda (Slack) instead of email
+resource "aws_sns_topic_subscription" "ssogen_admin_dns_flip_subscription" {
+  topic_arn = aws_sns_topic.ssogen_admin_dns_flip_topic.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.ssogen_admin_dns_flip.arn
+}
 
-# data "aws_iam_policy_document" "ssogen_admin_dns_flip_topic_policy" {
-#   version = "2012-10-17"
-#   statement {
-#     sid    = "EventsAllowPublishSnsTopic"
-#     effect = "Allow"
-#     actions = [
-#       "sns:Publish",
-#     ]
-#     resources = [
-#       aws_sns_topic.ssogen_admin_dns_flip_topic.arn
-#     ]
-#     principals {
-#       type = "Service"
-#       identifiers = [
-#         "cloudwatch.amazonaws.com",
-#       ]
-#     }
-#   }
+data "aws_iam_policy_document" "ssogen_admin_dns_flip_topic_policy" {
+  version = "2012-10-17"
+  statement {
+    sid    = "EventsAllowPublishSnsTopic"
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+    ]
+    resources = [
+      aws_sns_topic.ssogen_admin_dns_flip_topic.arn
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "cloudwatch.amazonaws.com",
+      ]
+    }
+  }
 
-# }
+}
