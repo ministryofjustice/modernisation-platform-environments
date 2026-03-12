b33d4b9195470f3308eb6299efe79473c96abd09
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-12 08:10:51 +0000

    fix: allow inbound from lambda

--- terraform/environments/ccms-ebs/ssogen-ec2-sg.tf
+++ terraform/environments/ccms-ebs/ssogen-ec2-sg.tf
@@ -19,7 +19,7 @@ resource "aws_vpc_security_group_ingress_rule" "ing_console_workspaces" {
   count             = local.is-development || local.is-test ? 1 : 0
   ip_protocol       = "tcp"
   description       = "admin port from Lambda SG"
-  security_group_id = aws_security_group.ssogen_sg[count.index].id
+  security_group_id = aws_security_group.ssogen_lambda_sg[count.index].id
   from_port         = 7001
   to_port           = 7001
   referenced_security_group_id = aws_security_group.ssogen_lambda_sg[count.index].id
