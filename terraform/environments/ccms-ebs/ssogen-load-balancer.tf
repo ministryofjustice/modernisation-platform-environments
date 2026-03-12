8856f91d437ad0b8eb967b9f6b12aa684acbb63a
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-12 13:33:43 +0000

    fix: reinstate listener

--- terraform/environments/ccms-ebs/ssogen-load-balancer.tf
+++ terraform/environments/ccms-ebs/ssogen-load-balancer.tf
@@ -20,31 +20,31 @@ resource "aws_lb" "ssogen_alb" {
   )
 }
 
-# resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_app" {
-#   count       = local.is-development || local.is-test ? 1 : 0
-#   name        = lower(format("tg-%s-app", local.application_name_ssogen))
-#   port        = local.application_data.accounts[local.environment].tg_ssogen_apps_port
-#   protocol    = "HTTP"
-#   vpc_id      = data.aws_vpc.shared.id
-#   target_type = "instance"
-#   # deregistration_delay = 60
-#   health_check {
-#     enabled             = true
-#     path                = "/"
-#     protocol            = "HTTP"
-#     matcher             = "200"
-#     interval            = 30
-#     timeout             = 5
-#     healthy_threshold   = 3
-#     unhealthy_threshold = 3
-#   }
+resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_app" {
+  count       = local.is-development || local.is-test ? 1 : 0
+  name        = lower(format("tg-%s-app", local.application_name_ssogen))
+  port        = local.application_data.accounts[local.environment].tg_ssogen_apps_port
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.shared.id
+  target_type = "instance"
+  # deregistration_delay = 60
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
 
-#   # stickiness {
-#   #   enabled         = true
-#   #   type            = "lb_cookie"
-#   #   cookie_duration = 3600
-#   # }
-# }
+  # stickiness {
+  #   enabled         = true
+  #   type            = "lb_cookie"
+  #   cookie_duration = 3600
+  # }
+}
 
 # resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_enc_app" {
 #   count       = local.is-development || local.is-test ? 1 : 0
