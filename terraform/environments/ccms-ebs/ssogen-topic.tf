1a2fdd9cf357765e1a622497c1c5bae30af70bfc
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-12 06:49:32 +0000

    fix: cosmetic count issue fixed

--- terraform/environments/ccms-ebs/ssogen-topic.tf
+++ terraform/environments/ccms-ebs/ssogen-topic.tf
@@ -1,4 +1,5 @@
 resource "aws_sns_topic" "ssogen_admin_dns_flip_topic" {
+  count = local.is-development || local.is-test ? 1 : 0
   name              = "ssogen-admin-dns-flip-topic"
   delivery_policy   = <<EOF
 {
@@ -27,18 +28,21 @@ EOF
 }
 
 resource "aws_sns_topic_policy" "ssogen_sns_policy" {
-  arn    = aws_sns_topic.ssogen_admin_dns_flip_topic.arn
-  policy = data.aws_iam_policy_document.ssogen_admin_dns_flip_topic_policy.json
+  count = local.is-development || local.is-test ? 1 : 0
+  arn    = aws_sns_topic.ssogen_admin_dns_flip_topic[count.index].arn
+  policy = data.aws_iam_policy_document.ssogen_admin_dns_flip_topic_policy[count.index].json
 }
 
 # S3 SNS -> Lambda (Slack) instead of email
 resource "aws_sns_topic_subscription" "ssogen_admin_dns_flip_subscription" {
-  topic_arn = aws_sns_topic.ssogen_admin_dns_flip_topic.arn
+  count = local.is-development || local.is-test ? 1 : 0
+  topic_arn = aws_sns_topic.ssogen_admin_dns_flip_topic[count.index].arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.ssogen_admin_dns_flip.arn
+  endpoint  = aws_lambda_function.ssogen_admin_dns_flip[count.index].arn
 }
 
 data "aws_iam_policy_document" "ssogen_admin_dns_flip_topic_policy" {
+  count = local.is-development || local.is-test ? 1 : 0
   version = "2012-10-17"
   statement {
     sid    = "EventsAllowPublishSnsTopic"
