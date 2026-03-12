02c2ab96ea80ce9c0f87c0aa8bc315a2f9467744
dan-criddle <dan.criddle@justice.gov.uk>
2026-03-12 12:26:25 +0000

    Disable loading config file for kubectl provider

--- terraform/environments/cloud-platform/cluster/providers.tf
+++ terraform/environments/cloud-platform/cluster/providers.tf
@@ -25,4 +25,5 @@ provider "helm" {
 provider "kubectl" {
   host                   = module.eks[0].cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data)
+  load_config_file       = false
 }
