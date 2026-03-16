f080b83df266b6073e570b3fe2292475781198f1
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-16 13:05:58 +0000

    fix: create an ec2 instance based on latest ami

--- terraform/environments/ccms-ebs/ssogen-ec2.tf
+++ terraform/environments/ccms-ebs/ssogen-ec2.tf
@@ -332,3 +332,60 @@ resource "aws_autoscaling_group" "ssogen-scaling-group-secondary" {
 
 #   depends_on = [aws_security_group.ssogen_sg]
 # }
+
+resource "aws_instance" "ec2_ssogen" {
+  count               = local.is-development || local.is-test ? 1 : 0
+  instance_type          = "m6.large"
+  ami                    = "ami-07130111704054fdf"
+  key_name               = aws_key_pair.ssogen[0].key_name
+  vpc_security_group_ids = [aws_security_group.ssogen_sg[0].id]
+  subnet_id              = local.private_subnets[count.index]
+  # monitoring                  = true
+  ebs_optimized               = false
+  associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.ssogen_instance_profile[0].name
+
+  # lifecycle {
+  #   ignore_changes = [
+  #     user_data
+  #   ]
+  # }
+
+  root_block_device {
+    volume_size = 60
+    volume_type = "gp2"
+    encrypted   = true
+    tags = merge(
+      local.tags,
+      { Name = "ec2-ccms-ebs-development-ssogen-${count.index + 1}" },
+      { "instance-role" = "${aws_iam_role.ssogen_ec2[count.index].name}" },
+      { "instance-scheduling" = local.application_data.accounts[local.environment]["instance-scheduling"] }
+      # { backup = "true" }
+    )
+  }
+
+  # user_data_replace_on_change = true
+  user_data = base64encode(templatefile("./templates/ec2_user_data_ssogen_test.sh", {
+    hostname = "ssogen-${count.index + 1}"
+    efs_id                = aws_efs_file_system.storage[count.index].id
+    mp_fqdn               = "${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
+    deploy_environment    = local.environment
+    DISKSARRAY            = local.disksmount_joined
+    EFS_MOUNT_POINT_ARRAY = local.efs_mount_points_joined
+  }))
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  tags = merge(
+    local.tags,
+    { Name = "ec2-ccms-ebs-development-ssogen-${count.index + 1}" },
+    { "instance-role" = local.application_data.accounts[local.environment].instance_role_ssogen },
+    { "instance-scheduling" = local.application_data.accounts[local.environment]["instance-scheduling"] },
+    { backup = "true" }
+  )
+
+  depends_on = [aws_security_group.ssogen_sg]
+}
