5ef857c45cc562c03762dfea6f83b6a4168781fd
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-03-18 16:00:38 +0000

    fix: cname added for cf distribution

--- terraform/environments/ccms-ebs/ccms-ebs-ssogen/ssogen-cloudfront.tf
+++ terraform/environments/ccms-ebs/ccms-ebs-ssogen/ssogen-cloudfront.tf
@@ -1,12 +1,12 @@
-# resource "aws_route53_record" "ssogen_cloudfront" {
-#   count    = (local.is-development || local.is-test) ? 1 : 0
-#   provider = aws.core-vpc
-#   zone_id  = data.aws_route53_zone.external.zone_id
-#   name     = "ccmsebs-sso-cf"
-#   type     = "CNAME"
-#   ttl      = 300
-#   records  = [aws_cloudfront_distribution.ssogen_cloudfront_distribution[count.index].domain_name]
-# }
+resource "aws_route53_record" "ssogen_cloudfront" {
+  count    = (local.is-development || local.is-test) ? 1 : 0
+  provider = aws.core-vpc
+  zone_id  = data.aws_route53_zone.external.zone_id
+  name     = "ccmsebs-sso-cf"
+  type     = "CNAME"
+  ttl      = 300
+  records  = [aws_cloudfront_distribution.ssogen_cloudfront_distribution[count.index].domain_name]
+}
 
 # #--Certs need to be created in us-east-1 as they are associated with Cloudfront
 # resource "aws_acm_certificate" "ssogen_cloudfront_cert" {
