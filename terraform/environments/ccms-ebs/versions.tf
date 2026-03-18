529752642fa5bca669d0ff453489bfa2903a5c41
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2026-03-18 11:04:16 +0000

    enabling object lock

--- terraform/environments/ccms-ebs/versions.tf
+++ terraform/environments/ccms-ebs/versions.tf
@@ -20,6 +20,10 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.0.0"
     }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.0"
+    }
     tls = {
       source  = "hashicorp/tls"
       version = ">= 4.1.0"
