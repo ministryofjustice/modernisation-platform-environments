3ff02225c9265381f753b94dc38c951932a86718
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2026-03-23 16:43:40 +0000

    test

--- terraform/environments/cloud-platform-live/platform_providers.tf
+++ terraform/environments/cloud-platform-live/platform_providers.tf
@@ -9,7 +9,7 @@ provider "aws" {
 provider "aws" {
   region = "eu-west-2"
   assume_role {
-    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
+    role_arn = !can(regex("githubactionsrolesession|githubactionsplanrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : can(regex("githubactionsplanrolesession", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/github-actions-plan" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccess"
   }
   default_tags { tags = local.tags }
 }
