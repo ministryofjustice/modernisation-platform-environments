6be488a7f4dd4067fda72933f018d1bbbe304e06
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2026-03-23 12:49:56 +0000

    enabling object lock

--- terraform/environments/ccms-ebs/ccms-s3.tf
+++ terraform/environments/ccms-ebs/ccms-s3.tf
@@ -225,7 +225,7 @@ resource "aws_s3_bucket_object_lock_configuration" "dbbackup" {
   rule {
     default_retention {
       mode = "GOVERNANCE"
-      days = 30
+      days = local.application_data.accounts[local.environment].rman_s3_lifecycle_days_expiration_current
     }
   }
 }
