05548a211576e523218ac270bf10f8748b609788
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-01 10:50:20 +0100

    fix: reenable listener

--- terraform/environments/ccms-ebs/ssogen-load-balancer.tf
+++ terraform/environments/ccms-ebs/ssogen-load-balancer.tf
@@ -20,31 +20,31 @@ resource "aws_lb" "ssogen_alb" {
   )
 }
 
-# resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_enc_console" {
-#   count       = local.is-development || local.is-test ? 1 : 0
-#   name        = lower(format("tg-%s-console-enc", local.application_name_ssogen))
-#   port        = "4443"
-#   protocol    = "HTTPS"
-#   vpc_id      = data.aws_vpc.shared.id
-#   target_type = "instance"
-#   # deregistration_delay = 60
-#   health_check {
-#     enabled             = true
-#     path                = "/"
-#     protocol            = "HTTPS"
-#     matcher             = "200"
-#     interval            = 30
-#     timeout             = 5
-#     healthy_threshold   = 3
-#     unhealthy_threshold = 3
-#   }
+resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_enc_console" {
+  count       = local.is-development || local.is-test ? 1 : 0
+  name        = lower(format("tg-%s-console-enc", local.application_name_ssogen))
+  port        = "4443"
+  protocol    = "HTTPS"
+  vpc_id      = data.aws_vpc.shared.id
+  target_type = "instance"
+  # deregistration_delay = 60
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTPS"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
 
-#   stickiness {
-#     enabled         = true
-#     type            = "lb_cookie"
-#     cookie_duration = 3600
-#   }
-# }
+  stickiness {
+    enabled         = true
+    type            = "lb_cookie"
+    cookie_duration = 3600
+  }
+}
 
 resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_enc_app" {
   count       = local.is-development || local.is-test ? 1 : 0
@@ -147,35 +147,35 @@ resource "aws_lb_listener" "ssogen_internal_app_listener" {
   depends_on = [aws_acm_certificate_validation.external_nonprod]
 }
 
-# resource "aws_lb_listener" "ssogen_internal_console_listener_encrypted" {
-#   count             = local.is-development || local.is-test ? 1 : 0
-#   load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
-#   port              = "5443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-#   certificate_arn   = data.aws_acm_certificate.external_ssogen[count.index].arn
+resource "aws_lb_listener" "ssogen_internal_console_listener_encrypted" {
+  count             = local.is-development || local.is-test ? 1 : 0
+  load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
+  port              = "5443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = data.aws_acm_certificate.external_ssogen[count.index].arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_enc_console[count.index].arn
-#   }
-#   # forward {
-#   #   target_group {
-#   #     arn    = aws_lb_target_group.ssogen_internal_tg_ssogen_app[count.index].arn
-#   #     weight = 50
-#   #   }
-#   #   target_group {
-#   #     arn    = aws_lb_target_group.ssogen_internal_tg_ssogen_enc_app[count.index].arn
-#   #     weight = 50
-#   #   }
-#   #   stickiness {
-#   #     enabled  = true
-#   #     duration = 3600
-#   #   }
-#   # }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_enc_console[count.index].arn
+  }
+  # forward {
+  #   target_group {
+  #     arn    = aws_lb_target_group.ssogen_internal_tg_ssogen_app[count.index].arn
+  #     weight = 50
+  #   }
+  #   target_group {
+  #     arn    = aws_lb_target_group.ssogen_internal_tg_ssogen_enc_app[count.index].arn
+  #     weight = 50
+  #   }
+  #   stickiness {
+  #     enabled  = true
+  #     duration = 3600
+  #   }
+  # }
 
-#   depends_on = [aws_acm_certificate_validation.external_nonprod]
-# }
+  depends_on = [aws_acm_certificate_validation.external_nonprod]
+}
 
 resource "aws_lb_listener" "ssogen_internal_console_listener" {
   count             = local.is-development || local.is-test ? 1 : 0
