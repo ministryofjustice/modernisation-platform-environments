7e7eebf4abd2b6acee462b929eb7e4bbd6f510ab
Luke Williams <luke.williams6@justice.gov.uk>
2026-04-01 15:55:13 +0100

    attempting a count

--- terraform/environments/electronic-monitoring-data/gdpr_batch_jobs.tf
+++ terraform/environments/electronic-monitoring-data/gdpr_batch_jobs.tf
@@ -32,6 +32,7 @@ resource "aws_iam_role_policy_attachment" "gdpr_batch_service_role_attachment" {
 # ==============================================================================
 
 resource "aws_batch_compute_environment" "shred_unstructured_from_zip_batch_compute_env" {
+  count                    = local.is-production || local.is-development || local.is-preproduction ? 1 : 0
   name                     = "shred-unstructured-from-zip-env"
   type                     = "MANAGED"
   service_role             = aws_iam_role.gdpr_batch_service_role.arn
@@ -41,7 +42,7 @@ resource "aws_batch_compute_environment" "shred_unstructured_from_zip_batch_comp
     type                = "SPOT"
     max_vcpus           = 16
     min_vcpus           = 0
-    security_group_ids  = [aws_security_group.gdpr_batch_sg.id]
+    security_group_ids  = [aws_security_group.gdpr_batch_sg[0].id]
     subnets             = data.aws_subnets.shared-private[*].id
     
     # Require large instances with high network/EBS bandwidth
