a63d31074629e4498f538606f6c1c3312e251808
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-13 17:06:07 +0100

    fix: reinstate listener

--- terraform/environments/ccms-ebs/ssogen-load-balancer.tf
+++ terraform/environments/ccms-ebs/ssogen-load-balancer.tf
@@ -117,35 +117,35 @@ resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_console" {
   }
 }
 
-# resource "aws_lb_listener" "ssogen_internal_app_listener" {
-#   count             = local.is-development || local.is-test ? 1 : 0
-#   load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-#   certificate_arn   = data.aws_acm_certificate.external_ssogen[count.index].arn
+resource "aws_lb_listener" "ssogen_internal_app_listener" {
+  count             = local.is-development || local.is-test ? 1 : 0
+  load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = data.aws_acm_certificate.external_ssogen[count.index].arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_enc_app[count.index].arn
-#   }
-#   # forward {
-#   #   target_group {
-#   #     arn    = aws_lb_target_group.ssogen_internal_tg_ssogen_app[count.index].arn
-#   #     weight = 50
-#   #   }
-#   #   target_group {
-#   #     arn    = aws_lb_target_group.ssogen_internal_tg_ssogen_enc_app[count.index].arn
-#   #     weight = 50
-#   #   }
-#   #   stickiness {
-#   #     enabled  = true
-#   #     duration = 3600
-#   #   }
-#   # }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_enc_app[count.index].arn
+  }
+  # forward {
+  #   target_group {
+  #     arn    = aws_lb_target_group.ssogen_internal_tg_ssogen_app[count.index].arn
+  #     weight = 50
+  #   }
+  #   target_group {
+  #     arn    = aws_lb_target_group.ssogen_internal_tg_ssogen_enc_app[count.index].arn
+  #     weight = 50
+  #   }
+  #   stickiness {
+  #     enabled  = true
+  #     duration = 3600
+  #   }
+  # }
 
-#   depends_on = [aws_acm_certificate_validation.external_nonprod]
-# }
+  depends_on = [aws_acm_certificate_validation.external_nonprod]
+}
 
 # resource "aws_lb_listener" "ssogen_internal_console_listener_encrypted" {
 #   count             = local.is-development || local.is-test ? 1 : 0
@@ -177,21 +177,21 @@ resource "aws_lb_target_group" "ssogen_internal_tg_ssogen_console" {
 #   depends_on = [aws_acm_certificate_validation.external_nonprod]
 # }
 
-# resource "aws_lb_listener" "ssogen_internal_console_listener" {
-#   count             = local.is-development || local.is-test ? 1 : 0
-#   load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
-#   port              = "7001"
-#   protocol          = "HTTP"
-#   # ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-#   # certificate_arn   = aws_acm_certificate.external.arn
+resource "aws_lb_listener" "ssogen_internal_console_listener" {
+  count             = local.is-development || local.is-test ? 1 : 0
+  load_balancer_arn = aws_lb.ssogen_alb[count.index].arn
+  port              = "7001"
+  protocol          = "HTTP"
+  # ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  # certificate_arn   = aws_acm_certificate.external.arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_console[count.index].arn
-#   }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ssogen_internal_tg_ssogen_console[count.index].arn
+  }
 
-#   # depends_on = [aws_acm_certificate_validation.external_nonprod]
-# }
+  # depends_on = [aws_acm_certificate_validation.external_nonprod]
+}
 
 # resource "aws_lb_listener_rule" "ssogen_internal_console_listener_encrypted" {
 #   count        = local.is-development || local.is-test ? 1 : 0
