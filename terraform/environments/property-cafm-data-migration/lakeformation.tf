dee9c4537663558f5a0c7b5c1ad0a39794148545
Stuart James <stuart.james1@justice.gov.uk>
2026-04-15 12:11:56 +0100

    Permissions

--- terraform/environments/property-cafm-data-migration/lakeformation.tf
+++ terraform/environments/property-cafm-data-migration/lakeformation.tf
@@ -1,28 +1,18 @@
 # ------------------------------------------------------------------------
 # Lake Formation - admin permissions
 # https://user-guide.modernisation-platform.service.justice.gov.uk/runbooks/adding-admin-data-lake-formation-permissions.html
+# SSO role (sandbox/data-eng) is NOT an LF admin to avoid conflicts with
+# explicit grants managed via data-engineering-datalake-access YAML.
+# See: digital-prison-reporting lake_formation.tf for reference pattern.
 # ------------------------------------------------------------------------
-locals {
-  lf_admin_roles = local.is-development ? "sandbox" : "data-eng"
-}
-
-data "aws_iam_roles" "modernisation_platform" {
-  name_regex  = "AWSReservedSSO_modernisation-platform-${local.lf_admin_roles}_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
-}
 
 resource "aws_lakeformation_data_lake_settings" "lake_formation" {
-  admins = concat(
-    length(data.aws_iam_roles.modernisation_platform.names) > 0 ? [
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.name}/${one(data.aws_iam_roles.modernisation_platform.names)}"
-    ] : [],
-    [
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/MemberInfrastructureAccess",
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lakeformation-share-role",
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-plan",
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-apply",
-    ]
-  )
+  admins = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/MemberInfrastructureAccess",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lakeformation-share-role",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-plan",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-apply",
+  ]
 
   parameters = {
     "CROSS_ACCOUNT_VERSION" = "4"
