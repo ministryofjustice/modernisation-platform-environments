111bd3b469d744e4d8676bb128a75fd28b05a36e
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-17 17:12:15 +0100

    fix: adding athena query for ssogen console lb

--- terraform/environments/ccms-ebs/ssogen-athena-query-execution.tf
+++ terraform/environments/ccms-ebs/ssogen-athena-query-execution.tf
@@ -2,7 +2,7 @@
 resource "null_resource" "execute_create_ssogen_table_queries" {
   count = local.is-development || local.is-test ? 1 : 0
   triggers = {
-    query_ids = join(",", ["${aws_athena_named_query.main_table_ssogen[count.index].id}", "${aws_athena_named_query.http_requests_ssogen[count.index].id}"])
+    query_ids = join(",", ["${aws_athena_named_query.main_table_ssogen[count.index].id}", "${aws_athena_named_query.http_requests_ssogen[count.index].id}","${aws_athena_named_query.main_table_ssogen_console[count.index].id}", "${aws_athena_named_query.http_requests_ssogen_console[count.index].id}"])
   }
 
   provisioner "local-exec" {
@@ -17,7 +17,17 @@ aws athena start-query-execution \
   --query-execution-context Database=${aws_athena_database.ssogen_lb-access-logs[count.index].name} \
   --region ${data.aws_region.current.name}
 aws athena start-query-execution \
+  --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.main_table_ssogen_console[count.index].id} --query 'NamedQuery.QueryString' --output text)" \
+  --work-group ${aws_athena_workgroup.ssogen_lb-access-logs[count.index].name} \
+  --query-execution-context Database=${aws_athena_database.ssogen_lb-access-logs[count.index].name} \
+  --region ${data.aws_region.current.name}
+aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.http_requests_ssogen[count.index] .id} --query 'NamedQuery.QueryString' --output text)" \
+  --work-group ${aws_athena_workgroup.ssogen_lb-access-logs[count.index].name} \
+  --query-execution-context Database=${aws_athena_database.ssogen_lb-access-logs[count.index].name} \
+  --region ${data.aws_region.current.name}
+aws athena start-query-execution \
+  --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.http_requests_ssogen_console[count.index] .id} --query 'NamedQuery.QueryString' --output text)" \
   --work-group ${aws_athena_workgroup.ssogen_lb-access-logs[count.index].name} \
   --query-execution-context Database=${aws_athena_database.ssogen_lb-access-logs[count.index].name} \
   --region ${data.aws_region.current.name}
@@ -27,6 +37,8 @@ EOF
   depends_on = [
     aws_athena_named_query.main_table_ssogen[0],
     aws_athena_named_query.http_requests_ssogen[0],
+    aws_athena_named_query.main_table_ssogen_console[0],
+    aws_athena_named_query.http_requests_ssogen_console[0],
     aws_athena_workgroup.ssogen_lb-access-logs[0],
     aws_athena_database.ssogen_lb-access-logs[0]
   ]
