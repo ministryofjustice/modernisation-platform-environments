6c9c22d666ab6764e163dd95e79a75eef6c1bfb7
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-04-17 16:02:47 +0100

    fix: execute athena query

--- terraform/environments/ccms-ebs/ssogen-athena-query-execution.tf
+++ terraform/environments/ccms-ebs/ssogen-athena-query-execution.tf
@@ -2,7 +2,7 @@
 resource "null_resource" "execute_create_ssogen_table_queries" {
   count = local.is-development || local.is-test ? 1 : 0
   triggers = {
-    query_ids = "${aws_athena_named_query.main_table_ssogen[count.index].id}"
+    query_ids = join(",", ["${aws_athena_named_query.main_table_ssogen[count.index].id}", "${aws_athena_named_query.http_requests_ssogen[count.index].id}"])
   }
 
   provisioner "local-exec" {
@@ -13,15 +13,21 @@ export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.Credentials.SecretAccessKey
 export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
 aws athena start-query-execution \
   --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.main_table_ssogen[count.index].id} --query 'NamedQuery.QueryString' --output text)" \
-  --work-group ${aws_athena_workgroup.lb-access-logs.name} \
-  --query-execution-context Database=${aws_athena_database.lb-access-logs.name} \
+  --work-group ${aws_athena_workgroup.ssogen_lb-access-logs.name} \
+  --query-execution-context Database=${aws_athena_database.ssogen_lb-access-logs.name} \
+  --region ${data.aws_region.current.name}
+aws athena start-query-execution \
+  --query-string "$(aws athena get-named-query --named-query-id ${aws_athena_named_query.http_requests_ssogen[count.index] .id} --query 'NamedQuery.QueryString' --output text)" \
+  --work-group ${aws_athena_workgroup.ssogen_lb-access-logs.name} \
+  --query-execution-context Database=${aws_athena_database.ssogen_lb-access-logs.name} \
   --region ${data.aws_region.current.name}
 EOF
   }
 
   depends_on = [
     aws_athena_named_query.main_table_ssogen[0],
-    aws_athena_workgroup.lb-access-logs,
-    aws_athena_database.lb-access-logs
+    aws_athena_named_query.http_requests_ssogen[0],
+    aws_athena_workgroup.ssogen_lb-access-logs,
+    aws_athena_database.ssogen_lb-access-logs
   ]
 }
