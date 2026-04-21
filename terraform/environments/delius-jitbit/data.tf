b24fb4495b3d67ff34db314a4522a8b5a29094a6
David Teeling <david.teeling1@justice.gov.uk>
2026-04-21 11:58:53 +0100

    Remove unnecessary data resources

--- terraform/environments/delius-jitbit/data.tf
+++ terraform/environments/delius-jitbit/data.tf
@@ -1,10 +1,1 @@
 #### This file can be used to store data specific to the member account ####
-# data "aws_ssm_parameter" "active_deployment_colour" {
-#   count = local.create_blue_green ? 1 : 0
-#   name = "/delius-jitbit/blue-green-active-colour"
-# }
-
-# data "aws_ssm_parameter" "sandbox_active_deployment_colour" {
-#   count = local.is-development ? 1 : 0
-#   name  = "/delius-jitbit/sandbox-blue-green-active-colour"
-# }
