bf9b880cb97233c0e5a699208f47f2684ca34e71
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-04-29 16:16:36 +0100

    Moving karpenter to module

--- terraform/environments/cloud-platform/cluster/karpenter.tf
+++ terraform/environments/cloud-platform/cluster/karpenter.tf
@@ -1,15 +1,7 @@
-# module "karpenter" {
-#   count   = contains(local.enabled_workspaces, local.cluster_environment) ? 1 : 0
-#   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-#   version = "~> 21.0"
+module "karpenter" {
+  count   = contains(local.enabled_workspaces, local.cluster_environment) ? 1 : 0
+  source  = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=initial-module"
 
-#   cluster_name = module.eks[0].cluster_name
-
-#   create_node_iam_role = false
-#   node_iam_role_arn    = module.eks[0].eks_managed_node_groups["default_ng"].iam_role_arn
-
-#   # Since the node group role will already have an access entry
-#   create_access_entry = false
-
-#   tags = local.tags
-# }
+  cluster_name = local.cluster_name
+  cluster_endpoint = module.eks[0].cluster_endpoint
+}
