b623eefa96fb8fd69227fdbfdd10db5d65166d5d
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-04-29 17:32:58 +0100

    Add module variables

--- terraform/environments/cloud-platform/cluster/karpenter.tf
+++ terraform/environments/cloud-platform/cluster/karpenter.tf
@@ -3,5 +3,13 @@ module "karpenter" {
   source  = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=initial-module"
 
   cluster_name = local.cluster_name
-  cluster_endpoint = module.eks[0].cluster_endpoint
+  cluster_endpoint = try(module.eks[0].cluster_endpoint, null)
+  cluster_ca_certificate = try(base64decode(module.eks[0].cluster_certificate_authority_data), null)
+  provider_token = try(data.aws_eks_cluster_auth.cluster[0].token, null)
+
+  provider_endpoint = try(data.aws_eks_cluster.cluster.endpoint, null)
+  provider_cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data), null)
+  provider_cluster_name = data.aws_eks_cluster.cluster.name
+
+  
 }
