7dab7c9f5e1084f06276c41a3d0faff60804295f
Seb Norris <sebastian.j.norris@gmail.com>
2026-04-30 13:02:03 +0100

    add secret for vcms test repo

--- terraform/environments/vcms/secrets.tf
+++ terraform/environments/vcms/secrets.tf
@@ -1,1 +1,13 @@
 #### This file can be used to store secrets specific to the member account ####
+
+
+resource "aws_secretsmanager_secret" "vcms-test-automation-key" {
+  name                    = "vcms-test-automation-key"
+  recovery_window_in_days = 0
+  tags = merge(
+    local.tags,
+    {
+      Name = "vcms-test-automation-key"
+    }
+  )
+}
