e62fc1e2bf7a39f47cba8073bc00d398513b3e87
jono-basford-moj <jono.basford@justice.gov.uk>
2026-04-30 12:12:34 +0100

    Added gdpr raw dbs

--- terraform/environments/electronic-monitoring-data/step_functions_main.tf
+++ terraform/environments/electronic-monitoring-data/step_functions_main.tf
@@ -77,6 +77,55 @@ module "ears_sars_step_function" {
 # GDPR Step Function
 # ------------------------------------------
 
+locals {
+  gdpr_databases = local.is-production ? [
+    "buddi_buddi_local",
+    "buddi_buddi",
+    "capita_alcohol_monitoring",
+    "capita_blob_storage",
+    "civica_orca",
+    "g4s_atrium",
+    "g4s_atrium_unstructured",
+    "g4s_atv",
+    "g4s_cap_dw",
+    "g4s_centurion",
+    "g4s_centurion_test",
+    "g4s_emsys_mvp",
+    "g4s_emsys_tpims",
+    "g4s_fep",
+    "g4s_gps",
+    "g4s_integrity",
+    "g4s_lcm_archive",
+    "g4s_lcm",
+    "g4s",
+    "g4s_rf_hours",
+    "g4s_tasking",
+    "scram_alcohol_monitoring"] : local.is-preproduction ? [
+    "buddi_buddi_local_preprod",
+    "buddi_buddi_preprod",
+    "capita_alcohol_monitoring_preprod",
+    "capita_blob_storage_preprod",
+    "civica_orca_preprod",
+    "g4s_atrium_preprod",
+    "g4s_atrium_unstructured_preprod",
+    "g4s_atv_preprod",
+    "g4s_cap_dw_preprod",
+    "g4s_centurion_preprod",
+    "g4s_centurion_test_preprod",
+    "g4s_emsys_mvp_preprod",
+    "g4s_emsys_tpims_preprod",
+    "g4s_fep_preprod",
+    "g4s_gps_preprod",
+    "g4s_integrity_preprod",
+    "g4s_lcm_archive_preprod",
+    "g4s_lcm_preprod",
+    "g4s_preprod",
+    "g4s_rf_hours_preprod",
+    "g4s_tasking_preprod",
+    "scram_alcohol_monitoring_preprod"
+  ] : local.is-development ? ["test"] : []
+
+}
 module "gdpr_deletion_step_function" {
   count        = local.is-development || local.is-preproduction ? 1 : 0
   source       = "./modules/step_function"
@@ -90,6 +139,7 @@ module "gdpr_deletion_step_function" {
       "security_groups_json"   = jsonencode([aws_security_group.ecs_generic.id])
       "subnets_json"           = jsonencode(data.aws_subnets.shared-private.ids)
       "athena_output_bucket"   = "s3://${module.s3-athena-bucket.bucket.id}/output/"
+      "gdpr_databases"         = jsonencode(local.gdpr_databases)
     }
   )
   type = "STANDARD"
