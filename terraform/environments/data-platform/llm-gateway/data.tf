38570ae3490abe908550f7fa31927879848e620c
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-04-30 09:14:10 +0000

    refactor: remove unnecessary tostring() from data source secret_id

--- terraform/environments/data-platform/llm-gateway/data.tf
+++ terraform/environments/data-platform/llm-gateway/data.tf
@@ -23,50 +23,17 @@ data "aws_secretsmanager_secret_version" "cloud_platform_live_namespace" {
 }
 
 data "aws_secretsmanager_secret_version" "litellm_license" {
-  secret_id = tostring(module.litellm_license_secret.secret_id)
+  secret_id = module.litellm_license_secret.secret_id
 }
 
 data "aws_secretsmanager_secret_version" "litellm_entra_id" {
-  secret_id = tostring(module.litellm_entra_id_secret.secret_id)
+  secret_id = module.litellm_entra_id_secret.secret_id
 }
 
 data "aws_secretsmanager_secret_version" "justiceai_azure_openai" {
-  secret_id = tostring(module.justiceai_azure_openai_secret.secret_id)
+  secret_id = module.justiceai_azure_openai_secret.secret_id
 }
 
 data "aws_secretsmanager_secret_version" "azure_openai_secret" {
-  secret_id = tostring(module.azure_openai_secret.secret_id)
-}
-
-data "kubernetes_secret" "elasticache" {
-  count = terraform.workspace == "data-platform-development" ? 1 : 0
-
-  provider = kubernetes.cloud_platform
-
-  metadata {
-    namespace = jsondecode(data.aws_secretsmanager_secret_version.cloud_platform_live_namespace[0].secret_string)["namespace"]
-    name      = "elasticache"
-  }
-}
-
-data "kubernetes_secret" "irsa" {
-  count = terraform.workspace == "data-platform-development" ? 1 : 0
-
-  provider = kubernetes.cloud_platform
-
-  metadata {
-    namespace = jsondecode(data.aws_secretsmanager_secret_version.cloud_platform_live_namespace[0].secret_string)["namespace"]
-    name      = "irsa"
-  }
-}
-
-data "kubernetes_secret" "rds" {
-  count = terraform.workspace == "data-platform-development" ? 1 : 0
-
-  provider = kubernetes.cloud_platform
-
-  metadata {
-    namespace = jsondecode(data.aws_secretsmanager_secret_version.cloud_platform_live_namespace[0].secret_string)["namespace"]
-    name      = "rds"
-  }
+  secret_id = module.azure_openai_secret.secret_id
 }
