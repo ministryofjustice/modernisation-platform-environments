40c2db8961185b5c6faed3a0b5608fa86ab3b820
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-04-30 16:32:27 +0100

    updating providers

--- terraform/environments/cloud-platform/cluster/karpenter.tf
+++ terraform/environments/cloud-platform/cluster/karpenter.tf
@@ -10,6 +10,4 @@ module "karpenter" {
   provider_endpoint = try(data.aws_eks_cluster.cluster.endpoint, null)
   provider_cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data), null)
   provider_cluster_name = data.aws_eks_cluster.cluster.name
-
-  
 }
