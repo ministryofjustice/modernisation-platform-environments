cb3e0283611eba666f9a98f195eeb7da77670bb4
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-05-01 16:16:46 +0100

    removing uneeded vars

--- terraform/environments/cloud-platform/cluster/karpenter.tf
+++ terraform/environments/cloud-platform/cluster/karpenter.tf
@@ -5,9 +5,4 @@ module "karpenter" {
   cluster_name = local.cluster_name
   cluster_endpoint = try(module.eks[0].cluster_endpoint, null)
   cluster_ca_certificate = try(base64decode(module.eks[0].cluster_certificate_authority_data), null)
-  provider_token = try(data.aws_eks_cluster_auth.cluster[0].token, null)
-
-  provider_endpoint = try(data.aws_eks_cluster.cluster.endpoint, null)
-  provider_cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data), null)
-  provider_cluster_name = data.aws_eks_cluster.cluster.name
 }
