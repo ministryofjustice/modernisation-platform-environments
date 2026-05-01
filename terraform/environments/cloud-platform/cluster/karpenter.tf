996c0d29e25e982243514b94a29103ee7650ddfb
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-05-01 16:22:03 +0100

    Fixing vars

--- terraform/environments/cloud-platform/cluster/karpenter.tf
+++ terraform/environments/cloud-platform/cluster/karpenter.tf
@@ -3,6 +3,5 @@ module "karpenter" {
   source  = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=initial-module"
 
   cluster_name = local.cluster_name
-  cluster_endpoint = try(module.eks[0].cluster_endpoint, null)
-  cluster_ca_certificate = try(base64decode(module.eks[0].cluster_certificate_authority_data), null)
+  cluster_endpoint = module.eks[0].cluster_endpoint
 }
