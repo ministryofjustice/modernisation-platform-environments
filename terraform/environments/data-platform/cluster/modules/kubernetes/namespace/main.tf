ffdaecb2120aba7f0a859cdd983dfb7188c79d7d
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-05-01 13:25:20 +0000

    feat: revert

--- terraform/environments/data-platform/cluster/modules/kubernetes/namespace/main.tf
+++ terraform/environments/data-platform/cluster/modules/kubernetes/namespace/main.tf
@@ -1,10 +1,8 @@
 resource "kubernetes_namespace_v1" "this" {
   metadata {
     name = var.name
-    labels = merge(
-      {
-        "compute.data-platform.service.justice.gov.uk/workload" = var.workload
-      }
-    )
+    labels = {
+      "compute.data-platform.service.justice.gov.uk/workload" = var.workload
+    }
   }
 }
