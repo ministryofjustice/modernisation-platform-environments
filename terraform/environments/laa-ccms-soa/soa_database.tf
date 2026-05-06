4b50b6e12794b62f776ad42874f960df48ea1510
srikanththummala0470 <Srikanth.Thummala@justice.gov.uk>
2026-05-06 11:35:44 +0100

    Enable OEM_AGENT option in SOA DB option group

--- terraform/environments/laa-ccms-soa/soa_database.tf
+++ terraform/environments/laa-ccms-soa/soa_database.tf
@@ -22,36 +22,36 @@ resource "aws_db_option_group" "soa_oracle_19" {
     version     = "1.0"
   }
 
-  # option {
-  #   option_name = "OEM_AGENT"
+  option {
+    option_name = "OEM_AGENT"
 
-  #   port    = tonumber(local.application_data.accounts[local.environment].oem.agent_port)
-  #   version = "13.5.0.0.v1"
+    port    = tonumber(local.application_data.accounts[local.environment].oem.agent_port)
+    version = "13.5.0.0.v1"
 
-  #   vpc_security_group_memberships = [
-  #     aws_security_group.soa_db.id
-  #   ]
+    vpc_security_group_memberships = [
+      aws_security_group.soa_db.id
+    ]
 
-  #   option_settings {
-  #     name  = "MINIMUM_TLS_VERSION"
-  #     value = "TLSv1"
-  #   }
+    option_settings {
+      name  = "MINIMUM_TLS_VERSION"
+      value = "TLSv1"
+    }
 
-  #   option_settings {
-  #     name  = "AGENT_REGISTRATION_PASSWORD"
-  #     value = jsondecode(data.aws_secretsmanager_secret_version.ccms_soa_quiesced_secrets_current.secret_string)["agent_registration_password"]
-  #   }
+    option_settings {
+      name  = "AGENT_REGISTRATION_PASSWORD"
+      value = jsondecode(data.aws_secretsmanager_secret_version.ccms_soa_quiesced_secrets_current.secret_string)["agent_registration_password"]
+    }
 
-  #   option_settings {
-  #     name  = "OMS_HOST"
-  #     value = local.application_data.accounts[local.environment].oem.oms_host
-  #   }
+    option_settings {
+      name  = "OMS_HOST"
+      value = local.application_data.accounts[local.environment].oem.oms_host
+    }
 
-  #   option_settings {
-  #     name  = "OMS_PORT"
-  #     value = local.application_data.accounts[local.environment].oem.oms_port
-  #   }
-  # }
+    option_settings {
+      name  = "OMS_PORT"
+      value = local.application_data.accounts[local.environment].oem.oms_port
+    }
+  }
 
   lifecycle {
     create_before_destroy = true
