4aebb1548cffc308a2c74ca1c0f03e27f5f8b14e
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-06 21:01:21 +0000

    refactor: remove unused moved block in data lake settings
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform-governance/lakeformation/data-lake-settings.tf
+++ terraform/environments/data-platform-governance/lakeformation/data-lake-settings.tf
@@ -5,8 +5,3 @@ module "data_lake_settings" {
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.platform_engineer_admin_sso_role.names)}"
   ]
 }
-
-moved {
-  from = aws_lakeformation_data_lake_settings.main
-  to   = module.data_lake_settings.aws_lakeformation_data_lake_settings.main
-} 
