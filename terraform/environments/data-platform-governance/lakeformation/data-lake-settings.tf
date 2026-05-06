61fa25fa7488833bff5c74dfd2be2277be4be701
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-06 20:50:35 +0000

    feat: refactor data lake settings to use module structure
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform-governance/lakeformation/data-lake-settings.tf
+++ terraform/environments/data-platform-governance/lakeformation/data-lake-settings.tf
@@ -1,9 +1,12 @@
-resource "aws_lakeformation_data_lake_settings" "main" {
+module "data_lake_settings" {
+  source = "git::https://github.com/ministryofjustice/terraform-aws-data-platform-lakeformation.git//modules/data-lake-settings?ref=feat/data-lake-settings"
+
   admins = [
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.platform_engineer_admin_sso_role.names)}"
   ]
-
-  parameters = {
-    "CROSS_ACCOUNT_VERSION" = "5"
-  }
 }
+
+moved {
+  from = aws_lakeformation_data_lake_settings.main
+  to   = module.data_lake_settings.aws_lakeformation_data_lake_settings.main
+} 
