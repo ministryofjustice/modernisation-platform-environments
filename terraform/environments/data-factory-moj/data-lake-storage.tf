fa0080d24f510bab0ae9e6adb03b38fbf281501a
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-07 15:50:49 +0000

    fix(data-lake-storage): correct source path for data lake storage module
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-factory-moj/data-lake-storage.tf
+++ terraform/environments/data-factory-moj/data-lake-storage.tf
@@ -1,5 +1,5 @@
 module "data_lake_storage" {
   count = local.environment == "development" ? 1 : 0
 
-  source = "git::https://github.com/ministryofjustice/terraform-aws-data-platform-lakeformation.git//modules/data-lake-settings?ref=feat/data-lake-storage"
+  source = "git::https://github.com/ministryofjustice/terraform-aws-data-platform-lakeformation.git//modules/data-lake-storage?ref=feat/data-lake-storage"
 }
