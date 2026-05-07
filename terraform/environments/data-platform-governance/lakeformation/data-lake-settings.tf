f896d87dcdcd5699d300f15373dc060c0a54e239
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-07 20:51:43 +0000

    feat(data-lake-storage): update source reference and add trusted resource owners in data lake settings module
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform-governance/lakeformation/data-lake-settings.tf
+++ terraform/environments/data-platform-governance/lakeformation/data-lake-settings.tf
@@ -1,7 +1,11 @@
 module "data_lake_settings" {
-  source = "git::https://github.com/ministryofjustice/terraform-aws-data-platform-lakeformation.git//modules/data-lake-settings?ref=43c73a901f630a37f55cc91516de3d41fe858e03" # v0.0.1
+  source = "git::https://github.com/ministryofjustice/terraform-aws-data-platform-lakeformation.git//modules/data-lake-settings?ref=feat/data-lake-storage"
 
   admins = [
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.platform_engineer_admin_sso_role.names)}"
+  ]
+
+  trusted_resource_owners = [
+    local.environment_management.account_ids["data-platform-governance-development"]
   ]
 }
