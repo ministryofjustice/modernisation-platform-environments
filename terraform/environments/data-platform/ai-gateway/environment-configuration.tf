9f9d2f7a77ecf2e1a1ec5f9fa0f2386c260ec1fa
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-05-07 18:00:03 +0000

    feat: configure admin_ingress

--- terraform/environments/data-platform/ai-gateway/environment-configuration.tf
+++ terraform/environments/data-platform/ai-gateway/environment-configuration.tf
@@ -135,8 +135,17 @@ locals {
         "213.121.161.112/28", # 102PF
         "51.149.2.0/24",      # 10SC
       ]
-      ai_gateway_admin_ingress_allowlist = []
-      ai_gateway_models                  = {}
+      ai_gateway_admin_ingress_allowlist = [
+        # VPN
+        "128.77.75.64/26",  # Prisma Corporate
+        "35.176.93.186/32", # GlobalProtect (Alpha)
+        # Sites
+        "213.121.161.112/28", # 102PF
+        "51.149.2.0/24",      # 10SC
+        # Hoose
+        "51.179.195.141/32",
+      ]
+      ai_gateway_models = {}
       ai_gateway_autoscaling = {
         min_replicas                      = 1
         max_replicas                      = 3
