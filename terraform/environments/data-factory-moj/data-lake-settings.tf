ca354ebc5dd9d155015f54faf21e3622af67bb39
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-07 16:46:19 +0000

    feat(data-lake-storage): add lakeformation access role and update IAM roles for sandbox
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-factory-moj/data-lake-settings.tf
+++ terraform/environments/data-factory-moj/data-lake-settings.tf
@@ -6,4 +6,12 @@ module "data_lake_settings" {
   admins = [
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.platform_engineer_admin_sso_role.names)}"
   ]
+
+  read_only_admins = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.sandbox_sso_role.names)}"
+  ]
+
+  trusted_resource_owners = [
+    local.environment_management.account_ids["data-platform-governance-development"] // TODO (@jacobwoffenden): make this dynamic
+  ]
 }
