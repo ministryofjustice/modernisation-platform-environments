e0c9d5ddc41bf05de565e67850f7d9152e17d9e2
Gary Henderson <26419401+Gary-H9@users.noreply.github.com>
2026-05-07 10:23:37 +0100

    correction

--- terraform/environments/data-platform/llm-gateway/environment-configuration.tf
+++ terraform/environments/data-platform/llm-gateway/environment-configuration.tf
@@ -3,8 +3,8 @@ locals {
   environment_configurations = {
     development = {
       litellm_versions = {
-        application = "main-v1.84.0.rc.1"
-        chart       = "v1.84.0.rc.1"
+        application = "main-v1.84.0-rc.1"
+        chart       = "v1.84.0-rc.1"
       }
       llm_gateway_hostname = "llm-gateway.development.data-platform.service.justice.gov.uk"
       llm_gateway_ingress_allowlist = [
