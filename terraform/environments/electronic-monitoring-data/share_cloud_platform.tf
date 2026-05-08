42eb4dbdbbab4fbd3562ffad34de71dd5b27053d
matt heery <matt.heery@justice.gov.uk>
2026-05-08 15:27:14 +0100

    fix: remove fi

--- terraform/environments/electronic-monitoring-data/share_cloud_platform.tf
+++ terraform/environments/electronic-monitoring-data/share_cloud_platform.tf
@@ -598,7 +598,7 @@ data "aws_iam_policy_document" "emac_di_permissions" {
       "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/data_insights*",
     ] : local.is-preproduction ? [
       "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/acquistive_crime*",
-      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:databasefi/data_insights*",
+      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/data_insights*",
     ] : []
   }
   statement {
