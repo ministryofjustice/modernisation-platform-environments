14343f9bc535fdd20c5b316ab4e7f74c98a5683f
ghill26 <George.Hill2@justice.gov.uk>
2026-05-08 11:14:50 +0100

    TM-2061: Initial commit for qa12c-nomis-web-a config

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -183,6 +183,28 @@ locals {
           oracle-db-hostname-b = "none"
         })
       })
+
+      qa12c-nomis-web-a = merge(local.ec2_autoscaling_groups.qa12c-nomis-web, {
+        autoscaling_schedules = {}
+        config = merge(local.ec2_autoscaling_groups.qa12c-nomis-web.config, {
+          instance_profile_policies = concat(local.ec2_instances.db.config.instance_profile_policies, [
+            "Ec2Qa11GWeblogicPolicy",
+            "Ec2Qa11G2WeblogicPolicy",
+            "Ec2Qa19CWeblogicPolicy",
+          ])
+        })
+        user_data_cloud_init = merge(local.ec2_autoscaling_groups.qa12c-nomis-web.user_data_cloud_init, {
+          args = merge(local.ec2_autoscaling_groups.qa12c-nomis-web.user_data_cloud_init.args, {
+            branch = "main"
+          })
+        })
+        tags = merge(local.ec2_autoscaling_groups.qa12c-nomis-web.tags, {
+          nomis-environment    = "qa11g"
+          oracle-db-name       = "qa11g"
+          oracle-db-hostname-a = "dev-nomis-db19c-1-b"
+          oracle-db-hostname-b = "none"
+        })
+      })
     }
 
     ec2_instances = {
