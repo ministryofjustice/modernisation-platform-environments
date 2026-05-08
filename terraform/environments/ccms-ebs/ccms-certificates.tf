2869cd0c22075159b93ecdea1b52b0fc5000f979
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-05-08 17:29:32 +0100

    fix: apply cert warning on ebs test

--- terraform/environments/ccms-ebs/ccms-certificates.tf
+++ terraform/environments/ccms-ebs/ccms-certificates.tf
@@ -81,3 +81,17 @@ resource "aws_acm_certificate_validation" "external_prod" {
     create = "10m"
   }
 }
+
+ resource "aws_cloudwatch_event_rule" "certificate_expiration_warning" {
+  name = "${local.application_name}-certificate-expiration-warning"
+  event_pattern = jsonencode({
+    "source" : ["aws.acm"],
+    "detail-type" : ["ACM Certificate Approaching Expiration", "ACM Certificate Expired", "ACM Certificate Renewal Failed"]
+  })
+}
+
+resource "aws_cloudwatch_event_target" "certificate_expiration_warning_to_sns" {
+  rule = aws_cloudwatch_event_rule.certificate_expiration_warning.name
+  target_id = "certificate-expiration-warning-target"
+  arn  = aws_lambda_function.cloudwatch_sns.arn
+}
