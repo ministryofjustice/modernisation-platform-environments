59d0bcb7ecada950a8fc3017cc7fac56e2afe038
srikanththummala0470 <Srikanth.Thummala@justice.gov.uk>
2026-05-11 15:31:39 +0100

    CC-4404: Remove soa_oracle_19 option group resource to resolve stuck deletion
    The old option group (soa-db-option-group with name_prefix) is held by RDS
    automated snapshots and cannot be deleted. Removing it from Terraform code;
    terraform state rm will be used to stop Terraform from attempting deletion.
    The DB instance now references soa_oracle_20 with a fixed name.

--- terraform/environments/laa-ccms-soa/soa_database.tf
+++ terraform/environments/laa-ccms-soa/soa_database.tf
@@ -7,57 +7,6 @@ resource "aws_db_subnet_group" "soa" {
   }
 }
 
-resource "aws_db_option_group" "soa_oracle_19" {
-  name_prefix          = "soa-db-option-group"
-  engine_name          = "oracle-ee"
-  major_engine_version = "19"
-
-  option {
-    option_name = "JVM"
-  }
-
-  option {
-    option_name = "S3_INTEGRATION"
-    port        = 0
-    version     = "1.0"
-  }
-
-  option {
-    option_name = "OEM_AGENT"
-
-    port    = tonumber(local.application_data.accounts[local.environment].oem.agent_port)
-    version = "13.5.0.0.v1"
-
-    vpc_security_group_memberships = [
-      aws_security_group.soa_db.id
-    ]
-
-    option_settings {
-      name  = "MINIMUM_TLS_VERSION"
-      value = "TLSv1"
-    }
-
-    option_settings {
-      name  = "AGENT_REGISTRATION_PASSWORD"
-      value = jsondecode(data.aws_secretsmanager_secret_version.ccms_soa_quiesced_secrets_current.secret_string)["agent_registration_password"]
-    }
-
-    option_settings {
-      name  = "OMS_HOST"
-      value = local.application_data.accounts[local.environment].oem.oms_host
-    }
-
-    option_settings {
-      name  = "OMS_PORT"
-      value = local.application_data.accounts[local.environment].oem.oms_port
-    }
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_db_option_group" "soa_oracle_20" {
   name                 = "soa-db-option-group-oracle19"
   engine_name          = "oracle-ee"
