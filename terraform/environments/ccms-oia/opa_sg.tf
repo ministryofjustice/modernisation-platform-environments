ac3d48d616926e6f94cb8e13ee9f64af62de6607
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-05-11 12:49:03 +0100

    fix: allowing 7001 temp from workspace

--- terraform/environments/ccms-oia/opa_sg.tf
+++ terraform/environments/ccms-oia/opa_sg.tf
@@ -20,6 +20,18 @@ resource "aws_vpc_security_group_ingress_rule" "alb_ingress_7001_all" {
   to_port           = 7001
   description       = "7001 from AWS Workspaces"
 }
+
+# # Temp only for DEV
+resource "aws_vpc_security_group_ingress_rule" "alb_ingress_7001_preproduction_all" {
+  count             = local.is-preproduction ? 1 : 0
+  security_group_id = aws_security_group.opahub_load_balancer.id
+  cidr_ipv4         = local.application_data.accounts[local.environment].aws_workspace
+  ip_protocol       = "tcp"
+  from_port         = 7001
+  to_port           = 7001
+  description       = "7001 from AWS Workspaces"
+}
+
 resource "aws_vpc_security_group_ingress_rule" "alb_ingress_443_all" {
   security_group_id = aws_security_group.opahub_load_balancer.id
   cidr_ipv4         = "0.0.0.0/0"
