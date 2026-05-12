f4025006121b553da7a4c4e2ecf6574068475cc9
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-12 16:14:13 +0000

    feat(data-lake-storage): refine data lake settings and update trusted resource owners for dynamic configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-factory-moj/data-lake-settings.tf
+++ terraform/environments/data-factory-moj/data-lake-settings.tf
@@ -4,6 +4,7 @@ module "data_lake_settings" {
   source = "git::https://github.com/ministryofjustice/terraform-aws-data-platform-lakeformation.git//modules/data-lake-settings?ref=feat/data-lake-storage"
 
   admins = [
+    // TODO: check the github-actions-* principals are actually required.
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-plan",
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-apply",
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/MemberInfrastructureAccess",
@@ -15,6 +16,6 @@ module "data_lake_settings" {
   ]
 
   trusted_resource_owners = [
-    local.environment_management.account_ids["data-platform-governance-development"] // TODO: make dynamic
+    local.environment_management.account_ids["data-platform-governance-development"]
   ]
 }
--- terraform/environments/data-platform-governance/lakeformation/data-lake-settings.tf
+++ terraform/environments/data-platform-governance/lakeformation/data-lake-settings.tf
@@ -2,6 +2,7 @@ module "data_lake_settings" {
   source = "git::https://github.com/ministryofjustice/terraform-aws-data-platform-lakeformation.git//modules/data-lake-settings?ref=feat/data-lake-storage"
 
   admins = [
+    // TODO: check the github-actions-* principals are actually required.
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-plan",
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-apply",
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/MemberInfrastructureAccess",
@@ -9,6 +10,6 @@ module "data_lake_settings" {
   ]
 
   trusted_resource_owners = [
-    for factory in local.lakeformation_configuration.factories : local.environment_management.account_ids[factory]
+    for factory in can(keys(local.lakeformation_configuration.factories)) ? keys(local.lakeformation_configuration.factories) : local.lakeformation_configuration.factories : local.environment_management.account_ids[factory]
   ]
 }
