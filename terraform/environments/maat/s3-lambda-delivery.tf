a0e63725995eccbaea73889965655c375c9f5773
srikanththummala0470 <Srikanth.Thummala@justice.gov.uk>
2026-05-12 11:39:43 +0100

    CC-4517: Remove moved blocks - bucket manually deleted, will be recreated by module

--- terraform/environments/maat/s3-lambda-delivery.tf
+++ terraform/environments/maat/s3-lambda-delivery.tf
@@ -2,21 +2,6 @@
 # Note: upload lambda_delivery/cloudwatch_sns_layer/layerV1.zip manually before first apply
 # See: https://dsdmoj.atlassian.net/wiki/spaces/LDD/pages/5975606239/Build+Layered+Function+for+Lambda
 
-moved {
-  from = aws_s3_bucket.maat_shared
-  to   = module.s3-bucket-shared.aws_s3_bucket.default
-}
-
-moved {
-  from = aws_s3_bucket_public_access_block.maat_shared
-  to   = module.s3-bucket-shared.aws_s3_bucket_public_access_block.default
-}
-
-moved {
-  from = aws_s3_bucket_versioning.maat_shared
-  to   = module.s3-bucket-shared.aws_s3_bucket_versioning.default
-}
-
 module "s3-bucket-shared" {
   # v9.0.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/9facf9fc8f8b8e3f93ffbda822028534b9a75399
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=9facf9fc8f8b8e3f93ffbda822028534b9a75399"
