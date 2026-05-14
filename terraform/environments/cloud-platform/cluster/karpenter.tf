e123fdfb16b65200352a43d26bbd1e8856693dc0
Tom Smith <51861897+tom-j-smith@users.noreply.github.com>
2026-05-14 13:50:43 +0100

    Update karpenter.tf
    Update to correct module release

--- terraform/environments/cloud-platform/cluster/karpenter.tf
+++ terraform/environments/cloud-platform/cluster/karpenter.tf
@@ -1,6 +1,6 @@
 module "karpenter" {
   count   = contains(local.enabled_workspaces, local.cluster_environment) ? 1 : 0
-  source  = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=initial-module"
+  source  = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=0.1.0"
 
   cluster_name = local.cluster_name
   cluster_endpoint = module.eks[0].cluster_endpoint
