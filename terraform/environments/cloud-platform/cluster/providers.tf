478e3356ec53cca778f61111fc9c0bb41d06d80b
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-05-15 15:42:18 +0100

    Updating Kubectl provider

--- terraform/environments/cloud-platform/cluster/providers.tf
+++ terraform/environments/cloud-platform/cluster/providers.tf
@@ -13,8 +13,8 @@ provider "helm" {
 }
 
 provider "kubectl" {
-  host                   = try(data.aws_eks_cluster.cluster[0].endpoint, null)
-  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster[0].certificate_authority[0].data), null)
-  token                  = try(data.aws_eks_cluster_auth.cluster[0].token, null)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
 }
