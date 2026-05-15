0b6c5a367415ab3b3c0234440c1e742efb10e3a2
Tom Ogle <tom.ogle@justice.gov.uk>
2026-05-15 11:22:22 +0100

    PDHD-162: Description for outputs and reverse formatting changes.

--- terraform/environments/digital-prison-reporting/modules/data_source_secret/outputs.tf
+++ terraform/environments/digital-prison-reporting/modules/data_source_secret/outputs.tf
@@ -1,17 +1,21 @@
 output "secret_arn" {
-  value = aws_secretsmanager_secret.this.arn
+  value       = aws_secretsmanager_secret.this.arn
+  description = "The ARN of the secret"
 }
 
 output "secret_id" {
-  value = aws_secretsmanager_secret.this.id
+  value       = aws_secretsmanager_secret.this.id
+  description = "The ID of the secret"
 }
 
 output "secret_name" {
-  value = aws_secretsmanager_secret.this.name
+  value       = aws_secretsmanager_secret.this.name
+  description = "The name of the secret"
 }
 
 output "secret_version_id" {
-  value = aws_secretsmanager_secret_version.this.version_id
+  value       = aws_secretsmanager_secret_version.this.version_id
+  description = "The ID of the secret version"
 }
 
 output "secret_contents_endpoint" {
