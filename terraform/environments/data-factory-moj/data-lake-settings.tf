2223c906a9cc39bb1dc3e3a0c302720dfc5f41d0
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-19 17:21:52 +0000

    feat: add GitHub Actions principals to data lake settings
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-factory-moj/data-lake-settings.tf
+++ terraform/environments/data-factory-moj/data-lake-settings.tf
@@ -4,6 +4,9 @@ module "data_lake_settings" {
   source = "git::https://github.com/ministryofjustice/terraform-aws-data-platform-lakeformation.git//modules/data-lake-settings?ref=feat/data-lake-storage"
 
   admins = [
+    // TODO: check the github-actions-* principals are actually required.
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-plan",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-apply",
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/MemberInfrastructureAccess",
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.platform_engineer_admin_sso_role.names)}",
   ]
