69195a3064ed72b14a94d04a47d7d54a0f4f808b
dan-criddle <dan.criddle@justice.gov.uk>
2026-05-20 12:40:41 +0100

    Bump Karpenter module

--- terraform/environments/cloud-platform/cluster/karpenter.tf
+++ terraform/environments/cloud-platform/cluster/karpenter.tf
@@ -1,11 +1,9 @@
 module "karpenter" {
   count   = contains(local.enabled_workspaces, local.cluster_environment) ? 1 : 0
-  source  = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=74ef416c6d9bc07f2a59445b078d8eb851b7fdfc" #unversioned max_pods_per_node
-
+  source  = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=74ef416c6d9bc07f2a59445b078d8eb851b7fdfc" #0.1.1
   cluster_name = local.cluster_name
   cluster_endpoint = module.eks[0].cluster_endpoint
   k8s_version = local.environment_configuration.eks_cluster_version
-  max_pods_per_node = 31
 
   depends_on = [ module.eks ]
 }
