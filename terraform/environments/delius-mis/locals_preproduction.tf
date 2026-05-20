7653080e0cecac8ebb82677822ec5363ce6c68be
ghill26 <George.Hill2@justice.gov.uk>
2026-05-20 10:02:36 +0100

    TM-2084: Limit initial PR to MIS DB

--- terraform/environments/delius-mis/locals_preproduction.tf
+++ terraform/environments/delius-mis/locals_preproduction.tf
@@ -276,8 +276,8 @@ locals {
 
   # BOE DB config
   boe_db_config_preprod = {
-    primary_instance_count = 1
-    standby_instance_count = 1
+    primary_instance_count = 0
+    standby_instance_count = 0
     instance_type          = "m7i.large"
     ami_name_regex         = "^delius_core_ol_8_5_oracle_db_19c_patch_2024-01-31T16-06-00.575Z"
 
@@ -322,8 +322,8 @@ locals {
 
   # DSD DB config
   dsd_db_config_preprod = {
-    primary_instance_count = 1
-    standby_instance_count = 1
+    primary_instance_count = 0
+    standby_instance_count = 0
     instance_type          = "r7i.large"
     ami_name_regex         = "^delius_core_ol_8_5_oracle_db_19c_patch_2024-01-31T16-06-00.575Z"
 
