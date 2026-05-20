2b3e932f0501451b177fc06baaf6a98787c997ca
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-20 07:28:57 +0000

    feat: refactor data factory configuration to use databases instead of products
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform-governance/lakeformation/databases.tf
+++ terraform/environments/data-platform-governance/lakeformation/databases.tf
@@ -2,49 +2,53 @@ resource "aws_glue_catalog_database" "main" {
   for_each = tomap({
     for database in flatten([
       for factory_name, factory in try(local.lakeformation_configuration.factories, {}) : [
-        for product_name, product in try(factory.products, {}) : {
+        for database_name, database in try(factory.databases, {}) : {
           factory_name = factory_name
           domain       = factory.domain
-          product      = product
-          product_name = product_name
-          name         = "${factory.domain}-${product_name}"
+          database      = database
+          database_name = database_name
+          name         = "${factory.domain}-${database_name}"
         }
       ]
     ]) : database.name => database
   })
 
   name         = each.value.name
-  location_uri = "s3://moj-data-lake-${local.environment_management.account_ids[each.value.factory_name]}-${data.aws_region.current.region}-an/${each.value.domain}/${each.value.product_name}/"
+  location_uri = "s3://moj-data-lake-${local.environment_management.account_ids[each.value.factory_name]}-${data.aws_region.current.region}-an/${each.value.domain}/${each.value.database_name}/"
 
   tags = merge(
     local.tags,
     {
       "justice-data-factory"          = each.value.factory_name
       "justice-data-lake-domain"      = each.value.domain
-      "justice-data-platform-project" = each.value.product.project
+      "justice-data-lake-database"     = each.value.database_name
+      "justice-data-platform-project" = each.value.database.project
     }
   )
 }
 
 resource "aws_lakeformation_permissions" "database" {
   for_each = tomap({
-    for database in flatten([
+    for grant in flatten([
       for factory_name, factory in try(local.lakeformation_configuration.factories, {}) : [
-        for product_name, product in try(factory.products, {}) : {
-          factory_name = factory_name
-          domain       = factory.domain
-          product      = product
-          product_name = product_name
-          name         = "${factory.domain}-${product_name}"
-        }
+        for database_name, database in try(factory.databases, {}) : [
+          for principal in try(database.principals, []) : {
+            factory_name = factory_name
+            domain       = factory.domain
+            principal    = principal
+            database      = database
+            database_name = database_name
+            name         = "${factory.domain}-${database_name}"
+          }
+        ]
       ]
-    ]) : database.name => database
+    ]) : "${grant.name}-${grant.principal}" => grant
   })
 
-  principal   = local.environment_management.account_ids[each.value.factory_name]
+  principal   = "arn:aws:iam::${local.environment_management.account_ids[each.value.factory_name]}:role/${each.value.principal}"
   permissions = ["DESCRIBE", "CREATE_TABLE"]
 
   database {
-    name = aws_glue_catalog_database.main[each.key].name
+    name = aws_glue_catalog_database.main[each.value.name].name
   }
 }
