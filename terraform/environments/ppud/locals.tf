ca70dc4d53a585dbfc457208bfffc4176b32bc80
nbuckingham72 <nick.buckingham@colt.net>
2026-05-21 08:38:26 +0100

    Update_210526_2

--- terraform/environments/ppud/locals.tf
+++ terraform/environments/ppud/locals.tf
@@ -1,6 +1,6 @@
 #### This file can be used to store locals specific to the member account ####
 
 locals {
-  ses_iam_user    = local.application_data.accounts[local.environment].ses_iam_user
-  ses_secret_name = local.application_data.accounts[local.environment].ses_secret_name
+  ses_iam_user    = try(local.application_data.accounts[local.environment].ses_iam_user, null)
+  ses_secret_name = try(local.application_data.accounts[local.environment].ses_secret_name, null)
 }
