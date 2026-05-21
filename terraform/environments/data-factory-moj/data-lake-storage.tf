26cc8a8fbb7e3f5ff702c7341288e0d5f7f781b4
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-21 14:55:20 +0000

    feat: add dbt access trusted role ARN to data lake storage module
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-factory-moj/data-lake-storage.tf
+++ terraform/environments/data-factory-moj/data-lake-storage.tf
@@ -5,4 +5,5 @@ module "data_lake_storage" {
 
   data_platform_governance_account_id = local.environment_management.account_ids["data-platform-governance-development"]
   data_platform_account_id            = local.environment_management.account_ids["data-platform-development"]
+  dbt_access_trusted_role_arn         = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.platform_engineer_admin_sso_role.names)}"
 }
