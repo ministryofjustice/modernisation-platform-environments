1b70be62feb1eda81d65e379df57cfd96e79d8d3
nbuckingham72 <nick.buckingham@colt.net>
2026-05-21 08:31:58 +0100

    Update_210526_1

--- terraform/environments/ppud/iam.tf
+++ terraform/environments/ppud/iam.tf
@@ -185,6 +185,9 @@ resource "aws_iam_user" "email" {
 resource "aws_iam_access_key" "email" {
   count = local.is-production == false ? 1 : 0
   user  = aws_iam_user.email[0].name
+    lifecycle {  # Key now managed by Lambda and not Terraform
+      ignore_changes = all
+  }
 }
 
 #tfsec:ignore:aws-iam-no-policy-wildcards
--- terraform/environments/ppud/locals.tf
+++ terraform/environments/ppud/locals.tf
@@ -1,1 +1,6 @@
 #### This file can be used to store locals specific to the member account ####
+
+locals {
+  ses_iam_user    = local.application_data.accounts[local.environment].ses_iam_user
+  ses_secret_name = local.application_data.accounts[local.environment].ses_secret_name
+}
