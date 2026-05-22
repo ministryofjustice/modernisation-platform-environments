0e91ab02b7b1a6e7be55a72ae6d5d94ae2f17252
David Elliott <david.elliott@digital.justice.gov.uk>
2026-05-22 13:16:26 +0100

    Add kubernetes provider version

--- terraform/environments/cloud-platform/cluster/versions.tf
+++ terraform/environments/cloud-platform/cluster/versions.tf
@@ -33,6 +33,11 @@ terraform {
       version = ">= 3.0"
     }
 
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+
     cloudinit = {
       version = "~> 2.0"
       source  = "hashicorp/cloudinit"
