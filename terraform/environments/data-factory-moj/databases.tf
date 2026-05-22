de78f65118e04ce913ce94645b52e532bc6c76c9
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-22 13:29:26 +0000

    feat: comment out Glue catalog database resource and permissions configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-factory-moj/databases.tf
+++ terraform/environments/data-factory-moj/databases.tf
@@ -1,42 +1,46 @@
-resource "aws_glue_catalog_database" "main" {
-  for_each = tomap(try(local.data_platform_lakeformation_configuration.databases, {}))
+# resource "aws_glue_catalog_database" "main" {
+#   for_each = tomap(try(local.data_platform_lakeformation_configuration.databases, {}))
 
-  name = "${local.data_platform_lakeformation_configuration.domain}-${each.key}"
+#   name = "${local.data_platform_lakeformation_configuration.domain}-${each.key}"
 
-  target_database {
-    catalog_id    = local.environment_management.account_ids["data-platform-governance-${local.environment_configuration.data_lake_environment}"]
-    database_name = "${local.data_platform_lakeformation_configuration.domain}-${each.key}"
-  }
+#   target_database {
+#     catalog_id    = local.environment_management.account_ids["data-platform-governance-${local.environment_configuration.data_lake_environment}"]
+#     database_name = "${local.data_platform_lakeformation_configuration.domain}-${each.key}"
+#   }
 
-  tags = merge(
-    local.tags,
-    {
-      "justice-data-factory"          = "${local.application_name}-${local.environment}"
-      "justice-data-lake-domain"      = local.data_platform_lakeformation_configuration.domain
-      "justice-data-lake-database"    = each.key
-      "justice-data-platform-project" = each.value.project
-    }
-  )
-}
+#   tags = merge(
+#     local.tags,
+#     {
+#       "justice-data-factory"          = "${local.application_name}-${local.environment}"
+#       "justice-data-lake-domain"      = local.data_platform_lakeformation_configuration.domain
+#       "justice-data-lake-database"    = each.key
+#       "justice-data-platform-project" = each.value.project
+#     }
+#   )
+# }
 
-resource "aws_lakeformation_permissions" "database" {
-  for_each = tomap({
-    for grant in flatten([
-      for database_name, database in try(local.data_platform_lakeformation_configuration.databases, {}) : [
-        for principal_name, principal in try(database.principals, {}) : {
-          database_name = database_name
-          name          = database_name
-          permissions   = try(principal.permissions.resource_link, [])
-          principal     = principal_name
-        } if length(try(principal.permissions.resource_link, [])) > 0
-      ]
-    ]) : "${grant.name}-${grant.principal}" => grant
-  })
+# resource "aws_lakeformation_permissions" "database" {
+#   for_each = tomap({
+#     for grant in flatten([
+#       for database_name, database in try(local.data_platform_lakeformation_configuration.databases, {}) : [
+#         for principal_name, principal in try(database.principals, {}) : {
+#           database_name = database_name
+#           name          = database_name
+#           permissions   = try(principal.permissions.resource_link, [])
+#           principal     = principal_name
+#         } if length(try(principal.permissions.resource_link, [])) > 0
+#       ]
+#     ]) : "${grant.name}-${grant.principal}" => grant
+#   })
 
-  principal   = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${each.value.principal}"
-  permissions = each.value.permissions
+#   principal   = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${each.value.principal}"
+#   permissions = each.value.permissions
 
-  database {
-    name = aws_glue_catalog_database.main[each.value.database_name].name
-  }
+#   database {
+#     name = aws_glue_catalog_database.main[each.value.database_name].name
+#   }
+# }
+
+resource "aws_glue_catalog_database" "test" {
+  name = "test"
 }
