08fcbcb0906dcae06cc2fe59391ebbb2c6ce87d9
lucy-astley-jones <lucy.astleyjones@justice.gov.uk>
2026-05-22 17:49:25 +0100

    add event lambda

--- terraform/environments/electronic-monitoring-data/lambdas_main.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_main.tf
@@ -899,14 +899,36 @@ module "landing_file_dlq_redriver" {
 # lambda loads - staged_mdss__position and acquisitive_crime__position
 #-----------------------------------------------------------------------------------
 
-module "merge_mdss_staged" {
+module "merge_mdss_staged_event" {
   count                          = local.is-preproduction || local.is-production ? 0 : 1
   source                         = "./modules/lambdas"
   is_image                       = true
-  function_name                  = "merge_mdss_staged"
+  function_name                  = "merge_mdss_staged_event"
   role_name                      = aws_iam_role.merge_load.name
   role_arn                       = aws_iam_role.merge_load.arn
-  handler                        = "merge_mdss_staged.handler"
+  handler                        = "merge_mdss_staged_event.handler"
+  memory_size                    = 1024
+  timeout                        = 900
+  reserved_concurrent_executions = 1
+  core_shared_services_id        = local.environment_management.account_ids["core-shared-services-production"]
+  production_dev                 = local.is-production ? "prod" : local.is-preproduction ? "preprod" : local.is-test ? "test" : "dev"
+  security_group_ids             = [aws_security_group.lambda_generic.id]
+  subnet_ids                     = data.aws_subnets.shared-private.ids
+  cloudwatch_retention_days      = 7
+  environment_variables = {
+    MOD_PLAT_ACCOUNT_ALIAS  = terraform.workspace
+    MOD_PLAT_ACCOUNT_NUMBER = local.env_account_id
+  }
+}
+
+module "merge_mdss_staged_position" {
+  count                          = local.is-preproduction || local.is-production ? 0 : 1
+  source                         = "./modules/lambdas"
+  is_image                       = true
+  function_name                  = "merge_mdss_staged_position"
+  role_name                      = aws_iam_role.merge_load.name
+  role_arn                       = aws_iam_role.merge_load.arn
+  handler                        = "merge_mdss_staged_position.handler"
   memory_size                    = 1024
   timeout                        = 900
   reserved_concurrent_executions = 1
