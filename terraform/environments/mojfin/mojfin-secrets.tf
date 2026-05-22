9693bf85dbb93179cd5f6f11d9a94d081dc4a9bf
srikanththummala0470 <Srikanth.Thummala@justice.gov.uk>
2026-05-22 11:38:29 +0100

    Move GuardDuty Slack secret resources to mojfin-secrets.tf

--- terraform/environments/mojfin/mojfin-secrets.tf
+++ terraform/environments/mojfin/mojfin-secrets.tf
@@ -21,3 +21,26 @@ resource "aws_secretsmanager_secret_version" "rds_password_secret_version" {
     }
   )
 }
+
+resource "aws_secretsmanager_secret" "guardduty_slack_secret" {
+  name                    = "${local.application_name}-${local.environment}-guardduty-slack"
+  description             = "Slack webhook URLs for GuardDuty and CloudWatch alerts (laa-alerts-guardduty-nonprod or laa-alerts-guardduty-prod)"
+  recovery_window_in_days = local.is-production ? 30 : 0
+
+  tags = merge(local.tags, {
+    Name = "${local.application_name}-${local.environment}-guardduty-slack"
+  })
+}
+
+resource "aws_secretsmanager_secret_version" "guardduty_slack_secret" {
+  secret_id = aws_secretsmanager_secret.guardduty_slack_secret.id
+  secret_string = jsonencode({
+    "slack_channel_webhook"           = ""
+    "slack_channel_webhook_guardduty" = ""
+    "slack_channel_webhook_s3"        = ""
+  })
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
