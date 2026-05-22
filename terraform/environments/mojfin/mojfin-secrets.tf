bb4df804781e8d0888a280a6c7e2d6b76136f46a
srikanththummala0470 <Srikanth.Thummala@justice.gov.uk>
2026-05-22 11:46:03 +0100

    Rename guardduty_slack_secret version resource to guardduty_slack_secret_version

--- terraform/environments/mojfin/mojfin-secrets.tf
+++ terraform/environments/mojfin/mojfin-secrets.tf
@@ -32,7 +32,7 @@ resource "aws_secretsmanager_secret" "guardduty_slack_secret" {
   })
 }
 
-resource "aws_secretsmanager_secret_version" "guardduty_slack_secret" {
+resource "aws_secretsmanager_secret_version" "guardduty_slack_secret_version" {
   secret_id = aws_secretsmanager_secret.guardduty_slack_secret.id
   secret_string = jsonencode({
     "slack_channel_webhook"           = ""
