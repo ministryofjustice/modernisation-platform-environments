1ff7dcc6075a267d3117f374d4e323be040a9dfa
JP Fletcher <johnpaul.fletcher@justice.gov.uk>
2026-05-22 09:24:22 +0100

    Clone of Tariff UAT for weblogic build

---
+++ terraform/environments/cica-tariff/tariff_ec2_app_uat_clone.tf
@@ -0,0 +1,51 @@
+# Clone of UAT App server for Weblogic build: CDI-357
+resource "aws_instance" "tariff_app_uat_clone" {
+  count = local.environment == "test" ? 1 : 0
+  ami   = "ami-08f6f2c21e2f16c68" # created 2am 22-May-2026
+  lifecycle {
+    ignore_changes = [user_data] # ami removed to allow restore
+  }
+  associate_public_ip_address = false
+  ebs_optimized               = true
+
+  iam_instance_profile   = aws_iam_instance_profile.tariff_instance_profile.name
+  instance_type          = "m5.2xlarge"
+  key_name               = aws_key_pair.key_pair_app.key_name
+  monitoring             = true
+  subnet_id              = data.aws_subnet.private_subnets_a.id
+  # vpc_security_group_ids = [module.tariff_app_security_group[0].security_group_id]
+  vpc_security_group_ids = [aws_security_group.temp_uat_ssm_only[0].id] # TEMPORARY ASSIGNMENT FOR NO NETWORK (except SSM)
+
+  # private_ip = ""
+
+  tags = merge(tomap({
+    "Name"     = lower(format("ec2-%s-%s-app-clone", local.application_name, local.environment)),
+    "hostname" = "${local.application_name}-app-poc-clone",
+    }), local.tags, local.environment != "test" ? tomap({ "backup" = "true" }) : tomap({})
+  )
+}
+# Temporary SG to restrict access to/from Clone above during configuration phase
+resource "aws_security_group" "temp_uat_ssm_only" {
+  count       = local.environment == "test" ? 1 : 0
+  name        = "temp-ssm-only"
+  description = "Allow only SSM traffic - temporary rule during Tariff App POC clone intial config"
+  vpc_id      = data.aws_vpc.shared.id
+}
+data "aws_security_group" "core_vpc_protected" {
+  provider = aws.core-vpc
+
+  tags = {
+    Name = "${local.vpc_name}-${local.environment}-int-endpoint"
+  }
+}
+resource "aws_security_group_rule" "temp_uat_ssm_only_egress" {
+  count             = local.environment == "test" ? 1 : 0
+  security_group_id = aws_security_group.temp_uat_ssm_only[0].id
+
+  description              = "${local.application_name}-app-clone_egress_to_interface_endpoints"
+  type                     = "egress"
+  from_port                = "443"
+  to_port                  = "443"
+  protocol                 = "TCP"
+  source_security_group_id = data.aws_security_group.core_vpc_protected.id
+}
