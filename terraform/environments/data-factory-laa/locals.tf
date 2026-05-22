ca8db4df3ae06979aaf0bd2742edcab33cf9c7b6
zerj9 <96551236+zerj9@users.noreply.github.com>
2026-05-22 11:21:40 +0100

    data lake database

--- terraform/environments/data-factory-laa/locals.tf
+++ terraform/environments/data-factory-laa/locals.tf
@@ -1,1 +1,5 @@
 #### This file can be used to store locals specific to the member account ####
+locals {
+  current_account_id     = data.aws_caller_identity.current.account_id
+  current_account_region = data.aws_region.current.region
+}
