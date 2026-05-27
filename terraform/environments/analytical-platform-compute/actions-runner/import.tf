c1ca8d1939c63bd1d074c8dbd3abad651683fa0d
Raghavendra Kamath <raghavvidya@gmail.com>
2026-05-27 15:37:33 +0100

    run import only if prod

--- terraform/environments/analytical-platform-compute/actions-runner/import.tf
+++ terraform/environments/analytical-platform-compute/actions-runner/import.tf
@@ -1,7 +1,8 @@
 # This file is used to import the existing Kubernetes namespace for actions runners into Terraform state.Will be deleted after the import is complete and the state file is updated.
 import {
-  to = kubernetes_namespace_v1.actions_runners[0]
-  id = "actions-runners"
+  for_each = terraform.workspace == "analytical-platform-compute-production" ? { "0" = "actions-runners" } : {}
+  to       = kubernetes_namespace_v1.actions_runners[0]
+  id       = each.value
 }
 
 removed {
