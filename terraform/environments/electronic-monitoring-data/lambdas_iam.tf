ccf977a08e8545f700279d497f353d38791648c4
jono-basford-moj <jono.basford@justice.gov.uk>
2026-05-27 15:47:32 +0100

    Further updates to iam role

--- terraform/environments/electronic-monitoring-data/lambdas_iam.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_iam.tf
@@ -2512,7 +2512,7 @@ data "aws_iam_policy_document" "specials_ingestion_lambda_role_policy_document" 
       "athena:StopQueryExecution"
     ]
     resources = [
-      "arn:aws:athena:${data.aws_region.current.region}:${data.aws_caller_identity.current.id}:workgroup/${data.aws_caller_identity.current.id}-*",
+      "*",
     ]
   }
   statement {
