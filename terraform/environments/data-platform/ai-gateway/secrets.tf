fbe9abe51587cd57e528227d3dea02bc4e038796
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-05-28 07:42:30 +0000

    refactor: consume component_name

--- terraform/environments/data-platform/ai-gateway/secrets.tf
+++ terraform/environments/data-platform/ai-gateway/secrets.tf
@@ -1,7 +1,7 @@
 module "litellm_license_secret" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=d03382d3ec9c12b849fbbe35b770eaa047f7bbea" # v2.1.0
 
-  name = "ai-gateway/litellm-license"
+  name = "${local.component_name}/litellm-license"
 
   secret_string         = "CHANGEME"
   ignore_secret_changes = true
@@ -10,7 +10,7 @@ module "litellm_license_secret" {
 module "litellm_salt_key_secret" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=d03382d3ec9c12b849fbbe35b770eaa047f7bbea" # v2.1.0
 
-  name = "ai-gateway/litellm-salt-key"
+  name = "${local.component_name}/litellm-salt-key"
 
   secret_string = random_password.litellm_salt_key.result
 }
@@ -18,7 +18,7 @@ module "litellm_salt_key_secret" {
 module "litellm_entra_id_secret" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=d03382d3ec9c12b849fbbe35b770eaa047f7bbea" # v2.1.0
 
-  name = "ai-gateway/litellm-entra-id"
+  name = "${local.component_name}/litellm-entra-id"
 
   secret_string = jsonencode({
     client_id      = "CHANGEME"
