a3447e6eeab1f2d635a7acace95433375ecb10ef
David Elliott <david.elliott@digital.justice.gov.uk>
2026-05-28 13:13:22 +0100

    Remove ARM node group

--- terraform/environments/cloud-platform/cluster/environment-configuration.tf
+++ terraform/environments/cloud-platform/cluster/environment-configuration.tf
@@ -13,8 +13,7 @@ locals {
       }
 
       /* Nodes */
-      ami_type     = "AL2023_x86_64_STANDARD"
-      ami_type_arm = "BOTTLEROCKET_ARM_64"
+      ami_type = "AL2023_x86_64_STANDARD"
 
       default_ng = {
         min_size         = 2
@@ -22,35 +21,6 @@ locals {
         max_size         = 10
 
         instance_types = ["r8i.large"]
-
-        block_device_mappings = {
-          xvda = {
-            device_name = "/dev/xvda"
-            ebs = {
-              volume_size           = 200
-              volume_type           = "gp3"
-              iops                  = 0
-              encrypted             = false
-              kms_key_id            = ""
-              delete_on_termination = true
-            }
-          }
-        }
-
-        labels = {
-          Terraform                                      = "true"
-          "cloud-platform.justice.gov.uk/default-ng"     = "true"
-          "container-platform.justice.gov.uk/default-ng" = "true"
-          Cluster                                        = local.environment
-        }
-      }
-
-      default_ng_arm = {
-        min_size         = 2
-        desired_capacity = 3
-        max_size         = 10
-
-        instance_types = ["m8g.large"]
 
         block_device_mappings = {
           xvda = {
@@ -147,7 +117,123 @@ locals {
         }
       }
     }
+    development = {
+      /* EKS */
+      eks_cluster_version = "1.35"
 
+      /* Addons */
+      eks_cluster_addon_versions = {
+        kube_proxy             = "v1.34.2-eksbuild.1"
+        vpc_cni                = "v1.20.1-eksbuild.1"
+        coredns                = "v1.12.2-eksbuild.4"
+        eks_pod_identity_agent = "v1.3.8-eksbuild.2"
+      }
+
+      /* Nodes */
+      ami_type = "AL2023_x86_64_STANDARD"
+
+      default_ng = {
+        min_size         = 2
+        desired_capacity = 3
+        max_size         = 10
+
+        instance_types = ["r8i.large"]
+
+        block_device_mappings = {
+          xvda = {
+            device_name = "/dev/xvda"
+            ebs = {
+              volume_size           = 200
+              volume_type           = "gp3"
+              iops                  = 0
+              encrypted             = false
+              kms_key_id            = ""
+              delete_on_termination = true
+            }
+          }
+        }
+
+        labels = {
+          Terraform                                      = "true"
+          "cloud-platform.justice.gov.uk/default-ng"     = "true"
+          "container-platform.justice.gov.uk/default-ng" = "true"
+          Cluster                                        = local.environment
+        }
+      }
+
+      monitoring_ng = {
+        min_size         = 1
+        desired_capacity = 2
+        max_size         = 5
+
+        instance_types = ["r8i.large"]
+
+        block_device_mappings = {
+          xvda = {
+            device_name = "/dev/xvda"
+            ebs = {
+              volume_size           = 140
+              volume_type           = "gp3"
+              iops                  = 0
+              encrypted             = false
+              kms_key_id            = ""
+              delete_on_termination = true
+            }
+          }
+        }
+
+        taints = {
+          monitoring = {
+            key    = "monitoring-node"
+            value  = "true"
+            effect = "NO_SCHEDULE"
+          }
+        }
+
+        labels = {
+          Terraform                                         = "true"
+          "cloud-platform.justice.gov.uk/monitoring-ng"     = "true"
+          "container-platform.justice.gov.uk/monitoring-ng" = "true"
+          Cluster                                           = local.environment
+        }
+      }
+      system_ng = {
+        min_size         = 2
+        desired_capacity = 2
+        max_size         = 4
+
+        instance_types = ["r8i.large"]
+
+        block_device_mappings = {
+          xvda = {
+            device_name = "/dev/xvda"
+            ebs = {
+              volume_size           = 140
+              volume_type           = "gp3"
+              iops                  = 0
+              encrypted             = false
+              kms_key_id            = ""
+              delete_on_termination = true
+            }
+          }
+        }
+
+        taints = {
+          monitoring = {
+            key    = "system-node"
+            value  = "true"
+            effect = "NO_SCHEDULE"
+          }
+        }
+
+        labels = {
+          Terraform                                     = "true"
+          "cloud-platform.justice.gov.uk/system-ng"     = "true"
+          "container-platform.justice.gov.uk/system-ng" = "true"
+          Cluster                                       = local.environment
+        }
+      }
+    }
     preproduction = {
       /* EKS */
       eks_cluster_version = "1.35"
