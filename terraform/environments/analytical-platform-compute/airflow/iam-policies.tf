f045a041eec31eb0c77a2c227e4363defda5d2bc
Raghavendra Kamath <raghavvidya@gmail.com>
2026-05-28 15:15:27 +0100

    deprecation fix

--- terraform/environments/analytical-platform-compute/airflow/iam-policies.tf
+++ terraform/environments/analytical-platform-compute/airflow/iam-policies.tf
@@ -105,7 +105,7 @@ data "aws_iam_policy_document" "mwaa_execution_policy" {
       "secretsmanager:DescribeSecret",
       "secretsmanager:ListSecretVersionIds"
     ]
-    resources = ["arn:aws:secretsmanager:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:secret:airflow/*"]
+    resources = ["arn:aws:secretsmanager:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:secret:airflow/*"]
   }
 }
 
