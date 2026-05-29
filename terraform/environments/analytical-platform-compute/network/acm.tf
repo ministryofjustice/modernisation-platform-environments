060d1e7a524d514aa77e6edf5c73e512872e8c43
Raghavendra Kamath <raghavvidya@gmail.com>
2026-05-29 15:28:43 +0100

    chore(deps):upgrade terraform modules and provider versions

--- terraform/environments/analytical-platform-compute/network/acm.tf
+++ terraform/environments/analytical-platform-compute/network/acm.tf
@@ -3,9 +3,9 @@ module "acm_certificate" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/acm/aws"
-  version = "5.1.1"
+  version = "6.3.0"
 
-  zone_id                   = module.route53_zones.route53_zone_zone_id[local.environment_configuration.route53_zone]
+  zone_id                   = module.route53_zone.id[local.environment_configuration.route53_zone]
   domain_name               = local.environment_configuration.route53_zone
   subject_alternative_names = ["*.${local.environment_configuration.route53_zone}"]
 
--- terraform/environments/analytical-platform-compute/network/route53-zones.tf
+++ terraform/environments/analytical-platform-compute/network/route53-zones.tf
@@ -1,16 +1,48 @@
-module "route53_zones" {
+module "route53_zone" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/route53/aws//modules/zones"
-  version = "5.0.0"
+  source  = "terraform-aws-modules/route53/aws"
+  version = "6.5.0"
 
-  zones = {
+   
     # tflint-ignore: terraform_deprecated_interpolation
-    "${local.environment_configuration.route53_zone}" = {
-      comment = local.environment_configuration.route53_zone
+   name =  "${local.environment_configuration.route53_zone}"
+   comment = local.environment_configuration.route53_zone
+
+    records = {
+    airflow = {
+      name    = "airflow"
+      type    = "CNAME"
+      ttl     = 300
+      records = [data.aws_lb.mwaa_alb.dns_name]
     }
-  }
+    _amazonses = {
+      name    = "_amazonses"
+      type    = "TXT"
+      ttl     = 600
+      records = [aws_ses_domain_identity.main.verification_token]
+    }
+    "${aws_ses_domain_dkim.main.dkim_tokens[0]}._domainkey" = {
+      name    = "${aws_ses_domain_dkim.main.dkim_tokens[0]}._domainkey"
+      type    = "CNAME"
+      ttl     = 300
+      records = ["${aws_ses_domain_dkim.main.dkim_tokens[0]}.dkim.amazonses.com"]
+    }
+    "${aws_ses_domain_dkim.main.dkim_tokens[1]}._domainkey" = {
+      name    = "${aws_ses_domain_dkim.main.dkim_tokens[1]}._domainkey"
+      type    = "CNAME"
+      ttl     = 300
+      records = ["${aws_ses_domain_dkim.main.dkim_tokens[1]}.dkim.amazonses.com"]
+    },
+    "${aws_ses_domain_dkim.main.dkim_tokens[2]}._domainkey" = {
+      name    = "${aws_ses_domain_dkim.main.dkim_tokens[2]}._domainkey"
+      type    = "CNAME"
+      ttl     = 300
+      records = ["${aws_ses_domain_dkim.main.dkim_tokens[2]}.dkim.amazonses.com"]
+    }
+}
+
   tags = local.tags
 }
 
