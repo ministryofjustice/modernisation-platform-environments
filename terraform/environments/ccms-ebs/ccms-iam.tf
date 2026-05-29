71c98701bb3f93cc1b28b68267af47b0243b54a6
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-05-29 11:37:37 +0100

    added changes to remove email_user from dev

--- terraform/environments/ccms-ebs/ccms-iam.tf
+++ terraform/environments/ccms-ebs/ccms-iam.tf
@@ -282,6 +282,7 @@ resource "aws_iam_role_policy_attachment" "ec2_operations_policy_att" {
 #tfsec:ignore:aws-iam-no-user-attached-policies
 resource "aws_iam_user" "email" {
   #checkov:skip=CKV_AWS_273: "Skipping as tfsec check is also set to ignore"
+  count = local.is_development ? 0 : 1
   name = format("%s-%s-email_user", local.application_name, local.environment)
   tags = merge(local.tags,
     { Name = format("%s-%s-email_user", local.application_name, local.environment) }
@@ -289,13 +290,15 @@ resource "aws_iam_user" "email" {
 }
 
 resource "aws_iam_access_key" "email" {
-  user = aws_iam_user.email.name
+  count = local.is_development ? 0 : 1
+  user = aws_iam_user.email[0].name
 }
 
 #tfsec:ignore:aws-iam-no-policy-wildcards
 resource "aws_iam_user_policy" "email_policy" {
+  count = local.is_development ? 0 : 1
   name   = "AmazonSesSendingAccess"
-  user   = aws_iam_user.email.name
+  user   = aws_iam_user.email[0].name
   policy = data.aws_iam_policy_document.email.json
 }
 
