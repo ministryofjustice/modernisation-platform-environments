666f2596b7f68f21a2f63ee094c6aecaec057119
Raghavendra Kamath <raghavvidya@gmail.com>
2026-05-29 21:16:53 +0100

    chore(deps):upgrade terraform modules and provider versions

--- terraform/environments/analytical-platform-compute/locals.tf
+++ terraform/environments/analytical-platform-compute/locals.tf
@@ -16,6 +16,6 @@ locals {
   /* Private subnet arns */
   private_subnet_arns = [
     for subnet_id in data.aws_subnets.apc_private.ids :
-    "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:subnet/${subnet_id}"
+    "arn:aws:ec2:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:subnet/${subnet_id}"
   ]
 }
