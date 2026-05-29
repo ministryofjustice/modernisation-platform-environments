03ead1445e9a670f0c0cf2d8b6840508a8936389
matt heery <matt.heery@justice.gov.uk>
2026-05-29 14:49:05 +0100

    feat: two separate endpoints

--- terraform/environments/electronic-monitoring-data/api_gateway.tf
+++ terraform/environments/electronic-monitoring-data/api_gateway.tf
@@ -133,17 +133,24 @@ resource "aws_api_gateway_rest_api" "update_p1_export" {
   }
 }
 
-resource "aws_api_gateway_resource" "update_p1_export" {
+resource "aws_api_gateway_resource" "update_p1_export_add" {
   count = local.is-development ? 1 : 0
   rest_api_id = aws_api_gateway_rest_api.update_p1_export[0].id
   parent_id   = aws_api_gateway_rest_api.update_p1_export[0].root_resource_id
-  path_part   = "update"
+  path_part   = "add"
 }
 
-resource "aws_api_gateway_method" "update_p1_export_post" {
+resource "aws_api_gateway_resource" "update_p1_export_remove" {
+  count = local.is-development ? 1 : 0
+  rest_api_id = aws_api_gateway_rest_api.update_p1_export[0].id
+  parent_id   = aws_api_gateway_rest_api.update_p1_export[0].root_resource_id
+  path_part   = "remove"
+}
+
+resource "aws_api_gateway_method" "update_p1_export_add_post" {
   count = local.is-development ? 1 : 0
   rest_api_id          = aws_api_gateway_rest_api.update_p1_export[0].id
-  resource_id          = aws_api_gateway_resource.update_p1_export[0].id
+  resource_id          = aws_api_gateway_resource.update_p1_export_add[0].id
   http_method          = "POST"
   authorization        = "AWS_IAM"
   request_validator_id = aws_api_gateway_request_validator.update_p1_export[0].id
@@ -152,6 +159,17 @@ resource "aws_api_gateway_method" "update_p1_export_post" {
   }
 }
 
+resource "aws_api_gateway_method" "update_p1_export_remove_post" {
+  count = local.is-development ? 1 : 0
+  rest_api_id          = aws_api_gateway_rest_api.update_p1_export[0].id
+  resource_id          = aws_api_gateway_resource.update_p1_export_remove[0].id
+  http_method          = "POST"
+  authorization        = "AWS_IAM"
+  request_validator_id = aws_api_gateway_request_validator.update_p1_export[0].id
+  request_models = {
+    "application/json" = aws_api_gateway_model.update_p1_export[0].name
+  }
+}
 
 # --------------------------------------------------------
 # update_p1_export Validator
@@ -185,11 +203,22 @@ resource "aws_api_gateway_model" "update_p1_export" {
   )
 }
 
-resource "aws_api_gateway_integration" "update_p1_export_lambda_post" {
+resource "aws_api_gateway_integration" "update_p1_export_add_lambda_post" {
   count = local.is-development ? 1 : 0
   rest_api_id = aws_api_gateway_rest_api.update_p1_export[0].id
-  resource_id = aws_api_gateway_resource.update_p1_export[0].id
-  http_method = aws_api_gateway_method.update_p1_export_post[0].http_method
+  resource_id = aws_api_gateway_resource.update_p1_export_add[0].id
+  http_method = aws_api_gateway_method.update_p1_export_add_post[0].http_method
+
+  integration_http_method = "POST"
+  type                   = "AWS_PROXY"
+  uri                    = module.update_p1_export[0].lambda_function_invoke_arn
+}
+
+resource "aws_api_gateway_integration" "update_p1_export_remove_lambda_post" {
+  count = local.is-development ? 1 : 0
+  rest_api_id = aws_api_gateway_rest_api.update_p1_export[0].id
+  resource_id = aws_api_gateway_resource.update_p1_export_remove[0].id
+  http_method = aws_api_gateway_method.update_p1_export_remove_post[0].http_method
 
   integration_http_method = "POST"
   type                   = "AWS_PROXY"
@@ -198,7 +227,10 @@ resource "aws_api_gateway_integration" "update_p1_export_lambda_post" {
 
 resource "aws_api_gateway_deployment" "update_p1_export" {
   count = local.is-development ? 1 : 0
-  depends_on = [aws_api_gateway_integration.update_p1_export_lambda_post[0],]
+  depends_on = [
+    aws_api_gateway_integration.update_p1_export_add_lambda_post[0],
+    aws_api_gateway_integration.update_p1_export_remove_lambda_post[0],
+  ]
 
   rest_api_id = aws_api_gateway_rest_api.update_p1_export[0].id
 }
