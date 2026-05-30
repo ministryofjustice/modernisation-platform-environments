077340003c8420d9a0918ccd7b5a4211619b0e0c
Raghavendra Kamath <raghavvidya@gmail.com>
2026-05-30 10:26:11 +0100

    chore(deps):upgrade terraform modules and provider versions

--- terraform/environments/analytical-platform-ingestion/route53-resolver-endpoints.tf
+++ terraform/environments/analytical-platform-ingestion/route53-resolver-endpoints.tf
@@ -1,8 +1,8 @@
 module "connected_vpc_outbound_route53_resolver_endpoint" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
-  source  = "terraform-aws-modules/route53/aws//modules/resolver-endpoints"
-  version = "5.0.0"
+  source  = "terraform-aws-modules/route53/aws//modules/resolver-endpoint"
+  version = "6.5.0"
 
   name      = "connected-vpc-outbound"
   vpc_id    = module.connected_vpc.vpc_id
@@ -18,12 +18,21 @@ module "connected_vpc_outbound_route53_resolver_endpoint" {
     }
   ]
 
-  security_group_ingress_cidr_blocks = [module.connected_vpc.vpc_cidr_block]
-  security_group_egress_cidr_blocks = [
+  security_group_ingress_rules = {
+  connected_vpc =  {
+   cidr_ipv4 = module.connected_vpc.vpc_cidr_block
+  }
+}
+
+  security_group_egress_rules = {
     /* MoJO DNS Resolver Service */
-    "10.180.80.5/32",
-    "10.180.81.5/32"
-  ]
+    moj_service_resolver1 = {
+     cidr_ipv4 = "10.180.80.5/32"
+    }
+    moj_service_resolver2 = {
+    cidr_ipv4 = "10.180.81.5/32"
+  }
+  }
 
   tags = local.tags
 }
