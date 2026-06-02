3ce37fa3173be60c4380bc314a19e6d34ac8fca5
wullub <13420301+wullub@users.noreply.github.com>
2026-06-02 12:24:37 +0100

    prison-retail-add-cw-dashboard

--- terraform/environments/prison-retail/locals.tf
+++ terraform/environments/prison-retail/locals.tf
@@ -15,7 +15,7 @@ locals {
   baseline_presets_all_environments = {
     options = {
       cloudwatch_dashboard_default_widget_groups = [
-        "all_windows_ec2",
+        "ec2_instance_only_windows",
       ]
       # cloudwatch_metric_alarms_default_actions    = ["pagerduty"]
       # cloudwatch_metric_oam_links_ssm_parameters  = ["hmpps-oem-${local.environment}"]
