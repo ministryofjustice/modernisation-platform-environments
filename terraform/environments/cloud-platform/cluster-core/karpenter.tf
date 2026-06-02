c109d97e4cc7100072dff88d00f75e481fd18050
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-06-02 16:18:56 +0100

    Disabling karpenter to test eks auto

--- terraform/environments/cloud-platform/cluster-core/karpenter.tf
+++ terraform/environments/cloud-platform/cluster-core/karpenter.tf
@@ -1,7 +1,7 @@
-module "karpenter" {
-  source = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=5028eb32e7fa4386a879a47307620eecfb41fe63" #0.1.0
+# module "karpenter" {
+#   source = "github.com/ministryofjustice/container-platform-terraform-karpenter?ref=5028eb32e7fa4386a879a47307620eecfb41fe63" #0.1.0
 
-  cluster_name     = local.cluster_name
-  cluster_endpoint = data.aws_eks_cluster.cluster.endpoint
-  k8s_version      = data.aws_eks_cluster.cluster.version
-}
+#   cluster_name     = local.cluster_name
+#   cluster_endpoint = data.aws_eks_cluster.cluster.endpoint
+#   k8s_version      = data.aws_eks_cluster.cluster.version
+# }
