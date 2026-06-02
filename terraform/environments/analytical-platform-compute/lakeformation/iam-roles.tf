3bfc0017f624809d1d0884e4f6a5650f4cbb477e
Raghavendra Kamath <raghavvidya@gmail.com>
2026-06-02 11:50:52 +0100

    Because of breaking changes, we are not proceeding with the IAM module upgrade.

--- terraform/environments/analytical-platform-compute/lakeformation/iam-roles.tf
+++ terraform/environments/analytical-platform-compute/lakeformation/iam-roles.tf
@@ -2,34 +2,25 @@ module "lake_formation_share_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
-  version = "6.6.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.59.0"
 
-  name            = "lake-formation-share"
-  use_name_prefix = false
+  create_role       = true
+  role_requires_mfa = false
 
+  role_name_prefix = "lake-formation-share"
 
-  policies = {
-    analytical_platform_lake_formation_share_policy = module.analytical_platform_lake_formation_share_policy.arn
-    aws_lakeformation_policy                        = "arn:aws:iam::aws:policy/AWSLakeFormationCrossAccountManager"
-  }
+  number_of_custom_role_policy_arns = 2
 
-  trust_policy_permissions = {
-    LakeformationExecutionRole = {
-      actions = ["sts:AssumeRole", "sts:TagSession"]
-      principals = [
-        {
-          type = "AWS"
-          identifiers = formatlist(
-            "arn:aws:iam::%s:root",
-            [
-              local.environment_management.account_ids["analytical-platform-management-production"],
-              local.environment_management.account_ids["analytical-platform-compute-development"]
-          ])
-        }
-      ]
-    }
-  }
+  custom_role_policy_arns = [
+    module.analytical_platform_lake_formation_share_policy.arn,
+    "arn:aws:iam::aws:policy/AWSLakeFormationCrossAccountManager"
+  ]
+
+  trusted_role_arns = [
+    "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-management-production"]}:root",
+    "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-compute-development"]}:root"
+  ]
 
   tags = local.tags
 }
@@ -38,43 +29,23 @@ module "analytical_platform_control_panel_service_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
-  version = "6.6.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.59.0"
 
+  allow_self_assume_role = true
+  trusted_role_arns = [
+    format("arn:aws:iam::%s:root", local.environment_management.account_ids[local.analytical_platform_environment])
 
-  trust_policy_permissions = {
-    LakeformationExecutionRole = {
-      actions = ["sts:AssumeRole", "sts:TagSession"]
-      principals = [
-        {
-          type        = "AWS"
-          identifiers = [format("arn:aws:iam::%s:root", local.environment_management.account_ids[local.analytical_platform_environment])]
-        }
-      ]
-    }
-    ExplicitSelfRoleAssumption = {
-      actions = ["sts:AssumeRole"]
-      principals = [
-        {
-          type        = "AWS"
-          identifiers = ["*"]
-        }
-      ]
-      condition = [{
-        test     = "ArnLike"
-        variable = "aws:PrincipalArn"
-        values   = [format("arn:aws:iam::%s:role/%s", data.aws_caller_identity.current.account_id, "analytical-platform-control-panel")]
-      }]
-    }
-  }
+  ]
+  create_role       = true
+  role_requires_mfa = false
+  role_name         = "analytical-platform-control-panel"
 
-  name = "analytical-platform-control-panel"
-
-  use_name_prefix = false
-  policies = {
-    lakeformation_share_policy = module.analytical_platform_lake_formation_share_policy.arn
-    aws_lakeformation_policy   = "arn:aws:iam::aws:policy/AWSLakeFormationCrossAccountManager"
-  }
+  custom_role_policy_arns = [
+    module.analytical_platform_lake_formation_share_policy.arn,
+    "arn:aws:iam::aws:policy/AWSLakeFormationCrossAccountManager"
+  ]
+  number_of_custom_role_policy_arns = 2
 
   tags = local.tags
 }
@@ -83,32 +54,20 @@ module "analytical_platform_data_eng_dba_service_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
-  version = "6.6.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.59.0"
 
+  allow_self_assume_role = false
+  trusted_role_arns      = formatlist("arn:aws:iam::%s:root", [local.environment_management.account_ids[local.analytical_platform_environment], local.environment_management.account_ids["analytical-platform-management-production"]])
+  create_role            = true
+  role_requires_mfa      = false
+  role_name              = "analytical-platform-data-engineering-database-access"
 
-  trust_policy_permissions = {
-    LakeformationExecutionRole = {
-      actions = ["sts:AssumeRole", "sts:TagSession"]
-      principals = [
-        {
-          type = "AWS"
-          identifiers = formatlist(
-            "arn:aws:iam::%s:root",
-            [
-              local.environment_management.account_ids[local.analytical_platform_environment],
-              local.environment_management.account_ids["analytical-platform-management-production"]
-          ])
-        }
-      ]
-    }
-  }
-  name            = "analytical-platform-data-engineering-database-access"
-  use_name_prefix = false
-  policies = {
-    lakeformation_share_policy = module.analytical_platform_lake_formation_share_policy.arn
-    aws_lakeformation_policy   = "arn:aws:iam::aws:policy/AWSLakeFormationCrossAccountManager"
-  }
+  custom_role_policy_arns = [
+    module.analytical_platform_lake_formation_share_policy.arn,
+    "arn:aws:iam::aws:policy/AWSLakeFormationCrossAccountManager"
+  ]
+  number_of_custom_role_policy_arns = 2
 
   tags = local.tags
 }
@@ -117,30 +76,28 @@ module "lake_formation_to_data_production_mojap_derived_tables_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
-  version = "6.6.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.59.0"
 
-  name            = "lake-formation-data-production-data-access"
-  use_name_prefix = false
-  policies = {
-    mojap_derived_bucket_lake_formation_policy = module.data_production_mojap_derived_bucket_lake_formation_policy.arn
-  }
+  create_role       = true
+  role_requires_mfa = false
 
+  role_name = "lake-formation-data-production-data-access"
 
-  trust_policy_permissions = {
-    LakeformationExecutionRole = {
-      actions = ["sts:AssumeRole", "sts:SetContext"]
-      principals = [
-        {
-          type = "Service"
-          identifiers = [
-            "glue.amazonaws.com",
-            "lakeformation.amazonaws.com"
-          ]
-        }
-      ]
-    }
-  }
+  custom_role_policy_arns = [
+    module.data_production_mojap_derived_bucket_lake_formation_policy.arn,
+  ]
+
+  trusted_role_actions = [
+    "sts:AssumeRole",
+    "sts:SetContext"
+  ]
+
+  trusted_role_services = [
+    "glue.amazonaws.com",
+    "lakeformation.amazonaws.com"
+  ]
+
   tags = local.tags
 }
 
@@ -148,31 +105,20 @@ module "copy_apdp_cadet_metadata_to_compute_assumable_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
-  version = "6.6.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.59.0"
 
-  trust_policy_permissions = {
-    LakeformationExecutionRole = {
-      actions = ["sts:AssumeRole", "sts:TagSession"]
-      principals = [
-        {
-          type = "AWS"
-          identifiers = [
-            "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/create-a-derived-table",
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.data_engineering_sso_role.names)}",
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.eks_sso_access_role.names)}",
-          ]
-        }
-      ]
-    }
-  }
+  allow_self_assume_role = false
+  trusted_role_arns = [
+    "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-production"]}:role/create-a-derived-table",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.data_engineering_sso_role.names)}",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${data.aws_region.current.region}/${one(data.aws_iam_roles.eks_sso_access_role.names)}",
+  ]
+  create_role       = true
+  role_requires_mfa = false
+  role_name         = "copy-apdp-cadet-metadata-to-compute"
 
-  name            = "copy-apdp-cadet-metadata-to-compute"
-  use_name_prefix = false
-
-  policies = {
-    copy_apdp_cadet_metadata_to_compute_policy = module.copy_apdp_cadet_metadata_to_compute_policy.arn
-  }
+  custom_role_policy_arns = [module.copy_apdp_cadet_metadata_to_compute_policy.arn]
 
   tags = local.tags
 }
