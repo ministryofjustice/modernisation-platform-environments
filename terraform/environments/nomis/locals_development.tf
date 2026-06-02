3ea250adad92b96d57aecf1143e5068974fe8906
ghill26 <George.Hill2@justice.gov.uk>
2026-06-02 08:59:49 +0100

    TM-2113: Ensure dev-nomis-weblogic-12 targets qa19c

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -177,8 +177,8 @@ locals {
           })
         })
         tags = merge(local.ec2_autoscaling_groups.web12.tags, {
-          nomis-environment    = "qa11g2"
-          oracle-db-name       = "qa11g2"
+          nomis-environment    = "qa19c"
+          oracle-db-name       = "qa19c"
           oracle-db-hostname-a = "dev-nomis-db19c-1-a"
           oracle-db-hostname-b = "none"
         })
