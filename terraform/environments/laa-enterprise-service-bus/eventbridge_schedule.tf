da452040e1985a808df1584b6029cf4c342bd561
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-06-03 12:40:53 +0100

    scheduling changes

--- terraform/environments/laa-enterprise-service-bus/eventbridge_schedule.tf
+++ terraform/environments/laa-enterprise-service-bus/eventbridge_schedule.tf
@@ -1,21 +1,23 @@
 # CWA Extract Schedule
-# resource "aws_scheduler_schedule" "cwa_extract_schedule" {
-#   count      = local.environment == "development" ? 1 : 0
-#   name       = "cwa-extract-schedule"
-#   group_name = "default"
-#
-#   flexible_time_window {
-#     mode = "OFF"
-#   }
-#
-#   schedule_expression          = "cron(0 22 ? * WED *)"
-#   schedule_expression_timezone = "Europe/London"
-#
-#   target {
-#     arn      = aws_sfn_state_machine.sfn_state_machine.arn
-#     role_arn = aws_iam_role.scheduler_invoke_sfn_role.arn
-#   }
-# }
+resource "aws_scheduler_schedule" "cwa_extract_schedule" {
+  count      = local.environment == "development" ? 1 : 0
+  name       = "cwa-extract-schedule"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression          = "cron(0 22 ? * WED *)"
+  schedule_expression_timezone = "Europe/London"
+
+  state = "DISABLED"
+
+  target {
+    arn      = aws_sfn_state_machine.sfn_state_machine.arn
+    role_arn = aws_iam_role.scheduler_invoke_sfn_role.arn
+  }
+}
 
 # CCMS Load Schedule
 resource "aws_scheduler_schedule" "ccms_load_schedule" {
