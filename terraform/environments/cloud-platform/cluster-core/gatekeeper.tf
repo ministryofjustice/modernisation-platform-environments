a7f20f0c6c16705811e87ce85209344bd53e0ad5
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-06-03 17:15:35 +0100

    Disabling cilium and gatekeeper to get clean builds whilst no label nodes

--- terraform/environments/cloud-platform/cluster-core/gatekeeper.tf
+++ terraform/environments/cloud-platform/cluster-core/gatekeeper.tf
@@ -1,21 +1,21 @@
-module "gatekeeper" {
-  source = "github.com/ministryofjustice/container-platform-terraform-gatekeeper?ref=1.0.0"
+# module "gatekeeper" {
+#   source = "github.com/ministryofjustice/container-platform-terraform-gatekeeper?ref=1.0.0"
 
-  # boolean expression for applying opa valid hostname for test clusters only.
-  dryrun_map = {
-    service_type                       = false,
-    warn_service_account_secret_delete = false,
-    user_ns_requires_psa_label         = false,
-    lock_priv_capabilities             = false,
-    warn_kubectl_create_sa             = false,
-  }
+#   # boolean expression for applying opa valid hostname for test clusters only.
+#   dryrun_map = {
+#     service_type                       = false,
+#     warn_service_account_secret_delete = false,
+#     user_ns_requires_psa_label         = false,
+#     lock_priv_capabilities             = false,
+#     warn_kubectl_create_sa             = false,
+#   }
 
-  constraint_violations_max_to_display = 25
-  is_production                        = contains(local.mp_environments, terraform.workspace) ? "true" : "false"
-  environment_name                     = terraform.workspace
-  out_of_hours_alert                   = "false"
-  controller_mem_limit                 = "1Gi"
-  controller_mem_req                   = "512Mi"
-  audit_mem_limit                      = "1Gi"
-  audit_mem_req                        = "512Mi"
-}
+#   constraint_violations_max_to_display = 25
+#   is_production                        = contains(local.mp_environments, terraform.workspace) ? "true" : "false"
+#   environment_name                     = terraform.workspace
+#   out_of_hours_alert                   = "false"
+#   controller_mem_limit                 = "1Gi"
+#   controller_mem_req                   = "512Mi"
+#   audit_mem_limit                      = "1Gi"
+#   audit_mem_req                        = "512Mi"
+# }
