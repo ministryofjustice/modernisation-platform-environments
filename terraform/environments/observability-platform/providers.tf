6f5be710491308c88177aec03898c02152fd04e5
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-06-03 10:24:17 +0100

    Update providers.tf

--- terraform/environments/observability-platform/providers.tf
+++ terraform/environments/observability-platform/providers.tf
@@ -1,10 +1,3 @@
-resource "aws_grafana_workspace_service_account_token" "automation" {
-  name               = "terraform"
-  service_account_id = aws_grafana_workspace_service_account.automation.service_account_id
-  seconds_to_live    = 2592000  # 30 days
-  workspace_id       = module.managed_grafana.workspace_id
-}
-
 provider "grafana" {
   url  = "https://${module.managed_grafana.workspace_endpoint}"
   auth = aws_grafana_workspace_service_account_token.automation.key
