feeed40df7fb603912782ebb7958d1f131e5d8b4
David Sibley <david.sibley@justice.gov.uk>
2026-06-04 13:09:28 +0100

    reinstate local.tags for modules

--- terraform/environments/integration-hub/managed-file-transfer/lambda.tf
+++ terraform/environments/integration-hub/managed-file-transfer/lambda.tf
@@ -85,6 +85,8 @@ module "lambda_unscanned_to_processing" {
 
   cloudwatch_logs_kms_key_id        = module.kms_cloudwatch_logs.key_arn
   cloudwatch_logs_retention_in_days = 30
+
+  tags = local.tags
 }
 
 module "lambda_processing_to_post_scan" {
