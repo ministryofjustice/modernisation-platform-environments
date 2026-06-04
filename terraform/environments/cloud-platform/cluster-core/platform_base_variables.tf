e4532acb3f66a09399860d9833cb71af8a11cd28
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2026-06-04 12:56:00 +0100

    feat: add starter pack module to cluster-core

--- terraform/environments/cloud-platform/cluster-core/platform_base_variables.tf
+++ terraform/environments/cloud-platform/cluster-core/platform_base_variables.tf
@@ -7,3 +7,9 @@ variable "collaborator_access" {
   default     = "developer"
   description = "Collaborators must specify which access level they are using, eg set an environment variable of export TF_VAR_collaborator_access=migration"
 }
+
+variable "enable_starter_pack" {
+  type        = bool
+  default     = true
+  description = "Toggle to enable starter pack service"
+}
---
+++ terraform/environments/cloud-platform/cluster-core/starter_pack.tf
@@ -0,0 +1,8 @@
+module "starter_pack" {
+  count             = var.enable_starter_pack && local.cluster_environment == "development_cluster" ? 1 : 0  source            = "github.com/ministryofjustice/container-platform-terraform-starter-pack?ref=1.0.0"
+  gateway_name      = "eg"
+  gateway_namespace = "default"
+  enable_httproute  = false # HTTPRoute can't be created until gateway API CRDs are installed 
+  image_repository  = format("%s.dkr.ecr.%s.amazonaws.com/cloud-platform/container-platform-terraform-starter-pack", data.aws_caller_identity.current.account_id, data.aws_region.current.region)
+  image_tag         = "1.0.0"
+}
