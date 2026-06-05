34bfcad5f3634fb257f882b9f9e6924f9a64bbee
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2026-06-05 11:40:35 +0100

    remove ecr module

--- terraform/environments/cloud-platform/ecr.tf
+++ terraform/environments/cloud-platform/ecr.tf
@@ -1,23 +1,3 @@
-# module "starter_pack_ecr" {
-#   source = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=8.0.1"
-#   count  = local.environment == "development" ? 1 : 0
-
-#   repo_name = "container-platform-terraform-starter-pack"
-
-#   # OpenID Connect configuration
-#   oidc_providers      = ["github"]
-#   github_repositories = ["container-platform-terraform-starter-pack"]
-
-#   # Tags
-#   business_unit          = "OCTO"
-#   application            = local.application_name
-#   is_production          = local.is-production
-#   team_name              = "cloud-platform"
-#   namespace              = "container-platform-terraform-starter-pack"
-#   environment_name       = local.environment
-#   infrastructure_support = "platforms@digital.justice.gov.uk"
-# }
-
 locals {
 	starter_pack_default_tags = {
 		business-unit = "OCTO"
