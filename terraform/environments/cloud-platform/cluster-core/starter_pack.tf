9085f77044f15ca69f189675efd2367ef6be904c
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2026-06-05 10:25:28 +0100

    use starter pack branch

--- terraform/environments/cloud-platform/cluster-core/starter_pack.tf
+++ terraform/environments/cloud-platform/cluster-core/starter_pack.tf
@@ -1,6 +1,6 @@
 module "starter_pack" {
   count             = var.enable_starter_pack && local.cluster_environment == "development_cluster" ? 1 : 0
-  source            = "github.com/ministryofjustice/container-platform-terraform-starter-pack?ref=1.0.0"
+  source            = "github.com/ministryofjustice/container-platform-terraform-starter-pack?ref=remove-leftover-latest-tag"
   gateway_name      = "eg"
   gateway_namespace = "default"
   enable_httproute  = false # HTTPRoute can't be created until gateway API CRDs are installed 
