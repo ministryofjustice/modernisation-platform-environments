36968e340d7301a38d94a00589b5115063c658fe
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-06-08 13:20:33 +0100

    fix: need to destroy ecs as well

--- terraform/environments/ccms-ebs/ccms-file-transfer/cloudwatch.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/cloudwatch.tf
@@ -24,27 +24,27 @@ resource "aws_cloudwatch_metric_alarm" "alb_sftp_bc_5xx" {
 }
 
 # Alarm for ECS Container Count for sftp_bc Service
-resource "aws_cloudwatch_metric_alarm" "container_sftp_bc_count" {
-  alarm_name          = "${local.application_name}-${local.environment}-sftp-bc-container-count-low"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "RunningTaskCount"
-  namespace           = "ECS/ContainerInsights"
-  period              = 60
-  statistic           = "Average"
-  threshold           = local.application_data.accounts[local.environment].app_count
-  dimensions = {
-    ClusterName = aws_ecs_cluster.main_cluster.name
-    ServiceName = aws_ecs_service.sftp_bc_ecs_service.name
-  }
-  alarm_description         = "The number of sftp_bc ECS tasks is less than ${local.application_data.accounts[local.environment].app_count}. Runbook: https://dsdmoj.atlassian.net/wiki/spaces/CCMS/pages/1408598133/Monitoring+and+Alerts"
-  treat_missing_data        = "breaching"
-  alarm_actions             = [data.aws_sns_topic.cw_alerts.arn]
-  ok_actions                = [data.aws_sns_topic.cw_alerts.arn]
-  insufficient_data_actions = []
+# resource "aws_cloudwatch_metric_alarm" "container_sftp_bc_count" {
+#   alarm_name          = "${local.application_name}-${local.environment}-sftp-bc-container-count-low"
+#   comparison_operator = "LessThanThreshold"
+#   evaluation_periods  = 1
+#   metric_name         = "RunningTaskCount"
+#   namespace           = "ECS/ContainerInsights"
+#   period              = 60
+#   statistic           = "Average"
+#   threshold           = local.application_data.accounts[local.environment].app_count
+#   dimensions = {
+#     ClusterName = aws_ecs_cluster.main_cluster.name
+#     ServiceName = aws_ecs_service.sftp_bc_ecs_service.name
+#   }
+#   alarm_description         = "The number of sftp_bc ECS tasks is less than ${local.application_data.accounts[local.environment].app_count}. Runbook: https://dsdmoj.atlassian.net/wiki/spaces/CCMS/pages/1408598133/Monitoring+and+Alerts"
+#   treat_missing_data        = "breaching"
+#   alarm_actions             = [data.aws_sns_topic.cw_alerts.arn]
+#   ok_actions                = [data.aws_sns_topic.cw_alerts.arn]
+#   insufficient_data_actions = []
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
 # Underlying waf Instance Status Check Failure
 resource "aws_cloudwatch_metric_alarm" "sftp_bc_waf_high_blocked_requests" {
@@ -71,44 +71,44 @@ resource "aws_cloudwatch_metric_alarm" "sftp_bc_waf_high_blocked_requests" {
   tags = local.tags
 }
 
-resource "aws_cloudwatch_metric_alarm" "sftp_bc_alb_healthyhosts" {
-  alarm_name          = "${local.application_name}-sftp-bc-${local.environment}-alb-targets-group"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "HealthyHostCount"
-  namespace           = "AWS/ApplicationELB"
-  period              = 240
-  statistic           = "Average"
-  threshold           = local.application_data.accounts[local.environment].app_count
-  alarm_description   = "Number of healthy nodes in Target Group"
-  actions_enabled     = true
-  treat_missing_data  = "breaching"
-  alarm_actions       = [data.aws_sns_topic.cw_alerts.arn]
-  ok_actions          = [data.aws_sns_topic.cw_alerts.arn]
-  dimensions = {
-    TargetGroup  = aws_lb_target_group.sftp_bc_target_group.arn_suffix
-    LoadBalancer = aws_lb.sftp_bc_load_balancer.arn_suffix
-  }
-}
+# resource "aws_cloudwatch_metric_alarm" "sftp_bc_alb_healthyhosts" {
+#   alarm_name          = "${local.application_name}-sftp-bc-${local.environment}-alb-targets-group"
+#   comparison_operator = "LessThanThreshold"
+#   evaluation_periods  = 1
+#   metric_name         = "HealthyHostCount"
+#   namespace           = "AWS/ApplicationELB"
+#   period              = 240
+#   statistic           = "Average"
+#   threshold           = local.application_data.accounts[local.environment].app_count
+#   alarm_description   = "Number of healthy nodes in Target Group"
+#   actions_enabled     = true
+#   treat_missing_data  = "breaching"
+#   alarm_actions       = [data.aws_sns_topic.cw_alerts.arn]
+#   ok_actions          = [data.aws_sns_topic.cw_alerts.arn]
+#   dimensions = {
+#     TargetGroup  = aws_lb_target_group.sftp_bc_target_group.arn_suffix
+#     LoadBalancer = aws_lb.sftp_bc_load_balancer.arn_suffix
+#   }
+# }
 
-resource "aws_cloudwatch_metric_alarm" "sftp_bc_ecs_high_memory" {
-  alarm_name          = "${local.application_name}-sftp-bc-${local.environment}-ecs-high-memory"
-  alarm_description   = "ECS Fargate service memory utilization is high"
-  comparison_operator = "GreaterThanThreshold"
-  threshold           = 85
-  evaluation_periods  = 3
-  datapoints_to_alarm = 3
-  treat_missing_data  = "notBreaching"
-  namespace           = "AWS/ECS"
-  metric_name         = "MemoryUtilization"
-  statistic           = "Average"
-  period              = 60
+# resource "aws_cloudwatch_metric_alarm" "sftp_bc_ecs_high_memory" {
+#   alarm_name          = "${local.application_name}-sftp-bc-${local.environment}-ecs-high-memory"
+#   alarm_description   = "ECS Fargate service memory utilization is high"
+#   comparison_operator = "GreaterThanThreshold"
+#   threshold           = 85
+#   evaluation_periods  = 3
+#   datapoints_to_alarm = 3
+#   treat_missing_data  = "notBreaching"
+#   namespace           = "AWS/ECS"
+#   metric_name         = "MemoryUtilization"
+#   statistic           = "Average"
+#   period              = 60
 
-  dimensions = {
-    ClusterName = aws_ecs_cluster.main_cluster.name
-    ServiceName = aws_ecs_service.sftp_bc_ecs_service.name
-  }
+#   dimensions = {
+#     ClusterName = aws_ecs_cluster.main_cluster.name
+#     ServiceName = aws_ecs_service.sftp_bc_ecs_service.name
+#   }
 
-  alarm_actions = [data.aws_sns_topic.cw_alerts.arn]
-  ok_actions    = [data.aws_sns_topic.cw_alerts.arn]
-}
+#   alarm_actions = [data.aws_sns_topic.cw_alerts.arn]
+#   ok_actions    = [data.aws_sns_topic.cw_alerts.arn]
+# }
--- terraform/environments/ccms-ebs/ccms-file-transfer/ecs_cluster.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/ecs_cluster.tf
@@ -9,78 +9,78 @@ resource "aws_ecs_cluster" "main_cluster" {
 }
 
 # ECS Task Definition
-resource "aws_ecs_task_definition" "sftp_bc_task_definition" {
-  family             = "${local.application_name}-sftp-bc-task"
-  execution_role_arn = aws_iam_role.bc_ecs_task_execution_role.arn
-  network_mode       = "awsvpc"
-  requires_compatibilities = [
-    "FARGATE",
-  ]
+# resource "aws_ecs_task_definition" "sftp_bc_task_definition" {
+#   family             = "${local.application_name}-sftp-bc-task"
+#   execution_role_arn = aws_iam_role.bc_ecs_task_execution_role.arn
+#   network_mode       = "awsvpc"
+#   requires_compatibilities = [
+#     "FARGATE",
+#   ]
 
   
-  cpu    = local.application_data.accounts[local.environment].container_cpu
-  memory = local.application_data.accounts[local.environment].container_memory
+#   cpu    = local.application_data.accounts[local.environment].container_cpu
+#   memory = local.application_data.accounts[local.environment].container_memory
 
-  runtime_platform {
-    operating_system_family = "LINUX"
-    cpu_architecture        = "X86_64"
-  }
+#   runtime_platform {
+#     operating_system_family = "LINUX"
+#     cpu_architecture        = "X86_64"
+#   }
 
-  container_definitions = templatefile(
-    "${path.module}/templates/task_definition_api.json.tpl",
-    {
-      app_name           = local.application_data.accounts[local.environment].app_name
-      app_image          = local.application_data.accounts[local.environment].app_image
-      api_server_port    = local.application_data.accounts[local.environment].api_server_port
-      cpu                = local.application_data.accounts[local.environment].container_cpu
-      memory             = local.application_data.accounts[local.environment].container_memory
-      aws_region         = local.application_data.accounts[local.environment].aws_region
-      container_version  = local.application_data.accounts[local.environment].container_version
-      ccms_s3_bucket     = local.sftp_bc_bucket_name
-      ORACLE_USERNAME    = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:ORACLE_USERNAME::"
-      ORACLE_PASSWORD    = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:ORACLE_PASSWORD::"
-      ORACLE_URL         = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:ORACLE_URL::"
-      SLACK_WEBHOOK      = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:SLACK_WEBHOOK::"
-      ENABLE_SWAGGER     = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:ENABLE_SWAGGER::"
-      AUTHORIZED_CLIENTS = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:AUTHORIZED_CLIENTS::"
-      AUTHORIZED_ROLES   = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:AUTHORIZED_ROLES::"
-      UNPROTECTED_URIS   = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:UNPROTECTED_URIS::"
-    }
-  )
+#   container_definitions = templatefile(
+#     "${path.module}/templates/task_definition_api.json.tpl",
+#     {
+#       app_name           = local.application_data.accounts[local.environment].app_name
+#       app_image          = local.application_data.accounts[local.environment].app_image
+#       api_server_port    = local.application_data.accounts[local.environment].api_server_port
+#       cpu                = local.application_data.accounts[local.environment].container_cpu
+#       memory             = local.application_data.accounts[local.environment].container_memory
+#       aws_region         = local.application_data.accounts[local.environment].aws_region
+#       container_version  = local.application_data.accounts[local.environment].container_version
+#       ccms_s3_bucket     = local.sftp_bc_bucket_name
+#       ORACLE_USERNAME    = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:ORACLE_USERNAME::"
+#       ORACLE_PASSWORD    = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:ORACLE_PASSWORD::"
+#       ORACLE_URL         = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:ORACLE_URL::"
+#       SLACK_WEBHOOK      = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:SLACK_WEBHOOK::"
+#       ENABLE_SWAGGER     = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:ENABLE_SWAGGER::"
+#       AUTHORIZED_CLIENTS = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:AUTHORIZED_CLIENTS::"
+#       AUTHORIZED_ROLES   = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:AUTHORIZED_ROLES::"
+#       UNPROTECTED_URIS   = "${data.aws_secretsmanager_secret_version.sftp_bc_secrets.arn}:UNPROTECTED_URIS::"
+#     }
+#   )
 
-  tags = merge(local.tags,
-    { Name = lower(format("%s-sftp-bc-%s-task", local.application_name, local.environment)) }
-  )
-}
+#   tags = merge(local.tags,
+#     { Name = lower(format("%s-sftp-bc-%s-task", local.application_name, local.environment)) }
+#   )
+# }
 
 # ECS Service
-resource "aws_ecs_service" "sftp_bc_ecs_service" {
-  name            = "${local.application_name}-sftp-bc-service"
-  cluster         = aws_ecs_cluster.main_cluster.id
-  task_definition = aws_ecs_task_definition.sftp_bc_task_definition.arn
-  desired_count   = local.application_data.accounts[local.environment].app_count
-  launch_type     = "FARGATE"
+# resource "aws_ecs_service" "sftp_bc_ecs_service" {
+#   name            = "${local.application_name}-sftp-bc-service"
+#   cluster         = aws_ecs_cluster.main_cluster.id
+#   task_definition = aws_ecs_task_definition.sftp_bc_task_definition.arn
+#   desired_count   = local.application_data.accounts[local.environment].app_count
+#   launch_type     = "FARGATE"
 
-  health_check_grace_period_seconds = 120
-  lifecycle {
-    ignore_changes = [
-      task_definition
-    ]
-  }
+#   health_check_grace_period_seconds = 120
+#   lifecycle {
+#     ignore_changes = [
+#       task_definition
+#     ]
+#   }
 
-  network_configuration {
-    security_groups = [aws_security_group.cluster_fargate_sg.id]
-    subnets         = data.aws_subnets.shared-private.ids
-  }
+#   network_configuration {
+#     security_groups = [aws_security_group.cluster_fargate_sg.id]
+#     subnets         = data.aws_subnets.shared-private.ids
+#   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.sftp_bc_target_group.arn
-    container_name   = local.application_data.accounts[local.environment].app_name
-    container_port   = local.application_data.accounts[local.environment].api_server_port
-  }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.sftp_bc_target_group.arn
+#     container_name   = local.application_data.accounts[local.environment].app_name
+#     container_port   = local.application_data.accounts[local.environment].api_server_port
+#   }
 
-  depends_on = [
-    aws_lb_listener.sftp_bc_listener,
-    aws_iam_role_policy_attachment.bc_ecs_task_execution_role
-  ]
-}
+#   depends_on = [
+#     aws_lb_listener.sftp_bc_listener,
+#     aws_iam_role_policy_attachment.bc_ecs_task_execution_role
+#   ]
+# }
