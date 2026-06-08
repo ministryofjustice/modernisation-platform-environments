c6fdb256c2ebd6b9fe56b9347038b4daaf071246
sivabathina2 <123939271+sivabathina2@users.noreply.github.com>
2026-06-08 15:57:16 +0100

    Update dms.tf

--- terraform/environments/data-factory-laa/dms.tf
+++ terraform/environments/data-factory-laa/dms.tf
@@ -474,7 +474,7 @@ module "dms_postgres" {
   # checkov:skip=CKV_TF_1: using branch ref for testing
   # checkov:skip=CKV_TF_2: using branch ref for testing
   count  = local.is-development ? 1 : 0
-  source = "github.com/ministryofjustice/terraform-dms-module?ref=c613aa7e78b2227204618713e45867e472aca2f4"
+  source = "github.com/ministryofjustice/terraform-dms-module?ref=2fdaf30207e33598ffd56f31e75fb3def5d91515"
 
   vpc_id      = data.aws_vpc.shared.id
   environment = local.environment
