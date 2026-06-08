1ee15d10ef2b4406a7aea4f108a5d36d26f83fd0
Siva Bathina <siva.bathina@justice.gov.uk>
2026-06-08 17:58:24 +0100

    updated module commit id

--- terraform/environments/data-factory-laa/dms.tf
+++ terraform/environments/data-factory-laa/dms.tf
@@ -479,7 +479,7 @@ module "dms_postgres" {
   # checkov:skip=CKV_TF_1: using branch ref for testing
   # checkov:skip=CKV_TF_2: using branch ref for testing
   count  = local.is-development ? 1 : 0
-  source = "github.com/ministryofjustice/terraform-dms-module?ref=2fdaf30207e33598ffd56f31e75fb3def5d91515"
+  source = "github.com/ministryofjustice/terraform-dms-module?ref=5a6b4dd49d1b2c92813f8bdb0b42e117ce247ba2"
 
   vpc_id      = data.aws_vpc.shared.id
   environment = local.environment
