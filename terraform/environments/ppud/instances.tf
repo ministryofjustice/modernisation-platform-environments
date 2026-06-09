73bd355786ca0e3e17406f56bf365ecc30d3760b
nbuckingham72 <nick.buckingham@colt.net>
2026-06-09 08:56:09 +0100

    Update_090626_2

--- terraform/environments/ppud/instances.tf
+++ terraform/environments/ppud/instances.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "PPUDWEBSERVER2" {
   instance_type          = "m5.large"
   source_dest_check      = true
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
-  vpc_security_group_ids = [aws_security_group.conditional["PPUD-Web-Portal-Server-Security-Group"].id]
+  vpc_security_group_ids = [aws_security_group.all["PPUD-Web-Portal-Server-Security-Group"].id]
   subnet_id              = data.aws_subnet.private_subnets_c.id
 
   metadata_options {
@@ -69,7 +69,7 @@ resource "aws_instance" "s609693lo6vw101" {
   instance_type          = "m5.large"
   source_dest_check      = true
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
-  vpc_security_group_ids = [aws_security_group.conditional["PPUD-Web-Portal-Server-Security-Group"].id]
+  vpc_security_group_ids = [aws_security_group.all["PPUD-Web-Portal-Server-Security-Group"].id]
   subnet_id              = data.aws_subnet.private_subnets_b.id
 
   metadata_options {
@@ -96,7 +96,7 @@ resource "aws_instance" "s609693lo6vw102" {
   instance_type          = "m5.large"
   source_dest_check      = true
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
-  vpc_security_group_ids = [aws_security_group.conditional["Document-Service-Server-Security-Group"].id]
+  vpc_security_group_ids = [aws_security_group.all["Document-Service-Server-Security-Group"].id]
   subnet_id              = data.aws_subnet.private_subnets_c.id
 
   metadata_options {
@@ -121,7 +121,7 @@ resource "aws_instance" "s609693lo6vw103" {
   instance_type          = "m5.large"
   source_dest_check      = true
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
-  vpc_security_group_ids = [aws_security_group.conditional["Document-Service-Server-Security-Group"].id]
+  vpc_security_group_ids = [aws_security_group.all["Document-Service-Server-Security-Group"].id]
   subnet_id              = data.aws_subnet.private_subnets_b.id
 
   metadata_options {
@@ -146,7 +146,7 @@ resource "aws_instance" "s609693lo6vw104" {
   instance_type          = "m5.large"
   source_dest_check      = true
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
-  vpc_security_group_ids = [aws_security_group.conditional["WAM-Data-Access-Server-Security-Group"].id]
+  vpc_security_group_ids = [aws_security_group.all["WAM-Data-Access-Server-Security-Group"].id]
   subnet_id              = data.aws_subnet.private_subnets_a.id
 
   metadata_options {
@@ -171,7 +171,7 @@ resource "aws_instance" "s609693lo6vw105" {
   instance_type          = "m5.large"
   source_dest_check      = true
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
-  vpc_security_group_ids = [aws_security_group.conditional["WAM-Web-Portal-Server-Security-Group"].id]
+  vpc_security_group_ids = [aws_security_group.all["WAM-Web-Portal-Server-Security-Group"].id]
   subnet_id              = data.aws_subnet.private_subnets_a.id
 
   metadata_options {
@@ -325,7 +325,7 @@ resource "aws_instance" "s609693lo6vw111" {
   instance_type          = "m5.large"
   source_dest_check      = true
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.id
-  vpc_security_group_ids = [aws_security_group.conditional["Document-Service-Server-Security-Group"].id]
+  vpc_security_group_ids = [aws_security_group.all["Document-Service-Server-Security-Group"].id]
   subnet_id              = data.aws_subnet.private_subnets_b.id
 
   metadata_options {
