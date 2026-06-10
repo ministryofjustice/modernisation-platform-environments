1fb629c4009cd1aff6c76930b269496fde188011
Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>
2026-06-10 14:20:52 +0100

    Get rid of commas

--- terraform/environments/data-platform/ai-gateway/environment-configuration.tf
+++ terraform/environments/data-platform/ai-gateway/environment-configuration.tf
@@ -18,7 +18,7 @@ locals {
         "35.176.93.186/32", # GlobalProtect (Alpha)
         # Sites
         "213.121.161.112/28", # 102PF
-        "51.149.2.0/24",      # 10SC
+        "51.149.2.0/24"       # 10SC
       ]
       ai_gateway_admin_ingress_allowlist = [
         # VPN
@@ -26,7 +26,7 @@ locals {
         "35.176.93.186/32", # GlobalProtect (Alpha)
         # Sites
         "213.121.161.112/28", # 102PF
-        "51.149.2.0/24",      # 10SC
+        "51.149.2.0/24"       # 10SC
       ]
       ai_gateway_models = local.ai_gateway_models
       ai_gateway_autoscaling = {
@@ -53,7 +53,7 @@ locals {
         "35.176.93.186/32", # GlobalProtect (Alpha)
         # Sites
         "213.121.161.112/28", # 102PF
-        "51.149.2.0/24",      # 10SC
+        "51.149.2.0/24"       # 10SC
       ]
       ai_gateway_admin_ingress_allowlist = [
         # VPN
@@ -61,7 +61,7 @@ locals {
         "35.176.93.186/32", # GlobalProtect (Alpha)
         # Sites
         "213.121.161.112/28", # 102PF
-        "51.149.2.0/24",      # 10SC
+        "51.149.2.0/24"       # 10SC
       ]
       ai_gateway_models = local.ai_gateway_models
       ai_gateway_autoscaling = {
@@ -88,7 +88,7 @@ locals {
         "35.176.93.186/32", # GlobalProtect (Alpha)
         # Sites
         "213.121.161.112/28", # 102PF
-        "51.149.2.0/24",      # 10SC
+        "51.149.2.0/24"       # 10SC
       ]
       ai_gateway_admin_ingress_allowlist = [
         # VPN
@@ -96,7 +96,7 @@ locals {
         "35.176.93.186/32", # GlobalProtect (Alpha)
         # Sites
         "213.121.161.112/28", # 102PF
-        "51.149.2.0/24",      # 10SC
+        "51.149.2.0/24"       # 10SC
       ]
       ai_gateway_models = local.ai_gateway_models
       ai_gateway_autoscaling = {
@@ -123,7 +123,7 @@ locals {
         "35.176.93.186/32", # GlobalProtect (Alpha)
         # Sites
         "213.121.161.112/28", # 102PF
-        "51.149.2.0/24",      # 10SC
+        "51.149.2.0/24"       # 10SC
       ]
       ai_gateway_admin_ingress_allowlist = [
         # VPN
@@ -131,7 +131,7 @@ locals {
         "35.176.93.186/32", # GlobalProtect (Alpha)
         # Sites
         "213.121.161.112/28", # 102PF
-        "51.149.2.0/24",      # 10SC
+        "51.149.2.0/24"       # 10SC
       ]
       ai_gateway_models = local.ai_gateway_models
       ai_gateway_autoscaling = {
