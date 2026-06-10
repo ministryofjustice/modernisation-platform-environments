e66d305d0fcf82cd13205d63028af7f118404f43
dami-moj <damilola.oyebade@justice.gov.uk>
2026-06-10 13:11:02 +0100

    ldf:update fabric-iam-role

--- terraform/environments/data-factory-laa/locals.tf
+++ terraform/environments/data-factory-laa/locals.tf
@@ -2,6 +2,6 @@
 locals {
   current_account_id              = data.aws_caller_identity.current.account_id
   current_account_region          = data.aws_region.current.region
-  fabric_tenant_id                = data.aws_secretsmanager_secret_version.fabric_tenant_id.secret_string
-  fabric_enterprise_app_object_id = data.aws_secretsmanager_secret_version.fabric_enterprise_app_object_id.secret_string
+  fabric_tenant_id                = local.is-development ? data.aws_secretsmanager_secret_version.fabric_tenant_id[0].secret_string : null
+  fabric_enterprise_app_object_id = local.is-development ? data.aws_secretsmanager_secret_version.fabric_enterprise_app_object_id[0].secret_string : null
 }
