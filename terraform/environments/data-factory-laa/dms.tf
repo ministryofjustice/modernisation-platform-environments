1fd2701f3a8850dd1b5b3ebf8c68d46fff51792f
Siva Bathina <siva.bathina@justice.gov.uk>
2026-06-11 12:51:27 +0100

    terraform module commit hash change

--- terraform/environments/data-factory-laa/dms.tf
+++ terraform/environments/data-factory-laa/dms.tf
@@ -479,7 +479,7 @@ module "dms_postgres" {
   # checkov:skip=CKV_TF_1: using branch ref for testing
   # checkov:skip=CKV_TF_2: using branch ref for testing
   count  = local.is-development ? 1 : 0
-  source = "github.com/ministryofjustice/terraform-dms-module?ref=075c7dde7f5259c0e28ba00af1cd1fd49336a3fb"
+  source = "github.com/ministryofjustice/terraform-dms-module?ref=6836884b6ddc6886922d802814ac0f44a6ce28ed"
 
   vpc_id      = data.aws_vpc.shared.id
   environment = local.environment
