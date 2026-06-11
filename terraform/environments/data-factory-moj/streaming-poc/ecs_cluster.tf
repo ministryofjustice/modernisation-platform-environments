76f38ebffbb55ec6581e16d8821e6c28f0040f1d
SloganAker <stuart.logan@justice.gov.uk>
2026-06-11 16:56:52 +0100

    Add cluster SG test #002

--- terraform/environments/data-factory-moj/streaming-poc/ecs_cluster.tf
+++ terraform/environments/data-factory-moj/streaming-poc/ecs_cluster.tf
@@ -4,8 +4,24 @@
 module "ecs_cluster" {
   count = contains(["development"], local.environment) ? 1 : 0
   
-  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//cluster?ref=v6.0.2"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-ecs-cluster//cluster?ref=v6.0.2"
   name   = "${local.ecs_prefix}-cluster"
   
   tags = local.tags
+}
+
+resource "aws_security_group" "cluster" {
+  count       = contains(["development"], local.environment) ? 1 : 0
+  
+  name_prefix = "${local.ecs_prefix}-cluster"
+  vpc_id      = data.aws_vpc.shared.id
+  description = "${local.ecs_prefix}-cluster SG"
+  
+  tags = merge(local.tags, {
+    Name = "${local.ecs_prefix}-cluster"
+  })
+  
+  lifecycle {
+    create_before_destroy = true
+  }
 }
