d4475958c87fe1a3b4feea0751c03f2d3c761d0a
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-06-11 12:40:13 +0000

    feat(data-platform): encrypt RDS secret with KMS key

--- terraform/environments/data-platform/app/secrets.tf
+++ terraform/environments/data-platform/app/secrets.tf
@@ -1,7 +1,8 @@
 module "app_rds_credentials" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=d03382d3ec9c12b849fbbe35b770eaa047f7bbea" # v2.1.0
 
-  name = "${local.component_name}/rds"
+  name       = "${local.component_name}/rds"
+  kms_key_id = module.rds_encryption.key_arn
 
   secret_string = jsonencode({
     username = module.app_rds.db_instance_username
