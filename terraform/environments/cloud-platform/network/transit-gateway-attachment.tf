590bcfe616fba178457e187c188979ce7ce1b959
jackstockley89 <jack.stockley@digital.justice.gov.uk>
2026-06-12 10:40:27 +0100

    feat(TF): add count to tgw resource

--- terraform/environments/cloud-platform/network/transit-gateway-attachment.tf
+++ terraform/environments/cloud-platform/network/transit-gateway-attachment.tf
@@ -1,7 +1,8 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "modernisation_platform" {
+  count = contains(["cloud-platform-development"], terraform.workspace) ? 1 : 0
   subnet_ids         = resource.aws_subnet.tgw_private[*].id
   transit_gateway_id = "tgw-053d9dd7f1222a554"
-  vpc_id             = module.cluster_vpc.vpc_id
+  vpc_id             = module.cluster_vpc[0].vpc_id
 
   tags = local.tags
 }
