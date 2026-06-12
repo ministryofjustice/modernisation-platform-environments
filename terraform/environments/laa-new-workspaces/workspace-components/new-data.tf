1efe6304b74072c9b3fb320b530a715fd9cc1003
Vladimirs Kovalovs <vladimirs.kovalovs1@justice.gov.uk>
2026-06-12 11:46:37 +0100

    [STB-4268-v1] monir change

--- terraform/environments/laa-new-workspaces/workspace-components/new-data.tf
+++ terraform/environments/laa-new-workspaces/workspace-components/new-data.tf
@@ -2,7 +2,7 @@
 ### MoJ Transit Gateway for Transit Gateway VPC attachments
 ##############################################################
 data "aws_ec2_transit_gateway" "moj_tgw" {
-  id = try(local.application_variables.accounts[local.environment].transit_gateway_id, null)
+  id = try(local.application_data.accounts[local.environment].transit_gateway_id, null)
 }
 
 
