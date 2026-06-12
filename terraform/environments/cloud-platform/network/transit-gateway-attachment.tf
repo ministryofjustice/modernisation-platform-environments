595106eb71228c83463b978615da8ba5a811f75f
jackstockley89 <jack.stockley@digital.justice.gov.uk>
2026-06-12 10:28:19 +0100

    feat(TF): add valid tags

--- terraform/environments/cloud-platform/network/transit-gateway-attachment.tf
+++ terraform/environments/cloud-platform/network/transit-gateway-attachment.tf
@@ -3,7 +3,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "modernisation_platform" {
   transit_gateway_id = "tgw-053d9dd7f1222a554"
   vpc_id             = module.cluster_vpc.vpc_id
 
-  tags = tags
+  tags = local.tags
 }
 
 
