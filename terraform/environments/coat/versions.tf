f3c0d1929ff14685b9cf72d0ff817d5cfdd78b4f
mbr-moj <belal.rashid@justice.gov.uk>
2026-06-12 09:58:48 +0100

    Update versions.tf

--- terraform/environments/coat/versions.tf
+++ terraform/environments/coat/versions.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/null"
       version = "~> 3.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
     external = {
       source  = "hashicorp/external"
       version = "~> 2.0"
