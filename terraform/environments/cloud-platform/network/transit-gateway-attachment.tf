b8f2f78ecd100ea951991160777170afe577f8c8
jackstockley89 <jack.stockley@digital.justice.gov.uk>
2026-06-12 10:25:43 +0100

    feat(TF): change subnets to tgw subnets

--- terraform/environments/cloud-platform/network/transit-gateway-attachment.tf
+++ terraform/environments/cloud-platform/network/transit-gateway-attachment.tf
@@ -1,5 +1,5 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "modernisation_platform" {
-  subnet_ids         = [module.cluster_vpc.private_subnet_ids, module.cluster_vpc.public_subnet_ids]
+  subnet_ids         = resource.aws_subnet.tgw_private[*].id
   transit_gateway_id = "tgw-053d9dd7f1222a554"
   vpc_id             = module.cluster_vpc.vpc_id
 
