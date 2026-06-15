935f0223773d127cdc54e3bbd826b33754e12ed5
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-06-16 00:20:07 +0100

    fix: warning fixed

--- terraform/environments/ccms-ebs/ccms-file-transfer/lambda-process-file-from-bucket.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/lambda-process-file-from-bucket.tf
@@ -84,7 +84,7 @@ resource "aws_lambda_function" "process_file_from_bucket_lambda_function" {
 
   lifecycle {
     ignore_changes = [
-      source_code_hash, filename, handler, qualified_arn, qualified_invoke_arn, version
+      source_code_hash, filename, handler, qualified_invoke_arn, version
     ]
   }
 }
