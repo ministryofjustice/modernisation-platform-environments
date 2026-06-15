dc572e1e8e1ba6af15d2519dbce72cbb9d90d340
Thomas Tipler <thomas.tipler@necsws.com>
2026-06-15 12:00:55 +0100

    update aurora provider

--- terraform/environments/youth-justice-app-framework/modules/aurora/providers.tf
+++ terraform/environments/youth-justice-app-framework/modules/aurora/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 5.0"
+      version               = "~> 5.93"
       configuration_aliases = [aws.core-network-services]
     }
     archive = {
