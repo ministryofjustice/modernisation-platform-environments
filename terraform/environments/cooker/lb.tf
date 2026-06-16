03967a2c6c60ace209dc762983aa95ab4e4c754c
mikereiddigital <mike.reid@justice.gov.uk>
2026-06-16 10:16:15 +0100

    cooker testing lb module

---
+++ terraform/environments/cooker/lb.tf
@@ -0,0 +1,47 @@
+locals {
+
+  loadbalancer_ingress_rules = {
+    "cluster_ec2_lb_ingress" = {
+      description = "allow access on HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["188.214.15.75/32"]
+      security_groups = []
+    }
+  }
+
+  loadbalancer_egress_rules = {
+    "cluster_ec2_lb_egress" = {
+      description     = "Open all outbound ports"
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  }
+
+
+}
+
+
+module "lb_access_logs_enabled" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-loadbalancer?ref=97cb25f5d84bc10f10d4cab2e63d4d2e2b282f2f" #vtesting
+  providers = {
+    aws.bucket-replication = aws
+  }
+  vpc_all                    = "${local.vpc_name}-${local.environment}"
+  force_destroy_bucket       = true # enables destruction of logging bucket
+  application_name           = local.application_name
+  public_subnets             = data.aws_subnets.shared-public.ids
+  loadbalancer_ingress_rules = local.loadbalancer_ingress_rules
+  loadbalancer_egress_rules  = local.loadbalancer_egress_rules
+  account_number             = local.environment_management.account_ids[terraform.workspace]
+  region                     = "eu-west-2"
+  enable_deletion_protection = false
+  idle_timeout               = 60
+  tags                       = { Name = "lb_module" }
+
+}
+
