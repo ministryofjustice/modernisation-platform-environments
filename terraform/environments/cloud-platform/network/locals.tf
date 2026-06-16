25c6a23046e1654d49bd36544b47b7b81b8cea70
David Elliott <david.elliott@digital.justice.gov.uk>
2026-06-16 16:58:52 +0100

    Create a new workflow for container platform business units (#17153)
    * Create a new workflow for container platform business units
    
    - add a dedicated container-platform workflow for cloud-platform environments
    - load account names from terraform/environments/cloud-platform/accounts.json
    - derive environment names from account names in the setup job
    - pass optional account_name through the reusable terraform workflow
    
    * Add component to job name
    
    Makes it easier to see which deploy is running when using multiple components.
    
    * Remove apply componenet name
    
    * Add logic to associate with the correct github environment.
    
    This is needed as we are using the cloud-platform application, but applying that code to different accounts.
    We want these accounts to have the correct environment so that we can amend the protection rules (approval gates) if we want.
    
    * Amend concurency so that correct environment is referenced
    
    If deploying to a custom account_name this is used in for the concurrency.
    
    * Fixes - route53, workspace selection and skip evaluator
    
    Route53 - refactors route53 config so that it now runs on workspace rather than environment.  Add entries for octo.
    
    Workspace selection - this was still defaulting to the application-environment.  Change to account name now that we are building from a different application for the container-platform accounts.
    
    Skip evaluator - there are going to be another 16 CP accounts, this will be too many for listing in the workflow file.  Moving it out into a config file to keep things neat and you no longer have to amend the workflow to add a new skip.
    
    * Add script to generate workpaces for container platform accounts in cp
    
    This is required as we are using the cloud-platform codebase to build out terraform in accounts outside that application.
    
    * Switch sprinkler to use the normal reusable workflow
    
    So that we can test that this works.
    
    * Add a test local to trigger plan on sprinkler
    
    * remove after testing

--- terraform/environments/cloud-platform/network/locals.tf
+++ terraform/environments/cloud-platform/network/locals.tf
@@ -4,14 +4,15 @@ locals {
     "cloud-platform-preproduction",
     "cloud-platform-live",
   ]
-  enabled_workspaces  = ["development_cluster"]
   cluster_environment = contains(local.mp_environments, terraform.workspace) ? local.environment : "development_cluster"
   cp_vpc_name         = terraform.workspace
 
   vpc_cidr = {
-    cloud-platform-development   = "10.195.32.0/20"
-    cloud-platform-preproduction = "10.195.16.0/20"
-    cloud-platform-live          = "10.195.0.0/20"
+    cloud-platform-development      = "10.195.32.0/20"
+    cloud-platform-preproduction    = "10.195.16.0/20"
+    cloud-platform-live             = "10.195.0.0/20"
+    container-platform-octo-nonlive = "10.195.48.0/20"
+    container-platform-octo-live    = "10.41.0.0/20"
   }
 
   vpc_flow_log_cloudwatch_log_group_name_prefix       = "/aws/vpc-flow-log/"
