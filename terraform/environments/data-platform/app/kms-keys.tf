ad21d93666adb867c9abea7a54bada51a6cd79b9
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-06-16 12:57:17 +0000

    Add app secret KMS key

--- terraform/environments/data-platform/app/kms-keys.tf
+++ terraform/environments/data-platform/app/kms-keys.tf
@@ -51,3 +51,51 @@ module "rds_encryption" {
 
   deletion_window_in_days = 7
 }
+
+module "app_secret_encryption" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-kms.git?ref=407e3db34a65b384c20ef718f55d9ceacb97a846" # v4.2.0
+
+  description           = "KMS key for app secrets encryption"
+  enable_default_policy = true
+
+  aliases = ["secrets/${local.component_name}"]
+  key_statements = [
+    {
+      sid = "AllowSecretsManagerServiceEncryption"
+      actions = [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncryptFrom",
+        "kms:ReEncryptTo",
+        "kms:GenerateDataKey",
+        "kms:GenerateDataKeyWithoutPlaintext",
+        "kms:DescribeKey",
+        "kms:CreateGrant",
+        "kms:ListGrants",
+        "kms:RevokeGrant",
+      ]
+      resources = ["*"]
+      effect    = "Allow"
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["secretsmanager.amazonaws.com"]
+        }
+      ]
+      condition = [
+        {
+          test     = "StringEquals"
+          variable = "kms:ViaService"
+          values   = ["secretsmanager.${data.aws_region.current.region}.amazonaws.com"]
+        },
+        {
+          test     = "StringEquals"
+          variable = "kms:CallerAccount"
+          values   = [data.aws_caller_identity.current.account_id]
+        }
+      ]
+    }
+  ]
+
+  deletion_window_in_days = 7
+}
