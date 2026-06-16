d63f78d726e54065a65c668cfa17a49d7fbceb0f
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2026-06-16 15:33:22 +0100

    Rename cooker state KMS backend canary

--- terraform/environments/cooker/state-kms-backend-canary/main.tf
+++ terraform/environments/cooker/state-kms-backend-canary/main.tf
@@ -1,5 +1,5 @@
 # Temporary member-side state write canary for validating the real Modernisation Platform state bucket.
-resource "terraform_data" "example_state_kms_backend_canary" {
+resource "terraform_data" "state_kms_backend_canary" {
   input = {
     purpose   = "validate cooker can write Terraform state to the real Modernisation Platform state bucket"
     workspace = terraform.workspace
