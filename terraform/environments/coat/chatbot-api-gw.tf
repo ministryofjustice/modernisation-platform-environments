aa41041ff8e00f89c73e5b8dc673973ee1249819
mbr-moj <belal.rashid@justice.gov.uk>
2026-06-16 10:46:17 +0100

    integration_http_method dont need for MOCK - causes plan loop

--- terraform/environments/coat/chatbot-api-gw.tf
+++ terraform/environments/coat/chatbot-api-gw.tf
@@ -50,11 +50,10 @@ resource "aws_api_gateway_method" "send_request_options" {
 }
 
 resource "aws_api_gateway_integration" "send_request_options_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id             = aws_api_gateway_resource.send_request.id
-  http_method             = aws_api_gateway_method.send_request_options.http_method
-  type                    = "MOCK"
-  integration_http_method = "OPTIONS"
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id = aws_api_gateway_resource.send_request.id
+  http_method = aws_api_gateway_method.send_request_options.http_method
+  type        = "MOCK"
 
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
