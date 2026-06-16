0c2e4e7d98e8b0a0a1cb23f92dad925a570022c3
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2026-06-16 15:21:21 +0100

    remove changes for new stage

--- terraform/environments/example/state-kms-backend-bootstrap/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "modernisation_platform_organisation_unit_id" {
-  description = "Modernisation Platform OU ID used to mirror the production state bucket KMS key policy."
-  type        = string
-}
-
-variable "canary_writer_account_id" {
-  description = "Member account ID allowed to write canary Terraform backend state into the example test bucket."
-  type        = string
-}
--- terraform/environments/example/test.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "test" {
-  name  = "/terraform/test"
-  type  = "String"
-  value = "hello"
-}
