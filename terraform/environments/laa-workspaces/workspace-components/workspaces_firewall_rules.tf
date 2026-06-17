4549d8cec6c05c44a7045a27b6e22ffa7735759c
Muhammad-MOJ <MuhammadImran.Khan@justice.gov.uk>
2026-06-17 12:51:03 +0100

    making stateful rule drop all and alert all

--- terraform/environments/laa-workspaces/workspace-components/workspaces_firewall_rules.tf
+++ terraform/environments/laa-workspaces/workspace-components/workspaces_firewall_rules.tf
@@ -348,7 +348,7 @@ resource "aws_networkfirewall_firewall_policy" "workspaces_web_allowlist" {
   firewall_policy {
     stateless_default_actions          = ["aws:forward_to_sfe"]
     stateless_fragment_default_actions = ["aws:forward_to_sfe"]
-    stateful_default_actions           = ["aws:drop_strict"]
+    stateful_default_actions           = ["aws:alert_strict", "aws:drop_strict"] 
 
     stateful_engine_options {
       rule_order = "STRICT_ORDER"
