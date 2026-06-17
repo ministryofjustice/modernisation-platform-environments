12949968ed329d229ce601fc41366742d66e45ae
David Elliott <david.elliott@digital.justice.gov.uk>
2026-06-17 12:26:11 +0100

    Fix local.environment not being correct
    Because we are using a different account to deploy to for container-platform-bus, the local.environment provided in platform_locals doesn't work any more as it uses the application-name which is cloud-platform.  Changing to using cluster_environment and amending that so that it gets the correct environment (eg nonlive or live)

--- terraform/environments/cloud-platform/cluster/locals.tf
+++ terraform/environments/cloud-platform/cluster/locals.tf
@@ -12,6 +12,7 @@ locals {
 
   environment_configuration = local.environment_configurations[local.cluster_environment]
   cp_vpc_name               = local.cluster_environment == "development_cluster" ? "cloud-platform-development" : terraform.workspace
-  cluster_name              = contains(local.mp_environments, terraform.workspace) ? local.environment : terraform.workspace
-  cluster_environment       = contains(local.mp_environments, terraform.workspace) ? local.environment : "development_cluster"
+  workspace_environment     = element(reverse(split("-", terraform.workspace)), 0)
+  cluster_name              = contains(local.mp_environments, terraform.workspace) ? local.workspace_environment : terraform.workspace
+  cluster_environment       = contains(local.mp_environments, terraform.workspace) ? local.workspace_environment : "development_cluster"
 }
