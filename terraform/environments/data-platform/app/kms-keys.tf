1133e75aa724c8ac5e839ce08ecc38753c286b5d
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-06-17 14:48:57 +0000

    refactor: use existing common key

--- terraform/environments/data-platform/app/kms-keys.tf
+++ terraform/environments/data-platform/app/kms-keys.tf
@@ -51,16 +51,3 @@ module "rds_encryption" {
 
   deletion_window_in_days = 7
 }
-
-module "app_secret_encryption" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
-
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-kms.git?ref=407e3db34a65b384c20ef718f55d9ceacb97a846" # v4.2.0
-
-  aliases               = ["secrets/${local.component_name}"]
-  description           = "App Secrets Manager KMS key"
-  enable_default_policy = true
-
-  deletion_window_in_days = 7
-}
