9ae15c35c29e15eba6f2b18c9e1a702a5b781b4a
SloganAker <stuart.logan@justice.gov.uk>
2026-06-18 11:25:23 +0100

    Investigating s3 object(jar file) unable to get issue

--- terraform/environments/data-factory-moj/streaming-poc-maf/security_groups.tf
+++ terraform/environments/data-factory-moj/streaming-poc-maf/security_groups.tf
@@ -27,34 +27,21 @@ resource "aws_vpc_security_group_egress_rule" "egress_to_s3" {
   }
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# TODO: Remove this security group once MSK is deployed to the shared account
+# ---------------------------------------------------------------------------------------------------------------------
 resource "aws_security_group" "allow_msk" {
   count       = contains(local.deploy_to, local.environment) ? 1 : 0
   name        = "allow-msk-egress-from-flink"
   description = "Allow egress to MSK Serverless cluster - VPC only access"
   vpc_id      = data.aws_vpc.shared.id
 
-  ingress {
-    from_port   = 9098
-    to_port     = 9098
-    protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.shared.cidr_block]
-    description = "IAM SASL authentication from VPC"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [data.aws_vpc.shared.cidr_block]
-    description = "Allow outbound within VPC only"
-  }
-
   tags = {
     Name = "allow-msk-egress-from-flink"
   }
 }
 
-resource "aws_vpc_security_group_egress_rule" "msk_iam_auth" {
+resource "aws_vpc_security_group_ingress_rule" "msk_iam_auth" {
   count             = contains(local.deploy_to, local.environment) ? 1 : 0
   security_group_id = aws_security_group.allow_msk[0].id
   description       = "IAM SASL authentication from VPC"
@@ -64,3 +51,15 @@ resource "aws_vpc_security_group_egress_rule" "msk_iam_auth" {
   ip_protocol = "tcp"
   cidr_ipv4   = data.aws_vpc.shared.cidr_block
 }
+
+resource "aws_vpc_security_group_egress_rule" "msk_outbound_vpc" {
+  count             = contains(local.deploy_to, local.environment) ? 1 : 0
+  security_group_id = aws_security_group.allow_msk[0].id
+  description       = "Allow outbound within VPC only"
+  
+  from_port   = 0
+  to_port     = 0
+  ip_protocol = "-1"
+  cidr_ipv4   = data.aws_vpc.shared.cidr_block
+}
+# ---------------------------------------------------------------------------------------------------------------------
