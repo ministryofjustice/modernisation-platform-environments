378d9590cefe3ca27e0dd3b7ab607f84cd445163
Vladimirs Kovalovs <vladimirs.kovalovs1@justice.gov.uk>
2026-06-18 11:21:56 +0100

    [STB-4268-v10] adding tgw data and attachment

--- terraform/environments/laa-new-workspaces/workspace-components/new-vpc-subnets-rt-igw-tgw.tf
+++ terraform/environments/laa-new-workspaces/workspace-components/new-vpc-subnets-rt-igw-tgw.tf
@@ -144,18 +144,18 @@ resource "aws_route_table_association" "public_b" {
 }
 
 
-# ##############################################
-# ### Transit Gateway Attachment
-# ##############################################
+##############################################
+### Transit Gateway Attachment
+##############################################
 
-# resource "aws_ec2_transit_gateway_vpc_attachment" "moj_tgw" {
-#   transit_gateway_id                 = data.aws_ec2_transit_gateway.moj_tgw.id
-#   vpc_id                             = aws_vpc.workspaces.id
-#   subnet_ids                         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
-#   security_group_referencing_support = "enable"
+resource "aws_ec2_transit_gateway_vpc_attachment" "moj_tgw" {
+  transit_gateway_id                 = data.aws_ec2_transit_gateway.moj_tgw.id
+  vpc_id                             = aws_vpc.workspaces.id
+  subnet_ids                         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+  security_group_referencing_support = "enable"
   
-#   tags = merge(
-#     local.tags,
-#     { "Name" = "${local.application_name}-${local.environment}-tgw-attachment" }
-#   )
-# }
+  tags = merge(
+    local.tags,
+    { "Name" = "${local.application_name}-${local.environment}-tgw-attachment" }
+  )
+}
