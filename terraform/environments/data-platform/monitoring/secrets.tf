c554eeb9ac57ced4cc29b557b16a8eb0cf7cfd31
Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>
2026-06-19 15:35:43 +0100

    Create environment configuration and set count

--- terraform/environments/data-platform/monitoring/secrets.tf
+++ terraform/environments/data-platform/monitoring/secrets.tf
@@ -1,6 +1,8 @@
 module "grafana_entra_id_secret" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager.git?ref=d03382d3ec9c12b849fbbe35b770eaa047f7bbea" # v2.1.0
 
+  count = local.environment_configuration.monitoring_stack_enabled ? 1 : 0
+
   name = "${local.component_name}/grafana-entra-id"
 
   secret_string = jsonencode({
