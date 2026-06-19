5526d4efce5b2f0bbd0ef7dd94e5f235e8bcdf89
Matthew Price <Matthew.Price2@justice.gov.uk>
2026-06-19 11:03:19 +0100

    update S3 bucket module source to MP s3 bucket

--- terraform/environments/data-factory-moj/s3.tf
+++ terraform/environments/data-factory-moj/s3.tf
@@ -1,6 +1,7 @@
 module "s3_bucket" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git?ref=af0286ff37a66c2b79faf360e6e2663744b8e5b5" # v5.13.0
-
-  bucket           = "intermediate-salad-aisle-rehearsal"
+  # source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git?ref=af0286ff37a66c2b79faf360e6e2663744b8e5b5" # v5.13.0
+  source = "git::https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=c60d60b9b31ee2544e34adc3b6e82db5f2b95672"
+  
+  bucket_name      = "intermediate-salad-aisle-rehearsal"
   bucket_namespace = "account-regional"
 }
