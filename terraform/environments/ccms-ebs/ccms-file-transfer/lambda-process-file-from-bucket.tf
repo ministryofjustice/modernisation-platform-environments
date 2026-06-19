7d7c0c0d5edcf0914ce2647a970c0e7aac53c6cc
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-06-19 15:28:27 +0100

    fix: lifecycle ignore enabled

--- terraform/environments/ccms-ebs/ccms-file-transfer/lambda-process-file-from-bucket.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/lambda-process-file-from-bucket.tf
@@ -87,11 +87,11 @@ resource "aws_lambda_function" "process_file_from_bucket_lambda_function" {
     Name = "${local.application_name}-${local.environment}-process-file-from-bucket"
   })
 
-  # lifecycle {
-  #   ignore_changes = [
-  #     source_code_hash, filename, handler
-  #   ]
-  # }
+  lifecycle {
+    ignore_changes = [
+      source_code_hash, filename, handler
+    ]
+  }
 }
 
 resource "aws_lambda_permission" "allow_s3_invoke" {
