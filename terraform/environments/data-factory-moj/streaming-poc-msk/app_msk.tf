6cd9dcfff5eaff175983fdb20e272407bae6f311
vshahaker <vishal.shah@justice.gov.uk>
2026-06-20 23:14:14 +0100

    corrected typo and updated component name

--- terraform/environments/data-factory-moj/streaming-poc-msk/app_msk.tf
+++ terraform/environments/data-factory-moj/streaming-poc-msk/app_msk.tf
@@ -5,7 +5,7 @@ resource "aws_msk_serverless_cluster" "cluster" {
 
   vpc_config {
     subnet_ids         = data.aws_subnets.shared-private.ids
-    security_group_ids = [aws_security_group.msk[0].id]
+    security_group_ids = [aws_security_group.msk_1[0].id]
   }
 
   lifecycle {
@@ -45,32 +45,32 @@ resource "aws_security_group" "msk" {
 
 }
 
-# resource "aws_security_group" "msk_1" {
-#   count       = contains(["development"], local.environment) ? 1 : 0
-#   name_prefix = "${local.cluster_name}-sg"
-#   description = "Security group for MSK Serverless cluster"
-#   vpc_id      = data.aws_vpc.shared.id
+resource "aws_security_group" "msk_1" {
+  count       = contains(["development"], local.environment) ? 1 : 0
+  name_prefix = "${local.cluster_name}-sg"
+  description = "Security group for MSK Serverless cluster"
+  vpc_id      = data.aws_vpc.shared.id
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
+  lifecycle {
+    create_before_destroy = true
+  }
 
-#   tags = merge(local.extended_tags, {
-#     name        = "{local.cluster_name}-sg",
-#     description = "Security group for MSK Serverless cluster"
-#   })
+  tags = merge(local.extended_tags, {
+    name        = "${local.cluster_name}-sg",
+    description = "Security group for MSK Serverless cluster"
+  })
 
-# }
+}
 
-# resource "aws_vpc_security_group_ingress_rule" "private_subnets_1" {
-#   for_each          = local.msk_sg_ingress_cidr
-#   security_group_id = aws_security_group.msk_1[0].id
-#   description       = "Allow inbound traffic from private subnet ${each.value}"
-#   from_port         = 9098
-#   to_port           = 9098
-#   ip_protocol       = "tcp"
-#   cidr_ipv4         = each.value
-# }
+resource "aws_vpc_security_group_ingress_rule" "private_subnets_1" {
+  for_each          = local.msk_sg_ingress_cidr
+  security_group_id = aws_security_group.msk_1[0].id
+  description       = "Allow inbound traffic from private subnet ${each.value}"
+  from_port         = 9098
+  to_port           = 9098
+  ip_protocol       = "tcp"
+  cidr_ipv4         = each.value
+}
 
 resource "aws_vpc_security_group_ingress_rule" "private_subnets" {
   for_each          = local.msk_sg_ingress_cidr
