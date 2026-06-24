b95443be7d1c33bdc310fea668b89c1408a49a2e
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2026-06-24 12:57:43 +0000

    Drop import blocks, now import complete

--- terraform/environments/data-platform/iam-roles.tf
+++ terraform/environments/data-platform/iam-roles.tf
@@ -1,24 +1,3 @@
 module "monitoring" {
   source = "./modules/monitoring"
 }
-
-# Import blocks for moving resources from monitoring workspace to data-platform workspace
-import {
-  to = module.monitoring.module.iam_role.aws_iam_role.this[0]
-  id = "data-platform-monitoring"
-}
-
-import {
-  to = module.monitoring.aws_iam_role_policy_attachment.cloudwatch_read_only_access
-  id = "data-platform-monitoring/arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
-}
-
-import {
-  to = module.monitoring.aws_iam_role_policy_attachment.amazon_prometheus_query_access
-  id = "data-platform-monitoring/arn:aws:iam::aws:policy/AmazonPrometheusQueryAccess"
-}
-
-import {
-  to = module.monitoring.aws_iam_role_policy_attachment.aws_xray_read_only_access
-  id = "data-platform-monitoring/arn:aws:iam::aws:policy/AWSXrayReadOnlyAccess"
-}
