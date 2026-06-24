87d3409c7fd94120da9d03827daa325044690aec
NasraMohammedjustice <Nasra.Mohammed@justice.gov.uk>
2026-06-24 12:58:06 +0100

    added a count to apply changes to dev  only

--- terraform/environments/apex/locals.tf
+++ terraform/environments/apex/locals.tf
@@ -70,11 +70,10 @@ locals {
     region            = local.application_data.accounts[local.environment].region
     app_db_url        = "${aws_route53_record.apex-db.fqdn}:1521:APEX"
     app_debug_enabled = local.application_data.accounts[local.environment].app_debug_enabled
-    # Note that the following secret is now stored in Secrets Manager
-    # Previous value kept for rollback:
-    # db_secret_arn = "arn:aws:ssm:${local.application_data.accounts[local.environment].region}:${local.env_account_id}:parameter/${local.app_db_password_name}"
-    db_secret_arn = aws_secretsmanager_secret.app_apex_dbpassword_tad.arn
+    db_secret_arn     = local.db_secret_arn
   })
+
+  db_secret_arn = local.environment == "development" ? try(aws_secretsmanager_secret.app_apex_dbpassword_tad[0].arn, "") : "arn:aws:ssm:${local.application_data.accounts[local.environment].region}:${local.env_account_id}:parameter/${local.app_db_password_name}"
 
   env_account_id       = local.environment_management.account_ids[terraform.workspace]
   app_db_password_name = "APP_APEX_DBPASSWORD_TAD"
