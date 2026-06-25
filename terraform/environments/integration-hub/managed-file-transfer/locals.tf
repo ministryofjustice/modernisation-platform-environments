34fbac6463e35628391ba4f45d161c237e622f1e
David Sibley <david.sibley@justice.gov.uk>
2026-06-25 17:27:52 +0100

    set hostname for cors rule deterministically

--- terraform/environments/integration-hub/managed-file-transfer/locals.tf
+++ terraform/environments/integration-hub/managed-file-transfer/locals.tf
@@ -3,6 +3,8 @@ locals {
   bucket_configuration     = try(local.application_data.accounts[local.environment].bucket_configuration, {})
   custom_idp_configuration = try(local.application_data.accounts[local.environment].custom_idp_configuration, {})
   iam_configuration        = try(local.application_data.accounts[local.environment].iam_configuration, {})
+  web_app_hostname         = local.is-production == false ? "web.${local.environment}.managed-file-transfer.service.justice.gov.uk" : "web.managed-file-transfer.service.justice.gov.uk"
+  web_app_origin           = "https://${local.web_app_hostname}"
   notification_configuration = try(
     local.application_data.accounts[local.environment].notification_configuration,
     {},
