59afecce81a7bcfd2f567aeaf0a74713bc9fddaa
vshahaker <vishal.shah@justice.gov.uk>
2026-06-25 07:42:54 +0100

    checking if session manager is installed

--- terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_locals.tf
+++ terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_locals.tf
@@ -14,7 +14,7 @@ locals {
       }),
       "\"",
       "\\\\\\\""
-    ), "ACCOUNT_ID", data.aws_caller_identity.current.account_id), "USER", local.os_creds.username)
+    ), "ACCOUNT_ID", data.aws_caller_identity.current.account_id), "USER", local.region)
   }
 
   #   geo_fence_events_b64 = base64encode(
--- terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_os_roles.tf
+++ terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_os_roles.tf
@@ -6,6 +6,8 @@ resource "null_resource" "run_role_mappings" {
 
   provisioner "local-exec" {
     command = <<EOT
+      set -x
+
       TASK_ARN=$(aws ecs list-tasks \
         --cluster "streaming-pov-ecs-cluster" \
         --service-name "streaming-pov-ecs-sdg" \
@@ -19,16 +21,18 @@ resource "null_resource" "run_role_mappings" {
         echo "Error: No running ECS tasks found for this service!"
         exit 1
       fi
-
+      session-manager-plugin --version || echo MISSING 
       script -q /dev/null aws ecs execute-command \
         --cluster "streaming-pov-ecs-cluster" \
         --task "$TASK_ARN" \
         --container "streaming-pov-ecs-sdg" \
         --interactive \
         --region "eu-west-2" \
-        --command "sh -c 'echo ${local.escaped_payloads[each.key]} > /tmp/rolemapping_payload.json; /usr/bin/curl -k -X PUT 'https://${data.aws_opensearch_domain.moj_domain.endpoint}/_plugins/_security/api/rolesmapping/${each.key}' -H \"osd-xsrf: true\" -H \"Content-Type: application/json\" -u '${local.os_creds.username}:${local.os_creds.password}' -d @/tmp/rolemapping_payload.json > /tmp/rolemapping.out'"
+        --command "sh -c 'echo ${local.escaped_payloads[each.key]} > /tmp/rolemapping_payload.json'"
 
       sleep 10
 EOT
   }
 }
+
+# --command "sh -c 'echo ${local.escaped_payloads[each.key]} > /tmp/rolemapping_payload.json; /usr/bin/curl -k -X PUT 'https://${data.aws_opensearch_domain.moj_domain.endpoint}/_plugins/_security/api/rolesmapping/${each.key}' -H \"osd-xsrf: true\" -H \"Content-Type: application/json\" -u '${local.os_creds.username}:${local.os_creds.password}' -d @/tmp/rolemapping_payload.json > /tmp/rolemapping.out'"
