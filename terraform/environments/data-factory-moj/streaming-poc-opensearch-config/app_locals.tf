1f64d5caba073842225ecdd0f546437bb3c8f153
vshahaker <vishal.shah@justice.gov.uk>
2026-06-25 21:57:05 +0100

    provision s3 resource

--- terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_locals.tf
+++ terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_locals.tf
@@ -1,23 +1,6 @@
 locals {
-  all_vars      = jsondecode(file("${path.cwd}/../application_variables.json"))
-  os_mappings   = try(local.all_vars.accounts[local.environment].opensearch_role_mappings, {})
-  saved_objects = fileset("${path.cwd}/saved-objects", "*.ndjson")
-  os_creds      = jsondecode(data.aws_secretsmanager_secret_version.opensearch_credentials.secret_string)
-  region        = "eu-west-2"
-  extended_tags = merge(local.tags, {})
-
-  escaped_payloads = {
-    for role_name, mapping_data in local.os_mappings : role_name => replace(replace(replace(
-      jsonencode({
-        backend_roles = mapping_data["backend_roles"]
-        users         = mapping_data["users"]
-      }),
-      "\"",
-      "\\\\\\\""
-    ), "ACCOUNT_ID", data.aws_caller_identity.current.account_id), "USER", local.region)
-  }
-
-  #   geo_fence_events_b64 = base64encode(
-  #     file("${path.cwd}/index/geo-fence-events.json")
-  #   )
+  deploy_to = ["development"]
+  extended_tags = merge(local.tags, {
+    component = "streaming-pov-opensearch-config"
+  })
 }
---
+++ terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_s3.tf
@@ -0,0 +1,108 @@
+resource "aws_kms_key" "s3_key" {
+  count                   = contains(local.deploy_to, local.environment) ? 1 : 0
+  description             = "KMS key for private S3 bucket encryption"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = merge(local.extended_tags, {
+    description = "KMS key for tfstate bucket"
+  })
+}
+
+resource "aws_kms_alias" "s3_key_alias" {
+  count         = contains(local.deploy_to, local.environment) ? 1 : 0
+  name          = "alias/streaming-poc-s3-tfstate"
+  target_key_id = aws_kms_key.s3_key[0].key_id
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  count         = contains(local.deploy_to, local.environment) ? 1 : 0
+  bucket        = "streaming-poc-tfstate"
+  force_destroy = false
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  count  = contains(local.deploy_to, local.environment) ? 1 : 0
+  bucket = aws_s3_bucket.tfstate[0].id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  count  = contains(local.deploy_to, local.environment) ? 1 : 0
+  bucket = aws_s3_bucket.tfstate[0].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_key[0].arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+# 5. Block All Public Access (Bucket Level)
+resource "aws_s3_bucket_public_access_block" "public_block" {
+  count  = contains(local.deploy_to, local.environment) ? 1 : 0
+  bucket = aws_s3_bucket.tfstate[0].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "secure_policy" {
+  count  = contains(local.deploy_to, local.environment) ? 1 : 0
+  bucket = aws_s3_bucket.tfstate[0].id
+
+  depends_on = [aws_s3_bucket_public_access_block.public_block]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnforceTLSRequestsOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.tfstate[0].arn,
+          "${aws_s3_bucket.tfstate[0].arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+      {
+        Sid    = "AllowAccountOwnerFullAccess"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = "s3:*"
+        Resource = [
+          aws_s3_bucket.tfstate[0].arn,
+          "${aws_s3_bucket.tfstate[0].arn}/*"
+        ]
+      }
+    ]
+  })
+}
