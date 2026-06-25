7d21f2e8cc82360b8ace3ef91606595b0ef52d7d
Ryan Kearsley <ryan.kearsley@justice.gov.uk>
2026-06-25 14:20:22 +0100

    Enable auto minor version upgrades for RabbitMQ broker

--- terraform/environments/cfo-data-management-system/rabbitmq.tf
+++ terraform/environments/cfo-data-management-system/rabbitmq.tf
@@ -5,6 +5,7 @@ resource "aws_mq_broker" "rabbitmq" {
   engine_type        = "RABBITMQ"
   engine_version     = "4.2"
   host_instance_type = "mq.m7g.medium"
+  auto_minor_version_upgrade = true
   security_groups    = [aws_security_group.rabbitmq.id]
   subnet_ids         = [data.aws_subnet.private_subnets_a.id]
 
