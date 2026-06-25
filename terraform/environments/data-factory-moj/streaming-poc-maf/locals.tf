59a1c024baef252603b949868678690108d6e00c
SloganAker <stuart.logan@justice.gov.uk>
2026-06-25 10:07:25 +0100

    Move email list to parameter store, comment out all SMS code as we cannot use just now.

--- terraform/environments/data-factory-moj/streaming-poc-maf/locals.tf
+++ terraform/environments/data-factory-moj/streaming-poc-maf/locals.tf
@@ -9,8 +9,13 @@ locals {
   # TODO: uncomment if a sender ID is created and sms is out of sandbox mode.
   #sns_sender_id              = "MOJSTREAMPC"
   #sns_monthly_spending_limit = 200
-
-  drone_incursion_alert_emails = compact(split(",", try(aws_ssm_parameter.drone_incursion_alert_emails[0].value, "test@test.com,test2@test.com")))
+  
+  drone_incursion_alert_emails = [
+    "stuart.logan@justice.gov.uk",
+    "vishal.shah@justice.gov.uk",
+  ]
+  
+  #drone_incursion_alert_emails = compact(split(",", try(aws_ssm_parameter.drone_incursion_alert_emails[0].value, "test@test.com,test2@test.com")))
   # TODO: uncomment if a sender ID is created and sms is out of sandbox mode.
   #drone_incursion_alert_phone_numbers = compact(split(",", try(aws_ssm_parameter.drone_incursion_alert_phone_numbers[0].value, "")))
 
--- terraform/environments/data-factory-moj/streaming-poc-maf/ssm.tf
+++ terraform/environments/data-factory-moj/streaming-poc-maf/ssm.tf
@@ -8,7 +8,7 @@ resource "aws_ssm_parameter" "drone_incursion_alert_emails" {
   name        = "/streaming-poc-maf/${local.environment}/drone-incursion-alert-emails"
   description = "Email recipients for drone incursion SNS alerts"
   type        = "StringList"
-  value       = ""
+  value       = "stuart.logan@justice.gov.uk,vishal.shah@justice.gov.uk"
 
   tags = local.extended_tags
 }
