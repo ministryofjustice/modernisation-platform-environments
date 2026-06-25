599aeaef30d7f2c5fee72a408a0086b7ffe45bd7
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-06-25 14:54:01 +0100

    fix: lifecycle ignore removed

--- terraform/environments/ccms-ebs/ccms-file-transfer/ecs_cluster.tf
+++ terraform/environments/ccms-ebs/ccms-file-transfer/ecs_cluster.tf
@@ -70,11 +70,11 @@ resource "aws_ecs_service" "sftp_ecs_service" {
   launch_type                       = "EC2"
   health_check_grace_period_seconds = 120
 
-  lifecycle {
-    ignore_changes = [
-      task_definition
-    ]
-  }
+  # lifecycle {
+  #   ignore_changes = [
+  #     task_definition
+  #   ]
+  # }
 
   ordered_placement_strategy {
     field = "attribute:ecs.availability-zone"
