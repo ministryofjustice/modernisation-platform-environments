b0a5bc5cacd2d7c6f312047b674d2e6fc280f516
vshahaker <vishal.shah@justice.gov.uk>
2026-06-25 22:11:27 +0100

    provision s3 resource

--- terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_locals.tf
+++ terraform/environments/data-factory-moj/streaming-poc-opensearch-config/app_locals.tf
@@ -8,30 +8,29 @@ locals {
   saved_objects = fileset("${path.cwd}/saved-objects", "*.ndjson")
   os_creds      = jsondecode(data.aws_secretsmanager_secret_version.opensearch_credentials.secret_string)
   region        = "eu-west-2"
-  extended_tags = merge(local.tags, {})
 
-#   opensearch_role_mappings = {
-#     "all_access" = {
-#       backend_roles = [
-#         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/flink-moj-geofence-flink-iam-role",
-#         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/flink-rules-flink-iam-role"
-#       ]
-#       users = ["${local.os_creds.username}"]
-#     }
-#   }
-
+  #   opensearch_role_mappings = {
+  #     "all_access" = {
+  #       backend_roles = [
+  #         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/flink-moj-geofence-flink-iam-role",
+  #         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/flink-rules-flink-iam-role"
+  #       ]
+  #       users = ["${local.os_creds.username}"]
+  #     }
+  #   }
 
 
-    escaped_payloads = {
-        for role_name, mapping_data in local.os_mappings : role_name => replace(replace(replace(
-        jsonencode({
-            backend_roles = mapping_data["backend_roles"]
-            users         = mapping_data["users"]
-        }),
-        "\"",
-        "\\\\\\\""
-        ), "ACCOUNT_ID", data.aws_caller_identity.current.account_id), "USER", local.region)
-    }
+
+  escaped_payloads = {
+    for role_name, mapping_data in local.os_mappings : role_name => replace(replace(replace(
+      jsonencode({
+        backend_roles = mapping_data["backend_roles"]
+        users         = mapping_data["users"]
+      }),
+      "\"",
+      "\\\\\\\""
+    ), "ACCOUNT_ID", data.aws_caller_identity.current.account_id), "USER", local.region)
+  }
 
   #   geo_fence_events_b64 = base64encode(
   #     file("${path.cwd}/index/geo-fence-events.json")
