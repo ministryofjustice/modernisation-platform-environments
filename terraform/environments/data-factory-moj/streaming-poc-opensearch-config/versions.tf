223a27c0e4880ba6c90afee8b11ace561b3d68fe
vshahaker <vishal.shah@justice.gov.uk>
2026-06-26 10:31:14 +0100

    solving plan failure at root

--- terraform/environments/data-factory-moj/streaming-poc-opensearch-config/versions.tf
+++ terraform/environments/data-factory-moj/streaming-poc-opensearch-config/versions.tf
@@ -24,6 +24,10 @@ terraform {
       source  = "hashicorp/null"
       version = "~> 3.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
   required_version = "~> 1.0"
 }
