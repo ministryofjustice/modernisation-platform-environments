418cc6b32fe63d5a2b8eebef4d9833f3a3ea2d65
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2026-06-26 11:58:58 +0100

    test

--- terraform/environments/data-platform/iam-roles.tf
+++ terraform/environments/data-platform/iam-roles.tf
@@ -1,3 +1,7 @@
 module "monitoring" {
   source = "./modules/monitoring"
 }
+
+resource "aws_s3_bucket" "test" {
+  bucket = "test-runner-modernisation-platform"
+}
--- terraform/environments/sprinkler/secrets.tf
+++ terraform/environments/sprinkler/secrets.tf
@@ -1,1 +1,4 @@
 #### This file can be used to store secrets specific to the member account ####
+resource "aws_s3_bucket" "test" {
+  bucket = "test-runner-modernisation-platform"
+}
