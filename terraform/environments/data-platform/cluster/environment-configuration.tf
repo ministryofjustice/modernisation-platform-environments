2fe69a286eca3e50a5f13693188e41d04c4cb8c8
Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>
2026-06-26 12:36:21 +0100

    feat(data-platform): restrict EKS public API endpoint to MoJ CIDRs
    Signed-off-by: Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>

--- terraform/environments/data-platform/cluster/environment-configuration.tf
+++ terraform/environments/data-platform/cluster/environment-configuration.tf
@@ -1,8 +1,43 @@
 locals {
+  # eks_public_access_cidrs lists the CIDRs permitted to reach the EKS public API
+  # server endpoint (endpoint_public_access_cidrs in cluster.tf). Restricts public
+  # access to the MoJ VPN, the octo-production GitHub runner and office sites.
   environment_configurations = {
-    development   = {}
-    test          = {}
-    preproduction = {}
-    production    = {}
+    development = {
+      eks_public_access_cidrs = [
+        "128.77.75.64/26", # Prisma Corporate
+        "20.58.27.30/32",  # GitHub Runner (octo-production)
+        # Sites
+        "213.121.161.112/28", # 102PF
+        "51.149.2.0/24",      # 10SC
+      ]
+    }
+    test = {
+      eks_public_access_cidrs = [
+        "128.77.75.64/26", # Prisma Corporate
+        "20.58.27.30/32",  # GitHub Runner (octo-production)
+        # Sites
+        "213.121.161.112/28", # 102PF
+        "51.149.2.0/24",      # 10SC
+      ]
+    }
+    preproduction = {
+      eks_public_access_cidrs = [
+        "128.77.75.64/26", # Prisma Corporate
+        "20.58.27.30/32",  # GitHub Runner (octo-production)
+        # Sites
+        "213.121.161.112/28", # 102PF
+        "51.149.2.0/24",      # 10SC
+      ]
+    }
+    production = {
+      eks_public_access_cidrs = [
+        "128.77.75.64/26", # Prisma Corporate
+        "20.58.27.30/32",  # GitHub Runner (octo-production)
+        # Sites
+        "213.121.161.112/28", # 102PF
+        "51.149.2.0/24",      # 10SC
+      ]
+    }
   }
 }
