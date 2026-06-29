110b07f32df1139ccc1cb541717eac0cd2901fc0
dms1981 <david.sibley@justice.gov.uk>
2026-06-29 16:23:32 +0100

    Built MFT MVP in production. (#17392)
    * small adjustments for clarity, populate variables to build in production account
    
    * derive EIP values with for expression
    
    * use auto mode for passive IP selection
    
    * small docs update
    
    * set hostname for cors rule deterministically

--- terraform/environments/integration-hub/managed-file-transfer/locals.tf
+++ terraform/environments/integration-hub/managed-file-transfer/locals.tf
@@ -1,22 +1,13 @@
 locals {
-  bucket_configuration     = local.application_data.accounts[local.environment].bucket_configuration
-  custom_idp_configuration = local.application_data.accounts[local.environment].custom_idp_configuration
-  iam_configuration        = local.application_data.accounts[local.environment].iam_configuration
+  build_mft                = try(local.application_data.accounts[local.environment].build_mft, false)
+  bucket_configuration     = try(local.application_data.accounts[local.environment].bucket_configuration, {})
+  custom_idp_configuration = try(local.application_data.accounts[local.environment].custom_idp_configuration, {})
+  iam_configuration        = try(local.application_data.accounts[local.environment].iam_configuration, {})
+  web_app_hostname         = local.is-production == false ? "web.${local.environment}.managed-file-transfer.service.justice.gov.uk" : "web.managed-file-transfer.service.justice.gov.uk"
+  web_app_origin           = "https://${local.web_app_hostname}"
   notification_configuration = try(
     local.application_data.accounts[local.environment].notification_configuration,
     {},
   )
-  team_channel_notification_configuration = try(
-    local.notification_configuration.team_channel,
-    {},
-  )
-  high_priority_alerts_notification_configuration = try(
-    local.notification_configuration.high_priority_alerts,
-    {},
-  )
-  low_priority_alerts_notification_configuration = try(
-    local.notification_configuration.low_priority_alerts,
-    {},
-  )
-  vpc_configuration = local.application_data.accounts[local.environment].vpc_configuration
+  vpc_configuration = try(local.application_data.accounts[local.environment].vpc_configuration, {})
 }
