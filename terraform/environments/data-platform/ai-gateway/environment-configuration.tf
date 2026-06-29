d9b0a7928f8d641799736f9a573544f4f4acf6e7
Raghavendra Kamath <raghavvidya@gmail.com>
2026-06-29 19:42:11 +0100

    move dummy_password from platform_locals.tf to environment_x.tf as platform_x is MP team-owned

--- terraform/environments/data-platform/ai-gateway/environment-configuration.tf
+++ terraform/environments/data-platform/ai-gateway/environment-configuration.tf
@@ -2,6 +2,7 @@ locals {
   environment_configuration = local.environment_configurations[local.environment]
   ai_gateway_models         = yamldecode(file("${path.module}/configuration/models.yml"))
   has_reader                = contains(keys(local.environment_configuration.aurora_instances), "reader")
+  dummy_password            = "iam-auth-dummy-password"
   environment_configurations = {
     proxy_admin_emails = [
       "Muhammad.Ahmad@justice.gov.uk",
