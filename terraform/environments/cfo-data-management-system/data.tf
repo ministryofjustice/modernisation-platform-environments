7ef136107a9066d9f61f94e5bdd032ec976200d9
Ryan Kearsley <ryan.kearsley@justice.gov.uk>
2026-06-30 14:07:48 +0100

    Fix RDS Password retrieval

--- terraform/environments/cfo-data-management-system/data.tf
+++ terraform/environments/cfo-data-management-system/data.tf
@@ -20,10 +20,19 @@ data "aws_db_instance" "rds" {
   depends_on = [module.rds]
 }
 
-# Read the RDS master user password from the RDS-managed Secrets Manager secret
-# Used to auto-generate database connection strings in secrets.tf
+# Look up the RDS master password secret created by the MP RDS module.
+# The module appends a random hex suffix to the name, so we filter by prefix.
+data "aws_secretsmanager_secrets" "rds-password" {
+  filter {
+    name   = "name"
+    values = ["${local.application_name_short}-${local.environment}-rds-master-password"]
+  }
+
+  depends_on = [module.rds]
+}
+
 data "aws_secretsmanager_secret_version" "rds-master-password" {
-  secret_id = data.aws_db_instance.rds.master_user_secret[0].secret_arn
+  secret_id = tolist(data.aws_secretsmanager_secrets.rds-password.arns)[0]
 }
 
 # CloudFront managed prefix list restricts visualiser ALB ingress to CloudFront edge nodes only
