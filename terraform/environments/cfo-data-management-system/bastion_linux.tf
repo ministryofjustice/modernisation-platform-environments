606c91a07ef9c9e1a4fc8be5ade9bac988d83459
Ryan Kearsley <ryan.kearsley@justice.gov.uk>
2026-07-01 12:38:54 +0100

    Update bastion Linux module source to version 6.0.0

--- terraform/environments/cfo-data-management-system/bastion_linux.tf
+++ terraform/environments/cfo-data-management-system/bastion_linux.tf
@@ -5,7 +5,7 @@ locals {
 
 # MP Bastion Linux module - https://github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux
 module "bastion_linux" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=e4a3840d4b7b327228d1397bb684bc79cd4e76cb" # v4.5.0
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=29c5908bd41b183808fe0c02d9ae06f0ede2036b" # v6.0.0
 
   providers = {
     aws.share-host   = aws.core-vpc
