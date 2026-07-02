00c132f1c829d12d4279597f8a342a4b7d829cdd
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2026-07-02 13:01:36 +0100

    CC-4707: Testing base module component

--- terraform/environments/ccms-feasibility/ccms-ebs/main.tf
+++ terraform/environments/ccms-feasibility/ccms-ebs/main.tf
@@ -1,5 +1,6 @@
 module "test_log_group" {
-  source = "git::https://github.com/ministryofjustice/laa-ccms-terraform-modules//modules/cloudwatch-log-group?ref=main"
+  # main = https://github.com/ministryofjustice/laa-ccms-terraform-modules/commit/7e165d5fbd77c835bc2ef509aedf0b503755cf60
+  source = "github.com/ministryofjustice/laa-ccms-terraform-modules//modules/cloudwatch-log-group?ref=7e165d5fbd77c835bc2ef509aedf0b503755cf60"
 
   name              = "ccms-feasibility-ccms-ebs-test"
   retention_in_days = 30
