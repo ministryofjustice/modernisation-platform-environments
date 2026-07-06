3368961c2b20fb4fd8ba6d9bd76dd865b04f73b7
Harsh Vasudev <harsh.vasudev@justice.gov.uk>
2026-07-06 19:00:12 +0100

    Use SSM bucket parameters in API fallback

--- terraform/environments/integration-hub-api/platform_data.tf
+++ terraform/environments/integration-hub-api/platform_data.tf
@@ -150,15 +150,16 @@ data "terraform_remote_state" "core_network_services" {
   }
 }
 
-data "terraform_remote_state" "managed_file_transfer" {
-  backend = "s3"
-  config = {
-    acl     = "bucket-owner-full-control"
-    bucket  = "modernisation-platform-terraform-state"
-    key     = "environments/members/integration-hub/managed-file-transfer/${terraform.workspace}/terraform.tfstate"
-    region  = "eu-west-2"
-    encrypt = "true"
-  }
+data "aws_ssm_parameter" "mft_upload_bucket_name" {
+  name = "${local.mft_upload_bucket_parameter_prefix}/name"
+}
+
+data "aws_ssm_parameter" "mft_upload_bucket_arn" {
+  name = "${local.mft_upload_bucket_parameter_prefix}/arn"
+}
+
+data "aws_ssm_parameter" "mft_upload_bucket_kms_key_arn" {
+  name = "${local.mft_upload_bucket_parameter_prefix}/kms-key-arn"
 }
 
 data "aws_organizations_organization" "root_account" {}
