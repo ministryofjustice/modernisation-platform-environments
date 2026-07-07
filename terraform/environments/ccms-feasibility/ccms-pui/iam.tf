f8f09c1575fb59e29a8c7cd4487d5db586cf4dc9
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2026-07-07 13:17:51 +0100

    Create EDRMS through modules

--- terraform/environments/ccms-feasibility/ccms-pui/iam.tf
+++ terraform/environments/ccms-feasibility/ccms-pui/iam.tf
@@ -1,14 +1,1 @@
-module "iam_ecs_ec2" {
-  # main = https://github.com/ministryofjustice/laa-ccms-terraform-modules/commit/COMMIT_SHA
-  source = "github.com/ministryofjustice/laa-ccms-terraform-modules//modules/iam-role?ref=main"
-
-  name = "${local.component_name}-${local.env_label}-ecs-ec2-role"
-  trust_services = ["ec2.amazonaws.com"]
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role",
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-  ]
-  create_instance_profile = true
-  instance_profile_name   = "${local.component_name}-${local.env_label}-ecs-ec2-profile"
-  tags                    = local.tags
-}
+# Resources will be added here when ccms-pui is built.
--- terraform/environments/ccms-feasibility/ccms-pui/security.tf
+++ terraform/environments/ccms-feasibility/ccms-pui/security.tf
@@ -1,9 +1,1 @@
-module "sg_cluster_ec2" {
-  # main = https://github.com/ministryofjustice/laa-ccms-terraform-modules/commit/COMMIT_SHA
-  source = "github.com/ministryofjustice/laa-ccms-terraform-modules//modules/security-group?ref=main"
-
-  name        = "${local.component_name}-${local.env_label}-cluster-ec2-sg"
-  description = "Controls access to the ${local.component_name} ECS cluster EC2 instances"
-  vpc_id      = data.aws_vpc.shared.id
-  tags        = local.tags
-}
+# Resources will be added here when ccms-pui is built.
