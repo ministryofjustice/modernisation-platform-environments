8238b2036f62e140a0c2453c12a895d72c5f63be
lucy-astley-jones <lucy.astleyjones@justice.gov.uk>
2026-07-08 15:35:41 +0100

    fix: rename for clarity

--- terraform/environments/electronic-monitoring-data/cloudwatch-dashboard-merge-lambdas.tf
+++ terraform/environments/electronic-monitoring-data/cloudwatch-dashboard-merge-lambdas.tf
@@ -93,7 +93,7 @@ locals {
         width  = 24
         height = 6
         properties = {
-          title  = "Average Execution Time"
+          title  = "Average Total Execution Time"
           region = "eu-west-2"
           stat   = "Average"
           period = 180
@@ -165,7 +165,7 @@ locals {
         width  = 24
         height = 6
         properties = {
-          title  = "Average Service Processing Time"
+          title  = "Average Service Pre-Processing Time"
           region = "eu-west-2"
           stat   = "Average"
           period = 180
