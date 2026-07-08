b0a04ca95e2872db5964ae97022f718c31fe0325
Vladimirs Kovalovs <vladimirs.kovalovs1@justice.gov.uk>
2026-07-08 11:52:26 +0100

    Add ECR VPC endpoints for ECS Fargate image pulls
    ECS tasks in private subnets require VPC endpoints to pull images from ECR.
    Added:
    - ecr.api endpoint (for GetAuthorizationToken)
    - ecr.dkr endpoint (for Docker image layers)
    
    Fixes ResourceInitializationError when tasks attempt to pull from ECR.

--- terraform/environments/laa-workspaces/workspace-components/new-vpc-endpoints.tf
+++ terraform/environments/laa-workspaces/workspace-components/new-vpc-endpoints.tf
@@ -168,3 +168,43 @@ resource "aws_vpc_endpoint" "s3" {
     }
   )
 }
+
+##############################################
+### ECR VPC Endpoints (for ECS Fargate to pull images)
+##############################################
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  count = local.environment == "development" ? 1 : 0
+
+  vpc_id              = aws_vpc.workspaces[0].id
+  service_name        = "com.amazonaws.eu-west-2.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.private_a[0].id, aws_subnet.private_b[0].id]
+  security_group_ids  = [aws_security_group.vpc_endpoints[0].id]
+  private_dns_enabled = true
+
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${local.application_name}-${local.environment}-ecr-api-endpoint"
+    }
+  )
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  count = local.environment == "development" ? 1 : 0
+
+  vpc_id              = aws_vpc.workspaces[0].id
+  service_name        = "com.amazonaws.eu-west-2.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.private_a[0].id, aws_subnet.private_b[0].id]
+  security_group_ids  = [aws_security_group.vpc_endpoints[0].id]
+  private_dns_enabled = true
+
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${local.application_name}-${local.environment}-ecr-dkr-endpoint"
+    }
+  )
+}
