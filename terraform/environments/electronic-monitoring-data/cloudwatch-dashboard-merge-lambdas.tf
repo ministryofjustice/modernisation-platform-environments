ca0b67ac2272d3c98bce3571b4e39ea3c4c9cb22
lucy-astley-jones <lucy.astleyjones@justice.gov.uk>
2026-07-08 08:32:18 +0100

    feat: add metrics

--- terraform/environments/electronic-monitoring-data/cloudwatch-dashboard-merge-lambdas.tf
+++ terraform/environments/electronic-monitoring-data/cloudwatch-dashboard-merge-lambdas.tf
@@ -1,69 +1,92 @@
 locals {
-    merge_lambda_widgets = [
+  lambda_functions = {
+    merge_staged_position_lambda_ops = "merge_mdss_staged_position"
+    merge_staged_event_lambda_ops    = "merge_mdss_staged_event"
+    merge_ac_position_lambda_ops     = "merge_ac_position"
+    merge_emdi_position_lambda_ops   = "merge_emdi_position"
+  }
+}
+
+locals {
+  merge_lambda_widgets = {
+    for dashboard, lambda_name in local.lambda_functions :
+    dashboard => [
       {
-      # --------------------------
-      # Queries passed
-      # --------------------------
+        # --------------------------
+        # Queries passed
+        # --------------------------
         type   = "metric"
         x      = 0
         y      = 0
         width  = 24
         height = 6
         properties = {
-          title  = "Successful Queries"
+          title  = "Total Successful Queries"
           region = "eu-west-2"
           stat   = "Sum"
           period = 180
           metrics = [
-            ["AWS/Lambda",
-            "TotalSuccessfulQueries"]
+            [
+              "AWS/Lambda",
+              "SuccessfulQueries",
+              "FunctionName",
+              lambda_name
+            ]
           ]
         }
       },
       {
-      # --------------------------
-      # Queries failed
-      # --------------------------
+        # --------------------------
+        # Queries failed
+        # --------------------------
         type   = "metric"
         x      = 0
         y      = 0
         width  = 24
         height = 6
         properties = {
-          title  = "Failed Queries"
+          title  = "Total Failed Queries"
           region = "eu-west-2"
           stat   = "Sum"
           period = 180
           metrics = [
-            ["AWS/Lambda",
-            "TotalFailedQueries"]
+            [
+              "AWS/Lambda",
+              "FailedQueries",
+              "FunctionName",
+              lambda_name
+            ]
           ]
         }
       },
       {
-      # --------------------------
-      # Polls made
-      # --------------------------
+        # --------------------------
+        # Polls made
+        # --------------------------
         type   = "metric"
         x      = 0
         y      = 0
         width  = 24
         height = 6
         properties = {
-          title  = "Average Athena polls"
+          title  = "Average Athena Polls"
           region = "eu-west-2"
           stat   = "Average"
           period = 180
           metrics = [
-            ["AWS/Lambda",
-            "AveragePollsMade"]
+            [
+              "AWS/Lambda",
+              "PollsMade",
+              "FunctionName",
+              lambda_name
+            ]
           ]
         }
       },
       {
-      # --------------------------
-      # Data scanned
-      # --------------------------
+        # --------------------------
+        # Data scanned
+        # --------------------------
         type   = "metric"
         x      = 0
         y      = 0
@@ -75,15 +98,19 @@ locals {
           stat   = "Sum"
           period = 180
           metrics = [
-            ["AWS/Lambda",
-            "AverageDataScanned"]
+            [
+              "AWS/Lambda",
+              "DataScanned",
+              "FunctionName",
+              lambda_name
+            ]
           ]
         }
       },
       {
-      # --------------------------
-      # Execution time
-      # --------------------------
+        # --------------------------
+        # Execution time
+        # --------------------------
         type   = "metric"
         x      = 0
         y      = 0
@@ -95,15 +122,19 @@ locals {
           stat   = "Average"
           period = 180
           metrics = [
-            ["AWS/Lambda",
-            "AverageExecutionTime"]
+            [
+              "AWS/Lambda",
+              "ExecutionTime",
+              "FunctionName",
+              lambda_name
+            ]
           ]
         }
       },
       {
-      # --------------------------
-      # Queue time
-      # --------------------------
+        # --------------------------
+        # Queue time
+        # --------------------------
         type   = "metric"
         x      = 0
         y      = 0
@@ -115,15 +146,19 @@ locals {
           stat   = "Average"
           period = 180
           metrics = [
-            ["AWS/Lambda",
-            "AverageTimeInQueue"]
+            [
+              "AWS/Lambda",
+              "TimeInQueue",
+              "FunctionName",
+              lambda_name
+            ]
           ]
         }
       },
       {
-      # --------------------------
-      # Planning time
-      # --------------------------
+        # --------------------------
+        # Planning time
+        # --------------------------
         type   = "metric"
         x      = 0
         y      = 0
@@ -135,40 +170,67 @@ locals {
           stat   = "Average"
           period = 180
           metrics = [
-            ["AWS/Lambda",
-            "AverageQueryPlanTime"]
+            [
+              "AWS/Lambda",
+              "QueryPlanTime",
+              "FunctionName",
+              lambda_name
+            ]
           ]
         }
       },
-
-
+      {
+        # --------------------------
+        # Lambda processing time
+        # --------------------------
+        type   = "metric"
+        x      = 0
+        y      = 0
+        width  = 24
+        height = 6
+        properties = {
+          title  = "Average Lambda Processing Time"
+          region = "eu-west-2"
+          stat   = "Average"
+          period = 180
+          metrics = [
+            [
+              "AWS/Lambda",
+              "LambdaProcessingTime",
+              "FunctionName",
+              lambda_name
+            ]
+          ]
+        }
+      },
     ]
+  }
 }
 
 resource "aws_cloudwatch_dashboard" "merge_staged_position_lambda_ops" {
-    dashboard_name = "merge-staged-position-lambda-ops-${local.environment_shorthand}"  
-    dashboard_body = jsonencode({
-        widgets = local.merge_lambda_widgets
-        })
-    }
+  dashboard_name = "merge-staged-position-lambda-ops"
+  dashboard_body = jsonencode({
+    widgets = local.merge_lambda_widgets["merge_staged_position_lambda_ops"]
+  })
+}
 
 resource "aws_cloudwatch_dashboard" "merge_staged_event_lambda_ops" {
-    dashboard_name = "merge-staged-event-lambda-ops-${local.environment_shorthand}" 
-    dashboard_body = jsonencode({
-        widgets = local.merge_lambda_widgets
-        })
-    }
+  dashboard_name = "merge-staged-event-lambda-ops"
+  dashboard_body = jsonencode({
+    widgets = local.merge_lambda_widgets["merge_staged_event_lambda_ops"]
+  })
+}
 
 resource "aws_cloudwatch_dashboard" "merge_ac_position_lambda_ops" {
-    dashboard_name = "merge-ac-position-lambda-ops-${local.environment_shorthand}" 
-    dashboard_body = jsonencode({
-        widgets = local.merge_lambda_widgets
-        })
-    }
+  dashboard_name = "merge-ac-position-lambda-ops"
+  dashboard_body = jsonencode({
+    widgets = local.merge_lambda_widgets["merge_ac_position_lambda_ops"]
+  })
+}
 
 resource "aws_cloudwatch_dashboard" "merge_emdi_position_lambda_ops" {
-    dashboard_name = "merge-emdi-position-lambda-ops-${local.environment_shorthand}"  
-    dashboard_body = jsonencode({
-        widgets = local.merge_lambda_widgets
-        })
-    }
+  dashboard_name = "merge-emdi-position-lambda-ops"
+  dashboard_body = jsonencode({
+    widgets = local.merge_lambda_widgets["merge_emdi_position_lambda_ops"]
+  })
+}
