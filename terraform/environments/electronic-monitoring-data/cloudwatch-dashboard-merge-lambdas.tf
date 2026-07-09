58be001f7371faaf3ce2f2fc8a5d1656fc97e05e
lucy-astley-jones <lucy.astleyjones@justice.gov.uk>
2026-07-09 09:09:02 +0100

    add GB label

--- terraform/environments/electronic-monitoring-data/cloudwatch-dashboard-merge-lambdas.tf
+++ terraform/environments/electronic-monitoring-data/cloudwatch-dashboard-merge-lambdas.tf
@@ -90,6 +90,15 @@ locals {
               }
             ]
           ]
+          annotations = {
+            horizontal = [
+              {
+                value = 1073741824
+                label = "1GB",
+                color = "#E34234"
+              }
+            ]
+          }
         }
       },
       {
