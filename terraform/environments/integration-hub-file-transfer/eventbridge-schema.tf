e1c87efff44a05f753422cc817618406082624d7
David Sibley <10881569+dms1981@users.noreply.github.com>
2026-07-13 10:18:38 +0100

    set correct schema type

--- terraform/environments/integration-hub-file-transfer/eventbridge-schema.tf
+++ terraform/environments/integration-hub-file-transfer/eventbridge-schema.tf
@@ -7,7 +7,7 @@ resource "aws_schemas_schema" "this" {
   for_each      = local.eventbridge_schemas
   name          = trimsuffix(basename(each.value), ".json")
   registry_name = aws_schemas_registry.this.name
-  type          = "OpenApi3"
+  type          = "JSONSchemaDraft4"
 
   content = file("${local.eventbridge_schema_directory}/${each.value}")
 }
