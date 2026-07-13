9a899f031c34e7eda0fa886db14003987b9bca9a
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-07-13 15:12:27 +0100

    removing default system nodepool

--- terraform/environments/cloud-platform/cluster/eks-cluster.tf
+++ terraform/environments/cloud-platform/cluster/eks-cluster.tf
@@ -16,7 +16,7 @@ module "eks" {
   # enable_cluster_creator_admin_permissions = true ## CP GitHub actions access to cluster, Adds to access entries
   compute_config = {
     enabled    = true
-    node_pools = ["system"] # US-028: general-purpose removed — user workloads use custom-networking NodePool only
+    # node_pools = ["system"] # US-028: general-purpose removed — user workloads use custom-networking NodePool only
   }
   enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
