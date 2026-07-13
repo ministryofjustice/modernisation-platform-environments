c2078bd8e2cd3ee6d7d849dd656c929db35252fa
David Sibley <10881569+dms1981@users.noreply.github.com>
2026-07-13 14:54:45 +0100

    transform S3 notification from incoming bucket into filereceived event on custom bus

---
+++ terraform/environments/integration-hub-file-transfer/eventbridge.tf
@@ -0,0 +1,77 @@
+module "eventbridge_default_bus" {
+  source  = "terraform-aws-modules/eventbridge/aws"
+  version = "4.3.0"
+
+  bus_name                    = "default"
+  create_bus                  = false
+  create_log_delivery         = false
+  create_log_delivery_source  = false
+  append_rule_postfix         = false
+  role_name                   = "${local.application_name}-incoming-s3"
+  attach_policy_statements    = true
+
+  rules = {
+    "incoming-s3-object-created" = {
+      description = "Transform incoming S3 Object Created notifications into FileReceived.v1 events"
+      event_pattern = jsonencode({
+        source        = ["aws.s3"]
+        "detail-type" = ["Object Created"]
+        detail = {
+          bucket = {
+            name = [module.s3_bucket["incoming"].s3_bucket_id]
+          }
+        }
+      })
+    }
+  }
+
+  targets = {
+    "incoming-s3-object-created" = [
+      {
+        name               = "file-received-v1"
+        arn                = module.eventbridge_file_transfer_bus.eventbridge_bus_arn
+        attach_role_arn    = true
+        input_transformer  = local.filereceived_transformer
+      }
+    ]
+  }
+
+  policy_statements = {
+    publish_file_received_events = {
+      effect    = "Allow"
+      actions   = ["events:PutEvents"]
+      resources = [module.eventbridge_file_transfer_bus.eventbridge_bus_arn]
+    }
+  }
+
+  tags = local.tags
+}
+
+
+module "eventbridge_file_transfer_bus" {
+  source  = "terraform-aws-modules/eventbridge/aws"
+  version = "4.3.0"
+
+  bus_name       = local.application_name
+  create_archives = true
+
+  archives = {
+    "${local.application_name}-archive" = {
+      description    = "Archive of all file transfer events"
+      retention_days = local.eventbridge_retention_days
+    }
+  }
+
+  log_config = {
+    include_detail = "FULL"
+    level          = "INFO"
+  }
+
+  log_delivery = {
+    cloudwatch_logs = {
+      destination_arn = module.cloudwatch_eventbridge.cloudwatch_log_group_arn
+    }
+  }
+
+  tags = local.tags
+}
--- terraform/environments/integration-hub-file-transfer/locals-eventbridge.tf
+++ terraform/environments/integration-hub-file-transfer/locals-eventbridge.tf
@@ -1,4 +1,49 @@
 locals {
   eventbridge_schema_directory = "${path.module}/schemas"
   eventbridge_schemas          = fileset("${path.module}/schemas", "*.json")
+
+  eventbridge_retention_days = local.is-production ? 400 : 30
+
+  filereceived_transformer = {
+    input_paths = {
+      account_id = "$.account"
+      bucket     = "$.detail.bucket.name"
+      event_id   = "$.id"
+      object_key = "$.detail.object.key"
+      region     = "$.region"
+      size_bytes = "$.detail.object.size"
+      timestamp  = "$.time"
+      version_id = "$.detail.object.version-id"
+    }
+
+    input_template = <<-EOF
+      {
+        "version": "0",
+        "id": <event_id>,
+        "detail-type": "FileReceived.v1",
+        "source": "uk.gov.justice.service.managed-file-transfer",
+        "account": <account_id>,
+        "time": <timestamp>,
+        "region": <region>,
+        "detail": {
+          "metadata": {
+            "correlationId": <event_id>,
+            "idempotencyKey": "<bucket>:<object_key>:<version_id>"
+          },
+          "data": {
+            "fileId": <event_id>,
+            "object": {
+              "bucket": <bucket>,
+              "key": <object_key>,
+              "versionId": <version_id>,
+              "sizeBytes": <size_bytes>
+            },
+            "provenance": {
+              "ingressMethod": "s3"
+            }
+          }
+        }
+      }
+    EOF
+  }
 }
