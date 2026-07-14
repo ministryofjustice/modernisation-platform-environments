0751f900f076f06e11fddb2fae4ca3f7cffd9853
NasraMohammedjustice <Nasra.Mohammed@justice.gov.uk>
2026-07-14 13:29:48 +0100

    Move Apex secret migration logic to secrets.tf only

--- terraform/environments/apex/platform_secrets.tf
+++ terraform/environments/apex/platform_secrets.tf
@@ -16,31 +16,3 @@ data "aws_secretsmanager_secret_version" "environment_management" {
   secret_id = data.aws_secretsmanager_secret.environment_management.id
 }
 
-data "aws_ssm_parameter" "app_apex_dbpassword_tad" {
-  name            = "APP_APEX_DBPASSWORD_TAD"
-  with_decryption = true
-}
-data "aws_ssm_parameter" "ec2_ssh_key" {
-  name            = "EC2_SSH_KEY"
-  with_decryption = true
-}
-
-
-resource "aws_secretsmanager_secret" "app_apex_dbpassword_tad" {
-  count = local.environment == "development" ? 1 : 0
-  name  = "APP_APEX_DBPASSWORD_TAD"
-}
-resource "aws_secretsmanager_secret_version" "app_apex_dbpassword_tad" {
-  count         = local.environment == "development" ? 1 : 0
-  secret_id     = aws_secretsmanager_secret.app_apex_dbpassword_tad[0].id
-  secret_string = data.aws_ssm_parameter.app_apex_dbpassword_tad.value
-}
-resource "aws_secretsmanager_secret" "ec2_ssh_key" {
-  count = local.environment == "development" ? 1 : 0
-  name  = "EC2_SSH_KEY"
-}
-resource "aws_secretsmanager_secret_version" "ec2_ssh_key" {
-  count         = local.environment == "development" ? 1 : 0
-  secret_id     = aws_secretsmanager_secret.ec2_ssh_key[0].id
-  secret_string = data.aws_ssm_parameter.ec2_ssh_key.value
-}
