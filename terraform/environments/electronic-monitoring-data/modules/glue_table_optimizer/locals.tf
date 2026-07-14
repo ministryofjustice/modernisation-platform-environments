e10ff3a09f1d5364b63e95e22fcecb2ed08900d9
matt heery <matt.heery@justice.gov.uk>
2026-07-14 22:29:04 +0100

    feat: generalise table optimizers

---
+++ terraform/environments/electronic-monitoring-data/modules/glue_table_optimizer/locals.tf
@@ -0,0 +1,33 @@
+locals {
+  default_domain_by_database = {
+    for db in var.databases : db => db
+  }
+
+  domain_by_database = merge(
+    local.default_domain_by_database,
+    var.dbt_domain_name_by_database
+  )
+
+  raw_prefix_by_database = {
+    for db in var.databases : db => "staging/${db}_pipeline/${db}"
+  }
+
+  orphan_prefix_by_database = merge(
+    local.default_orphan_prefix_by_database,
+    var.orphan_prefix_overrides_by_database
+  )
+
+  tables_to_optimize_flat = length(keys(data.external.glue_tables_by_database)) == 0 ? {} : merge([
+    for database_name in keys(data.external.glue_tables_by_database) : {
+      for table_name in try(jsondecode(data.external.glue_tables_by_database[database_name].result.tables_json), []) : "${database_name}.${table_name}" => merge(
+        var.table_optimizer_defaults,
+        {
+          database_name       = database_name
+          table_name          = table_name
+          raw_database_prefix = local.raw_prefix_by_database[database_name]
+          dbt_domain          = local.domain_by_database[database_name]
+        }
+      )
+    }
+  ]...)
+}
