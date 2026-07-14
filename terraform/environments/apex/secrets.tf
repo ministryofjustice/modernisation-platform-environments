f0b6795d6b703690fa17537a1e3fa97838d3267c
NasraMohammedjustice <Nasra.Mohammed@justice.gov.uk>
2026-07-14 15:05:17 +0100

    Restore dev-only Apex secret migration for TAD and EC2 SSH key

--- terraform/environments/apex/secrets.tf
+++ terraform/environments/apex/secrets.tf
@@ -8,37 +8,23 @@ data "aws_ssm_parameter" "ec2_ssh_key" {
   with_decryption = true
 }
 
-data "aws_ssm_parameter" "app_apex_dbpassword_admin" {
-  count           = contains(["test", "preproduction"], local.environment) ? 1 : 0
-  name            = "APP_APEX_DBPASSWORD_ADMIN"
-  with_decryption = true
-}
-
-
 resource "aws_secretsmanager_secret" "app_apex_dbpassword_tad" {
+  count = local.environment == "development" ? 1 : 0
   name  = "APP_APEX_DBPASSWORD_TAD"
 }
 resource "aws_secretsmanager_secret_version" "app_apex_dbpassword_tad" {
-  secret_id     = aws_secretsmanager_secret.app_apex_dbpassword_tad.id
+  count         = local.environment == "development" ? 1 : 0
+  secret_id     = aws_secretsmanager_secret.app_apex_dbpassword_tad[0].id
   secret_string = data.aws_ssm_parameter.app_apex_dbpassword_tad.value
 }
 
-
 resource "aws_secretsmanager_secret" "ec2_ssh_key" {
+  count = local.environment == "development" ? 1 : 0
   name  = "EC2_SSH_KEY"
 }
 resource "aws_secretsmanager_secret_version" "ec2_ssh_key" {
-  secret_id     = aws_secretsmanager_secret.ec2_ssh_key.id
+  count         = local.environment == "development" ? 1 : 0
+  secret_id     = aws_secretsmanager_secret.ec2_ssh_key[0].id
   secret_string = data.aws_ssm_parameter.ec2_ssh_key.value
-}
-
-resource "aws_secretsmanager_secret" "app_apex_dbpassword_admin" {
-  count = contains(["test", "preproduction"], local.environment) ? 1 : 0
-  name  = "APP_APEX_DBPASSWORD_ADMIN"
-}
-resource "aws_secretsmanager_secret_version" "app_apex_dbpassword_admin" {
-  count         = contains(["test", "preproduction"], local.environment) ? 1 : 0
-  secret_id     = aws_secretsmanager_secret.app_apex_dbpassword_admin[0].id
-  secret_string = data.aws_ssm_parameter.app_apex_dbpassword_admin[0].value
 }
 
