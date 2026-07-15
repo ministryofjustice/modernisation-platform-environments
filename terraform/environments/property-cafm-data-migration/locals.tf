3d5bf5b971df3e419116a4fb39996ad21886520b
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-07-15 14:52:51 +0000

    refactor: consolidate replication configuration into locals.tf

--- terraform/environments/property-cafm-data-migration/locals.tf
+++ terraform/environments/property-cafm-data-migration/locals.tf
@@ -49,4 +49,15 @@ locals {
     "development"   = "dev"
   }
   environment_shorthand = local.environment_map[local.environment]
+
+  # S3 replication configuration for property-datahub-staging bucket
+  replication_configuration = local.replication_configurations[local.environment]
+
+  replication_configurations = {
+    production = {
+      property_datahub_staging_egress_target_bucket = "mojap-ingestion-production-property-datahub-staging-egress"
+      property_datahub_staging_egress_account_id    = local.environment_management.account_ids["analytical-platform-ingestion-production"]
+      property_datahub_staging_egress_kms_arn       = "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["analytical-platform-ingestion-production"]}:key/6da79242-5b40-4a37-bbdf-961950ced1f4"
+    }
+  }
 }
