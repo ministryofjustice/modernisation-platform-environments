9b63b39f49212fa07e9500263b4e25040647c728
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-07-16 16:57:54 +0000

    feat: add helm provider to AI gateway configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/ai-gateway/versions.tf
+++ terraform/environments/data-platform/ai-gateway/versions.tf
@@ -32,6 +32,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 3.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
   }
   required_version = "~> 1.0"
 }
