6c58a0049482763b76f6b7aa221de941f3d4485d
NasraMohammedjustice <Nasra.Mohammed@justice.gov.uk>
2026-07-16 14:31:24 +0100

    Pin Apex bastion module to fix commit

--- terraform/environments/apex/bastion.tf
+++ terraform/environments/apex/bastion.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "bastion_linux" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=V4.5.0"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-bastion-linux?ref=e4a3840d4b7b327228d1397bb684bc79cd4e76cb" # v4.5.0
 
   providers = {
     aws.share-host   = aws.core-vpc # core-vpc-(environment) holds the networking for all accounts
