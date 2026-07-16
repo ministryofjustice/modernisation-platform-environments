32f298a1e83cb81a1e74cef353c72c98ef112c0f
Ky <ky.truong@digital.justice.gov.uk>
2026-07-16 17:08:09 +0100

    move nodepool to cluster layer

--- terraform/environments/cloud-platform/cluster/locals.tf
+++ terraform/environments/cloud-platform/cluster/locals.tf
@@ -15,4 +15,6 @@ locals {
   workspace_environment     = element(reverse(split("-", terraform.workspace)), 0)
   cluster_name              = terraform.workspace
   cluster_environment       = contains(local.mp_environments, terraform.workspace) ? local.workspace_environment : "development_cluster"
+
+  node_role_name = split("/", data.aws_eks_cluster.cluster.compute_config[0].node_role_arn)[1]
 }
