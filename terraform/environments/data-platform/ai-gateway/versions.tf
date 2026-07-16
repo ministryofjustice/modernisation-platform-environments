d8ba6f67b52454638a8f974be505e9eb7c910b79
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-07-16 16:55:01 +0000

    feat: add random and kubernetes providers to AI gateway configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/ai-gateway/versions.tf
+++ terraform/environments/data-platform/ai-gateway/versions.tf
@@ -24,6 +24,14 @@ terraform {
       source  = "ncecere/litellm"
       version = "~> 2.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
   }
   required_version = "~> 1.0"
 }
