872d5c633b68c67aae2e83270548276d8492abc8
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2026-07-16 13:24:00 +0100

    fix: workspace sg added

--- terraform/environments/ccms-ebs/ssogen-lb-sg.tf
+++ terraform/environments/ccms-ebs/ssogen-lb-sg.tf
@@ -93,7 +93,7 @@ resource "aws_security_group_rule" "ingress_traffic_ssogenalb_internal_443_dom1_
   cidr_blocks       = [local.application_data.accounts[local.environment].dom1_devices]
 }
 
-# CC-4731: MP Workspaces (V1) - DEV only, SSO HTTPS access
+#  MP Workspaces (V1) - DEV only, SSO HTTPS access
 resource "aws_security_group_rule" "ingress_traffic_ssogenalb_internal_443_mp_v1_workspaces" {
   count             = local.ssogen_enabled && local.environment == "development" ? 1 : 0
   security_group_id = aws_security_group.sg_ssogen_internal_alb[count.index].id
