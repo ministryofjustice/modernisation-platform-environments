bd15f3aa26c87204f84bfbea29c114f3201bd2c0
David Teeling <david.teeling1@justice.gov.uk>
2026-07-17 16:37:23 +0100

    Bucket policy

--- terraform/environments/delius-core/modules/delius_environment/s3.tf
+++ terraform/environments/delius-core/modules/delius_environment/s3.tf
@@ -10,5 +10,50 @@ module "weblogic_ecs_remote_state" {
     aws.bucket-replication = aws
   }
 
+  bucket_policy = local.bucket_policy
+
   tags = local.tags
+}
+
+locals{
+  bucket_policy = [
+    jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Sid    = "AllowTerraformStateBucketListing"
+          Effect = "Allow"
+          Principal = {
+            AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/modernisation-platform-oidc-cicd"
+          }
+          Action = [
+            "s3:ListBucket"
+          ]
+          Resource = "arn:aws:s3:::${var.account_info.application_name}-${var.env_name}-weblogic-ecs-remote-state"
+          Condition = {
+            StringLike = {
+              "s3:prefix" = [
+                "weblogic-ecs/*"
+              ]
+            }
+          }
+        },
+        {
+          Sid    = "AllowTerraformStateReadWrite"
+          Effect = "Allow"
+          Principal = {
+            AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/modernisation-platform-oidc-cicd"
+          }
+          Action = [
+            "s3:GetObject",
+            "s3:PutObject",
+            "s3:DeleteObject",
+          ]
+          Resource = [
+            "arn:aws:s3:::${var.account_info.application_name}-${var.env_name}-weblogic-ecs-remote-state/*"
+          ]
+        }
+      ]
+    })
+  ]
 }
