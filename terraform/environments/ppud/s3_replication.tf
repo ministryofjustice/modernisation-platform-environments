5fec1cbd49f65ffd84453a762a69c519ee185dac
nbuckingham72 <nick.buckingham@colt.net>
2026-07-17 13:03:56 +0100

    Update_170726_3

--- terraform/environments/ppud/s3_replication.tf
+++ terraform/environments/ppud/s3_replication.tf
@@ -82,9 +82,9 @@ locals {
 
   # Log bucket ARN lookup used in replication bucket policies
   log_bucket_arns = {
-    "moj-general-logs-dev"  = local.is-development ? aws_s3_bucket.moj-general-logs-dev[0].arn : ""
-    "moj-general-logs-uat"  = local.is-preproduction ? aws_s3_bucket.moj-general-logs-uat[0].arn : ""
-    "moj-general-logs-prod" = local.is-production ? aws_s3_bucket.moj-general-logs-prod[0].arn : ""
+    "moj-general-logs-dev"  = local.is-development ? aws_s3_bucket.s3_general["general_logs_dev"].arn : ""
+    "moj-general-logs-uat"  = local.is-preproduction ? aws_s3_bucket.s3_general["general_logs_uat"].arn : ""
+    "moj-general-logs-prod" = local.is-production ? aws_s3_bucket.s3_general["general_logs_prod"].arn : ""
   }
 }
 
