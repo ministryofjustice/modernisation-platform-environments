17f29765bc1cb96d0a83f3645be532746d27a315
matt heery <matt.heery@justice.gov.uk>
2026-07-20 16:14:42 +0100

    fix: add local

--- terraform/environments/electronic-monitoring-data/modules/merge_into_reconciler/local.tf
+++ terraform/environments/electronic-monitoring-data/modules/merge_into_reconciler/local.tf
@@ -1,4 +1,4 @@
 locals {
     name       = trimprefix(var.function_to_iterate.lambda_function_name, "merge_")
-    camel_name = replace(title(replace(name, "_", " ")), " ", "")
+    camel_name = replace(title(replace(local.name, "_", " ")), " ", "")
 }
