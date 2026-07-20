e9765ed0d4ce0e8714fe05a125de562b1e2c65af
David Sibley <10881569+dms1981@users.noreply.github.com>
2026-07-20 11:12:02 +0100

    added event publisher for FileStagedForScanning.v1

--- terraform/environments/integration-hub-file-transfer/locals-eventbridge.tf
+++ terraform/environments/integration-hub-file-transfer/locals-eventbridge.tf
@@ -1,4 +1,5 @@
 locals {
   eventbridge_schema_directory = "${path.module}/schemas"
   eventbridge_schemas          = fileset("${path.module}/schemas", "*.json")
+  file_transfer_event_bus_arn  = "arn:aws:events:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:event-bus/${local.application_name}"
 }
