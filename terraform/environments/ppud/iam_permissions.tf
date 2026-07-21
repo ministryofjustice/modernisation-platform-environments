63c60d783bac63016caad3b333e20a2f0746e2d6
nbuckingham72 <nick.buckingham@colt.net>
2026-07-21 12:44:12 +0100

    Update_210726_5

--- terraform/environments/ppud/iam_permissions.tf
+++ terraform/environments/ppud/iam_permissions.tf
@@ -388,7 +388,7 @@ resource "aws_iam_policy" "lambda_policies_v2" {
         } : each.value.policy_name == "filter_waf_log_events" ? {
         Effect   = "Allow"
         Action   = ["logs:FilterLogEvents"]
-        Resource = ["arn:aws:cloudwatch:eu-west-2:${local.environment_management.account_ids[each.value.env_config.account_key]}:*"]
+        Resource = ["arn:aws:logs:eu-west-2:${local.environment_management.account_ids[each.value.env_config.account_key]}:*"]
         } : each.value.policy_name == "update_ses_access_key" ? {
         Effect   = "Allow"
         Action   = ["iam:CreateAccessKey", "iam:DeleteAccessKey", "iam:ListAccessKeys", "iam:UpdateAccessKey"]
