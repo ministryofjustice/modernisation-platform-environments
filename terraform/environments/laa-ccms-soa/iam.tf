b1aa9a93d32c185a97e2febde25b98b634cfd6db
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-07-21 16:34:07 +0100

    final changes after review

--- terraform/environments/laa-ccms-soa/iam.tf
+++ terraform/environments/laa-ccms-soa/iam.tf
@@ -244,7 +244,7 @@ resource "aws_iam_policy" "ec2_instance_policy" {
                 "secretsmanager:PutSecretValue",
                 "secretsmanager:CreateSecret"
           ],
-          "Resource": ["arn:aws:secretsmanager:eu-west-2:*:secret:soa-secrets*"]
+          "Resource": ["arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:soa-secrets*"]
         }
     ]
 }
