04208caf38867ee1517c3553bfcb9a943029aab8
jude.dedzo@justice.gov.uk <jude.dedzo@justice.gov.uk>
2026-07-21 16:25:10 +0000

    changes to main.tf

--- terraform/environments/data-factory-corporate/main.tf
+++ terraform/environments/data-factory-corporate/main.tf
@@ -4,13 +4,13 @@ terraform{
 }
 
 
-module "glue "{
+module "glue"{
 
     source="https://github.com/ministryofjustice/modernisation-platform-environments/tree/dedzo_dev_v2/terraform/environments/data-factory-corporate/corporate_data/modules/glue_catalog"
     description ="glue_table"
 
     database_name = "corporate_database"
     database_description = "Corporate Glue database for S3 datasets"
-    location_uri=''
+    location_uri=""
 
 }
