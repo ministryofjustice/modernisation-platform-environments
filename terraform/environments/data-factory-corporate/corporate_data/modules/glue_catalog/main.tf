35cc25dcd995e84763478ac900297260fe56ca3a
jude.dedzo@justice.gov.uk <jude.dedzo@justice.gov.uk>
2026-07-21 16:22:56 +0000

    changes to main

--- terraform/environments/data-factory-corporate/corporate_data/modules/glue_catalog/main.tf
+++ terraform/environments/data-factory-corporate/corporate_data/modules/glue_catalog/main.tf
@@ -10,21 +10,11 @@ terraform{
 
 
 module "glue_catalog_database" {
-  # Use the Glue catalog database submodule from Cloud Posse.
-  source = "cloudposse/glue/aws//modules/glue-catalog-database"
+  source = "git::https://github.com/cloudposse/terraform-aws-glue.git//modules/glue-catalog-database?ref=e04ac37bd44efcf29ad0a8fc94149bccc9162a6d"
 
-  # Pin the module to a specific version for consistent deployments.
-  version = "0.4.0"
-
-  # Set the name of the Glue Data Catalog database.
-  catalog_database_name = "analytics"
-
-  # Describe the purpose of the Glue database.
+  catalog_database_name        = "analytics"
   catalog_database_description = "Glue Catalog database using data located in an S3 bucket"
+  location_uri                 = "s3://${module.s3_bucket_source.bucket_id}"
 
-  # Point the database to the source S3 bucket.
-  location_uri = "s3://${module.s3_bucket_source.bucket_id}"
-
-  # Apply the shared Cloud Posse naming, tagging, and environment context.
   context = module.this.context
 }
