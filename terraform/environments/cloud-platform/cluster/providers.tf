3b87baf4e5291daaab1e62aaa1311172677a762b
Ky <ky.truong@digital.justice.gov.uk>
2026-07-22 11:53:06 +0100

    re-enabling lazy_load

--- terraform/environments/cloud-platform/cluster/providers.tf
+++ terraform/environments/cloud-platform/cluster/providers.tf
@@ -17,5 +17,5 @@ provider "kubectl" {
   cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data), null)
   token                  = try(data.aws_eks_cluster_auth.cluster.token, null)
   load_config_file       = false
-  # lazy_load              = true
+  lazy_load              = true
 }
