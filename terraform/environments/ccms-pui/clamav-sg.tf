700196ba55a8f6f6b1647d859b74cf7a140555ba
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-07-22 15:58:54 +0100

    added egress in clamav

--- terraform/environments/ccms-pui/clamav-sg.tf
+++ terraform/environments/ccms-pui/clamav-sg.tf
@@ -24,11 +24,11 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_traffic_clamav_3310" {
 # EGRESS Rules
 
 # ### HTTPS
-# resource "aws_vpc_security_group_egress_rule" "egress_traffic_clamav_443" {
-#   security_group_id = aws_security_group.ec2_sg_clamav.id
-#   description       = "Outbound HTTPS"
-#   cidr_ipv4         = "0.0.0.0/0"
-#   ip_protocol       = "tcp"
-#   from_port         = 443
-#   to_port           = 443
-# }
+resource "aws_vpc_security_group_egress_rule" "egress_traffic_clamav_443" {
+  security_group_id = aws_security_group.ec2_sg_clamav.id
+  description       = "Outbound HTTPS"
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+}
