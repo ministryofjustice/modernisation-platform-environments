ee7e067c44e975bcdd3531dc21f14e0959f425ab
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-07-23 16:04:51 +0000

    feat: implement ElevenLabs SageMaker execution IAM role and KMS key configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/data-platform/sagemaker/data.tf
+++ terraform/environments/data-platform/sagemaker/data.tf
@@ -4,20 +4,8 @@ data "aws_secretsmanager_secret_version" "elevenlabs_configuration_secret" {
   secret_id = module.elevenlabs_configuration_secret[0].secret_id
 }
 
-# data "aws_secretsmanager_secret" "justiceuk_entra" {
-#   count = terraform.workspace == "data-platform-development" ? 1 : 0
+data "aws_secretsmanager_secret_version" "justiceai_entra_application_secret" {
+  count = terraform.workspace == "data-platform-development" ? 1 : 0
 
-#   name = "justiceuk/entra"
-# }
-
-# data "aws_secretsmanager_secret_version" "justiceuk_entra" {
-#   count = terraform.workspace == "data-platform-development" ? 1 : 0
-
-#   secret_id = data.aws_secretsmanager_secret.justiceuk_entra[0].id
-# }
-
-# data "aws_iam_openid_connect_provider" "justiceuk_entra" {
-#   count = terraform.workspace == "data-platform-development" ? 1 : 0
-
-#   url = "https://sts.windows.net/${jsondecode(data.aws_secretsmanager_secret_version.justiceuk_entra[0].secret_string)["tenant_id"]}/"
-# }
+  secret_id = module.justiceai_entra_application_secret[0].secret_id
+}
