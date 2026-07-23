cec49599815a067e921dddae4fd20a67c7d7df58
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-07-23 16:29:00 +0100

    change data to private subnet for alb

--- terraform/environments/ccms-pui/security.tf
+++ terraform/environments/ccms-pui/security.tf
@@ -28,9 +28,9 @@ resource "aws_security_group_rule" "alb_egress_targets" {
   from_port         = 8080
   to_port           = 8080
   cidr_blocks = [
-    data.aws_subnet.data_subnets_a.cidr_block,
-    data.aws_subnet.data_subnets_b.cidr_block,
-    data.aws_subnet.data_subnets_c.cidr_block,
+    data.aws_subnet.private_subnets_a.cidr_block,
+    data.aws_subnet.private_subnets_b.cidr_block,
+    data.aws_subnet.private_subnets_c.cidr_block,
   ]
 }
 
