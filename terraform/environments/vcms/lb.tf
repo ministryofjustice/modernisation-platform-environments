b13c41757a8ab42e7a77d6499cae8704bbaafe90
wullub <13420301+wullub@users.noreply.github.com>
2026-07-24 13:17:04 +0100

    vcms-private-lb

--- terraform/environments/vcms/lb.tf
+++ terraform/environments/vcms/lb.tf
@@ -117,17 +117,3 @@ resource "aws_lb_target_group" "frontend_private" {
   tags = local.tags
 }
 
-
-# HTTPS Listener
-resource "aws_lb_listener" "frontend_private_https" {
-  load_balancer_arn = aws_lb.frontend_private.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.external.arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend_private.arn
-  }
-}
