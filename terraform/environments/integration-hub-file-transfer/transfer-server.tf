9370acd6c039aa4df538751dafe9001b3114a97c
David Sibley <10881569+dms1981@users.noreply.github.com>
2026-07-27 10:03:56 +0100

    correctly assign number of subnets based on environment

--- terraform/environments/integration-hub-file-transfer/transfer-server.tf
+++ terraform/environments/integration-hub-file-transfer/transfer-server.tf
@@ -1,25 +1,19 @@
 resource "aws_transfer_server" "this" {
   #certificate            = aws_acm_certificate.ftps.arn
-  domain                 = "S3"
-  endpoint_type          = "VPC"
-  function               = module.lambda_custom_idp.lambda_function_arn
-  identity_provider_type = "AWS_LAMBDA"
-  logging_role           = module.iam_role_transfer.arn
-  protocols              = ["SFTP"]
-  security_policy_name   = "TransferSecurityPolicy-2025-03"
-  structured_log_destinations = [
-    "${module.cloudwatch_transfer.cloudwatch_log_group_arn}:*"
-  ]
+  domain                      = "S3"
+  endpoint_type               = "VPC"
+  function                    = module.lambda_custom_idp.lambda_function_arn
+  identity_provider_type      = "AWS_LAMBDA"
+  logging_role                = module.iam_role_transfer.arn
+  protocols                   = ["SFTP"]
+  security_policy_name        = "TransferSecurityPolicy-2025-03"
+  structured_log_destinations = ["${module.cloudwatch_transfer.cloudwatch_log_group_arn}:*"]
 
   endpoint_details {
-    vpc_id     = module.vpc_isolated.vpc_id
-    subnet_ids = module.vpc_isolated.public_subnets
-    address_allocation_ids = [
-      for key, value in aws_eip.this : value.id
-    ]
-    security_group_ids = [
-      aws_security_group.transfer.id
-    ]
+    vpc_id                 = module.vpc_isolated.vpc_id
+    subnet_ids             = local.transfer_subnet_ids
+    address_allocation_ids = local.transfer_address_allocation_ids
+    security_group_ids     = [aws_security_group.transfer.id]
   }
 
   tags = merge(
