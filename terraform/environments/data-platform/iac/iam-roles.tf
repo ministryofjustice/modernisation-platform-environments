2fae0326745a677108f19e4fc037660c10ef4ac5
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2026-07-28 20:44:00 +0000

    removing reference to secret until it is created

--- terraform/environments/data-platform/iac/iam-roles.tf
+++ terraform/environments/data-platform/iac/iam-roles.tf
@@ -49,7 +49,7 @@ module "data_platform_access_iam_role" {
         module.pagerduty_api_key_secret[0].secret_arn,
         module.slack_token_secret[0].secret_arn,
         module.pagerduty_slack_connection_api_key_secret[0].secret_arn,
-        module.pagerduty_orchestrator_integration_key_secret[0].secret_arn
+        # module.pagerduty_orchestrator_integration_key_secret[0].secret_arn
       ]
     }
     SecretsManagerWriteAccess = {
