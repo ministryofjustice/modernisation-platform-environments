48362df30ad2713dfa5dd58d89b40ab9aeb40cbf
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2026-07-28 19:02:08 +0100

    fix: enable managed scaled

--- terraform/environments/ccms-pui/capacity-provider.tf
+++ terraform/environments/ccms-pui/capacity-provider.tf
@@ -4,8 +4,8 @@ resource "aws_ecs_capacity_provider" "capacity-provider" {
   name = "${local.application_name}-capacity-provider"
 
   auto_scaling_group_provider {
-    auto_scaling_group_arn         = aws_autoscaling_group.cluster-scaling-group.arn
-    managed_termination_protection = "ENABLED"
+    auto_scaling_group_arn = aws_autoscaling_group.cluster-scaling-group.arn
+    managed_termination_protection = "DISABLED"
 
     # Lets ECS automatically scale the ASG out (up to ec2_max_capacity) when
     # it needs extra instance capacity to place the new task revision
