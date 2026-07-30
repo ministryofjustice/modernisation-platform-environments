20f80822077865a86b7884c923bd198925ad561a
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2026-07-30 16:52:31 +0100

    test

--- terraform/environments/cooker/secrets.tf
+++ terraform/environments/cooker/secrets.tf
@@ -1,1 +1,6 @@
 #### This file can be used to store secrets specific to the member account ####
+resource "aws_secretsmanager_secret" "cooker_client_id" {
+  provider    = aws.organisation-secret-read
+  name        = "platforms/client_id"
+  description = "test"
+}
