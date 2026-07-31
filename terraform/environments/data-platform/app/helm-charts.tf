93c6bac677c0fdab5064cc983b63995236e5d2e1
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2026-07-31 09:56:30 +0000

    Bump chart version to 0.1.5

--- terraform/environments/data-platform/app/helm-charts.tf
+++ terraform/environments/data-platform/app/helm-charts.tf
@@ -3,7 +3,7 @@ resource "helm_release" "app" {
   /* https://github.com/ministryofjustice/data-platform-app */
   name       = "app"
   repository = "oci://ghcr.io/ministryofjustice/data-platform-charts"
-  version    = "0.1.4"
+  version    = "0.1.5"
   chart      = "app"
   namespace  = module.app_namespace.name
   values = [
