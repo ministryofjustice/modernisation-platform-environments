3c65db8496387ed144c968990100f7c02be12c8c
David Sibley <10881569+dms1981@users.noreply.github.com>
2026-08-03 11:02:52 +0100

    added quick test for Group ID using unique attribute

--- terraform/environments/integration-hub-file-transfer/locals-transfer-web-app.tf
+++ terraform/environments/integration-hub-file-transfer/locals-transfer-web-app.tf
@@ -9,6 +9,17 @@ data "aws_identitystore_group" "this" {
   group_id          = each.value
 }
 
+data "aws_identitystore_group" "test" {
+  provider          = aws.sso-readonly
+  identity_store_id = one(data.aws_ssoadmin_instances.this.identity_store_ids)
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = "integration-hub"
+    }
+  }
+}
+
 locals {
   # GetGroupId does not reliably resolve groups by display name, so IDs are explicit here.
   transfer_iam_identity_center_groups = {
