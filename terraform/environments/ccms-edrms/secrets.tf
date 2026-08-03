66ee47c58d642469e4b10e16355151d75d4160c4
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-08-03 11:11:12 +0100

    fix: removing redundant secrets

--- terraform/environments/ccms-edrms/secrets.tf
+++ terraform/environments/ccms-edrms/secrets.tf
@@ -9,32 +9,6 @@ data "aws_secretsmanager_secret_version" "spring_datasource_password" {
   secret_id = aws_secretsmanager_secret.spring_datasource_password.id
 }
 
-# Slack Channel ID for Alerts
-resource "aws_secretsmanager_secret" "slack_channel_id" {
-  name        = "alerts_slack_channel_id"
-  description = "Slack Channel ID for EDRMS Alerts"
-}
-
-data "aws_secretsmanager_secret_version" "slack_channel_id" {
-  secret_id = aws_secretsmanager_secret.slack_channel_id.id
-}
-
-# Slack Channel ID for guardduty Alerts
-resource "aws_secretsmanager_secret" "guardduty_slack_channel_id" {
-  name        = "guardduty_slack_channel_id"
-  description = "Slack Channel ID for guardduty Alerts"
-}
-
-data "aws_secretsmanager_secret_version" "guardduty_slack_channel_id" {
-  secret_id = aws_secretsmanager_secret.guardduty_slack_channel_id.id
-}
-
-# Slack Channel Webhook Secret for EDRMS Docs Exception
-resource "aws_secretsmanager_secret" "edrms_secrets" {
-  name        = "${local.application_name}-secrets"
-  description = "EDRMS Secret"
-}
-
 resource "aws_secretsmanager_secret_version" "edrms_secrets" {
   secret_id = aws_secretsmanager_secret.edrms_secrets.id
   secret_string = jsonencode({
