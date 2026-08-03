cfb413d8b596dd8c089b0b7a260c8128f1e474ad
David Sibley <10881569+dms1981@users.noreply.github.com>
2026-08-03 12:11:33 +0100

    use identity center groups to populate assignments

--- terraform/environments/integration-hub-file-transfer/platform_providers.tf
+++ terraform/environments/integration-hub-file-transfer/platform_providers.tf
@@ -63,3 +63,16 @@ provider "aws" {
   }
   default_tags { tags = local.tags }
 }
+
+# Provider for CRUD operations on IAM Identity Center Application group assignments
+provider "aws" {
+  region = "eu-west-2"
+  alias  = "sso-application-assignment"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${local.environment_management.aws_organizations_root_account_id}:role/ModernisationPlatformSSOApplicationAssignment"
+    tags = {
+      ApplicationAccount = data.aws_caller_identity.original_session.account_id
+    }
+  }
+}
