ecc977443e2507f13c3da35386fbbd7ce31c98d8
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-08-03 16:59:56 +0000

    Add IAM policy and role for AI Gateway
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ terraform/environments/octo-engineering-ai-enablement/ai-gateway/iam-roles.tf
@@ -0,0 +1,22 @@
+module "iam_role" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-role?ref=d6e381ccfa95b944149c8b14ba4087e517c57ac7" # v6.8.0
+
+  name = local.component_name
+
+  trust_policy_permissions = {
+    TrustRoleAndServiceToAssume = {
+      actions = [
+        "sts:AssumeRole",
+        "sts:TagSession",
+      ]
+      principals = [{
+        type        = "AWS"
+        identifiers = ["arn:aws:iam::${local.environment_management.account_ids["data-platform-production"]}:role/ai-gateway-20260501124346396300000007"]
+      }]
+    }
+  }
+
+  policies = {
+    ai-gateway = module.ai_gateway_iam_policy.arn
+  }
+}
