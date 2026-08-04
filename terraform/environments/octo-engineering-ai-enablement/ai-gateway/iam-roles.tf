81c06345a5280988cab19231d12b7243315e3ab3
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-08-04 18:00:12 +0100

    Update iam-roles.tf

--- terraform/environments/octo-engineering-ai-enablement/ai-gateway/iam-roles.tf
+++ terraform/environments/octo-engineering-ai-enablement/ai-gateway/iam-roles.tf
@@ -1,7 +1,8 @@
 module "iam_role" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-role?ref=d6e381ccfa95b944149c8b14ba4087e517c57ac7" # v6.8.0
 
-  name = local.component_name
+  name            = local.component_name
+  use_name_prefix = false
 
   trust_policy_permissions = {
     TrustRoleAndServiceToAssume = {
