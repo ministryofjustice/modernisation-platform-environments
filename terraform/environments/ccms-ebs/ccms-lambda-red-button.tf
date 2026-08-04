77f9c8bd8118337166002c0f5b206f8efe28fe4a
srikanththummala0470 <Srikanth.Thummala@justice.gov.uk>
2026-08-04 10:19:38 +0100

    fix: encrypt CloudWatch log groups with customer-managed KMS keys (CC-4662)

--- terraform/environments/ccms-ebs/ccms-lambda-red-button.tf
+++ terraform/environments/ccms-ebs/ccms-lambda-red-button.tf
@@ -134,6 +134,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "red_button_data" 
 resource "aws_cloudwatch_log_group" "red_button_logs" {
   name              = "/aws/lambda/${aws_lambda_function.red_button_trigger.function_name}"
   retention_in_days = 14
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
   tags = merge(local.tags, {
     Name = "${local.application_name}-${local.environment}-red-button-trigger"
   })
--- terraform/environments/ccms-ebs/ssogen-cloudwatch.tf
+++ terraform/environments/ccms-ebs/ssogen-cloudwatch.tf
@@ -5,6 +5,7 @@ resource "aws_cloudwatch_log_group" "ssogen_groups" {
   for_each          = local.ssogen_enabled ? local.application_data.ssogen_cw_log_groups : {}
   name              = each.key
   retention_in_days = each.value.retention_days
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
 
   tags = merge(
     local.tags,
