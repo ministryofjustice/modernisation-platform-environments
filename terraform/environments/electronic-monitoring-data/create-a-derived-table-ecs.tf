90dfa275b8a4af7982ee8ae873de49204b335166
matt heery <matt.heery@justice.gov.uk>
2026-08-05 17:25:56 +0100

    fix: correct permissions

--- terraform/environments/electronic-monitoring-data/create-a-derived-table-ecs.tf
+++ terraform/environments/electronic-monitoring-data/create-a-derived-table-ecs.tf
@@ -142,7 +142,14 @@ data "aws_iam_policy_document" "cadt_api_trigger" {
     effect = "Allow"
     actions = ["apigateway:GET"]
     resources = [
-      "${module.trigger_cadt_api.api_gateway_arn}/resources/${module.trigger_cadt_api.resource_request_id}/methods/GET"
+      "${module.trigger_cadt_api.api_gateway_arn}/resources/${module.trigger_cadt_api.resource_status_id}/methods/GET"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = ["apigateway:GET"]
+    resources = [
+      "${module.trigger_cadt_api.api_gateway_arn}/resources/${module.trigger_cadt_api.resource_execution_id_id}/methods/GET"
     ]
   }
 }
--- terraform/environments/electronic-monitoring-data/modules/api_step_function/output.tf
+++ terraform/environments/electronic-monitoring-data/modules/api_step_function/output.tf
@@ -9,3 +9,11 @@ output "api_gateway_arn" {
 output "resource_request_id" {
   value = aws_api_gateway_resource.resource.id
 }
+
+output "resource_status_id" {
+  value = aws_api_gateway_resource.status.id
+}
+
+output "resource_execution_id_id" {
+  value = aws_api_gateway_resource.execution_id.id
+}
