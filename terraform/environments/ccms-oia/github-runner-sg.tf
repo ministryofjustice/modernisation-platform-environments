4694e10ee80d8d7627e965a3ba0b3083e0b845f9
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-08-06 13:32:08 +0100

    opened 0.0.0.0/0 on 443 for gh runner

--- terraform/environments/ccms-oia/github-runner-sg.tf
+++ terraform/environments/ccms-oia/github-runner-sg.tf
@@ -14,40 +14,50 @@ resource "aws_security_group" "ec2_sg_gh_runner" {
 
 # EGRESS Rules
 
-resource "aws_security_group_rule" "gh_runner_egress_vpce" {
-  security_group_id = aws_security_group.ec2_sg_gh_runner.id
-  type              = "egress"
-  description       = "Allow egress to protected subnets for VPC endpoints"
-  protocol          = "tcp"
-  from_port         = 443
-  to_port           = 443
-  cidr_blocks = [
-    data.aws_subnet.vpce_subnets_a.cidr_block,
-    data.aws_subnet.vpce_subnets_b.cidr_block,
-    data.aws_subnet.vpce_subnets_c.cidr_block,
-  ]
-}
+# resource "aws_security_group_rule" "gh_runner_egress_vpce" {
+#   security_group_id = aws_security_group.ec2_sg_gh_runner.id
+#   type              = "egress"
+#   description       = "Allow egress to protected subnets for VPC endpoints"
+#   protocol          = "tcp"
+#   from_port         = 443
+#   to_port           = 443
+#   cidr_blocks = [
+#     data.aws_subnet.vpce_subnets_a.cidr_block,
+#     data.aws_subnet.vpce_subnets_b.cidr_block,
+#     data.aws_subnet.vpce_subnets_c.cidr_block,
+#   ]
+# }
 
-resource "aws_security_group_rule" "gh_runner_egress_443" {
-  security_group_id = aws_security_group.ec2_sg_gh_runner.id
-  type              = "egress"
-  description       = "HTTPS"
-  protocol          = "tcp"
-  from_port         = 443
-  to_port           = 443
-  cidr_blocks = [
-    data.aws_subnet.private_subnets_a.cidr_block,
-    data.aws_subnet.private_subnets_b.cidr_block,
-    data.aws_subnet.private_subnets_c.cidr_block
-  ]
-}
+# resource "aws_security_group_rule" "gh_runner_egress_443" {
+#   security_group_id = aws_security_group.ec2_sg_gh_runner.id
+#   type              = "egress"
+#   description       = "HTTPS"
+#   protocol          = "tcp"
+#   from_port         = 443
+#   to_port           = 443
+#   cidr_blocks = [
+#     data.aws_subnet.private_subnets_a.cidr_block,
+#     data.aws_subnet.private_subnets_b.cidr_block,
+#     data.aws_subnet.private_subnets_c.cidr_block
+#   ]
+# }
 
-resource "aws_security_group_rule" "gh_runner_egress_to_specific_ip" {
+# resource "aws_security_group_rule" "gh_runner_egress_to_specific_ip" {
+#   security_group_id = aws_security_group.ec2_sg_gh_runner.id
+#   type              = "egress"
+#   description       = "Allow HTTPS to specific IP"
+#   protocol          = "tcp"
+#   from_port         = 443
+#   to_port           = 443
+#   cidr_blocks       = ["20.85.130.105/32"]
+# }
+
+resource "aws_security_group_rule" "gh_runner_egress_all_443" {
   security_group_id = aws_security_group.ec2_sg_gh_runner.id
   type              = "egress"
-  description       = "Allow HTTPS to specific IP"
+  description       = "Allow HTTPS to anywhere"
   protocol          = "tcp"
   from_port         = 443
   to_port           = 443
-  cidr_blocks       = ["20.85.130.105/32"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
