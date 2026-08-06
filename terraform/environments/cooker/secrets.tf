a889729e985183621ab84775c269548323972424
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2026-08-06 11:19:23 +0100

    test

--- terraform/environments/cooker/secrets.tf
+++ terraform/environments/cooker/secrets.tf
@@ -3,3 +3,8 @@ resource "aws_secretsmanager_secret" "database_password" {
   name        = "username"
   description = "Database username"
 }
+
+resource "aws_secretsmanager_secret" "client_id" {
+  name        = "client_id"
+  description = "Client ID"
+}
