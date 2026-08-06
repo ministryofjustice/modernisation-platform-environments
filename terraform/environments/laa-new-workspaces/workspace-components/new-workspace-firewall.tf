811db04b6c5b9e8dd93c69b61b67aa90d51d7de9
Vincent.Cheung <vincent.cheung1@justice.gov.uk>
2026-08-06 13:21:56 +0100

    Temp disable firewall to install tools 2

--- terraform/environments/laa-new-workspaces/workspace-components/new-workspace-firewall.tf
+++ terraform/environments/laa-new-workspaces/workspace-components/new-workspace-firewall.tf
@@ -356,7 +356,7 @@ resource "aws_networkfirewall_firewall_policy" "workspaces_web_allowlist" {
     stateless_default_actions          = ["aws:forward_to_sfe"]
     stateless_fragment_default_actions = ["aws:forward_to_sfe"]
     # stateful_default_actions           = ["aws:alert_strict", "aws:drop_established"]
-    stateful_default_actions           = ["aws:alert_strict", "aws:none"] # Temp to allow traffic for testing
+    stateful_default_actions           = ["aws:alert_strict"] # Temp to allow traffic for testing
 
     stateful_engine_options {
       rule_order = "STRICT_ORDER"
