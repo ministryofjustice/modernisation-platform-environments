19536c1f63053eeada7ab14d5dd556076fee6347
srikanththummala0470 <Srikanth.Thummala@justice.gov.uk>
2026-08-06 12:09:36 +0100

    CC-4660: revert S3 security controls changes, agreed not to proceed with this ticket per app ops call

--- terraform/environments/ccms-ebs/ccms-s3.tf
+++ terraform/environments/ccms-ebs/ccms-s3.tf
@@ -205,27 +205,12 @@ module "s3-bucket-dbbackup" {
           }
         },
         "Principal" : "*"
-      },
-      {
-        # CC-4660: require uploads to use SSE-KMS with the approved CMK
-        "Sid" : "DenyIncorrectEncryptionHeader",
-        "Effect" : "Deny",
-        "Principal" : "*",
-        "Action" : "s3:PutObject",
-        "Resource" : "${module.s3-bucket-dbbackup.bucket.arn}/*",
-        "Condition" : {
-          "StringNotEquals" : {
-            "s3:x-amz-server-side-encryption-aws-kms-key-id" : aws_kms_key.s3_cmk.arn
-          }
-        }
       }
     ]
   })]
 
-  log_bucket     = local.logging_bucket_name
-  log_prefix     = "s3access/${local.rsync_bucket_name}"
-  sse_algorithm  = "aws:kms"
-  custom_kms_key = aws_kms_key.s3_cmk.arn
+  log_bucket = local.logging_bucket_name
+  log_prefix = "s3access/${local.rsync_bucket_name}"
 
   # Refer to the below section "Replication" before enabling replication
   replication_enabled = false
@@ -314,27 +299,6 @@ resource "aws_s3_bucket_versioning" "ccms_ebs_shared" {
   }
 }
 
-# CC-4660: server access logging to the environment logging bucket
-resource "aws_s3_bucket_logging" "ccms_ebs_shared" {
-  bucket = aws_s3_bucket.ccms_ebs_shared.id
-
-  target_bucket = local.logging_bucket_name
-  target_prefix = "s3-access-logs/${aws_s3_bucket.ccms_ebs_shared.id}/"
-}
-
-# CC-4660: SSE-KMS with the customer-managed key (aws_kms_key.s3_cmk in ccms-kms.tf)
-resource "aws_s3_bucket_server_side_encryption_configuration" "ccms_ebs_shared" {
-  bucket = aws_s3_bucket.ccms_ebs_shared.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.s3_cmk.arn
-    }
-    bucket_key_enabled = true
-  }
-}
-
 data "aws_iam_policy_document" "shared_bucket_secure_transport" {
   statement {
     sid    = "DenyInsecureTransport"
@@ -352,23 +316,6 @@ data "aws_iam_policy_document" "shared_bucket_secure_transport" {
       test     = "Bool"
       variable = "aws:SecureTransport"
       values   = ["false"]
-    }
-  }
-
-  # CC-4660: require uploads to use SSE-KMS with the approved CMK
-  statement {
-    sid    = "DenyIncorrectEncryptionHeader"
-    effect = "Deny"
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.ccms_ebs_shared.arn}/*"]
-    condition {
-      test     = "StringNotEquals"
-      variable = "s3:x-amz-server-side-encryption-aws-kms-key-id"
-      values   = [aws_kms_key.s3_cmk.arn]
     }
   }
 }
@@ -402,27 +349,6 @@ resource "aws_s3_bucket_versioning" "lambda_payment_load" {
 
   versioning_configuration {
     status = "Enabled"
-  }
-}
-
-# CC-4660: server access logging to the environment logging bucket
-resource "aws_s3_bucket_logging" "lambda_payment_load" {
-  bucket = aws_s3_bucket.lambda_payment_load.id
-
-  target_bucket = local.logging_bucket_name
-  target_prefix = "s3-access-logs/${aws_s3_bucket.lambda_payment_load.id}/"
-}
-
-# CC-4660: SSE-KMS with the customer-managed key (aws_kms_key.s3_cmk in ccms-kms.tf)
-resource "aws_s3_bucket_server_side_encryption_configuration" "lambda_payment_load" {
-  bucket = aws_s3_bucket.lambda_payment_load.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.s3_cmk.arn
-    }
-    bucket_key_enabled = true
   }
 }
 
@@ -479,23 +405,6 @@ data "aws_iam_policy_document" "payment_load_bucket_secure_transport" {
       test     = "Bool"
       variable = "aws:SecureTransport"
       values   = ["false"]
-    }
-  }
-
-  # CC-4660: require uploads to use SSE-KMS with the approved CMK
-  statement {
-    sid    = "DenyIncorrectEncryptionHeader"
-    effect = "Deny"
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.lambda_payment_load.arn}/*"]
-    condition {
-      test     = "StringNotEquals"
-      variable = "s3:x-amz-server-side-encryption-aws-kms-key-id"
-      values   = [aws_kms_key.s3_cmk.arn]
     }
   }
 }
