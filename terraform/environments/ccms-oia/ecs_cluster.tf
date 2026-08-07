0a6cf1c965c5314a6652237445e2ef571755f225
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2026-08-07 23:43:37 +0100

    fix: enabling capacity provider

--- terraform/environments/ccms-oia/ecs_cluster.tf
+++ terraform/environments/ccms-oia/ecs_cluster.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_cluster" "main" {
   }
 }
 
-resource "aws_ecs_cluster_capacity_providers" "main" {
-  cluster_name       = aws_ecs_cluster.main.name
-  capacity_providers = [aws_ecs_capacity_provider.capacity_provider.name]
-}
+# resource "aws_ecs_cluster_capacity_providers" "main" {
+#   cluster_name       = aws_ecs_cluster.main.name
+#   capacity_providers = [aws_ecs_capacity_provider.capacity_provider.name]
+# }
