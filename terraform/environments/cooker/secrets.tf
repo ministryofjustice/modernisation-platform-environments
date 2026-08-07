aeec43da46966116591374a7a01631c6ecb14536
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2026-08-07 09:58:05 +0100

    tag

--- terraform/environments/cooker/secrets.tf
+++ terraform/environments/cooker/secrets.tf
@@ -1,4 +1,3 @@
-#### This file can be used to store secrets specific to the member account ####
 resource "aws_secretsmanager_secret" "database_password" {
   name        = "username"
   description = "Database username"
