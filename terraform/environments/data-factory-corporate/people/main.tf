6d046f6b0e7ffcae33f6ac79329bdd4f82232de9
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-08-11 15:44:17 +0100

    iamrole tag dupe fix

--- terraform/environments/data-factory-corporate/people/main.tf
+++ terraform/environments/data-factory-corporate/people/main.tf
@@ -16,7 +16,7 @@ data "aws_secretsmanager_secret" "external_account_id" {
   name = "external-aws-account"
 }
 
-data "aws_secretsmanager_secret_version" "external_account_id" {
+data "aws_secretsmanager_secret_version" "external_ account_id" {
   secret_id = data.aws_secretsmanager_secret.external_account_id.id
 }
 
@@ -138,7 +138,6 @@ module "assume_iam_role" {
   ]
 
   tags = {
-    Environment = terraform.workspace
     Application = "data-factory-corporate"
     Component   = "people"
     Infrastructure = "assume-iam-role"
