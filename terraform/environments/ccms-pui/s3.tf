1437ccdcc756db2272893ae8cda81a87a43195e7
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-08-13 16:51:39 +0100

    added logging bucket policy

--- terraform/environments/ccms-pui/s3.tf
+++ terraform/environments/ccms-pui/s3.tf
@@ -97,7 +97,7 @@ module "s3-bucket-logging" {
   log_buckets = {
    log_bucket_name = module.s3-bucket-logging.bucket.id
    log_bucket_arn  = module.s3-bucket-logging.bucket.arn
-   log_bucket_policy = jsonencode({Version   = "2012-10-17", Statement = []})
+   log_bucket_policy = aws_s3_bucket_policy.lb_access_logs.policy
      }
   log_prefix = "s3access/${local.application_name}-${local.environment}-logging"
 
