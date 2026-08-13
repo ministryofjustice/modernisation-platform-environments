2b7f153c6d86fc0fdc0f59db6bea96f1a77e7af6
David Teeling <david.teeling1@justice.gov.uk>
2026-08-13 15:49:38 +0100

    Add DNS record

--- terraform/environments/vcms/route53.tf
+++ terraform/environments/vcms/route53.tf
@@ -57,6 +57,20 @@ resource "aws_acm_certificate_validation" "external" {
 }
 
 # Internal ALB
+resource "aws_route53_record" "internal" {
+  provider = aws.core-vpc
+
+  zone_id = local.account_config.route53_inner_zone.zone_id
+  name    = local.account_config.internal_dns_suffix
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.frontend_internal.dns_name
+    zone_id                = aws_lb.frontend_internal.zone_id
+    evaluate_target_health = true
+  }
+}
+
 # resource "aws_acm_certificate" "internal" {
 #   domain_name       = local.account_config.internal_dns_suffix
 #   validation_method = "DNS"
