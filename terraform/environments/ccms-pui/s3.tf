0ab7d0a1148bf3bf71adaf9adc5a8d12f7e6187e
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-08-14 17:09:55 +0100

    retained and added additional policies

--- terraform/environments/ccms-pui/s3.tf
+++ terraform/environments/ccms-pui/s3.tf
@@ -194,7 +194,7 @@ resource "aws_s3_bucket_policy" "lb_access_logs" {
         Principal = {
           Service = "logdelivery.elasticloadbalancing.amazonaws.com"
         }
-        Action   = ["s3:PutObject"]
+        Action   = "s3:PutObject"
         Resource = "${module.s3-bucket-logging.bucket.arn}/*",
         Condition = {
           StringEquals = {
