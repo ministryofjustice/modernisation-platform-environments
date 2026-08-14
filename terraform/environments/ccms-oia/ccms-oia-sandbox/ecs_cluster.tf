0e98f3c141b4bc179ecf4ec08e43c98b4adb040b
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-08-14 14:39:57 +0100

    fix: oia component

---
+++ terraform/environments/ccms-oia/ccms-oia-sandbox/ecs_cluster.tf
@@ -0,0 +1,28 @@
+# ECS Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "${local.first_cluster_name}"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_cluster" "additional" {
+  name = "${local.second_cluster_name}"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name       = aws_ecs_cluster.main.name
+  capacity_providers = [aws_ecs_capacity_provider.capacity_provider.name]
+}
+
+resource "aws_ecs_cluster_capacity_providers" "additional" {
+  cluster_name       = aws_ecs_cluster.additional.name
+  capacity_providers = [aws_ecs_capacity_provider.capacity_provider.name]
+}
