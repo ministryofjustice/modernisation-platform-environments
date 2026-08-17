eee02eee1c6d9c8851e58a0a14e10e72996fb1e9
lucy-astley-jones <lucy.astleyjones@justice.gov.uk>
2026-08-17 16:31:02 +0100

    set iceberg extensions

--- terraform/environments/electronic-monitoring-data/dms_data_validation_glue_job_v2.tf
+++ terraform/environments/electronic-monitoring-data/dms_data_validation_glue_job_v2.tf
@@ -901,7 +901,7 @@ resource "aws_glue_job" "apply_sort_order" {
   worker_type       = "G.1X"
   number_of_workers = 2
   default_arguments = {
-    "--catalog"                          = "arn:aws:glue:${data.aws_region.current.region}:${local.env_account_id}:catalog"
+    "--catalog"                          = "${local.env_account_id}"
     "--database"                         = "staged_mdss${local.dbt_suffix}"
     "--table"                            = "event"
     "--remove_sort_order"                = "false"
@@ -910,6 +910,13 @@ resource "aws_glue_job" "apply_sort_order" {
     "--order_col"                        = "false"
     "--order_cols"                       = "false"
     "--enable-continuous-cloudwatch-log" = "true"
+    "--datalake-formats"                 = "iceberg"
+    "--conf"                             = <<EOF
+spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions 
+--conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog
+--conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog 
+--conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO
+EOF
   }
   command {
     python_version  = "3"
