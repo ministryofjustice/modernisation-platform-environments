b698a6025e1d16ae99907933d04f8c0bfca2c2ae
lucy-astley-jones <lucy.astleyjones@justice.gov.uk>
2026-08-17 13:53:12 +0100

    make arguments default

--- terraform/environments/electronic-monitoring-data/glue-job/apply_sort_order.py
+++ terraform/environments/electronic-monitoring-data/glue-job/apply_sort_order.py
@@ -10,13 +10,14 @@ args = getResolvedOptions(sys.argv, ["JOB_NAME",
                                      "catalog",
                                      "database",
                                      "table",
+                                     "remove_sort_order",
+                                     "order", 
                                      "order_col",
-                                     "order",
                                      "order_cols",
-                                     "remove_sort_order"
                                      ])
 
 
+# set spark session
 sc = SparkContext.getOrCreate()
 glue_context = GlueContext(sc)
 logger = glue_context.get_logger()
@@ -29,13 +30,23 @@ remove_sort_order_command = f"""ALTER TABLE {args['catalog']}.{args['database']}
 
 try:
     logger.info("Attempting to apply sort order...")
-    if args['order_cols'] != "":
-        spark.sql(z_sort_command)
-    elif args['remove_sort_order'] == "true":
+
+    if args['remove_sort_order'] == "true":
         spark.sql(remove_sort_order_command)
-    else: 
+        logger.info("Sort order removed.")
+
+    elif args['order_col'] != "false":
         spark.sql(sort_order_command)
-    logger.info("Sort order applied.")
+        logger.info("Sort order set.")
+
+    elif args['order_cols'] != "false":
+        spark.sql(z_sort_command)
+        logger.info("Z-Sort order applied.")
+
+    else:
+        msg = "either `remove_sort_order`, `order_col`, or `order_cols` must be set."
+        raise ValueError(msg)
+
 except Exception as e:
     logger.error("Failed.")
     raise e
