a05e3f8efbd16708dea92cb8a39bfa224038f594
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-08-18 11:53:10 +0100

    updated secret changes

--- terraform/environments/digital-prison-reporting/ppud-pipeline/secrets.tf
+++ terraform/environments/digital-prison-reporting/ppud-pipeline/secrets.tf
@@ -29,6 +29,7 @@ module "ppud_slack_webhook" {
   name = "ppud_notifications_slack_webhook"
 
   secret_string           = "CHANGEME"
+  ignore_secret_changes   = true
   recovery_window_in_days = 0
 
   tags = merge(
