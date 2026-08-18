5674e4c6d461ce48cd53b46299d7ae44ed5515be
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-08-18 10:28:30 +0100

    commented out SNS topic and updated secret

--- terraform/environments/digital-prison-reporting/ppud-pipeline/secrets.tf
+++ terraform/environments/digital-prison-reporting/ppud-pipeline/secrets.tf
@@ -28,7 +28,7 @@ module "ppud_slack_webhook" {
 
   name = "ppud_notifications_slack_webhook"
 
-  secret_string         = "https://CHANGEME"
+  secret_string         = "CHANGEME"
   ignore_secret_changes = false
   recovery_window_in_days = 0
 
