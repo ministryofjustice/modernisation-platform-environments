7d26c00f74d4899e90ee6d6307091c5dc5b73976
lucy-astley-jones <lucy.astleyjones@justice.gov.uk>
2026-08-19 10:54:18 +0100

    specify catalog

--- terraform/environments/electronic-monitoring-data/dms_data_validation_glue_job_v2.tf
+++ terraform/environments/electronic-monitoring-data/dms_data_validation_glue_job_v2.tf
@@ -912,7 +912,8 @@ resource "aws_glue_job" "rewrite_in_place" {
     "--conf"                             = <<EOF
 spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions 
 --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog 
---conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog 
+--conf spark.sql.catalog.glue_catalog.warehouse=s3:/${module.s3-create-a-derived-table-bucket.bucket.arn}/data/${local.environment_shorthand}/models
+--conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog
 --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO
 EOF
 
