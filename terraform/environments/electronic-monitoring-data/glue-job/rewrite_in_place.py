fdee0a6ce39b42ad5a6e7fecf6195d87f1f9eb73
lucy-astley-jones <lucy.astleyjones@justice.gov.uk>
2026-08-19 13:56:33 +0100

    alter command

--- terraform/environments/electronic-monitoring-data/glue-job/rewrite_in_place.py
+++ terraform/environments/electronic-monitoring-data/glue-job/rewrite_in_place.py
@@ -27,9 +27,9 @@ logger.info(f"SPARK VERSION: {spark.version}")
 logger.info(f"SPARK CONTEXT CONFIG: {spark.sparkContext.getConf().getAll()}")
 logger.info(f'SPARK SQL EXTENSIONS: {spark.conf.get("spark.sql.extensions", "NOT SET")}')
 
-replace = f"""REPLACE TABLE `{args['catalog']}`.{args['database']}.{args['table']} AS SELECT * FROM `{args['catalog']}`.{args['database']}.{args['table']}"""
-alter = f"""ALTER TABLE `{args['catalog']}`.{args['database']}.{args['table']} WRITE ORDERED BY {args['order_cols']}"""
-insert = f"""INSERT TABLE `{args['catalog']}`.{args['database']}.{args['table']} SELECT * FROM `{args['catalog']}`.{args['database']}.{args['table']};"""
+replace = f"""REPLACE TABLE {args['database']}.{args['table']} AS SELECT * FROM {args['database']}.{args['table']}"""
+alter = f"""ALTER TABLE {args['database']}.{args['table']} WRITE ORDERED BY {args['order_cols']}"""
+insert = f"""INSERT TABLE {args['database']}.{args['table']} SELECT * FROM {args['database']}.{args['table']};"""
 
 try:
     logger.info("Attempting to apply sort order...")
