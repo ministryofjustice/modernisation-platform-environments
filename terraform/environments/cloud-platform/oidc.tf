3643661228a1acdd2b81c6794f483bc85963eb26
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-08-20 16:33:31 +0100

    Adding additional log permissions to create delivery source and destination

--- terraform/environments/cloud-platform/oidc.tf
+++ terraform/environments/cloud-platform/oidc.tf
@@ -141,6 +141,7 @@ data "aws_iam_policy_document" "github_actions_development_cluster_oidc_policy" 
       "logs:PutLogEvents",
       "logs:PutRetentionPolicy",
       "logs:Describe*",
+      "logs:PutDelivery*",
       "cloudwatch:PutMetricData"
     ]
     resources = ["*"]
