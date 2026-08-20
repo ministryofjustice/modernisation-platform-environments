89bed7a5b4eded3340a24324319f5420310873bb
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-08-20 16:41:29 +0100

    Adding resource tagging

--- terraform/environments/cloud-platform/oidc.tf
+++ terraform/environments/cloud-platform/oidc.tf
@@ -142,6 +142,7 @@ data "aws_iam_policy_document" "github_actions_development_cluster_oidc_policy" 
       "logs:PutRetentionPolicy",
       "logs:Describe*",
       "logs:PutDelivery*",
+      "logs:TagResource",
       "cloudwatch:PutMetricData"
     ]
     resources = ["*"]
