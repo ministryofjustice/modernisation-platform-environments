7fefff70b14a90ae4ed93b05aae6a8c0459c6f5c
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-08-21 11:48:13 +0100

    changes to check recent file and formatting updates

--- terraform/environments/digital-prison-reporting/ppud-pipeline/lambda.tf
+++ terraform/environments/digital-prison-reporting/ppud-pipeline/lambda.tf
@@ -139,7 +139,7 @@ module "check_recent_file" {
     LAND_BUCKET               = module.ppud_replication_destination.bucket.id
     REGION                    = data.aws_region.current.region
     SLACK_WEBHOOK_SECRET_NAME = module.ppud_slack_webhook.secret_id
-    DAYS_BACK                 = local.days_back
+    DAYS_BACK                 = local.is-test ? 1 : local.days_back
   }
 
   source_path = [{
@@ -156,7 +156,7 @@ module "check_recent_file" {
 }
 
 resource "aws_lambda_permission" "allow_eventbridge_check_recent_file" {
-  count = local.is-test ? 0 : 1
+  count         = local.is-test ? 0 : 1
   statement_id  = "AllowExecutionFromEventBridgeCheckRecentFile"
   action        = "lambda:InvokeFunction"
   function_name = module.check_recent_file.lambda_function_arn
@@ -165,14 +165,14 @@ resource "aws_lambda_permission" "allow_eventbridge_check_recent_file" {
 }
 
 resource "aws_cloudwatch_event_rule" "check_recent_file_daily" {
-  count = local.is-test ? 0 : 1
+  count               = local.is-test ? 0 : 1
   name                = "${local.component_name}-check-recent-file-daily"
   description         = "Invoke recent-file checker daily at 15:15 UTC"
-  schedule_expression = local.cron_schedule
+  schedule_expression = local.is-test ? "rate(1 day)" : local.cron_schedule
 }
 
 resource "aws_cloudwatch_event_target" "check_recent_file_daily" {
-  count = local.is-test ? 0 : 1
+  count     = local.is-test ? 0 : 1
   rule      = aws_cloudwatch_event_rule.check_recent_file_daily[count.index].name
   target_id = "check-recent-file-lambda"
   arn       = module.check_recent_file.lambda_function_arn
