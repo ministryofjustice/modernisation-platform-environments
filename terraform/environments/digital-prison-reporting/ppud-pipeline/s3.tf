63033ba69cc8653dd3fb6b549932f770b4caea32
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-08-21 13:20:11 +0100

    updated s3 bucket policy

--- terraform/environments/digital-prison-reporting/ppud-pipeline/s3.tf
+++ terraform/environments/digital-prison-reporting/ppud-pipeline/s3.tf
@@ -1,3 +1,40 @@
+# policy to allow replication in destination bucket
+data "aws_iam_policy_document" "ppud_replication_destination_bucket_policy" {
+  statement {
+    sid    = "Set-permissions-for-objects"
+    effect = "Allow"
+
+    actions = [
+      "s3:ReplicateObject"
+    ]
+    resources = ["arn:aws:s3:::${module.ppud_replication_destination.bucket.id}/*"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${local.environment_management.account_ids["ppud-${local.environment}"]}:role/service-role/iam_role_s3_bucket_moj_database_source_dev"
+      ]
+    }
+  }
+
+  statement {
+    sid    = "Set-permissions-on-bucket"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetBucketVersioning",
+      "s3:PutBucketVersioning"
+    ]
+    resources = ["arn:aws:s3:::${module.ppud_replication_destination.bucket.id}"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${local.environment_management.account_ids["ppud-${local.environment}"]}:role/service-role/iam_role_s3_bucket_moj_database_source_dev"
+      ]
+    }
+  }
+}
+
 # S3 destination bucket for .bak file replication from ppud AWS account
 module "ppud_replication_destination" {
 
@@ -16,6 +53,8 @@ module "ppud_replication_destination" {
   }
 
   sse_algorithm = "AES256"
+
+  bucket_policy = data.aws_iam_policy_document.ppud_replication_destination_bucket_policy.json
 
   lifecycle_rule = [
     {
