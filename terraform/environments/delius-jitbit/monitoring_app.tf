7889c78ddfc7e8744f0797dda477ed7e327102c0
David Teeling <david.teeling1@justice.gov.uk>
2026-08-21 13:12:53 +0100

    Add blue/green Dashboard

--- terraform/environments/delius-jitbit/monitoring_app.tf
+++ terraform/environments/delius-jitbit/monitoring_app.tf
@@ -1,5 +1,26 @@
+moved {
+  from = aws_cloudwatch_log_group.app_logs
+  to   = aws_cloudwatch_log_group.app_logs[0]
+}
+
+moved {
+  from = aws_cloudwatch_log_metric_filter.error
+  to   = aws_cloudwatch_log_metric_filter.error[0]
+}
+
+moved {
+  from = aws_cloudwatch_metric_alarm.jitbit_high_error_volume
+  to   = aws_cloudwatch_metric_alarm.jitbit_high_error_volume[0]
+}
+
+locals {
+  service_name = "hmpps-${local.environment}-${local.application_name}"
+}
+
 resource "aws_cloudwatch_log_group" "app_logs" {
   #checkov:skip=CKV_AWS_338: "Logs required for 30 days"
+  count = local.create_blue_green ? 0 : 1
+
   name              = "delius-jitbit-ecs"
   retention_in_days = 30
   kms_key_id        = aws_kms_key.cloudwatch_logs.arn
@@ -9,9 +30,11 @@ resource "aws_cloudwatch_log_group" "app_logs" {
 
 // log metric filter for error logs in container that contain the phrase "Error in Helpdesk"
 resource "aws_cloudwatch_log_metric_filter" "error" {
+  count = local.create_blue_green ? 0 : 1
+
   name           = "jitbit-application-error"
   pattern        = "Error in Helpdesk"
-  log_group_name = aws_cloudwatch_log_group.app_logs.name
+  log_group_name = aws_cloudwatch_log_group.app_logs[0].name
 
   metric_transformation {
     name          = "ErrorCount"
@@ -21,9 +44,90 @@ resource "aws_cloudwatch_log_metric_filter" "error" {
   }
 }
 
+resource "aws_cloudwatch_log_group" "app_logs_blue" {
+  #checkov:skip=CKV_AWS_338: "Logs required for 30 days"
+  count = local.create_blue_green ? 1 : 0
+
+  name              = "delius-jitbit-blue-ecs"
+  retention_in_days = 30
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
+
+  tags = local.tags
+}
+
+resource "aws_cloudwatch_log_group" "app_logs_green" {
+  #checkov:skip=CKV_AWS_338: "Logs required for 30 days"
+  count = local.create_blue_green ? 1 : 0
+
+  name              = "delius-jitbit-green-ecs"
+  retention_in_days = 30
+  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
+
+  tags = local.tags
+}
+
+# Log metric filter for blue error logs
+resource "aws_cloudwatch_log_metric_filter" "error_blue" {
+  count = local.create_blue_green ? 1 : 0
+
+  name           = "jitbit-application-error-blue"
+  pattern        = "Error in Helpdesk"
+  log_group_name = aws_cloudwatch_log_group.app_logs_blue[0].name
+
+  metric_transformation {
+    name          = "ErrorCount"
+    namespace     = "JitbitMetrics"
+    value         = "1"
+    default_value = "0"
+
+    dimensions = {
+      Service = "${local.service_name}-blue"
+    }
+  }
+}
+
+# Log metric filter for green error logs
+resource "aws_cloudwatch_log_metric_filter" "error_green" {
+  count = local.create_blue_green ? 1 : 0
+
+  name           = "jitbit-application-error-green"
+  pattern        = "Error in Helpdesk"
+  log_group_name = aws_cloudwatch_log_group.app_logs_green[0].name
+
+  metric_transformation {
+    name          = "ErrorCount"
+    namespace     = "JitbitMetrics"
+    value         = "1"
+    default_value = "0"
+
+    dimensions = {
+      Service = "${local.service_name}-green"
+    }
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "jitbit_high_error_volume" {
+  count = local.create_blue_green ? 0 : 1
+
   alarm_name          = "jitbit-high-error-count"
-  alarm_description   = "Triggers alarm if there are more than 10 errors for 2 consecitive periods"
+  alarm_description   = "Triggers alarm if there are more than 10 errors for 2 consecutive periods"
+  namespace           = "JitbitMetrics"
+  metric_name         = "ErrorCount"
+  statistic           = "Sum"
+  period              = "300"
+  evaluation_periods  = "2" # number of periods over which CloudWatch evaluates the metric data
+  alarm_actions       = [aws_sns_topic.jitbit_alerting.arn]
+  ok_actions          = [aws_sns_topic.jitbit_alerting.arn]
+  threshold           = "10"
+  treat_missing_data  = "missing"
+  comparison_operator = "GreaterThanThreshold"
+}
+
+resource "aws_cloudwatch_metric_alarm" "jitbit_high_error_volume_blue" {
+  count = local.create_blue_green ? 1 : 0
+
+  alarm_name          = "jitbit-high-error-count-blue"
+  alarm_description   = "Triggers alarm if there are more than 10 errors for 2 consecutive periods"
   namespace           = "JitbitMetrics"
   metric_name         = "ErrorCount"
   statistic           = "Sum"
--- terraform/environments/delius-jitbit/monitoring_dashboard.tf
+++ terraform/environments/delius-jitbit/monitoring_dashboard.tf
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_dashboard" "jitbit" {
       account_id         = data.aws_caller_identity.current.account_id
       environment        = local.environment
       app_name           = local.application_name
-      app_log_group_name = aws_cloudwatch_log_group.app_logs.name
+      app_log_group_name = aws_cloudwatch_log_group.app_logs[0].name
       load_balancer_arn  = aws_lb.external.arn_suffix
       target_group_arn   = aws_lb_target_group.target_group_fargate[0].arn_suffix
       efs_id             = aws_efs_file_system.lucene.id
@@ -16,37 +16,21 @@ resource "aws_cloudwatch_dashboard" "jitbit" {
   )
 }
 
-resource "aws_cloudwatch_dashboard" "jitbit_blue" {
+resource "aws_cloudwatch_dashboard" "jitbit_blue_green" {
   count = local.create_blue_green ? 1 : 0
 
-  dashboard_name = "${local.application_name}-blue"
+  dashboard_name = local.application_name
   dashboard_body = templatefile(
-    "${path.module}/templates/dashboard.json",
+    "${path.module}/templates/dashboard-blue-green.json",
     {
       account_id         = data.aws_caller_identity.current.account_id
       environment        = local.environment
       app_name           = local.application_name
-      app_log_group_name = aws_cloudwatch_log_group.app_logs.name
+      app_log_group_name_blue = aws_cloudwatch_log_group.app_logs_blue[0].name
+      app_log_group_name_green = aws_cloudwatch_log_group.app_logs_green[0].name
       load_balancer_arn  = aws_lb.external.arn_suffix
-      target_group_arn   = aws_lb_target_group.target_group_fargate_blue[0].arn_suffix
-      efs_id             = aws_efs_file_system.lucene.id
-    }
-  )
-}
-
-resource "aws_cloudwatch_dashboard" "jitbit_green" {
-  count = local.create_blue_green ? 1 : 0
-
-  dashboard_name = "${local.application_name}-green"
-  dashboard_body = templatefile(
-    "${path.module}/templates/dashboard.json",
-    {
-      account_id         = data.aws_caller_identity.current.account_id
-      environment        = local.environment
-      app_name           = local.application_name
-      app_log_group_name = aws_cloudwatch_log_group.app_logs.name
-      load_balancer_arn  = aws_lb.external.arn_suffix
-      target_group_arn   = aws_lb_target_group.target_group_fargate_green[0].arn_suffix
+      target_group_arn_blue   = aws_lb_target_group.target_group_fargate_blue[0].arn_suffix
+      target_group_arn_green   = aws_lb_target_group.target_group_fargate_green[0].arn_suffix
       efs_id             = aws_efs_file_system.lucene.id
     }
   )
