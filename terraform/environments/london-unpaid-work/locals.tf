80bb30f93a10c318b0c22fbe573624ad7f90e55a
ghill26 <George.Hill2@justice.gov.uk>
2026-08-21 15:43:13 +0100

    PLAT-5: Initial configuration for London Unpaid Work (https://dsdmoj.atlassian.net/browse/PLAT-136)

--- terraform/environments/london-unpaid-work/locals.tf
+++ terraform/environments/london-unpaid-work/locals.tf
@@ -1,1 +1,18 @@
 #### This file can be used to store locals specific to the member account ####
+locals {
+  baseline_presets_all_environments = {
+    options = {
+      enable_ec2_cloud_watch_agent                = true
+      enable_ec2_oracle_enterprise_managed_server = true
+      enable_ec2_security_groups                  = true
+      enable_ec2_self_provision                   = true
+      enable_ec2_session_manager_cloudwatch_logs  = true
+      enable_ec2_ssm_agent_update                 = true
+      enable_ec2_user_keypair                     = true
+      enable_image_builder                        = true
+      enable_s3_bucket                            = true
+      enable_ssm_command_monitoring               = true
+      s3_iam_policies = ["EC2S3BucketWriteAndDeleteAccessPolicy"]
+    }
+  }
+}
