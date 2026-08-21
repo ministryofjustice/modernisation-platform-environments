83f9f3c717c5aeea8cd7935aed29b7b177abb197
matt heery <matt.heery@justice.gov.uk>
2026-08-21 15:27:06 +0100

    fix: dns

--- terraform/environments/electronic-monitoring-data/lambdas_sg.tf
+++ terraform/environments/electronic-monitoring-data/lambdas_sg.tf
@@ -150,3 +150,13 @@ resource "aws_security_group_rule" "cloud_platform_access" {
   security_group_id = aws_security_group.lambda_cp_sg.id
 }
 
+
+resource "aws_security_group_rule" "dns_resolution" {
+  type              = "egress"
+  description       = "allow dns resolution"
+  from_port         = 53
+  to_port           = 53
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lambda_cp_sg.id
+}
