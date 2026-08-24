204b1226f2c5d886b2b92b6895236be50ed3571c
Harsh Vasudev <harsh.vasudev@justice.gov.uk>
2026-08-24 17:00:40 +0100

    fix: allow scoped deployment branch OIDC subject

--- terraform/environments/integration-hub/downstream-mock-api/modules/service/app-deploy.tf
+++ terraform/environments/integration-hub/downstream-mock-api/modules/service/app-deploy.tf
@@ -18,7 +18,11 @@ data "aws_iam_policy_document" "app_deploy_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:ministryofjustice/integration-hub-downstream-mock-api:environment:${local.resource_application_name}-${local.environment}*"]
+      values = [
+        "repo:ministryofjustice/integration-hub-downstream-mock-api:environment:${local.resource_application_name}-${local.environment}*",
+        "repo:ministryofjustice/integration-hub-downstream-mock-api:ref:refs/heads/main",
+        "repo:ministryofjustice/integration-hub-downstream-mock-api:ref:refs/heads/aws-ecs-deployment"
+      ]
     }
   }
 }
