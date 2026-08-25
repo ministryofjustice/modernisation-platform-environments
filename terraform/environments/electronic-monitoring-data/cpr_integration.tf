36b388efeec040c03b1132d52e8e6b31a851ee83
matt heery <matt.heery@justice.gov.uk>
2026-08-25 15:41:06 +0100

    fix: update for s3 push

--- terraform/environments/electronic-monitoring-data/cpr_integration.tf
+++ terraform/environments/electronic-monitoring-data/cpr_integration.tf
@@ -40,68 +40,12 @@ data "aws_iam_policy_document" "cpr_integration" {
   statement {
     effect = "Allow"
     actions = [
-      "glue:GetDatabases",
-      "glue:GetDatabase",
-      "glue:GetTables",
-      "glue:GetTable",
-      "glue:CreateDatabase",
+      "s3:PutObject",
+      "s3:GetObjectAttributes",
+      "s3:GetObject",
+      "s3:DeleteObject"    
     ]
-    resources = [
-      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:catalog",
-    ]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "glue:GetDatabase",
-      "glue:GetTables",
-      "glue:GetTable",
-      "glue:CreateDatabase",
-    ]
-    resources = [
-      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/person_record${local.db_suffix}",
-      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/intermediate_fms${local.dbt_suffix}",
-    ]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "glue:GetTables",
-      "glue:GetTable",
-      "glue:GetPartition",
-      "glue:GetPartitions",
-    ]
-    resources = [
-      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:table/person_record${local.db_suffix}/*",
-      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:table/intermediate_fms${local.dbt_suffix}/*",
-
-    ]
-  }
-  statement {
-    actions = [
-      "athena:GetDataCatalog",
-      "athena:GetQueryExecution",
-      "athena:GetQueryResults",
-      "athena:GetWorkGroup",
-      "athena:StartQueryExecution",
-      "athena:StopQueryExecution",
-      "athena:CreatePreparedStatement",
-      "athena:UpdatePreparedStatement",
-      "athena:GetPreparedStatement",
-      "athena:ListPreparedStatements",
-      "athena:DeletePreparedStatement",
-    ]
-    resources = [
-      "arn:aws:athena:${data.aws_region.current.name}:${local.env_account_id}:*/*"
-    ]
-  }
-  statement {
-    actions   = ["athena:ListWorkGroups"]
-    resources = ["*"]
-  }
-  statement {
-    actions   = ["lakeformation:GetDataAccess"]
-    resources = ["*"]
+    resources = ["${module.s3-raw-formatted-data-bucket.bucket.arn}/staging/"]
   }
   statement {
     actions = [
@@ -110,18 +54,9 @@ data "aws_iam_policy_document" "cpr_integration" {
       "s3:ListBucketMultipartUploads",
       "s3:ListMultipartUploadParts"
     ]
-    resources = [module.s3-athena-bucket.bucket.arn]
-  }
-  statement {
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads",
-      "s3:ListMultipartUploadParts"
+    resources = [
+      module.s3-raw-formatted-data-bucket.bucket.arn,
     ]
-    resources = ["${module.s3-athena-bucket.bucket.arn}/*"]
   }
 }
 
@@ -147,45 +82,4 @@ resource "aws_iam_policy" "emd_cpr_integration_policy" {
 resource "aws_iam_role_policy_attachment" "emd_cpr_integration_permissions" {
   policy_arn = aws_iam_policy.emd_cpr_integration_policy.arn
   role       = module.emd_cpr_integration_role.iam_role_name
-}
-
-resource "aws_lakeformation_permissions" "cpr_integration_int_fms" {
-  principal   = module.emd_cpr_integration_role.iam_role_arn
-  permissions = ["DESCRIBE"]
-  database {
-    name = "intermediate_fms${local.dbt_suffix}"
-  }
-}
-
-resource "aws_lakeformation_permissions" "cpr_integration_int_fms_tables" {
-  principal   = module.emd_cpr_integration_role.iam_role_arn
-  permissions = ["SELECT", "DESCRIBE"]
-  table {
-    database_name = "intermediate_fms${local.dbt_suffix}"
-    name          = "dd_device_wearer_current"
-  }
-}
-
-resource "aws_lakeformation_permissions" "cpr_integration_db" {
-  principal   = module.emd_cpr_integration_role.iam_role_arn
-  permissions = ["DESCRIBE", "CREATE_TABLE"]
-  database {
-    name = "person_record${local.db_suffix}"
-  }
-}
-
-resource "aws_lakeformation_permissions" "cpr_integration_db_tables" {
-  principal   = module.emd_cpr_integration_role.iam_role_arn
-  permissions = ["SELECT", "DESCRIBE"]
-  table {
-    database_name = "person_record${local.db_suffix}"
-    wildcard      = true
-  }
-}
-
-
-resource "aws_lakeformation_permissions" "cpr_integration_create_dbs" {
-  principal   = module.emd_cpr_integration_role.iam_role_arn
-  permissions = ["CREATE_DATABASE"]
-  catalog_resource = true
 }
