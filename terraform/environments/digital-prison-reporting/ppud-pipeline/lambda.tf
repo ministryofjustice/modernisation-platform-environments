7e40899f80f9e8f8330e7b4e8169af73681f3aec
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-08-25 16:35:06 +0100

    updated lambda again

--- terraform/environments/digital-prison-reporting/ppud-pipeline/lambda.tf
+++ terraform/environments/digital-prison-reporting/ppud-pipeline/lambda.tf
@@ -92,7 +92,7 @@ resource "aws_s3_bucket_notification" "ppud_land_bucket" {
 }
 
 data "aws_iam_policy_document" "check_recent_file" {
-  count         = local.is-test ? 0 : 1
+  count         = local.is-development ? 1 : 0
   statement {
     // Allow the lambda to list objects from the replication destination S3 bucket
     actions = [
@@ -128,7 +128,7 @@ data "aws_iam_policy_document" "check_recent_file" {
 }
 
 module "check_recent_file" {
-  count         = local.is-test ? 0 : 1
+  count         = local.is-development ? 1 : 0
 
   # Commit hash for v8.8.1
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-lambda?ref=23d00f7daef40091e87ed2f9dc5d8532e9d2cc22"
@@ -166,7 +166,7 @@ module "check_recent_file" {
 }
 
 resource "aws_lambda_permission" "allow_eventbridge_check_recent_file" {
-  count         = local.is-test ? 0 : 1
+  count         = local.is-development ? 1 : 0
   statement_id  = "AllowExecutionFromEventBridgeCheckRecentFile"
   action        = "lambda:InvokeFunction"
   function_name = module.check_recent_file[0].lambda_function_arn
@@ -175,14 +175,14 @@ resource "aws_lambda_permission" "allow_eventbridge_check_recent_file" {
 }
 
 resource "aws_cloudwatch_event_rule" "check_recent_file_daily" {
-  count               = local.is-test ? 0 : 1
+  count         = local.is-development ? 1 : 0
   name                = "${local.component_name}-check-recent-file-daily"
   description         = "Invoke recent-file checker daily at 15:15 UTC"
   schedule_expression = local.cron_schedule
 }
 
 resource "aws_cloudwatch_event_target" "check_recent_file_daily" {
-  count     = local.is-test ? 0 : 1
+  count         = local.is-development ? 1 : 0
   rule      = aws_cloudwatch_event_rule.check_recent_file_daily[0].name
   target_id = "check-recent-file-lambda"
   arn       = module.check_recent_file[0].lambda_function_arn
