fdba417e8498dc670ac4e3e1b016cf5a4dc4b7f3
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-08-26 15:31:29 +0100

    removed jumprole and added iam user

---
+++ terraform/environments/data-factory-corporate/people/iam_user.tf
@@ -0,0 +1,58 @@
+
+variable "user_name" {
+  type    = string
+  default = "datafactory-user"
+}
+
+resource "aws_iam_user" "datafactory_user" {
+  name = "${var.user_name}-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_iam_user_policy" "assume_external_role" {
+  name = "assume-datafactory-dev-role"
+  user = aws_iam_user.datafactory_user.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "sts:AssumeRole"
+        ]
+
+        Resource = "arn:aws:iam::${data.aws_secretsmanager_secret_version.external_account_id.secret_string}:role/datafactory_dev_assume_role"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_user_login_profile" "datafactory_user" {
+  user                    = aws_iam_user.datafactory_user.name
+  password_reset_required = false
+}
+
+# Create programmatic access credentials
+resource "aws_iam_access_key" "datafactory_user" {
+  user = aws_iam_user.datafactory_user.name
+}
+
+# Create secret to store credentials
+resource "aws_secretsmanager_secret" "datafactory_user_credentials" {
+  name = "datafactory/${var.user_name}/credentials"
+}
+
+
+resource "aws_secretsmanager_secret_version" "datafactory_user_credentials" {
+  secret_id = aws_secretsmanager_secret.datafactory_user_credentials.id
+
+  secret_string = jsonencode({
+    username          = aws_iam_user.datafactory_user.name
+    password          = aws_iam_user_login_profile.datafactory_user.password
+    access_key_id     = aws_iam_access_key.datafactory_user.id
+    secret_access_key = aws_iam_access_key.datafactory_user.secret
+  })
+}
