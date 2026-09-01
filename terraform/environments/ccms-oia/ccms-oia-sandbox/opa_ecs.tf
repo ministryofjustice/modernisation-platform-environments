9547452b3240db640b33e8ca127bf0347ce23b79
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-09-01 17:11:46 +0100

    fix: lifecycle ignore on task definition disabled

--- terraform/environments/ccms-oia/ccms-oia-sandbox/opa_ecs.tf
+++ terraform/environments/ccms-oia/ccms-oia-sandbox/opa_ecs.tf
@@ -89,11 +89,11 @@ resource "aws_ecs_service" "opahub" {
 
   health_check_grace_period_seconds = 120
 
-  lifecycle {
-    ignore_changes = [
-      task_definition
-    ]
-  }
+  # lifecycle {
+  #   ignore_changes = [
+  #     task_definition
+  #   ]
+  # }
 
   ordered_placement_strategy {
     field = "attribute:ecs.availability-zone"
