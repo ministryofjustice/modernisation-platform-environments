2afdd8e6e7f01d5ce78bd1e8841a426aa90d6d6a
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-09-01 16:22:42 +0000

    Refactor configuration management by removing application_variables.json and introducing global.yml for S3 bucket prefix
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/developer-experience/github-management/locals.tf
+++ terraform/environments/developer-experience/github-management/locals.tf
@@ -1,3 +1,4 @@
 locals {
-  bucket_name = "mojdevx-${local.environment}-${local.component_name}"
+  global_config = yamldecode(file("${path.module}/../configuration/global.yml"))
+  bucket_name   = "${local.global_config.s3_bucket_prefix}-${local.environment}-${local.component_name}"
 }
