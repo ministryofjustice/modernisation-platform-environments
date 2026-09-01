3055a358a4bc4655c3cd8ecb051a4506f146ca24
SahidKhan89 <sahid.khan@digital.justice.gov.uk>
2026-09-01 14:28:40 +0100

    CC-4838: Provision GH runner for OIA

---
+++ terraform/environments/ccms-feasibility/ccms-oia/github-runner-ec2.tf
@@ -0,0 +1,24 @@
+module "gh_runner" {
+  # https://github.com/ministryofjustice/laa-ccms-terraform-modules/commit/b484555
+  source = "github.com/ministryofjustice/laa-ccms-terraform-modules//modules/ec2?ref=b484555"
+
+  name                  = lower(format("github-runner-%s", local.environment))
+  instance_profile_name = aws_iam_instance_profile.github_runner_instance_profile.name
+
+  instance_type      = local.application_data.accounts[local.environment].ec2_instance_type_gh_runner
+  ami_id             = local.application_data.accounts[local.environment].gh_runner_ami_id
+  subnet_id          = data.aws_subnet.private_subnets_a.id
+  security_group_ids = [aws_security_group.ec2_sg_gh_runner.id]
+  monitoring         = true
+  ebs_optimized      = true
+
+  user_data = base64encode(templatefile("./templates/user_data_gh_runner.sh", {
+    hostname = "github-runner"
+  }))
+
+  tags = merge(local.tags, {
+    instance-role       = local.application_data.accounts[local.environment].instance_role_gh_runner
+    instance-scheduling = "skip-scheduling"
+    backup              = "true"
+  })
+}
---
+++ terraform/environments/ccms-feasibility/ccms-oia/github-runner-sg.tf
@@ -0,0 +1,22 @@
+# Security Group for GitHub Runner Server
+resource "aws_security_group" "ec2_sg_gh_runner" {
+  name        = "ec2_sg_clamav"
+  description = "Security Group for GitHub Runner Server"
+  vpc_id      = data.aws_vpc.shared.id
+  tags = merge(local.tags,
+    { Name = lower(format("clamav-%s-sg", local.environment)) }
+  )
+}
+
+# INGRESS Rules - None
+
+# EGRESS Rules
+
+resource "aws_vpc_security_group_egress_rule" "egress_traffic_gh_runner_443" {
+  security_group_id = aws_security_group.ec2_sg_gh_runner.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+  description       = "Allow outbound HTTPS traffic"
+}
