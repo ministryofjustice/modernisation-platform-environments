57a9a0322af5f00878dddc216378e0ba1b44a63c
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-09-02 16:54:39 +0100

    added oia ec2 name

--- terraform/environments/ccms-oia/oia_sg.tf
+++ terraform/environments/ccms-oia/oia_sg.tf
@@ -83,7 +83,7 @@ resource "aws_vpc_security_group_egress_rule" "cluster_ec2_egress_vpce" {
   cidr_ipv4         = each.value
 }
 
-resource "aws_vpc_security_group_egress_rule" "cluster_ec2_egress_s3" {
+resource "aws_vpc_security_group_egress_rule" "cluster_oia_ec2_egress_s3" {
   security_group_id = aws_security_group.cluster_ec2.id
   description       = "Allow S3 access via gateway endpoint (prefix list)"
   ip_protocol       = "tcp"
