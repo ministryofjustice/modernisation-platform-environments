0421b802ec9b38acb394673f51900b71b49952d8
Joseph Phillips <joseph.phillips@justice.gov.uk>
2026-09-03 11:15:13 +0100

    attached role to access bucket

--- terraform/environments/digital-prison-reporting/ppud-pipeline/locals.tf
+++ terraform/environments/digital-prison-reporting/ppud-pipeline/locals.tf
@@ -3,5 +3,6 @@ locals {
   cron_schedule          = local.is-test ? "rate(1 day)" : local.application_data.accounts[local.environment].cron_schedule
   short_name_environment = local.is-test ? "test" : local.application_data.accounts[local.environment].short_name_environment
   source_bucket_role_arn = local.is-test ? "arn" : "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-engineering-production"]}:role/${local.application_data.accounts[local.environment].source_bucket_role_name}"
+  s3_read_write_policy   = "${local.short_name}_s3_read_write_policy"
 
 }
