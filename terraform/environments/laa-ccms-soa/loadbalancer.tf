725440cd88ff619c59c3c9949cc9d144b42c572d
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-09-03 11:13:46 +0100

    uncomments and updated the docker image tag and brining up

--- terraform/environments/laa-ccms-soa/loadbalancer.tf
+++ terraform/environments/laa-ccms-soa/loadbalancer.tf
@@ -22,52 +22,52 @@ resource "aws_lb" "admin" {
   depends_on = [module.s3-bucket-logging]
 }
 
-# resource "aws_lb_target_group" "admin_https" {
-#   name                 = "${local.application_data.accounts[local.environment].app_name}-admin-target-group"
-#   port                 = 443
-#   protocol             = "TLS"
-#   vpc_id               = data.aws_vpc.shared.id
-#   target_type          = "ip"
-#   deregistration_delay = 30
+resource "aws_lb_target_group" "admin_https" {
+  name                 = "${local.application_data.accounts[local.environment].app_name}-admin-target-group"
+  port                 = 443
+  protocol             = "TLS"
+  vpc_id               = data.aws_vpc.shared.id
+  target_type          = "ip"
+  deregistration_delay = 30
 
-#   health_check {
+  health_check {
 
-#     enabled             = true
-#     interval            = 30
-#     path                = "/weblogic/ready"
-#     port                = local.application_data.accounts[local.environment].admin_ssl_port
-#     protocol            = "HTTPS"
-#     timeout             = 5
-#     healthy_threshold   = 3
-#     unhealthy_threshold = 3
-#     matcher             = "200"
-#   }
-# }
+    enabled             = true
+    interval            = 30
+    path                = "/weblogic/ready"
+    port                = local.application_data.accounts[local.environment].admin_ssl_port
+    protocol            = "HTTPS"
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    matcher             = "200"
+  }
+}
 
-# resource "aws_lb_listener" "admin443" {
-#   load_balancer_arn = aws_lb.admin.id
-#   port              = 443
-#   protocol          = "TLS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = aws_acm_certificate_validation.soa.certificate_arn
-#   default_action {
-#     target_group_arn = aws_lb_target_group.admin_https.id
-#     type             = "forward"
-#   }
-# }
+resource "aws_lb_listener" "admin443" {
+  load_balancer_arn = aws_lb.admin.id
+  port              = 443
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = aws_acm_certificate_validation.soa.certificate_arn
+  default_action {
+    target_group_arn = aws_lb_target_group.admin_https.id
+    type             = "forward"
+  }
+}
 
-# resource "aws_lb_listener" "admin_ssl_port" {
-#   load_balancer_arn = aws_lb.admin.id
-#   port              = local.application_data.accounts[local.environment].admin_ssl_port
-#   protocol          = "TLS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = aws_acm_certificate_validation.soa.certificate_arn
+resource "aws_lb_listener" "admin_ssl_port" {
+  load_balancer_arn = aws_lb.admin.id
+  port              = local.application_data.accounts[local.environment].admin_ssl_port
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = aws_acm_certificate_validation.soa.certificate_arn
 
-#   default_action {
-#     target_group_arn = aws_lb_target_group.admin_https.id
-#     type             = "forward"
-#   }
-# }
+  default_action {
+    target_group_arn = aws_lb_target_group.admin_https.id
+    type             = "forward"
+  }
+}
 
 #--Managed
 resource "aws_lb" "managed" {
@@ -91,48 +91,48 @@ resource "aws_lb" "managed" {
   depends_on = [module.s3-bucket-logging]
 }
 
-# resource "aws_lb_target_group" "managed_https" {
-#   name                 = "${local.application_data.accounts[local.environment].app_name}-managed-target-group"
-#   port                 = 443
-#   protocol             = "TLS"
-#   vpc_id               = data.aws_vpc.shared.id
-#   target_type          = "ip"
-#   deregistration_delay = 30
+resource "aws_lb_target_group" "managed_https" {
+  name                 = "${local.application_data.accounts[local.environment].app_name}-managed-target-group"
+  port                 = 443
+  protocol             = "TLS"
+  vpc_id               = data.aws_vpc.shared.id
+  target_type          = "ip"
+  deregistration_delay = 30
 
-#   health_check {
-#     enabled             = true
-#     interval            = 30
-#     path                = "/weblogic/ready"
-#     port                = local.application_data.accounts[local.environment].admin_ssl_port
-#     protocol            = "HTTPS"
-#     timeout             = 5
-#     healthy_threshold   = 3
-#     unhealthy_threshold = 3
-#     matcher             = "200"
-#   }
-# }
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/weblogic/ready"
+    port                = local.application_data.accounts[local.environment].admin_ssl_port
+    protocol            = "HTTPS"
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    matcher             = "200"
+  }
+}
 
-# resource "aws_lb_listener" "managed443" {
-#   load_balancer_arn = aws_lb.managed.id
-#   port              = 443
-#   protocol          = "TLS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = aws_acm_certificate_validation.soa.certificate_arn
-#   default_action {
-#     target_group_arn = aws_lb_target_group.managed_https.id
-#     type             = "forward"
-#   }
-# }
+resource "aws_lb_listener" "managed443" {
+  load_balancer_arn = aws_lb.managed.id
+  port              = 443
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = aws_acm_certificate_validation.soa.certificate_arn
+  default_action {
+    target_group_arn = aws_lb_target_group.managed_https.id
+    type             = "forward"
+  }
+}
 
-# resource "aws_lb_listener" "managed_ssl_port" {
-#   load_balancer_arn = aws_lb.managed.id
-#   port              = local.application_data.accounts[local.environment].managed_ssl_port
-#   protocol          = "TLS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = aws_acm_certificate_validation.soa.certificate_arn
+resource "aws_lb_listener" "managed_ssl_port" {
+  load_balancer_arn = aws_lb.managed.id
+  port              = local.application_data.accounts[local.environment].managed_ssl_port
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = aws_acm_certificate_validation.soa.certificate_arn
 
-#   default_action {
-#     target_group_arn = aws_lb_target_group.managed_https.id
-#     type             = "forward"
-#   }
-# }
+  default_action {
+    target_group_arn = aws_lb_target_group.managed_https.id
+    type             = "forward"
+  }
+}
