bab63f3e7acf5dce62139d4f0fd8607b50d2ad34
ghill26 <George.Hill2@justice.gov.uk>
2026-09-04 10:02:01 +0100

    Platops-27: Update REL branch to be main

--- terraform/environments/nomis/locals_development.tf
+++ terraform/environments/nomis/locals_development.tf
@@ -443,7 +443,7 @@ locals {
         })
         user_data_cloud_init = merge(local.ec2_instances.web_12.user_data_cloud_init, {
           args = merge(local.ec2_instances.web_12.user_data_cloud_init.args, {
-            branch = "platops-27"
+            branch = "main"
           })
         })
         tags = merge(local.ec2_instances.web_12.tags, {
