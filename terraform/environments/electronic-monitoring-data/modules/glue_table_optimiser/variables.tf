8186357b176f1d9f58be6f777306280cb575dba7
gwionap <32736223+gwionap@users.noreply.github.com>
2026-09-07 11:39:46 +0100

    Add compaction configuration options. Assumes binpack strategy.

--- terraform/environments/electronic-monitoring-data/modules/glue_table_optimiser/variables.tf
+++ terraform/environments/electronic-monitoring-data/modules/glue_table_optimiser/variables.tf
@@ -8,6 +8,8 @@ variable "optimizer_bucket_id" {
 
 variable "table_optimizer_defaults" {
   type = object({
+    min_input_files                        = number
+    delete_file_threshold                  = number
     snapshot_retention_period_in_days      = number
     number_of_snapshots_to_retain          = number
     orphan_file_retention_period_in_days   = number
@@ -16,6 +18,8 @@ variable "table_optimizer_defaults" {
   })
 
   default = {
+    min_input_files                        = 100
+    delete_file_threshold                  = 1
     snapshot_retention_period_in_days      = 7
     number_of_snapshots_to_retain          = 3
     orphan_file_retention_period_in_days   = 7
