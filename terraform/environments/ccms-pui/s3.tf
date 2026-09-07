c09574d9bd301c98f029e3e72eb9b2bca9c4ce04
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-09-07 11:24:31 +0100

    added manage_log_bucket_policy

--- terraform/environments/ccms-pui/s3.tf
+++ terraform/environments/ccms-pui/s3.tf
@@ -10,6 +10,7 @@ module "s3_pui_docs" {
    log_bucket_arn  = module.s3-bucket-logging.bucket.arn
    log_bucket_policy = aws_s3_bucket_policy.lb_access_logs.policy
      }
+    manage_log_bucket_policy = false
 
     log_prefix = "s3access/${local.application_name}-docs-${local.environment}"
 
@@ -292,6 +293,7 @@ module "s3-bucket-shared" {
    log_bucket_arn  = module.s3-bucket-logging.bucket.arn
    log_bucket_policy = aws_s3_bucket_policy.lb_access_logs.policy
      }
+    manage_log_bucket_policy = false
 
   log_prefix = "s3access/${local.application_name}-${local.environment}-shared"
 
