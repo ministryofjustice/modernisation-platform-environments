3e73f2ba84234d13582b99195c3737bca9ef764e
ghill26 <George.Hill2@justice.gov.uk>
2026-09-07 16:29:25 +0100

    Platops-34: Add initial secrets

--- terraform/environments/london-unpaid-work/locals_development.tf
+++ terraform/environments/london-unpaid-work/locals_development.tf
@@ -1,7 +1,7 @@
 locals {
   baseline_presets_development = {
     options = {
-      enable_ec2_session_manager_cloudwatch_logs  = true
+      enable_ec2_session_manager_cloudwatch_logs = true
     }
   }
 
@@ -12,7 +12,11 @@ locals {
     }
 
     secretsmanager_secrets = {
-      "/london-unpaid-work-dev/london-unpaid-work/test" = merge(local.secretsmanager_secrets.london_unpaid_work_admin_credentials, local.secretsmanager_secrets.london_unpaid_work_test_credentials)
+      "/london-unpaid-work-dev/test"        = merge(local.secretsmanager_secrets.london_unpaid_work_admin_credentials, local.secretsmanager_secrets.london_unpaid_work_test_credentials)
+      "/london-unpaid-work-dev/application" = local.secretsmanager_secrets.london_unpaid_work_application_credentials
+      "/london-unpaid-work-dev/rds"         = local.secretsmanager_secrets.london_unpaid_work_rds_credentials
+      "/london-unpaid-work-dev/ses"         = local.secretsmanager_secrets.london_unpaid_work_ses_credentials
+      "/london-unpaid-work-dev/slack"       = local.secretsmanager_secrets.london_unpaid_work_slack_credentials
     }
 
     security_groups = local.security_groups
--- terraform/environments/london-unpaid-work/locals_secretsmanager.tf
+++ terraform/environments/london-unpaid-work/locals_secretsmanager.tf
@@ -2,14 +2,36 @@ locals {
   secretsmanager_secrets = {
     london_unpaid_work_admin_credentials = {
       secrets = {
-        london_unpaid_work_admin_password     = { description = "london-unpaid-work admin password" }
-        london_unpaid_work_admin_username     = { description = "london-unpaid-work admin username" }
+        admin_password = { description = "london-unpaid-work admin password" }
+        admin_username = { description = "london-unpaid-work admin username" }
       }
     }
     london_unpaid_work_test_credentials = {
       secrets = {
-        london_unpaid_work_test_password     = { description = "london-unpaid-work test password" }
-        london_unpaid_work_test_username     = { description = "london-unpaid-work test username" }
+        test_password = { description = "london-unpaid-work test password" }
+        test_username = { description = "london-unpaid-work test username" }
+      }
+    }
+    london_unpaid_work_application_credentials = {
+      secrets = {
+        encryption_key = { description = "london-unpaid-work application encryption key" }
+      }
+    }
+    london_unpaid_work_rds_credentials = {
+      secrets = {
+        app_password        = { description = "london-unpaid-work rds password" }
+        app_username        = { description = "london-unpaid-work rds username" }
+        rds_master_password = { description = "london-unpaid-work rds master password" }
+      }
+    }
+    london_unpaid_work_ses_credentials = {
+      secrets = {
+        ses_user_smtppassword = { description = "london-unpaid-work ses user smtp password" }
+      }
+    }
+    london_unpaid_work_slack_credentials = {
+      secrets = {
+        token = { description = "london-unpaid-work slack token" }
       }
     }
   }
