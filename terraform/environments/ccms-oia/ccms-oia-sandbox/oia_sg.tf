59323828576878c02bb3f704c20d3726af1b51a1
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-09-07 17:40:11 +0100

    fix: removing private subnet from connector

--- terraform/environments/ccms-oia/ccms-oia-sandbox/oia_sg.tf
+++ terraform/environments/ccms-oia/ccms-oia-sandbox/oia_sg.tf
@@ -160,14 +160,14 @@ resource "aws_vpc_security_group_egress_rule" "second_cluster_ec2_egress_vpce" {
   cidr_ipv4         = each.value
 }
 
-resource "aws_vpc_security_group_egress_rule" "second_cluster_ec2_egress_s3" {
-  security_group_id = aws_security_group.second_cluster_ec2.id
-  description       = "Allow S3 access via gateway endpoint (prefix list)"
-  ip_protocol       = "tcp"
-  from_port         = 443
-  to_port           = 443
-  prefix_list_id    = data.aws_prefix_list.s3.id
-}
+# resource "aws_vpc_security_group_egress_rule" "second_cluster_ec2_egress_s3" {
+#   security_group_id = aws_security_group.second_cluster_ec2.id
+#   description       = "Allow S3 access via gateway endpoint (prefix list)"
+#   ip_protocol       = "tcp"
+#   from_port         = 443
+#   to_port           = 443
+#   prefix_list_id    = data.aws_prefix_list.s3.id
+# }
 
 # resource "aws_vpc_security_group_egress_rule" "second_cluster_ec2_egress_443" {
 #   for_each          = toset([
--- terraform/environments/ccms-oia/ccms-oia-sandbox/service_adaptor_sg.tf
+++ terraform/environments/ccms-oia/ccms-oia-sandbox/service_adaptor_sg.tf
@@ -200,14 +200,14 @@ resource "aws_vpc_security_group_egress_rule" "ecs_tasks_adaptor_egress_vpce" {
   cidr_ipv4         = each.value
 }
 
-resource "aws_vpc_security_group_egress_rule" "ecs_tasks_adaptor_egress_s3" {
-  security_group_id = aws_security_group.ecs_tasks_adaptor.id
-  description       = "Allow S3 access via gateway endpoint (prefix list)"
-  ip_protocol       = "tcp"
-  from_port         = 443
-  to_port           = 443
-  prefix_list_id    = data.aws_prefix_list.s3.id
-}
+# resource "aws_vpc_security_group_egress_rule" "ecs_tasks_adaptor_egress_s3" {
+#   security_group_id = aws_security_group.ecs_tasks_adaptor.id
+#   description       = "Allow S3 access via gateway endpoint (prefix list)"
+#   ip_protocol       = "tcp"
+#   from_port         = 443
+#   to_port           = 443
+#   prefix_list_id    = data.aws_prefix_list.s3.id
+# }
 
 # resource "aws_vpc_security_group_egress_rule" "ecs_tasks_adaptor_egress_443" {
 #   for_each          = toset([
