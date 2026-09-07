c1a81604dc73ee15c1de27d7ec355f39d9f4450b
ghill26 <George.Hill2@justice.gov.uk>
2026-09-07 16:08:30 +0100

    Platops-34: Update LUW Test secrets

--- terraform/environments/london-unpaid-work/locals_secretsmanager.tf
+++ terraform/environments/london-unpaid-work/locals_secretsmanager.tf
@@ -3,7 +3,13 @@ locals {
     london_unpaid_work_admin_credentials = {
       secrets = {
         london_unpaid_work_admin_password     = { description = "london-unpaid-work admin password" }
-        london_unpaid_work_username_password  = { description = "london-unpaid-work admin username" }
+        london_unpaid_work_admin_username     = { description = "london-unpaid-work admin username" }
+      }
+    }
+    london_unpaid_work_test_credentials = {
+      secrets = {
+        london_unpaid_work_test_password     = { description = "london-unpaid-work test password" }
+        london_unpaid_work_test_username     = { description = "london-unpaid-work test username" }
       }
     }
   }
