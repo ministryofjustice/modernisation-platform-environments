68e0524c66b073a4bf8b628c0ad2a9210d465428
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-09-08 14:35:51 +0100

    commented to fix errors

--- terraform/environments/laa-ccms-soa/ecs_cluster.tf
+++ terraform/environments/laa-ccms-soa/ecs_cluster.tf
@@ -129,11 +129,11 @@ resource "aws_ecs_service" "admin" {
     subnets         = data.aws_subnets.shared-private.ids
   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.admin_https.id
-    container_name   = "${local.application_data.accounts[local.environment].app_name}-admin"
-    container_port   = local.application_data.accounts[local.environment].admin_ssl_port
-  }
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.admin_https.id
+  #   container_name   = "${local.application_data.accounts[local.environment].app_name}-admin"
+  #   container_port   = local.application_data.accounts[local.environment].admin_ssl_port
+  # }
 
   lifecycle {
     create_before_destroy = true
@@ -227,11 +227,11 @@ resource "aws_ecs_service" "managed" {
     subnets         = data.aws_subnets.shared-private.ids
   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.managed_https.id
-    container_name   = "${local.application_data.accounts[local.environment].app_name}-managed"
-    container_port   = local.application_data.accounts[local.environment].managed_ssl_port
-  }
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.managed_https.id
+  #   container_name   = "${local.application_data.accounts[local.environment].app_name}-managed"
+  #   container_port   = local.application_data.accounts[local.environment].managed_ssl_port
+  # }
 
   lifecycle {
     create_before_destroy = true
