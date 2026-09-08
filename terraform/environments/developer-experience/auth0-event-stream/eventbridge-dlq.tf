49ab2e7017a6abe74e81363647510747927dd365
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-09-08 12:52:58 +0000

    🫵 pin it
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/environments/developer-experience/auth0-event-stream/eventbridge-dlq.tf
+++ terraform/environments/developer-experience/auth0-event-stream/eventbridge-dlq.tf
@@ -1,7 +1,7 @@
 module "eventbridge_dlq" {
   count = local.is-production ? 1 : 0
 
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-sqs.git?ref=v5.2.2" # v5.2.2
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-sqs.git?ref=dd73a96c0155bc324dda5256f3e7a9ea2c710195" # v5.2.2
 
   name                    = "${local.component_name}-dlq"
   sqs_managed_sse_enabled = true
