f34ffa4515ed9f26e63755c3113d7191f66102f1
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-09-08 15:17:40 +0100

    lifecycle on secrtets

--- terraform/environments/laa-ccms-soa/secrets.tf
+++ terraform/environments/laa-ccms-soa/secrets.tf
@@ -52,6 +52,12 @@ data "aws_secretsmanager_secret_version" "trust_store_password" {
 resource "aws_secretsmanager_secret" "soa_secrets" {
   name        = "soa-secrets"
   description = "SOA Weblogic,EM Console for user weblogic, RDS Database Password for SOAPDB admin, PUI and other passwords in Key values"
+
+  lifecycle {
+    ignore_changes = [
+      name
+    ]
+  }
 }
 
 resource "aws_secretsmanager_secret_version" "ccms_soa_secrets_version" {
