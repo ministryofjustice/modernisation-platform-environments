3e9c66cc7d9a995b0e78b93e66f86d910892a6bd
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-09-08 12:03:20 +0100

    fix: ssl port added without lifecycle

--- terraform/environments/ccms-oia/ccms-oia-sandbox/service_adaptor_load_balancer.tf
+++ terraform/environments/ccms-oia/ccms-oia-sandbox/service_adaptor_load_balancer.tf
@@ -43,9 +43,9 @@ resource "aws_lb_target_group" "adaptor_target_group" {
     timeout             = 5
   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
   
   tags = merge(local.tags,
     { Name = lower(format("%s-tg", local.adaptor_app_name)) }
