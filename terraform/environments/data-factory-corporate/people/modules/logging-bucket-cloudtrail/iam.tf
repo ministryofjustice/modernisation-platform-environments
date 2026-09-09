1582612fd2cf6dff44eef2c331d4ebfc139942b2
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-09-09 19:25:06 +0100

    tags3

--- terraform/environments/data-factory-corporate/people/modules/logging-bucket-cloudtrail/iam.tf
+++ terraform/environments/data-factory-corporate/people/modules/logging-bucket-cloudtrail/iam.tf
@@ -86,8 +86,7 @@ data "aws_iam_policy_document" "cloudtrail_cloudwatch_logs" {
 resource "aws_iam_role" "cloudtrail_logs" {
   name               = "${local.cloudtrail_name}-cloudtrail-logs"
   assume_role_policy = data.aws_iam_policy_document.cloudtrail_assume_role.json
-
-  tags = local.common_tags
+  tags = var.tags
 }
 
 resource "aws_iam_role_policy" "cloudtrail_logs" {
