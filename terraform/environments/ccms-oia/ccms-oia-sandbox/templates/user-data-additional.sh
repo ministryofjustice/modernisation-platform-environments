b4797e1772fdf88e0bde007556befbf4156647fe
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-09-09 20:40:49 +0100

    fix: cleaning up efs id

--- terraform/environments/ccms-oia/ccms-oia-sandbox/templates/user-data-additional.sh
+++ terraform/environments/ccms-oia/ccms-oia-sandbox/templates/user-data-additional.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "ECS_CLUSTER=${cluster_name}" >> /etc/ecs/ecs.config
-echo 'ECS_VOLUME_PLUGIN_CAPABILITIES=["efsAuth"]' >> /etc/ecs/ecs.config
+# echo 'ECS_VOLUME_PLUGIN_CAPABILITIES=["efsAuth"]' >> /etc/ecs/ecs.config
 
 start ecs
 
