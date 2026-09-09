3264e1bb9b7a344037571fc6c8de8bd98d39059f
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-09-09 17:55:36 +0100

    added sns and expanded cloudtrail iam role

--- terraform/environments/data-factory-corporate/people/modules/logging-bucket-cloudtrail/iam.tf
+++ terraform/environments/data-factory-corporate/people/modules/logging-bucket-cloudtrail/iam.tf
@@ -53,3 +53,72 @@ data "aws_iam_policy_document" "cloudtrail_bucket" {
     }
   }
 }
+
+data "aws_iam_policy_document" "cloudtrail_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "cloudtrail_cloudwatch_logs" {
+  statement {
+    sid    = "WriteCloudTrailLogs"
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
+    ]
+  }
+}
+
+resource "aws_iam_role" "cloudtrail_logs" {
+  name               = "${local.cloudtrail_name}-cloudtrail-logs"
+  assume_role_policy = data.aws_iam_policy_document.cloudtrail_assume_role.json
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy" "cloudtrail_logs" {
+  name   = "${local.cloudtrail_name}-cloudtrail-logs"
+  role   = aws_iam_role.cloudtrail_logs.id
+  policy = data.aws_iam_policy_document.cloudtrail_cloudwatch_logs.json
+}
+
+data "aws_iam_policy_document" "cloudtrail_sns_publish" {
+  statement {
+    sid    = "AllowCloudTrailToPublishToSNS"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = ["sns:Publish"]
+
+    resources = [aws_sns_topic.cloudtrail.arn]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_cloudtrail.sherlock.arn]
+    }
+  }
+}
+
+resource "aws_sns_topic_policy" "cloudtrail" {
+  arn    = aws_sns_topic.cloudtrail.arn
+  policy = data.aws_iam_policy_document.cloudtrail_sns_publish.json
+}
