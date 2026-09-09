407b2329108c78937ab58e8794d60f83568978e9
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-09-09 19:16:45 +0100

    tags fix2

--- terraform/environments/data-factory-corporate/people/modules/logging-bucket-cloudtrail/locals.tf
+++ terraform/environments/data-factory-corporate/people/modules/logging-bucket-cloudtrail/locals.tf
@@ -4,10 +4,5 @@ locals {
   cloudtrail_name = "${var.bucket_prefix}-cloudtrail"
   cloudtrail_arn  = "arn:aws:cloudtrail:${data.aws_region.current.region}:${local.account_id}:trail/${local.cloudtrail_name}"
 
-  common_tags = merge(
-    var.tags,
-    {
-      ManagedBy = "Terraform"
-    }
-  )
+  common_tags = var.tags
 }
