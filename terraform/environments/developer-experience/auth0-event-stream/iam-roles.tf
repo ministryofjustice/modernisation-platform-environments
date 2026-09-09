97a680f174b1f56676a70e5a2bed079ddca7381e
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-09-09 16:42:37 +0000

    fix(auth0-event-stream): correct Firehose KMS permissions

--- terraform/environments/developer-experience/auth0-event-stream/iam-roles.tf
+++ terraform/environments/developer-experience/auth0-event-stream/iam-roles.tf
@@ -39,12 +39,16 @@ module "firehose_iam_role" {
     KMSAccess = {
       effect = "Allow"
       actions = [
-        "kms:CreateGrant",
         "kms:Decrypt",
         "kms:DescribeKey",
         "kms:Encrypt",
         "kms:GenerateDataKey",
       ]
+      resources = [module.destination_kms_key[0].key_arn]
+    }
+    KMSGrant = {
+      effect    = "Allow"
+      actions   = ["kms:CreateGrant"]
       resources = [module.destination_kms_key[0].key_arn]
       condition = [
         {
