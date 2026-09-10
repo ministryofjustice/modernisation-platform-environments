f1fc88aede509868fc622c60272f0e23ea5faa58
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-09-10 10:31:06 +0100

    fix: template updated

--- terraform/environments/ccms-oia/ccms-oia-sandbox/ec2.tf
+++ terraform/environments/ccms-oia/ccms-oia-sandbox/ec2.tf
@@ -7,7 +7,7 @@
 #   }
 # }
 
-data "template_file" "launch-template-main" {
+data "template_file" "launch-template-main-updated" {
   template = file("${path.module}/templates/user-data-mainone.sh")
   vars = {
     cluster_name       = "${local.first_cluster_name}"
@@ -53,7 +53,7 @@ resource "aws_launch_template" "ec2_launch_template_main" {
     }
   }
 
-  user_data = base64encode(data.template_file.launch-template-main.rendered)
+  user_data = base64encode(data.template_file.launch-template-main-updated.rendered)
 
   tag_specifications {
     resource_type = "instance"
