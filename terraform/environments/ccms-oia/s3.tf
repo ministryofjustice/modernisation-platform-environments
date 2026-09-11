9a5231b7cf07bfd61ae57701b623903d03a878eb
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-09-11 16:06:13 +0100

    changed the ref version

--- terraform/environments/ccms-oia/s3.tf
+++ terraform/environments/ccms-oia/s3.tf
@@ -102,7 +102,7 @@ module "s3_ccms_oia" {
 
 # S3 Bucket - Logging
 module "s3-bucket-logging" {
-  # v9.0.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/9facf9fc8f8b8e3f93ffbda822028534b9a75399
+  # v11.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/81230d03816f140ae912454815ec531d7cbe2c8e
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=81230d03816f140ae912454815ec531d7cbe2c8e"
 
   bucket_name        = local.logging_bucket_name
@@ -257,7 +257,7 @@ resource "aws_s3_bucket_policy" "lb_access_logs" {
 
 # S3 Bucket - Logging
 module "s3-bucket-shared" {
-  # v9.0.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/9facf9fc8f8b8e3f93ffbda822028534b9a75399
+  # v11.2.0 = https://github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket/commit/81230d03816f140ae912454815ec531d7cbe2c8e
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=81230d03816f140ae912454815ec531d7cbe2c8e"
 
   bucket_name        = "${local.application_name}-${local.environment}-shared"
