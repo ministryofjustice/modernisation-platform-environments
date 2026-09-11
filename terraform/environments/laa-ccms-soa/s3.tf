c767062b49639d181287fc635060abd0103acd5b
Ramya Manuneethi <ramya.manuneethi@justice.gov.uk>
2026-09-11 16:27:38 +0100

    changed source version

--- terraform/environments/laa-ccms-soa/s3.tf
+++ terraform/environments/laa-ccms-soa/s3.tf
@@ -5,7 +5,8 @@ module "s3-bucket-logging" {
 
   bucket_name        = local.logging_bucket_name
   versioning_enabled = true
-  #bucket_policy      = [aws_s3_bucket_policy.lb_access_logs.policy]
+  bucket_policy      = [aws_s3_bucket_policy.lb_access_logs.policy]
+  
 
   sse_algorithm  = "AES256"
   custom_kms_key = ""
