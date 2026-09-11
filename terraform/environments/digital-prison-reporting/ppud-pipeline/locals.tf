f9106b5dd12afe9d11682749279b759d88927e66
Joseph Phillips <joseph.phillips@justice.gov.uk>
2026-09-11 17:01:10 +0100

    added iam s3 bucket source name dependant on env

--- terraform/environments/digital-prison-reporting/ppud-pipeline/locals.tf
+++ terraform/environments/digital-prison-reporting/ppud-pipeline/locals.tf
@@ -4,5 +4,5 @@ locals {
   short_name_environment    = local.is-test ? "test" : local.application_data.accounts[local.environment].short_name_environment
   source_bucket_role_arn    = local.is-test ? "arn" : "arn:aws:iam::${local.environment_management.account_ids["analytical-platform-data-engineering-production"]}:role/${local.application_data.accounts[local.environment].source_bucket_role_name}"
   analytical_platform_share = can(local.application_data.accounts[local.environment].analytical_platform_share) ? { for share in local.application_data.accounts[local.environment].analytical_platform_share : share.target_account_name => share } : {}
-
+  ppud_replication_environment  = local.is-test ? "test" : local.application_data.accounts[local.environment].ppud_replication_environment
 }
