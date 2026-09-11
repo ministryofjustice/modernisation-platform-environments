656ac935218a5a3c41934daea4fc8a065aee5e3a
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-11 10:26:17 +0100

    chore: add provider versions and comment out unused locals

--- terraform/environments/mojfin/versions.tf
+++ terraform/environments/mojfin/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
     aws = {
       version = "~> 6.0"
       source  = "hashicorp/aws"
@@ -11,6 +15,10 @@ terraform {
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
     }
   }
   required_version = "~> 1.10"
