460af43579c39af75abfdcba0cd6697b05035596
Joseph Phillips <joseph.phillips@justice.gov.uk>
2026-09-11 15:30:27 +0100

    added replication destination for mp

--- terraform/environments/ppud/s3_replication.tf
+++ terraform/environments/ppud/s3_replication.tf
@@ -45,6 +45,13 @@ locals {
         expiration_days         = 6
         replication_destination = "arn:aws:s3:::mojap-data-engineering-production-ppud-preprod"
         replication_rule_id     = "ppud-database-replication-rule-uat"
+        additional_replication_rules = [
+          {
+            destination = "arn:aws:s3:::ppud-bak-replication-preproduction-${local.environment_management.account_ids["digital-prison-reporting-preproduction"]}-eu-west-2-an"
+            rule_id     = "ppud-database-replication-rule-uat-mp"
+            priority    = 2
+          }
+        ]
         iam_role_key            = "database_source_uat"
         ec2_account             = "ppud-preproduction"
       }
