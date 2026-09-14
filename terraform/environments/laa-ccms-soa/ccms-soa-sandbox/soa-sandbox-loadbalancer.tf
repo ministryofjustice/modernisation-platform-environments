1bbc5e1708c670596d477832a71e1df3c7dfcbf8
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-09-14 16:36:45 +0100

    removed the comments for ssl port and also testing block

--- terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-loadbalancer.tf
+++ terraform/environments/laa-ccms-soa/ccms-soa-sandbox/soa-sandbox-loadbalancer.tf
@@ -49,17 +49,17 @@ resource "aws_lb_target_group" "admin_https" {
   }
 }
 
-# resource "aws_lb_listener" "admin443" {
-#   load_balancer_arn = aws_lb.admin.id
-#   port              = 443
-#   protocol          = "TLS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = aws_acm_certificate_validation.soa-sandbox.certificate_arn
-#   default_action {
-#     target_group_arn = aws_lb_target_group.admin_https.id
-#     type             = "forward"
-#   }
-# }
+resource "aws_lb_listener" "admin443" {
+  load_balancer_arn = aws_lb.admin.id
+  port              = 443
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = aws_acm_certificate_validation.soa-sandbox.certificate_arn
+  default_action {
+    target_group_arn = aws_lb_target_group.admin_https.id
+    type             = "forward"
+  }
+}
 
 resource "aws_lb_listener" "admin_ssl_port" {
   load_balancer_arn = aws_lb.admin.id
@@ -72,19 +72,6 @@ resource "aws_lb_listener" "admin_ssl_port" {
     type             = "forward"
   }
 }
-
-#Temporary purpose for testing. It should be removed once the testing is done. 
-# resource "aws_lb_listener" "admin80" {
-#   load_balancer_arn = aws_lb.admin.id
-#   port              = 80 #--Don't know why HTTP is being listened, is this a redirect? Why? - Revist. AW
-#   protocol          = "TCP"
-
-#   default_action {
-#     target_group_arn = aws_lb_target_group.admin_https.id
-#     type             = "forward"
-#   }
-# }
-
 
 #--Managed
 resource "aws_lb" "managed" {
@@ -135,17 +122,17 @@ resource "aws_lb_target_group" "managed_https" {
   }
 }
 
-# resource "aws_lb_listener" "managed443" {
-#   load_balancer_arn = aws_lb.managed.id
-#   port              = 443
-#   protocol          = "TLS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = aws_acm_certificate_validation.soa-sandbox.certificate_arn
-#   default_action {
-#     target_group_arn = aws_lb_target_group.managed_https.id
-#     type             = "forward"
-#   }
-# }
+resource "aws_lb_listener" "managed443" {
+  load_balancer_arn = aws_lb.managed.id
+  port              = 443
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = aws_acm_certificate_validation.soa-sandbox.certificate_arn
+  default_action {
+    target_group_arn = aws_lb_target_group.managed_https.id
+    type             = "forward"
+  }
+}
 
 resource "aws_lb_listener" "managed_ssl_port" {
   load_balancer_arn = aws_lb.managed.id
@@ -158,15 +145,3 @@ resource "aws_lb_listener" "managed_ssl_port" {
     type             = "forward"
   }
 }
-
-# #Temporary purpose for testing. It should be removed once the testing is done.
-# resource "aws_lb_listener" "managed80" {
-#   load_balancer_arn = aws_lb.managed.id
-#   port              = 80 #--Don't know why HTTP is being listened, is this a redirect? Why? - Revist. AW
-#   protocol          = "TCP"
-
-#   default_action {
-#     target_group_arn = aws_lb_target_group.managed_https.id
-#     type             = "forward"
-#   }
-# }
