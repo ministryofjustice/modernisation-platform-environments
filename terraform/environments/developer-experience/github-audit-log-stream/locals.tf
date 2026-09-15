089413a3388fb67101595944573b3dbf4acb5fd5
Sandhya Buddharaju <sandhya.buddharaju@justice.gov.uk>
2026-09-15 17:44:10 +0100

    feat(github-audit-log-stream): add Cortex XSIAM WIF delivery

--- terraform/environments/developer-experience/github-audit-log-stream/locals.tf
+++ terraform/environments/developer-experience/github-audit-log-stream/locals.tf
@@ -1,4 +1,6 @@
 locals {
   global_config = yamldecode(file("${path.module}/../configuration/global.yml"))
   bucket_name   = "${local.global_config.s3_bucket_prefix}-${local.environment}-${local.component_name}"
+
+  cortex_xsiam_enabled = local.is-production && var.cortex_xsiam_workload_identity != null
 }
