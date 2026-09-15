87681f26abd3afcb817e28c2e91cebc73fc4cd4b
Timi Olubusi <timiolubusi@icloud.com>
2026-09-15 10:10:28 +0100

    Engine version bump and maintenance window change

--- terraform/environments/ncas/rds.tf
+++ terraform/environments/ncas/rds.tf
@@ -23,7 +23,7 @@ resource "aws_db_instance" "ncas_db" {
   ca_cert_identifier              = "rds-ca-rsa2048-g1"
   apply_immediately               = true
   copy_tags_to_snapshot           = true
-  maintenance_window              = local.is-production ? null : "wed:20:20-wed:20:50"
+  maintenance_window              = local.is-production ? null : "wed:19:20-wed:19:50"
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 }
 
