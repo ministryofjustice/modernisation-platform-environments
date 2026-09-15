85a9086aa666b73e7ca2564c7f2e5b41b9070fb4
Annesa <annesa.mariyam@justice.gov.uk>
2026-09-15 13:49:46 +0100

    adding api server config

--- terraform/environments/london-unpaid-work/locals_ec2_instance.tf
+++ terraform/environments/london-unpaid-work/locals_ec2_instance.tf
@@ -1,41 +1,6 @@
 locals {
   ec2_instances = {
 
-#     web = {
-#       config = {
-#         availability_zone         = "eu-west-2a"
-#         ebs_volume_root_name      = "/dev/xvda"
-#         iam_resource_names_prefix = "ec2-web"
-#         instance_profile_policies = []
-#         subnet_name               = "private"
-#   }
-
-#     instance = {
-#       ami                          = data.aws_ssm_parameter.amazon_linux_2023_ami.value
-#       disable_api_termination      = false
-#       instance_type                = "t2.small"
-#       key_name                     = null
-#       metadata_options_http_tokens = "required"
-#       vpc_security_group_ids       = ["ec2-linux"]
-#   }
-
-#     ebs_volumes = {
-#       "/dev/xvda" = {
-#         size = 8
-#         type = "gp3"
-#       }
-#   }
-
-#     tags = {
-#       backup                 = "false"
-#       component              = "web"
-#       description            = "London Unpaid Work web instance"
-#       instance-access-policy = "limited"
-#       os-type                = "Linux"
-#       server-type            = "web"
-#     }
-# }
-
     web = {
       config = {
         ami_name                      = "hmpps_windows_server_2022_release_2026-09-14T16-17-24.564Z"
@@ -60,7 +25,7 @@ locals {
 
       instance = {
         disable_api_termination      = false
-        instance_type                = "t2.small"
+        instance_type                = "t3.medium"
         key_name                     = null
         metadata_options_http_tokens = "optional"
         vpc_security_group_ids       = ["ec2-windows"]
@@ -86,12 +51,48 @@ locals {
         component              = "web"
         description            = "london-unpaid-work web instance"
         instance-access-policy = "limited"
-        # os-type                = "Linux"
-        # server-type            = "nomis-web"
-        #update-ssm-agent      = "patchgroup1" # not supported on RHEL6, don't include
+        os-type                = "Windows"
+        server-type            = "web"
       }
     }
 
-    
+    api = {
+      config = {
+        ami_name                      = "hmpps_windows_server_2022_release_2026-09-14T16-17-24.564Z"
+        availability_zone             = "eu-west-2a"
+        ebs_volumes_copy_all_from_ami = false
+        iam_resource_names_prefix     = "ec2-london-unpaid-work"
+        instance_profile_policies = [
+          "EC2Default",
+          "EC2S3BucketWriteAndDeleteAccessPolicy",
+          "ImageBuilderS3BucketWriteAndDeleteAccessPolicy",
+        ]
+        subnet_name = "private"
+      }
+
+      ebs_volumes = {
+        "/dev/sda1" = {
+          size = 30
+          type = "gp3"
+        }
+      }
+
+      instance = {
+        disable_api_termination      = false
+        instance_type                = "t3.medium"
+        key_name                     = null
+        metadata_options_http_tokens = "required"
+        vpc_security_group_ids       = ["ec2-windows"]
+      }
+
+      tags = {
+        backup                 = "false"
+        component              = "api"
+        description            = "london-unpaid-work API server"
+        instance-access-policy = "limited"
+        os-type                = "Windows"
+        server-type            = "api"
+      }
+    }
   }
 }
