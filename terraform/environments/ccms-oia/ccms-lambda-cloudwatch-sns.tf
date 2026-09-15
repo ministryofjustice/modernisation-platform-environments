d00e796f8c1b0dd124499f560967887b9a091dc5
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-15 13:27:09 +0100

    fix: aws_region data deprecation fix

--- terraform/environments/ccms-oia/ccms-lambda-cloudwatch-sns.tf
+++ terraform/environments/ccms-oia/ccms-lambda-cloudwatch-sns.tf
@@ -39,7 +39,7 @@ resource "aws_iam_role_policy" "lambda_cloudwatch_sns_policy" {
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ]
-        Resource = "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.cloudwatch_sns.function_name}:*"
+        Resource = "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.cloudwatch_sns.function_name}:*"
       },
       {
         Effect = "Allow"
