685dad78303350aba9ec77c9836ac65ff1addf3c
Sandhya Buddharaju <sandhya.buddharaju@justice.gov.uk>
2026-09-16 08:32:10 +0100

    refactor(github-audit-log-stream): use shared Terraform modules

--- terraform/environments/developer-experience/github-audit-log-stream/cortex-xsiam-sqs.tf
+++ terraform/environments/developer-experience/github-audit-log-stream/cortex-xsiam-sqs.tf
@@ -1,67 +1,53 @@
-resource "aws_sqs_queue" "cortex_xsiam_dlq" {
+module "cortex_xsiam_sqs" {
   count = local.is-production ? 1 : 0
 
-  name                      = "${local.component_name}-cortex-xsiam-dlq"
-  kms_master_key_id         = module.kms_key[0].key_arn
-  message_retention_seconds = 1209600
-}
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "5.1.0"
 
-resource "aws_sqs_queue" "cortex_xsiam" {
-  count = local.is-production ? 1 : 0
+  name                = "${local.component_name}-cortex-xsiam"
+  create_dlq          = true
+  create_queue_policy = true
 
-  name                      = "${local.component_name}-cortex-xsiam"
-  kms_master_key_id         = module.kms_key[0].key_arn
-  message_retention_seconds = 1209600
-  receive_wait_time_seconds = 20
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.cortex_xsiam_dlq[0].arn
-    maxReceiveCount     = 5
-  })
-}
+  kms_master_key_id             = module.kms_key[0].key_arn
+  dlq_kms_master_key_id         = module.kms_key[0].key_arn
+  message_retention_seconds     = 1209600
+  dlq_message_retention_seconds = 1209600
+  receive_wait_time_seconds     = 20
 
-data "aws_iam_policy_document" "cortex_xsiam_sqs" {
-  count = local.is-production ? 1 : 0
+  redrive_policy = {
+    maxReceiveCount = 5
+  }
 
-  statement {
-    sid       = "AllowS3ObjectNotifications"
-    effect    = "Allow"
-    actions   = ["sqs:SendMessage"]
-    resources = [aws_sqs_queue.cortex_xsiam[0].arn]
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = [module.s3_bucket[0].s3_bucket_arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values   = [data.aws_caller_identity.current.account_id]
+  queue_policy_statements = {
+    s3_publish = {
+      sid        = "AllowS3ObjectNotifications"
+      actions    = ["sqs:SendMessage"]
+      principals = [{ type = "Service", identifiers = ["s3.amazonaws.com"] }]
+      conditions = [
+        { test = "ArnEquals", variable = "aws:SourceArn", values = [module.s3_bucket[0].s3_bucket_arn] },
+        { test = "StringEquals", variable = "aws:SourceAccount", values = [data.aws_caller_identity.current.account_id] }
+      ]
     }
   }
 }
 
-resource "aws_sqs_queue_policy" "cortex_xsiam" {
+module "cortex_xsiam_s3_notification" {
   count = local.is-production ? 1 : 0
 
-  queue_url = aws_sqs_queue.cortex_xsiam[0].id
-  policy    = data.aws_iam_policy_document.cortex_xsiam_sqs[0].json
-}
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git//modules/notification?ref=dd0c434de5e74d8864e249ee020d917b076b6e32" # v5.15.4
 
-resource "aws_s3_bucket_notification" "cortex_xsiam" {
-  count  = local.is-production ? 1 : 0
-  bucket = module.s3_bucket[0].s3_bucket_id
+  bucket                   = module.s3_bucket[0].s3_bucket_id
+  create_sqs_policy        = false
+  create_sns_policy        = false
+  create_lambda_permission = false
 
-  queue {
-    queue_arn = aws_sqs_queue.cortex_xsiam[0].arn
-    events    = ["s3:ObjectCreated:*"]
+  sqs_notifications = {
+    cortex_xsiam = {
+      queue_arn = module.cortex_xsiam_sqs[0].queue_arn
+      queue_id  = module.cortex_xsiam_sqs[0].queue_url
+      events    = ["s3:ObjectCreated:*"]
+    }
   }
-
-  depends_on = [aws_sqs_queue_policy.cortex_xsiam]
 }
