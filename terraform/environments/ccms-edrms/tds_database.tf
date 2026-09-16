3c11814041d884d5c198ffd7d5c6ff3cdc312513
Sanjay Baranwal <sanjay.baranwal@justice.gov.uk>
2026-09-16 16:30:02 +0100

    fix: adding snapshot

--- terraform/environments/ccms-edrms/tds_database.tf
+++ terraform/environments/ccms-edrms/tds_database.tf
@@ -31,7 +31,8 @@ resource "aws_db_instance" "tds_db" {
   port                                = "1521"
   kms_key_id                          = data.aws_kms_key.rds_shared.arn
   storage_encrypted                   = true
-  skip_final_snapshot                 = true
+  skip_final_snapshot                 = false
+  final_snapshot_identifier            = "${local.application_name}-${formatdate("DDMMMYYYYhhmm", timestamp())}tds-db-final-snapshot"
   iam_database_authentication_enabled = false
   vpc_security_group_ids = [
     aws_security_group.tds_db.id
