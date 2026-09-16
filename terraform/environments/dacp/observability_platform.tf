13c5091604385b008e5170b0982689391cd08451
Timi Olubusi <81569910+Tim97eng@users.noreply.github.com>
2026-09-16 13:53:04 +0100

    RST-8573: Dacp postgres engine version bump & maintenance window change (#19127)
    * RST-8573: Postgres engine version bump & maintenance window change
    
    * Prod apply and window enforcement
    
    * Checkov source SHA fix
    
    * fixing version mistake
    
    * Checkov ignore bue to false positive
    
    * Ecs policy checkov fix
    
    * Checkov ignore
    
    * Fixing deprecated aws region call
    
    * Publicly accessible removed
    
    * Revert "Publicly accessible removed"
    
    This reverts commit adfc5ec9e2f8fa45576906ac5e5c8ebf7df36c0c.
    
    * removing public accessible
    
    * Update versions.tf

--- terraform/environments/dacp/observability_platform.tf
+++ terraform/environments/dacp/observability_platform.tf
@@ -1,6 +1,6 @@
 module "observability_platform_tenant" {
-  source  = "ministryofjustice/observability-platform-tenant/aws"
-  version = "2.0.0"
+  //v2.0.0
+  source = "github.com/ministryofjustice/terraform-aws-observability-platform-tenant?ref=f69b36c79ba18976d8e500e7cf61cf407158eb61"
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-production"]
 
