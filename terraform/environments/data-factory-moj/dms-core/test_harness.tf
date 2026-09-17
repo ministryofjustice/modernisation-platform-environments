dfff0c19a1f72888b01edc94a7788d004ab5d390
Frederic Jonquieres <fjonquieres83@gmail.com>
2026-09-17 10:21:53 +0100

    Increase DMS test db storage

--- terraform/environments/data-factory-moj/dms-core/test_harness.tf
+++ terraform/environments/data-factory-moj/dms-core/test_harness.tf
@@ -10,5 +10,7 @@ module "dms_test_harness" {
   kms_key_arn    = data.aws_kms_key.general_shared.arn
   seed_image_uri = "${aws_ecr_repository.dms_seed[0].repository_url}:dms-seed-v2"
 
+  allocated_storage = 30
+
   tags = local.tags
 }
