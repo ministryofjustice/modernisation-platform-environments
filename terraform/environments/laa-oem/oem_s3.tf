e23e0ed2708c0a3acf6176e80a471641a0238614
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-18 16:37:35 +0100

    feat: add s3_logging config for shared bucket

--- terraform/environments/laa-oem/oem_s3.tf
+++ terraform/environments/laa-oem/oem_s3.tf
@@ -1,3 +1,17 @@
 resource "aws_s3_bucket" "laa_oem_shared" {
   bucket = "${local.application_name}-${local.environment}-shared"
 }
+
+resource "aws_s3_bucket_logging" "laa_oem_shared" {
+  # Bucket is managed in a separate Terraform state. We can still apply logging by
+  # referencing the bucket name directly instead of a resource reference.
+  bucket = aws_s3_bucket.laa_oem_shared.id
+
+  target_bucket = module.laa_oem_shared.bucket.id
+  target_prefix = "log/"
+  target_object_key_format {
+    partitioned_prefix {
+      partition_date_source = "EventTime"
+    }
+  }
+}
