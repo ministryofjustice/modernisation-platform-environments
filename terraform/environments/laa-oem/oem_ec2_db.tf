9faf8a98250b309cd40929f872342b52988a5c6f
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-18 15:58:22 +0100

    fix: user_data to user_data_base64

--- terraform/environments/laa-oem/oem_ec2_db.tf
+++ terraform/environments/laa-oem/oem_ec2_db.tf
@@ -18,11 +18,11 @@ resource "aws_instance" "oem_db" {
   monitoring                  = true
   subnet_id                   = data.aws_subnet.data_subnets_a.id
   user_data_replace_on_change = true
-  user_data = base64encode(templatefile("./templates/oem-user-data-db.sh", {
+  user_data_base64 = templatefile("./templates/oem-user-data-db.sh", {
     efs_fqdn = aws_efs_file_system.oem_db_efs.dns_name
     env_fqdn = "${var.networking[0].business-unit}-${local.environment}.modernisation-platform.service.justice.gov.uk"
     hostname = "laa-oem-db"
-  }))
+  })
   vpc_security_group_ids = [aws_security_group.oem_db_security_group.id]
 
   root_block_device {
