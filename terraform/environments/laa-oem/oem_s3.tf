82f59490b271cd7da4583f16e6f671cf3e45380e
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-18 16:40:23 +0100

    fix: target bucket reference fix

--- terraform/environments/laa-oem/oem_s3.tf
+++ terraform/environments/laa-oem/oem_s3.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_logging" "laa_oem_shared" {
   # referencing the bucket name directly instead of a resource reference.
   bucket = aws_s3_bucket.laa_oem_shared.id
 
-  target_bucket = module.laa_oem_shared.bucket.id
+  target_bucket = module.laa_oem_logging.bucket.id
   target_prefix = "log/"
   target_object_key_format {
     partitioned_prefix {
