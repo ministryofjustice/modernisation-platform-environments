dcbecbe2fe031be1844f7bbff8ea389674ae7171
matt heery <matt.heery@justice.gov.uk>
2026-09-18 16:34:00 +0100

    chore: remove whitespace

--- terraform/environments/electronic-monitoring-data/share_cloud_platform.tf
+++ terraform/environments/electronic-monitoring-data/share_cloud_platform.tf
@@ -483,7 +483,6 @@ data "aws_iam_policy_document" "em_data_api_permissions" {
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/datamart${local.dbt_suffix}",
       "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/data_insights_${local.dbt_suffix}",
-
     ]
   }
 
@@ -498,7 +497,6 @@ data "aws_iam_policy_document" "em_data_api_permissions" {
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:table/datamart${local.dbt_suffix}/order_dim",
       "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:table/data_insights_${local.dbt_suffix}/*",
-
     ]
   }
 }
