2efc2ed052d5dd7f20300b9b6fa9faad40f4e29f
matt heery <matt.heery@justice.gov.uk>
2026-09-19 13:54:27 +0100

    fix: remove _

--- terraform/environments/electronic-monitoring-data/share_cloud_platform.tf
+++ terraform/environments/electronic-monitoring-data/share_cloud_platform.tf
@@ -482,7 +482,7 @@ data "aws_iam_policy_document" "em_data_api_permissions" {
     ]
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/datamart${local.dbt_suffix}",
-      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/data_insights_${local.dbt_suffix}",
+      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:database/data_insights${local.dbt_suffix}",
     ]
   }
 
@@ -496,7 +496,7 @@ data "aws_iam_policy_document" "em_data_api_permissions" {
     ]
     resources = [
       "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:table/datamart${local.dbt_suffix}/order_dim",
-      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:table/data_insights_${local.dbt_suffix}/*",
+      "arn:aws:glue:${data.aws_region.current.name}:${local.env_account_id}:table/data_insights${local.dbt_suffix}/*",
     ]
   }
 }
