dd6f8701bce4631b6f9e85e1e326284082831d53
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-09-22 10:08:19 +0100

    Removing container-platform- from bucket name as in workspace already

--- terraform/environments/cloud-platform/cluster/fluent-bit-s3.tf
+++ terraform/environments/cloud-platform/cluster/fluent-bit-s3.tf
@@ -10,7 +10,7 @@
 ## the name below is a placeholder derived from the workspace environment. Tom has
 ## confirmed the -fluentbit suffix is fine.
 locals {
-  log_archive_bucket_name = "container-platform-${terraform.workspace}-fluentbit"
+  log_archive_bucket_name = "${terraform.workspace}-fluentbit"
 }
 
 resource "aws_s3_bucket" "log_archive" {
