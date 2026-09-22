336cdce0fb1b3d07d4622502b18f53c02727b347
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-22 16:52:40 +0100

    fix: logging prefix

--- terraform/environments/laa-oem/oem_s3.tf
+++ terraform/environments/laa-oem/oem_s3.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket_logging" "laa_oem_shared" {
   bucket = aws_s3_bucket.laa_oem_shared.id
 
   target_bucket = module.laa_oem_logging.bucket.id
-  target_prefix = "log/"
+  target_prefix = "s3access/${local.application_name}-${local.environment}-shared"
   target_object_key_format {
     partitioned_prefix {
       partition_date_source = "EventTime"
