da6310c94f721c1315cbd5ce9d9648c3b49da364
David Sibley <10881569+dms1981@users.noreply.github.com>
2026-09-22 08:28:25 +0100

    feat: configure hosted pickup dispatch

--- terraform/environments/integration-hub-file-transfer/modules/file-dispatch-configuration/main.tf
+++ terraform/environments/integration-hub-file-transfer/modules/file-dispatch-configuration/main.tf
@@ -10,8 +10,14 @@ locals {
             teams = null
           }
         }
-        "/example/subdirectory/" = {
-          action = null
+        "/push-to-s3-with-hosted-pickup/" = {
+          action = {
+            name = "push-to-s3-with-hosted-pickup"
+            push_to_s3_with_hosted_pickup = {
+              destination_prefix = "pickup/"
+              retention_days     = 7
+            }
+          }
           notifications = {
             email = null
             slack = null
