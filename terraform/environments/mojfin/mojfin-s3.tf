585b92da098abac7db4a2eeb7e187e5c146c7c13
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-22 16:07:48 +0100

    feat: add logging configuration for oracle rds bucket

--- terraform/environments/mojfin/mojfin-s3.tf
+++ terraform/environments/mojfin/mojfin-s3.tf
@@ -27,3 +27,13 @@ resource "aws_s3_bucket_policy" "mojfin_rds_oracle_secure_transport" {
   bucket = aws_s3_bucket.mojfin_rds_oracle.id
   policy = data.aws_iam_policy_document.mojfin_rds_oracle_secure_transport.json
 }
+
+resource "aws_s3_bucket_logging" "mojfin_rds_oracle" {
+  bucket = aws_s3_bucket.mojfin_rds_oracle.id
+  target_bucket = module.s3-bucket-logging.bucket.id
+  target_prefix = "log/"
+  target_object_key_format {
+    partitioned_prefix {
+      partition_date_source = "EventTime"
+    }
+}
