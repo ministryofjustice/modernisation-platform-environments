7405307d6208faf489b58e8ddd4c9ef49799c4f5
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-22 16:26:59 +0100

    feat: update shared bucket with log buckets config

--- terraform/environments/mojfin/s3-lambda-delivery.tf
+++ terraform/environments/mojfin/s3-lambda-delivery.tf
@@ -3,7 +3,7 @@
 # See: https://dsdmoj.atlassian.net/wiki/spaces/LDD/pages/5975606239/Build+Layered+Function+for+Lambda
 
 module "s3-bucket-shared" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=355197b5695fcce014ad838c7b586b95f9eb4988"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=81230d03816f140ae912454815ec531d7cbe2c8e" # v11.2.0
 
   bucket_name         = "${local.application_name}-${local.environment}-shared"
   versioning_enabled  = true
