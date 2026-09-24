fac9856a7e50b663d31f707870a0154979e90fe7
Thomas Tipler <thomas.tipler@necsws.com>
2026-09-24 12:59:28 +0100

    Allow management server to reach yjsm-ui

--- terraform/environments/youth-justice-app-framework/yjsm_apps_alb.tf
+++ terraform/environments/youth-justice-app-framework/yjsm_apps_alb.tf
@@ -42,3 +42,27 @@ module "yjsm_apps_alb" {
 
   enable_access_logs = true
 }
+
+# Management server -> yjsm-ui, as it could reach the UI on the EC2 host's port
+# 8400 (modules/yjsm/security-groups.tf). The ingress rule lets it into the UI
+# listener; the egress rule is needed because the management server's security
+# group doesn't allow all outbound.
+resource "aws_security_group_rule" "mgmt_to_yjsm_apps_alb_ui" {
+  type                     = "ingress"
+  from_port                = local.yjsm_internal_apps["yjsm-ui"].port
+  to_port                  = local.yjsm_internal_apps["yjsm-ui"].port
+  protocol                 = "tcp"
+  security_group_id        = module.yjsm_apps_alb.alb_security_group_id
+  source_security_group_id = module.ds.management_server_sg_id
+  description              = "Management server to yjsm-ui on the yjsm apps ALB"
+}
+
+resource "aws_security_group_rule" "yjsm_apps_alb_ui_from_mgmt_egress" {
+  type                     = "egress"
+  from_port                = local.yjsm_internal_apps["yjsm-ui"].port
+  to_port                  = local.yjsm_internal_apps["yjsm-ui"].port
+  protocol                 = "tcp"
+  security_group_id        = module.ds.management_server_sg_id
+  source_security_group_id = module.yjsm_apps_alb.alb_security_group_id
+  description              = "Management server to yjsm-ui on the yjsm apps ALB"
+}
