659f5e50fca02fc6425cddf8b2ffa1b24931245c
Nick-MoJ-Pro <nicholas.aidudo@justice.gov.uk>
2026-09-24 12:58:14 +0100

    fix: setting force deployment to true

--- terraform/environments/ccms-edrms/ec2.tf
+++ terraform/environments/ccms-edrms/ec2.tf
@@ -74,7 +74,8 @@ resource "aws_autoscaling_group" "cluster-scaling-group" {
   max_size                = 3
   min_size                = 2
   protect_from_scale_in   = true
-  
+  default_instance_warmup = 0
+
 
   launch_template {
     id      = aws_launch_template.ec2-launch-template.id
