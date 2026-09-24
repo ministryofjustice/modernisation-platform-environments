859ed1ff1b6ab7de70b4993fe4d6d52665f98fb9
Prem Basumatary <prem.basumatary@justice.gov.uk>
2026-09-24 15:08:17 +0100

    PLAT-219 vcms test db prevent from changes

--- terraform/environments/vcms/rds.tf
+++ terraform/environments/vcms/rds.tf
@@ -19,7 +19,7 @@ resource "aws_db_instance" "mariadb" {
   skip_final_snapshot    = true
   storage_encrypted      = true
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
 
