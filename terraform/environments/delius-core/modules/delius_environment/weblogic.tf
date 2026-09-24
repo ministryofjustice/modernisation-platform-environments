2891d94e1c8a76b994516ae759278b8c878f256c
dteeling-moj <david.teeling1@justice.gov.uk>
2026-09-24 10:48:11 +0100

    Weblogic: cutover prod (#19243)

--- terraform/environments/delius-core/modules/delius_environment/weblogic.tf
+++ terraform/environments/delius-core/modules/delius_environment/weblogic.tf
@@ -256,7 +256,7 @@ resource "aws_lb_listener_rule" "allowed_paths_listener_rule" {
 
 
 locals {
-  weblogic_cutover_envs = ["dev", "test", "stage", "preprod"]
+  weblogic_cutover_envs = ["dev", "test", "stage", "preprod", "prod"]
 }
 
 # Cert for Legacy URL: https://dsdmoj.atlassian.net/browse/TM-2173
