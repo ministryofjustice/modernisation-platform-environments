f573da8e828df3ba24cc6eb45ff36f7ec52e2709
nathanstephenson-moj <nathan.stephenson1@justice.gov.uk>
2026-09-24 11:35:04 +0100

    chore: drop log bucket transitions

--- terraform/environments/laa-enterprise-service-bus/s3.tf
+++ terraform/environments/laa-enterprise-service-bus/s3.tf
@@ -92,35 +92,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "access_logs" {
     abort_incomplete_multipart_upload {
       days_after_initiation = local.application_data.accounts[local.environment].s3_lifecycle_days_abort_incomplete_multipart_upload_days
     }
-
-    # Current Version Lifecycle
     expiration {
       days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_current
     }
-
-    transition {
-      days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_standard
-      storage_class = "STANDARD_IA"
-    }
-
-    transition {
-      days          = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_current_glacier
-      storage_class = "GLACIER"
-    }
-
-
-    # Noncurrent Version  LifecyclE
     noncurrent_version_expiration {
       noncurrent_days = local.application_data.accounts[local.environment].s3_lifecycle_days_expiration_noncurrent
-    }
-    noncurrent_version_transition {
-      noncurrent_days = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_standard
-      storage_class   = "STANDARD_IA"
-    }
-
-    noncurrent_version_transition {
-      noncurrent_days = local.application_data.accounts[local.environment].s3_lifecycle_days_transition_noncurrent_glacier
-      storage_class   = "GLACIER"
     }
   }
 
